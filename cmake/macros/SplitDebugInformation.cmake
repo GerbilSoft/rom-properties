8829aab9c67ce16984ed76607960de14d1f1e6c4
David Korth <gerbilsoft@gerbilsoft.com>
2016-07-20 02:27:03 -0400

    Install debug files if INSTALL_DEBUG is specified.
    Use GET_TARGET_PROPERTY() instead of GET_PROPERTY().
    
    Various other adjustments to improve debug file installation.

--- cmake/macros/SplitDebugInformation.cmake
+++ cmake/macros/SplitDebugInformation.cmake
@@ -21,9 +21,7 @@ IF(NOT MSVC)
 	ENDIF()
 ENDIF(NOT MSVC)
 
-MACRO(SPLIT_DEBUG_INFORMATION EXE_TARGET)
-# TODO: Set target property containing the split debug filename
-# to make it easier to install it.
+MACRO(SPLIT_DEBUG_INFORMATION _target)
 SET(SPLIT_OK 1)
 IF(MSVC)
 	# MSVC splits debug information by itself.
@@ -40,8 +38,9 @@ IF(SPLIT_OK)
 	# NOTE: $<TARGET_FILE:gcbanner> is preferred,
 	# but this doesn't seem to work on Ubuntu 10.04.
 	# (cmake_2.8.0-5ubuntu1_i386)
-	GET_PROPERTY(SPLITDEBUG_EXE_LOCATION TARGET ${EXE_TARGET} PROPERTY LOCATION)
-	GET_FILENAME_COMPONENT(SPLITDEBUG_EXE_DIRECTORY ${SPLITDEBUG_EXE_LOCATION} DIRECTORY)
+	GET_TARGET_PROPERTY(SPLITDEBUG_LOCATION ${_target} LOCATION)
+	GET_FILENAME_COMPONENT(SPLITDEBUG_DIRECTORY ${SPLITDEBUG_LOCATION} DIRECTORY)
+	SET(SPLITDEBUG_FILENAME "${SPLITDEBUG_DIRECTORY}/${_target}.debug")
 
 	# NOTE: objcopy --strip-debug does NOT fully
 	# strip the binary; two sections are left:
@@ -49,16 +48,20 @@ IF(SPLIT_OK)
 	# - .strtab: String table.
 	# These sections are split into the .debug file, so there's
 	# no reason to keep them in the executable.
-	ADD_CUSTOM_COMMAND(TARGET ${EXE_TARGET} POST_BUILD
+	ADD_CUSTOM_COMMAND(TARGET ${_target} POST_BUILD
 		COMMAND ${CMAKE_OBJCOPY} --only-keep-debug
-			${SPLITDEBUG_EXE_LOCATION} "${SPLITDEBUG_EXE_DIRECTORY}/${EXE_TARGET}.debug"
+			${SPLITDEBUG_LOCATION} ${SPLITDEBUG_FILENAME}
 		COMMAND ${CMAKE_STRIP}
-			${SPLITDEBUG_EXE_LOCATION}
-		COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink="${EXE_TARGET}.debug"
-			${SPLITDEBUG_EXE_LOCATION}
+			${SPLITDEBUG_LOCATION}
+		COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink="${_target}.debug"
+			${SPLITDEBUG_LOCATION}
 		)
 
-	UNSET(SPLITDEBUG_EXE_LOCATION)
-	UNSET(SPLITDEBUG_EXE_DIRECTORY)
+	# Set the target property to allow installation.
+	SET_TARGET_PROPERTIES(${_target} PROPERTIES PDB ${SPLITDEBUG_FILENAME})
+
+	UNSET(SPLITDEBUG_LOCATION)
+	UNSET(SPLITDEBUG_DIRECTORY)
+	UNSET(SPLITDEBUG_FILENAME)
 ENDIF(SPLIT_OK)
 ENDMACRO(SPLIT_DEBUG_INFORMATION)
