6deb0324988f5819ab20d8c01444a7c46da13e69
David Korth <gerbilsoft@gerbilsoft.com>
2016-08-27 14:11:49 -0400

    [libcachemgr] uvector.h: FIXME: std::is_standard_layout<>() should be working on MSVC 2015, but it isn't...

--- src/libcachemgr/uvector.h
+++ src/libcachemgr/uvector.h
@@ -91,9 +91,12 @@ namespace ao {
 template<typename Tp, typename Alloc = std::allocator<Tp> >
 class uvector : private Alloc
 {
+// FIXME: Not working on MSVC 2015...
+#ifndef _MSC_VER
 #ifndef MSVC_NO_IS_STANDARD_LAYOUT
 	static_assert(std::is_standard_layout<Tp>(), "A uvector can only hold classes with standard layout");
 #endif /* !MSVC_NO_IS_STANDARD_LAYOUT */
+#endif /* _MSC_VER */
 public:
 	/// Element type
 	typedef Tp value_type;
