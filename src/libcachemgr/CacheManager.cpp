GerbilSoft/rom-properties

commit dde442a2a872c337487042e58d32bf9540b1c158

[libcachemgr] Initial CacheManager implementation.
CacheManager provides an interface to specify a URL and a cache key.
It then checks the local cache for a matching key. If an entry is found,
it either returns the filename, or an empty string if the cached file
is 0 bytes, which indicates the file didn't exist the last time a
download was attempted.

[libromdata]
- Added _RP_CHR() for character constants, which is required due to
  older MSVC (pre-2015) not supporting Unicode character constants
  from C++ 2011.
- IRpFile: Added a write() function. This is used when caching the
  image files.

FIXME:
- Need to implement various functions in CacheManager for Windows.
- Add a "fallback" cache entry for GameTDB. GameTDB usually considers
  both GCN and Wii as "wii", but we're using separate directories. If
  a file is not found in the GCN cache directory but is found in Wii,
  then the Wii file should be used.
- Don't negatively cache results other than 404.

src/libcachemgr/CacheManager.cpp
@@ -1,0 +1,400 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libcachemgr)                      *
+ * CacheManager.cpp: Local cache manager.                                  *
+ *                                                                         *
+ * Copyright (c) 2016 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "CacheManager.hpp"
+
+#include "libromdata/TextFuncs.hpp"
+#include "libromdata/RpFile.hpp"
+using LibRomData::rp_string;
+using LibRomData::IRpFile;
+using LibRomData::RpFile;
+
+// Windows includes.
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
+#include <shlobj.h>
+#endif /* _WIN32 */
+
+// POSIX includes.
+#ifndef _WIN32
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif /* !_WIN32 */
+
+// C++ includes.
+#include <string>
+using std::string;
+
+// Windows doesn't define X_OK, W_OK, or R_OK.
+#ifndef F_OK
+#define F_OK 0
+#endif
+#ifndef X_OK
+#define X_OK 1
+#endif
+#ifndef W_OK
+#define W_OK 2
+#endif
+#ifndef R_OK
+#define R_OK 4
+#endif
+
+// TODO: DownloaderFactory?
+#ifdef _WIN32
+#define DIR_SEP_CHR8 '\\'
+#define DIR_SEP_CHR16 L'\\'
+#include "UrlmonDownloader.hpp"
+#else
+#define DIR_SEP_CHR8 '/'
+#define DIR_SEP_CHR16 L'/'
+#include "CurlDownloader.hpp"
+#endif
+
+#ifdef RP_UTF8
+#define DIR_SEP_CHR DIR_SEP_CHR8
+#endif
+#ifdef RP_UTF16
+#define DIR_SEP_CHR DIR_SEP_CHR16
+#endif
+
+namespace LibCacheMgr {
+
+CacheManager::CacheManager()
+{
+	// TODO: DownloaderFactory?
+#ifdef _WIN32
+	m_downloader = new UrlmonDownloader();
+#else
+	m_downloader = new CurlDownloader();
+#endif
+
+	// TODO: Configure this somewhere?
+	m_downloader->setMaxSize(4*1024*1024);
+}
+
+CacheManager::~CacheManager()
+{
+	delete m_downloader;
+}
+
+/** Proxy server functions. **/
+// NOTE: This is only useful for downloaders that
+// can't retrieve the system proxy server normally.
+
+/**
+ * Get the proxy server.
+ * @return Proxy server URL.
+ */
+rp_string CacheManager::proxyUrl(void) const
+{
+	return m_proxyUrl;
+}
+
+/**
+ * Set the proxy server.
+ * @param proxyUrl Proxy server URL. (Use nullptr or blank string for default settings.)
+ */
+void CacheManager::setProxyUrl(const rp_char *proxyUrl)
+{
+	if (proxyUrl) {
+		m_proxyUrl = proxyUrl;
+	} else {
+		m_proxyUrl.clear();
+	}
+}
+
+/**
+ * Set the proxy server.
+ * @param proxyUrl Proxy server URL. (Use blank string for default settings.)
+ */
+void CacheManager::setProxyUrl(const LibRomData::rp_string &proxyUrl)
+{
+	m_proxyUrl = proxyUrl;
+}
+
+/**
+ * Get the ROM Properties cache directory.
+ * @return Cache directory.
+ */
+const LibRomData::rp_string &CacheManager::cacheDir(void)
+{
+	if (!m_cacheDir.empty()) {
+		// We already got the cache directory.
+		return m_cacheDir;
+	}
+
+#ifdef _WIN32
+	// Windows: Get CSIDL_LOCAL_APPDATA.
+	// XP: C:\Documents and Settings\username\Local Settings\Application Data
+	// Vista+: C:\Users\username\AppData\Local
+	wchar_t path[MAX_PATH];
+	HRESULT hr = SHGetFolderPath(nullptr, CSIDL_LOCAL_APPDATA,
+		nullptr, SHGFP_TYPE_CURRENT, path);
+	if (hr != S_OK)
+		return m_cacheDir;
+
+	// FIXME: Only works with RP_UTF16.
+	m_cacheDir = reinterpret_cast<const char16_t*>(path);
+	if (m_cacheDir.empty())
+		return m_cacheDir;
+
+	// Add a trailing backslash if necessary.
+	if (m_cacheDir.at(m_cacheDir.size()-1) != _RP_CHR('\\'))
+		m_cacheDir += _RP_CHR('\\');
+
+	// Append "rom-properties\\cache".
+	m_cacheDir += _RP("rom-properties\\cache");
+#else
+	// Linux: Cache directory is ~/.cache/rom-properties/.
+	// TODO: Mac OS X.
+	// TODO: What if the HOME environment variable isn't set?
+	const char *home = getenv("HOME");
+	if (!home || home[0] == 0)
+		return m_cacheDir;
+	string path = home;
+
+	// Add a trailing slash if necessary.
+	if (path.at(path.size()-1) != '/')
+		path += '/';
+
+	// Append ".cache/rom-properties".
+	path += ".cache/rom-properties";
+
+	// Convert to rp_string.
+	m_cacheDir = LibRomData::utf8_to_rp_string(path);
+#endif
+
+	return m_cacheDir;
+}
+
+/**
+ * Recursively mkdir() subdirectories.
+ *
+ * The last element in the path will be ignored, so if
+ * the entire pathname is a directory, a trailing slash
+ * must be included.
+ *
+ * @param path Path to recursively mkdir. (last component is ignored)
+ * @return 0 on success; non-zero on error.
+ */
+int CacheManager::rmkdir(const LibRomData::rp_string &path)
+{
+#ifdef _WIN32
+	// TODO: Windows version.
+	return -1;
+#else
+	string path8 = LibRomData::rp_string_to_utf8(path);
+
+	// Find all slashes and ensure the directory component exists.
+	size_t slash_pos = path8.find(DIR_SEP_CHR8, 0);
+	if (slash_pos == 0) {
+		// Root is always present.
+		slash_pos = path8.find(DIR_SEP_CHR8, 1);
+	}
+
+	do {
+		// Temporarily NULL out this slash.
+		path8[slash_pos] = 0;
+
+		// Does the directory exist?
+		if (!::access(path8.c_str(), F_OK)) {
+			// Directory component exists.
+			// Put the slash back in.
+			path8[slash_pos] = DIR_SEP_CHR8;
+			slash_pos++;
+			continue;
+		}
+
+		// Attempt to create this directory.
+		int ret = mkdir(path8.c_str(), 0777);
+		if (ret != 0) {
+			// Error creating the directory.
+			return -errno;
+		}
+
+		// Put the slash back in.
+		path8[slash_pos] = DIR_SEP_CHR8;
+		slash_pos++;
+	} while ((slash_pos = path8.find(DIR_SEP_CHR8, slash_pos)) != string::npos);
+#endif
+
+	// rmkdir() succeeded.
+	return 0;
+}
+
+/**
+ * Does a file exist? [wrapper function]
+ * @param pathname Pathname.
+ * @param mode Mode.
+ * @return 0 if the file exists with the specified mode; non-zero if not.
+ */
+int CacheManager::access(const LibRomData::rp_string &pathname, int mode)
+{
+#ifdef _WIN32
+	// TODO
+	return -1;
+#else /* !_WIN32 */
+#ifdef RP_UTF16
+	string pathname8 = LibRomData::rp_string_to_utf8(pathname);
+	return ::access(pathname8.c_str(), mode);
+#endif
+#ifdef RP_UTF8
+	return ::access(pathname.c_str(), mode);
+#endif
+#endif /* _WIN32 */
+
+	// Should not get here...
+	return -1;
+}
+
+/**
+ * Get a file's size.
+ * @param filename Filename.
+ * @return Size on success; -1 on error.
+ */
+int64_t CacheManager::filesize(const LibRomData::rp_string &filename)
+{
+	int ret = -1;
+
+#ifdef _WIN32
+	// FIXME: Only works with RP_UTF16.
+	struct _stati64 buf;
+	ret = _wstati64(reinterpret_cast<const wchar_t*>(filename.c_str()), &buf);
+#else /* !_WIN32 */
+	struct stat buf;
+#ifdef RP_UTF16
+	string filename8 = LibRomData::rp_string_to_utf8(filename);
+	ret = stat(filename8.c_str(), &buf);
+#endif
+#ifdef RP_UTF8
+	ret = stat(filename.c_str(), &buf);
+#endif
+#endif /* _WIN32 */
+
+	if (ret != 0) {
+		// stat() failed.
+		ret = -errno;
+		if (ret == 0) {
+			// Something happened...
+			ret = -EINVAL;
+		}
+
+		return ret;
+	}
+
+	// Return the filesize.
+	return buf.st_size;
+}
+
+/**
+ * Download a file.
+ * @param url URL.
+ * @param cacheKey Cache key.
+ *
+ * If the file is present in the cache, the cached version
+ * will be retrieved. Otherwise, the file will be downloaded.
+ *
+ * If the file was not found on the server, or it was not found
+ * the last time it was requested, an empty string will be
+ * returned, and a zero-byte file will be stored in the cache.
+ *
+ * @return Absolute path to cached file.
+ */
+LibRomData::rp_string CacheManager::download(const LibRomData::rp_string &url, const LibRomData::rp_string &cacheKey)
+{
+	// Get the cache filename.
+	// This is the cache directory plus the cache key.
+	rp_string cache_filename = cacheDir();
+	if (cache_filename.empty())
+		return rp_string();
+	if (cache_filename.at(cache_filename.size()-1) != DIR_SEP_CHR)
+		cache_filename += DIR_SEP_CHR;
+
+	// The cache key uses slashes. This must be converted
+	// to backslashes on Windows.
+#ifdef _WIN32
+	cache_filename.reserve(cache_filename.size() + cacheKey.size());
+	for (size_t i = 0; i < cacheKey.size(); i++) {
+		if (cacheKey[i] == '/')
+			cache_filename += DIR_SEP_CHR;
+		else
+			cache_filename += cacheKey[i];
+	}
+#else
+	// Use slashes in the cache key.
+	cache_filename += cacheKey;
+#endif
+
+	// NOTE: The filename portion MUST be kept in cache_filename,
+	// since the last component is ignored by rmkdir().
+	if (rmkdir(cache_filename) != 0)
+		return rp_string();
+
+	// Check if the file already exists.
+	if (!access(cache_filename, R_OK)) {
+		// File exists.
+		// Is it larger than 0 bytes?
+		int64_t sz = filesize(cache_filename);
+		if (sz == 0) {
+			// File is 0 bytes, which indicates it didn't exist
+			// on the server.
+			return rp_string();
+		} else if (sz > 0) {
+			// File is larger than 0 bytes, which indicates
+			// it was cached successfully.
+			return cache_filename;
+		}
+	}
+
+	// TODO: Keep-alive cURL connections (one per server)?
+	m_downloader->setUrl(url);
+	int ret = m_downloader->download();
+
+	// Write the file to the cache.
+	IRpFile *file = new RpFile(cache_filename, RpFile::FM_CREATE_WRITE);
+
+	if (ret != 0) {
+		// Error downloading the file.
+		// TODO: Only keep a negative cache if it's a 404.
+		// Keep the cached file as a 0-byte file to indicate
+		// a "negative" hit, but return an empty filename.
+		file->close();
+		delete file;
+		return rp_string();
+	}
+
+	// Write the file.
+	file->write((void*)m_downloader->data(), m_downloader->dataSize());
+	file->close();
+	delete file;
+
+	// Return the cache filename.
+	return cache_filename;
+}
+
+}

src/kde/RomThumbCreator.cpp
@@ -22,8 +22,8 @@
 #include "RomThumbCreator.hpp"
 
 // libcachemgr
-#include "libcachemgr/CurlDownloader.hpp"
-using LibCacheMgr::CurlDownloader;
+#include "libcachemgr/CacheManager.hpp"
+using LibCacheMgr::CacheManager;
 
 // C includes.
 #include <unistd.h>
@@ -169,35 +169,30 @@ bool RomThumbCreator::create(const QString &path, int width, int height, QImage 
 		// TODO: Cache to disk?
 		const std::vector<RomData::ExtURL> *extURLs = romData->extURLs(RomData::IMG_EXT_MEDIA);
 		if (extURLs && !extURLs->empty()) {
-			CurlDownloader curlDL;
-			curlDL.setMaxSize(4*1024*1024);	// TODO: Configure this somewhere?
+			CacheManager cache;
 			for (std::vector<RomData::ExtURL>::const_iterator iter = extURLs->begin();
 			     iter != extURLs->end(); ++iter)
 			{
 				const RomData::ExtURL &extURL = *iter;
-				curlDL.setUrl(extURL.url);
 
 				// Check if a proxy is required for this URL.
 				// TODO: Optimizations.
 				QString proxy = KProtocolManager::proxyForUrl(QUrl(rpToQS(extURL.url)));
 				if (proxy.isEmpty() || proxy == QLatin1String("DIRECT")) {
 					// No proxy.
-					curlDL.setProxyUrl(nullptr);
+					cache.setProxyUrl(nullptr);
 				} else {
 					// Proxy is specified.
-					curlDL.setProxyUrl((const rp_char*)proxy.utf16());
+					cache.setProxyUrl((const rp_char*)proxy.utf16());
 				}
 
-				int curlRet = curlDL.download();
-				if (curlRet != 0)
+				// TODO: Have download() return the actual data and/or load the cached file.
+				rp_string cache_filename = cache.download(extURL.url, extURL.cache_key);
+				if (cache_filename.empty())
 					continue;
 
 				// Attempt to load the image.
-				// NOTE: This QByteArray is NOT a deep copy.
-				QByteArray ba = QByteArray::fromRawData(
-					reinterpret_cast<const char*>(curlDL.data()), curlDL.dataSize());
-				QBuffer buffer(&ba);
-				QImageReader imageReader(&buffer);
+				QImageReader imageReader(rpToQS(cache_filename));
 				QImage dlImg = imageReader.read();
 				if (!dlImg.isNull()) {
 					// Image downloaded successfully.

src/libcachemgr/CMakeLists.txt
@@ -8,9 +8,11 @@ CMAKE_MINIMUM_REQUIRED(VERSION 2.8.12)
 
 SET(libcachemgr_SRCS
 	IDownloader.cpp
+	CacheManager.cpp
 	)
 SET(libcachemgr_H
 	IDownloader.hpp
+	CacheManager.hpp
 	)
 
 IF(NOT WIN32)

src/libcachemgr/CacheManager.hpp
@@ -1,0 +1,123 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libcachemgr)                      *
+ * CacheManager.hpp: Local cache manager.                                  *
+ *                                                                         *
+ * Copyright (c) 2016 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBCACHEMGR_CACHEMANAGER_HPP__
+#define __ROMPROPERTIES_LIBCACHEMGR_CACHEMANAGER_HPP__
+
+#include "libromdata/config.libromdata.h"
+
+namespace LibCacheMgr {
+
+class IDownloader;
+class CacheManager
+{
+	public:
+		CacheManager();
+		~CacheManager();
+
+	private:
+		CacheManager(const CacheManager &);
+		CacheManager &operator=(const CacheManager &);
+
+	public:
+		/** Proxy server functions. **/
+		// NOTE: This is only useful for downloaders that
+		// can't retrieve the system proxy server normally.
+
+		/**
+		 * Get the proxy server.
+		 * @return Proxy server URL.
+		 */
+		LibRomData::rp_string proxyUrl(void) const;
+
+		/**
+		 * Set the proxy server.
+		 * @param proxyUrl Proxy server URL. (Use nullptr or blank string for default settings.)
+		 */
+		void setProxyUrl(const rp_char *proxyUrl);
+
+		/**
+		 * Set the proxy server.
+		 * @param proxyUrl Proxy server URL. (Use blank string for default settings.)
+		 */
+		void setProxyUrl(const LibRomData::rp_string &proxyUrl);
+
+	protected:
+		/**
+		 * Get the ROM Properties cache directory.
+		 * @return Cache directory.
+		 */
+		const LibRomData::rp_string &cacheDir(void);
+
+		/**
+		 * Recursively mkdir() subdirectories.
+		 *
+		 * The last element in the path will be ignored, so if
+		 * the entire pathname is a directory, a trailing slash
+		 * must be included.
+		 *
+		 * @param path Path to recursively mkdir. (last component is ignored)
+		 * @return 0 on success; non-zero on error.
+		 */
+		static int rmkdir(const LibRomData::rp_string &path);
+
+		/**
+		 * Does a file exist? [wrapper function]
+		 * @param pathname Pathname.
+		 * @param mode Mode.
+		 * @return 0 if the file exists with the specified mode; non-zero if not.
+		 */
+		static int access(const LibRomData::rp_string &pathname, int mode);
+
+		/**
+		 * Get a file's size.
+		 * @param filename Filename.
+		 * @return Size on success; -1 on error.
+		 */
+		static int64_t filesize(const LibRomData::rp_string &filename);
+
+	public:
+		/**
+		 * Download a file.
+		 * @param url URL.
+		 * @param cacheKey Cache key.
+		 *
+		 * If the file is present in the cache, the cached version
+		 * will be retrieved. Otherwise, the file will be downloaded.
+		 *
+		 * If the file was not found on the server, or it was not found
+		 * the last time it was requested, an empty string will be
+		 * returned, and a zero-byte file will be stored in the cache.
+		 *
+		 * @return Absolute path to cached file.
+		 */
+		LibRomData::rp_string download(const LibRomData::rp_string &url, const LibRomData::rp_string &cacheKey);
+
+	protected:
+		LibRomData::rp_string m_proxyUrl;
+		LibRomData::rp_string m_cacheDir;
+
+		IDownloader *m_downloader;
+};
+
+}
+
+#endif /* __ROMPROPERTIES_LIBCACHEMGR_CACHEMANAGER_HPP__ */

src/libromdata/IRpFile.hpp
@@ -71,6 +71,14 @@ class IRpFile
 		virtual size_t read(void *ptr, size_t size) = 0;
 
 		/**
+		 * Write data to the file.
+		 * @param ptr Output data buffer.
+		 * @param size Amount of data to read, in bytes.
+		 * @return Number of bytes written.
+		 */
+		virtual size_t write(void *ptr, size_t size) = 0;
+
+		/**
 		 * Set the file position.
 		 * @param pos File position.
 		 * @return 0 on success; -1 on error.

src/libromdata/RpFile.hpp
@@ -57,6 +57,8 @@ class RpFile : public IRpFile
 	public:
 		virtual ~RpFile();
 
+	private:
+		typedef IRpFile super;
 	public:
 		RpFile(const RpFile &);
 		RpFile &operator=(const RpFile&);
@@ -67,7 +69,7 @@ class RpFile : public IRpFile
 		 * This usually only returns false if an error occurred.
 		 * @return True if the file is open; false if it isn't.
 		 */
-		virtual bool isOpen(void) const;
+		virtual bool isOpen(void) const override;
 
 		/**
 		 * dup() the file handle.
@@ -75,12 +77,12 @@ class RpFile : public IRpFile
 		 * pointers, not actual instances of the object.
 		 * @return dup()'d file, or nullptr on error.
 		 */
-		virtual IRpFile *dup(void);
+		virtual IRpFile *dup(void) override;
 
 		/**
 		 * Close the file.
 		 */
-		virtual void close(void);
+		virtual void close(void) override;
 
 		/**
 		 * Read data from the file.
@@ -88,31 +90,39 @@ class RpFile : public IRpFile
 		 * @param size Amount of data to read, in bytes.
 		 * @return Number of bytes read.
 		 */
-		virtual size_t read(void *ptr, size_t size);
+		virtual size_t read(void *ptr, size_t size) override;
+
+		/**
+		 * Write data to the file.
+		 * @param ptr Output data buffer.
+		 * @param size Amount of data to read, in bytes.
+		 * @return Number of bytes written.
+		 */
+		virtual size_t write(void *ptr, size_t size) override;
 
 		/**
 		 * Set the file position.
 		 * @param pos File position.
 		 * @return 0 on success; -1 on error.
 		 */
-		virtual int seek(int64_t pos);
+		virtual int seek(int64_t pos) override;
 
 		/**
 		 * Seek to the beginning of the file.
 		 */
-		virtual void rewind(void);
+		virtual void rewind(void) override;
 
 		/**
 		 * Get the file size.
 		 * @return File size, or negative on error.
 		 */
-		virtual int64_t fileSize(void);
+		virtual int64_t fileSize(void) override;
 
 		/**
 		 * Get the last error.
 		 * @return Last POSIX error, or 0 if no error.
 		 */
-		virtual int lastError(void) const;
+		virtual int lastError(void) const override;
 
 	protected:
 #ifdef _WIN32

src/libromdata/RpFile_Win32.cpp
@@ -78,7 +78,7 @@ static inline int mode_to_win32(RpFile::FileMode mode, DWORD *pdwDesiredAccess, 
  * @param mode File mode.
  */
 RpFile::RpFile(const rp_char *filename, FileMode mode)
-	: IRpFile()
+	: super()
 	, m_file(INVALID_HANDLE_VALUE)
 	, m_mode(mode)
 	, m_lastError(0)
@@ -288,6 +288,33 @@ size_t RpFile::read(void *ptr, size_t size)
 	}
 
 	return bytesRead;
+}
+
+/**
+ * Write data to the file.
+ * @param ptr Output data buffer.
+ * @param size Amount of data to read, in bytes.
+ * @return Number of bytes written.
+ */
+size_t RpFile::write(void *ptr, size_t size)
+{
+	if (!m_file || !(m_mode & FM_WRITE)) {
+		// Either the file isn't open,
+		// or it's read-only.
+		m_lastError = EBADF;
+		return 0;
+	}
+
+	DWORD bytesWritten;
+	BOOL bRet = WriteFile(m_file, ptr, (DWORD)size, &bytesWritten, nullptr);
+	if (bRet == 0) {
+		// An error occurred.
+		// TODO: Convert GetLastError() to POSIX?
+		m_lastError = EIO;
+		return 0;
+	}
+
+	return bytesWritten;
 }
 
 /**

src/libromdata/RpFile_stdio.cpp
@@ -69,7 +69,7 @@ static inline const mode_str_t *mode_to_str(RpFile::FileMode mode)
  * @param mode File mode.
  */
 RpFile::RpFile(const rp_char *filename, FileMode mode)
-	: IRpFile()
+	: super()
 	, m_file(nullptr)
 	, m_mode(mode)
 	, m_lastError(0)
@@ -323,6 +323,29 @@ size_t RpFile::read(void *ptr, size_t size)
 	}
 
 	size_t ret = fread(ptr, 1, size, m_file);
+	if (ferror(m_file)) {
+		// An error occurred.
+		m_lastError = errno;
+	}
+	return ret;
+}
+
+/**
+ * Write data to the file.
+ * @param ptr Output data buffer.
+ * @param size Amount of data to read, in bytes.
+ * @return Number of bytes written.
+ */
+size_t RpFile::write(void *ptr, size_t size)
+{
+	if (!m_file || !(m_mode & FM_WRITE)) {
+		// Either the file isn't open,
+		// or it's read-only.
+		m_lastError = EBADF;
+		return 0;
+	}
+
+	size_t ret = fwrite(ptr, 1, size, m_file);
 	if (ferror(m_file)) {
 		// An error occurred.
 		m_lastError = errno;

commit afe6f6892aedb339cbdcafe6759a887f6b0b6cdc

[libcachemgr] Set the proxy URL.
This is required in order to use proxies on e.g. KDE.

This is a regression from commit dde442a2a872c337487042e58d32bf9540b1c158.
([libcachemgr] Initial CacheManager implementation.)

src/libcachemgr/CacheManager.cpp
@@ -181,6 +181,7 @@ LibRomData::rp_string CacheManager::download(const rp_string &url, const rp_stri
 
 	// TODO: Keep-alive cURL connections (one per server)?
 	m_downloader->setUrl(url);
+	m_downloader->setProxyUrl(m_proxyUrl);
 	int ret = m_downloader->download();
 
 	// Write the file to the cache.