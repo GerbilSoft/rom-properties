GerbilSoft/rom-properties

commit be93ec0cd952f990cd914db7ad7ee6e79cc6a4b8

[libromdata] DMG.cpp: Only bit 7 of the CGB flag is checked by the boot ROM.
If bit 7 is set (0x80), the game supports CGB.
If bit 6 is also set (0xC0), the game is CGB-exclusive.

src/libromdata/DMG.cpp
@@ -408,34 +408,32 @@ int DMG::loadFieldData(void)
 	len = strnlen(romHeader->title,(romHeader->cgbflag < 0x80 ? 16 : 15));
 	
 	
-	if(	(romHeader->cgbflag&0x3F) == 0 &&	// gameid is only present when cgbflag is
-		4 == strnlen(romHeader->title+11,4)){	// gameid is always 4 characters long
+	if ((romHeader->cgbflag&0x3F) == 0 &&		// gameid is only present when cgbflag is
+	    4 == strnlen(romHeader->title+11,4)){	// gameid is always 4 characters long
 		m_fields->addData_string(ascii_to_rp_string(romHeader->title,std::min(len,11)));
 		m_fields->addData_string(ascii_to_rp_string(romHeader->title+11,4));
-	}
-	else{
+	} else{
 		m_fields->addData_string(ascii_to_rp_string(romHeader->title,len));
 		m_fields->addData_string(_RP("Unknown"));
 	}
-	
+
 	// System
-	uint32_t system;
-	switch(romHeader->cgbflag){
-		default:
-			system=DMG_SYSTEM_DMG;
-			break;
-		case 0x80:
-			system=DMG_SYSTEM_DMG|DMG_SYSTEM_CGB;
-			break;
-		case 0xC0:
-			system=DMG_SYSTEM_CGB;
-			break;
+	uint32_t dmg_system;
+	if (romHeader->cgbflag & 0x80) {
+		// Game supports CGB.
+		dmg_system = DMG_SYSTEM_CGB;
+		if (!(romHeader->cgbflag & 0x40)) {
+			// Not CGB exclusive.
+			dmg_system |= DMG_SYSTEM_DMG;
+		}
 	}
-	if(romHeader->old_publisher_code==0x33 && romHeader->sgbflag==0x03){
-		system|=DMG_SYSTEM_SGB;
+
+	if (romHeader->old_publisher_code == 0x33 && romHeader->sgbflag==0x03) {
+		// Game supports SGB.
+		dmg_system |= DMG_SYSTEM_SGB;
 	}
-	m_fields->addData_bitfield(system);
-	
+	m_fields->addData_bitfield(dmg_system);
+
 	// Entrypoint
 	if(romHeader->entry[0] == 0 && romHeader->entry[1] == 0xC3){
 		// this is the "standard" way of doing the entry point

commit 9989f70c144b7657333a6619243f2d3ba236b7ee

[libromdata] DMG: Set DMG_SYSTEM_DMG if the CGB flag isn't set.
Otherwise, DMG won't be checked for ROMs that don't support CGB.

This fixes a regression from commit be93ec0cd952f990cd914db7ad7ee6e79cc6a4b8.
([libromdata] DMG.cpp: Only bit 7 of the CGB flag is checked by the boot ROM.)

src/libromdata/DMG.cpp
@@ -430,6 +430,9 @@ int DMG::loadFieldData(void)
 			// Not CGB exclusive.
 			dmg_system |= DMG_SYSTEM_DMG;
 		}
+	} else {
+		// Game does not support CGB.
+		dmg_system |= DMG_SYSTEM_DMG;
 	}
 
 	if (romHeader->old_publisher_code == 0x33 && romHeader->sgbflag==0x03) {