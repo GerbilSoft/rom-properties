1dfe1e4a44603a6d8a6edc2b82f65c1eeea36480
David Korth <gerbilsoft@gerbilsoft.com>
2016-09-02 20:10:49 -0400

    [cmake] platform.cmake: Use CMake's built-in visibility functions.
    Removed the manual CFLAG testing from platform/gcc.cmake.
    
    Other required changes:
    - Removed CMAKE_MINIMUM_REQUIRED() from everything except the
      top-level CMakeLists.txt. This interfered with setting the
      CMake policy, since each invocation reset the policies to
      match the specified minimum version.
    - Added NO_POLICY_SCOPE to the cmake/platform.cmake include
      in order to allow policy changes within platform.cmake to
      propagate to the rest of the project.

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -10,6 +10,26 @@ ENDIF()
 # Don't embed rpaths in the executables.
 SET(CMAKE_SKIP_RPATH ON)
 
+# Check for visibility symbols.
+IF(NOT CMAKE_VERSION VERSION_LESS 3.3.0)
+	# CMake 3.3: Use CMake predefined variables.
+	# NOTE: CMake 3.0-3.2 do not apply these settings
+	# to static libraries, so we have to fall back to the
+	# "deprecated" ADD_COMPILER_EXPORT_FLAGS().
+	CMAKE_POLICY(SET CMP0063 NEW)
+	SET(CMAKE_C_VISIBILITY_PRESET "hidden")
+	SET(CMAKE_CXX_VISIBILITY_PRESET "hidden")
+	SET(CMAKE_VISIBILITY_INLINES_HIDDEN ON)
+ELSE()
+	# CMake 2.x, 3.0-3.2: Use ADD_COMPILER_EXPORT_FLAGS().
+	# NOTE: 3.0+ will show a deprecation warning.
+	INCLUDE(GenerateExportHeader)
+	ADD_COMPILER_EXPORT_FLAGS(RP_COMPILER_EXPORT_FLAGS)
+	SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${RP_COMPILER_EXPORT_FLAGS}")
+	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${RP_COMPILER_EXPORT_FLAGS}")
+	UNSET(RP_COMPILER_EXPORT_FLAGS)
+ENDIF()
+
 # Common flag variables:
 # [common]
 # - RP_C_FLAGS_COMMON
