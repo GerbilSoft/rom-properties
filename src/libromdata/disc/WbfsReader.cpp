GerbilSoft/rom-properties

commit e2e6c95d48220eb4010227f2d3b5fb6d76e1c8d8

[libromdata] CisoGcnReader::read(): If !d->file, return.
The assert() isn't present in release builds.

WbfsReader.cpp: Check all asserted variables after assert() because
assert() disappears in release builds.

src/libromdata/disc/WbfsReader.cpp
@@ -458,7 +458,7 @@ size_t WbfsReader::read(void *ptr, size_t size)
 	assert(d->file != nullptr);
 	assert(d->m_wbfs != nullptr);
 	assert(d->m_wbfs_disc != nullptr);
-	if (!d->m_wbfs_disc) {
+	if (!d->file || !d->m_wbfs || d->m_wbfs_disc) {
 		d->lastError = EBADF;
 		return 0;
 	}
@@ -576,7 +576,7 @@ int WbfsReader::seek(int64_t pos)
 	assert(d->file != nullptr);
 	assert(d->m_wbfs != nullptr);
 	assert(d->m_wbfs_disc != nullptr);
-	if (!d->m_wbfs_disc) {
+	if (!d->file || !d->m_wbfs || !d->m_wbfs_disc) {
 		d->lastError = EBADF;
 		return -1;
 	}
@@ -600,7 +600,7 @@ void WbfsReader::rewind(void)
 	assert(d->file != nullptr);
 	assert(d->m_wbfs != nullptr);
 	assert(d->m_wbfs_disc != nullptr);
-	if (!d->m_wbfs_disc) {
+	if (!d->file || !d->m_wbfs || !d->m_wbfs_disc) {
 		d->lastError = EBADF;
 		return;
 	}
@@ -617,7 +617,7 @@ int64_t WbfsReader::size(void) const
 	assert(d->file != nullptr);
 	assert(d->m_wbfs != nullptr);
 	assert(d->m_wbfs_disc != nullptr);
-	if (!d->m_wbfs_disc) {
+	if (!d->file || !d->m_wbfs || !d->m_wbfs_disc) {
 		d->lastError = EBADF;
 		return -1;
 	}

src/libromdata/disc/CisoGcnReader.cpp
@@ -269,6 +269,10 @@ void CisoGcnReader::clearError(void)
 size_t CisoGcnReader::read(void *ptr, size_t size)
 {
 	assert(d->file != nullptr);
+	if (!d->file) {
+		d->lastError = EBADF;
+		return -1;
+	}
 
 	uint8_t *ptr8 = reinterpret_cast<uint8_t*>(ptr);
 	size_t ret = 0;

commit 8467e10867ea219e4f12a6b0a3358a8c43fc1867

[libromdata] WbfsReader::read(): Fix d->m_wbfs_disc check.
The incorrect check broke WBFS support.

This fixes a regression from commit e2e6c95d48220eb4010227f2d3b5fb6d76e1c8d8.
([libromdata] CisoGcnReader::read(): If !d->file, return.)

src/libromdata/disc/WbfsReader.cpp
@@ -458,7 +458,7 @@ size_t WbfsReader::read(void *ptr, size_t size)
 	assert(d->file != nullptr);
 	assert(d->m_wbfs != nullptr);
 	assert(d->m_wbfs_disc != nullptr);
-	if (!d->file || !d->m_wbfs || d->m_wbfs_disc) {
+	if (!d->file || !d->m_wbfs || !d->m_wbfs_disc) {
 		d->lastError = EBADF;
 		return 0;
 	}