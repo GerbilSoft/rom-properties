aa5c23d40662dd0952176145f383ce55d13de139
David Korth <gerbilsoft@gerbilsoft.com>
2016-10-06 21:20:20 -0400

    appveyor.bat: Explicitly set -DBUILD_TESTING=ON.
    BUILD_TESTING seems to be enabled by default on my ThinkPad X230, but it
    isn't enabled by default on AppVeyor.
    
    NOTE: I'm also explicitly setting BUILD_TESTING on travis-ci, so I guess
    it isn't automatically enabled there, either.

--- appveyor.bat
+++ appveyor.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF "%PLATFORM%" == "x64" (
-	cmake . -G "Visual Studio 14 2015 Win64"
+	cmake . -G "Visual Studio 14 2015 Win64" -DBUILD_TESTING=ON
 ) ELSE (
-	cmake . -G "Visual Studio 14 2015"
+	cmake . -G "Visual Studio 14 2015" -DBUILD_TESTING=ON
 )
