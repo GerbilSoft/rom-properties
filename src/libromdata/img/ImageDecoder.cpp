GerbilSoft/rom-properties

commit ac8e4f9c85209df3a59f2b059e300bfb5350c603

[libromdata] ImageDecoder::fromNDS_CI4(): Actually use tilesX and tilesY.
We're only reading 32x32 icons right now, but we might as well use
tilesX and tilesY so we can support other image sizes later.

src/libromdata/img/ImageDecoder.cpp
@@ -222,8 +222,8 @@ rp_image *ImageDecoder::fromNDS_CI4(int width, int height,
 	// 4 tiles wide, 4 tiles tall.
 	uint8_t tileBuf[8*8];
 
-	for (int y = 0; y < 4; y++) {
-		for (int x = 0; x < 4; x++) {
+	for (int y = 0; y < tilesY; y++) {
+		for (int x = 0; x < tilesX; x++) {
 			// Convert each tile to 8-bit color manually.
 			for (int i = 0; i < 8*8; i += 2, img_buf++) {
 				tileBuf[i+0] = (*img_buf & 0x0F);

commit b6f85badf908e8f4d110b38a244ca49c6d4ae976

[libromdata] ImageDecoder::fromNDS_CI4(): Fixed tile calculation.
NDS tiles are 8x8, not 4x4. This caused crashes on everything when
attmepting to decode a Nintendo DS icon, though thumbnail caching
masked the issue on KDE and Windows 7.

This fixes a regression from commit ac8e4f9c85209df3a59f2b059e300bfb5350c603.
([libromdata] ImageDecoder::fromNDS_CI4(): Actually use tilesX and tilesY.)

src/libromdata/img/ImageDecoder.cpp
@@ -186,13 +186,13 @@ rp_image *ImageDecoder::fromNDS_CI4(int width, int height,
 	else if (img_siz < ((width * height) / 2) || pal_siz < 0x20)
 		return nullptr;
 
-	// NDS CI4 uses 4x4 tiles.
-	if (width % 4 != 0 || height % 4 != 0)
+	// NDS CI4 uses 8x8 tiles.
+	if (width % 8 != 0 || height % 8 != 0)
 		return nullptr;
 
 	// Calculate the total number of tiles.
-	const int tilesX = (width / 4);
-	const int tilesY = (height / 4);
+	const int tilesX = (width / 8);
+	const int tilesY = (height / 8);
 
 	// Create an rp_image.
 	rp_image *img = new rp_image(width, height, rp_image::FORMAT_CI8);