GerbilSoft/rom-properties

commit 9b6307e36955c868ee48bea27683cdfe53e03ea5

[libromdata] img/: Minor cleanups; unrolled some loops.
rp_image_backend:
- tr_idx() was changed a while back, so this comment is obsolete.
- has_translucent_palette_entries(): Unrolled the loop by 2x.

RpGdiplusBackend:
- convCI8toARGB32(): Unrolled the inner loop by 2x.

src/libromdata/img/RpGdiplusBackend.cpp
@@ -626,11 +626,19 @@ Gdiplus::Bitmap *RpGdiplusBackend::convCI8toARGB32(
 	const int dest_diff = (bmpArgbData.Stride / 4) - bmpArgbData.Width;
 
 	for (UINT y = bmpArgbData.Height; y > 0; y--) {
-		for (UINT x = bmpArgbData.Width; x > 0; x--) {
+		UINT x = bmpArgbData.Width;
+		for (; x > 2; x -= 2) {
+			dest[0] = pColorPalette->Entries[src[0]];
+			dest[1] = pColorPalette->Entries[src[1]];
+			dest += 2;
+			src += 2;
+		}
+		if (x == 1) {
 			*dest = pColorPalette->Entries[*src];
 			dest++;
 			src++;
 		}
+
 		dest += dest_diff;
 		src += src_diff;
 	}

src/libromdata/img/rp_image_backend.cpp
@@ -58,7 +58,7 @@ rp_image_backend::rp_image_backend(int width, int height, rp_image::Format forma
 	, format(format)
 	, palette(nullptr)
 	, palette_len(0)
-	, tr_idx(-1) // COMMIT NOTE: Changing default from 0 to -1.
+	, tr_idx(-1)
 {
 	// Calculate the stride.
 	// NOTE: If format == FORMAT_NONE, the subclass is
@@ -103,8 +103,20 @@ bool rp_image_backend::has_translucent_palette_entries(void) const
 		return false;
 
 	const uint32_t *palette = this->palette;
-	for (int i = this->palette_len; i > 0; i--, palette++) {
-		uint8_t alpha = (*palette >> 24);
+	int i = this->palette_len;
+	for (; i > 2; i -= 2, palette += 2) {
+		const uint8_t alpha1 = (palette[0] >> 24);
+		const uint8_t alpha2 = (palette[1] >> 24);
+		if (alpha1 != 0 && alpha1 != 255) {
+			// Found an alpha value other than 0 and 255.
+			return true;
+		} else if (alpha2 != 0 && alpha2 != 255) {
+			// Found an alpha value other than 0 and 255.
+			return true;
+		}
+	}
+	if (i == 1) {
+		const uint8_t alpha = (*palette >> 24);
 		if (alpha != 0 && alpha != 255) {
 			// Found an alpha value other than 0 and 255.
 			return true;

commit 99305a9df2b64970aa60190ca7d033350dee6543

[libromdata] Fixed incorrect loop unrolls.
This resulted in the last two rows of CI8 GameCube icons being cut off.
This was most notable with Eternal Darkness. (GEDE01)

This fixes a regression from commit 9b6307e36955c868ee48bea27683cdfe53e03ea5.
([libromdata] img/: Minor cleanups; unrolled some loops.)

src/libromdata/img/RpGdiplusBackend.cpp
@@ -627,7 +627,7 @@ Gdiplus::Bitmap *RpGdiplusBackend::convCI8toARGB32(
 
 	for (UINT y = bmpArgbData.Height; y > 0; y--) {
 		UINT x = bmpArgbData.Width;
-		for (; x > 2; x -= 2) {
+		for (; x > 1; x -= 2) {
 			dest[0] = pColorPalette->Entries[src[0]];
 			dest[1] = pColorPalette->Entries[src[1]];
 			dest += 2;

src/libromdata/img/rp_image_backend.cpp
@@ -104,7 +104,7 @@ bool rp_image_backend::has_translucent_palette_entries(void) const
 
 	const uint32_t *palette = this->palette;
 	int i = this->palette_len;
-	for (; i > 2; i -= 2, palette += 2) {
+	for (; i > 1; i -= 2, palette += 2) {
 		const uint8_t alpha1 = (palette[0] >> 24);
 		const uint8_t alpha2 = (palette[1] >> 24);
 		if (alpha1 != 0 && alpha1 != 255) {