GerbilSoft/rom-properties

commit 994851d04b0749ee20a539d5776525c4ce40858a

[kde] RomDataView: Align text to the top-left.
GameCube save files usually have two lines of text for the description,
so we need to explicitly set text alignment for the description label
to top-left. Otherwise, it ends up vertically centered.

Also set the actual string label to top-left alignment for consistency.

src/kde/RomDataView.cpp
@@ -149,6 +149,7 @@ void RomDataViewPrivate::updateDisplay(void)
 			continue;
 
 		QLabel *lblDesc = new QLabel(q);
+		lblDesc->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 		lblDesc->setTextFormat(Qt::PlainText);
 		lblDesc->setText(RomDataView::tr("%1:").arg(RP2Q(desc->name)));
 
@@ -161,6 +162,7 @@ void RomDataViewPrivate::updateDisplay(void)
 			case RomFields::RFT_STRING: {
 				// String type.
 				QLabel *lblString = new QLabel(q);
+				lblString->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 				lblString->setTextFormat(Qt::PlainText);
 				lblString->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByMouse);
 				if (data->str) {

commit 993e45f102ae55f8cdd3d5989da70f920dd3a234

[kde] RomDataView: Vertically center monospaced RFT_STRING value labels.
This is a partial regression from commit 994851d04b0749ee20a539d5776525c4ce40858a.
([kde] RomDataView: Align text to the top-left.)

src/kde/RomDataView.cpp
@@ -350,6 +350,7 @@ void RomDataViewPrivate::updateDisplay(void)
 						QFont font(QLatin1String("Monospace"));
 						font.setStyleHint(QFont::TypeWriter);
 						lblString->setFont(font);
+						lblString->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
 					}
 				}
 \
@@ -434,6 +435,7 @@ void RomDataViewPrivate::updateDisplay(void)
 				const RomFields::DateTimeDesc *const dateTimeDesc = desc->date_time;
 
 				QLabel *lblDateTime = new QLabel(q);
+				lblDateTime->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 				lblDateTime->setTextFormat(Qt::PlainText);
 				lblDateTime->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByMouse);
 