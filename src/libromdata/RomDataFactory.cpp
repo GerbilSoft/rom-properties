GerbilSoft/rom-properties

commit fff60a65407d474e628f60ed7a590cd27d453e70

[libromdata] IRpFile: New virtual function filename().
The default implementation returns an empty string.
TODO: Return a const string& once this is changed to pure virtual.

RpFile: Save the filename and return it in filename().

RomDataFactory: Set info.ext to the file extension. This will be used
for some types that don't have magic numbers, but do have well-known
file extensions.

src/libromdata/RomDataFactory.cpp
@@ -71,8 +71,26 @@ RomData *RomDataFactory::getInstance(IRpFile *file, bool thumbnail)
 		return nullptr;
 	}
 
-	// TODO: File extension? (Needed for .gci)
+	// Get the file extension.
 	info.ext = nullptr;
+	rp_string filename = file->filename();
+	if (!filename.empty()) {
+		// Get the last dot position.
+		size_t dot_pos = filename.find_last_of(_RP_CHR('.'));
+		if (dot_pos != rp_string::npos) {
+			// Dot must be after the last slash.
+			// Or backslash on Windows.)
+#ifdef _WIN32
+			size_t slash_pos = filename.find_last_of(_RP("/\\"));
+#else /* !_WIN32 */
+			size_t slash_pos = filename.find_last_of(_RP('/'));
+#endif /* _WIN32 */
+			if (slash_pos == rp_string::npos || slash_pos < dot_pos) {
+				// Valid file extension.
+				info.ext = filename.c_str() + dot_pos;
+			}
+		}
+	}
 
 #define CheckRomData(sys) \
 	do { \

src/libromdata/file/IRpFile.cpp
@@ -27,6 +27,18 @@
 namespace LibRomData {
 
 /**
+ * Get the filename.
+ * @return Filename. (May be empty if the filename is not available.)
+ */
+rp_string IRpFile::filename(void) const
+{
+	// Default is no filename.
+	// TODO: Return const reference?
+	// TODO: Pure virtual function.
+	return rp_string();
+}
+
+/**
  * Get a single character (byte) from the file
  * @return Character from file, or EOF on end of file or error.
  */

src/libromdata/file/IRpFile.hpp
@@ -22,6 +22,8 @@
 #ifndef __ROMPROPERTIES_LIBROMDATA_IRPFILE_HPP__
 #define __ROMPROPERTIES_LIBROMDATA_IRPFILE_HPP__
 
+#include <config.libromdata.h>
+
 // C includes.
 #include <stdint.h>
 
@@ -117,6 +119,12 @@ class IRpFile
 		 * @return File size, or negative on error.
 		 */
 		virtual int64_t fileSize(void) = 0;
+
+		/**
+		 * Get the filename.
+		 * @return Filename. (May be empty if the filename is not available.)
+		 */
+		virtual rp_string filename(void) const;
 
 	public:
 		/** Convenience functions implemented for all IRpFile classes. **/

src/libromdata/file/RpFile.hpp
@@ -145,6 +145,12 @@ class RpFile : public IRpFile
 		 */
 		virtual int64_t fileSize(void) final;
 
+		/**
+		 * Get the filename.
+		 * @return Filename. (May be empty if the filename is not available.)
+		 */
+		virtual rp_string filename(void) const final;
+
 	protected:
 #ifdef _WIN32
 		// Win32: m_file is a HANDLE.
@@ -154,6 +160,7 @@ class RpFile : public IRpFile
 		std::shared_ptr<FILE> m_file;
 #endif
 
+		rp_string m_filename;
 		FileMode m_mode;
 		int m_lastError;
 };

src/libromdata/file/RpFile_stdio.cpp
@@ -78,6 +78,7 @@ static inline const mode_str_t *mode_to_str(RpFile::FileMode mode)
 RpFile::RpFile(const rp_char *filename, FileMode mode)
 	: super()
 	, m_file(nullptr)
+	, m_filename(filename)
 	, m_mode(mode)
 	, m_lastError(0)
 {
@@ -93,6 +94,7 @@ RpFile::RpFile(const rp_char *filename, FileMode mode)
 RpFile::RpFile(const rp_string &filename, FileMode mode)
 	: super()
 	, m_file(nullptr)
+	, m_filename(filename)
 	, m_mode(mode)
 	, m_lastError(0)
 {
@@ -160,6 +162,7 @@ RpFile::~RpFile()
 RpFile::RpFile(const RpFile &other)
 	: super()
 	, m_file(other.m_file)
+	, m_filename(other.m_filename)
 	, m_mode(other.m_mode)
 	, m_lastError(0)
 { }
@@ -172,6 +175,7 @@ RpFile::RpFile(const RpFile &other)
 RpFile &RpFile::operator=(const RpFile &other)
 {
 	m_file = other.m_file;
+	m_filename = other.m_filename;
 	m_mode = other.m_mode;
 	m_lastError = other.m_lastError;
 	return *this;
@@ -344,6 +348,15 @@ int64_t RpFile::fileSize(void)
 
 	// Return the file size.
 	return end_pos;
+}
+
+/**
+ * Get the filename.
+ * @return Filename. (May be empty if the filename is not available.)
+ */
+rp_string RpFile::filename(void) const
+{
+	return m_filename;
 }
 
 }

src/libromdata/file/win32/RpFile_win32.cpp
@@ -76,6 +76,7 @@ static inline int mode_to_win32(RpFile::FileMode mode, DWORD *pdwDesiredAccess, 
 RpFile::RpFile(const rp_char *filename, FileMode mode)
 	: super()
 	, m_file(INVALID_HANDLE_VALUE, myFile_deleter())
+	, m_filename(filename)
 	, m_mode(mode)
 	, m_lastError(0)
 {
@@ -91,6 +92,7 @@ RpFile::RpFile(const rp_char *filename, FileMode mode)
 RpFile::RpFile(const rp_string &filename, FileMode mode)
 	: super()
 	, m_file(INVALID_HANDLE_VALUE, myFile_deleter())
+	, m_filename(filename)
 	, m_mode(mode)
 	, m_lastError(0)
 {
@@ -150,6 +152,7 @@ RpFile::~RpFile()
 RpFile::RpFile(const RpFile &other)
 	: super()
 	, m_file(other.m_file)
+	, m_filename(other.m_filename)
 	, m_mode(other.m_mode)
 	, m_lastError(0)
 { }
@@ -162,6 +165,7 @@ RpFile::RpFile(const RpFile &other)
 RpFile &RpFile::operator=(const RpFile &other)
 {
 	m_file = other.m_file;
+	m_filename = other.m_filename;
 	m_mode = other.m_mode;
 	m_lastError = other.m_lastError;
 	return *this;
@@ -352,6 +356,15 @@ int64_t RpFile::fileSize(void)
 	}
 
 	return liFileSize.QuadPart;
+}
+
+/**
+ * Get the filename.
+ * @return Filename. (May be empty if the filename is not available.)
+ */
+rp_string RpFile::filename(void) const
+{
+	return m_filename;
 }
 
 }

commit 991c3a216e1bed0db0234d0d9ec261f6020dcf0b

[libromdata] RomDataFactory: Characters need _RP_CHR(), not _RP().
This fixes the Linux build.

This fixes a regression from commit fff60a65407d474e628f60ed7a590cd27d453e70.
([libromdata] IRpFile: New virtual function filename().)

src/libromdata/RomDataFactory.cpp
@@ -83,7 +83,7 @@ RomData *RomDataFactory::getInstance(IRpFile *file, bool thumbnail)
 #ifdef _WIN32
 			size_t slash_pos = filename.find_last_of(_RP("/\\"));
 #else /* !_WIN32 */
-			size_t slash_pos = filename.find_last_of(_RP('/'));
+			size_t slash_pos = filename.find_last_of(_RP_CHR('/'));
 #endif /* _WIN32 */
 			if (slash_pos == rp_string::npos || slash_pos < dot_pos) {
 				// Valid file extension.