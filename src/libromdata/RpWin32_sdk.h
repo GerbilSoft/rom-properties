1c94926a1d0c3461e1f8ce7e742f1a2eeecdfb36
David Korth <gerbilsoft@gerbilsoft.com>
2016-10-27 22:46:44 -0400

    [libromdata] RpWin32_sdk.h: Split the Windows SDK defines and includes from RpWin32.hpp.
    Use RpWin32_sdk.h in win32 and cachemgr.
    
    CMakeLists.txt: Added both RpWin32.hpp and RpWin32_sdk.h.
    RpWin32.hpp was missing for some reason.

---
+++ src/libromdata/RpWin32_sdk.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * RpWin32_sdk.h: Windows SDK defines and includes.                        *
+ *                                                                         *
+ * Copyright (c) 2009-2016 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBROMDATA_RPWIN32_SDK_H__
+#define __ROMPROPERTIES_LIBROMDATA_RPWIN32_SDK_H__
+
+#ifndef _WIN32
+#error RpWin32_sdk.h should only be included in Windows builds.
+#endif
+
+// Show a warning if one of the macros isn't defined in CMake.
+#ifndef WINVER
+#pragma message("WINVER not defined; defaulting to 0x0500.")
+#define WINVER		0x0500
+#endif
+#ifndef _WIN32_WINNT
+#pragma message("_WIN32_WINNT not defined; defaulting to 0x0500.")
+#define _WIN32_WINNT	0x0500
+#endif
+#ifndef _WIN32_IE
+#pragma message("_WIN32_IE not defined; defaulting to 0x0500.")
+#define _WIN32_IE	0x0500
+#endif
+
+// Define this symbol to get XP themes. See:
+// http://msdn.microsoft.com/library/en-us/dnwxp/html/xptheming.asp
+// for more info. Note that as of May 2006, the page says the symbols should
+// be called "SIDEBYSIDE_COMMONCONTROLS" but the headers in my SDKs in VC 6 & 7
+// don't reference that symbol. If ISOLATION_AWARE_ENABLED doesn't work for you,
+// try changing it to SIDEBYSIDE_COMMONCONTROLS
+#define ISOLATION_AWARE_ENABLED 1
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#include <windows.h>
+
+#endif /* __ROMPROPERTIES_LIBROMDATA_RPWIN32_SDK_H__ */
