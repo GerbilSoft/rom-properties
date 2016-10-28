9150cc64e6b6af9468b2f2843da9c04a7b31e471
David Korth <gerbilsoft@gerbilsoft.com>
2016-10-27 22:56:28 -0400

    Disabled a ton of Windows functionality that we're not using.
    This may improve MSVC compile times, since it doesn't have to include
    unnecessary headers anymore.

--- src/libromdata/RpWin32_sdk.h
+++ src/libromdata/RpWin32_sdk.h
@@ -48,12 +48,45 @@
 // try changing it to SIDEBYSIDE_COMMONCONTROLS
 #define ISOLATION_AWARE_ENABLED 1
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
+// Disable functionality that we aren't using.
+#define WIN32_LEAN_AND_MEAN 1
+#define NOGDICAPMASKS 1
+//#define NOVIRTUALKEYCODES 1
+//#define NOWINMESSAGES 1
+//#define NOWINSTYLES 1
+#define NOSYSMETRICS 1
+#define NOMENUS 1
+#define NOICONS 1
+#define NOKEYSTATES 1
+#define NOSYSCOMMANDS 1
+//#define NORASTEROPS 1
+//#define NOSHOWWINDOW 1
+#define NOATOM 1
+//#define NOCLIPBOARD 1
+//#define NOCOLOR 1
+//#define NOCTLMGR 1
+#define NODRAWTEXT 1
+//#define NOGDI 1
+//#define NOKERNEL 1
+//#define NONLS 1
+//#define NOMB 1
+#define NOMEMMGR 1
+//#define NOMETAFILE 1
+#define NOMINMAX 1
+//#define NOMSG 1
+#define NOOPENFILE 1
+#define NOSCROLL 1
+#define NOSERVICE 1
+//#define NOSOUND 1
+//#define NOTEXTMETRIC 1
+#define NOWH 1
+#define NOWINOFFSETS 1
+#define NOCOMM 1
+#define NOKANJI 1
+#define NOHELP 1
+#define NOPROFILER 1
+#define NODEFERWINDOWPOS 1
+#define NOMCX 1
 
 #include <windows.h>
 
