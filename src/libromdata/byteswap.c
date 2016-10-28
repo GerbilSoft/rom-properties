5a1ce486bbf36225404527496fda2903f8cf13b5
David Korth <gerbilsoft@gerbilsoft.com>
2016-10-27 23:26:32 -0400

    [libromdata] byteswap.c: Copied the CPU-independent array byteswap code from Gens/GS II.

--- src/libromdata/byteswap.c
+++ src/libromdata/byteswap.c
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * byteswap.h: Byteswapping functions.                                     *
  *                                                                         *
- * Copyright (c) 2008-2016 by David Korth.                                 *
+ * Copyright (c) 2008-2016 by David Korth                                  *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -19,79 +19,89 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#ifndef __ROMPROPERTIES_LIBROMDATA_BYTESWAP_H__
-#define __ROMPROPERTIES_LIBROMDATA_BYTESWAP_H__
+#include "byteswap.h"
 
-/* Get the system byte order. */
-#include "byteorder.h"
-#include <stdint.h>
+// C includes.
+#include <assert.h>
 
-#define __swab16(x) ((uint16_t)(((x) << 8) | ((x) >> 8)))
-
-#define __swab32(x) \
-	((uint32_t)(((x) << 24) | ((x) >> 24) | \
-		(((x) & 0x0000FF00UL) << 8) | \
-		(((x) & 0x00FF0000UL) >> 8)))
-
-#define __swab64(x) \
-	((uint64_t)(((x) << 56) | ((x) >> 56) | \
-		(((x) & 0x000000000000FF00ULL) << 40) | \
-		(((x) & 0x0000000000FF0000ULL) << 24) | \
-		(((x) & 0x00000000FF000000ULL) << 8) | \
-		(((x) & 0x000000FF00000000ULL) >> 8) | \
-		(((x) & 0x0000FF0000000000ULL) >> 24) | \
-		(((x) & 0x00FF000000000000ULL) >> 40)))
-
-#if SYS_BYTEORDER == SYS_LIL_ENDIAN
-	#define be16_to_cpu(x)	__swab16(x)
-	#define be32_to_cpu(x)	__swab32(x)
-	#define be64_to_cpu(x)	__swab64(x)
-	#define le16_to_cpu(x)	(x)
-	#define le32_to_cpu(x)	(x)
-	#define le64_to_cpu(x)	(x)
-
-	#define cpu_to_be16(x)	__swab16(x)
-	#define cpu_to_be32(x)	__swab32(x)
-	#define cpu_to_be64(x)	__swab64(x)
-	#define cpu_to_le16(x)	(x)
-	#define cpu_to_le32(x)	(x)
-	#define cpu_to_le64(x)	(x)
-#else /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
-	#define be16_to_cpu(x)	(x)
-	#define be32_to_cpu(x)	(x)
-	#define be64_to_cpu(x)	(x)
-	#define le16_to_cpu(x)	__swab16(x)
-	#define le32_to_cpu(x)	__swab32(x)
-	#define le64_to_cpu(x)	__swab64(x)
-
-	#define cpu_to_be16(x)	(x)
-	#define cpu_to_be32(x)	(x)
-	#define cpu_to_be64(x)	(x)
-	#define cpu_to_le16(x)	__swab16(x)
-	#define cpu_to_le32(x)	__swab32(x)
-	#define cpu_to_le64(x)	__swab64(x)
+#ifdef _MSC_VER
+#define inline __inline
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/**
+ * Byteswap two 16-bit WORDs in a 32-bit DWORD.
+ * @param dword DWORD containing two 16-bit WORDs.
+ * @return DWORD containing the byteswapped 16-bit WORDs.
+ */
+static inline uint32_t swap_two_16_in_32(uint32_t dword)
+{
+	uint32_t tmp1 = (dword >> 8) & 0x00FF00FF;
+	uint32_t tmp2 = (dword << 8) & 0xFF00FF00;
+	return (tmp1 | tmp2);
+}
 
 /**
  * 16-bit byteswap function.
  * @param ptr Pointer to array to swap. (MUST be 16-bit aligned!)
  * @param n Number of bytes to swap. (Must be divisible by 2; an extra odd byte will be ignored.)
  */
-void __byte_swap_16_array(uint16_t *ptr, unsigned int n);
+void __byte_swap_16_array(uint16_t *ptr, unsigned int n)
+{
+	uint32_t *dwptr;
+
+	// Verify the block is 16-bit aligned
+	// and is a multiple of 2 bytes.
+	assert(((uintptr_t)ptr & 1) == 0);
+	assert((n & 1) == 0);
+	n &= ~1;
+
+	// Check if ptr is 32-bit aligned.
+	if (((uintptr_t)ptr & 3) != 0) {
+		// Byteswap the first WORD to fix alignment.
+		*ptr = __swab16(*ptr);
+		ptr++;
+	}
+
+	// Process 8 WORDs per iteration,
+	// using 32-bit accesses.
+	dwptr = (uint32_t*)ptr;
+	for (; n >= 16; n -= 16, dwptr += 4) {
+		*(dwptr+0) = swap_two_16_in_32(*(dwptr+0));
+		*(dwptr+1) = swap_two_16_in_32(*(dwptr+1));
+		*(dwptr+2) = swap_two_16_in_32(*(dwptr+2));
+		*(dwptr+3) = swap_two_16_in_32(*(dwptr+3));
+	}
+	ptr = (uint16_t*)dwptr;
+
+	// Process remaining WORDs.
+	for (; n > 0; n -= 2, ptr++) {
+		*ptr = __swab16(*ptr);
+	}
+}
 
 /**
  * 32-bit byteswap function.
  * @param ptr Pointer to array to swap. (MUST be 32-bit aligned!)
  * @param n Number of bytes to swap. (Must be divisible by 4; extra bytes will be ignored.)
  */
-void __byte_swap_32_array(uint32_t *ptr, unsigned int n);
+void __byte_swap_32_array(uint32_t *ptr, unsigned int n)
+{
+	// Verify the block is 32-bit aligned
+	// and is a multiple of 4 bytes.
+	assert(((uintptr_t)ptr & 3) == 0);
+	assert((n & 3) == 0);
+	n &= ~3;
 
-#ifdef __cplusplus
+	// Process 4 DWORDs per iteration.
+	for (; n >= 16; n -= 16, ptr += 4) {
+		*(ptr+0) = __swab32(*(ptr+0));
+		*(ptr+1) = __swab32(*(ptr+1));
+		*(ptr+2) = __swab32(*(ptr+2));
+		*(ptr+3) = __swab32(*(ptr+3));
+	}
+
+	// Process remaining DWORDs.
+	for (; n > 0; n -= 4, ptr++) {
+		*ptr = __swab32(*ptr);
+	}
 }
-#endif
-
-#endif /* __ROMPROPERTIES_LIBROMDATA_BYTESWAP_H__ */
