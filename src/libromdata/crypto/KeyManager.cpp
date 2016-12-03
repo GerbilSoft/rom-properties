GerbilSoft/rom-properties

commit cf637f203ecd0041b6ed920402e9ed95f9978ac3

[libromdata] KeyManagerPrivate::processConfigLine(): Don't use an std::string when converting to bytes.
This reduces the size of processConfigLine() slightly:
(gcc-6.2.0, 64-bit Linux)

- Debug build:   2,973 -> 2,650; difference = -323
- Release build: 1,549 -> 1,470; difference = -79

src/libromdata/crypto/KeyManager.cpp
@@ -205,8 +205,9 @@ void KeyManagerPrivate::processConfigLine(const string &line_buf)
 		chr = end - 1;
 	}
 
-	string value(equals_pos + 1, end - 1 - equals_pos);
-	if (value.empty() || (value.size() % 2 != 0)) {
+	const char *value = equals_pos + 1;
+	int value_len = end - 1 - equals_pos;
+	if (value[0] == 0 || (value_len % 2 != 0)) {
 		// Key is either empty, or is an odd length.
 		// (Odd length means half a byte...)
 		return;
@@ -223,7 +224,7 @@ void KeyManagerPrivate::processConfigLine(const string &line_buf)
 	unsigned int vKeys_pos = vKeys_start_pos;
 	// Reserve space for half of the key string.
 	// Key string is ASCII hex, so two characters make up one byte.
-	vKeys.resize(vKeys.size() + (value.size() / 2));
+	vKeys.resize(vKeys.size() + (value_len / 2));
 
 	// ASCII to HEX lookup table.
 	// Reference: http://codereview.stackexchange.com/questions/22757/char-hex-string-to-byte-array
@@ -248,12 +249,11 @@ void KeyManagerPrivate::processConfigLine(const string &line_buf)
 		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF //F
 	};
 
-	chr = value.data();
-	for (int i = (int)value.size(); i > 0; i -= 2, vKeys_pos++, chr += 2) {
+	for (; value_len > 0; value_len -= 2, vKeys_pos++, value++, value += 2) {
 		// Process two characters at a time.
 		// Two hexadecimal digits == one byte.
-		char chr0 = ascii_to_hex[(uint8_t)chr[0]];
-		char chr1 = ascii_to_hex[(uint8_t)chr[1]];
+		char chr0 = ascii_to_hex[(uint8_t)value[0]];
+		char chr1 = ascii_to_hex[(uint8_t)value[1]];
 		if (chr0 > 0x0F || chr1 > 0x0F) {
 			// Invalid character.
 			vKeys.resize(vKeys_start_pos);

commit 8562d51c45e4673785ddf022f127ad6b7b40099d

[libromdata] KeyManagerPrivate::processConfigLine(): Fix key parsing.
value was being incremented by 3, resulting in every other nybble being
skipped. This broke Wii encryption key parsing.

This fixes a regression from commit cf637f203ecd0041b6ed920402e9ed95f9978ac3.
([libromdata] KeyManagerPrivate::processConfigLine(): Don't use an std::string when converting to bytes.)

src/libromdata/crypto/KeyManager.cpp
@@ -249,7 +249,7 @@ void KeyManagerPrivate::processConfigLine(const string &line_buf)
 		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF //F
 	};
 
-	for (; value_len > 0; value_len -= 2, vKeys_pos++, value++, value += 2) {
+	for (; value_len > 0; value_len -= 2, vKeys_pos++, value += 2) {
 		// Process two characters at a time.
 		// Two hexadecimal digits == one byte.
 		char chr0 = ascii_to_hex[(uint8_t)value[0]];