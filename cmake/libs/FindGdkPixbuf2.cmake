4ec93ab62967b9230b2ef68d358c3d3cda9c5899
David Korth <gerbilsoft@gerbilsoft.com>
2017-01-03 23:20:05 -0500

    [cmake] libs/: Added version detection for GLib2, GTK3, GdkPixbuf2, and LibNautilusExtension.
    Renamed the variables to match the case of the filenames, since CMake's
    version detection requires this.
    
    Set some preliminary defaults for GLib2, GTK3, and LibNautilusExtension.
    
    TODO: Require the correct major version?

--- cmake/libs/FindGdkPixbuf2.cmake
+++ cmake/libs/FindGdkPixbuf2.cmake
@@ -1,9 +1,9 @@
 # Find GdkPixbuf2 libraries and headers.
 # If found, the following variables will be defined:
-# - GDKPIXBUF2_FOUND: System has GdkPixbuf2.
-# - GDKPIXBUF2_INCLUDE_DIRS: GdkPixbuf2 include directories.
-# - GDKPIXBUF2_LIBRARIES: GdkPixbuf2 libraries.
-# - GDKPIXBUF2_DEFINITIONS: Compiler switches required for using GdkPixbuf2.
+# - GdkPixbuf2_FOUND: System has GdkPixbuf2.
+# - GdkPixbuf2_INCLUDE_DIRS: GdkPixbuf2 include directories.
+# - GdkPixbuf2_LIBRARIES: GdkPixbuf2 libraries.
+# - GdkPixbuf2_DEFINITIONS: Compiler switches required for using GdkPixbuf2.
 #
 # In addition, a target GdkPixbuf2::gdkpixbuf2 will be created with all of
 # these definitions.
@@ -15,42 +15,50 @@
 
 FIND_PACKAGE(PkgConfig)
 IF(PkgConfig_FOUND)
-	PKG_CHECK_MODULES(PC_GDKPIXBUF2 QUIET gdk-pixbuf-2.0)
-	SET(GDKPIXBUF2_DEFINITIONS ${PC_GDKPIXBUF2_CFLAGS_OTHER})
+	PKG_CHECK_MODULES(PC_GdkPixbuf2 QUIET gdk-pixbuf-2.0)
+	SET(GdkPixbuf2_DEFINITIONS ${PC_GdkPixbuf2_CFLAGS_OTHER})
 
-	FIND_PATH(GDKPIXBUF2_INCLUDE_DIR gdk-pixbuf/gdk-pixbuf.h
-		HINTS ${PC_GDKPIXBUF2_INCLUDEDIR} ${PC_GDKPIXBUF2_INCLUDE_DIRS})
-	FIND_LIBRARY(GDKPIXBUF2_LIBRARY NAMES gdk_pixbuf-2.0 libgdk_pixbuf-2.0
-		HINTS ${PC_GDKPIXBUF2_LIBDIR} ${PC_GDKPIXBUF2_LIBRARY_DIRS})
+	FIND_PATH(GdkPixbuf2_INCLUDE_DIR gdk-pixbuf/gdk-pixbuf.h
+		HINTS ${PC_GdkPixbuf2_INCLUDEDIR} ${PC_GdkPixbuf2_INCLUDE_DIRS})
+	FIND_LIBRARY(GdkPixbuf2_LIBRARY NAMES gdk_pixbuf-2.0 libgdk_pixbuf-2.0
+		HINTS ${PC_GdkPixbuf2_LIBDIR} ${PC_GdkPixbuf2_LIBRARY_DIRS})
 
-	# Handle the QUIETLY and REQUIRED arguments and set GDKPIXBUF2_FOUND to TRUE
+	# Version must be set before calling FPHSA.
+	SET(GdkPixbuf2_VERSION "${PC_GdkPixbuf2_VERSION}")
+	MARK_AS_ADVANCED(GdkPixbuf2_VERSION)
+
+	# Handle the QUIETLY and REQUIRED arguments and set GdkPixbuf2_FOUND to TRUE
 	# if all listed variables are TRUE.
 	INCLUDE(FindPackageHandleStandardArgs)
-	FIND_PACKAGE_HANDLE_STANDARD_ARGS(GDKPIXBUF2 DEFAULT_MSG GDKPIXBUF2_LIBRARY GDKPIXBUF2_INCLUDE_DIR)
-	MARK_AS_ADVANCED(GDKPIXBUF2_INCLUDE_DIR GDKPIXBUF2_LIBRARY)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(GdkPixbuf2
+		FOUND_VAR GdkPixbuf2_FOUND
+		REQUIRED_VARS GdkPixbuf2_LIBRARY GdkPixbuf2_INCLUDE_DIR
+		VERSION_VAR GdkPixbuf2_VERSION
+		)
+	MARK_AS_ADVANCED(GdkPixbuf2_INCLUDE_DIR GdkPixbuf2_LIBRARY)
 
-	SET(GDKPIXBUF2_LIBRARIES ${GDKPIXBUF2_LIBRARY})
-	SET(GDKPIXBUF2_INCLUDE_DIRS ${GDKPIXBUF2_INCLUDE_DIR})
+	SET(GdkPixbuf2_LIBRARIES ${GdkPixbuf2_LIBRARY})
+	SET(GdkPixbuf2_INCLUDE_DIRS ${GdkPixbuf2_INCLUDE_DIR})
 
 	# Create the library target.
-	IF(GDKPIXBUF2_FOUND)
+	IF(GdkPixbuf2_FOUND)
 		IF(NOT TARGET GdkPixbuf2::gdkpixbuf2)
 			ADD_LIBRARY(GdkPixbuf2::gdkpixbuf2 UNKNOWN IMPORTED)
 		ENDIF(NOT TARGET GdkPixbuf2::gdkpixbuf2)
 		SET_TARGET_PROPERTIES(GdkPixbuf2::gdkpixbuf2 PROPERTIES
-			IMPORTED_LOCATION "${GDKPIXBUF2_LIBRARY}")
+			IMPORTED_LOCATION "${GdkPixbuf2_LIBRARY}")
 		SET_TARGET_PROPERTIES(GdkPixbuf2::gdkpixbuf2 PROPERTIES
-			COMPILE_DEFINITIONS "${GDKPIXBUF2_DEFINITIONS}")
+			COMPILE_DEFINITIONS "${GdkPixbuf2_DEFINITIONS}")
 
 		# Add include directories from the pkgconfig's Cflags section.
-		FOREACH(CFLAG ${PC_GDKPIXBUF2_CFLAGS})
+		FOREACH(CFLAG ${PC_GdkPixbuf2_CFLAGS})
 			IF(CFLAG MATCHES "^-I")
 				STRING(SUBSTRING "${CFLAG}" 2 -1 INCDIR)
-				LIST(APPEND GDKPIXBUF2_INCLUDE_DIRS "${INCDIR}")
+				LIST(APPEND GdkPixbuf2_INCLUDE_DIRS "${INCDIR}")
 				UNSET(INCDIR)
 			ENDIF(CFLAG MATCHES "^-I")
 		ENDFOREACH()
 		SET_TARGET_PROPERTIES(GdkPixbuf2::gdkpixbuf2 PROPERTIES
-			INTERFACE_INCLUDE_DIRECTORIES "${GDKPIXBUF2_INCLUDE_DIRS}")
-	ENDIF(GDKPIXBUF2_FOUND)
+			INTERFACE_INCLUDE_DIRECTORIES "${GdkPixbuf2_INCLUDE_DIRS}")
+	ENDIF(GdkPixbuf2_FOUND)
 ENDIF(PkgConfig_FOUND)
--- cmake/libs/FindLibNautilusExtension.cmake
+++ cmake/libs/FindLibNautilusExtension.cmake
@@ -1,10 +1,10 @@
 # Find Gnome's libnautilus-extension libraries and headers.
 # If found, the following variables will be defined:
-# - LIBNAUTILUSEXTENSION_FOUND: System has ThunarX.
-# - LIBNAUTILUSEXTENSION_INCLUDE_DIRS: ThunarX include directories.
-# - LIBNAUTILUSEXTENSION_LIBRARIES: ThunarX libraries.
-# - LIBNAUTILUSEXTENSION_DEFINITIONS: Compiler switches required for using ThunarX.
-# - LIBNAUTILUSEXTENSION_EXTENSION_DIR: Extensions directory. (for installation)
+# - LibNautilusExtension_FOUND: System has ThunarX.
+# - LibNautilusExtension_INCLUDE_DIRS: ThunarX include directories.
+# - LibNautilusExtension_LIBRARIES: ThunarX libraries.
+# - LibNautilusExtension_DEFINITIONS: Compiler switches required for using ThunarX.
+# - LibNautilusExtension_EXTENSION_DIR: Extensions directory. (for installation)
 #
 # In addition, a target Gnome::libnautilus-extension will be created with all of
 # these definitions.
@@ -16,48 +16,56 @@
 
 FIND_PACKAGE(PkgConfig)
 IF(PkgConfig_FOUND)
-	PKG_CHECK_MODULES(PC_LIBNAUTILUSEXTENSION QUIET libnautilus-extension)
-	SET(LIBNAUTILUSEXTENSION_DEFINITIONS ${PC_LIBNAUTILUSEXTENSION_CFLAGS_OTHER})
+	PKG_CHECK_MODULES(PC_LibNautilusExtension QUIET libnautilus-extension)
+	SET(LibNautilusExtension_DEFINITIONS ${PC_LibNautilusExtension_CFLAGS_OTHER})
 
-	FIND_PATH(LIBNAUTILUSEXTENSION_INCLUDE_DIR libnautilus-extension/nautilus-extension-types.h
-		HINTS ${PC_LIBNAUTILUSEXTENSION_INCLUDEDIR} ${PC_LIBNAUTILUSEXTENSION_INCLUDE_DIRS})
-	FIND_LIBRARY(LIBNAUTILUSEXTENSION_LIBRARY NAMES nautilus-extension libnautilus-extension
-		HINTS ${PC_LIBNAUTILUSEXTENSION_LIBDIR} ${PC_LIBNAUTILUSEXTENSION_LIBRARY_DIRS})
+	FIND_PATH(LibNautilusExtension_INCLUDE_DIR libnautilus-extension/nautilus-extension-types.h
+		HINTS ${PC_LibNautilusExtension_INCLUDEDIR} ${PC_LibNautilusExtension_INCLUDE_DIRS})
+	FIND_LIBRARY(LibNautilusExtension_LIBRARY NAMES nautilus-extension libnautilus-extension
+		HINTS ${PC_LibNautilusExtension_LIBDIR} ${PC_LibNautilusExtension_LIBRARY_DIRS})
 
-	# Handle the QUIETLY and REQUIRED arguments and set LIBNAUTILUSEXTENSION_FOUND to TRUE
+	# Version must be set before calling FPHSA.
+	SET(LibNautilusExtension_VERSION "${PC_LibNautilusExtension_VERSION}")
+	MARK_AS_ADVANCED(LibNautilusExtension_VERSION)
+
+	# Handle the QUIETLY and REQUIRED arguments and set LibNautilusExtension_FOUND to TRUE
 	# if all listed variables are TRUE.
 	INCLUDE(FindPackageHandleStandardArgs)
-	FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBNAUTILUSEXTENSION DEFAULT_MSG LIBNAUTILUSEXTENSION_LIBRARY LIBNAUTILUSEXTENSION_INCLUDE_DIR)
-	MARK_AS_ADVANCED(LIBNAUTILUSEXTENSION_INCLUDE_DIR LIBNAUTILUSEXTENSION_LIBRARY)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibNautilusExtension
+		FOUND_VAR LibNautilusExtension_FOUND
+		REQUIRED_VARS LibNautilusExtension_LIBRARY LibNautilusExtension_INCLUDE_DIR
+		VERSION_VAR LibNautilusExtension_VERSION
+		)
+	MARK_AS_ADVANCED(LibNautilusExtension_INCLUDE_DIR LibNautilusExtension_LIBRARY)
 
-	SET(LIBNAUTILUSEXTENSION_LIBRARIES ${LIBNAUTILUSEXTENSION_LIBRARY})
-	SET(LIBNAUTILUSEXTENSION_INCLUDE_DIRS ${LIBNAUTILUSEXTENSION_INCLUDE_DIR})
+	SET(LibNautilusExtension_LIBRARIES ${LibNautilusExtension_LIBRARY})
+	SET(LibNautilusExtension_INCLUDE_DIRS ${LibNautilusExtension_INCLUDE_DIR})
 
 	# Create the library target.
-	IF(LIBNAUTILUSEXTENSION_FOUND)
+	IF(LibNautilusExtension_FOUND)
 		IF(NOT TARGET Gnome::libnautilus-extension)
 			ADD_LIBRARY(Gnome::libnautilus-extension UNKNOWN IMPORTED)
 		ENDIF(NOT TARGET Gnome::libnautilus-extension)
 		SET_TARGET_PROPERTIES(Gnome::libnautilus-extension PROPERTIES
-			IMPORTED_LOCATION "${LIBNAUTILUSEXTENSION_LIBRARY}")
+			IMPORTED_LOCATION "${LibNautilusExtension_LIBRARY}")
 		SET_TARGET_PROPERTIES(Gnome::libnautilus-extension PROPERTIES
-			COMPILE_DEFINITIONS "${LIBNAUTILUSEXTENSION_DEFINITIONS}")
+			COMPILE_DEFINITIONS "${LibNautilusExtension_DEFINITIONS}")
 
 		# Add include directories from the pkgconfig's Cflags section.
-		FOREACH(CFLAG ${PC_LIBNAUTILUSEXTENSION_CFLAGS})
+		FOREACH(CFLAG ${PC_LibNautilusExtension_CFLAGS})
 			IF(CFLAG MATCHES "^-I")
 				STRING(SUBSTRING "${CFLAG}" 2 -1 INCDIR)
-				LIST(APPEND LIBNAUTILUSEXTENSION_INCLUDE_DIRS "${INCDIR}")
+				LIST(APPEND LibNautilusExtension_INCLUDE_DIRS "${INCDIR}")
 				UNSET(INCDIR)
 			ENDIF(CFLAG MATCHES "^-I")
 		ENDFOREACH()
 		SET_TARGET_PROPERTIES(Gnome::libnautilus-extension PROPERTIES
-			INTERFACE_INCLUDE_DIRECTORIES "${LIBNAUTILUSEXTENSION_INCLUDE_DIRS}")
+			INTERFACE_INCLUDE_DIRECTORIES "${LibNautilusExtension_INCLUDE_DIRS}")
 
 		# Extensions directory.
-		PKG_GET_VARIABLE(LIBNAUTILUSEXTENSION_EXTENSION_DIR libnautilus-extension extensiondir)
-		IF(NOT LIBNAUTILUSEXTENSION_EXTENSION_DIR)
-			MESSAGE(FATAL_ERROR "LIBNAUTILUSEXTENSION_EXTENSION_DIR isn't set.")
-		ENDIF(NOT LIBNAUTILUSEXTENSION_EXTENSION_DIR)
-	ENDIF(LIBNAUTILUSEXTENSION_FOUND)
+		PKG_GET_VARIABLE(LibNautilusExtension_EXTENSION_DIR libnautilus-extension extensiondir)
+		IF(NOT LibNautilusExtension_EXTENSION_DIR)
+			MESSAGE(FATAL_ERROR "LibNautilusExtension_EXTENSION_DIR isn't set.")
+		ENDIF(NOT LibNautilusExtension_EXTENSION_DIR)
+	ENDIF(LibNautilusExtension_FOUND)
 ENDIF(PkgConfig_FOUND)
