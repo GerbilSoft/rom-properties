61b30b1c99a55966b6f9ac56e6f48aee91e19e68
Egor <DankRank@users.noreply.github.com>
2017-01-07 07:33:48 +0200

    rpcli: copyright year bump

--- src/rpcli/bmp.hpp
+++ src/rpcli/bmp.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (rpcli)                            *
  * bmp.hpp: BMP output for rp_image.                                       *
  *                                                                         *
- * Copyright (c) 2016 by Egor.                                             *
+ * Copyright (c) 2016-2017 by Egor.                                        *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
