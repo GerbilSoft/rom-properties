236aa5738cc5371c0665bad7cf209c3c6eaffd91
David Korth <gerbilsoft@gerbilsoft.com>
2017-01-08 16:00:23 -0500

    [libromdata] Renamed iNES to NES; updated to use RomDataPrivate.
    It seems the NES class barely has anything in it right now...

---
+++ src/libromdata/NES.hpp
@@ -0,0 +1,130 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * NES.hpp: Nintendo Entertainment System/Famicom ROM reader.              *
+ *                                                                         *
+ * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2017 by Egor.                                        *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBROMDATA_NES_HPP__
+#define __ROMPROPERTIES_LIBROMDATA_NES_HPP__z
+
+#include <stdint.h>
+#include <string>
+#include "TextFuncs.hpp"
+
+#include "RomData.hpp"
+
+namespace LibRomData {
+
+class NESPrivate;
+class NES : public RomData
+{
+	public:
+		/**
+		 * Read an NES ROM.
+		 *
+		 * A ROM file must be opened by the caller. The file handle
+		 * will be dup()'d and must be kept open in order to load
+		 * data from the ROM.
+		 *
+		 * To close the file, either delete this object or call close().
+		 *
+		 * @param file Open ROM file.
+		 */
+		explicit NES(IRpFile *file);
+
+	protected:
+		/**
+		 * RomData destructor is protected.
+		 * Use unref() instead.
+		 */
+		virtual ~NES() { }
+
+	private:
+		typedef RomData super;
+		friend class NESPrivate;
+		NES(const NES &other);
+		NES &operator=(const NES &other);
+
+	public:
+		/** ROM detection functions. **/
+
+		/**
+		 * Is a ROM image supported by this class?
+		 * @param info DetectInfo containing ROM detection information.
+		 * @return Class-specific system ID (>= 0) if supported; -1 if not.
+		 */
+		static int isRomSupported_static(const DetectInfo *info);
+
+		/**
+		 * Is a ROM image supported by this object?
+		 * @param info DetectInfo containing ROM detection information.
+		 * @return Class-specific system ID (>= 0) if supported; -1 if not.
+		 */
+		virtual int isRomSupported(const DetectInfo *info) const override;
+
+		/**
+		 * Get the name of the system the loaded ROM is designed for.
+		 * @param type System name type. (See the SystemName enum.)
+		 * @return System name, or nullptr if type is invalid.
+		 */
+		virtual const rp_char *systemName(uint32_t type) const override;
+
+	public:
+		/**
+		 * Get a list of all supported file extensions.
+		 * This is to be used for file type registration;
+		 * subclasses don't explicitly check the extension.
+		 *
+		 * NOTE: The extensions include the leading dot,
+		 * e.g. ".bin" instead of "bin".
+		 *
+		 * NOTE 2: The strings in the std::vector should *not*
+		 * be freed by the caller.
+		 *
+		 * @return List of all supported file extensions.
+		 */
+		static std::vector<const rp_char*> supportedFileExtensions_static(void);
+
+		/**
+		 * Get a list of all supported file extensions.
+		 * This is to be used for file type registration;
+		 * subclasses don't explicitly check the extension.
+		 *
+		 * NOTE: The extensions include the leading dot,
+		 * e.g. ".bin" instead of "bin".
+		 *
+		 * NOTE 2: The strings in the std::vector should *not*
+		 * be freed by the caller.
+		 *
+		 * @return List of all supported file extensions.
+		 */
+		virtual std::vector<const rp_char*> supportedFileExtensions(void) const override;
+
+	protected:
+		/**
+		 * Load field data.
+		 * Called by RomData::fields() if the field data hasn't been loaded yet.
+		 * @return 0 on success; negative POSIX error code on error.
+		 */
+		virtual int loadFieldData(void) override;
+};
+
+}
+
+#endif /* __ROMPROPERTIES_LIBROMDATA_NES_HPP__ */
