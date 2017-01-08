GerbilSoft/rom-properties

commit 1441226090901ec84fe230dc00dbc6b8622935fc

[rpcli] Install rpcli.
rpcli is a program, so it's in a new component called "program".

[debian] New package rom-properties-cli. This package contains rp-cli.

cmake/macros/DirInstallPaths.cmake
@@ -47,9 +47,9 @@ ELSEIF(WIN32)
 	# Win32-style install paths.
 	# Files are installed relative to root, since the
 	# program is run out of its own directory.
-	SET(DIR_INSTALL_EXE "${arch}")
-	SET(DIR_INSTALL_DLL "${arch}")
-	SET(DIR_INSTALL_LIB "${arch}")
+	SET(DIR_INSTALL_EXE ".")
+	SET(DIR_INSTALL_DLL ".")
+	SET(DIR_INSTALL_LIB ".")
 	SET(DIR_INSTALL_TRANSLATIONS "translations")
 	SET(DIR_INSTALL_DOC "doc")
 	SET(DIR_INSTALL_DOC_ROOT ".")

CMakeLists.txt
@@ -171,8 +171,9 @@ ELSEIF(WIN32)
 ENDIF()
 
 # Components.
-SET(CPACK_COMPONENTS_ALL dll debug doc)
+SET(CPACK_COMPONENTS_ALL dll program debug doc)
 SET(CPACK_COMPONENT_DLL_DISPLAY_NAME "DLLs")
+SET(CPACK_COMPONENT_PROGRAM_DISPLAY_NAME "Programs")
 SET(CPACK_COMPONENT_DEBUG_DISPLAY_NAME "Debugging Symbols")
 SET(CPACK_COMPONENT_DOC_DISPLAY_NAME "Documents")
 

debian/control
@@ -52,3 +52,13 @@ Description: ROM Properties Page shell extension
  including Nintendo GameCube and Wii.
  .
  This package contains the XFCE version.
+
+Package: rom-properties-cli
+Architecture: any
+Depends: ${shlibs:Depends}, ${misc:Depends}
+Description: ROM Properties Page shell extension
+ This shell extension provides thumbnailing and property page functionality
+ for ROM images, disc images, and save files for various game consoles,
+ including Nintendo GameCube and Wii.
+ .
+ This package contains the command line version.

debian/rom-properties-cli.docs
@@ -1,0 +1,3 @@
+README.md
+LICENSE
+doc/keys.conf.example

debian/rom-properties-cli.install
@@ -1,0 +1,1 @@
+usr/bin/rpcli

debian/rules
@@ -10,7 +10,8 @@ CMAKE_OPTIONS := \
 	-DSPLIT_DEBUG=OFF \
 	-DBUILD_KDE4=ON \
 	-DBUILD_KDE5=ON \
-	-DBUILD_XFCE=ON
+	-DBUILD_XFCE=ON \
+	-DBUILD_CLI=ON
 ifeq (,$(filter nocheck,$(DEB_BUILD_OPTIONS)))
 CMAKE_OPTIONS += -DBUILD_TESTING=ON
 endif

src/rpcli/CMakeLists.txt
@@ -81,3 +81,28 @@ TARGET_INCLUDE_DIRECTORIES(rpcli
 		$<BUILD_INTERFACE:${CMAKE_BINARY_DIR}>
 	)
 TARGET_LINK_LIBRARIES(rpcli romdata8)
+
+#################
+# Installation. #
+#################
+
+INCLUDE(DirInstallPaths)
+
+# NOTE: Don't install libraries.
+# That installs the import library, which isn't used
+# for shell extensions.
+INSTALL(TARGETS rpcli
+	RUNTIME DESTINATION "${DIR_INSTALL_EXE}"
+	LIBRARY DESTINATION "${DIR_INSTALL_DLL}"
+	#ARCHIVE DESTINATION "${DIR_INSTALL_LIB}"
+	COMPONENT "program"
+	)
+IF(INSTALL_DEBUG)
+	# FIXME: Generator expression $<TARGET_PROPERTY:${_target},PDB> didn't work with CPack-3.6.1.
+	GET_TARGET_PROPERTY(DEBUG_FILENAME rpcli PDB)
+	INSTALL(FILES "${DEBUG_FILENAME}"
+		DESTINATION "${DIR_INSTALL_EXE_DEBUG}"
+		COMPONENT "debug"
+		)
+	UNSET(DEBUG_FILENAME)
+ENDIF(INSTALL_DEBUG)

commit 92f1c38803d714a9ca599d7db738fded34eb47d6

[cmake] DirInstallPaths.cmake: Don't remove ${arch} from Win32 install paths.
${arch} is needed for subdirectories.

This fixes a regression from commit 1441226090901ec84fe230dc00dbc6b8622935fc.
([rpcli] Install rpcli.)

cmake/macros/DirInstallPaths.cmake
@@ -47,9 +47,9 @@ ELSEIF(WIN32)
 	# Win32-style install paths.
 	# Files are installed relative to root, since the
 	# program is run out of its own directory.
-	SET(DIR_INSTALL_EXE ".")
-	SET(DIR_INSTALL_DLL ".")
-	SET(DIR_INSTALL_LIB ".")
+	SET(DIR_INSTALL_EXE "${arch}")
+	SET(DIR_INSTALL_DLL "${arch}")
+	SET(DIR_INSTALL_LIB "${arch}")
 	SET(DIR_INSTALL_TRANSLATIONS "translations")
 	SET(DIR_INSTALL_DOC "doc")
 	SET(DIR_INSTALL_DOC_ROOT ".")