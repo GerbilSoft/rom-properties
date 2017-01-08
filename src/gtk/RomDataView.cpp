GerbilSoft/rom-properties

commit 76bd07172b5f55d43b2e8406d8da0e266f5143d6

Fixed some icon animation issues with certain DSiWare titles.
The first sequence token in "The Legend of Zelda: Four Swords Adventures"
is 0x0F05. This means physical frame 0x05 is used. Due to an optimization
for non-animated icons, this ended up being used as frame 0, which
resulted in the wrong frame being used.

This was spotted after APNG export was added, since the APNG was correct.

Updated all UI frontends to handle frame 0 in animated icon sequences
correctly.

IconAnimHelper: Set last_frame_number to the first frame in the sequence
when initializing.

src/gtk/RomDataView.cpp
@@ -637,19 +637,11 @@ rom_data_view_init_header_row(RomDataView *page)
 		// Get the icon.
 		const rp_image *icon = page->romData->image(RomData::IMG_INT_ICON);
 		if (icon && icon->isValid()) {
-			GdkPixbuf *pixbuf = GdkImageConv::rp_image_to_GdkPixbuf(icon);
-			if (pixbuf) {
-				gtk_image_set_from_pixbuf(GTK_IMAGE(page->imgIcon), pixbuf);
-				page->iconFrames[0] = pixbuf;
-				gtk_widget_show(page->imgIcon);
-			}
-
-			// Get the animated icon data.
-			// TODO: Skip if the first frame is nullptr?
+			// Is this an animated icon?
 			page->iconAnimData = page->romData->iconAnimData();
 			if (page->iconAnimData) {
-				// Convert the icons to QPixmaps.
-				for (int i = 1; i < page->iconAnimData->count; i++) {
+				// Convert the icons to GdkPixbuf.
+				for (int i = 0; i < page->iconAnimData->count; i++) {
 					const rp_image *const frame = page->iconAnimData->frames[i];
 					if (frame && frame->isValid()) {
 						GdkPixbuf *pixbuf = GdkImageConv::rp_image_to_GdkPixbuf(frame);
@@ -661,7 +653,24 @@ rom_data_view_init_header_row(RomDataView *page)
 
 				// Set up the IconAnimHelper.
 				page->iconAnimHelper->setIconAnimData(page->iconAnimData);
+				// Initialize the animation.
+				page->last_frame_number = iconAnimHelper.frameNumber();
+
+				// Show the first frame.
+				gtk_image_set_from_pixbuf(GTK_IMAGE(page->imgIcon),
+					page->iconFrames[page->last_frame_number]);
+				gtk_widget_show(page->imgIcon);
+
 				// Icon animation timer is set in startAnimTimer().
+			} else {
+				// Not an animated icon.
+				page->last_frame_number = 0;
+				GdkPixbuf *pixbuf = GdkImageConv::rp_image_to_GdkPixbuf(icon);
+				if (pixbuf) {
+					gtk_image_set_from_pixbuf(GTK_IMAGE(page->imgIcon), pixbuf);
+					page->iconFrames[0] = pixbuf;
+					gtk_widget_show(page->imgIcon);
+				}
 			}
 		}
 	}

src/kde/RomDataView.cpp
@@ -310,21 +310,11 @@ void RomDataViewPrivate::initHeaderRow(void)
 		// Get the icon.
 		const rp_image *icon = romData->image(RomData::IMG_INT_ICON);
 		if (icon && icon->isValid()) {
-			QImage img = rpToQImage(icon);
-			if (!img.isNull()) {
-				iconFrames[0] = imgToPixmap(img);
-				ui.lblIcon->setPixmap(iconFrames[0]);
-				ui.lblIcon->show();
-			} else {
-				ui.lblIcon->hide();
-			}
-
-			// Get the animated icon data.
-			// TODO: Skip if the first frame is nullptr?
+			// Is this an animated icon?
 			iconAnimData = romData->iconAnimData();
 			if (iconAnimData) {
 				// Convert the icons to QPixmaps.
-				for (int i = 1; i < iconAnimData->count; i++) {
+				for (int i = 0; i < iconAnimData->count; i++) {
 					const rp_image *const frame = iconAnimData->frames[i];
 					if (frame && frame->isValid()) {
 						QImage img = rpToQImage(frame);
@@ -338,7 +328,7 @@ void RomDataViewPrivate::initHeaderRow(void)
 				iconAnimHelper.setIconAnimData(iconAnimData);
 				if (iconAnimHelper.isAnimated()) {
 					// Initialize the animation.
-					last_frame_number = 0;
+					last_frame_number = iconAnimHelper.frameNumber();
 					// Create the animation timer.
 					if (!ui.tmrIconAnim) {
 						ui.tmrIconAnim = new QTimer(q);
@@ -346,6 +336,23 @@ void RomDataViewPrivate::initHeaderRow(void)
 						QObject::connect(ui.tmrIconAnim, SIGNAL(timeout()),
 								q, SLOT(tmrIconAnim_timeout()));
 					}
+				}
+
+				// Show the first frame.
+				ui.lblIcon->setPixmap(iconFrames[iconAnimHelper.frameNumber()]);
+				ui.lblIcon->show();
+
+				// Icon animation timer is set in startAnimTimer().
+			} else {
+				// Not an animated icon.
+				last_frame_number = 0;
+				QImage img = rpToQImage(icon);
+				if (!img.isNull()) {
+					iconFrames[0] = imgToPixmap(img);
+					ui.lblIcon->setPixmap(iconFrames[0]);
+					ui.lblIcon->show();
+				} else {
+					ui.lblIcon->hide();
 				}
 			}
 		}

src/libromdata/img/IconAnimHelper.cpp
@@ -116,7 +116,7 @@ void IconAnimHelper::reset(void)
 		m_seq_idx = 0;
 		m_frame = m_iconAnimData->seq_index[0];
 		m_delay = m_iconAnimData->delays[0].ms;
-		m_last_valid_frame = 0;
+		m_last_valid_frame = m_frame;
 	} else {
 		// No animation.
 		m_seq_idx = 0;

src/win32/RP_ShellPropSheetExt.cpp
@@ -585,15 +585,11 @@ void RP_ShellPropSheetExt_Private::loadImages(HWND hDlg)
 		// Get the icon.
 		const rp_image *icon = romData->image(RomData::IMG_INT_ICON);
 		if (icon && icon->isValid()) {
-			// Convert to HBITMAP using the window background color.
-			// TODO: Redo if the window background color changes.
-			hbmpIconFrames[0] = RpImageWin32::toHBITMAP(icon, gdipBgColor, szIcon, true);
-
 			// Get the animated icon data.
 			if (iconAnimData) {
 				// Convert the icons to GDI+ bitmaps.
 				// TODO: Refactor this a bit...
-				for (int i = 1; i < iconAnimData->count; i++) {
+				for (int i = 0; i < iconAnimData->count; i++) {
 					if (iconAnimData->frames[i] && iconAnimData->frames[i]->isValid()) {
 						// Convert to HBITMAP using the window background color.
 						// TODO: Redo if the window background color changes.
@@ -604,9 +600,15 @@ void RP_ShellPropSheetExt_Private::loadImages(HWND hDlg)
 				// Set up the IconAnimHelper.
 				iconAnimHelper.setIconAnimData(iconAnimData);
 				last_frame_number = iconAnimHelper.frameNumber();
+
+				// Icon animation timer is set in startAnimTimer().
 			} else {
-				// Default to frame 0.
+				// Not an animated icon.
 				last_frame_number = 0;
+
+				// Convert to HBITMAP using the window background color.
+				// TODO: Redo if the window background color changes.
+				hbmpIconFrames[0] = RpImageWin32::toHBITMAP(icon, gdipBgColor, szIcon, true);
 			}
 		}
 	}

commit 23413632af4a7adba78205431f6aa7366e2cbec6

[gtk] RomDataView: Fixed the iconAnimHelper reference.
This fixes a regression from commit 76bd07172b5f55d43b2e8406d8da0e266f5143d6.
(Fixed some icon animation issues with certain DSiWare titles.)

src/gtk/RomDataView.cpp
@@ -654,7 +654,7 @@ rom_data_view_init_header_row(RomDataView *page)
 				// Set up the IconAnimHelper.
 				page->iconAnimHelper->setIconAnimData(page->iconAnimData);
 				// Initialize the animation.
-				page->last_frame_number = iconAnimHelper.frameNumber();
+				page->last_frame_number = page->iconAnimHelper->frameNumber();
 
 				// Show the first frame.
 				gtk_image_set_from_pixbuf(GTK_IMAGE(page->imgIcon),