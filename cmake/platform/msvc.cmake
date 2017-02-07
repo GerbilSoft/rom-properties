7bf50b1411abc775586c856d0c76ac174d00c0d3
David Korth <gerbilsoft@gerbilsoft.com>
2017-02-06 22:16:02 -0500

    [cmake] /TSAWARE is only valid for EXEs, not DLLs.
    MSVC 2015 explicitly rejects /TSAWARE for DLLs.
    
    win32-gcc.cmake: I'm not sure if --tsaware is rejected here,
    but let's not set it for DLLs just to be sure.

--- cmake/platform/msvc.cmake
+++ cmake/platform/msvc.cmake
@@ -9,7 +9,8 @@ ENDIF()
 # - MSVCRT "deprecated" functions
 SET(RP_C_FLAGS_COMMON "/nologo /wd4355 /wd4482 -D_CRT_SECURE_NO_WARNINGS -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE")
 SET(RP_CXX_FLAGS_COMMON "${RP_C_FLAGS_COMMON}")
-# NOTE: -tsaware is automatically set for Windows 2000 and later. (as of at least Visual Studio .NET 2003)
+# NOTE: /TSAWARE is automatically set for Windows 2000 and later. (as of at least Visual Studio .NET 2003)
+# NOTE 2: /TSAWARE is not applicable for DLLs.
 SET(RP_EXE_LINKER_FLAGS_COMMON "/NOLOGO /DYNAMICBASE /NXCOMPAT /LARGEADDRESSAWARE")
 SET(RP_SHARED_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON}")
 SET(RP_MODULE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON}")
--- cmake/platform/win32-gcc.cmake
+++ cmake/platform/win32-gcc.cmake
@@ -35,8 +35,7 @@ IF(CMAKE_BUILD_TYPE MATCHES ^release)
 ENDIF(CMAKE_BUILD_TYPE MATCHES ^release)
 
 # Test for various linker flags.
-# NOTE: Technically, --tsaware is only valid for EXEs, not DLLs,
-# but we should mark DLLs anyway.
+# NOTE: --tsaware is only valid for EXEs, not DLLs.
 # TODO: Make static linkage a CMake option: --static-libgcc, --static-libstdc++
 FOREACH(FLAG_TEST "-Wl,--large-address-aware" "-Wl,--nxcompat" "-Wl,--tsaware")
 	# CMake doesn't like "+" characters in variable names.
@@ -46,6 +45,18 @@ FOREACH(FLAG_TEST "-Wl,--large-address-aware" "-Wl,--nxcompat" "-Wl,--tsaware")
 		SET(RP_EXE_LINKER_FLAGS_WIN32 "${RP_EXE_LINKER_FLAGS_WIN32} ${FLAG_TEST}")
 		SET(RP_SHARED_LINKER_FLAGS_WIN32 "${RP_SHARED_LINKER_FLAGS_WIN32} ${FLAG_TEST}")
 		SET(RP_MODULE_LINKER_FLAGS_WIN32 "${RP_MODULE_LINKER_FLAGS_WIN32} ${FLAG_TEST}")
+	ENDIF(LDFLAG_${FLAG_TEST_VARNAME})
+	UNSET(LDFLAG_${FLAG_TEST_VARNAME})
+	UNSET(FLAG_TEST_VARNAME)
+ENDFOREACH()
+
+# EXE-only flags.
+FOREACH(FLAG_TEST "-Wl,--tsaware")
+	# CMake doesn't like "+" characters in variable names.
+	STRING(REPLACE "+" "_" FLAG_TEST_VARNAME "${FLAG_TEST}")
+
+	IF(LDFLAG_${FLAG_TEST_VARNAME})
+		SET(RP_EXE_LINKER_FLAGS_WIN32 "${RP_EXE_LINKER_FLAGS_WIN32} ${FLAG_TEST}")
 	ENDIF(LDFLAG_${FLAG_TEST_VARNAME})
 	UNSET(LDFLAG_${FLAG_TEST_VARNAME})
 	UNSET(FLAG_TEST_VARNAME)
