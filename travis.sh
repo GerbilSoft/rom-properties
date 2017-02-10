5a70c8b88babf5da2906a28ac27a4bb30c777703
David Korth <gerbilsoft@gerbilsoft.com>
2017-02-09 20:46:55 -0500

    travis.sh, .travis.yml: Make sure the GTK+ frontends are built.
    The GNOME and XFCE frontends aren't being built for some reason, which is
    resulting in travis-ci claiming this branch is building without errors.
    The GTK+ frontend hasn't been updated for the NEW allocation method,
    so this shouldn't be happening.
    
    - Explicitly specify the BUILD options in CMake.
    - Add the build dependencies to the .yml file.

--- travis.sh
+++ travis.sh
@@ -2,9 +2,15 @@
 RET=0
 mkdir "${TRAVIS_BUILD_DIR}/build"
 cd "${TRAVIS_BUILD_DIR}/build"
+# NOTE: KF5 is not available on Ubuntu 14.04,
+# so we can't build the KDE5 plugin.
 cmake .. \
 	-DCMAKE_INSTALL_PREFIX=/usr \
-	-DBUILD_TESTING=ON\
+	-DBUILD_TESTING=ON \
+	-DBUILD_KDE4=ON \
+	-DBUILD_KDE5=OFF \
+	-DBUILD_XFCE=ON \
+	-DBUILD_GNOME=ON
 	|| exit 1
 # Build UTF-8 and UTF-16 libraries for testing purposes.
 make romdata8 romdata16 cachemgr8 cachemgr16 || RET=1
