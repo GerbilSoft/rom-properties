GerbilSoft/rom-properties

commit dffa46be7631629f62b067959ea8ef26246492c5

[libromdata] RomData: New virtual function supportedImageSizes().
This function returns a vector containing supported image sizes for
a given ImageType.

The first element is always the "default" image size. If the width/height
are 0, then the size is unknown. (Amiibo scans don't have consistent
sizes, so the size is 0x0 here.)

Each size can have a name associated with it, e.g. S/M/HQ for GameTDB,
or nullptr for no name. (For GameTDB, nullptr would be the default size.)

Added supportedImageSizes() (using the static/virtual pattern) for all
subclasses that support images.

RomData.hpp: Removed the commented-out "small" icon image, since this
will now be handled by using multiple image sizes.

src/libromdata/Amiibo.cpp
@@ -334,7 +334,7 @@ vector<const rp_char*> Amiibo::supportedFileExtensions(void) const
  */
 uint32_t Amiibo::supportedImageTypes_static(void)
 {
-       return IMGBF_EXT_MEDIA;
+	return IMGBF_EXT_MEDIA;
 }
 
 /**
@@ -343,7 +343,51 @@ uint32_t Amiibo::supportedImageTypes_static(void)
  */
 uint32_t Amiibo::supportedImageTypes(void) const
 {
-       return supportedImageTypes_static();
+	return supportedImageTypes_static();
+}
+
+/**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> Amiibo::supportedImageSizes_static(ImageType imageType)
+{
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+		// ImageType is out of range.
+		return std::vector<ImageSizeDef>();
+	}
+
+	if (imageType != IMG_EXT_MEDIA) {
+		// Only media scans are supported.
+		return std::vector<ImageSizeDef>();
+	}
+
+	// Amiibo scan sizes may vary, but there's always one.
+	static const ImageSizeDef sz_EXT_MEDIA[] = {
+		{nullptr, 0, 0},
+	};
+	return vector<ImageSizeDef>(sz_EXT_MEDIA,
+		sz_EXT_MEDIA + ARRAY_SIZE(sz_EXT_MEDIA));
+}
+
+/**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> Amiibo::supportedImageSizes(ImageType imageType) const
+{
+	return supportedImageSizes_static(imageType);
 }
 
 /**

src/libromdata/DreamcastSave.cpp
@@ -1191,6 +1191,50 @@ uint32_t DreamcastSave::supportedImageTypes(void) const
 }
 
 /**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> DreamcastSave::supportedImageSizes_static(ImageType imageType)
+{
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+		// ImageType is out of range.
+		return std::vector<ImageSizeDef>();
+	}
+
+	if (imageType != IMG_INT_ICON) {
+		// Only icons are supported.
+		return std::vector<ImageSizeDef>();
+	}
+
+	// Dreamcast save files have 32x32 icons.
+	static const ImageSizeDef sz_INT_ICON[] = {
+		{nullptr, 32, 32},
+	};
+	return vector<ImageSizeDef>(sz_INT_ICON,
+		sz_INT_ICON + ARRAY_SIZE(sz_INT_ICON));
+}
+
+/**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> DreamcastSave::supportedImageSizes(ImageType imageType) const
+{
+	return supportedImageSizes_static(imageType);
+}
+
+/**
  * Load field data.
  * Called by RomData::fields() if the field data hasn't been loaded yet.
  * @return Number of fields read on success; negative POSIX error code on error.

src/libromdata/GameCube.cpp
@@ -1203,7 +1203,75 @@ uint32_t GameCube::supportedImageTypes_static(void)
  */
 uint32_t GameCube::supportedImageTypes(void) const
 {
-       return supportedImageTypes_static();
+	return supportedImageTypes_static();
+}
+
+/**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> GameCube::supportedImageSizes_static(ImageType imageType)
+{
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+		// ImageType is out of range.
+		return std::vector<ImageSizeDef>();
+	}
+
+	switch (imageType) {
+		case IMG_INT_BANNER: {
+			static const ImageSizeDef sz_INT_BANNER[] = {
+				{nullptr, 96, 32},
+			};
+			return vector<ImageSizeDef>(sz_INT_BANNER,
+				sz_INT_BANNER + ARRAY_SIZE(sz_INT_BANNER));
+		}
+		case IMG_EXT_MEDIA: {
+			static const ImageSizeDef sz_EXT_MEDIA[] = {
+				{nullptr, 160, 160},
+			};
+			return vector<ImageSizeDef>(sz_EXT_MEDIA,
+				sz_EXT_MEDIA + ARRAY_SIZE(sz_EXT_MEDIA));
+		}
+		case IMG_EXT_COVER: {
+			static const ImageSizeDef sz_EXT_COVER[] = {
+				{nullptr, 160, 224},
+			};
+			return vector<ImageSizeDef>(sz_EXT_COVER,
+				sz_EXT_COVER + ARRAY_SIZE(sz_EXT_COVER));
+		}
+		case IMG_EXT_COVER_3D: {
+			static const ImageSizeDef sz_EXT_COVER_3D[] = {
+				{nullptr, 176, 248},
+			};
+			return vector<ImageSizeDef>(sz_EXT_COVER_3D,
+				sz_EXT_COVER_3D + ARRAY_SIZE(sz_EXT_COVER_3D));
+		}
+		default:
+			break;
+	}
+
+	// Unsupported image type.
+	return std::vector<ImageSizeDef>();
+}
+
+/**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> GameCube::supportedImageSizes(ImageType imageType) const
+{
+	return supportedImageSizes_static(imageType);
 }
 
 /**

src/libromdata/GameCubeSave.cpp
@@ -774,6 +774,60 @@ uint32_t GameCubeSave::supportedImageTypes(void) const
 }
 
 /**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> GameCubeSave::supportedImageSizes_static(ImageType imageType)
+{
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+		// ImageType is out of range.
+		return std::vector<ImageSizeDef>();
+	}
+
+	switch (imageType) {
+		case IMG_INT_ICON: {
+			static const ImageSizeDef sz_INT_ICON[] = {
+				{nullptr, 32, 32},
+			};
+			return vector<ImageSizeDef>(sz_INT_ICON,
+				sz_INT_ICON + ARRAY_SIZE(sz_INT_ICON));
+		}
+		case IMG_INT_BANNER: {
+			static const ImageSizeDef sz_INT_BANNER[] = {
+				{nullptr, 96, 32},
+			};
+			return vector<ImageSizeDef>(sz_INT_BANNER,
+				sz_INT_BANNER + ARRAY_SIZE(sz_INT_BANNER));
+		}
+		default:
+			break;
+	}
+
+	// Unsupported image type.
+	return std::vector<ImageSizeDef>();
+}
+
+/**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> GameCubeSave::supportedImageSizes(ImageType imageType) const
+{
+	return supportedImageSizes_static(imageType);
+}
+
+/**
  * Load field data.
  * Called by RomData::fields() if the field data hasn't been loaded yet.
  * @return Number of fields read on success; negative POSIX error code on error.

src/libromdata/PlayStationSave.cpp
@@ -364,6 +364,50 @@ uint32_t PlayStationSave::supportedImageTypes(void) const
 }
 
 /**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> PlayStationSave::supportedImageSizes_static(ImageType imageType)
+{
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+		// ImageType is out of range.
+		return std::vector<ImageSizeDef>();
+	}
+
+	if (imageType != IMG_INT_ICON) {
+		// Only icons are supported.
+		return std::vector<ImageSizeDef>();
+	}
+
+	// PlayStation save files have 16x16 icons.
+	static const ImageSizeDef sz_INT_ICON[] = {
+		{nullptr, 16, 16},
+	};
+	return vector<ImageSizeDef>(sz_INT_ICON,
+		sz_INT_ICON + ARRAY_SIZE(sz_INT_ICON));
+}
+
+/**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> PlayStationSave::supportedImageSizes(ImageType imageType) const
+{
+	return supportedImageSizes_static(imageType);
+}
+
+/**
  * Load field data.
  * Called by RomData::fields() if the field data hasn't been loaded yet.
  * @return Number of fields read on success; negative POSIX error code on error.

src/libromdata/RomData.cpp
@@ -341,6 +341,22 @@ uint32_t RomData::supportedImageTypes(void) const
 }
 
 /**
+ * Get a list of all available image sizes for the specified image type.
+ *
+ * The first item in the returned vector is the "default" size.
+ * If the width/height is 0, then an image exists, but the size is unknown.
+ *
+ * @param imageType Image type.
+ * @return Vector of available image sizes, or empty vector if no images are available.
+ */
+std::vector<RomData::ImageSizeDef> RomData::supportedImageSizes(ImageType imageType) const
+{
+	// No images supported by default.
+	((void)imageType);
+	return std::vector<ImageSizeDef>();
+}
+
+/**
  * Load an internal image.
  * Called by RomData::image() if the image data hasn't been loaded yet.
  * @param imageType Image type to load.
@@ -355,6 +371,7 @@ int RomData::loadInternalImage(ImageType imageType)
 	}
 
 	// No images supported by the base class.
+	((void)imageType);
 	return -ENOENT;
 }
 
@@ -372,6 +389,7 @@ uint32_t RomData::imgpf_extURL(ImageType imageType) const
 	}
 
 	// No imgpf by default.
+	((void)imageType);
 	return 0;
 }
 

src/libromdata/Amiibo.hpp
@@ -128,6 +128,28 @@ class Amiibo : public RomData
 		 */
 		virtual uint32_t supportedImageTypes(void) const override final;
 
+		/**
+		 * Get a list of all available image sizes for the specified image type.
+		 *
+		 * The first item in the returned vector is the "default" size.
+		 * If the width/height is 0, then an image exists, but the size is unknown.
+		 *
+		 * @param imageType Image type.
+		 * @return Vector of available image sizes, or empty vector if no images are available.
+		 */
+		static std::vector<RomData::ImageSizeDef> supportedImageSizes_static(ImageType imageType);
+
+		/**
+		 * Get a list of all available image sizes for the specified image type.
+		 *
+		 * The first item in the returned vector is the "default" size.
+		 * If the width/height is 0, then an image exists, but the size is unknown.
+		 *
+		 * @param imageType Image type.
+		 * @return Vector of available image sizes, or empty vector if no images are available.
+		 */
+		virtual std::vector<RomData::ImageSizeDef> supportedImageSizes(ImageType imageType) const override final;
+
 	protected:
 		/**
 		 * Load field data.

src/libromdata/DreamcastSave.hpp
@@ -146,6 +146,28 @@ class DreamcastSave : public RomData
 		 */
 		virtual uint32_t supportedImageTypes(void) const override final;
 
+		/**
+		 * Get a list of all available image sizes for the specified image type.
+		 *
+		 * The first item in the returned vector is the "default" size.
+		 * If the width/height is 0, then an image exists, but the size is unknown.
+		 *
+		 * @param imageType Image type.
+		 * @return Vector of available image sizes, or empty vector if no images are available.
+		 */
+		static std::vector<RomData::ImageSizeDef> supportedImageSizes_static(ImageType imageType);
+
+		/**
+		 * Get a list of all available image sizes for the specified image type.
+		 *
+		 * The first item in the returned vector is the "default" size.
+		 * If the width/height is 0, then an image exists, but the size is unknown.
+		 *
+		 * @param imageType Image type.
+		 * @return Vector of available image sizes, or empty vector if no images are available.
+		 */
+		virtual std::vector<RomData::ImageSizeDef> supportedImageSizes(ImageType imageType) const override final;
+
 	protected:
 		/**
 		 * Load field data.

commit 3ee8552c37844c0182294fafffcc77a56f2afda1

[libromdata] supportedImageSizes_static(): Fixed assertions for external images.
This fixes a regression from commit dffa46be7631629f62b067959ea8ef26246492c5.
([libromdata] RomData: New virtual function supportedImageSizes().)

RomData::supportedImageSizes(): Added an assert() here.

src/libromdata/Amiibo.cpp
@@ -357,8 +357,8 @@ uint32_t Amiibo::supportedImageTypes(void) const
  */
 std::vector<RomData::ImageSizeDef> Amiibo::supportedImageSizes_static(ImageType imageType)
 {
-	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
-	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_EXT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_EXT_MAX) {
 		// ImageType is out of range.
 		return std::vector<ImageSizeDef>();
 	}

src/libromdata/DreamcastSave.cpp
@@ -1201,8 +1201,8 @@ uint32_t DreamcastSave::supportedImageTypes(void) const
  */
 std::vector<RomData::ImageSizeDef> DreamcastSave::supportedImageSizes_static(ImageType imageType)
 {
-	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
-	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_EXT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_EXT_MAX) {
 		// ImageType is out of range.
 		return std::vector<ImageSizeDef>();
 	}

src/libromdata/GameCube.cpp
@@ -1217,8 +1217,8 @@ uint32_t GameCube::supportedImageTypes(void) const
  */
 std::vector<RomData::ImageSizeDef> GameCube::supportedImageSizes_static(ImageType imageType)
 {
-	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
-	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_EXT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_EXT_MAX) {
 		// ImageType is out of range.
 		return std::vector<ImageSizeDef>();
 	}

src/libromdata/GameCubeSave.cpp
@@ -784,8 +784,8 @@ uint32_t GameCubeSave::supportedImageTypes(void) const
  */
 std::vector<RomData::ImageSizeDef> GameCubeSave::supportedImageSizes_static(ImageType imageType)
 {
-	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
-	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_EXT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_EXT_MAX) {
 		// ImageType is out of range.
 		return std::vector<ImageSizeDef>();
 	}

src/libromdata/PlayStationSave.cpp
@@ -374,8 +374,8 @@ uint32_t PlayStationSave::supportedImageTypes(void) const
  */
 std::vector<RomData::ImageSizeDef> PlayStationSave::supportedImageSizes_static(ImageType imageType)
 {
-	assert(imageType >= IMG_INT_MIN && imageType <= IMG_INT_MAX);
-	if (imageType < IMG_INT_MIN || imageType > IMG_INT_MAX) {
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_EXT_MAX);
+	if (imageType < IMG_INT_MIN || imageType > IMG_EXT_MAX) {
 		// ImageType is out of range.
 		return std::vector<ImageSizeDef>();
 	}

src/libromdata/RomData.cpp
@@ -390,6 +390,8 @@ uint32_t RomData::supportedImageTypes(void) const
  */
 std::vector<RomData::ImageSizeDef> RomData::supportedImageSizes(ImageType imageType) const
 {
+	assert(imageType >= IMG_INT_MIN && imageType <= IMG_EXT_MAX);
+
 	// No images supported by default.
 	((void)imageType);
 	return std::vector<ImageSizeDef>();