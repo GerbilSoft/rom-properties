c355fe8db161cf3fd31df751096a809a53b5d48c
David Korth <gerbilsoft@gerbilsoft.com>
2017-02-26 13:53:24 -0500

    [cmake] platform.cmake: Added an option to link in setargv.obj/wsetargv.obj.
    This enables automatic wildcard expansion of argv[].
    Reference: https://msdn.microsoft.com/en-us/library/8bch7bkk.aspx
    
    NOTE: This might not work on MinGW-w64, and the arguments aren't sorted.
    Maybe we should implement our own _setargv...
    
    For information on implementing _setargv:
    - https://msdn.microsoft.com/en-us/library/zay8tzh6.aspx
    - https://msdn.microsoft.com/en-us/library/e1w828yy.aspx

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -134,14 +134,23 @@ ENDFUNCTION()
 # Set Windows entrypoint.
 # _target: Target.
 # _entrypoint: Entry point. (main, wmain, WinMain, wWinMain)
-FUNCTION(SET_WINDOWS_ENTRYPOINT _target _entrypoint)
+# _setargv: If true, link to setargv.obj/wsetargv.obj in MSVC builds.
+FUNCTION(SET_WINDOWS_ENTRYPOINT _target _entrypoint _setargv)
 IF(WIN32)
 	IF(MSVC)
 		# MSVC automatically prepends an underscore if necessary.
 		SET(ENTRY_POINT_FLAG "/ENTRY:${_entrypoint}CRTStartup")
+		IF(_setargv)
+			IF(_entrypoint MATCHES ^wmain OR _entrypoint MATCHES ^wWinMain)
+				SET(SETARGV_FLAG "wsetargv.obj")
+			ELSE()
+				SET(SETARGV_FLAG "setargv.obj")
+			ENDIF()
+		ENDIF(_setargv)
 	ELSE(MSVC)
 		# MinGW does not automatically prepend an underscore.
 		# TODO: Does ARM Windows have a leading underscore?
+		# TODO: _setargv for MinGW.
 		STRING(TOLOWER "${CMAKE_SYSTEM_PROCESSOR}" arch)
 		IF(arch MATCHES "^(i.|x)86$|^x86_64$|^amd64$")
 			IF(CMAKE_SIZEOF_VOID_P EQUAL 4)
@@ -153,13 +162,14 @@ IF(WIN32)
 			SET(ENTRY_POINT "${_entrypoint}CRTStartup")
 		ENDIF(arch MATCHES "^(i.|x)86$|^x86_64$|^amd64$")
 		SET(ENTRY_POINT_FLAG "-Wl,-e,${ENTRY_POINT}")
+		UNSET(SETARGV_FLAG)
 	ENDIF(MSVC)
 
 	GET_TARGET_PROPERTY(TARGET_LINK_FLAGS ${_target} LINK_FLAGS)
 	IF(TARGET_LINK_FLAGS)
-		SET(TARGET_LINK_FLAGS "${TARGET_LINK_FLAGS} ${ENTRY_POINT_FLAG}")
+		SET(TARGET_LINK_FLAGS "${TARGET_LINK_FLAGS} ${ENTRY_POINT_FLAG} ${SETARGV_FLAG}")
 	ELSE()
-		SET(TARGET_LINK_FLAGS "${ENTRY_POINT_FLAG}")
+		SET(TARGET_LINK_FLAGS "${ENTRY_POINT_FLAG} ${SETARGV_FLAG}")
 	ENDIF()
 	SET_TARGET_PROPERTIES(${_target} PROPERTIES LINK_FLAGS "${TARGET_LINK_FLAGS}")
 ENDIF(WIN32)
