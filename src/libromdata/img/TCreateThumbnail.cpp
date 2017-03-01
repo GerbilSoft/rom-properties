GerbilSoft/rom-properties

commit 6c23f9dc5c671ce5c903289f5871c4a438739a91

[libromdata] TCreateThumbnail: Check IMG_EXT_COVER, then IMG_EXT_MEDIA, then IMG_INT_ICON.
[win32] RP_ExtractIcon: Likewise.

TODO:
- Add a function to retrieve the "default" image type in RomData,
  which can be customized per subclass.
- Add user customization.
- RP_ExtractIcon: Use TCreateThumbnail.

src/libromdata/img/TCreateThumbnail.cpp
@@ -189,21 +189,35 @@ inline void TCreateThumbnail<ImgClass>::rescale_aspect(ImgSize &rs_size, const I
 template<typename ImgClass>
 int TCreateThumbnail<ImgClass>::getThumbnail(const RomData *romData, int req_size, ImgClass &ret_img)
 {
-	// TODO: Customize which ones are used per-system.
-	// For now, check EXT MEDIA, then INT ICON.
 	uint32_t imgbf = romData->supportedImageTypes();
 	uint32_t imgpf = 0;
 	ImgSize img_sz;
 
-	if (imgbf & RomData::IMGBF_EXT_MEDIA) {
-		// External media scan.
-		ret_img = getExternalImage(romData, RomData::IMG_EXT_MEDIA, req_size, &img_sz);
-		imgpf = romData->imgpf(RomData::IMG_EXT_MEDIA);
+	/**
+	 * TODO:
+	 * - Add a function to retrieve the "default" image type in RomData,
+	 *   which can be customized per subclass.
+	 * - Add user customization.
+	 */
+
+	// Check for external images.
+	if (imgbf & RomData::IMGBF_EXT_COVER) {
+		// External cover scan.
+		ret_img = getExternalImage(romData, RomData::IMG_EXT_COVER, req_size, &img_sz);
+		imgpf = romData->imgpf(RomData::IMG_EXT_COVER);
+	}
+	if (!isImgClassValid(ret_img)) {
+		// No cover scan. Try external media scan.
+		if (imgbf & RomData::IMGBF_EXT_MEDIA) {
+			// External media scan.
+			ret_img = getExternalImage(romData, RomData::IMG_EXT_COVER, req_size, &img_sz);
+			imgpf = romData->imgpf(RomData::IMG_EXT_MEDIA);
+		}
 	}
 
 	if (!isImgClassValid(ret_img)) {
 		// No external media scan.
-		// Try an internal image.
+		// Try the internal icon.
 		if (imgbf & RomData::IMGBF_INT_ICON) {
 			// Internal icon.
 			ret_img = getInternalImage(romData, RomData::IMG_INT_ICON, &img_sz);

src/win32/RP_ExtractIcon.cpp
@@ -197,6 +197,8 @@ IFACEMETHODIMP RP_ExtractIcon::GetIconLocation(UINT uFlags,
 IFACEMETHODIMP RP_ExtractIcon::Extract(LPCWSTR pszFile, UINT nIconIndex,
 	HICON *phiconLarge, HICON *phiconSmall, UINT nIconSize)
 {
+	// TODO: Use TCreateThumbnail()?
+
 	// NOTE: pszFile and nIconIndex were set in GetIconLocation().
 	// TODO: Validate them to make sure they're the same values
 	// we returned in GetIconLocation()?
@@ -232,10 +234,30 @@ IFACEMETHODIMP RP_ExtractIcon::Extract(LPCWSTR pszFile, UINT nIconIndex,
 	const rp_image *img = nullptr;
 
 	uint32_t imgbf = d->romData->supportedImageTypes();
-	if (imgbf & RomData::IMGBF_EXT_MEDIA) {
-		// External media scan.
-		img = RpImageWin32::getExternalImage(d->romData, RomData::IMG_EXT_MEDIA);
+
+	/**
+	 * TODO:
+	 * - Add a function to retrieve the "default" image type in RomData,
+	 *   which can be customized per subclass.
+	 * - Add user customization.
+	 * - Use image sizes? May not be necessary since Vista+ uses the
+	 *   thumbnail interface when showing >24x24 icons...
+	 * - Handle image processing flags.
+	 */
+
+	// Check for external images.
+	if (imgbf & RomData::IMGBF_EXT_COVER) {
+		// External cover scan.
+		img = RpImageWin32::getExternalImage(d->romData, RomData::IMG_EXT_COVER);
 		needs_delete = (img != nullptr);
+	}
+	if (img != nullptr) {
+		// No cover scan. Try external media scan.
+		if (imgbf & RomData::IMGBF_EXT_MEDIA) {
+			// External media scan.
+			img = RpImageWin32::getExternalImage(d->romData, RomData::IMG_EXT_COVER);
+			needs_delete = (img != nullptr);
+		}
 	}
 
 	if (!img) {

commit 8a92557e921583acaa33061b6f507f2c6218a6ba

Fixed incorrect use of IMG_EXT_COVER where it should be IMG_EXT_MEDIA.
Copypasta regression from commit 6c23f9dc5c671ce5c903289f5871c4a438739a91.
([libromdata] TCreateThumbnail: Check IMG_EXT_COVER, then IMG_EXT_MEDIA, then IMG_INT_ICON.)

src/libromdata/img/TCreateThumbnail.cpp
@@ -210,7 +210,7 @@ int TCreateThumbnail<ImgClass>::getThumbnail(const RomData *romData, int req_siz
 		// No cover scan. Try external media scan.
 		if (imgbf & RomData::IMGBF_EXT_MEDIA) {
 			// External media scan.
-			ret_img = getExternalImage(romData, RomData::IMG_EXT_COVER, req_size, &img_sz);
+			ret_img = getExternalImage(romData, RomData::IMG_EXT_MEDIA, req_size, &img_sz);
 			imgpf = romData->imgpf(RomData::IMG_EXT_MEDIA);
 		}
 	}

src/win32/RP_ExtractIcon.cpp
@@ -255,7 +255,7 @@ IFACEMETHODIMP RP_ExtractIcon::Extract(LPCWSTR pszFile, UINT nIconIndex,
 		// No cover scan. Try external media scan.
 		if (imgbf & RomData::IMGBF_EXT_MEDIA) {
 			// External media scan.
-			img = RpImageWin32::getExternalImage(d->romData, RomData::IMG_EXT_COVER);
+			img = RpImageWin32::getExternalImage(d->romData, RomData::IMG_EXT_MEDIA);
 			needs_delete = (img != nullptr);
 		}
 	}