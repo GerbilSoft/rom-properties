GerbilSoft/rom-properties

commit 0d14636ba5a61a8a4a99fd4610e28af4d741ade2

[libromdata] crypto: Implemented CTR block chaining.
CryptoAPI and CryptoAPI NG don't support CTR natively, so it's
implemented on top of ECB.

Nettle does support CTR natively.

TODO:
- AesCipherTest: Verify that setting the IV fails if the chaining mode
  is set to CM_ECB.
- Nettle: Optimize key setting so we don't have to keep setting
  the key every time we call decrypt().

src/libromdata/crypto/AesNettle.cpp
@@ -31,6 +31,7 @@
 // Nettle AES functions.
 #include <nettle/aes.h>
 #include <nettle/cbc.h>
+#include <nettle/ctr.h>
 
 namespace LibRomData {
 
@@ -54,13 +55,27 @@ class AesNettlePrivate
 #else /* !HAVE_NETTLE_3 */
 		struct aes_ctx ctx;
 #endif /* HAVE_NETTLE_3 */
+
+		// Encryption key.
+		// Stored here because we need to
+		// use decryption for ECB and CBC,
+		// but encryption when using CTR.
+		uint8_t key[32];
+		int key_len;
+
+		// CBC: Initialization vector.
+		// CTR: Counter.
 		uint8_t iv[AES_BLOCK_SIZE];
 
 		IAesCipher::ChainingMode chainingMode;
 
 #ifdef HAVE_NETTLE_3
-		// Cipher function for cbc_decrypt().
+		// Cipher functions.
 		nettle_cipher_func *decrypt_fn;
+		nettle_cipher_func *encrypt_fn;
+		// Set Key functions.
+		nettle_set_key_func *setkey_dec_fn;
+		nettle_set_key_func *setkey_enc_fn;
 #endif /* HAVE_NETTLE_3 */
 };
 
@@ -70,10 +85,15 @@ AesNettlePrivate::AesNettlePrivate()
 	: chainingMode(IAesCipher::CM_ECB)
 #ifdef HAVE_NETTLE_3
 	, decrypt_fn(nullptr)
+	, encrypt_fn(nullptr)
+	, setkey_dec_fn(nullptr)
+	, setkey_enc_fn(nullptr)
 #endif /* HAVE_NETTLE_3 */
 {
-	// Clear the context and IV.
+	// Clear the context and keys.
 	memset(&ctx, 0, sizeof(ctx));
+	memset(key, 0, sizeof(key));
+	key_len = 0;
 	memset(iv, 0, sizeof(iv));
 }
 
@@ -133,22 +153,28 @@ int AesNettle::setKey(const uint8_t *key, unsigned int len)
 	switch (len) {
 		case 16:
 			d->decrypt_fn = (nettle_cipher_func*)aes128_decrypt;
-			aes128_set_decrypt_key(&d->ctx.aes128, key);
+			d->encrypt_fn = (nettle_cipher_func*)aes128_encrypt;
+			d->setkey_dec_fn = (nettle_set_key_func*)aes128_set_decrypt_key;
+			d->setkey_enc_fn = (nettle_set_key_func*)aes128_set_encrypt_key;
 			break;
 		case 24:
 			d->decrypt_fn = (nettle_cipher_func*)aes192_decrypt;
-			aes192_set_decrypt_key(&d->ctx.aes192, key);
+			d->encrypt_fn = (nettle_cipher_func*)aes192_encrypt;
+			d->setkey_dec_fn = (nettle_set_key_func*)aes192_set_decrypt_key;
+			d->setkey_enc_fn = (nettle_set_key_func*)aes192_set_encrypt_key;
 			break;
 		case 32:
 			d->decrypt_fn = (nettle_cipher_func*)aes256_decrypt;
-			aes256_set_decrypt_key(&d->ctx.aes256, key);
+			d->encrypt_fn = (nettle_cipher_func*)aes256_encrypt;
+			d->setkey_dec_fn = (nettle_set_key_func*)aes256_set_decrypt_key;
+			d->setkey_enc_fn = (nettle_set_key_func*)aes256_set_encrypt_key;
 			break;
 		default:
 			return -EINVAL;
 	}
-#else /* !HAVE_NETTLE_3 */
-	aes_set_decrypt_key(&d->ctx, len, key);
 #endif
+	memcpy(d->key, key, len);
+	d->key_len = len;
 
 	return 0;
 }
@@ -160,7 +186,7 @@ int AesNettle::setKey(const uint8_t *key, unsigned int len)
  */
 int AesNettle::setChainingMode(ChainingMode mode)
 {
-	if (mode < CM_ECB || mode > CM_CBC) {
+	if (mode < CM_ECB || mode > CM_CTR) {
 		return -EINVAL;
 	}
 
@@ -170,9 +196,9 @@ int AesNettle::setChainingMode(ChainingMode mode)
 }
 
 /**
- * Set the IV.
- * @param iv IV data.
- * @param len IV length, in bytes.
+ * Set the IV (CBC mode) or counter (CTR mode).
+ * @param iv IV/counter data.
+ * @param len IV/counter length, in bytes.
  * @return 0 on success; negative POSIX error code on error.
  */
 int AesNettle::setIV(const uint8_t *iv, unsigned int len)
@@ -202,35 +228,62 @@ unsigned int AesNettle::decrypt(uint8_t *data, unsigned int data_len)
 
 	// Decrypt the data.
 	RP_D(AesNettle);
+	if (!d->decrypt_fn) {
+		// No decryption function set...
+		return 0;
+	}
+
 #ifdef HAVE_NETTLE_3
 	if (!d->decrypt_fn) {
 		// No decryption function set...
 		return 0;
 	}
+
+	// TODO: Optimize key setting so it isn't
+	// done unless it's needed.
 	switch (d->chainingMode) {
 		case CM_ECB:
+			d->setkey_dec_fn(&d->ctx, d->key);
 			d->decrypt_fn(&d->ctx, data_len, data, data);
 			break;
 
 		case CM_CBC:
 			// IV is automatically updated for the next block.
+			d->setkey_dec_fn(&d->ctx, d->key);
 			cbc_decrypt(&d->ctx, d->decrypt_fn, AES_BLOCK_SIZE,
 				    d->iv, data_len, data, data);
 			break;
 
+		case CM_CTR:
+			// ctr is automatically updated for the next block.
+			// NOTE: ctr uses the *encrypt* function, even for decryption.
+			d->setkey_enc_fn(&d->ctx, d->key);
+			ctr_crypt(&d->ctx, d->encrypt_fn, AES_BLOCK_SIZE,
+				  d->iv, data_len, data, data);
+			break;
 		default:
 			return 0;
 	}
 #else /* !HAVE_NETTLE_3 */
 	switch (d->chainingMode) {
 		case CM_ECB:
+			aes_set_decrypt_key(&d->ctx, d->key_len, d->key);
 			aes_decrypt(&d->ctx, data_len, data, data);
 			break;
 
 		case CM_CBC:
 			// IV is automatically updated for the next block.
+			aes_set_decrypt_key(&d->ctx, d->key_len, d->key);
 			cbc_decrypt(&d->ctx, (nettle_crypt_func*)aes_decrypt, AES_BLOCK_SIZE,
 				    d->iv, data_len, data, data);
+			break;
+
+		case CM_CTR:
+			// ctr is automatically updated for the next block.
+			// NOTE: ctr uses the *encrypt* function, even for decryption.
+			aes_set_encrypt_key(&d->ctx, d->key_len, d->key);
+			ctr_crypt(&d->ctx, (nettle_crypt_func*)aes_encrypt, AES_BLOCK_SIZE,
+				  d->iv, data_len, data, data);
 			break;
 
 		default:
@@ -242,11 +295,11 @@ unsigned int AesNettle::decrypt(uint8_t *data, unsigned int data_len)
 }
 
 /**
- * Decrypt a block of data using the specified IV.
+ * Decrypt a block of data using the specified IV (CBC mode) or counter (CTR mode).
  * @param data Data block.
  * @param data_len Length of data block.
- * @param iv IV for the data block.
- * @param iv_len Length of the IV.
+ * @param iv IV/counter for the data block.
+ * @param iv_len Length of the IV/counter.
  * @return Number of bytes decrypted on success; 0 on error.
  */
 unsigned int AesNettle::decrypt(uint8_t *data, unsigned int data_len,

src/libromdata/crypto/AesCAPI.cpp
@@ -37,6 +37,8 @@
 //   [Google: "CryptImportKey" (no quotes)]
 // - http://etutorials.org/Programming/secure+programming/Chapter+5.+Symmetric+Encryption/5.25+Using+Symmetric+Encryption+with+Microsoft+s+CryptoAPI/
 //   [Google: "CryptoAPI set IV" (no quotes)]
+// - https://modexp.wordpress.com/2016/03/10/windows-ctr-mode-with-crypto-api/
+//   [Google: "cryptoapi-ng aes-ctr" (no quotes)]
 #include "../RpWin32.hpp"
 #include <wincrypt.h>
 
@@ -59,6 +61,12 @@ class AesCAPIPrivate
 
 		// Instance-specific key.
 		HCRYPTKEY hKey;
+
+		// Chaining mode.
+		IAesCipher::ChainingMode chainingMode;
+
+		// Counter for CTR mode.
+		uint8_t ctr[16];
 };
 
 /** AesCAPIPrivate **/
@@ -68,7 +76,11 @@ LONG AesCAPIPrivate::lRefCnt = 0;
 
 AesCAPIPrivate::AesCAPIPrivate()
 	: hKey(0)
+	, chainingMode(IAesCipher::CM_ECB)
 {
+	// Clear the counter.
+	memset(ctr, 0, sizeof(ctr));
+
 	if (InterlockedIncrement(&lRefCnt) == 1) {
 		// Initialize the CryptoAPI provider.
 		// TODO: Try multiple times, e.g.:
@@ -215,10 +227,13 @@ int AesCAPI::setChainingMode(ChainingMode mode)
 		// Key hasn't been set.
 		return -EBADF;
 	}
+	// TODO: Don't change if it matches?
+	// Need to ensure the default is ECB...
 
 	DWORD dwMode;
 	switch (mode) {
 		case CM_ECB:
+		case CM_CTR:
 			dwMode = CRYPT_MODE_ECB;
 			break;
 		case CM_CBC:
@@ -234,13 +249,14 @@ int AesCAPI::setChainingMode(ChainingMode mode)
 		return -w32err_to_posix(GetLastError());
 	}
 
+	d->chainingMode = mode;
 	return 0;
 }
 
 /**
- * Set the IV.
- * @param iv IV data.
- * @param len IV length, in bytes.
+ * Set the IV (CBC mode) or counter (CTR mode).
+ * @param iv IV/counter data.
+ * @param len IV/counter length, in bytes.
  * @return 0 on success; negative POSIX error code on error.
  */
 int AesCAPI::setIV(const uint8_t *iv, unsigned int len)
@@ -253,10 +269,21 @@ int AesCAPI::setIV(const uint8_t *iv, unsigned int len)
 		return -EBADF;
 	}
 
-	// Set the IV.
-	if (!CryptSetKeyParam(d->hKey, KP_IV, iv, 0)) {
-		// Error setting the IV.
-		return -w32err_to_posix(GetLastError());
+	switch (d->chainingMode) {
+		case CM_ECB:
+			// No IV.
+			return -EINVAL;
+		case CM_CBC:
+			// Set the IV.
+			if (!CryptSetKeyParam(d->hKey, KP_IV, iv, 0)) {
+				// Error setting the IV.
+				return -w32err_to_posix(GetLastError());
+			}
+			break;
+		case CM_CTR:
+			// Set the counter.
+			memcpy(d->ctr, iv, len);
+			break;
 	}
 
 	return 0;
@@ -293,18 +320,53 @@ unsigned int AesCAPI::decrypt(uint8_t *data, unsigned int data_len)
 	// NOTE: Specifying TRUE as the Final parameter results in
 	// CryptDecrypt failing with NTE_BAD_DATA, even though the
 	// data has the correct block length.
-	DWORD dwLen = data_len;
-	BOOL bRet = CryptDecrypt(hMyKey, 0, FALSE, 0, data, &dwLen);
-	CryptDestroyKey(hMyKey);
+	DWORD dwLen = 0;
+	BOOL bRet = FALSE;
+	if (d->chainingMode == CM_CTR) {
+		// CTR isn't supported by CryptoAPI directly.
+		// Need to decrypt each block manually.
+		uint8_t ctr_crypt[16];
+		for (; data_len > 0; data_len -= 16, data += 16) {
+			// Encrypt the current counter.
+			memcpy(ctr_crypt, d->ctr, sizeof(ctr_crypt));
+			DWORD dwTempLen = 16;
+			bRet = CryptEncrypt(hMyKey, 0, FALSE, 0, ctr_crypt, &dwTempLen, sizeof(ctr_crypt));
+			if (!bRet) {
+				// Encryption failed.
+				return 0;
+			}
+			dwLen += dwTempLen;
+
+			// XOR with the ciphertext.
+			// TODO: Optimized XOR.
+			for (int i = 15; i >= 0; i--) {
+				data[i] ^= ctr_crypt[i];
+			}
+
+			// Increment the counter.
+			for (int i = 15; i >= 0; i--) {
+				if (++d->ctr[i] != 0) {
+					// No carry needed.
+					break;
+				}
+			}
+		}
+	} else {
+		// EBC and/or CBC.
+		dwLen = data_len;
+		bRet = CryptDecrypt(hMyKey, 0, FALSE, 0, data, &dwLen);
+		CryptDestroyKey(hMyKey);
+	}
+
 	return (bRet ? dwLen : 0);
 }
 
 /**
- * Decrypt a block of data using the specified IV.
+ * Decrypt a block of data using the specified IV (CBC mode) or counter (CTR mode).
  * @param data Data block.
  * @param data_len Length of data block.
- * @param iv IV for the data block.
- * @param iv_len Length of the IV.
+ * @param iv IV/counter for the data block.
+ * @param iv_len Length of the IV/counter.
  * @return Number of bytes decrypted on success; 0 on error.
  */
 unsigned int AesCAPI::decrypt(uint8_t *data, unsigned int data_len,

src/libromdata/crypto/AesCAPI.hpp
@@ -69,9 +69,9 @@ class AesCAPI : public IAesCipher
 		virtual int setChainingMode(ChainingMode mode) override final;
 
 		/**
-		 * Set the IV.
-		 * @param iv IV data.
-		 * @param len IV length, in bytes.
+		 * Set the IV (CBC mode) or counter (CTR mode).
+		 * @param iv IV/counter data.
+		 * @param len IV/counter length, in bytes.
 		 * @return 0 on success; negative POSIX error code on error.
 		 */
 		virtual int setIV(const uint8_t *iv, unsigned int len) override final;
@@ -85,11 +85,11 @@ class AesCAPI : public IAesCipher
 		virtual unsigned int decrypt(uint8_t *data, unsigned int data_len) override final;
 
 		/**
-		 * Decrypt a block of data using the specified IV.
+		 * Decrypt a block of data using the specified IV (CBC mode) or counter (CTR mode).
 		 * @param data Data block.
 		 * @param data_len Length of data block.
-		 * @param iv IV for the data block.
-		 * @param iv_len Length of the IV.
+		 * @param iv IV/counter for the data block.
+		 * @param iv_len Length of the IV/counter.
 		 * @return Number of bytes decrypted on success; 0 on error.
 		 */
 		virtual unsigned int decrypt(uint8_t *data, unsigned int data_len,

src/libromdata/crypto/AesCAPI_NG.cpp
@@ -61,6 +61,7 @@ class AesCAPI_NG_Private
 		static DECL_FUNCPTR(BCryptGenerateSymmetricKey);
 		static DECL_FUNCPTR(BCryptDecrypt);
 		static DECL_FUNCPTR(BCryptDestroyKey);
+		static DECL_FUNCPTR(BCryptEncrypt);
 
 	public:
 		// NOTE: While the provider is shared in AesCAPI,
@@ -80,13 +81,12 @@ class AesCAPI_NG_Private
 		uint8_t key[32];
 		unsigned int key_len;
 
-		// Initialization vector.
-		// CryptoAPI NG doesn't store it in the key object,
-		// unlike the older CryptoAPI.
-		uint8_t iv[16];
-
 		// Chaining mode.
 		IAesCipher::ChainingMode chainingMode;
+
+		// CBC: Initialization vector.
+		// CTR: Counter.
+		uint8_t iv[16];
 };
 
 /** AesCAPI_NG_Private **/
@@ -100,6 +100,7 @@ DEF_FUNCPTR(BCryptCloseAlgorithmProvider);
 DEF_FUNCPTR(BCryptGenerateSymmetricKey);
 DEF_FUNCPTR(BCryptDecrypt);
 DEF_FUNCPTR(BCryptDestroyKey);
+DEF_FUNCPTR(BCryptEncrypt);
 
 AesCAPI_NG_Private::AesCAPI_NG_Private()
 	: hAesAlg(nullptr)
@@ -196,6 +197,7 @@ bool AesCAPI_NG::isUsable(void)
 	LOAD_FUNCPTR(BCryptGenerateSymmetricKey);
 	LOAD_FUNCPTR(BCryptDecrypt);
 	LOAD_FUNCPTR(BCryptDestroyKey);
+	LOAD_FUNCPTR(BCryptEncrypt);
 
 	// Make sure all of the function pointers are valid.
 	if (!AesCAPI_NG_Private::pBCryptOpenAlgorithmProvider ||
@@ -204,7 +206,8 @@ bool AesCAPI_NG::isUsable(void)
 	    !AesCAPI_NG_Private::pBCryptCloseAlgorithmProvider ||
 	    !AesCAPI_NG_Private::pBCryptGenerateSymmetricKey ||
 	    !AesCAPI_NG_Private::pBCryptDecrypt ||
-	    !AesCAPI_NG_Private::pBCryptDestroyKey)
+	    !AesCAPI_NG_Private::pBCryptDestroyKey ||
+	    !AesCAPI_NG_Private::pBCryptEncrypt)
 	{
 		// At least one function pointer is missing.
 		AesCAPI_NG_Private::pBCryptOpenAlgorithmProvider = nullptr;
@@ -214,6 +217,7 @@ bool AesCAPI_NG::isUsable(void)
 		AesCAPI_NG_Private::pBCryptGenerateSymmetricKey = nullptr;
 		AesCAPI_NG_Private::pBCryptDecrypt = nullptr;
 		AesCAPI_NG_Private::pBCryptDestroyKey = nullptr;
+		AesCAPI_NG_Private::pBCryptEncrypt = nullptr;
 
 		FreeLibrary(hBcryptDll);
 		return false;
@@ -348,6 +352,7 @@ int AesCAPI_NG::setChainingMode(ChainingMode mode)
 	ULONG cbMode;
 	switch (mode) {
 		case CM_ECB:
+		case CM_CTR:	// implemented using ECB
 			szMode = BCRYPT_CHAIN_MODE_ECB;
 			cbMode = sizeof(BCRYPT_CHAIN_MODE_ECB);
 			break;
@@ -379,9 +384,9 @@ int AesCAPI_NG::setChainingMode(ChainingMode mode)
 }
 
 /**
- * Set the IV.
- * @param iv IV data.
- * @param len IV length, in bytes.
+ * Set the IV (CBC mode) or counter (CTR mode).
+ * @param iv IV/counter data.
+ * @param len IV/counter length, in bytes.
  * @return 0 on success; negative POSIX error code on error.
  */
 int AesCAPI_NG::setIV(const uint8_t *iv, unsigned int len)
@@ -479,6 +484,42 @@ unsigned int AesCAPI_NG::decrypt(uint8_t *data, unsigned int data_len)
 						&cbResult, 0);
 			break;
 
+		case CM_CTR: {
+			// Need to decrypt each block manually.
+			uint8_t ctr_crypt[16];
+			ULONG cbTmpResult;
+			for (; data_len > 0; data_len -= 16, data += 16) {
+				// Encrypt the current counter.
+				memcpy(ctr_crypt, d->iv, sizeof(ctr_crypt));
+				status = d->pBCryptEncrypt(d->hKey,
+						ctr_crypt, sizeof(ctr_crypt),
+						nullptr,
+						nullptr, 0,
+						ctr_crypt, sizeof(ctr_crypt),
+						&cbTmpResult, 0);
+				if (!NT_SUCCESS(status)) {
+					// Encryption failed.
+					return 0;
+				}
+				cbResult += cbTmpResult;
+
+				// XOR with the ciphertext.
+				// TODO: Optimized XOR.
+				for (int i = 15; i >= 0; i--) {
+					data[i] ^= ctr_crypt[i];
+				}
+
+				// Increment the counter.
+				for (int i = 15; i >= 0; i--) {
+					if (++d->iv[i] != 0) {
+						// No carry needed.
+						break;
+					}
+				}
+			}
+			break;
+		}
+
 		default:
 			// Invalid chaining mode.
 			return 0;
@@ -488,11 +529,11 @@ unsigned int AesCAPI_NG::decrypt(uint8_t *data, unsigned int data_len)
 }
 
 /**
- * Decrypt a block of data using the specified IV.
+ * Decrypt a block of data using the specified IV (CBC mode) or counter (CTR mode).
  * @param data Data block.
  * @param data_len Length of data block.
- * @param iv IV for the data block.
- * @param iv_len Length of the IV.
+ * @param iv IV/counter for the data block.
+ * @param iv_len Length of the IV/counter.
  * @return Number of bytes decrypted on success; 0 on error.
  */
 unsigned int AesCAPI_NG::decrypt(uint8_t *data, unsigned int data_len,

src/libromdata/crypto/AesCAPI_NG.hpp
@@ -80,9 +80,9 @@ class AesCAPI_NG : public IAesCipher
 		virtual int setChainingMode(ChainingMode mode) override final;
 
 		/**
-		 * Set the IV.
-		 * @param iv IV data.
-		 * @param len IV length, in bytes.
+		 * Set the IV (CBC mode) or counter (CTR mode).
+		 * @param iv IV/counter data.
+		 * @param len IV/counter length, in bytes.
 		 * @return 0 on success; negative POSIX error code on error.
 		 */
 		virtual int setIV(const uint8_t *iv, unsigned int len) override final;
@@ -99,8 +99,8 @@ class AesCAPI_NG : public IAesCipher
 		 * Decrypt a block of data using the specified IV.
 		 * @param data Data block.
 		 * @param data_len Length of data block.
-		 * @param iv IV for the data block.
-		 * @param iv_len Length of the IV.
+		 * @param iv IV/counter for the data block.
+		 * @param iv_len Length of the IV/counter.
 		 * @return Number of bytes decrypted on success; 0 on error.
 		 */
 		virtual unsigned int decrypt(uint8_t *data, unsigned int data_len,

src/libromdata/crypto/AesNettle.hpp
@@ -69,9 +69,9 @@ class AesNettle : public IAesCipher
 		virtual int setChainingMode(ChainingMode mode) override final;
 
 		/**
-		 * Set the IV.
-		 * @param iv IV data.
-		 * @param len IV length, in bytes.
+		 * Set the IV (CBC mode) or counter (CTR mode).
+		 * @param iv IV/counter data.
+		 * @param len IV/counter length, in bytes.
 		 * @return 0 on success; negative POSIX error code on error.
 		 */
 		virtual int setIV(const uint8_t *iv, unsigned int len) override final;
@@ -85,11 +85,11 @@ class AesNettle : public IAesCipher
 		virtual unsigned int decrypt(uint8_t *data, unsigned int data_len) override final;
 
 		/**
-		 * Decrypt a block of data using the specified IV.
+		 * Decrypt a block of data using the specified IV (CBC mode) or counter (CTR mode).
 		 * @param data Data block.
 		 * @param data_len Length of data block.
-		 * @param iv IV for the data block.
-		 * @param iv_len Length of the IV.
+		 * @param iv IV/counter for the data block.
+		 * @param iv_len Length of the IV/counter.
 		 * @return Number of bytes decrypted on success; 0 on error.
 		 */
 		virtual unsigned int decrypt(uint8_t *data, unsigned int data_len,

src/libromdata/crypto/IAesCipher.hpp
@@ -64,6 +64,7 @@ class IAesCipher
 		enum ChainingMode {
 			CM_ECB,
 			CM_CBC,
+			CM_CTR,
 		};
 
 		/**
@@ -74,9 +75,9 @@ class IAesCipher
 		virtual int setChainingMode(ChainingMode mode) = 0;
 
 		/**
-		 * Set the IV.
-		 * @param iv IV data.
-		 * @param len IV length, in bytes.
+		 * Set the IV (CBC mode) or counter (CTR mode).
+		 * @param iv IV/counter data.
+		 * @param len IV/counter length, in bytes.
 		 * @return 0 on success; negative POSIX error code on error.
 		 */
 		virtual int setIV(const uint8_t *iv, unsigned int len) = 0;
@@ -90,11 +91,11 @@ class IAesCipher
 		virtual unsigned int decrypt(uint8_t *data, unsigned int data_len) = 0;
 
 		/**
-		 * Decrypt a block of data using the specified IV.
+		 * Decrypt a block of data using the specified IV (CBC mode) or counter (CTR mode).
 		 * @param data Data block.
 		 * @param data_len Length of data block.
-		 * @param iv IV for the data block.
-		 * @param iv_len Length of the IV.
+		 * @param iv IV/counter for the data block.
+		 * @param iv_len Length of the IV/counter.
 		 * @return Number of bytes decrypted on success; 0 on error.
 		 */
 		virtual unsigned int decrypt(uint8_t *data, unsigned int data_len,

src/libromdata/tests/AesCipherTest.cpp
@@ -76,6 +76,9 @@ inline ::std::ostream& operator<<(::std::ostream& os, const AesCipherTest_mode& 
 		case IAesCipher::CM_CBC:
 			cm_str = "CBC";
 			break;
+		case IAesCipher::CM_CTR:
+			cm_str = "CTR";
+			break;
 		default:
 			cm_str = "UNK";
 			break;
@@ -256,8 +259,11 @@ TEST_P(AesCipherTest, decryptTest)
 	EXPECT_EQ(0, m_cipher->setKey(aes_key, (unsigned int)mode.key_len));
 	EXPECT_EQ(0, m_cipher->setChainingMode(mode.chainingMode));
 
-	if (mode.chainingMode == IAesCipher::CM_CBC) {
-		// Set the IV.
+	if (mode.chainingMode == IAesCipher::CM_CBC ||
+	    mode.chainingMode == IAesCipher::CM_CTR)
+	{
+		// Set the IV/counter.
+		// TODO: Try setting this with CM_ECB and expect failure.
 		EXPECT_EQ(0, m_cipher->setIV(aes_iv, sizeof(aes_iv)));
 	}
 
@@ -293,6 +299,9 @@ string AesCipherTest::test_case_suffix_generator(const ::testing::TestParamInfo<
 			break;
 		case IAesCipher::CM_CBC:
 			cm_str = "CBC";
+			break;
+		case IAesCipher::CM_CTR:
+			cm_str = "CTR";
 			break;
 		default:
 			cm_str = "UNK";
@@ -370,6 +379,39 @@ static const uint8_t aes256cbc_ciphertext[64] = {
 	0xCE,0x35,0xAF,0x78,0x8A,0x0B,0x38,0xDB
 };
 
+static const uint8_t aes128ctr_ciphertext[64] = {
+	0xAC,0x52,0x86,0x43,0x5A,0x3D,0x8E,0x0A,
+	0xB0,0x9E,0xEE,0x90,0x27,0x3A,0xDA,0x81,
+	0xE9,0xC0,0x88,0x78,0x4F,0x81,0xE2,0xFD,
+	0x14,0x11,0x24,0xB1,0x61,0xA5,0x79,0x78,
+	0xC1,0xCC,0xB9,0x5B,0xD1,0x5B,0x3D,0xBB,
+	0x3D,0x25,0x20,0x55,0x95,0x98,0xBE,0x24,
+	0x09,0x79,0xAD,0xB0,0xEA,0x99,0x6C,0x98,
+	0x83,0x19,0xA7,0xAB,0xC4,0x2E,0x3C,0x08
+};
+
+static const uint8_t aes192ctr_ciphertext[64] = {
+	0x25,0x8C,0xF0,0x21,0x59,0x35,0xAF,0xB6,
+	0xD4,0x99,0xF5,0x11,0x29,0xEF,0xAF,0x8E,
+	0x6C,0x8D,0x9F,0xD5,0x76,0xBF,0x1F,0xB0,
+	0x10,0x10,0x14,0x6D,0x3B,0xBE,0x39,0x50,
+	0x1F,0x17,0xF6,0x73,0xF0,0x92,0xE3,0xDB,
+	0xE2,0x7F,0xED,0xB1,0xDA,0xE1,0x47,0xC3,
+	0xC8,0x83,0xA8,0x36,0xA4,0x58,0x0A,0x03,
+	0x92,0x70,0x03,0x5C,0x42,0x68,0x44,0x06
+};
+
+static const uint8_t aes256ctr_ciphertext[64] = {
+	0x35,0x3B,0xD6,0xA5,0xD2,0x18,0xC7,0x27,
+	0x84,0xCD,0x91,0x33,0xAC,0x05,0xF5,0x33,
+	0xD0,0x1E,0x31,0x71,0xF5,0x3E,0x22,0x92,
+	0x06,0x36,0x76,0x1D,0x8B,0x07,0x5C,0x29,
+	0x0E,0x2D,0x12,0xD8,0xD0,0x98,0x00,0x45,
+	0xFD,0x5B,0xB2,0xC1,0x7D,0x92,0xC0,0xF4,
+	0xB0,0x7E,0x8E,0x53,0x11,0xCB,0x9D,0xB1,
+	0xBA,0x23,0xD4,0x70,0x25,0x74,0xDB,0x8F
+};
+
 INSTANTIATE_TEST_CASE_P(AesDecryptTest, AesCipherTest,
 	::testing::Values(
 		AesCipherTest_mode(IAesCipher::CM_ECB, 16,
@@ -390,7 +432,17 @@ INSTANTIATE_TEST_CASE_P(AesDecryptTest, AesCipherTest,
 			sizeof(aes192cbc_ciphertext)),
 		AesCipherTest_mode(IAesCipher::CM_CBC, 32,
 			aes256cbc_ciphertext,
-			sizeof(aes256cbc_ciphertext))
+			sizeof(aes256cbc_ciphertext)),
+
+		AesCipherTest_mode(IAesCipher::CM_CTR, 16,
+			aes128ctr_ciphertext,
+			sizeof(aes128ctr_ciphertext)),
+		AesCipherTest_mode(IAesCipher::CM_CTR, 24,
+			aes192ctr_ciphertext,
+			sizeof(aes192ctr_ciphertext)),
+		AesCipherTest_mode(IAesCipher::CM_CTR, 32,
+			aes256ctr_ciphertext,
+			sizeof(aes256ctr_ciphertext))
 		)
 
 	, AesCipherTest::test_case_suffix_generator);

commit 4c6a91dcae8f3d70725bdfc22b3e01a8f28f0a62

[libromdata] AesNettle: Removed a duplicate d->decrypt_fn check.
The duplicate was outside of HAVE_NETTLE_3, which caused Nettle 2.x
builds to break. (Ubuntu 14.04, travis-ci)

This fixes a regression from commit 0d14636ba5a61a8a4a99fd4610e28af4d741ade2.
([libromdata] crypto: Implemented CTR block chaining.)

src/libromdata/crypto/AesNettle.cpp
@@ -235,10 +235,6 @@ unsigned int AesNettle::decrypt(uint8_t *data, unsigned int data_len)
 
 	// Decrypt the data.
 	RP_D(AesNettle);
-	if (!d->decrypt_fn) {
-		// No decryption function set...
-		return 0;
-	}
 
 #ifdef HAVE_NETTLE_3
 	if (!d->decrypt_fn) {