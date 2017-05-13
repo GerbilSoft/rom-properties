9d3c689375f2ff46ab1b40a063a18f3e01f2f880
David Korth <gerbilsoft@gerbilsoft.com>
2017-05-13 12:32:57 -0400

    [win32] rp-config.c: Search the registered CLSIDs for the DLL path.
    This is done only if rom-properties.dll isn't present in either
    rp-config.exe's directory or in an architecture-specific subdirectory.

--- src/win32/config/rp-config.c
+++ src/win32/config/rp-config.c
@@ -64,6 +64,17 @@ static const wchar_t rp_subdir[] = L"amd64/";
 #error Unsupported CPU architecture.
 #endif
 
+// NOTE: We're using strings because we have to use strings when
+// looking up CLSIDs in the registry, so we might as well have the
+// string format here instead of converting at runtime
+// CLSID paths.
+static const wchar_t CLSIDs[4][40] = {
+	L"{E51BC107-E491-4B29-A6A3-2A4309259802}",	// RP_ExtractIcon
+	L"{84573BC0-9502-42F8-8066-CC527D0779E5}",	// RP_ExtractImage
+	L"{2443C158-DF7C-4352-B435-BC9F885FFD52}",	// RP_ShellPropSheetExt
+	L"{4723DF58-463E-4590-8F4A-8D9DD4F4355A}",	// RP_ThumbnailProvider
+};
+
 /**
  * Try loading the ROM Properties Page DLL.
  *
@@ -81,6 +92,8 @@ static const wchar_t rp_subdir[] = L"amd64/";
 			/* Run the function. */ \
 			free(exe_path); \
 			free(dll_filename); \
+			if (hkeyCLSID) \
+				RegCloseKey(hkeyCLSID); \
 			return pfn(nullptr, hInstance, lpCmdLine, nCmdShow); \
 		} \
 		FreeLibrary(hRpDll); \
@@ -91,10 +104,14 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
 {
 	// Filename and path buffers.
 	wchar_t *exe_path = NULL;	// MAX_PATH
+	#define EXE_PATH_LEN (MAX_PATH)
 	wchar_t *dll_filename = NULL;	// MAX_PATH+32
+	#define DLL_FILENAME_LEN (MAX_PATH+32)
 
 	wchar_t *last_backslash;
+	HKEY hkeyCLSID = NULL;	// HKEY_CLASSES_ROOT\\CLSID
 	DWORD exe_path_len;
+	LONG lResult;
 
 	((void)hPrevInstance);
 
@@ -107,8 +124,8 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
 	exe_path = malloc(MAX_PATH*sizeof(wchar_t));
 	if (!exe_path)
 		goto fail;
-	exe_path_len = GetModuleFileName(hInstance, exe_path, MAX_PATH);
-	if (exe_path_len == 0 || exe_path_len >= MAX_PATH) {
+	exe_path_len = GetModuleFileName(hInstance, exe_path, EXE_PATH_LEN);
+	if (exe_path_len == 0 || exe_path_len >= EXE_PATH_LEN) {
 		// FIXME: Handle this.
 		goto fail;
 	}
@@ -126,7 +143,7 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
 	}
 
 	// Initialize dll_filename with exe_path.
-	dll_filename = malloc((MAX_PATH+32)*sizeof(wchar_t));
+	dll_filename = malloc(DLL_FILENAME_LEN*sizeof(wchar_t));
 	if (!dll_filename)
 		goto fail;
 	memcpy(dll_filename, exe_path, exe_path_len*sizeof(wchar_t));
@@ -141,11 +158,59 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
 	wcscpy(&dll_filename[exe_path_len + _countof(rp_subdir) - 1], L"rom-properties.dll");
 	TRY_LOAD_DLL(dll_filename);
 
-	// FIXME: Search CLSIDs.
+	// Check the CLSIDs.
+	lResult = RegOpenKeyEx(HKEY_CLASSES_ROOT, L"CLSID", 0, KEY_ENUMERATE_SUB_KEYS, &hkeyCLSID);
+	if (lResult == ERROR_SUCCESS) {
+		// Need to open "HKCR\\CLSID\\{CLSID}\\InprocServer32".
+		unsigned int i;
+		for (i = 0; i < _countof(CLSIDs); i++) {
+			HKEY hkeyClass, hkeyInprocServer32;
+			DWORD cbData, type;
+
+			lResult = RegOpenKeyEx(hkeyCLSID, CLSIDs[i], 0, KEY_ENUMERATE_SUB_KEYS, &hkeyClass);
+			if (lResult != ERROR_SUCCESS)
+				continue;
+
+			lResult = RegOpenKeyEx(hkeyClass, L"InprocServer32", 0, KEY_READ, &hkeyInprocServer32);
+			if (lResult != ERROR_SUCCESS) {
+				RegCloseKey(hkeyClass);
+				continue;
+			}
+
+			// Read the default value to get the DLL filename.
+			cbData = DLL_FILENAME_LEN*sizeof(wchar_t);
+			lResult = RegQueryValueEx(
+				hkeyInprocServer32,	// hKey
+				NULL,			// lpValueName
+				NULL,			// lpReserved
+				&type,			// lpType
+				(LPBYTE)dll_filename,	// lpData
+				&cbData);		// lpcbData
+			RegCloseKey(hkeyInprocServer32);
+			RegCloseKey(hkeyClass);
+
+			// TODO: REG_EXPAND_SZ?
+			if (lResult != ERROR_SUCCESS || type != REG_SZ)
+				continue;
+
+			// Verify the NULL terminator.
+			if ((cbData % 2 != 0) || dll_filename[(cbData/2)-1] != 0) {
+				// Either this isn't a multiple of 2 bytes,
+				// or there's no NULL terminator.
+				continue;
+			}
+
+			// Attempt to load this DLL.
+			TRY_LOAD_DLL(dll_filename);
+		}
+	}
+
 	// TODO: Show an error message.
 
 fail:
 	free(exe_path);
 	free(dll_filename);
+	if (hkeyCLSID)
+		RegCloseKey(hkeyCLSID); \
 	return EXIT_FAILURE;
 }
