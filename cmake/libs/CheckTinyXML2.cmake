7f84c707b9559b8c2e632b76d11c8788aac56796
David Korth <gerbilsoft@gerbilsoft.com>
2017-05-14 13:35:02 -0400

    [tinyxml2] CMakeLists.txt: Enable DLL builds.
    Rebased CMakeLists.txt on the original TinyXML2 CMakeLists.txt,
    but with lots of stuff disabled and some changes.
    
    - Disable installation of everything except the DLL.
    - Disable testing.
    - Set TINYXML2_IMPORT as an interface definition when building
      as a DLL.
    - Remove TINYXML2_EXPORT in the static library build, which didn't
      work anyway due to a typo.
    - Added a debug postfix for debug DLLs.
    - Disabled MSVC configuration that's alread yset in the platform
      configuration.
    - Added other MSVC options, including split debug settings.

--- cmake/libs/CheckTinyXML2.cmake
+++ cmake/libs/CheckTinyXML2.cmake
@@ -20,20 +20,27 @@ ENDIF(NOT USE_INTERNAL_XML)
 
 IF(USE_INTERNAL_XML)
 	# Using the internal TinyXML2 library.
-	SET(TinyXML2_FOUND 1)
-	SET(HAVE_XML 1)
-	# TODO: Use DLLs on Windows and Mac OS X.
-	# See CheckPNG.cmake for more information.
-	SET(USE_INTERNAL_XML_DLL OFF)
 	# NOTE: The tinyxml2 target has implicit include directories,
 	# so we don't need to set the variables.
-	SET(TinyXML2_LIBRARY tinyxml2 CACHE "TinyXML2 library." INTERNAL FORCE)
+	SET(TinyXML2_FOUND 1)
+	SET(HAVE_XML 1)
+	IF(WIN32 OR APPLE)
+		# Using DLLs on Windows and Mac OS X.
+		# NOTE: libjpeg-turbo 1.5.1's CMakeLists only builds
+		# a DLL version of turbojpeg, not libjpeg.
+		SET(USE_INTERNAL_XML_DLL ON)
+		SET(TinyXML2_LIBRARY tinyxml2 CACHE "TinyXML2 library." INTERNAL FORCE)
+	ELSE()
+		# Using static linking on other systems.
+		SET(USE_INTERNAL_XML_DLL OFF)
+		SET(TinyXML2_LIBRARY tinyxml2_static CACHE "TinyXML2 library." INTERNAL FORCE)
+	ENDIF()
 ELSE(USE_INTERNAL_XML)
 	SET(USE_INTERNAL_XML_DLL OFF)
 ENDIF(USE_INTERNAL_XML)
 
 ELSE(ENABLE_XML)
-	# Disable XML2.
+	# Disable TinyXML2.
 	UNSET(XML_FOUND)
 	UNSET(HAVE_XML)
 	UNSET(USE_INTERNAL_XML)
