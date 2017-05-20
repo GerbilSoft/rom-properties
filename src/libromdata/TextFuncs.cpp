GerbilSoft/rom-properties

commit ad8017dc0a66ab4e5fe2ceb8c4ae2c7546b9b559

[libromdata] TextFuncs: Use U+FFFD as the Unicode replacement character in the latin1_to_utf*() functions.
Check for NULL terminators in the `for` loop condition.

src/libromdata/TextFuncs.cpp
@@ -79,23 +79,20 @@ u16string utf16_bswap(const char16_t *str, int len)
  * @param len Length of str, in bytes. (-1 for NULL-terminated string)
  * @return UTF-8 string.
  */
-string latin1_to_utf8(const char* str, int len)
+string latin1_to_utf8(const char *str, int len)
 {
 	REMOVE_TRAILING_NULLS_STRLEN(string, strlen, str, len);
 
 	string mbs;
 	mbs.reserve(len*2);
-	for (; len > 0; len--, str++) {
+	for (; *str != 0 && len > 0; len--, str++) {
 		// TODO: Optimize the branches?
-		if (!*str) {
-			// NULL. End of string.
-			break;
-		} else if ((*str & 0x80) == 0) {
+		if ((*str & 0x80) == 0) {
 			// ASCII.
 			mbs.push_back(*str);
 		} else if ((*str & 0xE0) == 0x80) {
-			// Characters 0x80-0x9F. Replace with '?'.
-			mbs.push_back('?');
+			// Characters 0x80-0x9F. Replace with U+FFFD.
+			mbs.append("\xEF\xBF\xBD");
 		} else {
 			// Other character. 2 bytes are needed.
 			mbs.push_back(0xC0 | ((*str >> 6) & 0x03));
@@ -118,10 +115,10 @@ u16string latin1_to_utf16(const char *str, int len)
 
 	u16string wcs;
 	wcs.reserve(len);
-	for (; len > 0; len--, str++) {
+	for (; *str != 0 && len > 0; len--, str++) {
 		if ((*str & 0xE0) == 0x80) {
-			// Characters 0x80-0x9F. Replace with '?'.
-			wcs.push_back(_RP_U16_CHR('?'));
+			// Characters 0x80-0x9F. Replace with U+FFFD.
+			wcs.push_back((char16_t)0xFFFD);
 		} else {
 			// Other character.
 			wcs.push_back((char16_t)(uint8_t)*str);

src/libromdata/TextFuncs.hpp
@@ -182,7 +182,7 @@ static inline std::u16string utf16be_to_utf16(const char16_t *str, int len)
  * @param len Length of str, in bytes. (-1 for NULL-terminated string)
  * @return UTF-8 string.
  */
-std::string latin1_to_utf8(const char* str, int len);
+std::string latin1_to_utf8(const char *str, int len);
 
 /**
  * Convert Latin-1 (ISO-8859-1) text to UTF-16.
@@ -191,7 +191,7 @@ std::string latin1_to_utf8(const char* str, int len);
  * @param len Length of str, in bytes. (-1 for NULL-terminated string)
  * @return UTF-16 string.
  */
-std::u16string latin1_to_utf16(const char* str, int len);
+std::u16string latin1_to_utf16(const char *str, int len);
 
 /** Miscellaneous functions. **/
 

commit 4a74aecf6a33a7a43baa97b2fa63d7fcc81b79db

[libromdata/tests] TextFuncsTest: Fix test failures caused by the U+FFFD change.
Need to change the test data to expect U+FFFD instead of '?'.

This fixes a regression from commit ad8017dc0a66ab4e5fe2ceb8c4ae2c7546b9b559.
([libromdata] TextFuncs: Use U+FFFD as the Unicode replacement character in the latin1_to_utf*() functions.)

src/libromdata/TextFuncs.cpp
@@ -74,7 +74,7 @@ u16string utf16_bswap(const char16_t *str, int len)
 
 /**
  * Convert Latin-1 (ISO-8859-1) text to UTF-8.
- * NOTE: 0x80-0x9F (cp1252) is converted to '?'.
+ * NOTE: 0x80-0x9F (cp1252) is converted to U+FFFD.
  * @param str Latin-1 text.
  * @param len Length of str, in bytes. (-1 for NULL-terminated string)
  * @return UTF-8 string.
@@ -104,7 +104,7 @@ string latin1_to_utf8(const char *str, int len)
 
 /**
  * Convert Latin-1 (ISO-8859-1) text to UTF-16.
- * NOTE: 0x80-0x9F (cp1252) is converted to '?'.
+ * NOTE: 0x80-0x9F (cp1252) is converted to U+FFFD.
  * @param str Latin-1 text.
  * @param len Length of str, in bytes. (-1 for NULL-terminated string)
  * @return UTF-16 string.

src/libromdata/tests/TextFuncsTest.cpp
@@ -179,15 +179,15 @@ class TextFuncsTest : public ::testing::Test
 		 * Latin1 to UTF-8 test string.
 		 * Contains the expected result from:
 		 * - latin1_to_utf8(cp1252_data, ARRAY_SIZE(cp1252_data))
-		 * (NOTE: Unsupported characters are replaced with '?'.)
+		 * (NOTE: Unsupported characters are replaced with U+FFFD.)
 		 */
-		static const uint8_t latin1_utf8_data[346];
+		static const uint8_t latin1_utf8_data[346+(26*2)];
 
 		/**
 		 * Latin1 to UTF-16 test string.
 		 * Contains the expected result from:
 		 * - latin1_to_utf16(cp1252_data, ARRAY_SIZE(cp1252_data))
-		 * (NOTE: Unsupported characters are replaced with '?'.)
+		 * (NOTE: Unsupported characters are replaced with U+FFFD.)
 		 */
 		static const char16_t latin1_utf16_data[250];
 };

src/libromdata/tests/TextFuncsTest_data.hpp
@@ -463,13 +463,14 @@ const uint8_t TextFuncsTest::utf16be_data[558] = {
 	0x00,0x2E,0x00,0x0A,0x00,0x00
 };
 
+#define U8_FFFD 0xEF,0xBF,0xBD
 /**
  * Latin1 to UTF-8 test string.
  * Contains the expected result from:
  * - latin1_to_utf8(cp1252_data, ARRAY_SIZE(cp1252_data))
- * (NOTE: Unsupported characters are replaced with '?'.)
+ * (NOTE: Unsupported characters are replaced with U+FFFD.)
  */
-const uint8_t TextFuncsTest::latin1_utf8_data[346] = {
+const uint8_t TextFuncsTest::latin1_utf8_data[346+(26*2)] = {
 	0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,
 	0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,
 	0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,
@@ -485,11 +486,11 @@ const uint8_t TextFuncsTest::latin1_utf8_data[346] = {
 	0x61,0x62,0x63,0x64,0x65,0x66,0x67,0x68,
 	0x69,0x6A,0x6B,0x6C,0x6D,0x6E,0x6F,0x70,
 	0x71,0x72,0x73,0x74,0x75,0x76,0x77,0x78,
-	0x79,0x7A,0x7B,0x7C,0x7D,0x7E,0x7F,0x3F,
-	0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,
-	0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,
-	0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,0x3F,
-	0x3F,0xC2,0xA0,0xC2,0xA1,0xC2,0xA2,0xC2,
+	0x79,0x7A,0x7B,0x7C,0x7D,0x7E,0x7F,U8_FFFD,
+	U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,
+	U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,
+	U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,U8_FFFD,
+	U8_FFFD,0xC2,0xA0,0xC2,0xA1,0xC2,0xA2,0xC2,
 	0xA3,0xC2,0xA4,0xC2,0xA5,0xC2,0xA6,0xC2,
 	0xA7,0xC2,0xA8,0xC2,0xA9,0xC2,0xAA,0xC2,
 	0xAB,0xC2,0xAC,0xC2,0xAD,0xC2,0xAE,0xC2,
@@ -520,7 +521,7 @@ const uint8_t TextFuncsTest::latin1_utf8_data[346] = {
  * Latin1 to UTF-16 test string.
  * Contains the expected result from:
  * - latin1_to_utf16(cp1252_data, ARRAY_SIZE(cp1252_data))
- * (NOTE: Unsupported characters are replaced with '?'.)
+ * (NOTE: Unsupported characters are replaced with U+FFFD.)
  */
 const char16_t TextFuncsTest::latin1_utf16_data[250] =
 {
@@ -539,11 +540,11 @@ const char16_t TextFuncsTest::latin1_utf16_data[250] =
 	0x0061,0x0062,0x0063,0x0064,0x0065,0x0066,0x0067,0x0068,
 	0x0069,0x006A,0x006B,0x006C,0x006D,0x006E,0x006F,0x0070,
 	0x0071,0x0072,0x0073,0x0074,0x0075,0x0076,0x0077,0x0078,
-	0x0079,0x007A,0x007B,0x007C,0x007D,0x007E,0x007F,0x003F,
-	0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,
-	0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,
-	0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,0x003F,
-	0x003F,0x00A0,0x00A1,0x00A2,0x00A3,0x00A4,0x00A5,0x00A6,
+	0x0079,0x007A,0x007B,0x007C,0x007D,0x007E,0x007F,0xFFFD,
+	0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,
+	0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,
+	0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,0xFFFD,
+	0xFFFD,0x00A0,0x00A1,0x00A2,0x00A3,0x00A4,0x00A5,0x00A6,
 	0x00A7,0x00A8,0x00A9,0x00AA,0x00AB,0x00AC,0x00AD,0x00AE,
 	0x00AF,0x00B0,0x00B1,0x00B2,0x00B3,0x00B4,0x00B5,0x00B6,
 	0x00B7,0x00B8,0x00B9,0x00BA,0x00BB,0x00BC,0x00BD,0x00BE,