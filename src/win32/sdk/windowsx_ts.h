7a986a109e89469b7d3b19d67e47623519c87ac7
David Korth <gerbilsoft@gerbilsoft.com>
2017-06-14 23:25:42 -0400

    [win32] tsbase.h: Split common stuff out of windowsx_ts.h.
    These macros will also be used by the upcoming commctrl_ts.h.
    
    CMakeLists.txt: Added tsbase.h and windowsx_ts.h.

--- src/win32/sdk/windowsx_ts.h
+++ src/win32/sdk/windowsx_ts.h
@@ -8,49 +8,11 @@
 #ifndef __ROMPROPERTIES_WIN32_SDK_WINDOWSX_TS_H__
 #define __ROMPROPERTIES_WIN32_SDK_WINDOWSX_TS_H__
 
-#ifdef _MAC
-#error Old MSVC for Mac compilers are not supported.
-#endif
-
-// Make sure STRICT is defined for better type safety.
-#ifndef STRICT
-#define STRICT
-#endif
-
-#include "libwin32common/RpWin32_sdk.h"
-#include <windows.h>
+#include "tsbase.h"
 #include <windowsx.h>
 
 // C includes.
 #include <assert.h>
-
-#ifndef FORCEINLINE
-# if defined(_MSC_VER)
-#  define FORCEINLINE __forceinline
-# elif defined(__GNUC__)
-#  define FORCEINLINE __attribute__((always_inline))
-# else
-#  error FORCEINLINE not defined for this compiler.
-# endif
-#endif /* FORCEINLINE */
-
-// STATIC_CAST() macro that matches shlwapi.h's version.
-#ifndef STATIC_CAST
-# ifdef __cplusplus
-#  define STATIC_CAST(typ) static_cast<typ>
-# else
-#  define STATIC_CAST(typ) (typ)
-# endif
-#endif /* STATIC_CAST */
-
-// REINTERPRET_CAST() macro that matches shlwapi.h's STATIC_CAST().
-#ifndef REINTERPRET_CAST
-# ifdef __cplusplus
-#  define REINTERPRET_CAST(typ) reinterpret_cast<typ>
-# else
-#  define REINTERPRET_CAST(typ) (typ)
-# endif
-#endif /* REINTERPRET_CAST */
 
 // NOTE: Afx*() functions (_MAC) are not included here.
 
