GerbilSoft/rom-properties

commit db66b05d15cbb7cf92d5c97d9728269ae2618992

[libromdata] Amiibo, GameCube, Nintendo3DS: Use string tables instead of switch/case in some places.

src/libromdata/Amiibo.cpp
@@ -494,23 +494,13 @@ int Amiibo::loadFieldData(void)
 		RomFields::STRF_MONOSPACE);
 
 	// amiibo type.
-	const rp_char *type = nullptr;
-	switch (char_id & 0xFF) {
-		case NFP_TYPE_FIGURINE:
-			type = _RP("Figurine");
-			break;
-		case NFP_TYPE_CARD:
-			type = _RP("Card");
-			break;
-		case NFP_TYPE_YARN:
-			type = _RP("Yarn");
-			break;
-		default:
-			break;
-	}
-
-	if (type) {
-		d->fields->addField_string(_RP("amiibo Type"), type);
+	static const rp_char amiibo_type_tbl[3][12] = {
+		_RP("Figurine"),	// NFP_TYPE_FIGURINE
+		_RP("Card"),		// NFP_TYPE_CARD
+		_RP("Yarn"),		// NFP_TYPE_YARN
+	};
+	if ((char_id & 0xFF) < ARRAY_SIZE(amiibo_type_tbl)) {
+		d->fields->addField_string(_RP("amiibo Type"), amiibo_type_tbl[char_id & 0xFF]);
 	} else {
 		// Invalid amiibo type.
 		char buf[24];

src/libromdata/GameCube.cpp
@@ -1777,35 +1777,31 @@ int GameCube::loadFieldData(void)
 
 				// Partition type.
 				rp_string str;
-				switch (entry.type) {
-					case GameCubePrivate::PARTITION_GAME:
-						str = _RP("Game");
-						break;
-					case GameCubePrivate::PARTITION_UPDATE:
-						str = _RP("Update");
-						break;
-					case GameCubePrivate::PARTITION_CHANNEL:
-						str = _RP("Channel");
-						break;
-					default: {
-						// If all four bytes are ASCII,
-						// print it as-is. (SSBB demo channel)
-						// Otherwise, print the number.
-						// NOTE: Must be BE32 for proper display.
-						uint32_t be32_type = cpu_to_be32(entry.type);
-						memcpy(buf, &be32_type, 4);
-						if (isalnum(buf[0]) && isalnum(buf[1]) &&
-							isalnum(buf[2]) && isalnum(buf[3]))
-						{
-							// All four bytes are ASCII.
-							str = latin1_to_rp_string(buf, 4);
-						} else {
-							// Non-ASCII data. Print the hex values instead.
-							len = snprintf(buf, sizeof(buf), "%08X", entry.type);
-							if (len > (int)sizeof(buf))
-								len = sizeof(buf);
-							str = (len > 0 ? latin1_to_rp_string(buf, len) : _RP(""));
-						}
+				static const rp_char part_type_tbl[3][8] = {
+					_RP("Game"),	// GameCubePrivate::PARTITION_GAME
+					_RP("Update"),	// GameCubePrivate::PARTITION_UPDATE
+					_RP("Channel"),	// GameCubePrivate::PARTITION_CHANNEL
+				};
+				if (entry.type <= GameCubePrivate::PARTITION_CHANNEL) {
+					str = part_type_tbl[entry.type];
+				} else {
+					// If all four bytes are ASCII letters and/or numbers,
+					// print it as-is. (SSBB demo channel)
+					// Otherwise, print the hexadecimal value.
+					// NOTE: Must be BE32 for proper display.
+					uint32_t be32_type = cpu_to_be32(entry.type);
+					memcpy(buf, &be32_type, 4);
+					if (isalnum(buf[0]) && isalnum(buf[1]) &&
+					    isalnum(buf[2]) && isalnum(buf[3]))
+					{
+						// All four bytes are ASCII letters and/or numbers.
+						str = latin1_to_rp_string(buf, 4);
+					} else {
+						// Non-ASCII data. Print the hex values instead.
+						len = snprintf(buf, sizeof(buf), "%08X", entry.type);
+						if (len > (int)sizeof(buf))
+							len = sizeof(buf);
+						str = (len > 0 ? latin1_to_rp_string(buf, len) : _RP(""));
 					}
 				}
 				data_row.push_back(str);

src/libromdata/Nintendo3DS.cpp
@@ -2267,7 +2267,7 @@ int Nintendo3DS::loadFieldData(void)
 			latin1_to_rp_string(ncch_exheader->sci.title, sizeof(ncch_exheader->sci.title)));
 
 		// Application type. (resource limit category)
-		static const rp_char *const application_type_tbl[4] = {
+		static const rp_char application_type_tbl[4][16] = {
 			_RP("Application"),	// N3DS_NCCH_EXHEADER_ACI_ResLimit_Categry_APPLICATION
 			_RP("System Applet"),	// N3DS_NCCH_EXHEADER_ACI_ResLimit_Categry_SYS_APPLET
 			_RP("Library Applet"),	// N3DS_NCCH_EXHEADER_ACI_ResLimit_Categry_LIB_APPLET
@@ -2297,9 +2297,9 @@ int Nintendo3DS::loadFieldData(void)
 		// TODO: Figure out what "Core Version" is.
 
 		// System Mode.
-		static const rp_char *const old3ds_sys_mode_tbl[6] = {
+		static const rp_char old3ds_sys_mode_tbl[6][16] = {
 			_RP("Prod (64 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Prod
-			nullptr,
+			_RP(""),
 			_RP("Dev1 (96 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Dev1
 			_RP("Dev2 (80 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Dev2
 			_RP("Dev3 (72 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Dev3
@@ -2307,7 +2307,9 @@ int Nintendo3DS::loadFieldData(void)
 		};
 		const uint8_t old3ds_sys_mode = (ncch_exheader->aci.arm11_local.flags[2] &
 			N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Mask) >> 4;
-		if (old3ds_sys_mode < ARRAY_SIZE(old3ds_sys_mode_tbl)) {
+		if (old3ds_sys_mode < ARRAY_SIZE(old3ds_sys_mode_tbl) &&
+		    old3ds_sys_mode_tbl[old3ds_sys_mode][0] != 0)
+		{
 			d->fields->addField_string(_RP("Old3DS Sys Mode"),
 				old3ds_sys_mode_tbl[old3ds_sys_mode]);
 		} else {
@@ -2319,7 +2321,7 @@ int Nintendo3DS::loadFieldData(void)
 		}
 
 		// New3DS System Mode.
-		static const rp_char *const new3ds_sys_mode_tbl[4] = {
+		static const rp_char new3ds_sys_mode_tbl[4][16] = {
 			_RP("Legacy (64 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG1_New3DS_SysMode_Legacy
 			_RP("Prod (124 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG1_New3DS_SysMode_Prod
 			_RP("Dev1 (178 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG1_New3DS_SysMode_Dev1

commit 2db0b7f06d9da419d0387ef68cb1289f0c216a02

[libromdata] Amiibo, GameCube, Nintendo3DS: We can't use _RP() for character arrays in MSVC older than 2015.
Prior to MSVC 2015, _RP() casts from L"" to char16_t*. This prevents it
from being used to initialize character arrays. Hence, we have to use
pointers instead.

This fixes a regression from commit db66b05d15cbb7cf92d5c97d9728269ae2618992.
([libromdata] Amiibo, GameCube, Nintendo3DS: Use string tables instead of switch/case in some places.)

src/libromdata/Amiibo.cpp
@@ -491,7 +491,7 @@ int Amiibo::loadFieldData(void)
 		RomFields::STRF_MONOSPACE);
 
 	// amiibo type.
-	static const rp_char amiibo_type_tbl[3][12] = {
+	static const rp_char *const amiibo_type_tbl[3] = {
 		_RP("Figurine"),	// NFP_TYPE_FIGURINE
 		_RP("Card"),		// NFP_TYPE_CARD
 		_RP("Yarn"),		// NFP_TYPE_YARN

src/libromdata/GameCube.cpp
@@ -1769,7 +1769,7 @@ int GameCube::loadFieldData(void)
 
 				// Partition type.
 				rp_string str;
-				static const rp_char part_type_tbl[3][8] = {
+				static const rp_char *const part_type_tbl[3] = {
 					_RP("Game"),	// GameCubePrivate::PARTITION_GAME
 					_RP("Update"),	// GameCubePrivate::PARTITION_UPDATE
 					_RP("Channel"),	// GameCubePrivate::PARTITION_CHANNEL

src/libromdata/Nintendo3DS.cpp
@@ -935,7 +935,7 @@ void Nintendo3DSPrivate::addTitleIdAndProductCodeFields(bool showContentType)
 		// Content type.
 		// This is normally shown in the CIA content table.
 		if (showContentType) {
-			const rp_char *content_type = ncch->contentType();
+			const rp_char *const content_type = ncch->contentType();
 			fields->addField_string(_RP("Content Type"),
 				(content_type ? content_type : _RP("Unknown")));
 		}
@@ -2238,7 +2238,7 @@ int Nintendo3DS::loadFieldData(void)
 			latin1_to_rp_string(ncch_exheader->sci.title, sizeof(ncch_exheader->sci.title)));
 
 		// Application type. (resource limit category)
-		static const rp_char application_type_tbl[4][16] = {
+		static const rp_char *const application_type_tbl[4] = {
 			_RP("Application"),	// N3DS_NCCH_EXHEADER_ACI_ResLimit_Categry_APPLICATION
 			_RP("System Applet"),	// N3DS_NCCH_EXHEADER_ACI_ResLimit_Categry_SYS_APPLET
 			_RP("Library Applet"),	// N3DS_NCCH_EXHEADER_ACI_ResLimit_Categry_LIB_APPLET
@@ -2265,9 +2265,9 @@ int Nintendo3DS::loadFieldData(void)
 		// TODO: Figure out what "Core Version" is.
 
 		// System Mode.
-		static const rp_char old3ds_sys_mode_tbl[6][16] = {
+		static const rp_char *const old3ds_sys_mode_tbl[6] = {
 			_RP("Prod (64 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Prod
-			_RP(""),
+			nullptr,
 			_RP("Dev1 (96 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Dev1
 			_RP("Dev2 (80 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Dev2
 			_RP("Dev3 (72 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Dev3
@@ -2276,7 +2276,7 @@ int Nintendo3DS::loadFieldData(void)
 		const uint8_t old3ds_sys_mode = (ncch_exheader->aci.arm11_local.flags[2] &
 			N3DS_NCCH_EXHEADER_ACI_FLAG2_Old3DS_SysMode_Mask) >> 4;
 		if (old3ds_sys_mode < ARRAY_SIZE(old3ds_sys_mode_tbl) &&
-		    old3ds_sys_mode_tbl[old3ds_sys_mode][0] != 0)
+		    old3ds_sys_mode_tbl[old3ds_sys_mode] != nullptr)
 		{
 			d->fields->addField_string(_RP("Old3DS Sys Mode"),
 				old3ds_sys_mode_tbl[old3ds_sys_mode]);
@@ -2286,7 +2286,7 @@ int Nintendo3DS::loadFieldData(void)
 		}
 
 		// New3DS System Mode.
-		static const rp_char new3ds_sys_mode_tbl[4][16] = {
+		static const rp_char *const new3ds_sys_mode_tbl[4] = {
 			_RP("Legacy (64 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG1_New3DS_SysMode_Legacy
 			_RP("Prod (124 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG1_New3DS_SysMode_Prod
 			_RP("Dev1 (178 MB)"),	// N3DS_NCCH_EXHEADER_ACI_FLAG1_New3DS_SysMode_Dev1