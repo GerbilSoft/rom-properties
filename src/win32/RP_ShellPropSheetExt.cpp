GerbilSoft/rom-properties

commit 0fa2ab7175f653a39c721ae3c8d3394a84ceb31b

[win32] RP_ShellPropSheetExt: Load the images in WM_INITDIALOG.
If we don't load the images in WM_INITDIALOG, the animated icon won't be
initialized before startAnimTimer() is called by the WM_NOTIFY handler,
which results in the animation not starting until the tab is deactivated
and reactivated after its initial activation.

This fixes a regression from commit d1babb3d3f5bb156be7e4225c09821eb3b59af23.
([win32] RP_ShellPropSheetExt: Create the widgets in WM_SHOWWINDOW.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -185,9 +185,10 @@ class RP_ShellPropSheetExt_Private
 		 * This function should bee called on startup and if
 		 * the window's background color changes.
 		 *
-		 * @param hDlg	[in] Dialog window.
+		 * NOTE: The HWND isn't needed here, since this function
+		 * doesn't touch the dialog at all.
 		 */
-		void loadImages(HWND hDlg);
+		void loadImages(void);
 
 	private:
 		/**
@@ -406,10 +407,15 @@ void RP_ShellPropSheetExt_Private::stopAnimTimer(void)
  * This function should be called on startup and if
  * the window's background color changes.
  *
- * @param hDlg	[in] Dialog window.
+ * NOTE: The HWND isn't needed here, since this function
+ * doesn't touch the dialog at all.
  */
-void RP_ShellPropSheetExt_Private::loadImages(HWND hDlg)
+void RP_ShellPropSheetExt_Private::loadImages(void)
 {
+	assert(romData != nullptr);
+	if (!romData)
+		return;
+
 	// Window background color.
 	// Static controls don't support alpha transparency (?? test),
 	// so we have to fake it.
@@ -466,6 +472,7 @@ void RP_ShellPropSheetExt_Private::loadImages(HWND hDlg)
 		const rp_image *icon = romData->image(RomData::IMG_INT_ICON);
 		if (icon && icon->isValid()) {
 			// Get the animated icon data.
+			iconAnimData = romData->iconAnimData();
 			if (iconAnimData) {
 				// Convert the icons to GDI+ bitmaps.
 				// TODO: Refactor this a bit...
@@ -620,8 +627,9 @@ int RP_ShellPropSheetExt_Private::createHeaderRow(HWND hDlg, const POINT &pt_sta
 			}
 			total_widget_width += szIcon.cx;
 
-			// Get the animated icon data.
-			iconAnimData = romData->iconAnimData();
+			// NOTE: Animated icon data is retrieved in loadImages(),
+			// which is called in WM_INITDIALOG. This is needed in order to
+			// ensure that the animated icon timer is initialized properly.
 		} else {
 			// No icon.
 			icon = nullptr;
@@ -658,9 +666,6 @@ int RP_ShellPropSheetExt_Private::createHeaderRow(HWND hDlg, const POINT &pt_sta
 			curPt.x + szIcon.cx, curPt.y + szIcon.cy);
 		curPt.x += szIcon.cx + pt_start.x;
 	}
-
-	// Load the images.
-	loadImages(hDlg);
 
 	// Return the label height and some extra padding.
 	// TODO: Icon/banner height?
@@ -1908,6 +1913,11 @@ INT_PTR CALLBACK RP_ShellPropSheetExt::DlgProc(HWND hDlg, UINT uMsg, WPARAM wPar
 			// since some other extension (e.g. HashTab) may be
 			// resizing the dialog.
 
+			// We need to load the images here.
+			// Otherwise, the animation won't be initialized
+			// properly when the tab is first displayed.
+			d->loadImages();
+
 			// NOTE: We're using WM_SHOWWINDOW instead of WM_SIZE
 			// because WM_SIZE isn't sent for block devices,
 			// e.g. CD-ROM drives.
@@ -2061,7 +2071,7 @@ INT_PTR CALLBACK RP_ShellPropSheetExt::DlgProc(HWND hDlg, UINT uMsg, WPARAM wPar
 			RP_ShellPropSheetExt_Private *const d = static_cast<RP_ShellPropSheetExt_Private*>(
 				GetProp(hDlg, RP_ShellPropSheetExt_Private::D_PTR_PROP));
 			if (d) {
-				d->loadImages(hDlg);
+				d->loadImages();
 			}
 			break;
 		}

commit 6d154ca9d0e8985450952db402660cb8e49d76cf

[win32] RP_ShellPropSheetExt: Fixed icon resizing for icons smaller than 32x32.
This only affects PS1 save files at the moment.

The problem is loadImages() was called before szIcon and szBanner were
set by createHeaderRow(), since this was needed in order to fix a glitch
with animated icons when fixing alignment if a shell extension that
resizes the property dialog was loaded.

This fixes a regression from commit 0fa2ab7175f653a39c721ae3c8d3394a84ceb31b.
([win32] RP_ShellPropSheetExt: Load the images in WM_INITDIALOG.)

Reported by @DankRank in #retro.

src/win32/RP_ShellPropSheetExt.cpp
@@ -466,6 +466,13 @@ void RP_ShellPropSheetExt_Private::loadImages(void)
 		// Get the banner.
 		const rp_image *banner = romData->image(RomData::IMG_INT_BANNER);
 		if (banner && banner->isValid()) {
+			// Save the banner size.
+			if (szBanner.cx == 0) {
+				szBanner.cx = banner->width();
+				szBanner.cy = banner->height();
+				incSizeToMinimum(szBanner);
+			}
+
 			// Convert to HBITMAP using the window background color.
 			// TODO: Redo if the window background color changes.
 			hbmpBanner = RpImageWin32::toHBITMAP(banner, gdipBgColor, szBanner, true);
@@ -485,6 +492,13 @@ void RP_ShellPropSheetExt_Private::loadImages(void)
 		// Get the icon.
 		const rp_image *icon = romData->image(RomData::IMG_INT_ICON);
 		if (icon && icon->isValid()) {
+			// Save the icon size.
+			if (szIcon.cx == 0) {
+				szIcon.cx = icon->width();
+				szIcon.cy = icon->height();
+				incSizeToMinimum(szIcon);
+			}
+
 			// Get the animated icon data.
 			iconAnimData = romData->iconAnimData();
 			if (iconAnimData) {
@@ -608,9 +622,12 @@ int RP_ShellPropSheetExt_Private::createHeaderRow(HWND hDlg, const POINT &pt_sta
 		// Get the banner.
 		banner = romData->image(RomData::IMG_INT_BANNER);
 		if (banner && banner->isValid()) {
-			szBanner.cx = banner->width();
-			szBanner.cy = banner->height();
-			incSizeToMinimum(szBanner);
+			// Save the banner size.
+			if (szBanner.cx == 0) {
+				szBanner.cx = banner->width();
+				szBanner.cy = banner->height();
+				incSizeToMinimum(szBanner);
+			}
 
 			// Add the banner width.
 			// The banner will be assigned to a WC_STATIC control.
@@ -630,9 +647,12 @@ int RP_ShellPropSheetExt_Private::createHeaderRow(HWND hDlg, const POINT &pt_sta
 		// Get the icon.
 		icon = romData->image(RomData::IMG_INT_ICON);
 		if (icon && icon->isValid()) {
-			szIcon.cx = icon->width();
-			szIcon.cy = icon->height();
-			incSizeToMinimum(szIcon);
+			// Save the icon size.
+			if (szIcon.cx == 0) {
+				szIcon.cx = icon->width();
+				szIcon.cy = icon->height();
+				incSizeToMinimum(szIcon);
+			}
 
 			// Add the icon width.
 			// The icon will be assigned to a WC_STATIC control.