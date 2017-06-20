GerbilSoft/rom-properties

commit 890d051007e2270e62e2182f366636d0a2b1fd70

Split KeyStore into two separate classes.
- KeyStoreUI: Base class located in libromdata. Uses rp_string.
- KeyStoreQt: Qt glue code. Connects notification signals.

All of the important logic is in KeyStoreUI.

This will make it easier to implement KeyManagerTab on Windows.

src/kde/config/KeyStoreModel.cpp
@@ -20,7 +20,8 @@
  ***************************************************************************/
 
 #include "KeyStoreModel.hpp"
-#include "KeyStore.hpp"
+#include "KeyStoreQt.hpp"
+#include "RpQt.hpp"
 
 // Qt includes.
 #include <QtGui/QFont>
@@ -43,7 +44,7 @@ class KeyStoreModelPrivate
 		Q_DISABLE_COPY(KeyStoreModelPrivate)
 
 	public:
-		KeyStore *keyStore;
+		KeyStoreQt *keyStore;
 
 		// Style variables.
 		struct style_t {
@@ -260,17 +261,18 @@ QVariant KeyStoreModel::data(const QModelIndex& index, int role) const
 	}
 
 	// Key index.
-	const KeyStore::Key *key = d->keyStore->getKey(LOWORD(id), HIWORD(id));
+	const KeyStoreQt::Key *key = d->keyStore->getKey(LOWORD(id), HIWORD(id));
 	if (!key)
 		return QVariant();
 
+	// TODO: Make KeyStoreUI templated so we can use QString directly?
 	switch (role) {
 		case Qt::DisplayRole:
 			switch (index.column()) {
 				case COL_KEY_NAME:
-					return key->name;
+					return RP2Q(key->name);
 				case COL_VALUE:
-					return key->value;
+					return RP2Q(key->value);
 				default:
 					break;
 			}
@@ -279,7 +281,7 @@ QVariant KeyStoreModel::data(const QModelIndex& index, int role) const
 		case Qt::EditRole:
 			switch (index.column()) {
 				case COL_VALUE:
-					return key->value;
+					return RP2Q(key->value);
 				default:
 					break;
 			}
@@ -292,19 +294,19 @@ QVariant KeyStoreModel::data(const QModelIndex& index, int role) const
 				case COL_ISVALID:
 					switch (key->status) {
 						default:
-						case KeyStore::Key::Status_Unknown:
+						case KeyStoreQt::Key::Status_Unknown:
 							// Unknown...
 							return d->style.pxmIsValid_unknown;
-						case KeyStore::Key::Status_NotAKey:
+						case KeyStoreQt::Key::Status_NotAKey:
 							// The key data is not in the correct format.
 							return d->style.pxmIsValid_invalid;
-						case KeyStore::Key::Status_Empty:
+						case KeyStoreQt::Key::Status_Empty:
 							// Empty key.
 							break;
-						case KeyStore::Key::Status_Incorrect:
+						case KeyStoreQt::Key::Status_Incorrect:
 							// Key is incorrect.
 							return d->style.pxmIsValid_invalid;
-						case KeyStore::Key::Status_OK:
+						case KeyStoreQt::Key::Status_OK:
 							// Key is correct.
 							return d->style.pxmIsValid_good;
 					}
@@ -379,9 +381,9 @@ bool KeyStoreModel::setData(const QModelIndex& index, const QVariant& value, int
 
 	// Edit the value.
 	// TODO: Make sure it's hexadecimal, and verify the key.
-	// KeyStore::setKey() will emit a signal if the value changes,
+	// KeyStoreQt::setKey() will emit a signal if the value changes,
 	// which will cause KeyStoreModel to emit dataChanged().
-	d->keyStore->setKey(LOWORD(id), HIWORD(id), value.toString());
+	d->keyStore->setKey(LOWORD(id), HIWORD(id), Q2RP(value.toString()));
 	return true;
 }
 
@@ -438,7 +440,7 @@ QVariant KeyStoreModel::headerData(int section, Qt::Orientation orientation, int
  * Set the KeyStore to use in this model.
  * @param keyStore KeyStore.
  */
-void KeyStoreModel::setKeyStore(KeyStore *keyStore)
+void KeyStoreModel::setKeyStore(KeyStoreQt *keyStore)
 {
 	Q_D(KeyStoreModel);
 	if (d->keyStore == keyStore) {
@@ -505,7 +507,7 @@ void KeyStoreModel::setKeyStore(KeyStore *keyStore)
  * Get the KeyStore in use by this model.
  * @return KeyStore.
  */
-KeyStore *KeyStoreModel::keyStore(void) const
+KeyStoreQt *KeyStoreModel::keyStore(void) const
 {
 	Q_D(const KeyStoreModel);
 	return d->keyStore;

src/kde/CMakeLists.txt
@@ -15,7 +15,7 @@ SET(rom-properties-kde_SRCS
 	config/ImageTypesTab.cpp
 	config/DownloadsTab.cpp
 	config/QTreeViewOpt.cpp
-	config/KeyStore.cpp
+	config/KeyStoreQt.cpp
 	config/KeyStoreModel.cpp
 	config/KeyStoreItemDelegate.cpp
 	config/MessageWidget.cpp
@@ -32,7 +32,7 @@ SET(rom-properties-kde_H
 	config/ImageTypesTab.hpp
 	config/DownloadsTab.hpp
 	config/QTreeViewOpt.hpp
-	config/KeyStore.hpp
+	config/KeyStoreQt.hpp
 	config/KeyStoreModel.hpp
 	config/KeyStoreItemDelegate.hpp
 	config/MessageWidget.hpp

src/kde/config/KeyManagerTab.cpp
@@ -20,8 +20,9 @@
  ***************************************************************************/
 
 #include "KeyManagerTab.hpp"
+#include "RpQt.hpp"
 
-#include "KeyStore.hpp"
+#include "KeyStoreQt.hpp"
 #include "KeyStoreModel.hpp"
 #include "KeyStoreItemDelegate.hpp"
 
@@ -49,7 +50,7 @@ class KeyManagerTabPrivate
 
 	public:
 		// KeyStore.
-		KeyStore *keyStore;
+		KeyStoreQt *keyStore;
 		// KeyStoreModel.
 		KeyStoreModel *keyStoreModel;
 
@@ -65,14 +66,14 @@ class KeyManagerTabPrivate
 		 * @param iret ImportReturn.
 		 */
 		void showKeyImportReturnStatus(const QString &filename,
-			const QString &keyType, const KeyStore::ImportReturn &iret);
+			const QString &keyType, const KeyStoreQt::ImportReturn &iret);
 };
 
 /** KeyManagerTabPrivate **/
 
 KeyManagerTabPrivate::KeyManagerTabPrivate(KeyManagerTab* q)
 	: q_ptr(q)
-	, keyStore(new KeyStore(q))
+	, keyStore(new KeyStoreQt(q))
 	, keyStoreModel(new KeyStoreModel(q))
 {
 	// Set the KeyStoreModel's KeyStore.
@@ -107,47 +108,47 @@ void KeyManagerTabPrivate::resizeColumnsToContents(void)
 void KeyManagerTabPrivate::showKeyImportReturnStatus(
 	const QString &filename,
 	const QString &keyType,
-	const KeyStore::ImportReturn &iret)
+	const KeyStoreQt::ImportReturn &iret)
 {
 	QString msg;
 	MessageWidget::MsgIcon icon;
 	bool showKeyStats = false;
 	switch (iret.status) {
-		case KeyStore::Import_InvalidParams:
+		case KeyStoreQt::Import_InvalidParams:
 		default:
 			msg = KeyManagerTab::tr("An invalid parameter was passed to the key importer.\nTHIS IS A BUG; please report this to the developers!");
 			icon = MessageWidget::ICON_CRITICAL;
 			break;
 
-		case KeyStore::Import_OpenError:
+		case KeyStoreQt::Import_OpenError:
 			// TODO: Show error.
 			msg = KeyManagerTab::tr("An error occurred while opening '%1'.")
 				.arg(QFileInfo(filename).fileName());
 			icon = MessageWidget::ICON_CRITICAL;
 			break;
 
-		case KeyStore::Import_ReadError:
+		case KeyStoreQt::Import_ReadError:
 			// TODO: Show error.
 			msg = KeyManagerTab::tr("An error occurred while reading '%1'.")
 				.arg(QFileInfo(filename).fileName());
 			icon = MessageWidget::ICON_CRITICAL;
 			break;
 
-		case KeyStore::Import_InvalidFile:
+		case KeyStoreQt::Import_InvalidFile:
 			msg = KeyManagerTab::tr("The file '%1' is not a %2 file.")
 				.arg(QFileInfo(filename).fileName())
 				.arg(keyType);
 			icon = MessageWidget::ICON_WARNING;
 			break;
 
-		case KeyStore::Import_NoKeysImported:
+		case KeyStoreQt::Import_NoKeysImported:
 			msg = KeyManagerTab::tr("No keys were imported from '%1'.")
 				.arg(QFileInfo(filename).fileName());
 			icon = MessageWidget::ICON_WARNING;
 			showKeyStats = true;
 			break;
 
-		case KeyStore::Import_KeysImported:
+		case KeyStoreQt::Import_KeysImported:
 			msg = KeyManagerTab::tr("%Ln key(s) were imported from '%1'.",
 				 nullptr, iret.keysImportedVerify + iret.keysImportedNoVerify)
 				.arg(QFileInfo(filename).fileName());
@@ -289,13 +290,13 @@ void KeyManagerTab::save(QSettings *pSettings)
 	// KeyStore templated and make this a virtual function?
 	const int totalKeyCount = d->keyStore->totalKeyCount();
 	for (int i = 0; i < totalKeyCount; i++) {
-		const KeyStore::Key *const pKey = d->keyStore->getKey(i);
+		const KeyStoreQt::Key *const pKey = d->keyStore->getKey(i);
 		assert(pKey != nullptr);
 		if (!pKey || !pKey->modified)
 			continue;
 
 		// Save this key.
-		pSettings->setValue(pKey->name, pKey->value);
+		pSettings->setValue(RP2Q(pKey->name), RP2Q(pKey->value));
 	}
 
 	// End of [Keys]
@@ -320,7 +321,7 @@ void KeyManagerTab::on_actionImportWiiKeysBin_triggered(void)
 		return;
 
 	Q_D(KeyManagerTab);
-	KeyStore::ImportReturn iret = d->keyStore->importWiiKeysBin(filename);
+	KeyStoreQt::ImportReturn iret = d->keyStore->importWiiKeysBin(Q2RP(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("Wii keys.bin"), iret);
 }
 
@@ -337,7 +338,7 @@ void KeyManagerTab::on_actionImport3DSboot9bin_triggered(void)
 		return;
 
 	Q_D(KeyManagerTab);
-	KeyStore::ImportReturn iret = d->keyStore->import3DSboot9bin(filename);
+	KeyStoreQt::ImportReturn iret = d->keyStore->import3DSboot9bin(Q2RP(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("3DS boot9.bin"), iret);
 }
 
@@ -354,6 +355,6 @@ void KeyManagerTab::on_actionImport3DSaeskeydb_triggered(void)
 		return;
 
 	Q_D(KeyManagerTab);
-	KeyStore::ImportReturn iret = d->keyStore->import3DSaeskeydb(filename);
+	KeyStoreQt::ImportReturn iret = d->keyStore->import3DSaeskeydb(Q2RP(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("3DS aeskeydb.bin"), iret);
 }

src/kde/config/KeyStoreModel.hpp
@@ -25,13 +25,13 @@
 // Qt includes.
 #include <QtCore/QAbstractItemModel>
 
-class KeyStore;
+class KeyStoreQt;
 
 class KeyStoreModelPrivate;
 class KeyStoreModel : public QAbstractItemModel
 {
 	Q_OBJECT
-	Q_PROPERTY(KeyStore* keyStore READ keyStore WRITE setKeyStore NOTIFY keyStoreChanged)
+	Q_PROPERTY(KeyStoreQt* keyStore READ keyStore WRITE setKeyStore NOTIFY keyStoreChanged)
 
 	public:
 		explicit KeyStoreModel(QObject *parent = 0);
@@ -73,13 +73,13 @@ class KeyStoreModel : public QAbstractItemModel
 		 * Set the KeyStore to use in this model.
 		 * @param keyStore KeyStore.
 		 */
-		void setKeyStore(KeyStore *keyStore);
+		void setKeyStore(KeyStoreQt *keyStore);
 
 		/**
 		 * Get the KeyStore in use by this model.
 		 * @return KeyStore.
 		 */
-		KeyStore *keyStore(void) const;
+		KeyStoreQt *keyStore(void) const;
 
 	signals:
 		/**

src/kde/config/KeyStoreQt.cpp
@@ -1,0 +1,74 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (KDE)                              *
+ * KeyStoreQt.cpp: Key store object for Qt.                                *
+ *                                                                         *
+ * Copyright (c) 2012-2017 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "KeyStoreQt.hpp"
+#include "RpQt.hpp"
+
+/**
+ * Create a new KeyStore object.
+ * @param parent Parent object.
+ */
+KeyStoreQt::KeyStoreQt(QObject *parent)
+	: super(parent)
+{
+	// Load the keys.
+	reset();
+}
+
+KeyStoreQt::~KeyStoreQt()
+{ }
+
+/** Pure virtual functions for Qt signals. **/
+
+/**
+ * A key has changed.
+ * @param sectIdx Section index.
+ * @param keyIdx Key index.
+ */
+void KeyStoreQt::keyChanged_int(int sectIdx, int keyIdx)
+{
+	emit keyChanged(sectIdx, keyIdx);
+}
+
+/**
+ * A key has changed.
+ * @param idx Flat key index.
+ */
+void KeyStoreQt::keyChanged_int(int idx)
+{
+	emit keyChanged(idx);
+}
+
+/**
+ * All keys have changed.
+ */
+void KeyStoreQt::allKeysChanged_int(void)
+{
+	emit allKeysChanged();
+}
+
+/**
+ * KeyStore has been changed by the user.
+ */
+void KeyStoreQt::modified_int(void)
+{
+	emit modified();
+}

src/kde/config/KeyStoreQt.hpp
@@ -1,0 +1,98 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (KDE)                              *
+ * KeyStoreQt.hpp: Key store object for Qt.                                *
+ *                                                                         *
+ * Copyright (c) 2012-2017 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_KDE_CONFIG_KEYSTOREQT_HPP__
+#define __ROMPROPERTIES_KDE_CONFIG_KEYSTOREQT_HPP__
+
+#include "libromdata/crypto/KeyStoreUI.hpp"
+#include <QtCore/QObject>
+
+class KeyStoreQt : public QObject, public LibRomData::KeyStoreUI
+{
+	Q_OBJECT
+
+	Q_PROPERTY(int totalKeyCount READ totalKeyCount)
+	// NOTE: modified() isn't a modifier, since it's only emitted
+	// if d->changed becomes true.
+	Q_PROPERTY(bool changed READ hasChanged)
+
+	public:
+		KeyStoreQt(QObject *parent = 0);
+		virtual ~KeyStoreQt();
+
+	private:
+		typedef QObject super;
+		Q_DISABLE_COPY(KeyStoreQt)
+
+	protected:
+		/** Pure virtual functions for Qt signals. **/
+
+		/**
+		 * A key has changed.
+		 * @param sectIdx Section index.
+		 * @param keyIdx Key index.
+		 */
+		virtual void keyChanged_int(int sectIdx, int keyIdx) override final;
+
+		/**
+		 * A key has changed.
+		 * @param idx Flat key index.
+		 */
+		virtual void keyChanged_int(int idx) override final;
+
+		/**
+		 * All keys have changed.
+		 */
+		virtual void allKeysChanged_int(void) override final;
+
+		/**
+		 * KeyStore has been changed by the user.
+		 */
+		virtual void modified_int(void) override final;
+
+	signals:
+		/** Qt signals. **/
+
+		/**
+		 * A key has changed.
+		 * @param sectIdx Section index.
+		 * @param keyIdx Key index.
+		 */
+		void keyChanged(int sectIdx, int keyIdx);
+
+		/**
+		 * A key has changed.
+		 * @param idx Flat key index.
+		 */
+		void keyChanged(int idx);
+
+		/**
+		 * All keys have changed.
+		 */
+		void allKeysChanged(void);
+
+		/**
+		 * KeyStore has been changed by the user.
+		 */
+		void modified(void);
+};
+
+#endif /* __ROMPROPERTIES_KDE_CONFIG_KEYSTOREQT_HPP__ */

src/libromdata/CMakeLists.txt
@@ -117,10 +117,12 @@ IF(ENABLE_DECRYPTION)
 	SET(libromdata_CRYPTO_SRCS
 		crypto/CtrKeyScrambler.cpp
 		crypto/N3DSVerifyKeys.cpp
+		crypto/KeyStoreUI.cpp
 		)
 	SET(libromdata_CRYPTO_H
 		crypto/CtrKeyScrambler.hpp
 		crypto/N3DSVerifyKeys.hpp
+		crypto/KeyStoreUI.hpp
 		)
 ENDIF(ENABLE_DECRYPTION)
 

commit 88837c024f4f771126f6134575b27c1f6cb95d4d

[kde] KeyStoreModel: Use RP2Q() for d->keyStore->sectName().
KeyStoreUI returns const rp_char*, not QString. Returning an rp_char*
as QVariant confuses Qt, so it ends up showing the value as "true".

This fixes a regression from commit 890d051007e2270e62e2182f366636d0a2b1fd70.
(Split KeyStore into two separate classes.)

src/kde/config/KeyStoreModel.cpp
@@ -251,7 +251,7 @@ QVariant KeyStoreModel::data(const QModelIndex& index, int role) const
 
 		switch (role) {
 			case Qt::DisplayRole:
-				return d->keyStore->sectName(LOWORD(id));
+				return RP2Q(d->keyStore->sectName(LOWORD(id)));
 			default:
 				break;
 		}