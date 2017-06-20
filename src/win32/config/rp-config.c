GerbilSoft/rom-properties

commit 89bbc04815ae2093a4ba4162764dacf4414538eb

[win32] rp-config.c: FreeLibrary(hRpDll); before returning.
Again, not strictly necessary, but Coverity complains about it.

This fixes Coverity#1445810. (resource leak)

src/win32/config/rp-config.c
@@ -91,9 +91,12 @@ static const wchar_t CLSIDs[4][40] = {
 			/* Run the function. */ \
 			free(exe_path); \
 			free(dll_filename); \
-			if (hkeyCLSID) \
+			if (hkeyCLSID) { \
 				RegCloseKey(hkeyCLSID); \
-			return pfn(nullptr, hInstance, lpCmdLine, nCmdShow); \
+			} \
+			int ret = pfn(nullptr, hInstance, lpCmdLine, nCmdShow); \
+			FreeLibrary(hRpDll); \
+			return ret; \
 		} \
 		FreeLibrary(hRpDll); \
 	} \
@@ -219,7 +222,8 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
 fail:
 	free(exe_path);
 	free(dll_filename);
-	if (hkeyCLSID)
-		RegCloseKey(hkeyCLSID); \
+	if (hkeyCLSID) {
+		RegCloseKey(hkeyCLSID);
+	}
 	return EXIT_FAILURE;
 }

commit 2a169ac4503ced4ecfed30d39064f555469b34dd

[win32] rp-config.c: Fixed the build in MSVC 2010.
This fixes a regression from commit 89bbc04815ae2093a4ba4162764dacf4414538eb.
([win32] rp-config.c: FreeLibrary(hRpDll); before returning.)

src/win32/config/rp-config.c
@@ -89,12 +89,13 @@ static const wchar_t CLSIDs[4][40] = {
 		PFNRPSHOWCONFIGDIALOG pfn = (PFNRPSHOWCONFIGDIALOG)GetProcAddress(hRpDll, "rp_show_config_dialog"); \
 		if (pfn) { \
 			/* Run the function. */ \
+			int ret; \
 			free(exe_path); \
 			free(dll_filename); \
 			if (hkeyCLSID) { \
 				RegCloseKey(hkeyCLSID); \
 			} \
-			int ret = pfn(nullptr, hInstance, lpCmdLine, nCmdShow); \
+			ret = pfn(nullptr, hInstance, lpCmdLine, nCmdShow); \
 			FreeLibrary(hRpDll); \
 			return ret; \
 		} \