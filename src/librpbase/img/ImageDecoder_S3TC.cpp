bafe9f784a5c673d05ac8b07aa963b24c90f4c7a
David Korth <gerbilsoft@gerbilsoft.com>
2017-07-22 13:05:34 -0400

    [librpbase] ImageDecoder_S3TC.cpp: Factor out DXT1 tile palette calculation.

--- src/librpbase/img/ImageDecoder_S3TC.cpp
+++ src/librpbase/img/ImageDecoder_S3TC.cpp
@@ -28,6 +28,12 @@
 
 namespace LibRpBase {
 
+// DXT1 block format.
+struct dxt1_block {
+	uint16_t color[2];	// Colors 0 and 1, in RGB565 format.
+	uint32_t indexes;	// Two-bit color indexes.
+};
+
 // ARGB32 value with byte accessors.
 union argb32_t {
 	struct {
@@ -45,6 +51,47 @@ union argb32_t {
 	};
 	uint32_t u32;
 };
+
+/**
+ * Decode DXT1 tile palettes.
+ * @tparam big_endian If true, use big-endian value decoding.
+ * @tparam col3_black If true, color 3 is black. Otherwise, color 3 is transparent.
+ * @param pal		[out] Array of four argb32_t values.
+ * @param dxt1_src	[in] DXT1 block.
+ */
+template<bool big_endian, bool col3_black>
+static inline void decode_DXT1_tile_palette(argb32_t pal[4], const dxt1_block *dxt1_src)
+{
+	// Convert the first two colors from RGB565.
+	if (big_endian) {
+		pal[0].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(be16_to_cpu(dxt1_src->color[0]));
+		pal[1].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(be16_to_cpu(dxt1_src->color[1]));
+	} else {
+		pal[0].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(le16_to_cpu(dxt1_src->color[0]));
+		pal[1].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(le16_to_cpu(dxt1_src->color[1]));
+	}
+
+	// Calculate the second two colors.
+	if (pal[0].u32 > pal[1].u32) {
+		pal[2].r = ((2 * pal[0].r) + pal[1].r) / 3;
+		pal[2].g = ((2 * pal[0].g) + pal[1].g) / 3;
+		pal[2].b = ((2 * pal[0].b) + pal[1].b) / 3;
+		pal[2].a = 0xFF;
+
+		pal[3].r = ((2 * pal[1].r) + pal[0].r) / 3;
+		pal[3].g = ((2 * pal[1].g) + pal[0].g) / 3;
+		pal[3].b = ((2 * pal[1].b) + pal[0].b) / 3;
+		pal[3].a = 0xFF;
+	} else {
+		pal[2].r = (pal[0].r + pal[1].r) / 2;
+		pal[2].g = (pal[0].g + pal[1].g) / 2;
+		pal[2].b = (pal[0].b + pal[1].b) / 2;
+		pal[2].a = 0xFF;
+
+		// Black and/or transparent.
+		pal[3].u32 = (col3_black ? 0xFF000000 : 0x00000000);
+	}
+}
 
 /**
  * Convert a GameCube DXT1 image to rp_image.
@@ -81,15 +128,7 @@ rp_image *ImageDecoder::fromDXT1_GCN(int width, int height,
 
 	// Create an rp_image.
 	rp_image *img = new rp_image(width, height, rp_image::FORMAT_ARGB32);
-
-	// DXT1 block format.
-	struct dxt1_block {
-		uint16_t color[2];	// Colors 0 and 1, in RGB565 format.
-		uint32_t indexes;	// Two-bit color indexes.
-	};
 	const dxt1_block *dxt1_src = reinterpret_cast<const dxt1_block*>(img_buf);
-
-	argb32_t pal[4];		// 4-color palette per block.
 
 	// Temporary tile buffer.
 	uint32_t tileBuf[4*4];
@@ -100,31 +139,11 @@ rp_image *ImageDecoder::fromDXT1_GCN(int width, int height,
 	for (unsigned int x = 0; x < tilesX; x += 2) {
 		for (unsigned int y2 = 0; y2 < 2; y2++) {
 		for (unsigned int x2 = 0; x2 < 2; x2++, dxt1_src++) {
-			// Convert the first two colors from RGB565.
-			pal[0].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(be16_to_cpu(dxt1_src->color[0]));
-			pal[1].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(be16_to_cpu(dxt1_src->color[1]));
-
-			// Calculate the second two colors.
-			if (pal[0].u32 > pal[1].u32) {
-				pal[2].r = ((2 * pal[0].r) + pal[1].r) / 3;
-				pal[2].g = ((2 * pal[0].g) + pal[1].g) / 3;
-				pal[2].b = ((2 * pal[0].b) + pal[1].b) / 3;
-				pal[2].a = 0xFF;
-
-				pal[3].r = ((2 * pal[1].r) + pal[0].r) / 3;
-				pal[3].g = ((2 * pal[1].g) + pal[0].g) / 3;
-				pal[3].b = ((2 * pal[1].b) + pal[0].b) / 3;
-				pal[3].a = 0xFF;
-			} else {
-				pal[2].r = (pal[0].r + pal[1].r) / 2;
-				pal[2].g = (pal[0].g + pal[1].g) / 2;
-				pal[2].b = (pal[0].b + pal[1].b) / 2;
-				pal[2].a = 0xFF;
-
-				// TODO: This may be either black or transparent.
-				// Provide a way to specify that.
-				pal[3].u32 = 0;
-			}
+			// Decode the DXT1 tile palette.
+			// TODO: Color 3 may be either black or transparent.
+			// Figure out if there's a way to specify that in GVR.
+			argb32_t pal[4];
+			decode_DXT1_tile_palette<true, false>(pal, dxt1_src);
 
 			// Process the 16 color indexes.
 			// NOTE: MSB has the left-most pixel of the *bottom* row.
@@ -179,47 +198,18 @@ rp_image *ImageDecoder::fromDXT1(int width, int height,
 
 	// Create an rp_image.
 	rp_image *img = new rp_image(width, height, rp_image::FORMAT_ARGB32);
-
-	// DXT1 block format.
-	struct dxt1_block {
-		uint16_t color[2];	// Colors 0 and 1, in RGB565 format.
-		uint32_t indexes;	// Two-bit color indexes.
-	};
 	const dxt1_block *dxt1_src = reinterpret_cast<const dxt1_block*>(img_buf);
-
-	argb32_t pal[4];		// 4-color palette per block.
 
 	// Temporary tile buffer.
 	uint32_t tileBuf[4*4];
 
 	for (unsigned int y = 0; y < tilesY; y++) {
 	for (unsigned int x = 0; x < tilesX; x++, dxt1_src++) {
-		// Convert the first two colors from RGB565.
-		// TODO: Big-endian support?
-		pal[0].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(le16_to_cpu(dxt1_src->color[0]));
-		pal[1].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(le16_to_cpu(dxt1_src->color[1]));
-
-		// Calculate the second two colors.
-		if (pal[0].u32 > pal[1].u32) {
-			pal[2].r = ((2 * pal[0].r) + pal[1].r) / 3;
-			pal[2].g = ((2 * pal[0].g) + pal[1].g) / 3;
-			pal[2].b = ((2 * pal[0].b) + pal[1].b) / 3;
-			pal[2].a = 0xFF;
-
-			pal[3].r = ((2 * pal[1].r) + pal[0].r) / 3;
-			pal[3].g = ((2 * pal[1].g) + pal[0].g) / 3;
-			pal[3].b = ((2 * pal[1].b) + pal[0].b) / 3;
-			pal[3].a = 0xFF;
-		} else {
-			pal[2].r = (pal[0].r + pal[1].r) / 2;
-			pal[2].g = (pal[0].g + pal[1].g) / 2;
-			pal[2].b = (pal[0].b + pal[1].b) / 2;
-			pal[2].a = 0xFF;
-
-			// TODO: This may be either black or transparent.
-			// Provide a way to specify that.
-			pal[3].u32 = 0;
-		}
+		// Decode the DXT1 tile palette.
+		// TODO: Color 3 may be either black or transparent.
+		// Figure out if there's a way to specify that in DDS.
+		argb32_t pal[4];
+		decode_DXT1_tile_palette<false, false>(pal, dxt1_src);
 
 		// Process the 16 color indexes.
 		uint32_t indexes = dxt1_src->indexes;
