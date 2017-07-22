GerbilSoft/rom-properties

commit 90be8c66efb49693512f7919b215b5dadff4384f

[librpbase] ImageDecoder_S3TC: Fix DXTn_PALETTE_COLOR0_LE_COLOR1 handling.
We ended up always taking the `c0 > c1` path if the flag wasn't specified,
which is wrong for DXT1 and DXT5, and we ended up handling DXT3 as if it
was DXT5.

The end result was GameCube DXT1 GVRs weren't showing up properly,
among other things.

This fixes a regression from commit 46faa64bba2472f5001ca901f50fbede8568869a.
([libromdata] DirectDrawSurface: Added support for DXT3-encoded textures.)

src/librpbase/img/ImageDecoder_S3TC.cpp
@@ -68,7 +68,7 @@ static inline void decode_DXTn_tile_color_palette(argb32_t pal[4], const dxt1_bl
 	pal[1].u32 = ImageDecoderPrivate::RGB565_to_ARGB32(c1);
 
 	// Calculate the second two colors.
-	if (!(flags & DXTn_PALETTE_COLOR0_LE_COLOR1) || (c0 > c1)) {
+	if (!(flags & DXTn_PALETTE_COLOR0_LE_COLOR1) && (c0 > c1)) {
 		// color0 > color1
 		pal[2].r = ((2 * pal[0].r) + pal[1].r) / 3;
 		pal[2].g = ((2 * pal[0].g) + pal[1].g) / 3;

commit a4cbf470cc08473b4fec690b9d2ada14dc3a0a54

[librpbase] ImageDecoder_S3TC: Don't use DXTn_PALETTE_COLOR0_LE_COLOR1 for DXT3.
Khronos says it's needed, but it seems to cause random garbage pixels to
appear on the image. I found another implementation that has similar code
and does not use the DXT3 mode for DXT3.

https://github.com/kchapelier/decode-dxt/tree/master/lib

This fixes a regression from commit 90be8c66efb49693512f7919b215b5dadff4384f.
([librpbase] ImageDecoder_S3TC: Fix DXTn_PALETTE_COLOR0_LE_COLOR1 handling.)

src/librpbase/img/ImageDecoder_S3TC.cpp
@@ -375,7 +375,10 @@ rp_image *ImageDecoder::fromDXT3(int width, int height,
 	for (unsigned int x = 0; x < tilesX; x++, dxt3_src++) {
 		// Decode the DXT3 tile palette.
 		argb32_t pal[4];
-		decode_DXTn_tile_color_palette<DXTn_PALETTE_COLOR0_LE_COLOR1>(pal, &dxt3_src->colors);
+		// FIXME: DXTn_PALETTE_COLOR0_LE_COLOR1 seems to result in garbage pixels.
+		// https://github.com/kchapelier/decode-dxt/tree/master/lib has similar code
+		// but handles DXT3 like both DXT1 and DXT5, so disable this for now.
+		decode_DXTn_tile_color_palette<0/*DXTn_PALETTE_COLOR0_LE_COLOR1*/>(pal, &dxt3_src->colors);
 
 		// Process the 16 color indexes and apply alpha.
 		uint32_t indexes = dxt3_src->colors.indexes;