GerbilSoft/rom-properties

commit b5df196e42bfb55c753bf23fe0651a912ad107f2

[librpbase] RomData: Added a new image type IMG_INT_IMAGE.
This is now used by SegaPVR, since it contains an image, not an icon.

Updated all Image Types configuration code to handle this change.

[win32] resource.rc: Adjusted control positions to accomodate the wider
ImageTypesTab.

src/libromdata/config/TImageTypesConfig.cpp
@@ -49,6 +49,7 @@ const rp_char *const TImageTypesConfig<ComboBox>::imageTypeNames[IMG_TYPE_COUNT]
 	_RP("Internal\nIcon"),
 	_RP("Internal\nBanner"),
 	_RP("Internal\nMedia"),
+	_RP("Internal\nImage"),
 	_RP("External\nMedia"),
 	_RP("External\nCover"),
 	_RP("External\n3D Cover"),

src/libromdata/config/TImageTypesConfig.hpp
@@ -122,7 +122,7 @@ class TImageTypesConfig
 		{
 			assert(sys < SYS_COUNT);
 			assert(imageType < IMG_TYPE_COUNT);
-			return (sys << 3) | imageType;
+			return (sys << 4) | imageType;
 		}
 
 		/**
@@ -132,9 +132,9 @@ class TImageTypesConfig
 		 */
 		static inline unsigned int imageTypeFromCbid(unsigned int cbid)
 		{
-			assert(cbid < (SYS_COUNT << 3));
-			assert((cbid & 7) < IMG_TYPE_COUNT);
-			return (cbid & 7);
+			assert(cbid < (SYS_COUNT << 4));
+			assert((cbid & 15) < IMG_TYPE_COUNT);
+			return (cbid & 15);
 		}
 
 		/**
@@ -144,9 +144,9 @@ class TImageTypesConfig
 		 */
 		static inline unsigned int sysFromCbid(unsigned int cbid)
 		{
-			assert(cbid < (SYS_COUNT << 3));
-			assert((cbid & 7) < IMG_TYPE_COUNT);
-			return (cbid >> 3);
+			assert(cbid < (SYS_COUNT << 4));
+			assert((cbid & 15) < IMG_TYPE_COUNT);
+			return (cbid >> 4);
 		}
 
 		/**

src/librpbase/config/Config.cpp
@@ -124,6 +124,7 @@ const uint8_t ConfigPrivate::defImgTypePrio[] = {
 	RomData::IMG_EXT_MEDIA,
 	RomData::IMG_EXT_COVER,
 	RomData::IMG_EXT_BOX,
+	RomData::IMG_INT_IMAGE,
 	RomData::IMG_INT_MEDIA,
 	RomData::IMG_INT_ICON,
 	RomData::IMG_INT_BANNER,
@@ -290,6 +291,7 @@ int ConfigPrivate::processConfigLine(const char *section, const char *name, cons
 				"\x07" "IntIcon",
 				"\x09" "IntBanner",
 				"\x08" "IntMedia",
+				"\x08" "IntImage",
 				"\x08" "ExtMedia",
 				"\x08" "ExtCover",
 				"\x0A" "ExtCover3D",

src/kde/config/ImageTypesTab.cpp
@@ -232,7 +232,7 @@ void ImageTypesTabPrivate::addComboBoxStrings(unsigned int cbid, int max_prio)
 	// NOTE: One more string than the total number of image types,
 	// since we have a string for "No".
 	static const char s_values[IMG_TYPE_COUNT+1][4] = {
-		"No", "1", "2", "3", "4", "5", "6", "7", "8"
+		"No", "1", "2", "3", "4", "5", "6", "7", "8", "9"
 	};
 	static_assert(ARRAY_SIZE(s_values) == IMG_TYPE_COUNT+1, "s_values[] is the wrong size.");
 

src/libromdata/SegaPVR.cpp
@@ -244,8 +244,10 @@ const rp_image *SegaPVRPrivate::loadGvrImage(void)
 SegaPVR::SegaPVR(IRpFile *file)
 	: super(new SegaPVRPrivate(this, file))
 {
+	// This class handles texture files.
 	RP_D(SegaPVR);
 	d->className = "SegaPVR";
+	d->fileType = FTYPE_TEXTURE_FILE;
 
 	if (!d->file) {
 		// Could not dup() the file handle.
@@ -459,8 +461,7 @@ const rp_char *const *SegaPVR::supportedFileExtensions(void) const
  */
 uint32_t SegaPVR::supportedImageTypes_static(void)
 {
-	// TODO: "Internal image", not icon.
-	return IMGBF_INT_ICON;
+	return IMGBF_INT_IMAGE;
 }
 
 /**
@@ -485,9 +486,8 @@ vector<RomData::ImageSizeDef> SegaPVR::supportedImageSizes(ImageType imageType) 
 		return vector<ImageSizeDef>();
 	}
 
-	// TODO: "Internal image", not icon.
 	RP_D(SegaPVR);
-	if (!d->isValid || imageType != IMG_INT_ICON) {
+	if (!d->isValid || imageType != IMG_INT_IMAGE) {
 		return vector<ImageSizeDef>();
 	}
 
@@ -513,10 +513,9 @@ uint32_t SegaPVR::imgpf(ImageType imageType) const
 		return 0;
 	}
 
-	// TODO: "Internal image" instead of icon.
 	RP_D(SegaPVR);
-	if (imageType != IMG_INT_ICON) {
-		// Only IMG_INT_ICON is supported by PVR.
+	if (imageType != IMG_INT_IMAGE) {
+		// Only IMG_INT_IMAGE is supported by PVR.
 		return 0;
 	}
 
@@ -697,10 +696,9 @@ int SegaPVR::loadInternalImage(ImageType imageType, const rp_image **pImage)
 		return -ERANGE;
 	}
 
-	// TODO: "Internal image" instead of icon.
 	RP_D(SegaPVR);
-	if (imageType != IMG_INT_ICON) {
-		// Only IMG_INT_ICON is supported by PVR.
+	if (imageType != IMG_INT_IMAGE) {
+		// Only IMG_INT_IMAGE is supported by PVR.
 		*pImage = nullptr;
 		return -ENOENT;
 	} else if (!d->file) {

src/librpbase/RomData.cpp
@@ -442,6 +442,7 @@ const rp_char *RomData::fileType_string(void) const
 		_RP("eMMC Dump"),		// FTYPE_EMMC_DUMP
 		_RP("Title Contents"),		// FTYPE_TITLE_CONTENTS
 		_RP("Firmware Binary"),		// FTYPE_FIRMWARE_BINARY
+		_RP("Texture File"),		// FTYPE_TEXTURE_FILE
 	};
 	static_assert(ARRAY_SIZE(fileType_names) == FTYPE_LAST,
 		"fileType_names[] needs to be updated.");
@@ -632,6 +633,7 @@ const rp_char *RomData::getImageTypeName(ImageType imageType) {
 		_RP("Internal icon"),				// IMG_INT_ICON
 		_RP("Internal banner"),				// IMG_INT_BANNER
 		_RP("Internal media scan"),			// IMG_INT_MEDIA
+		_RP("Internal image"),				// IMG_INT_IMAGE
 		// External
 		_RP("External media scan"),			// IMG_EXT_MEDIA
 		_RP("External cover scan"),			// IMG_EXT_COVER

src/librpbase/RomData.hpp
@@ -227,6 +227,7 @@ class RomData
 			FTYPE_EMMC_DUMP,		// eMMC dump
 			FTYPE_TITLE_CONTENTS,		// Title contents, e.g. NCCH.
 			FTYPE_FIRMWARE_BINARY,		// Firmware binary, e.g. 3DS FIRM.
+			FTYPE_TEXTURE_FILE,		// Texture file, e.g. Sega PVR.
 
 			FTYPE_LAST			// End of FileType.
 		};
@@ -273,6 +274,7 @@ class RomData
 			IMG_INT_ICON = 0,	// Internal icon, e.g. DS launcher icon
 			IMG_INT_BANNER,		// Internal banner, e.g. GameCube discs
 			IMG_INT_MEDIA,		// Internal media scan, e.g. Dreamcast discs
+			IMG_INT_IMAGE,		// Internal image, e.g. PVR images.
 
 			// External images are downloaded from websites,
 			// such as GameTDB.
@@ -284,7 +286,7 @@ class RomData
 
 			// Ranges.
 			IMG_INT_MIN = IMG_INT_ICON,
-			IMG_INT_MAX = IMG_INT_MEDIA,
+			IMG_INT_MAX = IMG_INT_IMAGE,
 			IMG_EXT_MIN = IMG_EXT_MEDIA,
 			IMG_EXT_MAX = IMG_EXT_BOX,
 
@@ -303,6 +305,7 @@ class RomData
 			IMGBF_INT_ICON   = (1 << IMG_INT_ICON),		// Internal icon, e.g. DS launcher icon
 			IMGBF_INT_BANNER = (1 << IMG_INT_BANNER),	// Internal banner, e.g. GameCube discs
 			IMGBF_INT_MEDIA  = (1 << IMG_INT_MEDIA),	// Internal media scan, e.g. Dreamcast discs
+			IMGBF_INT_IMAGE  = (1 << IMG_INT_IMAGE),	// Internal image, e.g. PVR images.
 
 			// External images are downloaded from websites,
 			// such as GameTDB.

src/win32/resource.rc
@@ -106,70 +106,71 @@ BEGIN
 END
 
 /**
- * Configuration: Image type priorities. */
+ * Configuration: Image type priorities.
+ */
 LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
-IDD_CONFIG_IMAGETYPES DIALOGEX 0, 0, 333, 210
+IDD_CONFIG_IMAGETYPES DIALOGEX 0, 0, 365, 210
 STYLE DS_3DLOOK | DS_CONTROL | DS_SHELLFONT | WS_CAPTION | WS_VISIBLE | WS_CHILDWINDOW | WS_TABSTOP
 EXSTYLE WS_EX_NOPARENTNOTIFY | WS_EX_CONTROLPARENT
 CAPTION "Dialog"
 FONT 8, "MS Shell Dlg", 400, 0, 0
 BEGIN
-    LTEXT           "Select the image types you would like to use for each system as its thumbnail image.\nInternal images are contained within the ROM file.\nExternal images are downloaded from an external image database.", IDC_IMAGETYPES_DESC1, 7, 7, 302, 30, SS_LEFT, WS_EX_LEFT
-    LTEXT           "1 = highest priority; 2 = second highest priority; No = ignore\nFIXME: Generate the grid programmatically to avoid spacing issues?", IDC_IMAGETYPES_DESC2, 7, 37, 306, 8, SS_LEFT, WS_EX_LEFT
-    CONTROL         "GameCube, Wii, Wii U, Nintendo DS, and Nintendo 3DS external images\nare provided by <a href=""http://www.gametdb.com/"">GameTDB</a>.\namiibo images are provided by <a href=""http://amiibo.life/"">amiibo.life</a>, the Unofficial amiibo Database.", IDC_IMAGETYPES_CREDITS, "SysLink", 0x50010000, 7, 179, 302, 24, 0x00000000
+    LTEXT           "Select the image types you would like to use for each system as its thumbnail image.\nInternal images are contained within the ROM file.\nExternal images are downloaded from an external image database.", IDC_IMAGETYPES_DESC1, 7, 7, 349, 30, SS_LEFT, WS_EX_LEFT
+    LTEXT           "1 = highest priority; 2 = second highest priority; No = ignore\nFIXME: Generate the grid programmatically to avoid spacing issues?", IDC_IMAGETYPES_DESC2, 7, 37, 349, 8, SS_LEFT, WS_EX_LEFT
+    CONTROL         "GameCube, Wii, Wii U, Nintendo DS, and Nintendo 3DS external images\nare provided by <a href=""http://www.gametdb.com/"">GameTDB</a>.\namiibo images are provided by <a href=""http://amiibo.life/"">amiibo.life</a>, the Unofficial amiibo Database.", IDC_IMAGETYPES_CREDITS, "SysLink", 0x50010000, 7, 179, 349, 24, 0x00000000
 END
 
 /**
  * Configuration: Download settings.
  */
 LANGUAGE LANG_ENGLISH, SUBLANG_ENGLISH_US
-IDD_CONFIG_DOWNLOADS DIALOGEX 0, 0, 333, 210
+IDD_CONFIG_DOWNLOADS DIALOGEX 0, 0, 365, 210
 STYLE DS_3DLOOK | DS_CONTROL | DS_SHELLFONT | WS_VISIBLE | WS_CHILDWINDOW | WS_TABSTOP
 EXSTYLE WS_EX_NOPARENTNOTIFY | WS_EX_CONTROLPARENT
 FONT 8, "MS Shell Dlg", FW_NORMAL, 0, 0
 BEGIN
-    AUTOCHECKBOX    "Enable external image downloads.", IDC_EXTIMGDL, 7, 7, 319, 8, 0, WS_EX_TRANSPARENT
-    AUTOCHECKBOX    "Always use the internal icon (if present) for small sizes.", IDC_INTICONSMALL, 7, 21, 319, 8, 0, WS_EX_TRANSPARENT
-    AUTOCHECKBOX    "Download high-resolution scans if viewing large thumbnails.\nThis may increase bandwidth usage.", IDC_HIGHRESDL, 7, 35, 319, 16, BS_MULTILINE, WS_EX_TRANSPARENT
+    AUTOCHECKBOX    "Enable external image downloads.", IDC_EXTIMGDL, 7, 7, 349, 8, 0, WS_EX_TRANSPARENT
+    AUTOCHECKBOX    "Always use the internal icon (if present) for small sizes.", IDC_INTICONSMALL, 7, 21, 349, 8, 0, WS_EX_TRANSPARENT
+    AUTOCHECKBOX    "Download high-resolution scans if viewing large thumbnails.\nThis may increase bandwidth usage.", IDC_HIGHRESDL, 7, 35, 349, 16, BS_MULTILINE, WS_EX_TRANSPARENT
 END
 
 /**
  * Configuration: Thumbnail cache. (Windows Vista and later)
  */
 LANGUAGE LANG_ENGLISH, SUBLANG_ENGLISH_US
-IDD_CONFIG_CACHE DIALOGEX 0, 0, 333, 210
+IDD_CONFIG_CACHE DIALOGEX 0, 0, 365, 210
 STYLE DS_3DLOOK | DS_CONTROL | DS_SHELLFONT | WS_VISIBLE | WS_CHILDWINDOW | WS_TABSTOP
 EXSTYLE WS_EX_NOPARENTNOTIFY | WS_EX_CONTROLPARENT
 FONT 8, "MS Shell Dlg", FW_NORMAL, 0, 0
 BEGIN
-    LTEXT           "If any image type settings were changed, you will need to clear the system thumbnail cache.", IDC_STATIC, 7, 7, 317, 8, SS_LEFT, WS_EX_TRANSPARENT
-    PUSHBUTTON      "Clear the System Thumbnail Cache", IDC_CACHE_CLEAR_SYS_THUMBS, 7, 20, 317, 28, 0, WS_EX_LEFT
-    LTEXT           "ROM Properties Page maintains its own download cache for external images. Clearing this cache will force external images to be redownloaded.", IDC_STATIC, 7, 55, 317, 17, SS_LEFT, WS_EX_TRANSPARENT
-    PUSHBUTTON      "Clear the ROM Properties Page Download Cache", IDC_CACHE_CLEAR_RP_DL, 7, 75, 317, 28, 0, WS_EX_LEFT
-    LTEXT           "Clearing the cache...", IDC_CACHE_STATUS, 7, 176, 317, 9, NOT WS_VISIBLE | SS_LEFT, WS_EX_LEFT
-    CONTROL         "", IDC_CACHE_PROGRESS, PROGRESS_CLASS, NOT WS_VISIBLE, 7, 187, 317, 16, WS_EX_LEFT
+    LTEXT           "If any image type settings were changed, you will need to clear the system thumbnail cache.", IDC_STATIC, 7, 7, 349, 8, SS_LEFT, WS_EX_TRANSPARENT
+    PUSHBUTTON      "Clear the System Thumbnail Cache", IDC_CACHE_CLEAR_SYS_THUMBS, 7, 20, 349, 28, 0, WS_EX_LEFT
+    LTEXT           "ROM Properties Page maintains its own download cache for external images. Clearing this cache will force external images to be redownloaded.", IDC_STATIC, 7, 55, 349, 17, SS_LEFT, WS_EX_TRANSPARENT
+    PUSHBUTTON      "Clear the ROM Properties Page Download Cache", IDC_CACHE_CLEAR_RP_DL, 7, 75, 349, 28, 0, WS_EX_LEFT
+    LTEXT           "Clearing the cache...", IDC_CACHE_STATUS, 7, 176, 349, 9, NOT WS_VISIBLE | SS_LEFT, WS_EX_LEFT
+    CONTROL         "", IDC_CACHE_PROGRESS, PROGRESS_CLASS, NOT WS_VISIBLE, 7, 187, 349, 16, WS_EX_LEFT
 END
 
 /**
  * Configuration: Thumbnail cache. (Windows XP and earlier)
  */
 LANGUAGE LANG_ENGLISH, SUBLANG_ENGLISH_US
-IDD_CONFIG_CACHE_XP DIALOGEX 0, 0, 333, 210
+IDD_CONFIG_CACHE_XP DIALOGEX 0, 0, 365, 210
 STYLE DS_3DLOOK | DS_CONTROL | DS_SHELLFONT | WS_VISIBLE | WS_CHILDWINDOW | WS_TABSTOP
 EXSTYLE WS_EX_NOPARENTNOTIFY | WS_EX_CONTROLPARENT
 FONT 8, "MS Shell Dlg", FW_NORMAL, 0, 0
 BEGIN
-    LTEXT           "If any image type settings were changed, you will need to clear the thumbnail cache files.\nThis version of Windows does not have a centralized thumbnail database, so it may take a while for all Thumbs.db files to be located and deleted.", IDC_STATIC, 7, 7, 317, 25, SS_LEFT, WS_EX_TRANSPARENT
+    LTEXT           "If any image type settings were changed, you will need to clear the thumbnail cache files.\nThis version of Windows does not have a centralized thumbnail database, so it may take a while for all Thumbs.db files to be located and deleted.", IDC_STATIC, 7, 7, 349, 25, SS_LEFT, WS_EX_TRANSPARENT
     AUTORADIOBUTTON "Find all Thumbs.db files on the specified drive(s):", IDC_CACHE_XP_FIND_DRIVES, 7, 35, 168, 8, WS_GROUP, WS_EX_LEFT
     AUTORADIOBUTTON "Specify a path", IDC_CACHE_XP_FIND_PATH, 180, 35, 61, 8, 0, WS_EX_LEFT
-    CONTROL         "", IDC_CACHE_XP_DRIVES, WC_LISTVIEW, WS_TABSTOP | WS_BORDER | LVS_ALIGNLEFT | LVS_LIST | LVS_SINGLESEL, 7, 46, 317, 40, WS_EX_LEFT | WS_EX_CLIENTEDGE
-    EDITTEXT        IDC_CACHE_XP_PATH, 7, 46, 263, 14, NOT WS_VISIBLE | ES_AUTOHSCROLL, WS_EX_LEFT
-    PUSHBUTTON      "Browse...", IDC_CACHE_XP_BROWSE, 275, 45, 50, 15, NOT WS_VISIBLE, WS_EX_LEFT
-    PUSHBUTTON      "Find and Remove Thumbs.db Files", IDC_CACHE_XP_CLEAR_SYS_THUMBS, 7, 90, 318, 20, WS_DISABLED | BS_MULTILINE, WS_EX_LEFT
+    CONTROL         "", IDC_CACHE_XP_DRIVES, WC_LISTVIEW, WS_TABSTOP | WS_BORDER | LVS_ALIGNLEFT | LVS_LIST | LVS_SINGLESEL, 7, 46, 349, 40, WS_EX_LEFT | WS_EX_CLIENTEDGE
+    EDITTEXT        IDC_CACHE_XP_PATH, 7, 46, 295, 14, NOT WS_VISIBLE | ES_AUTOHSCROLL, WS_EX_LEFT
+    PUSHBUTTON      "Browse...", IDC_CACHE_XP_BROWSE, 307, 45, 50, 15, NOT WS_VISIBLE, WS_EX_LEFT
+    PUSHBUTTON      "Find and Remove Thumbs.db Files", IDC_CACHE_XP_CLEAR_SYS_THUMBS, 7, 90, 350, 20, WS_DISABLED | BS_MULTILINE, WS_EX_LEFT
     LTEXT           "ROM Properties Page maintains its own download cache for external images. Clearing this cache will force external images to be redownloaded.", IDC_STATIC, 7, 125, 265, 20, SS_LEFT, WS_EX_TRANSPARENT
-    PUSHBUTTON      "Clear", IDC_CACHE_CLEAR_RP_DL, 275, 127, 50, 15, 0, WS_EX_LEFT
-    LTEXT           "Clearing the cache...", IDC_CACHE_STATUS, 7, 176, 317, 9, NOT WS_VISIBLE | SS_LEFT, WS_EX_LEFT
-    CONTROL         "", IDC_CACHE_PROGRESS, PROGRESS_CLASS, NOT WS_VISIBLE, 7, 187, 317, 16, WS_EX_LEFT
+    PUSHBUTTON      "Clear", IDC_CACHE_CLEAR_RP_DL, 275, 127, 82, 15, 0, WS_EX_LEFT
+    LTEXT           "Clearing the cache...", IDC_CACHE_STATUS, 7, 176, 349, 9, NOT WS_VISIBLE | SS_LEFT, WS_EX_LEFT
+    CONTROL         "", IDC_CACHE_PROGRESS, PROGRESS_CLASS, NOT WS_VISIBLE, 7, 187, 349, 16, WS_EX_LEFT
 END
 
 #ifdef ENABLE_DECRYPTION
@@ -177,15 +178,15 @@ END
  * Configuration: Key Manager.
  */
 LANGUAGE LANG_NEUTRAL, SUBLANG_NEUTRAL
-IDD_CONFIG_KEYMANAGER DIALOGEX 0, 0, 333, 210
+IDD_CONFIG_KEYMANAGER DIALOGEX 0, 0, 365, 210
 STYLE DS_3DLOOK | DS_CONTROL | DS_SHELLFONT | WS_VISIBLE | WS_CHILDWINDOW | WS_TABSTOP
 EXSTYLE WS_EX_NOPARENTNOTIFY | WS_EX_CONTROLPARENT
 FONT 8, "MS Shell Dlg", FW_NORMAL, 0, 0
 BEGIN
-    CONTROL         "", IDC_KEYMANAGER_LIST, WC_LISTVIEW, WS_TABSTOP | WS_BORDER | LVS_ALIGNLEFT | LVS_REPORT | LVS_OWNERDATA | LVS_SINGLESEL, 7, 7, 317, 178, WS_EX_LEFT | WS_EX_CLIENTEDGE
+    CONTROL         "", IDC_KEYMANAGER_LIST, WC_LISTVIEW, WS_TABSTOP | WS_BORDER | LVS_ALIGNLEFT | LVS_REPORT | LVS_OWNERDATA | LVS_SINGLESEL, 7, 7, 349, 178, WS_EX_LEFT | WS_EX_CLIENTEDGE
     // EDIT control is created as a child control of the ListView.
     //EDITTEXT        IDC_KEYMANAGER_EDIT, 0, 0, 0, 0, NOT WS_VISIBLE | ES_AUTOHSCROLL | ES_WANTRETURN, WS_EX_LEFT
-    PUSHBUTTON      "Import", IDC_KEYMANAGER_IMPORT, 7, 189, 50, 14, 0, WS_EX_LEFT
+    PUSHBUTTON      "Import", IDC_KEYMANAGER_IMPORT, 7, 221, 50, 14, 0, WS_EX_LEFT
 END
 
 /**

commit f4b17f0498b60b942bf0a4b72958c7bd6d03fe26

[libromdata] TImageTypesConfig: Added `IntImage` to conf_imageTypeNames[].
This fixes a crash in `rp-config`.

Renamed imageTypeNames[] to conf_imageTypeNames[] to prevent confusion
with the class-local imageTypeNames[].

Removed the explicit array size. This prevented the `static_assert()`
from working, since the array was always the correct size.

Removed more explicit array sizes from TImageTypesConfig and Config.

This fixes a regression from commit b5df196e42bfb55c753bf23fe0651a912ad107f2.
([librpbase] RomData: Added a new image type IMG_INT_IMAGE.)

src/libromdata/config/TImageTypesConfig.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 // Image type names.
 template<typename ComboBox>
-const rp_char *const TImageTypesConfig<ComboBox>::imageTypeNames[IMG_TYPE_COUNT] = {
+const rp_char *const TImageTypesConfig<ComboBox>::imageTypeNames[] = {
 	_RP("Internal\nIcon"),
 	_RP("Internal\nBanner"),
 	_RP("Internal\nMedia"),
@@ -59,7 +59,7 @@ const rp_char *const TImageTypesConfig<ComboBox>::imageTypeNames[IMG_TYPE_COUNT]
 
 // System data.
 template<typename ComboBox>
-const SysData_t TImageTypesConfig<ComboBox>::sysData[SYS_COUNT] = {
+const SysData_t TImageTypesConfig<ComboBox>::sysData[] = {
 	SysDataEntry(Amiibo,		_RP("amiibo")),
 	SysDataEntry(DreamcastSave,	_RP("Dreamcast Saves")),
 	SysDataEntry(GameCube,		_RP("GameCube / Wii")),
@@ -75,6 +75,8 @@ TImageTypesConfig<ComboBox>::TImageTypesConfig()
 	: changed(false)
 {
 	static_assert(std::is_pointer<ComboBox>::value, "TImageTypesConfig template parameter must be a pointer.");
+	static_assert(ARRAY_SIZE(imageTypeNames) == IMG_TYPE_COUNT, "imageTypeNames[] is the wrong size.");
+	static_assert(ARRAY_SIZE(sysData) == SYS_COUNT, "sysData[] is the wrong size.");
 
 	// Clear the arrays.
 	memset(cboImageType, 0, sizeof(cboImageType));
@@ -323,17 +325,18 @@ int TImageTypesConfig<ComboBox>::save(void)
 
 		// Convert the image type priority to strings.
 		// TODO: Export the string data from Config.
-		static const rp_char *const imageTypeNames[IMG_TYPE_COUNT] = {
+		static const rp_char *const conf_imageTypeNames[] = {
 			_RP("IntIcon"),
 			_RP("IntBanner"),
 			_RP("IntMedia"),
+			_RP("IntImage"),
 			_RP("ExtMedia"),
 			_RP("ExtCover"),
 			_RP("ExtCover3D"),
 			_RP("ExtCoverFull"),
 			_RP("ExtBox"),
 		};
-		static_assert(ARRAY_SIZE(imageTypeNames) == IMG_TYPE_COUNT, "imageTypeNames[] is the wrong size.");
+		static_assert(ARRAY_SIZE(conf_imageTypeNames) == IMG_TYPE_COUNT, "conf_imageTypeNames[] is the wrong size.");
 
 		bool hasOne = false;
 		for (unsigned int i = 0; i < ARRAY_SIZE(imgTypePrio); i++) {
@@ -342,7 +345,7 @@ int TImageTypesConfig<ComboBox>::save(void)
 				if (hasOne)
 					imageTypeList += _RP_CHR(',');
 				hasOne = true;
-				imageTypeList += imageTypeNames[imageType];
+				imageTypeList += conf_imageTypeNames[imageType];
 			}
 		}
 

src/libromdata/config/TImageTypesConfig.hpp
@@ -105,11 +105,11 @@ class TImageTypesConfig
 		int save(void);
 
 	public:
-		// Image type data.
-		static const rp_char *const imageTypeNames[IMG_TYPE_COUNT];
+		// Image type data. (IMG_TYPE_COUNT)
+		static const rp_char *const imageTypeNames[];
 
-		// System data.
-		static const SysData_t sysData[SYS_COUNT];
+		// System data. (SYS_COUNT)
+		static const SysData_t sysData[];
 
 	public:
 		/**

src/librpbase/config/Config.cpp
@@ -287,7 +287,7 @@ int ConfigPrivate::processConfigLine(const char *section, const char *name, cons
 			// MSVC 2015 and gcc-4.5.2. In order to get it to work correctly,
 			// we have to store the length byte separately from the actual
 			// image type name.
-			static const char *const imageTypeNames[RomData::IMG_EXT_MAX+1] = {
+			static const char *const imageTypeNames[] = {
 				"\x07" "IntIcon",
 				"\x09" "IntBanner",
 				"\x08" "IntMedia",