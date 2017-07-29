aa5db8d26137b5f3651f97707d112af179776b06
David Korth <gerbilsoft@gerbilsoft.com>
2017-07-28 20:58:22 -0400

    [librpbase] APNG_dlopen.c: Use atomic functions for the reference counter.
    Probably not perfect, but better than before.

--- src/librpbase/img/APNG_dlopen.c
+++ src/librpbase/img/APNG_dlopen.c
@@ -28,6 +28,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "threads/Atomics.h"
+
 #ifndef _WIN32
 // Unix dlopen()
 #include <dlfcn.h>
@@ -166,21 +168,15 @@ static int init_apng(void)
 int APNG_ref(void)
 {
 	assert(ref_cnt >= 0);
-	if (ref_cnt <= 0) {
+	if (ATOMIC_INC_FETCH(&ref_cnt) == 1) {
+		// First APNG reference.
 		// Attempt to load APNG.
 		if (init_apng() != 0) {
 			// Error loading APNG.
+			ref_cnt = 0;
 			return -1;
 		}
-		if (ref_cnt < 0) {
-			// Invalid reference count...
-			ref_cnt = 0;
-		}
 	}
-
-	// Increment the reference counter.
-	// TODO: Atomic increment?
-	ref_cnt++;
 	return 0;
 }
 
@@ -191,14 +187,13 @@ int APNG_ref(void)
 void APNG_unref(void)
 {
 	assert(ref_cnt > 0);
-	if (ref_cnt <= 0)
-		return;
-
-	if (--ref_cnt == 0) {
+	if (ATOMIC_DEC_FETCH(&ref_cnt) == 0) {
 		// Unload APNG.
 		// TODO: Clear the function pointers?
-		dlclose(libpng_dll);
-		libpng_dll = NULL;
+		if (libpng_dll) {
+			dlclose(libpng_dll);
+			libpng_dll = NULL;
+		}
 	}
 }
 
@@ -211,8 +206,10 @@ void APNG_force_unload(void)
 	if (ref_cnt > 0) {
 		// Unload APNG.
 		// TODO: Clear the function pointers?
-		dlclose(libpng_dll);
-		libpng_dll = NULL;
+		if (libpng_dll) {
+			dlclose(libpng_dll);
+			libpng_dll = NULL;
+		}
 		ref_cnt = 0;
 	}
 }
