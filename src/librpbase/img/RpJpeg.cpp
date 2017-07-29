GerbilSoft/rom-properties

commit 1d9b0d3d12bd321fbf8a03fd5abf386ee4bdc542

[librpbase] RpJpeg, RpPng: Limit image size to 32768x32768.
This matches rp_image.

RpPng previously only checked for a non-zero size, and RpJpeg checked
for a maximum size of 65536x65536. Both now check for non-zero and a
maximum size of 32768x32768.

See commit 7092695d2e7e2bef7609c43cd02b86a524e13edf.
([librpbase] rp_image_backend.cpp: Limit image size to 32768x32768.)

src/librpbase/img/RpJpeg.cpp
@@ -333,13 +333,15 @@ rp_image *RpJpeg::loadUnchecked(IRpFile *file)
 	// b. We passed TRUE to reject a tables-only JPEG file as an error.
 	jpeg_read_header(&cinfo, TRUE);
 
-	// Sanity check: Don't allow images larger than 65536x65536.
-	assert(cinfo.output_width <= 65536);
-	assert(cinfo.output_height <= 65536);
-	if (cinfo.output_width > 65536 ||
-	    cinfo.output_height > 65536)
+	// Sanity check: Don't allow images larger than 32768x32768.
+	assert(cinfo.output_width > 0);
+	assert(cinfo.output_height > 0);
+	assert(cinfo.output_width <= 32768);
+	assert(cinfo.output_height <= 32768);
+	if (cinfo.output_width <= 0 || cinfo.output_height <= 0 ||
+	    cinfo.output_width > 32768 || cinfo.output_height > 32768)
 	{
-		// Image is too big.
+		// Image size is either invalid or too big.
 		jpeg_destroy_decompress(&cinfo);
 		return nullptr;
 	}

src/librpbase/img/RpPng.cpp
@@ -336,8 +336,15 @@ rp_image *RpPngPrivate::loadPng(png_structp png_ptr, png_infop info_ptr)
 	png_uint_32 width, height;
 	png_get_IHDR(png_ptr, info_ptr, &width, &height,
 		&bit_depth, &color_type, nullptr, nullptr, nullptr);
-	if (width <= 0 || height <= 0) {
-		// Invalid image size.
+	// Sanity check: Don't allow images larger than 32768x32768.
+	assert(width > 0);
+	assert(height > 0);
+	assert(width <= 32768);
+	assert(height <= 32768);
+	if (width <= 0 || height <= 0 ||
+	    width > 32768 || height > 32768)
+	{
+		// Image size is either invalid or too big.
 		return nullptr;
 	}
 

commit df6aae9d1bcbc4c9377692f025d35752a69d8490

[librpbase] RpJpeg: Fix incorrect image size assertions.
We need to check `image_width` and `image_height`, not `output_width`
and `output_height`. The latter two variables are always 0 if not
otherwise specified, which resulted in assertion failures. Prior to
the assertions checking for 0, the assertions checking for 65536
didn't work, since it was always 0.

This fixes a regression from commit 1d9b0d3d12bd321fbf8a03fd5abf386ee4bdc542.
([librpbase] RpJpeg, RpPng: Limit image size to 32768x32768.)

src/librpbase/img/RpJpeg.cpp
@@ -334,12 +334,12 @@ rp_image *RpJpeg::loadUnchecked(IRpFile *file)
 	jpeg_read_header(&cinfo, TRUE);
 
 	// Sanity check: Don't allow images larger than 32768x32768.
-	assert(cinfo.output_width > 0);
-	assert(cinfo.output_height > 0);
-	assert(cinfo.output_width <= 32768);
-	assert(cinfo.output_height <= 32768);
-	if (cinfo.output_width <= 0 || cinfo.output_height <= 0 ||
-	    cinfo.output_width > 32768 || cinfo.output_height > 32768)
+	assert(cinfo.image_width > 0);
+	assert(cinfo.image_width > 0);
+	assert(cinfo.image_width <= 32768);
+	assert(cinfo.image_width <= 32768);
+	if (cinfo.image_width <= 0 || cinfo.image_width <= 0 ||
+	    cinfo.image_width > 32768 || cinfo.image_width > 32768)
 	{
 		// Image size is either invalid or too big.
 		jpeg_destroy_decompress(&cinfo);