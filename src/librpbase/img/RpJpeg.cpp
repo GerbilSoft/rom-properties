GerbilSoft/rom-properties

commit df6aae9d1bcbc4c9377692f025d35752a69d8490

[librpbase] RpJpeg: Fix incorrect image size assertions.
We need to check `image_width` and `image_height`, not `output_width`
and `output_height`. The latter two variables are always 0 if not
otherwise specified, which resulted in assertion failures. Prior to
the assertions checking for 0, the assertions checking for 65536
didn't work, since it was always 0.

This fixes a regression from commit 1d9b0d3d12bd321fbf8a03fd5abf386ee4bdc542.
([librpbase] RpJpeg, RpPng: Limit image size to 32768x32768.)

src/librpbase/img/RpJpeg.cpp
@@ -334,12 +334,12 @@ rp_image *RpJpeg::loadUnchecked(IRpFile *file)
 	jpeg_read_header(&cinfo, TRUE);
 
 	// Sanity check: Don't allow images larger than 32768x32768.
-	assert(cinfo.output_width > 0);
-	assert(cinfo.output_height > 0);
-	assert(cinfo.output_width <= 32768);
-	assert(cinfo.output_height <= 32768);
-	if (cinfo.output_width <= 0 || cinfo.output_height <= 0 ||
-	    cinfo.output_width > 32768 || cinfo.output_height > 32768)
+	assert(cinfo.image_width > 0);
+	assert(cinfo.image_width > 0);
+	assert(cinfo.image_width <= 32768);
+	assert(cinfo.image_width <= 32768);
+	if (cinfo.image_width <= 0 || cinfo.image_width <= 0 ||
+	    cinfo.image_width > 32768 || cinfo.image_width > 32768)
 	{
 		// Image size is either invalid or too big.
 		jpeg_destroy_decompress(&cinfo);

commit 72f0d969ca5e13c5608211a503f0cc214c08fed5

[librpbase] RpJpeg: Check width and height, not width and width.
Found using cppcheck-1.81 dev. (git: master/366f219e)

This fixes a regression from commit df6aae9d1bcbc4c9377692f025d35752a69d8490.
([librpbase] RpJpeg: Fix incorrect image size assertions.)

src/librpbase/img/RpJpeg.cpp
@@ -415,11 +415,11 @@ rp_image *RpJpeg::loadUnchecked(IRpFile *file)
 
 	// Sanity check: Don't allow images larger than 32768x32768.
 	assert(cinfo.image_width > 0);
-	assert(cinfo.image_width > 0);
+	assert(cinfo.image_height > 0);
 	assert(cinfo.image_width <= 32768);
-	assert(cinfo.image_width <= 32768);
-	if (cinfo.image_width <= 0 || cinfo.image_width <= 0 ||
-	    cinfo.image_width > 32768 || cinfo.image_width > 32768)
+	assert(cinfo.image_height <= 32768);
+	if (cinfo.image_width <= 0 || cinfo.image_height <= 0 ||
+	    cinfo.image_width > 32768 || cinfo.image_height > 32768)
 	{
 		// Image size is either invalid or too big.
 		jpeg_destroy_decompress(&cinfo);