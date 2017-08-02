5d7da830f352948865936e7bc017ac56b599c579
David Korth <gerbilsoft@gerbilsoft.com>
2017-08-01 22:13:02 -0400

    [librpbase] RpPngWriter: Fix cache_t::set_sBIT() incorrectly checking for invalid sBIT data.
    The `memcmp()` check was inverted. We *don't* want `sBIT` to match
    `sBIT_invalid`, since that would mean it's invalid.

--- src/librpbase/img/RpPngWriter.cpp
+++ src/librpbase/img/RpPngWriter.cpp
@@ -162,7 +162,7 @@ class RpPngWriterPrivate
 			void set_sBIT(const rp_image::sBIT_t* sBIT)
 			{
 				static const rp_image::sBIT_t sBIT_invalid = {0,0,0,0,0};
-				if (sBIT && !memcmp(sBIT, &sBIT_invalid, sizeof(*sBIT))) {
+				if (sBIT && memcmp(sBIT, &sBIT_invalid, sizeof(*sBIT)) != 0) {
 					// sBIT is valid.
 					this->sBIT = *sBIT;
 					has_sBIT = true;
