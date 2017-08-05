GerbilSoft/rom-properties

commit 7835de96f19bf093cc6100efa92f633b1505f3a0

[libunixcommon] New common library for Unix and Unix-like systems.
Moved rp-stub/dll-search.c to libunixcommon.

New file userdirs.cpp, which has three functions:
- getHomeDirectory()
- getCacheDirectory()
- getConfigDirectory()

These are used by librpbase's FileSystem_posix.cpp and also by
rp-thumbnailer-dbus. The rp-thumbnailer-dbus implementation of the
cache directory code was used instead of FileSystem_posix.cpp, since
it supports $XDG_CACHE_HOME.

NOTE: userdirs.cpp does NOT cache any directory names. The caller must
cache the directory names if necessary. Also, since we don't cache the
home directory, an extra lookup is needed if multiple directories are
requested, but this shouldn't be that much of a problem.

rp-thumbnailer-dbus now links to libunixcommon instead of manually
compiling a copy of dll-search.c.

TODO:
- getConfigDirectory(): $XDG_CONFIG_HOME.
- Mac OS X support.

src/librpbase/file/FileSystem_posix.cpp
@@ -21,6 +21,8 @@
 
 #include "FileSystem.hpp"
 
+// libunixcommon
+#include "libunixcommon/userdirs.hpp"
 // libromdata
 #include "TextFuncs.hpp"
 
@@ -29,10 +31,8 @@
 
 // C includes.
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <pwd.h>
 #include <utime.h>
 
 // C includes. (C++ namespace)
@@ -160,65 +160,20 @@ int64_t filesize(const rp_string &filename)
  */
 static void initConfigDirectories(void)
 {
-	/** Home directory. **/
-	// NOTE: The home directory is NOT cached.
-	// It's only used to determine the other directories.
+	// Use LibUnixCommon to get the XDG directories.
 
-	rp_string home_dir;
-	const char *const home = getenv("HOME");
-	if (home && home[0] != 0) {
-		// HOME variable is set.
-		home_dir = utf8_to_rp_string(home);
-	} else {
-		// HOME variable is not set.
-		// Check the user's pwent.
-		// TODO: Can pwd_result be nullptr?
-		// TODO: Check for ENOMEM?
-		// TODO: Support getpwuid() if the system doesn't support getpwuid_r()?
-		char buf[2048];
-		struct passwd pwd;
-		struct passwd *pwd_result;
-		int ret = getpwuid_r(getuid(), &pwd, buf, sizeof(buf), &pwd_result);
-		if (ret != 0 || !pwd_result) {
-			// getpwuid_r() failed.
-			return;
-		}
+	// Cache directory.
+	cache_dir = utf8_to_rp_string(LibUnixCommon::getCacheDirectory());
 
-		if (pwd_result->pw_dir[0] == 0) {
-			// Empty home directory...
-			return;
-		}
-
-		home_dir = utf8_to_rp_string(pwd_result->pw_dir, strlen(pwd_result->pw_dir));
+	// Config directory.
+	config_dir = utf8_to_rp_string(LibUnixCommon::getConfigDirectory());
+	if (!config_dir.empty()) {
+		// Add a trailing slash if necessary.
+		if (config_dir.at(config_dir.size()-1) != '/')
+			config_dir += _RP_CHR('/');
+		// Append "rom-properties".
+		config_dir += _RP("rom-properties");
 	}
-	if (home_dir.empty()) {
-		// Unable to get the home directory...
-		return;
-	}
-
-	/** Cache directory. **/
-	// TODO: Check XDG variables.
-
-	// Unix/Linux: Cache directory is ~/.cache/rom-properties/.
-	// TODO: Mac OS X.
-	cache_dir = home_dir;
-	// Add a trailing slash if necessary.
-	if (cache_dir.at(cache_dir.size()-1) != '/')
-		cache_dir += _RP_CHR('/');
-	// Append ".cache/rom-properties".
-	cache_dir += _RP(".cache/rom-properties");
-
-	/** Configuration directory. **/
-	// TODO: Check XDG variables.
-
-	// Unix/Linux: Cache directory is ~/.config/rom-properties/.
-	// TODO: Mac OS X.
-	config_dir = home_dir;
-	// Add a trailing slash if necessary.
-	if (config_dir.at(config_dir.size()-1) != '/')
-		config_dir += _RP_CHR('/');
-	// Append ".config/rom-properties".
-	config_dir += _RP(".config/rom-properties");
 
 	// Directories have been initialized.
 }

src/CMakeLists.txt
@@ -23,6 +23,8 @@ CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/config.version.h.in" "${CMAKE_CURREN
 # Source Code subdirectories.
 IF(WIN32)
 	ADD_SUBDIRECTORY(libwin32common)
+ELSE(WIN32)
+	ADD_SUBDIRECTORY(libunixcommon)
 ENDIF(WIN32)
 ADD_SUBDIRECTORY(librpbase)
 ADD_SUBDIRECTORY(libromdata)

src/gtk/xfce/CMakeLists.txt
@@ -60,12 +60,10 @@ SET(rp-thumbnailer-dbus_SRCS
 	rp-thumbnailer-dbus.cpp
 	rp-thumbnailer-main.cpp
 	${CMAKE_CURRENT_BINARY_DIR}/SpecializedThumbnailer1.c
-	../../rp-stub/dll-search.c
 	)
 SET(rp-thumbnailer-dbus_H
 	rp-thumbnailer-dbus.hpp
 	${CMAKE_CURRENT_BINARY_DIR}/SpecializedThumbnailer1.h
-	../../rp-stub/dll-search.h
 	)
 
 # Process the .service file.
@@ -113,6 +111,7 @@ IF(BUILD_XFCE)
 			$<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/src>
 			$<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/src>
 		)
+	TARGET_LINK_LIBRARIES(rp-thumbnailer-dbus unixcommon)
 	TARGET_LINK_LIBRARIES(rp-thumbnailer-dbus GLib2::gio-unix GLib2::gio GLib2::gobject GLib2::glib)
 	# Link in libdl if it's required for dlopen().
 	IF(CMAKE_DL_LIBS)

src/gtk/xfce/rp-thumbnailer-main.cpp
@@ -20,16 +20,9 @@
  ***************************************************************************/
 
 #include "librpbase/common.h"
-#include "rp-stub/dll-search.h"
+#include "libunixcommon/userdirs.hpp"
+#include "libunixcommon/dll-search.h"
 #include "rp-thumbnailer-dbus.hpp"
-
-// C includes.
-#include <stdlib.h>
-#include <unistd.h>
-// C includes. (stat, passwd)
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <pwd.h>
 
 // C includes. (C++ namespace)
 #include <cerrno>
@@ -54,108 +47,18 @@ static PFN_RP_CREATE_THUMBNAIL pfn_rp_create_thumbnail = nullptr;
 static string cache_dir;
 
 /**
- * Check if a directory is writable.
- * @param path Directory path.
- * @return True if this is a writable directory; false if not.
- */
-static inline bool isWritableDirectory(const char *path)
-{
-	struct stat sb;
-	int ret = stat(path, &sb);
-	if (ret == 0 && S_ISDIR(sb.st_mode)) {
-		// This is a directory.
-		if (!access(path, R_OK|W_OK)) {
-			// Directory is writable.
-			return true;
-		}
-	}
-
-	// Not a writable directory.
-	return false;
-}
-
-/**
- * Remove trailing slashes.
- * @param path Path to remove trailing slashes from.
- */
-static inline void removeTrailingSlashes(string &path)
-{
-	while (!path.empty() && path[path.size()-1] == '/') {
-		path.resize(path.size()-1);
-	}
-}
-
-/**
  * Initialize the cache directory.
  * @return 0 on success; non-zero on error.
  */
 static int init_cache_dir(void)
 {
-	// Check $XDG_CACHE_HOME first.
-	const char *const xdg_cache_home_env = getenv("XDG_CACHE_HOME");
-	if (xdg_cache_home_env && xdg_cache_home_env[0] == '/') {
-		// Make sure this is a writable directory.
-		if (isWritableDirectory(xdg_cache_home_env)) {
-			// $XDG_CACHE_HOME is a writable directory.
-			cache_dir = xdg_cache_home_env;
-			// Remove trailing slashes.
-			removeTrailingSlashes(cache_dir);
-			// If the path was "/", this will result in an empty directory.
-			if (!cache_dir.empty()) {
-				g_debug("Cache directory: $XDG_CACHE_HOME == %s", cache_dir.c_str());
-				return 0;
-			}
-		}
-	}
-
-	// Fall back to $HOME/.cache/.
-	const char *const home_env = getenv("HOME");
-	if (home_env && home_env[0] == '/') {
-		// Make sure this is a writable directory.
-		if (isWritableDirectory(home_env)) {
-			// $HOME is a writable directory.
-			cache_dir = home_env;
-			// Remove trailing slashes.
-			removeTrailingSlashes(cache_dir);
-			// If the path was "/", this will result in an empty directory.
-			if (!cache_dir.empty()) {
-				cache_dir += "/.cache";
-				g_debug("Cache directory: $HOME/.cache == %s", cache_dir.c_str());
-				return 0;
-			}
-		}
-	}
-
-	// $HOME isn't valid. Use getpwuid_r().
-	char buf[2048];
-	struct passwd pwd;
-	struct passwd *pwd_result;
-	errno = 0;
-	int ret = getpwuid_r(getuid(), &pwd, buf, sizeof(buf), &pwd_result);
-	if (ret != 0 || !pwd_result) {
-		// getpwuid_r() failed.
-		g_warning("getpwuid_r() failed: %s", (ret != 0 ? strerror(errno) : "pwd_result is NULL"));
+	cache_dir = LibUnixCommon::getCacheDirectory();
+	if (cache_dir.empty()) {
+		g_critical("Unable to determine the XDG cache directory.");
 		return -1;
 	}
-
-	if (pwd_result->pw_dir[0] == '/') {
-		// Make sure this is a writable directory.
-		if (isWritableDirectory(pwd_result->pw_dir)) {
-			// $HOME is a writable directory.
-			cache_dir = pwd_result->pw_dir;
-			// Remove trailing slashes.
-			removeTrailingSlashes(cache_dir);
-			// If the path was "/", this will result in an empty directory.
-			if (!cache_dir.empty()) {
-				cache_dir += "/.cache";
-				g_debug("Cache directory: getpwuid_r() -> == %s", cache_dir.c_str());
-				return 0;
-			}
-		}
-	}
-
-	g_critical("Unable to determine the XDG cache directory.");
-	return -1;
+	g_debug("Cache directory: %s", cache_dir.c_str());
+	return 0;
 }
 
 /**

src/librpbase/CMakeLists.txt
@@ -310,6 +310,9 @@ FOREACH(_target rpbase8 rpbase16)
 		IF(ENABLE_DECRYPTION)
 			TARGET_LINK_LIBRARIES(${_target} advapi32)
 		ENDIF(ENABLE_DECRYPTION)
+	ELSE(WIN32)
+		# libunixcommon
+		TARGET_LINK_LIBRARIES(${_target} unixcommon)
 	ENDIF(WIN32)
 	IF(CMAKE_THREAD_LIBS_INIT)
 		TARGET_LINK_LIBRARIES(${_target} ${CMAKE_THREAD_LIBS_INIT})

src/libunixcommon/CMakeLists.txt
@@ -1,0 +1,70 @@
+# Unix common code.
+PROJECT(libunixcommon)
+
+# TODO: Mac OS X functionality.
+IF(NOT APPLE)
+	# Find the framework libraries in order to determine plugin paths.
+	# FIXME: Remove prefix from KDE4's path.
+	# NOTE: Need to skip the uninstall rule due to conflicts
+	# with the actual KDE4 frontend.
+	SET(_kde4_uninstall_rule_created TRUE)
+	FIND_PACKAGE(KDE4)
+	SET(KDE4_PLUGIN_INSTALL_DIR "${PLUGIN_INSTALL_DIR}")
+	UNSET(PLUGIN_INSTALL_DIR)
+	UNSET(PLUGIN_INSTALL_DIR CACHE)
+	# Get rid of the explicit C90 setting. (set by KDE4)
+	STRING(REPLACE "-std=iso9899:1990" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+
+	# Get the plugin directory and Qt prefix.
+	# Prefix will be removed from the plugin directory if necessary.
+	EXEC_PROGRAM(qtpaths ARGS --plugin-dir OUTPUT_VARIABLE KDE5_PLUGIN_INSTALL_DIR)
+	IF(NOT KDE5_PLUGIN_INSTALL_DIR)
+		MESSAGE(FATAL_ERROR "`qtpaths` isn't working correctly.")
+	ENDIF(NOT KDE5_PLUGIN_INSTALL_DIR)
+	EXEC_PROGRAM(qtpaths ARGS --install-prefix OUTPUT_VARIABLE QT5_INSTALL_PREFIX)
+	INCLUDE(ReplaceHardcodedPrefix)
+	REPLACE_HARDCODED_PREFIX(KDE5_PLUGIN_INSTALL_DIR "${QT5_INSTALL_PREFIX}")
+
+	# XFCE and GNOME.
+	FIND_PACKAGE(ThunarX2)
+	FIND_PACKAGE(LibNautilusExtension 3.0.0)
+
+	# Write the config.h file.
+	CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/config.libunixcommon.h.in" "${CMAKE_CURRENT_BINARY_DIR}/config.libunixcommon.h")
+ENDIF(NOT APPLE)
+
+# Sources and headers.
+SET(libunixcommon_SRCS
+	userdirs.cpp
+	dll-search.c
+	)
+SET(libunixcommon_H
+	userdirs.hpp
+	dll-search.h
+	)
+
+######################
+# Build the library. #
+######################
+
+ADD_LIBRARY(unixcommon STATIC
+	${libunixcommon_SRCS}
+	${libunixcommon_H}
+	)
+# Exclude from ALL builds.
+SET_TARGET_PROPERTIES(unixcommon PROPERTIES EXCLUDE_FROM_ALL TRUE)
+
+TARGET_INCLUDE_DIRECTORIES(unixcommon
+	PUBLIC	$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
+	PRIVATE	$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/..>
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/..>
+		$<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/src>
+		$<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/src>
+	)
+
+# Add -fpic/-fPIC in order to use this static library in plugins.
+IF(NOT APPLE)
+	SET(CMAKE_C_FLAGS	"${CMAKE_C_FLAGS} -fpic -fPIC")
+	SET(CMAKE_CXX_FLAGS	"${CMAKE_CXX_FLAGS} -fpic -fPIC")
+ENDIF(NOT APPLE)

src/libunixcommon/config.libunixcommon.h.in
@@ -1,8 +1,8 @@
 /***************************************************************************
  * ROM Properties Page shell extension. (rp-stub)                          *
- * dll-search.h: Function to search for a usable rom-properties library.   *
+ * config.libunixcommon.h.in: libunixcommon configuration. (source file)   *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2017 by David Korth.                                      *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -19,37 +19,19 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#ifndef __ROMPROPERTIES_RP_STUB_DLL_SEARCH_H__
-#define __ROMPROPERTIES_RP_STUB_DLL_SEARCH_H__
+#ifndef __ROMPROPERTIES_LIBUNIXCOMMON_CONFIG_H__
+#define __ROMPROPERTIES_LIBUNIXCOMMON_CONFIG_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/* KDE4 plugin path. */
+#cmakedefine KDE4_PLUGIN_INSTALL_DIR "@KDE4_PLUGIN_INSTALL_DIR@"
 
-#ifndef ATTR_PRINTF
-# ifdef __GNUC__
-#  define ATTR_PRINTF(arg_format, arg_varargs) __attribute__ ((format (printf, arg_format, arg_varargs)))
-# else
-#  define ATTR_PRINTF(arg_format, arg_varargs)
-# endif
-#endif
+/* KDE5 plugin path. */
+#cmakedefine KDE5_PLUGIN_INSTALL_DIR "@KDE5_PLUGIN_INSTALL_DIR@"
 
-#define LEVEL_DEBUG 0
-#define LEVEL_ERROR 1
-typedef int (*PFN_RP_DLL_DEBUG)(int level, const char *format, ...) ATTR_PRINTF(2, 3);
+/* ThunarX2 extensions path. */
+#cmakedefine ThunarX2_EXTENSIONS_DIR "@ThunarX2_EXTENSIONS_DIR@"
 
-/**
- * Search for a rom-properties library.
- * @param symname	[in] Symbol name to look up.
- * @param ppDll		[out] Handle to opened library.
- * @param ppfn		[out] Pointer to function.
- * @param pfnDebug	[in,opt] Pointer to debug logging function. (printf-style) (may be NULL)
- * @return 0 on success; negative POSIX error code on error.
- */
-int rp_dll_search(const char *symname, void **ppDll, void **ppfn, PFN_RP_DLL_DEBUG pfnDebug);
+/* libnautilus-extension extensions path. */
+#cmakedefine LibNautilusExtension_EXTENSION_DIR "@LibNautilusExtension_EXTENSION_DIR@"
 
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __ROMPROPERTIES_RP_STUB_DLL_SEARCH_H__ */
+#endif /* __ROMPROPERTIES_LIBUNIXCOMMON_CONFIG_H__ */

src/libunixcommon/dll-search.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- * ROM Properties Page shell extension. (rp-stub)                          *
+ * ROM Properties Page shell extension. (libunixcommon)                    *
  * dll-search.c: Function to search for a usable rom-properties library.   *
  *                                                                         *
  * Copyright (c) 2016-2017 by David Korth.                                 *
@@ -19,7 +19,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#include "rp-stub/config.rp-stub.h"
+#include "config.libunixcommon.h"
 #include "dll-search.h"
 
 // C includes.

commit 0b78c83688fc06e108402a9df0af7560509425a8

[librpbase] FileSystem_posix.cpp: Append "rom-properties" to the cache directory.
Storing all of the cached files in ~/.cache/ is bad and wrong.

This fixes a regression from commit 7835de96f19bf093cc6100efa92f633b1505f3a0.
([libunixcommon] New common library for Unix and Unix-like systems.)

src/librpbase/file/FileSystem_posix.cpp
@@ -164,6 +164,13 @@ static void initConfigDirectories(void)
 
 	// Cache directory.
 	cache_dir = utf8_to_rp_string(LibUnixCommon::getCacheDirectory());
+	if (!cache_dir.empty()) {
+		// Add a trailing slash if necessary.
+		if (cache_dir.at(cache_dir.size()-1) != '/')
+			cache_dir += _RP_CHR('/');
+		// Append "rom-properties".
+		cache_dir += _RP("rom-properties");
+	}
 
 	// Config directory.
 	config_dir = utf8_to_rp_string(LibUnixCommon::getConfigDirectory());