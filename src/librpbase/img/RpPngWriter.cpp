GerbilSoft/rom-properties

commit d69cd85982eb4440b1e3fc4783431c87eb25137d

[librpbase] rp_image.hpp: #include "byteorder.h"
Since we checked for SYS_LIL_ENDIAN, and both SYS_BYTEORDER and
SYS_LIL_ENDIAN were undefined, this resulted in `0 == 0`, which
"just works" on i386/amd64.

Note that other files that #include "rp_image.hpp" may have had this file
included already, so it probably didn't cause any significant issues.

src/librpbase/img/rp_image.hpp
@@ -24,6 +24,7 @@
 
 #include "librpbase/config.librpbase.h"
 #include "librpbase/common.h"
+#include "librpbase/byteorder.h"
 
 // C includes.
 #include <stdint.h>

commit 8feea1458fd2a0e6efe3237284beba0c4a176491

[librpbase] Fix two PNG writing issues caused by byteorder errors.
- rp_image.hpp: argb32_t's byte ordering was inverted.
- RpPngWriter: SYS_BYTE_ORDER isn't defined. It's SYS_BYTEORDER.

This fixes a regression from commit d69cd85982eb4440b1e3fc4783431c87eb25137d.
([librpbase] rp_image.hpp: #include "byteorder.h")

src/librpbase/img/RpPngWriter.cpp
@@ -23,6 +23,7 @@
 #include "RpPngWriter.hpp"
 
 #include "common.h"
+#include "byteorder.h"
 #include "TextFuncs.hpp"
 #include "file/RpFile.hpp"
 
@@ -730,9 +731,9 @@ int RpPngWriterPrivate::write_IDAT(const png_byte *const *row_pointers, bool is_
 	if (cache.skip_alpha && cache.format == rp_image::FORMAT_ARGB32) {
 		// Need to skip the alpha bytes.
 		// Assuming 'after' on LE, 'before' on BE.
-#if SYS_BYTE_ORDER == SYS_LIL_ENDIAN
+#if SYS_BYTEORDER == SYS_LIL_ENDIAN
 		static const int flags = PNG_FILLER_AFTER;
-#else /* SYS_BYTE_ORDER == SYS_BIG_ENDIAN */
+#else /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
 		static const int flags = PNG_FILLER_BEFORE;
 #endif
 		png_set_filler(png_ptr, 0xFF, flags);

src/librpbase/img/rp_image.hpp
@@ -37,15 +37,15 @@ namespace LibRpBase {
 union argb32_t {
 	struct {
 #if SYS_BYTEORDER == SYS_LIL_ENDIAN
-		uint8_t b;
-		uint8_t g;
-		uint8_t r;
 		uint8_t a;
+		uint8_t r;
+		uint8_t g;
+		uint8_t b;
 #else /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
-		uint8_t a;
-		uint8_t r;
-		uint8_t g;
 		uint8_t b;
+		uint8_t g;
+		uint8_t r;
+		uint8_t a;
 #endif
 	};
 	uint32_t u32;