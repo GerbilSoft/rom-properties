d8984bb808c6a98fe524792151b3bab832e25540
David Korth <gerbilsoft@gerbilsoft.com>
2017-08-05 01:29:23 -0400

    [librpbase/tests] TextFuncsTest: Updated the copyright date.
    Now with more Latin-1 testing.

--- src/librpbase/tests/TextFuncsTest.cpp
+++ src/librpbase/tests/TextFuncsTest.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase/tests)                  *
  * TextFuncsTest.cpp: TextFuncs class test.                                *
  *                                                                         *
- * Copyright (c) 2016 by David Korth.                                      *
+ * Copyright (c) 2016-2017 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
