GerbilSoft/rom-properties

commit 4517af2694e7d05e2166c1590a8b5001de827363

Consistently use `time_t` for date/time values instead of `int64_t`.
Since we're using libc functions that use `time_t`, we should also
use `time_t`. On 64-bit Linux and all Windows (when using MSVC 2005
and later), `time_t` is 64-bit, so this isn't a problem.

On 32-bit Linux, `time_t` is 32-bit, so using `int64_t` provides no
advantage, since the libc functions won't support 64-bit.

TODO: Revisit this once glibc adds 64-bit `time_t` on 32-bit Linux.
Reference: https://sourceware.org/glibc/wiki/Y2038ProofnessDesign

src/librpbase/RomFields.hpp
@@ -179,7 +179,7 @@ class RomFields
 				// RFT_DATETIME (UNIX format)
 				// NOTE: -1 is used to indicate
 				// an invalid date/time.
-				int64_t date_time;
+				time_t date_time;
 
 				// RFT_AGE_RATINGS
 				// See AgeRatingsCountry for field indexes.

src/libromdata/DreamcastSave.cpp
@@ -122,11 +122,11 @@ class DreamcastSavePrivate : public RomDataPrivate
 		// depending on if we loaded a VMI or DCI.
 		// If the original value is invalid, this will
 		// be set to -1.
-		int64_t ctime;
+		time_t ctime;
 
 		// Time conversion functions.
-		static int64_t vmi_to_unix_time(const DC_VMI_Timestamp *vmi_tm);
-		static int64_t vms_bcd_to_unix_time(const DC_VMS_BCD_Timestamp *vms_bcd_tm);
+		static time_t vmi_to_unix_time(const DC_VMI_Timestamp *vmi_tm);
+		static time_t vms_bcd_to_unix_time(const DC_VMS_BCD_Timestamp *vms_bcd_tm);
 
 		// Is this a VMS game file?
 		bool isGameFile;
@@ -250,7 +250,7 @@ DreamcastSavePrivate::~DreamcastSavePrivate()
  * NOTE: vmi_tm->year must have been byteswapped prior to
  * calling this function.
  */
-int64_t DreamcastSavePrivate::vmi_to_unix_time(const DC_VMI_Timestamp *vmi_tm)
+time_t DreamcastSavePrivate::vmi_to_unix_time(const DC_VMI_Timestamp *vmi_tm)
 {
 	// Convert the VMI time to Unix time.
 	// NOTE: struct tm has some oddities:
@@ -283,7 +283,7 @@ int64_t DreamcastSavePrivate::vmi_to_unix_time(const DC_VMI_Timestamp *vmi_tm)
  * not likely to be valid for Dreamcast, since Dreamcast
  * was released in 1998.
  */
-int64_t DreamcastSavePrivate::vms_bcd_to_unix_time(const DC_VMS_BCD_Timestamp *vms_bcd_tm)
+time_t DreamcastSavePrivate::vms_bcd_to_unix_time(const DC_VMS_BCD_Timestamp *vms_bcd_tm)
 {
 	// Convert the VMS BCD time to Unix time.
 	// NOTE: struct tm has some oddities:

src/libromdata/EXE.cpp
@@ -263,7 +263,7 @@ void EXEPrivate::addFields_VS_VERSION_INFO(const VS_FIXEDFILEINFO *pVsFfi, const
 #ifndef HECTONANOSEC_PER_SEC
 		#define HECTONANOSEC_PER_SEC 10000000LL
 #endif
-		int64_t fileTimeUnix = (fileTime - FILETIME_1970) / HECTONANOSEC_PER_SEC;
+		time_t fileTimeUnix = (time_t)((fileTime - FILETIME_1970) / HECTONANOSEC_PER_SEC);
 		fields->addField_dateTime(_RP("File Time"), fileTimeUnix,
 			RomFields::RFT_DATETIME_HAS_DATE |
 			RomFields::RFT_DATETIME_HAS_TIME

src/libromdata/GameCubeSave.cpp
@@ -941,7 +941,7 @@ int GameCubeSave::loadFieldData(void)
 
 	// Last Modified timestamp.
 	d->fields->addField_dateTime(_RP("Last Modified"),
-		(int64_t)d->direntry.lastmodified + GC_UNIX_TIME_DIFF,
+		(time_t)d->direntry.lastmodified + GC_UNIX_TIME_DIFF,
 		RomFields::RFT_DATETIME_HAS_DATE |
 		RomFields::RFT_DATETIME_HAS_TIME |
 		RomFields::RFT_DATETIME_IS_UTC	// GameCube doesn't support timezones.

src/libromdata/NES.cpp
@@ -121,7 +121,7 @@ class NESPrivate : public RomDataPrivate
 		 * not likely to be valid for NES/Famicom, since the Famicom
 		 * was released in 1983.
 		 */
-		static int64_t fds_bcd_datestamp_to_unix(const FDS_BCD_DateStamp *fds_bcd_ds);
+		static time_t fds_bcd_datestamp_to_unix_time(const FDS_BCD_DateStamp *fds_bcd_ds);
 };
 
 /** NESPrivate **/
@@ -157,9 +157,9 @@ inline rp_string NESPrivate::formatBankSizeKB(unsigned int size)
  * not likely to be valid for NES/Famicom, since the Famicom
  * was released in 1983.
  */
-int64_t NESPrivate::fds_bcd_datestamp_to_unix(const FDS_BCD_DateStamp *fds_bcd_ds)
+time_t NESPrivate::fds_bcd_datestamp_to_unix_time(const FDS_BCD_DateStamp *fds_bcd_ds)
 {
-	// Convert the VMI time to Unix time.
+	// Convert the FDS time to Unix time.
 	// NOTE: struct tm has some oddities:
 	// - tm_year: year - 1900
 	// - tm_mon: 0 == January
@@ -871,7 +871,7 @@ int NES::loadFieldData(void)
 			d->header.fds.revision, RomFields::FB_DEC, 2);
 
 		// Manufacturing Date.
-		int64_t mfr_date = d->fds_bcd_datestamp_to_unix(&d->header.fds.mfr_date);
+		time_t mfr_date = d->fds_bcd_datestamp_to_unix_time(&d->header.fds.mfr_date);
 		d->fields->addField_dateTime(_RP("Manufacturing Date"), mfr_date,
 			RomFields::RFT_DATETIME_HAS_DATE |
 			RomFields::RFT_DATETIME_IS_UTC  // Date only.

src/librpbase/RomFields.cpp
@@ -1016,7 +1016,7 @@ int RomFields::addField_listData(const rp_char *name,
  * @param flags Date/Time flags.
  * @return Field index, or -1 on error.
  */
-int RomFields::addField_dateTime(const rp_char *name, int64_t date_time, int flags)
+int RomFields::addField_dateTime(const rp_char *name, time_t date_time, int flags)
 {
 	RP_D(RomFields);
 

src/libwin32common/w32time.h
@@ -39,7 +39,7 @@
  * @param unix_time Unix time.
  * @param pSystemTime Win32 SYSTEMTIME.
  */
-static TIME_INLINE void UnixTimeToSystemTime(int64_t unix_time, SYSTEMTIME *pSystemTime)
+static TIME_INLINE void UnixTimeToSystemTime(time_t unix_time, SYSTEMTIME *pSystemTime)
 {
 	LARGE_INTEGER li;
 	FILETIME ft;

commit 3d2f60a5c201c22f12253bc7c3e941a8409a64c2

[librpbase] RomFields: Fix declaration of addField_dateTime().
Use `time_t`, not `int64_t`. This didn't cause a problem on 64-bit
Linux builds or any Windows builds, since those use 64-bit `time_t`,
but it will cause problems on 32-bit Linux builds.

This bug was reported by @DankRank in #retro.

This fixes a regression from commit 4517af2694e7d05e2166c1590a8b5001de827363.
(Consistently use `time_t` for date/time values instead of `int64_t`.)

src/librpbase/RomFields.hpp
@@ -448,7 +448,7 @@ class RomFields
 		 * @param flags Date/Time flags.
 		 * @return Field index, or -1 on error.
 		 */
-		int addField_dateTime(const rp_char *name, int64_t date_time, unsigned int flags = 0);
+		int addField_dateTime(const rp_char *name, time_t date_time, unsigned int flags = 0);
 
 		/**
 		 * Add age ratings.