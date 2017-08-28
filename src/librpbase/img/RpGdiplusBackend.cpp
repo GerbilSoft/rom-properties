3520e5efd75a14c9dd792e9c3207fd5259c1f602
David Korth <gerbilsoft@gerbilsoft.com>
2017-08-27 20:05:02 -0400

    [librpbase] RpGdiplusBackend.cpp: Updated the comment regarding why RpGdiplusBackend is in librpbase and not Win32.

--- src/librpbase/img/RpGdiplusBackend.cpp
+++ src/librpbase/img/RpGdiplusBackend.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RpGdiplusBackend.hpp: rp_image_backend using GDI+.                      *
  *                                                                         *
- * Copyright (c) 2016 by David Korth.                                      *
+ * Copyright (c) 2016-2017 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,13 +14,12 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
-// NOTE: This class is located in libromdata, not Win32,
-// since RpPng_gdiplus.cpp uses the backend directly.
+// NOTE: This class is located in librpbase, not Win32,
+// since gtest_init.cpp uses the backend directly.
 
 #include "RpGdiplusBackend.hpp"
 #include "../aligned_malloc.h"
