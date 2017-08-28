GerbilSoft/rom-properties

commit 22bb8a94d3f41c8d94377e7418a00646ef91d35c

[librpbase] RpGdiplusBackend: Ensure `stride` is a multiple of 16 bytes.
This will allow us to use more optimal SSE2 code.

The only way to specify `stride` is to manually allocate a buffer when
locking the GDI+ bitmap, so we do that now. m_pImgBuf is allocated on
the first lock() and freed when the backend is destroyed.

Updated the bitmap conversion functions to use the correct stride for
HBITMAP. HBITMAP stride is always a multiple of 4, so no adjustment is
necessary for ARGB32, but alignment is needed for CI8.

src/librpbase/img/RpGdiplusBackend.cpp
@@ -54,9 +54,13 @@ RpGdiplusBackend::RpGdiplusBackend(int width, int height, rp_image::Format forma
 	, m_gdipToken(0)
 	, m_pGdipBmp(nullptr)
 	, m_isLocked(false)
+	, m_bytesppShift(0)
 	, m_gdipFmt(0)
+	, m_pImgBuf(nullptr)
 	, m_pGdipPalette(nullptr)
 {
+	memset(&m_gdipBmpData, 0, sizeof(m_gdipBmpData));
+
 	if (this->width <= 0 || this->height <= 0) {
 		// Image did not initialize successfully.
 		return;
@@ -72,9 +76,11 @@ RpGdiplusBackend::RpGdiplusBackend(int width, int height, rp_image::Format forma
 	switch (format) {
 		case rp_image::FORMAT_CI8:
 			m_gdipFmt = PixelFormat8bppIndexed;
+			m_bytesppShift = 0;
 			break;
 		case rp_image::FORMAT_ARGB32:
 			m_gdipFmt = PixelFormat32bppARGB;
+			m_bytesppShift = 2;
 			break;
 		default:
 			assert(!"Unsupported rp_image::Format.");
@@ -111,11 +117,14 @@ RpGdiplusBackend::~RpGdiplusBackend()
 {
 	if (m_pGdipBmp) {
 		// TODO: Is an Unlock required here?
-		m_pGdipBmp->UnlockBits(&m_gdipBmpData);
+		if (m_isLocked) {
+			m_pGdipBmp->UnlockBits(&m_gdipBmpData);
+		}
 		delete m_pGdipBmp;
 	}
 
-	free(this->m_pGdipPalette);
+	free(m_pImgBuf);
+	free(m_pGdipPalette);
 	GdiplusHelper::ShutdownGDIPlus(m_gdipToken);
 }
 
@@ -139,11 +148,6 @@ int RpGdiplusBackend::doInitialLock(void)
 		this->format = rp_image::FORMAT_NONE;
 		return -1;
 	}
-
-	// Set the image stride.
-	// On Windows, it might not be the same as width*pixelsize.
-	// TODO: If Stride is negative, the image is upside-down.
-	this->stride = abs(m_gdipBmpData.Stride);
 	return 0;
 }
 
@@ -215,16 +219,45 @@ int RpGdiplusBackend::palette_len(void) const
 Gdiplus::Status RpGdiplusBackend::lock(void)
 {
 	// TODO: Recursive locks?
+	// TODO: Atomic locking?
 	if (m_isLocked)
 		return Gdiplus::Status::Ok;
 
+	// We're allocating our own image buffer in order to set a custom stride.
+	// Stride should be a multiple of 16 bytes for SSE2 optimization.
+	m_gdipBmpData.Stride = this->width << m_bytesppShift;
+	m_gdipBmpData.Stride = (m_gdipBmpData.Stride + 15) & ~15;
+
+	if (!m_pImgBuf) {
+		// Allocate the image buffer.
+		m_pImgBuf = malloc(m_gdipBmpData.Stride * this->height);
+		if (!m_pImgBuf) {
+			// malloc() failed.
+			return Gdiplus::Status::OutOfMemory;
+		}
+	}
+
+	// NOTE: Setting m_gdipBmpData values even if we set it before.
+	// Not sure if they get overwritten...
 	const Gdiplus::Rect bmpRect(0, 0, this->width, this->height);
+	m_gdipBmpData.Width = this->width;
+	m_gdipBmpData.Height = this->height;
+	m_gdipBmpData.PixelFormat = m_gdipFmt;
+	m_gdipBmpData.Scan0 = m_pImgBuf;
+
 	Gdiplus::Status status = m_pGdipBmp->LockBits(&bmpRect,
-		Gdiplus::ImageLockModeRead | Gdiplus::ImageLockModeWrite,
+		Gdiplus::ImageLockModeRead |
+		Gdiplus::ImageLockModeWrite |
+		Gdiplus::ImageLockModeUserInputBuf,
 		m_gdipFmt, &m_gdipBmpData);
 	if (status == Gdiplus::Status::Ok) {
 		m_isLocked = true;
 	}
+
+	// Save the image stride.
+	// On Windows, it might not be the same as width*pixelsize.
+	// TODO: If Stride is negative, the image is upside-down.
+	this->stride = abs(m_gdipBmpData.Stride);
 	return status;
 }
 
@@ -610,13 +643,15 @@ HBITMAP RpGdiplusBackend::convBmpData_ARGB32(const Gdiplus::BitmapData *pBmpData
 	}
 
 	// Copy the data from the GDI+ bitmap to the HBITMAP directly.
-	// FIXME: Do we need to handle special cases for odd widths?
+	// HBITMAP stride is a multiple of 4, so we can assume that
+	// it's equal to row_bytes.
 	const uint8_t *gdip_px = static_cast<const uint8_t*>(pBmpData->Scan0);
-	const size_t active_px_sz = pBmpData->Width * 4;
+	const size_t row_bytes = pBmpData->Width * 4;
+	const int gdip_stride = pBmpData->Stride;
 	for (int y = (int)pBmpData->Height; y > 0; y--) {
-		memcpy(pvBits, gdip_px, active_px_sz);
-		pvBits += pBmpData->Stride;
-		gdip_px += pBmpData->Stride;
+		memcpy(pvBits, gdip_px, row_bytes);
+		pvBits += row_bytes;
+		gdip_px += gdip_stride;
 	}
 
 	// Bitmap is ready.
@@ -669,12 +704,14 @@ HBITMAP RpGdiplusBackend::convBmpData_CI8(const Gdiplus::BitmapData *pBmpData)
 	}
 
 	// Copy the data from the GDI+ bitmap to the HBITMAP directly.
-	// FIXME: Do we need to handle special cases for odd widths?
+	// HBITMAP stride is a multiple of 4.
 	const uint8_t *gdip_px = static_cast<const uint8_t*>(pBmpData->Scan0);
-	const size_t active_px_sz = pBmpData->Width;
+	const size_t row_bytes = pBmpData->Width;
+	const int hbmp_stride = (row_bytes + 3) & ~3;
+	const int gdip_stride = pBmpData->Stride;
 	for (int y = (int)pBmpData->Height; y > 0; y--) {
-		memcpy(pvBits, gdip_px, active_px_sz);
-		pvBits += pBmpData->Stride;
+		memcpy(pvBits, gdip_px, row_bytes);
+		pvBits += hbmp_stride;
 		gdip_px += pBmpData->Stride;
 	}
 

src/librpbase/img/RpGdiplusBackend.hpp
@@ -209,8 +209,12 @@ class RpGdiplusBackend : public rp_image_backend
 
 		// BitmapData for locking.
 		bool m_isLocked;
+		uint8_t m_bytesppShift;	// bytespp shift value.
 		Gdiplus::PixelFormat m_gdipFmt;
 		Gdiplus::BitmapData m_gdipBmpData;
+
+		// Allocated image buffer for Bitmap::LockBits().
+		void *m_pImgBuf;
 
 		// Color palette.
 		// Pointer to Entries[0] is used for rp_image_backend::palette.

src/librpbase/img/rp_image.cpp
@@ -489,7 +489,7 @@ void rp_image::set_sBIT(const sBIT_t *sBIT)
 
 /**
  * Get the number of significant bits per channel.
- * @param sBIT	[out] sBIT_t struct.
+ * @param sBIT	[out,opt] sBIT_t struct.
  * @return 0 on success; non-zero if not set or error.
  */
 int rp_image::get_sBIT(sBIT_t *sBIT) const

commit 090a54fa745d5efae9a3e82c057d1fff9b12dbae

[librpbase] RpGdiplusBackend::dup_ARGB32(): Convert from CI8 manually.
Commit 22bb8a94d3f41c8d94377e7418a00646ef91d35c added the flag
Gdiplus::ImageLockModeUserInputBuf to LockBits(), which allows us to
specify our own buffer so we can ensure it's 16-byte aligned. For some
reason, on Windows 7, this causes the converted image to have color 8
converted as white instead of its original color.

Workaround: Convert from CI8 manually instead of using Bitmap::Clone().

This fixes a regression from commit 22bb8a94d3f41c8d94377e7418a00646ef91d35c.
([librpbase] RpGdiplusBackend: Ensure `stride` is a multiple of 16 bytes.)

src/librpbase/img/RpGdiplusBackend.cpp
@@ -315,24 +315,90 @@ Gdiplus::Status RpGdiplusBackend::unlock(void)
  */
 Gdiplus::Bitmap *RpGdiplusBackend::dup_ARGB32(void) const
 {
-	Gdiplus::Bitmap *pBmp;
+	Gdiplus::Bitmap *pBmp = nullptr;
 	Gdiplus::Status status;
 
-	status = const_cast<RpGdiplusBackend*>(this)->unlock();
-	if (status != Gdiplus::Status::Ok) {
-		return nullptr;
-	}
+	switch (this->format) {
+		case rp_image::FORMAT_CI8: {
+			// FIXME: Since adding custom stride, Bitmap::Clone() seems to
+			// automatically replace CI8 color 8 with white.
+			// Hence, we have to manually copy the image for CI8.
+			pBmp = new Gdiplus::Bitmap(this->width, this->height, PixelFormat32bppARGB);
+			assert(pBmp != nullptr);
+			if (!pBmp) {
+				return nullptr;
+			}
 
-	if (this->format == rp_image::FORMAT_CI8) {
-		// Copy the local palette to the GDI+ image.
-		m_pGdipBmp->SetPalette(m_pGdipPalette);
-	}
+			const Gdiplus::Rect bmpDestRect(0, 0, this->width, this->height);
+			Gdiplus::BitmapData bmpDestData;
+			status = pBmp->LockBits(&bmpDestRect, Gdiplus::ImageLockModeWrite,
+				PixelFormat32bppARGB, &bmpDestData);
+			assert(status == Gdiplus::Status::Ok);
+			if (status != Gdiplus::Status::Ok) {
+				delete pBmp;
+				return nullptr;
+			}
 
-	pBmp = m_pGdipBmp->Clone(0, 0, this->width, this->height, PixelFormat32bppARGB);
-	status = const_cast<RpGdiplusBackend*>(this)->lock();
-	if (status != Gdiplus::Status::Ok) {
-		delete pBmp;
-		return nullptr;
+			// Convert on a row-by-row basis.
+			// FIXME: Handle upside-down images. (stride is negative)
+			assert(this->stride > 0);
+			if (this->stride <= 0) {
+				pBmp->UnlockBits(&bmpDestData);
+				delete pBmp;
+				return nullptr;
+			}
+
+			const int src_stride_adj = this->stride - this->width;
+			const int dest_stride_adj = (bmpDestData.Stride / sizeof(uint32_t)) - this->width;
+			const uint8_t *src = static_cast<const uint8_t*>(m_pImgBuf);
+			uint32_t *dest = static_cast<uint32_t*>(bmpDestData.Scan0);
+			for (unsigned int y = this->height; y > 0; y--) {
+				// Convert two pixels at a time.
+				unsigned int x = this->width;
+				for (; x > 1; x -= 2) {
+					dest[0] = m_pGdipPalette->Entries[src[0]];
+					dest[1] = m_pGdipPalette->Entries[src[1]];
+					src += 2;
+					dest += 2;
+				}
+				if (x == 1) {
+					*dest = m_pGdipPalette->Entries[*src];
+					src++;
+					dest++;
+				}
+
+				// Next row.
+				src += src_stride_adj;
+				dest += dest_stride_adj;
+			}
+
+			status = pBmp->UnlockBits(&bmpDestData);
+			assert(status == Gdiplus::Status::Ok);
+			if (status != Gdiplus::Status::Ok) {
+				delete pBmp;
+				return nullptr;
+			}
+			break;
+		}
+
+		case rp_image::FORMAT_ARGB32:
+			status = const_cast<RpGdiplusBackend*>(this)->unlock();
+			assert(status == Gdiplus::Status::Ok);
+			if (status != Gdiplus::Status::Ok) {
+				return nullptr;
+			}
+
+			pBmp = m_pGdipBmp->Clone(0, 0, this->width, this->height, PixelFormat32bppARGB);
+			status = const_cast<RpGdiplusBackend*>(this)->lock();
+			assert(status == Gdiplus::Status::Ok);
+			if (status != Gdiplus::Status::Ok) {
+				delete pBmp;
+				return nullptr;
+			}
+			break;
+
+		default:
+			assert(!"Unsupported rp_image::Format.");
 	}
 
 	return pBmp;