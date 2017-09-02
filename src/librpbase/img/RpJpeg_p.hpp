GerbilSoft/rom-properties

commit d5bb5af5977d43d1e9f662063343998383a5431e

Updated everything to build correctly with stdcall as the default calling convention on 32-bit MSVC builds.
[librpbase] common.h: New macro RP_C_API that expands to __cdecl on MSVC
and nothing on other compilers.

Use RP_C_API on `bsearch()` callback functions.

For libpng and libjpeg callback functions, use PNGCAPI and JPEGCALL,
respectively.

Use RP_C_API on `main()` functions, since `main()` must be __cdecl.

src/librpbase/img/RpJpeg_p.hpp
@@ -61,13 +61,13 @@ class RpJpegPrivate
 		 * error_exit replacement for JPEG.
 		 * @param cinfo j_common_ptr
 		 */
-		static void my_error_exit(j_common_ptr cinfo);
+		static void JPEGCALL my_error_exit(j_common_ptr cinfo);
 
 		/**
 		 * output_message replacement for JPEG.
 		 * @param cinfo j_common_ptr
 		 */
-		static void my_output_message(j_common_ptr cinfo);
+		static void JPEGCALL my_output_message(j_common_ptr cinfo);
 
 		/** I/O functions. **/
 
@@ -86,14 +86,14 @@ class RpJpegPrivate
 		 * Initialize MySourceMgr.
 		 * @param cinfo j_decompress_ptr
 		 */
-		static void init_source(j_decompress_ptr cinfo);
+		static void JPEGCALL init_source(j_decompress_ptr cinfo);
 
 		/**
 		 * Fill the input buffer.
 		 * @param cinfo j_decompress_ptr
 		 * @return TRUE on success. (NOTE: 'boolean' is a JPEG typedef.)
 		 */
-		static boolean fill_input_buffer(j_decompress_ptr cinfo);
+		static boolean JPEGCALL fill_input_buffer(j_decompress_ptr cinfo);
 
 		/**
 		 * Terminate the source.
@@ -102,14 +102,14 @@ class RpJpegPrivate
 		 *
 		 * @param cinfo j_decompress_ptr
 		 */
-		static void term_source(j_decompress_ptr cinfo);
+		static void JPEGCALL term_source(j_decompress_ptr cinfo);
 
 		/**
 		 * Skip data in the source file.
 		 * @param cinfo j_decompress_ptr
 		 * @param num_bytes Number of bytes to skip.
 		 */
-		static void skip_input_data(j_decompress_ptr cinfo, long num_bytes);
+		static void JPEGCALL skip_input_data(j_decompress_ptr cinfo, long num_bytes);
 
 		/**
 		 * Initialize a JPEG source manager for an IRpFile.

src/libromdata/data/AmiiboData.cpp
@@ -144,7 +144,7 @@ class AmiiboDataPrivate {
 		 * @param b
 		 * @return
 		 */
-		static int char_id_t_compar(const void *a, const void *b);
+		static int RP_C_API char_id_t_compar(const void *a, const void *b);
 
 		/** Page 22 (raw offset 0x58): amiibo series **/
 
@@ -1151,7 +1151,7 @@ const AmiiboDataPrivate::char_id_t AmiiboDataPrivate::char_ids[] = {
  * @param b
  * @return
  */
-int AmiiboDataPrivate::char_id_t_compar(const void *a, const void *b)
+int RP_C_API AmiiboDataPrivate::char_id_t_compar(const void *a, const void *b)
 {
 	uint16_t id1 = static_cast<const char_id_t*>(a)->char_id;
 	uint16_t id2 = static_cast<const char_id_t*>(b)->char_id;

src/libromdata/data/EXEData.cpp
@@ -48,7 +48,7 @@ class EXEDataPrivate {
 		 * @param b
 		 * @return
 		 */
-		static int MachineType_compar(const void *a, const void *b);
+		static int RP_C_API MachineType_compar(const void *a, const void *b);
 };
 
 // PE machine types.
@@ -110,7 +110,7 @@ const EXEDataPrivate::MachineType EXEDataPrivate::machineTypes_LE[] = {
  * @param b
  * @return
  */
-int EXEDataPrivate::MachineType_compar(const void *a, const void *b)
+int RP_C_API EXEDataPrivate::MachineType_compar(const void *a, const void *b)
 {
 	const uint16_t cpu1 = static_cast<const MachineType*>(a)->cpu;
 	const uint16_t cpu2 = static_cast<const MachineType*>(b)->cpu;

src/libromdata/data/NESMappers.cpp
@@ -95,7 +95,7 @@ class NESMappersPrivate
 		 * @param b
 		 * @return
 		 */
-		static int SubmapperInfo_compar(const void *a, const void *b);
+		static int RP_C_API SubmapperInfo_compar(const void *a, const void *b);
 
 		/**
 		 * bsearch() comparison function for SubmapperEntry.
@@ -103,7 +103,7 @@ class NESMappersPrivate
 		 * @param b
 		 * @return
 		 */
-		static int SubmapperEntry_compar(const void *a, const void *b);
+		static int RP_C_API SubmapperEntry_compar(const void *a, const void *b);
 };
 
 /**
@@ -520,7 +520,7 @@ const NESMappersPrivate::SubmapperEntry NESMappersPrivate::submappers[] = {
  * @param b
  * @return
  */
-int NESMappersPrivate::SubmapperInfo_compar(const void *a, const void *b)
+int RP_C_API NESMappersPrivate::SubmapperInfo_compar(const void *a, const void *b)
 {
 	uint8_t submapper1 = static_cast<const SubmapperInfo*>(a)->submapper;
 	uint8_t submapper2 = static_cast<const SubmapperInfo*>(b)->submapper;
@@ -535,7 +535,7 @@ int NESMappersPrivate::SubmapperInfo_compar(const void *a, const void *b)
  * @param b
  * @return
  */
-int NESMappersPrivate::SubmapperEntry_compar(const void *a, const void *b)
+int RP_C_API NESMappersPrivate::SubmapperEntry_compar(const void *a, const void *b)
 {
 	uint16_t mapper1 = static_cast<const SubmapperEntry*>(a)->mapper;
 	uint16_t mapper2 = static_cast<const SubmapperEntry*>(b)->mapper;

src/libromdata/data/Nintendo3DSFirmData.cpp
@@ -46,7 +46,7 @@ class Nintendo3DSFirmDataPrivate {
 		 * @param b
 		 * @return
 		 */
-		static int compar(const void *a, const void *b);
+		static int RP_C_API compar(const void *a, const void *b);
 };
 
 /** Nintendo3DSFirmDataPrivate **/
@@ -106,7 +106,7 @@ const Nintendo3DSFirmData::FirmBin_t Nintendo3DSFirmDataPrivate::firmBins[] = {
  * @param b
  * @return
  */
-int Nintendo3DSFirmDataPrivate::compar(const void *a, const void *b)
+int RP_C_API Nintendo3DSFirmDataPrivate::compar(const void *a, const void *b)
 {
 	uint32_t crc_1 = static_cast<const Nintendo3DSFirmData::FirmBin_t*>(a)->crc;
 	uint32_t crc_2 = static_cast<const Nintendo3DSFirmData::FirmBin_t*>(b)->crc;

src/libromdata/data/NintendoPublishers.cpp
@@ -441,7 +441,7 @@ const NintendoPublishers::ThirdPartyList NintendoPublishers::ms_thirdPartyList[]
  * @param b
  * @return
  */
-int NintendoPublishers::compar(const void *a, const void *b)
+int RP_C_API NintendoPublishers::compar(const void *a, const void *b)
 {
 	uint16_t code1 = static_cast<const ThirdPartyList*>(a)->code;
 	uint16_t code2 = static_cast<const ThirdPartyList*>(b)->code;

src/libromdata/data/NintendoPublishers.hpp
@@ -58,7 +58,7 @@ class NintendoPublishers
 		 * @param b
 		 * @return
 		 */
-		static int compar(const void *a, const void *b);
+		static int RP_C_API compar(const void *a, const void *b);
 
 	public:
 		/**

src/libromdata/data/SegaPublishers.cpp
@@ -50,7 +50,7 @@ class SegaPublishersPrivate {
 		 * @param b
 		 * @return
 		 */
-		static int compar(const void *a, const void *b);
+		static int RP_C_API compar(const void *a, const void *b);
 
 };
 
@@ -416,7 +416,7 @@ const SegaPublishersPrivate::TCodeEntry SegaPublishersPrivate::tcodeList[] = {
  * @param b
  * @return
  */
-int SegaPublishersPrivate::compar(const void *a, const void *b)
+int RP_C_API SegaPublishersPrivate::compar(const void *a, const void *b)
 {
 	unsigned int code1 = static_cast<const TCodeEntry*>(a)->t_code;
 	unsigned int code2 = static_cast<const TCodeEntry*>(b)->t_code;

commit aef9e82426c9f758ae85545cf3b98ce549c81785

[librpbase] RpJpeg_p.hpp: #define JPEGCALL if it isn't defined.
JPEGCALL might not be defined if we're using the system libjpeg.

This fixes a regression from commit d5bb5af5977d43d1e9f662063343998383a5431e.
(Updated everything to build correctly with stdcall as the default calling convention on 32-bit MSVC builds.)

src/librpbase/img/RpJpeg_p.hpp
@@ -30,6 +30,15 @@
 #include <jerror.h>
 #include <setjmp.h>
 
+// JPEGCALL might not be defined if we're using the system libjpeg.
+#ifndef JPEGCALL
+# ifdef _MSC_VER
+#  define JPEGCALL __cdecl
+# else
+#  define JPEGCALL
+# endif
+#endif /* !JPEGCALL */
+
 #if defined(__i386__) || defined(__x86_64__) || \
     defined(_M_IX86) || defined(_M_X64)
 # define RPJPEG_HAS_SSSE3 1