GerbilSoft/rom-properties

commit d5bb5af5977d43d1e9f662063343998383a5431e

Updated everything to build correctly with stdcall as the default calling convention on 32-bit MSVC builds.
[librpbase] common.h: New macro RP_C_API that expands to __cdecl on MSVC
and nothing on other compilers.

Use RP_C_API on `bsearch()` callback functions.

For libpng and libjpeg callback functions, use PNGCAPI and JPEGCALL,
respectively.

Use RP_C_API on `main()` functions, since `main()` must be __cdecl.

src/librpbase/img/RpPng.cpp
@@ -102,7 +102,7 @@ class RpPngPrivate
 		 * @param data		[out] Buffer for the data to read.
 		 * @param length	[in]  Size of data.
 		 */
-		static void png_io_IRpFile_read(png_structp png_ptr, png_bytep data, png_size_t length);
+		static void PNGCAPI png_io_IRpFile_read(png_structp png_ptr, png_bytep data, png_size_t length);
 
 		/**
 		 * libpng I/O write handler for IRpFile.
@@ -110,13 +110,13 @@ class RpPngPrivate
 		 * @param data		[in] Data to write.
 		 * @param length	[in] Size of data.
 		 */
-		static void png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length);
+		static void PNGCAPI png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length);
 
 		/**
 		 * libpng I/O flush handler for IRpFile.
 		 * @param png_ptr	[in] PNG pointer.
 		 */
-		static void png_io_IRpFile_flush(png_structp png_ptr);
+		static void PNGCAPI png_io_IRpFile_flush(png_structp png_ptr);
 
 		/** Read functions. **/
 
@@ -149,7 +149,7 @@ class RpPngPrivate
  * @param data		[out] Buffer for the data to read.
  * @param length	[in]  Size of data.
  */
-void RpPngPrivate::png_io_IRpFile_read(png_structp png_ptr, png_bytep data, png_size_t length)
+void PNGCAPI RpPngPrivate::png_io_IRpFile_read(png_structp png_ptr, png_bytep data, png_size_t length)
 {
 	// Assuming io_ptr is an IRpFile*.
 	IRpFile *file = static_cast<IRpFile*>(png_get_io_ptr(png_ptr));
@@ -175,7 +175,7 @@ void RpPngPrivate::png_io_IRpFile_read(png_structp png_ptr, png_bytep data, png_
  * @param data		[in] Data to write.
  * @param length	[in] Size of data.
  */
-void RpPngPrivate::png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length)
+void PNGCAPI RpPngPrivate::png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length)
 {
 	// Assuming io_ptr is an IRpFile*.
 	IRpFile *file = static_cast<IRpFile*>(png_get_io_ptr(png_ptr));
@@ -191,7 +191,7 @@ void RpPngPrivate::png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png
  * libpng I/O flush handler for IRpFile.
  * @param png_ptr	[in] PNG pointer.
  */
-void RpPngPrivate::png_io_IRpFile_flush(png_structp png_ptr)
+void PNGCAPI RpPngPrivate::png_io_IRpFile_flush(png_structp png_ptr)
 {
 	// Assuming io_ptr is an IRpFile*.
 	IRpFile *file = static_cast<IRpFile*>(png_get_io_ptr(png_ptr));

src/librpbase/img/RpPngWriter.cpp
@@ -230,13 +230,13 @@ class RpPngWriterPrivate
 		 * @param data		[in] Data to write.
 		 * @param length	[in] Size of data.
 		 */
-		static void png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length);
+		static void PNGCAPI png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length);
 
 		/**
 		 * libpng I/O flush handler for IRpFile.
 		 * @param png_ptr	[in] PNG pointer.
 		 */
-		static void png_io_IRpFile_flush(png_structp png_ptr);
+		static void PNGCAPI png_io_IRpFile_flush(png_structp png_ptr);
 
 	public:
 		/** Internal functions. **/
@@ -605,7 +605,7 @@ void RpPngWriterPrivate::close(void)
  * @param data		[in] Data to write.
  * @param length	[in] Size of data.
  */
-void RpPngWriterPrivate::png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length)
+void PNGCAPI RpPngWriterPrivate::png_io_IRpFile_write(png_structp png_ptr, png_bytep data, png_size_t length)
 {
 	// Assuming io_ptr is an IRpFile*.
 	IRpFile *file = static_cast<IRpFile*>(png_get_io_ptr(png_ptr));
@@ -621,7 +621,7 @@ void RpPngWriterPrivate::png_io_IRpFile_write(png_structp png_ptr, png_bytep dat
  * libpng I/O flush handler for IRpFile.
  * @param png_ptr	[in] PNG pointer.
  */
-void RpPngWriterPrivate::png_io_IRpFile_flush(png_structp png_ptr)
+void PNGCAPI RpPngWriterPrivate::png_io_IRpFile_flush(png_structp png_ptr)
 {
 	// Assuming io_ptr is an IRpFile*.
 	IRpFile *file = static_cast<IRpFile*>(png_get_io_ptr(png_ptr));

src/libromdata/data/AmiiboData.cpp
@@ -144,7 +144,7 @@ class AmiiboDataPrivate {
 		 * @param b
 		 * @return
 		 */
-		static int char_id_t_compar(const void *a, const void *b);
+		static int RP_C_API char_id_t_compar(const void *a, const void *b);
 
 		/** Page 22 (raw offset 0x58): amiibo series **/
 
@@ -1151,7 +1151,7 @@ const AmiiboDataPrivate::char_id_t AmiiboDataPrivate::char_ids[] = {
  * @param b
  * @return
  */
-int AmiiboDataPrivate::char_id_t_compar(const void *a, const void *b)
+int RP_C_API AmiiboDataPrivate::char_id_t_compar(const void *a, const void *b)
 {
 	uint16_t id1 = static_cast<const char_id_t*>(a)->char_id;
 	uint16_t id2 = static_cast<const char_id_t*>(b)->char_id;

src/libromdata/data/EXEData.cpp
@@ -48,7 +48,7 @@ class EXEDataPrivate {
 		 * @param b
 		 * @return
 		 */
-		static int MachineType_compar(const void *a, const void *b);
+		static int RP_C_API MachineType_compar(const void *a, const void *b);
 };
 
 // PE machine types.
@@ -110,7 +110,7 @@ const EXEDataPrivate::MachineType EXEDataPrivate::machineTypes_LE[] = {
  * @param b
  * @return
  */
-int EXEDataPrivate::MachineType_compar(const void *a, const void *b)
+int RP_C_API EXEDataPrivate::MachineType_compar(const void *a, const void *b)
 {
 	const uint16_t cpu1 = static_cast<const MachineType*>(a)->cpu;
 	const uint16_t cpu2 = static_cast<const MachineType*>(b)->cpu;

src/libromdata/data/NESMappers.cpp
@@ -95,7 +95,7 @@ class NESMappersPrivate
 		 * @param b
 		 * @return
 		 */
-		static int SubmapperInfo_compar(const void *a, const void *b);
+		static int RP_C_API SubmapperInfo_compar(const void *a, const void *b);
 
 		/**
 		 * bsearch() comparison function for SubmapperEntry.
@@ -103,7 +103,7 @@ class NESMappersPrivate
 		 * @param b
 		 * @return
 		 */
-		static int SubmapperEntry_compar(const void *a, const void *b);
+		static int RP_C_API SubmapperEntry_compar(const void *a, const void *b);
 };
 
 /**
@@ -520,7 +520,7 @@ const NESMappersPrivate::SubmapperEntry NESMappersPrivate::submappers[] = {
  * @param b
  * @return
  */
-int NESMappersPrivate::SubmapperInfo_compar(const void *a, const void *b)
+int RP_C_API NESMappersPrivate::SubmapperInfo_compar(const void *a, const void *b)
 {
 	uint8_t submapper1 = static_cast<const SubmapperInfo*>(a)->submapper;
 	uint8_t submapper2 = static_cast<const SubmapperInfo*>(b)->submapper;
@@ -535,7 +535,7 @@ int NESMappersPrivate::SubmapperInfo_compar(const void *a, const void *b)
  * @param b
  * @return
  */
-int NESMappersPrivate::SubmapperEntry_compar(const void *a, const void *b)
+int RP_C_API NESMappersPrivate::SubmapperEntry_compar(const void *a, const void *b)
 {
 	uint16_t mapper1 = static_cast<const SubmapperEntry*>(a)->mapper;
 	uint16_t mapper2 = static_cast<const SubmapperEntry*>(b)->mapper;

src/libromdata/data/Nintendo3DSFirmData.cpp
@@ -46,7 +46,7 @@ class Nintendo3DSFirmDataPrivate {
 		 * @param b
 		 * @return
 		 */
-		static int compar(const void *a, const void *b);
+		static int RP_C_API compar(const void *a, const void *b);
 };
 
 /** Nintendo3DSFirmDataPrivate **/
@@ -106,7 +106,7 @@ const Nintendo3DSFirmData::FirmBin_t Nintendo3DSFirmDataPrivate::firmBins[] = {
  * @param b
  * @return
  */
-int Nintendo3DSFirmDataPrivate::compar(const void *a, const void *b)
+int RP_C_API Nintendo3DSFirmDataPrivate::compar(const void *a, const void *b)
 {
 	uint32_t crc_1 = static_cast<const Nintendo3DSFirmData::FirmBin_t*>(a)->crc;
 	uint32_t crc_2 = static_cast<const Nintendo3DSFirmData::FirmBin_t*>(b)->crc;

src/libromdata/data/NintendoPublishers.cpp
@@ -441,7 +441,7 @@ const NintendoPublishers::ThirdPartyList NintendoPublishers::ms_thirdPartyList[]
  * @param b
  * @return
  */
-int NintendoPublishers::compar(const void *a, const void *b)
+int RP_C_API NintendoPublishers::compar(const void *a, const void *b)
 {
 	uint16_t code1 = static_cast<const ThirdPartyList*>(a)->code;
 	uint16_t code2 = static_cast<const ThirdPartyList*>(b)->code;

src/libromdata/data/NintendoPublishers.hpp
@@ -58,7 +58,7 @@ class NintendoPublishers
 		 * @param b
 		 * @return
 		 */
-		static int compar(const void *a, const void *b);
+		static int RP_C_API compar(const void *a, const void *b);
 
 	public:
 		/**

commit 1ffff511a31e30e1034865fd7b99f912b900b897

[librpbase] RpPng, RpPngWriter: #define PNGCAPI if it isn't defined.
PNGCAPI was added in libpng-1.5.0beta14. travis-ci has Ubuntu 14.04,
which has libpng-1.2.50-1ubuntu2.14.04.2.

This fixes a regression from commit d5bb5af5977d43d1e9f662063343998383a5431e.
(Updated everything to build correctly with stdcall as the default calling convention on 32-bit MSVC builds.)

src/librpbase/img/RpPng.cpp
@@ -54,6 +54,16 @@ using std::unique_ptr;
 	png_set_gray_1_2_4_to_8(png_ptr)
 #endif
 
+// PNGCAPI was added in libpng-1.5.0beta14.
+// Older versions will need this.
+#ifndef PNGCAPI
+# ifdef _MSC_VER
+#  define PNGCAPI __cdecl
+# else
+#  define PNGCAPI
+# endif
+#endif /* !PNGCAPI */
+
 // pngcheck()
 #include "pngcheck/pngcheck.hpp"
 

src/librpbase/img/RpPngWriter.cpp
@@ -48,6 +48,16 @@
 	png_set_gray_1_2_4_to_8(png_ptr)
 #endif
 
+// PNGCAPI was added in libpng-1.5.0beta14.
+// Older versions will need this.
+#ifndef PNGCAPI
+# ifdef _MSC_VER
+#  define PNGCAPI __cdecl
+# else
+#  define PNGCAPI
+# endif
+#endif /* !PNGCAPI */
+
 // libpng-1.5.0beta42 marked several function parameters as const.
 // Older versions don't have them marked as const, but they're
 // effectively const anyway.