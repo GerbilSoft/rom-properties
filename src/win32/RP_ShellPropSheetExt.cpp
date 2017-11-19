GerbilSoft/rom-properties

commit 00a985bd0a6601a13c3296834204a994971b9e67

Localized strings in the property pages in the UI frontends.
[kde] Replaced tr() with C_().

Translatable string count:
- Before: 768
- After:  772
- Diff:    +4

src/win32/RP_ShellPropSheetExt.cpp
@@ -43,6 +43,11 @@ using LibWin32Common::AutoGetDC;
 #include "librpbase/img/rp_image.hpp"
 using namespace LibRpBase;
 
+// libi18n
+// NOTE: Using "RomDataView" for the context, since that
+// matches what's used for the KDE and GTK+ frontends.
+#include "libi18n/i18n.h"
+
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
 using LibRomData::RomDataFactory;
@@ -1287,7 +1292,8 @@ int RP_ShellPropSheetExt_Private::initDateTime(HWND hDlg, HWND hWndTab,
 
 	if (field->data.date_time == -1) {
 		// Invalid date/time.
-		return initString(hDlg, hWndTab, pt_start, idx, size, field, L"Unknown");
+		return initString(hDlg, hWndTab, pt_start, idx, size, field,
+			RP2W_c(C_("RomDataView", "Unknown")).c_str());
 	}
 
 	// Format the date/time using the system locale.
@@ -1392,7 +1398,8 @@ int RP_ShellPropSheetExt_Private::initAgeRatings(HWND hDlg, HWND hWndTab,
 	assert(age_ratings != nullptr);
 	if (!age_ratings) {
 		// No age ratings data.
-		return initString(hDlg, hWndTab, pt_start, idx, size, field, L"ERROR");
+		return initString(hDlg, hWndTab, pt_start, idx, size, field,
+			RP2W_c(C_("RomDataView", "ERROR")).c_str());
 	}
 
 	// Convert the age ratings field to a string.
@@ -1710,10 +1717,8 @@ void RP_ShellPropSheetExt_Private::initDialog(HWND hDlg)
 		// Current tab.
 		auto &tab = tabs[tabIdx];
 
-		// Append a ":" to the description.
-		// TODO: Localization.
-		wstring desc_text = RP2W_s(field->name);
-		desc_text += L':';
+		// Field description label.
+		wstring txt = RP2W_s(rp_sprintf(C_("RomDataView", "%s:"), field->name.c_str()));
 
 		// Create the static text widget. (FIXME: Disable mnemonics?)
 		HWND hStatic = CreateWindowEx(WS_EX_NOPARENTNOTIFY | WS_EX_TRANSPARENT,
@@ -1971,6 +1976,9 @@ IFACEMETHODIMP RP_ShellPropSheetExt::AddPages(LPFNADDPROPSHEETPAGE pfnAddPage, L
 	// Based on CppShellExtPropSheetHandler.
 	// https://code.msdn.microsoft.com/windowsapps/CppShellExtPropSheetHandler-d93b49b7
 
+	// Tab title.
+	wstring wsTabTitle = RP2W_c(C_("RomDataView", "ROM Properties"));
+
 	// Create a property sheet page.
 	PROPSHEETPAGE psp;
 	psp.dwSize = sizeof(psp);
@@ -1978,7 +1986,7 @@ IFACEMETHODIMP RP_ShellPropSheetExt::AddPages(LPFNADDPROPSHEETPAGE pfnAddPage, L
 	psp.hInstance = HINST_THISCOMPONENT;
 	psp.pszTemplate = MAKEINTRESOURCE(IDD_PROPERTY_SHEET);
 	psp.pszIcon = nullptr;
-	psp.pszTitle = L"ROM Properties";
+	psp.pszTitle = wsTabTitle.c_str();
 	psp.pfnDlgProc = RP_ShellPropSheetExt_Private::DlgProc;
 	psp.pcRefParent = nullptr;
 	psp.pfnCallback = RP_ShellPropSheetExt_Private::CallbackProc;

locale/rom-properties.pot
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: rom-properties 1.2+\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2017-11-18 19:45-0500\n"
+"POT-Creation-Date: 2017-11-18 20:02-0500\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -17,6 +17,39 @@ msgstr ""
 "Content-Type: text/plain; charset=CHARSET\n"
 "Content-Transfer-Encoding: 8bit\n"
 "Plural-Forms: nplurals=INTEGER; plural=EXPRESSION;\n"
+
+#. Based on CppShellExtPropSheetHandler.
+#. https://code.msdn.microsoft.com/windowsapps/CppShellExtPropSheetHandler-d93b49b7
+#. Tab title.
+#: ../src/gtk/gnome/rom-properties-provider.cpp:136
+#: ../src/gtk/xfce/rom-properties-page.cpp:158
+#: ../src/kde/RomPropertiesDialogPlugin.cpp:84
+#: ../src/win32/RP_ShellPropSheetExt.cpp:1980
+msgctxt "RomDataView"
+msgid "ROM Properties"
+msgstr ""
+
+#. Invalid date/time.
+#: ../src/gtk/RomDataView.cpp:1099 ../src/kde/RomDataView.cpp:683
+#: ../src/win32/RP_ShellPropSheetExt.cpp:1296
+msgctxt "RomDataView"
+msgid "Unknown"
+msgstr ""
+
+#. No age ratings data.
+#: ../src/gtk/RomDataView.cpp:1169 ../src/kde/RomDataView.cpp:748
+#: ../src/win32/RP_ShellPropSheetExt.cpp:1402
+msgctxt "RomDataView"
+msgid "ERROR"
+msgstr ""
+
+#. Field description label.
+#: ../src/gtk/RomDataView.cpp:1359 ../src/kde/RomDataView.cpp:870
+#: ../src/win32/RP_ShellPropSheetExt.cpp:1721
+#, c-format
+msgctxt "RomDataView"
+msgid "%s:"
+msgstr ""
 
 #: ../src/libromdata/Amiibo.cpp:481
 msgctxt "Amiibo"

src/gtk/RomDataView.cpp
@@ -33,6 +33,9 @@
 #include "librpbase/img/IconAnimHelper.hpp"
 using namespace LibRpBase;
 
+// libi18n
+#include "libi18n/i18n.h"
+
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
 using LibRomData::RomDataFactory;
@@ -1093,7 +1096,7 @@ rom_data_view_init_datetime(G_GNUC_UNUSED RomDataView *page, const RomFields::Fi
 
 	if (field->data.date_time == -1) {
 		// Invalid date/time.
-		gtk_label_set_text(GTK_LABEL(widget), "Unknown");
+		gtk_label_set_text(GTK_LABEL(widget), C_("RomDataView", "Unknown"));
 		return widget;
 	}
 
@@ -1163,7 +1166,7 @@ rom_data_view_init_age_ratings(G_GNUC_UNUSED RomDataView *page, const RomFields:
 	assert(age_ratings != nullptr);
 	if (!age_ratings) {
 		// No age ratings data.
-		gtk_label_set_text(GTK_LABEL(widget), "ERROR");
+		gtk_label_set_text(GTK_LABEL(widget), C_("RomDataView", "ERROR"));
 		return widget;
 	}
 
@@ -1352,12 +1355,9 @@ rom_data_view_update_display(RomDataView *page)
 			// Add the widget to the table.
 			auto &tab = page->tabs->at(tabIdx);
 
-			// TODO: Localization.
-			std::string gtkdesc = field->name;
-			gtkdesc += ':';
-
-			// Description label.
-			GtkWidget *lblDesc = gtk_label_new(gtkdesc.c_str());
+			// Field description label.
+			string txt = rp_sprintf(C_("RomDataView", "%s:"), field->name.c_str());
+			GtkWidget *lblDesc = gtk_label_new(txt.c_str());
 			gtk_label_set_use_underline(GTK_LABEL(lblDesc), false);
 			gtk_widget_show(lblDesc);
 			gtk_size_group_add_widget(size_group, lblDesc);

src/gtk/gnome/rom-properties-provider.cpp
@@ -36,6 +36,9 @@
 #include "librpbase/TextFuncs.hpp"
 using namespace LibRpBase;
 
+// libi18n
+#include "libi18n/i18n.h"
+
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
 using LibRomData::RomDataFactory;
@@ -130,7 +133,7 @@ rom_properties_provider_get_pages(NautilusPropertyPageProvider *provider, GList 
 		// Create the NautilusPropertyPage.
 		NautilusPropertyPage *page = nautilus_property_page_new(
 			"RomPropertiesPage::property_page",
-			gtk_label_new("ROM Properties"),
+			gtk_label_new(C_("RomDataView", "ROM Properties")),
 			romDataView);
 
 		/* Add the page to the pages provided by this plugin */

src/gtk/xfce/rom-properties-page.cpp
@@ -19,18 +19,23 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
+/**
+ * References:
+ * - audio-tags plugin
+ * - http://api.xfce.m8t.in/xfce-4.10/thunarx-1.4.0/ThunarxPropertyPage.html
+ */
+
 #include "rom-properties-page.hpp"
 #include "../RomDataView.hpp"
+
+// libi18n
+#include "libi18n/i18n.h"
 
 // C++ includes.
 #include <string>
 #include <vector>
 using std::string;
 using std::vector;
-
-// References:
-// - audio-tags plugin
-// - http://api.xfce.m8t.in/xfce-4.10/thunarx-1.4.0/ThunarxPropertyPage.html
 
 /* Property identifiers */
 enum {
@@ -147,8 +152,10 @@ rom_properties_page_finalize(GObject *object)
 RomPropertiesPage*
 rom_properties_page_new(void)
 {
-	RomPropertiesPage *page = static_cast<RomPropertiesPage*>(g_object_new(TYPE_ROM_PROPERTIES_PAGE, nullptr));
-	thunarx_property_page_set_label(THUNARX_PROPERTY_PAGE(page), "ROM Properties");
+	RomPropertiesPage *page = static_cast<RomPropertiesPage*>(
+		g_object_new(TYPE_ROM_PROPERTIES_PAGE, nullptr));
+	thunarx_property_page_set_label(THUNARX_PROPERTY_PAGE(page),
+		C_("RomDataView", "ROM Properties"));
 	return page;
 }
 

src/kde/RomDataView.cpp
@@ -31,6 +31,9 @@
 #include "librpbase/img/IconAnimHelper.hpp"
 using namespace LibRpBase;
 
+// libi18n
+#include "libi18n/i18n.h"
+
 // C includes. (C++ namespace)
 #include <cassert>
 
@@ -677,7 +680,7 @@ void RomDataViewPrivate::initDateTime(QLabel *lblDesc, const RomFields::Field *f
 
 	if (field->data.date_time == -1) {
 		// Invalid date/time.
-		lblDateTime->setText(RomDataView::tr("Unknown"));
+		lblDateTime->setText(QString::fromUtf8(C_("RomDataView", "Unknown")));
 		tabs[field->tabIdx].formLayout->addRow(lblDesc, lblDateTime);
 		return;
 	}
@@ -742,7 +745,7 @@ void RomDataViewPrivate::initAgeRatings(QLabel *lblDesc, const RomFields::Field 
 	assert(age_ratings != nullptr);
 	if (!age_ratings) {
 		// No age ratings data.
-		lblAgeRatings->setText(RomDataView::tr("ERROR"));
+		lblAgeRatings->setText(QString::fromUtf8(C_("RomDataView", "ERROR")));
 		tabs[field->tabIdx].formLayout->addRow(lblDesc, lblAgeRatings);
 		return;
 	}
@@ -863,10 +866,11 @@ void RomDataViewPrivate::initDisplayWidgets(void)
 			continue;
 		}
 
-		QLabel *lblDesc = new QLabel(q);
+		// Field description label.
+		string txt = rp_sprintf(C_("RomDataView", "%s:"), field->name.c_str());
+		QLabel *lblDesc = new QLabel(QString::fromUtf8(txt.c_str()), q);
 		lblDesc->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 		lblDesc->setTextFormat(Qt::PlainText);
-		lblDesc->setText(RomDataView::tr("%1:").arg(U82Q(field->name)));
 
 		switch (field->type) {
 			case RomFields::RFT_INVALID:

src/kde/RomPropertiesDialogPlugin.cpp
@@ -37,6 +37,9 @@
 using LibRpBase::RomData;
 using LibRpBase::RpFile;
 
+// libi18n
+#include "libi18n/i18n.h"
+
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
 using LibRomData::RomDataFactory;
@@ -54,36 +57,40 @@ RomPropertiesDialogPlugin::RomPropertiesDialogPlugin(KPropertiesDialog *props, c
 #else /* QT_VERSION < 0x050000 */
 	KUrl url = props->kurl();
 #endif
-	if (url.isValid() && url.isLocalFile()) {
-		// Single file, and it's local.
-		// Open it and read the first 65536+512 bytes.
-		// TODO: Use KIO and transparent decompression?
-		QString filename = url.toLocalFile();
-		if (!filename.isEmpty()) {
-			// TODO: RpQFile wrapper.
-			// For now, using RpFile, which is an stdio wrapper.
-			unique_ptr<RpFile> file(new RpFile(Q2U8(filename), RpFile::FM_OPEN_READ));
-			if (file && file->isOpen()) {
-				// Get the appropriate RomData class for this ROM.
-				// file is dup()'d by RomData.
-				RomData *romData = RomDataFactory::create(file.get());
-				if (romData) {
-					// ROM is supported. Show the properties.
-					RomDataView *romDataView = new RomDataView(romData, props);
-					props->addPage(romDataView, tr("ROM Properties"));
+	if (!url.isValid() || !url.isLocalFile())
+		return;
 
-					// Make sure the underlying file handle is closed,
-					// since we don't need it once the RomData has been
-					// loaded by RomDataView.
-					romData->close();
+	// Single file, and it's local.
+	// Open it and read the first 65536+512 bytes.
+	// TODO: Use KIO and transparent decompression?
+	QString filename = url.toLocalFile();
+	if (filename.isEmpty())
+		return;
 
-					// RomDataView takes a reference to the RomData object.
-					// We don't need to hold on to it.
-					romData->unref();
-				}
-			}
-		}
-	}
+	// TODO: RpQFile wrapper.
+	// For now, using RpFile, which is an stdio wrapper.
+	unique_ptr<RpFile> file(new RpFile(Q2U8(filename), RpFile::FM_OPEN_READ));
+	if (!file || !file->isOpen())
+		return;
+
+	// Get the appropriate RomData class for this ROM.
+	// file is dup()'d by RomData.
+	RomData *romData = RomDataFactory::create(file.get());
+	if (!romData)
+		return;
+
+	// ROM is supported. Show the properties.
+	RomDataView *romDataView = new RomDataView(romData, props);
+	props->addPage(romDataView, QString::fromUtf8(C_("RomDataView", "ROM Properties")));
+
+	// Make sure the underlying file handle is closed,
+	// since we don't need it once the RomData has been
+	// loaded by RomDataView.
+	romData->close();
+
+	// RomDataView takes a reference to the RomData object.
+	// We don't need to hold on to it.
+	romData->unref();
 }
 
 RomPropertiesDialogPlugin::~RomPropertiesDialogPlugin()

commit 57af4d2774f94c1b7249df2581d6509331ddc9d8

[win32] RP_ShellPropSheetExt: RP2W_c() returns `const char*`, so no need to use `c_str()`.
Also, fixed an incorrect variable name.

This fixes a regression from commit 00a985bd0a6601a13c3296834204a994971b9e67.
(Localized strings in the property pages in the UI frontends.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -1293,7 +1293,7 @@ int RP_ShellPropSheetExt_Private::initDateTime(HWND hDlg, HWND hWndTab,
 	if (field->data.date_time == -1) {
 		// Invalid date/time.
 		return initString(hDlg, hWndTab, pt_start, idx, size, field,
-			RP2W_c(C_("RomDataView", "Unknown")).c_str());
+			RP2W_c(C_("RomDataView", "Unknown")));
 	}
 
 	// Format the date/time using the system locale.
@@ -1399,7 +1399,7 @@ int RP_ShellPropSheetExt_Private::initAgeRatings(HWND hDlg, HWND hWndTab,
 	if (!age_ratings) {
 		// No age ratings data.
 		return initString(hDlg, hWndTab, pt_start, idx, size, field,
-			RP2W_c(C_("RomDataView", "ERROR")).c_str());
+			RP2W_c(C_("RomDataView", "ERROR")));
 	}
 
 	// Convert the age ratings field to a string.
@@ -1718,7 +1718,7 @@ void RP_ShellPropSheetExt_Private::initDialog(HWND hDlg)
 		auto &tab = tabs[tabIdx];
 
 		// Field description label.
-		wstring txt = RP2W_s(rp_sprintf(C_("RomDataView", "%s:"), field->name.c_str()));
+		wstring desc_text = RP2W_s(rp_sprintf(C_("RomDataView", "%s:"), field->name.c_str()));
 
 		// Create the static text widget. (FIXME: Disable mnemonics?)
 		HWND hStatic = CreateWindowEx(WS_EX_NOPARENTNOTIFY | WS_EX_TRANSPARENT,