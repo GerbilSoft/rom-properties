GerbilSoft/rom-properties

commit d7fcb7c85171c0970f36aa86d36f73cd60a5dc2b

[win32] Use __ImageBase instead of saving hInstance in g_hInstance.
This saves 8 bytes of memory for the variable, may improve performance a
bit because __ImageBase is a linker variable, not a real variable, and is
compatible with both MSVC and MinGW-w64.

Reference: https://blogs.msdn.microsoft.com/oldnewthing/20041025-00/?p=37483

src/win32/DllMain.cpp
@@ -1,4 +1,4 @@
-/***************************************************************************
+\/***************************************************************************
  * ROM Properties Page shell extension. (Win32)                            *
  * DllMain.cpp: DLL entry point and COM registration handler.              *
  *                                                                         *
@@ -67,8 +67,6 @@ using std::unique_ptr;
 using std::vector;
 using std::wstring;
 
-extern HINSTANCE g_hInstance;
-HINSTANCE g_hInstance = nullptr;
 extern wchar_t dll_filename[];
 wchar_t dll_filename[MAX_PATH];
 
@@ -87,12 +85,9 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
 {
 	switch (dwReason) {
 		case DLL_PROCESS_ATTACH: {
-			// DLL loaded by a process.
-			g_hInstance = hInstance;
-
 			// Get the DLL filename.
 			SetLastError(ERROR_SUCCESS);
-			DWORD dwResult = GetModuleFileName(g_hInstance,
+			DWORD dwResult = GetModuleFileName(hInstance,
 				dll_filename, ARRAY_SIZE(dll_filename));
 			if (dwResult == 0 || GetLastError() != ERROR_SUCCESS) {
 				// Cannot get the DLL filename.

src/libwin32common/RpWin32_sdk.h
@@ -122,4 +122,10 @@ static inline ULONG InterlockedDecrement(ULONG volatile *Addend)
 #define _Outptr_
 #endif
 
+// Current image instance.
+// This is filled in by the linker.
+// Reference: https://blogs.msdn.microsoft.com/oldnewthing/20041025-00/?p=37483
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+
 #endif /* __ROMPROPERTIES_LIBWIN32COMMON_RPWIN32_SDK_H__ */

src/svrplus/svrplus.cpp
@@ -51,7 +51,6 @@ namespace {
 
 	// Globals
 
-	HINSTANCE g_hInstance; /**< hInstance of this application */
 #ifdef _WIN64
 	constexpr bool g_is64bit = true;	/**< true if running on 64-bit system */
 #else /* !_WIN64 */
@@ -291,7 +290,7 @@ namespace {
 		// Construct arguments
 		wchar_t args[14 + MAX_PATH + 4 + 3 + _countof(str_rp64path)] = L"regsvr32.exe \"";
 		// Construct path to rom-properties.dll inside the arguments
-		DWORD szModuleFn = GetModuleFileName(g_hInstance, &args[14], MAX_PATH);
+		DWORD szModuleFn = GetModuleFileName(HINST_THISCOMPONENT, &args[14], MAX_PATH);
 		assert(szModuleFn != 0);
 		assert(szModuleFn < MAX_PATH);
 		if (szModuleFn == 0 || szModuleFn >= MAX_PATH) {
@@ -749,8 +748,6 @@ namespace {
  */
 int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR lpCmdLine, int nCmdShow)
 {
-	g_hInstance = hInstance;
-
 	// Set Win32 security options.
 	secoptions_init();
 
@@ -783,11 +780,11 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR lpCmdLine, int nCmd
 
 	// Load the icon.
 	hIconDialog = static_cast<HICON>(LoadImage(
-		g_hInstance, MAKEINTRESOURCE(IDI_SVRPLUS), IMAGE_ICON,
+		hInstance, MAKEINTRESOURCE(IDI_SVRPLUS), IMAGE_ICON,
 		GetSystemMetrics(SM_CXICON),
 		GetSystemMetrics(SM_CYICON), 0));
 	hIconDialogSmall = static_cast<HICON>(LoadImage(
-		g_hInstance, MAKEINTRESOURCE(IDI_SVRPLUS), IMAGE_ICON,
+		hInstance, MAKEINTRESOURCE(IDI_SVRPLUS), IMAGE_ICON,
 		GetSystemMetrics(SM_CXSMICON),
 		GetSystemMetrics(SM_CYSMICON), 0));
 

src/win32/RP_ShellPropSheetExt.cpp
@@ -1656,8 +1656,8 @@ void RP_ShellPropSheetExt_Private::initDialog(HWND hDlg)
 			auto &tab = tabs[i];
 
 			// Create a child dialog for the tab.
-			extern HINSTANCE g_hInstance;
-			tab.hDlg = CreateDialog(g_hInstance, MAKEINTRESOURCE(IDD_SUBTAB_CHILD_DIALOG),
+			tab.hDlg = CreateDialog(HINST_THISCOMPONENT,
+				MAKEINTRESOURCE(IDD_SUBTAB_CHILD_DIALOG),
 				hDlg, SubtabDlgProc);
 			SetWindowPos(tab.hDlg, nullptr,
 				dlgRect.left, dlgRect.top,
@@ -1965,11 +1965,10 @@ IFACEMETHODIMP RP_ShellPropSheetExt::AddPages(LPFNADDPROPSHEETPAGE pfnAddPage, L
 	// https://code.msdn.microsoft.com/windowsapps/CppShellExtPropSheetHandler-d93b49b7
 
 	// Create a property sheet page.
-	extern HINSTANCE g_hInstance;
 	PROPSHEETPAGE psp;
 	psp.dwSize = sizeof(psp);
 	psp.dwFlags = PSP_USECALLBACK | PSP_USETITLE;
-	psp.hInstance = g_hInstance;
+	psp.hInstance = HINST_THISCOMPONENT;
 	psp.pszTemplate = MAKEINTRESOURCE(IDD_PROPERTY_SHEET);
 	psp.pszIcon = nullptr;
 	psp.pszTitle = L"ROM Properties";

src/win32/config/AboutTab.cpp
@@ -853,12 +853,10 @@ HPROPSHEETPAGE AboutTab::getHPropSheetPage(void)
 		return nullptr;
 	}
 
-	extern HINSTANCE g_hInstance;
-
 	PROPSHEETPAGE psp;
 	psp.dwSize = sizeof(psp);	
 	psp.dwFlags = PSP_USECALLBACK | PSP_USETITLE;
-	psp.hInstance = g_hInstance;
+	psp.hInstance = HINST_THISCOMPONENT;
 	psp.pszTemplate = MAKEINTRESOURCE(IDD_CONFIG_ABOUT);
 	psp.pszIcon = nullptr;
 	psp.pszTitle = L"About";

src/win32/config/CacheTab.cpp
@@ -458,12 +458,10 @@ HPROPSHEETPAGE CacheTab::getHPropSheetPage(void)
 		return nullptr;
 	}
 
-	extern HINSTANCE g_hInstance;
-
 	PROPSHEETPAGE psp;
 	psp.dwSize = sizeof(psp);	
 	psp.dwFlags = PSP_USECALLBACK | PSP_USETITLE;
-	psp.hInstance = g_hInstance;
+	psp.hInstance = HINST_THISCOMPONENT;
 	psp.pszIcon = nullptr;
 	psp.pszTitle = L"Thumbnail Cache";
 	psp.pfnDlgProc = CacheTabPrivate::dlgProc;

src/win32/config/ConfigDialog.cpp
@@ -38,9 +38,6 @@ using LibRpBase::Config;
 // C includes. (C++ namespace)
 #include <cassert>
 
-// DllMain.cpp
-extern HINSTANCE g_hInstance;
-
 // Property sheet tabs.
 #include "ImageTypesTab.hpp"
 #include "DownloadsTab.hpp"
@@ -138,7 +135,7 @@ ConfigDialogPrivate::ConfigDialogPrivate()
 	psh.dwSize = sizeof(psh);
 	psh.dwFlags = PSH_USECALLBACK | PSH_NOCONTEXTHELP | PSH_USEHICON;
 	psh.hwndParent = nullptr;
-	psh.hInstance = g_hInstance;
+	psh.hInstance = HINST_THISCOMPONENT;
 	psh.hIcon = PropSheetIcon::getSmallIcon();	// Small icon only!
 	psh.pszCaption = L"ROM Properties Page Configuration";
 	psh.nPages = ARRAY_SIZE(hpsp);

src/win32/config/DownloadsTab.cpp
@@ -389,12 +389,10 @@ HPROPSHEETPAGE DownloadsTab::getHPropSheetPage(void)
 		return nullptr;
 	}
 
-	extern HINSTANCE g_hInstance;
-
 	PROPSHEETPAGE psp;
 	psp.dwSize = sizeof(psp);	
 	psp.dwFlags = PSP_USECALLBACK | PSP_USETITLE;
-	psp.hInstance = g_hInstance;
+	psp.hInstance = HINST_THISCOMPONENT;
 	psp.pszTemplate = MAKEINTRESOURCE(IDD_CONFIG_DOWNLOADS);
 	psp.pszIcon = nullptr;
 	psp.pszTitle = L"Downloads";

commit 1e64d55bb09bfc3888f61a20b17db7bfc251358f

[win32] DllMain.cpp: Removed a stray backslash.
MSVC doesn't care about it, but gcc does.

This fixes a regression from commit d7fcb7c85171c0970f36aa86d36f73cd60a5dc2b.
([win32] Use __ImageBase instead of saving hInstance in g_hInstance.)

src/win32/DllMain.cpp
@@ -1,4 +1,4 @@
-\/***************************************************************************
+/***************************************************************************
  * ROM Properties Page shell extension. (Win32)                            *
  * DllMain.cpp: DLL entry point and COM registration handler.              *
  *                                                                         *