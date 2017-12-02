fc1cbd92060bcbe7c2b267677b7f79434acd9815
David Korth <gerbilsoft@gerbilsoft.com>
2017-12-02 11:19:36 -0500

    [libromdata] gl_defs.h: Added definitions that are missing in the Microsoft SDK.
    TODO: Add all definitions and get rid of the gl.h dependency?

--- src/libromdata/gl_defs.h
+++ src/libromdata/gl_defs.h
@@ -22,7 +22,17 @@
 #ifndef __ROMPROPERTIES_LIBROMDATA_GL_DEFS_H__
 #define __ROMPROPERTIES_LIBROMDATA_GL_DEFS_H__
 
+// Windows requires windows.h.
+// NOTE: May conflict with exe_structs.h.
+#ifdef _WIN32
+# ifdef __ROMPROPERTIES_LIBROMDATA_EXE_STRUCTS_H__
+#  error Cannot include both gl_defs.h and exe_structs.h
+# endif
+# include <windows.h>
+#endif
+
 // System OpenGL header.
+// TODO: Eliminate this and just define everything here?
 #ifdef __APPLE__
 # include <OpenGL/gl.h>
 #else
@@ -30,12 +40,370 @@
 #endif
 
 // Definitions that might not be present on the host system.
+// (Windows SDK only has GL 1.1 definitions.)
 // (Mostly taken from GLEW 1.13.0.)
+
+// GL_ARB_half_float_vertex
+// from GLEW 1.13.0
+#ifndef GL_HALF_FLOAT
+#define GL_HALF_FLOAT 0x140B
+#endif
+
+// GL_VERSION_1_2
+// from GLEW 1.13.0
+#ifndef GL_UNSIGNED_BYTE_3_3_2
+#define GL_UNSIGNED_BYTE_3_3_2 0x8032
+#endif
+#ifndef GL_UNSIGNED_SHORT_4_4_4_4
+#define GL_UNSIGNED_SHORT_4_4_4_4 0x8033
+#endif
+#ifndef GL_UNSIGNED_SHORT_5_5_5_1
+#define GL_UNSIGNED_SHORT_5_5_5_1 0x8034
+#endif
+#ifndef GL_UNSIGNED_INT_8_8_8_8
+#define GL_UNSIGNED_INT_8_8_8_8 0x8035
+#endif
+#ifndef GL_UNSIGNED_INT_10_10_10_2
+#define GL_UNSIGNED_INT_10_10_10_2 0x8036
+#endif
+#ifndef GL_BGR
+#define GL_BGR 0x80E0
+#endif
+#ifndef GL_BGRA
+#define GL_BGRA 0x80E1
+#endif
+#ifndef GL_UNSIGNED_BYTE_2_3_3_REV
+#define GL_UNSIGNED_BYTE_2_3_3_REV 0x8362
+#endif
+#ifndef GL_UNSIGNED_SHORT_5_6_5
+#define GL_UNSIGNED_SHORT_5_6_5 0x8363
+#endif
+#ifndef GL_UNSIGNED_SHORT_5_6_5_REV
+#define GL_UNSIGNED_SHORT_5_6_5_REV 0x8364
+#endif
+#ifndef GL_UNSIGNED_SHORT_4_4_4_4_REV
+#define GL_UNSIGNED_SHORT_4_4_4_4_REV 0x8365
+#endif
+#ifndef GL_UNSIGNED_SHORT_1_5_5_5_REV
+#define GL_UNSIGNED_SHORT_1_5_5_5_REV 0x8366
+#endif
+#ifndef GL_UNSIGNED_INT_8_8_8_8_REV
+#define GL_UNSIGNED_INT_8_8_8_8_REV 0x8367
+#endif
+
+// GL_VERSION_1_4
+// from GLEW 1.13.0
+#ifndef GL_DEPTH_COMPONENT16
+#define GL_DEPTH_COMPONENT16 0x81A5
+#endif
+#ifndef GL_DEPTH_COMPONENT24
+#define GL_DEPTH_COMPONENT24 0x81A6
+#endif
+#ifndef GL_DEPTH_COMPONENT32
+#define GL_DEPTH_COMPONENT32 0x81A7
+#endif
+
+// GL_ARB_texture_rg
+// from GLEW 1.13.0
+#ifndef GL_COMPRESSED_RED
+#define GL_COMPRESSED_RED 0x8225
+#endif
+#ifndef GL_COMPRESSED_RG
+#define GL_COMPRESSED_RG 0x8226
+#endif
+#ifndef GL_RG
+#define GL_RG 0x8227
+#endif
+#ifndef GL_RG_INTEGER
+#define GL_RG_INTEGER 0x8228
+#endif
+#ifndef GL_R8
+#define GL_R8 0x8229
+#endif
+#ifndef GL_R16
+#define GL_R16 0x822A
+#endif
+#ifndef GL_RG8
+#define GL_RG8 0x822B
+#endif
+#ifndef GL_RG16
+#define GL_RG16 0x822C
+#endif
+#ifndef GL_R16F
+#define GL_R16F 0x822D
+#endif
+#ifndef GL_R32F
+#define GL_R32F 0x822E
+#endif
+#ifndef GL_RG16F
+#define GL_RG16F 0x822F
+#endif
+#ifndef GL_RG32F
+#define GL_RG32F 0x8230
+#endif
+#ifndef GL_R8I
+#define GL_R8I 0x8231
+#endif
+#ifndef GL_R8UI
+#define GL_R8UI 0x8232
+#endif
+#ifndef GL_R16I
+#define GL_R16I 0x8233
+#endif
+#ifndef GL_R16UI
+#define GL_R16UI 0x8234
+#endif
+#ifndef GL_R32I
+#define GL_R32I 0x8235
+#endif
+#ifndef GL_R32UI
+#define GL_R32UI 0x8236
+#endif
+#ifndef GL_RG8I
+#define GL_RG8I 0x8237
+#endif
+#ifndef GL_RG8UI
+#define GL_RG8UI 0x8238
+#endif
+#ifndef GL_RG16I
+#define GL_RG16I 0x8239
+#endif
+#ifndef GL_RG16UI
+#define GL_RG16UI 0x823A
+#endif
+#ifndef GL_RG32I
+#define GL_RG32I 0x823B
+#endif
+#ifndef GL_RG32UI
+#define GL_RG32UI 0x823C
+#endif
+
+// GL_ARB_vertex_type_2_10_10_10_rev
+// from GLEW 1.13.0
+#ifndef GL_UNSIGNED_INT_2_10_10_10_REV
+#define GL_UNSIGNED_INT_2_10_10_10_REV 0x8368
+#endif
+
+// GL_VERSION_1_3
+// from GLEW 1.13.0
+#ifndef GL_COMPRESSED_RGB
+#define GL_COMPRESSED_RGB 0x84ED
+#endif
+#ifndef GL_COMPRESSED_RGBA
+#define GL_COMPRESSED_RGBA 0x84EE
+#endif
+
+// GL_ARB_framebuffer_object
+// from GLEW 1.13.0
+#ifndef GL_DEPTH_STENCIL
+#define GL_DEPTH_STENCIL 0x84F9
+#endif
+#ifndef GL_UNSIGNED_INT_24_8
+#define GL_UNSIGNED_INT_24_8 0x84FA
+#endif
+#ifndef GL_DEPTH24_STENCIL8
+#define GL_DEPTH24_STENCIL8 0x88F0
+#endif
+#ifndef GL_STENCIL_INDEX1
+#define GL_STENCIL_INDEX1 0x8D46
+#endif
+#ifndef GL_STENCIL_INDEX4
+#define GL_STENCIL_INDEX4 0x8D47
+#endif
+#ifndef GL_STENCIL_INDEX8
+#define GL_STENCIL_INDEX8 0x8D48
+#endif
+#ifndef GL_STENCIL_INDEX16
+#define GL_STENCIL_INDEX16 0x8D49
+#endif
+
+// GL_VERSION_2_1
+// from GLEW 1.13.0
+#ifndef GL_SRGB8
+#define GL_SRGB8 0x8C41
+#endif
+#ifndef GL_SRGB8_ALPHA8
+#define GL_SRGB8_ALPHA8 0x8C43
+#endif
+#ifndef GL_COMPRESSED_SRGB
+#define GL_COMPRESSED_SRGB 0x8C48
+#endif
+#ifndef GL_COMPRESSED_SRGB_ALPHA
+#define GL_COMPRESSED_SRGB_ALPHA 0x8C49
+#endif
+
+// GL_VERSION_3_0
+// from GLEW 1.13.0
+#ifndef GL_RGBA32F
+#define GL_RGBA32F 0x8814
+#endif
+#ifndef GL_RGB32F
+#define GL_RGB32F 0x8815
+#endif
+#ifndef GL_RGBA16F
+#define GL_RGBA16F 0x881A
+#endif
+#ifndef GL_RGB16F
+#define GL_RGB16F 0x881B
+#endif
+#ifndef GL_R11F_G11F_B10F
+#define GL_R11F_G11F_B10F 0x8C3A
+#endif
+#ifndef GL_RGB9_E5
+#define GL_RGB9_E5 0x8C3D
+#endif
+#ifndef GL_UNSIGNED_INT_5_9_9_9_REV
+#define GL_UNSIGNED_INT_5_9_9_9_REV 0x8C3E
+#endif
+#ifndef GL_RGBA32UI
+#define GL_RGBA32UI 0x8D70
+#endif
+#ifndef GL_RGB32UI
+#define GL_RGB32UI 0x8D71
+#endif
+#ifndef GL_RGBA16UI
+#define GL_RGBA16UI 0x8D76
+#endif
+#ifndef GL_RGB16UI
+#define GL_RGB16UI 0x8D77
+#endif
+#ifndef GL_RGBA8UI
+#define GL_RGBA8UI 0x8D7C
+#endif
+#ifndef GL_RGB8UI
+#define GL_RGB8UI 0x8D7D
+#endif
+#ifndef GL_RGBA32I
+#define GL_RGBA32I 0x8D82
+#endif
+#ifndef GL_RGB32I
+#define GL_RGB32I 0x8D83
+#endif
+#ifndef GL_RGBA16I
+#define GL_RGBA16I 0x8D88
+#endif
+#ifndef GL_RGB16I
+#define GL_RGB16I 0x8D89
+#endif
+#ifndef GL_RGBA8I
+#define GL_RGBA8I 0x8D8E
+#endif
+#ifndef GL_RGB8I
+#define GL_RGB8I 0x8D8F
+#endif
+#ifndef GL_RED_INTEGER
+#define GL_RED_INTEGER 0x8D94
+#endif
+#ifndef GL_GREEN_INTEGER
+#define GL_GREEN_INTEGER 0x8D95
+#endif
+#ifndef GL_BLUE_INTEGER
+#define GL_BLUE_INTEGER 0x8D96
+#endif
+#ifndef GL_RGB_INTEGER
+#define GL_RGB_INTEGER 0x8D98
+#endif
+#ifndef GL_RGBA_INTEGER
+#define GL_RGBA_INTEGER 0x8D99
+#endif
+#ifndef GL_BGR_INTEGER
+#define GL_BGR_INTEGER 0x8D9A
+#endif
+#ifndef GL_BGRA_INTEGER
+#define GL_BGRA_INTEGER 0x8D9B
+#endif
+
+// GL_ARB_vertex_type_10f_11f_11f_rev
+// from GLEW 1.13.0
+#ifndef GL_UNSIGNED_INT_10F_11F_11F_REV
+#define GL_UNSIGNED_INT_10F_11F_11F_REV 0x8C3B
+#endif
+
+// GL_ARB_depth_buffer_float
+// from GLEW 1.13.0
+#ifndef GL_DEPTH_COMPONENT32F
+#define GL_DEPTH_COMPONENT32F 0x8CAC
+#endif
+#ifndef GL_DEPTH32F_STENCIL8
+#define GL_DEPTH32F_STENCIL8 0x8CAD
+#endif
+#ifndef GL_FLOAT_32_UNSIGNED_INT_24_8_REV
+#define GL_FLOAT_32_UNSIGNED_INT_24_8_REV 0x8DAD
+#endif
+
+// GL_ARB_ES2_compatibility
+// from GLEW 1.13.0
+#ifndef GL_RGB565
+#define GL_RGB565 0x8D62
+#endif
 
 // GL_OES_compressed_ETC1_RGB8_texture
 // https://www.khronos.org/registry/OpenGL/extensions/OES/OES_compressed_ETC1_RGB8_texture.txt
 #ifndef GL_ETC1_RGB8_OES
 #define GL_ETC1_RGB8_OES 0x8D64
+#endif
+
+// GL_ARB_texture_compression_rgtc
+// from GLEW 1.13.0
+#ifndef GL_COMPRESSED_RED_RGTC1
+#define GL_COMPRESSED_RED_RGTC1 0x8DBB
+#endif
+#ifndef GL_COMPRESSED_SIGNED_RED_RGTC1
+#define GL_COMPRESSED_SIGNED_RED_RGTC1 0x8DBC
+#endif
+#ifndef GL_COMPRESSED_RG_RGTC2
+#define GL_COMPRESSED_RG_RGTC2 0x8DBD
+#endif
+#ifndef GL_COMPRESSED_SIGNED_RG_RGTC2
+#define GL_COMPRESSED_SIGNED_RG_RGTC2 0x8DBE
+#endif
+
+// GL_VERSION_4_2
+// from GLEW 1.13.0
+#ifndef GL_COMPRESSED_RGBA_BPTC_UNORM
+#define GL_COMPRESSED_RGBA_BPTC_UNORM 0x8E8C
+#endif
+#ifndef GL_COMPRESSED_SRGB_ALPHA_BPTC_UNORM
+#define GL_COMPRESSED_SRGB_ALPHA_BPTC_UNORM 0x8E8D
+#endif
+#ifndef GL_COMPRESSED_RGB_BPTC_SIGNED_FLOAT
+#define GL_COMPRESSED_RGB_BPTC_SIGNED_FLOAT 0x8E8E
+#endif
+#ifndef GL_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT
+#define GL_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT 0x8E8F
+#endif
+
+// GL_EXT_texture_snorm (later GL 3.1)
+// from GLEW 1.13.0
+#ifndef GL_R8_SNORM
+#define GL_R8_SNORM 0x8F94
+#endif
+#ifndef GL_RG8_SNORM
+#define GL_RG8_SNORM 0x8F95
+#endif
+#ifndef GL_RGB8_SNORM
+#define GL_RGB8_SNORM 0x8F96
+#endif
+#ifndef GL_RGBA8_SNORM
+#define GL_RGBA8_SNORM 0x8F97
+#endif
+#ifndef GL_R16_SNORM
+#define GL_R16_SNORM 0x8F98
+#endif
+#ifndef GL_RG16_SNORM
+#define GL_RG16_SNORM 0x8F99
+#endif
+#ifndef GL_RGB16_SNORM
+#define GL_RGB16_SNORM 0x8F9A
+#endif
+#ifndef GL_RGBA16_SNORM
+#define GL_RGBA16_SNORM 0x8F9B
+#endif
+
+// GL_ARB_texture_rgb10_a2ui (later GL 3.3)
+// from GLEW 1.13.0
+#ifndef GL_RGB10_A2UI
+#define GL_RGB10_A2UI 0x906F
 #endif
 
 // GL_ARB_ES3_compatibility
