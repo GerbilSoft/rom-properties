07bcc87d7db332d7d2dac74df21f1a0f56acd92e
David Korth <gerbilsoft@gerbilsoft.com>
2017-12-17 12:20:42 -0500

    package.cmd: Updated for MSVC 2017 v15.5. (Compiler v14.12)

--- scripts/package.cmd
+++ scripts/package.cmd
@@ -74,23 +74,23 @@ IF EXIST "%PRGFILES%\Microsoft Visual Studio 14.0\VC\bin\cl.exe" (
 	SET "CMAKE_GENERATOR=14 2015"
 	SET CMAKE_TOOLSET=v140_xp
 )
-IF EXIST "%PRGFILES%\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX86\x86\cl.exe" (
-	SET "MSVC_DIR=%PRGFILES%\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503"
-	SET MSVC_VERSION=14.11
+IF EXIST "%PRGFILES%\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\bin\HostX86\x86\cl.exe" (
+	SET "MSVC_DIR=%PRGFILES%\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827"
+	SET MSVC_VERSION=14.12
 	SET MSVC_YEAR=2017
 	SET "CMAKE_GENERATOR=15 2017"
 	SET CMAKE_TOOLSET=v141_xp
 )
-IF EXIST "%PRGFILES%\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.11.25503\bin\HostX86\x86\cl.exe" (
-	SET "MSVC_DIR=%PRGFILES%\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.11.25503"
-	SET MSVC_VERSION=14.11
+IF EXIST "%PRGFILES%\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.12.25827\bin\HostX86\x86\cl.exe" (
+	SET "MSVC_DIR=%PRGFILES%\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.12.25827"
+	SET MSVC_VERSION=14.12
 	SET MSVC_YEAR=2017
 	SET "CMAKE_GENERATOR=15 2017"
 	SET CMAKE_TOOLSET=v141_xp
 )
-IF EXIST "%PRGFILES%\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.11.25503\bin\HostX86\x86\cl.exe" (
-	SET "MSVC_DIR=%PRGFILES%\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.11.25503"
-	SET MSVC_VERSION=14.11
+IF EXIST "%PRGFILES%\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.12.25827\bin\HostX86\x86\cl.exe" (
+	SET "MSVC_DIR=%PRGFILES%\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.12.25827"
+	SET MSVC_VERSION=14.12
 	SET MSVC_YEAR=2017
 	SET "CMAKE_GENERATOR=15 2017"
 	SET CMAKE_TOOLSET=v141_xp
