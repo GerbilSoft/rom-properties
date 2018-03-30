52594aaf1365769f2b0a38da7e235f109c6dfb2e
David Korth <gerbilsoft@gerbilsoft.com>
2018-03-30 10:22:59 -0400

    [libi18n] i18n.c: Search for 'locale' directories on Windows.
    The directory can either be in the current module's directory (e.g. for
    EXEs in the root directory of the package) or in the parent directory
    (e.g. for DLLs in amd64/ and i386/).
    
    TODO: Only check parent directory if the current subdirectory is amd64/
    or i386/.

--- src/libi18n/i18n.c
+++ src/libi18n/i18n.c
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libi18n)                          *
  * i18n.c: Internationalization support code.                              *
  *                                                                         *
- * Copyright (c) 2017 by David Korth.                                      *
+ * Copyright (c) 2017-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -19,6 +19,9 @@
  ***************************************************************************/
 
 #include "i18n.h"
+#ifdef _WIN32
+# include "libwin32common/RpWin32_sdk.h"
+#endif
 
 /**
  * Initialize the internationalization subsystem.
@@ -26,15 +29,74 @@
  */
 int rp_i18n_init(void)
 {
-#ifdef DIR_INSTALL_LOCALE
-	static const char dirname[] = DIR_INSTALL_LOCALE;
-#else
-	// Use the current directory.
-	// TODO: On Windows, use the DLL directory.
-	static const char dirname[] = "locale";
-#endif
+	const char *base;
 
-	const char *base = bindtextdomain(RP_I18N_DOMAIN, dirname);
+#ifndef _WIN32
+	// Unix/Linux: Use the system-wide locale directory.
+	base = bindtextdomain(RP_I18N_DOMAIN, DIR_INSTALL_LOCALE);
+	if (!base) {
+		// bindtextdomain() failed.
+		return -1;
+	}
+	return 0;
+#else /* _WIN32 */
+	// Windows: Use the application-specific locale directory.
+	wchar_t dll_fullpath[MAX_PATH+16];
+	wchar_t *bs;
+	unsigned int path_len;
+	DWORD dwAttrs;
+
+	// Get the current module filename.
+	// NOTE: Delay-load only supports ANSI module names.
+	// We'll assume it's ASCII and do a simple conversion to Unicode.
+	SetLastError(ERROR_SUCCESS);
+	DWORD dwResult = GetModuleFileName(HINST_THISCOMPONENT,
+		dll_fullpath, _countof(dll_fullpath));
+	if (dwResult == 0 || GetLastError() != ERROR_SUCCESS) {
+		// Cannot get the current module filename.
+		// TODO: Windows XP doesn't SetLastError() if the
+		// filename is too big for the buffer.
+		return -1;
+	}
+
+	// Find the last backslash in dll_fullpath[].
+	bs = wcsrchr(dll_fullpath, L'\\');
+	if (!bs) {
+		// No backslashes...
+		return -1;
+	}
+
+	// Append the "locale" subdirectory.
+	wcscpy(bs+1, L"locale");
+	dwAttrs = GetFileAttributes(dll_fullpath);
+	if (dwAttrs == INVALID_FILE_ATTRIBUTES ||
+	    !(dwAttrs & FILE_ATTRIBUTE_DIRECTORY))
+	{
+		// Not found, or not a directory.
+		// Try one level up.
+		// TODO: Only if the current subdirectory is
+		// amd64/ or i386/.
+		*bs = 0;
+		bs = wcsrchr(dll_fullpath, L'\\');
+		if (!bs) {
+			// No backslashes...
+			return -1;
+		}
+
+		// Append the "locale" subdirectory.
+		wcscpy(bs+1, L"locale");
+		dwAttrs = GetFileAttributes(dll_fullpath);
+		if (dwAttrs == INVALID_FILE_ATTRIBUTES ||
+		    !(dwAttrs & FILE_ATTRIBUTE_DIRECTORY))
+		{
+			// Not found, or not a subdirectory.
+			return -1;
+		}
+	}
+
+	// Found the locale subdirectory.
+	// Bind the gettext domain.
+	base = bindtextdomain(RP_I18N_DOMAIN, dirname);
 	if (!base) {
 		// bindtextdomain() failed.
 		return -1;
@@ -42,4 +104,5 @@ int rp_i18n_init(void)
 
 	// Initialized.
 	return 0;
+#endif
 }
