GerbilSoft/rom-properties

commit 87f2e0fb850be05d65f4594b24f7187605904f4a

[libromdata] EXE: File extension should be ".vxd", not "*.vxd".
[win32] DllMain: Delete the "*.vxd" key entirely. It's not valid, so
there's no point in keeping it here.

src/win32/DllMain.cpp
@@ -591,6 +591,28 @@ STDAPI DllRegisterServer(void)
 		}
 	}
 
+	// NOTE: "*.vxd" was accidentally used in LibRomData::EXE.
+	// Manually deleting it here.
+	lResult = hkcr.deleteSubKey(L"*.vxd");
+	if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
+		return SELFREG_E_CLASS;
+	for (auto sid_iter = user_SIDs.cbegin(); sid_iter != user_SIDs.cend(); ++sid_iter) {
+		wchar_t regPath[288];
+		int len = swprintf_s(regPath, ARRAY_SIZE(regPath),
+			L"%s\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts",
+			sid_iter->c_str());
+		if (len <= 0 || len >= ARRAY_SIZE(regPath)) {
+			// Buffer isn't large enough...
+			continue;
+		}
+
+		RegKey hku(HKEY_USERS, regPath, KEY_WRITE, false);
+		if (!hku.isOpen()) return SELFREG_E_CLASS;
+		lResult = hku.deleteSubKey(L"*.vxd");
+		if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
+			return SELFREG_E_CLASS;
+	}
+
 	// Register RP_ShellPropSheetExt for all file types.
 	// Fixes an issue where it doesn't show up for .dds if
 	// Visual Studio 2017 is installed.
@@ -652,6 +674,28 @@ STDAPI DllUnregisterServer(void)
 			lResult = UnregisterUserFileType(*sid_iter, *ext_iter);
 			if (lResult != ERROR_SUCCESS) return SELFREG_E_CLASS;
 		}
+	}
+
+	// NOTE: "*.vxd" was accidentally used in LibRomData::EXE.
+	// Manually deleting it here.
+	lResult = hkcr.deleteSubKey(L"*.vxd");
+	if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
+		return SELFREG_E_CLASS;
+	for (auto sid_iter = user_SIDs.cbegin(); sid_iter != user_SIDs.cend(); ++sid_iter) {
+		wchar_t regPath[288];
+		int len = swprintf_s(regPath, ARRAY_SIZE(regPath),
+			L"%s\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts",
+			sid_iter->c_str());
+		if (len <= 0 || len >= ARRAY_SIZE(regPath)) {
+			// Buffer isn't large enough...
+			continue;
+		}
+
+		RegKey hku(HKEY_USERS, regPath, KEY_WRITE, false);
+		if (!hku.isOpen()) return SELFREG_E_CLASS;
+		lResult = hku.deleteSubKey(L"*.vxd");
+		if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
+			return SELFREG_E_CLASS;
 	}
 
 	// Unregister RP_ShellPropSheetExt for all file types.

src/libromdata/Other/EXE.cpp
@@ -1320,7 +1320,7 @@ const char *const *EXE::supportedFileExtensions_static(void)
 		".fon", ".icl",
 
 		// LE extensions
-		"*.vxd", ".386",
+		".vxd", ".386",
 
 		nullptr
 	};

commit 92a086bf0b13bf55e87089d7584290fc285bfb71

[win32] DllMain.cpp: Don't fail if "*.vxd" couldn't be deleted.
It's not that important.

Also, this failed if the HKEY_USERS subkey was not found, since we
didn't check for ERROR_FILE_NOT_FOUND.

This fixes a regression from commit 87f2e0fb850be05d65f4594b24f7187605904f4a.
([libromdata] EXE: File extension should be ".vxd", not "*.vxd".)

src/win32/DllMain.cpp
@@ -593,9 +593,8 @@ STDAPI DllRegisterServer(void)
 
 	// NOTE: "*.vxd" was accidentally used in LibRomData::EXE.
 	// Manually deleting it here.
-	lResult = hkcr.deleteSubKey(L"*.vxd");
-	if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
-		return SELFREG_E_CLASS;
+	// NOTE: Ignoring any errors to prevent `regsvr32` from failing.
+	hkcr.deleteSubKey(L"*.vxd");
 	for (auto sid_iter = user_SIDs.cbegin(); sid_iter != user_SIDs.cend(); ++sid_iter) {
 		wchar_t regPath[288];
 		int len = swprintf_s(regPath, ARRAY_SIZE(regPath),
@@ -606,11 +605,10 @@ STDAPI DllRegisterServer(void)
 			continue;
 		}
 
-		RegKey hku(HKEY_USERS, regPath, KEY_WRITE, false);
-		if (!hku.isOpen()) return SELFREG_E_CLASS;
-		lResult = hku.deleteSubKey(L"*.vxd");
-		if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
-			return SELFREG_E_CLASS;
+		RegKey hkuvxd(HKEY_USERS, regPath, KEY_WRITE, false);
+		if (!hkuvxd.isOpen() || hkuvxd.lOpenRes() == ERROR_FILE_NOT_FOUND)
+			continue;
+		hkuvxd.deleteSubKey(L"*.vxd");
 	}
 
 	// Register RP_ShellPropSheetExt for all file types.
@@ -678,9 +676,8 @@ STDAPI DllUnregisterServer(void)
 
 	// NOTE: "*.vxd" was accidentally used in LibRomData::EXE.
 	// Manually deleting it here.
-	lResult = hkcr.deleteSubKey(L"*.vxd");
-	if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
-		return SELFREG_E_CLASS;
+	// NOTE: Ignoring any errors to prevent `regsvr32` from failing.
+	hkcr.deleteSubKey(L"*.vxd");
 	for (auto sid_iter = user_SIDs.cbegin(); sid_iter != user_SIDs.cend(); ++sid_iter) {
 		wchar_t regPath[288];
 		int len = swprintf_s(regPath, ARRAY_SIZE(regPath),
@@ -691,11 +688,10 @@ STDAPI DllUnregisterServer(void)
 			continue;
 		}
 
-		RegKey hku(HKEY_USERS, regPath, KEY_WRITE, false);
-		if (!hku.isOpen()) return SELFREG_E_CLASS;
-		lResult = hku.deleteSubKey(L"*.vxd");
-		if (lResult != ERROR_SUCCESS && lResult != ERROR_FILE_NOT_FOUND)
-			return SELFREG_E_CLASS;
+		RegKey hkuvxd(HKEY_USERS, regPath, KEY_WRITE, false);
+		if (!hkuvxd.isOpen() || hkuvxd.lOpenRes() == ERROR_FILE_NOT_FOUND)
+			continue;
+		hkuvxd.deleteSubKey(L"*.vxd");
 	}
 
 	// Unregister RP_ShellPropSheetExt for all file types.