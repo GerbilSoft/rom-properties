GerbilSoft/rom-properties

commit 505703839fe429b9e05b9f649963285483d57b51

[win32] Make sure the file isn't a directory before creating a RomData object.
This would only happen if we register under the 'Directory' class
handler, which we aren't doing right now.

Also check for CD-ROM drives if the filename is a root directory.

src/win32/RP_ExtractIcon.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * RP_ExtractIcon.cpp: IExtractIcon implementation.                        *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,9 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
 // Reference: http://www.codeproject.com/Articles/338268/COM-in-C
@@ -126,6 +125,31 @@ IFACEMETHODIMP RP_ExtractIcon::Load(LPCOLESTR pszFileName, DWORD dwMode)
 	// pszFileName is the file being worked on.
 	// TODO: If the file was already loaded, don't reload it.
 	d->filename = W2U8(pszFileName);
+
+	// Check if this is a drive letter.
+	// TODO: Move to GetLocation()?
+	if (iswalpha(pszFileName[0]) &&
+	    pszFileName[1] == ':' && pszFileName[2] == '\\')
+	{
+		// This is a drive letter.
+		// Only CD-ROM (and similar) drives are supported.
+		// TODO: Verify if opening by drive letter works,
+		// or if we have to resolve the physical device name.
+		if (GetDriveType(pszFileName) != DRIVE_CDROM) {
+			// Not a CD-ROM drive.
+			return E_UNEXPECTED;
+		}
+	} else {
+		// Make sure this isn't a directory.
+		// TODO: Other checks?
+		DWORD dwAttr = GetFileAttributes(pszFileName);
+		if (dwAttr == INVALID_FILE_ATTRIBUTES ||
+		    (dwAttr & FILE_ATTRIBUTE_DIRECTORY))
+		{
+			// File cannot be opened or is a directory.
+			return E_UNEXPECTED;
+		}
+	}
 
 	// Attempt to open the ROM file.
 	unique_ptr<IRpFile> file(new RpFile(d->filename, RpFile::FM_OPEN_READ));

src/win32/RP_ExtractImage.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * RP_ExtractImage.hpp: IExtractImage implementation.                      *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,9 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
 // Reference: http://www.codeproject.com/Articles/338268/COM-in-C
@@ -130,6 +129,31 @@ IFACEMETHODIMP RP_ExtractImage::Load(LPCOLESTR pszFileName, DWORD dwMode)
 	// pszFileName is the file being worked on.
 	// TODO: If the file was already loaded, don't reload it.
 	d->filename = W2U8(pszFileName);
+
+	// Check if this is a drive letter.
+	// TODO: Move to GetLocation()?
+	if (iswalpha(pszFileName[0]) &&
+	    pszFileName[1] == ':' && pszFileName[2] == '\\')
+	{
+		// This is a drive letter.
+		// Only CD-ROM (and similar) drives are supported.
+		// TODO: Verify if opening by drive letter works,
+		// or if we have to resolve the physical device name.
+		if (GetDriveType(pszFileName) != DRIVE_CDROM) {
+			// Not a CD-ROM drive.
+			return E_UNEXPECTED;
+		}
+	} else {
+		// Make sure this isn't a directory.
+		// TODO: Other checks?
+		DWORD dwAttr = GetFileAttributes(pszFileName);
+		if (dwAttr == INVALID_FILE_ATTRIBUTES ||
+		    (dwAttr & FILE_ATTRIBUTE_DIRECTORY))
+		{
+			// File cannot be opened or is a directory.
+			return E_UNEXPECTED;
+		}
+	}
 
 	// Attempt to open the ROM file.
 	unique_ptr<IRpFile> file(new RpFile(d->filename, RpFile::FM_OPEN_READ));

src/win32/RP_ShellPropSheetExt.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * RP_ShellPropSheetExt.cpp: IShellPropSheetExt implementation.            *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,9 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
 // References:
@@ -1966,6 +1965,17 @@ IFACEMETHODIMP RP_ShellPropSheetExt::Initialize(
 		// or if we have to resolve the physical device name.
 		if (GetDriveType(filename) != DRIVE_CDROM) {
 			// Not a CD-ROM drive.
+			goto cleanup;
+		}
+	} else {
+		// Make sure this isn't a directory.
+		// TODO: Other checks?
+		// TODO BEFORE COMMIT: check if it's actually called for dirs
+		DWORD dwAttr = GetFileAttributes(filename);
+		if (dwAttr == INVALID_FILE_ATTRIBUTES ||
+		    (dwAttr & FILE_ATTRIBUTE_DIRECTORY))
+		{
+			// File cannot be opened or is a directory.
 			goto cleanup;
 		}
 	}

commit 8dac6f4f5c2537e0aaf35ecb86b07c79ea869ac7

[win32] RP_ExtractIcon, RP_ExtractImage: Drive letter filenames must be 3 characters long.
This broke icon thumbnailing on Windows 7, and probably both icon and
regular thumbnailing on Windows XP.

Also, use Unicode character constants.

This fixes a regression from commit 505703839fe429b9e05b9f649963285483d57b51.
([win32] Make sure the file isn't a directory before creating a RomData object.)

src/win32/RP_ExtractIcon.cpp
@@ -128,8 +128,8 @@ IFACEMETHODIMP RP_ExtractIcon::Load(LPCOLESTR pszFileName, DWORD dwMode)
 
 	// Check if this is a drive letter.
 	// TODO: Move to GetLocation()?
-	if (iswalpha(pszFileName[0]) &&
-	    pszFileName[1] == ':' && pszFileName[2] == '\\')
+	if (iswalpha(pszFileName[0]) && pszFileName[1] == L':' &&
+	    pszFileName[2] == L'\\' && pszFileName[3] == L'\0')
 	{
 		// This is a drive letter.
 		// Only CD-ROM (and similar) drives are supported.

src/win32/RP_ExtractImage.cpp
@@ -132,8 +132,8 @@ IFACEMETHODIMP RP_ExtractImage::Load(LPCOLESTR pszFileName, DWORD dwMode)
 
 	// Check if this is a drive letter.
 	// TODO: Move to GetLocation()?
-	if (iswalpha(pszFileName[0]) &&
-	    pszFileName[1] == ':' && pszFileName[2] == '\\')
+	if (iswalpha(pszFileName[0]) && pszFileName[1] == L':' &&
+	    pszFileName[2] == L'\\' && pszFileName[3] == L'\0')
 	{
 		// This is a drive letter.
 		// Only CD-ROM (and similar) drives are supported.