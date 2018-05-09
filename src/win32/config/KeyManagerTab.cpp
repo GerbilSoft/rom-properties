GerbilSoft/rom-properties

commit 9cfffd7c20233123b1a8c64834481878155999d9

KeyManagerTab: Keep the same current directory between Open dialog invocations.
This was definitely needed on KDE. On Windows, it seems to preserve the
last directory between invocations, though it might change if some other
component calls GetOpenFileName(), so I added it anyway.

TODO: Save the directory to the configuration file.

src/win32/config/KeyManagerTab.cpp
@@ -200,6 +200,16 @@ class KeyManagerTabPrivate
 	public:
 		/** "Import" menu actions. **/
 
+		// Starting directory for importing keys.
+		// TODO: Save this in the configuration file?
+		wstring wsKeyFileDir;
+
+		/**
+		 * Save the key file directory.
+		 * @param filename Key filename.
+		 */
+		void saveKeyFileDir(const wchar_t *filename);
+
 		/**
 		 * Convert pipes (L'|') to NULLs (L'\0').
 		 *
@@ -1432,6 +1442,26 @@ void KeyManagerTabPrivate::loadImages(void)
 /** "Import" menu actions. **/
 
 /**
+ * Save the key file directory.
+ * @param filename Key filename.
+ */
+void KeyManagerTabPrivate::saveKeyFileDir(const wchar_t *filename)
+{
+	// Save the filename, then remove everything after the first backslash.
+	// NOTE: If this is the root directory, the backslash is left intact.
+	// Otherwise, the backslash is removed.
+	wsKeyFileDir = filename;
+	size_t bspos = wsKeyFileDir.rfind(L'\\');
+	if (bspos != wstring::npos) {
+		if (bspos > 2) {
+			wsKeyFileDir.resize(bspos);
+		} else if (bspos == 2) {
+			wsKeyFileDir.resize(3);
+		}
+	}
+}
+
+/**
  * Import keys from Wii keys.bin. (BootMii format)
  */
 void KeyManagerTabPrivate::importWiiKeysBin(void)
@@ -1458,12 +1488,16 @@ void KeyManagerTabPrivate::importWiiKeysBin(void)
 	ofn.lpstrCustomFilter = nullptr;
 	ofn.lpstrFile = filename;
 	ofn.nMaxFile = ARRAY_SIZE(filename);
+	ofn.lpstrInitialDir = wsKeyFileDir.c_str();
 	ofn.lpstrTitle = wsDlgTitle.c_str();
 	ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
 
 	BOOL bRet = GetOpenFileName(&ofn);
 	if (!bRet || filename[0] == 0)
 		return;
+
+	// Save the key file directory.
+	saveKeyFileDir(filename):
 
 	KeyStoreWin32::ImportReturn iret = keyStore->importWiiKeysBin(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
@@ -1497,12 +1531,16 @@ void KeyManagerTabPrivate::importWiiUOtpBin(void)
 	ofn.lpstrCustomFilter = nullptr;
 	ofn.lpstrFile = filename;
 	ofn.nMaxFile = ARRAY_SIZE(filename);
+	ofn.lpstrInitialDir = wsKeyFileDir.c_str();
 	ofn.lpstrTitle = wsDlgTitle.c_str();
 	ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
 
 	BOOL bRet = GetOpenFileName(&ofn);
 	if (!bRet || filename[0] == 0)
 		return;
+
+	// Save the key file directory.
+	saveKeyFileDir(filename):
 
 	KeyStoreWin32::ImportReturn iret = keyStore->importWiiUOtpBin(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
@@ -1536,12 +1574,16 @@ void KeyManagerTabPrivate::import3DSboot9bin(void)
 	ofn.lpstrCustomFilter = nullptr;
 	ofn.lpstrFile = filename;
 	ofn.nMaxFile = ARRAY_SIZE(filename);
+	ofn.lpstrInitialDir = wsKeyFileDir.c_str();
 	ofn.lpstrTitle = wsDlgTitle.c_str();
 	ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
 
 	BOOL bRet = GetOpenFileName(&ofn);
 	if (!bRet || filename[0] == 0)
 		return;
+
+	// Save the key file directory.
+	saveKeyFileDir(filename):
 
 	KeyStoreWin32::ImportReturn iret = keyStore->import3DSboot9bin(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
@@ -1575,12 +1617,16 @@ void KeyManagerTabPrivate::import3DSaeskeydb(void)
 	ofn.lpstrCustomFilter = nullptr;
 	ofn.lpstrFile = filename;
 	ofn.nMaxFile = ARRAY_SIZE(filename);
+	ofn.lpstrInitialDir = wsKeyFileDir.c_str();
 	ofn.lpstrTitle = wsDlgTitle.c_str();
 	ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
 
 	BOOL bRet = GetOpenFileName(&ofn);
 	if (!bRet || filename[0] == 0)
 		return;
+
+	// Save the key file directory.
+	saveKeyFileDir(filename):
 
 	KeyStoreWin32::ImportReturn iret = keyStore->import3DSaeskeydb(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.

src/kde/config/KeyManagerTab.cpp
@@ -70,6 +70,10 @@ class KeyManagerTabPrivate
 		// KeyStoreModel.
 		KeyStoreModel *keyStoreModel;
 
+		// Starting directory for importing keys.
+		// TODO: Save this in the configuration file?
+		QString keyFileDir;
+
 		/**
 		 * Resize the QTreeView's columns to fit their contents.
 		 */
@@ -405,16 +409,17 @@ void KeyManagerTab::save(QSettings *pSettings)
  */
 void KeyManagerTab::on_actionImportWiiKeysBin_triggered(void)
 {
+	Q_D(KeyManagerTab);
 	QString filename = QFileDialog::getOpenFileName(this,
 		// tr: Wii keys.bin dialog title.
 		U82Q(C_("KeyManagerTab", "Select Wii keys.bin File")),
-		QString(),			// dir (TODO)
+		d->keyFileDir,	// dir
 		// tr: Wii keys.bin file filter. (Qt)
 		U82Q(C_("KeyManagerTab", "keys.bin (keys.bin);;Binary Files (*.bin);;All Files (*.*)")));
 	if (filename.isEmpty())
 		return;
+	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	Q_D(KeyManagerTab);
 	KeyStoreQt::ImportReturn iret = d->keyStore->importWiiKeysBin(Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("Wii keys.bin"), iret);
 }
@@ -424,16 +429,17 @@ void KeyManagerTab::on_actionImportWiiKeysBin_triggered(void)
  */
 void KeyManagerTab::on_actionImportWiiUOtpBin_triggered(void)
 {
+	Q_D(KeyManagerTab);
 	QString filename = QFileDialog::getOpenFileName(this,
 		// tr: Wii U otp.bin dialog title.
 		U82Q(C_("KeyManagerTab", "Select Wii U otp.bin File")),
-		QString(),				// dir (TODO)
+		d->keyFileDir,	// dir
 		// tr: Wii U otp.bin file filter. (Qt)
 		U82Q(C_("KeyManagerTab", "otp.bin (otp.bin);;Binary Files (*.bin);;All Files (*.*)")));
 	if (filename.isEmpty())
 		return;
+	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	Q_D(KeyManagerTab);
 	KeyStoreQt::ImportReturn iret = d->keyStore->importWiiUOtpBin(Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("Wii U otp.bin"), iret);
 }
@@ -443,16 +449,17 @@ void KeyManagerTab::on_actionImportWiiUOtpBin_triggered(void)
  */
 void KeyManagerTab::on_actionImport3DSboot9bin_triggered(void)
 {
+	Q_D(KeyManagerTab);
 	QString filename = QFileDialog::getOpenFileName(this,
 		// tr: 3DS boot9.bin dialog title.
 		U82Q(C_("KeyManagerTab", "Select 3DS boot9.bin File")),
-		QString(),			// dir (TODO)
+		d->keyFileDir,	// dir
 		// tr: 3DS boot9.bin file filter. (Qt)
 		U82Q(C_("KeyManagerTab", "boot9.bin (boot9.bin);;Binary Files (*.bin);;All Files (*.*)")));
 	if (filename.isEmpty())
 		return;
+	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	Q_D(KeyManagerTab);
 	KeyStoreQt::ImportReturn iret = d->keyStore->import3DSboot9bin(Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("3DS boot9.bin"), iret);
 }
@@ -462,16 +469,17 @@ void KeyManagerTab::on_actionImport3DSboot9bin_triggered(void)
  */
 void KeyManagerTab::on_actionImport3DSaeskeydb_triggered(void)
 {
+	Q_D(KeyManagerTab);
 	QString filename = QFileDialog::getOpenFileName(this,
 		// tr: 3DS aeskeydb.bin dialog title.
 		U82Q(C_("KeyManagerTab", "Select 3DS aeskeydb.bin File")),
-		QString(),				// dir (TODO)
+		d->keyFileDir,	// dir
 		// tr: 3DS aeskeydb.bin file filter. (Qt)
 		U82Q(C_("KeyManagerTab", "aeskeydb.bin (aeskeydb.bin);;Binary Files (*.bin);;All Files (*.*)")));
 	if (filename.isEmpty())
 		return;
+	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	Q_D(KeyManagerTab);
 	KeyStoreQt::ImportReturn iret = d->keyStore->import3DSaeskeydb(Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("3DS aeskeydb.bin"), iret);
 }

src/libwin32common/secoptions.h
@@ -214,7 +214,7 @@ static INLINE int secoptions_init(void)
 	// Remove the current directory from the DLL search path.
 	pfnSetDllDirectoryW = (PFNSETDLLDIRECTORYW)GetProcAddress(hKernel32, "SetDllDirectoryW");
 	if (pfnSetDllDirectoryW) {
-		pfnSetDllDirectoryW(L"");
+		//pfnSetDllDirectoryW(L"");
 	}
 
 	// Only search the system directory for DLLs.
@@ -223,7 +223,7 @@ static INLINE int secoptions_init(void)
 	// delay-loaded, it may be loaded from the application directory...
 	pfnSetDefaultDllDirectories = (PFNSETDEFAULTDLLDIRECTORIES)GetProcAddress(hKernel32, "SetDefaultDllDirectories");
 	if (pfnSetDefaultDllDirectories) {
-		pfnSetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_SYSTEM32);
+		//pfnSetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_SYSTEM32);
 	}
 
 	// Terminate the process if heap corruption is detected.

commit 283d0a3f06eca3dd9928255d3fdca9c92ea458d7

[win32] KeyManagerTab: Lines end with ';', not ':'.
This fixes a regression from commit 9cfffd7c20233123b1a8c64834481878155999d9.
(KeyManagerTab: Keep the same current directory between Open dialog invocations.)

src/win32/config/KeyManagerTab.cpp
@@ -1497,7 +1497,7 @@ void KeyManagerTabPrivate::importWiiKeysBin(void)
 		return;
 
 	// Save the key file directory.
-	saveKeyFileDir(filename):
+	saveKeyFileDir(filename);
 
 	KeyStoreWin32::ImportReturn iret = keyStore->importWiiKeysBin(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
@@ -1540,7 +1540,7 @@ void KeyManagerTabPrivate::importWiiUOtpBin(void)
 		return;
 
 	// Save the key file directory.
-	saveKeyFileDir(filename):
+	saveKeyFileDir(filename);
 
 	KeyStoreWin32::ImportReturn iret = keyStore->importWiiUOtpBin(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
@@ -1583,7 +1583,7 @@ void KeyManagerTabPrivate::import3DSboot9bin(void)
 		return;
 
 	// Save the key file directory.
-	saveKeyFileDir(filename):
+	saveKeyFileDir(filename);
 
 	KeyStoreWin32::ImportReturn iret = keyStore->import3DSboot9bin(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
@@ -1626,7 +1626,7 @@ void KeyManagerTabPrivate::import3DSaeskeydb(void)
 		return;
 
 	// Save the key file directory.
-	saveKeyFileDir(filename):
+	saveKeyFileDir(filename);
 
 	KeyStoreWin32::ImportReturn iret = keyStore->import3DSaeskeydb(W2U8(filename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.