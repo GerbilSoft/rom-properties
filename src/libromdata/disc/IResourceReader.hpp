GerbilSoft/rom-properties

commit 4083d4209f6688459b3a60a1e825fc3bdd3be68a

[libromdata] Moved alignFileDWORD() from the IResourceReader subclasses into IResourceReader.
This reduces code duplication and may reduce the binary size due to less
inlining of a not-that-small function.

Object sizes: (64-bit Linux, gcc-8.1.0, release build)

Before:
   text    data     bss     dec     hex filename
  10139     216       0   10355    2873 ./src/libromdata/CMakeFiles/romdata.dir/disc/NEResourceReader.cpp.o
  12428     216       0   12644    3164 ./src/libromdata/CMakeFiles/romdata.dir/disc/PEResourceReader.cpp.o
  22567     432       0   22999    59d7 Total

After:
   text    data     bss     dec     hex filename
    100       0       0     100      64 ./src/libromdata/CMakeFiles/romdata.dir/disc/IResourceReader.cpp.o
  10091     216       0   10307    2843 ./src/libromdata/CMakeFiles/romdata.dir/disc/NEResourceReader.cpp.o
  12348     216       0   12564    3114 ./src/libromdata/CMakeFiles/romdata.dir/disc/PEResourceReader.cpp.o
  22539     432       0   22971    59bb Total

Difference: -28

src/libromdata/disc/IResourceReader.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * IResourceReader.hpp: Interface for Windows resource readers.            *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,9 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
 #ifndef __ROMPROPERTIES_LIBROMDATA_DISC_IRESOURCEREADER_HPP__
@@ -48,6 +47,14 @@ class IResourceReader : public LibRpBase::IPartition
 
 	private:
 		RP_DISABLE_COPY(IResourceReader)
+
+	protected:
+		/**
+		 * DWORD alignment function.
+		 * @param file	[in] File to DWORD align.
+		 * @return 0 on success; non-zero on error.
+		 */
+		static int alignFileDWORD(LibRpBase::IRpFile *file);
 
 	public:
 		/** Resource access functions. **/

src/libromdata/CMakeLists.txt
@@ -79,6 +79,7 @@ SET(libromdata_SRCS
 	disc/GcnPartitionPrivate.cpp
 	disc/WiiPartition.cpp
 	disc/GcnFst.cpp
+	disc/IResourceReader.cpp
 	disc/PEResourceReader.cpp
 	disc/NEResourceReader.cpp
 	disc/NCCHReader.cpp

src/libromdata/disc/IResourceReader.cpp
@@ -1,0 +1,44 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * IResourceReader.cpp: Interface for Windows resource readers.            *
+ *                                                                         *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+#include "IResourceReader.hpp"
+
+#include "librpbase/file/IRpFile.hpp"
+using LibRpBase::IRpFile;
+
+namespace LibRomData {
+
+/**
+ * DWORD alignment function.
+ * @param file	[in] File to DWORD align.
+ * @return 0 on success; non-zero on error.
+ */
+int IResourceReader::alignFileDWORD(IRpFile *file)
+{
+	int ret = 0;
+	int64_t pos = file->tell();
+	if (pos % 4 != 0) {
+		pos = ALIGN(4, pos);
+		ret = file->seek(pos);
+	}
+	return ret;
+}
+
+}

src/libromdata/disc/NEResourceReader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * NEResourceReader.cpp: New Executable resource reader.                   *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,9 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
 #include "NEResourceReader.hpp"
@@ -97,13 +96,6 @@ class NEResourceReaderPrivate
 		 * @return 0 if the header matches; non-zero on error.
 		 */
 		static int load_VS_VERSION_INFO_header(IRpFile *file, const char *key, uint16_t *pLen, uint16_t *pValueLen);
-
-		/**
-		 * DWORD alignment function.
-		 * @param file	[in] File to DWORD align.
-		 * @return 0 on success; non-zero on error.
-		 */
-		static inline int alignFileDWORD(IRpFile *file);
 
 		/**
 		 * Load a string table.
@@ -351,22 +343,6 @@ int NEResourceReaderPrivate::load_VS_VERSION_INFO_header(IRpFile *file, const ch
 }
 
 /**
- * DWORD alignment function.
- * @param file	[in] File to DWORD align.
- * @return 0 on success; non-zero on error.
- */
-inline int NEResourceReaderPrivate::alignFileDWORD(IRpFile *file)
-{
-	int ret = 0;
-	int64_t pos = file->tell();
-	if (pos % 4 != 0) {
-		pos = ALIGN(4, pos);
-		ret = file->seek(pos);
-	}
-	return ret;
-}
-
-/**
  * Load a string table.
  * @param file		[in] PE version resource.
  * @param st		[out] String Table.
@@ -417,7 +393,7 @@ int NEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
 	}
 
 	// DWORD alignment.
-	alignFileDWORD(file);
+	IResourceReader::alignFileDWORD(file);
 
 	// Total string table size (in bytes) is wLength - (pos_strings - pos_start).
 	const int64_t pos_strings = file->tell();
@@ -794,7 +770,7 @@ int NEResourceReader::load_VS_VERSION_INFO(int id, int lang, VS_FIXEDFILEINFO *p
 #endif /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
 
 	// DWORD alignment, if necessary.
-	NEResourceReaderPrivate::alignFileDWORD(f_ver.get());
+	alignFileDWORD(f_ver.get());
 
 	// Read the StringFileInfo section header.
 	static const char vssfi[] = "StringFileInfo";

src/libromdata/disc/PEResourceReader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * PEResourceReader.cpp: Portable Executable resource reader.              *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,9 +14,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
 #include "PEResourceReader.hpp"
@@ -134,13 +133,6 @@ class PEResourceReaderPrivate
 		 * @return 0 if the header matches; non-zero on error.
 		 */
 		static int load_VS_VERSION_INFO_header(IRpFile *file, const char16_t *key, uint16_t type, uint16_t *pLen, uint16_t *pValueLen);
-
-		/**
-		 * DWORD alignment function.
-		 * @param file	[in] File to DWORD align.
-		 * @return 0 on success; non-zero on error.
-		 */
-		static inline int alignFileDWORD(IRpFile *file);
 
 		/**
 		 * Load a string table.
@@ -425,22 +417,6 @@ int PEResourceReaderPrivate::load_VS_VERSION_INFO_header(IRpFile *file, const ch
 }
 
 /**
- * DWORD alignment function.
- * @param file	[in] File to DWORD align.
- * @return 0 on success; non-zero on error.
- */
-inline int PEResourceReaderPrivate::alignFileDWORD(IRpFile *file)
-{
-	int ret = 0;
-	int64_t pos = file->tell();
-	if (pos % 4 != 0) {
-		pos = ALIGN(4, pos);
-		ret = file->seek(pos);
-	}
-	return ret;
-}
-
-/**
  * Load a string table.
  * @param file		[in] PE version resource.
  * @param st		[out] String Table.
@@ -492,7 +468,7 @@ int PEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
 	}
 
 	// DWORD alignment.
-	alignFileDWORD(file);
+	IResourceReader::alignFileDWORD(file);
 
 	// Total string table size (in bytes) is wLength - (pos_strings - pos_start).
 	const int64_t pos_strings = file->tell();
@@ -510,7 +486,7 @@ int PEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
 		return -EIO;
 	}
 	// DWORD alignment.
-	alignFileDWORD(file);
+	IResourceReader::alignFileDWORD(file);
 
 	// Parse the string table.
 	// TODO: Optimizations.
@@ -896,7 +872,7 @@ int PEResourceReader::load_VS_VERSION_INFO(int id, int lang, VS_FIXEDFILEINFO *p
 #endif /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
 
 	// DWORD alignment, if necessary.
-	PEResourceReaderPrivate::alignFileDWORD(f_ver.get());
+	alignFileDWORD(f_ver.get());
 
 	// Read the StringFileInfo section header.
 	static const char16_t vssfi[] = {'S','t','r','i','n','g','F','i','l','e','I','n','f','o',0};

commit 30f79e6dcabdb2e4271d023b4a17785751c4d1a0

[libromdata] IResourceReader: Marked alignFileDWORD() as public.
This broke the Mac OS X build, even though it worked fine everywhere else.

This fixes a regression from commit 4083d4209f6688459b3a60a1e825fc3bdd3be68a.
([libromdata] Moved alignFileDWORD() from the IResourceReader subclasses into IResourceReader.)

src/libromdata/disc/IResourceReader.hpp
@@ -48,7 +48,7 @@ class IResourceReader : public LibRpBase::IPartition
 	private:
 		RP_DISABLE_COPY(IResourceReader)
 
-	protected:
+	public:
 		/**
 		 * DWORD alignment function.
 		 * @param file	[in] File to DWORD align.