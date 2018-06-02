GerbilSoft/rom-properties

commit 2d79d088f79f961ef1201f1d79b9288e53544318

IFUNC functions: Return __typeof__(&function) instead of RP_IFUNC_ptr_t.
gcc-8.1 complained about this:

src/librpbase/byteswap_ifunc.c:31:23: warning: ‘ifunc’ resolver for ‘__byte_swap_16_array’ should return ‘void (*)(uint16_t *, unsigned int)’ {aka ‘void(*)(short unsigned int *, unsigned int)’} [-Wattribute-alias]
 static RP_IFUNC_ptr_t __byte_swap_16_array_resolve(void)
                       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~

cpu_dispatch.h: Removed RP_IFUNC_ptr_t since it isn't used anymore.

src/librpbase/byteswap_ifunc.c
@@ -28,21 +28,21 @@
  * IFUNC resolver function for __byte_swap_16_array().
  * @return Function pointer.
  */
-static RP_IFUNC_ptr_t __byte_swap_16_array_resolve(void)
+static __typeof__(&__byte_swap_16_array_c) __byte_swap_16_array_resolve(void)
 {
 #ifdef BYTESWAP_HAS_SSSE3
 	if (RP_CPU_HasSSSE3()) {
-		return (RP_IFUNC_ptr_t)&__byte_swap_16_array_ssse3;
+		return &__byte_swap_16_array_ssse3;
 	} else
 #endif /* BYTESWAP_HAS_SSSE3 */
 #ifdef BYTESWAP_ALWAYS_HAS_SSE2
 	{
-		return (RP_IFUNC_ptr_t)&__byte_swap_16_array_sse2;
+		return &__byte_swap_16_array_sse2;
 	}
 #else /* !BYTESWAP_ALWAYS_HAS_SSE2 */
 # ifdef BYTESWAP_HAS_SSE2
 	if (RP_CPU_HasSSE2()) {
-		return (RP_IFUNC_ptr_t)&__byte_swap_16_array_sse2;
+		return &__byte_swap_16_array_sse2;
 	} else
 # endif /* BYTESWAP_HAS_SSE2 */
 # ifdef BYTESWAP_HAS_MMX
@@ -60,27 +60,27 @@ static RP_IFUNC_ptr_t __byte_swap_16_array_resolve(void)
  * IFUNC resolver function for __byte_swap_32_array().
  * @return Function pointer.
  */
-static RP_IFUNC_ptr_t __byte_swap_32_array_resolve(void)
+static __typeof__(&__byte_swap_32_array_c) __byte_swap_32_array_resolve(void)
 {
 #ifdef BYTESWAP_HAS_SSSE3
 	if (RP_CPU_HasSSSE3()) {
-		return (RP_IFUNC_ptr_t)&__byte_swap_32_array_ssse3;
+		return &__byte_swap_32_array_ssse3;
 	} else
 #endif /* BYTESWAP_HAS_SSSE3 */
 #ifdef BYTESWAP_ALWAYS_HAS_SSE2
 	{
-		return (RP_IFUNC_ptr_t)&__byte_swap_32_array_sse2;
+		return &__byte_swap_32_array_sse2;
 	}
 #else /* !BYTESWAP_ALWAYS_HAS_SSE2 */
 # ifdef BYTESWAP_HAS_SSE2
 	if (RP_CPU_HasSSE2()) {
-		return (RP_IFUNC_ptr_t)&__byte_swap_32_array_sse2;
+		return &__byte_swap_32_array_sse2;
 	} else
 # endif /* BYTESWAP_HAS_SSE2 */
 # if 0 /* FIXME: The MMX version is actually *slower* than the C version. */
 # ifdef BYTESWAP_HAS_MMX
 	if (RP_CPU_HasMMX()) {
-		return (RP_IFUNC_ptr_t)&__byte_swap_32_array_mmx;
+		return &__byte_swap_32_array_mmx;
 	} else
 # endif /* BYTESWAP_HAS_MMX */
 # endif /* 0 */

src/gtk/GdkImageConv_ifunc.cpp
@@ -33,15 +33,15 @@ extern "C" {
  * IFUNC resolver function for rp_image_to_GdkPixbuf().
  * @return Function pointer.
  */
-static RP_IFUNC_ptr_t rp_image_to_GdkPixbuf_resolve(void)
+static __typeof__(&GdkImageConv::rp_image_to_GdkPixbuf_cpp) rp_image_to_GdkPixbuf_resolve(void)
 {
 #ifdef GDKIMAGECONV_HAS_SSSE3
 	if (RP_CPU_HasSSSE3()) {
-		return (RP_IFUNC_ptr_t)&GdkImageConv::rp_image_to_GdkPixbuf_ssse3;
+		return &GdkImageConv::rp_image_to_GdkPixbuf_ssse3;
 	} else
 #endif /* GDKIMAGECONV_HAS_SSSE3 */
 	{
-		return (RP_IFUNC_ptr_t)&GdkImageConv::rp_image_to_GdkPixbuf_cpp;
+		return &GdkImageConv::rp_image_to_GdkPixbuf_cpp;
 	}
 }
 

src/librpbase/cpu_dispatch.h
@@ -86,7 +86,6 @@
 #  define IFUNC_SSE2_INLINE
 # endif
 # define IFUNC_ATTR(func) __attribute__((ifunc(#func)))
-typedef void (*RP_IFUNC_ptr_t)(void);
 #else
 # define IFUNC_INLINE inline
 # define IFUNC_SSE2_INLINE inline

src/librpbase/img/ImageDecoder_ifunc.cpp
@@ -34,15 +34,15 @@ extern "C" {
  * IFUNC resolver function for fromLinear16().
  * @return Function pointer.
  */
-static RP_IFUNC_ptr_t fromLinear16_resolve(void)
+static __typeof__(&ImageDecoder::fromLinear16_cpp) fromLinear16_resolve(void)
 {
 #ifdef IMAGEDECODER_HAS_SSE2
 	if (RP_CPU_HasSSE2()) {
-		return (RP_IFUNC_ptr_t)&ImageDecoder::fromLinear16_sse2;
+		return &ImageDecoder::fromLinear16_sse2;
 	} else
 #endif /* IMAGEDECODER_HAS_SSE2 */
 	{
-		return (RP_IFUNC_ptr_t)&ImageDecoder::fromLinear16_cpp;
+		return &ImageDecoder::fromLinear16_cpp;
 	}
 }
 #endif /* IMAGEDECODER_ALWAYS_HAS_SSE2 */
@@ -51,15 +51,15 @@ static RP_IFUNC_ptr_t fromLinear16_resolve(void)
  * IFUNC resolver function for fromLinear24().
  * @return Function pointer.
  */
-static RP_IFUNC_ptr_t fromLinear24_resolve(void)
+static __typeof__(&ImageDecoder::fromLinear24_cpp) fromLinear24_resolve(void)
 {
 #ifdef IMAGEDECODER_HAS_SSSE3
 	if (RP_CPU_HasSSSE3()) {
-		return (RP_IFUNC_ptr_t)&ImageDecoder::fromLinear24_ssse3;
+		return &ImageDecoder::fromLinear24_ssse3;
 	} else
 #endif /* IMAGEDECODER_HAS_SSSE3 */
 	{
-		return (RP_IFUNC_ptr_t)&ImageDecoder::fromLinear24_cpp;
+		return &ImageDecoder::fromLinear24_cpp;
 	}
 }
 
@@ -67,15 +67,15 @@ static RP_IFUNC_ptr_t fromLinear24_resolve(void)
  * IFUNC resolver function for fromLinear32().
  * @return Function pointer.
  */
-static RP_IFUNC_ptr_t fromLinear32_resolve(void)
+static __typeof__(&ImageDecoder::fromLinear32_cpp) fromLinear32_resolve(void)
 {
 #ifdef IMAGEDECODER_HAS_SSSE3
 	if (RP_CPU_HasSSSE3()) {
-		return (RP_IFUNC_ptr_t)&ImageDecoder::fromLinear32_ssse3;
+		return &ImageDecoder::fromLinear32_ssse3;
 	} else
 #endif /* IMAGEDECODER_HAS_SSSE3 */
 	{
-		return (RP_IFUNC_ptr_t)&ImageDecoder::fromLinear32_cpp;
+		return &ImageDecoder::fromLinear32_cpp;
 	}
 }
 

commit 60dc2e771237fc095e1be88d8337480bc6b6f0a2

Fix the 32-bit builds: RP_IFUNC_ptr_t was removed.
I missed these when removing RP_IFUNC_ptr_t before, since these specific
instances are only used in 32-bit builds.

This fixes a regression from commit 2d79d088f79f961ef1201f1d79b9288e53544318.
(IFUNC functions: Return __typeof__(&function) instead of RP_IFUNC_ptr_t.)

src/libromdata/utils/SuperMagicDrive_ifunc.cpp
@@ -34,20 +34,20 @@ extern "C" {
  * IFUNC resolver function for decodeBlock().
  * @return Function pointer.
  */
-static RP_IFUNC_ptr_t decodeBlock_resolve(void)
+static __typeof__(&SuperMagicDrive::decodeBlock_cpp) decodeBlock_resolve(void)
 {
 #ifdef SMD_HAS_SSE2
 	if (RP_CPU_HasSSE2()) {
-		return (RP_IFUNC_ptr_t)&SuperMagicDrive::decodeBlock_sse2;
+		return &SuperMagicDrive::decodeBlock_sse2;
 	} else
 #endif /* SMD_HAS_SSE2 */
 #ifdef SMD_HAS_MMX
 	if (RP_CPU_HasMMX()) {
-		return (RP_IFUNC_ptr_t)&SuperMagicDrive::decodeBlock_mmx;
+		return &SuperMagicDrive::decodeBlock_mmx;
 	} else
 #endif /* SMD_HAS_MMX */
 	{
-		return (RP_IFUNC_ptr_t)&SuperMagicDrive::decodeBlock_cpp;
+		return &SuperMagicDrive::decodeBlock_cpp;
 	}
 }
 #endif /* SMD_ALWAYS_HAS_SSE2 */

src/librpbase/byteswap_ifunc.c
@@ -47,11 +47,11 @@ static __typeof__(&__byte_swap_16_array_c) __byte_swap_16_array_resolve(void)
 # endif /* BYTESWAP_HAS_SSE2 */
 # ifdef BYTESWAP_HAS_MMX
 	if (RP_CPU_HasMMX()) {
-		return (RP_IFUNC_ptr_t)&__byte_swap_16_array_mmx;
+		return &__byte_swap_16_array_mmx;
 	} else
 # endif /* BYTESWAP_HAS_MMX */
 	{
-		return (RP_IFUNC_ptr_t)&__byte_swap_16_array_c;
+		return &__byte_swap_16_array_c;
 	}
 #endif /* BYTESWAP_ALWAYS_HAS_SSE2 */
 }
@@ -85,7 +85,7 @@ static __typeof__(&__byte_swap_32_array_c) __byte_swap_32_array_resolve(void)
 # endif /* BYTESWAP_HAS_MMX */
 # endif /* 0 */
 	{
-		return (RP_IFUNC_ptr_t)&__byte_swap_32_array_c;
+		return &__byte_swap_32_array_c;
 	}
 #endif /* !BYTESWAP_ALWAYS_HAS_SSE2 */
 }