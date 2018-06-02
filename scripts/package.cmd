GerbilSoft/rom-properties

commit d55c1ddfa6e35f3aca5a87c059dcf1d361a69254

rom-properties 1.3

scripts/package.cmd
@@ -185,11 +185,11 @@ CMD /C "EXIT /B 0"
 :: Clear the packaging prefix.
 ECHO Clearing the pkg_windows directory...
 RMDIR /S /Q pkg_windows
-@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+::@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 MKDIR pkg_windows
-@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+::@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 CHDIR pkg_windows
-@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+::@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
 :: Compile the 32-bit version.
 ECHO.

CMakeLists.txt
@@ -42,9 +42,9 @@ SET(DESCRIPTION "ROM Properties Page shell extension")
 SET(PACKAGE_NAME "rom-properties")
 SET(AUTHOR "David Korth")
 SET(VERSION_MAJOR 1)
-SET(VERSION_MINOR 2)
+SET(VERSION_MINOR 3)
 SET(VERSION_PATCH 0)
-SET(VERSION_DEVEL 1)
+SET(VERSION_DEVEL 0)
 IF(VERSION_PATCH)
 	SET(VERSION_STRING "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
 ELSE(VERSION_PATCH)

commit 0e57d6382b463ff20118e5d002188b93be74e35f

package.cmd: Reverted some temporary changes for local building.
This doesn't really matter too much for the release build, since most
users won't be building a Windows distribution package.

This fixes a regression from commit d55c1ddfa6e35f3aca5a87c059dcf1d361a69254.
(rom-properties 1.3)

scripts/package.cmd
@@ -185,11 +185,11 @@ CMD /C "EXIT /B 0"
 :: Clear the packaging prefix.
 ECHO Clearing the pkg_windows directory...
 RMDIR /S /Q pkg_windows
-::@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 MKDIR pkg_windows
-::@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 CHDIR pkg_windows
-::@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+@IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
 :: Compile the 32-bit version.
 ECHO.