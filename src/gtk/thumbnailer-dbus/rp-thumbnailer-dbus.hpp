181084b9aaa0b6a8e47948b019148797e1582237
David Korth <gerbilsoft@gerbilsoft.com>
2018-06-07 20:20:36 -0400

    [gtk] Split the D-Bus thumbnailer out from xfce/ and into thumbnailer-dbus/.
    Preparations for Thunar 1.8.0, which was released today and uses GTK+ 3.x.
    
    NOTE: The .thumbnailer file is common for both GTK+ 2.x and 3.x,
    so it's installed as part of thumbnailer-dbus.
    
    [debian] Added a new package, rom-properties-thumbnailer-dbus.
    
    rom-properties-xfce and rom-properties-all now have dependencies on
    this package.

--- src/gtk/thumbnailer-dbus/rp-thumbnailer-dbus.hpp
+++ src/gtk/thumbnailer-dbus/rp-thumbnailer-dbus.hpp
@@ -1,8 +1,8 @@
 /***************************************************************************
- * ROM Properties Page shell extension. (GNOME)                            *
+ * ROM Properties Page shell extension. (D-Bus Thumbnailer)                *
  * rp-thumbnailer-dbus.hpp: D-Bus thumbnailer service.                     *
  *                                                                         *
- * Copyright (c) 2017 by David Korth.                                      *
+ * Copyright (c) 2017-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -14,13 +14,12 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
-#ifndef __ROMPROPERTIES_GTK_XFCE_RP_THUMBNAILER_DBUS_HPP__
-#define __ROMPROPERTIES_GTK_XFCE_RP_THUMBNAILER_DBUS_HPP__
+#ifndef __ROMPROPERTIES_GTK_THUMBNAILER_DBUS_RP_THUMBNAILER_DBUS_HPP__
+#define __ROMPROPERTIES_GTK_THUMBNAILER_DBUS_RP_THUMBNAILER_DBUS_HPP__
 
 #include <glib.h>
 #include <gio/gio.h>
@@ -57,4 +56,4 @@ gboolean	rp_thumbnailer_is_exported		(RpThumbnailer *thumbnailer);
 
 G_END_DECLS;
 
-#endif /* __ROMPROPERTIES_GTK_XFCE_RP_THUMBNAILER_DBUS_HPP__ */
+#endif /* __ROMPROPERTIES_GTK_THUMBNAILER_DBUS_RP_THUMBNAILER_DBUS_HPP__ */
