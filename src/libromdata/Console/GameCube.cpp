GerbilSoft/rom-properties

commit c4102d094833e1ddf2995f389942ca41eb0d0a6d

Minor optimizations for extURLs() functions in RomData subclasses.
Removed the d->file checks, since we don't actually need to access the
file here.

Amiibo::extURLs(): Use snprintf() instead of rp_sprintf() for the
amiibo ID to eliminate a string allocation.

----------------

File size changes: (release build, 64-bit Linux, gcc-8.1.0)

|        Filename        |   Before  |   After   |   Diff  |
|-----------------------:|----------:|----------:|--------:|
|         GameCube.cpp.o | 1,067,416 | 1,041,752 | -25,664 |
|             WiiU.cpp.o |   367,936 |   340,048 | -27,888 |
|      Nintendo3DS.cpp.o |   885,120 |   858,736 | -26,384 |
|      Nintendo3DS.cpp.o |   464,072 |   437,184 | -26,888 |
|           Amiibo.cpp.o |   243,072 |   239,856 |  -3,216 |
| rom-properties-kde5.so | 1,075,368 | 1,071,272 |  -4,096 |

Most of the reductions in the .o file was for debug symbols.

`size` changes: (release build, 64-bit Linux, gcc-8.1.0)

Before:

```
   text    data     bss     dec     hex filename
  32110     568       0   32678    7fa6 Console/GameCube.cpp.o
   8145     440       0    8585    2189 Console/WiiU.cpp.o
  31510    1320       0   32830    803e Handheld/Nintendo3DS.cpp.o
  14289     696       0   14985    3a89 Handheld/NintendoDS.cpp.o
   6750     312       0    7062    1b96 Other/Amiibo.cpp.o
 973377   96681   18192 1088250  109afa rom-properties-kde5.so
```

After:

```
   text    data     bss     dec     hex filename
  31406     568       0   31974    7ce6 Console/GameCube.cpp.o
   7287     440       0    7727    1e2f Console/WiiU.cpp.o
  30797    1320       0   32117    7d75 Handheld/Nintendo3DS.cpp.o
  13478     696       0   14174    375e Handheld/NintendoDS.cpp.o
   6708     312       0    7020    1b6c Other/Amiibo.cpp.o
 971605   96681   18192 1086478  10940e rom-properties-kde5.so
```

src/libromdata/Console/GameCube.cpp
@@ -2000,7 +2000,10 @@ int GameCube::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) c
 	pExtURLs->clear();
 
 	RP_D(const GameCube);
-	if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_TGC) {
+	if (d->discType < 0) {
+		// Unknown disc type.
+		return -EIO;
+	} else if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_TGC) {
 		// TGC game IDs aren't unique, so we can't get
 		// an image URL that makes any sense.
 		return -ENOENT;
@@ -2013,14 +2016,6 @@ int GameCube::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) c
 		return -ENOENT;
 	}
 
-	if (!d->file || !d->file->isOpen()) {
-		// File isn't open.
-		return -EBADF;
-	} else if (d->discType < 0) {
-		// Unknown disc type.
-		return -EIO;
-	}
-
 	// Get the image sizes and sort them based on the
 	// requested image size.
 	vector<ImageSizeDef> sizeDefs = supportedImageSizes(imageType);
@@ -2030,7 +2025,7 @@ int GameCube::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) c
 	}
 
 	// Select the best size.
-	const ImageSizeDef *sizeDef = d->selectBestSize(sizeDefs, size);
+	const ImageSizeDef *const sizeDef = d->selectBestSize(sizeDefs, size);
 	if (!sizeDef) {
 		// No size available...
 		return -ENOENT;
@@ -2084,7 +2079,10 @@ int GameCube::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) c
 	// TODO: If multiple image sizes are added, add the
 	// "default" size to the end of ExtURLs in case the
 	// user has high-resolution downloads disabled.
-	pExtURLs->reserve(4);
+	// See Nintendo3DS for an example.
+	// (NOTE: For GameTDB, currently only applies to coverfullHQ on GCN/Wii.)
+	pExtURLs->resize(tdb_regions.size());
+	auto extURL_iter = pExtURLs->begin();
 
 	// Disc scan: Is this not the first disc?
 	if (imageType == IMG_EXT_MEDIA &&
@@ -2096,29 +2094,26 @@ int GameCube::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) c
 		snprintf(discName, sizeof(discName), "%s%u",
 			 imageTypeName, d->discHeader.disc_number+1);
 
-		for (auto iter = tdb_regions.cbegin(); iter != tdb_regions.cend(); ++iter) {
-			int idx = (int)pExtURLs->size();
-			pExtURLs->resize(idx+1);
-			auto &extURL = pExtURLs->at(idx);
-
-			extURL.url = d->getURL_GameTDB("wii", discName, *iter, id6, ".png");
-			extURL.cache_key = d->getCacheKey_GameTDB("wii", discName, *iter, id6, ".png");
-			extURL.width = sizeDef->width;
-			extURL.height = sizeDef->height;
+		pExtURLs->resize(pExtURLs->size() * 2);
+		for (auto tdb_iter = tdb_regions.cbegin();
+		     tdb_iter != tdb_regions.cend(); ++tdb_iter, ++extURL_iter)
+		{
+			extURL_iter->url = d->getURL_GameTDB("wii", discName, *tdb_iter, id6, ".png");
+			extURL_iter->cache_key = d->getCacheKey_GameTDB("wii", discName, *tdb_iter, id6, ".png");
+			extURL_iter->width = sizeDef->width;
+			extURL_iter->height = sizeDef->height;
 		}
 	}
 
 	// First disc, or not a disc scan.
-	for (auto iter = tdb_regions.cbegin(); iter != tdb_regions.cend(); ++iter) {
-		int idx = (int)pExtURLs->size();
-		pExtURLs->resize(idx+1);
-		auto &extURL = pExtURLs->at(idx);
-
-		extURL.url = d->getURL_GameTDB("wii", imageTypeName, *iter, id6, ".png");
-		extURL.cache_key = d->getCacheKey_GameTDB("wii", imageTypeName, *iter, id6, ".png");
-		extURL.width = sizeDef->width;
-		extURL.height = sizeDef->height;
-		extURL.high_res = false;	// Only one size is available.
+	for (auto tdb_iter = tdb_regions.cbegin();
+	     tdb_iter != tdb_regions.cend(); ++tdb_iter, ++extURL_iter)
+	{
+		extURL_iter->url = d->getURL_GameTDB("wii", imageTypeName, *tdb_iter, id6, ".png");
+		extURL_iter->cache_key = d->getCacheKey_GameTDB("wii", imageTypeName, *tdb_iter, id6, ".png");
+		extURL_iter->width = sizeDef->width;
+		extURL_iter->height = sizeDef->height;
+		extURL_iter->high_res = false;	// Only one size is available.
 	}
 
 	// All URLs added.

src/libromdata/Console/WiiU.cpp
@@ -440,10 +440,7 @@ int WiiU::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) const
 	pExtURLs->clear();
 
 	RP_D(const WiiU);
-	if (!d->file || !d->file->isOpen()) {
-		// File isn't open.
-		return -EBADF;
-	} else if (!d->isValid) {
+	if (!d->isValid) {
 		// Disc image isn't valid.
 		return -EIO;
 	}
@@ -457,7 +454,7 @@ int WiiU::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) const
 	}
 
 	// Select the best size.
-	const ImageSizeDef *sizeDef = d->selectBestSize(sizeDefs, size);
+	const ImageSizeDef *const sizeDef = d->selectBestSize(sizeDefs, size);
 	if (!sizeDef) {
 		// No size available...
 		return -ENOENT;
@@ -544,7 +541,8 @@ int WiiU::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) const
 	}
 
 	// Add the URLs.
-	pExtURLs->reserve(4*szdef_count);
+	pExtURLs->resize(szdef_count * tdb_regions.size());
+	auto extURL_iter = pExtURLs->begin();
 	for (unsigned int i = 0; i < szdef_count; i++) {
 		// Current image type.
 		char imageTypeName[16];
@@ -552,16 +550,14 @@ int WiiU::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) const
 			 imageTypeName_base, (szdefs_dl[i]->name ? szdefs_dl[i]->name : ""));
 
 		// Add the images.
-		for (auto iter = tdb_regions.cbegin(); iter != tdb_regions.cend(); ++iter) {
-			int idx = (int)pExtURLs->size();
-			pExtURLs->resize(idx+1);
-			auto &extURL = pExtURLs->at(idx);
-
-			extURL.url = d->getURL_GameTDB("wiiu", imageTypeName, *iter, id6, ext);
-			extURL.cache_key = d->getCacheKey_GameTDB("wiiu", imageTypeName, *iter, id6, ext);
-			extURL.width = szdefs_dl[i]->width;
-			extURL.height = szdefs_dl[i]->height;
-			extURL.high_res = (szdefs_dl[i]->index > 0);
+		for (auto tdb_iter = tdb_regions.cbegin();
+		     tdb_iter != tdb_regions.cend(); ++tdb_iter, ++extURL_iter)
+		{
+			extURL_iter->url = d->getURL_GameTDB("wiiu", imageTypeName, *tdb_iter, id6, ext);
+			extURL_iter->cache_key = d->getCacheKey_GameTDB("wiiu", imageTypeName, *tdb_iter, id6, ext);
+			extURL_iter->width = szdefs_dl[i]->width;
+			extURL_iter->height = szdefs_dl[i]->height;
+			extURL_iter->high_res = (szdefs_dl[i]->index > 0);
 		}
 	}
 

src/libromdata/Handheld/Nintendo3DS.cpp
@@ -2540,7 +2540,10 @@ int Nintendo3DS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size
 	pExtURLs->clear();
 
 	RP_D(const Nintendo3DS);
-	if (d->romType == Nintendo3DSPrivate::ROM_TYPE_CIA) {
+	if (!d->isValid || d->romType < 0) {
+		// ROM image isn't valid.
+		return -EIO;
+	} else if (d->romType == Nintendo3DSPrivate::ROM_TYPE_CIA) {
 		// TMD needs to be loaded so we can check if it's a DSiWare SRL.
 		if (!(d->headers_loaded & Nintendo3DSPrivate::HEADER_TMD)) {
 			const_cast<Nintendo3DSPrivate*>(d)->loadTicketAndTMD();
@@ -2619,7 +2622,7 @@ int Nintendo3DS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size
 
 	// Make sure the ID4 has only printable characters.
 	// NOTE: We're checking for NULL termination above.
-	const char *id4 = &ncch_header->product_code[6];
+	const char *const id4 = &ncch_header->product_code[6];
 	for (int i = 3; i >= 0; i--) {
 		if (!ISPRINT(id4[i])) {
 			// Non-printable character found.
@@ -2636,14 +2639,6 @@ int Nintendo3DS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size
 		return -ENOENT;
 	}
 
-	if (!d->file || !d->file->isOpen()) {
-		// File isn't open.
-		return -EBADF;
-	} else if (!d->isValid || d->romType < 0) {
-		// ROM image isn't valid.
-		return -EIO;
-	}
-
 	// Get the image sizes and sort them based on the
 	// requested image size.
 	vector<ImageSizeDef> sizeDefs = supportedImageSizes(imageType);
@@ -2653,7 +2648,7 @@ int Nintendo3DS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size
 	}
 
 	// Select the best size.
-	const ImageSizeDef *sizeDef = d->selectBestSize(sizeDefs, size);
+	const ImageSizeDef *const sizeDef = d->selectBestSize(sizeDefs, size);
 	if (!sizeDef) {
 		// No size available...
 		return -ENOENT;
@@ -2711,7 +2706,8 @@ int Nintendo3DS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size
 	}
 
 	// Add the URLs.
-	pExtURLs->reserve(4*szdef_count);
+	pExtURLs->resize(szdef_count * tdb_regions.size());
+	auto extURL_iter = pExtURLs->begin();
 	for (unsigned int i = 0; i < szdef_count; i++) {
 		// Current image type.
 		char imageTypeName[16];
@@ -2719,16 +2715,14 @@ int Nintendo3DS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size
 			 imageTypeName_base, (szdefs_dl[i]->name ? szdefs_dl[i]->name : ""));
 
 		// Add the images.
-		for (auto iter = tdb_regions.cbegin(); iter != tdb_regions.cend(); ++iter) {
-			int idx = (int)pExtURLs->size();
-			pExtURLs->resize(idx+1);
-			auto &extURL = pExtURLs->at(idx);
-
-			extURL.url = d->getURL_GameTDB("3ds", imageTypeName, *iter, id4, ext);
-			extURL.cache_key = d->getCacheKey_GameTDB("3ds", imageTypeName, *iter, id4, ext);
-			extURL.width = szdefs_dl[i]->width;
-			extURL.height = szdefs_dl[i]->height;
-			extURL.high_res = (szdefs_dl[i]->index >= 2);
+		for (auto tdb_iter = tdb_regions.cbegin();
+		     tdb_iter != tdb_regions.cend(); ++tdb_iter, ++extURL_iter)
+		{
+			extURL_iter->url = d->getURL_GameTDB("3ds", imageTypeName, *tdb_iter, id4, ext);
+			extURL_iter->cache_key = d->getCacheKey_GameTDB("3ds", imageTypeName, *tdb_iter, id4, ext);
+			extURL_iter->width = szdefs_dl[i]->width;
+			extURL_iter->height = szdefs_dl[i]->height;
+			extURL_iter->high_res = (szdefs_dl[i]->index >= 2);
 		}
 	}
 

src/libromdata/Handheld/NintendoDS.cpp
@@ -1357,26 +1357,21 @@ int NintendoDS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size)
 
 	// Check for DS ROMs that don't have boxart.
 	RP_D(const NintendoDS);
-	if (!memcmp(d->romHeader.id4, "NTRJ", 4) ||
-	    !memcmp(d->romHeader.id4, "####", 4))
+	if (!d->isValid || d->romType < 0) {
+		// ROM image isn't valid.
+		return -EIO;
+	} else if (!memcmp(d->romHeader.id4, "NTRJ", 4) ||
+	           !memcmp(d->romHeader.id4, "####", 4))
 	{
 		// This is either a prototype, a download demo, or homebrew.
 		// No external images are available.
 		return -ENOENT;
 	} else if ((d->romHeader.unitcode & NintendoDSPrivate::DS_HW_DSi) &&
-		d->romHeader.dsi.filetype != DSi_FTYPE_CARTRIDGE)
+		    d->romHeader.dsi.filetype != DSi_FTYPE_CARTRIDGE)
 	{
 		// This is a DSi SRL that isn't a cartridge dump.
 		// No external images are available.
 		return -ENOENT;
-	}
-
-	if (!d->file || !d->file->isOpen()) {
-		// File isn't open.
-		return -EBADF;
-	} else if (!d->isValid || d->romType < 0) {
-		// ROM image isn't valid.
-		return -EIO;
 	}
 
 	// Get the image sizes and sort them based on the
@@ -1388,7 +1383,7 @@ int NintendoDS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size)
 	}
 
 	// Select the best size.
-	const ImageSizeDef *sizeDef = d->selectBestSize(sizeDefs, size);
+	const ImageSizeDef *const sizeDef = d->selectBestSize(sizeDefs, size);
 	if (!sizeDef) {
 		// No size available...
 		return -ENOENT;
@@ -1462,7 +1457,8 @@ int NintendoDS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size)
 	}
 
 	// Add the URLs.
-	pExtURLs->reserve(4*szdef_count);
+	pExtURLs->resize(szdef_count * tdb_regions.size());
+	auto extURL_iter = pExtURLs->begin();
 	for (unsigned int i = 0; i < szdef_count; i++) {
 		// Current image type.
 		char imageTypeName[16];
@@ -1470,16 +1466,14 @@ int NintendoDS::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size)
 			 imageTypeName_base, (szdefs_dl[i]->name ? szdefs_dl[i]->name : ""));
 
 		// Add the images.
-		for (auto iter = tdb_regions.cbegin(); iter != tdb_regions.cend(); ++iter) {
-			int idx = (int)pExtURLs->size();
-			pExtURLs->resize(idx+1);
-			auto &extURL = pExtURLs->at(idx);
-
-			extURL.url = d->getURL_GameTDB("ds", imageTypeName, *iter, id4, ext);
-			extURL.cache_key = d->getCacheKey_GameTDB("ds", imageTypeName, *iter, id4, ext);
-			extURL.width = szdefs_dl[i]->width;
-			extURL.height = szdefs_dl[i]->height;
-			extURL.high_res = (szdefs_dl[i]->index >= 2);
+		for (auto tdb_iter = tdb_regions.cbegin();
+		     tdb_iter != tdb_regions.cend(); ++tdb_iter, ++extURL_iter)
+		{
+			extURL_iter->url = d->getURL_GameTDB("ds", imageTypeName, *tdb_iter, id4, ext);
+			extURL_iter->cache_key = d->getCacheKey_GameTDB("ds", imageTypeName, *tdb_iter, id4, ext);
+			extURL_iter->width = szdefs_dl[i]->width;
+			extURL_iter->height = szdefs_dl[i]->height;
+			extURL_iter->high_res = (szdefs_dl[i]->index >= 2);
 		}
 	}
 

src/libromdata/Other/Amiibo.cpp
@@ -514,10 +514,7 @@ int Amiibo::extURLs(ImageType imageType, std::vector<ExtURL> *pExtURLs, int size
 	RP_UNUSED(size);
 
 	RP_D(Amiibo);
-	if (!d->file || !d->file->isOpen()) {
-		// File isn't open.
-		return -EBADF;
-	} else if (!d->isValid) {
+	if (!d->isValid) {
 		// Invalid file.
 		return -EIO;
 	}
@@ -535,7 +532,8 @@ int Amiibo::extURLs(ImageType imageType, std::vector<ExtURL> *pExtURLs, int size
 	auto &extURL = pExtURLs->at(0);
 
 	// Amiibo ID.
-	const string amiibo_id = rp_sprintf("%08X-%08X",
+	char amiibo_id[20];
+	snprintf(amiibo_id, sizeof(amiibo_id), "%08X-%08X",
 		be32_to_cpu(d->nfpData.char_id), be32_to_cpu(d->nfpData.amiibo_id));
 
 	// Cache key. (amiibo ID)

commit d8a8aeb1d7d255aeac468679d6b32f764a4a5ad6

[libromdata] GameCube: Fixed a crash when downloading external images for Disc 2 from multi-disc games.
The disc 2 code resizes the vector, which invalidates the iterator.
Accessing the iterator afterwards results in a crash.

Check for disc 2 before doing anything else, then resize the vector
to the appropriate size initially.

This fixes issue #158: Windows 10 - Explorer crashes when loading
multi-disc gamecube games

Thanks to @Nomelas for reporting this bug.

This fixes a regression from commit c4102d094833e1ddf2995f389942ca41eb0d0a6d.
(Minor optimizations for extURLs() functions in RomData subclasses.)

src/libromdata/Console/GameCube.cpp
@@ -2075,26 +2075,35 @@ int GameCube::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) c
 	}
 	id6[6] = 0;
 
+	// External images with multiple discs must be handled differently.
+	const bool isDisc2 =
+		(imageType >= IMG_EXT_MIN && imageType <= IMG_EXT_MAX) &&
+		 d->discHeader.disc_number > 0;
+
 	// ExtURLs.
 	// TODO: If multiple image sizes are added, add the
 	// "default" size to the end of ExtURLs in case the
 	// user has high-resolution downloads disabled.
 	// See Nintendo3DS for an example.
 	// (NOTE: For GameTDB, currently only applies to coverfullHQ on GCN/Wii.)
-	pExtURLs->resize(tdb_regions.size());
+	size_t vsz = tdb_regions.size();
+	if (isDisc2) {
+		// Need to increase the initial size.
+		// Increasing the size later invalidates the iterator.
+		vsz *= 2;
+	}
+
+	pExtURLs->resize(vsz);
 	auto extURL_iter = pExtURLs->begin();
 
-	// Disc scan: Is this not the first disc?
-	if (imageType == IMG_EXT_MEDIA &&
-	    d->discHeader.disc_number > 0)
-	{
+	// Is this not the first disc?
+	if (isDisc2) {
 		// Disc 2 (or 3, or 4...)
 		// Request the disc 2 image first.
 		char discName[16];
 		snprintf(discName, sizeof(discName), "%s%u",
 			 imageTypeName, d->discHeader.disc_number+1);
 
-		pExtURLs->resize(pExtURLs->size() * 2);
 		for (auto tdb_iter = tdb_regions.cbegin();
 		     tdb_iter != tdb_regions.cend(); ++tdb_iter, ++extURL_iter)
 		{