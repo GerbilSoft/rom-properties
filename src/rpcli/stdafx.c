dd62b1bae8aff71699e997f415cd5918cf70df79
David Korth <gerbilsoft@gerbilsoft.com>
2018-09-09 12:11:31 -0400

    Revert "[rpcli] Removed stdafx.c."
    This reverts commit 901e900a2b9a369ec4921980e6dec79bddca6211.
    
    ...DelayLoadHelper.c is C code, but it's in a different directory

---
+++ src/rpcli/stdafx.c
@@ -0,0 +1,28 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (rpcli)                            *
+ * stdafx.c: Common definitions and includes for COM.                      *
+ *                                                                         *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+// stdafx.c : source file that includes just the standard includes
+// hidecon.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
