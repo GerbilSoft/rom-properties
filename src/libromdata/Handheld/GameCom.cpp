GerbilSoft/rom-properties

commit e4d1ae2c3818505a92fc4882bda08bae6f0d5c9f

[libromdata] Close files in RomData subclass constructors if they turn out to be unsupported.
There's no point in keeping the files open here.

NOTE: The caller will usually delete the RomData subclass once it
determines the file isn't supported, which would close the files.
Regardless, we should do it ourselves anyway.

Reduced indentation in some constructors.

src/libromdata/Handheld/GameCom.cpp
@@ -294,7 +294,7 @@ GameCom::GameCom(IRpFile *file)
 
 	// Read the ROM header at the standard address.
 	size_t size = d->file->seekAndRead(GCOM_HEADER_ADDRESS, &d->romHeader, sizeof(d->romHeader));
-	if (size == sizeof(d->romHeader)) {
+	if (size != sizeof(d->romHeader)) {
 		// Check if this ROM image is supported.
 		DetectInfo info;
 		info.header.addr = GCOM_HEADER_ADDRESS;
@@ -318,6 +318,12 @@ GameCom::GameCom(IRpFile *file)
 			info.szFile = 0;	// Not needed for Gcom.
 			d->isValid = (isRomSupported_static(&info) >= 0);
 		}
+	}
+
+	if (!d->isValid) {
+		// Still not valid.
+		delete d->file;
+		d->file = nullptr;
 	}
 }
 

src/libromdata/Audio/ADX.cpp
@@ -143,8 +143,11 @@ ADX::ADX(IRpFile *file)
 	uint8_t header[4096];
 	d->file->rewind();
 	size_t size = d->file->read(header, sizeof(header));
-	if (size != sizeof(header))
+	if (size != sizeof(header)) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	// Check if this file is supported.
 	DetectInfo info;
@@ -155,8 +158,11 @@ ADX::ADX(IRpFile *file)
 	info.szFile = 0;	// Not needed for ADX.
 	d->isValid = (isRomSupported_static(&info) >= 0);
 
-	if (!d->isValid)
+	if (!d->isValid) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	// Save the ROM header.
 	memcpy(&d->adxHeader, header, sizeof(d->adxHeader));

src/libromdata/Console/Dreamcast.cpp
@@ -263,8 +263,11 @@ Dreamcast::Dreamcast(IRpFile *file)
 	CDROM_2352_Sector_t sector;
 	d->file->rewind();
 	size_t size = d->file->read(&sector, sizeof(sector));
-	if (size == 0 || size > sizeof(sector))
+	if (size == 0 || size > sizeof(sector)) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	// Check if this disc image is supported.
 	DetectInfo info;
@@ -276,8 +279,11 @@ Dreamcast::Dreamcast(IRpFile *file)
 	info.szFile = 0;	// Not needed for Dreamcast.
 	d->discType = isRomSupported_static(&info);
 
-	if (d->discType < 0)
+	if (d->discType < 0) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	switch (d->discType) {
 		case DreamcastPrivate::DISC_ISO_2048:
@@ -304,6 +310,10 @@ Dreamcast::Dreamcast(IRpFile *file)
 			const int lba_track03 = d->gdiReader->startingLBA(3);
 			if (lba_track03 < 0) {
 				// Error getting the track 03 LBA.
+				delete d->gdiReader;
+				delete d->file;
+				d->gdiReader = nullptr;
+				d->file = nullptr;
 				return;
 			}
 			// TODO: Don't hard-code 2048?

src/libromdata/Console/DreamcastSave.cpp
@@ -884,7 +884,8 @@ DreamcastSave::DreamcastSave(IRpFile *file)
 		int ret = d->readVmiHeader(d->file);
 		if (ret != 0) {
 			// Read error.
-			d->file->close();
+			delete d->file;
+			d->file = nullptr;
 			return;
 		}
 
@@ -894,7 +895,8 @@ DreamcastSave::DreamcastSave(IRpFile *file)
 	} else {
 		// Not valid.
 		d->saveType = DreamcastSavePrivate::SAVE_TYPE_UNKNOWN;
-		d->file->close();
+		delete d->file;
+		d->file = nullptr;
 		return;
 	}
 
@@ -913,7 +915,8 @@ DreamcastSave::DreamcastSave(IRpFile *file)
 			d->loaded_headers |= headerLoaded;
 		} else {
 			// Not valid.
-			d->file->close();
+			delete d->file;
+			d->file = nullptr;
 			return;
 		}
 
@@ -936,7 +939,8 @@ DreamcastSave::DreamcastSave(IRpFile *file)
 				d->loaded_headers |= headerLoaded;
 			} else {
 				// Not valid.
-				d->file->close();
+				delete d->file;
+				d->file = nullptr;
 				return;
 			}
 		}
@@ -979,7 +983,8 @@ DreamcastSave::DreamcastSave(IRpFile *vms_file, IRpFile *vmi_file)
 	d->vmi_file = vmi_file->dup();
 	if (!d->vmi_file) {
 		// Could not dup() the VMI file.
-		d->file->close();
+		delete d->file;
+		d->file = nullptr;
 		return;
 	}
 
@@ -993,9 +998,10 @@ DreamcastSave::DreamcastSave(IRpFile *vms_file, IRpFile *vmi_file)
 	      vmi_fileSize != DC_VMI_Header_SIZE)
 	{
 		// Invalid file(s).
-		d->file->close();
 		delete d->vmi_file;
+		delete d->file;
 		d->vmi_file = nullptr;
+		d->file = nullptr;
 		return;
 	}
 
@@ -1009,9 +1015,10 @@ DreamcastSave::DreamcastSave(IRpFile *vms_file, IRpFile *vmi_file)
 	int ret = d->readVmiHeader(d->vmi_file);
 	if (ret != 0) {
 		// Error reading the VMI header.
-		d->file->close();
 		delete d->vmi_file;
+		delete d->file;
 		d->vmi_file = nullptr;
+		d->file = nullptr;
 		return;
 	}
 
@@ -1029,9 +1036,10 @@ DreamcastSave::DreamcastSave(IRpFile *vms_file, IRpFile *vmi_file)
 			d->loaded_headers |= headerLoaded;
 		} else {
 			// Not valid.
-			d->file->close();
 			delete d->vmi_file;
+			delete d->file;
 			d->vmi_file = nullptr;
+			d->file = nullptr;
 			return;
 		}
 	}

src/libromdata/Console/GameCube.cpp
@@ -987,8 +987,11 @@ GameCube::GameCube(IRpFile *file)
 	uint8_t header[4096+256];
 	d->file->rewind();
 	size_t size = d->file->read(&header, sizeof(header));
-	if (size != sizeof(header))
+	if (size != sizeof(header)) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	// Check if this disc image is supported.
 	DetectInfo info;
@@ -1040,6 +1043,8 @@ GameCube::GameCube(IRpFile *file)
 	d->isValid = (d->discType >= 0);
 	if (!d->isValid) {
 		// Nothing else to do here.
+		delete d->file;
+		d->file = nullptr;
 		return;
 	}
 
@@ -1049,14 +1054,15 @@ GameCube::GameCube(IRpFile *file)
 		if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_WIA) {
 			// GCN/Wii header starts at 0x58.
 			memcpy(&d->discHeader, &header[0x58], sizeof(d->discHeader));
-			return;
 		} else {
 			// Non-WIA formats must have a valid DiscReader.
 			d->discType = GameCubePrivate::DISC_UNKNOWN;
 			d->isValid = false;
-			return;
+			delete d->file;
+			d->file = nullptr;
 		}
-	}
+		return;
+ 	}
 
 	// Save the disc header for later.
 	d->discReader->rewind();
@@ -1064,7 +1070,9 @@ GameCube::GameCube(IRpFile *file)
 	if (size != sizeof(d->discHeader)) {
 		// Error reading the disc header.
 		delete d->discReader;
+		delete d->file;
 		d->discReader = nullptr;
+		d->file = nullptr;
 		d->discType = GameCubePrivate::DISC_UNKNOWN;
 		d->isValid = false;
 		return;
@@ -1098,7 +1106,9 @@ GameCube::GameCube(IRpFile *file)
 		} else {
 			// Unknown system type.
 			delete d->discReader;
+			delete d->file;
 			d->discReader = nullptr;
+			d->file = nullptr;
 			d->discType = GameCubePrivate::DISC_UNKNOWN;
 			d->isValid = false;
 			return;
@@ -1114,7 +1124,9 @@ GameCube::GameCube(IRpFile *file)
 			if (size != sizeof(bootInfo)) {
 				// Cannot read bi2.bin.
 				delete d->discReader;
+				delete d->file;
 				d->discReader = nullptr;
+				d->file = nullptr;
 				d->discType = GameCubePrivate::DISC_UNKNOWN;
 				d->isValid = false;
 				return;
@@ -1129,7 +1141,9 @@ GameCube::GameCube(IRpFile *file)
 			if (size != sizeof(d->regionSetting)) {
 				// Cannot read RVL_RegionSetting.
 				delete d->discReader;
+				delete d->file;
 				d->discReader = nullptr;
+				d->file = nullptr;
 				d->discType = GameCubePrivate::DISC_UNKNOWN;
 				d->isValid = false;
 				return;
@@ -1141,7 +1155,9 @@ GameCube::GameCube(IRpFile *file)
 		default:
 			// Unknown system.
 			delete d->discReader;
+			delete d->file;
 			d->discReader = nullptr;
+			d->file = nullptr;
 			d->discType = GameCubePrivate::DISC_UNKNOWN;
 			d->isValid = false;
 			return;

src/libromdata/Console/GameCubeSave.cpp
@@ -565,8 +565,11 @@ GameCubeSave::GameCubeSave(IRpFile *file)
 	uint8_t header[1024];
 	d->file->rewind();
 	size_t size = d->file->read(&header, sizeof(header));
-	if (size != sizeof(header))
+	if (size != sizeof(header)) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	// Check if this disc image is supported.
 	DetectInfo info;
@@ -592,6 +595,8 @@ GameCubeSave::GameCubeSave(IRpFile *file)
 		default:
 			// Unknown save type.
 			d->saveType = GameCubeSavePrivate::SAVE_TYPE_UNKNOWN;
+			delete d->file;
+			d->file = nullptr;
 			return;
 	}
 

src/libromdata/Console/MegaDrive.cpp
@@ -541,8 +541,11 @@ MegaDrive::MegaDrive(IRpFile *file)
 	// Read the ROM header. [0x400 bytes]
 	uint8_t header[0x400];
 	size_t size = d->file->read(header, sizeof(header));
-	if (size != sizeof(header))
+	if (size != sizeof(header)) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	// Check if this ROM is supported.
 	DetectInfo info;
@@ -624,6 +627,10 @@ MegaDrive::MegaDrive(IRpFile *file)
 		// Parse the MD region code.
 		d->md_region = MegaDriveRegions::parseRegionCodes(
 			d->romHeader.region_codes, sizeof(d->romHeader.region_codes));
+	} else {
+		// Not valid. Close the file.
+		delete d->file;
+		d->file = nullptr;
 	}
 }
 

src/libromdata/Console/N64.cpp
@@ -111,8 +111,11 @@ N64::N64(IRpFile *file)
 	// Read the ROM image header.
 	d->file->rewind();
 	size_t size = d->file->read(&d->romHeader, sizeof(d->romHeader));
-	if (size != sizeof(d->romHeader))
+	if (size != sizeof(d->romHeader)) {
+		delete d->file;
+		d->file = nullptr;
 		return;
+	}
 
 	// Check if this ROM image is supported.
 	DetectInfo info;
@@ -155,6 +158,8 @@ N64::N64(IRpFile *file)
 		default:
 			// Unknown ROM type.
 			d->romType = N64Private::ROM_TYPE_UNKNOWN;
+			delete d->file;
+			d->file = nullptr;
 			return;
 	}
 

commit 576dbac5da9b09b306dc37a72fb5b4b5ad0539ff

[libromdata] GameCom: Fixed detection of ROMs with the header at 0.
I'm not sure how the '==' got changed to a '!=' here...

This fixes a regression from commit e4d1ae2c3818505a92fc4882bda08bae6f0d5c9f.
([libromdata] Close files in RomData subclass constructors if they turn out to be unsupported.)

src/libromdata/Handheld/GameCom.cpp
@@ -294,7 +294,7 @@ GameCom::GameCom(IRpFile *file)
 
 	// Read the ROM header at the standard address.
 	size_t size = d->file->seekAndRead(GCOM_HEADER_ADDRESS, &d->romHeader, sizeof(d->romHeader));
-	if (size != sizeof(d->romHeader)) {
+	if (size == sizeof(d->romHeader)) {
 		// Check if this ROM image is supported.
 		DetectInfo info;
 		info.header.addr = GCOM_HEADER_ADDRESS;