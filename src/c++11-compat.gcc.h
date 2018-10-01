9c5d57ce7b273175ddb13be12e5c8a3af098f1f2
David Korth <gerbilsoft@gerbilsoft.com>
2018-09-30 20:49:59 -0400

    Updated the copyright headers in some of the C++ 2011 compatibility header files.

--- src/c++11-compat.gcc.h
+++ src/c++11-compat.gcc.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * c++11-compat.gcc.h: C++ 2011 compatibility header. (gcc)                *
  *                                                                         *
- * Copyright (c) 2011-2015 by David Korth.                                 *
+ * Copyright (c) 2011-2018 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -13,9 +13,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  * GNU General Public License for more details.                            *
  *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
 #ifndef __CXX11_COMPAT_GCC_H__
