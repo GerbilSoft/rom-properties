a83fb2d76f521cd876e95adc407a03f04d331643
David Korth <gerbilsoft@gerbilsoft.com>
2019-01-02 11:14:44 -0500

    [librpbase] RomFields::ageRatingsDecode(): Use an iterator.
    We're keeping the `for` loop index for the fallback value, but this
    still helps to reduce code size: (64-bit Gentoo Linux, gcc-8.2.0-r6)
    
       text    data     bss     dec     hex filename
      20385       8       0   20393    4fa9 ./src/librpbase/CMakeFiles/rpbase.dir/RomFields.cpp.o [before]
      20369       8       0   20377    4f99 ./src/librpbase/CMakeFiles/rpbase.dir/RomFields.cpp.o [after]
        -16       0       0     -16     -10 Difference

--- src/librpbase/RomFields.cpp
+++ src/librpbase/RomFields.cpp
@@ -490,8 +490,9 @@ string RomFields::ageRatingsDecode(const age_ratings_t *age_ratings, bool newlin
 	string str;
 	str.reserve(64);
 	unsigned int ratings_count = 0;
+	auto iter = age_ratings->cbegin();
 	for (int i = 0; i < static_cast<int>(age_ratings->size()); i++) {
-		const uint16_t rating = age_ratings->at(i);
+		const uint16_t rating = *iter;
 		if (!(rating & RomFields::AGEBF_ACTIVE))
 			continue;
 
