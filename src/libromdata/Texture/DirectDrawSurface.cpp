GerbilSoft/rom-properties

commit 75ffdd5359f3f0ffd8dcceb49a3fe68dc19801c3

[libromdata] DirectDrawSurface: Fix FourCC endianness handling.
Since we switched to multi-character constants, we have to handle FourCC
as if it's big-endian, not little-endian. Otherwise, the checks will all
fail, since the FourCC will be byteswapped.

This fixes a regression from commit be366f15ebdd406bad8ad91d118e7b28ab4162ef.
([libromdata] dds_structs.h: Use multi-character constants for FourCCs.)

src/libromdata/Texture/DirectDrawSurface.cpp
@@ -834,8 +834,8 @@ DirectDrawSurface::DirectDrawSurface(IRpFile *file)
 
 	// Is this a DXT10 texture?
 	const DDS_HEADER *const pSrcHeader = reinterpret_cast<const DDS_HEADER*>(&header[4]);
-	if (pSrcHeader->ddspf.dwFourCC == le32_to_cpu(DDPF_FOURCC_DX10) ||
-	    pSrcHeader->ddspf.dwFourCC == le32_to_cpu(DDPF_FOURCC_XBOX))
+	if (pSrcHeader->ddspf.dwFourCC == cpu_to_be32(DDPF_FOURCC_DX10) ||
+	    pSrcHeader->ddspf.dwFourCC == cpu_to_be32(DDPF_FOURCC_XBOX))
 	{
 		const bool isXbox = (pSrcHeader->ddspf.dwFourCC == le32_to_cpu(DDPF_FOURCC_XBOX));
 		// Verify the size.
@@ -903,16 +903,19 @@ DirectDrawSurface::DirectDrawSurface(IRpFile *file)
 	d->ddsHeader.dwCaps4	= le32_to_cpu(d->ddsHeader.dwCaps4);
 
 	// Byteswap the DDS pixel format.
+	// NOTE: FourCC is handled separately.
 	DDS_PIXELFORMAT &ddspf = d->ddsHeader.ddspf;
 	ddspf.dwSize		= le32_to_cpu(ddspf.dwSize);
 	ddspf.dwFlags		= le32_to_cpu(ddspf.dwFlags);
-	ddspf.dwFourCC		= le32_to_cpu(ddspf.dwFourCC);
 	ddspf.dwRGBBitCount	= le32_to_cpu(ddspf.dwRGBBitCount);
 	ddspf.dwRBitMask	= le32_to_cpu(ddspf.dwRBitMask);
 	ddspf.dwGBitMask	= le32_to_cpu(ddspf.dwGBitMask);
 	ddspf.dwBBitMask	= le32_to_cpu(ddspf.dwBBitMask);
 	ddspf.dwABitMask	= le32_to_cpu(ddspf.dwABitMask);
 #endif /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
+
+	// FourCC is considered to be big-endian.
+	d->ddsHeader.ddspf.dwFourCC = be32_to_cpu(d->ddsHeader.ddspf.dwFourCC);
 
 	// Update the pixel format.
 	d->updatePixelFormat();

commit b153f5219c5676a33deaa4056dcc836c8f4b5cd2

[libromdata] DirectDrawSurface: Fix swapped FourCC in field data.
This fixes a regression from commit 75ffdd5359f3f0ffd8dcceb49a3fe68dc19801c3.
([libromdata] DirectDrawSurface: Fix FourCC endianness handling.)

Also, only do the FourCC byteswap on little-endian. Granted, gcc would
probably optimize it out, but we should be explicit.

src/libromdata/Texture/DirectDrawSurface.cpp
@@ -913,10 +913,10 @@ DirectDrawSurface::DirectDrawSurface(IRpFile *file)
 	ddspf.dwGBitMask	= le32_to_cpu(ddspf.dwGBitMask);
 	ddspf.dwBBitMask	= le32_to_cpu(ddspf.dwBBitMask);
 	ddspf.dwABitMask	= le32_to_cpu(ddspf.dwABitMask);
-#endif /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
-
+#else /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
 	// FourCC is considered to be big-endian.
 	d->ddsHeader.ddspf.dwFourCC = be32_to_cpu(d->ddsHeader.ddspf.dwFourCC);
+#endif
 
 	// Update the pixel format.
 	d->updatePixelFormat();
@@ -1125,12 +1125,14 @@ int DirectDrawSurface::loadFieldData(void)
 	if (ddspf.dwFlags & DDPF_FOURCC) {
 		// Compressed RGB data.
 		// TODO: Union of uint32_t and char?
+		// NOTE: dwFourCC is byteswapped from 'big-endian'
+		// to host-endian, so it needs to be reversed here.
 		d->fields->addField_string(C_("DirectDrawSurface", "Pixel Format"),
 			rp_sprintf("%c%c%c%c",
-				 ddspf.dwFourCC        & 0xFF,
-				(ddspf.dwFourCC >>  8) & 0xFF,
+				(ddspf.dwFourCC >> 24) & 0xFF,
 				(ddspf.dwFourCC >> 16) & 0xFF,
-				(ddspf.dwFourCC >> 24) & 0xFF));
+				(ddspf.dwFourCC >>  8) & 0xFF,
+				 ddspf.dwFourCC        & 0xFF));
 	} else if (ddspf.dwFlags & DDPF_RGB) {
 		// Uncompressed RGB data.
 		const char *pxfmt = d->getPixelFormatName(ddspf);