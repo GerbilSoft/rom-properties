GerbilSoft/rom-properties

commit 4cd315dcf324189b882d4946a906b677db6df6aa

[librpbase] ImageDecoder_BC7: std::swap<> seems to be rather inefficient in some cases when using -ftree-vectorize.
When using uint8_t and -ftree-vectorize on Compiler Explorer (godbolt.org),
gcc-8.2.0 generates a ton of SSE2 instructions for seemingly no reason.

Maybe it's also unrolling loops?

src/librpbase/img/ImageDecoder_BC7.cpp
@@ -23,10 +23,6 @@
 #include "ImageDecoder.hpp"
 #include "ImageDecoder_p.hpp"
 
-// C++ includes.
-#include <algorithm>	// std::swap() until C++11
-#include <utility>	// std::swap() since C++11
-
 // References:
 // - https://msdn.microsoft.com/en-us/library/windows/desktop/hh308953(v=vs.85).aspx
 // - https://msdn.microsoft.com/en-us/library/windows/desktop/hh308954(v=vs.85).aspx
@@ -796,19 +792,25 @@ rp_image *ImageDecoder::fromBC7(int width, int height,
 			case 1:
 				// RAGB: Swap A and R.
 				for (unsigned int i = 0; i < 16; i++) {
-					std::swap(tileBuf[i].a, tileBuf[i].r);
+					uint8_t a = tileBuf[i].a;
+					tileBuf[i].a = tileBuf[i].r;
+					tileBuf[i].r = a;
 				}
 				break;
 			case 2:
 				// GRAB: Swap A and G.
 				for (unsigned int i = 0; i < 16; i++) {
-					std::swap(tileBuf[i].a, tileBuf[i].g);
+					uint8_t a = tileBuf[i].a;
+					tileBuf[i].a = tileBuf[i].g;
+					tileBuf[i].r = a;
 				}
 				break;
 			case 3:
 				// BRGA: Swap A and B.
 				for (unsigned int i = 0; i < 16; i++) {
-					std::swap(tileBuf[i].a, tileBuf[i].b);
+					uint8_t a = tileBuf[i].a;
+					tileBuf[i].a = tileBuf[i].b;
+					tileBuf[i].r = a;
 				}
 				break;
 		}

commit b52405997e6372878d04f997230ae7b037b3d9fd

[librpbase] ImageDecoder_BC7: Fix copy/paste errors in component rotation.
This fixes a regression from commit 4cd315dcf324189b882d4946a906b677db6df6aa.
([librpbase] ImageDecoder_BC7: std::swap<> seems to be rather inefficient
in some cases when using -ftree-vectorize.)

src/librpbase/img/ImageDecoder_BC7.cpp
@@ -684,7 +684,7 @@ rp_image *ImageDecoder::fromBC7(int width, int height,
 				for (unsigned int i = 0; i < 16; i++) {
 					uint8_t a = tileBuf[i].a;
 					tileBuf[i].a = tileBuf[i].g;
-					tileBuf[i].r = a;
+					tileBuf[i].g = a;
 				}
 				break;
 			case 3:
@@ -692,7 +692,7 @@ rp_image *ImageDecoder::fromBC7(int width, int height,
 				for (unsigned int i = 0; i < 16; i++) {
 					uint8_t a = tileBuf[i].a;
 					tileBuf[i].a = tileBuf[i].b;
-					tileBuf[i].r = a;
+					tileBuf[i].b = a;
 				}
 				break;
 		}