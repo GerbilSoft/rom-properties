GerbilSoft/rom-properties

commit f1642dc77ad2ce44412e63fa16ede4802ee64195

Factor out C_("RomDataView", "%s:") in the UI frontends.
This probably won't reduce code size, but it should improve performance
a bit, especially in RomDataView where it has to call things once per
field.

src/win32/RP_ShellPropSheetExt.cpp
@@ -1645,6 +1645,10 @@ void RP_ShellPropSheetExt_Private::initDialog(HWND hDlg)
 	// TODO: Line breaks?
 	int max_text_width = 0;
 	SIZE textSize;
+
+	// tr: Field description label.
+	const wstring desc_text = U82W_s(rp_sprintf(
+		C_("RomDataView", "%s:"), field->name.c_str()));
 	for (int i = 0; i < count; i++) {
 		const RomFields::Field *field = fields->field(i);
 		assert(field != nullptr);
@@ -1655,10 +1659,6 @@ void RP_ShellPropSheetExt_Private::initDialog(HWND hDlg)
 			w_desc_text.push_back(wstring());
 			continue;
 		}
-
-		// tr: Field description label.
-		const wstring desc_text = U82W_s(rp_sprintf(
-			C_("RomDataView", "%s:"), field->name.c_str()));
 
 		// Get the width of this specific entry.
 		// TODO: Use measureTextSize()?

src/gtk/RomDataView.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (GTK+ common)                      *
  * RomDataView.cpp: RomData viewer widget.                                 *
  *                                                                         *
- * Copyright (c) 2017 by David Korth.                                      *
+ * Copyright (c) 2017-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -1254,6 +1254,9 @@ rom_data_view_update_display(RomDataView *page)
 	// need to manage it ourselves.
 	GtkSizeGroup *const size_group = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
 
+	// tr: Field description label.
+	const char *const desc_label_fmt = C_("RomDataView", "%s:");
+
 	// Create the data widgets.
 	for (int i = 0; i < count; i++) {
 		const RomFields::Field *field = fields->field(i);
@@ -1309,7 +1312,7 @@ rom_data_view_update_display(RomDataView *page)
 			auto &tab = page->tabs->at(tabIdx);
 
 			// tr: Field description label.
-			string txt = rp_sprintf(C_("RomDataView", "%s:"), field->name.c_str());
+			string txt = rp_sprintf(desc_label_fmt, field->name.c_str());
 			GtkWidget *lblDesc = gtk_label_new(txt.c_str());
 			gtk_label_set_use_underline(GTK_LABEL(lblDesc), false);
 			gtk_widget_show(lblDesc);

src/kde/RomDataView.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KDE5)                        *
  * RomDataView.hpp: RomData viewer.                                        *
  *                                                                         *
- * Copyright (c) 2016-2017 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -861,6 +861,9 @@ void RomDataViewPrivate::initDisplayWidgets(void)
 	// TODO: Ensure the description column has the
 	// same width on all tabs.
 
+	// tr: Field description label.
+	const char *const desc_label_fmt = C_("RomDataView", "%s:");
+
 	// Create the data widgets.
 	for (int i = 0; i < count; i++) {
 		const RomFields::Field *field = fields->field(i);
@@ -880,7 +883,7 @@ void RomDataViewPrivate::initDisplayWidgets(void)
 		}
 
 		// tr: Field description label.
-		string txt = rp_sprintf(C_("RomDataView", "%s:"), field->name.c_str());
+		string txt = rp_sprintf(desc_label_fmt, field->name.c_str());
 		QLabel *lblDesc = new QLabel(U82Q(txt.c_str()), q);
 		lblDesc->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 		lblDesc->setTextFormat(Qt::PlainText);

commit 9fd3a02279d970f3a37cb8f7533b6a8959cd7e8b

[win32] RP_ShellPropSheetExt: Fix desc_text.
I completely screwed up the C_() refactoring here.

This fixes a regression from commit f1642dc77ad2ce44412e63fa16ede4802ee64195.
(Factor out C_("RomDataView", "%s:") in the UI frontends.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -1647,8 +1647,7 @@ void RP_ShellPropSheetExt_Private::initDialog(HWND hDlg)
 	SIZE textSize;
 
 	// tr: Field description label.
-	const wstring desc_text = U82W_s(rp_sprintf(
-		C_("RomDataView", "%s:"), field->name.c_str()));
+	const char *const desc_label_fmt = C_("RomDataView", "%s:");
 	for (int i = 0; i < count; i++) {
 		const RomFields::Field *field = fields->field(i);
 		assert(field != nullptr);
@@ -1659,6 +1658,9 @@ void RP_ShellPropSheetExt_Private::initDialog(HWND hDlg)
 			w_desc_text.push_back(wstring());
 			continue;
 		}
+
+		const wstring desc_text = U82W_s(rp_sprintf(
+			desc_label_fmt, field->name.c_str()));
 
 		// Get the width of this specific entry.
 		// TODO: Use measureTextSize()?