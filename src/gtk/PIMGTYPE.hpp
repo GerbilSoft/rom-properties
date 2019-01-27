72afa5dd2e8573a2d94b4f5c9ca1f24844311d99
David Korth <gerbilsoft@gerbilsoft.com>
2019-01-27 02:20:03 -0500

    [gtk] RomDataView: Scale the RFT_LISTDATA icons to 32x32.
    Moved CreateThumbnail's scaling code to PIMGTYPE.hpp and it it for both
    CreateThumbnail and RomDataView.
    
    Fixed some potential memory leaks in Cairo error paths.
    If e.g. `surface` is allocated but has an error, it still needs to be
    destroyed instead of leaked. Same for `cr`.

--- src/gtk/PIMGTYPE.hpp
+++ src/gtk/PIMGTYPE.hpp
@@ -50,7 +50,7 @@ static inline PIMGTYPE rp_image_to_PIMGTYPE(const rp_image *img)
 	return CairoImageConv::rp_image_to_cairo_surface_t(img);
 #else /* !RP_GTK_USE_CAIRO */
 	return GdkImageConv::rp_image_to_GdkPixbuf(img);
-#endif
+#endif /* RP_GTK_USE_CAIRO */
 }
 
 // gtk_image_set_from_PIMGTYPE wrapper function.
@@ -60,7 +60,7 @@ static inline void gtk_image_set_from_PIMGTYPE(GtkImage *image, PIMGTYPE pImgTyp
 	gtk_image_set_from_surface(image, pImgType);
 #else /* !RP_GTK_USE_CAIRO */
 	gtk_image_set_from_pixbuf(image, pImgType);
-#endif
+#endif /* RP_GTK_USE_CAIRO */
 }
 
 // PIMGTYPE free() wrapper function.
@@ -70,7 +70,80 @@ static inline void PIMGTYPE_destroy(PIMGTYPE pImgType)
 	cairo_surface_destroy(pImgType);
 #else /* !RP_GTK_USE_CAIRO */
 	g_object_unref(pImgType);
-#endif
+#endif /* RP_GTK_USE_CAIRO */
+}
+
+/**
+ * PIMGTYPE size comparison function.
+ * @param pImgType PIMGTYPE
+ * @param width Expected width
+ * @param height Expected height
+ * @return True if the size matches; false if not.
+ */
+static inline bool PIMGTYPE_size_check(PIMGTYPE pImgType, int width, int height)
+{
+#ifdef RP_GTK_USE_CAIRO
+	return (cairo_image_surface_get_width(pImgType)  == width &&
+	        cairo_image_surface_get_height(pImgType) == height);
+#else /* !RP_GTK_USE_CAIRO */
+	return (gdk_pixbuf_get_width(pImgType)  == width &&
+	        gdk_pixbuf_get_height(pImgType) == height);
+#endif /* RP_GTK_USE_CAIRO */
+}
+
+/**
+ * PIMGTYPE scaling function.
+ * @param pImgType PIMGTYPE
+ * @param width New width
+ * @param height New height
+ * @param bilinear If true, use bilinear interpolation.
+ * @return Rescaled image. (If unable to rescale, returns a new reference to pImgType.)
+ */
+static inline PIMGTYPE PIMGTYPE_scale(PIMGTYPE pImgType, int width, int height, bool bilinear)
+{
+	// TODO: Maintain aspect ratio, and use nearest-neighbor
+	// when scaling up from small sizes.
+#ifdef RP_GTK_USE_CAIRO
+	int srcWidth = cairo_image_surface_get_width(pImgType);
+	int srcHeight = cairo_image_surface_get_width(pImgType);
+	assert(srcWidth > 0 && srcHeight > 0);
+	if (unlikely(srcWidth <= 0 || srcHeight <= 0)) {
+		return cairo_surface_reference(pImgType);
+	}
+
+	cairo_surface_t *const surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
+	assert(surface != nullptr);
+	assert(cairo_surface_status(surface) == CAIRO_STATUS_SUCCESS);
+	if (unlikely(!surface)) {
+		return cairo_surface_reference(pImgType);
+	} else if (cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS) {
+		cairo_surface_destroy(surface);
+		return cairo_surface_reference(pImgType);
+	}
+
+	cairo_t *const cr = cairo_create(surface);
+	assert(cr != nullptr);
+	assert(cairo_status(cr) == CAIRO_STATUS_SUCCESS);
+	if (unlikely(!cr)) {
+		cairo_surface_destroy(surface);
+		return cairo_surface_reference(pImgType);
+	} else if (cairo_status(cr) != CAIRO_STATUS_SUCCESS) {
+		cairo_destroy(cr);
+		cairo_surface_destroy(surface);
+		return cairo_surface_reference(pImgType);
+	}
+
+	cairo_scale(cr, (double)width / (double)srcWidth, (double)height / (double)srcHeight);
+	cairo_set_source_surface(cr, pImgType, 0, 0);
+	cairo_pattern_set_filter(cairo_get_source(cr),
+		(bilinear ? CAIRO_FILTER_BILINEAR : CAIRO_FILTER_NEAREST));
+	cairo_paint(cr);
+	cairo_destroy(cr);
+	return surface;
+#else /* !RP_GTK_USE_CAIRO */
+	return gdk_pixbuf_scale_simple(pImgType, width, height,
+		(bilinear ? GDK_INTERP_BILINEAR : GDK_INTERP_BILINEAR));
+#endif /* RP_GTK_USE_CAIRO */
 }
 
 #endif /* __ROMPROPERTIES_GTK_PIMGTYPE_HPP__ */
