GerbilSoft/rom-properties

commit 626a5765f668478e056219740b1afca1811f6159

[win32] RP_ShellPropSheetExt: Rename the RFT_LISTDATA `szIcon` variable to `szLstIcon`.
This prevents confusion with the class member `szIcon`.

src/win32/RP_ShellPropSheetExt.cpp
@@ -1343,18 +1343,18 @@ int RP_ShellPropSheetExt_Private::initListData(HWND hDlg, HWND hWndTab,
 			// 16 pixels.
 			// TODO: Handle this better.
 			// FIXME: This only works if the RFT_LISTDATA has icons.
-			SIZE szIcon = {32, 32};
+			SIZE szLstIcon = {32, 32};
 			bool resizeNeeded = false;
 			float factor = 1.0f;
 			if (nl_max >= 2) {
 				// Two or more newlines.
 				// Add 16px per newline over 1.
-				szIcon.cy += (16 * (nl_max - 1));
+				szLstIcon.cy += (16 * (nl_max - 1));
 				resizeNeeded = true;
 				factor = (float)szIcon.cy / 32.0f;
 			}
 
-			HIMAGELIST himl = ImageList_Create(szIcon.cx, szIcon.cy,
+			HIMAGELIST himl = ImageList_Create(szLstIcon.cx, szLstIcon.cy,
 				ILC_COLOR32, static_cast<int>(list_data->size()), 0);
 			assert(himl != nullptr);
 			if (himl) {

commit eef316e08ad93a3cc5707049be92a1b945205651

[win32] RP_ShellPropSheetExt: Another `szIcon` -> `szLstIcon`.
This broke vertical centering for Xbox 360 achievements.

This fixes a regression from commit 626a5765f668478e056219740b1afca1811f6159.
([win32] RP_ShellPropSheetExt: Rename the RFT_LISTDATA `szIcon` variable to `szLstIcon`.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -1370,7 +1370,7 @@ int RP_ShellPropSheetExt_Private::initListData(HWND hDlg, HWND hWndTab,
 				// Add 16px per newline over 1.
 				szLstIcon.cy += (16 * (nl_max - 1));
 				resizeNeeded = true;
-				factor = (float)szIcon.cy / 32.0f;
+				factor = (float)szLstIcon.cy / 32.0f;
 			}
 
 			HIMAGELIST himl = ImageList_Create(szLstIcon.cx, szLstIcon.cy,