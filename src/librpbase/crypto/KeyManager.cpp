GerbilSoft/rom-properties

commit 422f8bc9025ce31ddb1713e6bc34fc482851eaff

[librpbase] KeyManager: New function hexStringToBytes().
This function converts a hexadecimal ASCII string to bytes.
It's templated to allow use of both 8-bit and 16-bit strings.

Use hexStringToBytes() in:
- KeyManagerPrivate::processConfigLine()
- KeyStorePrivate::verifyKey()

src/librpbase/crypto/KeyManager.cpp
@@ -205,42 +205,11 @@ int KeyManagerPrivate::processConfigLine(const char *section, const char *name, 
 	// Reserve space for half of the key string.
 	// Key string is ASCII hex, so two characters make up one byte.
 	vKeys.resize(vKeys.size() + len);
-
-	// ASCII to HEX lookup table.
-	// Reference: http://codereview.stackexchange.com/questions/22757/char-hex-string-to-byte-array
-	static const uint8_t ascii_to_hex[0x100] = {
-		//0     1     2     3     4     5     6    7      8     9     A     B     C     D     E     F
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//0
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//1
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//2
-		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//3
-		0xFF, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//4
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//5
-		0xFF, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//6
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//7
-
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//8
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//9
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//A
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//B
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//C
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//D
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//E
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF //F
-	};
-
-	for (; value_len > 0; value_len -= 2, vKeys_pos++, value += 2) {
-		// Process two characters at a time.
-		// Two hexadecimal digits == one byte.
-		uint8_t chr0 = ascii_to_hex[(uint8_t)value[0]];
-		uint8_t chr1 = ascii_to_hex[(uint8_t)value[1]];
-		if (chr0 > 0x0F || chr1 > 0x0F) {
-			// Invalid character.
-			vKeys.resize(vKeys_start_pos);
-			return 1;
-		}
-
-		vKeys[vKeys_pos] = (chr0 << 4 | chr1);
+	int ret = KeyManager::hexStringToBytes(value, &vKeys[vKeys_pos], len);
+	if (ret != 0) {
+		// Invalid character(s) encountered.
+		vKeys.resize(vKeys_start_pos);
+		return 1;
 	}
 
 	// Value parsed successfully.
@@ -453,6 +422,59 @@ KeyManager::VerifyResult KeyManager::getAndVerify(const char *keyName, KeyData_t
 	// Test data verified.
 	return VERIFY_OK;
 }
+
+/**
+ * Convert string data from hexadecimal to bytes.
+ * @param str	[in] String data. (Must be len*2 characters.)
+ * @param buf	[out] Output buffer.
+ * @param len	[in] Size of buf, in bytes.
+ * @return 0 on success; non-zero on error.
+ */
+template<typename Char>
+int KeyManager::hexStringToBytes(const Char *str, uint8_t *buf, unsigned int len)
+{
+	// ASCII to HEX lookup table.
+	// Reference: http://codereview.stackexchange.com/questions/22757/char-hex-string-to-byte-array
+	static const uint8_t ascii_to_hex[0x80] = {
+		//0     1     2     3     4     5     6    7      8     9     A     B     C     D     E     F
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//0
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//1
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//2
+		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//3
+		0xFF, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//4
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//5
+		0xFF, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//6
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//7
+	};
+
+	for (; len > 0; len--, str += 2, buf++) {
+		// Process two characters at a time.
+		// Two hexadecimal digits == one byte.
+		if ((unsigned int)str[0] > 0x80 || (unsigned int)str[1] > 0x80) {
+			// Invalid character.
+			return -EINVAL;
+		}
+
+		uint8_t chr0 = ascii_to_hex[(uint8_t)str[0]];
+		uint8_t chr1 = ascii_to_hex[(uint8_t)str[1]];
+		if (chr0 > 0x0F || chr1 > 0x0F) {
+			// Invalid character.
+			return -EINVAL;
+		}
+
+		*buf = (chr0 << 4 | chr1);
+	}
+
+	// String processed.
+	return 0;
+}
+
+// Explicit instantiation of hexStringToBytes().
+template int KeyManager::hexStringToBytes<char>(const char *str, uint8_t *buf, unsigned int len);
+#ifdef RP_UTF16
+template int KeyManager::hexStringToBytes<rp_char>(const rp_char *str, uint8_t *buf, unsigned int len);
+#endif
+
 #endif /* ENABLE_DECRYPTION */
 
 }

src/kde/config/KeyStore.cpp
@@ -451,55 +451,20 @@ void KeyStorePrivate::verifyKey(int sectIdx, int keyIdx)
 		return;
 	}
 
-	// ASCII to HEX lookup table.
-	// Reference: http://codereview.stackexchange.com/questions/22757/char-hex-string-to-byte-array
-	// FIXME: Add a function to KeyManager to convert a string to bytes.
-	static const uint8_t ascii_to_hex[0x100] = {
-		//0     1     2     3     4     5     6    7      8     9     A     B     C     D     E     F
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//0
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//1
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//2
-		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//3
-		0xFF, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//4
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//5
-		0xFF, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//6
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//7
-
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//8
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//9
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//A
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//B
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//C
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//D
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,//E
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF //F
-	};
-
 	// Convert the key to bytes.
-	// TODO: Use uvector?
-	QByteArray ba;
-	ba.reserve(16);
-	const QChar *value = key.value.constData();
-	uint8_t *p = (uint8_t*)ba.data();
-
-	for (int len = key.value.size(); len > 0; len -= 2, value += 2, p++) {
-		// Process two characters at a time.
-		// Two hexadecimal digits == one byte.
-		uint8_t chr0 = ascii_to_hex[(uint8_t)value[0].unicode()];
-		uint8_t chr1 = ascii_to_hex[(uint8_t)value[1].unicode()];
-		if (chr0 > 0x0F || chr1 > 0x0F) {
-			// Invalid character.
-			key.status = KeyStore::Key::Status_NotAKey;
-			return;
-		}
-
-		ba.append(chr0 << 4 | chr1);
+	// TODO: Support keys that aren't 128-bit.
+	uint8_t keyBytes[16];
+	int ret = KeyManager::hexStringToBytes(Q2RP(key.value), keyBytes, sizeof(keyBytes));
+	if (ret != 0) {
+		// Invalid character(s) encountered.
+		key.status = KeyStore::Key::Status_NotAKey;
+		return;
 	}
 
 	// Attempt to decrypt the verification data using the key.
 	uint8_t testData[16];
 	memcpy(testData, verifyData, sizeof(testData));
-	int ret = cipher->setKey(reinterpret_cast<const uint8_t*>(ba.constData()), ba.size());
+	ret = cipher->setKey(keyBytes, sizeof(keyBytes));
 	if (ret != 0) {
 		// Error setting the key.
 		key.status = KeyStore::Key::Status_Unknown;
@@ -510,9 +475,6 @@ void KeyStorePrivate::verifyKey(int sectIdx, int keyIdx)
 		// Error decrypting the data.
 		key.status = KeyStore::Key::Status_Unknown;
 		return;
-	}
-	if (sectIdx == 0 && keyIdx == 0) {
-		printf("\n");
 	}
 
 	// Check if the decrypted data is correct.

src/librpbase/crypto/KeyManager.hpp
@@ -116,6 +116,16 @@ class KeyManager : public ConfReader
 		// Verification test string.
 		// NOTE: This string is NOT NULL-terminated!
 		static const char verifyTestString[16];
+
+		/**
+		 * Convert string data from hexadecimal to bytes.
+		 * @param str	[in] String data. (Must be len*2 characters.)
+		 * @param buf	[out] Output buffer.
+		 * @param len	[in] Size of buf, in bytes.
+		 * @return 0 on success; non-zero on error.
+		 */
+		template<typename Char>
+		static int hexStringToBytes(const Char *str, uint8_t *buf, unsigned int len);
 #endif /* ENABLE_DECRYPTION */
 };
 

commit aee7e25e65afe964899dc12df82c257eadf78723

[librpbase] KeyManager::hexStringToBytes(): Fix an off-by-one error.
Need to check for >= 0x80, not > 0x80. Otherwise, if the string has a
0x80 byte, it will end up reading outside the bounds of the array.

Found using clang's static analyzer. (clang-7.0.0)

This fixes a regression from commit 422f8bc9025ce31ddb1713e6bc34fc482851eaff.
([librpbase] KeyManager: New function hexStringToBytes().)

[Bug introduced in v1.1, though it probably wasn't encountered by anyone.]

src/librpbase/crypto/KeyManager.cpp
@@ -468,8 +468,8 @@ int KeyManager::hexStringToBytes(const Char *str, uint8_t *buf, unsigned int len
 	for (; len > 0; len--, str += 2, buf++) {
 		// Process two characters at a time.
 		// Two hexadecimal digits == one byte.
-		if (static_cast<unsigned int>(str[0]) > 0x80 ||
-			static_cast<unsigned int>(str[1]) > 0x80)
+		if (static_cast<unsigned int>(str[0]) >= 0x80 ||
+			static_cast<unsigned int>(str[1]) >= 0x80)
 		{
 			// Invalid character.
 			return -EINVAL;