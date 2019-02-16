GerbilSoft/rom-properties

commit 44a94c29ad5a54866ebb381d5db411a040d459cd

[libromdata] XboxDisc, RomDataFactory: Added support for extracted XDVDFS.
Some older dumps have the XDVDFS partition without the ISO-9660/UDF
video partition. We won't be able to determine the XGD type or show
the ISO-9660 PVD, but we can at least identify the XDVDFS and process
the Xbox-specific stuff.

src/libromdata/Console/XboxDisc.cpp
@@ -65,9 +65,10 @@ class XboxDiscPrivate : public LibRpBase::RomDataPrivate
 		enum DiscType {
 			DISC_UNKNOWN = -1,	// Unknown disc type.
 
-			DISC_TYPE_XGD1	= 0,	// XGD1 (Original Xbox)
-			DISC_TYPE_XGD2	= 1,	// XGD2 (Xbox 360)
-			DISC_TYPE_XGD3	= 2,	// XGD3 (Xbox 360)
+			DISC_TYPE_EXTRACTED	= 0,	// Extracted XDVDFS
+			DISC_TYPE_XGD1		= 1,	// XGD1 (Original Xbox)
+			DISC_TYPE_XGD2		= 2,	// XGD2 (Xbox 360)
+			DISC_TYPE_XGD3		= 3,	// XGD3 (Xbox 360)
 		};
 		int discType;
 		uint8_t wave;
@@ -146,10 +147,9 @@ XboxDisc::XboxDisc(IRpFile *file)
 			d->xdvdfs_addr = XDVDFS_LBA_OFFSET_XGD3 * XDVDFS_BLOCK_SIZE;
 			break;
 		default:
-			// Not supported.
-			d->file->unref();
-			d->file = nullptr;
-			return;
+			// This might be an extracted XDVDFS.
+			d->xdvdfs_addr = 0;
+			break;
 	}
 
 	// Read the XDVDFS header.
@@ -171,6 +171,12 @@ XboxDisc::XboxDisc(IRpFile *file)
 		d->file->unref();
 		d->file = nullptr;
 		return;
+	}
+
+	// Magic strings are correct.
+	if (d->discType <= XboxDiscPrivate::DISC_UNKNOWN) {
+		// This is an extracted XDVDFS.
+		d->discType = XboxDiscPrivate::DISC_TYPE_EXTRACTED;
 	}
 
 #if SYS_BYTEORDER == SYS_BIG_ENDIAN
@@ -416,6 +422,10 @@ int XboxDisc::loadFieldData(void)
 	const char *const s_disc_type = C_("XboxDisc", "Disc Type");
 	// NOTE: Not translating "Xbox Game Disc".
 	switch (d->discType) {
+		case XboxDiscPrivate::DISC_TYPE_EXTRACTED:
+			d->fields->addField_string(s_disc_type,
+				C_("XboxDisc", "Extracted XDVDFS"));
+			break;
 		case XboxDiscPrivate::DISC_TYPE_XGD1:
 			d->fields->addField_string(s_disc_type, "Xbox Game Disc 1");
 			break;
@@ -447,13 +457,15 @@ int XboxDisc::loadFieldData(void)
 	// TODO: Get the XBE and/or XEX.
 
 	// ISO object for ISO-9660 PVD
-	ISO *const isoData = new ISO(d->file);
-	if (isoData->isOpen()) {
-		// Add the fields.
-		d->fields->addFields_romFields(isoData->fields(),
-			RomFields::TabOffset_AddTabs);
+	if (d->discType >= XboxDiscPrivate::DISC_TYPE_XGD1) {
+		ISO *const isoData = new ISO(d->file);
+		if (isoData->isOpen()) {
+			// Add the fields.
+			d->fields->addFields_romFields(isoData->fields(),
+				RomFields::TabOffset_AddTabs);
+		}
+		isoData->unref();
 	}
-	isoData->unref();
 
 	// Finished reading the field data.
 	return static_cast<int>(d->fields->count());

src/libromdata/RomDataFactory.cpp
@@ -68,6 +68,7 @@ using std::vector;
 
 // Special handling for Xbox discs.
 #include "iso_structs.h"
+#include "Console/xdvdfs_structs.h"
 #include "Console/XboxDisc.hpp"
 
 // RomData subclasses: Handhelds
@@ -458,8 +459,25 @@ RomData *RomDataFactoryPrivate::checkISO(IRpFile *file)
 	// Try various game disc file systems.
 
 	// Xbox / Xbox 360
-	// TODO: Also check for trimmed XDVDFS. (offset == 0)
-	if (XboxDisc::isRomSupported_static(&pvd) >= 0) {
+	bool mayBeXbox = (XboxDisc::isRomSupported_static(&pvd) >= 0);
+	if (!mayBeXbox) {
+		// This might be an extracted XDVDFS.
+		// Check for the magic number at the base offset.
+		XDVDFS_Header xdvdfsHeader;
+		size = file->seekAndRead(XDVDFS_HEADER_LBA_OFFSET * XDVDFS_BLOCK_SIZE,
+			&xdvdfsHeader, sizeof(xdvdfsHeader));
+		if (size == sizeof(xdvdfsHeader)) {
+			// Check the magic numbers.
+			if (!memcmp(xdvdfsHeader.magic, XDVDFS_MAGIC, sizeof(xdvdfsHeader.magic)) &&
+			    !memcmp(xdvdfsHeader.magic_footer, XDVDFS_MAGIC, sizeof(xdvdfsHeader.magic_footer)))
+			{
+				// It's a match!
+				mayBeXbox = true;
+			}
+		}
+	}
+
+	if (mayBeXbox) {
 		RomData *const romData = new XboxDisc(file);
 		if (romData->isValid()) {
 			// Got an Xbox disc.

commit e5a1566bb013656ce269a44f51bdefe1c8798aab

[libromdata] XboxDisc::isRomSupported(): Don't subtract 1 from the XGD number anymore.
DISC_TYPE_EXTRACTED is 0, and everything else has been pushed up by 1.

This broke detection of both XGD1 and XGD2 discs:
- XGD1 was assumed to be extracted.
- XGD2 was given the XGD1 XDVDFS address.

This fixes a regression from commit 44a94c29ad5a54866ebb381d5db411a040d459cd.
([libromdata] XboxDisc, RomDataFactory: Added support for extracted XDVDFS.)

src/libromdata/Console/XboxDisc.cpp
@@ -290,7 +290,7 @@ int XboxDisc::isRomSupported_static(
 			if (pWave) {
 				*pWave = pXgd->wave;
 			}
-			return pXgd->xgd - 1;
+			return pXgd->xgd;
 		}
 	}
 