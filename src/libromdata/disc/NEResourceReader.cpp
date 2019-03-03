GerbilSoft/rom-properties

commit 7ee118465034349de1846efc80ec5a1df815048d

[libromdata] IDiscReader: Moved m_file / m_discReader to the base class.
Every subclass has one or the other, and NCCHReader might have
either one, so move it to the common base class so we can
de-virtualize some functions.

IDiscReader's IRpFile* constructor now takes a reference to the
file and unreferences it in the destructor.

IDiscReader's IDiscReader* constructor doesn't take any reference,
since IDiscReader doesn't support references right now.
- TODO: Add references?

IDiscReader::isDevice() is now a standard function.

GcnPartitionPrivate: Moved offsetShift to reduce padding.

Code size reduction for `rpcli`: (64-bit Gentoo Linux, gcc-8.2.0-r6, release build)

   text    data     bss     dec     hex filename
1049769   98377   17456 1165602  11c922 bin/rpcli [before]
1048285   98305   17456 1164046  11c30e bin/rpcli [after]
  -1484     -72       0   -1556    -614 Difference

src/libromdata/disc/NEResourceReader.cpp
@@ -51,7 +51,7 @@ namespace LibRomData {
 class NEResourceReaderPrivate
 {
 	public:
-		NEResourceReaderPrivate(NEResourceReader *q, IRpFile *file,
+		NEResourceReaderPrivate(NEResourceReader *q,
 			uint32_t rsrc_tbl_addr, uint32_t rsrc_tbl_size);
 
 	private:
@@ -110,27 +110,27 @@ class NEResourceReaderPrivate
 /** NEResourceReaderPrivate **/
 
 NEResourceReaderPrivate::NEResourceReaderPrivate(
-		NEResourceReader *q, IRpFile *file,
+		NEResourceReader *q,
 		uint32_t rsrc_tbl_addr,
 		uint32_t rsrc_tbl_size)
 	: q_ptr(q)
-	, file(file)
 	, rsrc_tbl_addr(rsrc_tbl_addr)
 	, rsrc_tbl_size(rsrc_tbl_size)
 {
-	if (!file) {
-		this->file = nullptr;
+	if (!q->m_file) {
 		q->m_lastError = -EBADF;
 		return;
 	} else if (rsrc_tbl_addr == 0) {
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = -EIO;
 		return;
 	} else if (rsrc_tbl_size < 6 || rsrc_tbl_size >= 65536) {
 		// 64 KB is the segment size, so this shouldn't be possible.
 		// Also, it should be at least 6 bytes.
 		// (TODO: Larger minimum size?)
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = -EIO;
 		return;
 	}
@@ -144,7 +144,8 @@ NEResourceReaderPrivate::NEResourceReaderPrivate(
 	const int64_t fileSize_i64 = file->size();
 	if (fileSize_i64 > fileSize_MAX) {
 		// A Win16 executable larger than 16 MB doesn't make any sense.
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = -EIO;
 		return;
 	}
@@ -156,7 +157,8 @@ NEResourceReaderPrivate::NEResourceReaderPrivate(
 	{
 		// Starting address is past the end of the file,
 		// or resource ends past the end of the file.
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = -EIO;
 		return;
 	}
@@ -165,7 +167,8 @@ NEResourceReaderPrivate::NEResourceReaderPrivate(
 	int ret = loadResTbl();
 	if (ret != 0) {
 		// No resources, or an error occurred.
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 	}
 }
 
@@ -195,10 +198,10 @@ int NEResourceReaderPrivate::loadResTbl(void)
 
 	// Load the resource table.
 	unique_ptr<uint8_t[]> rsrcTblData(new uint8_t[rsrc_tbl_size]);
-	size_t size = file->seekAndRead(rsrc_tbl_addr, rsrcTblData.get(), rsrc_tbl_size);
+	size_t size = q->m_file->seekAndRead(rsrc_tbl_addr, rsrcTblData.get(), rsrc_tbl_size);
 	if (size != rsrc_tbl_size) {
 		// Seek and/or read error.
-		q->m_lastError = file->lastError();
+		q->m_lastError = q->m_file->lastError();
 		return q->m_lastError;
 	}
 
@@ -399,7 +402,6 @@ int NEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
 		// TODO: Better error code?
 		return -EIO;
 	}
-
 	// DWORD alignment.
 	IResourceReader::alignFileDWORD(file);
 
@@ -498,7 +500,8 @@ int NEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
  * @param rsrc_tbl_size Resource table size.
  */
 NEResourceReader::NEResourceReader(IRpFile *file, uint32_t rsrc_tbl_addr, uint32_t rsrc_tbl_size)
-	: d_ptr(new NEResourceReaderPrivate(this, file, rsrc_tbl_addr, rsrc_tbl_size))
+	: super(file)
+	, d_ptr(new NEResourceReaderPrivate(this, rsrc_tbl_addr, rsrc_tbl_size))
 { }
 
 NEResourceReader::~NEResourceReader()
@@ -515,8 +518,7 @@ NEResourceReader::~NEResourceReader()
  */
 bool NEResourceReader::isOpen(void) const
 {
-	RP_D(const NEResourceReader);
-	return (d->file && d->file->isOpen());
+	return (m_file && m_file->isOpen());
 }
 
 /**
@@ -527,10 +529,9 @@ bool NEResourceReader::isOpen(void) const
  */
 size_t NEResourceReader::read(void *ptr, size_t size)
 {
-	RP_D(NEResourceReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
-	if (!d->file || !d->file->isOpen()) {
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
+	if (!m_file || !m_file->isOpen()) {
 		m_lastError = EBADF;
 		return 0;
 	}
@@ -538,7 +539,7 @@ size_t NEResourceReader::read(void *ptr, size_t size)
 	// There isn't a separate resource "section" in
 	// NE executables, so forward all read requests
 	// to the underlying file.
-	return d->file->read(ptr, size);
+	return m_file->read(ptr, size);
 }
 
 /**
@@ -548,10 +549,9 @@ size_t NEResourceReader::read(void *ptr, size_t size)
  */
 int NEResourceReader::seek(int64_t pos)
 {
-	RP_D(NEResourceReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
-	if (!d->file || !d->file->isOpen()) {
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
+	if (!m_file || !m_file->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -559,7 +559,7 @@ int NEResourceReader::seek(int64_t pos)
 	// There isn't a separate resource "section" in
 	// NE executables, so forward all read requests
 	// to the underlying file.
-	return d->file->seek(pos);
+	return m_file->seek(pos);
 }
 
 /**
@@ -568,10 +568,9 @@ int NEResourceReader::seek(int64_t pos)
  */
 int64_t NEResourceReader::tell(void)
 {
-	RP_D(NEResourceReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
-	if (!d->file || !d->file->isOpen()) {
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
+	if (!m_file || !m_file->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -579,7 +578,7 @@ int64_t NEResourceReader::tell(void)
 	// There isn't a separate resource "section" in
 	// NE executables, so forward all read requests
 	// to the underlying file.
-	return d->file->tell();
+	return m_file->tell();
 }
 
 /**
@@ -591,10 +590,9 @@ int64_t NEResourceReader::tell(void)
 int64_t NEResourceReader::size(void)
 {
 	// TODO: Errors?
-	RP_D(NEResourceReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
-	if (!d->file || !d->file->isOpen()) {
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
+	if (!m_file || !m_file->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -602,19 +600,7 @@ int64_t NEResourceReader::size(void)
 	// There isn't a separate resource "section" in
 	// NE executables, so forward all read requests
 	// to the underlying file.
-	return static_cast<int64_t>(d->file->size());
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool NEResourceReader::isDevice(void) const
-{
-	RP_D(const NEResourceReader);
-	return (d->file && d->file->isDevice());
+	return static_cast<int64_t>(m_file->size());
 }
 
 /** IPartition **/
@@ -627,12 +613,11 @@ bool NEResourceReader::isDevice(void) const
 int64_t NEResourceReader::partition_size(void) const
 {
 	// TODO: Errors?
-	RP_D(const NEResourceReader);
 
 	// There isn't a separate resource "section" in
 	// NE executables, so forward all read requests
 	// to the underlying file.
-	return static_cast<int64_t>(d->file->size());
+	return static_cast<int64_t>(m_file->size());
 }
 
 /**
@@ -644,12 +629,11 @@ int64_t NEResourceReader::partition_size(void) const
 int64_t NEResourceReader::partition_size_used(void) const
 {
 	// TODO: Errors?
-	RP_D(const NEResourceReader);
 
 	// There isn't a separate resource "section" in
 	// NE executables, so forward all read requests
 	// to the underlying file.
-	return static_cast<int64_t>(d->file->size());
+	return static_cast<int64_t>(m_file->size());
 }
 
 /** Resource access functions. **/

src/libromdata/disc/PEResourceReader.cpp
@@ -51,7 +51,7 @@ namespace LibRomData {
 class PEResourceReaderPrivate
 {
 	public:
-		PEResourceReaderPrivate(PEResourceReader *q, IRpFile *file,
+		PEResourceReaderPrivate(PEResourceReader *q,
 			uint32_t rsrc_addr, uint32_t rsrc_size, uint32_t rsrc_va);
 
 	private:
@@ -147,32 +147,32 @@ class PEResourceReaderPrivate
 /** PEResourceReaderPrivate **/
 
 PEResourceReaderPrivate::PEResourceReaderPrivate(
-		PEResourceReader *q, IRpFile *file,
+		PEResourceReader *q,
 		uint32_t rsrc_addr, uint32_t rsrc_size,
 		uint32_t rsrc_va)
 	: q_ptr(q)
-	, file(file)
 	, rsrc_addr(rsrc_addr)
 	, rsrc_size(rsrc_size)
 	, rsrc_va(rsrc_va)
 	, pos(0)
 {
-	if (!file) {
-		this->file = nullptr;
+	if (!q->m_file) {
 		q->m_lastError = -EBADF;
 		return;
 	} else if (rsrc_addr == 0 || rsrc_size == 0) {
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = -EIO;
 		return;
 	}
 
 	// Validate the starting address and size.
 	static const uint32_t fileSize_MAX = 2U*1024*1024*1024;
-	const int64_t fileSize_i64 = file->size();
+	const int64_t fileSize_i64 = q->m_file->size();
 	if (fileSize_i64 > fileSize_MAX) {
 		// A Win32/Win64 executable larger than 2 GB doesn't make any sense.
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = -EIO;
 		return;
 	}
@@ -184,7 +184,8 @@ PEResourceReaderPrivate::PEResourceReaderPrivate(
 	{
 		// Starting address is past the end of the file,
 		// or resource ends past the end of the file.
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = -EIO;
 		return;
 	}
@@ -193,7 +194,8 @@ PEResourceReaderPrivate::PEResourceReaderPrivate(
 	int ret = loadResDir(0, res_types);
 	if (ret <= 0) {
 		// No resources, or an error occurred.
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 	}
 }
 
@@ -212,10 +214,10 @@ int PEResourceReaderPrivate::loadResDir(uint32_t addr, rsrc_dir_t &dir)
 	RP_Q(PEResourceReader);
 
 	IMAGE_RESOURCE_DIRECTORY root;
-	size_t size = file->seekAndRead(rsrc_addr + addr, &root, sizeof(root));
+	size_t size = q->m_file->seekAndRead(rsrc_addr + addr, &root, sizeof(root));
 	if (size != sizeof(root)) {
 		// Seek and/or read error.
-		q->m_lastError = file->lastError();
+		q->m_lastError = q->m_file->lastError();
 		return q->m_lastError;
 	}
 
@@ -228,10 +230,10 @@ int PEResourceReaderPrivate::loadResDir(uint32_t addr, rsrc_dir_t &dir)
 	}
 	uint32_t szToRead = static_cast<uint32_t>(entryCount * sizeof(IMAGE_RESOURCE_DIRECTORY_ENTRY));
 	unique_ptr<IMAGE_RESOURCE_DIRECTORY_ENTRY[]> irdEntries(new IMAGE_RESOURCE_DIRECTORY_ENTRY[entryCount]);
-	size = file->read(irdEntries.get(), szToRead);
+	size = q->m_file->read(irdEntries.get(), szToRead);
 	if (size != szToRead) {
 		// Read error.
-		q->m_lastError = file->lastError();
+		q->m_lastError = q->m_file->lastError();
 		return q->m_lastError;
 	}
 
@@ -474,7 +476,6 @@ int PEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
 		// TODO: Better error code?
 		return -EIO;
 	}
-
 	// DWORD alignment.
 	IResourceReader::alignFileDWORD(file);
 
@@ -580,7 +581,8 @@ int PEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
  * @param rsrc_va .rsrc virtual address.
  */
 PEResourceReader::PEResourceReader(IRpFile *file, uint32_t rsrc_addr, uint32_t rsrc_size, uint32_t rsrc_va)
-	: d_ptr(new PEResourceReaderPrivate(this, file, rsrc_addr, rsrc_size, rsrc_va))
+	: super(file)
+	, d_ptr(new PEResourceReaderPrivate(this, rsrc_addr, rsrc_size, rsrc_va))
 { }
 
 PEResourceReader::~PEResourceReader()
@@ -597,8 +599,7 @@ PEResourceReader::~PEResourceReader()
  */
 bool PEResourceReader::isOpen(void) const
 {
-	RP_D(const PEResourceReader);
-	return (d->file && d->file->isOpen());
+	return (m_file && m_file->isOpen());
 }
 
 /**
@@ -610,9 +611,9 @@ bool PEResourceReader::isOpen(void) const
 size_t PEResourceReader::read(void *ptr, size_t size)
 {
 	RP_D(PEResourceReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
-	if (!d->file || !d->file->isOpen()) {
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
+	if (!m_file || !m_file->isOpen()) {
 		m_lastError = EBADF;
 		return 0;
 	}
@@ -630,10 +631,10 @@ size_t PEResourceReader::read(void *ptr, size_t size)
 	}
 
 	// Read the data.
-	size_t read = d->file->seekAndRead(static_cast<int64_t>(d->rsrc_addr) + static_cast<int64_t>(d->pos), ptr, size);
+	size_t read = m_file->seekAndRead(static_cast<int64_t>(d->rsrc_addr) + static_cast<int64_t>(d->pos), ptr, size);
 	if (read != size) {
 		// Seek and/or read error.
-		m_lastError = d->file->lastError();
+		m_lastError = m_file->lastError();
 	}
 	d->pos += read;
 	return read;
@@ -647,9 +648,9 @@ size_t PEResourceReader::read(void *ptr, size_t size)
 int PEResourceReader::seek(int64_t pos)
 {
 	RP_D(PEResourceReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
-	if (!d->file || !d->file->isOpen()) {
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
+	if (!m_file || !m_file->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -674,9 +675,9 @@ int PEResourceReader::seek(int64_t pos)
 int64_t PEResourceReader::tell(void)
 {
 	RP_D(const PEResourceReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
-	if (!d->file || !d->file->isOpen()) {
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
+	if (!m_file || !m_file->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -695,18 +696,6 @@ int64_t PEResourceReader::size(void)
 	// TODO: Errors?
 	RP_D(const PEResourceReader);
 	return static_cast<int64_t>(d->rsrc_size);
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool PEResourceReader::isDevice(void) const
-{
-	RP_D(const PEResourceReader);
-	return (d->file && d->file->isDevice());
 }
 
 /** IPartition **/
@@ -794,10 +783,10 @@ IRpFile *PEResourceReader::open(uint16_t type, int id, int lang)
 
 	// Get the IMAGE_RESOURCE_DATA_ENTRY.
 	IMAGE_RESOURCE_DATA_ENTRY irdata;
-	size_t size = d->file->seekAndRead(d->rsrc_addr + dirEntry->addr, &irdata, sizeof(irdata));
+	size_t size = m_file->seekAndRead(d->rsrc_addr + dirEntry->addr, &irdata, sizeof(irdata));
 	if (size != sizeof(irdata)) {
 		// Seek and/or read error.
-		m_lastError = d->file->lastError();
+		m_lastError = m_file->lastError();
 		return nullptr;
 	}
 

src/libromdata/disc/CIAReader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * CIAReader.cpp: Nintendo 3DS CIA reader.                                 *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -43,7 +43,7 @@ namespace LibRomData {
 class CIAReaderPrivate
 {
 	public:
-		CIAReaderPrivate(CIAReader *q, LibRpBase::IRpFile *file,
+		CIAReaderPrivate(CIAReader *q,
 			int64_t content_offset, uint32_t content_length,
 			const N3DS_Ticket_t *ticket,
 			uint16_t tmd_content_index);
@@ -55,7 +55,6 @@ class CIAReaderPrivate
 		CIAReader *const q_ptr;
 
 	public:
-		LibRpBase::IRpFile *file;	// 3DS CIA file.
 		CBCReader *cbcReader;		// CBC reader.
 
 #ifdef ENABLE_DECRYPTION
@@ -68,11 +67,10 @@ class CIAReaderPrivate
 
 /** CIAReaderPrivate **/
 
-CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
+CIAReaderPrivate::CIAReaderPrivate(CIAReader *q,
 	int64_t content_offset, uint32_t content_length,
 	const N3DS_Ticket_t *ticket, uint16_t tmd_content_index)
 	: q_ptr(q)
-	, file(file)
 	, cbcReader(nullptr)
 #ifdef ENABLE_DECRYPTION
 	, titleKeyEncIdx(0)
@@ -83,11 +81,17 @@ CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
 	RP_UNUSED(tmd_content_index);
 #endif /* ENABLE_DECRYPTION */
 
+	assert(q->m_file != nullptr);
+	if (!q->m_file) {
+		// No file...
+		return;
+	}
+
 	assert(ticket != nullptr);
 	if (!ticket) {
 		// No ticket. Assuming no encryption.
 		// Create a passthru CBCReader anyway.
-		cbcReader = new CBCReader(file, content_offset, content_length, nullptr, nullptr);
+		cbcReader = new CBCReader(q->m_file, content_offset, content_length, nullptr, nullptr);
 		return;
 	}
 
@@ -175,17 +179,19 @@ CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
 		memset(&cia_iv.u8[2], 0, sizeof(cia_iv.u8)-2);
 
 		// Create a CBC reader to decrypt the CIA.
-		cbcReader = new CBCReader(file, content_offset, content_length, title_key, cia_iv.u8);
+		cbcReader = new CBCReader(q->m_file, content_offset, content_length, title_key, cia_iv.u8);
 	} else {
 		// Unable to get the CIA encryption keys.
 		// TODO: Set an error.
 		//verifyResult = res;
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 	}
 #else /* !ENABLE_DECRYPTION */
 	// Cannot decrypt the CIA.
 	// TODO: Set an error.
-	this->file = nullptr;
+	q->m_file->unref();
+	q->m_file = nullptr;
 #endif /* ENABLE_DECRYPTION */
 }
 
@@ -212,7 +218,8 @@ CIAReader::CIAReader(IRpFile *file,
 		int64_t content_offset, uint32_t content_length,
 		const N3DS_Ticket_t *ticket,
 		uint16_t tmd_content_index)
-	: d_ptr(new CIAReaderPrivate(this, file, content_offset, content_length, ticket, tmd_content_index))
+	: super(file)
+	, d_ptr(new CIAReaderPrivate(this, content_offset, content_length, ticket, tmd_content_index))
 { }
 
 CIAReader::~CIAReader()
@@ -229,8 +236,7 @@ CIAReader::~CIAReader()
  */
 bool CIAReader::isOpen(void) const
 {
-	RP_D(const CIAReader);
-	return (d->file && d->file->isOpen());
+	return (m_file && m_file->isOpen());
 }
 
 /**
@@ -243,13 +249,13 @@ size_t CIAReader::read(void *ptr, size_t size)
 {
 	RP_D(CIAReader);
 	assert(ptr != nullptr);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
 	if (!ptr) {
 		m_lastError = EINVAL;
 		return 0;
-	} else if (!d->file || !d->file->isOpen() || !d->cbcReader) {
+	} else if (!m_file || !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return 0;
 	} else if (size == 0) {
@@ -270,10 +276,10 @@ size_t CIAReader::read(void *ptr, size_t size)
 int CIAReader::seek(int64_t pos)
 {
 	RP_D(CIAReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -292,9 +298,9 @@ int CIAReader::seek(int64_t pos)
 int64_t CIAReader::tell(void)
 {
 	RP_D(const CIAReader);
-	assert(d->file != nullptr);
+	assert(m_file != nullptr);
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -313,10 +319,10 @@ int64_t CIAReader::tell(void)
 int64_t CIAReader::size(void)
 {
 	RP_D(const CIAReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -324,18 +330,6 @@ int64_t CIAReader::size(void)
 	int64_t ret = d->cbcReader->size();
 	m_lastError = d->cbcReader->lastError();
 	return ret;
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool CIAReader::isDevice(void) const
-{
-	RP_D(const CIAReader);
-	return (d->file && d->file->isDevice());
 }
 
 /** IPartition **/

src/libromdata/disc/CIAReader.hpp
@@ -102,15 +102,6 @@ class CIAReader : public LibRpBase::IPartition
 		int64_t size(void) final;
 
 	public:
-		/** Device file functions **/
-
-		/**
-		 * Is the underlying file a device file?
-		 * @return True if the underlying file is a device file; false if not.
-		 */
-		bool isDevice(void) const final;
-
-	public:
 		/** IPartition **/
 
 		/**

src/libromdata/disc/Cdrom2352Reader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file);
+		Cdrom2352ReaderPrivate(Cdrom2352Reader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -68,22 +68,22 @@ class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 const uint8_t Cdrom2352ReaderPrivate::CDROM_2352_MAGIC[12] =
 	{0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00};
 
-Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file)
-	: super(q, file)
+Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q)
+	: super(q)
 	, blockCount(0)
 {
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Check the disc size.
 	// Should be a multiple of 2352.
-	int64_t fileSize = file->size();
+	int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0 || fileSize % 2352 != 0) {
 		// Invalid disc size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -101,7 +101,7 @@ Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file
 /** Cdrom2352Reader **/
 
 Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
-	: super(new Cdrom2352ReaderPrivate(this, file))
+	: super(new Cdrom2352ReaderPrivate(this), file)
 { }
 
 /**

src/libromdata/disc/Cdrom2352Reader.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * Cdrom2352Reader.hpp: CD-ROM reader for 2352-byte sector images.         *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *

src/libromdata/disc/CisoGcnReader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file);
+		CisoGcnReaderPrivate(CisoGcnReader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -72,8 +72,8 @@ class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 // CISO magic.
 const char CisoGcnReaderPrivate::CISO_MAGIC[4] = {'C','I','S','O'};
 
-CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
-	: super(q, file)
+CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
+	: super(q)
 	, maxLogicalBlockUsed(-1)
 {
 	// Clear the CISO header struct.
@@ -81,7 +81,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Clear the CISO block map initially.
 	blockMap.fill(0xFFFF);
 
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
@@ -89,12 +89,12 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Read the CISO header.
 	static_assert(sizeof(CISOHeader) == CISO_HEADER_SIZE,
 		"CISOHeader is the wrong size. (Should be 32,768 bytes.)");
-	this->file->rewind();
-	size_t sz = this->file->read(&cisoHeader, sizeof(cisoHeader));
+	q->m_file->rewind();
+	size_t sz = q->m_file->read(&cisoHeader, sizeof(cisoHeader));
 	if (sz != sizeof(cisoHeader)) {
 		// Error reading the CISO header.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -102,8 +102,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Verify the CISO header.
 	if (memcmp(cisoHeader.magic, CISO_MAGIC, sizeof(CISO_MAGIC)) != 0) {
 		// Invalid magic.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -117,8 +117,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 		// If the block size is 0x18, then this is
 		// actually a PSP CISO, and this field is
 		// the CISO header size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -138,8 +138,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 				break;
 			default:
 				// Invalid entry.
-				this->file->unref();
-				this->file = nullptr;
+				q->m_file->unref();
+				q->m_file = nullptr;
 				q->m_lastError = EIO;
 				return;
 		}
@@ -155,7 +155,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 /** CisoGcnReader **/
 
 CisoGcnReader::CisoGcnReader(IRpFile *file)
-	: super(new CisoGcnReaderPrivate(this, file))
+	: super(new CisoGcnReaderPrivate(this), file)
 { }
 
 /**

commit b72a391fac4be93875d634dde9953378381dc5e0

[libromdata] NEResourceReader, PEResourceReader: Removed `file` from the private class.
NEResourceReader: Missed updating one use of `file` to `q->m_file`.

PVS-Studio warning V614 (High): Uninitialized pointer 'file' used.

This fixes a regression from commit 7ee118465034349de1846efc80ec5a1df815048d.
([libromdata] IDiscReader: Moved m_file / m_discReader to the base class.)

src/libromdata/disc/NEResourceReader.cpp
@@ -60,9 +60,6 @@ class NEResourceReaderPrivate
 		NEResourceReader *const q_ptr;
 
 	public:
-		// EXE file.
-		IRpFile *file;
-
 		// Resource segment table.
 		uint32_t rsrc_tbl_addr;
 		uint32_t rsrc_tbl_size;
@@ -141,7 +138,7 @@ NEResourceReaderPrivate::NEResourceReaderPrivate(
 	static const uint32_t fileSize_MAX = 16U*1024*1024;
 
 	// Validate the starting address and size.
-	const int64_t fileSize_i64 = file->size();
+	const int64_t fileSize_i64 = q->m_file->size();
 	if (fileSize_i64 > fileSize_MAX) {
 		// A Win16 executable larger than 16 MB doesn't make any sense.
 		q->m_file->unref();

src/libromdata/disc/PEResourceReader.cpp
@@ -60,9 +60,6 @@ class PEResourceReaderPrivate
 		PEResourceReader *const q_ptr;
 
 	public:
-		// EXE file.
-		IRpFile *file;
-
 		// .rsrc section.
 		uint32_t rsrc_addr;
 		uint32_t rsrc_size;