GerbilSoft/rom-properties

commit e8b3cbfd8a01bc67e5c88e75879f0d45f51f2f8a

[libromdata] CisoGcnReader: Use a uint32_t for the magic number.
Code size changes: (64-bit Gentoo Linux, gcc-8.2.0-r6, release build)

   text    data     bss     dec     hex filename
   1093     192       0    1285     505 CisoGcnReader.cpp.o [before]
   1132     192       0    1324     52c CisoGcnReader.cpp.o [after]
    +39       0       0     +39     -27 Difference

This seems to have *increased* the code size for some reason...

src/libromdata/disc/CisoGcnReader.cpp
@@ -52,9 +52,6 @@ class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 		RP_DISABLE_COPY(CisoGcnReaderPrivate)
 
 	public:
-		// CISO magic.
-		static const char CISO_MAGIC[4];
-
 		// CISO header.
 		CISOHeader cisoHeader;
 
@@ -68,9 +65,6 @@ class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 };
 
 /** CisoGcnReaderPrivate **/
-
-// CISO magic.
-const char CisoGcnReaderPrivate::CISO_MAGIC[4] = {'C','I','S','O'};
 
 CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
 	: super(q)
@@ -100,7 +94,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
 	}
 
 	// Verify the CISO header.
-	if (memcmp(cisoHeader.magic, CISO_MAGIC, sizeof(CISO_MAGIC)) != 0) {
+	if (cisoHeader.magic == cpu_to_be32(CISO_MAGIC)) {
 		// Invalid magic.
 		q->m_file->unref();
 		q->m_file = nullptr;
@@ -172,7 +166,8 @@ int CisoGcnReader::isDiscSupported_static(const uint8_t *pHeader, size_t szHeade
 	}
 
 	// Check the CISO magic.
-	if (memcmp(pHeader, CisoGcnReaderPrivate::CISO_MAGIC, sizeof(CisoGcnReaderPrivate::CISO_MAGIC)) != 0) {
+	const CISOHeader *const cisoHeader = reinterpret_cast<const CISOHeader*>(pHeader);
+	if (cisoHeader->magic == cpu_to_be32(CISO_MAGIC)) {
 		// Invalid magic.
 		return -1;
 	}
@@ -180,8 +175,7 @@ int CisoGcnReader::isDiscSupported_static(const uint8_t *pHeader, size_t szHeade
 	// Check if the block size is a supported power of two.
 	// - Minimum: CISO_BLOCK_SIZE_MIN (32 KB, 1 << 15)
 	// - Maximum: CISO_BLOCK_SIZE_MAX (16 MB, 1 << 24)
-	const uint32_t *pHeader32 = reinterpret_cast<const uint32_t*>(pHeader);
-	const unsigned int block_size = le32_to_cpu(pHeader32[1]);
+	const uint32_t block_size = le32_to_cpu(cisoHeader->block_size);
 	bool isPow2 = false;
 	for (unsigned int i = 15; i <= 24; i++) {
 		if (block_size == (1U << i)) {

src/libromdata/disc/ciso_gcn.h
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * ciso_gcn.h: GameCube/Wii CISO structs.                                  *
  *                                                                         *
- * Copyright (c) 2016 by David Korth.                                      *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -43,10 +43,16 @@ extern "C" {
 #define CISO_BLOCK_SIZE_MIN (32768)
 #define CISO_BLOCK_SIZE_MAX (16*1024*1024)
 
+/**
+ * CISO (GameCube) header struct.
+ *
+ * All fields are in little-endian.
+ */
+#define CISO_MAGIC 'CISO'
 typedef struct PACKED _CISOHeader {
-	char magic[4];			// "CISO"
-	uint32_t block_size;		// LE32
-	uint8_t map[CISO_MAP_SIZE];	// 0 == unused; 1 == used; other == invalid
+	uint32_t magic;			// [0x000] 'CISO'
+	uint32_t block_size;		// [0x004] LE32
+	uint8_t map[CISO_MAP_SIZE];	// [0x008] 0 == unused; 1 == used; other == invalid
 } CISOHeader;
 
 #pragma pack()

commit c2de09cf360c2d3a9abee76a98a00b58a54351b6

[libromdata] CisoGcnReader: Fix inverted magic check.
Otherwise, it ends up detecting everything as CISO, which results in
assertions when viewing something like an MP3 file.

This fixes a regression from commit e8b3cbfd8a01bc67e5c88e75879f0d45f51f2f8a.
([libromdata] CisoGcnReader: Use a uint32_t for the magic number.)

src/libromdata/disc/CisoGcnReader.cpp
@@ -94,7 +94,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
 	}
 
 	// Verify the CISO header.
-	if (cisoHeader.magic == cpu_to_be32(CISO_MAGIC)) {
+	if (cisoHeader.magic != cpu_to_be32(CISO_MAGIC)) {
 		// Invalid magic.
 		q->m_file->unref();
 		q->m_file = nullptr;
@@ -169,7 +169,7 @@ int CisoGcnReader::isDiscSupported_static(const uint8_t *pHeader, size_t szHeade
 
 	// Check the CISO magic.
 	const CISOHeader *const cisoHeader = reinterpret_cast<const CISOHeader*>(pHeader);
-	if (cisoHeader->magic == cpu_to_be32(CISO_MAGIC)) {
+	if (cisoHeader->magic != cpu_to_be32(CISO_MAGIC)) {
 		// Invalid magic.
 		return -1;
 	}