GerbilSoft/rom-properties

commit acb369aaf13d0a9f0c0dd4e40d4726ea39157c4d

Added a configuration option to disable thumbnailing and metadata extraction on network file systems.
Default is disabled, i.e. no thumbnailing or metadata extraction on
network file systems.

This also handles certain "bad" file systems on Linux, e.g. procfs.

TODO:
- Parse /proc/mounts instead of checking statfs() magic.
- Make Windows support more comprehensive instead of just checking
  for "\\\\" followed by something that isn't '?' or '.'.
- Add support for Mac OS X.

src/kde/RomThumbCreator.cpp
@@ -25,6 +25,7 @@
 // librpbase
 #include "librpbase/RomData.hpp"
 #include "librpbase/TextFuncs.hpp"
+#include "librpbase/file/FileSystem.hpp"
 #include "librpbase/img/rp_image.hpp"
 #include "librpbase/img/RpPngWriter.hpp"
 using namespace LibRpBase;
@@ -269,11 +270,19 @@ RomThumbCreator::~RomThumbCreator()
 bool RomThumbCreator::create(const QString &path, int width, int height, QImage &img)
 {
 	Q_UNUSED(height);
+	const QByteArray u8path = path.toUtf8();
+
+	// Check for "bad" file systems.
+	const Config *const config = Config::instance();
+	if (FileSystem::isOnBadFS(u8path.constData(), config->enableThumbnailOnNetworkFS())) {
+		// This file is on a "bad" file system.
+		return RPCT_SOURCE_FILE_BAD_FS;
+	}
 
 	// Assuming width and height are the same.
 	// TODO: What if they aren't?
 	Q_D(RomThumbCreator);
-	int ret = d->getThumbnail(Q2U8(path), width, img);
+	int ret = d->getThumbnail(u8path.constData(), width, img);
 	return (ret == 0);
 }
 
@@ -307,6 +316,13 @@ Q_DECL_EXPORT int rp_create_thumbnail(const char *source_file, const char *outpu
 	// Register RpQImageBackend.
 	// TODO: Static initializer somewhere?
 	rp_image::setBackendCreatorFn(RpQImageBackend::creator_fn);
+
+	// Check for "bad" file systems.
+	const Config *const config = Config::instance();
+	if (FileSystem::isOnBadFS(source_file, config->enableThumbnailOnNetworkFS())) {
+		// This file is on a "bad" file system.
+		return RPCT_SOURCE_FILE_BAD_FS;
+	}
 
 	// Attempt to open the ROM file.
 	// TODO: RpQFile wrapper.

src/gtk/CreateThumbnail.cpp
@@ -21,6 +21,7 @@
 // librpbase
 #include "librpbase/common.h"
 #include "librpbase/RomData.hpp"
+#include "librpbase/file/FileSystem.hpp"
 #include "librpbase/file/RpFile.hpp"
 #include "librpbase/img/rp_image.hpp"
 #include "librpbase/img/RpPngWriter.hpp"
@@ -246,6 +247,13 @@ G_MODULE_EXPORT int rp_create_thumbnail(const char *source_file, const char *out
 	// NOTE: TCreateThumbnail() has wrappers for opening the
 	// ROM file and getting RomData*, but we're doing it here
 	// in order to return better error codes.
+
+	// Check for "bad" file systems.
+	const Config *const config = Config::instance();
+	if (FileSystem::isOnBadFS(source_file, config->enableThumbnailOnNetworkFS())) {
+		// This file is on a "bad" file system.
+		return RPCT_SOURCE_FILE_BAD_FS;
+	}
 
 	// Attempt to open the ROM file.
 	// TODO: RpGVfsFile wrapper.

src/kde/kde5/RpExtractorPlugin.cpp
@@ -28,11 +28,10 @@
 // librpbase
 #include "librpbase/RomData.hpp"
 #include "librpbase/RomMetaData.hpp"
+#include "librpbase/file/FileSystem.hpp"
 #include "librpbase/file/RpFile.hpp"
-using LibRpBase::RomData;
-using LibRpBase::RomMetaData;
-using LibRpBase::IRpFile;
-using LibRpBase::RpFile;
+#include "librpbase/config/Config.hpp"
+using namespace LibRpBase;
 
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
@@ -102,9 +101,18 @@ void RpExtractorPlugin::extract(ExtractionResult *result)
 		return;
 
 	// Single file, and it's local.
+	const QByteArray u8filename = filename.toUtf8();
+
+	// Check for "bad" file systems.
+	const Config *const config = Config::instance();
+	if (FileSystem::isOnBadFS(u8filename.constData(), config->enableThumbnailOnNetworkFS())) {
+		// This file is on a "bad" file system.
+		return;
+	}
+
 	// TODO: RpQFile wrapper.
 	// For now, using RpFile, which is an stdio wrapper.
-	RpFile *const file = new RpFile(Q2U8(filename), RpFile::FM_OPEN_READ_GZ);
+	RpFile *const file = new RpFile(u8filename.constData(), RpFile::FM_OPEN_READ_GZ);
 	if (!file->isOpen()) {
 		// Could not open the file.
 		return;

src/kde/kde5/RpOverlayIconPlugin.cpp
@@ -27,12 +27,10 @@
 
 // librpbase
 #include "librpbase/RomData.hpp"
+#include "librpbase/file/FileSystem.hpp"
 #include "librpbase/file/RpFile.hpp"
 #include "librpbase/config/Config.hpp"
-using LibRpBase::RomData;
-using LibRpBase::IRpFile;
-using LibRpBase::RpFile;
-using LibRpBase::Config;
+using namespace LibRpBase;
 
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
@@ -116,9 +114,17 @@ QStringList RpOverlayIconPlugin::getOverlays(const QUrl &item)
 	}
 
 	// Single file, and it's local.
+	const QByteArray u8filename = filename.toUtf8();
+
+	// Check for "bad" file systems.
+	if (FileSystem::isOnBadFS(u8filename.constData(), config->enableThumbnailOnNetworkFS())) {
+		// This file is on a "bad" file system.
+		return sl;
+	}
+
 	// TODO: RpQFile wrapper.
 	// For now, using RpFile, which is an stdio wrapper.
-	RpFile *const file = new RpFile(Q2U8(filename), RpFile::FM_OPEN_READ_GZ);
+	RpFile *const file = new RpFile(u8filename.constData(), RpFile::FM_OPEN_READ_GZ);
 	if (!file->isOpen()) {
 		// Could not open the file.
 		file->unref();

src/libromdata/img/TCreateThumbnail.hpp
@@ -43,6 +43,7 @@ typedef enum {
 	RPCT_SOURCE_FILE_NO_IMAGE	= 4,	// Source file has no image.
 	RPCT_OUTPUT_FILE_FAILED		= 5,	// Failed to save the output file.
 	RPCT_SOURCE_FILE_CLASS_DISABLED	= 6,	// User configuration has disabled thumbnails for this class.
+	RPCT_SOURCE_FILE_BAD_FS		= 7,	// Source file is located on a "bad" file system.
 } RpCreateThumbnailError;
 
 /**

src/librpbase/config/Config.cpp
@@ -107,6 +107,7 @@ class ConfigPrivate : public ConfReaderPrivate
 		bool useIntIconForSmallSizes;
 		bool downloadHighResScans;
 		bool showDangerousPermissionsOverlayIcon;
+		bool enableThumbnailOnNetworkFS;
 };
 
 /** ConfigPrivate **/
@@ -142,6 +143,8 @@ ConfigPrivate::ConfigPrivate()
 	, downloadHighResScans(true)
 	/* Overlay icon */
 	, showDangerousPermissionsOverlayIcon(true)
+	/* Enable thumbnailing and metadata on network FS */
+	, enableThumbnailOnNetworkFS(false)
 {
 	// NOTE: Configuration is also initialized in the reset() function.
 }
@@ -162,12 +165,14 @@ void ConfigPrivate::reset(void)
 	mapImgTypePrio.reserve(16);
 #endif
 
-	// Download options.
+	// Download options
 	extImgDownloadEnabled = true;
 	useIntIconForSmallSizes = true;
 	downloadHighResScans = true;
-	// Overlay icon.
+	// Overlay icon
 	showDangerousPermissionsOverlayIcon = true;
+	// Enable thumbnail and metadata on network FS
+	enableThumbnailOnNetworkFS = false;
 }
 
 /**
@@ -221,6 +226,8 @@ int ConfigPrivate::processConfigLine(const char *section, const char *name, cons
 		bool *param;
 		if (!strcasecmp(name, "ShowDangerousPermissionsOverlayIcon")) {
 			param = &showDangerousPermissionsOverlayIcon;
+		} else if (!strcasecmp(name, "EnableThumbnailOnNetworkFS")) {
+			param = &enableThumbnailOnNetworkFS;
 		} else {
 			// Invalid option.
 			return 1;
@@ -526,6 +533,16 @@ bool Config::showDangerousPermissionsOverlayIcon(void) const
 {
 	RP_D(const Config);
 	return d->showDangerousPermissionsOverlayIcon;
+}
+
+/**
+ * Enable thumbnailing and metadata on network filesystems?
+ * @return True if we should enable; false if not.
+ */
+bool Config::enableThumbnailOnNetworkFS(void) const
+{
+	RP_D(const Config);
+	return d->enableThumbnailOnNetworkFS;
 }
 
 }

src/librpbase/config/Config.hpp
@@ -127,6 +127,12 @@ class Config : public ConfReader
 		 * @return True if we should show the overlay icon; false if not.
 		 */
 		bool showDangerousPermissionsOverlayIcon(void) const;
+
+		/**
+		 * Enable thumbnailing and metadata on network filesystems?
+		 * @return True if we should enable; false if not.
+		 */
+		bool enableThumbnailOnNetworkFS(void) const;
 };
 
 }

src/librpbase/file/FileSystem.hpp
@@ -165,6 +165,19 @@ bool is_symlink(const char *filename);
  */
 std::string resolve_symlink(const char *filename);
 
+/**
+ * Is a file located on a "bad" file system?
+ *
+ * We don't want to check files on e.g. procfs,
+ * or on network file systems if the option is disabled.
+ *
+ * @param filename Filename.
+ * @param netFS If true, allow network file systems.
+ *
+ * @return True if this file is on a "bad" file system; false if not.
+ */
+bool isOnBadFS(const char *filename, bool netFS = false);
+
 } }
 
 #endif /* __ROMPROPERTIES_LIBRPBASE_FILESYSTEM_HPP__ */

commit 00164ec98f82437ec3de4d2da81019bde1869d8f

[kde] RomThumbCreator::create(): Return `false`, not `RPCT_SOURCE_FILE_BAD_FS`.
src/kde/RomThumbCreator.cpp: In member function ‘virtual bool RomThumbCreator::create(const QString&, int, int, QImage&)’:
src/kde/RomThumbCreator.cpp:279:10: warning: enum constant in boolean context [-Wint-in-bool-context]
   return RPCT_SOURCE_FILE_BAD_FS;
          ^~~~~~~~~~~~~~~~~~~~~~~

This fixes a regression from commit acb369aaf13d0a9f0c0dd4e40d4726ea39157c4d.
(Added a configuration option to disable thumbnailing and metadata extraction on network file systems.)

src/kde/RomThumbCreator.cpp
@@ -276,7 +276,7 @@ bool RomThumbCreator::create(const QString &path, int width, int height, QImage 
 	const Config *const config = Config::instance();
 	if (FileSystem::isOnBadFS(u8path.constData(), config->enableThumbnailOnNetworkFS())) {
 		// This file is on a "bad" file system.
-		return RPCT_SOURCE_FILE_BAD_FS;
+		return false;
 	}
 
 	// Assuming width and height are the same.