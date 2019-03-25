GerbilSoft/rom-properties

commit 7ee118465034349de1846efc80ec5a1df815048d

[libromdata] IDiscReader: Moved m_file / m_discReader to the base class.
Every subclass has one or the other, and NCCHReader might have
either one, so move it to the common base class so we can
de-virtualize some functions.

IDiscReader's IRpFile* constructor now takes a reference to the
file and unreferences it in the destructor.

IDiscReader's IDiscReader* constructor doesn't take any reference,
since IDiscReader doesn't support references right now.
- TODO: Add references?

IDiscReader::isDevice() is now a standard function.

GcnPartitionPrivate: Moved offsetShift to reduce padding.

Code size reduction for `rpcli`: (64-bit Gentoo Linux, gcc-8.2.0-r6, release build)

   text    data     bss     dec     hex filename
1049769   98377   17456 1165602  11c922 bin/rpcli [before]
1048285   98305   17456 1164046  11c30e bin/rpcli [after]
  -1484     -72       0   -1556    -614 Difference

src/libromdata/disc/Cdrom2352Reader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file);
+		Cdrom2352ReaderPrivate(Cdrom2352Reader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -68,22 +68,22 @@ class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 const uint8_t Cdrom2352ReaderPrivate::CDROM_2352_MAGIC[12] =
 	{0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00};
 
-Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file)
-	: super(q, file)
+Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q)
+	: super(q)
 	, blockCount(0)
 {
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Check the disc size.
 	// Should be a multiple of 2352.
-	int64_t fileSize = file->size();
+	int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0 || fileSize % 2352 != 0) {
 		// Invalid disc size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -101,7 +101,7 @@ Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file
 /** Cdrom2352Reader **/
 
 Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
-	: super(new Cdrom2352ReaderPrivate(this, file))
+	: super(new Cdrom2352ReaderPrivate(this), file)
 { }
 
 /**

src/libromdata/disc/CisoGcnReader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file);
+		CisoGcnReaderPrivate(CisoGcnReader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -72,8 +72,8 @@ class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 // CISO magic.
 const char CisoGcnReaderPrivate::CISO_MAGIC[4] = {'C','I','S','O'};
 
-CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
-	: super(q, file)
+CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
+	: super(q)
 	, maxLogicalBlockUsed(-1)
 {
 	// Clear the CISO header struct.
@@ -81,7 +81,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Clear the CISO block map initially.
 	blockMap.fill(0xFFFF);
 
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
@@ -89,12 +89,12 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Read the CISO header.
 	static_assert(sizeof(CISOHeader) == CISO_HEADER_SIZE,
 		"CISOHeader is the wrong size. (Should be 32,768 bytes.)");
-	this->file->rewind();
-	size_t sz = this->file->read(&cisoHeader, sizeof(cisoHeader));
+	q->m_file->rewind();
+	size_t sz = q->m_file->read(&cisoHeader, sizeof(cisoHeader));
 	if (sz != sizeof(cisoHeader)) {
 		// Error reading the CISO header.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -102,8 +102,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Verify the CISO header.
 	if (memcmp(cisoHeader.magic, CISO_MAGIC, sizeof(CISO_MAGIC)) != 0) {
 		// Invalid magic.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -117,8 +117,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 		// If the block size is 0x18, then this is
 		// actually a PSP CISO, and this field is
 		// the CISO header size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -138,8 +138,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 				break;
 			default:
 				// Invalid entry.
-				this->file->unref();
-				this->file = nullptr;
+				q->m_file->unref();
+				q->m_file = nullptr;
 				q->m_lastError = EIO;
 				return;
 		}
@@ -155,7 +155,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 /** CisoGcnReader **/
 
 CisoGcnReader::CisoGcnReader(IRpFile *file)
-	: super(new CisoGcnReaderPrivate(this, file))
+	: super(new CisoGcnReaderPrivate(this), file)
 { }
 
 /**

src/libromdata/disc/GdiReader.cpp
@@ -58,7 +58,7 @@ namespace LibRomData {
 
 class GdiReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		GdiReaderPrivate(GdiReader *q, IRpFile *file);
+		GdiReaderPrivate(GdiReader *q);
 		virtual ~GdiReaderPrivate();
 
 	private:
@@ -116,24 +116,24 @@ class GdiReaderPrivate : public SparseDiscReaderPrivate {
 
 /** GdiReaderPrivate **/
 
-GdiReaderPrivate::GdiReaderPrivate(GdiReader *q, IRpFile *file)
-	: super(q, file)
+GdiReaderPrivate::GdiReaderPrivate(GdiReader *q)
+	: super(q)
 	, blockCount(0)
 {
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Save the filename for later.
-	filename = this->file->filename();
+	filename = q->m_file->filename();
 
 	// GDI file should be 4k or less.
-	int64_t fileSize = file->size();
+	int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0 || fileSize > 4096) {
 		// Invalid GDI file size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -141,12 +141,12 @@ GdiReaderPrivate::GdiReaderPrivate(GdiReader *q, IRpFile *file)
 	// Read the GDI and parse the track information.
 	const unsigned int gdisize = static_cast<unsigned int>(fileSize);
 	unique_ptr<char[]> gdibuf(new char[gdisize+1]);
-	file->rewind();
-	size_t size = file->read(gdibuf.get(), gdisize);
+	q->m_file->rewind();
+	size_t size = q->m_file->read(gdibuf.get(), gdisize);
 	if (size != gdisize) {
 		// Read error.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -233,9 +233,10 @@ void GdiReaderPrivate::close(void)
 	trackMappings.clear();
 
 	// GDI file.
-	if (this->file) {
-		this->file->unref();
-		this->file = nullptr;
+	RP_Q(GdiReader);
+	if (q->m_file) {
+		q->m_file->unref();
+		q->m_file = nullptr;
 	}
 }
 
@@ -404,17 +405,18 @@ int GdiReaderPrivate::openTrack(int trackNumber)
 	}
 
 	// File opened. Get its size and calculate the end block.
-	int64_t fileSize = file->size();
+	RP_Q(GdiReader);
+	const int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0) {
 		// Empty or invalid flie...
-		this->file->unref();
+		q->m_file->unref();
 		return -EIO;
 	}
 
 	// Is the file a multiple of the sector size?
 	if (fileSize % blockRange->sectorSize != 0) {
 		// Not a multiple of the sector size.
-		this->file->unref();
+		q->m_file->unref();
 		return -EIO;
 	}
 
@@ -427,7 +429,7 @@ int GdiReaderPrivate::openTrack(int trackNumber)
 /** GdiReader **/
 
 GdiReader::GdiReader(IRpFile *file)
-	: super(new GdiReaderPrivate(this, file))
+	: super(new GdiReaderPrivate(this), file)
 { }
 
 /**

commit a6466b361ce38b9f4d79338157756a9abd328bb6

[libromdata] SparseDiscReader subclasses: q->m_file isn't usable in the private class constructor.
The private class is created *before* the main class's superclass
constructor is called, so q->m_file will be undefined at this point.

Move most of the private class constructor code to the main class
constructor. (Struct clearing code is left as-is.)

This fixes the following assertion:

$ rpcli RSBE01.wbfs
== Reading file 'RSBE01.wbfs'...
rpcli: src/librpbase/disc/SparseDiscReader.cpp:168: virtual int LibRpBase::SparseDiscReader::seek(int64_t): Assertion `d->disc_size > 0' failed.
Aborted

Note that release builds would have simply claimed the file wasn't
supported.

This fixes a regression from commit 7ee118465034349de1846efc80ec5a1df815048d.
([libromdata] IDiscReader: Moved m_file / m_discReader to the base class.)

src/libromdata/disc/Cdrom2352Reader.cpp
@@ -71,38 +71,39 @@ const uint8_t Cdrom2352ReaderPrivate::CDROM_2352_MAGIC[12] =
 Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q)
 	: super(q)
 	, blockCount(0)
+{ }
+
+/** Cdrom2352Reader **/
+
+Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
+	: super(new Cdrom2352ReaderPrivate(this), file)
 {
-	if (!q->m_file) {
+	if (!m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Check the disc size.
 	// Should be a multiple of 2352.
-	int64_t fileSize = q->m_file->size();
+	int64_t fileSize = m_file->size();
 	if (fileSize <= 0 || fileSize % 2352 != 0) {
 		// Invalid disc size.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
 		return;
 	}
 
 	// Disc parameters.
 	// TODO: 64-bit block count?
-	blockCount = static_cast<unsigned int>(fileSize / 2352);
-	block_size = 2048;
-	disc_size = fileSize / 2352 * 2048;
+	RP_D(Cdrom2352Reader);
+	d->blockCount = static_cast<unsigned int>(fileSize / 2352);
+	d->block_size = 2048;
+	d->disc_size = fileSize / 2352 * 2048;
 
 	// Reset the disc position.
-	pos = 0;
+	d->pos = 0;
 }
-
-/** Cdrom2352Reader **/
-
-Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
-	: super(new Cdrom2352ReaderPrivate(this), file)
-{ }
 
 /**
  * Is a disc image supported by this class?

src/libromdata/disc/CisoGcnReader.cpp
@@ -74,85 +74,86 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
 	memset(&cisoHeader, 0, sizeof(cisoHeader));
 	// Clear the CISO block map initially.
 	blockMap.fill(0xFFFF);
-
-	if (!q->m_file) {
-		// File could not be ref()'d.
-		return;
-	}
-
-	// Read the CISO header.
-	static_assert(sizeof(CISOHeader) == CISO_HEADER_SIZE,
-		"CISOHeader is the wrong size. (Should be 32,768 bytes.)");
-	q->m_file->rewind();
-	size_t sz = q->m_file->read(&cisoHeader, sizeof(cisoHeader));
-	if (sz != sizeof(cisoHeader)) {
-		// Error reading the CISO header.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Verify the CISO header.
-	if (cisoHeader.magic != cpu_to_be32(CISO_MAGIC)) {
-		// Invalid magic.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Check if the block size is a supported power of two.
-	// - Minimum: CISO_BLOCK_SIZE_MIN (32 KB, 1 << 15)
-	// - Maximum: CISO_BLOCK_SIZE_MAX (16 MB, 1 << 24)
-	block_size = le32_to_cpu(cisoHeader.block_size);
-	if (!isPow2(block_size) ||
-	    block_size < CISO_BLOCK_SIZE_MIN || block_size > CISO_BLOCK_SIZE_MAX)
-	{
-		// Block size is out of range.
-		// If the block size is 0x18, then this is
-		// actually a PSP CISO, and this field is
-		// the CISO header size.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Parse the CISO block map.
-	uint16_t physBlockIdx = 0;
-	for (int i = 0; i < static_cast<int>(blockMap.size()); i++) {
-		switch (cisoHeader.map[i]) {
-			case 0:
-				// Empty block.
-				continue;
-			case 1:
-				// Used block.
-				blockMap[i] = physBlockIdx;
-				physBlockIdx++;
-				maxLogicalBlockUsed = i;
-				break;
-			default:
-				// Invalid entry.
-				q->m_file->unref();
-				q->m_file = nullptr;
-				q->m_lastError = EIO;
-				return;
-		}
-	}
-
-	// Calculate the disc size based on the highest logical block index.
-	disc_size = (static_cast<int64_t>(maxLogicalBlockUsed) + 1) * static_cast<int64_t>(block_size);
-
-	// Reset the disc position.
-	pos = 0;
 }
 
 /** CisoGcnReader **/
 
 CisoGcnReader::CisoGcnReader(IRpFile *file)
 	: super(new CisoGcnReaderPrivate(this), file)
-{ }
+{
+	if (!m_file) {
+		// File could not be ref()'d.
+		return;
+	}
+
+	// Read the CISO header.
+	RP_D(CisoGcnReader);
+	static_assert(sizeof(CISOHeader) == CISO_HEADER_SIZE,
+		"CISOHeader is the wrong size. (Should be 32,768 bytes.)");
+	m_file->rewind();
+	size_t sz = m_file->read(&d->cisoHeader, sizeof(d->cisoHeader));
+	if (sz != sizeof(d->cisoHeader)) {
+		// Error reading the CISO header.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Verify the CISO header.
+	if (d->cisoHeader.magic != cpu_to_be32(CISO_MAGIC)) {
+		// Invalid magic.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Check if the block size is a supported power of two.
+	// - Minimum: CISO_BLOCK_SIZE_MIN (32 KB, 1 << 15)
+	// - Maximum: CISO_BLOCK_SIZE_MAX (16 MB, 1 << 24)
+	d->block_size = le32_to_cpu(d->cisoHeader.block_size);
+	if (!isPow2(d->block_size) ||
+	    d->block_size < CISO_BLOCK_SIZE_MIN || d->block_size > CISO_BLOCK_SIZE_MAX)
+	{
+		// Block size is out of range.
+		// If the block size is 0x18, then this is
+		// actually a PSP CISO, and this field is
+		// the CISO header size.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Parse the CISO block map.
+	uint16_t physBlockIdx = 0;
+	for (int i = 0; i < static_cast<int>(d->blockMap.size()); i++) {
+		switch (d->cisoHeader.map[i]) {
+			case 0:
+				// Empty block.
+				continue;
+			case 1:
+				// Used block.
+				d->blockMap[i] = physBlockIdx;
+				physBlockIdx++;
+				d->maxLogicalBlockUsed = i;
+				break;
+			default:
+				// Invalid entry.
+				m_file->unref();
+				m_file = nullptr;
+				m_lastError = EIO;
+				return;
+		}
+	}
+
+	// Calculate the disc size based on the highest logical block index.
+	d->disc_size = (static_cast<int64_t>(d->maxLogicalBlockUsed) + 1) * static_cast<int64_t>(d->block_size);
+
+	// Reset the disc position.
+	d->pos = 0;
+}
 
 /**
  * Is a disc image supported by this class?

src/libromdata/disc/GdiReader.cpp
@@ -119,100 +119,7 @@ class GdiReaderPrivate : public SparseDiscReaderPrivate {
 GdiReaderPrivate::GdiReaderPrivate(GdiReader *q)
 	: super(q)
 	, blockCount(0)
-{
-	if (!q->m_file) {
-		// File could not be ref()'d.
-		return;
-	}
-
-	// Save the filename for later.
-	filename = q->m_file->filename();
-
-	// GDI file should be 4k or less.
-	int64_t fileSize = q->m_file->size();
-	if (fileSize <= 0 || fileSize > 4096) {
-		// Invalid GDI file size.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Read the GDI and parse the track information.
-	const unsigned int gdisize = static_cast<unsigned int>(fileSize);
-	unique_ptr<char[]> gdibuf(new char[gdisize+1]);
-	q->m_file->rewind();
-	size_t size = q->m_file->read(gdibuf.get(), gdisize);
-	if (size != gdisize) {
-		// Read error.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Make sure the string is NULL-terminated.
-	gdibuf[gdisize] = 0;
-
-	// Parse the GDI file.
-	int ret = parseGdiFile(gdibuf.get());
-	if (ret != 0) {
-		// Error parsing the GDI file.
-		close();
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Open track 03 (primary data track) and the last data track.
-	if (trackMappings.size() >= 3) {
-		ret = openTrack(3);
-		if (ret != 0) {
-			// Error opening track 03.
-			close();
-			q->m_lastError = -ret;
-			return;
-		}
-	}
-
-	// Find the last data track.
-	// NOTE: Searching in reverse order.
-	int lastDataTrack = 0;	// 1-based; 0 is invalid.
-	for (auto iter = trackMappings.crbegin(); iter != trackMappings.crend(); ++iter) {
-		const BlockRange *blockRange = *iter;
-		if (blockRange) {
-			if (static_cast<int>(blockRange->trackNumber) > lastDataTrack) {
-				lastDataTrack = blockRange->trackNumber;
-			}
-		}
-	}
-
-	if (lastDataTrack > 0 && lastDataTrack != 3) {
-		ret = openTrack(lastDataTrack);
-		if (ret != 0) {
-			// Error opening the last data track.
-			close();
-			q->m_lastError = -ret;
-			return;
-		}
-	}
-
-	const BlockRange *const lastBlockRange = trackMappings[lastDataTrack-1];
-	if (!lastBlockRange) {
-		// Should not get here...
-		close();
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Disc parameters.
-	// A full Dreamcast disc has 549,150 sectors.
-	block_size = 2048;
-	blockCount = lastBlockRange->blockEnd + 1;
-	disc_size = blockCount * 2048;
-
-	// Reset the disc position.
-	pos = 0;
-}
+{ }
 
 GdiReaderPrivate::~GdiReaderPrivate()
 {
@@ -430,7 +337,101 @@ int GdiReaderPrivate::openTrack(int trackNumber)
 
 GdiReader::GdiReader(IRpFile *file)
 	: super(new GdiReaderPrivate(this), file)
-{ }
+{
+	if (!m_file) {
+		// File could not be ref()'d.
+		return;
+	}
+
+	// Save the filename for later.
+	RP_D(GdiReader);
+	d->filename = m_file->filename();
+
+	// GDI file should be 4k or less.
+	int64_t fileSize = m_file->size();
+	if (fileSize <= 0 || fileSize > 4096) {
+		// Invalid GDI file size.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Read the GDI and parse the track information.
+	const unsigned int gdisize = static_cast<unsigned int>(fileSize);
+	unique_ptr<char[]> gdibuf(new char[gdisize+1]);
+	m_file->rewind();
+	size_t size = m_file->read(gdibuf.get(), gdisize);
+	if (size != gdisize) {
+		// Read error.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Make sure the string is NULL-terminated.
+	gdibuf[gdisize] = 0;
+
+	// Parse the GDI file.
+	int ret = d->parseGdiFile(gdibuf.get());
+	if (ret != 0) {
+		// Error parsing the GDI file.
+		d->close();
+		m_lastError = EIO;
+		return;
+	}
+
+	// Open track 03 (primary data track) and the last data track.
+	if (d->trackMappings.size() >= 3) {
+		ret = d->openTrack(3);
+		if (ret != 0) {
+			// Error opening track 03.
+			d->close();
+			m_lastError = -ret;
+			return;
+		}
+	}
+
+	// Find the last data track.
+	// NOTE: Searching in reverse order.
+	int lastDataTrack = 0;	// 1-based; 0 is invalid.
+	for (auto iter = d->trackMappings.crbegin(); iter != d->trackMappings.crend(); ++iter) {
+		const GdiReaderPrivate::BlockRange *blockRange = *iter;
+		if (blockRange) {
+			if (static_cast<int>(blockRange->trackNumber) > lastDataTrack) {
+				lastDataTrack = blockRange->trackNumber;
+			}
+		}
+	}
+
+	if (lastDataTrack > 0 && lastDataTrack != 3) {
+		ret = d->openTrack(lastDataTrack);
+		if (ret != 0) {
+			// Error opening the last data track.
+			d->close();
+			m_lastError = -ret;
+			return;
+		}
+	}
+
+	const GdiReaderPrivate::BlockRange *const lastBlockRange = d->trackMappings[lastDataTrack-1];
+	if (!lastBlockRange) {
+		// Should not get here...
+		d->close();
+		m_lastError = EIO;
+		return;
+	}
+
+	// Disc parameters.
+	// A full Dreamcast disc has 549,150 sectors.
+	d->block_size = 2048;
+	d->blockCount = lastBlockRange->blockEnd + 1;
+	d->disc_size = d->blockCount * 2048;
+
+	// Reset the disc position.
+	d->pos = 0;
+}
 
 /**
  * Is a disc image supported by this class?

src/libromdata/disc/NASOSReader.cpp
@@ -85,20 +85,27 @@ NASOSReaderPrivate::NASOSReaderPrivate(NASOSReader *q)
 {
 	// Clear the NASOSHeader structs.
 	memset(&header, 0, sizeof(header));
+}
 
-	if (!q->m_file) {
+/** NASOSReader **/
+
+NASOSReader::NASOSReader(IRpFile *file)
+	: super(new NASOSReaderPrivate(this), file)
+{
+	if (!m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Read the NASOS header.
-	q->m_file->rewind();
-	size_t sz = q->m_file->read(&header, sizeof(header));
-	if (sz != sizeof(header)) {
+	RP_D(NASOSReader);
+	m_file->rewind();
+	size_t sz = m_file->read(&d->header, sizeof(d->header));
+	if (sz != sizeof(d->header)) {
 		// Error reading the NASOS header.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
 		return;
 	}
 
@@ -106,26 +113,26 @@ NASOSReaderPrivate::NASOSReaderPrivate(NASOSReader *q)
 	// TODO: WII9 magic?
 	// TODO: Check the actual disc header magic?
 	unsigned int blockMapStart, blockCount;
-	if (header.nasos.magic == cpu_to_be32(NASOS_MAGIC_GCML)) {
-		discType = DT_GCML;
-		block_size = 2048;			// NOTE: Not stored in the header.
-		blockMapStart = sizeof(header.gcml);
+	if (d->header.nasos.magic == cpu_to_be32(NASOS_MAGIC_GCML)) {
+		d->discType = NASOSReaderPrivate::DT_GCML;
+		d->block_size = 2048;			// NOTE: Not stored in the header.
+		blockMapStart = sizeof(d->header.gcml);
 		blockCount = NASOS_GCML_BlockCount;	// NOTE: Not stored in the header.
-		blockMapShift = 0;
-	} else if ((header.nasos.magic == cpu_to_be32(NASOS_MAGIC_WII5)) ||
-		   (header.nasos.magic == cpu_to_be32(NASOS_MAGIC_WII9)))
+		d->blockMapShift = 0;
+	} else if ((d->header.nasos.magic == cpu_to_be32(NASOS_MAGIC_WII5)) ||
+		   (d->header.nasos.magic == cpu_to_be32(NASOS_MAGIC_WII9)))
 	{
-		discType = DT_WIIx;
-		block_size = 1024;	// TODO: Is this stored in the header?
-		blockMapStart = sizeof(header.wiix);
+		d->discType = NASOSReaderPrivate::DT_WIIx;
+		d->block_size = 1024;	// TODO: Is this stored in the header?
+		blockMapStart = sizeof(d->header.wiix);
 		// TODO: Verify against WII5 (0x460900) and WII9 (0x7ED380).
-		blockCount = le32_to_cpu(header.wiix.block_count) >> 8;
-		blockMapShift = 8;
+		blockCount = le32_to_cpu(d->header.wiix.block_count) >> 8;
+		d->blockMapShift = 8;
 	} else {
 		// Invalid magic.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
 		return;
 	}
 
@@ -133,30 +140,24 @@ NASOSReaderPrivate::NASOSReaderPrivate(NASOSReader *q)
 
 	// Read the block map.
 	// TODO: Restrict the maximum block count?
-	blockMap.resize(blockCount);
-	const size_t sz_blockMap = blockMap.size() * sizeof(uint32_t);
-	sz = q->m_file->seekAndRead(blockMapStart, blockMap.data(), sz_blockMap);
+	d->blockMap.resize(blockCount);
+	const size_t sz_blockMap = d->blockMap.size() * sizeof(uint32_t);
+	sz = m_file->seekAndRead(blockMapStart, d->blockMap.data(), sz_blockMap);
 	if (sz != sz_blockMap) {
 		// Error reading the block map.
-		blockMap.clear();
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
+		d->blockMap.clear();
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
 		return;
 	}
 
 	// Disc size is based on the block map size.
-	disc_size = static_cast<int64_t>(blockMap.size()) * block_size;
+	d->disc_size = static_cast<int64_t>(d->blockMap.size()) * d->block_size;
 
 	// Reset the disc position.
-	pos = 0;
+	d->pos = 0;
 }
-
-/** NASOSReader **/
-
-NASOSReader::NASOSReader(IRpFile *file)
-	: super(new NASOSReaderPrivate(this), file)
-{ }
 
 /**
  * Is a disc image supported by this class?

src/libromdata/disc/WbfsReader.cpp
@@ -107,42 +107,7 @@ WbfsReaderPrivate::WbfsReaderPrivate(WbfsReader *q)
 	, m_wbfs(nullptr)
 	, m_wbfs_disc(nullptr)
 	, wlba_table(nullptr)
-{
-	if (!q->m_file) {
-		// File could not be ref()'d.
-		return;
-	}
-
-	// Read the WBFS header.
-	m_wbfs = readWbfsHeader();
-	if (!m_wbfs) {
-		// Error reading the WBFS header.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Open the first disc.
-	m_wbfs_disc = openWbfsDisc(m_wbfs, 0);
-	if (!m_wbfs_disc) {
-		// Error opening the WBFS disc.
-		freeWbfsHeader(m_wbfs);
-		m_wbfs = nullptr;
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Save important values for later.
-	wlba_table = m_wbfs_disc->header->wlba_table;
-	block_size = m_wbfs->wbfs_sec_sz;
-	pos = 0;	// Reset the read position.
-
-	// Get the size of the WBFS disc.
-	disc_size = getWbfsDiscSize(m_wbfs_disc);
-}
+{ }
 
 WbfsReaderPrivate::~WbfsReaderPrivate()
 {
@@ -395,7 +360,43 @@ int64_t WbfsReaderPrivate::getWbfsDiscSize(const wbfs_disc_t *disc) const
 
 WbfsReader::WbfsReader(IRpFile *file)
 	: super(new WbfsReaderPrivate(this), file)
-{ }
+{
+	if (!m_file) {
+		// File could not be ref()'d.
+		return;
+	}
+
+	// Read the WBFS header.
+	RP_D(WbfsReader);
+	d->m_wbfs = d->readWbfsHeader();
+	if (!d->m_wbfs) {
+		// Error reading the WBFS header.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Open the first disc.
+	d->m_wbfs_disc = d->openWbfsDisc(d->m_wbfs, 0);
+	if (!d->m_wbfs_disc) {
+		// Error opening the WBFS disc.
+		d->freeWbfsHeader(d->m_wbfs);
+		d->m_wbfs = nullptr;
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Save important values for later.
+	d->wlba_table = d->m_wbfs_disc->header->wlba_table;
+	d->block_size = d->m_wbfs->wbfs_sec_sz;
+	d->pos = 0;	// Reset the read position.
+
+	// Get the size of the WBFS disc.
+	d->disc_size = d->getWbfsDiscSize(d->m_wbfs_disc);
+}
 
 /**
  * Is a disc image supported by this class?

src/libromdata/disc/WuxReader.cpp
@@ -75,87 +75,88 @@ WuxReaderPrivate::WuxReaderPrivate(WuxReader *q)
 {
 	// Clear the .wux header struct.
 	memset(&wuxHeader, 0, sizeof(wuxHeader));
-
-	if (!q->m_file) {
-		// File could not be ref()'d.
-		return;
-	}
-
-	// Read the .wux header.
-	q->m_file->rewind();
-	size_t sz = q->m_file->read(&wuxHeader, sizeof(wuxHeader));
-	if (sz != sizeof(wuxHeader)) {
-		// Error reading the .wux header.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Verify the .wux header.
-	if (wuxHeader.magic[0] != cpu_to_le32(WUX_MAGIC_0) ||
-	    wuxHeader.magic[1] != cpu_to_le32(WUX_MAGIC_1))
-	{
-		// Invalid magic.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Check if the block size is a supported power of two.
-	// - Minimum: WUX_BLOCK_SIZE_MIN (256 bytes, 1 << 8)
-	// - Maximum: WUX_BLOCK_SIZE_MAX (128 MB, 1 << 28)
-	block_size = le32_to_cpu(wuxHeader.sectorSize);
-	if (!isPow2(block_size) ||
-	    block_size < WUX_BLOCK_SIZE_MIN || block_size > WUX_BLOCK_SIZE_MAX)
-	{
-		// Block size is out of range.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Read the index table.
-	disc_size = static_cast<int64_t>(le64_to_cpu(wuxHeader.uncompressedSize));
-	if (disc_size < 0 || disc_size > 50LL*1024*1024*1024) {
-		// Disc size is out of range.
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	const uint32_t idxTbl_count = static_cast<uint32_t>(
-		(disc_size + (block_size-1)) / block_size);
-	const size_t idxTbl_size = idxTbl_count * sizeof(uint32_t);
-	idxTbl.resize(idxTbl_count);
-	sz = q->m_file->read(idxTbl.data(), idxTbl_size);
-	if (sz != idxTbl_size) {
-		// Read error.
-		idxTbl.clear();
-		disc_size = 0;
-		q->m_file->unref();
-		q->m_file = nullptr;
-		q->m_lastError = EIO;
-		return;
-	}
-
-	// Data starts after the index table,
-	// aligned to a block_size boundary.
-	dataOffset = sizeof(wuxHeader) + (idxTbl_count * sizeof(uint32_t));
-	dataOffset = ALIGN(block_size, dataOffset);
-
-	// Reset the disc position.
-	pos = 0;
 }
 
 /** WuxReader **/
 
 WuxReader::WuxReader(IRpFile *file)
 	: super(new WuxReaderPrivate(this), file)
-{ }
+{
+	if (!m_file) {
+		// File could not be ref()'d.
+		return;
+	}
+
+	// Read the .wux header.
+	RP_D(WuxReader);
+	m_file->rewind();
+	size_t sz = m_file->read(&d->wuxHeader, sizeof(d->wuxHeader));
+	if (sz != sizeof(d->wuxHeader)) {
+		// Error reading the .wux header.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Verify the .wux header.
+	if (d->wuxHeader.magic[0] != cpu_to_le32(WUX_MAGIC_0) ||
+	    d->wuxHeader.magic[1] != cpu_to_le32(WUX_MAGIC_1))
+	{
+		// Invalid magic.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Check if the block size is a supported power of two.
+	// - Minimum: WUX_BLOCK_SIZE_MIN (256 bytes, 1 << 8)
+	// - Maximum: WUX_BLOCK_SIZE_MAX (128 MB, 1 << 28)
+	d->block_size = le32_to_cpu(d->wuxHeader.sectorSize);
+	if (!isPow2(d->block_size) ||
+	    d->block_size < WUX_BLOCK_SIZE_MIN || d->block_size > WUX_BLOCK_SIZE_MAX)
+	{
+		// Block size is out of range.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Read the index table.
+	d->disc_size = static_cast<int64_t>(le64_to_cpu(d->wuxHeader.uncompressedSize));
+	if (d->disc_size < 0 || d->disc_size > 50LL*1024*1024*1024) {
+		// Disc size is out of range.
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	const uint32_t idxTbl_count = static_cast<uint32_t>(
+		(d->disc_size + (d->block_size-1)) / d->block_size);
+	const size_t idxTbl_size = idxTbl_count * sizeof(uint32_t);
+	d->idxTbl.resize(idxTbl_count);
+	sz = m_file->read(d->idxTbl.data(), idxTbl_size);
+	if (sz != idxTbl_size) {
+		// Read error.
+		d->idxTbl.clear();
+		d->disc_size = 0;
+		m_file->unref();
+		m_file = nullptr;
+		m_lastError = EIO;
+		return;
+	}
+
+	// Data starts after the index table,
+	// aligned to a block_size boundary.
+	d->dataOffset = sizeof(d->wuxHeader) + (idxTbl_count * sizeof(uint32_t));
+	d->dataOffset = ALIGN(d->block_size, d->dataOffset);
+
+	// Reset the disc position.
+	d->pos = 0;
+}
 
 /**
  * Is a disc image supported by this class?