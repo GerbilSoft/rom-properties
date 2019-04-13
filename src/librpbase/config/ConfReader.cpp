GerbilSoft/rom-properties

commit 354795436af777aa2ee63d507d4077928e5069b5

[inih] Updated to inih-r43.
Removed ini_parse_w(), since this should be handled by the caller.

[librpbase] ConfReader.cpp: On Windows, use _wfopen() to open the file
using a Unicode pathname, then use ini_parse_file() to parse the opened
INI file.

src/librpbase/config/ConfReader.cpp
@@ -190,18 +190,29 @@ int ConfReader::load(bool force)
 	// on the local file system, and it's easier to let inih
 	// manage the file itself.
 #ifdef _WIN32
-	// Win32: Use ini_parse_w().
-#ifdef UNICODE
-	int ret = ini_parse_w(U82T_s(d->conf_filename),
-			ConfReaderPrivate::processConfigLine_static, d);
-#else /* !UNICODE */
+# ifdef _UNICODE
+	// Win32, Unicode: Open the file using _wfopen(),
+	// then parse it using ini_parse_file().
+	int ret = 0;
+	errno = 0;
+	FILE *f_ini = _wfopen(U82T_s(d->conf_filename), "rb");
+	if (f_ini) {
+		// Parse the INI file.
+		ret = ini_parse_file(f_ini, ConfReaderPrivate::processConfigLine_static, d);
+		fclose(f_ini);
+	} else {
+		// Error opening the INI file.
+		ret = errno;
+	}
+# else /* !_UNICODE */
+	// Win32, ANSI: Use ini_parse().
 	int ret = ini_parse(U82T_s(d->conf_filename),
-			ConfReaderPrivate::processConfigLine_static, d);
-#endif /* UNICODE */
+		ConfReaderPrivate::processConfigLine_static, d);
+# endif /* _UNICODE */
 #else /* !_WIN32 */
 	// Linux or other systems: Use ini_parse().
 	int ret = ini_parse(d->conf_filename.c_str(),
-			ConfReaderPrivate::processConfigLine_static, d);
+		ConfReaderPrivate::processConfigLine_static, d);
 #endif /* _WIN32 */
 	if (ret != 0) {
 		// Error parsing the INI file.

extlib/inih/.travis.yml
@@ -1,0 +1,11 @@
+language: c
+
+# Setting sudo access to false will let Travis CI use containers
+# rather than VMs to run the tests. For more details see:
+# https://docs.travis-ci.com/user/reference/overview/
+sudo: false
+
+script:
+  - cd tests
+  - ./unittest.sh
+  - git diff --exit-code

extlib/inih/cpp/INIReader.cpp
@@ -30,6 +30,12 @@ string INIReader::Get(const string& section, const string& name, const string& d
     return _values.count(key) ? _values.find(key)->second : default_value;
 }
 
+string INIReader::GetString(const string& section, const string& name, const string& default_value) const
+{
+    const string str = Get(section, name, "");
+    return str.empty() ? default_value : str;
+}
+
 long INIReader::GetInteger(const string& section, const string& name, long default_value) const
 {
     string valstr = Get(section, name, "");
@@ -62,6 +68,12 @@ bool INIReader::GetBoolean(const string& section, const string& name, bool defau
         return default_value;
 }
 
+bool INIReader::HasValue(const std::string& section, const std::string& name) const
+{
+    string key = MakeKey(section, name);
+    return _values.count(key);
+}
+
 string INIReader::MakeKey(const string& section, const string& name)
 {
     string key = section + "=" + name;
@@ -73,7 +85,7 @@ string INIReader::MakeKey(const string& section, const string& name)
 int INIReader::ValueHandler(void* user, const char* section, const char* name,
                             const char* value)
 {
-    INIReader* reader = (INIReader*)user;
+    INIReader* reader = static_cast<INIReader*>(user);
     string key = MakeKey(section, name);
     if (reader->_values[key].size() > 0)
         reader->_values[key] += "\n";

extlib/inih/cpp/INIReader.h
@@ -18,7 +18,7 @@ class INIReader
 public:
     // Construct INIReader and parse given filename. See ini.h for more info
     // about the parsing.
-    INIReader(const std::string& filename);
+    explicit INIReader(const std::string& filename);
 
     // Return the result of ini_parse(), i.e., 0 on success, line number of
     // first error on parse error, or -1 on file open error.
@@ -26,6 +26,11 @@ public:
 
     // Get a string value from INI file, returning default_value if not found.
     std::string Get(const std::string& section, const std::string& name,
+                    const std::string& default_value) const;
+    
+    // Get a string value from INI file, returning default_value if not found,
+    // empty, or contains only whitespace.
+    std::string GetString(const std::string& section, const std::string& name,
                     const std::string& default_value) const;
 
     // Get an integer (long) value from INI file, returning default_value if
@@ -41,6 +46,9 @@ public:
     // not a valid true/false value. Valid true values are "true", "yes", "on", "1",
     // and valid false values are "false", "no", "off", "0" (not case sensitive).
     bool GetBoolean(const std::string& section, const std::string& name, bool default_value) const;
+
+    // Return true if a value exists with the given section and field names.
+    bool HasValue(const std::string& section, const std::string& name) const;
 
 private:
     int _error;

extlib/inih/ini.c
@@ -87,7 +87,7 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
     char* line;
     int max_line = INI_INITIAL_ALLOC;
 #endif
-#if INI_ALLOW_REALLOC
+#if INI_ALLOW_REALLOC && !INI_USE_STACK
     char* new_line;
     int offset;
 #endif
@@ -116,7 +116,7 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
 
     /* Scan through stream line by line */
     while (reader(line, max_line, stream) != NULL) {
-#if INI_ALLOW_REALLOC
+#if INI_ALLOW_REALLOC && !INI_USE_STACK
         offset = strlen(line);
         while (offset == max_line - 1 && line[offset - 1] != '\n') {
             max_line *= 2;
@@ -166,6 +166,10 @@ int ini_parse_stream(ini_reader reader, void* stream, ini_handler handler,
                 *end = '\0';
                 strncpy0(section, start + 1, sizeof(section));
                 *prev_name = '\0';
+#if INI_CALL_HANDLER_ON_NEW_SECTION
+                if (!HANDLER(user, section, NULL, NULL) && !error)
+                    error = lineno;
+#endif
             }
             else if (!error) {
                 /* No ']' found on section line */
@@ -230,22 +234,6 @@ int ini_parse(const char* filename, ini_handler handler, void* user)
     fclose(file);
     return error;
 }
-
-#ifdef _WIN32
-/* See documentation in header file. */
-int ini_parse_w(const wchar_t* filename, ini_handler handler, void* user)
-{
-    FILE* file;
-    int error;
-
-    file = _wfopen(filename, L"rb");
-    if (!file)
-        return -1;
-    error = ini_parse_file(file, handler, user);
-    fclose(file);
-    return error;
-}
-#endif /* _WIN32 */
 
 /* An ini_reader function to read the next line from a string buffer. This
    is the fgets() equivalent used by ini_parse_string(). */

extlib/inih/ini.h
@@ -57,12 +57,6 @@ typedef char* (INIHCALL *ini_reader)(char* str, int num, void* stream);
 */
 int ini_parse(const char* filename, ini_handler handler, void* user);
 
-#ifdef _WIN32
-/* Same as ini_parse(), but takes a wide character filename. Needed for
-   Unicode filenames on Windows. */
-int ini_parse_w(const wchar_t* filename, ini_handler handler, void* user);
-#endif /* _WIN32 */
-
 /* Same as ini_parse(), but takes a FILE* instead of filename. This doesn't
    close the file when it's finished -- the caller must do that. */
 int ini_parse_file(FILE* file, ini_handler handler, void* user);
@@ -86,7 +80,7 @@ int ini_parse_string(const char* string, ini_handler handler, void* user);
 #endif
 
 /* Nonzero to allow a UTF-8 BOM sequence (0xEF 0xBB 0xBF) at the start of
-   the file. See http://code.google.com/p/inih/issues/detail?id=21 */
+   the file. See https://github.com/benhoyt/inih/issues/21 */
 #ifndef INI_ALLOW_BOM
 #define INI_ALLOW_BOM 1
 #endif

extlib/inih/tests/duplicate_sections.ini
@@ -1,0 +1,6 @@
+[section1]
+single1 = abc
+single2 = xyz
+[section1]
+single1 = def
+single2 = qrs

extlib/inih/tests/unittest.bat
@@ -9,3 +9,4 @@
 @call tcc ..\ini.c -I..\ -DINI_USE_STACK=0 -DINI_ALLOW_REALLOC=1 -DINI_INITIAL_ALLOC=5 -run unittest.c > baseline_heap_realloc.txt
 @call tcc ..\ini.c -I..\ -DINI_USE_STACK=0 -DINI_MAX_LINE=20 -DINI_ALLOW_REALLOC=1 -DINI_INITIAL_ALLOC=5 -run unittest.c > baseline_heap_realloc_max_line.txt
 @call tcc ..\ini.c -I..\ -DINI_USE_STACK=0 -DINI_MAX_LINE=20 -DINI_INITIAL_ALLOC=20 -run unittest.c > baseline_heap_string.txt
+@call tcc ..\ini.c -I..\ -DINI_CALL_HANDLER_ON_NEW_SECTION=1 -run unittest.c > baseline_call_handler_on_new_section.txt

commit 6a0510fa71c9d9dc9c897c9ac62c12d56a74a9ea

[librpbase] ConfReader: Need to use _T() for _wfopen()'s mode parameter.
Use _tfopen() and remove the separate Unicode and ANSI branches.

This fixes a regression from commit 354795436af777aa2ee63d507d4077928e5069b5.
([inih] Updated to inih-r43.)

src/librpbase/config/ConfReader.cpp
@@ -190,12 +190,11 @@ int ConfReader::load(bool force)
 	// on the local file system, and it's easier to let inih
 	// manage the file itself.
 #ifdef _WIN32
-# ifdef _UNICODE
-	// Win32, Unicode: Open the file using _wfopen(),
+	// Win32: Open the file using _tfopen(),
 	// then parse it using ini_parse_file().
 	int ret = 0;
 	errno = 0;
-	FILE *f_ini = _wfopen(U82T_s(d->conf_filename), "rb");
+	FILE *f_ini = _tfopen(U82T_s(d->conf_filename), _T("rb"));
 	if (f_ini) {
 		// Parse the INI file.
 		ret = ini_parse_file(f_ini, ConfReaderPrivate::processConfigLine_static, d);
@@ -204,11 +203,6 @@ int ConfReader::load(bool force)
 		// Error opening the INI file.
 		ret = errno;
 	}
-# else /* !_UNICODE */
-	// Win32, ANSI: Use ini_parse().
-	int ret = ini_parse(U82T_s(d->conf_filename),
-		ConfReaderPrivate::processConfigLine_static, d);
-# endif /* _UNICODE */
 #else /* !_WIN32 */
 	// Linux or other systems: Use ini_parse().
 	int ret = ini_parse(d->conf_filename.c_str(),