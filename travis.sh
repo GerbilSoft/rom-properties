db999473caacb07157813fad5f011012ef126e12
David Korth <gerbilsoft@gerbilsoft.com>
2019-06-08 17:34:48 -0400

    .travis.yml, travis.sh: Updated for XFCE3 and MATE.

--- travis.sh
+++ travis.sh
@@ -19,8 +19,8 @@ case "$OSTYPE" in
 		;;
 	*)
 		# Linux. Enable everything.
-		# NOTE: KF5 is not available on Ubuntu 14.04,
-		# so we can't build the KDE5 plugin.
+		# NOTE: KF5 and MATE (GTK3) are not available on Ubuntu 14.04,
+		# so we can't build the KDE5 or MATE plugins.
 		cmake .. \
 			-DCMAKE_INSTALL_PREFIX=/usr \
 			-DENABLE_LTO=OFF \
@@ -30,7 +30,9 @@ case "$OSTYPE" in
 			-DBUILD_KDE4=ON \
 			-DBUILD_KDE5=OFF \
 			-DBUILD_XFCE=ON \
+			-DBUILD_XFCE3=OFF \
 			-DBUILD_GNOME=ON \
+			-DBUILD_MATE=OFF
 			|| exit 1
 esac
 
