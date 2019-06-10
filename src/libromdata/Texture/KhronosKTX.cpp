4ca933bd44f528581be3d948eac99398d0c89274
David Korth <gerbilsoft@gerbilsoft.com>
2019-06-10 01:24:08 -0400

    [libromdata] KhronosKTX: Handle sRGB ETCn as RGB ETCn so they at least show up for now.
    TODO: sRGB post-processing?

--- src/libromdata/Texture/KhronosKTX.cpp
+++ src/libromdata/Texture/KhronosKTX.cpp
@@ -345,23 +345,29 @@ const rp_image *KhronosKTXPrivate::loadImage(void)
 					break;
 
 				case GL_COMPRESSED_RGB8_ETC2:
+				case GL_COMPRESSED_SRGB8_ETC2:
 					// ETC2-compressed RGB texture.
+					// TODO: Handle sRGB.
 					img = ImageDecoder::fromETC2_RGB(
 						ktxHeader.pixelWidth, height,
 						buf.get(), expected_size);
 					break;
 
 				case GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+				case GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
 					// ETC2-compressed RGB texture
 					// with punchthrough alpha.
+					// TODO: Handle sRGB.
 					img = ImageDecoder::fromETC2_RGB_A1(
 						ktxHeader.pixelWidth, height,
 						buf.get(), expected_size);
 					break;
 
 				case GL_COMPRESSED_RGBA8_ETC2_EAC:
+				case GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
 					// ETC2-compressed RGB texture
 					// with EAC-compressed alpha channel.
+					// TODO: Handle sRGB.
 					img = ImageDecoder::fromETC2_RGBA(
 						ktxHeader.pixelWidth, height,
 						buf.get(), expected_size);
