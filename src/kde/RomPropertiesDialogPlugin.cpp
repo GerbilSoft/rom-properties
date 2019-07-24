GerbilSoft/rom-properties

commit 7f03c806bc04422a34f939329f880cfeddaff3b2

Don't initialize the Linux UI frontends if running as root.
This covers rp-stub, rp-config, the standalone thumbnailer executables,
forwarding libraries, and the actual rom-properties extensions.

We shouldn't be running as root, since that exposes an unreasonable
attack surface, especially if a file download is requested.

rpcli will still allow running as root for now.

[gtk] Use G_LOG_DOMAIN instead of defining RP_EXTENSION_NAME.

- Add definitions using TARGET_COMPILE_DEFINITIONS everywhere instead
  of ADD_DEFINITIONS().
- xfce: Use G_LOG_DOMAIN so we get xfce3 for the GTK3 version.

[kde] Use QT_VERSION to differentiate between kde4 and kde5.

- FIXME: This is a runtime conversion using printf(). Maybe change it
  to be compile-time somehow.

src/kde/RomPropertiesDialogPlugin.cpp
@@ -37,6 +37,11 @@ using LibRomData::RomDataFactory;
 RomPropertiesDialogPlugin::RomPropertiesDialogPlugin(KPropertiesDialog *props, const QVariantList&)
 	: super(props)
 {
+	if (getuid() == 0 || geteuid() == 0) {
+		qCritical("*** rom-properties-kde%u does not support running as root.", QT_VERSION >> 16);
+		return;
+	}
+
 	// Check if a single file was specified.
 	KFileItemList items = props->items();
 	if (items.size() != 1) {

src/gtk/CreateThumbnail.cpp
@@ -226,6 +226,11 @@ G_MODULE_EXPORT int rp_create_thumbnail(const char *source_file, const char *out
 	// Some of this is based on the GNOME Thumbnailer skeleton project.
 	// https://github.com/hadess/gnome-thumbnailer-skeleton/blob/master/gnome-thumbnailer-skeleton.c
 
+	if (getuid() == 0 || geteuid() == 0) {
+		g_critical("*** " G_LOG_DOMAIN " does not support running as root.");
+		return RPCT_RUNNING_AS_ROOT;
+	}
+
 	// Make sure glib is initialized.
 	// NOTE: This is a no-op as of glib-2.36.
 #if !GLIB_CHECK_VERSION(2,36,0)

src/gtk/gnome/CMakeLists.txt
@@ -99,8 +99,10 @@ IF(BUILD_GNOME)
 	TARGET_LINK_LIBRARIES(rom-properties-gnome PUBLIC Gnome::libnautilus-extension)
 	TARGET_LINK_LIBRARIES(rom-properties-gnome PRIVATE Cairo::cairo)
 	TARGET_LINK_LIBRARIES(rom-properties-gnome PUBLIC Gtk3::gtk3 GLib2::gobject GLib2::glib)
-	ADD_DEFINITIONS(-DG_LOG_DOMAIN=\"rom-properties-gnome\")
-	ADD_DEFINITIONS(-DRP_UI_GTK3_GNOME)
+	TARGET_COMPILE_DEFINITIONS(rom-properties-gnome
+		PRIVATE G_LOG_DOMAIN=\"rom-properties-gnome\"
+			RP_UI_GTK3_GNOME
+		)
 ENDIF(BUILD_GNOME)
 
 #######################

src/gtk/gnome/rom-properties-plugin.c
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (GNOME)                            *
  * rom-properties-plugin.c: Nautilus Plugin Definition.                    *
  *                                                                         *
- * Copyright (c) 2017 by David Korth.                                      *
+ * Copyright (c) 2017-2019 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -21,10 +21,8 @@ static GType type_list[1];
 
 #if defined(RP_UI_GTK3_GNOME)
 // GNOME 3 desktop
-# define RP_EXTENSION_NAME		"rom-properties-gnome"
 #elif defined(RP_UI_GTK3_MATE)
 // MATE desktop (v1.18.0+; GTK+ 3.x)
-# define RP_EXTENSION_NAME		"rom-properties-mate"
 # define nautilus_module_initialize	caja_module_initialize
 # define nautilus_module_shutdown	caja_module_shutdown
 # define nautilus_module_list_types	caja_module_list_types
@@ -40,8 +38,13 @@ G_MODULE_EXPORT void nautilus_module_list_types		(const GType	**types,
 G_MODULE_EXPORT void
 nautilus_module_initialize(GTypeModule *module)
 {
+	if (getuid() == 0 || geteuid() == 0) {
+		g_critical("*** " G_LOG_DOMAIN " does not support running as root.");
+		return;
+	}
+
 #ifdef G_ENABLE_DEBUG
-	g_message("Initializing " RP_EXTENSION_NAME " extension");
+	g_message("Initializing " G_LOG_DOMAIN " extension");
 #endif
 
 	/* Register the types provided by this module */
@@ -57,7 +60,7 @@ G_MODULE_EXPORT void
 nautilus_module_shutdown(void)
 {
 #ifdef G_ENABLE_DEBUG
-	g_message("Shutting down " RP_EXTENSION_NAME " extension");
+	g_message("Shutting down " G_LOG_DOMAIN " extension");
 #endif
 }
 

src/gtk/mate/CMakeLists.txt
@@ -90,8 +90,10 @@ IF(BUILD_MATE)
 	TARGET_LINK_LIBRARIES(rom-properties-mate PUBLIC Mate::libcaja-extension)
 	TARGET_LINK_LIBRARIES(rom-properties-mate PRIVATE Cairo::cairo)
 	TARGET_LINK_LIBRARIES(rom-properties-mate PUBLIC Gtk3::gtk3 GLib2::gobject GLib2::glib)
-	ADD_DEFINITIONS(-DG_LOG_DOMAIN=\"rom-properties-mate\")
-	ADD_DEFINITIONS(-DRP_UI_GTK3_MATE)
+	TARGET_COMPILE_DEFINITIONS(rom-properties-mate
+		PRIVATE G_LOG_DOMAIN=\"rom-properties-mate\"
+			RP_UI_GTK3_MATE
+		)
 ENDIF(BUILD_MATE)
 
 #######################

src/gtk/thumbnailer-dbus/rp-thumbnailer-main.cpp
@@ -118,6 +118,11 @@ int main(int argc, char *argv[])
 	RP_UNUSED(argc);
 	RP_UNUSED(argv);
 
+	if (getuid() == 0 || geteuid() == 0) {
+		fprintf(stderr, "*** %s does not support running as root.", argv[0]);
+		return EXIT_FAILURE;
+	}
+
 #if !GLIB_CHECK_VERSION(2,36,0)
 	// g_type_init() is automatic as of glib-2.36.0
 	// and is marked deprecated.

src/gtk/xfce/rom-properties-plugin.c
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (XFCE)                             *
  * rom-properties-plugin.c: ThunarX Plugin Definition.                     *
  *                                                                         *
- * Copyright (c) 2017 by David Korth.                                      *
+ * Copyright (c) 2017-2019 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -28,17 +28,20 @@ static GType type_list[1];
 G_MODULE_EXPORT void
 thunar_extension_initialize(ThunarxProviderPlugin *plugin)
 {
-	const gchar *mismatch;
+	if (getuid() == 0 || geteuid() == 0) {
+		g_critical("*** " G_LOG_DOMAIN " does not support running as root.");
+		return;
+	}
 
 	/* verify that the thunarx versions are compatible */
-	mismatch = thunarx_check_version(THUNARX_MAJOR_VERSION, THUNARX_MINOR_VERSION, THUNARX_MICRO_VERSION);
+	const gchar *mismatch = thunarx_check_version(THUNARX_MAJOR_VERSION, THUNARX_MINOR_VERSION, THUNARX_MICRO_VERSION);
 	if (G_UNLIKELY(mismatch != NULL)) {
 		g_warning ("Version mismatch: %s", mismatch);
 		return;
 	}
 
 #ifdef G_ENABLE_DEBUG
-	g_message("Initializing rom-properties-xfce extension");
+	g_message("Initializing " G_LOG_DOMAIN " extension");
 #endif
 
 	/* Register the types provided by this plugin */
@@ -53,7 +56,7 @@ G_MODULE_EXPORT void
 thunar_extension_shutdown(void)
 {
 #ifdef G_ENABLE_DEBUG
-	g_message("Shutting down rom-properties-xfce extension");
+	g_message("Shutting down " G_LOG_DOMAIN " extension");
 #endif
 }
 

src/gtk/xfce3/CMakeLists.txt
@@ -86,7 +86,9 @@ IF(BUILD_XFCE3)
 	TARGET_LINK_LIBRARIES(rom-properties-xfce3 PUBLIC Xfce::thunarx-3)
 	TARGET_LINK_LIBRARIES(rom-properties-xfce3 PRIVATE Cairo::cairo)
 	TARGET_LINK_LIBRARIES(rom-properties-xfce3 PUBLIC Gtk3::gtk3 GLib2::gobject GLib2::glib)
-	ADD_DEFINITIONS(-DG_LOG_DOMAIN=\"rom-properties-xfce3\")
+	TARGET_COMPILE_DEFINITIONS(rom-properties-xfce3
+		PRIVATE G_LOG_DOMAIN=\"rom-properties-xfce3\"
+		)
 ENDIF(BUILD_XFCE3)
 
 #######################

commit 24e1c585fb8f9656fe179a793921699ff9498724

[kde] RomPropertiesDialogPlugin.cpp: #include <unistd.h>
This should fix the travis-ci build.

This fixes a regression from commit 7f03c806bc04422a34f939329f880cfeddaff3b2.
(Don't initialize the Linux UI frontends if running as root.)

src/kde/RomPropertiesDialogPlugin.cpp
@@ -31,6 +31,9 @@ using LibRpBase::RpFile;
 #include "libromdata/RomDataFactory.hpp"
 using LibRomData::RomDataFactory;
 
+// C includes.
+#include <unistd.h>
+
 // Qt includes.
 #include <QtCore/QFileInfo>
 