8cae1948ca8d203e7e05efa59c1980d19e0a1f9d
David Korth <gerbilsoft@gerbilsoft.com>
2019-08-06 22:01:23 -0400

    Use GDI+ for JPEG decoding on Windows.
    JPEG decoding isn't as strictly checked as PNG decoding, and we don't
    need specific JPEG writing functionality e.g. for APNGs and tEXt chunks,
    so we can drop the libjpeg-turbo requirement on Windows.
    
    This reduces the Windows distribution by around 1 MB uncompressed.
    (521 KB for the 32-bit jpeg62.dll; 535 KB for the 64-bit jpeg32.dll.)
    
    Related: Commit f548b5372d3309bb426555863ec88920c610f1f8
    (Removed the GDI+ PNG loader; renamed RpPng_libpng.cpp to RpPng.cpp.)
    
    [librptexture] rp_image_backend: Allow width and height to be 0 if the
    specified format is rp_image::FORMAT_NONE. This is done by
    RpGdiplusBackend when constructing a backend using an existing
    Gdiplus::Bitmap.

---
+++ src/librpbase/img/RpJpeg_gdiplus.cpp
@@ -0,0 +1,93 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librpbase)                        *
+ * RpJpeg_gdiplus.cpp: JPEG image handler. (GDI+ version)                  *
+ *                                                                         *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "RpJpeg.hpp"
+
+#include "file/IRpFile.hpp"
+#include "file/win32/IStreamWrapper.hpp"
+using LibRpBase::IRpFile;
+using LibRpBase::IStreamWrapper;
+
+// librptexture
+#include "librptexture/img/rp_image.hpp"
+#include "librptexture/img/RpGdiplusBackend.hpp"
+using LibRpTexture::rp_image;
+using LibRpTexture::RpGdiplusBackend;
+
+// Gdiplus for JPEG decoding.
+// NOTE: Gdiplus requires min/max.
+#include <algorithm>
+namespace Gdiplus {
+	using std::min;
+	using std::max;
+}
+#include <gdiplus.h>
+#include "librptexture/img/GdiplusHelper.hpp"
+
+namespace LibRpBase {
+
+/** RpJpeg **/
+
+/**
+ * Load a JPEG image from an IRpFile.
+ *
+ * This image is NOT checked for issues; do not use
+ * with untrusted images!
+ *
+ * @param file IRpFile to load from.
+ * @return rp_image*, or nullptr on error.
+ */
+rp_image *RpJpeg::loadUnchecked(IRpFile *file)
+{
+	if (!file)
+		return nullptr;
+
+	// Initialize GDI+.
+	// TODO: Don't init/shutdowno n every image.
+	ScopedGdiplus gdip;
+	if (!gdip.isValid()) {
+		// Failed to initialized GDI+.
+		return nullptr;
+	}
+
+	// Rewind the file.
+	file->rewind();
+
+	// Load the image using IStreamWrapper.
+	IStreamWrapper *const stream = new IStreamWrapper(file);
+	Gdiplus::Bitmap *const pGdipBmp = Gdiplus::Bitmap::FromStream(stream, FALSE);
+	stream->Release();
+	if (!pGdipBmp) {
+		// Could not load the image.
+		return nullptr;
+	}
+
+	// Create an rp_image using the GDI+ bitmap.
+	RpGdiplusBackend *const backend = new RpGdiplusBackend(pGdipBmp);
+	return new rp_image(backend);
+}
+
+/**
+ * Load a JPEG image from an IRpFile.
+ *
+ * This image is verified with various tools to ensure
+ * it doesn't have any errors.
+ *
+ * @param file IRpFile to load from.
+ * @return rp_image*, or nullptr on error.
+ */
+rp_image *RpJpeg::load(IRpFile *file)
+{
+	if (!file)
+		return nullptr;
+
+	// FIXME: Add a JPEG equivalent of pngcheck().
+	return loadUnchecked(file);
+}
+
+}
