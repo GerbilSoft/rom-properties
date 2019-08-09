8a95e140a267928b113b426699d8bd501d9e9a22
David Korth <gerbilsoft@gerbilsoft.com>
2019-08-08 21:23:34 -0400

    [librptexture] ImageDecoder.hpp: Removed extra qualification on fromLinear32().
    I noticed this when attempting to build for FreeBSD. (Linux builds never
    hit this path due to the use of IFUNC, and MSVC doesn't seem to care.)

--- src/librptexture/decoder/ImageDecoder.hpp
+++ src/librptexture/decoder/ImageDecoder.hpp
@@ -777,7 +777,7 @@ static inline rp_image *fromLinear24(PixelFormat px_format,
  * @param stride	[in,opt] Stride, in bytes. If 0, assumes width*bytespp.
  * @return rp_image, or nullptr on error.
  */
-static rp_image *ImageDecoder::fromLinear32(PixelFormat px_format,
+static rp_image *fromLinear32(PixelFormat px_format,
 	int width, int height,
 	const uint32_t *RESTRICT img_buf, int img_siz, int stride)
 {
