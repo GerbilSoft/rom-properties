GerbilSoft/rom-properties

commit 107224487f3d690fb9d4a88ad478eb9537191e42

[cmake] Reworked --compress-debug-sections logic for compatibility with older systems.
Ubuntu 14.04's ld (binutils-2.24) doesn't have --compress-debug-sections,
which causes travis-ci to fail.

cmake-3.6's try_compile() attempts to build an executable by default,
which *should* test the linker flags, but that isn't working, either.
Ubuntu 14.04 has cmake-3.3, so that wouldn't help anyway.

Instead, check the output of `ld --help` for the various options.
This seems to work for the most part.

Enabled this method for Mac OS X as well.

If the --compress-debug-sections=zlib linker test fails, then we'll
fall back to the old objcopy method. SplitDebugInformation.cmake uses
the same detection method to check if objcopy supports the option,
and if it does, it uses it.

cmake/platform/gcc.cmake
@@ -96,26 +96,68 @@ IF(ENABLE_COVERAGE)
 ENDIF(ENABLE_COVERAGE)
 
 # Test for common LDFLAGS.
-# TODO: Doesn't work on OS X. (which means it's not really testing it!)
-IF(NOT APPLE)
-	FOREACH(FLAG_TEST "-Wl,-O1" "-Wl,--sort-common" "-Wl,--as-needed" "-Wl,--build-id" "-Wl,--compress-debug-sections=zlib")
-		CHECK_C_COMPILER_FLAG("${FLAG_TEST}" LDFLAG_${FLAG_TEST})
+# NOTE: CHECK_C_COMPILER_FLAG() doesn't seem to work, even with
+# CMAKE_TRY_COMPILE_TARGET_TYPE. Check `ld --help` for the various
+# parameters instead.
+EXECUTE_PROCESS(COMMAND ${CMAKE_LINKER} --help
+	OUTPUT_VARIABLE _ld_out
+	ERROR_QUIET)
+
+FOREACH(FLAG_TEST "-O" "--sort-common" "--as-needed" "--build-id")
+	IF(NOT DEFINED LDFLAG_${FLAG_TEST})
+		MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST}")
+		IF(_ld_out MATCHES "${FLAG_TEST}")
+			MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - yes")
+			SET(LDFLAG_${FLAG_TEST} 1 CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+		ELSE()
+			MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - no")
+			SET(LDFLAG_${FLAG_TEST} "" CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+		ENDIF()
+	ENDIF()
+
+	IF(LDFLAG_${FLAG_TEST})
+		SET(RP_EXE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON} -Wl,${FLAG_TEST}")
+	ENDIF(LDFLAG_${FLAG_TEST})
+ENDFOREACH()
+
+# Special case for --compress-debug-sections.
+SET(FLAG_TEST "--compress-debug-sections")
+	IF(NOT DEFINED LDFLAG_${FLAG_TEST})
+		MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST}")
+		IF(_ld_out MATCHES "${FLAG_TEST}")
+			MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - yes")
+			SET(LDFLAG_${FLAG_TEST} 1 CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+		ELSE()
+			MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - no")
+			SET(LDFLAG_${FLAG_TEST} "" CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+		ENDIF()
+	ENDIF()
+
+	IF(LDFLAG_${FLAG_TEST})
+		SET(RP_EXE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON} -Wl,${FLAG_TEST}=zlib")
+	ENDIF(LDFLAG_${FLAG_TEST})
+UNSET(FLAG_TEST)
+
+IF(NOT WIN32)
+	# Bsymbolic-functions doesn't make sense on Windows.
+	FOREACH(FLAG_TEST "-Bsymbolic-functions")
+		IF(NOT DEFINED LDFLAG_${FLAG_TEST})
+			MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST}")
+			IF(_ld_out MATCHES "${FLAG_TEST}")
+				MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - yes")
+				SET(LDFLAG_${FLAG_TEST} 1 CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+			ELSE()
+				MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - no")
+				SET(LDFLAG_${FLAG_TEST} "" CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+			ENDIF()
+		ENDIF()
+
 		IF(LDFLAG_${FLAG_TEST})
-			SET(RP_EXE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON} ${FLAG_TEST}")
+			SET(RP_EXE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON} -Wl,${FLAG_TEST}")
 		ENDIF(LDFLAG_${FLAG_TEST})
-		UNSET(LDFLAG_${FLAG_TEST})
 	ENDFOREACH()
-	IF(NOT WIN32)
-		# Bsymbolic-functions doesn't make sense on Windows.
-		FOREACH(FLAG_TEST "-Wl,-Bsymbolic-functions")
-			CHECK_C_COMPILER_FLAG("${FLAG_TEST}" LDFLAG_${FLAG_TEST})
-			IF(LDFLAG_${FLAG_TEST})
-				SET(RP_EXE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON} ${FLAG_TEST}")
-			ENDIF(LDFLAG_${FLAG_TEST})
-			UNSET(LDFLAG_${FLAG_TEST})
-		ENDFOREACH()
-	ENDIF(NOT WIN32)
-ENDIF(NOT APPLE)
+ENDIF(NOT WIN32)
+
 SET(RP_SHARED_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON}")
 SET(RP_MODULE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON}")
 

cmake/macros/SplitDebugInformation.cmake
@@ -36,6 +36,29 @@ ELSEIF(NOT CMAKE_STRIP)
 ENDIF()
 
 IF(SPLIT_OK)
+	# If the linker doesn't support --compress-debug-sections=zlib,
+	# check if objcopy supports --compress-debug-sections.
+	IF(HAS_LD_COMPRESS_DEBUG_SECTIONS)
+		# ld supports --compress-debug-sections=zlib.
+		SET(OBJCOPY_COMPRESS_DEBUG_SECTIONS_PARAM "" CACHE INTERNAL "objcopy parameter to compress debug sections.")
+	ELSEIF(NOT HAS_LD_COMPRESS_DEBUG_SECTIONS AND NOT DEFINED OBJCOPY_COMPRESS_DEBUG_SECTIONS_PARAM)
+		# Check for objcopy --compress-debug-sections.
+		MESSAGE(STATUS "Checking if objcopy supports --compress-debug-sections")
+		EXECUTE_PROCESS(COMMAND ${CMAKE_OBJCOPY} --help
+			OUTPUT_VARIABLE _xc_out
+			ERROR_QUIET)
+		IF(_xc_out MATCHES "--compress-debug-sections")
+			# objcopy has --compress-debug-sections.
+			MESSAGE(STATUS "Checking if objcopy supports --compress-debug-sections - yes")
+			SET(OBJCOPY_COMPRESS_DEBUG_SECTIONS_PARAM "--compress-debug-sections" CACHE INTERNAL "objcopy parameter to compress debug sections.")
+		ELSE()
+			# objcopy does *not* have --compress-debug-sections.
+			MESSAGE(STATUS "Checking if objcopy supports --compress-debug-sections - no")
+			SET(OBJCOPY_COMPRESS_DEBUG_SECTIONS_PARAM "" CACHE INTERNAL "objcopy parameter to compress debug sections.")
+		ENDIF()
+		UNSET(_xc_out)
+	ENDIF()
+
 	# Handle target prefixes if not overridden.
 	# NOTE: Cannot easily use the TYPE property in a generator expression...
 	GET_PROPERTY(TARGET_TYPE TARGET ${_target} PROPERTY TYPE)
@@ -60,7 +83,7 @@ IF(SPLIT_OK)
 	# These sections are split into the .debug file, so there's
 	# no reason to keep them in the executable.
 	ADD_CUSTOM_COMMAND(TARGET ${_target} POST_BUILD
-		COMMAND ${CMAKE_OBJCOPY} --only-keep-debug
+		COMMAND ${CMAKE_OBJCOPY} --only-keep-debug ${OBJCOPY_COMPRESS_DEBUG_SECTIONS_PARAM}
 			${SPLITDEBUG_SOURCE} ${SPLITDEBUG_TARGET}
 		COMMAND ${CMAKE_STRIP}
 			${SPLITDEBUG_SOURCE}

commit d9078ff921b79fac9b5934569d5f1cd8b87abbb7

[cmake] gcc.cmake: Check for "-O", but use "-O1" instead.
GNU ld treats these as the same, but clang ld requires a value to be
specified.

This fixes a regression on FreeBSD from commit 107224487f3d690fb9d4a88ad478eb9537191e42.
([cmake] Reworked --compress-debug-sections logic for compatibility with older systems.)

cmake/platform/gcc.cmake
@@ -103,7 +103,7 @@ EXECUTE_PROCESS(COMMAND ${CMAKE_LINKER} --help
 	OUTPUT_VARIABLE _ld_out
 	ERROR_QUIET)
 
-FOREACH(FLAG_TEST "-O" "--sort-common" "--as-needed" "--build-id")
+FOREACH(FLAG_TEST "--sort-common" "--as-needed" "--build-id")
 	IF(NOT DEFINED LDFLAG_${FLAG_TEST})
 		MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST}")
 		IF(_ld_out MATCHES "${FLAG_TEST}")
@@ -119,6 +119,24 @@ FOREACH(FLAG_TEST "-O" "--sort-common" "--as-needed" "--build-id")
 		SET(RP_EXE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON} -Wl,${FLAG_TEST}")
 	ENDIF(LDFLAG_${FLAG_TEST})
 ENDFOREACH()
+
+# Special case for -O/-O1.
+SET(FLAG_TEST "-O")
+	IF(NOT DEFINED LDFLAG_${FLAG_TEST})
+		MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST}")
+		IF(_ld_out MATCHES "${FLAG_TEST}")
+			MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - yes")
+			SET(LDFLAG_${FLAG_TEST} 1 CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+		ELSE()
+			MESSAGE(STATUS "Checking if ld supports ${FLAG_TEST} - no")
+			SET(LDFLAG_${FLAG_TEST} "" CACHE INTERNAL "Linker supports ${FLAG_TEST}")
+		ENDIF()
+	ENDIF()
+
+	IF(LDFLAG_${FLAG_TEST})
+		SET(RP_EXE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON} -Wl,-O1")
+	ENDIF(LDFLAG_${FLAG_TEST})
+UNSET(FLAG_TEST)
 
 # Special case for --compress-debug-sections.
 SET(FLAG_TEST "--compress-debug-sections")