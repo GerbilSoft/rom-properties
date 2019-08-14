GerbilSoft/rom-properties

commit 9d48dc1c6cff568cd2fe41486aa0c75f798f5887

[librpbase] RpFile_scsi_freebsd.cpp: Keep the CAM device open.
RpFile_p.hpp: Store `struct cam_device` in DeviceInfo. The struct is
automatically freed in DeviceInfo's destructor if it was allocated.

src/librpbase/file/RpFile_p.hpp
@@ -48,6 +48,11 @@ using std::vector;
 # define INVALID_HANDLE_VALUE nullptr
 #endif
 
+// OS-specific headers for DeviceInfo.
+#if defined(__FreeBSD__) || defined(__DragonFly__)
+# include <camlib.h>
+#endif
+
 namespace LibRpBase {
 
 /** RpFilePrivate **/
@@ -98,6 +103,11 @@ class RpFilePrivate
 			uint8_t *sector_cache;	// Sector cache.
 			uint32_t lba_cache;	// Last LBA cached.
 
+			// OS-specific buffers.
+#if defined(__FreeBSD__) || defined(__DragonFly__)
+			struct cam_device *cam;
+#endif
+
 			DeviceInfo()
 				: device_pos(0)
 				, device_size(0)
@@ -105,9 +115,21 @@ class RpFilePrivate
 				, isKreonUnlocked(0)
 				, sector_cache(nullptr)
 				, lba_cache(~0U)
-			{ }
+			{
+#if defined(__FreeBSD__) || defined(__DragonFly__)
+				cam = nullptr;
+#endif
+			}
 
-			~DeviceInfo() { delete[] sector_cache; }
+			~DeviceInfo()
+			{
+				delete[] sector_cache;
+#if defined(__FreeBSD__) || defined(__DragonFly__)
+				if (cam) {
+					cam_close_device(cam);
+				}
+#endif
+			}
 
 			void alloc_sector_cache(void)
 			{

src/librpbase/file/scsi/RpFile_scsi_freebsd.cpp
@@ -100,27 +100,30 @@ int RpFile::scsi_send_cdb(const void *cdb, uint8_t cdb_len,
 	union ccb ccb;
 	memset(&ccb, 0, sizeof(ccb));
 
-	// FIXME: Don't repeatedly open/close the CAM device.
-	// Store the struct somewhere and delete it on RpFile delete.
-
-	// Get the device name for CAM.
 	RP_D(RpFile);
-	const int fd = fileno(d->file);
-	if (ioctl(fd, CDIOCALLOW) == -1) {
-		// CDIOCALLOW failed.
-		return -errno;
-	}
-	if (ioctl(fd, CAMGETPASSTHRU, &ccb) < 0) {
-		// CAMGETPASSTHRU failed.
-		return -errno;
-	}
-	char pass[128];
-	snprintf(pass, sizeof(pass), "/dev/%.15s%u", ccb.cgdl.periph_name, ccb.cgdl.unit_number);
-
-	struct cam_device *cam = cam_open_pass(pass, O_RDWR, nullptr);
+	struct cam_device *cam = d->devInfo->cam;
 	if (!cam) {
-		// Unable to open the CAM device.
-		return -EIO;
+		// Get the device name for CAM.
+		const int fd = fileno(d->file);
+		if (ioctl(fd, CDIOCALLOW) == -1) {
+			// CDIOCALLOW failed.
+			return -errno;
+		}
+		if (ioctl(fd, CAMGETPASSTHRU, &ccb) < 0) {
+			// CAMGETPASSTHRU failed.
+			return -errno;
+		}
+		char pass[40];
+		snprintf(pass, sizeof(pass), "/dev/%.15s%u", ccb.cgdl.periph_name, ccb.cgdl.unit_number);
+
+		struct cam_device *cam = cam_open_pass(pass, O_RDWR, nullptr);
+		if (!cam) {
+			// Unable to open the CAM device.
+			return -EIO;
+		}
+
+		// Save the CAM device for later.
+		d->devInfo->cam = cam;
 	}
 
 	// Create the command.
@@ -160,11 +163,9 @@ int RpFile::scsi_send_cdb(const void *cdb, uint8_t cdb_len,
 	// Attempt to send the CDB.
 	if (cam_send_ccb(cam, &ccb) < 0) {
 		// Error sending the CDB.
-		cam_close_device(cam);
 		return -errno;
 	}
 
-	cam_close_device(cam);
 	if ((ccb.ccb_h.status & CAM_STATUS_MASK) == CAM_REQ_CMP) {
 		// CDB submitted successfully.
 		return 0;

commit a46a896b1a7d161b6fb05959886200c5218b27ea

[librpbase] RpFile_p.hpp: Disable CAM calls if SCSI is disabled.
This fixes the FreeBSD build.

This fixes a regression from commit 9d48dc1c6cff568cd2fe41486aa0c75f798f5887.
([librpbase] RpFile_scsi_freebsd.cpp: Keep the CAM device open.)

src/librpbase/file/RpFile_p.hpp
@@ -9,6 +9,7 @@
 #ifndef __ROMPROPERTIES_LIBRPBASE_FILE_RPFILE_P_HPP__
 #define __ROMPROPERTIES_LIBRPBASE_FILE_RPFILE_P_HPP__
 
+#include "config.librpbase.h"
 #include "RpFile.hpp"
 
 // C includes. (C++ namespace)
@@ -48,10 +49,12 @@ using std::vector;
 # define INVALID_HANDLE_VALUE nullptr
 #endif
 
+#ifdef RP_OS_SCSI_SUPPORTED
 // OS-specific headers for DeviceInfo.
-#if defined(__FreeBSD__) || defined(__DragonFly__)
-# include <camlib.h>
-#endif
+# if defined(__FreeBSD__) || defined(__DragonFly__)
+#  include <camlib.h>
+# endif /* __FreeBSD__ || __DragonFly__ */
+#endif /* RP_OS_SCSI_SUPPORTED */
 
 namespace LibRpBase {
 
@@ -103,10 +106,12 @@ class RpFilePrivate
 			uint8_t *sector_cache;	// Sector cache.
 			uint32_t lba_cache;	// Last LBA cached.
 
+#ifdef RP_OS_SCSI_SUPPORTED
 			// OS-specific buffers.
-#if defined(__FreeBSD__) || defined(__DragonFly__)
+# if defined(__FreeBSD__) || defined(__DragonFly__)
 			struct cam_device *cam;
-#endif
+# endif /* __FreeBSD__ || __DragonFly__ */
+#endif /* RP_OS_SCSI_SUPPORTED */
 
 			DeviceInfo()
 				: device_pos(0)
@@ -116,19 +121,23 @@ class RpFilePrivate
 				, sector_cache(nullptr)
 				, lba_cache(~0U)
 			{
-#if defined(__FreeBSD__) || defined(__DragonFly__)
+#ifdef RP_OS_SCSI_SUPPORTED
+# if defined(__FreeBSD__) || defined(__DragonFly__)
 				cam = nullptr;
-#endif
+# endif /* __FreeBSD__ || __DragonFly__ */
+#endif /* RP_OS_SCSI_SUPPORTED */
 			}
 
 			~DeviceInfo()
 			{
 				delete[] sector_cache;
-#if defined(__FreeBSD__) || defined(__DragonFly__)
+#ifdef RP_OS_SCSI_SUPPORTED
+# if defined(__FreeBSD__) || defined(__DragonFly__)
 				if (cam) {
 					cam_close_device(cam);
 				}
-#endif
+# endif /* __FreeBSD__ || __DragonFly__ */
+#endif /* RP_OS_SCSI_SUPPORTED */
 			}
 
 			void alloc_sector_cache(void)
@@ -147,12 +156,14 @@ class RpFilePrivate
 				delete[] sector_cache;
 				sector_cache = nullptr;
 
-#if defined(__FreeBSD__) || defined(__DragonFly__)
+#ifdef RP_OS_SCSI_SUPPORTED
+# if defined(__FreeBSD__) || defined(__DragonFly__)
 				if (cam) {
 					cam_close_device(cam);
 					cam = nullptr;
 				}
-#endif
+# endif /* __FreeBSD__ || __DragonFly__ */
+#endif /* RP_OS_SCSI_SUPPORTED */
 			}
 		};
 