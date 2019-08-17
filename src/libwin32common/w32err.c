GerbilSoft/rom-properties

commit fd5eab8253cd7212efd72a19caef1861035c3fc0

Split libromdata's RpWin32.hpp and RpWin32.cpp into multiple files.
libromdata:
- TextFuncs_wchar.hpp: wchar_t text macros and inline functions.
  This is now automatically included by TextFuncs.hpp on Windows.

libwin32common:
- msvc_common.c: gettimeofday() replacement.
- w32err.c: Windows to POSIX error code mapping.
- w32time.h: Time conversion inline functions.

Moved the MinGW interlocked function overloads and UUID_ATTR() to
RpWin32_sdk.h.

UNUSED() has been removed. Use RP_UNUSED() from libromdata/common.h.

src/libwin32common/w32err.c
@@ -1,8 +1,8 @@
 /***************************************************************************
- * ROM Properties Page shell extension. (libromdata)                       *
- * RpWin32.cpp: Windows-specific functions.                                *
+ * ROM Properties Page shell extension. (libwin32common)                   *
+ * w32err.c: Error code mapping. (Windows to POSIX)                        *
  *                                                                         *
- * Copyright (c) 2009-2016 by David Korth.                                 *
+ * Copyright (c) 2016-2017 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -19,9 +19,11 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#include "RpWin32.hpp"
+#include "w32err.h"
 
-namespace LibRomData {
+// C includes.
+#include <errno.h>
+#include <stdlib.h>
 
 typedef struct _errmap {
         DWORD w32;	// Win32 error code.
@@ -124,8 +126,8 @@ static const errmap w32_to_posix[] = {
  */
 static int errmap_compar(const void *a, const void *b)
 {
-	DWORD err1 = static_cast<const errmap*>(a)->w32;
-	DWORD err2 = static_cast<const errmap*>(b)->w32;
+	const DWORD err1 = ((const errmap*)a)->w32;
+	const DWORD err2 = ((const errmap*)a)->w32;
 	if (err1 < err2) return -1;
 	if (err1 > err2) return 1;
 	return 0;
@@ -140,8 +142,9 @@ int w32err_to_posix(DWORD w32err)
 {
 	// Check the error code table.
 	const errmap key = {w32err, 0};
-	const errmap *entry = static_cast<const errmap*>(bsearch(&key,
-			w32_to_posix, ARRAY_SIZE(w32_to_posix),
+	const errmap *const entry = (const errmap*)(bsearch(&key,
+			w32_to_posix,
+			sizeof(w32_to_posix)/sizeof(w32_to_posix[0]),
 			sizeof(errmap), errmap_compar));
 	if (entry) {
 		// Found an error code.
@@ -160,45 +163,3 @@ int w32err_to_posix(DWORD w32err)
 	// Return the default EINVAL.
 	return EINVAL;
 }
-
-}
-
-/** C99/POSIX replacement functions. **/
-
-#ifdef _MSC_VER
-// MSVC doesn't have struct timeval or gettimeofday().
-int gettimeofday(struct timeval *tv, struct timezone *tz)
-{
-	union {
-		uint64_t ns100;	// Time since 1/1/1601 in 100ns units.
-		FILETIME ft;
-	} _now;
-	TIME_ZONE_INFORMATION TimeZoneInformation;
-	DWORD tzi;
-
-	if (tz) {
-		// Get the timezone information.
-		if ((tzi = GetTimeZoneInformation(&TimeZoneInformation)) != TIME_ZONE_ID_INVALID) {
-			tz->tz_minuteswest = TimeZoneInformation.Bias;
-			if (tzi == TIME_ZONE_ID_DAYLIGHT) {
-				tz->tz_dsttime = 1;
-			} else {
-				tz->tz_dsttime = 0;
-			}
-		} else {
-			tz->tz_minuteswest = 0;
-			tz->tz_dsttime = 0;
-		}
-	}
-
-	if (tv) {
-		GetSystemTimeAsFileTime(&_now.ft);	// 100ns units since 1/1/1601
-		// Windows XP's accuract seems to be ~125,000ns == 125us == 0.125ms
-		_now.ns100 -= FILETIME_1970;
-		tv->tv_sec = _now.ns100 / HECTONANOSEC_PER_SEC;			// seconds since 1/1/1970
-		tv->tv_usec = (_now.ns100 % HECTONANOSEC_PER_SEC) / 10LL;	// microseconds
-	}
-
-	return 0;
-}
-#endif /* _MSC_VER */

src/libcachemgr/CMakeLists.txt
@@ -66,6 +66,7 @@ FOREACH(_target cachemgr8 cachemgr16)
 	# Exclude from ALL builds.
 	SET_TARGET_PROPERTIES(${_target} PROPERTIES EXCLUDE_FROM_ALL TRUE)
 	IF(WIN32)
+		TARGET_LINK_LIBRARIES(${_target} win32common)
 		TARGET_LINK_LIBRARIES(${_target} urlmon wininet)
 	ELSE(WIN32)
 		TARGET_LINK_LIBRARIES(${_target} ${CURL_LIBRARIES})

src/libcachemgr/CacheManager.cpp
@@ -33,15 +33,17 @@ using namespace LibRomData::FileSystem;
 
 // Windows includes.
 #ifdef _WIN32
-#include "libromdata/RpWin32.hpp"
+#include "libwin32common/RpWin32_sdk.h"
 #endif /* _WIN32 */
 
 // gettimeofday()
+// NOTE: MSVC doesn't actually have gettimeofday().
+// We have our own version in msvc_common.h.
 #ifdef _MSC_VER
-#include <time.h>
-#else
+#include "libwin32common/msvc_common.h"
+#else /* !_MSC_VER */
 #include <sys/time.h>
-#endif
+#endif /* _MSC_VER */
 
 // C includes. (C++ namespace)
 #include <cctype>

src/libcachemgr/UrlmonDownloader.cpp
@@ -22,9 +22,13 @@
 #include "stdafx.h"
 #include "UrlmonDownloader.hpp"
 
+// libwin32common
+#include "libwin32common/RpWin32_sdk.h"
+#include "libwin32common/w32time.h"
+
+// libromdata
 #include "libromdata/TextFuncs.hpp"
 #include "libromdata/file/RpFile.hpp"
-#include "libromdata/RpWin32.hpp"
 using LibRomData::IRpFile;
 using LibRomData::RpFile;
 using LibRomData::rp_string;

src/libromdata/CMakeLists.txt
@@ -228,7 +228,6 @@ ENDIF(ENABLE_XML)
 
 IF(WIN32)
 	SET(libromdata_OS_SRCS
-		RpWin32.cpp
 		TextFuncs_win32.cpp
 		file/win32/FileSystem_win32.cpp
 		file/win32/RpFile_win32.cpp
@@ -238,7 +237,6 @@ IF(WIN32)
 		threads/InitOnceExecuteOnceXP.c
 		)
 	SET(libromdata_OS_H
-		RpWin32.hpp
 		file/win32/IStreamWrapper.hpp
 		img/GdiplusHelper.hpp
 		img/RpGdiplusBackend.hpp

src/libromdata/SystemRegion.cpp
@@ -23,17 +23,12 @@
 #include "common.h"
 
 #ifdef _WIN32
-#include "RpWin32.hpp"
+#include "libwin32common/RpWin32_sdk.h"
+#include "threads/InitOnceExecuteOnceXP.h"
 #else
 #include <cctype>
 #include <clocale>
 #include <cstring>
-#endif
-
-// One-time initialization.
-#ifdef _WIN32
-#include "threads/InitOnceExecuteOnceXP.h"
-#else
 #include <pthread.h>
 #endif
 

src/libromdata/TextFuncs.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * TextFuncs.hpp: Text encoding functions.                                 *
  *                                                                         *
- * Copyright (c) 2009-2016 by David Korth.                                 *
+ * Copyright (c) 2009-2017 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -569,5 +569,11 @@ static inline int rp_strcasecmp(const rp_char *str1, const rp_char *str2)
 extern "C"
 size_t strnlen(const char *str, size_t len);
 #endif /* HAVE_STRNLEN */
+
+#ifdef _WIN32
+// wchar_t text conversion macros.
+// Generally used only on Windows.
+#include "TextFuncs_wchar.hpp"
+#endif
 
 #endif /* __ROMPROPERTIES_LIBROMDATA_TEXTFUNCS_HPP__ */

src/libromdata/TextFuncs_wchar.hpp
@@ -1,8 +1,9 @@
 /***************************************************************************
  * ROM Properties Page shell extension. (libromdata)                       *
- * RpWin32.hpp: Windows-specific functions.                                *
+ * TextFuncs_wchar.hpp: wchar_t text conversion macros.                    *
+ * Generally only used on Windows.                                         *
  *                                                                         *
- * Copyright (c) 2009-2016 by David Korth.                                 *
+ * Copyright (c) 2009-2017 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -19,28 +20,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#ifndef __ROMPROPERTIES_LIBROMDATA_RPWIN32_HPP__
-#define __ROMPROPERTIES_LIBROMDATA_RPWIN32_HPP__
-
-#ifndef _WIN32
-#error RpWin32.hpp should only be included in Windows builds.
-#endif
-
-#include "libwin32common/RpWin32_sdk.h"
-#include "common.h"
-
-namespace LibRomData {
-
-/**
- * Convert a Win32 error code to a POSIX error code.
- * @param w32err Win32 error code.
- * @return Positive POSIX error code. (If no equivalent is found, default is EINVAL.)
- */
-int w32err_to_posix(DWORD w32err);
-
-}
-
-/** Windows-specific wrappers for wchar_t. **/
+#ifndef __ROMPROPERTIES_LIBROMDATA_TEXTFUNCS_WCHAR_HPP__
+#define __ROMPROPERTIES_LIBROMDATA_TEXTFUNCS_WCHAR_HPP__
 
 /**
  * NOTE: These are defined outside of the LibRomData
@@ -54,7 +35,9 @@ int w32err_to_posix(DWORD w32err);
  * result in a dangling pointer.
  */
 
+// Make sure TextFuncs.hpp was included.
 #include "libromdata/TextFuncs.hpp"
+#include "libromdata/common.h"
 
 #if defined(RP_UTF8)
 
@@ -204,106 +187,4 @@ static inline const LibRomData::rp_string W2RP_ss(const std::wstring &wcs)
 
 #endif /* RP_UTF16 */
 
-/** Time conversion functions. **/
-
-// Macros from MinGW-w64's gettimeofday.c.
-#define FILETIME_1970 116444736000000000LL	// Seconds between 1/1/1601 and 1/1/1970.
-#define HECTONANOSEC_PER_SEC 10000000LL
-
-/**
- * Convert from Unix time to Win32 SYSTEMTIME.
- * @param unix_time Unix time.
- * @param pSystemTime Win32 SYSTEMTIME.
- */
-static inline void UnixTimeToSystemTime(int64_t unix_time, SYSTEMTIME *pSystemTime)
-{
-	// Reference: https://support.microsoft.com/en-us/kb/167296
-	LARGE_INTEGER li;
-	li.QuadPart = (unix_time * HECTONANOSEC_PER_SEC) + FILETIME_1970;
-
-	FILETIME ft;
-	ft.dwLowDateTime = li.LowPart;
-	ft.dwHighDateTime = (DWORD)li.HighPart;
-	FileTimeToSystemTime(&ft, pSystemTime);
-}
-
-/**
- * Convert from Win32 FILETIME to Unix time.
- * @param pFileTime Win32 FILETIME.
- * @return Unix time.
- */
-static inline int64_t FileTimeToUnixTime(const FILETIME *pFileTime)
-{
-	// Reference: https://support.microsoft.com/en-us/kb/167296
-	LARGE_INTEGER li;
-	li.LowPart = pFileTime->dwLowDateTime;
-	li.HighPart = (LONG)pFileTime->dwHighDateTime;
-	return (li.QuadPart - FILETIME_1970) / HECTONANOSEC_PER_SEC;
-}
-
-/**
- * Convert from Win32 SYSTEMTIME to Unix time.
- * @param pFileTime Win32 SYSTEMTIME.
- * @return Unix time.
- */
-static inline int64_t SystemTimeToUnixTime(const SYSTEMTIME *pSystemTime)
-{
-	FILETIME fileTime;
-	SystemTimeToFileTime(pSystemTime, &fileTime);
-	return FileTimeToUnixTime(&fileTime);
-}
-
-#if defined(__GNUC__) && defined(__MINGW32__) && _WIN32_WINNT < 0x0502
-/**
- * MinGW-w64 only defines ULONG overloads for the various atomic functions
- * if _WIN32_WINNT > 0x0502.
- */
-static inline ULONG InterlockedIncrement(ULONG volatile *Addend)
-{
-	return (ULONG)(InterlockedIncrement(reinterpret_cast<LONG volatile*>(Addend)));
-}
-static inline ULONG InterlockedDecrement(ULONG volatile *Addend)
-{
-	return (ULONG)(InterlockedDecrement(reinterpret_cast<LONG volatile*>(Addend)));
-}
-#endif /* __GNUC__ && __MINGW32__ && _WIN32_WINNT < 0x0502 */
-
-#ifdef _MSC_VER
-#define UUID_ATTR(str) __declspec(uuid(str))
-#else /* !_MSC_VER */
-// UUID attribute is not supported by gcc-5.2.0.
-#define UUID_ATTR(str)
-#endif /* _MSC_VER */
-
-#define UNUSED(x) ((void)x)
-
-/** C99/POSIX replacement functions. **/
-
-#ifdef _MSC_VER
-// MSVC doesn't have gettimeofday().
-// NOTE: MSVC does have struct timeval in winsock.h,
-// but it uses long, which is 32-bit.
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define timeval rp_timeval
-struct timeval {
-	time_t   tv_sec;	// seconds
-	uint32_t tv_usec;	// microseconds
-};
-
-#define timezone rp_timezone
-struct timezone {
-	int tz_minuteswest;	// minutes west of Greenwich
-	int tz_dsttime;		// type of DST correction
-};
-
-int gettimeofday(struct timeval *tv, struct timezone *tz);
-#ifdef __cplusplus
-}
-#endif
-#endif /* _MSC_VER */
-
-#endif /* __ROMPROPERTIES_LIBROMDATA_RPWIN32_HPP__ */
+#endif /* __ROMPROPERTIES_LIBROMDATA_TEXTFUNCS_WCHAR_HPP__ */

commit 846cb46e86db6f3c824b3aecad0cf573edfbfa09

[libwin32common] w32err.c: Fix bsearch issues; added ERROR_IO_DEVICE.
Error 124 is ERROR_INVALID_LEVEL, not ERROR_INVALID_HANDLE. This caused
bsearch() to fail because the list ended up not being sorted, since
ERROR_INVALID_HANDLE is 6. This appears to have been broken since the
error lookup was implemented.

errmap_compar(): We were comparing `a` to `a`. It should be comparing
`a` to `b`. The first way doesn't even make any sense.

This basically completely broke the lookup and made it useless.

errmap_compar() broke when splitting error lookup out from libromdata's
RpWin32.cpp into multiple files in libwin32common. (introduced almost
immediately *after* the v1.0 release)

This fixes a regression from commit fd5eab8253cd7212efd72a19caef1861035c3fc0.
(Split libromdata's RpWin32.hpp and RpWin32.cpp into multiple files.)

Also, fix inconsistent tabs in the error map array.

src/libwin32common/w32err.c
@@ -19,24 +19,24 @@ typedef struct _errmap {
 
 static const errmap w32_to_posix[] = {
 	{ERROR_SUCCESS,			0         },  // 0
-        {ERROR_INVALID_FUNCTION,	EINVAL    },  // 1
-        {ERROR_FILE_NOT_FOUND,		ENOENT    },  // 2
-        {ERROR_PATH_NOT_FOUND,		ENOENT    },  // 3
-        {ERROR_TOO_MANY_OPEN_FILES,	EMFILE    },  // 4
-        {ERROR_ACCESS_DENIED,		EACCES    },  // 5
-        {ERROR_INVALID_HANDLE,		EBADF     },  // 6
-        {ERROR_ARENA_TRASHED,		ENOMEM    },  // 7
-        {ERROR_NOT_ENOUGH_MEMORY,	ENOMEM    },  // 8
-        {ERROR_INVALID_BLOCK,		ENOMEM    },  // 9
-        {ERROR_BAD_ENVIRONMENT,		E2BIG     },  // 10
-        {ERROR_BAD_FORMAT,		ENOEXEC   },  // 11
-        {ERROR_INVALID_ACCESS,		EINVAL    },  // 12
-        {ERROR_INVALID_DATA,		EINVAL    },  // 13
+	{ERROR_INVALID_FUNCTION,	EINVAL    },  // 1
+	{ERROR_FILE_NOT_FOUND,		ENOENT    },  // 2
+	{ERROR_PATH_NOT_FOUND,		ENOENT    },  // 3
+	{ERROR_TOO_MANY_OPEN_FILES,	EMFILE    },  // 4
+	{ERROR_ACCESS_DENIED,		EACCES    },  // 5
+	{ERROR_INVALID_HANDLE,		EBADF     },  // 6
+	{ERROR_ARENA_TRASHED,		ENOMEM    },  // 7
+	{ERROR_NOT_ENOUGH_MEMORY,	ENOMEM    },  // 8
+	{ERROR_INVALID_BLOCK,		ENOMEM    },  // 9
+	{ERROR_BAD_ENVIRONMENT,		E2BIG     },  // 10
+	{ERROR_BAD_FORMAT,		ENOEXEC   },  // 11
+	{ERROR_INVALID_ACCESS,		EINVAL    },  // 12
+	{ERROR_INVALID_DATA,		EINVAL    },  // 13
 	{ERROR_OUTOFMEMORY,		ENOMEM    },  // 14
-        {ERROR_INVALID_DRIVE,		ENOENT    },  // 15
-        {ERROR_CURRENT_DIRECTORY,	EACCES    },  // 16
-        {ERROR_NOT_SAME_DEVICE,		EXDEV     },  // 17
-        {ERROR_NO_MORE_FILES,		ENOENT    },  // 18
+	{ERROR_INVALID_DRIVE,		ENOENT    },  // 15
+	{ERROR_CURRENT_DIRECTORY,	EACCES    },  // 16
+	{ERROR_NOT_SAME_DEVICE,		EXDEV     },  // 17
+	{ERROR_NO_MORE_FILES,		ENOENT    },  // 18
 	{ERROR_WRITE_PROTECT,		EROFS     },  // 19
 	{ERROR_BAD_UNIT,		ENODEV    },  // 20
 	{ERROR_WRITE_FAULT,		EIO       },  // 29
@@ -46,42 +46,42 @@ static const errmap w32_to_posix[] = {
 	// ETXTBSY is not defined in MinGW-w64 4.0.6.
 	{ERROR_SHARING_VIOLATION,	ETXTBSY   },  // 32 (TODO)
 #endif
-        {ERROR_LOCK_VIOLATION,		EACCES    },  // BAD33
+	{ERROR_LOCK_VIOLATION,		EACCES    },  // BAD33
 	{ERROR_HANDLE_DISK_FULL,	ENOSPC    },  // 39
 	{ERROR_NOT_SUPPORTED,		ENOTSUP   },  // 50
 #ifdef ENOTUNIQ /* MSVC 2015 doesn't have ENOTUNIQ. */
 	{ERROR_DUP_NAME,		ENOTUNIQ  },  // 52
 #endif
-        {ERROR_BAD_NETPATH,		ENOENT    },  // 53
+	{ERROR_BAD_NETPATH,		ENOENT    },  // 53
 	{ERROR_DEV_NOT_EXIST,		ENODEV    },  // 55
-        {ERROR_NETWORK_ACCESS_DENIED,	EACCES    },  // 65
-        {ERROR_BAD_NET_NAME,		ENOENT    },  // 67
-        {ERROR_FILE_EXISTS,		EEXIST    },  // 80
-        {ERROR_CANNOT_MAKE,		EACCES    },  // 82
-        {ERROR_FAIL_I24,		EACCES    },  // 83
-        {ERROR_INVALID_PARAMETER,	EINVAL    },  // 87
-        {ERROR_NO_PROC_SLOTS,		EAGAIN    },  // 89
-        {ERROR_DRIVE_LOCKED,		EACCES    },  // 108
-        {ERROR_BROKEN_PIPE,		EPIPE     },  // 109
+	{ERROR_NETWORK_ACCESS_DENIED,	EACCES    },  // 65
+	{ERROR_BAD_NET_NAME,		ENOENT    },  // 67
+	{ERROR_FILE_EXISTS,		EEXIST    },  // 80
+	{ERROR_CANNOT_MAKE,		EACCES    },  // 82
+	{ERROR_FAIL_I24,		EACCES    },  // 83
+	{ERROR_INVALID_PARAMETER,	EINVAL    },  // 87
+	{ERROR_NO_PROC_SLOTS,		EAGAIN    },  // 89
+	{ERROR_DRIVE_LOCKED,		EACCES    },  // 108
+	{ERROR_BROKEN_PIPE,		EPIPE     },  // 109
 	{ERROR_OPEN_FAILED,		EIO       },  // 110
 	{ERROR_BUFFER_OVERFLOW,		ENAMETOOLONG}, // 111
-        {ERROR_DISK_FULL,		ENOSPC    },  // 112
-        {ERROR_INVALID_TARGET_HANDLE,	EBADF     },  // 114
+	{ERROR_DISK_FULL,		ENOSPC    },  // 112
+	{ERROR_INVALID_TARGET_HANDLE,	EBADF     },  // 114
 	{ERROR_CALL_NOT_IMPLEMENTED,	ENOSYS    },  // 120
-        {ERROR_INVALID_HANDLE,		EINVAL    },  // 124
-        {ERROR_WAIT_NO_CHILDREN,	ECHILD    },  // 128
-        {ERROR_CHILD_NOT_COMPLETE,	ECHILD    },  // 129
-        {ERROR_DIRECT_ACCESS_HANDLE,	EBADF     },  // 130
-        {ERROR_NEGATIVE_SEEK,		EINVAL    },  // 131
-        {ERROR_SEEK_ON_DEVICE,		EACCES    },  // 132
-        {ERROR_DIR_NOT_EMPTY,		ENOTEMPTY },  // 145
-        {ERROR_NOT_LOCKED,		EACCES    },  // 158
-        {ERROR_BAD_PATHNAME,		ENOENT    },  // 161
-        {ERROR_MAX_THRDS_REACHED,	EAGAIN    },  // 164
-        {ERROR_LOCK_FAILED,		EACCES    },  // 167
-        {ERROR_ALREADY_EXISTS,		EEXIST    },  // 183
-        {ERROR_FILENAME_EXCED_RANGE,	ENOENT    },  // 206
-        {ERROR_NESTING_NOT_ALLOWED,	EAGAIN    },  // 215
+	{ERROR_INVALID_LEVEL,		EINVAL    },  // 124
+	{ERROR_WAIT_NO_CHILDREN,	ECHILD    },  // 128
+	{ERROR_CHILD_NOT_COMPLETE,	ECHILD    },  // 129
+	{ERROR_DIRECT_ACCESS_HANDLE,	EBADF     },  // 130
+	{ERROR_NEGATIVE_SEEK,		EINVAL    },  // 131
+	{ERROR_SEEK_ON_DEVICE,		EACCES    },  // 132
+	{ERROR_DIR_NOT_EMPTY,		ENOTEMPTY },  // 145
+	{ERROR_NOT_LOCKED,		EACCES    },  // 158
+	{ERROR_BAD_PATHNAME,		ENOENT    },  // 161
+	{ERROR_MAX_THRDS_REACHED,	EAGAIN    },  // 164
+	{ERROR_LOCK_FAILED,		EACCES    },  // 167
+	{ERROR_ALREADY_EXISTS,		EEXIST    },  // 183
+	{ERROR_FILENAME_EXCED_RANGE,	ENOENT    },  // 206
+	{ERROR_NESTING_NOT_ALLOWED,	EAGAIN    },  // 215
 	{ERROR_EXE_MACHINE_TYPE_MISMATCH, ENOEXEC },  // 216
 #ifdef ERROR_IMAGE_SUBSYSTEM_NOT_PRESENT
 	// ERROR_IMAGE_SUBSYSTEM_NOT_PRESENT is not defined in MinGW-w64 4.0.6.
@@ -92,7 +92,8 @@ static const errmap w32_to_posix[] = {
 	{ERROR_DISK_RESOURCES_EXHAUSTED, ENOSPC   },  // 314
 #endif
 	{ERROR_INVALID_ADDRESS,		EFAULT    },  // 487
-        {ERROR_NOT_ENOUGH_QUOTA,	ENOMEM    }   // 1816
+	{ERROR_IO_DEVICE,		EIO       },  // 1117
+	{ERROR_NOT_ENOUGH_QUOTA,	ENOMEM    }   // 1816
 };
 
 /* The following two constants must be the minimum and maximum
@@ -122,7 +123,7 @@ static const errmap w32_to_posix[] = {
 static int RP_C_API errmap_compar(const void *a, const void *b)
 {
 	const DWORD err1 = ((const errmap*)a)->w32;
-	const DWORD err2 = ((const errmap*)a)->w32;
+	const DWORD err2 = ((const errmap*)b)->w32;
 	if (err1 < err2) return -1;
 	if (err1 > err2) return 1;
 	return 0;