146f50c9a63d4b03ecb86ca29b790050cf58ec76
David Korth <gerbilsoft@gerbilsoft.com>
2019-08-16 22:50:17 -0400

    [librptexture] Moved ValveVTF from libromdata to libtexture.
    TODO:
    - Add some way to display all of the not-common fields.
    - Implement mipmap().
    - Maybe add some way in rpcli to extract a particular mipmap?
    - FileFormatFactory class.

---
+++ src/librptexture/fileformat/ValveVTF.hpp
@@ -0,0 +1,75 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librptexture)                     *
+ * ValveVTF.hpp: Valve VTF texture reader.                                 *
+ *                                                                         *
+ * Copyright (c) 2017-2019 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_VALVEVTF_HPP__
+#define __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_VALVEVTF_HPP__
+
+#include "FileFormat.hpp"
+
+namespace LibRpBase {
+	class IRpFile;
+}
+
+namespace LibRpTexture {
+
+class ValveVTFPrivate;
+class ValveVTF : public FileFormat
+{
+	public:
+		explicit ValveVTF(LibRpBase::IRpFile *file);
+
+	private:
+		typedef FileFormat super;
+		RP_DISABLE_COPY(ValveVTF)
+	protected:
+		friend class ValveVTFPrivate;
+
+	public:
+		/** Propety accessors **/
+
+		/**
+		 * Get the texture format name.
+		 * @return Texture format name, or nullptr on error.
+		 */
+		const char *textureFormatName(void) const final;
+
+		/**
+		 * Get the pixel format, e.g. "RGB888" or "DXT1".
+		 * @return Pixel format, or nullptr if unavailable.
+		 */
+		const char *pixelFormat(void) const final;
+
+		/**
+		 * Get the mipmap count.
+		 * @return Number of mipmaps. (0 if none; -1 if format doesn't support mipmaps)
+		 */
+		int mipmapCount(void) const final;
+
+	public:
+		/** Image accessors **/
+
+		/**
+		 * Get the image.
+		 * For textures with mipmaps, this is the largest mipmap.
+		 * The image is owned by this object.
+		 * @return Image, or nullptr on error.
+		 */
+		const rp_image *image(void) const final;
+
+		/**
+		 * Get the image for the specified mipmap.
+		 * Mipmap 0 is the largest image.
+		 * @param num Mipmap number.
+		 * @return Image, or nullptr on error.
+		 */
+		const rp_image *mipmap(int num) const final;
+};
+
+}
+
+#endif /* __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_VALVEVTF_HPP__ */
--- src/librptexture/fileformat/vtf_structs.h
+++ src/librptexture/fileformat/vtf_structs.h
@@ -1,13 +1,13 @@
 /***************************************************************************
- * ROM Properties Page shell extension. (libromdata)                       *
+ * ROM Properties Page shell extension. (librptexture)                     *
  * vtf_structs.h: Valve VTF texture format data structures.                *
  *                                                                         *
  * Copyright (c) 2017 by David Korth.                                      *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
-#ifndef __ROMPROPERTIES_LIBROMDATA_VTF_STRUCTS_H__
-#define __ROMPROPERTIES_LIBROMDATA_VTF_STRUCTS_H__
+#ifndef __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_VTF_STRUCTS_H__
+#define __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_VTF_STRUCTS_H__
 
 #include "librpbase/common.h"
 #include <stdint.h>
@@ -104,4 +104,4 @@ typedef enum {
 }
 #endif
 
-#endif /* __ROMPROPERTIES_LIBROMDATA_VTF_STRUCTS_H__ */
+#endif /* __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_VTF_STRUCTS_H__ */
