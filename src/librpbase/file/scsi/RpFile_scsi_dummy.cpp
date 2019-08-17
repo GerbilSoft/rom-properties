176b8c54c88f1f604a8ea4d74e06477cbce98417
David Korth <gerbilsoft@gerbilsoft.com>
2019-08-17 11:43:03 -0400

    [librpbase] RpFile_scsi_dummy.cpp: Dummy implementation.
    This is needed in order to prevent linker errors if a SCSI implementation
    isn't available for the current OS.
    
    Specifically, for RpFile::rereadDeviceSizeOS().

---
+++ src/librpbase/file/scsi/RpFile_scsi_dummy.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librpbase)                        *
+ * RpFile_scsi_dummy.cpp: Dummy implementation.                            *
+ *                                                                         *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "../RpFile.hpp"
+#include "../RpFile_p.hpp"
+
+namespace LibRpBase {
+
+/**
+ * Re-read device size using the native OS API.
+ * @param pDeviceSize	[out,opt] If not NULL, retrieves the device size, in bytes.
+ * @param pSectorSize	[out,opt] If not NULL, retrieves the sector size, in bytes.
+ * @return 0 on success, negative for POSIX error code.
+ */
+int RpFile::rereadDeviceSizeOS(int64_t *pDeviceSize, uint32_t *pSectorSize)
+{
+	// Not supported on this OS.
+	RP_UNUSED(pDeviceSize);
+	RP_UNUSED(pSectorSize);
+	return -ENOSYS;
+}
+
+}
