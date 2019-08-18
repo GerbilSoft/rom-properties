a9309708ed7418f4894e9a60c87e0668277fd719
David Korth <gerbilsoft@gerbilsoft.com>
2019-08-18 01:52:09 -0400

    [rpcli] device.cpp: Decode several of the numeric values from SCSI INQUIRY.
    TODO: Add a check for supported disc types for CD/DVD/BD-ROM?
    That's a bit more than SCSI INQUIRY, though...

--- src/rpcli/device.cpp
+++ src/rpcli/device.cpp
@@ -65,10 +65,70 @@ std::ostream& operator<<(std::ostream& os, const ScsiInquiry& si)
 	// TODO: i18n?
 	StreamStateSaver state(os);
 	os << "-- SCSI INQUIRY data for: " << si.file->filename() << endl;
-	os << "Peripheral device type: " << rp_sprintf("%02X", (resp.PeripheralDeviceType & 0x1F)) << endl;
-	os << "Peripheral qualifier:   " << rp_sprintf("%02X", (resp.PeripheralDeviceType >> 5)) << endl;
+
+	static const char *const pdt_tbl[0x20] = {
+		"Direct-access block device",		// 0x00
+		"Sequential-access device",		// 0x01
+		"Printer",				// 0x02
+		"Processor",				// 0x03
+		"Write-once media",			// 0x04
+		"CD/DVD/BD-ROM",			// 0x05
+		"Scanner",				// 0x06
+		"Optical memory device",		// 0x07
+		"Medium changer",			// 0x08
+		"Communications device",		// 0x09
+		nullptr,				// 0x0A
+		nullptr,				// 0x0B
+		"Storage array controller device",	// 0x0C
+		"Enclosure services device",		// 0x0D
+		"Simplified direct-access device",	// 0x0E
+		"Optical card reader/writer",		// 0x0F
+		"Bridge controller",			// 0x10
+		"Object-based storage device",		// 0x11
+		"Automation/Drive interface",		// 0x12
+		"Security manager device",		// 0x13
+		"Simplified MMC device",		// 0x14
+		nullptr, nullptr, nullptr,		// 0x15-0x17
+		nullptr, nullptr, nullptr,		// 0x18-0x1A
+		nullptr, nullptr, nullptr,		// 0x1B-0x1D
+		"Well-known logical unit",		// 0x1E
+		"Unknown or no device type",		// 0x1F
+	};
+	os << "Peripheral device type: ";
+	const char *const pdt = pdt_tbl[resp.PeripheralDeviceType & 0x1F];
+	os << (pdt ? pdt : rp_sprintf("0x%02X", resp.PeripheralDeviceType & 0x1F)) << endl;
+
+	os << "Peripheral qualifier:   ";
+	static const char *const pq_tbl[8] = {
+		"Connected",		// 000b
+		"Not connected",	// 001b
+		"010b",			// 010b
+		"Not supported",	// 011b
+		"100b", "101b",		// 100b,101b
+		"110b", "111b",		// 110b,111b
+	};
+	os << pq_tbl[resp.PeripheralDeviceType >> 5] << endl;
+
 	os << "Removable media:        " << (resp.RMB_DeviceTypeModifier & 0x80 ? "Yes" : "No") << endl;
-	os << "Version:                " << rp_sprintf("%02X", resp.Version) << endl;
+
+	os << "SCSI version:           ";
+	if (resp.Version <= 0x07) {
+		static const char *const ver_tbl[8] = {
+			"Any",		// 0x00
+			"SCSI-1",	// 0x01
+			"SCSI-2",	// 0x02
+			"SPC",		// 0x03
+			"SPC-2",	// 0x04
+			"SPC-3",	// 0x05
+			"SPC-4",	// 0x06
+			"SPC-5",	// 0x07
+		};
+		os << ver_tbl[resp.Version];
+	} else {
+		os << rp_sprintf("0x%02X", resp.Version);
+	}
+	os << endl;
+
 	// TODO: ResponseDataFormat and high bits?
 	// TODO: Verify AdditionalLength field?
 	// TODO: Flags.
@@ -77,5 +137,8 @@ std::ostream& operator<<(std::ostream& os, const ScsiInquiry& si)
 	os << "Product ID:             " << latin1_to_utf8(resp.product_id, sizeof(resp.product_id)) << endl;
 	os << "Firmware version:       " << latin1_to_utf8(resp.product_revision_level, sizeof(resp.product_revision_level)) << endl;
 	os << "Vendor notes:           " << latin1_to_utf8(resp.VendorSpecific, sizeof(resp.VendorSpecific)) << endl;
+
+	// TODO: Check supported media types for CD/DVD/BD-ROM drives?
+	// That's a bit more than an INQUIRY command...
 	return os;
 }
