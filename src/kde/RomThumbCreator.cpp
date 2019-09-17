GerbilSoft/rom-properties

commit 7ee118465034349de1846efc80ec5a1df815048d

[libromdata] IDiscReader: Moved m_file / m_discReader to the base class.
Every subclass has one or the other, and NCCHReader might have
either one, so move it to the common base class so we can
de-virtualize some functions.

IDiscReader's IRpFile* constructor now takes a reference to the
file and unreferences it in the destructor.

IDiscReader's IDiscReader* constructor doesn't take any reference,
since IDiscReader doesn't support references right now.
- TODO: Add references?

IDiscReader::isDevice() is now a standard function.

GcnPartitionPrivate: Moved offsetShift to reduce padding.

Code size reduction for `rpcli`: (64-bit Gentoo Linux, gcc-8.2.0-r6, release build)

   text    data     bss     dec     hex filename
1049769   98377   17456 1165602  11c922 bin/rpcli [before]
1048285   98305   17456 1164046  11c30e bin/rpcli [after]
  -1484     -72       0   -1556    -614 Difference

src/libromdata/disc/GdiReader.cpp
@@ -58,7 +58,7 @@ namespace LibRomData {
 
 class GdiReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		GdiReaderPrivate(GdiReader *q, IRpFile *file);
+		GdiReaderPrivate(GdiReader *q);
 		virtual ~GdiReaderPrivate();
 
 	private:
@@ -116,24 +116,24 @@ class GdiReaderPrivate : public SparseDiscReaderPrivate {
 
 /** GdiReaderPrivate **/
 
-GdiReaderPrivate::GdiReaderPrivate(GdiReader *q, IRpFile *file)
-	: super(q, file)
+GdiReaderPrivate::GdiReaderPrivate(GdiReader *q)
+	: super(q)
 	, blockCount(0)
 {
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Save the filename for later.
-	filename = this->file->filename();
+	filename = q->m_file->filename();
 
 	// GDI file should be 4k or less.
-	int64_t fileSize = file->size();
+	int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0 || fileSize > 4096) {
 		// Invalid GDI file size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -141,12 +141,12 @@ GdiReaderPrivate::GdiReaderPrivate(GdiReader *q, IRpFile *file)
 	// Read the GDI and parse the track information.
 	const unsigned int gdisize = static_cast<unsigned int>(fileSize);
 	unique_ptr<char[]> gdibuf(new char[gdisize+1]);
-	file->rewind();
-	size_t size = file->read(gdibuf.get(), gdisize);
+	q->m_file->rewind();
+	size_t size = q->m_file->read(gdibuf.get(), gdisize);
 	if (size != gdisize) {
 		// Read error.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -233,9 +233,10 @@ void GdiReaderPrivate::close(void)
 	trackMappings.clear();
 
 	// GDI file.
-	if (this->file) {
-		this->file->unref();
-		this->file = nullptr;
+	RP_Q(GdiReader);
+	if (q->m_file) {
+		q->m_file->unref();
+		q->m_file = nullptr;
 	}
 }
 
@@ -404,17 +405,18 @@ int GdiReaderPrivate::openTrack(int trackNumber)
 	}
 
 	// File opened. Get its size and calculate the end block.
-	int64_t fileSize = file->size();
+	RP_Q(GdiReader);
+	const int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0) {
 		// Empty or invalid flie...
-		this->file->unref();
+		q->m_file->unref();
 		return -EIO;
 	}
 
 	// Is the file a multiple of the sector size?
 	if (fileSize % blockRange->sectorSize != 0) {
 		// Not a multiple of the sector size.
-		this->file->unref();
+		q->m_file->unref();
 		return -EIO;
 	}
 
@@ -427,7 +429,7 @@ int GdiReaderPrivate::openTrack(int trackNumber)
 /** GdiReader **/
 
 GdiReader::GdiReader(IRpFile *file)
-	: super(new GdiReaderPrivate(this, file))
+	: super(new GdiReaderPrivate(this), file)
 { }
 
 /**

src/librpbase/disc/SparseDiscReader.cpp
@@ -33,34 +33,26 @@ namespace LibRpBase {
 
 /** SparseDiscReaderPrivate **/
 
-SparseDiscReaderPrivate::SparseDiscReaderPrivate(SparseDiscReader *q, IRpFile *file)
+SparseDiscReaderPrivate::SparseDiscReaderPrivate(SparseDiscReader *q)
 	: q_ptr(q)
-	, file(nullptr)
 	, disc_size(0)
 	, pos(-1)
 	, block_size(0)
 {
-	if (!file) {
+	if (!q->m_file) {
 		q->m_lastError = EBADF;
 		return;
 	}
-	this->file = file->ref();
 
 	// disc_size, pos, and block_size must be
 	// set by the subclass.
 }
 
-SparseDiscReaderPrivate::~SparseDiscReaderPrivate()
-{
-	if (this->file) {
-		this->file->unref();
-	}
-}
-
 /** SparseDiscReader **/
 
-SparseDiscReader::SparseDiscReader(SparseDiscReaderPrivate *d)
-	: d_ptr(d)
+SparseDiscReader::SparseDiscReader(SparseDiscReaderPrivate *d, IRpFile *file)
+	: super(file)
+	, d_ptr(d)
 { }
 
 SparseDiscReader::~SparseDiscReader()
@@ -77,8 +69,7 @@ SparseDiscReader::~SparseDiscReader()
  */
 bool SparseDiscReader::isOpen(void) const
 {
-	RP_D(const SparseDiscReader);
-	return (d->file != nullptr);
+	return (m_file != nullptr);
 }
 
 /**
@@ -90,11 +81,11 @@ bool SparseDiscReader::isOpen(void) const
 size_t SparseDiscReader::read(void *ptr, size_t size)
 {
 	RP_D(SparseDiscReader);
-	assert(d->file != nullptr);
+	assert(m_file != nullptr);
 	assert(d->disc_size > 0);
 	assert(d->pos >= 0);
 	assert(d->block_size != 0);
-	if (!d->file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
+	if (!m_file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
 		// Disc image wasn't initialized properly.
 		m_lastError = EBADF;
 		return -1;
@@ -183,11 +174,11 @@ size_t SparseDiscReader::read(void *ptr, size_t size)
 int SparseDiscReader::seek(int64_t pos)
 {
 	RP_D(SparseDiscReader);
-	assert(d->file != nullptr);
+	assert(m_file != nullptr);
 	assert(d->disc_size > 0);
 	assert(d->pos >= 0);
 	assert(d->block_size != 0);
-	if (!d->file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
+	if (!m_file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
 		// Disc image wasn't initialized properly.
 		m_lastError = EBADF;
 		return -1;
@@ -213,11 +204,11 @@ int SparseDiscReader::seek(int64_t pos)
 int64_t SparseDiscReader::tell(void)
 {
 	RP_D(const SparseDiscReader);
-	assert(d->file != nullptr);
+	assert(m_file != nullptr);
 	assert(d->disc_size > 0);
 	assert(d->pos >= 0);
 	assert(d->block_size != 0);
-	if (!d->file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
+	if (!m_file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
 		// Disc image wasn't initialized properly.
 		m_lastError = EBADF;
 		return -1;
@@ -233,29 +224,17 @@ int64_t SparseDiscReader::tell(void)
 int64_t SparseDiscReader::size(void)
 {
 	RP_D(const SparseDiscReader);
-	assert(d->file != nullptr);
+	assert(m_file != nullptr);
 	assert(d->disc_size > 0);
 	assert(d->pos >= 0);
 	assert(d->block_size != 0);
-	if (!d->file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
+	if (!m_file || d->disc_size <= 0 || d->pos < 0 || d->block_size == 0) {
 		// Disc image wasn't initialized properly.
 		m_lastError = EBADF;
 		return -1;
 	}
 
 	return d->disc_size;
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool SparseDiscReader::isDevice(void) const
-{
-	RP_D(const SparseDiscReader);
-	return (d->file && d->file->isDevice());
 }
 
 /** SparseDiscReader **/
@@ -307,8 +286,8 @@ int SparseDiscReader::readBlock(uint32_t blockIdx, void *ptr, int pos, size_t si
 	}
 
 	// Read from the block.
-	size_t sz_read = d->file->seekAndRead(physBlockAddr + pos, ptr, size);
-	m_lastError = d->file->lastError();
+	size_t sz_read = m_file->seekAndRead(physBlockAddr + pos, ptr, size);
+	m_lastError = m_file->lastError();
 	return (sz_read > 0 ? (int)sz_read : -1);
 }
 

src/libromdata/disc/CIAReader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * CIAReader.cpp: Nintendo 3DS CIA reader.                                 *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -43,7 +43,7 @@ namespace LibRomData {
 class CIAReaderPrivate
 {
 	public:
-		CIAReaderPrivate(CIAReader *q, LibRpBase::IRpFile *file,
+		CIAReaderPrivate(CIAReader *q,
 			int64_t content_offset, uint32_t content_length,
 			const N3DS_Ticket_t *ticket,
 			uint16_t tmd_content_index);
@@ -55,7 +55,6 @@ class CIAReaderPrivate
 		CIAReader *const q_ptr;
 
 	public:
-		LibRpBase::IRpFile *file;	// 3DS CIA file.
 		CBCReader *cbcReader;		// CBC reader.
 
 #ifdef ENABLE_DECRYPTION
@@ -68,11 +67,10 @@ class CIAReaderPrivate
 
 /** CIAReaderPrivate **/
 
-CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
+CIAReaderPrivate::CIAReaderPrivate(CIAReader *q,
 	int64_t content_offset, uint32_t content_length,
 	const N3DS_Ticket_t *ticket, uint16_t tmd_content_index)
 	: q_ptr(q)
-	, file(file)
 	, cbcReader(nullptr)
 #ifdef ENABLE_DECRYPTION
 	, titleKeyEncIdx(0)
@@ -83,11 +81,17 @@ CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
 	RP_UNUSED(tmd_content_index);
 #endif /* ENABLE_DECRYPTION */
 
+	assert(q->m_file != nullptr);
+	if (!q->m_file) {
+		// No file...
+		return;
+	}
+
 	assert(ticket != nullptr);
 	if (!ticket) {
 		// No ticket. Assuming no encryption.
 		// Create a passthru CBCReader anyway.
-		cbcReader = new CBCReader(file, content_offset, content_length, nullptr, nullptr);
+		cbcReader = new CBCReader(q->m_file, content_offset, content_length, nullptr, nullptr);
 		return;
 	}
 
@@ -175,17 +179,19 @@ CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
 		memset(&cia_iv.u8[2], 0, sizeof(cia_iv.u8)-2);
 
 		// Create a CBC reader to decrypt the CIA.
-		cbcReader = new CBCReader(file, content_offset, content_length, title_key, cia_iv.u8);
+		cbcReader = new CBCReader(q->m_file, content_offset, content_length, title_key, cia_iv.u8);
 	} else {
 		// Unable to get the CIA encryption keys.
 		// TODO: Set an error.
 		//verifyResult = res;
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 	}
 #else /* !ENABLE_DECRYPTION */
 	// Cannot decrypt the CIA.
 	// TODO: Set an error.
-	this->file = nullptr;
+	q->m_file->unref();
+	q->m_file = nullptr;
 #endif /* ENABLE_DECRYPTION */
 }
 
@@ -212,7 +218,8 @@ CIAReader::CIAReader(IRpFile *file,
 		int64_t content_offset, uint32_t content_length,
 		const N3DS_Ticket_t *ticket,
 		uint16_t tmd_content_index)
-	: d_ptr(new CIAReaderPrivate(this, file, content_offset, content_length, ticket, tmd_content_index))
+	: super(file)
+	, d_ptr(new CIAReaderPrivate(this, content_offset, content_length, ticket, tmd_content_index))
 { }
 
 CIAReader::~CIAReader()
@@ -229,8 +236,7 @@ CIAReader::~CIAReader()
  */
 bool CIAReader::isOpen(void) const
 {
-	RP_D(const CIAReader);
-	return (d->file && d->file->isOpen());
+	return (m_file && m_file->isOpen());
 }
 
 /**
@@ -243,13 +249,13 @@ size_t CIAReader::read(void *ptr, size_t size)
 {
 	RP_D(CIAReader);
 	assert(ptr != nullptr);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
 	if (!ptr) {
 		m_lastError = EINVAL;
 		return 0;
-	} else if (!d->file || !d->file->isOpen() || !d->cbcReader) {
+	} else if (!m_file || !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return 0;
 	} else if (size == 0) {
@@ -270,10 +276,10 @@ size_t CIAReader::read(void *ptr, size_t size)
 int CIAReader::seek(int64_t pos)
 {
 	RP_D(CIAReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -292,9 +298,9 @@ int CIAReader::seek(int64_t pos)
 int64_t CIAReader::tell(void)
 {
 	RP_D(const CIAReader);
-	assert(d->file != nullptr);
+	assert(m_file != nullptr);
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -313,10 +319,10 @@ int64_t CIAReader::tell(void)
 int64_t CIAReader::size(void)
 {
 	RP_D(const CIAReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -324,18 +330,6 @@ int64_t CIAReader::size(void)
 	int64_t ret = d->cbcReader->size();
 	m_lastError = d->cbcReader->lastError();
 	return ret;
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool CIAReader::isDevice(void) const
-{
-	RP_D(const CIAReader);
-	return (d->file && d->file->isDevice());
 }
 
 /** IPartition **/

src/libromdata/disc/CIAReader.hpp
@@ -102,15 +102,6 @@ class CIAReader : public LibRpBase::IPartition
 		int64_t size(void) final;
 
 	public:
-		/** Device file functions **/
-
-		/**
-		 * Is the underlying file a device file?
-		 * @return True if the underlying file is a device file; false if not.
-		 */
-		bool isDevice(void) const final;
-
-	public:
 		/** IPartition **/
 
 		/**

src/libromdata/disc/Cdrom2352Reader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file);
+		Cdrom2352ReaderPrivate(Cdrom2352Reader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -68,22 +68,22 @@ class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 const uint8_t Cdrom2352ReaderPrivate::CDROM_2352_MAGIC[12] =
 	{0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00};
 
-Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file)
-	: super(q, file)
+Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q)
+	: super(q)
 	, blockCount(0)
 {
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Check the disc size.
 	// Should be a multiple of 2352.
-	int64_t fileSize = file->size();
+	int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0 || fileSize % 2352 != 0) {
 		// Invalid disc size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -101,7 +101,7 @@ Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file
 /** Cdrom2352Reader **/
 
 Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
-	: super(new Cdrom2352ReaderPrivate(this, file))
+	: super(new Cdrom2352ReaderPrivate(this), file)
 { }
 
 /**

src/libromdata/disc/Cdrom2352Reader.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * Cdrom2352Reader.hpp: CD-ROM reader for 2352-byte sector images.         *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *

src/libromdata/disc/CisoGcnReader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file);
+		CisoGcnReaderPrivate(CisoGcnReader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -72,8 +72,8 @@ class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 // CISO magic.
 const char CisoGcnReaderPrivate::CISO_MAGIC[4] = {'C','I','S','O'};
 
-CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
-	: super(q, file)
+CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
+	: super(q)
 	, maxLogicalBlockUsed(-1)
 {
 	// Clear the CISO header struct.
@@ -81,7 +81,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Clear the CISO block map initially.
 	blockMap.fill(0xFFFF);
 
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
@@ -89,12 +89,12 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Read the CISO header.
 	static_assert(sizeof(CISOHeader) == CISO_HEADER_SIZE,
 		"CISOHeader is the wrong size. (Should be 32,768 bytes.)");
-	this->file->rewind();
-	size_t sz = this->file->read(&cisoHeader, sizeof(cisoHeader));
+	q->m_file->rewind();
+	size_t sz = q->m_file->read(&cisoHeader, sizeof(cisoHeader));
 	if (sz != sizeof(cisoHeader)) {
 		// Error reading the CISO header.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -102,8 +102,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Verify the CISO header.
 	if (memcmp(cisoHeader.magic, CISO_MAGIC, sizeof(CISO_MAGIC)) != 0) {
 		// Invalid magic.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -117,8 +117,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 		// If the block size is 0x18, then this is
 		// actually a PSP CISO, and this field is
 		// the CISO header size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -138,8 +138,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 				break;
 			default:
 				// Invalid entry.
-				this->file->unref();
-				this->file = nullptr;
+				q->m_file->unref();
+				q->m_file = nullptr;
 				q->m_lastError = EIO;
 				return;
 		}
@@ -155,7 +155,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 /** CisoGcnReader **/
 
 CisoGcnReader::CisoGcnReader(IRpFile *file)
-	: super(new CisoGcnReaderPrivate(this, file))
+	: super(new CisoGcnReaderPrivate(this), file)
 { }
 
 /**

src/libromdata/disc/GcnPartition.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * GcnPartition.cpp: GameCube partition reader.                            *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -53,7 +53,8 @@ namespace LibRomData {
  * @param partition_offset Partition start offset.
  */
 GcnPartition::GcnPartition(IDiscReader *discReader, int64_t partition_offset)
-	: d_ptr(new GcnPartitionPrivate(this, discReader, partition_offset))
+	: super(discReader)
+	, d_ptr(new GcnPartitionPrivate(this, partition_offset))
 { }
 
 GcnPartition::~GcnPartition()
@@ -64,9 +65,11 @@ GcnPartition::~GcnPartition()
 /**
  * Construct a GcnPartition. (subclass version)
  * @param d GcnPartitionPrivate subclass.
+ * @param discReader IDiscReader.
  */
-GcnPartition::GcnPartition(GcnPartitionPrivate *d)
-	: d_ptr(d)
+GcnPartition::GcnPartition(GcnPartitionPrivate *d, IDiscReader *discReader)
+	: super(discReader)
+	, d_ptr(d)
 { }
 
 /** IDiscReader **/
@@ -78,8 +81,7 @@ GcnPartition::GcnPartition(GcnPartitionPrivate *d)
  */
 bool GcnPartition::isOpen(void) const
 {
-	RP_D(const GcnPartition);
-	return (d->discReader && d->discReader->isOpen());
+	return (m_discReader && m_discReader->isOpen());
 }
 
 /**
@@ -90,17 +92,16 @@ bool GcnPartition::isOpen(void) const
  */
 size_t GcnPartition::read(void *ptr, size_t size)
 {
-	RP_D(GcnPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader || !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader || !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return 0;
 	}
 
 	// GCN partitions are stored as-is.
 	// TODO: data_size checks?
-	return d->discReader->read(ptr, size);
+	return m_discReader->read(ptr, size);
 }
 
 /**
@@ -111,17 +112,17 @@ size_t GcnPartition::read(void *ptr, size_t size)
 int GcnPartition::seek(int64_t pos)
 {
 	RP_D(GcnPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader ||  !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader ||  !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
 
 	// Use the IDiscReader directly for GCN partitions.
-	int ret = d->discReader->seek(d->data_offset + pos);
+	int ret = m_discReader->seek(d->data_offset + pos);
 	if (ret != 0) {
-		m_lastError = d->discReader->lastError();
+		m_lastError = m_discReader->lastError();
 	}
 	return ret;
 }
@@ -132,18 +133,17 @@ int GcnPartition::seek(int64_t pos)
  */
 int64_t GcnPartition::tell(void)
 {
-	RP_D(GcnPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader ||  !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader || !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
 
 	// Use the IDiscReader directly for GCN partitions.
-	int64_t ret = d->discReader->tell();
+	int64_t ret = m_discReader->tell();
 	if (ret < 0) {
-		m_lastError = d->discReader->lastError();
+		m_lastError = m_discReader->lastError();
 	}
 	return ret;
 }
@@ -159,18 +159,6 @@ int64_t GcnPartition::size(void)
 	// TODO: Errors?
 	RP_D(const GcnPartition);
 	return d->data_size;
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool GcnPartition::isDevice(void) const
-{
-	RP_D(const GcnPartition);
-	return (d->discReader && d->discReader->isDevice());
 }
 
 /** IPartition **/

commit 3ad5cf7a7a642e549d18ad47754085955c2f5d79

[libromdata] GdiReaderPrivate::openTrack(): Fix confusion between `file` and `q->m_file`.
`q->m_file` is the GDI cuesheet.
`file` is the track file.

This confusion caused two problems:

1. We ended up unreferencing `q->m_file` too many times, which resulted
   in crashes on Windows. On Linux, this didn't seem to cause any issues
   other than GDI files not being thumbnailed correctly.

2. Since we were checking the cuesheet filesize, it would almost always
   fail the sector size check.

This bug was reported by @HyperPolygon64.

This fixes a regression from commit 7ee118465034349de1846efc80ec5a1df815048d.
([libromdata] IDiscReader: Moved m_file / m_discReader to the base class.)

src/kde/RomThumbCreator.cpp
@@ -320,7 +320,7 @@ Q_DECL_EXPORT int rp_create_thumbnail(const char *source_file, const char *outpu
 	// Attempt to open the ROM file.
 	// TODO: RpQFile wrapper.
 	// For now, using RpFile, which is an stdio wrapper.
-	IRpFile *const file = new RpFile(source_file, RpFile::FM_OPEN_READ_GZ);
+	RpFile *const file = new RpFile(source_file, RpFile::FM_OPEN_READ_GZ);
 	if (!file->isOpen()) {
 		// Could not open the file.
 		file->unref();

src/libromdata/disc/GdiReader.cpp
@@ -301,17 +301,17 @@ int GdiReaderPrivate::openTrack(int trackNumber)
 
 	// File opened. Get its size and calculate the end block.
 	RP_Q(GdiReader);
-	const int64_t fileSize = q->m_file->size();
+	const int64_t fileSize = file->size();
 	if (fileSize <= 0) {
 		// Empty or invalid flie...
-		q->m_file->unref();
+		file->unref();
 		return -EIO;
 	}
 
 	// Is the file a multiple of the sector size?
 	if (fileSize % blockRange->sectorSize != 0) {
 		// Not a multiple of the sector size.
-		q->m_file->unref();
+		file->unref();
 		return -EIO;
 	}
 

src/librpbase/disc/SparseDiscReader.cpp
@@ -27,10 +27,7 @@ SparseDiscReaderPrivate::SparseDiscReaderPrivate(SparseDiscReader *q)
 	, pos(-1)
 	, block_size(0)
 {
-	if (!q->m_file) {
-		q->m_lastError = EBADF;
-		return;
-	}
+	// NOTE: Can't check q->m_file here.
 
 	// disc_size, pos, and block_size must be
 	// set by the subclass.

src/win32/RP_ExtractIcon.cpp
@@ -122,7 +122,7 @@ IFACEMETHODIMP RP_ExtractIcon::Load(LPCOLESTR pszFileName, DWORD dwMode)
 	}
 
 	// Attempt to open the ROM file.
-	RpFile *file = new RpFile(d->filename, RpFile::FM_OPEN_READ_GZ);
+	RpFile *const file = new RpFile(d->filename, RpFile::FM_OPEN_READ_GZ);
 	if (!file->isOpen()) {
 		file->unref();
 		return E_FAIL;