GerbilSoft/rom-properties

commit 060d8c227ca4916b3aba013fe1facf362ad334fc

[libromdata] Use 32-bit comparisons instead of memcmp() where possible.
gcc-6.1 and later actually optimizes memcmp() on 4-byte and 8-byte values
to 32-bit and 64-bit compares, respectively. MSVC 2015+ (and possibly some
earlier versions) does this too, though its 64-bit code generation isn't
as optimal as it could be.

Switching to 32-bit compares in the source directly ensures that optimal
code is generated for all cases.

TODO: Make sure this doesn't break anything.

src/libromdata/disc/NCCHReader.cpp
@@ -121,18 +121,20 @@ void NCCHReaderPrivate::init(void)
 
 	// Verify the NCCH magic number.
 	RP_Q(NCCHReader);
-	if (memcmp(ncch_header.hdr.magic, N3DS_NCCH_HEADER_MAGIC, sizeof(ncch_header.hdr.magic)) != 0) {
+	if (ncch_header.hdr.magic != cpu_to_be32(N3DS_NCCH_HEADER_MAGIC)) {
 		// NCCH magic number is incorrect.
 		// Check for non-NCCH types.
-		const uint8_t *u8hdr = reinterpret_cast<const uint8_t*>(&ncch_header);
-		if (!memcmp(u8hdr, "NDHT", 4)) {
+		if (ncch_header.hdr.magic != cpu_to_be32('NDHT')) {
 			// NDHT. (DS Whitelist)
 			// 0004800F-484E4841
 			verifyResult = KeyManager::VERIFY_OK;
 			nonNcchContentType = NONCCH_NDHT;
 			this->file = nullptr;
 			return;
-		} else if (!memcmp(&u8hdr[0x80], "NARC", 4)) {
+		}
+
+		const uint32_t magic_narc = ((const uint32_t*)&ncch_header)[0x80/4];
+		if (magic_narc == cpu_to_be32('NARC')) {
 			// NARC. (TWL Version Data)
 			// 0004800F-484E4C41
 			verifyResult = KeyManager::VERIFY_OK;

src/kde/RomPropertiesDialogPlugin.cpp
@@ -64,6 +64,8 @@ RomPropertiesDialogPlugin::RomPropertiesDialogPlugin(KPropertiesDialog *props, c
 	// References:
 	// - https://bugs.kde.org/show_bug.cgi?id=392100
 	// - https://cgit.kde.org/kio.git/commit/?id=7d6e4965dfcd7fc12e8cba7b1506dde22de5d2dd
+	// TODO: https://cgit.kde.org/kdenetwork-filesharing.git/commit/?id=abf945afd4f08d80cdc53c650d80d300f245a73d
+	// (and other uses) [use mostLocalPath()]
 	const KFileItem &item = items.at(0);
 	QString filename = item.localPath();
 	if (filename.isEmpty())

src/libromdata/Audio/GBS.cpp
@@ -145,7 +145,7 @@ int GBS::isRomSupported_static(const DetectInfo *info)
 		reinterpret_cast<const GBS_Header*>(info->header.pData);
 
 	// Check the GBS magic number.
-	if (!memcmp(gbsHeader->magic, GBS_MAGIC, sizeof(gbsHeader->magic))) {
+	if (gbsHeader->magic == cpu_to_be32(GBS_MAGIC)) {
 		// Found the GBS magic number.
 		return 0;
 	}

src/libromdata/Audio/VGM.cpp
@@ -265,7 +265,7 @@ int VGM::isRomSupported_static(const DetectInfo *info)
 		reinterpret_cast<const VGM_Header*>(info->header.pData);
 
 	// Check the VGM magic number.
-	if (vgmHeader->magic == cpu_to_le32(VGM_MAGIC)) {
+	if (vgmHeader->magic == cpu_to_be32(VGM_MAGIC)) {
 		// Found the VGM magic number.
 		return 0;
 	}

src/libromdata/Audio/gbs_structs.h
@@ -35,11 +35,13 @@ extern "C" {
 
 /**
  * Game Boy Sound System.
- * All fields are little-endian.
+ *
+ * All fields are little-endian,
+ * except for the magic number.
  */
-#define GBS_MAGIC "GBS\x01"
+#define GBS_MAGIC 'GBS\x01'
 typedef struct PACKED _GBS_Header {
-	char magic[4];			// [0x000] "GBS\x01"
+	uint32_t magic;			// [0x000] 'GBS\x01' (big-endian)
 					//         NOTE: \x01 is technically a version number.
 	uint8_t track_count;		// [0x004] Number of tracks
 	uint8_t default_track;		// [0x005] Default track number, plus one. (usually 1)

src/libromdata/Console/GameCube.cpp
@@ -1218,7 +1218,8 @@ int GameCube::isRomSupported_static(const DetectInfo *info)
 	}
 
 	// Check for the magic numbers.
-	const GCN_DiscHeader *gcn_header = reinterpret_cast<const GCN_DiscHeader*>(info->header.pData);
+	const GCN_DiscHeader *gcn_header =
+		reinterpret_cast<const GCN_DiscHeader*>(info->header.pData);
 	if (gcn_header->magic_wii == cpu_to_be32(WII_MAGIC)) {
 		// Wii disc image.
 		return (GameCubePrivate::DISC_SYSTEM_WII | GameCubePrivate::DISC_FORMAT_RAW);
@@ -1237,22 +1238,25 @@ int GameCube::isRomSupported_static(const DetectInfo *info)
 	// Check for SDK headers.
 	// TODO: More comprehensive?
 	// TODO: Checksum at 0x0830. (For GCN, makeGCM always puts 0xAB0B here...)
-	static const uint8_t sdk_0x0000[4] = {0xFF,0xFF,0x00,0x00};
-	static const uint8_t sdk_0x082C[4] = {0x00,0x00,0xE0,0x06};
-	if (!memcmp(&info->header.pData[0x0000], sdk_0x0000, sizeof(sdk_0x0000))) {
+	static const uint32_t sdk_0x0000 = 0xFFFF0000;	// BE32
+	static const uint32_t sdk_0x082C = 0x0000E006;	// BE32
+	const uint32_t *const pData32 =
+		reinterpret_cast<const uint32_t*>(info->header.pData);
+	if (pData32[0] == cpu_to_be32(sdk_0x0000)) {
 		if (info->header.size < 0x0830) {
 			// Can't check 0x082C, so assume it has the SDK headers.
 			return (GameCubePrivate::DISC_SYSTEM_UNKNOWN | GameCubePrivate::DISC_FORMAT_SDK);
 		}
 
-		if (!memcmp(&info->header.pData[0x082C], sdk_0x082C, sizeof(sdk_0x082C))) {
+		if (pData32[0x082C/4] == cpu_to_be32(sdk_0x082C)) {
 			// This is a valid GCN/Wii SDK disc image header.
 			return (GameCubePrivate::DISC_SYSTEM_UNKNOWN | GameCubePrivate::DISC_FORMAT_SDK);
 		}
 	}
 
 	// Check for TGC.
-	const GCN_TGC_Header *const tgcHeader = reinterpret_cast<const GCN_TGC_Header*>(info->header.pData);
+	const GCN_TGC_Header *const tgcHeader =
+		reinterpret_cast<const GCN_TGC_Header*>(info->header.pData);
 	if (tgcHeader->tgc_magic == cpu_to_be32(TGC_MAGIC)) {
 		// TGC images have their own 32 KB header, so we can't
 		// check the actual GCN/Wii header here.
@@ -1291,8 +1295,8 @@ int GameCube::isRomSupported_static(const DetectInfo *info)
 	}
 
 	// Check for WIA.
-	static const uint8_t wia_magic[4] = {'W','I','A',1};
-	if (!memcmp(info->header.pData, wia_magic, sizeof(wia_magic))) {
+	static const uint32_t wia_magic = 'WIA\x01';
+	if (pData32[0] == cpu_to_be32(wia_magic)) {
 		// This is a WIA image.
 		// NOTE: We're using the WIA system ID if it's valid.
 		// Otherwise, fall back to GCN/Wii magic.
@@ -1309,14 +1313,12 @@ int GameCube::isRomSupported_static(const DetectInfo *info)
 
 		// Check the GameCube/Wii magic.
 		// TODO: WIA struct when full WIA support is added.
-		uint32_t magic_tmp;
-		memcpy(&magic_tmp, &info->header.pData[0x70], sizeof(magic_tmp));
-		if (magic_tmp == cpu_to_be32(WII_MAGIC)) {
+		// FIXME BEFORE COMMIT: TEST THIS
+		gcn_header = reinterpret_cast<const GCN_DiscHeader*>(&info->header.pData[0x58]);
+		if (gcn_header->magic_wii == cpu_to_be32(WII_MAGIC)) {
 			// Wii disc image. (WIA format)
 			return (GameCubePrivate::DISC_SYSTEM_WII | GameCubePrivate::DISC_FORMAT_WIA);
-		}
-		memcpy(&magic_tmp, &info->header.pData[0x74], sizeof(magic_tmp));
-		if (magic_tmp == cpu_to_be32(GCN_MAGIC)) {
+		} else if (gcn_header->magic_gcn == cpu_to_be32(GCN_MAGIC)) {
 			// GameCube disc image. (WIA format)
 			return (GameCubePrivate::DISC_SYSTEM_GCN | GameCubePrivate::DISC_FORMAT_WIA);
 		}

src/libromdata/Console/N64.cpp
@@ -200,23 +200,19 @@ int N64::isRomSupported_static(const DetectInfo *info)
 	// Check the magic number.
 	// NOTE: This technically isn't a "magic number",
 	// but it appears to be the same for all N64 ROMs.
-	static const uint8_t magic[4][8] = {
-		{0x80,0x37,0x12,0x40,0x00,0x00,0x00,0x0F},	// Z64
-		{0x37,0x80,0x40,0x12,0x00,0x00,0x0F,0x00},	// V64
-		{0x12,0x40,0x80,0x37,0x00,0x0F,0x00,0x00},	// swap2
-		{0x40,0x12,0x37,0x80,0x0F,0x00,0x00,0x00},	// le32
-	};
-
-	for (int i = 0; i < 4; i++) {
-		if (!memcmp(romHeader->magic, magic[i], sizeof(magic[i]))) {
-			// Found a matching magic number.
-			// This corresponds to N64Private::RomType.
-			return i;
-		}
+	// TODO: Check 32-bit code generation.
+	int romType = N64Private::ROM_TYPE_UNKNOWN;
+	if (romHeader->magic64 == cpu_to_be64(N64_Z64_MAGIC)) {
+		romType = N64Private::ROM_TYPE_Z64;
+	} else if (romHeader->magic64 == cpu_to_be64(N64_V64_MAGIC)) {
+		romType = N64Private::ROM_TYPE_V64;
+	} else if (romHeader->magic64 == cpu_to_be64(N64_SWAP2_MAGIC)) {
+		romType = N64Private::ROM_TYPE_SWAP2;
+	} else if (romHeader->magic64 == cpu_to_be64(N64_LE32_MAGIC)) {
+		romType = N64Private::ROM_TYPE_LE32;
 	}
 
-	// Not supported.
-	return -1;
+	return romType;
 }
 
 /**

src/libromdata/Console/NES.cpp
@@ -329,13 +329,12 @@ int NES::isRomSupported_static(const DetectInfo *info)
 	// Check for iNES.
 	const INES_RomHeader *inesHeader =
 		reinterpret_cast<const INES_RomHeader*>(info->header.pData);
-	static const uint8_t ines_magic[4] = {'N','E','S',0x1A};
-	if (!memcmp(inesHeader->magic, ines_magic, 3) &&
-	    (inesHeader->magic[3] == 0x1A || inesHeader->magic[3] == 0x00))
+	if (inesHeader->magic == cpu_to_be32(INES_MAGIC) ||
+	    inesHeader->magic == cpu_to_be32(INES_MAGIC_WIIU_VC))
 	{
 		// Found an iNES ROM header.
 		// If the fourth byte is 0x00, it's Wii U VC.
-		int romType = (inesHeader->magic[3] == 0x00)
+		int romType = (inesHeader->magic == cpu_to_be32(INES_MAGIC_WIIU_VC))
 				? NESPrivate::ROM_SPECIAL_WIIU_VC
 				: 0;
 
@@ -406,8 +405,7 @@ int NES::isRomSupported_static(const DetectInfo *info)
 	// Check for TNES.
 	const TNES_RomHeader *tnesHeader =
 		reinterpret_cast<const TNES_RomHeader*>(info->header.pData);
-	static const uint8_t tnes_magic[4] = {'T','N','E','S'};
-	if (!memcmp(tnesHeader->magic, tnes_magic, sizeof(tnesHeader->magic))) {
+	if (tnesHeader->magic == cpu_to_be32(TNES_MAGIC)) {
 		// Found a TNES ROM header.
 		if (tnesHeader->mapper == TNES_MAPPER_FDS) {
 			// This is an FDS game.
@@ -423,13 +421,12 @@ int NES::isRomSupported_static(const DetectInfo *info)
 
 	// Check for FDS.
 	// TODO: Check that the block code is 0x01?
-	static const uint8_t fwNES_magic[] = "FDS\x1A";
 	static const uint8_t fds_magic[] = "*NINTENDO-HVC*";
 
 	// Check for headered FDS.
 	const FDS_DiskHeader_fwNES *fwNESHeader =
 		reinterpret_cast<const FDS_DiskHeader_fwNES*>(info->header.pData);
-	if (!memcmp(fwNESHeader->magic, fwNES_magic, sizeof(fwNESHeader->magic)-1)) {
+	if (fwNESHeader->magic == cpu_to_be32(fwNES_MAGIC)) {
 		// fwNES header is present.
 		// TODO: Check required NULL bytes.
 		// For now, assume this is correct.

commit f0dc05496e587f1dfbc5896da8c97c20c9e78be6

[libromdata] NCCHReader.cpp: Fix incorrect NDHT comparison.
This caused misdetections of NCCH sections as NDHT in cases where the
keys configuration was missing, and it prevented NDHT from being
detected correctly.

This fixes a regression from commit 060d8c227ca4916b3aba013fe1facf362ad334fc.
([libromdata] Use 32-bit comparisons instead of memcmp() where possible.)

This bug is present in v1.4 through v1.4.3.

src/libromdata/disc/NCCHReader.cpp
@@ -75,7 +75,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 	if (ncch_header.hdr.magic != cpu_to_be32(N3DS_NCCH_HEADER_MAGIC)) {
 		// NCCH magic number is incorrect.
 		// Check for non-NCCH types.
-		if (ncch_header.hdr.magic != cpu_to_be32('NDHT')) {
+		if (ncch_header.hdr.magic == cpu_to_be32('NDHT')) {
 			// NDHT. (DS Whitelist)
 			// 0004800F-484E4841
 			verifyResult = KeyManager::VERIFY_OK;