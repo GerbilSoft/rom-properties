c16f773772930ea7fa5e3207abc1fd4c9bdcd6d4
David Korth <gerbilsoft@gerbilsoft.com>
2019-09-30 17:40:53 -0400

    [kde] DragImageLabel: Implemented drag & drop to PNG format.
    Supports banners, icons, and animated icons, in (A)PNG format.
    
    RpQByteArrayFile: IRpFile implementation using QByteArray. This is
    similar to RpMemFile, but it's writable, dynamically resizable, and
    can be directly passed to QMimeData::setData().

---
+++ src/kde/RpQByteArrayFile.cpp
@@ -0,0 +1,169 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librpbase)                        *
+ * RpQByteArrayFile.cpp: IRpFile implementation using a QByteArray.        *
+ *                                                                         *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "RpQByteArrayFile.hpp"
+
+// C includes. (C++ namespace)
+#include <cerrno>
+#include <cstring>
+
+// C++ includes.
+#include <string>
+using std::string;
+
+/**
+ * Open an IRpFile backed by a QByteArray.
+ * The resulting IRpFile is writable.
+ */
+RpQByteArrayFile::RpQByteArrayFile()
+	: super()
+	, m_pos(0)
+{
+	// Reserve at least 16 KB.
+	m_byteArray.reserve(16*1024);
+}
+
+/**
+ * Is the file open?
+ * This usually only returns false if an error occurred.
+ * @return True if the file is open; false if it isn't.
+ */
+bool RpQByteArrayFile::isOpen(void) const
+{
+	// RpQByteArrayFile is always open.
+	return true;
+}
+
+/**
+ * Close the file.
+ */
+void RpQByteArrayFile::close(void)
+{
+	// Not really useful...
+}
+
+/**
+ * Read data from the file.
+ * @param ptr Output data buffer.
+ * @param size Amount of data to read, in bytes.
+ * @return Number of bytes read.
+ */
+size_t RpQByteArrayFile::read(void *ptr, size_t size)
+{
+	if (unlikely(size == 0)) {
+		// Not reading anything...
+		return 0;
+	}
+
+	// Check if size is in bounds.
+	// NOTE: Need to use a signed comparison here.
+	if (static_cast<int64_t>(m_pos) > (static_cast<int64_t>(m_byteArray.size()) - static_cast<int64_t>(size))) {
+		// Not enough data.
+		// Copy whatever's left in the buffer.
+		size = m_byteArray.size() - m_pos;
+	}
+
+	// Copy the data.
+	const char *const buf = m_byteArray.constData();
+	memcpy(ptr, &buf[m_pos], size);
+	m_pos += size;
+	return size;
+}
+
+/**
+ * Write data to the file.
+ * @param ptr Input data buffer.
+ * @param size Amount of data to read, in bytes.
+ * @return Number of bytes written.
+ */
+size_t RpQByteArrayFile::write(const void *ptr, size_t size)
+{
+	if (unlikely(size == 0)) {
+		// Not writing anything...
+		return 0;
+	}
+
+	printf("WRITE: cur size == %d, pos == %lld, writing %zu bytes\n", m_byteArray.size(), m_pos, size);
+	// Do we need to expand the QByteArray?
+	int64_t req_size = static_cast<int64_t>(m_byteArray.size()) + size;
+	if (req_size < 0) {
+		// Overflow...
+		return 0;
+	} else if (req_size > m_byteArray.size()) {
+		// Need to expand the QByteArray.
+		m_byteArray.resize(req_size);
+	}
+	printf("WRITE: new size == %lld\n", req_size);
+
+	// Copy the data to the buffer.
+	char *const buf = m_byteArray.data();
+	memcpy(&buf[m_pos], ptr, size);
+	m_pos += size;
+	return size;
+}
+
+/**
+ * Set the file position.
+ * @param pos File position.
+ * @return 0 on success; -1 on error.
+ */
+int RpQByteArrayFile::seek(int64_t pos)
+{
+	// NOTE: m_pos is size_t, since it's referring to
+	// a position within a memory buffer.
+	if (pos <= 0) {
+		m_pos = 0;
+	} else if (pos >= m_byteArray.size()) {
+		m_pos = m_byteArray.size();
+	} else {
+		m_pos = static_cast<size_t>(pos);
+	}
+
+	return 0;
+}
+
+/**
+ * Get the file position.
+ * @return File position, or -1 on error.
+ */
+int64_t RpQByteArrayFile::tell(void)
+{
+	return (int64_t)m_pos;
+}
+
+/**
+ * Truncate the file.
+ * @param size New size. (default is 0)
+ * @return 0 on success; -1 on error.
+ */
+int RpQByteArrayFile::truncate(int64_t size)
+{
+	m_byteArray.resize(size);
+	return 0;
+}
+
+/** File properties **/
+
+/**
+ * Get the file size.
+ * @return File size, or negative on error.
+ */
+int64_t RpQByteArrayFile::size(void)
+{
+	return m_byteArray.size();
+}
+
+/**
+ * Get the filename.
+ * @return Filename. (May be empty if the filename is not available.)
+ */
+string RpQByteArrayFile::filename(void) const
+{
+	// TODO: Implement this?
+	return string();
+}
