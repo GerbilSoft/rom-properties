b8a4fb660c15c97edff63c6d3e908961e8c57f34
David Korth <gerbilsoft@gerbilsoft.com>
2019-09-30 18:30:56 -0400

    [kde] DragImageTreeWidget: New drag & drop QTreeWidget subclass.
    RomDataView: Use DragImageTreeWidget for RFT_LISTDATA fields with icons.
    Icons for e.g. Xbox 360 achievements can now be dragged and dropped into
    PNG files.
    
    TODO:
    - Multiple achievements at once?
    - Default filenames.
    - Metadata, e.g. source game and achievement ID.

---
+++ src/kde/DragImageTreeWidget.hpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (KDE4/KDE5)                        *
+ * DragImageTreeView.hpp: Drag & Drop tree view.                           *
+ *                                                                         *
+ * Copyright (c) 2019 by David Korth.                                      *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+// References:
+// - https://doc.qt.io/qt-5/dnd.html
+// - https://wiki.qt.io/QList_Drag_and_Drop_Example
+
+#ifndef __ROMPROPERTIES_KDE_DRAGIMAGETREEWIDGET_HPP__
+#define __ROMPROPERTIES_KDE_DRAGIMAGETREEWIDGET_HPP__
+
+#include <QTreeWidget>
+
+class DragImageTreeWidget : public QTreeWidget
+{
+	Q_OBJECT
+
+	public:
+		explicit DragImageTreeWidget(QWidget *parent = nullptr)
+			: super(parent) { }
+
+		// Role for an rp_image*.
+		static const int RpImageRole = Qt::UserRole + 0x4049;
+
+	private:
+		typedef QTreeWidget super;
+		Q_DISABLE_COPY(DragImageTreeWidget)
+
+	protected:
+		/** Overridden QWidget functions **/
+		void startDrag(Qt::DropActions supportedActions) override;
+};
+
+#endif /* __ROMPROPERTIES_KDE_DRAGIMAGETREEWIDGET_HPP__ */
