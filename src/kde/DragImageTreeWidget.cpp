11d70faa464d6d422b6fea642fc15f0182352c04
David Korth <gerbilsoft@gerbilsoft.com>
2019-09-30 19:53:39 -0400

    [kde] DragImageTreeWidget: Preliminary work for multiple image drag & drop.
    It doesn't seem to work; only the last image is saved.
    
    We might have to save the images to the temporary directory and use the
    MIME type "text/uri-list". This would also let us set default filenames.

--- src/kde/DragImageTreeWidget.cpp
+++ src/kde/DragImageTreeWidget.cpp
@@ -30,51 +30,76 @@ void DragImageTreeWidget::startDrag(Qt::DropActions supportedActions)
 {
 	// TODO: Handle supportedActions?
 	// TODO: Multiple PNG images if multiple items are selected?
+	// - May need to write images to a temp directory and use a URI list...
 	Q_UNUSED(supportedActions)
 
-	// Check if we have an rp_image* object here.
+	// Get the current item.
 	QTreeWidgetItem *const item = currentItem();
-	if (!item)
-		return;
-
-	const rp_image *const img = static_cast<const rp_image*>(item->data(0, RpImageRole).value<void*>());
-	if (!img)
-		return;
-
-	// Convert the rp_image to PNG.
-	RpQByteArrayFile *const pngData = new RpQByteArrayFile();
-	RpPngWriter *const pngWriter = new RpPngWriter(pngData, img);
-	if (!pngWriter->isOpen()) {
-		// Unable to open the PNG writer.
-		pngData->unref();
+	if (!item) {
+		// No item selected.
 		return;
 	}
 
-	// TODO: Add text fields indicating the source game.
-
-	int pwRet = pngWriter->write_IHDR();
-	if (pwRet != 0) {
-		// Error writing the PNG image...
-		pngData->unref();
+#if 0
+	// TODO
+	QList<QTreeWidgetItem*> items = selectedItems();
+	if (items.isEmpty()) {
+		// No items selected.
 		return;
 	}
-	pwRet = pngWriter->write_IDAT();
-	if (pwRet != 0) {
-		// Error writing the PNG image...
+#endif
+
+	// Find rp_image* objects in the items.
+	QMimeData *const mimeData = new QMimeData;
+	bool hasOne = false;
+	//foreach (QTreeWidgetItem *item, items) {
+	do {
+		const rp_image *const img = static_cast<const rp_image*>(item->data(0, RpImageRole).value<void*>());
+		if (!img)
+			continue;
+
+		// Convert the rp_image to PNG.
+		RpQByteArrayFile *const pngData = new RpQByteArrayFile();
+		RpPngWriter *const pngWriter = new RpPngWriter(pngData, img);
+		if (!pngWriter->isOpen()) {
+			// Unable to open the PNG writer.
+			pngData->unref();
+			continue;
+		}
+
+		// TODO: Add text fields indicating the source game.
+
+		int pwRet = pngWriter->write_IHDR();
+		if (pwRet != 0) {
+			// Error writing the PNG image...
+			pngData->unref();
+			continue;
+		}
+		pwRet = pngWriter->write_IDAT();
+		if (pwRet != 0) {
+			// Error writing the PNG image...
+			pngData->unref();
+			continue;
+		}
+
+		// RpPngWriter will finalize the PNG on delete.
+		delete pngWriter;
+
+		// Set the PNG data.
+		QByteArray ba = pngData->qByteArray();
+		mimeData->setData(QLatin1String("image/png"), pngData->qByteArray());
 		pngData->unref();
+		hasOne = true;
+	} while (0);
+
+	if (!hasOne) {
+		// No rp_image* objects...
+		delete mimeData;
 		return;
 	}
-
-	// RpPngWriter will finalize the PNG on delete.
-	delete pngWriter;
 
 	QDrag *const drag = new QDrag(this);
-	QMimeData *const mimeData = new QMimeData;
-
-	QByteArray ba = pngData->qByteArray();
-	mimeData->setData(QLatin1String("image/png"), pngData->qByteArray());
 	drag->setMimeData(mimeData);
 
 	drag->exec(Qt::CopyAction);
-	pngData->unref();
 }
