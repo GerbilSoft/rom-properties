dc29aec335d127cd8373077ee09c5cb00d7c7c44
David Korth <gerbilsoft@gerbilsoft.com>
2019-12-08 18:44:49 -0500

    [librptexture] gl_defs.h: Define all relevant GL enum values here instead of using the system GL headers.
    This allows us to eliminate the OpenGL dependency, which lets us get rid
    of the CMake option ENABLE_GL.
    
    Removed all ENABLE_GL checks.
    Removed #include "config.librptexture.h" where it's no longer needed.

--- src/libromdata/Other/RpTextureWrapper.cpp
+++ src/libromdata/Other/RpTextureWrapper.cpp
@@ -6,8 +6,6 @@
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
-#include "config.librptexture.h"
-
 #include "RpTextureWrapper.hpp"
 #include "librpbase/RomData_p.hpp"
 
