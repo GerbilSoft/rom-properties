GerbilSoft/rom-properties

commit a6ef730959ad0c5b2830d582cd45535b6aeae79f

[librptexture] ValveVTF: Limit to 128 mipmaps.
Not likely, but just in case...

PowerVR3 has the same limit, and others will have this limit once mipmap
support is implemented.

src/librptexture/fileformat/ValveVTF.cpp
@@ -323,9 +323,13 @@ void ValveVTFPrivate::getMipmapInfo(void)
 
 	// Resize based on mipmap count.
 	unsigned int mipmapCount = vtfHeader.mipmapCount;
+	assert(mipmapCount <= 128);
 	if (mipmapCount == 0) {
 		// No mipmaps == one image.
 		mipmapCount = 1;
+	} else if (mipmapCount > 128) {
+		// Too many mipmaps...
+		return;
 	}
 
 	// Starting address.

commit 58f3d6d2bfa00165bbadbebf26ad68bd2249937d

[librptexture] ValveVTFPrivate::getMipmapInfo(): Need to return a value...
src/librptexture/fileformat/ValveVTF.cpp: In member function ‘int LibRpTexture::ValveVTFPrivate::getMipmapInfo()’:
src/librptexture/fileformat/ValveVTF.cpp:334:3: error: return-statement with no value, in function returning ‘int’ [-fpermissive]
  334 |   return;
      |   ^~~~~~

This was on the PowerVR branch, but it didn't cause any compile errors
there, even though it definitely should have...

This fixes a build regression from commit a6ef730959ad0c5b2830d582cd45535b6aeae79f.
([librptexture] ValveVTF: Limit to 128 mipmaps.)

src/librptexture/fileformat/ValveVTF.cpp
@@ -331,7 +331,7 @@ int ValveVTFPrivate::getMipmapInfo(void)
 		mipmapCount = 1;
 	} else if (mipmapCount > 128) {
 		// Too many mipmaps...
-		return;
+		return -ENOMEM;
 	}
 
 	// Starting address.