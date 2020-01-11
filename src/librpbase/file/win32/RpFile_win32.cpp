670fb8f952754c14efe7617048b0e0799127b988
David Korth <gerbilsoft@gerbilsoft.com>
2020-01-11 14:12:07 -0500

    [librpbase] RpFile_win32.cpp: Added Zone.Identifier handling to setOriginInfo().
    This stores the zone ID (3, Internet) and the origin URL.
    
    FIXME: NtCreateFile() seems to have issues, and we end up getting
    STATUS_INVALID_PARAMETER (0xC000000D). We'll use a regular CreateFile()
    call for now.
    
    FIXME: Chromium has some shenanigans for Windows 10.
    Reference: https://github.com/chromium/chromium/blob/55f44515cd0b9e7739b434d1c62f4b7e321cd530/components/services/quarantine/quarantine_win.cc

--- src/librpbase/file/win32/RpFile_win32.cpp
+++ src/librpbase/file/win32/RpFile_win32.cpp
@@ -12,6 +12,9 @@
 #include "../RpFile.hpp"
 #include "../RpFile_p.hpp"
 
+// Config class (for storeFileOriginInfo)
+#include "../../config/Config.hpp"
+
 // librpbase
 #include "TextFuncs_wchar.hpp"
 
@@ -218,23 +221,25 @@ int RpFilePrivate::reOpenFile(void)
 	if (file && file != INVALID_HANDLE_VALUE) {
 		CloseHandle(file);
 	}
-	file = CreateFile(tfilename.c_str(),
-			dwDesiredAccess,
-			dwShareMode,
-			nullptr,
-			dwCreationDisposition,
-			FILE_ATTRIBUTE_NORMAL,
-			nullptr);
+	file = CreateFile(
+		tfilename.c_str(),	// lpFileName
+		dwDesiredAccess,	// dwDesiredAccess
+		dwShareMode,		// dwShareMode
+		nullptr,		// lpSecurityAttributes
+		dwCreationDisposition,	// dwCreationDisposition
+		FILE_ATTRIBUTE_NORMAL,	// dwFlagsAndAttributes
+		nullptr);		// hTemplateFile
 	if (isDevice) {
 		if (!file || file == INVALID_HANDLE_VALUE) {
 			// Try again without WRITE permission.
-			file = CreateFile(tfilename.c_str(),
-					GENERIC_READ,
-					FILE_SHARE_READ,
-					nullptr,
-					dwCreationDisposition,
-					FILE_ATTRIBUTE_NORMAL,
-					nullptr);
+			file = CreateFile(
+				tfilename.c_str(),	// lpFileName
+				GENERIC_READ,		// dwDesiredAccess
+				FILE_SHARE_READ,	// dwShareMode
+				nullptr,		// lpSecurityAttributes
+				dwCreationDisposition,	// dwCreationDisposition
+				FILE_ATTRIBUTE_NORMAL,	// dwFlagsAndAttributes
+				nullptr);		// hTemplateFile
 		}
 	}
 	if (!file || file == INVALID_HANDLE_VALUE) {
@@ -720,11 +725,67 @@ int RpFile::setOriginInfo(const std::string &url, time_t mtime)
 {
 	RP_D(RpFile);
 
+	// NOTE: Even if one of the xattr functions fails, we'll
+	// continue with others and setting mtime. The first error
+	// will be returned at the end of the function.
+	int err = 0;
+
 	// TODO: Add a static_warning() macro?
 	// - http://stackoverflow.com/questions/8936063/does-there-exist-a-static-warning
 #if _USE_32BIT_TIME_T
 # error 32-bit time_t is not supported. Get a newer compiler.
 #endif
+
+	// NOTE: This will force a configuration timestamp check.
+	const Config *const config = Config::instance();
+	const bool storeFileOriginInfo = config->storeFileOriginInfo();
+	if (storeFileOriginInfo) {
+		// Create an ADS named "Zone.Identifier".
+		// References:
+		// - https://cqureacademy.com/blog/alternate-data-streams
+		// - https://stackoverflow.com/questions/46141321/open-alternate-data-stream-ads-from-file-handle-or-file-id
+		// - https://stackoverflow.com/a/46141949
+		// FIXME: NtCreateFile() seems to have issues, and we end up
+		// getting STATUS_INVALID_PARAMETER (0xC000000D).
+		// We'll use a regular CreateFile() call for now.
+		tstring tfilename = U82T_s(d->filename);
+		tfilename += _T(":Zone.Identifier");
+		HANDLE hAds = CreateFile(
+			tfilename.c_str(),	// lpFileName
+			GENERIC_WRITE,		// dwDesiredAccess
+			FILE_SHARE_READ,	// dwShareMode
+			nullptr,		// lpSecurityAttributes
+			CREATE_ALWAYS,		// dwCreationDisposition
+			FILE_ATTRIBUTE_NORMAL,	// dwFlagsAndAttributes
+			nullptr);		// hTemplateFile
+		if (hAds && hAds != INVALID_HANDLE_VALUE) {
+			// Write a zone identifier.
+			// NOTE: Assuming UTF-8 encoding.
+			// FIXME: Chromium has some shenanigans for Windows 10.
+			// Reference: https://github.com/chromium/chromium/blob/55f44515cd0b9e7739b434d1c62f4b7e321cd530/components/services/quarantine/quarantine_win.cc
+			std::string s_zoneID;
+			s_zoneID.reserve(1024);
+			s_zoneID = "[ZoneTransfer]\r\nZoneID=3\r\nHostUrl=";
+			s_zoneID += url;
+			s_zoneID += "\r\n";
+			DWORD dwBytesWritten = 0;
+			BOOL bRet = WriteFile(hAds, s_zoneID.data(),
+				static_cast<DWORD>(s_zoneID.size()),
+				&dwBytesWritten, nullptr);
+			if ((!bRet || dwBytesWritten != static_cast<DWORD>(s_zoneID.size())) && err != 0) {
+				// Some error occurred...
+				m_lastError = w32err_to_posix(GetLastError());
+				err = m_lastError;
+			}
+			CloseHandle(hAds);
+		} else {
+			// Error opening the ADS.
+			if (err != 0) {
+				m_lastError = w32err_to_posix(GetLastError());
+				err = m_lastError;
+			}
+		}
+	}
 
 	if (mtime >= 0) {
 		// Convert to FILETIME.
@@ -737,14 +798,13 @@ int RpFile::setOriginInfo(const std::string &url, time_t mtime)
 
 		// Set the mtime.
 		BOOL bRet = SetFileTime(d->file, nullptr, nullptr, &ft_mtime);
-		if (!bRet) {
+		if (!bRet && err != 0) {
 			m_lastError = w32err_to_posix(GetLastError());
-			return -m_lastError;
+			err = m_lastError;
 		}
 	}
 
-	// We're done here.
-	return 0;
+	return -err;
 }
 
 /** Device file functions **/
