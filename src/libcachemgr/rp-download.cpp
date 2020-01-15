37bc9247eb2e93611a329d954b076679432f3452
David Korth <gerbilsoft@gerbilsoft.com>
2020-01-14 21:37:09 -0500

    [libcachemgr] rp-download.cpp: Added a verbose option and more error messages.

--- src/libcachemgr/rp-download.cpp
+++ src/libcachemgr/rp-download.cpp
@@ -9,6 +9,7 @@
 #include "stdafx.h"
 
 // C includes. (C++ namespace)
+#include <cstdarg>
 #include <cstdio>
 #include <cstring>
 #include <stdlib.h>
@@ -41,15 +42,50 @@ using std::string;
 # define _T(x) (x)
 # define _tmain main
 # define _tcschr strchr
+# define _tcscmp strcmp
 # define _tcsncmp strncmp
+# define _fputtc fputc
 # define _ftprintf fprintf
 # define _tprintf printf
 # define _sntprintf snprintf
+# define _vftprintf vfprintf
 #endif
 
 #ifndef _countof
 # define _countof(x) (sizeof(x)/sizeof(x[0]))
 #endif
+
+static const TCHAR *argv0 = nullptr;
+static bool verbose = false;
+
+/**
+ * Show command usage.
+ */
+static void show_usage(void)
+{
+	_ftprintf(stderr, _T("Syntax: %s [-v] cache_key\n"), argv0);
+}
+
+// TODO: SHOW_ERROR() macro that checks for verbose before calling show_error().
+/**
+ * Show an error message.
+ * @param format printf() format string.
+ * @param ... printf() format parameters.
+ */
+static void
+#ifndef _MSC_VER
+__attribute__ ((format (printf, 1, 2)))
+#endif /* _MSC_VER */
+show_error(const TCHAR *format, ...)
+{
+	va_list ap;
+
+	_ftprintf(stderr, _T("%s: "), argv0);
+	va_start(ap, format);
+	_vftprintf(stderr, format, ap);
+	va_end(ap);
+	_fputtc(_T('\n'), stderr);
+}
 
 /**
  * rp-download: Download an image from a supported online database.
@@ -58,7 +94,6 @@ using std::string;
  *
  * TODO:
  * - More error codes based on the error.
- * - Verbose logging. (-v, --verbose)
  */
 int RP_C_API _tmain(int argc, TCHAR *argv[])
 {
@@ -77,11 +112,28 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	secoptions_init();
 #endif /* _WIN32 */
 
+	// Store argv[0] globally.
+	argv0 = argv[0];
+
+	const TCHAR *cache_key = argv[1];
 	if (argc < 2) {
 		// TODO: Add a verbose option to print messages.
 		// Normally, the only output is a return value.
-		_ftprintf(stderr, _T("Syntax: %s cache_key\n"), argv[0]);
+		show_usage();
 		return EXIT_FAILURE;
+	}
+
+	// Check for "-v" or "--verbose".
+	if (!_tcscmp(argv[1], _T("-v")) || !_tcscmp(argv[1], _T("--verbose"))) {
+		// Verbose mode is enabled.
+		verbose = true;
+		// We need at least three parameters now.
+		if (argc < 3) {
+			show_error(_T("No cache key specified."));
+			show_usage();
+			return EXIT_FAILURE;
+		}
+		cache_key = argv[2];
 	}
 
 	// Check the cache key prefix. The prefix indicates the system
@@ -92,24 +144,35 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	// - 3ds:    https://art.gametdb.com/3ds/[key]
 	// - ds:     https://art.gametdb.com/3ds/[key]
 	// - amiibo: https://amiibo.life/[key]/image
-	const TCHAR *const cache_key = argv[1];
 	const TCHAR *slash_pos = _tcschr(cache_key, _T('/'));
 	if (slash_pos == nullptr) {
 		// No slash. Not a valid cache key.
+		if (verbose) {
+			show_error(_T("Cache key '%s' is invalid."), cache_key);
+		}
 		return EXIT_FAILURE;
 	} else if (slash_pos[1] == '\0') {
 		// Slash is the last character.
 		// Not a valid cache key.
+		if (verbose) {
+			show_error(_T("Cache key '%s' is invalid."), cache_key);
+		}
 		return EXIT_FAILURE;
 	} else if (slash_pos == cache_key) {
 		// Slash is the first character.
 		// Not a valid cache key.
+		if (verbose) {
+			show_error(_T("Cache key '%s' is invalid."), cache_key);
+		}
 		return EXIT_FAILURE;
 	}
 
 	const ptrdiff_t prefix_len = (slash_pos - cache_key);
 	if (prefix_len <= 0) {
 		// Empty prefix.
+		if (verbose) {
+			show_error(_T("Cache key '%s' is invalid."), cache_key);
+		}
 		return EXIT_FAILURE;
 	}
 
@@ -144,6 +207,9 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 			_T("https://amiibo.life/nfc/%s/image"), slash_pos+1);
 	} else {
 		// Prefix is not supported.
+		if (verbose) {
+			show_error(_T("Cache key '%s' has an unsupported prefix."), cache_key);
+		}
 		return EXIT_FAILURE;
 	}
 
@@ -153,6 +219,9 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	tstring cache_filename = LibCacheCommon::getCacheFilename(cache_key);
 	if (cache_filename.empty()) {
 		// Invalid cache filename.
+		if (verbose) {
+			show_error(_T("Cache key '%s' is invalid."), cache_key);
+		}
 		return EXIT_FAILURE;
 	}
 	cache_filename += ext;
