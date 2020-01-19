GerbilSoft/rom-properties

commit 5f1654b9c6f12adeadff1148eae3dad9016e48cc

[rpcli] properties.cpp: Some minor iterator optimizations.
- Use iterators and a separate counter instead of indexing.

- extURLs: Use cend() as the end iterator, not end(). MSVC's debug CRT
  would have caught this.

src/rpcli/properties.cpp
@@ -244,8 +244,9 @@ public:
 		unique_ptr<size_t[]> colSize(new size_t[col_count]());
 		size_t totalWidth = col_count + 1;
 		if (listDataDesc.names) {
-			for (int i = (int)listDataDesc.names->size()-1; i >= 0; i--) {
-				colSize[i] = listDataDesc.names->at(i).size();
+			int i = 0;
+			for (auto it = listDataDesc.names->cbegin(); it != listDataDesc.names->cend(); ++it, ++i) {
+				colSize[i] = it->size();
 			}
 		}
 
@@ -268,9 +269,10 @@ public:
 		bool skipFirstNL = true;
 		if (listDataDesc.names) {
 			// Print the column names.
-			for (int i = 0; i < (int)listDataDesc.names->size(); i++) {
+			size_t i = 0;
+			for (auto it = listDataDesc.names->cbegin(); it != listDataDesc.names->cend(); ++it, ++i) {
 				totalWidth += colSize[i]; // this could be in a separate loop, but whatever
-				os << '|' << setw(colSize[i]) << listDataDesc.names->at(i);
+				os << '|' << setw(colSize[i]) << *it;
 			}
 			os << '|' << endl << Pad(field.width) << string(totalWidth, '-');
 			// Don't skip the first newline, since we're
@@ -285,7 +287,6 @@ public:
 			colSize[0] -= 4;
 		}
 		for (auto it = list_data->cbegin(); it != list_data->cend(); ++it) {
-			int i = 0;
 			if (!skipFirstNL) {
 				os << endl << Pad(field.width);
 			} else {
@@ -296,8 +297,9 @@ public:
 				os << '[' << ((checkboxes & 1) ? 'x' : ' ') << "] ";
 				checkboxes >>= 1;
 			}
-			for (auto jt = it->cbegin(); jt != it->cend(); ++jt) {
-				os << setw(colSize[i++]) << SafeString(jt->c_str(), false) << '|';
+			size_t i = 0;
+			for (auto jt = it->cbegin(); jt != it->cend(); ++jt, ++i) {
+				os << setw(colSize[i]) << SafeString(jt->c_str(), false) << '|';
 			}
 		}
 		return os;
@@ -608,9 +610,12 @@ public:
 							os << ((checkboxes & 1) ? "true" : "false") << ',';
 							checkboxes >>= 1;
 						}
+
+						bool did_one = false;
 						for (auto jt = it->cbegin(); jt != it->cend(); ++jt) {
-							if (jt != it->cbegin()) os << ',';
+							if (!did_one) os << ',';
 							os << JSONString(jt->c_str());
+							did_one = true;
 						}
 						os << ']';
 					}
@@ -743,7 +748,7 @@ std::ostream& operator<<(std::ostream& os, const ROMOutput& fo) {
 		if (ret != 0 || extURLs.empty())
 			continue;
 
-		for (auto iter = extURLs.cbegin(); iter != extURLs.end(); ++iter) {
+		for (auto iter = extURLs.cbegin(); iter != extURLs.cend(); ++iter) {
 			os << "-- " <<
 				RomData::getImageTypeName((RomData::ImageType)i) << ": " << iter->url <<
 				" (cache_key: " << iter->cache_key << ')' << endl;
@@ -853,7 +858,7 @@ std::ostream& operator<<(std::ostream& os, const JSONROMOutput& fo) {
 		os << ",\"exturls\":[";
 		bool firsturl = true;
 
-		for (auto iter = extURLs.cbegin(); iter != extURLs.end(); ++iter) {
+		for (auto iter = extURLs.cbegin(); iter != extURLs.cend(); ++iter) {
 			if (firsturl) firsturl = false;
 			else os << ',';
 

commit 09a9a21d9d238ea66d368c2f66eb138dda0c3ded

[rpcli] properties.cpp: Fixed several JSON output errors.
RFT_LISTDATA commas broke in commit 5f1654b9c6f12adeadff1148eae3dad9016e48cc.
([rpcli] properties.cpp: Some minor iterator optimizations.) [v1.4]

RFT_DIMENSIONS broke in commit 5d42d17248d1c4e2fe88fe855a6ad71a819b079e.
([librpbase] RomFields: Added a new field type RFT_DIMENSIONS.)
[Note that this was the first commit to support RFT_DIMENSIONS, so it was
basically always broken...] [v1.4]

External image URL formatting likely broke in commit
b56bd49974025cf6de837387fe9ce88db2d78fc1.
([rpcli] properties.cpp: JSON: Don't print empty extURLs fields.) [v1.1]

src/rpcli/properties.cpp
@@ -823,9 +823,9 @@ public:
 
 						bool did_one = false;
 						for (auto jt = it->cbegin(); jt != it->cend(); ++jt) {
-							if (!did_one) os << ',';
-							os << JSONString(jt->c_str());
+							if (did_one) os << ',';
 							did_one = true;
+							os << JSONString(jt->c_str());
 						}
 						os << ']';
 					}
@@ -889,14 +889,14 @@ public:
 				   << "},\"data\":";
 
 				const int *const dimensions = romField->data.dimensions;
-				os << "[\"w\":" << dimensions[0];
+				os << "{\"w\":" << dimensions[0];
 				if (dimensions[1] > 0) {
 					os << ",\"h\":" << dimensions[1];
 					if (dimensions[2] > 0) {
 						os << ",\"d\":" << dimensions[2];
 					}
 				}
-				os << "]}";
+				os << "}}";
 				break;
 			}
 
@@ -1108,15 +1108,15 @@ std::ostream& operator<<(std::ostream& os, const JSONROMOutput& fo) {
 		}
 		// NOTE: IMGPF_ICON_ANIMATED won't ever appear in external image
 		os << ",\"exturls\":[";
-		bool firsturl = true;
-
+		bool did_one = false;
 		for (auto iter = extURLs.cbegin(); iter != extURLs.cend(); ++iter) {
-			if (firsturl) firsturl = false;
-			else os << ',';
+			if (did_one) os << ',';
+			did_one = true;
 
 			os << "{\"url\":" << JSONString(iter->url.c_str());
 			os << ",\"cache_key\":" << JSONString(iter->cache_key.c_str()) << '}';
 		}
+		os << "]}";
 	}
 	if (!first) {
 		os << ']';