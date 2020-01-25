GerbilSoft/rom-properties

commit f0673c19f821287e86de431b4ed5c52f6be5e636

[libromdata] WiiPartition: Calculate the used partition size correctly for unencrypted RVT-H Reader disc images.
Previously, GcnPartition was always multiplying by 32/31, regardless of
the encryption status.

The Last Story (PAL) v2340 has ~4.70 GiB of data when extracted, including
the FST and main.dol. rpcli was previously showing 4.85 GiB used; now it
shows 4.70 GiB, which includes the FST, main.dol, and partition header.

Also check if the DOL is after the FST, and if it is, use the DOL offset.
TODO: Get the DOL size if the DOL is located after the FST. This is
unlikely, though...

src/libromdata/disc/GcnPartition.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * GcnPartition.cpp: GameCube partition reader.                            *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -168,23 +168,25 @@ int64_t GcnPartition::partition_size_used(void) const
 		}
 	}
 
-	// FST offset and size.
-	int64_t size = static_cast<int64_t>(d->bootBlock.fst_offset) +
-				   static_cast<int64_t>(d->bootBlock.fst_size);
+	// FST/DOL offset and size.
+	int64_t size;
 	size <<= d->offsetShift;
-	
+	if (d->bootBlock.dol_offset > d->bootBlock.fst_offset) {
+		// DOL is after the FST.
+		// TODO: Get the DOL size. (This case is unlikely, though...)
+		size = static_cast<int64_t>(d->bootBlock.dol_offset);
+	} else {
+		// FST is after the DOL.
+		size = static_cast<int64_t>(d->bootBlock.fst_offset) +
+		       static_cast<int64_t>(d->bootBlock.fst_size);
+	}
+	size <<= d->offsetShift;
+
 	// Get the FST used size.
 	size += d->fst->totalUsedSize();
 
 	// Add the difference between partition and data sizes.
 	size += (d->partition_size - d->data_size);
-
-	// FIXME: Handle the hashes size correctly on Wii. For now, use a
-	// quick and dirty hack.
-	if (d->offsetShift == 2) {
-		// Multiply the size by 31/32.
-		size = (size * 32) / 31;
-	}
 
 	// We're done here.
 	return size;

src/libromdata/disc/GcnPartition.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * GcnPartition.hpp: GameCube partition reader.                            *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -94,7 +94,7 @@ class GcnPartition : public LibRpBase::IPartition
 		 * but does not include "empty" sectors.
 		 * @return Used partition size, or -1 on error.
 		 */
-		int64_t partition_size_used(void) const final;
+		int64_t partition_size_used(void) const override;
 
 	public:
 		/** IFst wrapper functions. **/

src/libromdata/disc/WiiPartition.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * WiiPartition.hpp: Wii partition reader.                                 *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -735,6 +735,36 @@ int64_t WiiPartition::tell(void)
 	}
 
 	return d->pos_7C00;
+}
+
+/**
+ * Get the used partition size.
+ * This size includes the partition header and hashes,
+ * but does not include "empty" sectors.
+ * @return Used partition size, or -1 on error.
+ */
+int64_t WiiPartition::partition_size_used(void) const
+{
+	// Get the FST used size from GcnPartition.
+	int64_t size = super::partition_size_used();
+	if (size <= 0) {
+		// Error retrieving the FST used size.
+		return size;
+	}
+
+	// Add the data offset from the partition header.
+	RP_D(const WiiPartition);
+	size += (static_cast<int64_t>(be32_to_cpu(d->partitionHeader.data_offset)) << 2);
+
+	// Are sectors hashed?
+	if ((d->cryptoMethod & WiiPartition::CM_MASK_SECTOR) == CM_1K_31K) {
+		// Partition has hashed sectors.
+		// Multiply the FST used size by 32/31 to adjust for the difference.
+		size = (size * 32) / 31;
+	}
+
+	// We're done here.
+	return size;
 }
 
 /** WiiPartition **/

src/libromdata/disc/WiiPartition.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * WiiPartition.hpp: Wii partition reader.                                 *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -85,6 +85,16 @@ class WiiPartition : public GcnPartition
 		 */
 		int64_t tell(void) final;
 
+	public:
+		/**
+		 * Get the used partition size.
+		 * This size includes the partition header and hashes,
+		 * but does not include "empty" sectors.
+		 * @return Used partition size, or -1 on error.
+		 */
+		int64_t partition_size_used(void) const final;
+
+	public:
 		/** WiiPartition **/
 
 		/**

commit 7e6adc9b5cf1e7766ddd604f7b1609c1d17aac40

[libromdata] GcnPartition: Remove a duplicated shift.
This was operating on an uninitialized variable, and it was effectively
useless because `size` is overwritten immediately after.

The AppVeyor build failed because of this:

gcnpartition.cpp(173): error C4700: uninitialized local variable 'size' used

This fixes a regression from commit f0673c19f821287e86de431b4ed5c52f6be5e636.
([libromdata] WiiPartition: Calculate the used partition size correctly for unencrypted RVT-H Reader disc images.)

src/libromdata/disc/GcnPartition.cpp
@@ -170,7 +170,6 @@ int64_t GcnPartition::partition_size_used(void) const
 
 	// FST/DOL offset and size.
 	int64_t size;
-	size <<= d->offsetShift;
 	if (d->bootBlock.dol_offset > d->bootBlock.fst_offset) {
 		// DOL is after the FST.
 		// TODO: Get the DOL size. (This case is unlikely, though...)