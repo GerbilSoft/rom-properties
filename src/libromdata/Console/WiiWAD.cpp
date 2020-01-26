GerbilSoft/rom-properties

commit ff645ab7bbfc05076e579ffe07f6b0069774c8a0

[libromdata] WiiWAD: Use RFT_STRING_MULTI for the Game Info field.
WiiCommon: Split the Wii Game Info function into a separate static class.
This function is now used by both GameCube and WiiWAD.

src/libromdata/Console/WiiWAD.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * WiiWAD.cpp: Nintendo Wii WAD file reader.                               *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -17,6 +17,7 @@
 #include "data/NintendoLanguage.hpp"
 #include "data/WiiSystemMenuVersion.hpp"
 #include "GameCubeRegions.hpp"
+#include "WiiCommon.hpp"
 
 // librpbase, librptexture
 using namespace LibRpBase;
@@ -923,10 +924,20 @@ int WiiWAD::loadFieldData(void)
 	{
 		// No WIBN data.
 		// Get the IMET data if it's available.
-		string gameInfo = d->getGameInfo();
-		if (!gameInfo.empty()) {
-			d->fields->addField_string(C_("WiiWAD", "Game Info"), gameInfo);
+#ifdef ENABLE_DECRYPTION
+		// Get the string map.
+		RomFields::StringMultiMap_t *const pMap_bannerName = WiiCommon::getWiiBannerStrings(
+			&d->imet, gcnRegion, id4_region);
+		if (!pMap_bannerName) {
+			// Error getting the map...
+			return -EIO;
 		}
+
+		// Add the field.
+		const uint32_t def_lc = NintendoLanguage::getWiiLanguageCode(
+			NintendoLanguage::getWiiLanguage());
+		d->fields->addField_string_multi(C_("WiiWAD", "Game Info"), pMap_bannerName, def_lc);
+#endif /* ENABLE_DECRYPTION */
 	}
 
 	// Console ID.
@@ -964,6 +975,7 @@ int WiiWAD::loadMetaData(void)
 
 	// NOTE: We can only get the title if the encryption key is valid.
 	// If we can't get the title, don't bother creating RomMetaData.
+	// TODO: Use WiiCommon for multi-language strings?
 	string gameInfo = d->getGameInfo();
 	if (gameInfo.empty()) {
 		return -EIO;

src/libromdata/CMakeLists.txt
@@ -23,6 +23,7 @@ SET(libromdata_SRCS
 	Console/SegaSaturn.cpp
 	Console/SNES.cpp
 	Console/PlayStationSave.cpp
+	Console/WiiCommon.cpp
 	Console/WiiSave.cpp
 	Console/WiiU.cpp
 	Console/WiiWAD.cpp
@@ -127,6 +128,7 @@ SET(libromdata_H
 	Console/Sega8Bit.hpp
 	Console/SegaSaturn.hpp
 	Console/SNES.hpp
+	Console/WiiCommon.hpp
 	Console/WiiSave.hpp
 	Console/WiiU.hpp
 	Console/WiiWAD.hpp

src/libromdata/Console/GameCube.cpp
@@ -17,6 +17,7 @@
 #include "data/WiiSystemMenuVersion.hpp"
 #include "data/NintendoLanguage.hpp"
 #include "GameCubeRegions.hpp"
+#include "WiiCommon.hpp"
 
 // librpbase, librptexture
 #include "librpbase/SystemRegion.hpp"
@@ -580,64 +581,12 @@ int GameCubePrivate::wii_addBannerName(void) const
 		}
 	}
 
-	// Check if English is valid.
-	// If it is, we'll de-duplicate fields.
-	bool dedupe_titles = (opening_bnr.wii.imet->names[WII_LANG_ENGLISH][0][0] != cpu_to_be16('\0'));
-
-	RomFields::StringMultiMap_t *const pMap_bannerName = new RomFields::StringMultiMap_t();
-	for (int langID = 0; langID < WII_LANG_MAX; langID++) {
-		if (langID == 7 || langID == 8) {
-			// Unknown languages. Skip them. (Maybe these were Chinese?)
-			// TODO: Just skip if NintendoLanguage::getWiiLanguageCode() returns 0?
-			continue;
-		}
-
-		if (dedupe_titles && langID != WII_LANG_ENGLISH) {
-			// Check if the comments match English.
-			// NOTE: Not converting to host-endian first, since
-			// u16_strncmp() checks for equality and for 0.
-			if (!u16_strncmp(opening_bnr.wii.imet->names[langID][0],
-			                 opening_bnr.wii.imet->names[WII_LANG_ENGLISH][0],
-			                 ARRAY_SIZE(opening_bnr.wii.imet->names[WII_LANG_ENGLISH][0])) &&
-			    !u16_strncmp(opening_bnr.wii.imet->names[langID][1],
-			                 opening_bnr.wii.imet->names[WII_LANG_ENGLISH][1],
-			                 ARRAY_SIZE(opening_bnr.wii.imet->names[WII_LANG_ENGLISH][1])))
-			{
-				// All fields match English.
-				continue;
-			}
-		}
-
-		uint32_t lc;
-		if (gcnRegion == GCN_REGION_JPN && discHeader.id4[3] == 'W' &&
-		    langID == WII_LANG_JAPANESE)
-		{
-			// Special case: RVL-001(TWN) has a JPN region code.
-			// Game discs with disc ID region 'W' are localized
-			// for Taiwan and use Traditional Chinese in the
-			// Japanese language slot.
-			lc = 'hant';
-		} else {
-			lc = NintendoLanguage::getWiiLanguageCode(langID);
-			assert(lc != 0);
-			if (lc == 0)
-				continue;
-		}
-
-		if (opening_bnr.wii.imet->names[langID][0][0] != cpu_to_be16('\0')) {
-			// NOTE: The banner may have two lines.
-			// Each line is a maximum of 21 characters.
-			// Convert from UTF-16 BE and split into two lines at the same time.
-			string info = utf16be_to_utf8(opening_bnr.wii.imet->names[langID][0],
-			                              ARRAY_SIZE(opening_bnr.wii.imet->names[langID][0]));
-			if (opening_bnr.wii.imet->names[langID][1][0] != cpu_to_be16('\0')) {
-				info += '\n';
-				info += utf16be_to_utf8(opening_bnr.wii.imet->names[langID][1],
-				        ARRAY_SIZE(opening_bnr.wii.imet->names[langID][1]));
-			}
-
-			pMap_bannerName->insert(std::make_pair(lc, std::move(info)));
-		}
+	// Get the string map.
+	RomFields::StringMultiMap_t *const pMap_bannerName = WiiCommon::getWiiBannerStrings(
+		opening_bnr.wii.imet, gcnRegion, discHeader.id4[3]);
+	if (!pMap_bannerName) {
+		// Error getting the map...
+		return -EIO;
 	}
 
 	// Add the field.

src/libromdata/Console/WiiCommon.cpp
@@ -1,0 +1,102 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * WiiCommon.hpp: Nintendo Wii common functions.                           *
+ *                                                                         *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include <stdafx.h>
+#include "WiiCommon.hpp"
+
+#include "data/NintendoLanguage.hpp"
+#include "gcn_structs.h"
+
+// librpbase
+using namespace LibRpBase;
+
+// C++ STL classes.
+using std::string;
+
+namespace LibRomData {
+
+/**
+ * Get a multi-language string map from a Wii banner.
+ * @param pImet		[in] Wii_IMET_t
+ * @param gcnRegion	[in] GameCube region code.
+ * @param id4_region	[in] ID4 region.
+ * @return Allocated RomFields::StringMultiMap_t, or nullptr on error.
+ */
+RomFields::StringMultiMap_t *WiiCommon::getWiiBannerStrings(
+	const Wii_IMET_t *pImet, uint32_t gcnRegion, char id4_region)
+{
+	assert(pImet != nullptr);
+	if (!pImet) {
+		return nullptr;
+	}
+
+	// Validate the IMET magic number.
+	if (pImet->magic != cpu_to_be32(WII_IMET_MAGIC)) {
+		// Not valid.
+		return nullptr;
+	}
+
+	// Check if English is valid.
+	// If it is, we'll de-duplicate fields.
+	bool dedupe_titles = (pImet->names[WII_LANG_ENGLISH][0][0] != cpu_to_be16('\0'));
+
+	RomFields::StringMultiMap_t *const pMap_bannerName = new RomFields::StringMultiMap_t();
+	for (int langID = 0; langID < WII_LANG_MAX; langID++) {
+		if (langID == 7 || langID == 8) {
+			// Unknown languages. Skip them. (Maybe these were Chinese?)
+			// TODO: Just skip if NintendoLanguage::getWiiLanguageCode() returns 0?
+			continue;
+		}
+
+		if (dedupe_titles && langID != WII_LANG_ENGLISH) {
+			// Check if the comments match English.
+			// NOTE: Not converting to host-endian first, since
+			// u16_strncmp() checks for equality and for 0.
+			if (!u16_strncmp(pImet->names[langID][0], pImet->names[WII_LANG_ENGLISH][0],
+			                 ARRAY_SIZE(pImet->names[WII_LANG_ENGLISH][0])) &&
+			    !u16_strncmp(pImet->names[langID][1], pImet->names[WII_LANG_ENGLISH][1],
+			                 ARRAY_SIZE(pImet->names[WII_LANG_ENGLISH][1])))
+			{
+				// All fields match English.
+				continue;
+			}
+		}
+
+		uint32_t lc;
+		if (gcnRegion == GCN_REGION_JPN && id4_region == 'W' && langID == WII_LANG_JAPANESE) {
+			// Special case: RVL-001(TWN) has a JPN region code.
+			// Game discs with disc ID region 'W' are localized
+			// for Taiwan and use Traditional Chinese in the
+			// Japanese language slot.
+			lc = 'hant';
+		} else {
+			lc = NintendoLanguage::getWiiLanguageCode(langID);
+			assert(lc != 0);
+			if (lc == 0)
+				continue;
+		}
+
+		if (pImet->names[langID][0][0] != cpu_to_be16('\0')) {
+			// NOTE: The banner may have two lines.
+			// Each line is a maximum of 21 characters.
+			// Convert from UTF-16 BE and split into two lines at the same time.
+			string info = utf16be_to_utf8(pImet->names[langID][0], ARRAY_SIZE(pImet->names[langID][0]));
+			if (pImet->names[langID][1][0] != cpu_to_be16('\0')) {
+				info += '\n';
+				info += utf16be_to_utf8(pImet->names[langID][1], ARRAY_SIZE(pImet->names[langID][1]));
+			}
+
+			pMap_bannerName->insert(std::make_pair(lc, std::move(info)));
+		}
+	}
+
+	// Map is done.
+	return pMap_bannerName;
+}
+
+}

src/libromdata/Console/WiiCommon.hpp
@@ -1,0 +1,44 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * WiiCommon.hpp: Nintendo Wii common functions.                           *
+ *                                                                         *
+ * Copyright (c) 2016-2018 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBROMDATA_WIICOMMON_HPP__
+#define __ROMPROPERTIES_LIBROMDATA_WIICOMMON_HPP__
+
+#include "librpbase/RomData.hpp"
+#include "librpbase/RomFields.hpp"
+
+#include "wii_banner.h"
+
+// C includes.
+#include <stdint.h>
+
+namespace LibRomData {
+
+class WiiCommon
+{
+	private:
+		WiiCommon() { }
+		~WiiCommon() { }
+	private:
+		RP_DISABLE_COPY(WiiCommon)
+
+	public:
+		/**
+		 * Get a multi-language string map from a Wii banner.
+		 * @param pImet		[in] Wii_IMET_t
+		 * @param gcnRegion	[in] GameCube region code.
+		 * @param id4_region	[in] ID4 region.
+		 * @return Allocated RomFields::StringMultiMap_t, or nullptr on error.
+		 */
+		static LibRpBase::RomFields::StringMultiMap_t *getWiiBannerStrings(
+			const Wii_IMET_t *pImet, uint32_t gcnRegion, char id4_region);
+};
+
+}
+
+#endif /* __ROMPROPERTIES_LIBROMDATA_WIICOMMON_HPP__ */

commit 2dea689584e74329495eb32371432639f350aa1f

[libromdata] WiiWAD: Don't fail if we can't get the Wii banner strings.
This happens if the IMET section is actually a WIBN, e.g. for DLC WADs.

This fixes a regression from commit ff645ab7bbfc05076e579ffe07f6b0069774c8a0.
([libromdata] WiiWAD: Use RFT_STRING_MULTI for the Game Info field.)

src/libromdata/Console/WiiWAD.cpp
@@ -928,15 +928,12 @@ int WiiWAD::loadFieldData(void)
 		// Get the string map.
 		RomFields::StringMultiMap_t *const pMap_bannerName = WiiCommon::getWiiBannerStrings(
 			&d->imet, gcnRegion, id4_region);
-		if (!pMap_bannerName) {
-			// Error getting the map...
-			return -EIO;
+		if (pMap_bannerName) {
+			// Add the field.
+			const uint32_t def_lc = NintendoLanguage::getWiiLanguageCode(
+				NintendoLanguage::getWiiLanguage());
+			d->fields->addField_string_multi(C_("WiiWAD", "Game Info"), pMap_bannerName, def_lc);
 		}
-
-		// Add the field.
-		const uint32_t def_lc = NintendoLanguage::getWiiLanguageCode(
-			NintendoLanguage::getWiiLanguage());
-		d->fields->addField_string_multi(C_("WiiWAD", "Game Info"), pMap_bannerName, def_lc);
 #endif /* ENABLE_DECRYPTION */
 	}
 