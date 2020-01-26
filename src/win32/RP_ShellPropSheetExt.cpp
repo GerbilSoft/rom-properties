GerbilSoft/rom-properties

commit 4a3db1e1da898f50043ae52f27dfab0b520e88a4

[win32] RP_ShellPropSheetExt: Initial high-DPI support for RFT_LISTDATA.
Basically scaling 32x32 by the DPI value instead of just hard-coding
32x32.

[win32] HiDPI.h: New inline function rp_AdjustSizeForDpi().

src/win32/RP_ShellPropSheetExt.cpp
@@ -1281,8 +1281,7 @@ int RP_ShellPropSheetExt_Private::initListData(HWND hDlg, HWND hWndTab,
 
 		// Icons.
 		if (hasIcons) {
-			// TODO: Ideal icon size?
-			// Using 32x32 for now.
+			// Icon size is 32x32, adjusted for DPI. (TODO: WM_DPICHANGED)
 			// ImageList will resize the original icons to 32x32.
 
 			// NOTE: LVS_REPORT doesn't allow variable row sizes,
@@ -1292,18 +1291,19 @@ int RP_ShellPropSheetExt_Private::initListData(HWND hDlg, HWND hWndTab,
 			// 16 pixels.
 			// TODO: Handle this better.
 			// FIXME: This only works if the RFT_LISTDATA has icons.
-			SIZE szLstIcon = {32, 32};
+			const int px = rp_AdjustSizeForDpi(32, rp_GetDpiForWindow(hDlg));
+			SIZE sizeListIcon = {px, px};
 			bool resizeNeeded = false;
 			float factor = 1.0f;
 			if (nl_max >= 2) {
 				// Two or more newlines.
-				// Add 16px per newline over 1.
-				szLstIcon.cy += (16 * (nl_max - 1));
+				// Add half of the icon size per newline over 1.
+				sizeListIcon.cy += ((px/2) * (nl_max - 1));
 				resizeNeeded = true;
-				factor = (float)szLstIcon.cy / 32.0f;
+				factor = (float)sizeListIcon.cy / (float)sizeListIcon.cy;
 			}
 
-			HIMAGELIST himl = ImageList_Create(szLstIcon.cx, szLstIcon.cy,
+			HIMAGELIST himl = ImageList_Create(sizeListIcon.cx, sizeListIcon.cy,
 				ILC_COLOR32, static_cast<int>(list_data->size()), 0);
 			assert(himl != nullptr);
 			if (himl) {

src/libwin32common/HiDPI.h
@@ -29,6 +29,22 @@ void rp_DpiUnloadModules(void);
 UINT rp_GetDpiForWindow(HWND hWnd);
 
 /**
+ * Adjust any size for DPI.
+ * @param px Size, in pixels.
+ * @param dpi DPI. (96dpi == 1x)
+ * @return Adjusted size.
+ */
+static inline int rp_AdjustSizeForDpi(int px, UINT dpi)
+{
+	if (dpi <= 96) {
+		// 96dpi, or invalid.
+		// Return the original size.
+		return px;
+	}
+	return (px * dpi) / 96;
+}
+
+/**
  * GetSystemMetricsForDpi() implementation.
  * This function was first implemented in Windows 10 v1607,
  * but it's basically just GetSystemMetrics() with scaling.
@@ -39,7 +55,7 @@ UINT rp_GetDpiForWindow(HWND hWnd);
 static inline int rp_GetSystemMetricsForDpi(int nIndex, UINT dpi)
 {
 	int sm = GetSystemMetrics(nIndex);
-	if (sm != 0 && dpi != 96) {
+	if (sm != 0 && dpi > 96) {
 		// Scale using 96dpi as the base value.
 		sm = (sm * dpi) / 96;
 	}

commit cb4b230e772b947400398e2eba56ce0964824206

[win32] RP_ShellPropSheetExt: Fix icon vertical positioning when adjusting for newlines.
This fixes a regression from commit 4a3db1e1da898f50043ae52f27dfab0b520e88a4.
([win32] RP_ShellPropSheetExt: Initial high-DPI support for RFT_LISTDATA.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -1403,7 +1403,7 @@ int RP_ShellPropSheetExt_Private::initListData(HWND hDlg, HWND hWndTab,
 			// Add half of the icon size per newline over 1.
 			sizeListIcon.cy += ((px/2) * (nl_max - 1));
 			resizeNeeded = true;
-			factor = (float)sizeListIcon.cy / (float)sizeListIcon.cy;
+			factor = (float)sizeListIcon.cy / (float)px;
 		}
 
 		HIMAGELIST himl = ImageList_Create(sizeListIcon.cx, sizeListIcon.cy,