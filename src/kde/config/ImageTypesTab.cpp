210e13d144fc26aec89f350c1ffd8d0d5d42f221
David Korth <gerbilsoft@gerbilsoft.com>
2020-02-05 00:37:11 -0500

    ImageTypesTab: Don't localize the numbers 1 through 10.
    This added ten localizable strings with no real benefit, since pretty
    much everyone uses standard Arabic numerals.
    
    Removing localization here also improves performance slightly, since
    we don't have to keep retrieving strings and converting to QString or
    std::wstring.

--- src/kde/config/ImageTypesTab.cpp
+++ src/kde/config/ImageTypesTab.cpp
@@ -242,29 +242,12 @@ void ImageTypesTabPrivate::addComboBoxStrings(unsigned int cbid, int max_prio)
 	if (!cbo)
 		return;
 
-	// Dropdown strings.
-	// NOTE: One more string than the total number of image types,
-	// since we have a string for "No".
-	static const char s_values[][4] = {
-		NOP_C_("ImageTypesTab|Values", "No"),
-		NOP_C_("ImageTypesTab|Values", "1"),
-		NOP_C_("ImageTypesTab|Values", "2"),
-		NOP_C_("ImageTypesTab|Values", "3"),
-		NOP_C_("ImageTypesTab|Values", "4"),
-		NOP_C_("ImageTypesTab|Values", "5"),
-		NOP_C_("ImageTypesTab|Values", "6"),
-		NOP_C_("ImageTypesTab|Values", "7"),
-		NOP_C_("ImageTypesTab|Values", "8"),
-		NOP_C_("ImageTypesTab|Values", "9"),
-		NOP_C_("ImageTypesTab|Values", "10"),
-	};
-	static_assert(ARRAY_SIZE(s_values) == IMG_TYPE_COUNT+1, "s_values[] is the wrong size.");
-
 	// NOTE: Need to add one more than the total number,
 	// since "No" counts as an entry.
-	for (int i = 0; i <= max_prio; i++) {
-		cbo->addItem(U82Q(
-			dpgettext_expr(RP_I18N_DOMAIN, "ImageTypesTab|Values", s_values[i])));
+	assert(max_prio <= IMG_TYPE_COUNT);
+	cbo->addItem(U82Q(C_("ImageTypesTab|Values", "No")));
+	for (int i = 1; i <= max_prio; i++) {
+		cbo->addItem(QString::number(i));
 	}
 	cbo->setCurrentIndex(0);
 }
--- src/win32/config/ImageTypesTab.cpp
+++ src/win32/config/ImageTypesTab.cpp
@@ -371,29 +371,14 @@ void ImageTypesTabPrivate::addComboBoxStrings(unsigned int cbid, int max_prio)
 	if (!cboImageType)
 		return;
 
-	// Dropdown strings.
-	// NOTE: One more string than the total number of image types,
-	// since we have a string for "No".
-	static const char s_values[][4] = {
-		NOP_C_("ImageTypesTab|Values", "No"),
-		NOP_C_("ImageTypesTab|Values", "1"),
-		NOP_C_("ImageTypesTab|Values", "2"),
-		NOP_C_("ImageTypesTab|Values", "3"),
-		NOP_C_("ImageTypesTab|Values", "4"),
-		NOP_C_("ImageTypesTab|Values", "5"),
-		NOP_C_("ImageTypesTab|Values", "6"),
-		NOP_C_("ImageTypesTab|Values", "7"),
-		NOP_C_("ImageTypesTab|Values", "8"),
-		NOP_C_("ImageTypesTab|Values", "9"),
-		NOP_C_("ImageTypesTab|Values", "10"),
-	};
-	static_assert(ARRAY_SIZE(s_values) == IMG_TYPE_COUNT+1, "s_values[] is the wrong size.");
-
 	// NOTE: Need to add one more than the total number,
 	// since "No" counts as an entry.
-	for (int i = 0; i <= max_prio; i++) {
-		ComboBox_AddString(cboImageType, U82T_c(
-			dpgettext_expr(RP_I18N_DOMAIN, "ImageTypesTab|Values", s_values[i])));
+	assert(max_prio <= IMG_TYPE_COUNT);
+	ComboBox_AddString(cboImageType, U82T_c(C_("ImageTypesTab|Values", "No")));
+	for (int i = 1; i <= max_prio; i++) {
+		TCHAR buf[16];
+		_sntprintf(buf, _countof(buf), _T("%d"), i);
+		ComboBox_AddString(cboImageType, buf);
 	}
 	ComboBox_SetCurSel(cboImageType, 0);
 }
