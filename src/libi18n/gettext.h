fabe771298d2fde85adce70ebb5bc08ce8aa5e8e
David Korth <gerbilsoft@gerbilsoft.com>
2020-02-05 23:41:53 -0500

    [libi18n] gettext.h: Updated to the latest version from gettext-0.20.1.
    No actual changes, but the license is now LGPL-2.1+ instead of GPL-3+.
    The version in gettext seems to have always been LGPL, so I'm not sure
    why mine was GPL.
    
    [debian] copyright: Added gettext.h license information.
    - s/poublisher/publisher/
    - Use https for the GNU website.
    - Added LGPL-2.1+.
    - Updated LGPL-2.1 to refer to LGPL, not GPL.
    
    This should have been done in commit b3f17f63180f88c7948b3d615dea0fae74c46249
    ([libi18n] gettext.h: Rebased to gettext-0.20.1's version.)
    but it wasn't for some reason.

--- src/libi18n/gettext.h
+++ src/libi18n/gettext.h
@@ -3,19 +3,19 @@
    Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 2.1 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #ifndef _LIBGETTEXT_H
 #define _LIBGETTEXT_H 1
