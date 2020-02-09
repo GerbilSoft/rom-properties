7a4caf313317a91a88ec7a61bd88bcf2488dddd1
David Korth <gerbilsoft@gerbilsoft.com>
2020-02-08 21:13:32 -0500

    [rp-download] os-secure_openbsd.c: Added support for the older tame() function from OpenBSD 5.8.
    NOTE: Untested, as is the older pledge() variant.
    
    tame() is basically the same, except it takes a bitfield instead of a
    string of pledges. Also, stdio includes fattr, e.g. utimes().

--- src/rp-download/os-secure_openbsd.c
+++ src/rp-download/os-secure_openbsd.c
@@ -14,6 +14,9 @@
 // C includes.
 #include <errno.h>
 #include <unistd.h>
+#ifdef HAVE_TAME
+# include <sys/tame.h>
+#endif /* HAVE_TAME */
 
 /**
  * Enable OS-specific security functionality.
@@ -23,6 +26,7 @@ int rp_download_os_secure(void)
 {
 	int ret;
 
+#if defined(HAVE_PLEDGE)
 	// Promises:
 	// - stdio: General stdio functionality.
 	// - rpath: Read from ~/.config/rom-properties/ and ~/.cache/rom-properties/
@@ -32,13 +36,22 @@ int rp_download_os_secure(void)
 	// - fattr: Modify file attributes, e.g. mtime.
 	// - dns: Resolve hostnames.
 	// - getpw: Get user's home directory if HOME is empty.
-#ifdef HAVE_PLEDGE_EXECPROMISES
+# ifdef HAVE_PLEDGE_EXECPROMISES
 	// OpenBSD 6.3+: Second parameter is `const char *execpromises`.
 	ret = pledge("stdio rpath wpath cpath inet fattr dns getpw", "");
-#else /* !HAVE_PLEDGE_EXECPROMISES */
+# else /* !HAVE_PLEDGE_EXECPROMISES */
 	// OpenBSD 5.9-6.2: Second parameter is `const char *paths[]`.
 	ret = pledge("stdio rpath wpath cpath inet fattr dns getpw", NULL);
-#endif /* HAVE_PLEDGE_EXECPROMISES */
+# endif /* HAVE_PLEDGE_EXECPROMISES */
+
+#elif defined(HAVE_TAME)
+	// OpenBSD 5.8: tame() function.
+	// Similar to pledge(), but it takes a bitfield instead of
+	// a string of pledges.
+	// NOTE: stdio includes fattr, e.g. utimes().
+	tame(TAME_STDIO | TAME_RPATH | TAME_WPATH | TAME_CPATH |
+	     TAME_INET | TAME_DNS | TAME_GETPW);
+#endif
 
 	return (ret == 0 ? 0 : -errno);
 }
