GerbilSoft/rom-properties

commit e1f947e22c9aad251fa24aa7d4aca8920e333ca5

[libromdata] NEResourceReader: key_len is always > 0 at this point.
We already check for an empty key and return an error if found.

Found using LGTM. (cpp/constant-comparison)

Comparison is always true because key_len >= 1.
This alert was introduced in 4f9daf7 14 days ago

src/libromdata/disc/NEResourceReader.cpp
@@ -452,16 +452,15 @@ int NEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
 			return -EIO;
 		}
 
-		string key_utf8, value_utf8;
-		if (key_len > 0) {
-			key_utf8 = cpN_to_utf8(codepage, key, key_len);
-			assert(!key_utf8.empty());
-			if (key_utf8.empty()) {
-				// Code page conversion failed.
-				// Default to 1252.
-				key_utf8 = cp1252_to_utf8(key, key_len);
-			}
+		const string key_utf8 = cpN_to_utf8(codepage, key, key_len);
+		assert(!key_utf8.empty());
+		if (key_utf8.empty()) {
+			// Code page conversion failed.
+			// Default to 1252.
+			key_utf8 = cp1252_to_utf8(key, key_len);
 		}
+
+		string value_utf8;
 		if (value_len > 0) {
 			value_utf8 = cpN_to_utf8(codepage, value, value_len);
 			assert(!value_utf8.empty());

commit 4b256e0e681774b181accbb8d3b45b232e761640

[libromdata] NEResourceReader: We can't mark key_utf8 as const.
If the specified code page doesn't work, we try again with cp1252,
so key_utf8 can't be marked as const.

This fixes a regression from commit e1f947e22c9aad251fa24aa7d4aca8920e333ca5.
([libromdata] NEResourceReader: key_len is always > 0 at this point.)

src/libromdata/disc/NEResourceReader.cpp
@@ -452,7 +452,7 @@ int NEResourceReaderPrivate::load_StringTable(IRpFile *file, IResourceReader::St
 			return -EIO;
 		}
 
-		const string key_utf8 = cpN_to_utf8(codepage, key, key_len);
+		string key_utf8 = cpN_to_utf8(codepage, key, key_len);
 		assert(!key_utf8.empty());
 		if (key_utf8.empty()) {
 			// Code page conversion failed.