GerbilSoft/rom-properties

commit ba71e97f5d2576f49c1b79f269d0a88f83704320

[libromdata] MachOData: Fix CPU_SUBTYPE_XEON without CPU_SUBTYPE_XEON_MP.
PVS-Studio warning:

V523 (High): The 'then' statement is equivalent to the 'else' statement.

src/libromdata/data/MachOData.cpp
@@ -191,7 +191,6 @@ const char *MachOData::lookup_cpu_subtype(uint32_t cputype, uint32_t cpusubtype)
 						if (cpusubtype != CPU_SUBTYPE_XEON_MP) {
 							s_cpu_subtype = "Xeon MP";
 						} else {
-							s_cpu_subtype = "Xeon MP";
 						}
 						break;
 				}

commit 4f6b3edaac912ff6ccbc12fb97834a9a2ee6b9aa

[libromdata] MachOData.cpp: Really fix CPU_SUBTYPE_XEON without CPU_SUBTYPE_XEON_MP.
It ended up setting the subtype to "Xeon MP" if it wasn't, and no
subtype if it was.

This fixes a regression from commit ba71e97f5d2576f49c1b79f269d0a88f83704320.
([libromdata] MachOData: Fix CPU_SUBTYPE_XEON without CPU_SUBTYPE_XEON_MP.)

Found using LGTM. (cpp/empty-block)

Empty block without comment
This alert was introduced in ba71e97 a year ago

src/libromdata/data/MachOData.cpp
@@ -171,9 +171,8 @@ const char *MachOData::lookup_cpu_subtype(uint32_t cputype, uint32_t cpusubtype)
 						break;
 
 					case CPU_SUBTYPE_XEON:
-						if (cpusubtype != CPU_SUBTYPE_XEON_MP) {
+						if (cpusubtype == CPU_SUBTYPE_XEON_MP) {
 							s_cpu_subtype = "Xeon MP";
-						} else {
 						}
 						break;
 				}