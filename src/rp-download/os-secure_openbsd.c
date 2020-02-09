81f6bfa6ffb15a5537e025b97456742fbafd84d2
David Korth <gerbilsoft@gerbilsoft.com>
2020-02-08 21:00:50 -0500

    [rp-download] os-secure_openbsd.c: Added support for the older pledge() from OpenBSD 5.9-6.2.
    This version took `const char *paths[]`, which ended up not being used
    for anything.

--- src/rp-download/os-secure_openbsd.c
+++ src/rp-download/os-secure_openbsd.c
@@ -7,6 +7,8 @@
  ***************************************************************************/
 
 #include "stdafx.h"
+
+#include "config.rp-download.h"
 #include "os-secure.h"
 
 // C includes.
@@ -19,6 +21,8 @@
  */
 int rp_download_os_secure(void)
 {
+	int ret;
+
 	// Promises:
 	// - stdio: General stdio functionality.
 	// - rpath: Read from ~/.config/rom-properties/ and ~/.cache/rom-properties/
@@ -27,6 +31,13 @@ int rp_download_os_secure(void)
 	// - inet: Internet access.
 	// - fattr: Modify file attributes, e.g. mtime.
 	// - dns: Resolve hostnames.
-	int ret = pledge("stdio rpath wpath cpath inet fattr dns", "");
+#ifdef HAVE_PLEDGE_EXECPROMISES
+	// OpenBSD 6.3+: Second parameter is `const char *execpromises`.
+	ret = pledge("stdio rpath wpath cpath inet fattr dns", "");
+#else /* !HAVE_PLEDGE_EXECPROMISES */
+	// OpenBSD 5.9-6.2: Second parameter is `const char *paths[]`.
+	ret = pledge("stdio rpath wpath cpath inet fattr dns", NULL);
+#endif /* HAVE_PLEDGE_EXECPROMISES */
+
 	return (ret == 0 ? 0 : -errno);
 }
