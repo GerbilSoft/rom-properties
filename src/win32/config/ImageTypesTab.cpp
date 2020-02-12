GerbilSoft/rom-properties

commit fdcd884513caaa680e58023d244371f993e3431e

Moved the FileSystem::rmkdir() call from ConfReader to rp-config.
There's no point in creating the directory on access. It should only be
created on save.

This allows us to remove mkdir() from rpcli's seccomp filter.
(Not committed yet, but it's in progress...)

[win32] OptionsTab.cpp: Convert the filename from UTF-8 to TCHAR *once*
instead of once per setting.

src/win32/config/ImageTypesTab.cpp
@@ -424,6 +424,15 @@ int ImageTypesTabPrivate::saveStart(void)
 		return -ENOENT;
 	}
 
+	// Make sure the configuration directory exists.
+	// NOTE: The filename portion MUST be kept in config_path,
+	// since the last component is ignored by rmkdir().
+	int ret = FileSystem::rmkdir(filename);
+	if (ret != 0) {
+		// rmkdir() failed.
+		return;
+	}
+
 	// Store the configuration filename.
 	assert(tmp_conf_filename.empty());
 	tmp_conf_filename = U82T_s(filename);

src/win32/config/OptionsTab.cpp
@@ -212,23 +212,34 @@ void OptionsTabPrivate::save(void)
 		return;
 	}
 
+	// Make sure the configuration directory exists.
+	// NOTE: The filename portion MUST be kept in config_path,
+	// since the last component is ignored by rmkdir().
+	int ret = FileSystem::rmkdir(filename);
+	if (ret != 0) {
+		// rmkdir() failed.
+		return;
+	}
+
+	const tstring tfilename = U82T_c(filename);
+
 	const TCHAR *btstr = bstCheckedToBoolString(IsDlgButtonChecked(hWndPropSheet, IDC_EXTIMGDL));
-	WritePrivateProfileString(_T("Downloads"), _T("ExtImageDownload"), btstr, U82T_c(filename));
+	WritePrivateProfileString(_T("Downloads"), _T("ExtImageDownload"), btstr, tfilename.c_str());
 
 	btstr = bstCheckedToBoolString(IsDlgButtonChecked(hWndPropSheet, IDC_INTICONSMALL));
-	WritePrivateProfileString(_T("Downloads"), _T("UseIntIconForSmallSizes"), btstr, U82T_c(filename));
+	WritePrivateProfileString(_T("Downloads"), _T("UseIntIconForSmallSizes"), btstr, tfilename.c_str());
 
 	btstr = bstCheckedToBoolString(IsDlgButtonChecked(hWndPropSheet, IDC_HIGHRESDL));
-	WritePrivateProfileString(_T("Downloads"), _T("DownloadHighResScans"), btstr, U82T_c(filename));
+	WritePrivateProfileString(_T("Downloads"), _T("DownloadHighResScans"), btstr, tfilename.c_str());
 
 	btstr = bstCheckedToBoolString(IsDlgButtonChecked(hWndPropSheet, IDC_STOREFILEORIGININFO));
-	WritePrivateProfileString(_T("Downloads"), _T("StoreFileOriginInfo"), btstr, U82T_c(filename));
+	WritePrivateProfileString(_T("Downloads"), _T("StoreFileOriginInfo"), btstr, tfilename.c_str());
 
 	btstr = bstCheckedToBoolString(IsDlgButtonChecked(hWndPropSheet, IDC_DANGEROUSPERMISSIONS));
-	WritePrivateProfileString(_T("Options"), _T("ShowDangerousPermissionsOverlayIcon"), btstr, U82T_c(filename));
+	WritePrivateProfileString(_T("Options"), _T("ShowDangerousPermissionsOverlayIcon"), btstr, tfilename.c_str());
 
 	btstr = bstCheckedToBoolString(IsDlgButtonChecked(hWndPropSheet, IDC_ENABLETHUMBNAILONNETWORKFS));
-	WritePrivateProfileString(_T("Options"), _T("EnableThumbnailOnNetworkFS"), btstr, U82T_c(filename));
+	WritePrivateProfileString(_T("Options"), _T("EnableThumbnailOnNetworkFS"), btstr, tfilename.c_str());
 
 	// No longer changed.
 	changed = false;

src/kde/config/ConfigDialog.cpp
@@ -11,7 +11,8 @@
 
 // librpbase
 #include "librpbase/config/Config.hpp"
-using LibRpBase::Config;
+#include "librpbase/file/FileSystem.hpp"
+using namespace LibRpBase;
 
 // libi18n
 #include "libi18n/i18n.h"
@@ -189,7 +190,7 @@ ConfigDialog::ConfigDialog(QWidget *parent)
 }
 
 /**
- * Shut down the save file editor.
+ * Shut down the configuration dialog.
  */
 ConfigDialog::~ConfigDialog()
 {
@@ -279,6 +280,15 @@ void ConfigDialog::apply(void)
 	assert(filename != nullptr);
 	if (!filename) {
 		// No configuration filename...
+		return;
+	}
+
+	// Make sure the configuration directory exists.
+	// NOTE: The filename portion MUST be kept in config_path,
+	// since the last component is ignored by rmkdir().
+	int ret = FileSystem::rmkdir(filename);
+	if (ret != 0) {
+		// rmkdir() failed.
 		return;
 	}
 

src/librpbase/config/ConfReader.cpp
@@ -134,16 +134,6 @@ int ConfReader::load(bool force)
 			}
 			d->conf_filename += d->conf_rel_filename;
 		}
-
-		// Make sure the configuration directory exists.
-		// NOTE: The filename portion MUST be kept in config_path,
-		// since the last component is ignored by rmkdir().
-		int ret = FileSystem::rmkdir(d->conf_filename);
-		if (ret != 0) {
-			// rmkdir() failed.
-			d->conf_filename.clear();
-			return -ENOENT;
-		}
 	} else if (!force && d->conf_was_found) {
 		// Check if the keys.conf timestamp has changed.
 		// NOTE: Second check once the mutex is locked.

src/win32/config/KeyManagerTab.cpp
@@ -523,6 +523,15 @@ void KeyManagerTabPrivate::save(void)
 		return;
 	}
 
+	// Make sure the configuration directory exists.
+	// NOTE: The filename portion MUST be kept in config_path,
+	// since the last component is ignored by rmkdir().
+	int ret = FileSystem::rmkdir(filename);
+	if (ret != 0) {
+		// rmkdir() failed.
+		return;
+	}
+
 	// Save the keys.
 	const tstring tfilename = U82T_c(filename);
 	const int totalKeyCount = keyStore->totalKeyCount();

commit ae9c39ef27cab01921bac0e2a59cf13c129b9fd7

[win32] ImageTypesTab, OptionsTab: Fix rmkdir() issues.
This fixes a regression from commit fdcd884513caaa680e58023d244371f993e3431e.
(Moved the FileSystem::rmkdir() call from ConfReader to rp-config.)

src/win32/config/ImageTypesTab.cpp
@@ -430,7 +430,7 @@ int ImageTypesTabPrivate::saveStart(void)
 	int ret = FileSystem::rmkdir(filename);
 	if (ret != 0) {
 		// rmkdir() failed.
-		return;
+		return -EIO;
 	}
 
 	// Store the configuration filename.

src/win32/config/OptionsTab.cpp
@@ -11,7 +11,7 @@
 #include "res/resource.h"
 
 // librpbase
-using LibRpBase::Config;
+using namespace LibRpBase;
 
 // C++ STL classes.
 using std::tstring;