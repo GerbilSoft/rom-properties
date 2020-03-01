GerbilSoft/rom-properties

commit 7ee118465034349de1846efc80ec5a1df815048d

[libromdata] IDiscReader: Moved m_file / m_discReader to the base class.
Every subclass has one or the other, and NCCHReader might have
either one, so move it to the common base class so we can
de-virtualize some functions.

IDiscReader's IRpFile* constructor now takes a reference to the
file and unreferences it in the destructor.

IDiscReader's IDiscReader* constructor doesn't take any reference,
since IDiscReader doesn't support references right now.
- TODO: Add references?

IDiscReader::isDevice() is now a standard function.

GcnPartitionPrivate: Moved offsetShift to reduce padding.

Code size reduction for `rpcli`: (64-bit Gentoo Linux, gcc-8.2.0-r6, release build)

   text    data     bss     dec     hex filename
1049769   98377   17456 1165602  11c922 bin/rpcli [before]
1048285   98305   17456 1164046  11c30e bin/rpcli [after]
  -1484     -72       0   -1556    -614 Difference

src/libromdata/disc/XDVDFSPartition.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 class XDVDFSPartitionPrivate
 {
 	public:
-		XDVDFSPartitionPrivate(XDVDFSPartition *q, LibRpBase::IDiscReader *discReader,
+		XDVDFSPartitionPrivate(XDVDFSPartition *q,
 			int64_t partition_offset, int64_t partition_size);
 		~XDVDFSPartitionPrivate();
 
@@ -55,8 +55,6 @@ class XDVDFSPartitionPrivate
 		XDVDFSPartition *const q_ptr;
 
 	public:
-		LibRpBase::IDiscReader *discReader;
-
 		// Partition start offset. (in bytes)
 		int64_t partition_offset;
 		int64_t partition_size;		// Calculated partition size.
@@ -88,29 +86,33 @@ class XDVDFSPartitionPrivate
 
 /** XDVDFSPartitionPrivate **/
 
-XDVDFSPartitionPrivate::XDVDFSPartitionPrivate(XDVDFSPartition *q, IDiscReader *discReader,
+XDVDFSPartitionPrivate::XDVDFSPartitionPrivate(XDVDFSPartition *q,
 	int64_t partition_offset, int64_t partition_size)
 	: q_ptr(q)
-	, discReader(discReader)
 	, partition_offset(partition_offset)
 	, partition_size(partition_size)
 {
 	// Clear the XDVDFS header struct.
 	memset(&xdvdfsHeader, 0, sizeof(xdvdfsHeader));
 
-	if (!discReader->isOpen()) {
-		q->m_lastError = discReader->lastError();
-		this->discReader = nullptr;
+	if (!q->m_discReader) {
+		q->m_lastError = EIO;
 		return;
+	} else if (!q->m_discReader->isOpen()) {
+		q->m_lastError = q->m_discReader->lastError();
+		if (q->m_lastError == 0) {
+			q->m_lastError = EIO;
+		}
+		q->m_discReader = nullptr;
 	}
 
 	// Load the XDVDFS header.
-	size_t size = discReader->seekAndRead(partition_offset + (XDVDFS_HEADER_LBA_OFFSET * XDVDFS_BLOCK_SIZE),
+	size_t size = q->m_discReader->seekAndRead(partition_offset + (XDVDFS_HEADER_LBA_OFFSET * XDVDFS_BLOCK_SIZE),
 		&xdvdfsHeader, sizeof(xdvdfsHeader));
 	if (size != sizeof(xdvdfsHeader)) {
 		// Seek and/or read error.
 		memset(&xdvdfsHeader, 0, sizeof(xdvdfsHeader));
-		this->discReader = nullptr;
+		q->m_discReader = nullptr;
 		return;
 	}
 
@@ -120,7 +122,7 @@ XDVDFSPartitionPrivate::XDVDFSPartitionPrivate(XDVDFSPartition *q, IDiscReader *
 	{
 		// Invalid XDVDFS header.
 		memset(&xdvdfsHeader, 0, sizeof(xdvdfsHeader));
-		this->discReader = nullptr;
+		q->m_discReader = nullptr;
 		return;
 	}
 
@@ -176,7 +178,7 @@ int XDVDFSPartitionPrivate::loadRootDirectory(void)
 	if (unlikely(!rootDir_data.empty())) {
 		// Root directory is already loaded.
 		return 0;
-	} else if (unlikely(!this->discReader)) {
+	} else if (unlikely(!q->m_discReader)) {
 		// DiscReader isn't open.
 		q->m_lastError = EIO;
 		return -q->m_lastError;
@@ -200,11 +202,11 @@ int XDVDFSPartitionPrivate::loadRootDirectory(void)
 	const int64_t rootDir_addr = partition_offset +
 		static_cast<int64_t>(xdvdfsHeader.root_dir_sector) *
 		XDVDFS_BLOCK_SIZE;
-	size_t size = discReader->seekAndRead(rootDir_addr, rootDir_data.data(), rootDir_data.size());
+	size_t size = q->m_discReader->seekAndRead(rootDir_addr, rootDir_data.data(), rootDir_data.size());
 	if (size != rootDir_data.size()) {
 		// Seek and/or read error.
 		rootDir_data.clear();
-		q->m_lastError = discReader->lastError();
+		q->m_lastError = q->m_discReader->lastError();
 		if (q->m_lastError == 0) {
 			q->m_lastError = EIO;
 		}
@@ -228,7 +230,8 @@ int XDVDFSPartitionPrivate::loadRootDirectory(void)
  * @param partition_size Partition size.
  */
 XDVDFSPartition::XDVDFSPartition(IDiscReader *discReader, int64_t partition_offset, int64_t partition_size)
-	: d_ptr(new XDVDFSPartitionPrivate(this, discReader, partition_offset, partition_size))
+	: super(discReader)
+	, d_ptr(new XDVDFSPartitionPrivate(this, partition_offset, partition_size))
 { }
 
 XDVDFSPartition::~XDVDFSPartition()
@@ -245,8 +248,7 @@ XDVDFSPartition::~XDVDFSPartition()
  */
 bool XDVDFSPartition::isOpen(void) const
 {
-	RP_D(const XDVDFSPartition);
-	return (d->discReader && d->discReader->isOpen());
+	return (m_discReader && m_discReader->isOpen());
 }
 
 /**
@@ -257,17 +259,16 @@ bool XDVDFSPartition::isOpen(void) const
  */
 size_t XDVDFSPartition::read(void *ptr, size_t size)
 {
-	RP_D(XDVDFSPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader || !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader || !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return 0;
 	}
 
-	// GCN partitions are stored as-is.
+	// XDVDFS partitions are stored as-is.
 	// TODO: data_size checks?
-	return d->discReader->read(ptr, size);
+	return m_discReader->read(ptr, size);
 }
 
 /**
@@ -278,16 +279,16 @@ size_t XDVDFSPartition::read(void *ptr, size_t size)
 int XDVDFSPartition::seek(int64_t pos)
 {
 	RP_D(XDVDFSPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader ||  !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader ||  !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
 
-	int ret = d->discReader->seek(d->partition_offset + pos);
+	int ret = m_discReader->seek(d->partition_offset + pos);
 	if (ret != 0) {
-		m_lastError = d->discReader->lastError();
+		m_lastError = m_discReader->lastError();
 	}
 	return ret;
 }
@@ -299,16 +300,16 @@ int XDVDFSPartition::seek(int64_t pos)
 int64_t XDVDFSPartition::tell(void)
 {
 	RP_D(XDVDFSPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader ||  !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader ||  !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
 
-	int64_t ret = d->discReader->tell() - d->partition_offset;
+	int64_t ret = m_discReader->tell() - d->partition_offset;
 	if (ret < 0) {
-		m_lastError = d->discReader->lastError();
+		m_lastError = m_discReader->lastError();
 	}
 	return ret;
 }
@@ -323,7 +324,7 @@ int64_t XDVDFSPartition::size(void)
 {
 	// TODO: Restrict partition size?
 	RP_D(const XDVDFSPartition);
-	if (!d->discReader)
+	if (!m_discReader)
 		return -1;
 	return d->partition_size;
 }
@@ -339,7 +340,7 @@ int64_t XDVDFSPartition::partition_size(void) const
 {
 	// TODO: Restrict partition size?
 	RP_D(const XDVDFSPartition);
-	if (!d->discReader)
+	if (!m_discReader)
 		return -1;
 	return d->partition_size;
 }
@@ -357,21 +358,9 @@ int64_t XDVDFSPartition::partition_size_used(void) const
 	return partition_size();
 }
 
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool XDVDFSPartition::isDevice(void) const
-{
-	RP_D(const XDVDFSPartition);
-	return (d->discReader && d->discReader->isDevice());
-}
-
 /** XDVDFSPartition **/
 
-/** GcnFst wrapper functions. **/
+/** IFst wrapper functions. **/
 
 // TODO
 
@@ -552,9 +541,9 @@ IRpFile *XDVDFSPartition::open(const char *filename)
 time_t XDVDFSPartition::xdvdfsTimestamp(void) const
 {
 	RP_D(const XDVDFSPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader ||  !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader ||  !m_discReader->isOpen()) {
 		// XDVDFS isn't loaded.
 		const_cast<XDVDFSPartition*>(this)->m_lastError = EBADF;
 		return -1;

src/libromdata/disc/CIAReader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * CIAReader.cpp: Nintendo 3DS CIA reader.                                 *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -43,7 +43,7 @@ namespace LibRomData {
 class CIAReaderPrivate
 {
 	public:
-		CIAReaderPrivate(CIAReader *q, LibRpBase::IRpFile *file,
+		CIAReaderPrivate(CIAReader *q,
 			int64_t content_offset, uint32_t content_length,
 			const N3DS_Ticket_t *ticket,
 			uint16_t tmd_content_index);
@@ -55,7 +55,6 @@ class CIAReaderPrivate
 		CIAReader *const q_ptr;
 
 	public:
-		LibRpBase::IRpFile *file;	// 3DS CIA file.
 		CBCReader *cbcReader;		// CBC reader.
 
 #ifdef ENABLE_DECRYPTION
@@ -68,11 +67,10 @@ class CIAReaderPrivate
 
 /** CIAReaderPrivate **/
 
-CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
+CIAReaderPrivate::CIAReaderPrivate(CIAReader *q,
 	int64_t content_offset, uint32_t content_length,
 	const N3DS_Ticket_t *ticket, uint16_t tmd_content_index)
 	: q_ptr(q)
-	, file(file)
 	, cbcReader(nullptr)
 #ifdef ENABLE_DECRYPTION
 	, titleKeyEncIdx(0)
@@ -83,11 +81,17 @@ CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
 	RP_UNUSED(tmd_content_index);
 #endif /* ENABLE_DECRYPTION */
 
+	assert(q->m_file != nullptr);
+	if (!q->m_file) {
+		// No file...
+		return;
+	}
+
 	assert(ticket != nullptr);
 	if (!ticket) {
 		// No ticket. Assuming no encryption.
 		// Create a passthru CBCReader anyway.
-		cbcReader = new CBCReader(file, content_offset, content_length, nullptr, nullptr);
+		cbcReader = new CBCReader(q->m_file, content_offset, content_length, nullptr, nullptr);
 		return;
 	}
 
@@ -175,17 +179,19 @@ CIAReaderPrivate::CIAReaderPrivate(CIAReader *q, IRpFile *file,
 		memset(&cia_iv.u8[2], 0, sizeof(cia_iv.u8)-2);
 
 		// Create a CBC reader to decrypt the CIA.
-		cbcReader = new CBCReader(file, content_offset, content_length, title_key, cia_iv.u8);
+		cbcReader = new CBCReader(q->m_file, content_offset, content_length, title_key, cia_iv.u8);
 	} else {
 		// Unable to get the CIA encryption keys.
 		// TODO: Set an error.
 		//verifyResult = res;
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 	}
 #else /* !ENABLE_DECRYPTION */
 	// Cannot decrypt the CIA.
 	// TODO: Set an error.
-	this->file = nullptr;
+	q->m_file->unref();
+	q->m_file = nullptr;
 #endif /* ENABLE_DECRYPTION */
 }
 
@@ -212,7 +218,8 @@ CIAReader::CIAReader(IRpFile *file,
 		int64_t content_offset, uint32_t content_length,
 		const N3DS_Ticket_t *ticket,
 		uint16_t tmd_content_index)
-	: d_ptr(new CIAReaderPrivate(this, file, content_offset, content_length, ticket, tmd_content_index))
+	: super(file)
+	, d_ptr(new CIAReaderPrivate(this, content_offset, content_length, ticket, tmd_content_index))
 { }
 
 CIAReader::~CIAReader()
@@ -229,8 +236,7 @@ CIAReader::~CIAReader()
  */
 bool CIAReader::isOpen(void) const
 {
-	RP_D(const CIAReader);
-	return (d->file && d->file->isOpen());
+	return (m_file && m_file->isOpen());
 }
 
 /**
@@ -243,13 +249,13 @@ size_t CIAReader::read(void *ptr, size_t size)
 {
 	RP_D(CIAReader);
 	assert(ptr != nullptr);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
 	if (!ptr) {
 		m_lastError = EINVAL;
 		return 0;
-	} else if (!d->file || !d->file->isOpen() || !d->cbcReader) {
+	} else if (!m_file || !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return 0;
 	} else if (size == 0) {
@@ -270,10 +276,10 @@ size_t CIAReader::read(void *ptr, size_t size)
 int CIAReader::seek(int64_t pos)
 {
 	RP_D(CIAReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -292,9 +298,9 @@ int CIAReader::seek(int64_t pos)
 int64_t CIAReader::tell(void)
 {
 	RP_D(const CIAReader);
-	assert(d->file != nullptr);
+	assert(m_file != nullptr);
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -313,10 +319,10 @@ int64_t CIAReader::tell(void)
 int64_t CIAReader::size(void)
 {
 	RP_D(const CIAReader);
-	assert(d->file != nullptr);
-	assert(d->file->isOpen());
+	assert(m_file != nullptr);
+	assert(m_file->isOpen());
 	assert(d->cbcReader != nullptr);
-	if (!d->file ||  !d->file->isOpen() || !d->cbcReader) {
+	if (!m_file ||  !m_file->isOpen() || !d->cbcReader) {
 		m_lastError = EBADF;
 		return -1;
 	}
@@ -324,18 +330,6 @@ int64_t CIAReader::size(void)
 	int64_t ret = d->cbcReader->size();
 	m_lastError = d->cbcReader->lastError();
 	return ret;
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool CIAReader::isDevice(void) const
-{
-	RP_D(const CIAReader);
-	return (d->file && d->file->isDevice());
 }
 
 /** IPartition **/

src/libromdata/disc/CIAReader.hpp
@@ -102,15 +102,6 @@ class CIAReader : public LibRpBase::IPartition
 		int64_t size(void) final;
 
 	public:
-		/** Device file functions **/
-
-		/**
-		 * Is the underlying file a device file?
-		 * @return True if the underlying file is a device file; false if not.
-		 */
-		bool isDevice(void) const final;
-
-	public:
 		/** IPartition **/
 
 		/**

src/libromdata/disc/Cdrom2352Reader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file);
+		Cdrom2352ReaderPrivate(Cdrom2352Reader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -68,22 +68,22 @@ class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 const uint8_t Cdrom2352ReaderPrivate::CDROM_2352_MAGIC[12] =
 	{0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00};
 
-Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file)
-	: super(q, file)
+Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q)
+	: super(q)
 	, blockCount(0)
 {
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
 	// Check the disc size.
 	// Should be a multiple of 2352.
-	int64_t fileSize = file->size();
+	int64_t fileSize = q->m_file->size();
 	if (fileSize <= 0 || fileSize % 2352 != 0) {
 		// Invalid disc size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -101,7 +101,7 @@ Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, IRpFile *file
 /** Cdrom2352Reader **/
 
 Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
-	: super(new Cdrom2352ReaderPrivate(this, file))
+	: super(new Cdrom2352ReaderPrivate(this), file)
 { }
 
 /**

src/libromdata/disc/Cdrom2352Reader.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * Cdrom2352Reader.hpp: CD-ROM reader for 2352-byte sector images.         *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *

src/libromdata/disc/CisoGcnReader.cpp
@@ -45,7 +45,7 @@ namespace LibRomData {
 
 class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file);
+		CisoGcnReaderPrivate(CisoGcnReader *q);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -72,8 +72,8 @@ class CisoGcnReaderPrivate : public SparseDiscReaderPrivate {
 // CISO magic.
 const char CisoGcnReaderPrivate::CISO_MAGIC[4] = {'C','I','S','O'};
 
-CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
-	: super(q, file)
+CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q)
+	: super(q)
 	, maxLogicalBlockUsed(-1)
 {
 	// Clear the CISO header struct.
@@ -81,7 +81,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Clear the CISO block map initially.
 	blockMap.fill(0xFFFF);
 
-	if (!this->file) {
+	if (!q->m_file) {
 		// File could not be ref()'d.
 		return;
 	}
@@ -89,12 +89,12 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Read the CISO header.
 	static_assert(sizeof(CISOHeader) == CISO_HEADER_SIZE,
 		"CISOHeader is the wrong size. (Should be 32,768 bytes.)");
-	this->file->rewind();
-	size_t sz = this->file->read(&cisoHeader, sizeof(cisoHeader));
+	q->m_file->rewind();
+	size_t sz = q->m_file->read(&cisoHeader, sizeof(cisoHeader));
 	if (sz != sizeof(cisoHeader)) {
 		// Error reading the CISO header.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -102,8 +102,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 	// Verify the CISO header.
 	if (memcmp(cisoHeader.magic, CISO_MAGIC, sizeof(CISO_MAGIC)) != 0) {
 		// Invalid magic.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -117,8 +117,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 		// If the block size is 0x18, then this is
 		// actually a PSP CISO, and this field is
 		// the CISO header size.
-		this->file->unref();
-		this->file = nullptr;
+		q->m_file->unref();
+		q->m_file = nullptr;
 		q->m_lastError = EIO;
 		return;
 	}
@@ -138,8 +138,8 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 				break;
 			default:
 				// Invalid entry.
-				this->file->unref();
-				this->file = nullptr;
+				q->m_file->unref();
+				q->m_file = nullptr;
 				q->m_lastError = EIO;
 				return;
 		}
@@ -155,7 +155,7 @@ CisoGcnReaderPrivate::CisoGcnReaderPrivate(CisoGcnReader *q, IRpFile *file)
 /** CisoGcnReader **/
 
 CisoGcnReader::CisoGcnReader(IRpFile *file)
-	: super(new CisoGcnReaderPrivate(this, file))
+	: super(new CisoGcnReaderPrivate(this), file)
 { }
 
 /**

src/libromdata/disc/GcnPartition.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * GcnPartition.cpp: GameCube partition reader.                            *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -53,7 +53,8 @@ namespace LibRomData {
  * @param partition_offset Partition start offset.
  */
 GcnPartition::GcnPartition(IDiscReader *discReader, int64_t partition_offset)
-	: d_ptr(new GcnPartitionPrivate(this, discReader, partition_offset))
+	: super(discReader)
+	, d_ptr(new GcnPartitionPrivate(this, partition_offset))
 { }
 
 GcnPartition::~GcnPartition()
@@ -64,9 +65,11 @@ GcnPartition::~GcnPartition()
 /**
  * Construct a GcnPartition. (subclass version)
  * @param d GcnPartitionPrivate subclass.
+ * @param discReader IDiscReader.
  */
-GcnPartition::GcnPartition(GcnPartitionPrivate *d)
-	: d_ptr(d)
+GcnPartition::GcnPartition(GcnPartitionPrivate *d, IDiscReader *discReader)
+	: super(discReader)
+	, d_ptr(d)
 { }
 
 /** IDiscReader **/
@@ -78,8 +81,7 @@ GcnPartition::GcnPartition(GcnPartitionPrivate *d)
  */
 bool GcnPartition::isOpen(void) const
 {
-	RP_D(const GcnPartition);
-	return (d->discReader && d->discReader->isOpen());
+	return (m_discReader && m_discReader->isOpen());
 }
 
 /**
@@ -90,17 +92,16 @@ bool GcnPartition::isOpen(void) const
  */
 size_t GcnPartition::read(void *ptr, size_t size)
 {
-	RP_D(GcnPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader || !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader || !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return 0;
 	}
 
 	// GCN partitions are stored as-is.
 	// TODO: data_size checks?
-	return d->discReader->read(ptr, size);
+	return m_discReader->read(ptr, size);
 }
 
 /**
@@ -111,17 +112,17 @@ size_t GcnPartition::read(void *ptr, size_t size)
 int GcnPartition::seek(int64_t pos)
 {
 	RP_D(GcnPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader ||  !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader ||  !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
 
 	// Use the IDiscReader directly for GCN partitions.
-	int ret = d->discReader->seek(d->data_offset + pos);
+	int ret = m_discReader->seek(d->data_offset + pos);
 	if (ret != 0) {
-		m_lastError = d->discReader->lastError();
+		m_lastError = m_discReader->lastError();
 	}
 	return ret;
 }
@@ -132,18 +133,17 @@ int GcnPartition::seek(int64_t pos)
  */
 int64_t GcnPartition::tell(void)
 {
-	RP_D(GcnPartition);
-	assert(d->discReader != nullptr);
-	assert(d->discReader->isOpen());
-	if (!d->discReader ||  !d->discReader->isOpen()) {
+	assert(m_discReader != nullptr);
+	assert(m_discReader->isOpen());
+	if (!m_discReader || !m_discReader->isOpen()) {
 		m_lastError = EBADF;
 		return -1;
 	}
 
 	// Use the IDiscReader directly for GCN partitions.
-	int64_t ret = d->discReader->tell();
+	int64_t ret = m_discReader->tell();
 	if (ret < 0) {
-		m_lastError = d->discReader->lastError();
+		m_lastError = m_discReader->lastError();
 	}
 	return ret;
 }
@@ -159,18 +159,6 @@ int64_t GcnPartition::size(void)
 	// TODO: Errors?
 	RP_D(const GcnPartition);
 	return d->data_size;
-}
-
-/** Device file functions **/
-
-/**
- * Is the underlying file a device file?
- * @return True if the underlying file is a device file; false if not.
- */
-bool GcnPartition::isDevice(void) const
-{
-	RP_D(const GcnPartition);
-	return (d->discReader && d->discReader->isDevice());
 }
 
 /** IPartition **/

src/libromdata/disc/GcnPartition.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * GcnPartition.hpp: GameCube partition reader.                            *
  *                                                                         *
- * Copyright (c) 2016-2018 by David Korth.                                 *
+ * Copyright (c) 2016-2019 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -45,10 +45,10 @@ class GcnPartition : public LibRpBase::IPartition
 		 * @param discReader IDiscReader.
 		 * @param partition_offset Partition start offset.
 		 */
-		GcnPartition(IDiscReader *discReader, int64_t partition_offset);
+		explicit GcnPartition(IDiscReader *discReader, int64_t partition_offset);
 		virtual ~GcnPartition();
 	protected:
-		explicit GcnPartition(GcnPartitionPrivate *d);
+		explicit GcnPartition(GcnPartitionPrivate *d, IDiscReader *discReader);
 
 	private:
 		typedef IPartition super;
@@ -96,15 +96,6 @@ class GcnPartition : public LibRpBase::IPartition
 		 * @return Data size, or -1 on error.
 		 */
 		int64_t size(void) final;
-
-	public:
-		/** Device file functions **/
-
-		/**
-		 * Is the underlying file a device file?
-		 * @return True if the underlying file is a device file; false if not.
-		 */
-		bool isDevice(void) const final;
 
 	public:
 		/** IPartition **/

commit a1fed809ddc309206b72604124105919898b4306

[libromdata] XDVDFSPartition private class constructor: Return if the DiscReader isn't open.
This fixes a regression from commit 7ee118465034349de1846efc80ec5a1df815048d.
([libromdata] IDiscReader: Moved m_file / m_discReader to the base class.)

NOTE: This bug is present in v1.4, but it likely won't cause any issues
in practice.

This fixes the following Coverity Scan bugs:
- #1483985: Explicit null dereferenced

src/libromdata/disc/XDVDFSPartition.cpp
@@ -93,6 +93,7 @@ XDVDFSPartitionPrivate::XDVDFSPartitionPrivate(XDVDFSPartition *q,
 			q->m_lastError = EIO;
 		}
 		q->m_discReader = nullptr;
+		return;
 	}
 
 	// Load the XDVDFS header.