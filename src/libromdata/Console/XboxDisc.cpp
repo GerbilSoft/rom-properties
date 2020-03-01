GerbilSoft/rom-properties

commit 4584be137faffb8e8cb75b661ff1cbfd108db7ec

[libromdata] XboxDisc: Initial parser for XDVDFS disc images.
Reworked the way ISO is handled. If RomDataFactory detects a
possible ISO-9660 image, it first checks if it's a game disc image.
If it is, it creates the relevant class instead of ISO.

This only applies to discs that have an empty boot sector.
Sega Mega CD, Sega Saturn, and Sega Dreamcast disc images have a
boot sector with identifying information, so those are always
handled by their relevant classes.

Moved pvd_time_to_unix_time() from ISO to RomDataPrivate, since
XboxDisc uses it to identify the XGD type. Note that it now takes
a string buffer (not NULL terminated) and timezone offset instead
of const ISO_PVD_DateTime_t* to reduce header dependencies.

TODO:
- XboxDisc currently only shows the XDVDFS header timestamp. It should
  also show the PVD and, eventually, load the XBE/XEX.
- Handle XDVDFS images that are relocated to 0, i.e. no video partition.
- Add support for Kreon drives to preview real discs?

src/libromdata/Console/XboxDisc.cpp
@@ -1,0 +1,413 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * XboxDisc.cpp: Microsoft Xbox disc image parser.                         *
+ *                                                                         *
+ * Copyright (c) 2019 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+#include "XboxDisc.hpp"
+#include "librpbase/RomData_p.hpp"
+
+#include "../iso_structs.h"
+#include "xdvdfs_structs.h"
+
+// librpbase
+#include "librpbase/common.h"
+#include "librpbase/byteswap.h"
+#include "librpbase/TextFuncs.hpp"
+#include "librpbase/file/IRpFile.hpp"
+#include "libi18n/i18n.h"
+using namespace LibRpBase;
+
+// C includes. (C++ namespace)
+#include <cassert>
+#include <cerrno>
+#include <cstring>
+#include <ctime>
+
+// C++ includes.
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+namespace LibRomData {
+
+ROMDATA_IMPL(XboxDisc)
+
+class XboxDiscPrivate : public LibRpBase::RomDataPrivate
+{
+	public:
+		XboxDiscPrivate(XboxDisc *q, LibRpBase::IRpFile *file);
+
+	private:
+		typedef RomDataPrivate super;
+		RP_DISABLE_COPY(XboxDiscPrivate)
+
+	public:
+		// Disc type.
+		enum DiscType {
+			DISC_UNKNOWN = -1,	// Unknown disc type.
+
+			DISC_TYPE_XGD1	= 0,	// XGD1 (Original Xbox)
+			DISC_TYPE_XGD2	= 1,	// XGD2 (Xbox 360)
+			DISC_TYPE_XGD3	= 2,	// XGD3 (Xbox 360)
+		};
+		int discType;
+
+		// XDVDFS starting address.
+		int64_t xdvdfs_addr;
+
+		// XDVDFS header.
+		// All fields are byteswapped in the constructor.
+		XDVDFS_Header xdvdfsHeader;
+};
+
+/** XboxDiscPrivate **/
+
+XboxDiscPrivate::XboxDiscPrivate(XboxDisc *q, IRpFile *file)
+	: super(q, file)
+	, discType(DISC_UNKNOWN)
+	, xdvdfs_addr(0)
+{
+	// Clear the various headers.
+	memset(&xdvdfsHeader, 0, sizeof(xdvdfsHeader));
+}
+
+/** XboxDisc **/
+
+/**
+ * Read a Microsoft Xbox disc image.
+ *
+ * A ROM file must be opened by the caller. The file handle
+ * will be ref()'d and must be kept open in order to load
+ * data from the ROM.
+ *
+ * To close the file, either delete this object or call close().
+ *
+ * NOTE: Check isValid() to determine if this is a valid ROM.
+ *
+ * @param file Open ROM image.
+ */
+XboxDisc::XboxDisc(IRpFile *file)
+	: super(new XboxDiscPrivate(this, file))
+{
+	// This class handles disc images.
+	RP_D(XboxDisc);
+	d->className = "XboxDisc";
+	d->fileType = FTYPE_DISC_IMAGE;
+
+	if (!d->file) {
+		// Could not ref() the file handle.
+		return;
+	}
+
+	// TODO: Also check for trimmed XDVDFS. (offset == 0)
+
+	// Read the ISO-9660 PVD.
+	// NOTE: Only 2048-byte sectors, since this is DVD.
+	ISO_Primary_Volume_Descriptor pvd;
+	size_t size = d->file->seekAndRead(ISO_PVD_ADDRESS_2048, &pvd, sizeof(pvd));
+	if (size != sizeof(pvd)) {
+		d->file->unref();
+		d->file = nullptr;
+		return;
+	}
+
+	// Check if this disc image is supported.
+	d->discType = isRomSupported_static(&pvd);
+
+	switch (d->discType) {
+		case XboxDiscPrivate::DISC_TYPE_XGD1:
+			d->xdvdfs_addr = XDVDFS_LBA_OFFSET_XGD1 * XDVDFS_BLOCK_SIZE;
+			break;
+		case XboxDiscPrivate::DISC_TYPE_XGD2:
+			d->xdvdfs_addr = XDVDFS_LBA_OFFSET_XGD2 * XDVDFS_BLOCK_SIZE;
+			break;
+		case XboxDiscPrivate::DISC_TYPE_XGD3:
+			d->xdvdfs_addr = XDVDFS_LBA_OFFSET_XGD3 * XDVDFS_BLOCK_SIZE;
+			break;
+		default:
+			// Not supported.
+			d->file->unref();
+			d->file = nullptr;
+			return;
+	}
+
+	// Read the XDVDFS header.
+	size = d->file->seekAndRead(
+		d->xdvdfs_addr + (XDVDFS_HEADER_LBA_OFFSET * XDVDFS_BLOCK_SIZE),
+		&d->xdvdfsHeader, sizeof(d->xdvdfsHeader));
+	if (size != sizeof(d->xdvdfsHeader)) {
+		// Seek and/or read error.
+		d->file->unref();
+		d->file = nullptr;
+		return;
+	}
+
+	// Verify the magic strings.
+	if (memcmp(d->xdvdfsHeader.magic, XDVDFS_MAGIC, sizeof(d->xdvdfsHeader.magic)) != 0 ||
+	    memcmp(d->xdvdfsHeader.magic_footer, XDVDFS_MAGIC, sizeof(d->xdvdfsHeader.magic_footer)) != 0)
+	{
+		// One or both of the magic strings are incorrect.
+		d->file->unref();
+		d->file = nullptr;
+		return;
+	}
+
+#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+	// Byteswap the fields.
+	d->xdvdfsHeader.root_dir_sector	= le32_to_cpu(d->xdvdfsHeader.root_dir_sector);
+	d->xdvdfsHeader.root_dir_size	= le32_to_cpu(d->xdvdfsHeader.root_dir_size);
+	d->xdvdfsHeader.timestamp	= le64_to_cpu(d->xdvdfsHeader.timestamp);
+#endif /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
+
+	// Disc image is ready.
+	d->isValid = true;
+}
+
+/** ROM detection functions. **/
+
+/**
+ * Is a ROM image supported by this class?
+ * @param info DetectInfo containing ROM detection information.
+ * @return Class-specific system ID (>= 0) if supported; -1 if not.
+ */
+int XboxDisc::isRomSupported_static(const DetectInfo *info)
+{
+	// NOTE: This version is NOT supported for XboxDisc.
+	// Use the ISO-9660 PVD check instead.
+	RP_UNUSED(info);
+	assert(!"Use the ISO-9660 PVD check instead.");
+	return -1;
+}
+
+/**
+ * Is a ROM image supported by this class?
+ * @param pvd ISO-9660 Primary Volume Descriptor.
+ * @return Class-specific system ID (>= 0) if supported; -1 if not.
+ */
+int XboxDisc::isRomSupported_static(const ISO_Primary_Volume_Descriptor *pvd)
+{
+	// Xbox PVDs from the same manufacturing wave
+	// match, so we can check the PVD timestamp
+	// to determine if this is an Xbox disc.
+	// TODO: Other ISO fields?
+	assert(pvd != nullptr);
+	if (!pvd) {
+		// Bad.
+		return -1;
+	}
+
+	// Get the creation time.
+	const time_t btime = RomDataPrivate::pvd_time_to_unix_time(
+		pvd->btime.full, pvd->btime.tz_offset);
+	if (btime == -1) {
+		// Invalid creation time.
+		return -1;
+	}
+
+	// Compare to known times.
+	// TODO: Enum with XGD waves?
+
+	struct xgd_pvd_t {
+		uint8_t xgd;	// XGD type. (1, 2, 3)
+		uint8_t wave;	// Manufacturing wave.
+
+		// NOTE: Using int32_t as an optimization, since
+		// there won't be any Xbox 360 games released
+		// after January 2038. (probably...)
+		int32_t btime;	// Creation time.
+	};
+
+	static const xgd_pvd_t xgd_tbl[] = {
+		// TODO: XGD1
+
+		{2,  1, 1128716326},	// XGD2 Wave 1:  2005-10-07 12:18:46.00 -08:00
+		{2,  2, 1141708147},	// XGD2 Wave 2:  2006-03-06 21:09:07.00 -08:00
+		{2,  3, 1231977600},	// XGD2 Wave 3:  2009-01-14 16:00:00.00 -08:00
+		{2,  4, 1251158400},	// XGD2 Wave 4:  2009-08-24 17:00:00.00 -07:00
+		{2,  5, 1254787200},	// XGD2 Wave 5:  2009-10-05 17:00:00.00 -07:00
+		{2,  6, 1256860800},	// XGD2 Wave 6:  2009-10-29 17:00:00.00 -07:00
+		{2,  7, 1266796800},	// XGD2 Wave 7:  2010-02-21 16:00:00.00 -08:00
+		{2,  8, 1283644800},	// XGD2 Wave 8:  2010-09-04 17:00:00.00 -07:00
+		{2,  9, 1284595200},	// XGD2 Wave 9:  2010-09-15 17:00:00.00 -07:00
+		{2, 10, 1288310400},	// XGD2 Wave 10: 2010-10-28 17:00:00.00 -07:00
+		{2, 11, 1295395200},	// XGD2 Wave 11: 2011-01-18 16:00:00.00 -08:00
+		{2, 12, 1307923200},	// XGD2 Wave 12: 2011-06-12 17:00:00.00 -07:00
+		{2, 13, 1310515200},	// XGD2 Wave 13: 2011-07-12 17:00:00.00 -07:00
+		{2, 14, 1323302400},	// XGD2 Wave 14: 2011-12-07 16:00:00.00 -08:00
+		{2, 15, 1329868800},	// XGD2 Wave 15: 2012-02-21 16:00:00.00 -08:00
+		{2, 16, 1340323200},	// XGD2 Wave 16: 2012-06-21 17:00:00.00 -07:00
+		{2, 17, 1352332800},	// XGD2 Wave 17: 2012-11-07 16:00:00.00 -08:00
+		{2, 18, 1353283200},	// XGD2 Wave 18: 2012-11-18 16:00:00.00 -08:00
+		{2, 19, 1377561600},	// XGD2 Wave 19: 2013-08-26 17:00:00.00 -07:00
+		{2, 20, 1430092800},	// XGD2 Wave 20: 2015-04-26 17:00:00.00 -07:00
+
+		// XGD3 does not have shared PVDs per wave,
+		// but the timestamps all have a similar pattern:
+		// - Year: 2011+
+		// - Min, Sec, Csec: 00
+		// - Hour and TZ:
+		//   - 17, -07:00
+		//   - 16, -08:00
+
+		{0, 0, 0}
+	};
+
+	// TODO: Use a binary search instead of linear?
+	for (const xgd_pvd_t *pXgd = xgd_tbl; pXgd->xgd != 0; pXgd++) {
+		if (pXgd->btime == btime) {
+			// Found a match!
+			return pXgd->xgd - 1;
+		}
+	}
+
+	// No match in the XGD table.
+	// Check for XGD3.
+	static const char xgd3_pvd_times[][9+1] = {
+		"17000000\xE4",	// 17:00:00.00 -07:00
+		"16000000\xE0",	// 16:00:00.00 -08:00
+
+		"\0"
+	};
+	// TODO: Verify that this works correctly.
+	for (const char *pXgd = &xgd3_pvd_times[0][0]; pXgd[0] != '\0'; pXgd++) {
+		if (!memcmp(&pvd->btime.full[8], pXgd, 9)) {
+			// Found a match!
+			return XboxDiscPrivate::DISC_TYPE_XGD3;
+		}
+	}
+
+	// Not XGD.
+	return -1;
+}
+
+/**
+ * Get the name of the system the loaded ROM is designed for.
+ * @param type System name type. (See the SystemName enum.)
+ * @return System name, or nullptr if type is invalid.
+ */
+const char *XboxDisc::systemName(unsigned int type) const
+{
+	RP_D(const XboxDisc);
+	if (!d->isValid || !isSystemNameTypeValid(type))
+		return nullptr;
+
+	// XboxDisc has the same name worldwide, so we can
+	// ignore the region selection.
+	// TODO: Identify the OS, or list that in the fields instead?
+	static_assert(SYSNAME_TYPE_MASK == 3,
+		"XboxDisc::systemName() array index optimization needs to be updated.");
+
+	// TODO: Check for default.xbe and/or default.xex
+	// to determine if it's Xbox or Xbox 360.
+	static const char *const sysNames[4] = {
+		"Microsoft Xbox", "Xbox", "Xbox", nullptr
+	};
+
+	return sysNames[type & SYSNAME_TYPE_MASK];
+}
+
+/**
+ * Get a list of all supported file extensions.
+ * This is to be used for file type registration;
+ * subclasses don't explicitly check the extension.
+ *
+ * NOTE: The extensions do not include the leading dot,
+ * e.g. "bin" instead of ".bin".
+ *
+ * NOTE 2: The array and the strings in the array should
+ * *not* be freed by the caller.
+ *
+ * @return NULL-terminated array of all supported file extensions, or nullptr on error.
+ */
+const char *const *XboxDisc::supportedFileExtensions_static(void)
+{
+	static const char *const exts[] = {
+		".iso",		// ISO
+		".xiso",	// Xbox ISO image
+		// TODO: More?
+
+		nullptr
+	};
+	return exts;
+}
+
+/**
+ * Get a list of all supported MIME types.
+ * This is to be used for metadata extractors that
+ * must indicate which MIME types they support.
+ *
+ * NOTE: The array and the strings in the array should
+ * *not* be freed by the caller.
+ *
+ * @return NULL-terminated array of all supported file extensions, or nullptr on error.
+ */
+const char *const *XboxDisc::supportedMimeTypes_static(void)
+{
+	static const char *const mimeTypes[] = {
+		// Unofficial MIME types from FreeDesktop.org..
+		"application/x-iso9660-image",
+
+		// TODO: XDVDFS?
+		nullptr
+	};
+	return mimeTypes;
+}
+
+/**
+ * Load field data.
+ * Called by RomData::fields() if the field data hasn't been loaded yet.
+ * @return Number of fields read on success; negative POSIX error code on error.
+ */
+int XboxDisc::loadFieldData(void)
+{
+	RP_D(XboxDisc);
+	if (!d->fields->empty()) {
+		// Field data *has* been loaded...
+		return 0;
+	} else if (!d->file || !d->file->isOpen()) {
+		// File isn't open.
+		return -EBADF;
+	} else if (!d->isValid || d->discType < 0) {
+		// Unknown disc type.
+		return -EIO;
+	}
+
+	// XDVDFS header.
+	const XDVDFS_Header *const xdvdfsHeader = &d->xdvdfsHeader;
+	d->fields->reserve(1);	// Maximum of 1 field.
+
+	// Timestamp
+	// NOTE: Timestamp is stored in Windows FILETIME format,
+	// which is 100ns units since 1601/01/01 00:00:00 UTC.
+	// Based on libwin32common/w32time.h.
+#define FILETIME_1970 116444736000000000LL	// Seconds between 1/1/1601 and 1/1/1970.
+#define HECTONANOSEC_PER_SEC 10000000LL
+	time_t timestamp = static_cast<time_t>(
+		(xdvdfsHeader->timestamp - FILETIME_1970) / HECTONANOSEC_PER_SEC);
+	d->fields->addField_dateTime(C_("XboxDisc", "Timestamp"), timestamp,
+		RomFields::RFT_DATETIME_HAS_DATE |
+		RomFields::RFT_DATETIME_HAS_TIME);
+
+	// TODO: Get the XBE and/or XEX.
+
+	// Finished reading the field data.
+	return static_cast<int>(d->fields->count());
+}
+
+}

src/libromdata/CMakeLists.txt
@@ -24,6 +24,7 @@ SET(libromdata_SRCS
 	Console/WiiWIBN.cpp
 	Console/Xbox360_XDBF.cpp
 	Console/Xbox360_XEX.cpp
+	Console/XboxDisc.cpp
 
 	Handheld/DMG.cpp
 	Handheld/GameBoyAdvance.cpp
@@ -126,6 +127,7 @@ SET(libromdata_H
 	Console/WiiWIBN.hpp
 	Console/Xbox360_XDBF.hpp
 	Console/Xbox360_XEX.hpp
+	Console/XboxDisc.hpp
 
 	Console/dc_structs.h
 	Console/gcn_structs.h

src/libromdata/Console/XboxDisc.hpp
@@ -1,0 +1,43 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * XboxDisc.hpp: Microsoft Xbox disc image parser.                         *
+ *                                                                         *
+ * Copyright (c) 2019 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBROMDATA_CONSOLE_XBOXDISC_HPP__
+#define __ROMPROPERTIES_LIBROMDATA_CONSOLE_XBOXDISC_HPP__
+
+#include "librpbase/RomData.hpp"
+#include "../iso_structs.h"
+
+namespace LibRomData {
+
+ROMDATA_DECL_BEGIN(XboxDisc)
+
+	public:
+		/**
+		 * Is a ROM image supported by this class?
+		 * @param pvd ISO-9660 Primary Volume Descriptor.
+		 * @return Class-specific system ID (>= 0) if supported; -1 if not.
+		 */
+		static int isRomSupported_static(const ISO_Primary_Volume_Descriptor *pvd);
+
+ROMDATA_DECL_END()
+
+}
+
+#endif /* __ROMPROPERTIES_LIBROMDATA_CONSOLE_XBOXDISC_HPP__ */

src/libromdata/Console/xdvdfs_structs.h
@@ -1,0 +1,111 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (libromdata)                       *
+ * xdvdfs_structs.h: Xbox XDVDFS structs.                                  *
+ *                                                                         *
+ * Copyright (c) 2019 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+// References:
+// - https://xboxdevwiki.net/Xbox_Game_Disc
+// - https://github.com/XboxDev/extract-xiso/blob/master/extract-xiso.c
+// - https://github.com/multimediamike/xbfuse/blob/master/src/xdvdfs.c
+// - https://www.eurasia.nu/wiki/index.php/XboxFileSystemDetails
+
+#ifndef __ROMPROPERTIES_LIBROMDATA_CONSOLE_XDVDFS_STRUCTS_H__
+#define __ROMPROPERTIES_LIBROMDATA_CONSOLE_XDVDFS_STRUCTS_H__
+
+#include "librpbase/common.h"
+#include "librpbase/byteorder.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma pack(1)
+
+// XDVDFS offsets, in LBAs.
+// TODO: Multiple XGD1 offsets? (This matches Futurama.)
+#define XDVDFS_BLOCK_SIZE	2048U	/* same as CD/DVD */
+#define XDVDFS_LBA_OFFSET_XGD1	0x30600U
+#define XDVDFS_LBA_OFFSET_XGD2	0x1FB20U
+#define XDVDFS_LBA_OFFSET_XGD3	 0x4100U
+
+/**
+ * XDVDFS header.
+ * Located at the start of the XDVDFS portion of the disc.
+ *
+ * All fields are in little-endian.
+ */
+#define XDVDFS_MAGIC "MICROSOFT*XBOX*MEDIA"
+#define XDVDFS_HEADER_LBA_OFFSET	32U	/* relative to XDVDFS offset, in LBAs */
+typedef struct PACKED _XDVDFS_Header {
+	char magic[20];			// [0x000] "MICROSOFT*XBOX*MEDIA"
+	uint32_t root_dir_sector;	// [0x014] Root directory sector
+	uint32_t root_dir_size;		// [0x018] Root directory size
+	uint64_t timestamp;		// [0x01C] Timestamp (Windows FILETIME format)
+	uint8_t unused[0x7C8];		// [0x020]
+	char magic_footer[20];		// [0x7EC] "MICROSOFT*XBOX*MEDIA" (footer magic)
+} XDVDFS_Header;
+ASSERT_STRUCT(XDVDFS_Header, XDVDFS_BLOCK_SIZE);
+
+/**
+ * Directory entry.
+ * XDVDFS directories use a binary tree structure for fast searches.
+ *
+ * Binary search is case-insensitive.
+ * NOTE: Not sure if strcasecmp() should be used - manual handling
+ * of only 'a'-'z'? (this is what extract-xiso does)
+ *
+ * Filename is stored immediately after the directory entry.
+ * If the filename does not end on a DWORD boundary, it is
+ * padded using 0xFF.
+ *
+ * If left_offset or right_offset are 0, those subtrees don't exist.
+ * If left_Offset or right_offset are 0xFFFF, we've probably reached
+ * the end of the directory.
+ *
+ * All fields are in little-endian.
+ */
+typedef struct PACKED _XDVDFS_DirEntry {
+	uint16_t left_offset;		// [0x000] Offset to left subtree entry, in DWORDs. (0 for none)
+	uint16_t right_offset;		// [0x002] Offset to right subtree entry, in DWORDs. (0 for none)
+	uint32_t start_sector;		// [0x004] Starting sector
+	uint32_t file_size;		// [0x008] File size, in bytes
+	uint8_t attributes;		// [0x00C] Attributes bitfield (See XDVDFS_Attributes_e)
+	uint8_t name_length;		// [0x00D] Filename length, in bytes
+} XDVDFS_DirEntry;
+ASSERT_STRUCT(XDVDFS_DirEntry, 14);
+
+/**
+ * File attributes. (bitfield)
+ */
+typedef enum {
+	XDVDFS_ATTR_READONLY	= 0x01,
+	XDVDFS_ATTR_HIDDEN	= 0x02,
+	XDVDFS_ATTR_SYSTEM	= 0x04,
+	XDVDFS_ATTR_DIRECTORY	= 0x10,
+	XDVDFS_ATTR_ARCHIVE	= 0x20,
+	XDVDFS_ATTR_NORMAL	= 0x80,
+} XDVDFS_Attributes_e;
+
+#pragma pack()
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ROMPROPERTIES_LIBROMDATA_CONSOLE_XDVDFS_STRUCTS_H__ */

src/libromdata/Other/ISO.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  * ROM Properties Page shell extension. (libromdata)                       *
- * ISO.hpp: ISO-9660 disc image parser.                                    *
+ * ISO.cpp: ISO-9660 disc image parser.                                    *
  *                                                                         *
  * Copyright (c) 2019 by David Korth.                                      *
  *                                                                         *
@@ -83,7 +83,12 @@ class ISOPrivate : public LibRpBase::RomDataPrivate
 		 * @param pvd_time PVD timestamp
 		 * @return UNIX time, or -1 if invalid or not set.
 		 */
-		static time_t pvd_time_to_unix_time(const ISO_PVD_DateTime_t *pvd_time);
+		static inline time_t pvd_time_to_unix_time(const ISO_PVD_DateTime_t *pvd_time)
+		{
+			// Wrapper for RomData::pvd_time_to_unix_time(),
+			// which doesn't take an ISO_PVD_DateTime_t struct.
+			return RomDataPrivate::pvd_time_to_unix_time(pvd_time->full, pvd_time->tz_offset);
+		}
 };
 
 /** ISOPrivate **/
@@ -186,75 +191,10 @@ void ISOPrivate::checkVolumeDescriptors(void)
 	// TODO: More descriptors?
 }
 
-/**
- * Convert an ISO PVD timestamp to UNIX time.
- * @param pvd_time PVD timestamp
- * @return UNIX time, or -1 if invalid or not set.
- */
-time_t ISOPrivate::pvd_time_to_unix_time(const ISO_PVD_DateTime_t *pvd_time)
-{
-	assert(pvd_time != nullptr);
-
-	// PVD time is in ASCII format:
-	// YYYYMMDDHHmmssccz
-	// - YYYY: Year
-	// - MM: Month
-	// - DD: Day
-	// - HH: Hour
-	// - mm: Minute
-	// - ss: Second
-	// - cc: Centisecond (not supported in UNIX time)
-	// - z: (int8) Timezone offset in 15min intervals: [0, 100] -> [-48, 52]
-	//   - -48: GMT-1200
-	//   -  52: GMT+1300
-
-	// NOTE: pvd_time is NOT null-terminated, so we need to
-	// copy it to a temporary buffer.
-	char buf[17];
-	memcpy(buf, pvd_time->full, 16);
-	buf[16] = '\0';
-
-	struct tm pvdtime;
-	int csec;
-	int ret = sscanf(buf, "%04d%02d%02d%02d%02d%02d%02d",
-		&pvdtime.tm_year, &pvdtime.tm_mon, &pvdtime.tm_mday,
-		&pvdtime.tm_hour, &pvdtime.tm_min, &pvdtime.tm_sec, &csec);
-	if (ret != 7) {
-		// Some argument wasn't parsed correctly.
-		return -1;
-	}
-
-	// If year is 0, the entry is probably all zeroes.
-	if (pvdtime.tm_year == 0) {
-		return -1;
-	}
-
-	// Adjust values for struct tm.
-	pvdtime.tm_year -= 1900;	// struct tm: year - 1900
-	pvdtime.tm_mon--;		// struct tm: 0-11
-
-	// tm_wday and tm_yday are output variables.
-	pvdtime.tm_wday = 0;
-	pvdtime.tm_yday = 0;
-	pvdtime.tm_isdst = 0;
-
-	// If conversion fails, this will return -1.
-	time_t unixtime = timegm(&pvdtime);
-	if (unixtime == -1)
-		return -1;
-
-	// Convert to UTC using the timezone offset.
-	// NOTE: Timezone offset is negative for west of GMT,
-	// so we need to subtract it from the UNIX timestamp.
-	// TODO: Return the timezone offset separately.
-	unixtime -= (static_cast<int>(pvd_time->tz_offset) * (15*60));
-	return unixtime;
-}
-
 /** ISO **/
 
 /**
- * Read an ISO-9660 executable.
+ * Read an ISO-9660 disc image.
  *
  * A ROM file must be opened by the caller. The file handle
  * will be ref()'d and must be kept open in order to load
@@ -330,8 +270,6 @@ ISO::ISO(IRpFile *file)
 
 	// Check for additional volume descriptors.
 	d->checkVolumeDescriptors();
-
-	// TODO: Search for Xbox disc images.
 }
 
 /** ROM detection functions. **/
@@ -379,7 +317,7 @@ const char *ISO::systemName(unsigned int type) const
 	static_assert(SYSNAME_TYPE_MASK == 3,
 		"ISO::systemName() array index optimization needs to be updated.");
 
-	// TODO: UDF, XDVDFS, HFS, others?
+	// TODO: UDF, HFS, others?
 	static const char *const sysNames[4] = {
 		"ISO-9660", "ISO", "ISO", nullptr
 	};
@@ -429,8 +367,7 @@ const char *const *ISO::supportedMimeTypes_static(void)
 		// Unofficial MIME types from FreeDesktop.org..
 		"application/x-iso9660-image",
 
-		// TODO: BIN (2352), XDVDFS?
-
+		// TODO: BIN (2352)?
 		nullptr
 	};
 	return mimeTypes;

commit 46c0294dcf00f8fc2784114baeb00f590e5bca00

[libromdata] XboxDisc: Simplify the XGD3 check.
The previous method was incorrect, since it was adding 1 to the
`const char*` pointer when it really needed to add 10. Coverity
complained about a potential buffer overflow because of it.

Simplified by doing two explicit memcmp() calls instead of using a loop.

This bug has been present since XboxDisc was introduced in
commit 4584be137faffb8e8cb75b661ff1cbfd108db7ec.
([libromdata] XboxDisc: Initial parser for XDVDFS disc images.)

NOTE: This bug is present in v1.4, but it likely won't cause any issues
in practice (unless someone is intentionally fuzzing a disc image).

This fixes the following Coverity Scan bugs:
- #1483989: Out-of-bounds access

src/libromdata/Console/XboxDisc.cpp
@@ -527,19 +527,17 @@ int XboxDisc::isRomSupported_static(
 		static const char xgd3_pvd_times[][9+1] = {
 			"17000000\xE4",	// 17:00:00.00 -07:00
 			"16000000\xE0",	// 16:00:00.00 -08:00
-
-			"\0"
 		};
 
 		// TODO: Verify that this works correctly.
-		for (const char *pXgd = &xgd3_pvd_times[0][0]; pXgd[0] != '\0'; pXgd++) {
-			if (!memcmp(&pvd->btime.full[8], pXgd, 9)) {
-				// Found a match!
-				if (pWave) {
-					*pWave = 0;
-				}
-				return XboxDiscPrivate::DISC_TYPE_XGD3;
+		if (!memcmp(&pvd->btime.full[8], xgd3_pvd_times[0], 9) ||
+		     memcmp(&pvd->btime.full[8], xgd3_pvd_times[1], 9))
+		{
+			// Found a match!
+			if (pWave) {
+				*pWave = 0;
 			}
+			return XboxDiscPrivate::DISC_TYPE_XGD3;
 		}
 	}
 