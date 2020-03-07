GerbilSoft/rom-properties

commit 5712f216e1b8ee284680335d5c20df502eb7fbbb

[libwin32common] DelayLoadHelper.c: Don't use an explicit length.
Due to the way strncasecmp() works, this would allow e.g. "zlib12.dll"
to match "zlib1".

Changed the array to use pointers, since "libgnuintl-8.dll" is 16
characters, and increasing the array to 20 would add more overhead
than using pointers.

src/libwin32common/DelayLoadHelper.c
@@ -46,12 +46,11 @@ static HMODULE WINAPI rp_loadLibrary(LPCSTR pszModuleName)
 {
 	// We only want to handle DLLs included with rom-properties.
 	// System DLLs should be handled normally.
-	// DLL whitelist: First byte is an explicit length.
-	static const char prefix_whitelist[][16] = {
-		"\x05" "zlib1",
-		"\x06" "libpng",
-		"\x08" "tinyxml2",
-		"\x0C" "libgnuintl-8",
+	static const char *const dll_whitelist[] = {
+		"zlib1.dll",
+		"libpng.dll",
+		"tinyxml2.dll",
+		"libgnuintl-8.dll",
 	};
 
 	TCHAR dll_fullpath[MAX_PATH+64];
@@ -65,10 +64,8 @@ static HMODULE WINAPI rp_loadLibrary(LPCSTR pszModuleName)
 	unsigned int i;
 	bool match = false;
 
-	for (i = 0; i < _countof(prefix_whitelist); i++) {
-		if (!strncasecmp(pszModuleName, &prefix_whitelist[i][1],
-		     (unsigned int)prefix_whitelist[i][0]))
-		{
+	for (i = 0; i < _countof(dll_whitelist); i++) {
+		if (!strcasecmp(pszModuleName, dll_whitelist[i])) {
 			// Found a match.
 			match = true;
 			break;

commit 4b2469797add416e9bd323ef528a1c5463ca783f

[libwin32common] DelayLoadHelper.c: Fix libpng filename; handle Debug builds.
Need to check for e.g. "zlib1d.dll" in debug builds.

The PNG major version was missing. We need to check for "libpng16.dll",
not "libpng.dll".

Flag icons in cboLanguage weren't showing up because zlib and libpng
coudn't be loaded.

This fixes a regression from commit 5712f216e1b8ee284680335d5c20df502eb7fbbb.
([libwin32common] DelayLoadHelper.c: Don't use an explicit length.)

src/libwin32common/DelayLoadHelper.c
@@ -47,9 +47,15 @@ static HMODULE WINAPI rp_loadLibrary(LPCSTR pszModuleName)
 	// We only want to handle DLLs included with rom-properties.
 	// System DLLs should be handled normally.
 	static const char *const dll_whitelist[] = {
+#ifdef NDEBUG
 		"zlib1.dll",
-		"libpng.dll",
+		"libpng16.dll",
 		"tinyxml2.dll",
+#else /* !NDEBUG */
+		"zlib1d.dll",
+		"libpng16d.dll",
+		"tinyxml2d.dll",
+#endif /* NDEBUG */
 		"libgnuintl-8.dll",
 	};
 