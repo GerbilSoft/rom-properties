GerbilSoft/rom-properties

commit d7c1a36e360c017cf0c9cde2c92127a167cc80c3

[librpbase] timegm.c: FIXME: MinGW-w64 doesn't like the const initializer using days_from_0().
Manually inline the constant.

src/librpbase/timegm.c
@@ -34,7 +34,11 @@ static inline int32_t days_from_0(int32_t year)
 
 static inline int32_t days_from_1970(int32_t year)
 {
-	static const int32_t days_from_0_to_1970 = days_from_0(1970);
+	// FIXME: MinGW-w64 doesn't like the following initializer.
+	// error: initializer element is not constant
+	//static const int32_t days_from_0_to_1970 = days_from_0(1970);
+	static const int32_t days_from_0_to_1970 =
+		(365 * (1970-1) + ((1970-1) / 400) - ((1970-1)/100) + ((1970-1) / 4))
 	return days_from_0(year) - days_from_0_to_1970;
 }
 

commit 4d7fe4f3f3224736d454b0c5729350d97375e4c1

[librpbase/tests] timegm.c: Missing ';'.
This fixes a regression from commit d7c1a36e360c017cf0c9cde2c92127a167cc80c3.
([librpbase] timegm.c: FIXME: MinGW-w64 doesn't like the const initializer using days_from_0().)

src/librpbase/timegm.c
@@ -38,7 +38,7 @@ static inline int32_t days_from_1970(int32_t year)
 	// error: initializer element is not constant
 	//static const int32_t days_from_0_to_1970 = days_from_0(1970);
 	static const int32_t days_from_0_to_1970 =
-		(365 * (1970-1) + ((1970-1) / 400) - ((1970-1)/100) + ((1970-1) / 4))
+		(365 * (1970-1) + ((1970-1) / 400) - ((1970-1)/100) + ((1970-1) / 4));
 	return days_from_0(year) - days_from_0_to_1970;
 }
 