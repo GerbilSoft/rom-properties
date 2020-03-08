4ab7156c436a5d0bf304a949e03c82f0fc13b364
David Korth <gerbilsoft@gerbilsoft.com>
2020-03-08 01:57:01 -0500

    appveyor.yml, appveyor.cmd: Initial attempt to add support for MinGW-w64 on AppVeyor.
    This uses a new matrix configuration with platform, configuration,
    and compiler. Note that we're only testing debug builds with MinGW-w64
    for now.

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,7 +1,18 @@
 @ECHO OFF
 cmake --version
-IF "%PLATFORM%" == "x64" (
-	cmake . -G "Visual Studio 12 2013 Win64" -DCMAKE_GENERATOR_TOOLSET=v120_xp -DENABLE_JPEG=ON -DBUILD_TESTING=ON -DENABLE_LTO=OFF -DENABLE_PCH=ON
-) ELSE (
-	cmake . -G "Visual Studio 12 2013" -DCMAKE_GENERATOR_TOOLSET=v120_xp -DENABLE_JPEG=ON -DBUILD_TESTING=ON -DENABLE_LTO=OFF -DENABLE_PCH=ON
-)
+
+if "%compiler%" == "msvc2013" goto :msvc2013
+if "%compiler%" == "mingw-w64" goto :mingw-w64
+echo *** ERROR: Unsupported compiler '%compiler%'.
+exit /b 1
+
+:msvc2013
+set "CMAKE_GENERATOR=Visual Studio 12 2013 Win64"
+set CMAKE_GENERATOR_TOOLSET=v120_xp
+if "%platform%" == "x64" set "CMAKE_GENERATOR=%CMAKE_GENERATOR% Win64"
+cmake . -G "%CMAKE_GENERATOR%" -DCMAKE_GENERATOR_TOOLSET=%CMAKE_GENERATOR_TOOLSET% -DENABLE_JPEG=ON -DBUILD_TESTING=ON -DENABLE_LTO=OFF -DENABLE_PCH=ON
+exit /b %ERRORLEVEL%
+
+:mingw-w64
+cmake . -G "MinGW Makefiles" -DCMAKE_PREFIX_PATH=C:\mingw64 -DCMAKE_BUILD_TYPE=%configuration% -DENABLE_JPEG=ON -DBUILD_TESTING=ON -DENABLE_LTO=OFF -DENABLE_PCH=ON
+exit /b %ERRORLEVEL%
