GerbilSoft/rom-properties

commit f5dfb31192ae05aa14f2e67dfa25d127a837c7a5

RomData, FileFormat: New function mimeType(); updated subclasses.
RomData subclasses should set RomDataPrivate::mimeType() to a valid
MIME type that represents the file.

For some classes, the MIME types array in supportedMimeTypes_static()
was moved to the private class struct. This array is then used to
initialize d->mimeType. In a few cases, the MIME types needed to be
reordered to match the d->romType (or d->audioFormat, etc.) enum values.

Renamed some RomType enums to have a system-specific prefix. This
isn't *required*, but it might make a difference somewhere...

ELF: Added a MIME type for object files.

ISO, XboxDisc: Prefer "application/x-cd-image" to "application/x-iso9660-image".
DVDs usually have UDF file systems. Xbox discs have XDVDFS.

ISO: Added the ".iso9660" file extension. This is listed in
shared-mime-info.

MachO:
- Fixed some incorrect Mach-O file type assignments due to incorrect
  array indexing.
- Added a MIME type for object files.
- Added placeholders for some new types.

MegaDrive: Added "application/x-sega-cd-32x-rom" for MCD32X. This type is
*not* on FreeDesktop.org, whereas the other types are.

Nintendo3DS: Remove ROM_TYPE_SMDH, since it was split out into
Nintendo3DS_SMDH in v1.4.

RpTextureWrapper: Get the MIME type from the FileFormat subclass.

Sega8Bit: TODO: Distinguish between SMS and GG for both system name and
MIME type. (Use the file extension if the "TMR SEGA" header is ambiguous.)

[xdg] rom-properties.xml:
- MachO: Added "application/x-mach-object"; updated all file type IDs.
- Added "application/x-sega-cd-32x-rom".
- Changed CD-based types to have sub-class-of="application/x-cd-image"
  instead of "application/x-raw-disk-image".
- TODO: Upstream various entries and apply the above CD changes to
  existing CD-like types.

src/libromdata/Console/GameCube.cpp
@@ -690,6 +690,7 @@ GameCube::GameCube(IRpFile *file)
 	d->discType = isRomSupported_static(&info);
 
 	// TODO: DiscReaderFactory?
+	// TODO: More MIME types for e.g. Triforce, CISO, TGC, etc.
 	if (d->discType >= 0) {
 		switch (d->discType & GameCubePrivate::DISC_FORMAT_MASK) {
 			case GameCubePrivate::DISC_FORMAT_RAW:
@@ -710,17 +711,20 @@ GameCube::GameCube(IRpFile *file)
 				break;
 			}
 			case GameCubePrivate::DISC_FORMAT_WBFS:
+				d->mimeType = "application/x-wbfs";
 				d->discReader = new WbfsReader(d->file);
 				break;
 			case GameCubePrivate::DISC_FORMAT_CISO:
 				d->discReader = new CisoGcnReader(d->file);
 				break;
 			case GameCubePrivate::DISC_FORMAT_NASOS:
+				d->mimeType = "application/x-nasos-image";
 				d->discReader = new NASOSReader(d->file);
 				break;
 			case GameCubePrivate::DISC_FORMAT_WIA:
 				// TODO: Implement WiaReader.
 				// For now, only the header will be readable.
+				d->mimeType = "application/x-wia";
 				d->discReader = nullptr;
 				break;
 
@@ -737,6 +741,24 @@ GameCube::GameCube(IRpFile *file)
 		d->file->unref();
 		d->file = nullptr;
 		return;
+	}
+
+	if (!d->mimeType) {
+		// MIME type hasn't been set yet.
+		// Set it based on the system ID.
+		switch (d->discType & GameCubePrivate::DISC_SYSTEM_MASK) {
+			case GameCubePrivate::DISC_SYSTEM_GCN:
+			case GameCubePrivate::DISC_SYSTEM_TRIFORCE:
+				// TODO: Separate MIME type for Triforce.
+				d->mimeType = "application/x-gamecube-iso-image";
+				break;
+			case GameCubePrivate::DISC_SYSTEM_WII:
+				d->mimeType = "application/x-wii-iso-image";
+				break;
+			default:
+				assert(!"Invalid system ID...");
+				break;
+		}
 	}
 
 	if (!d->discReader) {

commit 668d322778597942fbe5ab5d444c51e168d90fed

[libromdata] GameCube: Set the MIME type after checking other format stuff.
This fixes an assertion when viewing SDK GCMs. In release builds, it
resulted in the MIME type not being set properly.

This fixes a regression from commit f5dfb31192ae05aa14f2e67dfa25d127a837c7a5.
(RomData, FileFormat: New function mimeType(); updated subclasses.)

Affects versions: 1.5

src/libromdata/Console/GameCube.cpp
@@ -744,24 +744,6 @@ GameCube::GameCube(IRpFile *file)
 		return;
 	}
 
-	if (!d->mimeType) {
-		// MIME type hasn't been set yet.
-		// Set it based on the system ID.
-		switch (d->discType & GameCubePrivate::DISC_SYSTEM_MASK) {
-			case GameCubePrivate::DISC_SYSTEM_GCN:
-			case GameCubePrivate::DISC_SYSTEM_TRIFORCE:
-				// TODO: Separate MIME type for Triforce.
-				d->mimeType = "application/x-gamecube-iso-image";
-				break;
-			case GameCubePrivate::DISC_SYSTEM_WII:
-				d->mimeType = "application/x-wii-iso-image";
-				break;
-			default:
-				assert(!"Invalid system ID...");
-				break;
-		}
-	}
-
 	if (!d->discReader) {
 		// No WiaReader yet. If this is WIA,
 		// retrieve the header from header[].
@@ -947,6 +929,24 @@ GameCube::GameCube(IRpFile *file)
 		default:
 			// Unknown system.
 			goto notSupported;
+	}
+
+	if (!d->mimeType) {
+		// MIME type hasn't been set yet.
+		// Set it based on the system ID.
+		switch (d->discType & GameCubePrivate::DISC_SYSTEM_MASK) {
+			case GameCubePrivate::DISC_SYSTEM_GCN:
+			case GameCubePrivate::DISC_SYSTEM_TRIFORCE:
+				// TODO: Separate MIME type for Triforce.
+				d->mimeType = "application/x-gamecube-iso-image";
+				break;
+			case GameCubePrivate::DISC_SYSTEM_WII:
+				d->mimeType = "application/x-wii-iso-image";
+				break;
+			default:
+				assert(!"Invalid system ID...");
+				break;
+		}
 	}
 
 	// Disc image loaded successfully.