a20d835291be18a24944c5f933197cfe8da89007
David Korth <gerbilsoft@gerbilsoft.com>
2020-05-21 21:48:14 -0400

    [librpbase] Achievements: Implemented save() and load().
    save() is called if an achievement's status changes, e.g. the count is
    incremented or a bit in the bitfield changes from 0 to 1.
    
    load() is called if the achievements have not been loaded and unlock()
    has been called. I'll need to add another function to actually retrieve
    the achievements later to display them.
    
    save() saves using a custom binary format. This format is normally
    obfuscated in release builds, and stored in "ach.bin" in the config
    directory. In debug builds, it's stored unobfuscated in "achd.bin".
    
    The obfuscation isn't that complex, so cheating is possible, but don't
    do it. Thanks. :)

--- src/librpbase/Achievements.cpp
+++ src/librpbase/Achievements.cpp
@@ -10,10 +10,37 @@
 #include "Achievements.hpp"
 #include "libi18n/i18n.h"
 
+// librpcpu, librpfile
+#include "librpcpu/byteswap.h"
+#include "librpfile/FileSystem.hpp"
+#include "librpfile/RpFile.hpp"
+using namespace LibRpFile;
+
 // C++ STL classes.
+using std::string;
 using std::unordered_map;
 
+// Uninitialized vector class.
+// Reference: http://andreoffringa.org/?q=uvector
+#include "uvector.h"
+
+// zlib for CRC32.
+#include <zlib.h>
+#ifdef _MSC_VER
+// MSVC: Exception handling for /DELAYLOAD.
+# include "libwin32common/DelayLoadHelper.h"
+#endif /* _MSC_VER */
+
+// DEBUG: Uncomment this to force obfuscation in debug builds.
+// This will use ach.bin and "RPACH10R" magic.
+//#define FORCE_OBFUSCATE 1
+
 namespace LibRpBase {
+
+#ifdef _MSC_VER
+// DelayLoad test implementation.
+DELAYLOAD_TEST_FUNCTION_IMPL0(zlibVersion);
+#endif /* _MSC_VER */
 
 class AchievementsPrivate
 {
@@ -35,12 +62,17 @@ class AchievementsPrivate
 		Achievements::NotifyFunc notifyFunc;
 		intptr_t user_data;
 
+		// Have achievements been loaded from disk?
+		bool loaded;
+
 	public:
 		// Achievement types
 		enum AchType : uint8_t {
 			AT_COUNT = 0,	// Count (requires the same action X number of times)
 					// For BOOLEAN achievements, set count to 1.
 			AT_BITFIELD,	// Bitfield (multiple actions)
+
+			AT_MAX
 		};
 
 		// Achievement information.
@@ -69,12 +101,74 @@ class AchievementsPrivate
 		// TODO: Map vs. unordered_map for performance?
 		unordered_map<Achievements::ID, uint8_t, EnumClassHash> mapAchData_count;
 		unordered_map<Achievements::ID, uint64_t, EnumClassHash> mapAchData_bitfield;
+
+		// Achievements filename and magic number.
+#if defined(NDEBUG) || defined(FORCE_OBFUSCATE)
+		// Release version is obfuscated.
+		#define ACH_BIN_MAGIC    "RPACH10R"
+		#define ACH_BIN_FILENAME "ach.bin"
+#else /* !NDEBUG && !FORCE_OBFUSCATE */
+		// Debug version is not obfuscated.
+		#define ACH_BIN_MAGIC    "RPACH10D"
+		#define ACH_BIN_FILENAME "achd.bin"
+#endif /* NDEBUG || FORCE_OBFUSCATE*/
+
+		// Serialized achievement header.
+		// All fields are in little-endian.
+		struct AchBinHeader {
+			char magic[8];		// [0x000] "RPACH10R" or "RPACH10D"
+			uint32_t length;	// [0x00C] Length of remainder of file, in bytes. [includes count]
+			uint32_t crc32;		// [0x010] CRC32 of remainder of file. [includes count]
+			uint32_t count;		// [0x008] Number of achievements.
+		};
+
+		// The header is followed by achievement data: (1-byte alignment, little-endian)
+		// - uint16_t: Achievement ID
+		// - uint8_t: Achievement type
+		// - Data (uint8_t for AT_COUNT, uint64_t for AT_BITFIELD)
+
+		/**
+		 * Symmetric obfuscation function.
+		 * @param buf Data buffer.
+		 * @param size Size. (must be a multiple of 2)
+		 */
+		static void doObfuscate(uint8_t *buf, size_t size);
+
+		/**
+		 * Get the achievements filename.
+		 * @return Achievements filename.
+		 */
+		string getFilename(void) const
+		{
+			string filename = FileSystem::getConfigDirectory();
+			if (filename.empty())
+				return filename;
+
+			if (filename.at(filename.size()-1) != DIR_SEP_CHR) {
+				filename += DIR_SEP_CHR;
+			}
+			filename += ACH_BIN_FILENAME;
+			return filename;
+		}
+
+		/**
+		 * Save the achievements data.
+		 * @return 0 on success; negative POSIX error code on error.
+		 */
+		int save(void) const;
+
+		/**
+		 * Load the achievements data.
+		 * @return 0 on success; negative POSIX error code on error.
+		 */
+		int load(void);
 };
 
 /** AchievementsPrivate **/
 
 // Achievement information.
-const struct AchievementsPrivate::AchInfo_t AchievementsPrivate::achInfo[] = {
+const struct AchievementsPrivate::AchInfo_t AchievementsPrivate::achInfo[] =
+{
 	{
 		// tr: ViewedDebugCryptedFile (name)
 		NOP_C_("Achievements", "You are now a developer!"),
@@ -113,7 +207,356 @@ Achievements AchievementsPrivate::instance;
 AchievementsPrivate::AchievementsPrivate()
 	: notifyFunc(nullptr)
 	, user_data(0)
+	, loaded(false)
 { }
+
+/**
+ * Symmetric obfuscation function.
+ * @param buf Data buffer.
+ * @param size Size. (must be a multiple of 2)
+ */
+void AchievementsPrivate::doObfuscate(uint8_t *buf, size_t size)
+{
+	assert(size % 2 == 0);
+	uint16_t *buf16 = reinterpret_cast<uint16_t*>(buf);
+
+	// Based on 16-stage LFSR, similar to SMS SN76489.
+	uint16_t lfsr = 0x8000;
+	// Run for 32 cycles to initialize the LFSR.
+	for (unsigned int i = 32; i > 0; i--) {
+		// Bits 3 and 0 are XOR'd to form the next input.
+		unsigned int n = ((lfsr & 0x08) >> 3) ^ (lfsr & 0x01);
+		lfsr >>= 1;
+		lfsr |= (n << 15);
+	}
+
+	for (; size > 1; buf16++, size -= 2) {
+		uint16_t data = ~(*buf16);
+		data ^= lfsr;
+		if (size & 4) {
+			data ^= 0x5A5A;
+		} else {
+			data ^= 0xA5A5;
+		}
+		*buf16 = data;
+
+		// Bits 3 and 0 are XOR'd to form the next input.
+		unsigned int n = ((lfsr & 0x08) >> 3) ^ (lfsr & 0x01);
+		lfsr >>= 1;
+		lfsr |= (n << 15);
+	}
+}
+
+/**
+ * Save the achievements data.
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int AchievementsPrivate::save(void) const
+{
+#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
+	// Delay load verification.
+	// TODO: Only if linked with /DELAYLOAD?
+	if (DelayLoad_test_zlibVersion() != 0) {
+		// Delay load failed.
+		// Can't calculate the CRC32.
+		return -ENOTSUP;
+	}
+#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
+
+	// Create the achievements file in memory.
+	ao::uvector<uint8_t> buf;
+	buf.reserve(sizeof(AchBinHeader) + ((int)Achievements::ID::Max * 12));
+	buf.resize(sizeof(AchBinHeader));
+
+	// Header.
+	AchBinHeader *const header = reinterpret_cast<AchBinHeader*>(buf.data());
+	memcpy(header->magic, ACH_BIN_MAGIC, sizeof(header->magic));
+	header->length = 0;
+	header->crc32 = 0;
+	header->count = cpu_to_le32(static_cast<uint32_t>(mapAchData_count.size() + mapAchData_bitfield.size()));
+
+	// Process all achievements in order of ID.
+	for (unsigned int i = 0; i < (int)Achievements::ID::Max; i++) {
+		switch (achInfo[i].type) {
+			case AT_COUNT: {
+				auto iter = mapAchData_count.find((Achievements::ID)i);
+				if (iter == mapAchData_count.end())
+					break;
+				// Found achievement data. Write it out.
+				buf.resize(buf.size()+4);
+				uint8_t *const p = &buf.data()[buf.size()-4];
+				// uint16_t: Achievement ID
+				p[0] = (i & 0xFF);
+				p[1] = (i >> 8) & 0xFF;
+				// uint8_t: Achievement type
+				p[2] = AT_COUNT;
+				// uint8_t: Count
+				p[3] = iter->second;
+				break;
+			}
+
+			case AT_BITFIELD: {
+				// TODO: NEEDS TESTING!!!
+				auto iter = mapAchData_bitfield.find((Achievements::ID)i);
+				if (iter == mapAchData_bitfield.end())
+					break;
+				// Found achievement data. Write it out.
+				buf.resize(buf.size()+4);
+				uint8_t *p = &buf.data()[buf.size()-4];
+				// uint16_t: Achievement ID
+				p[0] = (i & 0xFF);
+				p[1] = (i >> 8) & 0xFF;
+				// uint8_t: Achievement type
+				p[2] = AT_COUNT;
+				// uint64_t: Bitfield
+				// NOTE: Writing a byte at a time to prevent alignment issues.
+				p += 3;
+				uint64_t val = iter->second;
+				for (unsigned int n = 8; n > 0; n--, val >>= 8, p++) {
+					*p = (val & 0xFF);
+				}
+				break;
+			}
+
+			default:
+				assert(!"Unsupported achievement.");
+				break;
+		}
+	}
+
+	static const size_t HeaderSizeMinusCount = sizeof(AchBinHeader) - sizeof(header->count);
+
+	// Length of achievement data.
+	// Includes count, but not crc32.
+	header->length = buf.size() - HeaderSizeMinusCount;
+
+	// CRC32 of achievement data.
+	// Includes count.
+	if (buf.size() > HeaderSizeMinusCount) {
+		header->crc32 = cpu_to_le32(crc32(0, &buf.data()[HeaderSizeMinusCount], buf.size() - HeaderSizeMinusCount));
+	}
+
+#if defined(NDEBUG) || defined(FORCE_OBFUSCATE)
+	// Obfuscate the data.
+	if (buf.size() % 2 != 0) {
+		buf.push_back(0);
+	};
+	doObfuscate(buf.data(), buf.size());
+#endif /* NDEBUG || FORCE_OBFUSCATE */
+
+	// Write the achievements file.
+	string filename = getFilename();
+	if (filename.empty()) {
+		// Unable to get the filename.
+		return -EIO;
+	}
+
+	RpFile *const file = new RpFile(filename, RpFile::FM_CREATE_WRITE);
+	if (!file->isOpen()) {
+		int ret = -file->lastError();
+		if (ret == 0) {
+			ret = -EIO;
+		}
+		file->unref();
+		return ret;
+	}
+	size_t size = file->write(buf.data(), buf.size());
+	if (size != buf.size()) {
+		// Short write.
+		// TODO: Delete the file?
+		int ret = -file->lastError();
+		if (ret == 0) {
+			ret = -EIO;
+		}
+		file->unref();
+		return -EIO;
+	}
+	file->unref();
+
+	// Achievements written.
+	const_cast<AchievementsPrivate*>(this)->loaded = true;
+	return 0;
+}
+
+/**
+ * Load the achievements data.
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int AchievementsPrivate::load(void)
+{
+#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
+	// Delay load verification.
+	// TODO: Only if linked with /DELAYLOAD?
+	if (DelayLoad_test_zlibVersion() != 0) {
+		// Delay load failed.
+		// Can't calculate the CRC32.
+		return -ENOTSUP;
+	}
+#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
+
+	// Clear loaded achievements.
+	loaded = false;
+	mapAchData_count.clear();
+	mapAchData_bitfield.clear();
+
+	// Load the achievements file in memory.
+	string filename = getFilename();
+	if (filename.empty()) {
+		// Unable to get the filename.
+		return -EIO;
+	}
+	RpFile *const file = new RpFile(filename, RpFile::FM_OPEN_READ);
+	if (!file->isOpen()) {
+		int ret = -file->lastError();
+		if (ret == 0) {
+			ret = -EIO;
+		}
+		file->unref();
+		return ret;
+	}
+
+	const int64_t fileSize = file->size();
+	if (fileSize > 1*1024*1024) {
+		// 1 MB is probably way too much...
+		file->unref();
+		return -ENOMEM;
+	}
+
+	ao::uvector<uint8_t> buf;
+	buf.resize(static_cast<size_t>(fileSize));
+	size_t size = file->read(buf.data(), buf.size());
+	if (size != buf.size()) {
+		int ret = -file->lastError();
+		if (ret == 0) {
+			ret = -EIO;
+		}
+		file->unref();
+		return ret;
+	}
+	file->unref();
+
+#if defined(NDEBUG) || defined(FORCE_OBFUSCATE)
+	// Deobfuscate the data.
+	if (buf.size() % 2 != 0) {
+		buf.push_back(0);
+	};
+	doObfuscate(buf.data(), buf.size());
+#endif /* NDEBUG || FORCE_OBFUSCATE */
+
+	// Check the header.
+	AchBinHeader *const header = reinterpret_cast<AchBinHeader*>(buf.data());
+	if (memcmp(header->magic, ACH_BIN_MAGIC, sizeof(header->magic)) != 0) {
+		// Incorrect header.
+		return -EBADF;
+	}
+
+	// Length should be >= HeaderSizeMinusCount, and less than 1 MB.
+	static const size_t HeaderSizeMinusCount = sizeof(AchBinHeader) - sizeof(header->count);
+	const uint32_t data_len = le32_to_cpu(header->length);
+	if (data_len < sizeof(header->count) ||
+	    data_len >= ((1*1024*1024)-HeaderSizeMinusCount) ||
+	    data_len + HeaderSizeMinusCount < buf.size())
+	{
+		// Incorrect length.
+		return -EBADF;
+	}
+
+	// Resize the buffer to get rid of any extra data.
+	buf.resize(data_len + HeaderSizeMinusCount);
+
+	// Verify the CRC32.
+	const uint32_t crc = crc32(0, &buf.data()[HeaderSizeMinusCount], data_len);
+	if (crc != le32_to_cpu(header->crc32)) {
+		// Incorrect CRC32.
+		return -EBADF;
+	}
+
+	// Process all achievements.
+	bool ok = true;
+	const uint8_t *p = &buf.data()[sizeof(AchBinHeader)];
+	const uint8_t *const p_end = &buf.data()[buf.size()];
+	uint32_t ach_count = le32_to_cpu(header->count);
+	for (; ok && p+3 < p_end && ach_count > 0; ach_count--) {
+		// Achievement ID.
+		const uint16_t id = p[0] | (p[1] << 8);
+		if (id >= (int)Achievements::ID::Max) {
+			// Invalid ID.
+			// NOTE: Allowing this in case the user is downgrading
+			// from a newer version that has more achievements.
+			// NOTE: We can't validate the type if the achievement ID is out of range.
+			assert(!"Achievement ID is out of range.");
+		} else {
+			// Make sure the type is correct.
+			if (achInfo[id].type != p[2]) {
+				// Incorrect type.
+				assert(!"Achievement type is incorrect for specified ID.");
+				ok = false;
+				break;
+			}
+		}
+
+		// Check the type byte.
+		switch (p[2]) {
+			case AT_COUNT: {
+				// Check for duplicates.
+				// If found, ignore this.
+				auto iter = mapAchData_count.find((Achievements::ID)id);
+				if (iter != mapAchData_count.end()) {
+					// Found a duplicate.
+					assert(!"Duplicate AT_COUNT achievement found!");
+					p += 4;
+					break;
+				}
+				// Save the achievement count.
+				mapAchData_count[(Achievements::ID)id] = p[3];
+				break;
+			}
+
+			case AT_BITFIELD: {
+				// TODO: NEEDS TESTING!!!
+				// Check for duplicates.
+				// If found, ignore this.
+				auto iter = mapAchData_bitfield.find((Achievements::ID)id);
+				if (iter != mapAchData_bitfield.end()) {
+					// Found a duplicate.
+					assert(!"Duplicate AT_BITFIELD achievement found!");
+					p += 8;
+					break;
+				}
+				// Make sure we have 8 bytes available.
+				if (p+3+8 >= p_end) {
+					// Out of range.
+					assert(!"Not enough data bytes for AT_BITFIELD.");
+					ok = false;
+					break;
+				}
+
+				// Convert the uint64_t.
+				uint64_t val = 0;
+				p += 3;
+				for (unsigned int n = 8; n > 0; n--, p++) {
+					val <<= 8;
+					val |= *p;
+				}
+
+				// Save the achievement bitfield.
+				mapAchData_bitfield[(Achievements::ID)id] = val;
+				break;
+			}
+
+			default:
+				// Unhandled achievement type.
+				// This is an error!
+				assert(!"Unhandled achievement type.");
+				ok = false;
+				break;
+		}
+	}
+
+	// Achievements loaded.
+	loaded = true;
+	return 0;
+}
 
 /** Achievements **/
 
@@ -197,7 +640,13 @@ void Achievements::unlock(ID id, int bit)
 		return;
 	}
 
+	// Make sure achievements have been loaded.
+	if (!d->loaded) {
+		d->load();
+	}
+
 	// Check the type.
+	bool unlocked = false;
 	const AchievementsPrivate::AchInfo_t *const achInfo = &d->achInfo[(int)id];
 	switch (achInfo->type) {
 		default:
@@ -214,12 +663,11 @@ void Achievements::unlock(ID id, int bit)
 			}
 
 			// Increment the count.
-			// TODO: Serialize and save the achievements.
 			count++;
 			d->mapAchData_count[id] = count;
-			if (count < achInfo->count) {
-				// Not unlocked yet.
-				return;
+			if (count >= achInfo->count) {
+				// Achievement unlocked!
+				unlocked = true;
 			}
 			break;
 		}
@@ -238,28 +686,37 @@ void Achievements::unlock(ID id, int bit)
 			const uint64_t bf_filled = (1ULL << achInfo->count) - 1;
 			uint64_t bf_value = d->mapAchData_bitfield[id];
 			if (bf_value == bf_filled) {
-				// Bitfield has been filled.
+				// Bitfield is already filled.
 				// Achievement is already unlocked.
 				return;
 			}
 
 			// Set the bit.
-			// TODO: Serialize and save the achievements.
-			bf_value |= (1 << (unsigned int)bit);
-			d->mapAchData_bitfield[id] = bf_value;
-			if (bf_value != bf_filled) {
-				// Bitfield is not filled yet.
+			uint64_t bf_new = bf_value | (1 << (unsigned int)bit);
+			if (bf_new == bf_value) {
+				// No change.
 				return;
+			}
+
+			d->mapAchData_bitfield[id] = bf_new;
+			if (bf_new == bf_filled) {
+				// Achievement unlocked!
+				unlocked = true;
 			}
 			break;
 		}
 	}
 
-	// Achievement unlocked!
-	if (d->notifyFunc) {
-		d->notifyFunc(d->user_data,
-			dpgettext_expr(RP_I18N_DOMAIN, "Achievements", achInfo->name),
-			dpgettext_expr(RP_I18N_DOMAIN, "Achievements", achInfo->desc));
+	// Save the achievement data.
+	d->save();
+
+	if (unlocked) {
+		// Achievement unlocked!
+		if (d->notifyFunc) {
+			d->notifyFunc(d->user_data,
+				dpgettext_expr(RP_I18N_DOMAIN, "Achievements", achInfo->name),
+				dpgettext_expr(RP_I18N_DOMAIN, "Achievements", achInfo->desc));
+		}
 	}
 }
 
