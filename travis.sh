7053d7d1df759143af9ed9e375730a841a08a165
David Korth <gerbilsoft@gerbilsoft.com>
2020-06-19 18:01:20 -0400

    travis.sh: Build once with optional features disabled, then again with them enabled.
    This should help us catch build issues with non-default build
    configuration options.

--- travis.sh
+++ travis.sh
@@ -4,6 +4,7 @@ mkdir "${TRAVIS_BUILD_DIR}/build"
 cd "${TRAVIS_BUILD_DIR}/build"
 cmake --version
 
+# Initial build with optional components disabled.
 case "$OSTYPE" in
 	darwin*)
 		# Mac OS X. Disable gettext for now.
@@ -14,8 +15,14 @@ case "$OSTYPE" in
 			-DENABLE_LTO=OFF \
 			-DENABLE_PCH=ON \
 			-DBUILD_TESTING=ON \
-			-DENABLE_JPEG=ON \
 			-DENABLE_NLS=OFF \
+			\
+			-DENABLE_JPEG=OFF \
+			-DENABLE_XML=OFF \
+			-DENABLE_DECRYPTION=OFF \
+			-DENABLE_UNICE68=OFF \
+			-DENABLE_LIBMSPACK=OFF \
+			-DENABLE_PVRTC=OFF \
 			|| exit 1
 		;;
 	*)
@@ -27,15 +34,21 @@ case "$OSTYPE" in
 			-DENABLE_LTO=OFF \
 			-DENABLE_PCH=ON \
 			-DBUILD_TESTING=ON \
-			-DENABLE_JPEG=ON \
 			-DENABLE_NLS=ON \
-			-DUSE_SECCOMP=ON \
 			-DBUILD_KDE4=ON \
 			-DBUILD_KDE5=OFF \
 			-DBUILD_XFCE=ON \
 			-DBUILD_XFCE3=OFF \
 			-DBUILD_GNOME=ON \
 			-DBUILD_MATE=OFF \
+			\
+			-DUSE_SECCOMP=OFF \
+			-DENABLE_JPEG=OFF \
+			-DENABLE_XML=OFF \
+			-DENABLE_DECRYPTION=OFF \
+			-DENABLE_UNICE68=OFF \
+			-DENABLE_LIBMSPACK=OFF \
+			-DENABLE_PVRTC=OFF
 			|| exit 1
 esac
 
@@ -45,4 +58,25 @@ make -k || RET=1
 LC_ALL="en_US.UTF8" ctest -V || RET=1
 # Test with fr_FR.UTF8 to find i18n issues.
 LC_ALL="fr_FR.UTF8" ctest -V || RET=1
+
+# Second build with optional components enabled.
+# NOTE: Seccomp is Linux only, so a warning will be printed
+# on other platforms.
+cmake .. \
+	-DUSE_SECCOMP=ON \
+	-DENABLE_JPEG=ON \
+	-DENABLE_XML=ON \
+	-DENABLE_DECRYPTION=ON \
+	-DENABLE_UNICE68=ON \
+	-DENABLE_LIBMSPACK=ON \
+	-DENABLE_PVRTC=ON
+
+# Build everything.
+make -k || RET=1
+# Test with en_US.UTF8.
+LC_ALL="en_US.UTF8" ctest -V || RET=1
+# Test with fr_FR.UTF8 to find i18n issues.
+LC_ALL="fr_FR.UTF8" ctest -V || RET=1
+
+# All done!
 exit "${RET}"
