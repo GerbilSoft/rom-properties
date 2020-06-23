GerbilSoft/rom-properties

commit ac75e09031d8d505484c44ec3a435ad8d3933593

[libromdata] GameCube: Added support for split WBFS files.
This adds proper detection for .wbfs and .wbf1 files.

[librpfile] DualFile: New IRpFile subclass that makes it easy to read
a split file (in two segments) as if it's a single file. This is used
for .wbfs/.wbf1.

src/librpfile/DualFile.cpp
@@ -1,0 +1,247 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librpfile)                        *
+ * DualFile.cpp: Special wrapper for handling a split file as one.         *
+ *                                                                         *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "stdafx.h"
+#include "DualFile.hpp"
+
+// C++ STL classes.
+using std::string;
+
+namespace LibRpFile {
+
+/**
+ * Open two files and handle them as if they're a single file.
+ * The resulting IRpFile is read-only.
+ *
+ * @param file0 First file.
+ * @param file1 Second file.
+ */
+DualFile::DualFile(IRpFile *file0, IRpFile *file1)
+	: super()
+	, m_pos(0)
+{
+	assert(file0 != nullptr);
+	assert(file1 != nullptr);
+	if (!file0 || !file1) {
+		// File is missing.
+		m_lastError = EBADF;
+		return;
+	}
+
+	m_file[0] = file0->ref();
+	m_file[1] = file1->ref();
+	m_size[0] = file0->size();
+	m_size[1] = file1->size();
+
+	m_fullSize = m_size[0] + m_size[1];
+}
+
+/**
+ * Internal constructor for use by subclasses.
+ * This initializes everything to nullptr.
+ */
+DualFile::DualFile()
+	: super()
+	, m_fullSize(0)
+	, m_pos(0)
+{
+	m_file[0] = nullptr;
+	m_file[1] = nullptr;
+	m_size[0] = 0;
+	m_size[1] = 0;
+}
+
+/**
+ * Is the file open?
+ * This usually only returns false if an error occurred.
+ * @return True if the file is open; false if it isn't.
+ */
+bool DualFile::isOpen(void) const
+{
+	return (m_file[0] != nullptr && m_file[1] != nullptr);
+}
+
+/**
+ * Close the file.
+ */
+void DualFile::close(void)
+{
+	if (m_file[0]) {
+		m_file[0]->unref();
+		m_file[0] = nullptr;
+	}
+	if (m_file[1]) {
+		m_file[1]->unref();
+		m_file[1] = nullptr;
+	}
+
+	m_size[0] = 0;
+	m_size[1] = 0;
+
+	m_pos = 0;
+}
+
+/**
+ * Read data from the file.
+ * @param ptr Output data buffer.
+ * @param size Amount of data to read, in bytes.
+ * @return Number of bytes read.
+ */
+size_t DualFile::read(void *ptr, size_t size)
+{
+	if (!m_file[0] || !m_file[1]) {
+		m_lastError = EBADF;
+		return 0;
+	}
+
+	if (unlikely(size == 0)) {
+		// Not reading anything...
+		return 0;
+	}
+
+	// uint8_t pointer access.
+	uint8_t *ptr8 = static_cast<uint8_t*>(ptr);
+
+	// Check if the read is fully within file 0.
+	if (m_pos < m_size[0] && ((m_pos + static_cast<off64_t>(size)) < m_size[0])) {
+		// Read is fully within file 0.
+		const size_t sz_read = m_file[0]->seekAndRead(m_pos, ptr8, size);
+		m_lastError = m_file[0]->lastError();
+		m_pos += sz_read;
+		return sz_read;
+	}
+
+	// Check if the read is fully within file 1.
+	if (m_pos >= m_size[0]) {
+		// Fully within file 1.
+		// NOTE: If the size is past the bounds, the read will be truncated.
+		const size_t sz_read = m_file[1]->seekAndRead(m_pos - m_size[0], ptr8, size);
+		m_lastError = m_file[1]->lastError();
+		m_pos += sz_read;
+		return sz_read;
+	}
+
+	// Read crosses the boundary between file 0 and file 1.
+
+	// File 0 portion.
+	const size_t file0_sz = m_size[0] - m_pos;
+	size_t sz0_read = m_file[0]->seekAndRead(m_pos, ptr8, file0_sz);
+	m_lastError = m_file[0]->lastError();
+	m_pos += sz0_read;
+	if (sz0_read != file0_sz) {
+		// Short read.
+		return sz0_read;
+	}
+	size -= sz0_read;
+	ptr8 += sz0_read;
+
+	// File 1 portion.
+	size_t sz1_read = m_file[1]->seekAndRead(0, ptr8, size);
+	m_lastError = m_file[1]->lastError();
+	m_pos += sz1_read;
+
+	return (sz0_read + sz1_read);
+}
+
+/**
+ * Write data to the file.
+ * (NOTE: Not valid for DualFile; this will always return 0.)
+ * @param ptr Input data buffer.
+ * @param size Amount of data to read, in bytes.
+ * @return Number of bytes written.
+ */
+size_t DualFile::write(const void *ptr, size_t size)
+{
+	// Not a valid operation for DualFile.
+	RP_UNUSED(ptr);
+	RP_UNUSED(size);
+	m_lastError = EBADF;
+	return 0;
+}
+
+/**
+ * Set the file position.
+ * @param pos File position.
+ * @return 0 on success; -1 on error.
+ */
+int DualFile::seek(off64_t pos)
+{
+	if (!m_file[0] || !m_file[1]) {
+		m_lastError = EBADF;
+		return -1;
+	}
+
+	// NOTE: m_pos is size_t, since it's referring to
+	// a position within a memory buffer.
+	if (pos <= 0) {
+		m_pos = 0;
+	} else if (pos >= m_fullSize) {
+		m_pos = m_fullSize;
+	} else {
+		m_pos = pos;
+	}
+
+	return 0;
+}
+
+/**
+ * Get the file position.
+ * @return File position, or -1 on error.
+ */
+off64_t DualFile::tell(void)
+{
+	if (!m_file[0] || !m_file[1]) {
+		m_lastError = EBADF;
+		return 0;
+	}
+
+	return m_pos;
+}
+
+/**
+ * Truncate the file.
+ * (NOTE: Not valid for DualFile; this will always return -1.)
+ * @param size New size. (default is 0)
+ * @return 0 on success; -1 on error.
+ */
+int DualFile::truncate(off64_t size)
+{
+	// Not supported.
+	// TODO: Writable DualFile?
+	RP_UNUSED(size);
+	m_lastError = ENOTSUP;
+	return -1;
+}
+
+/** File properties **/
+
+/**
+ * Get the file size.
+ * @return File size, or negative on error.
+ */
+off64_t DualFile::size(void)
+{
+	if (!m_file[0] || !m_file[1]) {
+		m_lastError = EBADF;
+		return -1;
+	}
+
+	return m_fullSize;
+}
+
+/**
+ * Get the filename.
+ * @return Filename. (May be empty if the filename is not available.)
+ */
+string DualFile::filename(void) const
+{
+	// TODO: Implement this?
+	return string();
+}
+
+}

src/librpfile/DualFile.hpp
@@ -1,0 +1,115 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librpfile)                        *
+ * DualFile.hpp: Special wrapper for handling a split file as one.         *
+ *                                                                         *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBRPFILE_DUALFILE_HPP__
+#define __ROMPROPERTIES_LIBRPFILE_DUALFILE_HPP__
+
+#include "IRpFile.hpp"
+
+namespace LibRpFile {
+
+class DualFile : public IRpFile
+{
+	public:
+		/**
+		 * Open two files and handle them as if they're a single file.
+		 * The resulting IRpFile is read-only.
+		 *
+		 * @param file0 First file.
+		 * @param file1 Second file.
+		 */
+		DualFile(IRpFile *file0, IRpFile *file1);
+	protected:
+		/**
+		 * Internal constructor for use by subclasses.
+		 * This initializes everything to nullptr.
+		 */
+		DualFile();
+	protected:
+		virtual ~DualFile() { }	// call unref() instead
+
+	private:
+		typedef IRpFile super;
+		RP_DISABLE_COPY(DualFile)
+
+	public:
+		/**
+		 * Is the file open?
+		 * This usually only returns false if an error occurred.
+		 * @return True if the file is open; false if it isn't.
+		 */
+		bool isOpen(void) const final;
+
+		/**
+		 * Close the file.
+		 */
+		void close(void) override;
+
+		/**
+		 * Read data from the file.
+		 * @param ptr Output data buffer.
+		 * @param size Amount of data to read, in bytes.
+		 * @return Number of bytes read.
+		 */
+		size_t read(void *ptr, size_t size) final;
+
+		/**
+		 * Write data to the file.
+		 * (NOTE: Not valid for DualFile; this will always return 0.)
+		 * @param ptr Input data buffer.
+		 * @param size Amount of data to read, in bytes.
+		 * @return Number of bytes written.
+		 */
+		size_t write(const void *ptr, size_t size) final;
+
+		/**
+		 * Set the file position.
+		 * @param pos File position.
+		 * @return 0 on success; -1 on error.
+		 */
+		int seek(off64_t pos) final;
+
+		/**
+		 * Get the file position.
+		 * @return File position, or -1 on error.
+		 */
+		off64_t tell(void) final;
+
+		/**
+		 * Truncate the file.
+		 * (NOTE: Not valid for DualFile; this will always return -1.)
+		 * @param size New size. (default is 0)
+		 * @return 0 on success; -1 on error.
+		 */
+		int truncate(off64_t size = 0) final;
+
+	public:
+		/** File properties **/
+
+		/**
+		 * Get the file size.
+		 * @return File size, or negative on error.
+		 */
+		off64_t size(void) final;
+
+		/**
+		 * Get the filename.
+		 * @return Filename. (May be empty if the filename is not available.)
+		 */
+		std::string filename(void) const final;
+
+	protected:
+		IRpFile *m_file[2];
+		off64_t m_size[2];
+		off64_t m_fullSize;	// Combined sizes.
+		off64_t m_pos;		// Current position.
+};
+
+}
+
+#endif /* __ROMPROPERTIES_LIBRPFILE_DUALFILE_HPP__ */

src/libromdata/Console/GameCube.cpp
@@ -20,9 +20,11 @@
 #include "WiiCommon.hpp"
 
 // librpbase, librpfile, librptexture
+#include "librpfile/DualFile.hpp"
+#include "librpfile/RelatedFile.hpp"
 #include "librpbase/SystemRegion.hpp"
 using namespace LibRpBase;
-using LibRpFile::IRpFile;
+using namespace LibRpFile;
 using LibRpTexture::rp_image;
 
 // DiscReader
@@ -686,59 +688,141 @@ GameCube::GameCube(IRpFile *file)
 	info.header.addr = 0;
 	info.header.size = sizeof(header);
 	info.header.pData = header;
-	info.ext = nullptr;	// Not needed for GCN.
+	const string filename = file->filename();
+	info.ext = FileSystem::file_ext(filename);
 	info.szFile = 0;	// Not needed for GCN.
 	d->discType = isRomSupported_static(&info);
 
+	d->isValid = (d->discType >= 0);
+	if (!d->isValid) {
+		// Not supported.
+		d->file->unref();
+		d->file = nullptr;
+		return;
+	}
+
 	// TODO: DiscReaderFactory?
 	// TODO: More MIME types for e.g. Triforce, CISO, TGC, etc.
-	if (d->discType >= 0) {
-		switch (d->discType & GameCubePrivate::DISC_FORMAT_MASK) {
-			case GameCubePrivate::DISC_FORMAT_RAW:
-			case GameCubePrivate::DISC_FORMAT_PARTITION:
-				d->discReader = new DiscReader(d->file);
-				break;
-			case GameCubePrivate::DISC_FORMAT_SDK:
-				// Skip the SDK header.
-				d->discReader = new DiscReader(d->file, 32768, -1);
-				break;
-			case GameCubePrivate::DISC_FORMAT_TGC: {
-				d->fileType = FTYPE_EMBEDDED_DISC_IMAGE;
+	switch (d->discType & GameCubePrivate::DISC_FORMAT_MASK) {
+		case GameCubePrivate::DISC_FORMAT_RAW:
+		case GameCubePrivate::DISC_FORMAT_PARTITION:
+			d->discReader = new DiscReader(d->file);
+			break;
+		case GameCubePrivate::DISC_FORMAT_SDK:
+			// Skip the SDK header.
+			d->discReader = new DiscReader(d->file, 32768, -1);
+			break;
 
+		case GameCubePrivate::DISC_FORMAT_TGC: {
+			d->fileType = FTYPE_EMBEDDED_DISC_IMAGE;
 				// Check the TGC header for the disc offset.
-				const GCN_TGC_Header *tgcHeader = reinterpret_cast<const GCN_TGC_Header*>(header);
-				uint32_t gcm_offset = be32_to_cpu(tgcHeader->header_size);
-				d->discReader = new DiscReader(d->file, gcm_offset, -1);
-				break;
-			}
-			case GameCubePrivate::DISC_FORMAT_WBFS:
-				d->mimeType = "application/x-wbfs";
-				d->discReader = new WbfsReader(d->file);
-				break;
-			case GameCubePrivate::DISC_FORMAT_CISO:
-				d->discReader = new CisoGcnReader(d->file);
-				break;
-			case GameCubePrivate::DISC_FORMAT_NASOS:
-				d->mimeType = "application/x-nasos-image";
-				d->discReader = new NASOSReader(d->file);
-				break;
-			case GameCubePrivate::DISC_FORMAT_WIA:
-				// TODO: Implement WiaReader.
-				// For now, only the header will be readable.
-				d->mimeType = "application/x-wia";
-				d->discReader = nullptr;
-				break;
-
-			case GameCubePrivate::DISC_FORMAT_UNKNOWN:
-			default:
-				d->discType = GameCubePrivate::DISC_UNKNOWN;
-				break;
+			const GCN_TGC_Header *tgcHeader = reinterpret_cast<const GCN_TGC_Header*>(header);
+			uint32_t gcm_offset = be32_to_cpu(tgcHeader->header_size);
+			d->discReader = new DiscReader(d->file, gcm_offset, -1);
+			break;
 		}
+
+		case GameCubePrivate::DISC_FORMAT_WBFS: {
+			d->mimeType = "application/x-wbfs";
+			// Check for split WBFS.
+			if (info.ext && !strcasecmp(info.ext, ".wbf1")) {
+				// Second part of split WBFS.
+				IRpFile *const wbfs0 = FileSystem::openRelatedFile(file->filename().c_str(), nullptr, ".wbfs");
+				if (unlikely(!wbfs0) || !wbfs0->isOpen()) {
+					// Unable to open the .wbfs file.
+					if (wbfs0) {
+						wbfs0->unref();
+					}
+					d->discType = GameCubePrivate::DISC_UNKNOWN;
+					break;
+				}
+
+				// Split .wbfs/.wbf1.
+				DualFile *const dualFile = new DualFile(wbfs0, d->file);
+				if (!dualFile->isOpen()) {
+					// Unable to open DualFile.
+					dualFile->unref();
+					d->discType = GameCubePrivate::DISC_UNKNOWN;
+					break;
+				}
+
+				// DualFile maintains its own references, so unreference
+				// d->file and replace it with the DualFile.
+				IRpFile *const file_tmp = d->file;
+				d->file = dualFile;
+				file_tmp->unref();
+				wbfs0->unref();
+
+				// Open the WbfsReader.
+				d->discReader = new WbfsReader(d->file);
+			} else if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_WBFS) {
+				// First part of split WBFS.
+				// Check for a WBF1 file.
+				IRpFile *wbfs1 = FileSystem::openRelatedFile(file->filename().c_str(), nullptr, ".wbf1");
+				if (unlikely(wbfs1 && !wbfs1->isOpen())) {
+					// Unable to open the .wbf1 file.
+					// Assume it's a single .wbfs file.
+					if (wbfs1) {
+						wbfs1->unref();
+						wbfs1 = nullptr;
+					}
+				}
+
+				if (likely(!wbfs1)) {
+					// Single .wbfs file.
+					d->discReader = new WbfsReader(d->file);
+					break;
+				}
+
+				// Split .wbfs/.wbf1.
+				DualFile *const dualFile = new DualFile(d->file, wbfs1);
+				if (!dualFile->isOpen()) {
+					// Unable to open DualFile.
+					dualFile->unref();
+					d->discType = GameCubePrivate::DISC_UNKNOWN;
+					break;
+				}
+
+				// DualFile maintains its own references, so unreference
+				// d->file and replace it with the DualFile.
+				IRpFile *const file_tmp = d->file;
+				d->file = dualFile;
+				file_tmp->unref();
+				wbfs1->unref();
+
+				// Open the WbfsReader.
+				d->discReader = new WbfsReader(d->file);
+			} else {
+				// Not supported.
+				d->discType = GameCubePrivate::DISC_UNKNOWN;
+			}
+
+			break;
+		}
+
+		case GameCubePrivate::DISC_FORMAT_CISO:
+			d->discReader = new CisoGcnReader(d->file);
+			break;
+		case GameCubePrivate::DISC_FORMAT_NASOS:
+			d->mimeType = "application/x-nasos-image";
+			d->discReader = new NASOSReader(d->file);
+			break;
+		case GameCubePrivate::DISC_FORMAT_WIA:
+			// TODO: Implement WiaReader.
+			// For now, only the header will be readable.
+			d->mimeType = "application/x-wia";
+			d->discReader = nullptr;
+			break;
+
+		case GameCubePrivate::DISC_FORMAT_UNKNOWN:
+		default:
+			d->discType = GameCubePrivate::DISC_UNKNOWN;
+			break;
 	}
 
 	d->isValid = (d->discType >= 0);
 	if (!d->isValid) {
-		// Nothing else to do here.
+		// Not supported.
 		d->file->unref();
 		d->file = nullptr;
 		return;
@@ -1080,6 +1164,12 @@ int GameCube::isRomSupported_static(const DetectInfo *info)
 				return (GameCubePrivate::DISC_SYSTEM_GCN | GameCubePrivate::DISC_FORMAT_WBFS);
 			}
 		}
+	}
+	// Check for WBF1. (second file of WBFS split files)
+	if (info->ext && !strcasecmp(info->ext, ".wbf1")) {
+		// WBF1 file.
+		// We'll need to check the first file for the system information later.
+		return (GameCubePrivate::DISC_SYSTEM_UNKNOWN | GameCubePrivate::DISC_FORMAT_WBFS);
 	}
 
 	// Check for CISO.

src/librpfile/CMakeLists.txt
@@ -17,6 +17,7 @@ SET(librpfile_SRCS
 	RpVectorFile.cpp
 	FileSystem_common.cpp
 	RelatedFile.cpp
+	DualFile.cpp
 	scsi/RpFile_Kreon.cpp
 	scsi/RpFile_scsi.cpp
 	)
@@ -29,6 +30,7 @@ SET(librpfile_H
 	RpVectorFile.hpp
 	FileSystem.hpp
 	RelatedFile.hpp
+	DualFile.hpp
 	scsi/ata_protocol.h
 	scsi/scsi_protocol.h
 	scsi/scsi_ata_cmds.h

commit d51382df55158a86959f0fcd65baf0588e2b2333

[librpfile] DualFile: Make sure both files are unref()'d when the DualFile is deleted.
Potential resource leak when parsing split WBFS files.

This fixes a regression from commit ac75e09031d8d505484c44ec3a435ad8d3933593.
([libromdata] GameCube: Added support for split WBFS files.)

src/librpfile/DualFile.cpp
@@ -56,6 +56,16 @@ DualFile::DualFile()
 	m_size[1] = 0;
 }
 
+DualFile::~DualFile()
+{
+	if (m_file[0]) {
+		m_file[0]->unref();
+	}
+	if (m_file[1]) {
+		m_file[1]->unref();
+	}
+}
+
 /**
  * Is the file open?
  * This usually only returns false if an error occurred.

src/librpfile/DualFile.hpp
@@ -31,7 +31,7 @@ class DualFile : public IRpFile
 		 */
 		DualFile();
 	protected:
-		virtual ~DualFile() { }	// call unref() instead
+		virtual ~DualFile();	// call unref() instead
 
 	private:
 		typedef IRpFile super;