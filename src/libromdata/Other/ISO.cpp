GerbilSoft/rom-properties

commit df7e8b5704ce02dbf9dc36dd90637b7976f634a9

[libromdata] Added support for reading CD-ROM XA Mode 2 sectors.
Mode 2 has a different data start offset, so we need to check the mode
value in the raw header before reading the raw data.

PS1 BIN/CUEs can now be read properly using PlayStationDisc.

src/libromdata/Other/ISO.cpp
@@ -9,6 +9,8 @@
 
 #include "stdafx.h"
 #include "ISO.hpp"
+
+#include "../cdrom_structs.h"
 #include "../iso_structs.h"
 #include "hsfs_structs.h"
 
@@ -334,16 +336,14 @@ void ISOPrivate::addPVDTimestamps(const T *pvd)
  */
 ISOPrivate::DiscType ISOPrivate::checkPVD(void) const
 {
-	if (pvd.iso.header.type == ISO_VDT_PRIMARY &&
-	    pvd.iso.header.version == ISO_VD_VERSION &&
+	if (pvd.iso.header.type == ISO_VDT_PRIMARY && pvd.iso.header.version == ISO_VD_VERSION &&
 	    !memcmp(pvd.iso.header.identifier, ISO_VD_MAGIC, sizeof(pvd.iso.header.identifier)))
 	{
 		// ISO-9660 PVD.
 		return DiscType::ISO9660;
 	}
 
-	if (pvd.hsfs.header.type == ISO_VDT_PRIMARY &&
-	    pvd.hsfs.header.version == HSFS_VD_VERSION &&
+	if (pvd.hsfs.header.type == ISO_VDT_PRIMARY && pvd.hsfs.header.version == HSFS_VD_VERSION &&
 	    !memcmp(pvd.hsfs.header.identifier, HSFS_VD_MAGIC, sizeof(pvd.hsfs.header.identifier)))
 	{
 		// High Sierra PVD.
@@ -403,20 +403,29 @@ ISO::ISO(IRpFile *file)
 		d->sector_offset = ISO_DATA_OFFSET_MODE1_COOKED;
 	} else {
 		// Try again using 2352-byte sectors.
-		size = d->file->seekAndRead(ISO_PVD_ADDRESS_2352 + ISO_DATA_OFFSET_MODE1_RAW,
-			&d->pvd, sizeof(d->pvd));
-		if (size != sizeof(d->pvd)) {
+		CDROM_2352_Sector_t sector;
+		size = d->file->seekAndRead(ISO_PVD_ADDRESS_2352, &sector, sizeof(sector));
+		if (size != sizeof(sector)) {
 			// Seek and/or read error.
 			d->file->unref();
 			d->file = nullptr;
 			return;
 		}
 
+		// Data start offset depends on whether it's Mode 1 or Mode 2.
+		const uint8_t *data;
+		if (sector.mode == 2) {
+			data = sector.m2xa_f1.data;
+		} else {
+			data = sector.m1.data;
+		}
+		memcpy(&d->pvd, data, sizeof(d->pvd));
+
 		d->discType = d->checkPVD();
 		if (d->discType > ISOPrivate::DiscType::Unknown) {
 			// Found the PVD using 2352-byte sectors.
 			d->sector_size = ISO_SECTOR_SIZE_MODE1_RAW;
-			d->sector_offset = ISO_DATA_OFFSET_MODE1_RAW;
+			d->sector_offset = (sector.mode == 2 ? ISO_DATA_OFFSET_MODE2_XA : ISO_DATA_OFFSET_MODE1_RAW);
 		} else {
 			// Not a PVD.
 			d->file->unref();

src/libromdata/RomDataFactory.cpp
@@ -55,6 +55,7 @@ using std::vector;
 #include "Console/Xbox360_XEX.hpp"
 
 // Special handling for Xbox and PlayStation discs.
+#include "cdrom_structs.h"
 #include "iso_structs.h"
 #include "Console/XboxDisc.hpp"
 #include "Console/PlayStationDisc.hpp"
@@ -434,17 +435,43 @@ RomData *RomDataFactoryPrivate::checkISO(IRpFile *file)
 {
 	// Check for specific disc file systems.
 	// TODO: 2352-byte sector handling?
-	ISO_Primary_Volume_Descriptor pvd;
-	size_t size = file->seekAndRead(ISO_PVD_ADDRESS_2048, &pvd, sizeof(pvd));
-	if (size != sizeof(pvd)) {
+	CDROM_2352_Sector_t sector;
+	size_t size = file->seekAndRead(ISO_PVD_ADDRESS_2048, &sector.m1.data, sizeof(sector.m1.data));
+	if (size != sizeof(sector.m1.data)) {
 		// Unable to read the PVD.
 		return nullptr;
 	}
 
-	// Try various game disc file systems.
+	const ISO_Primary_Volume_Descriptor *pvd =
+		reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(sector.m1.data);
+	if (pvd->header.type != ISO_VDT_PRIMARY || pvd->header.version != ISO_VD_VERSION ||
+	    memcmp(pvd->header.identifier, ISO_VD_MAGIC, sizeof(pvd->header.identifier)) != 0)
+	{
+		// Not a valid PVD.
+		// Try 2352-byte sectors.
+		size_t size = file->seekAndRead(ISO_PVD_ADDRESS_2352, &sector, sizeof(sector));
+		if (size != sizeof(sector)) {
+			// Unable to read the PVD.
+			return nullptr;
+		}
+
+		// Data start offset depends on whether it's Mode 1 or Mode 2.
+		if (sector.mode == 2) {
+			pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(sector.m2xa_f1.data);
+		} else {
+			pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(sector.m1.data);
+		}
+
+		if (pvd->header.type != ISO_VDT_PRIMARY || pvd->header.version != ISO_VD_VERSION ||
+		    memcmp(pvd->header.identifier, ISO_VD_MAGIC, sizeof(pvd->header.identifier)) != 0)
+		{
+			// Not a valid PVD.
+			return nullptr;
+		}
+	}
 
 	// Xbox / Xbox 360
-	bool mayBeXbox = (XboxDisc::isRomSupported_static(&pvd) >= 0);
+	bool mayBeXbox = (XboxDisc::isRomSupported_static(pvd) >= 0);
 	if (!mayBeXbox) {
 		// This might be an extracted XDVDFS.
 		// Check for the magic number at the base offset.
@@ -472,7 +499,7 @@ RomData *RomDataFactoryPrivate::checkISO(IRpFile *file)
 	}
 
 	// PlayStation 1 and 2.
-	if (PlayStationDisc::isRomSupported_static(&pvd) >= 0) {
+	if (PlayStationDisc::isRomSupported_static(pvd) >= 0) {
 		// This might be a PS1 or PS2 disc.
 		RomData *const romData = new PlayStationDisc(file);
 		if (romData->isValid()) {

src/libromdata/Console/PlayStationDisc.cpp
@@ -16,6 +16,7 @@ using LibRpFile::IRpFile;
 using LibRpFile::RpFile;
 
 // IsoPartition
+#include "../cdrom_structs.h"
 #include "../iso_structs.h"
 #include "../disc/Cdrom2352Reader.hpp"
 #include "../disc/IsoPartition.hpp"
@@ -284,7 +285,7 @@ PlayStationDisc::PlayStationDisc(IRpFile *file)
 		d->file = nullptr;
 		return;
 	}
-	if (d->pvd.header.type == ISO_VDT_PRIMARY &&
+	if (d->pvd.header.type == ISO_VDT_PRIMARY && d->pvd.header.version == ISO_VD_VERSION &&
 	    !memcmp(d->pvd.header.identifier, ISO_VD_MAGIC, sizeof(d->pvd.header.identifier)))
 	{
 		// Disc has 2048-byte sectors.
@@ -292,13 +293,22 @@ PlayStationDisc::PlayStationDisc(IRpFile *file)
 		discReader = new DiscReader(d->file);
 	} else {
 		// Check for a PVD with 2352-byte sectors.
-		size_t size = d->file->seekAndRead(ISO_PVD_ADDRESS_2352, &d->pvd, sizeof(d->pvd));
-		if (size != sizeof(d->pvd)) {
+		CDROM_2352_Sector_t sector;
+		size_t size = d->file->seekAndRead(ISO_PVD_ADDRESS_2352, &sector, sizeof(sector));
+		if (size != sizeof(sector)) {
 			d->file->unref();
 			d->file = nullptr;
 			return;
 		}
-		if (d->pvd.header.type == ISO_VDT_PRIMARY &&
+
+		// Data start offset depends on whether it's Mode 1 or Mode 2.
+		if (sector.mode == 2) {
+			memcpy(&d->pvd, sector.m2xa_f1.data, sizeof(d->pvd));
+		} else {
+			memcpy(&d->pvd, sector.m1.data, sizeof(d->pvd));
+		}
+
+		if (d->pvd.header.type == ISO_VDT_PRIMARY && d->pvd.header.version == ISO_VD_VERSION &&
 		    !memcmp(d->pvd.header.identifier, ISO_VD_MAGIC, sizeof(d->pvd.header.identifier)))
 		{
 			// Disc has 2352-byte sectors.

src/libromdata/disc/Cdrom2352Reader.cpp
@@ -145,4 +145,63 @@ off64_t Cdrom2352Reader::getPhysBlockAddr(uint32_t blockIdx) const
 	return (static_cast<off64_t>(blockIdx) * d->physBlockSize) + 16;
 }
 
+/**
+ * Read the specified block.
+ *
+ * This can read either a full block or a partial block.
+ * For a full block, set pos = 0 and size = block_size.
+ *
+ * @param blockIdx	[in] Block index.
+ * @param ptr		[out] Output data buffer.
+ * @param pos		[in] Starting position. (Must be >= 0 and <= the block size!)
+ * @param size		[in] Amount of data to read, in bytes. (Must be <= the block size!)
+ * @return Number of bytes read, or -1 if the block index is invalid.
+ */
+int Cdrom2352Reader::readBlock(uint32_t blockIdx, void *ptr, int pos, size_t size)
+{
+	// Read 'size' bytes of block 'blockIdx', starting at 'pos'.
+	// NOTE: This can only be called by SparseDiscReader,
+	// so the main assertions are already checked there.
+	RP_D(Cdrom2352Reader);
+	assert(pos >= 0 && pos < (int)d->block_size);
+	assert(size <= d->block_size);
+	// TODO: Make sure overflow doesn't occur.
+	assert(static_cast<off64_t>(pos + size) <= static_cast<off64_t>(d->block_size));
+	if (pos < 0 || static_cast<off64_t>(pos + size) > static_cast<off64_t>(d->block_size)) {
+		// pos+size is out of range.
+		return -1;
+	}
+
+	if (unlikely(size == 0)) {
+		// Nothing to read.
+		return 0;
+	}
+
+	// Get the physical address first.
+	const off64_t physBlockAddr = static_cast<off64_t>(blockIdx) * d->physBlockSize;
+
+	// Read from the block.
+	// NOTE: We need to read the entire 2352-byte block in order to
+	// determine the data offset, since Mode 1 and Mode 2 have different
+	// sector layouts.
+	CDROM_2352_Sector_t sector;
+	size_t sz_read = m_file->seekAndRead(physBlockAddr, &sector, sizeof(sector));
+	m_lastError = m_file->lastError();
+	if (sz_read != sizeof(sector)) {
+		// Read error.
+		return -1;
+	}
+
+	// Determine the data start offset.
+	const uint8_t *data;
+	if (sector.mode == 2) {
+		data = sector.m2xa_f1.data;
+	} else {
+		data = sector.m1.data;
+	}
+
+	memcpy(ptr, &data[pos], size);
+	return size;
+}
+
 }

src/libromdata/disc/Cdrom2352Reader.hpp
@@ -60,6 +60,20 @@ class Cdrom2352Reader : public LibRpBase::SparseDiscReader
 		 * @return Physical address. (0 == empty block; -1 == invalid block index)
 		 */
 		off64_t getPhysBlockAddr(uint32_t blockIdx) const final;
+
+		/**
+		 * Read the specified block.
+		 *
+		 * This can read either a full block or a partial block.
+		 * For a full block, set pos = 0 and size = block_size.
+		 *
+		 * @param blockIdx	[in] Block index.
+		 * @param ptr		[out] Output data buffer.
+		 * @param pos		[in] Starting position. (Must be >= 0 and <= the block size!)
+		 * @param size		[in] Amount of data to read, in bytes. (Must be <= the block size!)
+		 * @return Number of bytes read, or -1 if the block index is invalid.
+		 */
+		int readBlock(uint32_t blockIdx, void *ptr, int pos, size_t size) final;
 };
 
 }

src/libromdata/disc/IsoPartition.cpp
@@ -85,7 +85,7 @@ IsoPartitionPrivate::IsoPartitionPrivate(IsoPartition *q,
 	// Load the primary volume descriptor.
 	// TODO: Assuming this is the first one.
 	// Check for multiple?
-	size_t size = q->m_discReader->seekAndRead(partition_offset + 0x8000, &pvd, sizeof(pvd));
+	size_t size = q->m_discReader->seekAndRead(partition_offset + ISO_PVD_ADDRESS_2048, &pvd, sizeof(pvd));
 	if (size != sizeof(pvd)) {
 		// Seek and/or read error.
 		q->m_discReader = nullptr;
@@ -93,8 +93,7 @@ IsoPartitionPrivate::IsoPartitionPrivate(IsoPartition *q,
 	}
 
 	// Verify the signature and volume descriptor type.
-	if (pvd.header.type != ISO_VDT_PRIMARY ||
-	    pvd.header.version != ISO_VD_VERSION ||
+	if (pvd.header.type != ISO_VDT_PRIMARY || pvd.header.version != ISO_VD_VERSION ||
 	    memcmp(pvd.header.identifier, ISO_VD_MAGIC, sizeof(pvd.header.identifier)) != 0)
 	{
 		// Invalid volume descriptor.
@@ -123,7 +122,7 @@ int IsoPartitionPrivate::loadRootDirectory(void)
 		// DiscReader isn't open.
 		q->m_lastError = EIO;
 		return -q->m_lastError;
-	} else if (unlikely(pvd.header.type != ISO_VDT_PRIMARY)) {
+	} else if (unlikely(pvd.header.type != ISO_VDT_PRIMARY || pvd.header.version != ISO_VD_VERSION)) {
 		// PVD isn't loaded.
 		q->m_lastError = EIO;
 		return -q->m_lastError;

src/libromdata/iso_structs.h
@@ -29,6 +29,7 @@ extern "C" {
 // Data offsets.
 #define ISO_DATA_OFFSET_MODE1_RAW    16
 #define ISO_DATA_OFFSET_MODE1_COOKED 0
+#define ISO_DATA_OFFSET_MODE2_XA     (16+8)
 
 // strD: [A-Z0-9_]
 // strA: strD plus: ! " % & ' ( ) * + , - . / : ; < = > ?

commit 19558473aed419f57d5c3e2e6f407816ef966d14

[libromdata] RomDataFactory: Fixed the disc image check for High Sierra.
When we added support for CD-ROM XA Mode 2 sectors, we added a check for
ISO-9660 PVDs in RomDataFactoryPrivate::checkISO(). This check didn't
include a check for High Sierra, so HSFS discs broke.

This commit moves ISO::checkPVD() to the public interface (returning an
int instead of a DiscType) and makes use of ISO::checkPVD() to check the
PVD.

This fixes a regression from commit df7e8b5704ce02dbf9dc36dd90637b7976f634a9.
([libromdata] Added support for reading CD-ROM XA Mode 2 sectors.)

TODO: Use ISO::checkPVD() in other RomData subclasses.

src/libromdata/Other/ISO.cpp
@@ -63,6 +63,7 @@ class ISOPrivate : public LibRpBase::RomDataPrivate
 		union {
 			ISO_Primary_Volume_Descriptor iso;	// ISO-9660
 			HSFS_Primary_Volume_Descriptor hsfs;	// High Sierra
+			uint8_t data[ISO_DATA_OFFSET_MODE1_COOKED];
 		} pvd;
 
 		// Sector size.
@@ -125,7 +126,10 @@ class ISOPrivate : public LibRpBase::RomDataPrivate
 		 * Check the PVD and determine its type.
 		 * @return DiscType value. (DiscType::Unknown if not valid)
 		 */
-		DiscType checkPVD(void) const;
+		inline DiscType checkPVD(void) const
+		{
+			return static_cast<DiscType>(ISO::checkPVD(pvd.data));
+		}
 };
 
 /** ISOPrivate **/
@@ -330,30 +334,6 @@ void ISOPrivate::addPVDTimestamps(const T *pvd)
 		RomFields::RFT_DATETIME_HAS_TIME);
 }
 
-/**
- * Check the PVD and determine its type.
- * @return DiscType value. (DiscType::Unknown if not valid)
- */
-ISOPrivate::DiscType ISOPrivate::checkPVD(void) const
-{
-	if (pvd.iso.header.type == ISO_VDT_PRIMARY && pvd.iso.header.version == ISO_VD_VERSION &&
-	    !memcmp(pvd.iso.header.identifier, ISO_VD_MAGIC, sizeof(pvd.iso.header.identifier)))
-	{
-		// ISO-9660 PVD.
-		return DiscType::ISO9660;
-	}
-
-	if (pvd.hsfs.header.type == ISO_VDT_PRIMARY && pvd.hsfs.header.version == HSFS_VD_VERSION &&
-	    !memcmp(pvd.hsfs.header.identifier, HSFS_VD_MAGIC, sizeof(pvd.hsfs.header.identifier)))
-	{
-		// High Sierra PVD.
-		return DiscType::HighSierra;
-	}
-
-	// Not recognized.
-	return DiscType::Unknown;
-}
-
 /** ISO **/
 
 /**
@@ -436,6 +416,37 @@ ISO::ISO(IRpFile *file)
 }
 
 /** ROM detection functions. **/
+
+/**
+ * Check for a valid PVD.
+ * @param data Potential PVD. (Must be 2048 bytes)
+ * @return DiscType if valid; -1 if not.
+ */
+int ISO::checkPVD(const uint8_t *data)
+{
+	// Check for an ISO-9660 PVD.
+	const ISO_Primary_Volume_Descriptor *const pvd_iso =
+		reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(data);
+	if (pvd_iso->header.type == ISO_VDT_PRIMARY && pvd_iso->header.version == ISO_VD_VERSION &&
+	    !memcmp(pvd_iso->header.identifier, ISO_VD_MAGIC, sizeof(pvd_iso->header.identifier)))
+	{
+		// This is an ISO-9660 PVD.
+		return static_cast<int>(ISOPrivate::DiscType::ISO9660);
+	}
+
+	// Check for a High Sierra PVD.
+	const HSFS_Primary_Volume_Descriptor *const pvd_hsfs =
+		reinterpret_cast<const HSFS_Primary_Volume_Descriptor*>(data);
+	if (pvd_hsfs->header.type == ISO_VDT_PRIMARY && pvd_hsfs->header.version == HSFS_VD_VERSION &&
+	    !memcmp(pvd_hsfs->header.identifier, HSFS_VD_MAGIC, sizeof(pvd_hsfs->header.identifier)))
+	{
+		// This is a High Sierra PVD.
+		return static_cast<int>(ISOPrivate::DiscType::HighSierra);
+	}
+
+	// Not supported.
+	return static_cast<int>(ISOPrivate::DiscType::Unknown);
+}
 
 /**
  * Is a ROM image supported by this class?

src/libromdata/Other/ISO.hpp
@@ -14,6 +14,15 @@
 namespace LibRomData {
 
 ROMDATA_DECL_BEGIN(ISO)
+
+	public:
+		/**
+		* Check for a valid PVD.
+		* @param data Potential PVD. (Must be 2048 bytes)
+		* @return DiscType if valid; -1 if not.
+		*/
+		static int checkPVD(const uint8_t *data);
+
 ROMDATA_DECL_END()
 
 }

src/libromdata/RomDataFactory.cpp
@@ -435,8 +435,7 @@ RomData *RomDataFactoryPrivate::openDreamcastVMSandVMI(IRpFile *file)
  */
 RomData *RomDataFactoryPrivate::checkISO(IRpFile *file)
 {
-	// Check for specific disc file systems.
-	// TODO: 2352-byte sector handling?
+	// Check for a CD file system with 2048-byte sectors.
 	CDROM_2352_Sector_t sector;
 	size_t size = file->seekAndRead(ISO_PVD_ADDRESS_2048, &sector.m1.data, sizeof(sector.m1.data));
 	if (size != sizeof(sector.m1.data)) {
@@ -444,27 +443,26 @@ RomData *RomDataFactoryPrivate::checkISO(IRpFile *file)
 		return nullptr;
 	}
 
-	const ISO_Primary_Volume_Descriptor *pvd =
-		reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(sector.m1.data);
-	if (pvd->header.type != ISO_VDT_PRIMARY || pvd->header.version != ISO_VD_VERSION ||
-	    memcmp(pvd->header.identifier, ISO_VD_MAGIC, sizeof(pvd->header.identifier)) != 0)
-	{
-		// Not a valid PVD.
-		// Try 2352-byte sectors.
+	const ISO_Primary_Volume_Descriptor *pvd;
+	int discType = ISO::checkPVD(sector.m1.data);
+	if (discType >= 0) {
+		// Found a Mode 1 PVD.
+		pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(sector.m1.data);
+	} else {
+		// Check for a CD file system with 2352-byte sectors.
 		size_t size = file->seekAndRead(ISO_PVD_ADDRESS_2352, &sector, sizeof(sector));
 		if (size != sizeof(sector)) {
 			// Unable to read the PVD.
 			return nullptr;
 		}
 
-		// Copy the PVD from the sector.
-		// NOTE: Sector user data area position depends on the sector mode.
-		pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(cdromSectorDataPtr(&sector));
-
-		if (pvd->header.type != ISO_VDT_PRIMARY || pvd->header.version != ISO_VD_VERSION ||
-		    memcmp(pvd->header.identifier, ISO_VD_MAGIC, sizeof(pvd->header.identifier)) != 0)
-		{
-			// Not a valid PVD.
+		const uint8_t *const pData = cdromSectorDataPtr(&sector);
+		discType = ISO::checkPVD(pData);
+		if (discType >= 0) {
+			// Found a Mode 2 PVD.
+			pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(pData);
+		} else {
+			// Not a supported disc.
 			return nullptr;
 		}
 	}