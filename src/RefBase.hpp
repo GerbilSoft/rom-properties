GerbilSoft/rom-properties

commit 1d553f45d88298ae34850dce9fdf3300e1955576

Moved unique_IRpFile<> down to unique_RefBase<>.
It can now be used for other RefBase subclasses, e.g. RomData,
FileFormat, and IDiscReader.

src/RefBase.hpp
@@ -80,4 +80,71 @@ class RefBase
 		volatile int m_ref_cnt;
 };
 
+/**
+ * unique_ptr<>-style class for RefBase subclasses.
+ * Takes the implied ref() for the RefBase subclass,
+ * and unref()'s it when it goes out of scope.
+ */
+template<class T>
+class unique_RefBase
+{
+	public:
+		inline explicit unique_RefBase(T *refBase)
+			: m_refBase(refBase)
+		{
+			static_assert(std::is_base_of<RefBase, T>::value,
+				"unique_RefBase<> only supports RefBase subclasses");
+		}
+
+		inline ~unique_RefBase()
+		{
+			UNREF(m_refBase);
+		}
+
+		/**
+		 * Get the RefBase*.
+		 * @return RefBase*
+		 */
+		inline T *get(void) { return m_refBase; }
+
+		/**
+		 * Release the IRpFile*.
+		 * This class will reset its pointer to nullptr,
+		 * and won't unref() the IRpFile* on destruction.
+		 * @return IRpFile*
+		 */
+		inline T *release(void)
+		{
+			T *const ptr = m_refBase;
+			m_refBase = nullptr;
+			return ptr;
+		}
+
+		/**
+		 * Dereference the RefBase*.
+		 * @return RefBase*
+		 */
+		inline T *operator->(void) const { return m_refBase; }
+
+		/**
+		 * Is the RefBase* valid or nullptr?
+		 * @return True if valid; false if nullptr.
+		 */
+		operator bool(void) const { return (m_refBase != nullptr); }
+
+	private:
+		// Disable copy/assignment constructors.
+#if __cplusplus >= 201103L
+		unique_RefBase(const unique_RefBase &) = delete;
+		unique_RefBase &operator=(const unique_RefBase &) = delete;
+#else
+#define RP_DISABLE_COPY(klass) \
+		unique_RefBase(const unique_RefBase &); \
+		unique_RefBase &operator=(const unique_RefBase &);
+#endif
+
+	private:
+		T *m_refBase;
+};
+
 #endif /* __ROMPROPERTIES_REFBASE_HPP__ */

src/libromdata/crypto/KeyStoreUI.cpp
@@ -1064,7 +1064,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiKeysBin(const char *filename)
 {
 	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
-	unique_IRpFile<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
+	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
 		// TODO: file->lastError()?
 		iret.status = ImportStatus::OpenError;
@@ -1125,7 +1125,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiUOtpBin(const char *filename)
 {
 	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
-	unique_IRpFile<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
+	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
 		iret.status = ImportStatus::OpenError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
@@ -1195,7 +1195,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 {
 	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
-	unique_IRpFile<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
+	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
 		iret.status = ImportStatus::OpenError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
@@ -1267,7 +1267,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 {
 	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
-	unique_IRpFile<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
+	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
 		iret.status = ImportStatus::OpenError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());

src/libromdata/disc/NEResourceReader.cpp
@@ -695,7 +695,7 @@ int NEResourceReader::load_VS_VERSION_INFO(int id, int lang, VS_FIXEDFILEINFO *p
 	}
 
 	// Open the VS_VERSION_INFO resource.
-	unique_IRpFile<IRpFile> f_ver(this->open(RT_VERSION, id, lang));
+	unique_RefBase<IRpFile> f_ver(this->open(RT_VERSION, id, lang));
 	if (!f_ver) {
 		// Not found.
 		return -ENOENT;

src/libromdata/disc/PEResourceReader.cpp
@@ -782,7 +782,7 @@ int PEResourceReader::load_VS_VERSION_INFO(int id, int lang, VS_FIXEDFILEINFO *p
 	}
 
 	// Open the VS_VERSION_INFO resource.
-	unique_IRpFile<IRpFile> f_ver(this->open(RT_VERSION, id, lang));
+	unique_RefBase<IRpFile> f_ver(this->open(RT_VERSION, id, lang));
 	if (!f_ver) {
 		// Not found.
 		return -ENOENT;

src/libromdata/img/TCreateThumbnail.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * TCreateThumbnail.cpp: Thumbnail creator template.                       *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -176,7 +176,7 @@ ImgClass TCreateThumbnail<ImgClass>::getExternalImage(
 			continue;
 
 		// Attempt to load the image.
-		unique_IRpFile<RpFile> file(new RpFile(cache_filename, RpFile::FM_OPEN_READ));
+		unique_RefBase<RpFile> file(new RpFile(cache_filename, RpFile::FM_OPEN_READ));
 		if (file->isOpen()) {
 			unique_ptr<rp_image> dl_img(RpImageLoader::load(file.get()));
 			if (dl_img && dl_img->isValid()) {

src/libromdata/tests/img/ImageDecoderTest.cpp
@@ -272,7 +272,7 @@ void ImageDecoderTest::SetUp(void)
 	path.resize(18);	// Back to "ImageDecoder_data/".
 	path += mode.png_filename;
 	replace_slashes(path);
-	unique_IRpFile<RpFile> file(new RpFile(path, RpFile::FM_OPEN_READ));
+	unique_RefBase<RpFile> file(new RpFile(path, RpFile::FM_OPEN_READ));
 	ASSERT_TRUE(file->isOpen()) << "Error loading PNG image file: " <<
 		mode.png_filename << " - " << strerror(file->lastError());
 
@@ -391,7 +391,7 @@ void ImageDecoderTest::decodeTest_internal(void)
 	const ImageDecoderTest_mode &mode = GetParam();
 
 	// Load the PNG image.
-	unique_IRpFile<RpMemFile> f_png(new RpMemFile(m_png_buf.data(), m_png_buf.size()));
+	unique_RefBase<RpMemFile> f_png(new RpMemFile(m_png_buf.data(), m_png_buf.size()));
 	ASSERT_TRUE(f_png->isOpen()) << "Could not create RpMemFile for the PNG image.";
 	unique_ptr<rp_image> img_png(RpImageLoader::load(f_png.get()));
 	ASSERT_TRUE(img_png != nullptr) << "Could not load the PNG image as rp_image.";

src/librpbase/tests/img/RpPngFormatTest.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase/tests)                  *
  * RpPngFormatTest.cpp: RpImageLoader PNG format test.                     *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -311,7 +311,7 @@ void RpPngFormatTest::SetUp(void)
 	string path = "png_data";
 	path += DIR_SEP_CHR;
 	path += mode.png_filename;
-	unique_IRpFile<RpFile> file(new RpFile(path, RpFile::FM_OPEN_READ));
+	unique_RefBase<RpFile> file(new RpFile(path, RpFile::FM_OPEN_READ));
 	ASSERT_TRUE(file->isOpen());
 
 	// Maximum image size.
@@ -871,7 +871,7 @@ TEST_P(RpPngFormatTest, loadTest)
 	EXPECT_EQ(mode.ihdr.interlace_method,	ihdr.interlace_method);
 
 	// Create an RpMemFile.
-	unique_IRpFile<RpMemFile> png_mem_file(new RpMemFile(m_png_buf.data(), m_png_buf.size()));
+	unique_RefBase<RpMemFile> png_mem_file(new RpMemFile(m_png_buf.data(), m_png_buf.size()));
 	ASSERT_TRUE(png_mem_file->isOpen());
 
 	// Load the PNG image from memory.

src/librpfile/IRpFile.hpp
@@ -176,64 +176,6 @@ class IRpFile : public RefBase
 		int m_lastError;
 };
 
-/**
- * unique_ptr<>-style class for IRpFile.
- * Takes the implied ref() for the IRpFile,
- * and unref()'s it when it goes out of scope.
- */
-template<class T>
-class unique_IRpFile
-{
-	public:
-		inline explicit unique_IRpFile(T *file)
-			: m_file(file)
-		{
-			static_assert(std::is_base_of<IRpFile, T>::value, "unique_IRpFile<> only supports IRpFile subclasses");
-		}
-
-		inline ~unique_IRpFile()
-		{
-			UNREF(m_file);
-		}
-
-		/**
-		 * Get the IRpFile*.
-		 * @return IRpFile*
-		 */
-		inline T *get(void) { return m_file; }
-
-		/**
-		 * Release the IRpFile*.
-		 * This class will reset its pointer to nullptr,
-		 * and won't unref() the IRpFile* on destruction.
-		 * @return IRpFile*
-		 */
-		inline T *release(void)
-		{
-			T *const ptr = m_file;
-			m_file = nullptr;
-			return ptr;
-		}
-
-		/**
-		 * Dereference the IRpFile*.
-		 * @return IRpFile*
-		 */
-		inline T *operator->(void) const { return m_file; }
-
-		/**
-		 * Is the IRpFile* valid or nullptr?
-		 * @return True if valid; false if nullptr.
-		 */
-		operator bool(void) const { return (m_file != nullptr); }
-
-	private:
-		RP_DISABLE_COPY(unique_IRpFile)
-
-	private:
-		T *m_file;
-};
-
 }
 
 #endif /* __ROMPROPERTIES_LIBRPFILE_IRPFILE_HPP__ */

commit c88e066d5bfea5e8bdb82a3e60d78bc681878c15

RefBase.hpp: Fix incorrect RP_DISABLE_COPY() implementation.
Forgot to remove a #define and some backslashes.

This fixes a regression from commit 1d553f45d88298ae34850dce9fdf3300e1955576.
(Moved unique_IRpFile<> down to unique_RefBase<>.)

src/RefBase.hpp
@@ -138,8 +138,7 @@ class unique_RefBase
 		unique_RefBase(const unique_RefBase &) = delete;
 		unique_RefBase &operator=(const unique_RefBase &) = delete;
 #else
-#define RP_DISABLE_COPY(klass) \
-		unique_RefBase(const unique_RefBase &); \
+		unique_RefBase(const unique_RefBase &);
 		unique_RefBase &operator=(const unique_RefBase &);
 #endif
 