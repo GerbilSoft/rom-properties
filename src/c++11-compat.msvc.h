GerbilSoft/rom-properties

commit 6e6499dc5b9bae0a9f58fc1e0beda953104be3ae

Minimum MSVC version is now MSVC 2012 due to `enum class`.
MSVC 2010 supports explicit enum sizes, but not `enum class`.
Adding an ENUM_CLASS macro helps in some areas, but causes issues
due to the lack of `enum class` namespacing, so it's not worth it.

MSVC 2017 can still target Windows XP, so we're fine there.

I'm not sure if later MSVC can still be used with Windows 2000,
so I might remove oldwincompat at some point.

New formatting change: Using two spaces for tabs within preprocessor
macros instead of one space.

src/c++11-compat.msvc.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * c++11-compat.msvc.h: C++ 2011 compatibility header. (MSVC)              *
  *                                                                         *
- * Copyright (c) 2011-2015 by David Korth.                                 *
+ * Copyright (c) 2011-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -15,6 +15,10 @@
 /** C++ 2011 **/
 
 #ifdef __cplusplus
+
+#if (_MSC_VER < 1700)
+#  error Minimum supported MSVC version is MSVC 2012 (11.0)
+#endif
 
 /**
  * Enable compatibility for C++ 2011 features that aren't
@@ -37,32 +41,6 @@
  */
 #define CXX11_COMPAT_CONSTEXPR
 #endif
-
-#if (_MSC_VER < 1700)
-/**
- * MSVC 2010 (10.0) does support override, but not final.
- * However, it has a "sealed" keyword that works almost
- * the same way as final.
- *
- * 'sealed' is available starting with MSVC 2005 (8.0).
- * TODO: Verify MSVC 2002 and 2003.
- */
-#if (_MSC_VER >= 1400)
-#define final sealed
-#endif
-#endif
-
-#if (_MSC_VER < 1600)
-/**
- * MSVC 2008 (9.0) and older: No C++ 2011 support at all.
- * Probably won't compile at all due to lack of stdint.h.
- */
-#define CXX11_COMPAT_NULLPTR
-#define CXX11_COMPAT_OVERRIDE
-#define CXX11_COMPAT_STATIC_ASSERT
-#endif
-
-#endif /* __cplusplus */
 
 /**
  * MSVC doesn't have typeof(), but as of MSVC 2010,

src/c++11-compat.clang.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * c++11-compat.clang.h: C++ 2011 compatibility header. (clang)            *
  *                                                                         *
- * Copyright (c) 2011-2018 by David Korth.                                 *
+ * Copyright (c) 2011-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -9,7 +9,7 @@
 #define __CXX11_COMPAT_CLANG_H__
 
 #if !defined(__clang__)
-#error c++11-compat.clang.h should only be included in LLVM/clang builds.
+#  error c++11-compat.clang.h should only be included in LLVM/clang builds.
 #endif
 
 /**
@@ -39,33 +39,33 @@
 /* Older versions didn't set the correct value for __cplusplus in order to    */
 /* maintain compatibility with gcc-4.6 and earlier. */
 #if (__clang_major__ > 3 || (__clang_major__ == 3 && __clang_minor__ >= 1))
-# if __cplusplus < 201103L
-#  error Please compile with -std=c++11 or -std=gnu++11.
-# endif /* __cplusplus */
+#  if __cplusplus < 201103L
+#    error Please compile with -std=c++11 or -std=gnu++11.
+#  endif /* __cplusplus */
 #endif /* __clang_major__ */
 
 /* clang-3.1: constexpr */
 #if !__has_feature(cxx_constexpr)
-# define CXX11_COMPAT_CONSTEXPR
+#  define CXX11_COMPAT_CONSTEXPR
 #endif
 
 /* clang-3.0: nullptr, explicit virtual override */
 #if !__has_feature(cxx_nullptr)
-# define CXX11_COMPAT_NULLPTR
+#  define CXX11_COMPAT_NULLPTR
 #endif
 #if !__has_feature(cxx_override_control)
-# define CXX11_COMPAT_OVERRIDE
+#  define CXX11_COMPAT_OVERRIDE
 #endif
 
 /* clang-2.9: New character types, static assertions */
 // NOTE: Can't find a feature macro for char16_t/char32_t,
 // so using Unicode literals instead.
 #if !__has_feature(cxx_unicode_literals)
-# define CXX11_COMPAT_CHARTYPES
+#  define CXX11_COMPAT_CHARTYPES
 #endif
 
 #if !__has_feature(cxx_static_assert)
-# define CXX11_COMPAT_STATIC_ASSERT
+#  define CXX11_COMPAT_STATIC_ASSERT
 #endif
 
 #endif /* __cplusplus */

src/c++11-compat.gcc.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * c++11-compat.gcc.h: C++ 2011 compatibility header. (gcc)                *
  *                                                                         *
- * Copyright (c) 2011-2018 by David Korth.                                 *
+ * Copyright (c) 2011-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -9,7 +9,7 @@
 #define __CXX11_COMPAT_GCC_H__
 
 #if !defined(__GNUC__) || defined(__clang__) || defined(__INTEL_COMPILER)
-#error c++11-compat.gcc.h should only be included in gcc builds.
+#  error c++11-compat.gcc.h should only be included in gcc builds.
 #endif
 
 /** C++ 2011 **/
@@ -28,30 +28,30 @@
 /* For gcc-4.7+, make sure we're compiling with -std=c++11 or -std=gnu++11. */
 /* Older versions didn't set the correct value for __cplusplus. */
 #if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7))
-#if __cplusplus < 201103L
-#error Please compile with -std=c++11 or -std=gnu++11.
-#endif /* __cplusplus */
+#  if __cplusplus < 201103L
+#    error Please compile with -std=c++11 or -std=gnu++11.
+#  endif /* __cplusplus */
 #endif /* __GNUC__ */
 
 /* gcc-4.7: Explicit virtual override */
 #if (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 7))
-#define CXX11_COMPAT_OVERRIDE
+#  define CXX11_COMPAT_OVERRIDE
 #endif
 
 /* gcc-4.6: nullptr, constexpr */
 #if (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 6))
-#define CXX11_COMPAT_NULLPTR
-#define CXX11_COMPAT_CONSTEXPR
+#  define CXX11_COMPAT_NULLPTR
+#  define CXX11_COMPAT_CONSTEXPR
 #endif
 
 /* gcc-4.4: New character types */
 #if (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 4))
-#define CXX11_COMPAT_CHARTYPES
+#  define CXX11_COMPAT_CHARTYPES
 #endif
 
 /* gcc-4.3: Static assertions (first version to support C++11) */
 #if (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 3))
-#define CXX11_COMPAT_STATIC_ASSERT
+#  define CXX11_COMPAT_STATIC_ASSERT
 #endif
 
 #endif /* __cplusplus */

src/c++11-compat.h
@@ -11,7 +11,7 @@
 // config.libc.h must be included externally for
 // off_t/off64_t checks.
 #ifndef __ROMPROPERTIES_CONFIG_LIBC_H__
-# error config.libc.h was not included.
+#  error config.libc.h was not included.
 #endif /* __ROMPROPERTIES_CONFIG_LIBC_H__ */
 
 #ifndef __cplusplus
@@ -28,39 +28,39 @@
 
 // static_assert() is present in C11.
 #if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 201112L
-#define CXX11_COMPAT_STATIC_ASSERT
+#  define CXX11_COMPAT_STATIC_ASSERT
 #endif
 #endif /* !__cplusplus */
 
 /** Compiler-specific headers. **/
 
 #if defined(__clang__)
-#include "c++11-compat.clang.h"
+#  include "c++11-compat.clang.h"
 #elif defined(__GNUC__)
-#include "c++11-compat.gcc.h"
+#  include "c++11-compat.gcc.h"
 #elif defined(_MSC_VER)
-#include "c++11-compat.msvc.h"
+#  include "c++11-compat.msvc.h"
 #else
-#error Unknown compiler, please update c++11-compat.h.
+#  error Unknown compiler, please update c++11-compat.h.
 #endif
 
 /* nullptr: Represents a NULL pointer. NULL == 0 */
 #ifdef CXX11_COMPAT_NULLPTR
-# if defined __GNUG__ && \
-    (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 8))
-#  define nullptr (__null)
-# else
-#  if !defined(__cplusplus)
-#   define nullptr ((void*)0)
+#  if defined __GNUG__ && \
+       (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 8))
+#    define nullptr (__null)
 #  else
-#   define nullptr (0)
+#    if !defined(__cplusplus)
+#      define nullptr ((void*)0)
+#    else
+#      define nullptr (0)
+#    endif
 #  endif
-# endif
 #endif
 
 /* static_assert(): Compile-time assertions. */
 #ifdef CXX11_COMPAT_STATIC_ASSERT
-#define static_assert(expr, msg) switch (0) { case 0: case (expr): ; }
+#  define static_assert(expr, msg) switch (0) { case 0: case (expr): ; }
 #endif
 
 /* Unicode characters and strings. */
@@ -81,16 +81,16 @@ namespace std {
 /* constexpr */
 /* NOTE: This might not work in all cases. */
 #ifdef CXX11_COMPAT_CONSTEXPR
-#define constexpr const
+#  define constexpr const
 #endif
 
 /* Explicit override/final. */
 #ifdef CXX11_COMPAT_OVERRIDE
-#define override
+#  define override
 /* final may be defined as 'sealed' on older MSVC */
-#ifndef final
-#define final
-#endif
+#  ifndef final
+#    define final
+#  endif
 #endif
 
 /** Other compatibility stuff. **/
@@ -100,28 +100,28 @@ namespace std {
  * Reference: http://gcc.gnu.org/onlinedocs/gcc/Function-Names.html
  */
 #if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
-# if (defined(__GNUC__) && __GNUC__ >= 2) || defined(_MSC_VER)
-#  define __func__ __FUNCTION__
-# else
-#  define __func__ "<unknown>"
-# endif
+#  if (defined(__GNUC__) && __GNUC__ >= 2) || defined(_MSC_VER)
+#    define __func__ __FUNCTION__
+#  else
+#    define __func__ "<unknown>"
+#  endif
 #endif
 
 #if !defined(SIZEOF_OFF64_T)
 // off64_t isn't available.
-# if defined(SIZEOF_OFF_T) && SIZEOF_OFF_T >= 8
+#  if defined(SIZEOF_OFF_T) && SIZEOF_OFF_T >= 8
 // off_t is at least 64-bit. Use it as off64_t.
-#  include <unistd.h>
+#    include <unistd.h>
 typedef off_t off64_t;
-# else /* !(defined(SIZEOF_OFF_T) && SIZEOF_OFF_T >= 8) */
+#  else /* !(defined(SIZEOF_OFF_T) && SIZEOF_OFF_T >= 8) */
 // off_t either doesn't exist or is not at least 64-bit.
-#  ifdef _MSC_VER
+#    ifdef _MSC_VER
 typedef __int64 off64_t;
-#  else /* !_MSC_VER */
-#   include <stdint.h>
+#    else /* !_MSC_VER */
+#      include <stdint.h>
 typedef int64_t off64_t;
-#  endif /* _MSC_VER */
-# endif /* defined(SIZEOF_OFF_T) && SIZEOF_OFF_T >= 8 */
+#    endif /* _MSC_VER */
+#  endif /* defined(SIZEOF_OFF_T) && SIZEOF_OFF_T >= 8 */
 #endif /* !defined(SIZEOF_OFF64_t) */
 
 /**
@@ -129,7 +129,7 @@ typedef int64_t off64_t;
  * Note that MinGW-w64 uses MSVCRT.
  */
 #ifdef _WIN32
-#include "c99-compat.msvcrt.h"
+#  include "c99-compat.msvcrt.h"
 #endif
 
 #endif /* __CXX11_COMPAT_H__ */

src/c99-compat.msvcrt.h
@@ -9,7 +9,7 @@
 #define __C99_COMPAT_MSVCRT_H__
 
 #ifndef _WIN32
-# error c99-compat.msvcrt.h should only be included in Win32 builds.
+#  error c99-compat.msvcrt.h should only be included in Win32 builds.
 #endif
 
 /**
@@ -22,13 +22,13 @@
 
 /** snprintf() **/
 #ifdef _MSC_VER
-# if _MSC_VER < 1400
+#  if _MSC_VER < 1400
 /* MSVC 2003 and older. Don't use variadic macros. */
-#  define snprintf  _snprintf
-# elif _MSC_VER < 1900
+#    define snprintf  _snprintf
+#  elif _MSC_VER < 1900
 /* MSVC 2005 through MSVC 2013. Use variadic macros. */
-#  define snprintf(str, size, format, ...)  _snprintf(str, size, format, __VA_ARGS__)
-# endif
+#    define snprintf(str, size, format, ...)  _snprintf(str, size, format, __VA_ARGS__)
+#  endif
 #endif /* _MSC_VER */
 
 /**
@@ -37,9 +37,9 @@
  * but they do support '__inline' in both C and C++.
  */
 #if defined(_MSC_VER) && _MSC_VER < 1900
-# if !defined(__cplusplus)
-#  define inline __inline
-# endif /* !defined(__cplusplus) */
+#  if !defined(__cplusplus)
+#    define inline __inline
+#  endif /* !defined(__cplusplus) */
 #endif /* defined(_MSC_VER) && _MSC_VER < 1900 */
 
 /** fseeko(), ftello() **/
@@ -54,8 +54,8 @@
 // (NOTE 2: MinGW-w64 redirects to _fseeki64() and _ftelli64() when
 //  building with MSVCRT 14.0 [2015] or later.)
 #ifdef _MSC_VER
-# define fseeko(stream, offset, origin) _fseeki64(stream, offset, origin)
-# define ftello(stream) _ftelli64(stream)
+#  define fseeko(stream, offset, origin) _fseeki64(stream, offset, origin)
+#  define ftello(stream) _ftelli64(stream)
 #endif /* _MSC_VER */
 
 /** C99 **/
@@ -77,10 +77,10 @@
  * the equivalent functions _strtoi64() and _strtoui64().
  */
 #if defined(_MSC_VER) && _MSC_VER < 1800
-# define strtoll(nptr, endptr, base)  _strtoi64(nptr, endptr, base)
-# define strtoull(nptr, endptr, base) _strtoui64(nptr, endptr, base)
-# define wcstoll(nptr, endptr, base)  _wcstoi64(nptr, endptr, base)
-# define wcstoull(nptr, endptr, base) _wcstoui64(nptr, endptr, base)
+#  define strtoll(nptr, endptr, base)  _strtoi64(nptr, endptr, base)
+#  define strtoull(nptr, endptr, base) _strtoui64(nptr, endptr, base)
+#  define wcstoll(nptr, endptr, base)  _wcstoi64(nptr, endptr, base)
+#  define wcstoull(nptr, endptr, base) _wcstoui64(nptr, endptr, base)
 #endif /* defined(_MSC_VER) && _MSC_VER < 1800 */
 
 /** strcasecmp() and related **/
@@ -90,25 +90,25 @@
  * MinGW-w64 has the ANSI versions, but not the Unicode versions.
  */
 #ifdef _MSC_VER
-# ifndef strcasecmp
-#  define strcasecmp(s1, s2)     _stricmp(s1, s2)
-# endif
-# ifndef strncasecmp
-#  define strncasecmp(s1, s2, n) _strnicmp(s1, s2, n)
-# endif
+#  ifndef strcasecmp
+#    define strcasecmp(s1, s2)     _stricmp(s1, s2)
+#  endif
+#  ifndef strncasecmp
+#    define strncasecmp(s1, s2, n) _strnicmp(s1, s2, n)
+#  endif
 #endif /* _MSC_VER */
 #ifndef wcscasecmp
-# define wcscasecmp(s1, s2)     _wcsicmp(s1, s2)
+#  define wcscasecmp(s1, s2)     _wcsicmp(s1, s2)
 #endif
 #ifndef wcsncasecmp
-# define wcsncasecmp(s1, s2, n) _wcsnicmp(s1, s2, n)
+#  define wcsncasecmp(s1, s2, n) _wcsnicmp(s1, s2, n)
 #endif
 
 /** strtok_r() **/
 
 // MSVC has strtok_s(), which is basically the same as strtok_r().
 #ifdef _MSC_VER
-# define strtok_r(str, delim, saveptr) strtok_s(str, delim, saveptr)
+#  define strtok_r(str, delim, saveptr) strtok_s(str, delim, saveptr)
 #endif
 
 #endif /* __C99_COMPAT_MSVCRT_H__ */

commit b82d758d714be2e0e98a04b39b668ea0844b36d4

c++11-compat.msvc.h: Removed one too many lines here.
This fixes a regression from commit 6e6499dc5b9bae0a9f58fc1e0beda953104be3ae.
(Minimum MSVC version is now MSVC 2012 due to `enum class`.)

src/c++11-compat.msvc.h
@@ -42,6 +42,8 @@
 #define CXX11_COMPAT_CONSTEXPR
 #endif
 
+#endif /* __cplusplus */
+
 /**
  * MSVC doesn't have typeof(), but as of MSVC 2010,
  * it has decltype(), which is essentially the same thing.