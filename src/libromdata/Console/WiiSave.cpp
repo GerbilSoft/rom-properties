GerbilSoft/rom-properties

commit 66e466b9258a3b041dc2fe8e28dac656d73522c2

[libromdata] WiiSave: Show permissions as if they're Unix-style permissions, and show the "No Copy from NAND" flag.
WiiWIBN: Added an accessor to get the "No Copy from NAND" flag.

src/libromdata/Console/WiiSave.cpp
@@ -473,7 +473,7 @@ int WiiSave::loadFieldData(void)
 	// Wii save and backup headers.
 	const Wii_SaveGame_Header_t *const svHeader = &d->svHeader;
 	const Wii_Bk_Header_t *const bkHeader = &d->bkHeader;
-	d->fields->reserve(4);	// Maximum of 4 fields.
+	d->fields->reserve(5);	// Maximum of 5 fields.
 
 	// Check if the headers are valid.
 	// TODO: Do this in the constructor instead?
@@ -506,11 +506,35 @@ int WiiSave::loadFieldData(void)
 		}
 	}
 
-	// Permissions. (TODO)
+	// Permissions.
 	if (isSvValid) {
-		d->fields->addField_string_numeric(C_("WiiSave", "Permissions"),
-			svHeader->permissions,
-			RomFields::Base::Hex, 2, RomFields::STRF_MONOSPACE);
+		// Unix-style permissions field.
+		char s_perms[] = "----------";
+		uint8_t perms = svHeader->permissions;
+		for (char *p = &s_perms[1]; p < s_perms + sizeof(s_perms); p += 3, perms <<= 2) {
+			if (perms & 0x20) {
+				p[0] = 'r';
+			}
+			if (perms & 0x10) {
+				p[1] = 'w';
+			}
+		}
+
+		d->fields->addField_string(C_("WiiSave", "Permissions"), s_perms,
+			RomFields::STRF_MONOSPACE);
+	}
+
+	// NoCopy? (separate from permissions)
+	if (d->wibnData) {
+		// Flags bitfield.
+		static const char *const flags_names[] = {
+			NOP_C_("WiiSave|Flags", "No Copy from NAND"),
+		};
+		vector<string> *const v_flags_names = RomFields::strArrayToVector_i18n(
+			"WiiSave|Flags", flags_names, ARRAY_SIZE(flags_names));
+		const uint32_t flags = (d->wibnData->isNoCopyFlagSet() ? 1 : 0);
+		d->fields->addField_bitfield(C_("WiiSave", "Flags"),
+			v_flags_names, 3, flags);
 	}
 
 	// MAC address.

src/libromdata/Console/WiiWIBN.cpp
@@ -603,4 +603,14 @@ const IconAnimData *WiiWIBN::iconAnimData(void) const
 	return d->iconAnimData;
 }
 
+/**
+ * Is the NoCopy flag set?
+ * @return True if set; false if not.
+ */
+bool WiiWIBN::isNoCopyFlagSet(void) const
+{
+	RP_D(const WiiWIBN);
+	return !!(d->wibnHeader.flags & cpu_to_be32(BANNER_WIBN_FLAGS_NOCOPY));
+}
+
 }

src/libromdata/Console/WiiWIBN.hpp
@@ -18,6 +18,14 @@ ROMDATA_DECL_IMGSUPPORT()
 ROMDATA_DECL_IMGPF()
 ROMDATA_DECL_IMGINT()
 ROMDATA_DECL_ICONANIM()
+
+	public:
+		/**
+		 * Is the NoCopy flag set?
+		 * @return True if set; false if not.
+		 */
+		bool isNoCopyFlagSet(void) const;
+
 ROMDATA_DECL_END()
 
 }

src/libromdata/Console/wii_banner.h
@@ -131,12 +131,29 @@ typedef enum {
 typedef struct _Wii_SaveGame_Header_t {
 	RVL_TitleID_t savegame_id;	// [0x000] Savegame ID (title ID)
 	uint32_t banner_size;		// [0x008] Size of banner+icons, with header. (max 0xF0A0)
-	uint8_t permissions;		// [0x00C] Permissions (???)
+	uint8_t permissions;		// [0x00C] Permissions (See Wii_SaveGame_Perm_e.)
 	uint8_t unknown1;		// [0x00D]
 	uint8_t md5_header[16];		// [0x00E] MD5 of plaintext header, with md5 blanker applied
 	uint8_t unknown2[2];		// [0x01E]
 } Wii_SaveGame_Header_t;
 ASSERT_STRUCT(Wii_SaveGame_Header_t, 32);
+
+/**
+ * Wii save game permissions.
+ * Similar to Unix permissions, except there's no Execute bit.
+ */
+typedef enum {
+	Wii_SaveGame_Perm_User_Read	= 0x20,
+	Wii_SaveGame_Perm_User_Write	= 0x10,
+	Wii_SaveGame_Perm_Group_Read	= 0x08,
+	Wii_SaveGame_Perm_Group_Write	= 0x04,
+	Wii_SaveGame_Perm_Other_Read	= 0x02,
+	Wii_SaveGame_Perm_Other_Write	= 0x01,
+
+	Wii_SaveGame_Perm_Mask_User	= 0x30,
+	Wii_SaveGame_Perm_Mask_Group	= 0x0C,
+	Wii_SaveGame_Perm_Mask_Other	= 0x03,
+} Wii_SaveGame_Perm_e;
 
 /**
  * Wii save game Bk (backup) header.

commit c082eefb477c26fd15995609b5d1f24b3d3b7e13

[libromdata] WiiSave: Wrap the no-copy flag check in #ifdef ENABLE_DECRYPTION.
This fixes a regression from commit 66e466b9258a3b041dc2fe8e28dac656d73522c2.
([libromdata] WiiSave: Show permissions as if they're Unix-style permissions, and show the "No Copy from NAND" flag.)

Reported by @DankRank.

src/libromdata/Console/WiiSave.cpp
@@ -524,6 +524,7 @@ int WiiSave::loadFieldData(void)
 			RomFields::STRF_MONOSPACE);
 	}
 
+#ifdef ENABLE_DECRYPTION
 	// NoCopy? (separate from permissions)
 	if (d->wibnData) {
 		// Flags bitfield.
@@ -536,6 +537,7 @@ int WiiSave::loadFieldData(void)
 		d->fields->addField_bitfield(C_("WiiSave", "Flags"),
 			v_flags_names, 3, flags);
 	}
+#endif /* ENABLE_DECRYPTION */
 
 	// MAC address.
 	if (isBkValid) {