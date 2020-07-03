GerbilSoft/rom-properties

commit 242bf61e72c6d3e4652d7413a1f93959705e9593

Use new/delete in some places instead of malloc()/free().
This eliminates a few things:
- Don't need to check the return value.
- Don't need to manually multiply by sizeof().

There might be slightly more overhead, but nothing noticeable,
since these are primitive types.

src/libromdata/disc/GcnPartitionPrivate.cpp
@@ -153,12 +153,7 @@ int GcnPartitionPrivate::loadFst(void)
 	// Read the FST.
 	// TODO: Eliminate the extra copy?
 	uint32_t fstData_len = bootBlock.fst_size << offsetShift;
-	uint8_t *fstData = static_cast<uint8_t*>(malloc(fstData_len));
-	if (!fstData) {
-		// malloc() failed.
-		q->m_lastError = ENOMEM;
-		return -ENOMEM;
-	}
+	uint8_t *const fstData = new uint8_t[fstData_len];
 	size_t size = q->read(fstData, fstData_len);
 	if (size != fstData_len) {
 		// Short read.

src/libcachecommon/CacheKeys.cpp
@@ -356,14 +356,10 @@ static inline wstring U82W(const string &mbs)
 		return ws_ret;
 	}
 
-	wchar_t *wcs = static_cast<wchar_t*>(malloc(cchWcs * sizeof(wchar_t)));
-	assert(wcs != nullptr);
-	if (!wcs) {
-		return ws_ret;
-	}
+	wchar_t *wcs = new wchar_t[cchWcs];
 	MultiByteToWideChar(CP_UTF8, 0, mbs.c_str(), static_cast<int>(mbs.size()), wcs, cchWcs);
 	ws_ret.assign(wcs, cchWcs);
-	free(wcs);
+	delete[] wcs;
 	return ws_ret;
 }
 

src/libromdata/disc/GcnFst.cpp
@@ -125,12 +125,7 @@ GcnFstPrivate::GcnFstPrivate(const uint8_t *fstData, uint32_t len, uint8_t offse
 
 	// Copy the FST data.
 	// NOTE: +1 for NULL termination.
-	uint8_t *fst8 = static_cast<uint8_t*>(malloc(fstData_sz + 1));
-	if (!fst8) {
-		// Could not allocate memory for the FST.
-		hasErrors = true;
-		return;
-	}
+	uint8_t *const fst8 = new uint8_t[fstData_sz + 1];
 	memcpy(fst8, fstData, fstData_sz);
 	fst8[fstData_sz] = 0; // Make sure the string table is NULL-terminated.
 	this->fstData = reinterpret_cast<GCN_FST_Entry*>(fst8);
@@ -149,7 +144,7 @@ GcnFstPrivate::GcnFstPrivate(const uint8_t *fstData, uint32_t len, uint8_t offse
 GcnFstPrivate::~GcnFstPrivate()
 {
 	assert(fstDirCount == 0);
-	free(fstData);
+	delete[] fstData;
 }
 
 /**

src/libromdata/tests/disc/GcnFstPrint.cpp
@@ -98,13 +98,7 @@ int RP_C_API main(int argc, char *argv[])
 	fseeko(f, 0, SEEK_SET);
 
 	// Read the FST into memory.
-	uint8_t *fstData = static_cast<uint8_t*>(malloc(filesize));
-	if (!fstData) {
-		printf(C_("GcnFstPrint", "ERROR: malloc(%u) failed."), static_cast<unsigned int>(filesize));
-		putchar('\n');
-		fclose(f);
-		return EXIT_FAILURE;
-	}
+	uint8_t *const fstData = new uint8_t[filesize];
 	size_t rd_size = fread(fstData, 1, filesize, f);
 	fclose(f);
 	if (rd_size != static_cast<size_t>(filesize)) {

src/librpbase/TextFuncs.cpp
@@ -105,7 +105,7 @@ size_t u16_strnlen(const char16_t *wcs, size_t maxlen)
 char16_t *u16_strdup(const char16_t *wcs)
 {
 	size_t len = u16_strlen(wcs)+1;	// includes terminator
-	char16_t *ret = (char16_t*)malloc(len*sizeof(*wcs));
+	char16_t *const ret = static_cast<char16_t*>(malloc(len * sizeof(*wcs)));
 	memcpy(ret, wcs, len*sizeof(*wcs));
 	return ret;
 }

src/librpbase/TextFuncs_win32.cpp
@@ -49,7 +49,7 @@ static char16_t *W32U_mbs_to_UTF16(const char *mbs, unsigned int codepage, DWORD
 	if (cchWcs <= 0)
 		return nullptr;
 
-	wchar_t *wcs = static_cast<wchar_t*>(malloc(cchWcs * sizeof(wchar_t)));
+	wchar_t *const wcs = static_cast<wchar_t*>(malloc(cchWcs * sizeof(wchar_t)));
 	MultiByteToWideChar(codepage, dwFlags, mbs, -1, wcs, cchWcs);
 	return reinterpret_cast<char16_t*>(wcs);
 }
@@ -71,7 +71,7 @@ static char16_t *W32U_mbs_to_UTF16(const char *mbs, int cbMbs,
 	if (cchWcs <= 0)
 		return nullptr;
 
-	wchar_t *wcs = static_cast<wchar_t*>(malloc(cchWcs * sizeof(wchar_t)));
+	wchar_t *const wcs = static_cast<wchar_t*>(malloc(cchWcs * sizeof(wchar_t)));
 	MultiByteToWideChar(codepage, dwFlags, mbs, cbMbs, wcs, cchWcs);
 
 	if (cchWcs_ret)
@@ -91,7 +91,7 @@ static char *W32U_UTF16_to_mbs(const char16_t *wcs, unsigned int codepage)
 	if (cbMbs <= 0)
 		return nullptr;
  
-	char *mbs = static_cast<char*>(malloc(cbMbs));
+	char *const mbs = static_cast<char*>(malloc(cbMbs));
 	WideCharToMultiByte(codepage, 0, reinterpret_cast<const wchar_t*>(wcs), -1, mbs, cbMbs, nullptr, nullptr);
 	return mbs;
 }
@@ -112,7 +112,7 @@ static char *W32U_UTF16_to_mbs(const char16_t *wcs, int cchWcs,
 	if (cbMbs <= 0)
 		return nullptr;
 
-	char *mbs = static_cast<char*>(malloc(cbMbs));
+	char *const mbs = static_cast<char*>(malloc(cbMbs));
 	WideCharToMultiByte(codepage, 0, reinterpret_cast<const wchar_t*>(wcs), cchWcs, mbs, cbMbs, nullptr, nullptr);
 
 	if (cbMbs_ret)

src/libwin32common/MiniU82T.cpp
@@ -35,14 +35,10 @@ string T2U8_c(const TCHAR *wcs)
 	}
 	cbMbs--;
  
-	char *mbs = static_cast<char*>(malloc(cbMbs * sizeof(char)));
-	assert(mbs != nullptr);
-	if (!mbs) {
-		return s_ret;
-	}
+	char *const mbs = new char[cbMbs];
 	WideCharToMultiByte(CP_UTF8, 0, wcs, -1, mbs, cbMbs, nullptr, nullptr);
 	s_ret.assign(mbs, cbMbs);
-	free(mbs);
+	delete[] mbs;
 	return s_ret;
 }
 #endif /* UNICODE */
@@ -63,14 +59,10 @@ wstring U82W_c(const char *mbs)
 	}
 	cchWcs--;
  
-	wchar_t *wcs = static_cast<wchar_t*>(malloc(cchWcs * sizeof(wchar_t)));
-	assert(wcs != nullptr);
-	if (!wcs) {
-		return ts_ret;
-	}
+	wchar_t *const wcs = new wchar_t[cchWcs];
 	MultiByteToWideChar(CP_UTF8, 0, mbs, -1, wcs, cchWcs);
 	ts_ret.assign(wcs, cchWcs);
-	free(wcs);
+	delete[] wcs;
 	return ts_ret;
 }
 

src/libwin32common/userdirs.cpp
@@ -29,7 +29,7 @@ namespace LibWin32Common {
  * @param wcs TCHAR string.
  * @return UTF-8 C++ string.
  */
-static inline string W2U8(const WCHAR *wcs)
+static inline string W2U8(const wchar_t *wcs)
 {
 	string s_ret;
 
@@ -40,14 +40,10 @@ static inline string W2U8(const WCHAR *wcs)
 	}
 	cbMbs--;
  
-	char *mbs = static_cast<char*>(malloc(cbMbs));
-	assert(mbs != nullptr);
-	if (!mbs) {
-		return s_ret;
-	}
+	char *const mbs = new char[cbMbs];
 	WideCharToMultiByte(CP_UTF8, 0, wcs, -1, mbs, cbMbs, nullptr, nullptr);
 	s_ret.assign(mbs, cbMbs);
-	free(mbs);
+	delete[] mbs;
 	return s_ret;
 }
 #ifdef UNICODE

commit d45054dcc0a756a74934cefd8fea8aac19a1d38a

[libromdata] GcnPartitionPrivate: Need to delete[] fstData, not free().
Found using PVS-Studio v7.08.
V611 (High): The memory was allocated using 'new' operator but was
released using the 'free' function. Consider inspecting operation
logics behind the 'fstData' variable.

This fixes a regression from commit 242bf61e72c6d3e4652d7413a1f93959705e9593.
(Use new/delete in some places instead of malloc()/free().)

src/libromdata/disc/GcnPartitionPrivate.cpp
@@ -157,14 +157,14 @@ int GcnPartitionPrivate::loadFst(void)
 	size_t size = q->read(fstData, fstData_len);
 	if (size != fstData_len) {
 		// Short read.
-		free(fstData);
+		delete[] fstData;
 		q->m_lastError = EIO;
 		return -EIO;
 	}
 
 	// Create the GcnFst.
 	GcnFst *const gcnFst = new GcnFst(fstData, fstData_len, offsetShift);
-	free(fstData);	// TODO: Eliminate the extra copy?
+	delete[] fstData;	// TODO: Eliminate the extra copy?
 	if (gcnFst->hasErrors()) {
 		// FST has errors.
 		delete gcnFst;