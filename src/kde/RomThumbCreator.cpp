GerbilSoft/rom-properties

commit eef656926e46b1aac81d7963bbe6f605fbafbd8c

[kde] RpQt: New functions localizeQUrl() and openQUrl().
This consolidates all of the URL logic from the various other KDE
classes into two functions.

openQUrl() calls localizeQUrl(), so callers don't need to do this
explicitly. As far as I can tell, the only KDE function that explicitly
needs localizeQUrl() is rp_create_thumbnail(). Everything else works
just fine with openQUrl().

Code size reduction: (64-bit Gentoo Linux, gcc-9.2.0-r3, release build, no LTO)

   text    data     bss     dec     hex filename
1472511  122744   16936 1612191  18999f rom-properties-kf5.so [before]
1468403  122736   16936 1608075  18898b rom-properties-kf5.so [after]
  -4108      -8       0   -4116   -1014 Difference

src/kde/RomThumbCreator.cpp
@@ -55,11 +55,9 @@ using std::unique_ptr;
 
 #if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
 # include <QtCore/QMimeDatabase>
-# include <QtCore/QStandardPaths>
-#else
-# include <QtGui/QDesktopServices>
+#else /* QT_VERSION < QT_VERSION_CHECK(5,0,0) */
 # include <kmimetype.h>
-#endif
+#endif /* QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
 
 // KDE protocol manager.
 // Used to find the KDE proxy settings.
@@ -72,7 +70,7 @@ using std::unique_ptr;
 # define QT_MAJOR_STR "5"
 #elif QT_VERSION >= QT_VERSION_CHECK(4,0,0)
 # define QT_MAJOR_STR "4"
-#else
+#else /* QT_VERSION < QT_VERSION_CHECK(4,0,0) */
 # error Qt is too old.
 #endif
 
@@ -269,64 +267,10 @@ bool RomThumbCreator::create(const QString &path, int width, int height, QImage 
 {
 	Q_UNUSED(height);
 
-	// Check if the source filename is a URI.
-	QUrl url(path);
-	QFileInfo fi_src;
-	QString qs_source_filename;
-	if (url.scheme().isEmpty()) {
-		// No scheme. This is a plain old filename.
-		fi_src = QFileInfo(path);
-		qs_source_filename = fi_src.absoluteFilePath();
-		url = QUrl::fromLocalFile(qs_source_filename);
-	} else if (url.isLocalFile()) {
-		// "file://" scheme. This is a local file.
-		qs_source_filename = url.toLocalFile();
-		fi_src = QFileInfo(qs_source_filename);
-		url = QUrl::fromLocalFile(fi_src.absoluteFilePath());
-	} else if (url.scheme() == QLatin1String("desktop")) {
-		// Desktop folder.
-		// KFileItem::localPath() isn't working for "desktop:/" here,
-		// so handle it manually.
-		// TODO: Also handle "trash:/"?
-#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-		qs_source_filename = QStandardPaths::locate(QStandardPaths::DesktopLocation, url.path());
-#else /* QT_VERSION < QT_VERSION_CHECK(5,0,0) */
-		// TODO: Is the extra slash necessary?
-		qs_source_filename = QDesktopServices::storageLocation(QDesktopServices::DesktopLocation);
-		qs_source_filename += QChar(L'/');
-		qs_source_filename += url.path();
-#endif /* QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
-		fi_src = QFileInfo(qs_source_filename);
-		url = QUrl::fromLocalFile(fi_src.absoluteFilePath());
-	} else {
-		// Has a scheme that isn't "file://".
-		// This is probably a remote file.
-	}
-
-	if (!qs_source_filename.isEmpty()) {
-		// Check for "bad" file systems.
-		const Config *const config = Config::instance();
-		if (FileSystem::isOnBadFS(qs_source_filename.toUtf8().constData(), config->enableThumbnailOnNetworkFS())) {
-			// This file is on a "bad" file system.
-			return false;
-		}
-	}
-
 	// Attempt to open the ROM file.
-	IRpFile *file = nullptr;
-	if (!qs_source_filename.isEmpty()) {
-		// Local file. Use RpFile.
-		file = new RpFile(
-			QDir::toNativeSeparators(qs_source_filename).toUtf8().constData(),
-			RpFile::FM_OPEN_READ_GZ);
-	} else {
-#ifdef HAVE_RPFILE_KIO
-		// Not a local file. Use RpFileKio.
-		file = new RpFileKio(url);
-#else /* !HAVE_RPFILE_KIO */
-		// RpFileKio is not available.
+	IRpFile *const file = openQUrl(QUrl(path), true);
+	if (!file) {
 		return false;
-#endif /* HAVE_RPFILE_KIO */
 	}
 
 	// Assuming width and height are the same.
@@ -372,80 +316,17 @@ Q_DECL_EXPORT int rp_create_thumbnail(const char *source_file, const char *outpu
 	// TODO: Static initializer somewhere?
 	rp_image::setBackendCreatorFn(RpQImageBackend::creator_fn);
 
-	// Check if the source filename is a URI.
-	QUrl url(QString::fromUtf8(source_file));
-	QFileInfo fi_src;
-	QString qs_source_filename;
-	if (url.scheme().isEmpty()) {
-		// No scheme. This is a plain old filename.
-		fi_src = QFileInfo(QString::fromUtf8(source_file));
-		qs_source_filename = fi_src.absoluteFilePath();
-		url = QUrl::fromLocalFile(qs_source_filename);
-	} else if (url.isLocalFile()) {
-		// "file://" scheme. This is a local file.
-		qs_source_filename = url.toLocalFile();
-		fi_src = QFileInfo(qs_source_filename);
-		url = QUrl::fromLocalFile(fi_src.absoluteFilePath());
-	} else if (url.scheme() == QLatin1String("desktop")) {
-		// Desktop folder.
-		// KFileItem::localPath() isn't working for "desktop:/" here,
-		// so handle it manually.
-		// TODO: Also handle "trash:/"?
-#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-		qs_source_filename = QStandardPaths::locate(QStandardPaths::DesktopLocation, url.path());
-#else /* QT_VERSION < QT_VERSION_CHECK(5,0,0) */
-		// TODO: Is the extra slash necessary?
-		qs_source_filename = QDesktopServices::storageLocation(QDesktopServices::DesktopLocation);
-		qs_source_filename += QChar(L'/');
-		qs_source_filename += url.path();
-#endif /* QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
-		fi_src = QFileInfo(qs_source_filename);
-		url = QUrl::fromLocalFile(fi_src.absoluteFilePath());
-	} else {
-		// Has a scheme that isn't "file://".
-		// This is probably a remote file.
-	}
-
-	if (!url.isValid() || url.isEmpty()) {
-		// Empty URL...
-		return RPCT_SOURCE_FILE_ERROR;
-	}
-
-	// Check for "bad" file systems.
-	if (!qs_source_filename.isEmpty()) {
-		const Config *const config = Config::instance();
-		if (FileSystem::isOnBadFS(source_file, config->enableThumbnailOnNetworkFS())) {
-			// This file is on a "bad" file system.
-			return RPCT_SOURCE_FILE_BAD_FS;
-		}
-	}
-
 	// Attempt to open the ROM file.
-	IRpFile *file = nullptr;
-	if (!qs_source_filename.isEmpty()) {
-		// Local file. Use RpFile.
-		file = new RpFile(
-			QDir::toNativeSeparators(qs_source_filename).toUtf8().constData(),
-			RpFile::FM_OPEN_READ_GZ);
-	} else {
-#ifdef HAVE_RPFILE_KIO
-		// Not a local file. Use RpFileKio.
-		file = new RpFileKio(url);
-#else /* !HAVE_RPFILE_KIO */
-		// RpFileKio is not available.
-		return RPCT_SOURCE_FILE_ERROR;
-#endif /* HAVE_RPFILE_KIO */
-	}
-
-	if (!file->isOpen()) {
+	QUrl localUrl = localizeQUrl(QUrl(QString::fromUtf8(source_file)));
+	IRpFile *const file = openQUrl(localUrl, true);
+	if (!file) {
 		// Could not open the file.
-		file->unref();
 		return RPCT_SOURCE_FILE_ERROR;
 	}
 
 	// Get the appropriate RomData class for this ROM.
 	// RomData class *must* support at least one image type.
-	RomData *romData = RomDataFactory::create(file, RomDataFactory::RDA_HAS_THUMBNAIL);
+	RomData *const romData = RomDataFactory::create(file, RomDataFactory::RDA_HAS_THUMBNAIL);
 	file->unref();	// file is ref()'d by RomData.
 	if (!romData) {
 		// ROM is not supported.
@@ -509,10 +390,20 @@ Q_DECL_EXPORT int rp_create_thumbnail(const char *source_file, const char *outpu
 	static const char sw[] = "ROM Properties Page shell extension (" RP_KDE_UPPER QT_MAJOR_STR ")";
 	kv.emplace_back("Software", sw);
 
+	// Local filename.
+	QString qs_source_filename;
+	if (localUrl.scheme().isEmpty() || localUrl.isLocalFile()) {
+		qs_source_filename = localUrl.toLocalFile();
+	}
+	QFileInfo fi_src(qs_source_filename);
+
 	// Modification time.
-	int64_t mtime = fi_src.lastModified().toMSecsSinceEpoch() / 1000;
-	if (mtime > 0) {
-		kv.emplace_back("Thumb::Size", rp_sprintf("%" PRId64, mtime));
+	// FIXME: Local files only. Figure out how to handle this for remote.
+	if (!qs_source_filename.isEmpty()) {
+		int64_t mtime = fi_src.lastModified().toMSecsSinceEpoch() / 1000;
+		if (mtime > 0) {
+			kv.emplace_back("Thumb::Size", rp_sprintf("%" PRId64, mtime));
+		}
 	}
 
 	// MIME type.
@@ -521,10 +412,11 @@ Q_DECL_EXPORT int rp_create_thumbnail(const char *source_file, const char *outpu
 	// Use QMimeDatabase for Qt5.
 	// TODO: Verify if KIO works.
 	QMimeDatabase mimeDatabase;
-	QMimeType mimeType = mimeDatabase.mimeTypeForUrl(url);
+	QMimeType mimeType = mimeDatabase.mimeTypeForUrl(localUrl);
 	kv.emplace_back("Thumb::Mimetype", mimeType.name().toUtf8().constData());
 #else /* QT_VERSION < QT_VERSION_CHECK(5,0,0) */
 	// Use KMimeType for Qt4.
+	// FIXME: Verify if KIO works.
 	if (!qs_source_filename.isEmpty()) {
 		KMimeType::Ptr mimeType = KMimeType::findByPath(qs_source_filename, 0, true);
 		if (mimeType) {
@@ -542,7 +434,8 @@ Q_DECL_EXPORT int rp_create_thumbnail(const char *source_file, const char *outpu
 	// URI.
 	// NOTE: KDE desktops don't urlencode spaces or non-ASCII characters.
 	// GTK+ desktops *do* urlencode spaces and non-ASCII characters.
-	kv.emplace_back("Thumb::URI", url.toString().toUtf8().constData());
+	// FIXME: Do we want to store the local URI or the original URI?
+	kv.emplace_back("Thumb::URI", localUrl.toString().toUtf8().constData());
 
 	// Write the tEXt chunks.
 	pngWriter->write_tEXt(kv);

src/kde/RomPropertiesDialogPlugin.cpp
@@ -20,13 +20,9 @@
 
 // librpbase
 #include "librpbase/RomData.hpp"
-#include "librpbase/file/RpFile.hpp"
+#include "librpbase/file/IRpFile.hpp"
 using LibRpBase::RomData;
 using LibRpBase::IRpFile;
-using LibRpBase::RpFile;
-
-// RpFileKio
-#include "RpFile_kio.hpp"
 
 // libi18n
 #include "libi18n/i18n.h"
@@ -56,31 +52,9 @@ RomPropertiesDialogPlugin::RomPropertiesDialogPlugin(KPropertiesDialog *props, c
 		return;
 	}
 
-	// Get the local path if it's available. This is needed in order to
-	// support files on the local file system that don't have a file:/
-	// URL, e.g. desktop:/.
-	// References:
-	// - https://bugs.kde.org/show_bug.cgi?id=392100
-	// - https://cgit.kde.org/kio.git/commit/?id=7d6e4965dfcd7fc12e8cba7b1506dde22de5d2dd
-	// TODO: https://cgit.kde.org/kdenetwork-filesharing.git/commit/?id=abf945afd4f08d80cdc53c650d80d300f245a73d
-	// (and other uses) [use mostLocalPath()]
-	IRpFile *file = nullptr;
-	const KFileItem &fileItem = items.at(0);
-	const QString filename = fileItem.localPath();
-	if (!filename.isEmpty()) {
-		// Got a local path. Use RpFile.
-		file = new RpFile(Q2U8(filename), RpFile::FM_OPEN_READ_GZ);
-	} else {
-#ifdef HAVE_RPFILE_KIO
-		// Not a local file. Use RpFileKio.
-		file = new RpFileKio(fileItem.url());
-#else /* !HAVE_RPFILE_KIO */
-		// RpFileKio is not available.
-		return;
-#endif /* HAVE_RPFILE_KIO */
-	}
-
-	if (!file->isOpen()) {
+	// Attempt to open the ROM file.
+	IRpFile *const file = openQUrl(items.at(0).url(), false);
+	if (!file) {
 		// Unable to open the file.
 		file->unref();
 		return;

src/kde/RpQt.cpp
@@ -10,11 +10,33 @@
 #include "RpQImageBackend.hpp"
 
 // librpbase
+#include "librpbase/config/Config.hpp"
+#include "librpbase/file/FileSystem.hpp"
+#include "librpbase/file/RpFile.hpp"
+using namespace LibRpBase;
+
+// librptexture
 #include "librptexture/img/rp_image.hpp"
 using LibRpTexture::rp_image;
 
+// RpFileKio
+#include "RpFile_kio.hpp"
+
 // C includes. (C++ namespace)
 #include <cassert>
+
+// C++ includes.
+#include <string>
+using std::string;
+
+// Qt includes.
+#include <QtCore/QFileInfo>
+
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+# include <QtCore/QStandardPaths>
+#else /* QT_VERSION < QT_VERSION_CHECK(5,0,0) */
+# include <QtGui/QDesktopServices>
+#endif /* QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
 
 /**
  * Convert an rp_image to QImage.
@@ -36,4 +58,158 @@ QImage rpToQImage(const rp_image *image)
 	}
 
 	return backend->getQImage();
+}
+
+/**
+ * Localize a QUrl.
+ * This function automatically converts certain URL schemes, e.g. desktop:/, to local paths.
+ *
+ * @param qUrl QUrl.
+ * @return Localize QUrl, or empty QUrl on error.
+ */
+QUrl localizeQUrl(const QUrl &url)
+{
+	// Some things work better with local paths than with remote.
+	// KDE uses some custom URL schemes, e.g. desktop:/, to represent
+	// files that are actually stored locally. Detect this and convert
+	// it to a file:/ URL instead.
+
+	// NOTE: KDE's KFileItem has a function to do this, but it only works
+	// if KIO::UDSEntry::UDS_LOCAL_PATH is set. This is the case with
+	// KPropertiesDialogPlugin, but not the various forwarding plugins
+	// when converting a URL from a string.
+
+	// References:
+	// - https://bugs.kde.org/show_bug.cgi?id=392100
+	// - https://cgit.kde.org/kio.git/commit/?id=7d6e4965dfcd7fc12e8cba7b1506dde22de5d2dd
+	// TODO: https://cgit.kde.org/kdenetwork-filesharing.git/commit/?id=abf945afd4f08d80cdc53c650d80d300f245a73d
+	// (and other uses) [use mostLocalPath()]
+
+	// TODO: Handle "trash:/"; check KDE for other "local" URL schemes.
+
+	if (url.isEmpty()) {
+		// Empty URL. Nothing to do here.
+		return url;
+	}
+
+	if (url.scheme().isEmpty()) {
+		// No scheme. Assume this is a plain old filename.
+		QFileInfo fi(url.path());
+		return QUrl::fromLocalFile(fi.absoluteFilePath());
+	} else if (url.isLocalFile()) {
+		// This is a local file. ("file://" scheme)
+		QFileInfo fi(url.toLocalFile());
+		return QUrl::fromLocalFile(fi.absoluteFilePath());
+	} else if (url.scheme() == QLatin1String("desktop")) {
+		// Desktop folder.
+		QString url_path = url.path();
+		if (!url_path.isEmpty() && url_path.at(0) == QChar(L'/')) {
+			url_path.remove(0, 1);
+		}
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+		QString qs_local_filename = QStandardPaths::locate(QStandardPaths::DesktopLocation, url_path);
+#else /* QT_VERSION < QT_VERSION_CHECK(5,0,0) */
+		QString qs_local_filename = QDesktopServices::storageLocation(QDesktopServices::DesktopLocation);
+		if (!qs_local_filename.empty()) {
+			if (qs_local_filename.at(qs_local_filename.size()-1) != QChar(L'/')) {
+				qs_local_filename += QChar(L'/');
+			}
+			qs_local_filename += url_path;
+		}
+#endif /* QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
+		return QUrl::fromLocalFile(qs_local_filename);
+	}
+
+	// Not a recognized local file scheme.
+	// This is probably a remote file.
+	return url;
+}
+
+/**
+ * Open a QUrl as an IRpFile. (read-only)
+ * This function automatically converts certain URL schemes, e.g. desktop:/, to local paths.
+ *
+ * @param qUrl QUrl.
+ * @param isThumbnail If true, this file is being used for thumbnailing. Handle "bad FS" checking.
+ *
+ * @return IRpFile, or nullptr on error.
+ */
+IRpFile *openQUrl(const QUrl &url, bool isThumbnail)
+{
+	// Some things work better with local paths than with remote.
+	// KDE uses some custom URL schemes, e.g. desktop:/, to represent
+	// files that are actually stored locally. Detect this and convert
+	// it to a file:/ URL instead.
+
+	// NOTE: KDE's KFileItem has a function to do this, but it only works
+	// if KIO::UDSEntry::UDS_LOCAL_PATH is set. This is the case with
+	// KPropertiesDialogPlugin, but not the various forwarding plugins
+	// when converting a URL from a string.
+
+	// References:
+	// - https://bugs.kde.org/show_bug.cgi?id=392100
+	// - https://cgit.kde.org/kio.git/commit/?id=7d6e4965dfcd7fc12e8cba7b1506dde22de5d2dd
+	// TODO: https://cgit.kde.org/kdenetwork-filesharing.git/commit/?id=abf945afd4f08d80cdc53c650d80d300f245a73d
+	// (and other uses) [use mostLocalPath()]
+
+	// TODO: Handle "trash:/"; check KDE for other "local" URL schemes.
+
+	if (url.isEmpty()) {
+		// Empty URL. Nothing to do here.
+		return nullptr;
+	}
+
+	QUrl localUrl = localizeQUrl(url);
+	if (localUrl.isEmpty()) {
+		// Unable to localize the URL.
+		return nullptr;
+	}
+
+	string s_local_filename;
+	if (localUrl.scheme().isEmpty() || localUrl.isLocalFile()) {
+		s_local_filename = localUrl.toLocalFile().toUtf8().constData();
+	}
+
+	if (isThumbnail) {
+		// We're thumbnailing the file. Check "bad FS" settings.
+		const Config *const config = Config::instance();
+		const bool enableThumbnailOnNetworkFS = config->enableThumbnailOnNetworkFS();
+		if (!s_local_filename.empty()) {
+			// This is a local file. Check if it's on a "bad" file system.
+			if (FileSystem::isOnBadFS(s_local_filename.c_str(), enableThumbnailOnNetworkFS)) {
+				// This file is on a "bad" file system.
+				return nullptr;
+			}
+		} else {
+			// This is a remote file. Assume it's a "bad" file system.
+			if (!enableThumbnailOnNetworkFS) {
+				// Thumbnailing on network file systems is disabled.
+				return nullptr;
+			}
+		}
+	}
+
+	// Attempt to open an IRpFile.
+	IRpFile *file;
+	if (!s_local_filename.empty()) {
+		// Local filename. Use RpFile.
+		file = new RpFile(s_local_filename, RpFile::FM_OPEN_READ_GZ);
+	} else {
+		// Remote filename. Use RpFile_kio.
+#ifdef HAVE_RPFILE_KIO
+		file = new RpFileKio(url);
+#else /* !HAVE_RPFILE_KIO */
+		// Not supported...
+		return nullptr;
+#endif
+	}
+
+	if (!file->isOpen()) {
+		// Unable to open the file...
+		// TODO: Return an error code?
+		file->unref();
+		file = nullptr;
+	}
+
+	return file;
 }

src/kde/RpQt.hpp
@@ -9,6 +9,10 @@
 #ifndef __ROMPROPERTIES_KDE_RPQT_HPP__
 #define __ROMPROPERTIES_KDE_RPQT_HPP__
 
+namespace LibRpBase {
+	class IRpFile;
+}
+
 namespace LibRpTexture {
 	class rp_image;
 }
@@ -18,6 +22,7 @@ namespace LibRpTexture {
 
 // Qt includes.
 #include <QtCore/QString>
+#include <QtCore/QUrl>
 #include <QtGui/QImage>
 
 // KDE Frameworks prefix. (KDE4/KF5)
@@ -75,5 +80,25 @@ static inline QString U82Q(const char *str, int len = -1)
  * @return QImage.
  */
 QImage rpToQImage(const LibRpTexture::rp_image *image);
+
+/**
+ * Localize a QUrl.
+ * This function automatically converts certain URL schemes, e.g. desktop:/, to local paths.
+ *
+ * @param qUrl QUrl.
+ * @return Localize QUrl, or empty QUrl on error.
+ */
+QUrl localizeQUrl(const QUrl &url);
+
+/**
+ * Open a QUrl as an IRpFile. (read-only)
+ * This function automatically converts certain URL schemes, e.g. desktop:/, to local paths.
+ *
+ * @param qUrl QUrl.
+ * @param isThumbnail If true, this file is being used for thumbnailing. Handle "bad FS" checking.
+ *
+ * @return IRpFile, or nullptr on error.
+ */
+LibRpBase::IRpFile *openQUrl(const QUrl &url, bool isThumbnail = false);
 
 #endif /* __ROMPROPERTIES_KDE_RPQT_HPP__ */

src/kde/kde4/CMakeLists.txt
@@ -67,7 +67,7 @@ IF(BUILD_KDE4)
 			$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/../..>	# src
 			$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/../..>	# src
 		)
-	TARGET_LINK_LIBRARIES(rom-properties-kde4 PRIVATE romdata rpbase)
+	TARGET_LINK_LIBRARIES(rom-properties-kde4 PRIVATE romdata rpbase unixcommon)
 	IF(ENABLE_NLS)
 		TARGET_LINK_LIBRARIES(rom-properties-kde4 PRIVATE i18n)
 	ENDIF(ENABLE_NLS)

src/kde/kf5/CMakeLists.txt
@@ -137,7 +137,7 @@ IF(BUILD_KF5)
 			$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/../..>	# src
 			$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/../..>	# src
 		)
-	TARGET_LINK_LIBRARIES(rom-properties-kf5 PRIVATE romdata rpbase)
+	TARGET_LINK_LIBRARIES(rom-properties-kf5 PRIVATE romdata rpbase unixcommon)
 	IF(ENABLE_NLS)
 		TARGET_LINK_LIBRARIES(rom-properties-kf5 PRIVATE i18n)
 	ENDIF(ENABLE_NLS)

src/kde/kf5/RpExtractorPlugin.cpp
@@ -21,9 +21,6 @@
 #include "librpbase/config/Config.hpp"
 using namespace LibRpBase;
 
-// RpFileKio
-#include "../RpFile_kio.hpp"
-
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
 using LibRomData::RomDataFactory;
@@ -95,62 +92,9 @@ QStringList RpExtractorPlugin::mimetypes(void) const
 
 void RpExtractorPlugin::extract(ExtractionResult *result)
 {
-	// Check if the source filename is a URI.
-	const QString source_file = result->inputUrl();
-	QUrl url(source_file);
-	QFileInfo fi_src;
-	QString qs_source_filename;
-	if (url.scheme().isEmpty()) {
-		// No scheme. This is a plain old filename.
-		fi_src = QFileInfo(source_file);
-		qs_source_filename = fi_src.absoluteFilePath();
-		url = QUrl::fromLocalFile(qs_source_filename);
-	} else if (url.isLocalFile()) {
-		// "file://" scheme. This is a local file.
-		qs_source_filename = url.toLocalFile();
-		fi_src = QFileInfo(qs_source_filename);
-		url = QUrl::fromLocalFile(fi_src.absoluteFilePath());
-	} else if (url.scheme() == QLatin1String("desktop")) {
-		// Desktop folder.
-		// KFileItem::localPath() isn't working for "desktop:/" here,
-		// so handle it manually.
-		// TODO: Also handle "trash:/"?
-		qs_source_filename = QStandardPaths::locate(QStandardPaths::DesktopLocation, url.path());
-	} else {
-		// Has a scheme that isn't "file://".
-		// This is probably a remote file.
-	}
-
-	if (!url.isValid() || url.isEmpty()) {
-		// Invalid or empty URL.
-		return;
-	}
-
-	if (!qs_source_filename.isEmpty()) {
-		// Check for "bad" file systems.
-		const Config *const config = Config::instance();
-		if (FileSystem::isOnBadFS(qs_source_filename.toUtf8().constData(), config->enableThumbnailOnNetworkFS())) {
-			// This file is on a "bad" file system.
-			return;
-		}
-	}
-
 	// Attempt to open the ROM file.
-	IRpFile *file = nullptr;
-	if (!qs_source_filename.isEmpty()) {
-		// Local file. Use RpFile.
-		file = new RpFile(qs_source_filename.toUtf8().constData(), RpFile::FM_OPEN_READ_GZ);
-	} else {
-#ifdef HAVE_RPFILE_KIO
-		// Not a local file. Use RpFileKio.
-		file = new RpFileKio(url);
-#else /* !HAVE_RPFILE_KIO */
-		// RpFileKio is not available.
-		return;
-#endif /* HAVE_RPFILE_KIO */
-	}
-
-	if (!file->isOpen()) {
+	IRpFile *const file = openQUrl(QUrl(result->inputUrl()), false);
+	if (!file) {
 		// Could not open the file.
 		return;
 	}

src/kde/kf5/RpOverlayIconPlugin.cpp
@@ -20,9 +20,6 @@
 #include "librpbase/config/Config.hpp"
 using namespace LibRpBase;
 
-// RpFileKio
-#include "../RpFile_kio.hpp"
-
 // libromdata
 #include "libromdata/RomDataFactory.hpp"
 using LibRomData::RomDataFactory;
@@ -87,51 +84,9 @@ QStringList RpOverlayIconPlugin::getOverlays(const QUrl &item)
 		return sl;
 	}
 
-	// Check if the source URL is a local file.
-	QString qs_source_filename;
-	if (item.isLocalFile()) {
-		// "file://" scheme. This is a local file.
-		qs_source_filename = item.toLocalFile();
-	} else if (item.scheme() == QLatin1String("desktop")) {
-		// Desktop folder.
-		// KFileItem::localPath() isn't working for "desktop:/" here,
-		// so handle it manually.
-		// TODO: Also handle "trash:/"?
-		qs_source_filename = QStandardPaths::locate(QStandardPaths::DesktopLocation, item.path());
-	} else {
-		// Has a scheme that isn't "file://".
-		// This is probably a remote file.
-	}
-
-	if (!item.isValid() || item.isEmpty()) {
-		// Invalid or empty URL.
-		return sl;
-	}
-
-	if (!qs_source_filename.isEmpty()) {
-		// Check for "bad" file systems.
-		if (FileSystem::isOnBadFS(qs_source_filename.toUtf8().constData(), config->enableThumbnailOnNetworkFS())) {
-			// This file is on a "bad" file system.
-			return sl;
-		}
-	}
-
 	// Attempt to open the ROM file.
-	IRpFile *file = nullptr;
-	if (!qs_source_filename.isEmpty()) {
-		// Local file. Use RpFile.
-		file = new RpFile(qs_source_filename.toUtf8().constData(), RpFile::FM_OPEN_READ_GZ);
-	} else {
-#ifdef HAVE_RPFILE_KIO
-		// Not a local file. Use RpFileKio.
-		file = new RpFileKio(item);
-#else /* !HAVE_RPFILE_KIO */
-		// RpFileKio is not available.
-		return sl;
-#endif /* HAVE_RPFILE_KIO */
-	}
-
-	if (!file->isOpen()) {
+	IRpFile *const file = openQUrl(item, true);
+	if (!file) {
 		// Could not open the file.
 		return sl;
 	}

commit 73f21a9e64cd2c72fc1c8735ef64f9b2fee48b33

[kde] RomThumbCreator: Escape '#' characters in filenames.
QUrl assumes anything after the '#' isn't relevant, which breaks
handling files with '#' in the filename.

This fixes a regression from commit eef656926e46b1aac81d7963bbe6f605fbafbd8c.
([kde] RpQt: New functions localizeQUrl() and openQUrl().)

This affects v1.5 and v1.6 on KDE only.

src/kde/RomThumbCreator.cpp
@@ -209,9 +209,25 @@ RomThumbCreator::~RomThumbCreator()
 bool RomThumbCreator::create(const QString &path, int width, int height, QImage &img)
 {
 	Q_UNUSED(height);
+	if (path.isEmpty()) {
+		return false;
+	}
 
 	// Attempt to open the ROM file.
-	IRpFile *const file = openQUrl(QUrl(path), true);
+	// NOTE: QUrl will truncate the filename if a '#' is present,
+	// so we need to urlencode it first if it's an absolute path.
+	QUrl path_url;
+	if (path[0] == QChar(L'/')) {
+		// Encode '#' as "%23" to prevent issues.
+		QString path_enc = path;
+		path_enc.replace(QChar(L'#'), QLatin1String("%23"));
+		path_url.setUrl(path_enc);
+	} else {
+		// Use the path as-is.
+		path_url.setUrl(path);
+	}
+
+	IRpFile *const file = openQUrl(path_url, true);
 	if (!file) {
 		return false;
 	}