GerbilSoft/rom-properties

commit 19d4e8dc06740c9154d01d1e527fa00012a8d25b

[gtk] Split the icon/banner code from RomDataView into DragImage.
Similar to KDE, but it's called DragImage instead of DragImageLabel
because it's a subclass of GtkImage, not GtkLabel.

Note that there's no drag & drop functionality yet.

src/gtk/RomDataView.cpp
@@ -10,9 +10,10 @@
 #include "RomDataView.hpp"
 #include "rp-gtk-enums.h"
 
+// DragImage (GtkImage subclass)
+#include "DragImage.hpp"
+
 // librpbase, librptexture
-#include "librpbase/img/IconAnimData.hpp"
-#include "librpbase/img/IconAnimHelper.hpp"
 using namespace LibRpBase;
 using LibRpTexture::rp_image;
 
@@ -93,11 +94,6 @@ static void	tree_view_realize_signal_handler    (GtkTreeView	*treeView,
 static void	cboLanguage_changed_signal_handler  (GtkComboBox	*widget,
 						     gpointer	 	 user_data);
 
-/** Icon animation timer. **/
-static void	start_anim_timer(RomDataView *page);
-static void	stop_anim_timer (RomDataView *page);
-static gboolean	anim_timer_func (RomDataView *page);
-
 #if GTK_CHECK_VERSION(3,0,0)
 typedef GtkBoxClass superclass;
 typedef GtkBox super;
@@ -108,7 +104,7 @@ typedef GtkVBox super;
 #define GTK_TYPE_SUPER GTK_TYPE_VBOX
 #endif
 
-// XFCE property page class.
+// GTK+ property page class.
 struct _RomDataViewClass {
 	superclass __parent__;
 };
@@ -131,7 +127,7 @@ struct Data_ListDataMulti_t {
 		, field(field) { }
 };
 
-// GTK+ property page.
+// GTK+ property page instance.
 struct _RomDataView {
 	super __parent__;
 
@@ -150,15 +146,6 @@ struct _RomDataView {
 
 	// ROM data.
 	RomData		*romData;
-
-	// Animated icon data.
-	array<PIMGTYPE, IconAnimData::MAX_FRAMES> iconFrames;
-	IconAnimHelper	*iconAnimHelper;
-	int		last_frame_number;	// Last frame number.
-
-	// Icon animation timer.
-	guint		tmrIconAnim;
-	int		last_delay;		// Last delay value.
 
 	// Tab layout.
 	GtkWidget	*tabWidget;
@@ -311,8 +298,6 @@ rom_data_view_init(RomDataView *page)
 	// No ROM data initially.
 	page->uri = nullptr;
 	page->romData = nullptr;
-	page->last_frame_number = 0;
-	page->iconAnimHelper = new IconAnimHelper();
 	page->tabWidget = nullptr;
 	page->tabs = new vector<RomDataView::tab>();
 
@@ -325,10 +310,6 @@ rom_data_view_init(RomDataView *page)
 	page->vecDescLabels = new vector<GtkWidget*>();
 	page->vecStringMulti = new vector<Data_StringMulti_t>();
 	page->vecListDataMulti = new vector<Data_ListDataMulti_t>();
-
-	// Animation timer.
-	page->tmrIconAnim = 0;
-	page->last_delay = 0;
 
 	/**
 	 * Base class is:
@@ -377,11 +358,11 @@ rom_data_view_init(RomDataView *page)
 	gtk_widget_show(page->lblSysInfo);
 
 	// Banner.
-	page->imgBanner = gtk_image_new();
+	page->imgBanner = drag_image_new();
 	gtk_box_pack_start(GTK_BOX(page->hboxHeaderRow), page->imgBanner, false, false, 0);
 
 	// Icon.
-	page->imgIcon = gtk_image_new();
+	page->imgIcon = drag_image_new();
 	gtk_box_pack_start(GTK_BOX(page->hboxHeaderRow), page->imgIcon, false, false, 0);
 
 	// Make lblSysInfo bold.
@@ -402,18 +383,12 @@ rom_data_view_init(RomDataView *page)
 static void
 rom_data_view_dispose(GObject *object)
 {
-	RomDataView *page = ROM_DATA_VIEW(object);
+	RomDataView *const page = ROM_DATA_VIEW(object);
 
 	/* Unregister the changed_idle */
 	if (G_UNLIKELY(page->changed_idle > 0)) {
 		g_source_remove(page->changed_idle);
 		page->changed_idle = 0;
-	}
-
-	// Delete the animation timer.
-	if (page->tmrIconAnim > 0) {
-		g_source_remove(page->tmrIconAnim);
-		page->tmrIconAnim = 0;
 	}
 
 	// Delete the icon frames and tabs.
@@ -432,7 +407,6 @@ rom_data_view_finalize(GObject *object)
 	g_free(page->uri);
 
 	// Delete the C++ objects.
-	delete page->iconAnimHelper;
 	delete page->tabs;
 	delete page->vecDescLabels;
 
@@ -624,8 +598,9 @@ rom_data_view_init_header_row(RomDataView *page)
 	assert(page != nullptr);
 
 	// NOTE: romData might be nullptr in some cases.
-	//assert(page->romData != nullptr);
-	if (!page->romData) {
+	const RomData *const romData = page->romData;
+	//assert(romData != nullptr);
+	if (!romData) {
 		// No ROM data.
 		// Hide the widgets.
 		gtk_widget_hide(page->hboxHeaderRow);
@@ -634,9 +609,9 @@ rom_data_view_init_header_row(RomDataView *page)
 
 	// System name and file type.
 	// TODO: System logo and/or game title?
-	const char *systemName = page->romData->systemName(
+	const char *systemName = romData->systemName(
 		RomData::SYSNAME_TYPE_LONG | RomData::SYSNAME_REGION_ROM_LOCAL);
-	const char *fileType = page->romData->fileType_string();
+	const char *fileType = romData->fileType_string();
 	assert(systemName != nullptr);
 	assert(fileType != nullptr);
 	if (!systemName) {
@@ -652,20 +627,15 @@ rom_data_view_init_header_row(RomDataView *page)
 	gtk_label_set_text(GTK_LABEL(page->lblSysInfo), sysInfo.c_str());
 
 	// Supported image types.
-	const uint32_t imgbf = page->romData->supportedImageTypes();
+	const uint32_t imgbf = romData->supportedImageTypes();
 
 	// Banner.
 	gtk_widget_hide(page->imgBanner);
 	if (imgbf & RomData::IMGBF_INT_BANNER) {
 		// Get the banner.
-		const rp_image *banner = page->romData->image(RomData::IMG_INT_BANNER);
-		if (banner && banner->isValid()) {
-			PIMGTYPE pImgType = rp_image_to_PIMGTYPE(banner);
-			if (pImgType) {
-				gtk_image_set_from_PIMGTYPE(GTK_IMAGE(page->imgBanner), pImgType);
-				PIMGTYPE_destroy(pImgType);
-				gtk_widget_show(page->imgBanner);
-			}
+		bool ok = drag_image_set_rp_image(DRAG_IMAGE(page->imgBanner), romData->image(RomData::IMG_INT_BANNER));
+		if (ok) {
+			gtk_widget_show(page->imgBanner);
 		}
 	}
 
@@ -673,42 +643,17 @@ rom_data_view_init_header_row(RomDataView *page)
 	gtk_widget_hide(page->imgIcon);
 	if (imgbf & RomData::IMGBF_INT_ICON) {
 		// Get the icon.
-		const rp_image *icon = page->romData->image(RomData::IMG_INT_ICON);
+		const rp_image *const icon = romData->image(RomData::IMG_INT_ICON);
 		if (icon && icon->isValid()) {
 			// Is this an animated icon?
-			const IconAnimData *const iconAnimData = page->romData->iconAnimData();
-			if (iconAnimData) {
-				// Convert the icons to PIMGTYPE.
-				for (int i = iconAnimData->count-1; i >= 0; i--) {
-					const rp_image *const frame = iconAnimData->frames[i];
-					if (frame && frame->isValid()) {
-						PIMGTYPE pImgType = rp_image_to_PIMGTYPE(frame);
-						if (pImgType) {
-							page->iconFrames[i] = pImgType;
-						}
-					}
-				}
-
-				// Set up the IconAnimHelper.
-				page->iconAnimHelper->setIconAnimData(iconAnimData);
-				// Initialize the animation.
-				page->last_frame_number = page->iconAnimHelper->frameNumber();
-
-				// Show the first frame.
-				gtk_image_set_from_PIMGTYPE(GTK_IMAGE(page->imgIcon),
-					page->iconFrames[page->last_frame_number]);
+			bool ok = drag_image_set_icon_anim_data(DRAG_IMAGE(page->imgIcon), romData->iconAnimData());
+			if (!ok) {
+				// Not an animated icon, or invalid icon data.
+				// Set the static icon.
+				ok = drag_image_set_rp_image(DRAG_IMAGE(page->imgIcon), icon);
+			}
+			if (ok) {
 				gtk_widget_show(page->imgIcon);
-
-				// Icon animation timer is set in start_anim_timer().
-			} else {
-				// Not an animated icon.
-				page->last_frame_number = 0;
-				PIMGTYPE pImgType = rp_image_to_PIMGTYPE(icon);
-				if (pImgType) {
-					gtk_image_set_from_PIMGTYPE(GTK_IMAGE(page->imgIcon), pImgType);
-					page->iconFrames[0] = pImgType;
-					gtk_widget_show(page->imgIcon);
-				}
 			}
 		}
 	}
@@ -1932,14 +1877,6 @@ rom_data_view_delete_tabs(RomDataView *page)
 	page->set_lc->clear();
 	page->vecStringMulti->clear();
 	page->vecListDataMulti->clear();
-
-	// Delete the icon frames.
-	for (int i = page->iconFrames.size()-1; i >= 0; i--) {
-		if (page->iconFrames[i]) {
-			PIMGTYPE_destroy(page->iconFrames[i]);
-			page->iconFrames[i] = nullptr;
-		}
-	}
 }
 
 /** Signal handlers. **/
@@ -1973,7 +1910,7 @@ rom_data_view_map_signal_handler(RomDataView	*page,
 				 gpointer	 user_data)
 {
 	RP_UNUSED(user_data);
-	start_anim_timer(page);
+	drag_image_start_anim_timer(DRAG_IMAGE(page->imgIcon));
 }
 
 /**
@@ -1986,7 +1923,7 @@ rom_data_view_unmap_signal_handler(RomDataView	*page,
 				   gpointer	 user_data)
 {
 	RP_UNUSED(user_data);
-	stop_anim_timer(page);
+	drag_image_start_anim_timer(DRAG_IMAGE(page->imgIcon));
 }
 
 /**
@@ -2102,83 +2039,4 @@ cboLanguage_changed_signal_handler(GtkComboBox	*widget,
 	uint32_t lc = 0;
 	gtk_tree_model_get(GTK_TREE_MODEL(page->lstoreLanguage), &iter, SM_COL_LC, &lc, -1);
 	rom_data_view_update_multi(page, lc);
-}
-
-/** Icon animation timer. **/
-
-/**
- * Start the animation timer.
- */
-static void start_anim_timer(RomDataView *page)
-{
-	if (!page->iconAnimHelper->isAnimated()) {
-		// Not an animated icon.
-		return;
-	}
-
-	// Get the current frame information.
-	page->last_frame_number = page->iconAnimHelper->frameNumber();
-	const int delay = page->iconAnimHelper->frameDelay();
-	assert(delay > 0);
-	if (delay <= 0) {
-		// Invalid delay value.
-		return;
-	}
-
-	// Stop the animation timer.
-	stop_anim_timer(page);
-
-	// Set a timer for the current frame.
-	page->last_delay = delay;
-	page->tmrIconAnim = g_timeout_add(delay,
-		reinterpret_cast<GSourceFunc>(anim_timer_func), page);
-}
-
-/**
- * Stop the animation timer.
- */
-static void stop_anim_timer(RomDataView *page)
-{
-	if (page->tmrIconAnim > 0) {
-		g_source_remove(page->tmrIconAnim);
-		page->tmrIconAnim = 0;
-		page->last_delay = 0;
-	}
-}
-
-/**
- * Animated icon timer.
- */
-static gboolean anim_timer_func(RomDataView *page)
-{
-	if (page->tmrIconAnim == 0) {
-		// Shutting down...
-		return false;
-	}
-
-	// Next frame.
-	int delay = 0;
-	int frame = page->iconAnimHelper->nextFrame(&delay);
-	if (delay <= 0 || frame < 0) {
-		// Invalid frame...
-		page->tmrIconAnim = 0;
-		return false;
-	}
-
-	if (frame != page->last_frame_number) {
-		// New frame number.
-		// Update the icon.
-		gtk_image_set_from_PIMGTYPE(GTK_IMAGE(page->imgIcon), page->iconFrames[frame]);
-		page->last_frame_number = frame;
-	}
-
-	if (page->last_delay != delay) {
-		// Set a new timer and unset the current one.
-		page->last_delay = delay;
-		page->tmrIconAnim = g_timeout_add(delay,
-			reinterpret_cast<GSourceFunc>(anim_timer_func), page);
-		return false;
-	}
-	// Keep the current timer running.
-	return true;
 }

src/gtk/CMakeLists.txt
@@ -35,6 +35,7 @@ ENDIF(CPU_i386 OR CPU_amd64)
 # Sources and headers.
 SET(rom-properties-gtk_SRCS
 	RomDataView.cpp
+	DragImage.cpp
 	CreateThumbnail.cpp
 	PIMGTYPE.cpp
 	rp-gtk-enums.c
@@ -42,6 +43,7 @@ SET(rom-properties-gtk_SRCS
 	)
 SET(rom-properties-gtk_H
 	RomDataView.hpp
+	DragImage.hpp
 	GdkImageConv.hpp
 	PIMGTYPE.hpp
 	rp-gtk-enums.h

src/gtk/DragImage.cpp
@@ -1,0 +1,440 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (GTK+ common)                      *
+ * DragImage.cpp: Drag & Drop image.                                       *
+ *                                                                         *
+ * Copyright (c) 2017-2020 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "stdafx.h"
+#include "DragImage.hpp"
+#include "PIMGTYPE.hpp"
+
+// librpbase, librptexture
+#include "librpbase/img/IconAnimData.hpp"
+#include "librpbase/img/IconAnimHelper.hpp"
+using namespace LibRpBase;
+using LibRpTexture::rp_image;
+
+// TODO: Adjust minimum image size based on DPI.
+#define DIL_MIN_IMAGE_SIZE 32
+
+static void	drag_image_dispose	(GObject	*object);
+static void	drag_image_finalize	(GObject	*object);
+
+// DragImage class.
+struct _DragImageClass {
+	GtkImageClass __parent__;
+};
+
+// DragImage instance.
+struct _DragImage {
+	GtkImage __parent__;
+
+	// Minimum image size.
+	struct {
+		int width;
+		int height;
+	} minimumImageSize;
+
+	// rp_image. (NOTE: Not owned by this object.)
+	const rp_image *img;
+
+	// Animated icon data.
+	struct anim_vars {
+		const IconAnimData *iconAnimData;
+		guint tmrIconAnim;	// Timer ID
+		int last_delay;		// Last delay value.
+		std::array<PIMGTYPE, IconAnimData::MAX_FRAMES> iconFrames;
+		IconAnimHelper iconAnimHelper;
+		int last_frame_number;	// Last frame number.
+
+		anim_vars()
+			: tmrIconAnim(0)
+			, last_delay(0)
+			, last_frame_number(0)
+		{
+			iconFrames.fill(nullptr);
+		}
+		~anim_vars() {
+			if (tmrIconAnim > 0) {
+				g_source_remove(tmrIconAnim);
+			}
+
+			for (auto iter = iconFrames.begin(); iter != iconFrames.end(); ++iter) {
+				if (*iter) {
+					PIMGTYPE_destroy(*iter);
+				}
+			}
+		}
+	};
+	anim_vars *anim;
+};
+
+// NOTE: G_DEFINE_TYPE() doesn't work in C++ mode with gcc-6.2
+// due to an implicit int to GTypeFlags conversion.
+G_DEFINE_TYPE_EXTENDED(DragImage, drag_image,
+	GTK_TYPE_IMAGE, static_cast<GTypeFlags>(0), {});
+
+static void
+drag_image_class_init(DragImageClass *klass)
+{
+	GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
+	gobject_class->dispose = drag_image_dispose;
+	gobject_class->finalize = drag_image_finalize;
+
+	// TODO
+	//gobject_class->get_property = drag_image_get_property;
+	//gobject_class->set_property = drag_image_set_property;
+}
+
+static void
+drag_image_init(DragImage *image)
+{
+	image->minimumImageSize.width = DIL_MIN_IMAGE_SIZE;
+	image->minimumImageSize.height = DIL_MIN_IMAGE_SIZE;
+	image->img = nullptr;
+	image->anim = nullptr;
+}
+
+static void
+drag_image_dispose(GObject *object)
+{
+	DragImage *const image = DRAG_IMAGE(object);
+
+	// Delete the animation data if present.
+	// This will automatically unregister the timer.
+	delete image->anim;
+	image->anim = nullptr;
+
+	// Call the superclass dispose() function.
+	G_OBJECT_CLASS(drag_image_parent_class)->dispose(object);
+}
+
+static void
+drag_image_finalize(GObject *object)
+{
+	//DragImage *const image = DRAG_IMAGE(object);
+
+	// Nothing to do here right now...
+
+	// Call the superclass finalize() function.
+	G_OBJECT_CLASS(drag_image_parent_class)->finalize(object);
+}
+
+GtkWidget*
+drag_image_new(void)
+{
+	return static_cast<GtkWidget*>(g_object_new(TYPE_DRAG_IMAGE, nullptr));
+}
+
+/**
+ * Update the pixmap(s).
+ * @param image DragImage
+ * @return True on success; false on error.
+ */
+static bool
+drag_image_update_pixmaps(DragImage *image)
+{
+	g_return_val_if_fail(IS_DRAG_IMAGE(image), false);
+
+	auto *const anim = image->anim;
+	if (anim && anim->iconAnimData) {
+		const IconAnimData *const iconAnimData = anim->iconAnimData;
+
+		// Convert the frames to PIMGTYPE.
+		for (int i = iconAnimData->count-1; i >= 0; i--) {
+			// Remove the existing frame first.
+			if (anim->iconFrames[i]) {
+				PIMGTYPE_destroy(anim->iconFrames[i]);
+				anim->iconFrames[i] = nullptr;
+			}
+
+			const rp_image *const frame = iconAnimData->frames[i];
+			if (frame && frame->isValid()) {
+				// NOTE: Allowing NULL frames here...
+				anim->iconFrames[i] = rp_image_to_PIMGTYPE(frame);
+			}
+		}
+
+		// Set up the IconAnimHelper.
+		anim->iconAnimHelper.setIconAnimData(iconAnimData);
+		if (anim->iconAnimHelper.isAnimated()) {
+			// Initialize the animation.
+			anim->last_frame_number = anim->iconAnimHelper.frameNumber();
+
+			// Animation timer will be set up when animation is started.
+		}
+
+		// Show the first frame.
+		gtk_image_set_from_PIMGTYPE(GTK_IMAGE(image),
+			anim->iconFrames[anim->iconAnimHelper.frameNumber()]);
+		return true;
+	}
+
+	if (image->img && image->img->isValid()) {
+		// Single image.
+		PIMGTYPE pImg = rp_image_to_PIMGTYPE(image->img);
+		gtk_image_set_from_PIMGTYPE(GTK_IMAGE(image), pImg);
+		if (pImg) {
+			PIMGTYPE_destroy(pImg);
+		}
+		return true;
+	}
+
+	// No image or animated icon data.
+	return false;
+}
+
+void
+drag_image_get_minimum_image_size(DragImage *image, int *width, int *height)
+{
+	g_return_if_fail(IS_DRAG_IMAGE(image));
+
+	*width = image->minimumImageSize.width;
+	*height = image->minimumImageSize.height;
+}
+
+void
+drag_image_set_minimum_image_size(DragImage *image, int width, int height)
+{
+	g_return_if_fail(IS_DRAG_IMAGE(image));
+
+	if (image->minimumImageSize.width != width &&
+	    image->minimumImageSize.height != height)
+	{
+		image->minimumImageSize.width = width;
+		image->minimumImageSize.height = height;
+		drag_image_update_pixmaps(image);
+	}
+}
+
+/**
+ * Set the rp_image for this image.
+ *
+ * NOTE: The rp_image pointer is stored and used if necessary.
+ * Make sure to call this function with nullptr before deleting
+ * the rp_image object.
+ *
+ * NOTE 2: If animated icon data is specified, that supercedes
+ * the individual rp_image.
+ *
+ * @param image DragImage
+ * @param img rp_image, or nullptr to clear.
+ * @return True on success; false on error or if clearing.
+ */
+bool
+drag_image_set_rp_image(DragImage *image, const LibRpTexture::rp_image *img)
+{
+	g_return_val_if_fail(IS_DRAG_IMAGE(image), false);
+
+	if (!img) {
+		image->img = nullptr;
+		if (!image->anim || !image->anim->iconAnimData) {
+			gtk_image_clear(GTK_IMAGE(image));
+		} else {
+			return drag_image_update_pixmaps(image);
+		}
+		return false;
+	}
+
+	// Don't check if the image pointer matches the
+	// previously stored image, since the underlying
+	// image may have changed.
+	image->img = img;
+	return drag_image_update_pixmaps(image);
+}
+
+/**
+ * Set the icon animation data for this image.
+ *
+ * NOTE: The iconAnimData pointer is stored and used if necessary.
+ * Make sure to call this function with nullptr before deleting
+ * the IconAnimData object.
+ *
+ * NOTE 2: If animated icon data is specified, that supercedes
+ * the individual rp_image.
+ *
+ * @param image DragImage
+ * @param iconAnimData IconAnimData, or nullptr to clear.
+ * @return True on success; false on error or if clearing.
+ */
+bool
+drag_image_set_icon_anim_data(DragImage *image, const LibRpBase::IconAnimData *iconAnimData)
+{
+	g_return_val_if_fail(IS_DRAG_IMAGE(image), false);
+
+	if (!image->anim) {
+		image->anim = new DragImage::anim_vars();
+	}
+	auto *const anim = image->anim;
+
+	if (!iconAnimData) {
+		if (anim->tmrIconAnim > 0) {
+			g_source_remove(anim->tmrIconAnim);
+			anim->tmrIconAnim = 0;
+		}
+		anim->iconAnimData = nullptr;
+
+		if (!image->img) {
+			gtk_image_clear(GTK_IMAGE(image));
+		} else {
+			return drag_image_update_pixmaps(image);
+		}
+		return false;
+	}
+
+	// Don't check if the data pointer matches the
+	// previously stored data, since the underlying
+	// data may have changed.
+	anim->iconAnimData = iconAnimData;
+	return drag_image_update_pixmaps(image);
+}
+
+/**
+ * Clear the rp_image and iconAnimData.
+ * This will stop the animation timer if it's running.
+ * @param image DragImage
+ */
+void
+drag_image_clear(DragImage *image)
+{
+	g_return_if_fail(IS_DRAG_IMAGE(image));
+
+	auto *const anim = image->anim;
+	if (anim) {
+		if (anim->tmrIconAnim > 0) {
+			g_source_remove(anim->tmrIconAnim);
+			anim->tmrIconAnim = 0;
+		}
+		anim->iconAnimData = nullptr;
+	}
+
+	image->img = nullptr;
+	gtk_image_clear(GTK_IMAGE(image));
+}
+
+/**
+ * Animated icon timer.
+ * @param image DragImage
+ * @return True to continue the timer; false to stop it.
+ */
+static gboolean
+drag_image_anim_timer_func(DragImage *image)
+{
+	g_return_val_if_fail(IS_DRAG_IMAGE(image), false);
+	auto *const anim = image->anim;
+	g_return_val_if_fail(anim != nullptr, false);
+
+	if (anim->tmrIconAnim == 0) {
+		// Shutting down...
+		return false;
+	}
+
+	// Next frame.
+	int delay = 0;
+	int frame = anim->iconAnimHelper.nextFrame(&delay);
+	if (delay <= 0 || frame < 0) {
+		// Invalid frame...
+		anim->tmrIconAnim = 0;
+		return false;
+	}
+
+	if (frame != anim->last_frame_number) {
+		// New frame number.
+		// Update the icon.
+		gtk_image_set_from_PIMGTYPE(GTK_IMAGE(image), anim->iconFrames[frame]);
+		anim->last_frame_number = frame;
+	}
+
+	if (anim->last_delay != delay) {
+		// Set a new timer and unset the current one.
+		anim->last_delay = delay;
+		anim->tmrIconAnim = g_timeout_add(delay,
+			reinterpret_cast<GSourceFunc>(drag_image_anim_timer_func), image);
+		return false;
+	}
+
+	// Keep the current timer running.
+	return true;
+}
+
+/**
+ * Start the animation timer.
+ * @param image DragImage
+ */
+void
+drag_image_start_anim_timer(DragImage *image)
+{
+	g_return_if_fail(IS_DRAG_IMAGE(image));
+
+	auto *const anim = image->anim;
+	if (!anim || !anim->iconAnimHelper.isAnimated()) {
+		// Not an animated icon.
+		return;
+	}
+
+	// Get the current frame information.
+	anim->last_frame_number = anim->iconAnimHelper.frameNumber();
+	const int delay = anim->iconAnimHelper.frameDelay();
+	assert(delay > 0);
+	if (delay <= 0) {
+		// Invalid delay value.
+		return;
+	}
+
+	// Stop the animation timer first.
+	drag_image_stop_anim_timer(image);
+
+	// Set a single-shot timer for the current frame.
+	image->anim->last_delay = delay;
+	image->anim->tmrIconAnim = g_timeout_add(delay,
+		reinterpret_cast<GSourceFunc>(drag_image_anim_timer_func), image);
+}
+
+/**
+ * Stop the animation timer.
+ * @param image DragImage
+ */
+void
+drag_image_stop_anim_timer(DragImage *image)
+{
+	g_return_if_fail(IS_DRAG_IMAGE(image));
+	auto *const anim = image->anim;
+	g_return_if_fail(anim != nullptr);
+
+	if (anim->tmrIconAnim > 0) {
+		g_source_remove(anim->tmrIconAnim);
+		anim->tmrIconAnim = 0;
+		anim->last_delay = 0;
+	}
+}
+
+/**
+ * Is the animation timer running?
+ * @param image DragImage
+ * @return True if running; false if not.
+ */
+bool
+drag_image_is_anim_timer_running(DragImage *image)
+{
+	g_return_val_if_fail(IS_DRAG_IMAGE(image), false);
+	auto *const anim = image->anim;
+	return (anim && (anim->tmrIconAnim > 0));
+}
+
+/**
+ * Reset the animation frame.
+ * This does NOT update the animation frame.
+ * @param image DragImage
+ */
+void
+drag_image_reset_anim_frame(DragImage *image)
+{
+	g_return_if_fail(IS_DRAG_IMAGE(image));
+
+	auto *const anim = image->anim;
+	if (anim) {
+		anim->last_frame_number = 0;
+	}
+}

src/gtk/DragImage.hpp
@@ -1,0 +1,121 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (GTK+ common)                      *
+ * DragImage.hpp: Drag & Drop image.                                       *
+ *                                                                         *
+ * Copyright (c) 2017-2020 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_GTK_DRAGIMAGE_HPP__
+#define __ROMPROPERTIES_GTK_DRAGIMAGE_HPP__
+
+#ifdef __cplusplus
+namespace LibRpTexture {
+	class rp_image;
+}
+
+// librpbase
+#include "librpbase/img/IconAnimData.hpp"
+#include "librpbase/img/IconAnimHelper.hpp"
+
+// C++ includes.
+#include <array>
+#endif /* __cplusplus */
+
+// GTK+ includes.
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+typedef struct _DragImageClass	DragImageClass;
+typedef struct _DragImage	DragImage;
+
+#define TYPE_DRAG_IMAGE            (drag_image_get_type())
+#define DRAG_IMAGE(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), TYPE_DRAG_IMAGE, DragImage))
+#define DRAG_IMAGE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),  TYPE_DRAG_IMAGE, DragImageClass))
+#define IS_DRAG_IMAGE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), TYPE_DRAG_IMAGE))
+#define IS_DRAG_IMAGE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),  TYPE_DRAG_IMAGE))
+#define DRAG_IMAGE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj),  TYPE_DRAG_IMAGE, DragImageClass))
+
+/* these two functions are implemented automatically by the G_DEFINE_TYPE macro */
+GType		drag_image_get_type		(void) G_GNUC_CONST G_GNUC_INTERNAL;
+void		drag_image_register_type	(GtkWidget *widget) G_GNUC_INTERNAL;
+
+GtkWidget	*drag_image_new			(void) G_GNUC_INTERNAL G_GNUC_MALLOC;
+
+// TODO: Make these properties?
+
+void drag_image_get_minimum_image_size(DragImage *image, int *width, int *height);
+void drag_image_set_minimum_image_size(DragImage *image, int width, int height);
+
+#ifdef __cplusplus
+/**
+ * Set the rp_image for this image.
+ *
+ * NOTE: The rp_image pointer is stored and used if necessary.
+ * Make sure to call this function with nullptr before deleting
+ * the rp_image object.
+ *
+ * NOTE 2: If animated icon data is specified, that supercedes
+ * the individual rp_image.
+ *
+ * @param image DragImage
+ * @param img rp_image, or nullptr to clear.
+ * @return True on success; false on error or if clearing.
+ */
+bool drag_image_set_rp_image(DragImage *image, const LibRpTexture::rp_image *img);
+
+/**
+ * Set the icon animation data for this image.
+ *
+ * NOTE: The iconAnimData pointer is stored and used if necessary.
+ * Make sure to call this function with nullptr before deleting
+ * the IconAnimData object.
+ *
+ * NOTE 2: If animated icon data is specified, that supercedes
+ * the individual rp_image.
+ *
+ * @param image DragImage
+ * @param iconAnimData IconAnimData, or nullptr to clear.
+ * @return True on success; false on error or if clearing.
+ */
+bool drag_image_set_icon_anim_data(DragImage *image, const LibRpBase::IconAnimData *iconAnimData);
+
+/**
+ * Clear the rp_image and iconAnimData.
+ * This will stop the animation timer if it's running.
+ * @param image DragImage
+ */
+void drag_image_clear(DragImage *image);
+
+/**
+ * Start the animation timer.
+ * @param image DragImage
+ */
+void drag_image_start_anim_timer(DragImage *image);
+
+/**
+ * Stop the animation timer.
+ * @param image DragImage
+ */
+void drag_image_stop_anim_timer(DragImage *image);
+
+/**
+ * Is the animation timer running?
+ * @param image DragImage
+ * @return True if running; false if not.
+ */
+bool drag_image_is_anim_timer_running(DragImage *image);
+
+/**
+ * Reset the animation frame.
+ * This does NOT update the animation frame.
+ * @param image DragImage
+ */
+void drag_image_reset_anim_frame(DragImage *image);
+
+#endif /* __cplusplus */
+
+G_END_DECLS
+
+#endif /* __ROMPROPERTIES_GTK_DRAGIMAGE_HPP__ */

src/gtk/RomDataView.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (GTK+ common)                      *
  * RomDataView.hpp: RomData viewer widget.                                 *
  *                                                                         *
- * Copyright (c) 2017 by David Korth.                                      *
+ * Copyright (c) 2017-2020 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 

commit fec49fd0c7899a1aba2420a4ef67d2cc59d2086c

[gtk] RomDataView: unmap() should stop the animation timer, not start it (again).
This fixes a regression from commit 19d4e8dc06740c9154d01d1e527fa00012a8d25b.
([gtk] Split the icon/banner code from RomDataView into DragImage.)

Affects release versions: v1.5, v1.6, v1.6.1, v1.6.2

src/gtk/RomDataView.cpp
@@ -1963,7 +1963,7 @@ rom_data_view_unmap_signal_handler(RomDataView	*page,
 				   gpointer	 user_data)
 {
 	RP_UNUSED(user_data);
-	drag_image_start_anim_timer(DRAG_IMAGE(page->imgIcon));
+	drag_image_stop_anim_timer(DRAG_IMAGE(page->imgIcon));
 }
 
 /**