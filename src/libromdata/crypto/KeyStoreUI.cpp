GerbilSoft/rom-properties

commit 97dbed071686933165dc40694d09d86e040308db

[libromdata] crypto: Added Nintendo 3DS SPI boot keys.
Not used yet, but these will eventually be used to determine ARM9 homebrew
titles used for SPI boot.

SPI boot has an extra layer of AES-128-CBC encryption compared to regular
NAND boot. This encryption is basically useless, since it was discovered
at the same time as sighax.

src/libromdata/crypto/KeyStoreUI.cpp
@@ -1272,8 +1272,11 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 
 	// Key addresses and indexes.
 	static const KeyStoreUIPrivate::KeyBinAddress keyBinAddress[] = {
+		{0xD720, N3DSVerifyKeys::Key_Retail_SpiBoot},
 		{0x59D0, N3DSVerifyKeys::Key_Retail_Slot0x2CKeyX},
 		{0x5A20, N3DSVerifyKeys::Key_Retail_Slot0x3DKeyX},
+
+		{0xD740, N3DSVerifyKeys::Key_Debug_SpiBoot},
 		{0x5DD0, N3DSVerifyKeys::Key_Debug_Slot0x2CKeyX},
 		{0x5E20, N3DSVerifyKeys::Key_Debug_Slot0x3DKeyX},
 

src/libromdata/crypto/N3DSVerifyKeys.cpp
@@ -403,6 +403,7 @@ const uint8_t *N3DSVerifyKeys::encryptionVerifyData_static(int keyIdx)
 // Verification key names.
 const char *const N3DSVerifyKeys::EncryptionKeyNames[Key_Max] = {
 	// Retail
+	"ctr-spi-boot",
 	"ctr-Slot0x18KeyX",
 	"ctr-Slot0x1BKeyX",
 	"ctr-Slot0x25KeyX",
@@ -422,6 +423,7 @@ const char *const N3DSVerifyKeys::EncryptionKeyNames[Key_Max] = {
 	"ctr-Slot0x3DKeyNormal-5",
 
 	// Debug
+	"ctr-dev-spi-boot",
 	"ctr-dev-FixedCryptoKey",
 	"ctr-dev-Slot0x18KeyX",
 	"ctr-dev-Slot0x1BKeyX",
@@ -445,6 +447,10 @@ const char *const N3DSVerifyKeys::EncryptionKeyNames[Key_Max] = {
 // Verification key data.
 const uint8_t N3DSVerifyKeys::EncryptionKeyVerifyData[Key_Max][16] = {
 	/** Retail **/
+
+	// Key_Retail_SpiBoot
+	{0xCB,0x41,0xA2,0x74,0xD8,0x51,0x3A,0x38,
+	 0x9A,0x4A,0xBB,0x2E,0x87,0x2C,0xB8,0xB9},
 
 	// Key_Retail_Slot0x18KeyX
 	{0xE6,0x2E,0x52,0x4A,0x3A,0x17,0x28,0xC8,
@@ -507,6 +513,10 @@ const uint8_t N3DSVerifyKeys::EncryptionKeyVerifyData[Key_Max][16] = {
 	 0x92,0xF6,0x60,0x5A,0x93,0x0B,0x17,0x2E},
 
 	/** Debug **/
+
+	// Key_Debug_SpiBoot
+	{0xDD,0xB7,0xA0,0x17,0x55,0xB2,0x84,0xB8,
+	 0x7A,0x65,0xD5,0x64,0x10,0x5E,0x07,0x99},
 
 	// Key_Debug_FixedCryptoKey
 	{0x1E,0x95,0x82,0xCD,0x65,0x2A,0xE3,0x3F,

src/libromdata/crypto/N3DSVerifyKeys.hpp
@@ -98,6 +98,7 @@ class N3DSVerifyKeys
 		// Encryption key indexes.
 		enum EncryptionKeys {
 			// Retail
+			Key_Retail_SpiBoot,
 			Key_Retail_Slot0x18KeyX,
 			Key_Retail_Slot0x1BKeyX,
 			Key_Retail_Slot0x25KeyX,
@@ -117,6 +118,7 @@ class N3DSVerifyKeys
 			Key_Retail_Slot0x3DKeyNormal_5,
 
 			// Debug
+			Key_Debug_SpiBoot,
 			Key_Debug_FixedCryptoKey,
 			Key_Debug_Slot0x18KeyX,
 			Key_Debug_Slot0x1BKeyX,

commit 32417ad8d5ff512c31cadb34ed610e594e9dfdea

[libromdata] KeyStoreUI: Fix addresses of SPI boot keys.
We're reading the *second* half of the boot ROM, so the addresses
need to be offset by 0x8000. Otherwise, an assertion failure occurs.

In the release build, this simply means the boot keys won't be loaded
properly, and it might affect loading other keys.

This fixes a regression from commit 97dbed071686933165dc40694d09d86e040308db.
([libromdata] crypto: Added Nintendo 3DS SPI boot keys.)

src/libromdata/crypto/KeyStoreUI.cpp
@@ -1274,11 +1274,11 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 
 	// Key addresses and indexes.
 	static const KeyStoreUIPrivate::KeyBinAddress keyBinAddress[] = {
-		{0xD720, N3DSVerifyKeys::Key_Retail_SpiBoot},
+		{0x5720, N3DSVerifyKeys::Key_Retail_SpiBoot},
 		{0x59D0, N3DSVerifyKeys::Key_Retail_Slot0x2CKeyX},
 		{0x5A20, N3DSVerifyKeys::Key_Retail_Slot0x3DKeyX},
 
-		{0xD740, N3DSVerifyKeys::Key_Debug_SpiBoot},
+		{0x5740, N3DSVerifyKeys::Key_Debug_SpiBoot},
 		{0x5DD0, N3DSVerifyKeys::Key_Debug_Slot0x2CKeyX},
 		{0x5E20, N3DSVerifyKeys::Key_Debug_Slot0x3DKeyX},
 