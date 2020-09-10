GerbilSoft/rom-properties

commit e60d3f2051980dd18287ac99e60bd2e12b852479

Removed a bunch of old TODOs and some other cleanup.
[gtk3] RpNautilusPlugin: Renamed the types to "*Interface" instead
of "Iface". This matches current versions of Nautilus.

[librpfile] RpFile: gzseek() doesn't set errno, but the underlying
stdio calls do.

src/win32/RP_ShellPropSheetExt.cpp
@@ -785,7 +785,7 @@ int RP_ShellPropSheetExt_Private::initString(_In_ HWND hDlg, _In_ HWND hWndTab,
 		if (field.type != RomFields::RFT_STRING)
 			return 0;
 
-		// TODO: NULL string == empty string?
+		// NULL string == empty string
 		if (field.data.str) {
 			str_nl = LibWin32Common::unix2dos(U82T_s(*(field.data.str)), &lf_count);
 		}
@@ -2160,8 +2160,9 @@ void RP_ShellPropSheetExt_Private::updateMulti(uint32_t user_lc)
 			const uint32_t lc = *iter;
 			const char *lc_str = SystemRegion::getLocalizedLanguageName(lc);
 			if (lc_str) {
-				// FIXME: std::move might be redundant here...
-				vec_lc_str.emplace_back(std::move(U82T_c(lc_str)));
+				// NOTE: U82T_c() returns c_str(), which prevents
+				// the std::move() optimization from working.
+				vec_lc_str.emplace_back(utf8_to_utf16(lc_str));
 			} else {
 				// Invalid language code.
 				tstring s_lc;

src/gtk/RomDataView.cpp
@@ -1783,13 +1783,10 @@ rom_data_view_create_options_button(RomDataView *page)
 	gtk_menu_button_set_direction(GTK_MENU_BUTTON(page->btnOptions), GTK_ARROW_UP);
 #else /* !USE_GTK_MENU_BUTTON */
 	// Use plain old GtkButton.
-	// TODO: Menu functionality.
 	page->btnOptions = gtk_button_new();
 #endif /* USE_GTK_MENU_BUTTON */
 	gtk_container_add(GTK_CONTAINER(page->btnOptions), hboxOptions);
 
-	// TODO: Connect the signal.
-	// TODO: Submenu.
 	gtk_dialog_add_action_widget(GTK_DIALOG(parent), page->btnOptions, GTK_RESPONSE_NONE);
 	gtk_widget_hide(page->btnOptions);
 	assert(page->btnOptions != nullptr);
@@ -2780,6 +2777,4 @@ menuOptions_triggered_signal_handler(GtkMenuItem *menuItem,
 #endif /* AUTO_TIMEOUT_MESSAGEWIDGET */
 		}
 	}
-
-	// TODO: RomOps
 }

src/gtk/gtk3/RpNautilusPlugin.h
@@ -16,14 +16,14 @@
 G_BEGIN_DECLS;
 
 // Opaque structs.
-struct _NautilusFileInfoIface;
-typedef struct _NautilusFileInfoIface NautilusFileInfoIface;
+struct _NautilusFileInfoInterface;
+typedef struct _NautilusFileInfoInterface NautilusFileInfoInterface;
 
 struct _NautilusFileInfo;
 typedef struct _NautilusFileInfo NautilusFileInfo;
 
-struct _NautilusPropertyPageProviderIface;
-typedef struct _NautilusPropertyPageProviderIface NautilusPropertyPageProviderIface;
+struct _NautilusPropertyPageProviderInterface;
+typedef struct _NautilusPropertyPageProviderInterface NautilusPropertyPageProviderInterface;
 
 struct _NautilusPropertyPageProvider;
 typedef struct _NautilusPropertyPageProvider NautilusPropertyPageProvider;
@@ -54,12 +54,12 @@ extern PFN_NAUTILUS_PROPERTY_PAGE_NEW pfn_nautilus_property_page_new;
 #define NAUTILUS_TYPE_FILE_INFO				(pfn_nautilus_file_info_get_type ())
 #define NAUTILUS_FILE_INFO(obj)				(G_TYPE_CHECK_INSTANCE_CAST((obj), NAUTILUS_TYPE_FILE_INFO, NautilusFileInfo))
 #define NAUTILUS_IS_FILE_INFO(obj)			(G_TYPE_CHECK_INSTANCE_TYPE((obj), NAUTILUS_TYPE_FILE_INFO))
-#define NAUTILUS_FILE_INFO_GET_IFACE(obj)		(G_TYPE_INSTANCE_GET_INTERFACE((obj),  NAUTILUS_TYPE_FILE_INFO, NautilusFileInfoIface))
+#define NAUTILUS_FILE_INFO_GET_IFACE(obj)		(G_TYPE_INSTANCE_GET_INTERFACE((obj),  NAUTILUS_TYPE_FILE_INFO, NautilusFileInfoInterface))
 
 #define NAUTILUS_TYPE_PROPERTY_PAGE_PROVIDER		(pfn_nautilus_property_page_provider_get_type ())
 #define NAUTILUS_PROPERTY_PAGE_PROVIDER(obj)		(G_TYPE_CHECK_INSTANCE_CAST((obj), NAUTILUS_TYPE_PROPERTY_PAGE_PROVIDER, NautilusPropertyPageProvider))
 #define NAUTILUS_IS_PROPERTY_PAGE_PROVIDER(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), NAUTILUS_TYPE_PROPERTY_PAGE_PROVIDER))
-#define NAUTILUS_PROPERTY_PAGE_PROVIDER_GET_IFACE(obj)	(G_TYPE_INSTANCE_GET_INTERFACE((obj),  NAUTILUS_TYPE_PROPERTY_PAGE_PROVIDER, NautilusPropertyPageProviderIface))
+#define NAUTILUS_PROPERTY_PAGE_PROVIDER_GET_IFACE(obj)	(G_TYPE_INSTANCE_GET_INTERFACE((obj),  NAUTILUS_TYPE_PROPERTY_PAGE_PROVIDER, NautilusPropertyPageProviderInterface))
 
 G_END_DECLS;
 

src/gtk/gtk3/RpNautilusProvider.cpp
@@ -22,18 +22,16 @@
 
 #include "../RomDataView.hpp"
 
-// NautilusPropertyPageProviderIface definition.
-// TODO: Make sure it's identical to caja and nemo.
-// TODO: Rename from Iface to Interface? (latest Nautilus does this)
+// NautilusPropertyPageProviderInterface definition.
 extern "C"
-struct _NautilusPropertyPageProviderIface {
+struct _NautilusPropertyPageProviderInterface {
 	GTypeInterface g_iface;
 
 	GList *(*get_pages) (NautilusPropertyPageProvider *provider,
 	                     GList                        *files);
 };
 
-static void   rp_nautilus_provider_page_provider_init	(NautilusPropertyPageProviderIface	*iface);
+static void   rp_nautilus_provider_page_provider_init	(NautilusPropertyPageProviderInterface	*iface);
 static GList *rp_nautilus_provider_get_pages		(NautilusPropertyPageProvider		*provider,
 							 GList					*files);
 
@@ -93,7 +91,7 @@ rp_nautilus_provider_init(RpNautilusProvider *sbr_provider)
 }
 
 static void
-rp_nautilus_provider_page_provider_init(NautilusPropertyPageProviderIface *iface)
+rp_nautilus_provider_page_provider_init(NautilusPropertyPageProviderInterface *iface)
 {
 	iface->get_pages = rp_nautilus_provider_get_pages;
 }

src/kde/RomDataView.cpp
@@ -389,7 +389,6 @@ void RomDataViewPrivate::createOptionsButton(void)
 	}
 
 	// Create the menu and (for Qt4) signal mapper.
-	// TODO: Signals.
 	menuOptions = new QMenu(s_options, q);
 	btnOptions->setMenu(menuOptions);
 

src/libromdata/Audio/SNDH.cpp
@@ -416,7 +416,6 @@ SNDHPrivate::TagData SNDHPrivate::parseTags(void)
 			}
 
 			case 'FLAG': {
-				// TODO: Optimize this!
 				// TODO: This is non-standard.
 				// Observed variants: (after the tag)
 				// - Two bytes, and a NULL terminator.

src/libromdata/Console/Xbox360_XDBF.cpp
@@ -1043,6 +1043,7 @@ int Xbox360_XDBF_Private::addFields_avatarAwards(void)
 		const uint16_t unlocked_desc_id = be16_to_cpu(p->unlocked_desc_id);
 
 		// TODO: Localized numeric formatting?
+		// FIXME: Should this be decimal instead of hex?
 		char s_avatar_award_id[16];
 		snprintf(s_avatar_award_id, sizeof(s_avatar_award_id), "%04X", be16_to_cpu(p->avatar_award_id));
 

src/libromdata/Console/Xbox360_XEX.cpp
@@ -301,8 +301,9 @@ const XEX2_Optional_Header_Tbl *Xbox360_XEX_Private::getOptHdrTblEntry(uint32_t 
 #endif /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
 
 	// Search for the header.
-	// TODO: Can we assume that the optional headers are sorted
-	// and use a binary search?
+	// NOTE: The optional headers are *usually* sorted, but we
+	// can't depend on that always being the case, since someone
+	// might do something screwy.
 	auto iter = std::find_if(optHdrTbl.cbegin(), optHdrTbl.cend(),
 		[header_id](const XEX2_Optional_Header_Tbl &tbl) -> bool {
 			return (tbl.header_id == header_id);

commit 6aa8e9992699ab90aed9a9e1eb073e3cbb29568e

[win32] RP_ShellPropSheetExt: Can't use utf8_to_utf16() here.
vec_lc_str is tstring, not u16string.

This fixes a regression from commit e60d3f2051980dd18287ac99e60bd2e12b852479.
(Removed a bunch of old TODOs and some other cleanup.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -2160,9 +2160,7 @@ void RP_ShellPropSheetExt_Private::updateMulti(uint32_t user_lc)
 			const uint32_t lc = *iter;
 			const char *lc_str = SystemRegion::getLocalizedLanguageName(lc);
 			if (lc_str) {
-				// NOTE: U82T_c() returns c_str(), which prevents
-				// the std::move() optimization from working.
-				vec_lc_str.emplace_back(utf8_to_utf16(lc_str));
+				vec_lc_str.emplace_back(U82T_c(lc_str));
 			} else {
 				// Invalid language code.
 				tstring s_lc;