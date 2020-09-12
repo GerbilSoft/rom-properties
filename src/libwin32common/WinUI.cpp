GerbilSoft/rom-properties

commit 55e0025aa7bec4cd7ff3a82aa51b9a99d026a429

[libwin32common] WinUI: Use IFileDialog if available.
No major difference UI-wise, but IFileDialog supports paths longer than
MAX_PATH on Windows 10 v1607 and later.

src/libwin32common/WinUI.cpp
@@ -15,16 +15,24 @@
 #include <commdlg.h>
 #include <shlwapi.h>
 
+#include <comdef.h>
+#include <shobjidl.h>
+
 // C++ includes.
 #include <algorithm>
 #include <memory>
 #include <string>
 #include <unordered_set>
+#include <vector>
 using std::string;
 using std::unique_ptr;
 using std::unordered_set;
 using std::tstring;
 using std::vector;
+
+// COM smart pointer typedefs.
+_COM_SMARTPTR_TYPEDEF(IFileDialog, IID_IFileDialog);
+_COM_SMARTPTR_TYPEDEF(IShellItem, IID_IShellItem);
 
 namespace LibWin32Common {
 
@@ -340,6 +348,179 @@ LRESULT CALLBACK SingleLineEditProc(
 	return DefSubclassProc(hWnd, uMsg, wParam, lParam);
 }
 
+#ifdef UNICODE
+/**
+ * Get a filename using IFileDialog.
+ * @param ts_ret	[out] Output filename. (Empty if none.)
+ * @param bSave		[in] True for save; false for open.
+ * @param hWnd		[in] Owner.
+ * @param dlgTitle	[in] Dialog title.
+ * @param filterSpec	[in] Filter specification. (pipe-delimited)
+ * @param origFilename	[in,opt] Starting filename.
+ * @return 0 on success; HRESULT on error.
+ */
+static inline HRESULT getFileName_int_IFileDialog(tstring &ts_ret, bool bSave, HWND hWnd,
+	const TCHAR *dlgTitle, const char *filterSpec, const TCHAR *origFilename)
+{
+	IFileDialogPtr pFileDlg;
+	HRESULT hr = CoCreateInstance(
+		bSave ? CLSID_FileSaveDialog : CLSID_FileOpenDialog,
+		nullptr, CLSCTX_ALL, IID_PPV_ARGS(&pFileDlg));
+	if (FAILED(hr)) {
+		// Assume the class isn't available.
+		return REGDB_E_CLASSNOTREG;
+	}
+
+	// Convert the filter spec to COMDLG_FILTERSPEC.
+	vector<COMDLG_FILTERSPEC> v_cdfs;
+
+	// Convert '|' characters to NULL bytes.
+	// _tcstok_s() can do this for us.
+	tstring tmpfilter = U82T_c(filterSpec);
+	TCHAR *saveptr = nullptr;
+	TCHAR *token = _tcstok_s(&tmpfilter[0], _T("|"), &saveptr);
+	while (token != nullptr) {
+		COMDLG_FILTERSPEC cdfs;
+
+		// Separator 1: Between display name and pattern.
+		// (_tcstok_s() call was done in the previous iteration.)
+		assert(token != nullptr);
+		if (!token) {
+			// Missing token...
+			return E_INVALIDARG;
+		}
+		cdfs.pszName = token;
+
+		// Separator 2: Between pattern and MIME types.
+		token = _tcstok_s(nullptr, _T("|"), &saveptr);
+		assert(token != nullptr);
+		if (!token) {
+			// Missing token...
+			return E_INVALIDARG;
+		}
+		cdfs.pszSpec = token;
+		v_cdfs.emplace_back(cdfs);
+
+		// Separator 3: Between MIME types and the next display name.
+		// NOTE: May be missing if this is the end of the string
+		// and a MIME type isn't set.
+		// NOTE: Not used by Qt.
+		token = _tcstok_s(nullptr, _T("|"), &saveptr);
+
+		// Next token.
+		token = _tcstok_s(nullptr, _T("|"), &saveptr);
+	}
+
+	if (v_cdfs.empty()) {
+		// No filter spec...
+		return E_INVALIDARG;
+	}
+
+	hr = pFileDlg->SetFileTypes(static_cast<int>(v_cdfs.size()), v_cdfs.data());
+	v_cdfs.clear();
+	if (FAILED(hr))
+		return hr;
+
+	// Check if the original filename is a directory or a file.
+	if (origFilename && origFilename[0] != _T('\0')) {
+		// NOTE: SHCreateItemFromParsingName() was added in Vista, so we
+		// need to use GetProcAddress().
+		typedef HRESULT(STDAPICALLTYPE* PFNSHCREATEITEMFROMPARSINGNAME)(
+			PCWSTR pszPath, IBindCtx* pbc, REFIID riid, void** ppv);
+		HMODULE hShell32 = GetModuleHandle(_T("shell32"));
+		assert(hShell32 != nullptr);
+		if (!hShell32)
+			return E_FAIL;
+
+		PFNSHCREATEITEMFROMPARSINGNAME pfnSHCreateItemFromParsingName =
+			reinterpret_cast<PFNSHCREATEITEMFROMPARSINGNAME>(
+				GetProcAddress(hShell32, "SHCreateItemFromParsingName"));
+		assert(pfnSHCreateItemFromParsingName != nullptr);
+		if (!pfnSHCreateItemFromParsingName)
+			return E_FAIL;
+
+		DWORD dwAttrs = GetFileAttributes(origFilename);
+		if (dwAttrs != INVALID_FILE_ATTRIBUTES) {
+			IShellItemPtr pFolder;
+			if (dwAttrs & FILE_ATTRIBUTE_DIRECTORY) {
+				// It's a directory.
+				hr = pfnSHCreateItemFromParsingName(origFilename, nullptr, IID_PPV_ARGS(&pFolder));
+				if (FAILED(hr))
+					return hr;
+			} else {
+				// It's a filename. Get the directory portion,
+				// for IFileDialog::SetFolder(), then set the
+				// filename portion with IFileDialog::SetFileName().
+				const TCHAR *bs = _tcsrchr(origFilename, _T('\\'));
+				assert(bs != nullptr);
+				if (!bs) {
+					// No backslash...
+					return E_INVALIDARG;
+				}
+
+				// Set the filename.
+				hr = pFileDlg->SetFileName(bs + 1);
+				if (FAILED(hr))
+					return hr;
+
+				// Get the folder portion.
+				tstring ts_folder(origFilename, bs - origFilename);
+				hr = pfnSHCreateItemFromParsingName(ts_folder.c_str(),
+					nullptr, IID_PPV_ARGS(&pFolder));
+				if (FAILED(hr))
+					return hr;
+			}
+
+			// Set the directory.
+			hr = pFileDlg->SetFolder(pFolder);
+			if (FAILED(hr))
+				return hr;
+		}
+	}
+
+	// TODO: Use SetDefaultFolder instead?
+	// FIXME: Need to convert tsKeyFileDir to IShellItem.
+	// SHCreateItemFromParsingName()?
+	// Alternatively, rely on Windows' built-in folder memory.
+	/*
+	hr = pFileDlg->SetFolder(pFolder);
+	if (FAILED(hr))
+		return hr;
+	*/
+	hr = pFileDlg->SetTitle(dlgTitle);
+	if (FAILED(hr))
+		return hr;
+	hr = pFileDlg->SetOptions(bSave
+		? FOS_DONTADDTORECENT | FOS_OVERWRITEPROMPT
+		: FOS_DONTADDTORECENT | FOS_FILEMUSTEXIST);
+	if (FAILED(hr))
+		return hr;
+	hr = pFileDlg->Show(hWnd);
+	if (FAILED(hr)) {
+		// User cancelled the dialog box.
+		return hr;
+	}
+
+	IShellItemPtr pShellItem;
+	hr = pFileDlg->GetResult(&pShellItem);
+	if (FAILED(hr)) {
+		// Unable to get the result...
+		return hr;
+	}
+	PWSTR pszFilePath;
+	hr = pShellItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+	if (FAILED(hr)) {
+		// Unable to get the filename.
+		return hr;
+	}
+
+	// Filename retrieved.
+	ts_ret = pszFilePath;
+	CoTaskMemFree(pszFilePath);
+	return S_OK;
+}
+#endif /* UNICODE */
+
 /**
  * Convert an RP file dialog filter to Win32.
  *
@@ -435,40 +616,44 @@ static tstring rpFileDialogFilterToWin32(const char *filter)
  * @param origFilename	[in,opt] Starting filename.
  * @return Filename, or empty string on error.
  */
-static tstring getFileName_int(bool bSave, HWND hWnd, const TCHAR *dlgTitle, const char *filterSpec, const TCHAR *origFilename)
+static tstring getFileName_int(bool bSave, HWND hWnd,
+	const TCHAR *dlgTitle, const char *filterSpec, const TCHAR *origFilename)
 {
 	assert(dlgTitle != nullptr);
 	assert(filterSpec != nullptr);
 	tstring ts_ret;
 
-	if (0) {
-		// TODO: Implement IFileOpenDialog and IFileSaveDialog.
-		// This should support >MAX_PATH on Windows 10 v1607 and later.
-		// Reference: https://msdn.microsoft.com/en-us/library/windows/desktop/bb776913%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
-		// Requires the following:
-		// - -DWINVER=0x0600
-		// - IFileDialogEvents object
-	} else {
-		// GetOpenFileName() / GetSaveFileName()
-		tstring ts_filterSpec = rpFileDialogFilterToWin32(filterSpec);
+#ifdef UNICODE
+	// Try IFileDialog first. (Unicode only)
+	HRESULT hr = getFileName_int_IFileDialog(ts_ret, bSave,
+		hWnd, dlgTitle, filterSpec, origFilename);
+	if (hr != REGDB_E_CLASSNOTREG) {
+		// IFileDialog succeeded.
+		return ts_ret;
+	}
+#endif /* UNICODE */
 
-		TCHAR tfilename[MAX_PATH];
-		tfilename[0] = 0;
+	// GetOpenFileName() / GetSaveFileName()
+	tstring ts_filterSpec = rpFileDialogFilterToWin32(filterSpec);
 
-		OPENFILENAME ofn;
-		memset(&ofn, 0, sizeof(ofn));
-		ofn.lStructSize = sizeof(ofn);
-		ofn.hwndOwner = hWnd;
-		ofn.lpstrFilter = ts_filterSpec.c_str();
-		ofn.lpstrCustomFilter = nullptr;
-		ofn.lpstrFile = tfilename;
-		ofn.nMaxFile = _countof(tfilename);
-		ofn.lpstrTitle = dlgTitle;
+	TCHAR tfilename[MAX_PATH];
+	tfilename[0] = 0;
 
-		// Check if the original filename is a directory or a file.
-		if (origFilename) {
-			DWORD dwAttrs = GetFileAttributes(origFilename);
-			if (dwAttrs != INVALID_FILE_ATTRIBUTES && (dwAttrs & FILE_ATTRIBUTE_DIRECTORY)) {
+	OPENFILENAME ofn;
+	memset(&ofn, 0, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hWnd;
+	ofn.lpstrFilter = ts_filterSpec.c_str();
+	ofn.lpstrCustomFilter = nullptr;
+	ofn.lpstrFile = tfilename;
+	ofn.nMaxFile = _countof(tfilename);
+	ofn.lpstrTitle = dlgTitle;
+
+	// Check if the original filename is a directory or a file.
+	if (origFilename && origFilename[0] != _T('\0')) {
+		DWORD dwAttrs = GetFileAttributes(origFilename);
+		if (dwAttrs != INVALID_FILE_ATTRIBUTES) {
+			if (dwAttrs & FILE_ATTRIBUTE_DIRECTORY) {
 				// It's a directory.
 				ofn.lpstrInitialDir = origFilename;
 			} else {
@@ -477,24 +662,25 @@ static tstring getFileName_int(bool bSave, HWND hWnd, const TCHAR *dlgTitle, con
 				ofn.lpstrInitialDir = nullptr;
 				_tcscpy_s(tfilename, _countof(tfilename), origFilename);
 			}
-		}
-
-		// TODO: Make OFN_DONTADDTORECENT customizable?
-		BOOL bRet;
-		if (!bSave) {
-			ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
-			bRet = GetOpenFileName(&ofn);
 		} else {
-			ofn.Flags = OFN_DONTADDTORECENT | OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY;
-			bRet = GetSaveFileName(&ofn);
-		}
-
-		if (bRet != 0 && tfilename[0] != _T('\0')) {
-			ts_ret = tfilename;
+			// Invalid filename.
+			ofn.lpstrInitialDir = nullptr;
 		}
 	}
 
-	// Return the filename.
+	// TODO: Make OFN_DONTADDTORECENT customizable?
+	BOOL bRet;
+	if (!bSave) {
+		ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY;
+		bRet = GetOpenFileName(&ofn);
+	} else {
+		ofn.Flags = OFN_DONTADDTORECENT | OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY;
+		bRet = GetSaveFileName(&ofn);
+	}
+
+	if (bRet != 0 && tfilename[0] != _T('\0')) {
+		ts_ret = tfilename;
+	}
 	return ts_ret;
 }
 

commit e737dadc71b81a05b4532ec149db77c64b823eed

[libwin32common] WinUI: Fix default filename when saving files.
dwFileAttributes will be INVALID_FILE_ATTRIBUTES if the file doesn't
exist, which can happen when saving files.

This fixes a regression from commit 55e0025aa7bec4cd7ff3a82aa51b9a99d026a429.
([libwin32common] WinUI: Use IFileDialog if available.)

src/libwin32common/WinUI.cpp
@@ -440,24 +440,23 @@ static inline HRESULT getFileName_int_IFileDialog(tstring &ts_ret, bool bSave, H
 			return E_FAIL;
 
 		DWORD dwAttrs = GetFileAttributes(origFilename);
-		if (dwAttrs != INVALID_FILE_ATTRIBUTES) {
-			IShellItemPtr pFolder;
-			if (dwAttrs & FILE_ATTRIBUTE_DIRECTORY) {
-				// It's a directory.
-				hr = pfnSHCreateItemFromParsingName(origFilename, nullptr, IID_PPV_ARGS(&pFolder));
+		IShellItemPtr pFolder;
+		if (dwAttrs != INVALID_FILE_ATTRIBUTES && (dwAttrs & FILE_ATTRIBUTE_DIRECTORY)) {
+			// It's a directory.
+			hr = pfnSHCreateItemFromParsingName(origFilename, nullptr, IID_PPV_ARGS(&pFolder));
+			if (FAILED(hr))
+				return hr;
+		} else {
+			// It's a filename, or invalid. Get the directory portion,
+			// for IFileDialog::SetFolder(), then set the
+			// filename portion with IFileDialog::SetFileName().
+			const TCHAR *bs = _tcsrchr(origFilename, _T('\\'));
+			if (!bs) {
+				// No backslash. Use the whole filename.
+				hr = pFileDlg->SetFileName(origFilename);
 				if (FAILED(hr))
 					return hr;
 			} else {
-				// It's a filename. Get the directory portion,
-				// for IFileDialog::SetFolder(), then set the
-				// filename portion with IFileDialog::SetFileName().
-				const TCHAR *bs = _tcsrchr(origFilename, _T('\\'));
-				assert(bs != nullptr);
-				if (!bs) {
-					// No backslash...
-					return E_INVALIDARG;
-				}
-
 				// Set the filename.
 				hr = pFileDlg->SetFileName(bs + 1);
 				if (FAILED(hr))
@@ -652,19 +651,14 @@ static tstring getFileName_int(bool bSave, HWND hWnd,
 	// Check if the original filename is a directory or a file.
 	if (origFilename && origFilename[0] != _T('\0')) {
 		DWORD dwAttrs = GetFileAttributes(origFilename);
-		if (dwAttrs != INVALID_FILE_ATTRIBUTES) {
-			if (dwAttrs & FILE_ATTRIBUTE_DIRECTORY) {
-				// It's a directory.
-				ofn.lpstrInitialDir = origFilename;
-			} else {
-				// Not a directory, or invalid.
-				// Assume it's a filename.
-				ofn.lpstrInitialDir = nullptr;
-				_tcscpy_s(tfilename, _countof(tfilename), origFilename);
-			}
+		if (dwAttrs != INVALID_FILE_ATTRIBUTES && (dwAttrs & FILE_ATTRIBUTE_DIRECTORY)) {
+			// It's a directory.
+			ofn.lpstrInitialDir = origFilename;
 		} else {
-			// Invalid filename.
+			// Not a directory, or invalid.
+			// Assume it's a filename.
 			ofn.lpstrInitialDir = nullptr;
+			_tcscpy_s(tfilename, _countof(tfilename), origFilename);
 		}
 	}
 