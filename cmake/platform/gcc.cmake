GerbilSoft/rom-properties

commit 2b4ada24e4b4221f515ab7c0e8776fc344e5de58

[librpbase] IconAnimData: Changed to a reference-counted class.
Updated all RomData subclasses and UI frontends for this change.

Animated icons now work correctly in UI frontends after e.g. extracting
an SRL from a DSi TAD package or Nintendo 3DS CIA.

cmake/platform/gcc.cmake
@@ -198,12 +198,12 @@ SET(RP_MODULE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON}")
 # Check for -Og.
 # This flag was added in gcc-4.8, and enables optimizations that
 # don't interfere with debugging.
-CHECK_C_COMPILER_FLAG("-Og" CFLAG_OPTIMIZE_DEBUG)
-IF (CFLAG_OPTIMIZE_DEBUG)
-	SET(CFLAG_OPTIMIZE_DEBUG "-Og")
-ELSE(CFLAG_OPTIMIZE_DEBUG)
+#CHECK_C_COMPILER_FLAG("-Og" CFLAG_OPTIMIZE_DEBUG)
+#IF (CFLAG_OPTIMIZE_DEBUG)
+	#SET(CFLAG_OPTIMIZE_DEBUG "-Og")
+#ELSE(CFLAG_OPTIMIZE_DEBUG)
 	SET(CFLAG_OPTIMIZE_DEBUG "-O0")
-ENDIF(CFLAG_OPTIMIZE_DEBUG)
+#ENDIF(CFLAG_OPTIMIZE_DEBUG)
 
 # Debug/release flags.
 SET(RP_C_FLAGS_DEBUG		"${CFLAG_OPTIMIZE_DEBUG} -ggdb -DDEBUG -D_DEBUG")

src/gtk/DragImage.cpp
@@ -81,6 +81,8 @@ struct _DragImage {
 					PIMGTYPE_destroy(frame);
 				}
 			});
+
+			UNREF(iconAnimData);
 		}
 	};
 	anim_vars *anim;
@@ -322,12 +324,16 @@ drag_image_set_icon_anim_data(DragImage *image, const LibRpBase::IconAnimData *i
 	}
 	auto *const anim = image->anim;
 
+	// NOTE: We're not checking if the image pointer matches the
+	// previously stored image, since the underlying image may
+	// have changed.
+	UNREF_AND_NULL(anim->iconAnimData);
+
 	if (!iconAnimData) {
 		if (anim->tmrIconAnim > 0) {
 			g_source_remove(anim->tmrIconAnim);
 			anim->tmrIconAnim = 0;
 		}
-		anim->iconAnimData = nullptr;
 
 		if (!image->img) {
 			gtk_image_clear(image->imageWidget);
@@ -337,10 +343,7 @@ drag_image_set_icon_anim_data(DragImage *image, const LibRpBase::IconAnimData *i
 		return false;
 	}
 
-	// Don't check if the data pointer matches the
-	// previously stored data, since the underlying
-	// data may have changed.
-	anim->iconAnimData = iconAnimData;
+	anim->iconAnimData = iconAnimData->ref();
 	return drag_image_update_pixmaps(image);
 }
 
@@ -360,7 +363,7 @@ drag_image_clear(DragImage *image)
 			g_source_remove(anim->tmrIconAnim);
 			anim->tmrIconAnim = 0;
 		}
-		anim->iconAnimData = nullptr;
+		UNREF_AND_NULL(anim->iconAnimData);
 	}
 
 	UNREF_AND_NULL(image->img);

src/kde/DragImageLabel.cpp
@@ -91,11 +91,15 @@ bool DragImageLabel::setIconAnimData(const IconAnimData *iconAnimData)
 		m_anim = new anim_vars();
 	}
 
+	// NOTE: We're not checking if the image pointer matches the
+	// previously stored image, since the underlying image may
+	// have changed.
+	UNREF_AND_NULL(m_anim->iconAnimData);
+
 	if (!iconAnimData) {
 		if (m_anim->tmrIconAnim) {
 			m_anim->tmrIconAnim->stop();
 		}
-		m_anim->iconAnimData = nullptr;
 		m_anim->anim_running = false;
 
 		if (!m_img) {
@@ -106,10 +110,7 @@ bool DragImageLabel::setIconAnimData(const IconAnimData *iconAnimData)
 		return false;
 	}
 
-	// Don't check if the data pointer matches the
-	// previously stored data, since the underlying
-	// data may have changed.
-	m_anim->iconAnimData = iconAnimData;
+	m_anim->iconAnimData = iconAnimData->ref();
 	return updatePixmaps();
 }
 
@@ -123,8 +124,8 @@ void DragImageLabel::clearRp(void)
 		if (m_anim->tmrIconAnim) {
 			m_anim->tmrIconAnim->stop();
 		}
-		m_anim->iconAnimData = nullptr;
 		m_anim->anim_running = false;
+		UNREF_AND_NULL(m_anim->iconAnimData);
 	}
 
 	UNREF_AND_NULL(m_img);

src/kde/DragImageLabel.hpp
@@ -173,7 +173,10 @@ class DragImageLabel : public QLabel
 				, tmrIconAnim(nullptr)
 				, last_frame_number(0)
 				, anim_running(false) { }
-			~anim_vars() { delete tmrIconAnim; }
+			~anim_vars() {
+				UNREF(iconAnimData);
+				delete tmrIconAnim;
+			}
 		};
 		anim_vars *m_anim;
 };

src/libromdata/Console/DreamcastSave.cpp
@@ -224,12 +224,7 @@ DreamcastSavePrivate::~DreamcastSavePrivate()
 	UNREF(vmi_file);
 
 	UNREF(img_banner);
-	if (iconAnimData) {
-		for (int i = iconAnimData->count-1; i >= 0; i--) {
-			UNREF(iconAnimData->frames[i]);
-		}
-		delete iconAnimData;
-	}
+	UNREF(iconAnimData);
 }
 
 /**
@@ -1602,6 +1597,9 @@ int DreamcastSave::loadInternalImage(ImageType imageType, const rp_image **pImag
  *
  * Check imgpf for IMGPF_ICON_ANIMATED first to see if this
  * object has an animated icon.
+ *
+ * The retrieved IconAnimData must be ref()'d by the caller if the
+ * caller stores it instead of using it immediately.
  *
  * @return Animated icon data, or nullptr if no animated icon is present.
  */

src/libromdata/Console/GameCubeSave.cpp
@@ -117,12 +117,7 @@ GameCubeSavePrivate::GameCubeSavePrivate(GameCubeSave *q, IRpFile *file)
 GameCubeSavePrivate::~GameCubeSavePrivate()
 {
 	UNREF(img_banner);
-	if (iconAnimData) {
-		for (int i = iconAnimData->count-1; i >= 0; i--) {
-			UNREF(iconAnimData->frames[i]);
-		}
-		delete iconAnimData;
-	}
+	UNREF(iconAnimData);
 }
 
 /**
@@ -1091,6 +1086,9 @@ int GameCubeSave::loadInternalImage(ImageType imageType, const rp_image **pImage
  *
  * Check imgpf for IMGPF_ICON_ANIMATED first to see if this
  * object has an animated icon.
+ *
+ * The retrieved IconAnimData must be ref()'d by the caller if the
+ * caller stores it instead of using it immediately.
  *
  * @return Animated icon data, or nullptr if no animated icon is present.
  */

src/libromdata/Console/PlayStationSave.cpp
@@ -90,12 +90,7 @@ PlayStationSavePrivate::PlayStationSavePrivate(PlayStationSave *q, IRpFile *file
 
 PlayStationSavePrivate::~PlayStationSavePrivate()
 {
-	if (iconAnimData) {
-		for (int i = iconAnimData->count-1; i >= 0; i--) {
-			UNREF(iconAnimData->frames[i]);
-		}
-		delete iconAnimData;
-	}
+	UNREF(iconAnimData);
 }
 
 /**
@@ -618,6 +613,9 @@ int PlayStationSave::loadInternalImage(ImageType imageType, const rp_image **pIm
  *
  * Check imgpf for IMGPF_ICON_ANIMATED first to see if this
  * object has an animated icon.
+ *
+ * The retrieved IconAnimData must be ref()'d by the caller if the
+ * caller stores it instead of using it immediately.
  *
  * @return Animated icon data, or nullptr if no animated icon is present.
  */

src/libromdata/Console/WiiSave.cpp
@@ -585,6 +585,9 @@ int WiiSave::loadInternalImage(ImageType imageType, const rp_image **pImage)
  * Check imgpf for IMGPF_ICON_ANIMATED first to see if this
  * object has an animated icon.
  *
+ * The retrieved IconAnimData must be ref()'d by the caller if the
+ * caller stores it instead of using it immediately.
+ *
  * @return Animated icon data, or nullptr if no animated icon is present.
  */
 const IconAnimData *WiiSave::iconAnimData(void) const

commit 691d6320988a6332ba3a908ae108d3a9c5258f74

[cmake] gcc.cmake: Undo accidentally-committed changes that disabled "-Og".
I set this while testing some refcounting stuff earlier.

This fixes a regression from commit 2b4ada24e4b4221f515ab7c0e8776fc344e5de58.
([librpbase] IconAnimData: Changed to a reference-counted class.)

cmake/platform/gcc.cmake
@@ -198,12 +198,12 @@ SET(RP_MODULE_LINKER_FLAGS_COMMON "${RP_EXE_LINKER_FLAGS_COMMON}")
 # Check for -Og.
 # This flag was added in gcc-4.8, and enables optimizations that
 # don't interfere with debugging.
-#CHECK_C_COMPILER_FLAG("-Og" CFLAG_OPTIMIZE_DEBUG)
-#IF (CFLAG_OPTIMIZE_DEBUG)
-	#SET(CFLAG_OPTIMIZE_DEBUG "-Og")
-#ELSE(CFLAG_OPTIMIZE_DEBUG)
+CHECK_C_COMPILER_FLAG("-Og" CFLAG_OPTIMIZE_DEBUG)
+IF (CFLAG_OPTIMIZE_DEBUG)
+	SET(CFLAG_OPTIMIZE_DEBUG "-Og")
+ELSE(CFLAG_OPTIMIZE_DEBUG)
 	SET(CFLAG_OPTIMIZE_DEBUG "-O0")
-#ENDIF(CFLAG_OPTIMIZE_DEBUG)
+ENDIF(CFLAG_OPTIMIZE_DEBUG)
 
 # Debug/release flags.
 SET(RP_C_FLAGS_DEBUG		"${CFLAG_OPTIMIZE_DEBUG} -ggdb -DDEBUG -D_DEBUG")