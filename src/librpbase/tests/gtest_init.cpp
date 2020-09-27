GerbilSoft/rom-properties

commit 1a92afa95b20da16e48ababbaea5a4a8192c44ce

Added an initial set of achievement icons.
ViewedDebugCryptedFile: KDE Oxygen - categories/application-development

ViewedNonX86PE: Modified version of the IBM PowerPC logo.
https://commons.wikimedia.org/wiki/File:PowerPC_logo.svg

ViewedMegaDriveSKwithSK: Sonic & Knuckles logo from the Sonic Screensaver.
http://info.sonicretro.org/File:SSS_SONIC42.png

[win32] Display the achievement icon instead of a generic ROM icon.
Split the sprite sheet code from RP_ShellPropSheetExt into
RpImageWin32::getSubBitmap().

NOTE: The GDI+ deadlock fixes from the previous commit seems to have fixed
the issue where the achievement popup disappeared when the property dialog
was closed. Maybe we *don't* need to keep track of threads and HWNDs...

TODO:
- Photoshop the S&K logo to Knuckles & Knuckles?
- Add an icon for ViewedBroadOnWADFile.

src/librpbase/Achievements.hpp
@@ -68,6 +68,10 @@ class Achievements
 			Max
 		};
 
+		// Achievements sprite sheet columns/rows.
+		static const unsigned int ACH_SPRITE_SHEET_COLS = 4;
+		static const unsigned int ACH_SPRITE_SHEET_ROWS = 4;
+
 	public:
 		/**
 		 * Notification function.

src/res/CMakeLists.txt
@@ -13,6 +13,10 @@ SET(RESOURCE_LIST
 	flags/flags-16x16.png
 	flags/flags-24x24.png
 	flags/flags-32x32.png
+	ach/ach-16x16.png
+	ach/ach-24x24.png
+	ach/ach-32x32.png
+	ach/ach-64x64.png
 	)
 COMPILE_GRESOURCES(GLIB_RESOURCE_SRC
 	XML_OUT

src/res/ach/ach.qrc
@@ -1,0 +1,7 @@
+<RCC>
+  <qresource prefix="ach">
+    <file>ach-16x16.png</file>
+    <file>ach-24x24.png</file>
+    <file>ach-32x32.png</file>
+  </qresource>
+</RCC>

src/res/ach/src/PowerPC_logo.svg
@@ -1,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="2426.4871"
+   height="432.1076"
+   viewBox="0 0 2426.4871 432.10761"
+   id="svg3353"
+   version="1.1"
+   inkscape:version="0.91 r13725"
+   sodipodi:docname="PowerPC logo.svg">
+  <defs
+     id="defs3355" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="1.1867187"
+     inkscape:cx="1179.1323"
+     inkscape:cy="122.59758"
+     inkscape:document-units="px"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0"
+     units="px"
+     inkscape:window-width="1920"
+     inkscape:window-height="1017"
+     inkscape:window-x="-8"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1" />
+  <metadata
+     id="metadata3358">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(976.45624,-222.85218)">
+    <path
+       d="m -815.24207,222.85218 121.39699,0 c 77.62404,0 169.16463,37.80686 117.42824,165.16994 -51.73639,127.37606 -121.39699,145.2873 -177.12807,145.2873 l -37.80686,0 35.82249,-99.50403 c 39.80421,-1.98437 59.71279,-39.8042 65.66592,-57.70248 5.96609,-17.92421 13.92952,-51.74936 -31.84078,-51.74936 l -11.93218,0 -123.39433,324.39974 -119.42559,0 161.21417,-425.90111 z m 533.3556,101.4884 105.48309,0 -9.94781,123.38137 67.66326,-123.38137 105.48309,0 -5.96609,121.39699 75.62669,-121.39699 111.4362,0 -197.02367,324.41271 -115.4309,0 5.97906,-129.3734 -81.60575,129.3734 -97.51966,0 35.82249,-324.41271 z m 1024.93563,-101.4884 121.4099,0 c 77.6111,0 169.16474,37.80686 117.4153,165.16994 -51.7234,127.37606 -121.384,145.2873 -177.1151,145.2873 l -37.8198,0 35.8225,-99.50403 c 39.8171,-1.98437 59.7128,-39.8042 65.6788,-57.70248 5.9532,-17.92421 13.9296,-51.74936 -31.8407,-51.74936 l -11.9581,0 -123.3684,324.39974 -119.4386,0 161.2142,-425.90111 z m -276.6192,101.4884 -123.3943,324.39974 119.3867,0 47.7806,-123.39433 c 17.9112,-49.75202 69.6606,-79.62139 125.3657,-81.61873 l 49.778,-129.34745 c -23.8903,0 -83.5901,13.92952 -119.4126,55.7181 l 17.9112,-45.7703 -117.4153,0 0,0.013 z m -214.9349,127.37606 -27.872,73.64232 135.3525,0 15.9139,-39.79124 c 23.8903,-67.6892 35.8224,-163.19854 -71.658,-175.14369 -107.4545,-11.93218 -179.0995,89.55622 -195.0263,159.21682 -15.9139,69.6606 -21.893,179.12541 113.4465,179.12541 l 91.5406,0 35.8484,-97.53263 -85.6004,0 c -49.752,0 -73.6294,-17.91124 -51.7364,-77.61106 21.8929,-59.69983 47.7676,-67.65029 57.7154,-65.67889 9.9608,1.99735 23.8903,7.96344 7.9505,43.78593 l -25.8747,0 0,-0.013 z m 1135.81414,-222.43146 -13.2551,0 0,-4.44862 31.8278,0 0,4.44862 -13.2551,0 0,35.35558 -5.3176,0 0,-35.35558 z m 23.0732,-4.44862 7.2501,0 12.5418,33.44902 12.6066,-33.44902 7.2501,0 0,39.8042 -5.0193,0 0,-33.11181 -0.1297,0 -12.4121,33.11181 -4.5264,0 -12.438,-33.11181 -0.1038,0 0,33.11181 -5.0193,0 0,-39.8042 z M -437.75709,493.64788 c -15.93984,41.08821 -39.22057,70.29612 -58.06564,66.54786 -17.13305,-3.44996 -7.71701,-41.91828 1.59529,-66.82022 l -110.52833,0.0259 c -19.73998,72.50098 -30.60865,155.76686 88.03875,161.33089 127.36309,5.97906 175.13073,-107.48043 191.0576,-155.2351 l 1.9714,-6.005 -114.06907,0.15563 z m -47.71576,-21.28338 c 11.50418,-27.15869 27.21056,-63.85015 55.31604,-59.46637 16.78287,5.31761 12.39909,23.95516 0.0519,59.38855 l 112.73319,0.15564 c 16.21218,-59.09025 28.92255,-157.75124 -85.92471,-156.0522 -68.81757,1.9714 -143.47153,9.75326 -195.15604,155.83171 l 112.97962,0.14267 z M 986.20626,431.89884 c -42.7094,169.87797 22.16534,216.84148 121.05984,216.84148 l 67.6632,0 45.7703,-123.39433 -81.5927,0 c -45.7703,0 -42.8132,-41.80155 -26.8734,-93.53794 l -126.02724,0.0908 z m 133.47184,-20.97211 c 20.3366,-43.9675 57.508,-86.58615 107.0006,-86.58615 l 71.632,0 35.8225,-101.4884 -111.4362,0 c -64.797,0 -179.9555,13.79983 -230.82294,188.03564 l 127.80404,0.0389 z"
+       id="path8"
+       inkscape:connector-curvature="0"
+       style="clip-rule:evenodd;fill:#ce4f32;fill-rule:evenodd" />
+  </g>
+</svg>

commit 7faeab024c679605de50fd8cb20d3b9b666ff3c0

[librpbase/tests] gtest_init.cpp: Initialize GDI+.
AppVeyor started failing in all image-based tests due to GDI+ not being
initialized properly.

The following tests FAILED:
	  3 - RpImageLoaderTest (Failed)
	  7 - ImageDecoderLinearTest (Failed)
	  8 - UnPremultiplyTest (Failed)
	 11 - ImageDecoderTest (Exit code 0xc0000409)

This fixes a regression from commit 1a92afa95b20da16e48ababbaea5a4a8192c44ce.
(Added an initial set of achievement icons.)

src/librpbase/tests/gtest_init.cpp
@@ -23,8 +23,10 @@ using std::locale;
 
 #ifdef _WIN32
 // rp_image backend registration.
-#include "librptexture/img/RpGdiplusBackend.hpp"
-#include "librptexture/img/rp_image.hpp"
+#  include "libwin32common/RpWin32_sdk.h"
+#  include "librptexture/img/GdiplusHelper.hpp"
+#  include "librptexture/img/RpGdiplusBackend.hpp"
+#  include "librptexture/img/rp_image.hpp"
 using LibRpTexture::RpGdiplusBackend;
 using LibRpTexture::rp_image;
 #endif /* _WIN32 */
@@ -95,6 +97,14 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	rp_secure_enable(param);
 
 #ifdef _WIN32
+	// Initialize GDI+.
+	const ULONG_PTR gdipToken = GdiplusHelper::InitGDIPlus();
+	assert(gdipToken != 0);
+	if (gdipToken == 0) {
+		fprintf(stderr, "*** ERROR: GDI+ initialization failed.\n");
+		return EXIT_FAILURE;
+	}
+
 	// Register RpGdiplusBackend.
 	// TODO: Static initializer somewhere?
 	rp_image::setBackendCreatorFn(RpGdiplusBackend::creator_fn);
@@ -109,5 +119,10 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	locale::global(locale("C"));
 
 	// Call the actual main function.
-	return gtest_main(argc, argv);
+	int ret = gtest_main(argc, argv);
+#ifdef _WIN32
+	// Shut down GDI+.
+	GdiplusHelper::ShutdownGDIPlus(gdipToken);
+#endif /* _WIN32 */
+	return ret;
 }