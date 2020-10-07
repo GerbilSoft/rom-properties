GerbilSoft/rom-properties

commit 808f3141f4c5a2b2a1b8ffc6130bb12f6afad72b

[librptexture] rp_image: Changed to a reference-counted class.
This should fix issues when the main content object is deleted and
reopened in order to extract e.g. an SRL.

Added special const rp_image::ref() and const rp_image::unref()
overloads to allow ref() and unref() of const rp_image objects,
since this is used everywhere.

[libromdata] ImageDecoderTest: Use a custom deleter for unique_ptr<rp_image*>.

In general, I'd rather not use unique_ptr<> with refcounted objects,
since that causes confusion with the reference count, but for this
particular case, it should be fine.

src/librpbase/img/RpPng.cpp
@@ -348,7 +348,7 @@ rp_image *RpPngPrivate::loadPng(png_structp png_ptr, png_infop info_ptr)
 	if (setjmp(png_jmpbuf(png_ptr))) {
 		// PNG read failed.
 		png_free(png_ptr, row_pointers);
-		delete img;
+		img->unref();
 		return nullptr;
 	}
 #endif
@@ -459,7 +459,7 @@ rp_image *RpPngPrivate::loadPng(png_structp png_ptr, png_infop info_ptr)
 	img = new rp_image(width, height, fmt);
 	if (!img->isValid()) {
 		// Could not allocate the image.
-		delete img;
+		img->unref();
 		return nullptr;
 	}
 
@@ -467,7 +467,7 @@ rp_image *RpPngPrivate::loadPng(png_structp png_ptr, png_infop info_ptr)
 	row_pointers = static_cast<const png_byte**>(
 		png_malloc(png_ptr, sizeof(const png_byte*) * height));
 	if (!row_pointers) {
-		delete img;
+		img->unref();
 		return nullptr;
 	}
 

src/gtk/CairoImageConv.cpp
@@ -85,7 +85,7 @@ cairo_surface_t *CairoImageConv::rp_image_to_cairo_surface_t(const rp_image *img
 			// Mark the surface as dirty.
 			cairo_surface_mark_dirty(surface);
 			if (premultiply) {
-				delete img_prex;
+				img_prex->unref();
 			}
 			break;
 		}

src/libromdata/Console/DreamcastSave.cpp
@@ -223,10 +223,10 @@ DreamcastSavePrivate::~DreamcastSavePrivate()
 {
 	UNREF(vmi_file);
 
-	delete img_banner;
+	UNREF(img_banner);
 	if (iconAnimData) {
 		for (int i = iconAnimData->count-1; i >= 0; i--) {
-			delete iconAnimData->frames[i];
+			UNREF(iconAnimData->frames[i]);
 		}
 		delete iconAnimData;
 	}

src/libromdata/Console/GameCubeBNR.cpp
@@ -90,7 +90,7 @@ GameCubeBNRPrivate::GameCubeBNRPrivate(GameCubeBNR *q, IRpFile *file)
 
 GameCubeBNRPrivate::~GameCubeBNRPrivate()
 {
-	delete img_banner;
+	UNREF(img_banner);
 }
 
 /**

src/libromdata/Console/GameCubeSave.cpp
@@ -116,10 +116,10 @@ GameCubeSavePrivate::GameCubeSavePrivate(GameCubeSave *q, IRpFile *file)
 
 GameCubeSavePrivate::~GameCubeSavePrivate()
 {
-	delete img_banner;
+	UNREF(img_banner);
 	if (iconAnimData) {
 		for (int i = iconAnimData->count-1; i >= 0; i--) {
-			delete iconAnimData->frames[i];
+			UNREF(iconAnimData->frames[i]);
 		}
 		delete iconAnimData;
 	}

src/libromdata/Console/PlayStationSave.cpp
@@ -91,10 +91,8 @@ PlayStationSavePrivate::PlayStationSavePrivate(PlayStationSave *q, IRpFile *file
 PlayStationSavePrivate::~PlayStationSavePrivate()
 {
 	if (iconAnimData) {
-		// This class owns all of the icons in iconAnimData, so we
-		// must delete all of them.
 		for (int i = iconAnimData->count-1; i >= 0; i--) {
-			delete iconAnimData->frames[i];
+			UNREF(iconAnimData->frames[i]);
 		}
 		delete iconAnimData;
 	}

src/libromdata/Console/WiiWIBN.cpp
@@ -76,10 +76,10 @@ WiiWIBNPrivate::WiiWIBNPrivate(WiiWIBN *q, IRpFile *file)
 
 WiiWIBNPrivate::~WiiWIBNPrivate()
 {
-	delete img_banner;
+	UNREF(img_banner);
 	if (iconAnimData) {
 		for (int i = iconAnimData->count-1; i >= 0; i--) {
-			delete iconAnimData->frames[i];
+			UNREF(iconAnimData->frames[i]);
 		}
 		delete iconAnimData;
 	}

src/libromdata/Console/Xbox360_STFS.cpp
@@ -170,7 +170,7 @@ Xbox360_STFS_Private::~Xbox360_STFS_Private()
 	UNREF(xex);
 	UNREF(xexReader);
 
-	delete img_icon;
+	UNREF(img_icon);
 }
 
 /**

commit 8a0dfa42809cbe00071c561def4e30f79902a6c4

[librpbase] RpPngPrivate::loadPng(): Don't unref() img if it's nullptr.
This caused a ton of crashes when fuzzing Xbox 360 GPD files with my
upcoming GPD changes.

This fixes a regression from commit 808f3141f4c5a2b2a1b8ffc6130bb12f6afad72b.
([librptexture] rp_image: Changed to a reference-counted class.)

Affects: v1.7, v1.7.1, v1.7.2, v1.7.3

src/librpbase/img/RpPng.cpp
@@ -347,7 +347,7 @@ rp_image *RpPngPrivate::loadPng(png_structp png_ptr, png_infop info_ptr)
 	if (setjmp(png_jmpbuf(png_ptr))) {
 		// PNG read failed.
 		png_free(png_ptr, row_pointers);
-		img->unref();
+		UNREF(img);
 		return nullptr;
 	}
 #endif