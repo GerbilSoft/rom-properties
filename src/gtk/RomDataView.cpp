f714915669a18324b639fd6c9d98fd5e267081ba
David Korth <gerbilsoft@gerbilsoft.com>
2020-10-10 01:50:19 -0400

    [gtk] RomDataView: Allow the user to resize COLSZ_STRETCH columns.
    Unlike Qt, we can't shrink it by shrinking the window.

--- src/gtk/RomDataView.cpp
+++ src/gtk/RomDataView.cpp
@@ -1180,7 +1180,9 @@ rom_data_view_init_listdata(RomDataView *page,
 				break;*/
 			case RomFields::ColSizing::COLSZ_STRETCH:
 				// TODO: Wordwrapping and/or text elision?
-				gtk_tree_view_column_set_resizable(column, false);
+				// NOTE: Allowing the user to resize the column because
+				// unlike Qt, we can't shrink it by shrinking the window.
+				gtk_tree_view_column_set_resizable(column, true);
 				gtk_tree_view_column_set_expand(column, true);
 				gtk_tree_view_column_set_sizing(column, GTK_TREE_VIEW_COLUMN_GROW_ONLY);
 				break;
