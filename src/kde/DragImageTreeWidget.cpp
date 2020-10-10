GerbilSoft/rom-properties

commit 8b8032c19020b91abe0e9c0fb75893e8a0a1cf61

[kde] DragImageTreeWidget: Use the QList<> even though we're only handling one item for now.
This simplifies the loop, since `continue;` in a `do { } while (0)` loop
doesn't make much sense.

Use for instead of std::for_each since the loop isn't "trivial".

Get the icon from the first usable item and use it as the drag icon.

This fixes the following Coverity Scan bugs:
- #1491543: Unexpected control flow

src/kde/DragImageTreeWidget.cpp
@@ -24,27 +24,23 @@ void DragImageTreeWidget::startDrag(Qt::DropActions supportedActions)
 	// - May need to write images to a temp directory and use a URI list...
 	Q_UNUSED(supportedActions)
 
-	// Get the current item.
-	QTreeWidgetItem *const item = currentItem();
-	if (!item) {
-		// No item selected.
-		return;
-	}
-
-#if 0
-	// TODO
+	// Get the selected items.
 	QList<QTreeWidgetItem*> items = selectedItems();
 	if (items.isEmpty()) {
 		// No items selected.
 		return;
 	}
-#endif
+
+	// TODO: Handle more than one selected item.
+	items = items.mid(0, 1);
 
 	// Find rp_image* objects in the items.
 	QMimeData *const mimeData = new QMimeData;
+	QIcon dragIcon;
 	bool hasOne = false;
-	//std::for_each(items.begin(), items.end(), [](QTreeWidgetItem *item) {
-	do {
+	const auto iter_end = items.end();
+	for (auto iter = items.begin(); iter != iter_end; ++iter) {
+		const QTreeWidgetItem *const item = *iter;
 		const rp_image *const img = static_cast<const rp_image*>(item->data(0, RpImageRole).value<void*>());
 		if (!img)
 			continue;
@@ -80,8 +76,14 @@ void DragImageTreeWidget::startDrag(Qt::DropActions supportedActions)
 		QByteArray ba = pngData->qByteArray();
 		mimeData->setData(QLatin1String("image/png"), pngData->qByteArray());
 		pngData->unref();
+
+		// Save the icon.
+		if (!dragIcon.isNull()) {
+			dragIcon = item->icon(0);
+		}
+
 		hasOne = true;
-	} while (0);
+	}
 
 	if (!hasOne) {
 		// No rp_image* objects...
@@ -97,9 +99,8 @@ void DragImageTreeWidget::startDrag(Qt::DropActions supportedActions)
 	// TODO: Make the icon size accessible to the QTreeWidgetItem.
 	// It's currently set in the QTreeWidget in RomDataView.
 	QPixmap qpxm;
-	const QIcon icon = item->icon(0);
-	if (!icon.isNull()) {
-		qpxm = icon.pixmap(QSize(32, 32));
+	if (!dragIcon.isNull()) {
+		qpxm = dragIcon.pixmap(QSize(32, 32));
 	}
 	if (!qpxm.isNull()) {
 		drag->setPixmap(qpxm);

commit 65c8616daff9574bdfe5035df23ed400c1b2ee08

[kde] DragImageTreeWidget: Set the drag icon if it's null.
Otherwise, we won't get any custom drag icon.

This may be a regression from commit 8b8032c19020b91abe0e9c0fb75893e8a0a1cf61.
([kde] DragImageTreeWidget: Use the QList<> even though we're only handling one item for now.)

Affects: v1.5 - v1.7.2

src/kde/DragImageTreeWidget.cpp
@@ -81,7 +81,7 @@ void DragImageTreeWidget::startDrag(Qt::DropActions supportedActions)
 		pngData->unref();
 
 		// Save the icon.
-		if (!dragIcon.isNull()) {
+		if (dragIcon.isNull()) {
 			dragIcon = item->icon(0);
 		}
 