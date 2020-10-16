GerbilSoft/rom-properties

commit 0c73df139d737fbd1f2ef930259a8afe1c959ced

[libromdata] DiscReader: Add an empty virtual destructor marked as protected.
Callers have been updated to use unref() instead of deleting the
DiscReader object directly.

src/libromdata/Console/Xbox360_STFS.cpp
@@ -170,7 +170,9 @@ Xbox360_STFS_Private::~Xbox360_STFS_Private()
 	if (xex) {
 		xex->unref();
 	}
-	delete xexReader;
+	if (xexReader) {
+		xexReader->unref();
+	}
 
 	delete img_icon;
 }
@@ -518,6 +520,8 @@ Xbox360_XEX *Xbox360_STFS_Private::openDefaultXex(void)
 			if (xex_tmp->isOpen()) {
 				this->xex = xex_tmp;
 				this->xexReader = discReader;
+				// We don't need to keep the DiscReader reference.
+				discReader->unref();
 				discReader = nullptr;
 			} else {
 				xex_tmp->unref();
@@ -525,7 +529,9 @@ Xbox360_XEX *Xbox360_STFS_Private::openDefaultXex(void)
 		}
 		xexFile_tmp->unref();
 	}
-	delete discReader;
+	if (discReader) {
+		discReader->unref();
+	}
 
 	return this->xex;
 }
@@ -599,6 +605,10 @@ void Xbox360_STFS::close(void)
 	// that may be used by the UI later.
 	if (d->xex) {
 		d->xex->close();
+	}
+	if (d->xexReader) {
+		d->xexReader->unref();
+		d->xexReader = nullptr;
 	}
 
 	// Call the superclass function.

src/libromdata/Console/Xbox_XBE.cpp
@@ -125,7 +125,9 @@ Xbox_XBE_Private::~Xbox_XBE_Private()
 		}
 	}
 
-	delete discReader;
+	if (discReader) {
+		discReader->unref();
+	}
 }
 
 /**
@@ -248,7 +250,7 @@ int Xbox_XBE_Private::initXPR0_xtImage(void)
 		DiscReader *const discReader_tmp = new DiscReader(this->file);
 		if (!discReader_tmp->isOpen()) {
 			// Unable to open the DiscReader.
-			delete discReader_tmp;
+			discReader_tmp->unref();
 			return -EIO;
 		}
 		discReader = discReader_tmp;
@@ -340,7 +342,7 @@ const EXE *Xbox_XBE_Private::initEXE(void)
 		DiscReader *const discReader_tmp = new DiscReader(this->file);
 		if (!discReader_tmp->isOpen()) {
 			// Unable to open the DiscReader.
-			delete discReader_tmp;
+			discReader_tmp->unref();
 			return nullptr;
 		}
 		discReader = discReader_tmp;
@@ -457,8 +459,10 @@ void Xbox_XBE::close(void)
 		}
 	}
 
-	delete d->discReader;
-	d->discReader = nullptr;
+	if (d->discReader) {
+		d->discReader->unref();
+		d->discReader = nullptr;
+	}
 
 	// Call the superclass function.
 	super::close();

src/libromdata/Handheld/DMG.cpp
@@ -1172,7 +1172,7 @@ int DMG::loadFieldData(void)
 					}
 					ptFile->unref();
 				}
-				delete reader;
+				reader->unref();
 			}
 		}
 	}

src/librpbase/disc/DiscReader.hpp
@@ -35,6 +35,8 @@ class DiscReader : public IDiscReader
 		 * @param length Disc length. (-1 for "until end of file")
 		 */
 		explicit DiscReader(LibRpFile::IRpFile *file, off64_t offset, off64_t length);
+	protected:
+		virtual ~DiscReader() { };	// call unref() instead
 
 	private:
 		typedef IDiscReader super;

commit f1e5aacb38ff35372564404700fb035a4d57e924

[libromdata] Xbox360_STFS: Don't keep the XEX DiscReader reference.
This might get unreferenced at a different point without NULLing out
this->xexReader, which results in one too many unreferences.

This may be a regression from commit 0c73df139d737fbd1f2ef930259a8afe1c959ced.
([libromdata] DiscReader: Add an empty virtual destructor marked as protected.)

Fixes #275: Properties box force closes when you try to view the ROM
Properties for an XBLA Arcade Package
Reported by @Masamune3210.

Affects: v1.6 - v1.7.3

src/libromdata/Console/Xbox360_STFS.cpp
@@ -104,7 +104,6 @@ class Xbox360_STFS_Private final : public RomDataPrivate
 
 	public:
 		// XEX executable.
-		DiscReader *xexReader;
 		Xbox360_XEX *xex;
 
 		// File table.
@@ -156,7 +155,6 @@ Xbox360_STFS_Private::Xbox360_STFS_Private(Xbox360_STFS *q, IRpFile *file)
 	, stfsType(StfsType::Unknown)
 	, img_icon(nullptr)
 	, headers_loaded(0)
-	, xexReader(nullptr)
 	, xex(nullptr)
 {
 	// Clear the headers.
@@ -168,8 +166,6 @@ Xbox360_STFS_Private::Xbox360_STFS_Private(Xbox360_STFS *q, IRpFile *file)
 Xbox360_STFS_Private::~Xbox360_STFS_Private()
 {
 	UNREF(xex);
-	UNREF(xexReader);
-
 	UNREF(img_icon);
 }
 
@@ -516,9 +512,6 @@ Xbox360_XEX *Xbox360_STFS_Private::openDefaultXex(void)
 			Xbox360_XEX *const xex_tmp = new Xbox360_XEX(xexFile_tmp);
 			if (xex_tmp->isOpen()) {
 				this->xex = xex_tmp;
-				this->xexReader = discReader;
-				// We don't need to keep the DiscReader reference.
-				UNREF_AND_NULL_NOCHK(discReader);
 			} else {
 				xex_tmp->unref();
 			}
@@ -598,7 +591,6 @@ void Xbox360_STFS::close(void)
 	if (d->xex) {
 		d->xex->close();
 	}
-	UNREF_AND_NULL(d->xexReader);
 
 	// Call the superclass function.
 	super::close();

src/libromdata/Console/Xbox360_XEX.cpp
@@ -1920,9 +1920,8 @@ int Xbox360_XEX::loadFieldData(void)
 
 		// Disc number
 		// NOTE: Not shown for single-disc games.
-		const char *const disc_number_title = C_("RomData", "Disc #");
 		if (d->executionID.disc_number != 0 && d->executionID.disc_count > 1) {
-			d->fields->addField_string(disc_number_title,
+			d->fields->addField_string(C_("RomData", "Disc #"),
 				// tr: Disc X of Y (for multi-disc games)
 				rp_sprintf_p(C_("RomData|Disc", "%1$u of %2$u"),
 					d->executionID.disc_number,