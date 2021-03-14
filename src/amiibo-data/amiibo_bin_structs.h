9460eeaf9b5b928377722a46166550fd675ee30e
David Korth <gerbilsoft@gerbilsoft.com>
2021-03-14 09:16:29 -0400

    [amiibo-data] New amiibo database binary format.
    amiibo-bin.txt: Description of the binary format.
    amiibo-data.txt: Amiibo database in text format.
    
    The database will be compiled to a binary file using amiiboc.
    
    TODO: CMake's cross-compilation functionality is a bit iffy and requires
    building a host system version of amiiboc in a separate CMake run. Maybe
    we should convert it to Perl or Python?

---
+++ src/amiibo-data/amiibo_bin_structs.h
@@ -0,0 +1,94 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (amiibo-data)                      *
+ * amiibo_bin_structs.h: Nintendo amiibo binary data structs.              *
+ *                                                                         *
+ * Copyright (c) 2016-2020 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_AMIIBO_DATA_AMIIBO_BIN_STRUCTS_H__
+#define __ROMPROPERTIES_AMIIBO_DATA_AMIIBO_BIN_STRUCTS_H__
+
+#include "common.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * amiibo binary database file header.
+ * This file format is specific to rom-properties.
+ *
+ * All offsets are absolute offsets. (relative to the start of the file)
+ *
+ * All fields are little-endian.
+ */
+#define AMIIBO_BIN_MAGIC "RPNFPB10"
+typedef struct _AmiiboBinHeader {
+	char magic[8];		// [0x000] "RPNFPB10"
+	uint32_t strtbl_offset;	// [0x008] String table
+	uint32_t strtbl_len;	// [0x00C]
+
+	// Page 21
+	uint32_t series_offset;	// [0x010] Series table
+	uint32_t series_len;	// [0x014]
+	uint32_t char_offset;	// [0x018] Character table
+	uint32_t char_len;	// [0x01C]
+	uint32_t cvar_offset;	// [0x020] Character variant table
+	uint32_t cvar_len;	// [0x024]
+
+	// Page 22
+	uint32_t amiibo_offset;	// [0x028] amiibo ID table
+	uint32_t amiibo_len;
+
+	// Reserved
+	uint32_t reserved[4];	// [0x030]
+} AmiiboBinHeader;
+ASSERT_STRUCT(AmiiboBinHeader, 0x040);
+
+/**
+ * Character table entry. (p.21)
+ *
+ * If bit 32 of char_id is set, character variants are present.
+ * The character variant table should be checked.
+ *
+ * All fields are little-endian.
+ */
+typedef struct _CharTableEntry {
+	uint32_t char_id;	// Character ID (low 16 bits are significant)
+	uint32_t name;		// Character name (string table offset)
+} CharTableEntry;
+ASSERT_STRUCT(CharTableEntry, 2*sizeof(uint32_t));
+
+/**
+ * Character table entry. (p.21)
+ *
+ * All fields are little-endian.
+ */
+typedef struct _CharVariantTableEntry {
+	uint16_t char_id;	// Character ID
+	uint8_t var_id;		// Variant ID
+	uint8_t reserved;
+	uint32_t name;		// Character variant name (string table)
+} CharVariantTableEntry;
+ASSERT_STRUCT(CharVariantTableEntry, 2*sizeof(uint32_t));
+
+/**
+ * amiibo ID table entry. (p.22)
+ *
+ * All fields are little-endian.
+ */
+typedef struct _AmiiboIDTableEntry {
+	uint16_t release_no;	// Release number
+	uint8_t wave_no;	// Wave number
+	uint8_t reserved;
+	uint32_t name;		// amiibo name (string table)
+} AmiiboIDTableEntry;
+ASSERT_STRUCT(AmiiboIDTableEntry, 2*sizeof(uint32_t));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ROMPROPERTIES_AMIIBO_DATA_AMIIBO_BIN_STRUCTS_H__ */
---
+++ src/amiibo-data/amiiboc.cpp
@@ -0,0 +1,611 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (amiibo-data)                      *
+ * amiiboc.cpp: Nintendo amiibo binary data compiler.                      *
+ *                                                                         *
+ * Copyright (c) 2016-2021 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "amiibo_bin_structs.h"
+
+// Byteswapping
+#include "byteswap_rp.h"
+
+// C includes. (C++ namespace)
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+
+// C++ includes.
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using std::map;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+// NOTE: Tables in memory are HOST-endian.
+// They will be swapped to little-endian when written to disk.
+
+// String table.
+static vector<char> stringTable;
+static unordered_map<string, uint32_t> stringTableMap;		// string table indexes
+
+// amiibo data.
+static AmiiboBinHeader binHeader;
+static vector<uint32_t> charSeriesTable;			// index is char series ID; value is string table offset
+static map<uint16_t, CharTableEntry> charTable;			// key is char ID
+
+// Character Variant table:
+// key: char ID
+// value: map with key=variant ID
+typedef map<uint8_t, CharVariantTableEntry> CharVariantMap;
+static map<uint16_t, CharVariantMap> charVarTable;
+
+static vector<uint32_t> amiiboSeriesTable;			// index is amiibo series ID; value is string table offset
+static vector<AmiiboIDTableEntry> amiiboTable;			// index is amiibo ID
+
+/**
+ * Get the string table offset for the specified string.
+ *
+ * If the string is found in the string table, the existing offset
+ * will be returned.
+ *
+ * If the string is not found in the string table, it will be added
+ * and the new string offset will be returned.
+ *
+ * @param str String to look up.
+ * @return String table offset.
+ */
+static uint32_t getStringTableOffset(const char *str)
+{
+	if (!str || str[0] == '\0') {
+		// Empty string.
+		return 0;
+	}
+
+	// Check if the string is already in the string table.
+	auto iter = stringTableMap.find(str);
+	if (iter != stringTableMap.end()) {
+		// Found the string.
+		return iter->second;
+	}
+
+	// Not found. Add the string
+	// TODO: Check generated assembly; maybe memcmp() with ao::uvector<> is faster?
+	const uint32_t offset = static_cast<uint32_t>(stringTable.size());
+	string entry(str);
+	stringTable.insert(stringTable.end(), entry.c_str(), entry.c_str() + entry.size() + 1);
+	stringTableMap.emplace(std::make_pair(std::move(entry), offset));
+	return offset;
+}
+
+/**
+ * Align a file to 16 bytes by writing NULL bytes.
+ * @param f File.
+ */
+static void alignFileTo16Bytes(FILE *f)
+{
+	off_t offset = ftello(f);
+	if (offset % 16 == 0)
+		return;
+
+	uint8_t zerobytes[16];
+	memset(zerobytes, 0, sizeof(zerobytes));
+	const unsigned int count = 16 - (offset % 16);
+	fwrite(zerobytes, 1, count, f);
+}
+
+int main(int argc, char *argv[])
+{
+	// TODO: Better command line parsing.
+	if (argc != 3) {
+		fprintf(stderr, "syntax: %s amiibo-data.txt amiibo.bin\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	FILE *f_in = fopen(argv[1], "r");
+	if (!f_in) {
+		fprintf(stderr, "*** ERROR opening input file '%s': %s\n", argv[1], strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	// Initialize the header and tables.
+	// TODO: Check map::reserve(), unordered_map::reserve()
+	memset(&binHeader, 0, sizeof(binHeader));
+	charSeriesTable.reserve(0x3A4/4);
+	//charTable.reserve(1024);
+	//charVarTable.reserve(128);
+	amiiboSeriesTable.reserve(32);
+	amiiboTable.reserve(0x1000);
+	
+	// Initialize the string table.
+	// The string table always starts with a NULL byte. (empty string)
+	stringTable.reserve(32768);	// TODO: Optimal reservation?
+	stringTableMap.reserve(2048);	// TODO: Optimal reservation?
+	stringTable.push_back('\0');
+	stringTableMap.emplace(std::make_pair("", 0));
+
+	// Parse the file.
+	bool foundHeader = false;	// true if we found the header line
+	bool err = false;
+	int line = 1;
+	char buf[256];
+	for (; fgets(buf, sizeof(buf), f_in) != nullptr; line++) {
+		// Remove leading and trailing whitespace.
+		const char *const buf_end = buf + sizeof(buf);
+		char *p = buf;
+		while (p < buf_end && *p != '\0' && isspace(*p)) {
+			p++;
+		}
+		if (*p == '#') {
+			// Comment line.
+			continue;
+		}
+		size_t plen = strlen(p);
+		while (plen > 0) {
+			if (!isspace(p[plen-1]))
+				break;
+
+			p[plen-1] = '\0';
+			plen--;
+		}
+		if (plen == 0)
+			continue;
+
+		// Tokenize the line.
+		char *saveptr;
+		const char *token = strtok_r(p, ":", &saveptr);
+		assert(token != nullptr);
+		if (!token)
+			continue;
+
+		if (!strcmp(token, AMIIBO_BIN_MAGIC)) {
+			if (foundHeader) {
+				// Duplicate file header.
+				fprintf(stderr, "*** ERROR: Line %d: Duplicate %s header.\n", line, AMIIBO_BIN_MAGIC);
+				err = true;
+				break;
+			} else {
+				// Found the file header.
+				memcpy(binHeader.magic, token, sizeof(binHeader.magic));
+				foundHeader = true;
+				continue;
+			}
+		}
+
+		// For anything else, the header must have been found already.
+		if (!foundHeader) {
+			fprintf(stderr, "*** ERROR: Missing %s header.\n", AMIIBO_BIN_MAGIC);
+			err = true;
+			break;
+		}
+
+		// Check the command.
+		if (!strcmp(token, "CS")) {
+			// Character Series
+			// Fields: ID, Name
+
+			// ID
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'CS' command is missing ID field.\n", line);
+				err = true;
+				break;
+			}
+			char *endptr;
+			unsigned int id = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'CS' command: Invalid ID '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (id > 16384) {
+				fprintf(stderr, "*** ERROR: Line %d: 'CS' command: ID is out of range: %u (0x%04X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// Name
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token || token[0] == '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'CS' command is missing Name field.\n", line);
+				err = true;
+				break;
+			}
+
+			// Check if we already have this character series.
+			unsigned int idx = id / 4;
+			if (idx < charSeriesTable.size()) {
+				if (charSeriesTable[idx] != 0) {
+					fprintf(stderr, "*** ERROR: Line %d: 'CS' command has duplicate ID: %u (0x%04X)\n", line, id, id);
+					err = true;
+					break;
+				}
+			} else {
+				charSeriesTable.resize(idx+1);
+			}
+
+			// Add the name to the string table and save the series name.
+			charSeriesTable[idx] = getStringTableOffset(token);
+			printf("CS: ID=%04X, name=%s, offset=%u\n", id, token, charSeriesTable[idx]);
+		} else if (!strcmp(token, "C")) {
+			// Character
+			// Fields: ID, Name
+
+			// ID
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'C' command is missing ID field.\n", line);
+				err = true;
+				break;
+			}
+			char *endptr;
+			unsigned int id = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'C' command: Invalid ID '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (id > 0xFFFF) {
+				fprintf(stderr, "*** ERROR: Line %d: 'C' command: ID is out of range: %u (0x%04X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// Name
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token || token[0] == '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'C' command is missing Name field.\n", line);
+				err = true;
+				break;
+			}
+
+			// Check if we already have this character.
+			auto iter = charTable.find(id);
+			if (iter != charTable.end()) {
+				fprintf(stderr, "*** ERROR: Line %d: 'C' command has duplicate ID: %u (0x%04X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// Add the name to the string table and save the character.
+			CharTableEntry charTableEntry;
+			charTableEntry.char_id = id;
+			charTableEntry.name = getStringTableOffset(token);
+			printf("C: ID=%04X, name=%s, offset=%u\n", id, token, charTableEntry.name);
+			charTable[id] = std::move(charTableEntry);
+		} else if (!strcmp(token, "CV")) {
+			// Character Variant
+			// Fields: ID, VarID, Name
+
+			// ID
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command is missing ID field.\n", line);
+				err = true;
+				break;
+			}
+			char *endptr;
+			unsigned int id = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command: Invalid ID '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (id > 0xFFFF) {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command: ID is out of range: %u (0x%04X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// VarID
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command is missing VarID field.\n", line);
+				err = true;
+				break;
+			}
+			unsigned int var_id = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command: Invalid VarID '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (var_id > 0xFF) {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command: VarID is out of range: %u (0x%04X)\n", line, var_id, var_id);
+				err = true;
+				break;
+			}
+
+			// Name
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token || token[0] == '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command is missing Name field.\n", line);
+				err = true;
+				break;
+			}
+
+			// Check if this character was set.
+			auto iter = charTable.find(id);
+			if (iter == charTable.end()) {
+				fprintf(stderr, "*** ERROR: Line %d: 'CV' command has unassigned char ID: %u (0x%04X)\n", line, id, id);
+				err = true;
+				break;
+			}
+			// Set the high bit in the character ID.
+			// This indicates character variants are present.
+			iter->second.char_id |= (1U << 31);
+
+			// Check if we already have this character variant.
+			CharVariantMap *pMap;
+			auto iterV = charVarTable.find(id);
+			if (iterV != charVarTable.end()) {
+				// We have at least one character variant.
+				pMap = &(iterV->second);
+				auto iterV2 = pMap->find(var_id);
+				if (iterV2 != pMap->end()) {
+					fprintf(stderr, "*** ERROR: Line %d: 'C' command has duplicate variant ID: %u:%u (0x%04X:0x%02X)\n", line, id, var_id, id, var_id);
+					err = true;
+					break;
+				}
+			} else {
+				// No character variants yet.
+				auto ret = charVarTable.emplace(std::make_pair(id, CharVariantMap()));
+				pMap = &(ret.first->second);
+			}
+
+			// Add the variant ID.
+			CharVariantTableEntry entry;
+			entry.char_id = id;
+			entry.var_id = var_id;
+			entry.reserved = 0;
+			entry.name = getStringTableOffset(token);
+			printf("CV: ID=%04X, VarID=%02X, name=%s, offset=%u\n", id, var_id, token, entry.name);
+			pMap->emplace(std::make_pair(var_id, std::move(entry)));
+		} else if (!strcmp(token, "AS")) {
+			// amiibo Series
+			// Fields: ID, Name
+
+			// ID
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'AS' command is missing ID field.\n", line);
+				err = true;
+				break;
+			}
+			char *endptr;
+			unsigned int id = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'AS' command: Invalid ID '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (id > 0xFF) {
+				fprintf(stderr, "*** ERROR: Line %d: 'AS' command: ID is out of range: %u (0x%02X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// Name
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token || token[0] == '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'AS' command is missing Name field.\n", line);
+				err = true;
+				break;
+			}
+
+			// Check if we already have this amiibo series.
+			if (id < amiiboSeriesTable.size()) {
+				if (amiiboSeriesTable[id] != 0) {
+					fprintf(stderr, "*** ERROR: Line %d: 'AS' command has duplicate ID: %u (0x%04X)\n", line, id, id);
+					err = true;
+					break;
+				}
+			} else {
+				amiiboSeriesTable.resize(id+1);
+			}
+
+			// Add the name to the string table and save the series name.
+			amiiboSeriesTable[id] = getStringTableOffset(token);
+			printf("AS: ID=%04X, name=%s, offset=%u\n", id, token, amiiboSeriesTable[id]);
+		} else if (!strcmp(token, "A")) {
+			// amiibo
+			// Fields: ID, Release No., Wave, Name
+
+			// ID
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command is missing ID field.\n", line);
+				err = true;
+				break;
+			}
+			char *endptr;
+			unsigned int id = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command: Invalid ID '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (id > 0xFFFF) {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command: ID is out of range: %u (0x%04X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// Release No.
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command is missing Release No. field.\n", line);
+				err = true;
+				break;
+			}
+			unsigned int release_no = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command: Invalid Release No. '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (release_no > 0xFFFF) {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command: Release No. is out of range: %u (0x%04X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// Wave
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token) {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command is missing Wave field.\n", line);
+				err = true;
+				break;
+			}
+			unsigned int wave_no = strtoul(token, &endptr, 0);
+			if (*endptr != '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command: Invalid Wave. '%s'.\n", line, token);
+				err = true;
+				break;
+			} else if (wave_no > 0xFF) {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command: Wave is out of range: %u (0x%02X)\n", line, id, id);
+				err = true;
+				break;
+			}
+
+			// Name
+			token = strtok_r(nullptr, ":", &saveptr);
+			if (!token || token[0] == '\0') {
+				fprintf(stderr, "*** ERROR: Line %d: 'A' command is missing Name field.\n", line);
+				err = true;
+				break;
+			}
+
+			// Check if we already have this amiibo.
+			if (id < amiiboTable.size()) {
+				if (amiiboTable[id].name != 0) {
+					fprintf(stderr, "*** ERROR: Line %d: 'A' command has duplicate ID: %u (0x%04X)\n", line, id, id);
+					err = true;
+					break;
+				}
+			} else {
+				amiiboTable.resize(id+1);
+			}
+
+			// Add the name to the string table and save the amiibo.
+			AmiiboIDTableEntry &entry = amiiboTable[id];
+			entry.release_no = release_no;
+			entry.wave_no = wave_no;
+			entry.reserved = 0;
+			entry.name = getStringTableOffset(token);
+			printf("A: ID=%04X, release_no=%u, wave_no=%u, name=%s, offset=%u\n",
+				id, release_no, wave_no, token, entry.name);
+		} else {
+			// Invalid command.
+			fprintf(stderr, "*** ERROR: Line %d: Invalid command '%s'.\n", line, token);
+			err = true;
+			break;
+		}
+	}
+	fclose(f_in);
+
+	if (err) {
+		return EXIT_FAILURE;
+	}
+
+	// Write the binary data.
+	FILE *f_out = fopen(argv[2], "wb");
+	if (!f_out) {
+		fprintf(stderr, "*** ERROR opening output file '%s': %s\n", argv[2], strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	// TODO: Check fwrite() return values.
+	// TODO: Check if any tables are 0 bytes.
+
+	// Write the initial header.
+	// It will be rewritten once everything is finalized.
+	fwrite(&binHeader, 1, sizeof(binHeader), f_out);
+
+	// Series table.
+	alignFileTo16Bytes(f_out);
+	const uint32_t series_len = static_cast<uint32_t>(charSeriesTable.size() * sizeof(charSeriesTable[0]));
+	binHeader.series_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
+	binHeader.series_len = cpu_to_le32(series_len);
+#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+	// Byteswap the series table to little-endian.
+	__byte_swap_32_array(charSeriesTable.data(), charSeriesTable.size() * sizeof(charSeriesTable[0]));
+#endif /* SYS_BYTEORDER == SYS_BIG_ENDIAN */
+	fwrite(charSeriesTable.data(), 1, series_len, f_out);
+
+	// Character table
+	alignFileTo16Bytes(f_out);
+	const uint32_t char_len = static_cast<uint32_t>(charTable.size() * sizeof(CharTableEntry));
+	binHeader.char_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
+	binHeader.char_len = cpu_to_le32(char_len);
+	const auto charTable_cend = charTable.cend();
+	for (auto iter = charTable.cbegin(); iter != charTable_cend; ++iter) {
+#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+		// Byteswap the entry first.
+		CharTableEntry entry = iter->second;
+		entry.char_id	= cpu_to_le32(entry.char_id);
+		entry.name	= cpu_to_le32(entry.name);
+		fwrite(&entry, 1, sizeof(entry), f_out);
+#else /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
+		// No byteswap is needed.
+		fwrite(&(iter->second), 1, sizeof(iter->second), f_out);
+#endif
+	}
+
+	// Character variant table
+	alignFileTo16Bytes(f_out);
+	uint32_t cvar_len = 0;
+	binHeader.cvar_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
+	const auto charVarTable_cend = charVarTable.cend();
+	for (auto iter = charVarTable.cbegin(); iter != charVarTable_cend; ++iter) {
+		const CharVariantMap &map = iter->second;
+		const auto map_cend = map.cend();
+		for (auto iter = map.cbegin(); iter != map_cend; ++iter) {
+#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+			// Byteswap the entry first.
+			CharVariantTableEntry entry = iter->second;
+			entry.char_id	= cpu_to_le16(entry.char_id);
+			entry.name	= cpu_to_le32(entry.name);
+			fwrite(&entry, 1, sizeof(entry), f_out);
+#else /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
+			// No byteswap is needed.
+			fwrite(&(iter->second), 1, sizeof(iter->second), f_out);
+#endif
+			cvar_len += static_cast<uint32_t>(sizeof(iter->second));
+		}
+	}
+	binHeader.cvar_len = cpu_to_le32(cvar_len);
+
+	// amiibo ID table
+	alignFileTo16Bytes(f_out);
+	const uint32_t amiibo_len = static_cast<uint32_t>(amiiboTable.size() * sizeof(AmiiboIDTableEntry));
+	binHeader.amiibo_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
+	binHeader.amiibo_len = cpu_to_le32(amiibo_len);
+#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+	// Byteswapping is needed, so we have to process each entry.
+	const auto amiiboTable_cend = amiiboTable.cend();
+	for (auto iter = amiiboTable.cbegin(); iter != amiiboTable_cend; ++iter) {
+		AmiiboIDTableEntry entry = *iter;
+		entry.release_no	= cpu_to_le16(entry.release_no);
+		entry.name		= cpu_to_le32(entry.name);
+		fwrite(&entry, 1, sizeof(entry), f_out);
+	}
+#else /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
+	// No byteswap is needed.
+	fwrite(amiiboTable.data(), 1, amiibo_len, f_out);
+#endif
+
+	// Make sure the string table is a mulitple of 16 bytes.
+	stringTable.resize(ALIGN_BYTES(16, stringTable.size()));
+	alignFileTo16Bytes(f_out);
+	binHeader.strtbl_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
+	binHeader.strtbl_len = cpu_to_le32(static_cast<uint32_t>(stringTable.size()));
+	fwrite(stringTable.data(), 1, stringTable.size(), f_out);
+
+	// Write the updated header.
+	rewind(f_out);
+	fwrite(&binHeader, 1, sizeof(binHeader), f_out);
+	fclose(f_out);
+
+	// We're done here.
+	return EXIT_SUCCESS;
+}
