GerbilSoft/rom-properties

commit 04bd2db5a9adff2690fe7bfa14148dc01dbdd3df

[libromdata] Consolidate all "Game ID" strings into the "RomData" context.

src/libromdata/Handheld/DMG.cpp
@@ -886,12 +886,8 @@ int DMG::loadFieldData(void)
 	string s_title, s_gameID;
 	d->getTitleAndGameID(s_title, s_gameID);
 	d->fields->addField_string(C_("RomData", "Title"), s_title);
-	if (!s_gameID.empty()) {
-		d->fields->addField_string(C_("DMG", "Game ID"), s_gameID);
-	} else {
-		d->fields->addField_string(C_("DMG", "Game ID"),
-			C_("RomData", "Unknown"));
-	}
+	d->fields->addField_string(C_("RomData", "Game ID"),
+		s_gameID.empty() ? s_gameID.c_str() : C_("RomData", "Unknown"));
 
 	// System
 	const uint32_t dmg_system = d->systemID();

src/libromdata/Console/GameCube.cpp
@@ -1544,7 +1544,7 @@ int GameCube::loadFieldData(void)
 			? d->discHeader.id6[i]
 			: '_');
 	}
-	d->fields->addField_string(C_("GameCube", "Game ID"), latin1_to_utf8(id6, 6));
+	d->fields->addField_string(C_("RomData", "Game ID"), latin1_to_utf8(id6, 6));
 
 	// Publisher.
 	d->fields->addField_string(C_("RomData", "Publisher"), d->getPublisher());

src/libromdata/Console/GameCubeSave.cpp
@@ -875,7 +875,7 @@ int GameCubeSave::loadFieldData(void)
 			: '_');
 	}
 	id6[6] = 0;
-	d->fields->addField_string(C_("GameCubeSave", "Game ID"), latin1_to_utf8(id6, 6));
+	d->fields->addField_string(C_("RomData", "Game ID"), latin1_to_utf8(id6, 6));
 
 	// Look up the publisher.
 	const char *publisher = NintendoPublishers::lookup(direntry->company);

src/libromdata/Console/NES.cpp
@@ -846,7 +846,7 @@ int NES::loadFieldData(void)
 	if ((d->romType & NESPrivate::ROM_SYSTEM_MASK) == NESPrivate::ROM_SYSTEM_FDS) {
 		// Game ID
 		// TODO: Check for invalid characters?
-		d->fields->addField_string(C_("NES", "Game ID"),
+		d->fields->addField_string(C_("RomData", "Game ID"),
 			rp_sprintf("%s-%.3s",
 				(d->header.fds.disk_type == FDS_DTYPE_FSC ? "FSC" : "FMC"),
 				d->header.fds.game_id));

src/libromdata/Console/SNES.cpp
@@ -1291,7 +1291,7 @@ int SNES::loadFieldData(void)
 	d->fields->addField_string(C_("RomData", "Title"), d->getRomTitle());
 
 	// Game ID
-	const char *const game_id_title = C_("SNES", "Game ID");
+	const char *const game_id_title = C_("RomData", "Game ID");
 	string gameID = d->getGameID();
 	if (!gameID.empty()) {
 		d->fields->addField_string(game_id_title, gameID);

src/libromdata/Console/SufamiTurbo.cpp
@@ -376,7 +376,7 @@ int SufamiTurbo::loadFieldData(void)
 	char gameID[16];
 	snprintf(gameID, sizeof(gameID), "%02X%02X%02X",
 		romHeader->game_id[0], romHeader->game_id[1], romHeader->game_id[2]);
-	d->fields->addField_string(C_("SufamiTurbo", "Game ID"), gameID,
+	d->fields->addField_string(C_("RomData", "Game ID"), gameID,
 		RomFields::STRF_MONOSPACE);
 #endif
 

src/libromdata/Console/WiiSave.cpp
@@ -501,7 +501,7 @@ int WiiSave::loadFieldData(void)
 		{
 			// Print the game ID.
 			// TODO: Is the publisher code available anywhere?
-			d->fields->addField_string(C_("WiiSave", "Game ID"),
+			d->fields->addField_string(C_("RomData", "Game ID"),
 				latin1_to_utf8(bkHeader->id4, sizeof(bkHeader->id4)));
 		}
 	}

src/libromdata/Console/WiiU.cpp
@@ -432,7 +432,7 @@ int WiiU::loadFieldData(void)
 	d->fields->reserve(4);	// Maximum of 4 fields.
 
 	// Game ID.
-	d->fields->addField_string(C_("WiiU", "Game ID"),
+	d->fields->addField_string(C_("RomData", "Game ID"),
 		latin1_to_utf8(d->discHeader.id, sizeof(d->discHeader.id)));
 
 	// Publisher.

commit adc3850f5e8b7b42fa471881005f56693735269e

[libromdata] DMG: Fix an inverted condition for the game ID display.
Fixes: 04bd2db5a9adff2690fe7bfa14148dc01dbdd3df
([libromdata] Consolidate all "Game ID" strings into the "RomData" context.)

Affects: v1.7 - v1.7.3

src/libromdata/Handheld/DMG.cpp
@@ -887,7 +887,7 @@ int DMG::loadFieldData(void)
 	d->getTitleAndGameID(s_title, s_gameID);
 	d->fields->addField_string(C_("RomData", "Title"), s_title);
 	d->fields->addField_string(C_("RomData", "Game ID"),
-		s_gameID.empty() ? s_gameID.c_str() : C_("RomData", "Unknown"));
+		!s_gameID.empty() ? s_gameID.c_str() : C_("RomData", "Unknown"));
 
 	// System
 	const uint32_t dmg_system = d->systemID();