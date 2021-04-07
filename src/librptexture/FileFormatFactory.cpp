GerbilSoft/rom-properties

commit b8ff049997820ee8af6f844591d5f4bec4366b4c

[librptexture] Initial support for TGA format.
Currently supports true color images without RLE compression, and
handles image orientation.

TODO:
- Test 16-bit (ARGB1555) true color.
- Add color map (palette) and grayscale support.
- Add RLE support.
- Add more fields, including the Extension Area.

src/librptexture/FileFormatFactory.cpp
@@ -13,8 +13,9 @@
 #include "fileformat/FileFormat.hpp"
 
 // librpbase, librpfile
+#include "librpfile/FileSystem.hpp"
 using namespace LibRpBase;
-using LibRpFile::IRpFile;
+using namespace LibRpFile;
 
 // C++ STL classes.
 using std::string;
@@ -28,6 +29,7 @@ using std::vector;
 #include "fileformat/KhronosKTX2.hpp"
 #include "fileformat/PowerVR3.hpp"
 #include "fileformat/SegaPVR.hpp"
+#include "fileformat/TGA.hpp"
 #include "fileformat/ValveVTF.hpp"
 #include "fileformat/ValveVTF3.hpp"
 #include "fileformat/XboxXPR.hpp"
@@ -133,8 +135,24 @@ FileFormat *FileFormatFactory::create(IRpFile *file)
 		return nullptr;
 	}
 
+	// Special case: TGA doesn't have a magic number in the header.
+	// It *might* have a footer. We'll just check the file extension
+	// for now.
+	const string filename = file->filename();
+	const char *ext = FileSystem::file_ext(filename);
+	if (!strcasecmp(ext, ".tga")) {
+		// TGA file.
+		FileFormat *const fileFormat = new TGA(file);
+		if (fileFormat->isValid()) {
+			// FileFormat subclass obtained.
+			return fileFormat;
+		}
+
+		// Not actually supported.
+		fileFormat->unref();
+	}
+
 	// Read the file's magic number.
-	// TODO: Special case for TGA?
 	uint32_t magic[2];
 	file->rewind();
 	size_t size = file->read(&magic, sizeof(magic));

src/librptexture/CMakeLists.txt
@@ -34,6 +34,7 @@ SET(librptexture_SRCS
 	fileformat/KhronosKTX2.cpp
 	fileformat/PowerVR3.cpp
 	fileformat/SegaPVR.cpp
+	fileformat/TGA.cpp
 	fileformat/ValveVTF.cpp
 	fileformat/ValveVTF3.cpp
 	fileformat/XboxXPR.cpp
@@ -63,6 +64,7 @@ SET(librptexture_H
 	fileformat/KhronosKTX2.hpp
 	fileformat/PowerVR3.hpp
 	fileformat/SegaPVR.hpp
+	fileformat/TGA.hpp
 	fileformat/ValveVTF.hpp
 	fileformat/ValveVTF3.hpp
 	fileformat/XboxXPR.hpp

src/librptexture/fileformat/TGA.cpp
@@ -1,0 +1,542 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librptexture)                     *
+ * TGA.cpp: TrueVision TGA reader.                                         *
+ *                                                                         *
+ * Copyright (c) 2019-2021 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "stdafx.h"
+#include "TGA.hpp"
+#include "FileFormat_p.hpp"
+
+#include "tga_structs.h"
+
+// librpbase, librpfile
+using LibRpBase::rp_sprintf;
+using LibRpBase::RomFields;
+using LibRpFile::IRpFile;
+
+// librptexture
+#include "img/rp_image.hpp"
+#include "decoder/ImageDecoder.hpp"
+
+// C++ STL classes.
+using std::string;
+using std::unique_ptr;
+
+namespace LibRpTexture {
+
+FILEFORMAT_IMPL(TGA)
+
+class TGAPrivate final : public FileFormatPrivate
+{
+	public:
+		TGAPrivate(TGA *q, IRpFile *file);
+		~TGAPrivate();
+
+	private:
+		typedef FileFormatPrivate super;
+		RP_DISABLE_COPY(TGAPrivate)
+
+	public:
+		enum class TexType {
+			Unknown		= -1,
+
+			TGA1		= 0,	// Old TGA (1.0)
+			TGA2		= 1,	// New TGA (2.0)
+
+			Max
+		};
+		TexType texType;
+
+		// TGA headers.
+		TGA_Header tgaHeader;
+		TGA_ExtArea tgaExtArea;
+		TGA_Footer tgaFooter;
+
+		// Decoded image.
+		rp_image *img;
+
+		// Is HFlip/VFlip needed?
+		// Some textures may be stored upside-down due to
+		// the way GL texture coordinates are interpreted.
+		// Default without orientation metadata is HFlip=false, VFlip=false
+		rp_image::FlipOp flipOp;
+
+		/**
+		 * Load the TGA image.
+		 * @return Image, or nullptr on error.
+		 */
+		const rp_image *loadTGAImage(void);
+};
+
+/** TGAPrivate **/
+
+TGAPrivate::TGAPrivate(TGA *q, IRpFile *file)
+	: super(q, file)
+	, texType(TexType::Unknown)
+	, img(nullptr)
+	, flipOp(rp_image::FLIP_V)	// default orientation requires vertical flip
+{
+	// Clear the structs.
+	memset(&tgaHeader, 0, sizeof(tgaHeader));
+	memset(&tgaExtArea, 0, sizeof(tgaExtArea));
+	memset(&tgaFooter, 0, sizeof(tgaFooter));
+}
+
+TGAPrivate::~TGAPrivate()
+{
+	UNREF(img);
+}
+
+/**
+ * Load the .tex image.
+ * @return Image, or nullptr on error.
+ */
+const rp_image *TGAPrivate::loadTGAImage(void)
+{
+	if (img) {
+		// Image has already been loaded.
+		return img;
+	} else if (!this->file) {
+		// Can't load the image.
+		return nullptr;
+	}
+
+        // Sanity check: Maximum image dimensions of 32768x32768.
+        assert(tgaHeader.img.width > 0);
+	assert(tgaHeader.img.width <= 32768);
+        assert(tgaHeader.img.height > 0);
+	assert(tgaHeader.img.height <= 32768);
+	if (tgaHeader.img.width == 0  || tgaHeader.img.width > 32768 ||
+	    tgaHeader.img.height == 0 || tgaHeader.img.height > 32768)
+	{
+		// Invalid image dimensions.
+		return nullptr;
+	}
+
+	// Allocate a buffer for the image.
+	// NOTE: Assuming scanlines are not padded. (pitch == width)
+	int img_size = tgaHeader.img.width * tgaHeader.img.height;
+	img_size *= (tgaHeader.img.bpp == 15 ? 2 : (tgaHeader.img.bpp / 8));
+	auto img_data = aligned_uptr<uint8_t>(16, img_size);
+
+	// Read the image.
+	size_t size = file->seekAndRead(sizeof(tgaHeader), img_data.get(), img_size);
+	if (size != static_cast<size_t>(img_size)) {
+		// Seek and/or read error.
+		return nullptr;
+	}
+
+	// Decode the image.
+	// TODO: RLE; orientation bits
+	rp_image *imgtmp = nullptr;
+	switch (tgaHeader.image_type) {
+		case TGA_IMAGETYPE_COLORMAP:
+			// Palette
+			// TODO: Load the color map.
+			break;
+
+		case TGA_IMAGETYPE_TRUECOLOR:
+			// Truecolor
+			switch (tgaHeader.img.bpp) {
+				case 16:
+					// ARGB1555
+					imgtmp = ImageDecoder::fromLinear16(
+						ImageDecoder::PixelFormat::ARGB1555,
+						tgaHeader.img.width, tgaHeader.img.height,
+						reinterpret_cast<const uint16_t*>(img_data.get()), img_size);
+					break;
+
+				case 24:
+					// RGB888
+					imgtmp = ImageDecoder::fromLinear24(
+						ImageDecoder::PixelFormat::RGB888,
+						tgaHeader.img.width, tgaHeader.img.height,
+						img_data.get(), img_size);
+					break;
+
+				case 32:
+					// RGB888
+					// TODO: Verify alpha channel depth.
+					imgtmp = ImageDecoder::fromLinear32(
+						ImageDecoder::PixelFormat::ARGB8888,
+						tgaHeader.img.width, tgaHeader.img.height,
+						reinterpret_cast<const uint32_t*>(img_data.get()), img_size);
+					break;
+			}
+			break;
+
+		default:
+			assert(!"Unsupported TGA format.");
+			break;
+	}
+
+	// Post-processing: Check if a flip is needed.
+	if (imgtmp && flipOp != rp_image::FLIP_NONE) {
+		rp_image *const flipimg = imgtmp->flip(flipOp);
+		if (flipimg) {
+			imgtmp->unref();
+			imgtmp = flipimg;
+		}
+	}
+
+	img = imgtmp;
+	return img;
+}
+
+/** TGA **/
+
+/**
+ * Read a Leapster Didj .tex image file.
+ *
+ * A ROM image must be opened by the caller. The file handle
+ * will be ref()'d and must be kept open in order to load
+ * data from the ROM image.
+ *
+ * To close the file, either delete this object or call close().
+ *
+ * NOTE: Check isValid() to determine if this is a valid ROM.
+ *
+ * @param file Open ROM image.
+ */
+TGA::TGA(IRpFile *file)
+	: super(new TGAPrivate(this, file))
+{
+	RP_D(TGA);
+	d->mimeType = "image/x-tga";	// unofficial
+
+	if (!d->file) {
+		// Could not ref() the file handle.
+		return;
+	}
+
+	// Sanity check: TGA file shouldn't be larger than 16 MB.
+	const int64_t fileSize = d->file->size();
+	assert(fileSize <= 16*1024*1024);
+	if (fileSize > 16*1024*1024) {
+		UNREF_AND_NULL_NOCHK(d->file);
+		return;
+	}
+
+	// Read the .tga footer to verify if this is TGA 1.0 or 2.0.
+	size_t size = d->file->seekAndRead(fileSize - sizeof(d->tgaFooter), &d->tgaFooter, sizeof(d->tgaFooter));
+	if (size != sizeof(d->tgaFooter)) {
+		// Could not read the TGA footer.
+		// The file is likely too small to be valid.
+		UNREF_AND_NULL_NOCHK(d->file);
+		return;
+	}
+
+	// Check if it's TGA1 or TGA2.
+	if (!memcmp(d->tgaFooter.signature, TGA_SIGNATURE, sizeof(d->tgaFooter.signature))) {
+		// TGA2 signature found.
+		// Extension Area and Developer Area may be present.
+		// These would be located *after* the image data.
+		d->texType = TGAPrivate::TexType::TGA2;
+	} else {
+		// No signature. Assume TGA1.
+		d->texType = TGAPrivate::TexType::TGA1;
+	}
+
+	// Read the .tga header.
+	d->file->rewind();
+	size = d->file->read(&d->tgaHeader, sizeof(d->tgaHeader));
+	if (size != sizeof(d->tgaHeader)) {
+		// Seek and/or read error.
+		UNREF_AND_NULL_NOCHK(d->file);
+		return;
+	}
+
+	if (d->texType == TGAPrivate::TexType::TGA2) {
+		// Check for an extension area.
+		const uint32_t ext_offset = le32_to_cpu(d->tgaFooter.ext_offset);
+		if (ext_offset != 0 && fileSize > (int64_t)sizeof(d->tgaExtArea) &&
+		    (ext_offset < (fileSize - (int64_t)sizeof(d->tgaExtArea))))
+		{
+			// We have an extension area.
+			size = d->file->seekAndRead(ext_offset, &d->tgaExtArea, sizeof(d->tgaExtArea));
+			if (size != sizeof(d->tgaExtArea)) {
+				// Error reading the extension area.
+				d->tgaExtArea.size = 0;
+			}
+		}
+
+		// TODO: Developer area?
+	}
+
+	// Looks like it's valid.
+	d->isValid = true;
+
+#if SYS_BYTEORDER == SYS_LIL_ENDIAN
+	// Byteswap the header.
+	d->tgaHeader.cmap.idx0		= le16_to_cpu(d->tgaHeader.cmap.idx0);
+	d->tgaHeader.cmap.len		= le16_to_cpu(d->tgaHeader.cmap.len);
+	d->tgaHeader.img.x_origin	= le16_to_cpu(d->tgaHeader.img.x_origin);
+	d->tgaHeader.img.y_origin	= le16_to_cpu(d->tgaHeader.img.y_origin);
+	d->tgaHeader.img.width		= le16_to_cpu(d->tgaHeader.img.width);
+	d->tgaHeader.img.height		= le16_to_cpu(d->tgaHeader.img.height);
+#endif /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
+
+	// Cache the texture dimensions.
+	d->dimensions[0] = d->tgaHeader.img.width;
+	d->dimensions[1] = d->tgaHeader.img.height;
+	d->dimensions[2] = 0;
+
+	// Is a flip operation required?
+	// H-flip: Default is no; if set, flip.
+	d->flipOp = rp_image::FLIP_NONE;
+	if (d->tgaHeader.img.alpha_dir & TGA_ORIENTATION_X_MASK) {
+		d->flipOp = rp_image::FLIP_H;
+	}
+	// V-flip: Default is yes; if set, don't flip.
+	if (!(d->tgaHeader.img.alpha_dir & TGA_ORIENTATION_Y_MASK)) {
+		d->flipOp = static_cast<rp_image::FlipOp>(d->flipOp | rp_image::FLIP_V);
+	}
+}
+
+/** Class-specific functions that can be used even if isValid() is false. **/
+
+/**
+ * Get a list of all supported file extensions.
+ * This is to be used for file type registration;
+ * subclasses don't explicitly check the extension.
+ *
+ * NOTE: The extensions include the leading dot,
+ * e.g. ".bin" instead of "bin".
+ *
+ * NOTE 2: The array and the strings in the array should
+ * *not* be freed by the caller.
+ *
+ * @return NULL-terminated array of all supported file extensions, or nullptr on error.
+ */
+const char *const *TGA::supportedFileExtensions_static(void)
+{
+	static const char *const exts[] = {
+		".tga",
+		// TODO: Other obsolete file extensions?
+
+		nullptr
+	};
+	return exts;
+}
+
+/**
+ * Get a list of all supported MIME types.
+ * This is to be used for metadata extractors that
+ * must indicate which MIME types they support.
+ *
+ * NOTE: The array and the strings in the array should
+ * *not* be freed by the caller.
+ *
+ * @return NULL-terminated array of all supported file extensions, or nullptr on error.
+ */
+const char *const *TGA::supportedMimeTypes_static(void)
+{
+	static const char *const mimeTypes[] = {
+		// Unofficial MIME types from FreeDesktop.org.
+		"image/x-tga",
+
+		nullptr
+	};
+	return mimeTypes;
+}
+
+/** Property accessors **/
+
+/**
+ * Get the texture format name.
+ * @return Texture format name, or nullptr on error.
+ */
+const char *TGA::textureFormatName(void) const
+{
+	RP_D(const TGA);
+	if (!d->isValid || (int)d->texType < 0)
+		return nullptr;
+
+	return "TrueVision TGA";
+}
+
+/**
+ * Get the pixel format, e.g. "RGB888" or "DXT1".
+ * @return Pixel format, or nullptr if unavailable.
+ */
+const char *TGA::pixelFormat(void) const
+{
+	RP_D(const TGA);
+	if (!d->isValid || (int)d->texType < 0) {
+		// Not supported.
+		return nullptr;
+	}
+
+	const char *fmt = nullptr;
+	switch (d->tgaHeader.image_type) {
+		case TGA_IMAGETYPE_COLORMAP:
+		case TGA_IMAGETYPE_RLE_COLORMAP:
+			// Palette
+			if (d->tgaHeader.cmap.len <= 256) {
+				// 8bpp
+				switch (d->tgaHeader.cmap.bpp) {
+					case 15:
+						fmt = "8bpp with RGB555 palette";
+						break;
+					case 16:
+						fmt = "8bpp with ARGB1555 palette";
+						break;
+					case 24:
+						fmt = "8bpp with RGB888 palette";
+						break;
+					case 32:
+						fmt = "8bpp with ARGB8888 palette";
+						break;
+					default:
+						break;
+				}
+			} else {
+				// 16bpp
+				switch (d->tgaHeader.cmap.bpp) {
+					case 15:
+						fmt = "16bpp with RGB555 palette";
+						break;
+					case 16:
+						fmt = "16bpp with ARGB1555 palette";
+						break;
+					case 24:
+						fmt = "16bpp with RGB888 palette";
+						break;
+					case 32:
+						fmt = "16bpp with ARGB8888 palette";
+						break;
+					default:
+						break;
+				}
+			}
+			break;
+
+		case TGA_IMAGETYPE_TRUECOLOR:
+		case TGA_IMAGETYPE_RLE_TRUECOLOR:
+			// True color
+			switch (d->tgaHeader.img.bpp) {
+				case 16:
+					fmt = "ARGB1555";
+					break;
+				case 24:
+					fmt = "RGB888";
+					break;
+				case 32:
+					fmt = "ARGB8888";
+					break;
+				default:
+					break;
+			}
+			break;
+
+		case TGA_IMAGETYPE_GRAYSCALE:
+		case TGA_IMAGETYPE_RLE_GRAYSCALE:
+			// Grayscale
+			switch (d->tgaHeader.img.bpp) {
+				case 8:
+					fmt = "8bpp grayscale";
+					break;
+				default:
+					break;
+			}
+			break;
+
+		default:
+			break;
+	}
+
+	// TODO: Indicate invalid formats?
+	return fmt;
+}
+
+/**
+ * Get the mipmap count.
+ * @return Number of mipmaps. (0 if none; -1 if format doesn't support mipmaps)
+ */
+int TGA::mipmapCount(void) const
+{
+	// Not supported.
+	return -1;
+}
+
+#ifdef ENABLE_LIBRPBASE_ROMFIELDS
+/**
+ * Get property fields for rom-properties.
+ * @param fields RomFields object to which fields should be added.
+ * @return Number of fields added, or 0 on error.
+ */
+int TGA::getFields(RomFields *fields) const
+{
+	// TODO: Localization.
+#define C_(ctx, str) str
+#define NOP_C_(ctx, str) str
+
+	assert(fields != nullptr);
+	if (!fields)
+		return 0;
+
+	RP_D(TGA);
+	if (!d->isValid || (int)d->texType < 0) {
+		// Not valid.
+		return -EIO;
+	}
+
+	const int initial_count = fields->count();
+	fields->reserve(initial_count + 1);	// Maximum of 1 field. (TODO)
+
+	// TGA header.
+	const TGA_Header *const tgaHeader = &d->tgaHeader;
+
+	// Orientation.
+	// Uses KTX1 format for display.
+	// Default 00 orientation: H-flip NO, V-flip YES
+	char str[16];
+	snprintf(str, sizeof(str), "S=%c,T=%c",
+		((tgaHeader->img.alpha_dir & TGA_ORIENTATION_X_MASK) ? 'l' : 'r'),
+		((tgaHeader->img.alpha_dir & TGA_ORIENTATION_Y_MASK) ? 'd' : 'u'));
+	fields->addField_string(C_("TGA", "Orientation"), str);
+
+	// Finished reading the field data.
+	return (fields->count() - initial_count);
+}
+#endif /* ENABLE_LIBRPBASE_ROMFIELDS */
+
+/** Image accessors **/
+
+/**
+ * Get the image.
+ * For textures with mipmaps, this is the largest mipmap.
+ * The image is owned by this object.
+ * @return Image, or nullptr on error.
+ */
+const rp_image *TGA::image(void) const
+{
+	RP_D(const TGA);
+	if (!d->isValid || (int)d->texType < 0) {
+		// Unknown file type.
+		return nullptr;
+	}
+
+	// Load the image.
+	return const_cast<TGAPrivate*>(d)->loadTGAImage();
+}
+
+/**
+ * Get the image for the specified mipmap.
+ * Mipmap 0 is the largest image.
+ * @param mip Mipmap number.
+ * @return Image, or nullptr on error.
+ */
+const rp_image *TGA::mipmap(int mip) const
+{
+	// Allowing mipmap 0 for compatibility.
+	if (mip == 0) {
+		return image();
+	}
+	return nullptr;
+}
+
+}

src/librptexture/fileformat/TGA.hpp
@@ -1,0 +1,21 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librptexture)                     *
+ * TGA.hpp: TrueVision TGA reader.                                         *
+ *                                                                         *
+ * Copyright (c) 2019-2021 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_TGA_HPP__
+#define __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_TGA_HPP__
+
+#include "FileFormat.hpp"
+
+namespace LibRpTexture {
+
+FILEFORMAT_DECL_BEGIN(TGA)
+FILEFORMAT_DECL_END()
+
+}
+
+#endif /* __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_TGA_HPP__ */

src/librptexture/fileformat/tga_structs.h
@@ -1,0 +1,191 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librptexture)                     *
+ * tga_structs.h: TrueVision TGA texture format data structures.           *
+ *                                                                         *
+ * Copyright (c) 2017-2021 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_TGA_STRUCTS_H__
+#define __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_TGA_STRUCTS_H__
+
+#include <stdint.h>
+#include "common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// References:
+// - https://en.wikipedia.org/wiki/Truevision_TGA
+// - https://www.ludorg.net/amnesia/TGA_File_Format_Spec.html
+// - http://www.paulbourke.net/dataformats/tga/
+
+// NOTE: 16-bit color is 15-bit RGB + 1-bit transparency.
+
+/**
+ * TrueVision TGA: Main header.
+ * All fields are little-endian.
+ */
+#pragma pack(1)
+typedef struct PACKED _TGA_Header {
+	uint8_t id_length;		// [0x000] Length of ID field. (0 if not present)
+	uint8_t color_map_type;		// [0x001] Color map type: 0 if none, 1 if present.
+	uint8_t image_type;		// [0x002] Image type. (See TGA_ImageType_e.)
+	struct {
+		uint16_t idx0;		// [0x003] Index of first color map entry.
+		uint16_t len;		// [0x005] Number of entries in color map.
+		uint8_t bpp;		// [0x007] Bits per pixel in color map. (15/16/24/32)
+	} cmap;
+	struct {
+		uint16_t x_origin;	// [0x008] Lower-left corner for displays where
+		uint16_t y_origin;	// [0x00A]     the origin is at the lower left.
+		uint16_t width;		// [0x00C]
+		uint16_t height;	// [0x00E]
+		uint8_t bpp;		// [0x010] Color depth (8/15/16/24/32)
+		uint8_t alpha_dir;	// [0x011] Bits 3-0 == alpha ch depth
+					//         Bits 5-4 == orientation (see TGA_Orientation_e)
+	} img;
+} TGA_Header;
+ASSERT_STRUCT(TGA_Header, 18);
+#pragma pack()
+
+/**
+ * TGA image type.
+ */
+typedef enum {
+	TGA_IMAGETYPE_NONE	= 0,	// No image data
+	TGA_IMAGETYPE_COLORMAP	= 1,	// Color-mapped image (8/15/16 bpp)
+	TGA_IMAGETYPE_TRUECOLOR	= 2,	// True color image
+	TGA_IMAGETYPE_GRAYSCALE	= 3,	// Grayscale image (8 bpp)
+
+	TGA_IMAGETYPE_RLE_FLAG		= 8,	// Flag indicates image is compressed using RLE.
+	TGA_IMAGETYPE_RLE_COLORMAP	= (TGA_IMAGETYPE_COLORMAP | TGA_IMAGETYPE_RLE_FLAG),
+	TGA_IMAGETYPE_RLE_TRUECOLOR	= (TGA_IMAGETYPE_TRUECOLOR | TGA_IMAGETYPE_RLE_FLAG),
+	TGA_IMAGETYPE_RLE_GRAYSCALE	= (TGA_IMAGETYPE_GRAYSCALE | TGA_IMAGETYPE_RLE_FLAG),
+} TGA_ImageType_e;
+
+/**
+ * TGA image orientation.
+ */
+typedef enum {
+	TGA_ORIENTATION_X_LTR	= (0U << 4),	// X: left-to-right
+	TGA_ORIENTATION_X_RTL	= (1U << 4),	// X: right-to-left
+	TGA_ORIENTATION_X_MASK	= (1U << 4),
+
+	TGA_ORIENTATION_Y_UP	= (0U << 5),	// Y: bottom-to-top
+	TGA_ORIENTATION_Y_DOWN	= (1U << 5),	// Y: top-to-bottom
+	TGA_ORIENTATION_Y_MASK	= (1U << 5),
+} TGA_Orientation_e;
+
+/**
+ * TrueVision TGA: Date stamp.
+ * All fields are little-endian.
+ */
+typedef struct _TGA_DateStamp {
+	uint16_t month;	// [0x000] 1-12
+	uint16_t day;	// [0x002] 1-31
+	uint16_t year;	// [0x004] 4-digit year, e.g. 1989
+	uint16_t hour;	// [0x006] 0-23
+	uint16_t min;	// [0x008] 0-59
+	uint16_t sec;	// [0x00A] 0-59
+} TGA_DateStamp;
+ASSERT_STRUCT(TGA_DateStamp, 6*sizeof(uint16_t));
+
+/**
+ * TrueVision TGA: Elapsed time.
+ * All fields are little-endian.
+ */
+typedef struct _TGA_ElapsedTime {
+	uint16_t hours;	// [0x000] 0-65535
+	uint16_t mins;	// [0x002] 0-59
+	uint16_t secs;	// [0x004] 0-59
+} TGA_ElapsedTime;
+ASSERT_STRUCT(TGA_ElapsedTime, 3*sizeof(uint16_t));
+
+/**
+ * TrueVision TGA: Software version.
+ * All fields are little-endian.
+ */
+#pragma pack(1)
+typedef struct PACKED _TGA_SW_Version {
+	uint16_t number;	// [0x000] Version number * 100 (0 for unused)
+				//             Example: 213 for version 2.13
+	char letter;		// [0x002] Version letter suffix (' ' for unused)
+				//             Example: 'b' for version 2.13b
+} TGA_SW_Version;
+#pragma pack()
+ASSERT_STRUCT(TGA_SW_Version, 3);
+
+/**
+ * TrueVision TGA: Ratio values.
+ * Used for pixel aspect ratio and gamma values.
+ * All fields are little-endian.
+ */
+typedef struct _TGA_Ratio {
+	uint16_t numerator;	// [0x000] Numerator
+	uint16_t denominator;	// [0x002] Denominator
+} TGA_Ratio;
+ASSERT_STRUCT(TGA_Ratio, 2*sizeof(uint16_t));
+
+/**
+ * TrueVision TGA: Extension area.
+ * Present if the size field is 495.
+ * All fields are little-endian.
+ */
+#pragma pack(1)
+typedef struct PACKED _TGA_ExtArea {
+	uint16_t size;			// [0x000] Extension area size. (Always 495)
+	char author_name[41];		// [0x002] Author name, NULL-terminated.
+	char author_comment[4][81];	// [0x02B] Comment lines, NULL-terminated.
+	TGA_DateStamp timestamp;	// [0x16F] Timestamp
+	char job_id[41];		// [0x17B] Job ID
+	TGA_ElapsedTime job_time;	// [0x1A4] Time taken to create the job
+	char software_id[41];		// [0x1AA] Application that created the file
+	TGA_SW_Version sw_version;	// [0x1D3] Software version
+	uint32_t key_color;		// [0x1D6] Key color (ARGB)
+	TGA_Ratio pixel_aspect_ratio;	// [0x1DA] Num=W, Denom=H
+	TGA_Ratio gamma_value;		// [0x1DE]
+
+	// Data offsets if non-zero.
+	uint32_t color_correction_offset;	// [0x1E2]
+	uint32_t postage_stamp_offset;		// [0x1E6]
+	uint32_t scan_line_offset;		// [0x1EA]
+
+	uint8_t attributes_type;	// [0x1EE] Alpha channel type (see TGA_AlphaType_e)
+} TGA_ExtArea;
+ASSERT_STRUCT(TGA_ExtArea, 495);
+#pragma pack()
+
+/**
+ * TGA alpha channel type.
+ */
+typedef enum {
+	TGA_ALPHATYPE_UNDEFINED_IGNORE	= 0,
+	TGA_ALPHATYPE_UNDEFINED_RETAIN	= 1,
+	TGA_ALPHATYPE_PRESENT		= 2,	// Standard alpha
+	TGA_ALPHATYPE_PREMULTIPLIED	= 3,	// Premultiplied alpha
+} TGA_AlphaType_e;
+
+/**
+ * TrueVision TGA: Footer.
+ * Optional, but present in most TGA v2.0 files.
+ * All fields are little-endian.
+ */
+#define TGA_SIGNATURE "TRUEVISION-XFILE"
+#pragma pack(1)
+typedef struct PACKED _TGA_Footer {
+	uint32_t ext_offset;		// [0x000] Offset from the beginning of the file.
+	uint32_t dev_area_offset;	// [0x004] Offset from the beginning of the file.
+	char signature[16];		// [0x008]
+	char dot;			// [0x018] '.'
+	char null_byte;			// [0x019] '\0'
+} TGA_Footer;
+#pragma pack()
+ASSERT_STRUCT(TGA_Footer, 26);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ROMPROPERTIES_LIBRPTEXTURE_FILEFORMAT_TGA_STRUCTS_H__ */

xdg/mime.no-thumbnail.types
@@ -64,3 +64,8 @@ application/x-mach-sharedlib		# MachO
 application/x-mach-core			# MachO
 application/x-mach-bundle		# MachO
 application/x-mach-fat-binary		# MachO
+
+# Texture
+# Formats listed here are widely supported by Linux desktop
+# environments, but not other systems.
+image/x-tga				# TGA

commit cd24672b8e1a24847651250d854b20ac1bf66927

[librptexture] FileFormatFactory: Make sure `ext` isn't nullptr before checking it.
Otherwise, we'll crash if checking a file without an extension.

This fixes a regression from commit b8ff049997820ee8af6f844591d5f4bec4366b4c.
([librptexture] Initial support for TGA format.)

src/librptexture/FileFormatFactory.cpp
@@ -140,7 +140,7 @@ FileFormat *FileFormatFactory::create(IRpFile *file)
 	// for now.
 	const string filename = file->filename();
 	const char *ext = FileSystem::file_ext(filename);
-	if (!strcasecmp(ext, ".tga")) {
+	if (ext && !strcasecmp(ext, ".tga")) {
 		// TGA file.
 		FileFormat *const fileFormat = new TGA(file);
 		if (fileFormat->isValid()) {