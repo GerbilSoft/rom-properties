GerbilSoft/rom-properties

commit 4e91b8646af0987facfedee481da74760ff098b6

[libromdata] GameCube: Added the ViewedDebugCryptedFile achievement.
GameCubePrivate::loadWiiPartitionTables(): Set wiiPtblLoaded = true.
I somehow forgot to do this, but it never caused problems before.
With the achievements code, it *does* cause problems, since the disc
image is closed before the achievements are checked.

Rebase notes:
- Handle all three Wii debug keys.
- EncKey was changed to `enum class` in feature/enum-class, which was
  merged to master on 2020/06/21.

src/libromdata/Console/GameCube.cpp
@@ -22,6 +22,7 @@
 // librpbase, librpfile, librptexture
 #include "librpfile/DualFile.hpp"
 #include "librpfile/RelatedFile.hpp"
+#include "librpbase/Achievements.hpp"
 #include "librpbase/SystemRegion.hpp"
 using namespace LibRpBase;
 using namespace LibRpFile;
@@ -392,6 +393,7 @@ int GameCubePrivate::loadWiiPartitionTables(void)
 	}
 
 	// Done reading the partition tables.
+	wiiPtblLoaded = true;
 	return 0;
 }
 
@@ -1611,7 +1613,7 @@ int GameCube::loadFieldData(void)
 	/** Wii-specific fields. **/
 
 	// Load the Wii partition tables.
-	int wiiPtLoaded = d->loadWiiPartitionTables();
+	const int wiiPtLoaded = d->loadWiiPartitionTables();
 
 	// TMD fields.
 	if (d->gamePartition) {
@@ -1878,7 +1880,7 @@ int GameCube::loadFieldData(void)
 			if ((int)encKey >= 0 && (int)encKey < ARRAY_SIZE(wii_key_tbl)) {
 				s_key_name = dpgettext_expr(RP_I18N_DOMAIN, "GameCube|KeyIdx", wii_key_tbl[(int)encKey]);
 			} else {
-				// WiiPartition::ENCKEY_UNKNOWN
+				// WiiPartition::EncKey::Unknown
 				s_key_name = C_("RomData", "Unknown");
 			}
 			data_row.emplace_back(s_key_name);
@@ -2219,6 +2221,53 @@ int GameCube::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) c
 
 	// All URLs added.
 	return 0;
+}
+
+/**
+ * Check for "viewed" achievements.
+ *
+ * @return Number of achievements unlocked.
+ */
+int GameCube::checkViewedAchievements(void) const
+{
+	RP_D(const GameCube);
+	if (!d->isValid || ((d->discType & GameCubePrivate::DISC_SYSTEM_MASK) != GameCubePrivate::DISC_SYSTEM_WII)) {
+		// Disc is either not valid or is not Wii.
+		return 0;
+	}
+
+	Achievements *const pAch = Achievements::instance();
+	int ret = 0;
+
+	const int wiiPtLoaded = const_cast<GameCubePrivate*>(d)->loadWiiPartitionTables();
+	if (wiiPtLoaded == 0) {
+		// Wii partition tables loaded.
+		WiiPartition::EncKey encKey;
+		if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_NASOS) {
+			// NASOS disc image.
+			// If this would normally be an encrypted image, use encKeyReal().
+			encKey = (d->discHeader.disc_noCrypto == 0
+				? d->gamePartition->encKeyReal()
+				: d->gamePartition->encKey());
+		} else {
+			// Other disc image. Use encKey().
+			encKey = d->gamePartition->encKey();
+		}
+
+		switch (encKey) {
+			default:
+				break;
+			case WiiPartition::EncKey::RVT_Debug:
+			case WiiPartition::EncKey::RVT_Korean:
+			case WiiPartition::EncKey::CAT_vWii:
+				// Debug encryption.
+				pAch->unlock(Achievements::ID::ViewedDebugCryptedFile);
+				ret++;
+				break;
+		}
+	}
+
+	return ret;
 }
 
 }

src/libromdata/Console/GameCube.hpp
@@ -20,6 +20,7 @@ ROMDATA_DECL_IMGSUPPORT()
 ROMDATA_DECL_IMGPF()
 ROMDATA_DECL_IMGINT()
 ROMDATA_DECL_IMGEXT()
+ROMDATA_DECL_VIEWED_ACHIEVEMENTS()
 ROMDATA_DECL_END()
 
 }

src/libromdata/disc/WiiPartition.hpp
@@ -118,6 +118,7 @@ class WiiPartition : public GcnPartition
 			CAT_vWii = 5,		// vWii key (debug)
 
 			None = 6,		// No encryption (RVT-H)
+
 			Max
 		};
 

commit b85448318417ed3afa6878ced3aa18fab103de57

[libromdata] GameCube::checkViewedAchievements(): Don't crash if this is a standalone Wii partition image.
If d->gamePartition is nullptr, check d->updatePartition.
If that's nullptr, don't bother checking anything.

rpcli works fine, since it doesn't check achievements.
The UI frontends crashed when the tab was activated.

This fixes a regression from commit 4e91b8646af0987facfedee481da74760ff098b6.
([libromdata] GameCube: Added the ViewedDebugCryptedFile achievement.)

src/libromdata/Console/GameCube.cpp
@@ -2235,34 +2235,46 @@ int GameCube::checkViewedAchievements(void) const
 	}
 
 	Achievements *const pAch = Achievements::instance();
-	int ret = 0;
 
 	const int wiiPtLoaded = const_cast<GameCubePrivate*>(d)->loadWiiPartitionTables();
-	if (wiiPtLoaded == 0) {
-		// Wii partition tables loaded.
-		WiiPartition::EncKey encKey;
-		if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_NASOS) {
-			// NASOS disc image.
-			// If this would normally be an encrypted image, use encKeyReal().
-			encKey = (d->discHeader.disc_noCrypto == 0
-				? d->gamePartition->encKeyReal()
-				: d->gamePartition->encKey());
-		} else {
-			// Other disc image. Use encKey().
-			encKey = d->gamePartition->encKey();
-		}
+	if (wiiPtLoaded != 0) {
+		// Unable to load Wii partition tables.
+		return 0;
+	}
 
-		switch (encKey) {
-			default:
-				break;
-			case WiiPartition::EncKey::RVT_Debug:
-			case WiiPartition::EncKey::RVT_Korean:
-			case WiiPartition::EncKey::CAT_vWii:
-				// Debug encryption.
-				pAch->unlock(Achievements::ID::ViewedDebugCryptedFile);
-				ret++;
-				break;
-		}
+	// Check for the main partition.
+	const WiiPartition *pt = d->gamePartition;
+	if (!pt) {
+		pt = d->updatePartition;
+	}
+	if (!pt) {
+		// No partitions...
+		return 0;
+	}
+
+	int ret = 0;
+	WiiPartition::EncKey encKey;
+	if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_NASOS) {
+		// NASOS disc image.
+		// If this would normally be an encrypted image, use encKeyReal().
+		encKey = (d->discHeader.disc_noCrypto == 0
+			? pt->encKeyReal()
+			: pt->encKey());
+	} else {
+		// Other disc image. Use encKey().
+		encKey = pt->encKey();
+	}
+
+	switch (encKey) {
+		default:
+			break;
+		case WiiPartition::EncKey::RVT_Debug:
+		case WiiPartition::EncKey::RVT_Korean:
+		case WiiPartition::EncKey::CAT_vWii:
+			// Debug encryption.
+			pAch->unlock(Achievements::ID::ViewedDebugCryptedFile);
+			ret++;
+			break;
 	}
 
 	return ret;