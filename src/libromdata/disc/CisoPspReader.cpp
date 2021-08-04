GerbilSoft/rom-properties

commit 4c7232faeece657d6791ad57b34eed52c6b0b8ab

Use get_crc_table() instead of zlibVersion() for zlib delayload.
If zlib is compiled with a dynamic CRC table, we need to call
get_crc_table() anyway in order to initialize it, so this ensures
that get_crc_table() is called.

src/libromdata/disc/CisoPspReader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * CisoPspReader.cpp: PlayStation Portable CISO disc image reader.         *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2021 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -50,7 +50,7 @@ namespace LibRomData {
 
 #ifdef _MSC_VER
 // DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(zlibVersion);
+DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 #  ifdef HAVE_LZ4
 DELAYLOAD_TEST_FUNCTION_IMPL0(LZ4_versionNumber);
 #  endif /* HAVE_LZ4 */
@@ -224,10 +224,8 @@ CisoPspReader::CisoPspReader(IRpFile *file)
 	}
 
 	unsigned int indexEntryTblPos;
+	bool isZlib = false;	// If zlib, we have to call get_crc_table().
 #ifdef _MSC_VER
-#  ifdef ZLIB_IS_DLL
-	bool isZlib = false;
-#  endif /* ZLIB_IS_DLL */
 #  if defined(HAVE_LZ4) && defined(LZ4_IS_DLL)
 	bool isLZ4 = false;
 #  endif /* HAVE_LZ4 && LZ4_IS_DLL */
@@ -244,9 +242,8 @@ CisoPspReader::CisoPspReader(IRpFile *file)
 			return;
 
 		case CisoPspReaderPrivate::CisoType::CISO:
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 			isZlib = true;
-#endif /* _MSC_VER && ZLIB_IS_DLL */
+			break;
 #ifdef HAVE_LZ4
 		case CisoPspReaderPrivate::CisoType::ZISO:
 #endif /* HAVE_LZ4 */
@@ -304,11 +301,9 @@ CisoPspReader::CisoPspReader(IRpFile *file)
 					isLZO = true;
 					break;
 #  endif /* HAVE_LZO && LZO_IS_DLL */
-#  ifdef ZLIB_IS_DLL
 				case JISO_METHOD_ZLIB:
 					isZlib = true;
 					break;
-#  endif /* ZLIB_IS_DLL */
 			}
 #endif /* _MSC_VER */
 
@@ -319,9 +314,7 @@ CisoPspReader::CisoPspReader(IRpFile *file)
 			break;
 
 		case CisoPspReaderPrivate::CisoType::DAX:
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 			isZlib = true;
-#endif /* _MSC_VER && ZLIB_IS_DLL */
 #if SYS_BYTEORDER != SYS_LIL_ENDIAN
 			// Byteswap the header.
 			d->header.dax.magic		= le32_to_cpu(d->header.dax.magic);
@@ -342,7 +335,7 @@ CisoPspReader::CisoPspReader(IRpFile *file)
 	// TODO: Only if linked with /DELAYLOAD?
 #  ifdef ZLIB_IS_DLL
 	if (isZlib) {
-		if (DelayLoad_test_zlibVersion() != 0) {
+		if (DelayLoad_test_get_crc_table() != 0) {
 			// Delay load for zlib failed.
 			UNREF_AND_NULL_NOCHK(m_file);
 			return;
@@ -368,6 +361,14 @@ CisoPspReader::CisoPspReader(IRpFile *file)
 	}
 #  endif /* HAVE_LZ4 && LZ4_IS_DLL */
 #endif /* _MSC_VER */
+
+#if !defined(_MSC_VER) || !defined(ZLIB_IS_DLL)
+	if (isZlib) {
+		// zlib isn't in a DLL, but we need to ensure that the
+		// CRC table is initialized anyway.
+		get_crc_table();
+	}
+#endif /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
 
 	// Calculate the number of blocks.
 	const uint32_t num_blocks = static_cast<uint32_t>(d->disc_size / d->block_size);
@@ -505,7 +506,7 @@ int CisoPspReader::isDiscSupported_static(const uint8_t *pHeader, size_t szHeade
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// GCZ is not supported without zlib.
 		return -1;

src/libromdata/Console/iQuePlayer.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * iQuePlayer.cpp: iQue Player .cmd reader.                                *
  *                                                                         *
- * Copyright (c) 2019-2020 by David Korth.                                 *
+ * Copyright (c) 2019-2021 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -38,7 +38,7 @@ ROMDATA_IMPL_IMG(iQuePlayer)
 
 #ifdef _MSC_VER
 // DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(zlibVersion);
+DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 #endif /* _MSC_VER */
 
 class iQuePlayerPrivate final : public RomDataPrivate
@@ -232,11 +232,15 @@ rp_image *iQuePlayerPrivate::loadImage(off64_t address, size_t z_size, size_t un
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// Can't decompress the thumbnail image.
 		return nullptr;
 	}
+#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
+	// zlib isn't in a DLL, but we need to ensure that the
+	// CRC table is initialized anyway.
+	get_crc_table();
 #endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 	// Read the compressed thumbnail image.

src/libromdata/disc/GczReader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * GczReader.cpp: GameCube/Wii GCZ disc image reader.                      *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2021 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -35,7 +35,7 @@ namespace LibRomData {
 
 #ifdef _MSC_VER
 // DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(zlibVersion);
+DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 #endif /* _MSC_VER */
 
 class GczReaderPrivate : public SparseDiscReaderPrivate {
@@ -124,12 +124,16 @@ GczReader::GczReader(IRpFile *file)
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// GCZ is not supported without zlib.
 		UNREF_AND_NULL_NOCHK(m_file);
 		return;
 	}
+#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
+		// zlib isn't in a DLL, but we need to ensure that the
+		// CRC table is initialized anyway.
+		get_crc_table();
 #endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 	// Read the GCZ header.
@@ -289,11 +293,15 @@ int GczReader::isDiscSupported_static(const uint8_t *pHeader, size_t szHeader)
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// GCZ is not supported without zlib.
 		return -1;
 	}
+#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
+	// zlib isn't in a DLL, but we need to ensure that the
+	// CRC table is initialized anyway.
+	get_crc_table();
 #endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 	// Check the GCZ magic.

src/librpbase/Achievements.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * Achievements.cpp: Achievements class.                                   *
  *                                                                         *
- * Copyright (c) 2020 by David Korth.                                      *
+ * Copyright (c) 2020-2021 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -43,7 +43,7 @@ namespace LibRpBase {
 
 #ifdef _MSC_VER
 // DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(zlibVersion);
+DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 #endif /* _MSC_VER */
 
 class AchievementsPrivate
@@ -351,11 +351,15 @@ int AchievementsPrivate::save(void) const
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// Can't calculate the CRC32.
 		return -ENOTSUP;
 	}
+#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
+	// zlib isn't in a DLL, but we need to ensure that the
+	// CRC table is initialized anyway.
+	get_crc_table();
 #endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 	// Create the achievements file in memory.
@@ -497,7 +501,7 @@ int AchievementsPrivate::load(void)
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// Can't calculate the CRC32.
 		return -ENOTSUP;
@@ -787,7 +791,7 @@ int Achievements::unlock(ID id, int bit)
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// We won't be able to calculate CRC32s, so don't
 		// enable achievements at all.
@@ -897,7 +901,7 @@ time_t Achievements::isUnlocked(ID id) const
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// We won't be able to calculate CRC32s, so don't
 		// enable achievements at all.

src/librpbase/img/RpPng.cpp
@@ -27,6 +27,7 @@ using LibRpTexture::argb32_t;
 using std::unique_ptr;
 
 // Image format libraries.
+#include <zlib.h>	// get_crc_table()
 #include <png.h>
 
 #if PNG_LIBPNG_VER < 10209 || \
@@ -70,7 +71,7 @@ static int DelayLoad_test_zlib_and_png(void)
 	static bool success = false;
 	if (!success) {
 		__try {
-			zlibVersion();
+			get_crc_table();
 			png_access_version_number();
 		} __except (DelayLoad_filter_zlib_and_png(GetExceptionCode())) {
 			return -ENOTSUP;
@@ -571,6 +572,10 @@ rp_image *RpPng::load(IRpFile *file)
 		// Delay load failed.
 		return nullptr;
 	}
+#else /* !defined(_MSC_VER) || (!defined(ZLIB_IS_DLL) && !defined(PNG_IS_DLL)) */
+	// zlib isn't in a DLL, but we need to ensure that the
+	// CRC table is initialized anyway.
+	get_crc_table();
 #endif /* defined(_MSC_VER) && (defined(ZLIB_IS_DLL) || defined(PNG_IS_DLL)) */
 
 	// Rewind the file.

src/librpbase/img/RpPngWriter.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RpPngWriter.cpp: PNG image writer.                                      *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2021 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -28,6 +28,7 @@ using LibRpTexture::argb32_t;
 #include "APNG_dlopen.h"
 
 // libpng
+#include <zlib.h>	// get_crc_table()
 #include <png.h>
 
 #if PNG_LIBPNG_VER < 10209 || \
@@ -99,7 +100,7 @@ static int DelayLoad_test_zlib_and_png(void)
 	static bool success = false;
 	if (!success) {
 		__try {
-			zlibVersion();
+			get_crc_table();
 			png_access_version_number();
 		} __except (DelayLoad_filter_zlib_and_png(GetExceptionCode())) {
 			return -ENOTSUP;
@@ -373,6 +374,10 @@ void RpPngWriterPrivate::init(IRpFile *file, int width, int height, rp_image::Fo
 		lastError = ENOTSUP;
 		return;
 	}
+#else /* !defined(_MSC_VER) || (!defined(ZLIB_IS_DLL) && !defined(PNG_IS_DLL)) */
+	// zlib isn't in a DLL, but we need to ensure that the
+	// CRC table is initialized anyway.
+	get_crc_table();
 #endif /* defined(_MSC_VER) && (defined(ZLIB_IS_DLL) || defined(PNG_IS_DLL)) */
 
 	if (!file->isOpen()) {

src/librpfile/win32/RpFile_win32.cpp
@@ -32,7 +32,7 @@ namespace LibRpFile {
 
 #ifdef _MSC_VER
 // DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(zlibVersion);
+DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 #endif /* _MSC_VER */
 
 /** RpFilePrivate **/
@@ -319,11 +319,15 @@ void RpFile::init(void)
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 		// Delay load verification.
 		// TODO: Only if linked with /DELAYLOAD?
-		if (DelayLoad_test_zlibVersion() != 0) {
+		if (DelayLoad_test_get_crc_table() != 0) {
 			// Delay load failed.
 			// Don't do any gzip checking.
 			return;
 		}
+#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
+		// zlib isn't in a DLL, but we need to ensure that the
+		// CRC table is initialized anyway.
+		get_crc_table();
 #endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 		DWORD bytesRead;
@@ -370,9 +374,6 @@ void RpFile::init(void)
 							// underlying Windows handle.
 							int gzfd_dup = _open_osfhandle((intptr_t)hGzDup, _O_RDONLY);
 							if (gzfd_dup >= 0) {
-								// Make sure the CRC32 table is initialized.
-								get_crc_table();
-
 								d->gzfd = gzdopen(gzfd_dup, "r");
 								if (d->gzfd) {
 									m_isCompressed = true;

src/librptexture/fileformat/DidjTex.cpp
@@ -39,7 +39,7 @@ FILEFORMAT_IMPL(DidjTex)
 
 #ifdef _MSC_VER
 // DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(zlibVersion);
+DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 #endif /* _MSC_VER */
 
 class DidjTexPrivate final : public FileFormatPrivate
@@ -124,11 +124,15 @@ const rp_image *DidjTexPrivate::loadDidjTexImage(void)
 #if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
 	// Delay load verification.
 	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_zlibVersion() != 0) {
+	if (DelayLoad_test_get_crc_table() != 0) {
 		// Delay load failed.
 		// Can't decompress the thumbnail image.
 		return nullptr;
 	}
+#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
+	// zlib isn't in a DLL, but we need to ensure that the
+	// CRC table is initialized anyway.
+	get_crc_table();
 #endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 	// Load the compressed data.

commit 7f818ff8cf1f675981daaabb7dceb8054dd77cc5

[libromdata] CisoPspReader.cpp: Fix regression that resulted in division by zero when reading CISO/CSO images.
This fixes a regression from commit 4c7232faeece657d6791ad57b34eed52c6b0b8ab.
(Use get_crc_table() instead of zlibVersion() for zlib delayload.)

This bug was reported by @NotaInutilis in issue #286.

src/libromdata/disc/CisoPspReader.cpp
@@ -243,7 +243,7 @@ CisoPspReader::CisoPspReader(IRpFile *file)
 
 		case CisoPspReaderPrivate::CisoType::CISO:
 			isZlib = true;
-			break;
+			// fall-through
 #ifdef HAVE_LZ4
 		case CisoPspReaderPrivate::CisoType::ZISO:
 #endif /* HAVE_LZ4 */