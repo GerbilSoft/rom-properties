GerbilSoft/rom-properties

commit b63689dd7c922ccba9938ab383f41303a71445ba

[librptexture] GodotSTEX: Handle ETCn and PVRTC images that have sizes that aren't powers of 2.
PVRTC images are always internally powers of 2, though unlike ETCn, Godot
doesn't seem to set rescaling parameters. We'll need to set that manually.
NOTE: PVRTC-II supports NPOT, but we're using a slightly-modified PVRTC-I
decoder, so it doesn't support NPOT.

calcImageSizePVRTC_PoT(): New function to calculate the PVRTC image size,
adjusting for powers of 2.

Godot 3's PVRTC textures are handled the same as ETCn with regards to
image dimensions (they're stretched out to the full size), but the rescale
values aren't set. Added a hack to handle this like ETCn so the texture is
displayed correctly.

src/librptexture/decoder/ImageDecoder_ETC1.cpp
@@ -624,11 +624,11 @@ rp_image *fromETC1(int width, int height,
 		return nullptr;
 	}
 
-	// ETC1 uses 4x4 tiles.
-	assert(width % 4 == 0);
-	assert(height % 4 == 0);
-	if (width % 4 != 0 || height % 4 != 0)
-		return nullptr;
+	// ETC1 uses 4x4 tiles, but some container formats allow
+	// the last tile to be cut off, so round up for the
+	// physical tile size.
+	const int physWidth = ALIGN_BYTES(4, width);
+	const int physHeight = ALIGN_BYTES(4, height);
 
 	// Create an rp_image.
 	rp_image *const img = new rp_image(width, height, rp_image::Format::ARGB32);
@@ -655,6 +655,11 @@ rp_image *fromETC1(int width, int height,
 		// Blit the tile to the main image buffer.
 		ImageDecoderPrivate::BlitTile<uint32_t, 4, 4>(img, tileBuf, x, y);
 	} }
+
+	if (width < physWidth || height < physHeight) {
+		// Shrink the image.
+		img->shrink(width, height);
+	}
 
 	// Set the sBIT metadata.
 	static const rp_image::sBIT_t sBIT = {8,8,8,0,0};
@@ -686,14 +691,14 @@ rp_image *fromETC2_RGB(int width, int height,
 		return nullptr;
 	}
 
-	// ETC2 uses 4x4 tiles.
-	assert(width % 4 == 0);
-	assert(height % 4 == 0);
-	if (width % 4 != 0 || height % 4 != 0)
-		return nullptr;
+	// ETC2 uses 4x4 tiles, but some container formats allow
+	// the last tile to be cut off, so round up for the
+	// physical tile size.
+	const int physWidth = ALIGN_BYTES(4, width);
+	const int physHeight = ALIGN_BYTES(4, height);
 
 	// Create an rp_image.
-	rp_image *const img = new rp_image(width, height, rp_image::Format::ARGB32);
+	rp_image *const img = new rp_image(physWidth, physHeight, rp_image::Format::ARGB32);
 	if (!img->isValid()) {
 		// Could not allocate the image.
 		img->unref();
@@ -703,8 +708,8 @@ rp_image *fromETC2_RGB(int width, int height,
 	const etc1_block *etc1_src = reinterpret_cast<const etc1_block*>(img_buf);
 
 	// Calculate the total number of tiles.
-	const unsigned int tilesX = static_cast<unsigned int>(width / 4);
-	const unsigned int tilesY = static_cast<unsigned int>(height / 4);
+	const unsigned int tilesX = static_cast<unsigned int>(physWidth / 4);
+	const unsigned int tilesY = static_cast<unsigned int>(physHeight / 4);
 
 	// Temporary tile buffer.
 	array<uint32_t, 4*4> tileBuf;
@@ -717,6 +722,11 @@ rp_image *fromETC2_RGB(int width, int height,
 		// Blit the tile to the main image buffer.
 		ImageDecoderPrivate::BlitTile<uint32_t, 4, 4>(img, tileBuf, x, y);
 	} }
+
+	if (width < physWidth || height < physHeight) {
+		// Shrink the image.
+		img->shrink(width, height);
+	}
 
 	// Set the sBIT metadata.
 	static const rp_image::sBIT_t sBIT = {8,8,8,0,0};
@@ -788,14 +798,14 @@ rp_image *fromETC2_RGBA(int width, int height,
 		return nullptr;
 	}
 
-	// ETC2 uses 4x4 tiles.
-	assert(width % 4 == 0);
-	assert(height % 4 == 0);
-	if (width % 4 != 0 || height % 4 != 0)
-		return nullptr;
+	// ETC2 uses 4x4 tiles, but some container formats allow
+	// the last tile to be cut off, so round up for the
+	// physical tile size.
+	const int physWidth = ALIGN_BYTES(4, width);
+	const int physHeight = ALIGN_BYTES(4, height);
 
 	// Create an rp_image.
-	rp_image *const img = new rp_image(width, height, rp_image::Format::ARGB32);
+	rp_image *const img = new rp_image(physWidth, physHeight, rp_image::Format::ARGB32);
 	if (!img->isValid()) {
 		// Could not allocate the image.
 		img->unref();
@@ -805,8 +815,8 @@ rp_image *fromETC2_RGBA(int width, int height,
 	const etc2_rgba_block *etc2_src = reinterpret_cast<const etc2_rgba_block*>(img_buf);
 
 	// Calculate the total number of tiles.
-	const unsigned int tilesX = static_cast<unsigned int>(width / 4);
-	const unsigned int tilesY = static_cast<unsigned int>(height / 4);
+	const unsigned int tilesX = static_cast<unsigned int>(physWidth / 4);
+	const unsigned int tilesY = static_cast<unsigned int>(physHeight / 4);
 
 	// Temporary tile buffer.
 	array<uint32_t, 4*4> tileBuf;
@@ -823,6 +833,11 @@ rp_image *fromETC2_RGBA(int width, int height,
 		// Blit the tile to the main image buffer.
 		ImageDecoderPrivate::BlitTile<uint32_t, 4, 4>(img, tileBuf, x, y);
 	} }
+
+	if (width < physWidth || height < physHeight) {
+		// Shrink the image.
+		img->shrink(width, height);
+	}
 
 	// Set the sBIT metadata.
 	static const rp_image::sBIT_t sBIT = {8,8,8,0,8};
@@ -854,14 +869,14 @@ rp_image *fromETC2_RGB_A1(int width, int height,
 		return nullptr;
 	}
 
-	// ETC2 uses 4x4 tiles.
-	assert(width % 4 == 0);
-	assert(height % 4 == 0);
-	if (width % 4 != 0 || height % 4 != 0)
-		return nullptr;
+	// ETC2 uses 4x4 tiles, but some container formats allow
+	// the last tile to be cut off, so round up for the
+	// physical tile size.
+	const int physWidth = ALIGN_BYTES(4, width);
+	const int physHeight = ALIGN_BYTES(4, height);
 
 	// Create an rp_image.
-	rp_image *const img = new rp_image(width, height, rp_image::Format::ARGB32);
+	rp_image *const img = new rp_image(physWidth, physHeight, rp_image::Format::ARGB32);
 	if (!img->isValid()) {
 		// Could not allocate the image.
 		img->unref();
@@ -871,8 +886,8 @@ rp_image *fromETC2_RGB_A1(int width, int height,
 	const etc1_block *etc1_src = reinterpret_cast<const etc1_block*>(img_buf);
 
 	// Calculate the total number of tiles.
-	const unsigned int tilesX = static_cast<unsigned int>(width / 4);
-	const unsigned int tilesY = static_cast<unsigned int>(height / 4);
+	const unsigned int tilesX = static_cast<unsigned int>(physWidth / 4);
+	const unsigned int tilesY = static_cast<unsigned int>(physHeight / 4);
 
 	// Temporary tile buffer.
 	array<uint32_t, 4*4> tileBuf;
@@ -885,6 +900,11 @@ rp_image *fromETC2_RGB_A1(int width, int height,
 		// Blit the tile to the main image buffer.
 		ImageDecoderPrivate::BlitTile<uint32_t, 4, 4>(img, tileBuf, x, y);
 	} }
+
+	if (width < physWidth || height < physHeight) {
+		// Shrink the image.
+		img->shrink(width, height);
+	}
 
 	// Set the sBIT metadata.
 	static const rp_image::sBIT_t sBIT = {8,8,8,0,1};

src/librpcpu/bitstuff.h
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpcpu)                         *
  * bitstuff.h: Bit manipulation inline functions.                          *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2021 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -68,7 +68,7 @@ static inline unsigned int popcount(unsigned int x)
 
 /**
  * Check if a value is a power of 2. (also must be non-zero)
- * @param x Value.
+ * @param x Value
  * @return True if this is value is a power of 2 and is non-zero; false if not.
  */
 static inline bool isPow2(unsigned int x)
@@ -77,6 +77,16 @@ static inline bool isPow2(unsigned int x)
 	// - https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2
 	// - https://stackoverflow.com/a/600492
 	return (x != 0 && ((x & (x - 1)) == 0));
+}
+
+/**
+ * Get the next power of 2.
+ * @param x Value
+ * @return Next power of 2.
+ */
+static inline unsigned int nextPow2(unsigned int x)
+{
+	return (1U << (uilog2(x) + 1));
 }
 
 #ifdef __cplusplus

src/librptexture/decoder/ImageDecoder_PVRTC.cpp
@@ -40,8 +40,33 @@ rp_image *fromPVRTC(int width, int height,
 	assert(width > 0);
 	assert(height > 0);
 
+	// PVRTC uses 4x4 tiles (4bpp) or 8x4 tiles (2bpp), but
+	// some container formats allow the last tile to be cut off.
+	// NOTE: PVRTC *requires* power-of-2 textures.
+	// Minimum image size: 8x8 (4bpp); 16x8 (2bpp) [based on PVRTC-II]
+	if ((mode & PVRTC_BPP_MASK) == PVRTC_2BPP) {
+		// PVRTC 2bpp
+		assert(width >= 16);
+		assert(height >= 8);
+		if (width < 16 || height < 8)
+			return nullptr;
+	} else {
+		// PVRTC 4bpp
+		assert(width >= 8);
+		assert(height >= 8);
+		if (width < 8 || height < 8)
+			return nullptr;
+	}
+	int physWidth = width, physHeight = height;
+	if (!isPow2(physWidth)) {
+		physWidth = nextPow2(physWidth);
+	}
+	if (!isPow2(physHeight)) {
+		physHeight = nextPow2(physHeight);
+	}
+
 	// Expected size to be read by the PowerVR Native SDK.
-	const uint32_t expected_size_in = ((width * height) /
+	const uint32_t expected_size_in = ((physWidth * physHeight) /
 		(((mode & PVRTC_BPP_MASK) == PVRTC_2BPP) ? 4 : 2));
 
 	assert(img_siz >= static_cast<int>(expected_size_in));
@@ -51,38 +76,8 @@ rp_image *fromPVRTC(int width, int height,
 		return nullptr;
 	}
 
-	// PVRTC 2bpp uses 8x4 tiles.
-	// PVRTC 4bpp uses 4x4 tiles.
-	// Minimum image size: 8x8 (4bpp); 16x8 (2bpp)
-	// TODO: PVRTDecompress supports images smaller than the minimum
-	// image size, but it will copy to a temporary buffer, and the
-	// return value will match the temporary buffer size.
-	if ((mode & PVRTC_BPP_MASK) == PVRTC_2BPP) {
-		// PVRTC 2bpp
-		assert(width % 8 == 0);
-		assert(height % 4 == 0);
-		if (width % 8 != 0 || height % 4 != 0)
-			return nullptr;
-
-		assert(width >= 16);
-		assert(height >= 8);
-		if (width < 16 || height < 8)
-			return nullptr;
-	} else {
-		// PVRTC 4bpp
-		assert(width % 4 == 0);
-		assert(height % 4 == 0);
-		if (width % 4 != 0 || height % 4 != 0)
-			return nullptr;
-
-		assert(width >= 8);
-		assert(height >= 8);
-		if (width < 8 || height < 8)
-			return nullptr;
-	}
-
 	// Create an rp_image.
-	rp_image *const img = new rp_image(width, height, rp_image::Format::ARGB32);
+	rp_image *const img = new rp_image(physWidth, physHeight, rp_image::Format::ARGB32);
 	if (!img->isValid()) {
 		// Could not allocate the image.
 		img->unref();
@@ -92,8 +87,8 @@ rp_image *fromPVRTC(int width, int height,
 	// Use the PowerVR Native SDK to decompress the texture.
 	// Return value is the size of the *input* data that was decompressed.
 	// TODO: Row padding?
-	uint32_t size = pvr::PVRTDecompressPVRTC(img_buf, ((mode & PVRTC_BPP_MASK) == PVRTC_2BPP), width, height,
-		static_cast<uint8_t*>(img->bits()));
+	uint32_t size = pvr::PVRTDecompressPVRTC(img_buf, ((mode & PVRTC_BPP_MASK) == PVRTC_2BPP),
+		physWidth, physHeight, static_cast<uint8_t*>(img->bits()));
 	assert(size == expected_size_in);
 	if (size != expected_size_in) {
 		// Read error...
@@ -102,6 +97,11 @@ rp_image *fromPVRTC(int width, int height,
 	}
 
 	// TODO: If !has_alpha, make sure the alpha channel is all 0xFF.
+
+	if (width < physWidth || height < physHeight) {
+		// Shrink the image.
+		img->shrink(width, height);
+	}
 
 	// Set the sBIT metadata.
 	static const rp_image::sBIT_t sBIT_alpha  = {8,8,8,0,8};
@@ -113,7 +113,7 @@ rp_image *fromPVRTC(int width, int height,
 }
 
 /**
- * Convert a PVRTC 2bpp or 4bpp image to rp_image.
+ * Convert a PVRTC-II 2bpp or 4bpp image to rp_image.
  * @param width Image width.
  * @param height Image height.
  * @param img_buf PVRTC image buffer.
@@ -131,29 +131,33 @@ rp_image *fromPVRTCII(int width, int height,
 	assert(height > 0);
 
 	// PVRTC-II uses 4x4 tiles (4bpp) or 8x4 tiles (2bpp), but
-	// PVRTC-II allows the last tile to be cut off, so round
-	// up for the physical tile size.
+	// PVRTC-II allows the last tile to be cut off.
+	// NOTE: PVRTC-II does not require power-of-2 textures, but
+	// we're using a slightly-modified PVRTC-I decoder, which *does*
+	// requires power-of-2 textures.
 	// Minimum image size: 8x8 (4bpp); 16x8 (2bpp)
 	// TODO: PVRTDecompress supports images smaller than the minimum
 	// image size, but it will copy to a temporary buffer, and the
 	// return value will match the temporary buffer size.
-	int physWidth;
 	if ((mode & PVRTC_BPP_MASK) == PVRTC_2BPP) {
-		assert(width >= 16);
-		assert(height >= 8);
+		// PVRTC-II 2bpp
+		assert(height >= 16);
 		if (width < 16 || height < 8)
 			return nullptr;
-
-		physWidth = ALIGN_BYTES(8, width);
 	} else {
-		assert(width >= 8);
+		// PVRTC-II 4bpp
 		assert(height >= 8);
 		if (width < 8 || height < 8)
 			return nullptr;
-
-		physWidth = ALIGN_BYTES(4, width);
 	}
-	const int physHeight = ALIGN_BYTES(4, height);
+	// TODO: PVRTC-II NPOT support.
+	int physWidth = width, physHeight = height;
+	if (!isPow2(physWidth)) {
+		physWidth = nextPow2(physWidth);
+	}
+	if (!isPow2(physHeight)) {
+		physHeight = nextPow2(physHeight);
+	}
 
 	// Expected size to be read by the PowerVR Native SDK.
 	const uint32_t expected_size_in = ((physWidth * physHeight) /

src/librptexture/decoder/ImageSizeCalc.hpp
@@ -9,6 +9,8 @@
 #ifndef __ROMPROPERTIES_LIBRPTEXTURE_DECODER_IMAGESIZECALC_HPP__
 #define __ROMPROPERTIES_LIBRPTEXTURE_DECODER_IMAGESIZECALC_HPP__
 
+#include "bitstuff.h"
+
 // C includes (C++ namespace)
 #include <cassert>
 
@@ -52,6 +54,33 @@ enum class OpCode : uint8_t {
 unsigned int calcImageSize(
 	const OpCode *op_tbl, size_t tbl_size,
 	unsigned int format, unsigned int width, unsigned int height);
+
+/**
+ * Calculate the expected size of a PVRTC-I compressed 2D image.
+ * PVRTC-I requires power-of-2 dimensions.
+ * @tparam is2bpp True for 2bpp; false for 4bpp.
+ * @param width Image width
+ * @param height Image height
+ * @return Expected size, in bytes
+ */
+template<bool is2bpp>
+static inline unsigned int calcImageSizePVRTC_PoT(int width, int height)
+{
+	static const int min_width = (is2bpp ? 8 : 4);
+	if (width < min_width) {
+		width = min_width;
+	} else if (!isPow2(width)) {
+		width = nextPow2(width);
+	}
+
+	if (height < 4) {
+		height = 4;
+	} else if (!isPow2(height)) {
+		height = nextPow2(height);
+	}
+
+	return (width * height / (is2bpp ? 4 : 2));
+}
 
 /**
  * Validate ASTC block size.

src/librptexture/fileformat/GodotSTEX.cpp
@@ -188,7 +188,7 @@ const ImageSizeCalc::OpCode GodotSTEXPrivate::op_tbl[] = {
 
 	// 0x18
 	OpCode::Align4,		// STEX_FORMAT_BPTC_RGBFU	// TODO: Verify
-	OpCode::Divide4,	// STEX_FORMAT_PVRTC1_2		// TODO: Alignment for PVRTC1?
+	OpCode::Divide4,	// STEX_FORMAT_PVRTC1_2
 	OpCode::Divide4,	// STEX_FORMAT_PVRTC1_2A
 	OpCode::Divide2,	// STEX_FORMAT_PVRTC1_4
 	OpCode::Divide2,	// STEX_FORMAT_PVRTC1_4A
@@ -783,6 +783,24 @@ GodotSTEX::GodotSTEX(IRpFile *file)
 
 	// Mask off the flags for the actual pixel format.
 	d->pixelFormat = static_cast<STEX_Format_e>(d->pixelFormat_flags & STEX_FORMAT_MASK);
+
+	// Special case: Godot 3 doesn't set rescaling parameters for NPOT PVRTC textures.
+	if (d->pixelFormat >= STEX_FORMAT_PVRTC1_2 && d->pixelFormat <= STEX_FORMAT_PVRTC1_4A) {
+		if (d->rescale_dimensions[0] == 0 &&
+		    (!isPow2(d->dimensions[0]) || !isPow2(d->dimensions[1])))
+		{
+			// NPOT PVRTC texture, and no rescaling dimensions are set.
+			d->rescale_dimensions[0] = d->dimensions[0];
+			d->rescale_dimensions[1] = d->dimensions[1];
+
+			if (!isPow2(d->dimensions[0])) {
+				d->dimensions[0] = nextPow2(d->dimensions[0]);
+			}
+			if (!isPow2(d->dimensions[1])) {
+				d->dimensions[1] = nextPow2(d->dimensions[1]);
+			}
+		}
+	}
 }
 
 /** Property accessors **/

commit 89b1a2003b9cfbe37e2ec5f4d6c70e8c56084031

[librptexture] ImageDecoder::fromETC1(): Use physWidth/physHeight when creating the rp_image.
Otherwise, we end up writing too much data per row, which skews the
image to the left. (negative slope)

This fixes a regression from commit b63689dd7c922ccba9938ab383f41303a71445ba.
([librptexture] GodotSTEX: Handle ETCn and PVRTC images that have sizes that a ren't powers of 2.)

src/librptexture/decoder/ImageDecoder_ETC1.cpp
@@ -631,7 +631,7 @@ rp_image *fromETC1(int width, int height,
 	const int physHeight = ALIGN_BYTES(4, height);
 
 	// Create an rp_image.
-	rp_image *const img = new rp_image(width, height, rp_image::Format::ARGB32);
+	rp_image *const img = new rp_image(physWidth, physHeight, rp_image::Format::ARGB32);
 	if (!img->isValid()) {
 		// Could not allocate the image.
 		img->unref();
@@ -641,8 +641,8 @@ rp_image *fromETC1(int width, int height,
 	const etc1_block *etc1_src = reinterpret_cast<const etc1_block*>(img_buf);
 
 	// Calculate the total number of tiles.
-	const unsigned int tilesX = static_cast<unsigned int>(width / 4);
-	const unsigned int tilesY = static_cast<unsigned int>(height / 4);
+	const unsigned int tilesX = static_cast<unsigned int>(physWidth / 4);
+	const unsigned int tilesY = static_cast<unsigned int>(physHeight / 4);
 
 	// Temporary tile buffer.
 	array<uint32_t, 4*4> tileBuf;