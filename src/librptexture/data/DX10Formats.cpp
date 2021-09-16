9f8e5113177d48d4b1a3b3ad5d91802bd95d8abe
David Korth <gerbilsoft@gerbilsoft.com>
2021-09-15 20:37:19 -0400

    [librptexture] Use string and offset tables for the various format string files.
    This reduces the size by quite a bit (especially on 64-bit, since it
    eliminates 64-bit string pointers), and significantly reduces the
    number of relocations.
    
    Note that the string tables all start with "\0", since string offset
    0 indicates "no string".
    
    Code size difference: (64-bit Gentoo Linux, gcc-11.2.0, release build, no LTO)
    
       text    data     bss     dec     hex filename
       2264    1528       0    3792     ed0 DX10Formats.cpp.o [before]
       3117       0       0    3117     c2d DX10Formats.cpp.o [after]
       +853   -1528       0    -675    -2a3 Difference
    
       text    data     bss     dec     hex filename
       4576    3376       0    7952    1f10 VkEnumStrings.cpp.o [before]
       5701       0       0    5701    1645 VkEnumStrings.cpp.o [after]
      +1125   -3376       0   -2251    -8cb Difference
    
       text    data     bss     dec     hex filename
       4423    3648       0    8071    1f87 GLenumStrings.cpp.o [before]
       5246       0       0    5246    147e GLenumStrings.cpp.o [after]
       +823   -3648       0   -2825    -b09 Difference

--- src/librptexture/data/DX10Formats.cpp
+++ src/librptexture/data/DX10Formats.cpp
@@ -22,11 +22,15 @@ class DX10FormatsPrivate
 
 	public:
 		/**
-		 * DirectX 10 format table.
-		 * NOTE: Only contains the contiguous section.
-		 * Other values have to be checked afterwards.
+		 * DirectX 10 format string table.
 		 */
-		static const char *const dxgiFormat_tbl[];
+		static const char dxgiFormat_strtbl[];
+
+		/**
+		 * DirectX 10 format offset table.
+		 * 0 == no string
+		 */
+		static const uint16_t dxgiFormat_offtbl[];
 };
 
 /**
@@ -34,94 +38,165 @@ class DX10FormatsPrivate
  * NOTE: Only contains the contiguous section.
  * Other values have to be checked afterwards.
  */
-const char *const DX10FormatsPrivate::dxgiFormat_tbl[] = {
-	nullptr, "R32G32B32A32_TYPELESS",			// 0,1
-	"R32G32B32A32_FLOAT", "R32G32B32A32_UINT",		// 2,3
-	"R32G32B32A32_SINT", "R32G32B32_TYPELESS",		// 4,5
-	"R32G32B32_FLOAT", "R32G32B32_UINT",			// 6,7
-	"R32G32B32_SINT", "R16G16B16A16_TYPELESS",		// 8,9
-	"R16G16B16A16_FLOAT", "R16G16B16A16_UNORM",		// 10,11
-	"R16G16B16A16_UINT", "R16G16B16A16_SNORM",		// 12,13
-	"R16G16B16A16_SINT", "R32G32_TYPELESS",			// 14,15
-	"R32G32_FLOAT", "R32G32_UINT",				// 16,17
-	"R32G32_SINT", "R32G8X24_TYPELESS",			// 18,19
-	"D32_FLOAT_S8X24_UINT", "R32_FLOAT_X8X24_TYPELESS",	// 20,21
-	"X32_TYPELESS_G8X24_UINT", "R10G10B10A2_TYPELESS",	// 22,23
-	"R10G10B10A2_UNORM", "R10G10B10A2_UINT",		// 24,25
-	"R11G11B10_FLOAT", "R8G8B8A8_TYPELESS",			// 26,27
-	"R8G8B8A8_UNORM", "R8G8B8A8_UNORM_SRGB",		// 28,29
-	"R8G8B8A8_UINT", "R8G8B8A8_SNORM",			// 30,31
-	"R8G8B8A8_SINT", "R16G16_TYPELESS",			// 32,33
-	"R16G16_FLOAT", "R16G16_UNORM",				// 34,35
-	"R16G16_UINT", "R16G16_SNORM",				// 36,37
-	"R16G16_SINT", "R32_TYPELESS",				// 38,39
-	"D32_FLOAT", "R32_FLOAT",				// 40,41
-	"R32_UINT", "R32_SINT",					// 42,43
-	"R24G8_TYPELESS", "D24_UNORM_S8_UINT",			// 44,45
-	"R24_UNORM_X8_TYPELESS", "X24_TYPELESS_G8_UINT",	// 46,47
-	"R8G8_TYPELESS", "R8G8_UNORM",				// 48,49
-	"R8G8_UINT", "R8G8_SNORM",				// 50,51
-	"R8G8_SINT", "R16_TYPELESS",				// 52,53
-	"R16_FLOAT", "D16_UNORM",				// 54,55
-	"R16_UNORM", "R16_UINT",				// 56,57
-	"R16_SNORM", "R16_SINT",				// 58,59
-	"R8_TYPELESS", "R8_UNORM",				// 60,61
-	"R8_UINT", "R8_SNORM",					// 62,63
-	"R8_SINT", "A8_UNORM",					// 64,65
-	"R1_UNORM", "R9G9B9E5_SHAREDEXP",			// 66,67
-	"R8G8_B8G8_UNORM", "G8R8_G8B8_UNORM",			// 68,69
-	"BC1_TYPELESS", "BC1_UNORM",				// 70,71
-	"BC1_UNORM_SRGB", "BC2_TYPELESS",			// 72,73
-	"BC2_UNORM", "BC2_UNORM_SRGB",				// 74,75
-	"BC3_TYPELESS", "BC3_UNORM",				// 76,77
-	"BC3_UNORM_SRGB", "BC4_TYPELESS",			// 78,79
-	"BC4_UNORM", "BC4_SNORM",				// 80,81
-	"BC5_TYPELESS", "BC5_UNORM",				// 82,83
-	"BC5_SNORM", "B5G6R5_UNORM",				// 84,85
-	"B5G5R5A1_UNORM", "B8G8R8A8_UNORM",			// 86,87
-	"B8G8R8X8_UNORM", "R10G10B10_XR_BIAS_A2_UNORM",		// 88,89
-	"B8G8R8A8_TYPELESS", "B8G8R8A8_UNORM_SRGB",		// 90,91
-	"B8G8R8X8_TYPELESS", "B8G8R8X8_UNORM_SRGB",		// 92,93
-	"BC6H_TYPELESS", "BC6H_UF16",				// 94,95
-	"BC6H_SF16", "BC7_TYPELESS",				// 96,97
-	"BC7_UNORM", "BC7_UNORM_SRGB",				// 98,99
-	"AYUV", "Y410", "Y416", "NV12",				// 100-103
-	"P010", "P016", "420_OPAQUE", "YUY2",			// 104-107
-	"Y210", "Y216", "NV11", "AI44",				// 108-111
-	"IA44", "P8", "A8P8", "B4G4R4A4_UNORM",			// 112-115
-	"XBOX_R10G10B10_7E2_A2_FLOAT",				// 116
-	"XBOX_R10G10B10_6E4_A2_FLOAT",				// 117
-	"XBOX_D16_UNORM_S8_UINT", "XBOX_R6_UNORM_X8_TYPELESS",	// 118-119
-	"XBOX_X16_TYPELESS_G8_UINT",				// 120
-	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,	// 121-126
-	nullptr, nullptr, nullptr,				// 127-129
-	"P208", "V208", "V408",					// 130-132
+const char DX10FormatsPrivate::dxgiFormat_strtbl[] = {
+	// 0
+	"\0" "R32G32B32A32_TYPELESS\0"
+	"R32G32B32A32_FLOAT\0" "R32G32B32A32_UINT\0"
+	"R32G32B32A32_SINT\0" "R32G32B32_TYPELESS\0"
+	"R32G32B32_FLOAT\0" "R32G32B32_UINT\0"
+	"R32G32B32_SINT\0" "R16G16B16A16_TYPELESS\0"
 
-	// ASTC
+	// 10
+	"R16G16B16A16_FLOAT\0" "R16G16B16A16_UNORM\0"
+	"R16G16B16A16_UINT\0" "R16G16B16A16_SNORM\0"
+	"R16G16B16A16_SINT\0" "R32G32_TYPELESS\0"
+	"R32G32_FLOAT\0" "R32G32_UINT\0"
+	"R32G32_SINT\0" "R32G8X24_TYPELESS\0"
+
+	// 20
+	"D32_FLOAT_S8X24_UINT\0" "R32_FLOAT_X8X24_TYPELESS\0"
+	"X32_TYPELESS_G8X24_UINT\0" "R10G10B10A2_TYPELESS\0"
+	"R10G10B10A2_UNORM\0" "R10G10B10A2_UINT\0"
+	"R11G11B10_FLOAT\0" "R8G8B8A8_TYPELESS\0"
+	"R8G8B8A8_UNORM\0" "R8G8B8A8_UNORM_SRGB\0"
+
+	// 30
+	"R8G8B8A8_UINT\0" "R8G8B8A8_SNORM\0"
+	"R8G8B8A8_SINT\0" "R16G16_TYPELESS\0"
+	"R16G16_FLOAT\0" "R16G16_UNORM\0"
+	"R16G16_UINT\0" "R16G16_SNORM\0"
+	"R16G16_SINT\0" "R32_TYPELESS\0"
+
+	// 40
+	"D32_FLOAT\0" "R32_FLOAT\0"
+	"R32_UINT\0" "R32_SINT\0"
+	"R24G8_TYPELESS\0" "D24_UNORM_S8_UINT\0"
+	"R24_UNORM_X8_TYPELESS\0" "X24_TYPELESS_G8_UINT\0"
+	"R8G8_TYPELESS\0" "R8G8_UNORM\0"
+
+	// 50
+	"R8G8_UINT\0" "R8G8_SNORM\0"
+	"R8G8_SINT\0" "R16_TYPELESS\0"
+	"R16_FLOAT\0" "D16_UNORM\0"
+	"R16_UNORM\0" "R16_UINT\0"
+	"R16_SNORM\0" "R16_SINT\0"
+
+	// 60
+	"R8_TYPELESS\0" "R8_UNORM\0"
+	"R8_UINT\0" "R8_SNORM\0"
+	"R8_SINT\0" "A8_UNORM\0"
+	"R1_UNORM\0" "R9G9B9E5_SHAREDEXP\0"
+	"R8G8_B8G8_UNORM\0" "G8R8_G8B8_UNORM\0"
+
+	// 70
+	"BC1_TYPELESS\0" "BC1_UNORM\0"
+	"BC1_UNORM_SRGB\0" "BC2_TYPELESS\0"
+	"BC2_UNORM\0" "BC2_UNORM_SRGB\0"
+	"BC3_TYPELESS\0" "BC3_UNORM\0"
+	"BC3_UNORM_SRGB\0" "BC4_TYPELESS\0"
+
+	// 80
+	"BC4_UNORM\0" "BC4_SNORM\0"
+	"BC5_TYPELESS\0" "BC5_UNORM\0"
+	"BC5_SNORM\0" "B5G6R5_UNORM\0"
+	"B5G5R5A1_UNORM\0" "B8G8R8A8_UNORM\0"
+	"B8G8R8X8_UNORM\0" "R10G10B10_XR_BIAS_A2_UNORM\0"
+
+	// 90
+	"B8G8R8A8_TYPELESS\0" "B8G8R8A8_UNORM_SRGB\0"
+	"B8G8R8X8_TYPELESS\0" "B8G8R8X8_UNORM_SRGB\0"
+	"BC6H_TYPELESS\0" "BC6H_UF16\0"
+	"BC6H_SF16\0" "BC7_TYPELESS\0"
+	"BC7_UNORM\0" "BC7_UNORM_SRGB\0"
+
+	// 100
+	"AYUV\0" "Y410\0" "Y416\0" "NV12\0"
+	"P010\0" "P016\0" "420_OPAQUE\0" "YUY2\0"
+	"Y210\0" "Y216\0" "NV11\0" "AI44\0"
+	"IA44\0" "P8\0" "A8P8\0" "B4G4R4A4_UNORM\0"
+
+	// 116
+	"XBOX_R10G10B10_7E2_A2_FLOAT\0"
+	"XBOX_R10G10B10_6E4_A2_FLOAT\0"
+	"XBOX_D16_UNORM_S8_UINT\0" "XBOX_R6_UNORM_X8_TYPELESS\0"
+
+	// 120
+	"XBOX_X16_TYPELESS_G8_UINT\0"
+
+	// 130
+	"P208\0" "V208\0" "V408\0"
+
+	// ASTC (133-188)
 	#define DXGI_ASTC(x,y) \
-		"ASTC_" #x "X" #x "_TYPELESS",\
-		"ASTC_" #x "X" #x "_UNORM", \
-		"ASTC_" #x "X" #x "_UNORM_SRGB", \
-		nullptr
-	DXGI_ASTC(4, 4),	// 133-135
-	DXGI_ASTC(5, 4),	// 137-139
-	DXGI_ASTC(5, 5),	// 141-143
-	DXGI_ASTC(6, 5),	// 145-147
-	DXGI_ASTC(6, 6),	// 149-151
-	DXGI_ASTC(8, 5),	// 153-155
-	DXGI_ASTC(8, 6),	// 157-159
-	DXGI_ASTC(8, 8),	// 161-163
-	DXGI_ASTC(10, 5),	// 165-167
-	DXGI_ASTC(10, 6),	// 169-171
-	DXGI_ASTC(10, 8),	// 173-175
-	DXGI_ASTC(10, 10),	// 177-179
-	DXGI_ASTC(12, 10),	// 181-183
-	DXGI_ASTC(12, 12),	// 185-187
+		"ASTC_" #x "X" #x "_TYPELESS\0" \
+		"ASTC_" #x "X" #x "_UNORM\0" \
+		"ASTC_" #x "X" #x "_UNORM_SRGB\0"
+	DXGI_ASTC(4, 4)
+	DXGI_ASTC(5, 4)
+	DXGI_ASTC(5, 5)
+	DXGI_ASTC(6, 5)
+	DXGI_ASTC(6, 6)
+	DXGI_ASTC(8, 5)
+	DXGI_ASTC(8, 6)
+	DXGI_ASTC(8, 8)
+	DXGI_ASTC(10, 5)
+	DXGI_ASTC(10, 6)
+	DXGI_ASTC(10, 8)
+	DXGI_ASTC(10, 10)
+	DXGI_ASTC(12, 10)
+	DXGI_ASTC(12, 12)
 
 	// Additional Xbox One formats
-	"XBOX_R10G10B10_SNORM_A2_UNORM",	// 189
-	"XBOX_R4G4_UNORM",			// 190
-	
+	// 189
+	"XBOX_R10G10B10_SNORM_A2_UNORM\0"
+	"XBOX_R4G4_UNORM\0"
+};
+
+/**
+ * DirectX 10 format offset table.
+ * 0 == no string
+ */
+const uint16_t DX10FormatsPrivate::dxgiFormat_offtbl[] = {
+	// 0
+	0, 1, 23, 42, 60, 78, 97, 113, 128, 143,
+	// 10
+	165, 184, 203, 221, 240, 258, 274, 287, 299, 311,
+	// 20
+	329, 350, 375, 399, 420, 438, 455, 471, 489, 504,
+	// 30
+	524, 538, 553, 567, 583, 596, 609, 621, 634, 646,
+	// 40
+	659, 669, 679, 688, 697, 712, 730, 752, 773, 787,
+	// 50
+	798, 808, 819, 829, 842, 852, 862, 872, 881, 891,
+	// 60
+	900, 912, 921, 929, 938, 946, 955, 964, 983, 999,
+	// 70
+	1015, 1028, 1038, 1053, 1066, 1076, 1091, 1104, 1114, 1129,
+	// 80
+	1142, 1152, 1162, 1175, 1185, 1195, 1208, 1223, 1238, 1253,
+	// 90
+	1280, 1298, 1318, 1336, 1356, 1370, 1380, 1390, 1403, 1413,
+	// 100
+	1428, 1433, 1438, 1443, 1448, 1453, 1458, 1469, 1474, 1479,
+	// 110
+	1484, 1489, 1494, 1499, 1502, 1507, 1522, 1550, 1578, 1601,
+	// 120
+	1627, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	// 130
+	1653, 0, 0, 1668, 1691, 1711, 0, 1736, 1759, 1779,
+	// 140
+	0, 1804, 1827, 1847, 0, 1872, 1895, 1915, 0, 1940,
+	// 150
+	1963, 1983, 0, 2008, 2031, 2051, 0, 2076, 2099, 2119,
+	// 160
+	0, 2144, 2167, 2187, 0, 2212, 2236, 2257, 0, 2283,
+	// 170
+	2307, 2328, 0, 2354, 2378, 2399, 0, 2425, 2450, 2472,
+	// 180
+	0, 2499, 2524, 2546, 0, 2573, 2598, 2620, 0, 2647,
+	// 190
+	2677,
 };
 
 /** DX10Formats **/
@@ -133,12 +208,15 @@ const char *const DX10FormatsPrivate::dxgiFormat_tbl[] = {
  */
 const char *DX10Formats::lookup_dxgiFormat(unsigned int dxgiFormat)
 {
-	static_assert(ARRAY_SIZE(DX10FormatsPrivate::dxgiFormat_tbl) == XBOX_DXGI_FORMAT_R4G4_UNORM+1,
+	static_assert(ARRAY_SIZE(DX10FormatsPrivate::dxgiFormat_offtbl) == XBOX_DXGI_FORMAT_R4G4_UNORM+1,
 		"DX10FormatsPrivate::dxgiFormat_tbl[] size is incorrect.");
 
 	const char *texFormat = nullptr;
-	if (/*dxgiFormat >= 0 &&*/ dxgiFormat < ARRAY_SIZE(DX10FormatsPrivate::dxgiFormat_tbl)) {
-		texFormat = DX10FormatsPrivate::dxgiFormat_tbl[dxgiFormat];
+	if (/*dxgiFormat >= 0 &&*/ dxgiFormat < ARRAY_SIZE(DX10FormatsPrivate::dxgiFormat_offtbl)) {
+		unsigned int offset = DX10FormatsPrivate::dxgiFormat_offtbl[dxgiFormat];
+		return (likely(offset != 0))
+			? &DX10FormatsPrivate::dxgiFormat_strtbl[offset]
+			: nullptr;
 	} else {
 		switch (dxgiFormat) {
 			case DXGI_FORMAT_FORCE_UINT:
