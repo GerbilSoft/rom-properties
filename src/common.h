775bb03fc9a77b8d8edfe52c04d60115acc2cdf1
David Korth <gerbilsoft@gerbilsoft.com>
2022-06-18 18:18:21 -0400

    [librpbase] CMakeLists.txt: Mark librpbase as an OBJECT library.
    This ensures that RpImageLoader doesn't get truncated when linking
    libromdata as a shared library.
    
    References:
    - https://gitlab.kitware.com/cmake/cmake/-/issues/16947
    - https://cmake.org/cmake/help/v3.9/manual/cmake-buildsystem.7.html#object-libraries

--- src/common.h
+++ src/common.h
@@ -236,22 +236,22 @@
 #if defined _WIN32 || defined __CYGWIN__
 #  ifdef RP_BUILDING_LIBROMDATA
 #    ifdef __GNUC__
-#      define RP_LIBROMDATA_PUBLIC __attribute__ ((dllexport))
+#      define RP_LIBROMDATA_PUBLIC __attribute__((dllexport))
 #    else
 #      define RP_LIBROMDATA_PUBLIC __declspec(dllexport)
 #    endif
 #  else
 #    ifdef __GNUC__
-#      define RP_LIBROMDATA_PUBLIC __attribute__ ((dllimport))
+#      define RP_LIBROMDATA_PUBLIC __attribute__((dllimport))
 #    else
-#      define RP_LIBROMDATA_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+#      define RP_LIBROMDATA_PUBLIC __declspec(dllimport)
 #    endif
 #  endif
 #  define RP_LIBROMDATA_LOCAL
 #else
 #  if __GNUC__ >= 4
-#    define RP_LIBROMDATA_PUBLIC __attribute__ ((visibility ("default")))
-#    define RP_LIBROMDATA_LOCAL  __attribute__ ((visibility ("hidden")))
+#    define RP_LIBROMDATA_PUBLIC __attribute__((visibility("default")))
+#    define RP_LIBROMDATA_LOCAL  __attribute__((visibility("hidden")))
 #  else
 #    define RP_LIBROMDATA_PUBLIC
 #    define RP_LIBROMDATA_LOCAL
