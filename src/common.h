6ba67c37826a5c328bd82b79b6995a03abef28fa
David Korth <gerbilsoft@gerbilsoft.com>
2022-06-18 23:13:26 -0400

    [libromdata] New macro RP_LIBROMDATA_IS_DLL.
    RP_BUILDING_LIBROMDATA is always defined when building libromdata.
    
    RP_LIBROMDATA_IS_DLL is only defined if libromdata is being built as a
    shared library. If it's defined, then the visibility macros, e.g.
    dllimport/dllexport on Windows, will be used. Otherwise, they will
    be defined as nothing.

--- src/common.h
+++ src/common.h
@@ -233,29 +233,34 @@
 
 // Visibility macros for libromdata.
 // Reference: https://gcc.gnu.org/wiki/Visibility
-#if defined _WIN32 || defined __CYGWIN__
-#  ifdef RP_BUILDING_LIBROMDATA
-#    ifdef __GNUC__
-#      define RP_LIBROMDATA_PUBLIC __attribute__((dllexport))
+#ifdef RP_LIBROMDATA_IS_DLL
+#  if defined(_WIN32) || defined (__CYGWIN__)
+#    ifdef RP_BUILDING_LIBROMDATA
+#      ifdef __GNUC__
+#        define RP_LIBROMDATA_PUBLIC __attribute__((dllexport))
+#      else
+#        define RP_LIBROMDATA_PUBLIC __declspec(dllexport)
+#      endif
 #    else
-#      define RP_LIBROMDATA_PUBLIC __declspec(dllexport)
+#      ifdef __GNUC__
+#        define RP_LIBROMDATA_PUBLIC __attribute__((dllimport))
+#      else
+#        define RP_LIBROMDATA_PUBLIC __declspec(dllimport)
+#      endif
 #    endif
-#  else
-#    ifdef __GNUC__
-#      define RP_LIBROMDATA_PUBLIC __attribute__((dllimport))
-#    else
-#      define RP_LIBROMDATA_PUBLIC __declspec(dllimport)
-#    endif
-#  endif
-#  define RP_LIBROMDATA_LOCAL
-#else
-#  if __GNUC__ >= 4
-#    define RP_LIBROMDATA_PUBLIC __attribute__((visibility("default")))
-#    define RP_LIBROMDATA_LOCAL  __attribute__((visibility("hidden")))
-#  else
-#    define RP_LIBROMDATA_PUBLIC
 #    define RP_LIBROMDATA_LOCAL
+#  else /* !defined(_WIN32) && !defined (__CYGWIN__) */
+#    if __GNUC__ >= 4
+#      define RP_LIBROMDATA_PUBLIC __attribute__((visibility("default")))
+#      define RP_LIBROMDATA_LOCAL  __attribute__((visibility("hidden")))
+#    else
+#      define RP_LIBROMDATA_PUBLIC
+#      define RP_LIBROMDATA_LOCAL
+#    endif
 #  endif
+#else /* !RP_LIBROMDATA_IS_DLL */
+#  define RP_LIBROMDATA_PUBLIC
+#  define RP_LIBROMDATA_LOCAL
 #endif
 
 #endif /* __ROMPROPERTIES_COMMON_H__ */
