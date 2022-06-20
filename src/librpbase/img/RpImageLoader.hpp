dc3d067cebfac572fa8b5f3321f4faaf06d40f30
David Korth <gerbilsoft@gerbilsoft.com>
2022-06-20 00:11:48 -0400

    [librpbase] RpImageLoader::load() still needs to be exported.
    TCreateThumbnail uses it because some downloaded images are JPEGs,
    and RpImageLoader identifies JPEG vs. PNG and dispatches to the
    correct class to decode the image.

--- src/librpbase/img/RpImageLoader.hpp
+++ src/librpbase/img/RpImageLoader.hpp
@@ -25,6 +25,7 @@ namespace LibRpBase { namespace RpImageLoader {
  * @param file IRpFile to load from.
  * @return rp_image*, or nullptr on error.
  */
+RP_LIBROMDATA_PUBLIC
 LibRpTexture::rp_image *load(LibRpFile::IRpFile *file);
 
 } }
