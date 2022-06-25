GerbilSoft/rom-properties

commit fd05f279096be6ba09cbd93e70a39767a0a10039

[librpbase] AesCAPI_NG.cpp: s/typeof/__typeof__/

src/librpbase/crypto/AesCAPI_NG.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * AesCAPI_NG.cpp: AES decryption class using Win32 CryptoAPI NG.          *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -21,9 +21,9 @@
 #include <winternl.h>
 
 // Function pointer macro.
-#define DECL_FUNCPTR(f) typeof(f) * p##f
-#define DEF_FUNCPTR(f) typeof(f) * AesCAPI_NG_Private::p##f = nullptr
-#define LOAD_FUNCPTR(f) AesCAPI_NG_Private::p##f = (typeof(f)*)GetProcAddress(hBcryptDll, #f)
+#define DECL_FUNCPTR(f) __typeof__w(f) * p##f
+#define DEF_FUNCPTR(f) __typeof__(f) * AesCAPI_NG_Private::p##f = nullptr
+#define LOAD_FUNCPTR(f) AesCAPI_NG_Private::p##f = (__typeof__(f)*)GetProcAddress(hBcryptDll, #f)
 
 // Workaround for RP_D() expecting the no-underscore naming convention.
 #define AesCAPI_NGPrivate AesCAPI_NG_Private

src/c++11-compat.msvc.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * c++11-compat.msvc.h: C++ 2011 compatibility header. (MSVC)              *
  *                                                                         *
- * Copyright (c) 2011-2021 by David Korth.                                 *
+ * Copyright (c) 2011-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 

commit da2906845f9597e13e7c1e1292d1a5de84850f4d

[librpbase] AesCAPI_NG.cpp: Remove a stray 'w'.
This fixes a regression from commit fd05f279096be6ba09cbd93e70a39767a0a10039.
([librpbase] AesCAPI_NG.cpp: s/typeof/__typeof__/)

src/librpbase/crypto/AesCAPI_NG.cpp
@@ -21,7 +21,7 @@
 #include <winternl.h>
 
 // Function pointer macro.
-#define DECL_FUNCPTR(f) __typeof__w(f) * p##f
+#define DECL_FUNCPTR(f) __typeof__(f) * p##f
 #define DEF_FUNCPTR(f) __typeof__(f) * AesCAPI_NG_Private::p##f = nullptr
 #define LOAD_FUNCPTR(f) AesCAPI_NG_Private::p##f = (__typeof__(f)*)GetProcAddress(hBcryptDll, #f)
 