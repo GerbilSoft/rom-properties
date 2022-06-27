GerbilSoft/rom-properties

commit a9d7f3de3045e5943d126befcd7799a31e9b4d5c

[gtk] Refactor some code to use the actual class type for user_data instead of gpointer.
This is for stuff that's guaranteed to be internal only, so we can
avoid the casts and clear things up a bit.

NOTE: G_SOURCE_FUNC() was added in glib-2.58, so we can't use it here.
Cast directly to GSourceFunc instead.

src/gtk/RomDataView.cpp
@@ -91,12 +91,12 @@ static void	rom_data_view_desc_format_type_changed(RomDataView *page,
 
 static void	rom_data_view_init_header_row	(RomDataView	*page);
 static void	rom_data_view_update_display	(RomDataView	*page);
-static gboolean	rom_data_view_load_rom_data	(gpointer	 data);
+static gboolean	rom_data_view_load_rom_data	(RomDataView	*page);
 static void	rom_data_view_delete_tabs	(RomDataView	*page);
 
 /** Signal handlers **/
 static void	checkbox_no_toggle_signal_handler   (GtkCheckButton	*checkbutton,
-						     gpointer		 user_data);
+						     RomDataView	*page);
 static void	rom_data_view_map_signal_handler    (RomDataView	*page,
 						     gpointer		 user_data);
 static void	rom_data_view_unmap_signal_handler  (RomDataView	*page,
@@ -105,10 +105,10 @@ static void	tree_view_realize_signal_handler    (GtkTreeView	*treeView,
 						     RomDataView	*page);
 static void	cboLanguage_lc_changed_signal_handler(GtkComboBox	*widget,
 						     uint32_t		 lc,
-						     gpointer		 user_data);
+						     RomDataView	*page);
 static void	btnOptions_triggered_signal_handler (OptionsMenuButton	*menuButton,
 						     gint		 id,
-						     gpointer		 user_data);
+						     RomDataView	*page);
 
 #if GTK_CHECK_VERSION(3,0,0)
 typedef GtkBoxClass superclass;
@@ -277,9 +277,9 @@ rom_data_view_class_init(RomDataViewClass *klass)
 
 /**
  * Set the label format type.
- * @param page RomDataView.
- * @param label GtkLabel.
- * @param desc_format_type Format type.
+ * @param page RomDataView
+ * @param label GtkLabel
+ * @param desc_format_type Format type
  */
 static inline void
 set_label_format_type(GtkLabel *label, RpDescFormatType desc_format_type)
@@ -510,7 +510,8 @@ rom_data_view_new_with_romData(const gchar *uri, RomData *romData, RpDescFormatT
 		}
 	}
 	if (G_LIKELY(uri != nullptr)) {
-		page->changed_idle = g_idle_add(rom_data_view_load_rom_data, page);
+		// NOTE: G_SOURCE_FUNC() was added in glib-2.58.
+		page->changed_idle = g_idle_add((GSourceFunc)rom_data_view_load_rom_data, page);
 	}
 
 	return reinterpret_cast<GtkWidget*>(page);
@@ -621,7 +622,8 @@ rom_data_view_set_uri(RomDataView	*page,
 	/* Connect to the new file (if any) */
 	if (G_LIKELY(page->uri != nullptr)) {
 		if (page->changed_idle == 0) {
-			page->changed_idle = g_idle_add(rom_data_view_load_rom_data, page);
+			// NOTE: G_SOURCE_FUNC() was added in glib-2.58.
+			page->changed_idle = g_idle_add((GSourceFunc)rom_data_view_load_rom_data, page);
 		}
 	} else {
 		// Hide the header row. (outerbox)
@@ -2290,11 +2292,16 @@ rom_data_view_update_display(RomDataView *page)
 	}
 }
 
+/**
+ * Load the actual ROM data.
+ * Call this function using g_idle_add().
+ * @param page RomDataView
+ * @return G_SOURCE_REMOVE
+ */
 static gboolean
-rom_data_view_load_rom_data(gpointer data)
+rom_data_view_load_rom_data(RomDataView *page)
 {
-	RomDataView *const page = ROM_DATA_VIEW(data);
-	g_return_val_if_fail(page != nullptr && IS_ROM_DATA_VIEW(page), G_SOURCE_REMOVE);
+	g_return_val_if_fail(!IS_ROM_DATA_VIEW(page), G_SOURCE_REMOVE);
 
 	if (G_UNLIKELY(page->uri == nullptr && page->romData == nullptr)) {
 		// No URI or RomData.
@@ -2342,7 +2349,7 @@ rom_data_view_load_rom_data(gpointer data)
 
 /**
  * Delete tabs and related widgets.
- * @param page RomDataView.
+ * @param page RomDataView
  */
 static void
 rom_data_view_delete_tabs(RomDataView *page)
@@ -2418,13 +2425,12 @@ rom_data_view_delete_tabs(RomDataView *page)
 
 /**
  * Prevent bitfield checkboxes from being toggled.
- * @param checkbutton Bitfield checkbox.
- * @param user_data RomDataView*.
+ * @param checkbutton Bitfield checkbox
+ * @param page RomDataView
  */
 static void
-checkbox_no_toggle_signal_handler(GtkCheckButton *checkbutton, gpointer user_data)
+checkbox_no_toggle_signal_handler(GtkCheckButton *checkbutton, RomDataView *page)
 {
-	RomDataView *const page = ROM_DATA_VIEW(user_data);
 	if (page->inhibit_checkbox_no_toggle) {
 		// Inhibiting the no-toggle handler.
 		return;
@@ -2441,7 +2447,7 @@ checkbox_no_toggle_signal_handler(GtkCheckButton *checkbutton, gpointer user_dat
 /**
  * RomDataView is being mapped onto the screen.
  * @param page RomDataView
- * @param user_data User data.
+ * @param user_data User data
  */
 static void
 rom_data_view_map_signal_handler(RomDataView	*page,
@@ -2463,7 +2469,7 @@ rom_data_view_map_signal_handler(RomDataView	*page,
 /**
  * RomDataView is being unmapped from the screen.
  * @param page RomDataView
- * @param user_data User data.
+ * @param user_data User data
  */
 static void
 rom_data_view_unmap_signal_handler(RomDataView	*page,
@@ -2569,30 +2575,29 @@ tree_view_realize_signal_handler(GtkTreeView	*treeView,
  * The RFT_MULTI_STRING language was changed.
  * @param widget	GtkComboBox
  * @param lc		Language code
- * @param user_data	RomDataView
+ * @param page		RomDataView
  */
 static void
 cboLanguage_lc_changed_signal_handler(GtkComboBox *widget,
 				      uint32_t     lc,
-				      gpointer     user_data)
+				      RomDataView *page)
 {
 	RP_UNUSED(widget);
-	rom_data_view_update_multi(ROM_DATA_VIEW(user_data), lc);
+	rom_data_view_update_multi(page, lc);
 }
 
 /**
  * An "Options" menu action was triggered.
  * @param menuButton	OptionsMenuButton
  * @param id		Menu options ID
- * @param user_data	RomDataView
+ * @param page		RomDataView
  */
 static void
 btnOptions_triggered_signal_handler(OptionsMenuButton *menuButton,
 				    gint id,
-				    gpointer user_data)
+				    RomDataView *page)
 {
 	RP_UNUSED(menuButton);
-	RomDataView *const page = ROM_DATA_VIEW(user_data);
 	GtkWindow *const parent = gtk_widget_get_toplevel_window(GTK_WIDGET(page));
 
 	if (id < 0) {

src/gtk/MessageWidget.cpp
@@ -29,8 +29,8 @@ static void	message_widget_get_property	(GObject	*object,
 						 GParamSpec	*pspec);
 
 /** Signal handlers. **/
-static void	closeButton_clicked_handler	(GtkButton	*button,
-						 gpointer	 user_data);
+static void	message_widget_close_button_clicked_handler	(GtkButton	*button,
+								 MessageWidget	*widget);
 static gboolean	timeout_hide_source_func	(gpointer	 user_data);
 
 #if GTK_CHECK_VERSION(3,0,0)
@@ -63,7 +63,7 @@ struct _MessageWidget {
 
 	GtkWidget *image;
 	GtkWidget *label;
-	GtkWidget *closeButton;
+	GtkWidget *close_button;
 
 	GtkMessageType messageType;
 	guint timeout_hide;
@@ -148,12 +148,10 @@ message_widget_init(MessageWidget *widget)
 #else /* !GTK_CHECK_VERSION(3,0,0) */
 	// Add a GtkEventBox for the inner color.
 	widget->evbox_inner = gtk_event_box_new();
-	gtk_widget_show(widget->evbox_inner);
 	gtk_container_add(GTK_CONTAINER(widget), widget->evbox_inner);
 
 	// Add a GtkHBox for all the other widgets.
 	widget->hbox = gtk_hbox_new(0, 0);
-	gtk_widget_show(widget->hbox);
 	gtk_container_add(GTK_CONTAINER(widget->evbox_inner), widget->hbox);
 	hbox = GTK_BOX(widget->hbox);
 #endif /* GTK_CHECK_VERSION(3,0,0) */
@@ -161,34 +159,39 @@ message_widget_init(MessageWidget *widget)
 	widget->messageType = GTK_MESSAGE_OTHER;
 	widget->image = gtk_image_new();
 	widget->label = gtk_label_new(nullptr);
-	gtk_widget_show(widget->label);
 
 	// TODO: Prpoer alignment.
 
-	widget->closeButton = gtk_button_new();
-	gtk_widget_show(widget->closeButton);
+	widget->close_button = gtk_button_new();
 #if GTK_CHECK_VERSION(4,0,0)
-	gtk_button_set_icon_name(GTK_BUTTON(widget->closeButton), "dialog-close");
-	gtk_button_set_has_frame(GTK_BUTTON(widget->closeButton), FALSE);
+	gtk_button_set_icon_name(GTK_BUTTON(widget->close_button), "dialog-close");
+	gtk_button_set_has_frame(GTK_BUTTON(widget->close_button), FALSE);
 #else /* !GTK_CHECK_VERSION(4,0,0) */
 	GtkWidget *const imageClose = gtk_image_new_from_icon_name("dialog-close", GTK_ICON_SIZE_BUTTON);
-	gtk_button_set_image(GTK_BUTTON(widget->closeButton), imageClose);
-	gtk_button_set_relief(GTK_BUTTON(widget->closeButton), GTK_RELIEF_NONE);
+	gtk_button_set_image(GTK_BUTTON(widget->close_button), imageClose);
+	gtk_button_set_relief(GTK_BUTTON(widget->close_button), GTK_RELIEF_NONE);
 #endif /* GTK_CHECK_VERSION(4,0,0) */
 
 #if GTK_CHECK_VERSION(4,0,0)
 	// TODO: Padding?
 	gtk_box_append(hbox, widget->image);
 	gtk_box_append(hbox, widget->label);
-	gtk_box_append(hbox, widget->closeButton);
+	gtk_box_append(hbox, widget->close_button);
 #else /* !GTK_CHECK_VERSION(4,0,0) */
+#  if !GTK_CHECK_VERSION(3,0,0)
+	gtk_widget_show(widget->evbox_inner);
+	gtk_widget_show(widget->hbox);
+#  endif /* GTK_CHECK_VERSION(3,0,0) */
+	gtk_widget_show(widget->label);
+	gtk_widget_show(widget->close_button);
+
 	gtk_box_pack_start(hbox, widget->image, FALSE, FALSE, 4);
 	gtk_box_pack_start(hbox, widget->label, FALSE, FALSE, 0);
-	gtk_box_pack_end(hbox, widget->closeButton, FALSE, FALSE, 0);
+	gtk_box_pack_end(hbox, widget->close_button, FALSE, FALSE, 0);
 #endif /* GTK_CHECK_VERSION(4,0,0) */
 
-	g_signal_connect(widget->closeButton, "clicked",
-		G_CALLBACK(closeButton_clicked_handler), widget);
+	g_signal_connect(widget->close_button, "clicked",
+		G_CALLBACK(message_widget_close_button_clicked_handler), widget);
 }
 
 GtkWidget*
@@ -372,14 +375,11 @@ message_widget_show_with_timeout(MessageWidget *widget)
 /**
  * The Close button was clicked.
  * @param button Close button
- * @param user_data MessageWidget*
+ * @param widget MessageWidget
  */
 static void
-closeButton_clicked_handler(GtkButton	*button,
-			    gpointer	 user_data)
+message_widget_close_button_clicked_handler(GtkButton *button, MessageWidget *widget)
 {
-	MessageWidget *const widget = MESSAGE_WIDGET(user_data);
-
 	// TODO: Animation like KMessageWidget.
 	RP_UNUSED(button);
 	if (widget->timeout_hide != 0) {

src/gtk/OptionsMenuButton.cpp
@@ -407,16 +407,15 @@ btnOptions_activate_signal_handler(GtkButton *button, OptionsMenuButton *widget)
 #ifndef USE_GTK_MENU_BUTTON
 /**
  * Menu positioning function.
- * @param menu		[in] GtkMenu*
+ * @param menu		[in] GtkMenu
  * @param x		[out] X position
  * @param y		[out] Y position
  * @param push_in
- * @param user_data	[in] GtkButton* the menu is attached to.
+ * @param button	[in] GtkWidget the menu is attached to.
  */
 static void
-btnOptions_menu_pos_func(GtkMenu *menu, gint *x, gint *y, gboolean *push_in, gpointer user_data)
+btnOptions_menu_pos_func(GtkMenu *menu, gint *x, gint *y, gboolean *push_in, GtkWidget *button)
 {
-	GtkWidget *const button = (GtkWidget*)GTK_BUTTON(user_data);
 	GdkWindow *const window = gtk_widget_get_window(button);
 
 	GtkAllocation button_alloc, menu_alloc;
@@ -456,7 +455,7 @@ btnOptions_event_signal_handler(GtkButton *button, GdkEvent *event, OptionsMenuB
 	} else
 #endif /* GTK_CHECK_VERSION(3,12,0) */
 	{
-		menuPositionFunc = btnOptions_menu_pos_func;
+		menuPositionFunc = (GtkMenuPositionFunc)btnOptions_menu_pos_func;
 	}
 
 	// Pop up the menu.

commit 23d128af00810a1a5b5ad0ab339b05a02c2b7798

[gtk] rom_data_view_load_rom_data(): Fix inverted assertion.
This fixes a regression from commit a9d7f3de3045e5943d126befcd7799a31e9b4d5c.
([gtk] Refactor some code to use the actual class type for user_data instead of gpointer.)

src/gtk/RomDataView.cpp
@@ -2301,7 +2301,7 @@ rom_data_view_update_display(RomDataView *page)
 static gboolean
 rom_data_view_load_rom_data(RomDataView *page)
 {
-	g_return_val_if_fail(!IS_ROM_DATA_VIEW(page), G_SOURCE_REMOVE);
+	g_return_val_if_fail(IS_ROM_DATA_VIEW(page), G_SOURCE_REMOVE);
 
 	if (G_UNLIKELY(page->uri == nullptr && page->romData == nullptr)) {
 		// No URI or RomData.