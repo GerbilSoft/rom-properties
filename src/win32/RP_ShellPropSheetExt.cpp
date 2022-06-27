GerbilSoft/rom-properties

commit 3cda0c01b6a0cd09f2942f419249ad8a316b8996

Refactored RomOps handlers to use a table for Standard Actions information.
The table contains the Save dialog title, file filter, default file
extension, and whether or not the action is copying to the clipboard.
Note that if it *is* copying to the clipboard, everything else is NULL,
which might waste a bit of space, but it's generally more efficient than
the previous setup.

Code size differences: (64-bit Gentoo Linux, gcc-12.1.0, release build, no LTO)

   text    data     bss     dec     hex filename
  31776      72      89   31937    7cc1 [GTK3] RomDataView.cpp.o [before]
  31626     168      89   31883    7c8b [GTK3] RomDataView.cpp.o [after]
   -150     +96       0     -54     -36 Difference

   text    data     bss     dec     hex filename
  34378       8       0   34386    8652 [KDE] RomDataView.cpp.o [before]
  34062     104       0   34166    8576 [KDE] RomDataView.cpp.o [after]
   -316     +96       0    -220     -dc Difference

src/win32/RP_ShellPropSheetExt.cpp
@@ -2646,38 +2646,44 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 		if (!rom_filename)
 			return;
 
-		bool toClipboard;
-		tstring ts_title;
-		const TCHAR *ts_default_ext = nullptr;
-		const char *s_filter = nullptr;
-		switch (menuId) {
-			case IDM_OPTIONS_MENU_EXPORT_TEXT:
-				toClipboard = false;
-				ts_title = U82T_c(C_("RomDataView", "Export to Text File"));
-				ts_default_ext = _T(".txt");
-				// tr: Text files filter. (RP format)
-				s_filter = C_("RomDataView", "Text Files|*.txt|text/plain|All Files|*.*|-");
-				break;
-			case IDM_OPTIONS_MENU_EXPORT_JSON:
-				toClipboard = false;
-				ts_title = U82T_c(C_("RomDataView", "Export to JSON File"));
-				ts_default_ext = _T(".json");
-				// tr: JSON files filter. (RP format)
-				s_filter = C_("RomDataView", "JSON Files|*.json|application/json|All Files|*.*|-");
-				break;
-			case IDM_OPTIONS_MENU_COPY_TEXT:
-			case IDM_OPTIONS_MENU_COPY_JSON:
-				toClipboard = true;
-				break;
-			default:
-				assert(!"Invalid action ID.");
-				return;
+		const unsigned int id2 = static_cast<unsigned int>(abs(menuId - IDM_OPTIONS_MENU_BASE)) - 1;
+		assert(id2 < 4);
+		if (id2 >= 4) {
+			// Out of range.
+			return;
 		}
+
+		struct StdActsInfo_t {
+			const char *title;	// NOP_C_
+			const char *filter;	// NOP_C_
+			TCHAR default_ext[7];
+			bool toClipboard;
+		};
+		static const StdActsInfo_t stdActsInfo[] = {
+			// OPTION_EXPORT_TEXT
+			{NOP_C_("RomDataView", "Export to Text File"),
+			 NOP_C_("RomDataView", "Text Files|*.txt|text/plain|All Files|*.*|-"),
+			 _T(".txt"), false},
+
+			// OPTION_EXPORT_JSON
+			{NOP_C_("RomDataView", "Export to JSON File"),
+			 NOP_C_("RomDataView", "JSON Files|*.json|application/json|All Files|*.*|-"),
+			 _T(".json"), false},
+
+			// OPTION_COPY_TEXT
+			{nullptr, nullptr, _T(""), true},
+
+			// OPTION_COPY_JSON
+			{nullptr, nullptr, _T(""), true},
+		};
+
+		// Standard Actions information for this action
+		const StdActsInfo_t *const info = &stdActsInfo[id2];
 
 		ofstream ofs;
 		tstring ts_out;
 
-		if (!toClipboard) {
+		if (!info->toClipboard) {
 			if (ts_prevExportDir.empty()) {
 				ts_prevExportDir = U82T_c(rom_filename);
 
@@ -2710,10 +2716,15 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 			if (extpos != string::npos) {
 				rom_basename.resize(extpos);
 			}
-			defaultFileName += rom_basename + ts_default_ext;
+			defaultFileName += rom_basename;
+			if (info->default_ext[0] != _T('\0')) {
+				defaultFileName += info->default_ext;
+			}
 
 			const tstring tfilename = LibWin32Common::getSaveFileName(hDlgSheet,
-				ts_title.c_str(), s_filter, defaultFileName.c_str());
+				U82T_c(dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->title)).c_str(),
+				dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->filter),
+				defaultFileName.c_str());
 			if (tfilename.empty())
 				return;
 

src/gtk/RomDataView.cpp
@@ -2606,39 +2606,45 @@ btnOptions_triggered_signal_handler(OptionsMenuButton *menuButton,
 		if (!rom_filename)
 			return;
 
-		bool toClipboard;
-		const char *s_title = nullptr;
-		const char *s_default_ext = nullptr;
-		const char *s_filter = nullptr;
-		switch (id) {
-			case OPTION_EXPORT_TEXT:
-				toClipboard = false;
-				s_title = C_("RomDataView", "Export to Text File");
-				s_default_ext = ".txt";
-				// tr: Text files filter. (RP format)
-				s_filter = C_("RomDataView", "Text Files|*.txt|text/plain|All Files|*.*|-");
-				break;
-			case OPTION_EXPORT_JSON:
-				toClipboard = false;
-				s_title = C_("RomDataView", "Export to JSON File");
-				s_default_ext = ".json";
-				// tr: JSON files filter. (RP format)
-				s_filter = C_("RomDataView", "JSON Files|*.json|application/json|All Files|*.*|-");
-				break;
-			case OPTION_COPY_TEXT:
-			case OPTION_COPY_JSON:
-				toClipboard = true;
-				break;
-			default:
-				assert(!"Invalid action ID.");
-				return;
+		const unsigned int id2 = static_cast<unsigned int>(abs(id)) - 1;
+		assert(id2 < 4);
+		if (id2 >= 4) {
+			// Out of range.
+			return;
 		}
+
+		struct StdActsInfo_t {
+			const char *title;	// NOP_C_
+			const char *filter;	// NOP_C_
+			char default_ext[7];
+			bool toClipboard;
+		};
+		static const StdActsInfo_t stdActsInfo[] = {
+			// OPTION_EXPORT_TEXT
+			{NOP_C_("RomDataView", "Export to Text File"),
+			 NOP_C_("RomDataView", "Text Files|*.txt|text/plain|All Files|*.*|-"),
+			 ".txt", false},
+
+			// OPTION_EXPORT_JSON
+			{NOP_C_("RomDataView", "Export to JSON File"),
+			 NOP_C_("RomDataView", "JSON Files|*.json|application/json|All Files|*.*|-"),
+			 ".json", false},
+
+			// OPTION_COPY_TEXT
+			{nullptr, nullptr, "", true},
+
+			// OPTION_COPY_JSON
+			{nullptr, nullptr, "", true},
+		};
+
+		// Standard Actions information for this action
+		const StdActsInfo_t *const info = &stdActsInfo[id2];
 
 		// TODO: GIO wrapper for ostream.
 		// For now, we'll use ofstream.
 		ofstream ofs;
 
-		if (!toClipboard) {
+		if (!info->toClipboard) {
 			if (!page->prevExportDir) {
 				page->prevExportDir = g_path_get_dirname(rom_filename);
 			}
@@ -2647,9 +2653,8 @@ btnOptions_triggered_signal_handler(OptionsMenuButton *menuButton,
 			// NOTE: GTK4 has *mandatory* overwrite confirmation.
 			// Reference: https://gitlab.gnome.org/GNOME/gtk/-/commit/063ad28b1a06328e14ed72cc4b99cd4684efed12
 			GtkFileChooserNative *const dialog = gtk_file_chooser_native_new(
-				s_title,			// title
-				parent,				// parent
-				GTK_FILE_CHOOSER_ACTION_SAVE,	// action
+				dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->title),
+				parent, GTK_FILE_CHOOSER_ACTION_SAVE,
 				_("_Save"), _("_Cancel"));
 			// TODO: URI?
 			GFile *const set_file = g_file_new_for_path(page->prevExportDir);
@@ -2659,9 +2664,8 @@ btnOptions_triggered_signal_handler(OptionsMenuButton *menuButton,
 			}
 #else /* !GTK_CHECK_VERSION(4,0,0) */
 			GtkWidget *const dialog = gtk_file_chooser_dialog_new(
-				s_title,			// title
-				parent,				// parent
-				GTK_FILE_CHOOSER_ACTION_SAVE,	// action
+				dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->title),
+				parent, GTK_FILE_CHOOSER_ACTION_SAVE,	// action
 				_("_Cancel"), GTK_RESPONSE_CANCEL,
 				_("_Save"), GTK_RESPONSE_ACCEPT,
 				nullptr);
@@ -2670,7 +2674,8 @@ btnOptions_triggered_signal_handler(OptionsMenuButton *menuButton,
 #endif /* GTK_CHECK_VERSION(4,0,0) */
 
 			// Set the filters.
-			rpFileDialogFilterToGtk(GTK_FILE_CHOOSER(dialog), s_filter);
+			rpFileDialogFilterToGtk(GTK_FILE_CHOOSER(dialog),
+				dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->filter));
 
 			gchar *const basename = g_path_get_basename(rom_filename);
 			string defaultName = basename;
@@ -2680,7 +2685,9 @@ btnOptions_triggered_signal_handler(OptionsMenuButton *menuButton,
 			if (extpos != string::npos) {
 				defaultName.resize(extpos);
 			}
-			defaultName += s_default_ext;
+			if (info->default_ext[0] != '\0') {
+				defaultName += info->default_ext;
+			}
 			gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog), defaultName.c_str());
 
 			// Prompt for a save file.

src/gtk/stdafx.h
@@ -30,6 +30,7 @@
 #include <cstring>
 #include <cinttypes>
 #include <stdint.h>
+#include <stdlib.h>
 
 // C++ includes.
 #include <algorithm>
@@ -51,6 +52,7 @@
 #include <inttypes.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #endif /* __cplusplus */
 

src/kde/RomDataView.cpp
@@ -1677,50 +1677,57 @@ void RomDataView::btnOptions_triggered(int id)
 			return;
 		QFileInfo fi(U82Q(rom_filename));
 
-		bool toClipboard;
-		QString qs_title;
-		const char *s_default_ext = nullptr;
-		const char *s_filter = nullptr;
-		switch (id) {
-			case OPTION_EXPORT_TEXT:
-				toClipboard = false;
-				qs_title = U82Q(C_("RomDataView", "Export to Text File"));
-				s_default_ext = ".txt";
-				// tr: Text files filter. (RP format)
-				s_filter = C_("RomDataView", "Text Files|*.txt|text/plain|All Files|*.*|-");
-				break;
-			case OPTION_EXPORT_JSON:
-				toClipboard = false;
-				qs_title = U82Q(C_("RomDataView", "Export to JSON File"));
-				s_default_ext = ".json";
-				// tr: JSON files filter. (RP format)
-				s_filter = C_("RomDataView", "JSON Files|*.json|application/json|All Files|*.*|-");
-				break;
-			case OPTION_COPY_TEXT:
-			case OPTION_COPY_JSON:
-				toClipboard = true;
-				break;
-			default:
-				assert(!"Invalid action ID.");
-				return;
+		const unsigned int id2 = static_cast<unsigned int>(abs(id)) - 1;
+		assert(id2 < 4);
+		if (id2 >= 4) {
+			// Out of range.
+			return;
 		}
+
+		struct StdActsInfo_t {
+			const char *title;	// NOP_C_
+			const char *filter;	// NOP_C_
+			char default_ext[7];
+			bool toClipboard;
+		};
+		static const StdActsInfo_t stdActsInfo[] = {
+			// OPTION_EXPORT_TEXT
+			{NOP_C_("RomDataView", "Export to Text File"),
+			 NOP_C_("RomDataView", "Text Files|*.txt|text/plain|All Files|*.*|-"),
+			 ".txt", false},
+
+			// OPTION_EXPORT_JSON
+			{NOP_C_("RomDataView", "Export to JSON File"),
+			 NOP_C_("RomDataView", "JSON Files|*.json|application/json|All Files|*.*|-"),
+			 ".json", false},
+
+			// OPTION_COPY_TEXT
+			{nullptr, nullptr, "", true},
+
+			// OPTION_COPY_JSON
+			{nullptr, nullptr, "", true},
+		};
+
+		// Standard Actions information for this action
+		const StdActsInfo_t *const info = &stdActsInfo[id2];
 
 		// TODO: QTextStream wrapper for ostream.
 		// For now, we'll use ofstream.
 		ofstream ofs;
 
-		if (!toClipboard) {
+		if (!info->toClipboard) {
 			if (d->prevExportDir.isEmpty()) {
 				d->prevExportDir = fi.path();
 			}
 
 			QString defaultFileName = d->prevExportDir + QChar(L'/') + fi.completeBaseName();
-			if (s_default_ext) {
-				defaultFileName += QLatin1String(s_default_ext);
+			if (info->default_ext[0] != '\0') {
+				defaultFileName += QLatin1String(info->default_ext);
 			}
 			QString out_filename = QFileDialog::getSaveFileName(this,
-				qs_title, defaultFileName,
-				rpFileDialogFilterToQt(s_filter));
+				U82Q(dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->title)),
+				defaultFileName,
+				rpFileDialogFilterToQt(dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->filter)));
 			if (out_filename.isEmpty())
 				return;
 

commit de8dc3be1ec4bcdeb3e64ce39f6092f40315b183

[win32] RP_ShellPropSheetExt.cpp: Fix Standard Actions compile errors.
I can't use my Win7 VM right now, so I had to test it in AppVeyor.

This fixes a regression from commit 3cda0c01b6a0cd09f2942f419249ad8a316b8996.
(Refactored RomOps handlers to use a table for Standard Actions information.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -2722,7 +2722,7 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 			}
 
 			const tstring tfilename = LibWin32Common::getSaveFileName(hDlgSheet,
-				U82T_c(dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->title)).c_str(),
+				U82T_c(dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->title)),
 				dpgettext_expr(RP_I18N_DOMAIN, "RomDataView", info->filter),
 				defaultFileName.c_str());
 			if (tfilename.empty())
@@ -2793,7 +2793,7 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 				return;
 		}
 
-		if (toClipboard && OpenClipboard(hDlgSheet)) {
+		if (info->toClipboard && OpenClipboard(hDlgSheet)) {
 			EmptyClipboard();
 			HGLOBAL hglbCopy = GlobalAlloc(GMEM_MOVEABLE, (ts_out.size() + 1) * sizeof(TCHAR));
 			if (hglbCopy) {