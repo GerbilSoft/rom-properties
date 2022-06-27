GerbilSoft/rom-properties

commit 67aeec299774fd473d5a444a916b9df975e41ebc

[gtk] OptionsTab, SystemsTab: Clean up init to fix some GTK4 assertions.
gtk_box_append() was being called twice on some frames, resulting in
the following assertion:

(process:921139): Gtk-CRITICAL **: 22:10:12.807: gtk_box_append: assertion 'gtk_widget_get_parent (child) == NULL' failed

Combine the two container packing sections into one and eliminate the
duplicate function calls.

NOTE: The GTK4 build is still crashing here in Mesa for some reason...

src/gtk/config/SystemsTab.cpp
@@ -209,17 +209,9 @@ systems_tab_init(SystemsTab *tab)
 	gtk_widget_show(lblCGB);
 	gtk_widget_show(tab->cboCGB);
 
+	gtk_box_pack_start(GTK_BOX(tab), fraDMG, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(vboxDMG), lblDMGDescription, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(vboxDMG), table, false, false, 0);
-#endif /* GTK_CHECK_VERSION(4,0,0) */
-
-	// Add the frames to the main VBox.
-	// TODO: Fill?
-#if GTK_CHECK_VERSION(4,0,0)
-	gtk_box_append(GTK_BOX(tab), fraDMG);
-#else /* !GTK_CHECK_VERSION(4,0,0) */
-	gtk_widget_show(fraDMG);
-	gtk_box_pack_start(GTK_BOX(tab), fraDMG, false, false, 0);
 #endif /* GTK_CHECK_VERSION(4,0,0) */
 
 	// Load the current configuration.

src/gtk/config/OptionsTab.cpp
@@ -164,7 +164,6 @@ options_tab_init(OptionsTab *tab)
 
 #if GTK_CHECK_VERSION(4,0,0)
 	gtk_box_append(GTK_BOX(tab), fraDownloads);
-
 	gtk_box_append(GTK_BOX(vboxDownloads), tab->chkExtImgDownloadEnabled);
 	gtk_box_append(GTK_BOX(vboxDownloads), tab->chkUseIntIconForSmallSizes);
 	gtk_box_append(GTK_BOX(vboxDownloads), tab->chkDownloadHighResScans);
@@ -176,6 +175,7 @@ options_tab_init(OptionsTab *tab)
 
 	gtk_box_append(GTK_BOX(tab), fraOptions);
 #else /* !GTK_CHECK_VERSION(4,0,0) */
+	gtk_widget_show(fraDownloads);
 	gtk_widget_show(vboxDownloads);
 	gtk_widget_show(tab->chkExtImgDownloadEnabled);
 	gtk_widget_show(tab->chkUseIntIconForSmallSizes);
@@ -186,10 +186,12 @@ options_tab_init(OptionsTab *tab)
 	gtk_widget_show(lblGameTDBPAL);
 	gtk_widget_show(tab->cboGameTDBPAL);
 
+	gtk_widget_show(fraOptions);
 	gtk_widget_show(vboxOptions);
 	gtk_widget_show(tab->chkShowDangerousPermissionsOverlayIcon);
 	gtk_widget_show(tab->chkEnableThumbnailOnNetworkFS);
 
+	gtk_box_pack_start(GTK_BOX(tab), fraDownloads, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(vboxDownloads), tab->chkExtImgDownloadEnabled, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(vboxDownloads), tab->chkUseIntIconForSmallSizes, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(vboxDownloads), tab->chkDownloadHighResScans, false, false, 0);
@@ -199,21 +201,9 @@ options_tab_init(OptionsTab *tab)
 	gtk_box_pack_start(GTK_BOX(hboxGameTDBPAL), lblGameTDBPAL, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(hboxGameTDBPAL), tab->cboGameTDBPAL, false, false, 0);
 
+	gtk_box_pack_start(GTK_BOX(tab), fraOptions, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(vboxOptions), tab->chkShowDangerousPermissionsOverlayIcon, false, false, 0);
 	gtk_box_pack_start(GTK_BOX(vboxOptions), tab->chkEnableThumbnailOnNetworkFS, false, false, 0);
-#endif /* GTK_CHECK_VERSION(4,0,0) */
-
-	// Add the frames to the main VBox.
-	// TODO: Fill?
-#if GTK_CHECK_VERSION(4,0,0)
-	gtk_box_append(GTK_BOX(tab), fraDownloads);
-	gtk_box_append(GTK_BOX(tab), fraOptions);
-#else /* !GTK_CHECK_VERSION(4,0,0) */
-	gtk_widget_show(fraDownloads);
-	gtk_widget_show(fraOptions);
-
-	gtk_box_pack_start(GTK_BOX(tab), fraDownloads, false, false, 0);
-	gtk_box_pack_start(GTK_BOX(tab), fraOptions, false, false, 0);
 #endif /* GTK_CHECK_VERSION(4,0,0) */
 
 	// Load the current configuration.

commit b2651079551eca9560901089681987a0820ba201

[gtk] SystemsTab: Make sure fraDMG is visible in GTK2/GTK3 builds.
This fixes a regression from commit 67aeec299774fd473d5a444a916b9df975e41ebc.
([gtk] OptionsTab, SystemsTab: Clean up init to fix some GTK4 assertions.)

src/gtk/config/SystemsTab.cpp
@@ -198,6 +198,7 @@ systems_tab_init(SystemsTab *tab)
 	gtk_box_append(GTK_BOX(vboxDMG), lblDMGDescription);
 	gtk_box_append(GTK_BOX(vboxDMG), table);
 #else /* !GTK_CHECK_VERSION(4,0,0) */
+	gtk_widget_show(fraDMG);
 	gtk_widget_show(vboxDMG);
 	gtk_widget_show(lblDMGDescription);
 