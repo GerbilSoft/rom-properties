GerbilSoft/rom-properties

commit 0a5fd876e2aeda150aef67cfcb922b39fcd20274

Various minor code cleanups and lines-of-code reductions.
[gtk] RomDataView:
- Copy the entire ListDataColAttrs_t struct. This struct has a few
  extra fields that aren't needed here, but it makes everything a
  bit cleaner.
- rom_data_view_load_rom_data(): Use rp_gtk_open_uri() from
  is-supported.hpp instead of duplicating the code here.

[win32] RP_ShellPropSheetExt:
- Remove checkLayoutRTL(), since it doesn't work right in most cases
  anyways. dwExStyleRTL is set by calling the LibWin32Common::isSystemRTL()
  function.
- initBitfield(): Don't set our own dwExStyleRTL variable using the
  now-deleted checkLayoutRTL() function.
- initDialog(): Use std::for_each() instead of iterators.

src/gtk/RomDataView.cpp
@@ -13,6 +13,7 @@
 #include "rp-gtk-enums.h"
 #include "RpGtk.hpp"
 #include "sort_funcs.h"
+#include "is-supported.hpp"
 
 // ENABLE_MESSAGESOUND is set by CMakeLists.txt.
 #ifdef ENABLE_MESSAGESOUND
@@ -1171,14 +1172,8 @@ rom_data_view_init_listdata(RomDataView *page,
 	};
 
 	// Set up the column names.
-	uint32_t align_headers = listDataDesc.col_attrs.align_headers;
-	uint32_t align_data = listDataDesc.col_attrs.align_data;
-	uint32_t sizing = listDataDesc.col_attrs.sizing;
-	uint32_t sorting = listDataDesc.col_attrs.sorting;
-	for (int i = 0; i < colCount; i++, align_headers >>= RomFields::TXA_BITS,
-	     align_data >>= RomFields::TXA_BITS,
-	     sizing >>= RomFields::COLSZ_BITS, sorting >>= RomFields::COLSORT_BITS)
-	{
+	RomFields::ListDataColAttrs_t col_attrs = listDataDesc.col_attrs;
+	for (int i = 0; i < colCount; i++, col_attrs.shiftRight()) {
 		const int listStore_col_idx = i + listStore_col_start;
 
 		// NOTE: Not skipping empty column names.
@@ -1197,21 +1192,18 @@ rom_data_view_init_listdata(RomDataView *page,
 		gtk_tree_view_column_add_attribute(column, renderer, "text", listStore_col_idx);
 		gtk_tree_view_append_column(GTK_TREE_VIEW(treeView), column);
 
-		// Header alignment
-		const float header_xalign = align_tbl_xalign[align_headers & RomFields::TXA_MASK];
-		// Data alignment
-		const float data_xalign = align_tbl_xalign[align_data & RomFields::TXA_MASK];
-		const PangoAlignment data_alignment =
-			static_cast<PangoAlignment>(align_tbl_pango[align_data & RomFields::TXA_MASK]);
-
-		g_object_set(column, "alignment", header_xalign, nullptr);
+		// Header/data alignment
+		g_object_set(column,
+			"alignment", align_tbl_xalign[col_attrs.align_headers & RomFields::TXA_MASK],
+			nullptr);
 		g_object_set(renderer,
-			"xalign", data_xalign,
-			"alignment", data_alignment, nullptr);
+			"xalign", align_tbl_xalign[col_attrs.align_data & RomFields::TXA_MASK],
+			"alignment", static_cast<PangoAlignment>(align_tbl_pango[col_attrs.align_data & RomFields::TXA_MASK]),
+			nullptr);
 
 		// Column sizing
 		// NOTE: We don't have direct equivalents to QHeaderView::ResizeMode.
-		switch (sizing & RomFields::COLSZ_MASK) {
+		switch (col_attrs.sizing & RomFields::COLSZ_MASK) {
 			case RomFields::ColSizing::COLSZ_INTERACTIVE:
 				gtk_tree_view_column_set_resizable(column, true);
 				gtk_tree_view_column_set_sizing(column, GTK_TREE_VIEW_COLUMN_GROW_ONLY);
@@ -1242,7 +1234,7 @@ rom_data_view_init_listdata(RomDataView *page,
 		// NOTE: We're setting the sorting functions on the proxy model.
 		// That way, it won't affect the underlying data, which ensures
 		// that RFT_LISTDATA_MULTI is still handled correctly.
-		switch (sorting & RomFields::COLSORT_MASK) {
+		switch (col_attrs.sorting & RomFields::COLSORT_MASK) {
 			default:
 				// Unsupported. We'll use standard sorting.
 				assert(!"Unsupported sorting method.");
@@ -1273,10 +1265,10 @@ rom_data_view_init_listdata(RomDataView *page,
 
 	// Set the default sorting column.
 	// NOTE: sort_dir maps directly to GtkSortType.
-	if (listDataDesc.col_attrs.sort_col >= 0) {
+	if (col_attrs.sort_col >= 0) {
 		gtk_tree_sortable_set_sort_column_id(GTK_TREE_SORTABLE(sortProxy),
-			listDataDesc.col_attrs.sort_col + listStore_col_start,
-			static_cast<GtkSortType>(listDataDesc.col_attrs.sort_dir));
+			col_attrs.sort_col + listStore_col_start,
+			static_cast<GtkSortType>(col_attrs.sort_dir));
 	}
 
 	// Set a minimum height for the scroll area.
@@ -1429,7 +1421,7 @@ rom_data_view_init_string_multi(RomDataView *page,
 	// NOTE: The string contents won't be initialized here.
 	// They will be initialized separately, since the user will
 	// be able to change the displayed language.
-	GtkWidget *const lblStringMulti = rom_data_view_init_string(page, field, fieldIdx, "");
+	GtkWidget *const lblStringMulti = rom_data_view_init_string(page, field, fieldIdx, nullptr);
 	if (lblStringMulti) {
 		page->cxx->vecStringMulti.emplace_back(lblStringMulti, &field);
 	}
@@ -1884,7 +1876,7 @@ rom_data_view_create_options_button(RomDataView *page)
 #if GTK_CHECK_VERSION(3,0,0)
 	if (!GTK_IS_DIALOG(parent)) {
 		// NOTE: As of Nautilus 40, there may be an HdyDeck here.
-		// We're not linking to libhandy, so check the class name.
+		// Check if it's HdyDeck using dynamically-loaded function pointers.
 		if (pfn_hdy_deck_get_type && G_OBJECT_TYPE(parent) == pfn_hdy_deck_get_type()) {
 			// Get the next parent widget.
 			isLibHandy = true;
@@ -2337,30 +2329,8 @@ rom_data_view_load_rom_data(gpointer data)
 	// Do we have a RomData object loaded already?
 	if (!page->romData) {
 		// No RomData object. Load the specified URI.
-
-		// Check if the URI maps to a local file.
-		IRpFile *file = nullptr;
-		gchar *const filename = g_filename_from_uri(page->uri, nullptr, nullptr);
-		if (filename) {
-			// Local file. Use RpFile.
-			file = new RpFile(filename, RpFile::FM_OPEN_READ_GZ);
-			g_free(filename);
-		} else {
-			// Not a local file. Use RpFileGio.
-			file = new RpFileGio(page->uri);
-		}
-
-		if (!file->isOpen()) {
-			// Unable to open the file.
-			file->unref();
-			page->changed_idle = 0;
-			return G_SOURCE_REMOVE;
-		}
-
-		// Create the RomData object.
-		// file is ref()'d by RomData.
-		RomData *const romData = RomDataFactory::create(file);
-		if (romData != page->romData) {
+		RomData *const romData = rp_gtk_open_uri(page->uri);
+		if (romData) {
 			// FIXME: If called from rom_data_view_set_property(), this might
 			// result in *two* notifications.
 			page->romData->unref();
@@ -2368,8 +2338,6 @@ rom_data_view_load_rom_data(gpointer data)
 			page->hasCheckedAchievements = false;
 			g_object_notify_by_pspec(G_OBJECT(page), props[PROP_SHOWING_DATA]);
 		}
-
-		file->unref();
 	} else {
 		// RomData object is already loaded.
 		page->hasCheckedAchievements = false;

src/librpbase/RomFields.hpp
@@ -231,8 +231,20 @@ class RomFields
 			uint32_t align_data;	// Data alignment
 			uint32_t sizing;	// Column sizing
 			uint32_t sorting;	// Column sorting
+
 			int8_t  sort_col;	// Default sort column. (-1 for none)
 			ColSortOrder sort_dir;	// Sort order.
+
+			/**
+			 * Shift the column alignment/sizing/sorting bits by one column.
+			 */
+			void shiftRight(void)
+			{
+				align_headers >>= TXA_BITS;
+				align_data >>= TXA_BITS;
+				sizing >>= COLSZ_BITS;
+				sorting >>= COLSORT_BITS;
+			}
 		};
 
 		// Typedefs for various containers.

src/win32/RP_ShellPropSheetExt.cpp
@@ -50,8 +50,8 @@ using std::set;
 using std::string;
 using std::unique_ptr;
 using std::unordered_map;
-using std::wstring;
 using std::vector;
+using std::wstring;	// for tstring
 
 // Windows 10 and later
 #ifndef DATE_MONTHDAY
@@ -112,9 +112,6 @@ class RP_ShellPropSheetExt_Private
 		HFONT hFontBold;	// Bold font.
 		FontHandler fontHandler;
 
-		// ListView controls. (for toggling LVS_EX_DOUBLEBUFFER)
-		vector<HWND> hwndListViewControls;
-
 		// Header row widgets.
 		HWND lblSysInfo;
 		POINT ptSysInfo;
@@ -122,6 +119,8 @@ class RP_ShellPropSheetExt_Private
 
 		// wtsapi32.dll for Remote Desktop status. (WinXP and later)
 		WTSSessionNotification wts;
+		// ListView controls (for toggling LVS_EX_DOUBLEBUFFER)
+		vector<HWND> hwndListViewControls;
 
 		// Alternate row color.
 		COLORREF colorAltRow;
@@ -212,21 +211,6 @@ class RP_ShellPropSheetExt_Private
 		void loadImages(void);
 
 	private:
-		/**
-		 * Check if we need to use WS_EX_LAYOUTRTL.
-		 * TODO: Cache this value?
-		 * @return WS_EX_LAYOUTRTL if process is RTL; otherwise 0.
-		 */
-		static inline DWORD checkLayoutRTL(void)
-		{
-			// Set WS_EX_LAYOUTRTL if the process is RTL.
-			DWORD dwDefaultLayout;
-			const BOOL bRet = GetProcessDefaultLayout(&dwDefaultLayout);
-			return (unlikely(bRet && dwDefaultLayout == LAYOUT_RTL))
-				? WS_EX_LAYOUTRTL
-				: 0;
-		}
-
 		/**
 		 * Rescale an image to be as close to the required size as possible.
 		 * @param req_sz	[in] Required size.
@@ -869,10 +853,12 @@ int RP_ShellPropSheetExt_Private::initString(_In_ HWND hDlg, _In_ HWND hWndTab,
 		LibWin32Common::measureTextSizeLink(hWndTab, hFont, str_nl, &szText);
 
 		// Determine the position.
-		const int x = (((winRect.right - winRect.left) - szText.cx) / 2) + winRect.left;
-		const int y = winRect.bottom - tmpRect.top - szText.cy;
+		const POINT pos = {
+			(((winRect.right - winRect.left) - szText.cx) / 2) + winRect.left,
+			winRect.bottom - tmpRect.top - szText.cy
+		};
 		// Set the position and size.
-		SetWindowPos(hDlgItem, 0, x, y, szText.cx, szText.cy,
+		SetWindowPos(hDlgItem, 0, pos.x, pos.y, szText.cx, szText.cy,
 			SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 
 		// Clear field_cy so the description widget won't show up
@@ -1059,9 +1045,6 @@ int RP_ShellPropSheetExt_Private::initBitfield(HWND hDlg, HWND hWndTab,
 		rect_subtract.bottom /= 2;
 	}
 	pt.y -= rect_subtract.bottom;
-
-	// WS_EX_LAYOUTRTL
-	const DWORD dwExStyleRTL = checkLayoutRTL();
 
 	row = 0; col = 0;
 	auto iter = tnames.cbegin();
@@ -2367,8 +2350,6 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 		// Add tabs.
 		// NOTE: Tabs must be added *before* calling TabCtrl_AdjustRect();
 		// otherwise, the tab bar height won't be taken into account.
-		TCITEM tcItem;
-		tcItem.mask = TCIF_TEXT;
 		for (int i = 0; i < tabCount; i++) {
 			// Create a tab.
 			const char *name = pFields->tabName(i);
@@ -2377,6 +2358,9 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 				continue;
 			}
 			const tstring tstr = U82T_c(name);
+
+			TCITEM tcItem;
+			tcItem.mask = TCIF_TEXT;
 			tcItem.pszText = const_cast<LPTSTR>(tstr.c_str());
 			// FIXME: Does the index work correctly if a tab is skipped?
 			TabCtrl_InsertItem(tabWidget, i, &tcItem);
@@ -2506,6 +2490,7 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 				// Create checkboxes starting at the current point.
 				field_cy = initBitfield(hDlgSheet, tab.hDlg, pt_start, field, fieldIdx);
 				break;
+
 			case RomFields::RFT_LISTDATA: {
 				// Create a ListView control.
 				size.cy *= 6;	// TODO: Is this needed?
@@ -2606,11 +2591,10 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 	// Update scrollbar settings.
 	// TODO: If a VScroll bar is added, adjust widths of RFT_LISTDATA.
 	// TODO: HScroll bar?
-	const auto tabs_cend = tabs.cend();
-	for (auto iter = tabs.cbegin(); iter != tabs_cend; ++iter) {
+	const int cy = dlgSize.cy;
+	std::for_each(tabs.cbegin(), tabs.cend(), [cy](const tab &tab) {
 		// Set scroll info.
 		// FIXME: Separate child dialog for no tabs.
-		const auto &tab = *iter;
 
 		// VScroll bar
 		SCROLLINFO si;
@@ -2618,7 +2602,7 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 		si.fMask = SIF_ALL;
 		si.nMin = 0;
 		si.nMax = tab.curPt.y - 2;	// max is exclusive
-		si.nPage = dlgSize.cy;
+		si.nPage = cy;
 		si.nPos = 0;
 		SetScrollInfo(tab.hDlg, SB_VERT, &si, TRUE);
 
@@ -2630,7 +2614,7 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 		si.nPage = 2;
 		si.nPos = 0;
 		SetScrollInfo(tab.hDlg, SB_HORZ, &si, TRUE);
-	}
+	});
 
 	// Initial update of RFT_MULTI_STRING fields.
 	if (!vecStringMulti.empty()) {
@@ -2665,16 +2649,14 @@ void RP_ShellPropSheetExt_Private::adjustTabsForMessageWidgetVisibility(bool bVi
 		tab_h -= rectMsgw.bottom;
 	}
 
-	std::for_each(tabs.cbegin(), tabs.cend(),
-		[tab_h](const tab &tab) {
-			RECT tabRect;
-			GetClientRect(tab.hDlg, &tabRect);
-			if (tabRect.bottom != tab_h) {
-				SetWindowPos(tab.hDlg, nullptr, 0, 0, tabRect.right, tab_h,
-					SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOOWNERZORDER | SWP_NOZORDER);
-			}
+	std::for_each(tabs.cbegin(), tabs.cend(), [tab_h](const tab &tab) {
+		RECT tabRect;
+		GetClientRect(tab.hDlg, &tabRect);
+		if (tabRect.bottom != tab_h) {
+			SetWindowPos(tab.hDlg, nullptr, 0, 0, tabRect.right, tab_h,
+				SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 		}
-	);
+	});
 }
 
 /**
@@ -2845,19 +2827,17 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 				return;
 		}
 
-		if (toClipboard) {
-			if (OpenClipboard(hDlgSheet)) {
-				EmptyClipboard();
-				HGLOBAL hglbCopy = GlobalAlloc(GMEM_MOVEABLE, (ts_out.size() + 1) * sizeof(TCHAR));
-				if (hglbCopy) {
-					LPTSTR lpszCopy = static_cast<LPTSTR>(GlobalLock(hglbCopy));
-					memcpy(lpszCopy, ts_out.data(), ts_out.size() * sizeof(TCHAR));
-					lpszCopy[ts_out.size()] = _T('\0');
-					GlobalUnlock(hglbCopy);
-					SetClipboardData(CF_UNICODETEXT, hglbCopy);
-				}
-				CloseClipboard();
+		if (toClipboard && OpenClipboard(hDlgSheet)) {
+			EmptyClipboard();
+			HGLOBAL hglbCopy = GlobalAlloc(GMEM_MOVEABLE, (ts_out.size() + 1) * sizeof(TCHAR));
+			if (hglbCopy) {
+				LPTSTR lpszCopy = static_cast<LPTSTR>(GlobalLock(hglbCopy));
+				memcpy(lpszCopy, ts_out.data(), ts_out.size() * sizeof(TCHAR));
+				lpszCopy[ts_out.size()] = _T('\0');
+				GlobalUnlock(hglbCopy);
+				SetClipboardData(CF_UNICODETEXT, hglbCopy);
 			}
+			CloseClipboard();
 		}
 		return;
 	}
@@ -2892,11 +2872,11 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 		string initialFile = FileSystem::replace_ext(romData->filename(), op->sfi.ext);
 
 		// Prompt for a save file.
-		tstring tstr = LibWin32Common::getSaveFileName(hDlgSheet,
+		tstring t_save_filename = LibWin32Common::getSaveFileName(hDlgSheet,
 			U82T_c(op->sfi.title), filter.c_str(), U82T_s(initialFile));
-		if (tstr.empty())
+		if (t_save_filename.empty())
 			return;
-		s_save_filename = T2U8(tstr);
+		s_save_filename = T2U8(t_save_filename);
 		params.save_filename = s_save_filename.c_str();
 	}
 
@@ -2945,11 +2925,8 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 			// Determine the position.
 			// TODO: Update on DPI change.
 			const int cySmIcon = GetSystemMetrics(SM_CYSMICON);
-			POINT ptMsgw; SIZE szMsgw;
-			szMsgw.cy = cySmIcon + 8;
-			ptMsgw.x = winRect.left;
-			ptMsgw.y = winRect.bottom - szMsgw.cy;
-			szMsgw.cx = winRect.right - winRect.left;
+			SIZE szMsgw = {winRect.right - winRect.left, cySmIcon + 8};
+			POINT ptMsgw = {winRect.left, winRect.bottom - szMsgw.cy};
 			if (tabs.size() > 1) {
 				ptMsgw.x += tmpRect.left;
 				ptMsgw.y -= tmpRect.top;

commit 906ffffbd628406b39e2c9b776b1cffac0e4cbf7

[gtk] rom_data_view_init_string_multi(): Use "", not nullptr.
nullptr tells rom_data_view_init_string() to use the field's string
data, which isn't a valid std::string. This results in a crash when
attempting to view a file with RFT_STRING_MULTI.

This only affected the GTK UI frontend due to an errant "cleanup".

This fixes a regression from commit 0a5fd876e2aeda150aef67cfcb922b39fcd20274.
(Various minor code cleanups and lines-of-code reductions.)

src/gtk/RomDataView.cpp
@@ -1302,7 +1302,7 @@ rom_data_view_init_string_multi(RomDataView *page,
 	// NOTE: The string contents won't be initialized here.
 	// They will be initialized separately, since the user will
 	// be able to change the displayed language.
-	GtkWidget *const lblStringMulti = rom_data_view_init_string(page, field, fieldIdx, nullptr);
+	GtkWidget *const lblStringMulti = rom_data_view_init_string(page, field, fieldIdx, "");
 	if (lblStringMulti) {
 		page->cxx->vecStringMulti.emplace_back(lblStringMulti, &field);
 	}