GerbilSoft/rom-properties

commit 14512c27909a9d7d8de0c9a102a5466030c9473e

[librpfile] IRpFile::filename(): Return `const char*` instead of an `std::string`.
This reduces copying overhead. Note that this does affect the public
libromdata ABI, but since we haven't done a release with it yet, that
shouldn't be a problem.

IRpFile::filename() is no longer virtual, since several subclasses
don't implement this function.

FileSystem::file_ext(): Take `const char*` instead of `const string&`.

Updated various files to handle `nullptr` return values from
IRpFile::filename().

Code size differences: (64-bit Gentoo Linux, gcc-12.1.0, release build, no LTO)

   text    data     bss     dec     hex filename
1571220   92552   17120 1680892  19a5fc libromdata.so.1.0 [before]
1568978   92552   17120 1678650  199d3a libromdata.so.1.0 [after]
  -2242       0       0   -2242    -8c2 Difference

   text    data     bss     dec     hex filename
 305634    9256     720  315610   4d0da rom-properties-gtk3.so [before]
 305442    9256     720  315418   4d01a rom-properties-gtk3.so [after]
   -192       0       0    -192     -c0 Difference

   text    data     bss     dec     hex filename
 501604   21520      96  523220   7fbd4 rom-properties-kf5.so [before]
 501588   21520      96  523204   7fbc4 rom-properties-kf5.so [after]
    -16       0       0     -16     -10 Difference

src/win32/RpFile_IStream.cpp
@@ -672,9 +672,9 @@ off64_t RpFile_IStream::size(void)
 
 /**
  * Get the filename.
- * @return Filename. (May be empty if the filename is not available.)
+ * @return Filename. (May be nullptr if the filename is not available.)
  */
-string RpFile_IStream::filename(void) const
+const char *RpFile_IStream::filename(void) const
 {
 	if (m_filename.empty()) {
 		// Get the filename.
@@ -696,5 +696,5 @@ string RpFile_IStream::filename(void) const
 	}
 
 	// Return the filename.
-	return m_filename;
+	return (!m_filename.empty() ? m_filename.c_str() : nullptr);
 }

src/gtk/RpFile_gio.cpp
@@ -287,10 +287,10 @@ off64_t RpFileGio::size(void)
 /**
  * Get the filename.
  * NOTE: For RpFileGio, this returns a GVfs URI.
- * @return Filename. (May be empty if the filename is not available.)
+ * @return Filename. (May be nullptr if the filename is not available.)
  */
-string RpFileGio::filename(void) const
+const char *RpFileGio::filename(void) const
 {
 	RP_D(const RpFileGio);
-	return d->uri;
+	return (!d->uri.empty() ? d->uri.c_str() : nullptr);
 }

src/gtk/RpFile_gio.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (GTK+ common)                      *
  * RpFile_gio.hpp: IRpFile implementation using GIO/GVfs.                  *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -92,9 +92,9 @@ class RpFileGio final : public LibRpFile::IRpFile
 		/**
 		 * Get the filename.
 		 * NOTE: For RpFileGio, this returns a GVfs URI.
-		 * @return Filename. (May be empty if the filename is not available.)
+		 * @return Filename. (May be nullptr if the filename is not available.)
 		 */
-		std::string filename(void) const final;
+		const char *filename(void) const final;
 };
 
 #endif /* __ROMPROPERTIES_GTK_RPFILE_GIO_HPP__ */

src/kde/RpFile_kio.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KF5)                         *
  * RpFile_kio.cpp: IRpFile implementation using KIO.                       *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -334,10 +334,10 @@ off64_t RpFileKio::size(void)
 /**
  * Get the filename.
  * NOTE: For RpFileKio, this returns a KIO URI.
- * @return Filename. (May be empty if the filename is not available.)
+ * @return Filename. (May be nullptr if the filename is not available.)
  */
-string RpFileKio::filename(void) const
+const char *RpFileKio::filename(void) const
 {
 	RP_D(const RpFileKio);
-	return d->uri.toString().toUtf8().constData();
+	return (!d->uri.isEmpty() ? d->uri.toString().toUtf8().constData() : nullptr);
 }

src/kde/RpFile_kio.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KF5)                         *
  * RpFile_kio.hpp: IRpFile implementation using KIO.                       *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -109,9 +109,9 @@ class RpFileKio : public QObject, public LibRpFile::IRpFile
 		/**
 		 * Get the filename.
 		 * NOTE: For RpFileKio, this returns a KIO URI.
-		 * @return Filename. (May be empty if the filename is not available.)
+		 * @return Filename. (May be nullptr if the filename is not available.)
 		 */
-		std::string filename(void) const final;
+		const char *filename(void) const final;
 
 	signals:
 		// Event loop handling.

src/kde/RpQByteArrayFile.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RpQByteArrayFile.cpp: IRpFile implementation using a QByteArray.        *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -150,14 +150,4 @@ int RpQByteArrayFile::truncate(off64_t size)
 off64_t RpQByteArrayFile::size(void)
 {
 	return m_byteArray.size();
-}
-
-/**
- * Get the filename.
- * @return Filename. (May be empty if the filename is not available.)
- */
-string RpQByteArrayFile::filename(void) const
-{
-	// TODO: Implement this?
-	return string();
 }

src/kde/RpQByteArrayFile.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RpQByteArrayFile.hpp: IRpFile implementation using a QByteArray.        *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -89,12 +89,6 @@ class RpQByteArrayFile : public LibRpFile::IRpFile
 		 * @return File size, or negative on error.
 		 */
 		off64_t size(void) final;
-
-		/**
-		 * Get the filename.
-		 * @return Filename. (May be empty if the filename is not available.)
-		 */
-		std::string filename(void) const final;
 
 	public:
 		/** RpQByteArrayFile-specific functions **/

src/libromdata/Console/Dreamcast.cpp
@@ -348,7 +348,7 @@ Dreamcast::Dreamcast(IRpFile *file)
 	}
 
 	// Check if this disc image is supported.
-	const string filename = file->filename();
+	const char *const filename = file->filename();
 	const DetectInfo info = {
 		{0, static_cast<unsigned int>(size), reinterpret_cast<const uint8_t*>(&sector)},
 		FileSystem::file_ext(filename),	// ext

commit 69e1bc607ed872be66776b09cee0f4d57044a285

[win32] Fix some more filename() and file_ext() breakage.
This fixes a regression from commit 14512c27909a9d7d8de0c9a102a5466030c9473e.
([librpfile] IRpFile::filename(): Return `const char*` instead of an `std::string`.)

src/win32/RP_ExtractIcon_Fallback.cpp
@@ -3,7 +3,7 @@
  * RP_ExtractIcon_Fallback.cpp: IExtractIcon implementation.               *
  * Fallback functions for unsupported files.                               *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -325,7 +325,7 @@ LONG RP_ExtractIcon_Private::Fallback(HICON *phiconLarge, HICON *phiconSmall, UI
 	if (filename.empty()) {
 		return ERROR_FILE_NOT_FOUND;
 	}
-	const char *file_ext = FileSystem::file_ext(filename);
+	const char *file_ext = FileSystem::file_ext(filename.c_str());
 	if (!file_ext) {
 		// Invalid or missing file extension.
 		return ERROR_FILE_NOT_FOUND;

src/win32/RP_ExtractImage_Fallback.cpp
@@ -3,7 +3,7 @@
  * RP_ExtractImage_Fallback.cpp: IExtractImage implementation.             *
  * Fallback functions for unsupported files.                               *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -117,7 +117,7 @@ HRESULT RP_ExtractImage_Private::Fallback(HBITMAP *phBmpImage)
 	if (filename.empty()) {
 		return E_INVALIDARG;
 	}
-	const wchar_t *file_ext = FileSystem::file_ext(filename);
+	const wchar_t *file_ext = FileSystem::file_ext(filename.c_str());
 	if (!file_ext) {
 		// Invalid or missing file extension.
 		return E_INVALIDARG;

src/win32/RpFile_IStream.cpp
@@ -685,7 +685,7 @@ const char *RpFile_IStream::filename(void) const
 		HRESULT hr = m_pStream->Stat(&statstg, STATFLAG_DEFAULT);
 		if (FAILED(hr)) {
 			// Stat() failed.
-			return string();
+			return nullptr;
 		}
 
 		if (statstg.pwcsName) {