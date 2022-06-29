GerbilSoft/rom-properties

commit 14512c27909a9d7d8de0c9a102a5466030c9473e

[librpfile] IRpFile::filename(): Return `const char*` instead of an `std::string`.
This reduces copying overhead. Note that this does affect the public
libromdata ABI, but since we haven't done a release with it yet, that
shouldn't be a problem.

IRpFile::filename() is no longer virtual, since several subclasses
don't implement this function.

FileSystem::file_ext(): Take `const char*` instead of `const string&`.

Updated various files to handle `nullptr` return values from
IRpFile::filename().

Code size differences: (64-bit Gentoo Linux, gcc-12.1.0, release build, no LTO)

   text    data     bss     dec     hex filename
1571220   92552   17120 1680892  19a5fc libromdata.so.1.0 [before]
1568978   92552   17120 1678650  199d3a libromdata.so.1.0 [after]
  -2242       0       0   -2242    -8c2 Difference

   text    data     bss     dec     hex filename
 305634    9256     720  315610   4d0da rom-properties-gtk3.so [before]
 305442    9256     720  315418   4d01a rom-properties-gtk3.so [after]
   -192       0       0    -192     -c0 Difference

   text    data     bss     dec     hex filename
 501604   21520      96  523220   7fbd4 rom-properties-kf5.so [before]
 501588   21520      96  523204   7fbc4 rom-properties-kf5.so [after]
    -16       0       0     -16     -10 Difference

src/librpfile/FileSystem_common.cpp
@@ -88,45 +88,51 @@ const string &getConfigDirectory(void)
 
 /**
  * Get the file extension from a filename or pathname.
+ * NOTE: Returned value points into the specified filename.
  * @param filename Filename
- * @return File extension, including the leading dot (pointer to within the filename) [nullptr if no extension]
+ * @return File extension, including the leading dot; nullptr if no extension.
  */
-template<typename T>
-static const T *T_file_ext(const std::basic_string<T> &filename)
+const char *file_ext(const char *filename)
 {
-	size_t dotpos = filename.find_last_of('.');
-	size_t slashpos = filename.find_last_of(DIR_SEP_CHR);
-	if (dotpos == string::npos ||
-	    dotpos >= filename.size()-1 ||
-	    (slashpos != string::npos && dotpos <= slashpos))
+	if (unlikely(!filename))
+		return nullptr;
+
+	const char *const dotpos = strrchr(filename, '.');
+	const char *const slashpos = strrchr(filename, DIR_SEP_CHR);
+	if (!dotpos || dotpos[1] == '\0' ||
+	    (slashpos && dotpos <= slashpos))
 	{
 		// Invalid or missing file extension.
 		return nullptr;
 	}
 
 	// Return the file extension. (pointer to within the filename)
-	return &filename[dotpos];
-}
-
-/**
- * Get the file extension from a filename or pathname.
- * @param filename Filename
- * @return File extension, including the leading dot (pointer to within the filename) [nullptr if no extension]
- */
-const char* file_ext(const string& filename)
-{
-	return T_file_ext(filename);
+	return dotpos;
 }
 
 #ifdef _WIN32
 /**
  * Get the file extension from a filename or pathname. (wchar_t version)
+ * NOTE: Returned value points into the specified filename.
  * @param filename Filename
- * @return File extension, including the leading dot (pointer to within the filename) [nullptr if no extension]
+ * @return File extension, including the leading dot; nullptr if no extension.
  */
-const wchar_t* file_ext(const std::wstring& filename)
+const char *file_ext(const char *filename)
 {
-	return T_file_ext(filename);
+	if (unlikely(!filename))
+		return nullptr;
+
+	const wchar_t *const dotpos = wcsrchr(filename, '.');
+	const wchar_t *const slashpos = wcsrchr(filename, DIR_SEP_CHR);
+	if (!dotpos || dotpos[1] == L'\0' ||
+	    (slashpos && dotpos <= slashpos))
+	{
+		// Invalid or missing file extension.
+		return nullptr;
+	}
+
+	// Return the file extension. (pointer to within the filename)
+	return dotpos;
 }
 #endif /* _WIN32 */
 

src/gtk/RpFile_gio.cpp
@@ -287,10 +287,10 @@ off64_t RpFileGio::size(void)
 /**
  * Get the filename.
  * NOTE: For RpFileGio, this returns a GVfs URI.
- * @return Filename. (May be empty if the filename is not available.)
+ * @return Filename. (May be nullptr if the filename is not available.)
  */
-string RpFileGio::filename(void) const
+const char *RpFileGio::filename(void) const
 {
 	RP_D(const RpFileGio);
-	return d->uri;
+	return (!d->uri.empty() ? d->uri.c_str() : nullptr);
 }

src/gtk/RpFile_gio.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (GTK+ common)                      *
  * RpFile_gio.hpp: IRpFile implementation using GIO/GVfs.                  *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -92,9 +92,9 @@ class RpFileGio final : public LibRpFile::IRpFile
 		/**
 		 * Get the filename.
 		 * NOTE: For RpFileGio, this returns a GVfs URI.
-		 * @return Filename. (May be empty if the filename is not available.)
+		 * @return Filename. (May be nullptr if the filename is not available.)
 		 */
-		std::string filename(void) const final;
+		const char *filename(void) const final;
 };
 
 #endif /* __ROMPROPERTIES_GTK_RPFILE_GIO_HPP__ */

src/kde/RpFile_kio.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KF5)                         *
  * RpFile_kio.cpp: IRpFile implementation using KIO.                       *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -334,10 +334,10 @@ off64_t RpFileKio::size(void)
 /**
  * Get the filename.
  * NOTE: For RpFileKio, this returns a KIO URI.
- * @return Filename. (May be empty if the filename is not available.)
+ * @return Filename. (May be nullptr if the filename is not available.)
  */
-string RpFileKio::filename(void) const
+const char *RpFileKio::filename(void) const
 {
 	RP_D(const RpFileKio);
-	return d->uri.toString().toUtf8().constData();
+	return (!d->uri.isEmpty() ? d->uri.toString().toUtf8().constData() : nullptr);
 }

src/kde/RpFile_kio.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KF5)                         *
  * RpFile_kio.hpp: IRpFile implementation using KIO.                       *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -109,9 +109,9 @@ class RpFileKio : public QObject, public LibRpFile::IRpFile
 		/**
 		 * Get the filename.
 		 * NOTE: For RpFileKio, this returns a KIO URI.
-		 * @return Filename. (May be empty if the filename is not available.)
+		 * @return Filename. (May be nullptr if the filename is not available.)
 		 */
-		std::string filename(void) const final;
+		const char *filename(void) const final;
 
 	signals:
 		// Event loop handling.

src/kde/RpQByteArrayFile.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RpQByteArrayFile.cpp: IRpFile implementation using a QByteArray.        *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -150,14 +150,4 @@ int RpQByteArrayFile::truncate(off64_t size)
 off64_t RpQByteArrayFile::size(void)
 {
 	return m_byteArray.size();
-}
-
-/**
- * Get the filename.
- * @return Filename. (May be empty if the filename is not available.)
- */
-string RpQByteArrayFile::filename(void) const
-{
-	// TODO: Implement this?
-	return string();
 }

src/kde/RpQByteArrayFile.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RpQByteArrayFile.hpp: IRpFile implementation using a QByteArray.        *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -89,12 +89,6 @@ class RpQByteArrayFile : public LibRpFile::IRpFile
 		 * @return File size, or negative on error.
 		 */
 		off64_t size(void) final;
-
-		/**
-		 * Get the filename.
-		 * @return Filename. (May be empty if the filename is not available.)
-		 */
-		std::string filename(void) const final;
 
 	public:
 		/** RpQByteArrayFile-specific functions **/

src/libromdata/Console/Dreamcast.cpp
@@ -348,7 +348,7 @@ Dreamcast::Dreamcast(IRpFile *file)
 	}
 
 	// Check if this disc image is supported.
-	const string filename = file->filename();
+	const char *const filename = file->filename();
 	const DetectInfo info = {
 		{0, static_cast<unsigned int>(size), reinterpret_cast<const uint8_t*>(&sector)},
 		FileSystem::file_ext(filename),	// ext

commit ae77172e3751beab0b75e4548a6282f4361a911f

[librpfile] FileSystem_common.cpp: The second file_ext() is supposed to take `const wchar_t*`.
This fixes a regression from commit 14512c27909a9d7d8de0c9a102a5466030c9473e.
([librpfile] IRpFile::filename(): Return `const char*` instead of an `std::string`.)

src/librpfile/FileSystem_common.cpp
@@ -117,7 +117,7 @@ const char *file_ext(const char *filename)
  * @param filename Filename
  * @return File extension, including the leading dot; nullptr if no extension.
  */
-const char *file_ext(const char *filename)
+const char *file_ext(const wchar_t *filename)
 {
 	if (unlikely(!filename))
 		return nullptr;