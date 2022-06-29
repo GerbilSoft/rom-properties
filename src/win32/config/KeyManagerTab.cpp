GerbilSoft/rom-properties

commit 4e65d553fe986d9321a1b00249033fa50b4bef5b

[libromdata] KeyStoreUI: Consolidate the import functions into a single dispatch function.
This removes four very-specific functions from the exported symbol table
and replaces it with a more generic function that can be expanded without
breaking the ABI later on.

Moved the GTK+ KeyManagerTab's ImportMenuID to KeyStoreUI and renamed it
to ImportFileID, and turned it into an enum class.

TODO: [kde,win32] KeyManagerTab: Rewrite the "Import" menu action handlers
to use a dispatch function, similar to the GTK+ version.

src/win32/config/KeyManagerTab.cpp
@@ -1388,6 +1388,8 @@ void KeyManagerTabPrivate::loadImages(void)
 
 /** "Import" menu actions. **/
 
+/** TODO: Combine into a single function by using the menu ID or something. **/
+
 /**
  * Import keys from Wii keys.bin. (BootMii format)
  */
@@ -1409,7 +1411,8 @@ void KeyManagerTabPrivate::importWiiKeysBin(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = keyStore->importWiiKeysBin(T2U8(tfilename).c_str());
+	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::WiiKeysBin, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "Wii keys.bin")), iret);
 }
@@ -1435,7 +1438,8 @@ void KeyManagerTabPrivate::importWiiUOtpBin(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = keyStore->importWiiUOtpBin(T2U8(tfilename).c_str());
+	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::WiiUOtpBin, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "Wii U otp.bin"), iret);
 }
@@ -1461,7 +1465,8 @@ void KeyManagerTabPrivate::import3DSboot9bin(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = keyStore->import3DSboot9bin(T2U8(tfilename).c_str());
+	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::N3DSboot9bin, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "3DS boot9.bin"), iret);
 }
@@ -1487,7 +1492,8 @@ void KeyManagerTabPrivate::import3DSaeskeydb(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = keyStore->import3DSaeskeydb(T2U8(tfilename).c_str());
+	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::N3DSaeskeydb, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "3DS aeskeydb.bin"), iret);
 }

src/gtk/config/KeyManagerTab.cpp
@@ -24,13 +24,7 @@ using namespace LibRpBase;
 // C++ STL classes
 using std::string;
 
-// TODO: Move to KeyStoreUI?
-enum ImportMenuID {
-	IMPORT_WII_KEYS_BIN	= 0,
-	IMPORT_WII_U_OTP_BIN	= 1,
-	IMPORT_3DS_BOOT9_BIN	= 2,
-	IMPORT_3DS_AESKEYDB_BIN	= 3,
-};
+// KeyStoreUI::ImportFileID
 static const char *const import_menu_actions[] = {
 	"Wii keys.bin",
 	"Wii U otp.bin",
@@ -561,9 +555,10 @@ key_manager_tab_menu_action_response(GtkFileChooserDialog *dialog, gint response
 static void
 key_manager_tab_handle_menu_action(KeyManagerTab *tab, gint id)
 {
-	assert(id >= IMPORT_WII_KEYS_BIN);
-	assert(id <= IMPORT_3DS_AESKEYDB_BIN);
-	if (id < IMPORT_WII_KEYS_BIN || id > IMPORT_3DS_AESKEYDB_BIN)
+	assert(id >= (int)KeyStoreUI::ImportFileID::WiiKeysBin);
+	assert(id <= (int)KeyStoreUI::ImportFileID::N3DSaeskeydb);
+	if (id < (int)KeyStoreUI::ImportFileID::WiiKeysBin ||
+	    id > (int)KeyStoreUI::ImportFileID::N3DSaeskeydb)
 		return;
 
 	static const char *const dialog_titles_tbl[] = {
@@ -659,6 +654,13 @@ key_manager_tab_show_key_import_return_status(KeyManagerTab	*tab,
 		default:
 			msg = C_("KeyManagerTab",
 				"An invalid parameter was passed to the key importer.\n"
+				"THIS IS A BUG; please report this to the developers!");
+			type = GTK_MESSAGE_ERROR;
+			break;
+
+		case KeyStoreUI::ImportStatus::UnknownKeyID:
+			msg = C_("KeyManagerTab",
+				"An unknown key ID was passed to the key importer.\n"
 				"THIS IS A BUG; please report this to the developers!");
 			type = GTK_MESSAGE_ERROR;
 			break;
@@ -817,8 +819,8 @@ key_manager_tab_menu_action_response(GtkFileChooserDialog *dialog, gint response
 	}
 
 	// Get the file ID from the dialog.
-	const ImportMenuID id = (ImportMenuID)
-		GPOINTER_TO_INT(g_object_get_data(G_OBJECT(dialog), "KeyManagerTab.fileID"));
+	const KeyStoreUI::ImportFileID id = static_cast<KeyStoreUI::ImportFileID>(
+		GPOINTER_TO_INT(g_object_get_data(G_OBJECT(dialog), "KeyManagerTab.fileID")));
 
 #if GTK_CHECK_VERSION(4,0,0)
 	// TODO: URIs?
@@ -840,28 +842,10 @@ key_manager_tab_menu_action_response(GtkFileChooserDialog *dialog, gint response
 	}
 
 	KeyStoreUI *const keyStoreUI = key_store_gtk_get_key_store_ui(tab->keyStore);
-	KeyStoreUI::ImportReturn iret;
-	switch (id) {
-		case IMPORT_WII_KEYS_BIN:
-			iret = keyStoreUI->importWiiKeysBin(in_filename);
-			break;
-		case IMPORT_WII_U_OTP_BIN:
-			iret = keyStoreUI->importWiiUOtpBin(in_filename);
-			break;
-		case IMPORT_3DS_BOOT9_BIN:
-			iret = keyStoreUI->import3DSboot9bin(in_filename);
-			break;
-		case IMPORT_3DS_AESKEYDB_BIN:
-			iret = keyStoreUI->import3DSaeskeydb(in_filename);
-			break;
-		default:
-			assert(!"Invalid key file ID.");
-			g_free(in_filename);
-			return;
-	}
+	KeyStoreUI::ImportReturn iret = keyStoreUI->importKeysFromBin(id, in_filename);
 
 	// TODO: Show the key import status in a MessageWidget.
-	key_manager_tab_show_key_import_return_status(tab, in_filename, import_menu_actions[id], iret);
+	key_manager_tab_show_key_import_return_status(tab, in_filename, import_menu_actions[(int)id], iret);
 	g_free(in_filename);
 }
 

src/kde/config/KeyManagerTab.cpp
@@ -9,12 +9,13 @@
 #include "stdafx.h"
 #include "KeyManagerTab.hpp"
 
-// librpbase
-using namespace LibRpBase;
-
 #include "KeyStoreQt.hpp"
 #include "KeyStoreModel.hpp"
 #include "KeyStoreItemDelegate.hpp"
+
+// librpbase, libromdata
+using namespace LibRpBase;
+using namespace LibRomData;
 
 // C++ STL classes.
 using std::string;
@@ -137,7 +138,7 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 	// TODO: Thread-safe strerror()?
 
 	switch (iret.status) {
-		case KeyStoreQt::ImportStatus::InvalidParams:
+		case KeyStoreUI::ImportStatus::InvalidParams:
 		default:
 			msg = C_("KeyManagerTab",
 				"An invalid parameter was passed to the key importer.\n"
@@ -146,7 +147,15 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 			icon = QStyle::SP_MessageBoxCritical;
 			break;
 
-		case KeyStoreQt::ImportStatus::OpenError:
+		case KeyStoreUI::ImportStatus::UnknownKeyID:
+			msg = C_("KeyManagerTab",
+				"An unknown key ID was passed to the key importer.\n"
+				"THIS IS A BUG; please report this to the developers!");
+			type = KMessageWidget::Error;
+			icon = QStyle::SP_MessageBoxCritical;
+			break;
+
+		case KeyStoreUI::ImportStatus::OpenError:
 			if (iret.error_code != 0) {
 				msg = rp_sprintf_p(C_("KeyManagerTab",
 					// tr: %1$s == filename, %2$s == error message
@@ -163,7 +172,7 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 			icon = QStyle::SP_MessageBoxCritical;
 			break;
 
-		case KeyStoreQt::ImportStatus::ReadError:
+		case KeyStoreUI::ImportStatus::ReadError:
 			// TODO: Error code for short reads.
 			if (iret.error_code != 0) {
 				msg = rp_sprintf_p(C_("KeyManagerTab",
@@ -181,7 +190,7 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 			icon = QStyle::SP_MessageBoxCritical;
 			break;
 
-		case KeyStoreQt::ImportStatus::InvalidFile:
+		case KeyStoreUI::ImportStatus::InvalidFile:
 			msg = rp_sprintf_p(C_("KeyManagerTab",
 				// tr: %1$s == filename, %2$s == type of file
 				"The file '%1$s' is not a valid %2$s file."),
@@ -191,7 +200,7 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 			icon = QStyle::SP_MessageBoxWarning;
 			break;
 
-		case KeyStoreQt::ImportStatus::NoKeysImported:
+		case KeyStoreUI::ImportStatus::NoKeysImported:
 			msg = rp_sprintf(C_("KeyManagerTab",
 				// tr: %s == filename
 				"No keys were imported from '%s'."),
@@ -201,7 +210,7 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 			showKeyStats = true;
 			break;
 
-		case KeyStoreQt::ImportStatus::KeysImported: {
+		case KeyStoreUI::ImportStatus::KeysImported: {
 			const unsigned int keyCount = iret.keysImportedVerify + iret.keysImportedNoVerify;
 			msg = rp_sprintf_p(NC_("KeyManagerTab",
 				// tr: %1$s == number of keys (formatted), %2$u == filename
@@ -423,6 +432,8 @@ void KeyManagerTab::save(QSettings *pSettings)
 
 /** Actions. **/
 
+/** TODO: Combine into a single function by storing an ID in the QActions. **/
+
 /**
  * Import keys from Wii keys.bin. (BootMii format)
  */
@@ -440,7 +451,8 @@ void KeyManagerTab::on_actionImportWiiKeysBin_triggered(void)
 		return;
 	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	KeyStoreQt::ImportReturn iret = d->keyStore->importWiiKeysBin(Q2U8(filename));
+	KeyStoreQt::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::WiiKeysBin, Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("Wii keys.bin"), iret);
 }
 
@@ -461,7 +473,8 @@ void KeyManagerTab::on_actionImportWiiUOtpBin_triggered(void)
 		return;
 	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	KeyStoreQt::ImportReturn iret = d->keyStore->importWiiUOtpBin(Q2U8(filename));
+	KeyStoreQt::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::WiiUOtpBin, Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("Wii U otp.bin"), iret);
 }
 
@@ -482,7 +495,8 @@ void KeyManagerTab::on_actionImport3DSboot9bin_triggered(void)
 		return;
 	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	KeyStoreQt::ImportReturn iret = d->keyStore->import3DSboot9bin(Q2U8(filename));
+	KeyStoreQt::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::N3DSboot9bin, Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("3DS boot9.bin"), iret);
 }
 
@@ -503,6 +517,7 @@ void KeyManagerTab::on_actionImport3DSaeskeydb_triggered(void)
 		return;
 	d->keyFileDir = QFileInfo(filename).canonicalPath();
 
-	KeyStoreQt::ImportReturn iret = d->keyStore->import3DSaeskeydb(Q2U8(filename));
+	KeyStoreQt::ImportReturn iret = d->keyStore->importKeysFromBin(
+		KeyStoreUI::ImportFileID::N3DSaeskeydb, Q2U8(filename));
 	d->showKeyImportReturnStatus(filename, QLatin1String("3DS aeskeydb.bin"), iret);
 }

src/libromdata/crypto/KeyStoreUI.cpp
@@ -195,6 +195,37 @@ class KeyStoreUIPrivate
 		 * @return Hexadecimal string.
 		 */
 		static string binToHexStr(const uint8_t *data, unsigned int len);
+
+	public:
+		/** Key import functions **/
+
+		/**
+		 * Import keys from Wii keys.bin. (BootMii format)
+		 * @param filename keys.bin filename.
+		 * @return Key import status.
+		 */
+		KeyStoreUI::ImportReturn importWiiKeysBin(const char *filename);
+
+		/**
+		 * Import keys from Wii U otp.bin.
+		 * @param filename otp.bin filename.
+		 * @return Key import status.
+		 */
+		KeyStoreUI::ImportReturn importWiiUOtpBin(const char *filename);
+
+		/**
+		 * Import keys from 3DS boot9.bin.
+		 * @param filename boot9.bin filename.
+		 * @return Key import status.
+		 */
+		KeyStoreUI::ImportReturn importN3DSboot9bin(const char *filename);
+
+		/**
+		 * Import keys from 3DS aeskeydb.bin.
+		 * @param filename aeskeydb.bin filename.
+		 * @return Key import status.
+		 */
+		KeyStoreUI::ImportReturn importN3DSaeskeydb(const char *filename);
 };
 
 /** KeyStoreUIPrivate **/
@@ -1069,21 +1100,21 @@ bool KeyStoreUI::hasChanged(void) const
  * @param filename keys.bin filename.
  * @return Number of keys imported if the file is valid; negative POSIX error code on error.
  */
-KeyStoreUI::ImportReturn KeyStoreUI::importWiiKeysBin(const char *filename)
+KeyStoreUI::ImportReturn KeyStoreUIPrivate::importWiiKeysBin(const char *filename)
 {
-	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
+	KeyStoreUI::ImportReturn iret = {KeyStoreUI::ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
 	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
 		// TODO: file->lastError()?
-		iret.status = ImportStatus::OpenError;
+		iret.status = KeyStoreUI::ImportStatus::OpenError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
 
 	// File must be 1,024 bytes.
 	if (file->size() != 1024) {
-		iret.status = ImportStatus::InvalidFile;
+		iret.status = KeyStoreUI::ImportStatus::InvalidFile;
 		return iret;
 	}
 
@@ -1093,7 +1124,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiKeysBin(const char *filename)
 	if (size != 1024) {
 		// Read error.
 		// TODO: file->lastError()?
-		iret.status = ImportStatus::ReadError;
+		iret.status = KeyStoreUI::ImportStatus::ReadError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
@@ -1104,7 +1135,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiKeysBin(const char *filename)
 	static const char BackupMii_magic[] = "BackupMii v1";
 	if (memcmp(buf, BackupMii_magic, sizeof(BackupMii_magic)-1) != 0) {
 		// TODO: Check for v0.
-		iret.status = ImportStatus::InvalidFile;
+		iret.status = KeyStoreUI::ImportStatus::InvalidFile;
 		return iret;
 	}
 
@@ -1120,8 +1151,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiKeysBin(const char *filename)
 	};
 
 	// Import the keys.
-	RP_D(KeyStoreUI);
-	return d->importKeysFromBlob(KeyStoreUIPrivate::SectionID::WiiPartition,
+	return importKeysFromBlob(KeyStoreUIPrivate::SectionID::WiiPartition,
 		keyBinAddress, buf, sizeof(buf));
 }
 
@@ -1130,20 +1160,20 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiKeysBin(const char *filename)
  * @param filename otp.bin filename.
  * @return Key import status.
  */
-KeyStoreUI::ImportReturn KeyStoreUI::importWiiUOtpBin(const char *filename)
+KeyStoreUI::ImportReturn KeyStoreUIPrivate::importWiiUOtpBin(const char *filename)
 {
-	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
+	KeyStoreUI::ImportReturn iret = {KeyStoreUI::ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
 	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
-		iret.status = ImportStatus::OpenError;
+		iret.status = KeyStoreUI::ImportStatus::OpenError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
 
 	// File must be 1,024 bytes.
 	if (file->size() != 1024) {
-		iret.status = ImportStatus::InvalidFile;
+		iret.status = KeyStoreUI::ImportStatus::InvalidFile;
 		return iret;
 	}
 
@@ -1152,7 +1182,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiUOtpBin(const char *filename)
 	size_t size = file->read(buf, sizeof(buf));
 	if (size != 1024) {
 		// Read error.
-		iret.status = ImportStatus::ReadError;
+		iret.status = KeyStoreUI::ImportStatus::ReadError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
@@ -1179,7 +1209,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiUOtpBin(const char *filename)
 		isDebug = true;
 	} else {
 		// Not a match.
-		iret.status = ImportStatus::InvalidFile;
+		iret.status = KeyStoreUI::ImportStatus::InvalidFile;
 		return iret;
 	}
 
@@ -1218,8 +1248,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiUOtpBin(const char *filename)
 		(likely(!isDebug) ? keyBinAddress_retail : keyBinAddress_debug);
 
 	// Import the keys.
-	RP_D(KeyStoreUI);
-	return d->importKeysFromBlob(KeyStoreUIPrivate::SectionID::WiiPartition,
+	return importKeysFromBlob(KeyStoreUIPrivate::SectionID::WiiPartition,
 		kba, buf, sizeof(buf));
 }
 
@@ -1228,13 +1257,13 @@ KeyStoreUI::ImportReturn KeyStoreUI::importWiiUOtpBin(const char *filename)
  * @param filename boot9.bin filename.
  * @return Number of keys imported if the file is valid; negative POSIX error code on error.
  */
-KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
+KeyStoreUI::ImportReturn KeyStoreUIPrivate::importN3DSboot9bin(const char *filename)
 {
-	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
+	KeyStoreUI::ImportReturn iret = {KeyStoreUI::ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
 	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
-		iret.status = ImportStatus::OpenError;
+		iret.status = KeyStoreUI::ImportStatus::OpenError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
@@ -1244,7 +1273,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 	// - 32,768 bytes: Protected boot9
 	const off64_t fileSize = file->size();
 	if (fileSize != 65536 && fileSize != 32768) {
-		iret.status = ImportStatus::InvalidFile;
+		iret.status = KeyStoreUI::ImportStatus::InvalidFile;
 		return iret;
 	}
 
@@ -1255,7 +1284,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 		int ret = file->seek(32768);
 		if (ret != 0) {
 			// Seek error.
-			iret.status = ImportStatus::ReadError;
+			iret.status = KeyStoreUI::ImportStatus::ReadError;
 			iret.error_code = static_cast<uint8_t>(file->lastError());
 			return iret;
 		}
@@ -1263,7 +1292,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 	size_t size = file->read(buf.get(), 32768);
 	if (size != 32768) {
 		// Read error.
-		iret.status = ImportStatus::ReadError;
+		iret.status = KeyStoreUI::ImportStatus::ReadError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
@@ -1292,7 +1321,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 		const uint32_t crc = crc32(0, buf.get(), 32768);
 		if (crc != 0x9D50A525) {
 			// Incorrect CRC32.
-			iret.status = ImportStatus::InvalidFile;
+			iret.status = KeyStoreUI::ImportStatus::InvalidFile;
 			return iret;
 		}
 	}
@@ -1311,8 +1340,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
 	};
 
 	// Import the keys.
-	RP_D(KeyStoreUI);
-	return d->importKeysFromBlob(KeyStoreUIPrivate::SectionID::N3DSVerifyKeys,
+	return importKeysFromBlob(KeyStoreUIPrivate::SectionID::N3DSVerifyKeys,
 		keyBinAddress, buf.get(), 32768);
 }
 
@@ -1321,13 +1349,13 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSboot9bin(const char *filename)
  * @param filename aeskeydb.bin filename.
  * @return Key import status.
  */
-KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
+KeyStoreUI::ImportReturn KeyStoreUIPrivate::importN3DSaeskeydb(const char *filename)
 {
-	ImportReturn iret = {ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
+	KeyStoreUI::ImportReturn iret = {KeyStoreUI::ImportStatus::InvalidParams, 0, 0, 0, 0, 0, 0, 0};
 
 	unique_RefBase<RpFile> file(new RpFile(filename, RpFile::FM_OPEN_READ));
 	if (!file->isOpen()) {
-		iret.status = ImportStatus::OpenError;
+		iret.status = KeyStoreUI::ImportStatus::OpenError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
@@ -1335,7 +1363,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 	// File must be <= 64 KB and a multiple of 32 bytes.
 	const off64_t fileSize = file->size();
 	if (fileSize == 0 || fileSize > 65536 || (fileSize % 32 != 0)) {
-		iret.status = ImportStatus::InvalidFile;
+		iret.status = KeyStoreUI::ImportStatus::InvalidFile;
 		return iret;
 	}
 
@@ -1344,7 +1372,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 	size_t size = file->read(buf.get(), static_cast<size_t>(fileSize));
 	if (size != static_cast<size_t>(fileSize)) {
 		// Read error.
-		iret.status = ImportStatus::ReadError;
+		iret.status = KeyStoreUI::ImportStatus::ReadError;
 		iret.error_code = static_cast<uint8_t>(file->lastError());
 		return iret;
 	}
@@ -1355,7 +1383,7 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 	// - Decrypt9WIP: isDevkitKey == 0 for retail, non-zero for debug
 	// - SafeB9SInstaller: keyUnitType == 0 for ALL units, 1 for retail only, 2 for debug only
 	// To prevent issues, we'll check both retail and debug for all keys.
-	typedef struct _AesKeyInfo {
+	struct AesKeyInfo {
 		uint8_t slot;		// keyslot, 0x00...0x3F 
 		char type;		// type 'X' / 'Y' / 'N' for normalKey / 'I' for IV
 		char id[10];		// key ID for special keys, all zero for standard keys
@@ -1363,24 +1391,24 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 		uint8_t keyUnitType;	// see above
 		uint8_t isEncrypted;	// if non-zero, key is encrypted using Slot0x2C, with KeyY = 0
 		uint8_t key[16];
-	} AesKeyInfo;
+	};
 	ASSERT_STRUCT(AesKeyInfo, 32);
 
 	// Slot0x2CKeyX is needed to decrypt keys if the
 	// aeskeydb.bin file is encrypted.
-	RP_D(KeyStoreUI);
 	unique_ptr<IAesCipher> cipher;
 	u128_t aeskeydb_key;
-	if (d->getAesKeyDB_key(&aeskeydb_key) == 0) {
+	if (getAesKeyDB_key(&aeskeydb_key) == 0) {
 		// TODO: Support for debug-encrypted aeskeydb.bin?
 		cipher.reset(AesCipherFactory::create());
 		cipher->setChainingMode(IAesCipher::ChainingMode::CTR);
 		cipher->setKey(aeskeydb_key.u8, sizeof(aeskeydb_key.u8));
 	}
 
+	RP_Q(KeyStoreUI);
 	AesKeyInfo *aesKey = reinterpret_cast<AesKeyInfo*>(buf.get());
 	const AesKeyInfo *const aesKeyEnd = reinterpret_cast<const AesKeyInfo*>(buf.get() + fileSize);
-	const int keyIdxStart = d->sections[(int)KeyStoreUIPrivate::SectionID::N3DSVerifyKeys].keyIdxStart;
+	const int keyIdxStart = sections[(int)KeyStoreUIPrivate::SectionID::N3DSVerifyKeys].keyIdxStart;
 	bool wereKeysImported = false;
 	do {
 		// Check if this is a supported keyslot.
@@ -1530,8 +1558,8 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 		// Check if the key is OK.
 		bool keyChecked = false;
 		for (int i = keyCount; i > 0; i--, pKeyIdx++) {
-			Key *const pKey = &d->keys[keyIdxStart + *pKeyIdx];
-			if (pKey->status == Key::Status::OK) {
+			KeyStoreUI::Key *const pKey = &keys[keyIdxStart + *pKeyIdx];
+			if (pKey->status == KeyStoreUI::Key::Status::OK) {
 				// Key is already OK. Don't bother with it.
 				iret.keysExist++;
 				keyChecked = true;
@@ -1542,18 +1570,18 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 			const uint8_t *const verifyData = N3DSVerifyKeys::encryptionVerifyData_static(*pKeyIdx);
 			if (verifyData) {
 				// Verify the key.
-				int ret = d->verifyKeyData(aesKey->key, verifyData, 16);
+				int ret = verifyKeyData(aesKey->key, verifyData, 16);
 				if (ret == 0) {
 					// Found a match!
-					const string new_value = d->binToHexStr(aesKey->key, sizeof(aesKey->key));
+					const string new_value = binToHexStr(aesKey->key, sizeof(aesKey->key));
 					if (pKey->value != new_value) {
 						pKey->value = new_value;
 						pKey->status = KeyStoreUI::Key::Status::OK;
 						pKey->modified = true;
 						iret.keysImportedVerify++;
 						wereKeysImported = true;
-						emit keyChanged_int((int)KeyStoreUIPrivate::SectionID::N3DSVerifyKeys, *pKeyIdx);
-						emit keyChanged_int(keyIdxStart + *pKeyIdx);
+						emit q->keyChanged_int((int)KeyStoreUIPrivate::SectionID::N3DSVerifyKeys, *pKeyIdx);
+						emit q->keyChanged_int(keyIdxStart + *pKeyIdx);
 					} else {
 						// No change.
 						iret.keysExist++;
@@ -1566,15 +1594,15 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 			} else {
 				// Can't verify this key...
 				// Import it anyway.
-				const string new_value = d->binToHexStr(aesKey->key, sizeof(aesKey->key));
+				const string new_value = binToHexStr(aesKey->key, sizeof(aesKey->key));
 				if (pKey->value != new_value) {
 					pKey->value = new_value;
 					pKey->status = KeyStoreUI::Key::Status::Unknown;
 					pKey->modified = true;
 					iret.keysImportedNoVerify++;
 					wereKeysImported = true;
-					emit keyChanged_int((int)KeyStoreUIPrivate::SectionID::N3DSVerifyKeys, *pKeyIdx);
-					emit keyChanged_int(keyIdxStart + *pKeyIdx);
+					emit q->keyChanged_int((int)KeyStoreUIPrivate::SectionID::N3DSVerifyKeys, *pKeyIdx);
+					emit q->keyChanged_int(keyIdxStart + *pKeyIdx);
 				} else {
 					// No change.
 					iret.keysExist++;
@@ -1592,11 +1620,40 @@ KeyStoreUI::ImportReturn KeyStoreUI::import3DSaeskeydb(const char *filename)
 	} while (++aesKey != aesKeyEnd);
 
 	if (wereKeysImported) {
-		emit modified_int();
+		emit q->modified_int();
 	}
 	iret.status = (wereKeysImported
 		? KeyStoreUI::ImportStatus::KeysImported
 		: KeyStoreUI::ImportStatus::NoKeysImported);
+	return iret;
+}
+
+/**
+ * Import keys from a binary file.
+ * @param fileID Type of file
+ * @param filename Filename
+ * @return ImportReturn
+ */
+KeyStoreUI::ImportReturn KeyStoreUI::importKeysFromBin(ImportFileID fileID, const char *filename)
+{
+	RP_D(KeyStoreUI);
+	switch (fileID) {
+		case ImportFileID::WiiKeysBin:
+			return d->importWiiKeysBin(filename);
+		case ImportFileID::WiiUOtpBin:
+			return d->importWiiUOtpBin(filename);
+		case ImportFileID::N3DSboot9bin:
+			return d->importN3DSboot9bin(filename);
+		case ImportFileID::N3DSaeskeydb:
+			return d->importN3DSaeskeydb(filename);
+		default:
+			break;
+	}
+
+	// Unknown key ID.
+	// TODO: Mark this as static const? Not sure if that's
+	// actually beneficial here.
+	KeyStoreUI::ImportReturn iret = {KeyStoreUI::ImportStatus::UnknownKeyID, 0, 0, 0, 0, 0, 0, 0};
 	return iret;
 }
 

src/libromdata/crypto/KeyStoreUI.hpp
@@ -197,6 +197,7 @@ class RP_LIBROMDATA_PUBLIC KeyStoreUI
 	public:
 		enum class ImportStatus : uint8_t {
 			InvalidParams = 0,	// Invalid parameters. (Should not happen!)
+			UnknownKeyID,		// Unknown key ID. (Should not happen!)
 			OpenError,		// Could not open the file. (TODO: More info?)
 			ReadError,		// Could not read the file. (TODO: More info?)
 			InvalidFile,		// File is not the correct type.
@@ -219,32 +220,22 @@ class RP_LIBROMDATA_PUBLIC KeyStoreUI
 		};
 
 		/**
-		 * Import keys from Wii keys.bin. (BootMii format)
-		 * @param filename keys.bin filename.
-		 * @return Key import status.
+		 * Import file ID
 		 */
-		ImportReturn importWiiKeysBin(const char *filename);
+		enum class ImportFileID {
+			WiiKeysBin = 0,
+			WiiUOtpBin,
+			N3DSboot9bin,
+			N3DSaeskeydb,
+		};
 
 		/**
-		 * Import keys from Wii U otp.bin.
-		 * @param filename otp.bin filename.
-		 * @return Key import status.
+		 * Import keys from a binary file.
+		 * @param fileID Type of file
+		 * @param filename Filename
+		 * @return ImportReturn
 		 */
-		ImportReturn importWiiUOtpBin(const char *filename);
-
-		/**
-		 * Import keys from 3DS boot9.bin.
-		 * @param filename boot9.bin filename.
-		 * @return Key import status.
-		 */
-		ImportReturn import3DSboot9bin(const char *filename);
-
-		/**
-		 * Import keys from 3DS aeskeydb.bin.
-		 * @param filename aeskeydb.bin filename.
-		 * @return Key import status.
-		 */
-		ImportReturn import3DSaeskeydb(const char *filename);
+		ImportReturn importKeysFromBin(ImportFileID fileID, const char *filename);
 };
 
 }

commit fc3e6bc2234a89142da0ec8d9f958db64af23b7e

[win32] KeyManagerTab: Remove "d->" from the import function calls.
Copy/paste error, derp.

This fixes a regression from commit 4e65d553fe986d9321a1b00249033fa50b4bef5b.
([libromdata] KeyStoreUI: Consolidate the import functions into a single dispatch function.)

src/win32/config/KeyManagerTab.cpp
@@ -1411,7 +1411,7 @@ void KeyManagerTabPrivate::importWiiKeysBin(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+	KeyStoreWin32::ImportReturn iret = keyStore->importKeysFromBin(
 		KeyStoreUI::ImportFileID::WiiKeysBin, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "Wii keys.bin")), iret);
@@ -1438,7 +1438,7 @@ void KeyManagerTabPrivate::importWiiUOtpBin(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+	KeyStoreWin32::ImportReturn iret = keyStore->importKeysFromBin(
 		KeyStoreUI::ImportFileID::WiiUOtpBin, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "Wii U otp.bin"), iret);
@@ -1465,7 +1465,7 @@ void KeyManagerTabPrivate::import3DSboot9bin(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+	KeyStoreWin32::ImportReturn iret = keyStore->importKeysFromBin(
 		KeyStoreUI::ImportFileID::N3DSboot9bin, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "3DS boot9.bin"), iret);
@@ -1492,7 +1492,7 @@ void KeyManagerTabPrivate::import3DSaeskeydb(void)
 	// Update ts_keyFileDir using the returned filename.
 	updateKeyFileDir(tfilename);
 
-	KeyStoreWin32::ImportReturn iret = d->keyStore->importKeysFromBin(
+	KeyStoreWin32::ImportReturn iret = keyStore->importKeysFromBin(
 		KeyStoreUI::ImportFileID::N3DSaeskeydb, T2U8(tfilename).c_str());
 	// TODO: Port showKeyImportReturnStatus from the KDE version.
 	//d->showKeyImportReturnStatus(filename, U82T_c(C_("KeyManagerTab", "3DS aeskeydb.bin"), iret);