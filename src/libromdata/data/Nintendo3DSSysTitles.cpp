GerbilSoft/rom-properties

commit f8ad1094f06ae7859248b528fc42df91985535fb

[libromdata] Converted the rest of the data/ static classes to namespaces.
Code size differences: (64-bit Gentoo Linux, gcc-12.1.0, release build, no LTO)

   text    data     bss     dec     hex filename
1568526   92552   17120 1678198  199b76 libromdata.so.1.0 [before]
1568042   92568   17120 1677730  1999a2 libromdata.so.1.0 [after]
   -484     +16       0    -468    -1d4 Difference

src/libromdata/data/Nintendo3DSSysTitles.cpp
@@ -2,59 +2,39 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * Nintendo3DSSysTitles.cpp: Nintendo 3DS system title lookup.             *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
 #include "stdafx.h"
 #include "Nintendo3DSSysTitles.hpp"
 
-namespace LibRomData {
+namespace LibRomData { namespace Nintendo3DSSysTitlesPrivate {
 
-class Nintendo3DSSysTitlesPrivate
-{
-	private:
-		// Static class.
-		Nintendo3DSSysTitlesPrivate();
-		~Nintendo3DSSysTitlesPrivate();
-		RP_DISABLE_COPY(Nintendo3DSSysTitlesPrivate)
-
-	public:
-		// System title groups are split up by tid_hi.
-		// New3DS-specific is indicated by $x0000000, where x == 2.
-		struct SysTitle {
-			uint32_t tid_lo[6];	// 6 regions. (If 0, not available.)
-			const char *desc;	// Description.
-		};
-
-		/* TODO: Use this if more groups are added.
-		struct SysTitleGroup {
-			uint32_t tid_hi;
-			const SysTitle *titles;
-			int titles_size;
-		}; */
-
-		// Regions.
-		static const char regions[6][4];
-
-		static const SysTitle sys_title_00040010[];	// System applications.
-		static const SysTitle sys_title_00040030[];	// System applets.
-
-		//static const SysTitleGroup sys_title_group[];	// All SysTitle[] arrays.
+// System title groups are split up by tid_hi.
+// New3DS-specific is indicated by $x0000000, where x == 2.
+struct SysTitle {
+	uint32_t tid_lo[6];	// 6 regions. (If 0, not available.)
+	const char *desc;	// Description.
 };
 
-/** Nintendo3DSSysTitlesPrivate **/
+/* TODO: Use this if more groups are added.
+struct SysTitleGroup {
+	uint32_t tid_hi;
+	const SysTitle *titles;
+	int titles_size;
+}; */
 
-// Regions.
-const char Nintendo3DSSysTitlesPrivate::regions[6][4] = {
+// Regions
+static const char regions[6][4] = {
 	"JPN", "USA", "EUR",
 	"CHN", "KOR", "TWN",
 };
 
 /**
- * System applications. (tid hi == 0x00040010)
+ * System applications (tid hi == 0x00040010)
  */
-const Nintendo3DSSysTitlesPrivate::SysTitle Nintendo3DSSysTitlesPrivate::sys_title_00040010[] = {
+static const SysTitle sys_title_00040010[] = {
 	/** Common titles **/
 	{{0x00020000, 0x00021000, 0x00022000, 0x00026000, 0x00027000, 0x00028000}, NOP_C_("Nintendo3DSSysTitles", "System Settings")},
 	{{0x00020100, 0x00021100, 0x00022100, 0x00026100, 0x00027100, 0x00028100}, NOP_C_("Nintendo3DSSysTitles", "Download Play")},
@@ -80,9 +60,9 @@ const Nintendo3DSSysTitlesPrivate::SysTitle Nintendo3DSSysTitlesPrivate::sys_tit
 };
 
 /**
- * System applets. (tid hi == 0x00040030)
+ * System applets (tid hi == 0x00040030)
  */
-const Nintendo3DSSysTitlesPrivate::SysTitle Nintendo3DSSysTitlesPrivate::sys_title_00040030[] = {
+static const SysTitle sys_title_00040030[] = {
 	/** Common titles **/
 	{{0x00008202, 0x00008F02, 0x00009802, 0x0000A102, 0x0000A902, 0x0000B102}, NOP_C_("Nintendo3DSSysTitles", "HOME Menu")},
 	{{0x00008402, 0x00009002, 0x00009902, 0x0000A202, 0x0000AA02, 0x0000B202}, NOP_C_("Nintendo3DSSysTitles", "Camera")},
@@ -106,18 +86,18 @@ const Nintendo3DSSysTitlesPrivate::SysTitle Nintendo3DSSysTitlesPrivate::sys_tit
 	{{0x2000C003, 0x2000C803, 0x2000D003,          0, 0x2000DE03,          0}, NOP_C_("Nintendo3DSSysTitles", "Software Keyboard (SAFE_MODE)")},
 };
 
-/** Nintendo3DSSysTitles **/
+/** Public functions **/
 
 /**
  * Look up a Nintendo 3DS system title.
- * @param tid_hi	[in] Title ID High.
- * @param tid_lo	[in] Title ID Low.
- * @param pRegion	[out,opt] Output for region name.
+ * @param tid_hi	[in] Title ID High
+ * @param tid_lo	[in] Title ID Low
+ * @param pRegion	[out,opt] Output for region name
  * @return System title name, or nullptr on error.
  */
-const char *Nintendo3DSSysTitles::lookup_sys_title(uint32_t tid_hi, uint32_t tid_lo, const char **pRegion)
+const char *lookup_sys_title(uint32_t tid_hi, uint32_t tid_lo, const char **pRegion)
 {
-	const Nintendo3DSSysTitlesPrivate::SysTitle *titles;
+	const SysTitle *titles;
 	int title_count;
 
 	if (tid_hi == 0 || tid_lo == 0 ||
@@ -131,11 +111,11 @@ const char *Nintendo3DSSysTitles::lookup_sys_title(uint32_t tid_hi, uint32_t tid
 	}
 
 	if (tid_hi == 0x00040010) {
-		titles = Nintendo3DSSysTitlesPrivate::sys_title_00040010;
-		title_count = ARRAY_SIZE(Nintendo3DSSysTitlesPrivate::sys_title_00040010);
+		titles = sys_title_00040010;
+		title_count = ARRAY_SIZE(sys_title_00040010);
 	} else if (tid_hi == 0x00040030) {
-		titles = Nintendo3DSSysTitlesPrivate::sys_title_00040030;
-		title_count = ARRAY_SIZE(Nintendo3DSSysTitlesPrivate::sys_title_00040030);
+		titles = sys_title_00040030;
+		title_count = ARRAY_SIZE(sys_title_00040030);
 	} else {
 		// tid_hi not supported.
 		if (pRegion) {
@@ -150,7 +130,7 @@ const char *Nintendo3DSSysTitles::lookup_sys_title(uint32_t tid_hi, uint32_t tid
 			if (titles->tid_lo[region] != 0 && titles->tid_lo[region] == tid_lo) {
 				// Found a match!
 				if (pRegion) {
-					*pRegion = Nintendo3DSSysTitlesPrivate::regions[region];
+					*pRegion = regions[region];
 				}
 				return dpgettext_expr(RP_I18N_DOMAIN, "Nintendo3DSSysTitles", titles->desc);
 			}
@@ -161,4 +141,4 @@ const char *Nintendo3DSSysTitles::lookup_sys_title(uint32_t tid_hi, uint32_t tid
 	return nullptr;
 }
 
-}
+} }

src/libromdata/data/SegaPublishers.cpp
@@ -2,48 +2,29 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * SegaPublishers.cpp: Sega third-party publishers list.                   *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
 #include "stdafx.h"
 #include "SegaPublishers.hpp"
 
-namespace LibRomData {
+namespace LibRomData { namespace SegaPublishersPrivate {
 
-class SegaPublishersPrivate {
-	private:
-		// Static class.
-		SegaPublishersPrivate();
-		~SegaPublishersPrivate();
-		RP_DISABLE_COPY(SegaPublishersPrivate)
-
-	public:
-		/**
-		 * Sega third-party publisher list.
-		 * Reference: http://segaretro.org/Third-party_T-series_codes
-		 */
-		struct TCodeEntry {
-			unsigned int t_code;
-			const char *publisher;
-		};
-		static const TCodeEntry tcodeList[];
-
-		/**
-		 * Comparison function for bsearch().
-		 * @param a
-		 * @param b
-		 * @return
-		 */
-		static int RP_C_API compar(const void *a, const void *b);
-
+/**
+ * Sega third-party publisher list.
+ * Reference: http://segaretro.org/Third-party_T-series_codes
+ */
+struct TCodeEntry {
+	unsigned int t_code;
+	const char *publisher;
 };
 
 /**
  * Sega third-party publisher list.
  * Reference: http://segaretro.org/Third-party_T-series_codes
  */
-const SegaPublishersPrivate::TCodeEntry SegaPublishersPrivate::tcodeList[] = {
+static const TCodeEntry tcodeList[] = {
 	{0,	"Sega"},
 	{11,	"Taito"},
 	{12,	"Capcom"},
@@ -401,7 +382,7 @@ const SegaPublishersPrivate::TCodeEntry SegaPublishersPrivate::tcodeList[] = {
  * @param b
  * @return
  */
-int RP_C_API SegaPublishersPrivate::compar(const void *a, const void *b)
+static int RP_C_API compar(const void *a, const void *b)
 {
 	unsigned int code1 = static_cast<const TCodeEntry*>(a)->t_code;
 	unsigned int code2 = static_cast<const TCodeEntry*>(b)->t_code;
@@ -410,22 +391,24 @@ int RP_C_API SegaPublishersPrivate::compar(const void *a, const void *b)
 	return 0;
 }
 
+/** Public functions **/
+
 /**
  * Look up a company code.
  * @param code Company code.
  * @return Publisher, or nullptr if not found.
  */
-const char *SegaPublishers::lookup(unsigned int code)
+const char *lookup(unsigned int code)
 {
 	// Do a binary search.
-	const SegaPublishersPrivate::TCodeEntry key = {code, nullptr};
-	const SegaPublishersPrivate::TCodeEntry *res =
-		static_cast<const SegaPublishersPrivate::TCodeEntry*>(bsearch(&key,
-			SegaPublishersPrivate::tcodeList,
-			ARRAY_SIZE(SegaPublishersPrivate::tcodeList)-1,
-			sizeof(SegaPublishersPrivate::TCodeEntry),
-			SegaPublishersPrivate::compar));
+	const TCodeEntry key = {code, nullptr};
+	const TCodeEntry *res =
+		static_cast<const TCodeEntry*>(bsearch(&key,
+			tcodeList,
+			ARRAY_SIZE(tcodeList)-1,
+			sizeof(TCodeEntry),
+			compar));
 	return (res ? res->publisher : nullptr);
 }
 
-}
+} }

src/libromdata/data/ELFData.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * ELFData.cpp: Executable and Linkable Format data.                       *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -10,39 +10,16 @@
 #include "ELFData.hpp"
 #include "Other/elf_structs.h"
 
-namespace LibRomData {
+namespace LibRomData { namespace ELFData {
 
-class ELFDataPrivate {
-	private:
-		// Static class.
-		ELFDataPrivate();
-		~ELFDataPrivate();
-		RP_DISABLE_COPY(ELFDataPrivate)
-
-	public:
-		// CPUs
-		struct MachineType {
-			uint16_t cpu;
-			const char *name;
-		};
-		static const char *const machineTypes_low[];
-		static const MachineType machineTypes_other[];
-
-		// OS ABIs
-		static const char *const osabi_names[];
-
-		/**
-		 * bsearch() comparison function for MachineType.
-		 * @param a
-		 * @param b
-		 * @return
-		 */
-		static int RP_C_API MachineType_compar(const void *a, const void *b);
+struct MachineType {
+	uint16_t cpu;
+	const char *name;
 };
 
 // ELF machine types. (contiguous low IDs)
 // Reference: https://github.com/file/file/blob/master/magic/Magdir/elf
-const char *const ELFDataPrivate::machineTypes_low[] = {
+static const char *const machineTypes_low[] = {
 	// 0
 	"No machine",
 	"AT&T WE 32100 (M32)",
@@ -291,7 +268,7 @@ const char *const ELFDataPrivate::machineTypes_low[] = {
 
 // ELF machine types. (other IDs)
 // Reference: https://github.com/file/file/blob/master/magic/Magdir/elf
-const ELFDataPrivate::MachineType ELFDataPrivate::machineTypes_other[] = {
+static const MachineType machineTypes_other[] = {
 	{243,		"RISC-V"},
 	{244,		"Lanai"},
 	{247,		"Linux eBPF"},
@@ -337,7 +314,7 @@ const ELFDataPrivate::MachineType ELFDataPrivate::machineTypes_other[] = {
 
 // ELF OS ABI names.
 // Reference: https://github.com/file/file/blob/master/magic/Magdir/elf
-const char *const ELFDataPrivate::osabi_names[] = {
+static const char *const osabi_names[] = {
 	// 0
 	"UNIX System V",
 	"HP-UX",
@@ -369,7 +346,7 @@ const char *const ELFDataPrivate::osabi_names[] = {
  * @param b
  * @return
  */
-int RP_C_API ELFDataPrivate::MachineType_compar(const void *a, const void *b)
+static int RP_C_API MachineType_compar(const void *a, const void *b)
 {
 	const uint16_t cpu1 = static_cast<const MachineType*>(a)->cpu;
 	const uint16_t cpu2 = static_cast<const MachineType*>(b)->cpu;
@@ -378,29 +355,31 @@ int RP_C_API ELFDataPrivate::MachineType_compar(const void *a, const void *b)
 	return 0;
 }
 
+/** Public functions **/
+
 /**
  * Look up an ELF machine type. (CPU)
  * @param cpu ELF machine type.
  * @return Machine type name, or nullptr if not found.
  */
-const char *ELFData::lookup_cpu(uint16_t cpu)
+const char *lookup_cpu(uint16_t cpu)
 {
-	static_assert(ARRAY_SIZE(ELFDataPrivate::machineTypes_low) == 224+2,
-		"ELFDataPrivate::machineTypes_low[] is missing entries.");
-	if (cpu < ARRAY_SIZE(ELFDataPrivate::machineTypes_low)-1) {
+	static_assert(ARRAY_SIZE(machineTypes_low) == 224+2,
+		"ELFData::machineTypes_low[] is missing entries.");
+	if (cpu < ARRAY_SIZE(machineTypes_low)-1) {
 		// CPU ID is in the contiguous low IDs array.
-		return ELFDataPrivate::machineTypes_low[cpu];
+		return machineTypes_low[cpu];
 	}
 
 	// CPU ID is in the "other" IDs array.
 	// Do a binary search.
-	const ELFDataPrivate::MachineType key = {cpu, nullptr};
-	const ELFDataPrivate::MachineType *res =
-		static_cast<const ELFDataPrivate::MachineType*>(bsearch(&key,
-			ELFDataPrivate::machineTypes_other,
-			ARRAY_SIZE(ELFDataPrivate::machineTypes_other)-1,
-			sizeof(ELFDataPrivate::MachineType),
-			ELFDataPrivate::MachineType_compar));
+	const MachineType key = {cpu, nullptr};
+	const MachineType *res =
+		static_cast<const MachineType*>(bsearch(&key,
+			machineTypes_other,
+			ARRAY_SIZE(machineTypes_other)-1,
+			sizeof(MachineType),
+			MachineType_compar));
 	return (res ? res->name : nullptr);
 }
 
@@ -409,11 +388,11 @@ const char *ELFData::lookup_cpu(uint16_t cpu)
  * @param osabi ELF OS ABI.
  * @return OS ABI name, or nullptr if not found.
  */
-const char *ELFData::lookup_osabi(uint8_t osabi)
+const char *lookup_osabi(uint8_t osabi)
 {
-	if (osabi < ARRAY_SIZE(ELFDataPrivate::osabi_names)-1) {
+	if (osabi < ARRAY_SIZE(osabi_names)-1) {
 		// OS ABI ID is in the array.
-		return ELFDataPrivate::osabi_names[osabi];
+		return osabi_names[osabi];
 	}
 
 	switch (osabi) {
@@ -428,4 +407,4 @@ const char *ELFData::lookup_osabi(uint8_t osabi)
 	return nullptr;
 }
 
-}
+} }

src/libromdata/data/ELFData.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * ELFData.hpp: Executable and Linkable Format data.                       *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -14,32 +14,22 @@
 // C includes.
 #include <stdint.h>
 
-namespace LibRomData {
+namespace LibRomData { namespace ELFData {
 
-class ELFData
-{
-	private:
-		// Static class.
-		ELFData();
-		~ELFData();
-		RP_DISABLE_COPY(ELFData)
+/**
+ * Look up an ELF machine type. (CPU)
+ * @param cpu ELF machine type.
+ * @return Machine type name, or nullptr if not found.
+ */
+const char *lookup_cpu(uint16_t cpu);
 
-	public:
-		/**
-		 * Look up an ELF machine type. (CPU)
-		 * @param cpu ELF machine type.
-		 * @return Machine type name, or nullptr if not found.
-		 */
-		static const char *lookup_cpu(uint16_t cpu);
+/**
+ * Look up an ELF OS ABI.
+ * @param osabi ELF OS ABI.
+ * @return OS ABI name, or nullptr if not found.
+ */
+const char *lookup_osabi(uint8_t osabi);
 
-		/**
-		 * Look up an ELF OS ABI.
-		 * @param osabi ELF OS ABI.
-		 * @return OS ABI name, or nullptr if not found.
-		 */
-		static const char *lookup_osabi(uint8_t osabi);
-};
-
-}
+} }
 
 #endif /* __ROMPROPERTIES_LIBROMDATA_ELFDATA_HPP__ */

src/libromdata/data/EXEData.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * EXEData.cpp: DOS/Windows executable data.                               *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -10,35 +10,16 @@
 #include "EXEData.hpp"
 #include "Other/exe_structs.h"
 
-namespace LibRomData {
+namespace LibRomData { namespace EXEData {
 
-class EXEDataPrivate {
-	private:
-		// Static class.
-		EXEDataPrivate();
-		~EXEDataPrivate();
-		RP_DISABLE_COPY(EXEDataPrivate)
-
-	public:
-		struct MachineType {
-			uint16_t cpu;
-			const char *name;
-		};
-		static const MachineType machineTypes_PE[];
-		static const MachineType machineTypes_LE[];
-
-		/**
-		 * bsearch() comparison function for MachineType.
-		 * @param a
-		 * @param b
-		 * @return
-		 */
-		static int RP_C_API MachineType_compar(const void *a, const void *b);
+struct MachineType {
+	uint16_t cpu;
+	const char *name;
 };
 
 // PE machine types.
 // NOTE: The cpu field *must* be sorted in ascending order.
-const EXEDataPrivate::MachineType EXEDataPrivate::machineTypes_PE[] = {
+static const MachineType machineTypes_PE[] = {
 	{IMAGE_FILE_MACHINE_I386,	"Intel i386"},
 	{IMAGE_FILE_MACHINE_R3000_BE,	"MIPS R3000 (big-endian)"},
 	{IMAGE_FILE_MACHINE_R3000,	"MIPS R3000"},
@@ -81,7 +62,7 @@ const EXEDataPrivate::MachineType EXEDataPrivate::machineTypes_PE[] = {
 
 // LE machine types.
 // NOTE: The cpu field *must* be sorted in ascending order.
-const EXEDataPrivate::MachineType EXEDataPrivate::machineTypes_LE[] = {
+static const MachineType machineTypes_LE[] = {
 	{LE_CPU_80286,		"Intel i286"},
 	{LE_CPU_80386,		"Intel i386"},
 	{LE_CPU_80486,		"Intel i486"},
@@ -101,7 +82,7 @@ const EXEDataPrivate::MachineType EXEDataPrivate::machineTypes_LE[] = {
  * @param b
  * @return
  */
-int RP_C_API EXEDataPrivate::MachineType_compar(const void *a, const void *b)
+static int RP_C_API MachineType_compar(const void *a, const void *b)
 {
 	const uint16_t cpu1 = static_cast<const MachineType*>(a)->cpu;
 	const uint16_t cpu2 = static_cast<const MachineType*>(b)->cpu;
@@ -110,21 +91,23 @@ int RP_C_API EXEDataPrivate::MachineType_compar(const void *a, const void *b)
 	return 0;
 }
 
+/** Public functions **/
+
 /**
  * Look up a PE machine type. (CPU)
  * @param cpu PE machine type.
  * @return Machine type name, or nullptr if not found.
  */
-const char *EXEData::lookup_pe_cpu(uint16_t cpu)
+const char *lookup_pe_cpu(uint16_t cpu)
 {
 	// Do a binary search.
-	const EXEDataPrivate::MachineType key = {cpu, nullptr};
-	const EXEDataPrivate::MachineType *res =
-		static_cast<const EXEDataPrivate::MachineType*>(bsearch(&key,
-			EXEDataPrivate::machineTypes_PE,
-			ARRAY_SIZE(EXEDataPrivate::machineTypes_PE)-1,
-			sizeof(EXEDataPrivate::MachineType),
-			EXEDataPrivate::MachineType_compar));
+	const MachineType key = {cpu, nullptr};
+	const MachineType *res =
+		static_cast<const MachineType*>(bsearch(&key,
+			machineTypes_PE,
+			ARRAY_SIZE(machineTypes_PE)-1,
+			sizeof(MachineType),
+			MachineType_compar));
 	return (res ? res->name : nullptr);
 }
 
@@ -133,17 +116,17 @@ const char *EXEData::lookup_pe_cpu(uint16_t cpu)
  * @param cpu LE machine type.
  * @return Machine type name, or nullptr if not found.
  */
-const char *EXEData::lookup_le_cpu(uint16_t cpu)
+const char *lookup_le_cpu(uint16_t cpu)
 {
 	// Do a binary search.
-	const EXEDataPrivate::MachineType key = {cpu, nullptr};
-	const EXEDataPrivate::MachineType *res =
-		static_cast<const EXEDataPrivate::MachineType*>(bsearch(&key,
-			EXEDataPrivate::machineTypes_LE,
-			ARRAY_SIZE(EXEDataPrivate::machineTypes_LE)-1,
-			sizeof(EXEDataPrivate::MachineType),
-			EXEDataPrivate::MachineType_compar));
+	const MachineType key = {cpu, nullptr};
+	const MachineType *res =
+		static_cast<const MachineType*>(bsearch(&key,
+			machineTypes_LE,
+			ARRAY_SIZE(machineTypes_LE)-1,
+			sizeof(MachineType),
+			MachineType_compar));
 	return (res ? res->name : nullptr);
 }
 
-}
+} }

src/libromdata/data/EXEData.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * EXEData.hpp: DOS/Windows executable data.                               *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -14,32 +14,22 @@
 // C includes.
 #include <stdint.h>
 
-namespace LibRomData {
+namespace LibRomData { namespace EXEData {
 
-class EXEData
-{
-	private:
-		// Static class.
-		EXEData();
-		~EXEData();
-		RP_DISABLE_COPY(EXEData)
+/**
+ * Look up a PE machine type. (CPU)
+ * @param cpu PE machine type.
+ * @return Machine type name, or nullptr if not found.
+ */
+const char *lookup_pe_cpu(uint16_t cpu);
 
-	public:
-		/**
-		 * Look up a PE machine type. (CPU)
-		 * @param cpu PE machine type.
-		 * @return Machine type name, or nullptr if not found.
-		 */
-		static const char *lookup_pe_cpu(uint16_t cpu);
+/**
+ * Look up an LE machine type. (CPU)
+ * @param cpu LE machine type.
+ * @return Machine type name, or nullptr if not found.
+ */
+const char *lookup_le_cpu(uint16_t cpu);
 
-		/**
-		 * Look up an LE machine type. (CPU)
-		 * @param cpu LE machine type.
-		 * @return Machine type name, or nullptr if not found.
-		 */
-		static const char *lookup_le_cpu(uint16_t cpu);
-};
-
-}
+} }
 
 #endif /* __ROMPROPERTIES_LIBROMDATA_EXEDATA_HPP__ */

src/libromdata/data/MachOData.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * MachOData.cpp: Mach-O executable format data.                           *
  *                                                                         *
- * Copyright (c) 2019 by David Korth.                                      *
+ * Copyright (c) 2019-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -10,14 +10,14 @@
 #include "MachOData.hpp"
 #include "Other/macho_structs.h"
 
-namespace LibRomData {
+namespace LibRomData { namespace MachOData {
 
 /**
  * Look up a Mach-O CPU type.
  * @param cputype Mach-O CPU type.
  * @return CPU type name, or nullptr if not found.
  */
-const char *MachOData::lookup_cpu_type(uint32_t cputype)
+const char *lookup_cpu_type(uint32_t cputype)
 {
 	const unsigned int abi = (cputype >> 24);
 	const unsigned int cpu = (cputype & 0xFFFFFF);
@@ -76,7 +76,7 @@ const char *MachOData::lookup_cpu_type(uint32_t cputype)
  * @param cpusubtype Mach-O CPU subtype.
  * @return OS ABI name, or nullptr if not found.
  */
-const char *MachOData::lookup_cpu_subtype(uint32_t cputype, uint32_t cpusubtype)
+const char *lookup_cpu_subtype(uint32_t cputype, uint32_t cpusubtype)
 {
 	const unsigned int abi = (cputype >> 24) & 1;
 	cpusubtype &= 0xFFFFFF;
@@ -293,4 +293,4 @@ const char *MachOData::lookup_cpu_subtype(uint32_t cputype, uint32_t cpusubtype)
 	return s_cpu_subtype;
 }
 
-}
+} }

src/libromdata/data/MachOData.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * MachOData.hpp: Mach-O executable format data.                           *
  *                                                                         *
- * Copyright (c) 2020 by David Korth.                                      *
+ * Copyright (c) 2020-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -14,33 +14,23 @@
 // C includes.
 #include <stdint.h>
 
-namespace LibRomData {
+namespace LibRomData { namespace MachOData {
 
-class MachOData
-{
-	private:
-		// Static class.
-		MachOData();
-		~MachOData();
-		RP_DISABLE_COPY(MachOData)
+/**
+ * Look up a Mach-O CPU type.
+ * @param cputype Mach-O CPU type.
+ * @return CPU type name, or nullptr if not found.
+ */
+const char *lookup_cpu_type(uint32_t cputype);
 
-	public:
-		/**
-		 * Look up a Mach-O CPU type.
-		 * @param cputype Mach-O CPU type.
-		 * @return CPU type name, or nullptr if not found.
-		 */
-		static const char *lookup_cpu_type(uint32_t cputype);
+/**
+ * Look up a Mach-O CPU subtype.
+ * @param cputype Mach-O CPU type.
+ * @param cpusubtype Mach-O CPU subtype.
+ * @return OS ABI name, or nullptr if not found.
+ */
+const char *lookup_cpu_subtype(uint32_t cputype, uint32_t cpusubtype);
 
-		/**
-		 * Look up a Mach-O CPU subtype.
-		 * @param cputype Mach-O CPU type.
-		 * @param cpusubtype Mach-O CPU subtype.
-		 * @return OS ABI name, or nullptr if not found.
-		 */
-		static const char *lookup_cpu_subtype(uint32_t cputype, uint32_t cpusubtype);
-};
-
-}
+} }
 
 #endif /* __ROMPROPERTIES_LIBROMDATA_DATA_MACHODATA_HPP__ */

commit 12e32bb71e1f0dbf14318bbb8f13a698cccdcd09

[libromdata] Nintendo3DSSysTitles, SegaPublishers: Fix some errors when converting to namespaces.
These shouldn't be "Private" namespaces.

[ 62%] Linking CXX executable ../../bin/rpcli
../../lib/libromdata.so.1.0: undefined reference to `LibRomData::SegaPublishers::lookup(unsigned int)'
../../lib/libromdata.so.1.0: undefined reference to `LibRomData::Nintendo3DSSysTitles::lookup_sys_title(unsigned int, unsigned int, char const**)'
collect2: error: ld returned 1 exit status
src/rpcli/CMakeFiles/rpcli.dir/build.make:180: recipe for target 'bin/rpcli' failed
make[2]: *** [bin/rpcli] Error 1

This fixes a regression from commit f8ad1094f06ae7859248b528fc42df91985535fb.
([libromdata] Converted the rest of the data/ static classes to namespaces.)

src/libromdata/data/Nintendo3DSSysTitles.cpp
@@ -9,7 +9,7 @@
 #include "stdafx.h"
 #include "Nintendo3DSSysTitles.hpp"
 
-namespace LibRomData { namespace Nintendo3DSSysTitlesPrivate {
+namespace LibRomData { namespace Nintendo3DSSysTitles {
 
 // System title groups are split up by tid_hi.
 // New3DS-specific is indicated by $x0000000, where x == 2.

src/libromdata/data/SegaPublishers.cpp
@@ -9,7 +9,7 @@
 #include "stdafx.h"
 #include "SegaPublishers.hpp"
 
-namespace LibRomData { namespace SegaPublishersPrivate {
+namespace LibRomData { namespace SegaPublishers {
 
 /**
  * Sega third-party publisher list.