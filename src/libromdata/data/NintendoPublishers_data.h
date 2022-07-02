d4eab710b69fcf01b8132f4212541f4bc8c24b4c
David Korth <gerbilsoft@gerbilsoft.com>
2022-07-01 23:01:42 -0400

    [libromdata] NintendoPublishers: Use string tables for the publishers.
    Two string tables are in use:
    
    - NintendoPublishers: Valid for most Nintendo systems. ID is a two-char
      identifier where each char is in range [0-9A-Z]; therefore, the index
      is calculated using Base 36.
    
    - NintendoPublishers_FDS: Specific to Famicom Disk System. This uses an
      8-bit ID (the "old" publisher code), but it has two names, EN and JP.
    
    TODO: Use the JP name for something.
    
    Code size differences: (64-bit Gentoo Linux, gcc-12.1.0, release build, no LTO)
    
       text    data     bss     dec     hex filename
       6793    7568       0   14361    3819 NintendoPublishers.cpp.o
      10889       0       0   10889    2a89 NintendoPublishers.cpp.o
      +4096   -7568       0   -3472    -d90 Difference
    
    TODO: Combine the various Python scripts together.

---
+++ src/libromdata/data/NintendoPublishers_data.h
@@ -0,0 +1,390 @@
+/** Nintendo Third-Party Publishers (generated from NintendoPublishers_data.txt) **/
+
+#include <stdint.h>
+
+static const char NintendoPublishers_strtbl[] =
+	"\x00" "<unlicensed>" "\x00" "Nintendo" "\x00" "Rocket Games / Aj"
+	"inomoto" "\x00" "Imagineer-Zoom" "\x00" "Gray Matter" "\x00" "Za"
+	"muse" "\x00" "Falcom" "\x00" "Enix" "\x00" "Capcom" "\x00" "Hot "
+	"B Co." "\x00" "Jaleco" "\x00" "Coconuts Japan" "\x00" "Coconuts "
+	"Japan / G.X.Media" "\x00" "Micronet" "\x00" "Technos" "\x00" "Me"
+	"bio Software" "\x00" "Shouei System" "\x00" "Starfish" "\x00" "M"
+	"itsui Fudosan / Dentsu" "\x00" "Warashi Inc." "\x00" "Nowpro" "\x00"
+	"Game Village" "\x00" "IE Institute" "\x00" "Infocom" "\x00" "Ele"
+	"ctronic Arts Japan" "\x00" "Cobra Team" "\x00" "Human / Field" "\x00"
+	"KOEI" "\x00" "Hudson Soft" "\x00" "S.C.P." "\x00" "Yanoman" "\x00"
+	"Tecmo Products" "\x00" "Japan Glary Business" "\x00" "Forum / Op"
+	"enSystem" "\x00" "Virgin Games (Japan)" "\x00" "SMDE" "\x00" "Da"
+	"ikokudenki" "\x00" "Creatures Inc." "\x00" "TDK Deep Impresion" "\x00"
+	"Zoo" "\x00" "Sunsoft / Tokai Engineering" "\x00" "POW (Planning "
+	"Office Wada) / VR1 Japan" "\x00" "Micro World" "\x00" "San-X" "\x00"
+	"Loriciel / Electro Brain" "\x00" "Kemco Japan" "\x00" "Seta" "\x00"
+	"Culture Brain" "\x00" "Palsoft" "\x00" "Visit Co.,Ltd." "\x00" "I"
+	"ntec" "\x00" "System Sacom" "\x00" "Poppo" "\x00" "Ubisoft Japan"
+	"\x00" "Media Works" "\x00" "NEC InterChannel" "\x00" "Tam" "\x00"
+	"Jordan" "\x00" "Smilesoft / Rocket" "\x00" "Mediakite" "\x00" "V"
+	"iacom" "\x00" "Carrozzeria" "\x00" "Dynamic" "\x00" "Magifact" "\x00"
+	"Hect" "\x00" "Codemasters" "\x00" "Taito / GAGA Communications" "\x00"
+	"Laguna" "\x00" "Telstar / Event / Taito" "\x00" "Soedesco" "\x00"
+	"Arcade Zone Ltd" "\x00" "Entertainment International / Empire So"
+	"ftware" "\x00" "Loriciel" "\x00" "Gremlin Graphics" "\x00" "K.Am"
+	"usement Leasing Co." "\x00" "Seika Corp." "\x00" "Ubi Soft Enter"
+	"tainment" "\x00" "Sunsoft US" "\x00" "Life Fitness" "\x00" "Syst"
+	"em 3" "\x00" "Spectrum Holobyte" "\x00" "Irem" "\x00" "Gakken" "\x09"
+	"// FDS" "\x00" "Raya Systems" "\x00" "Renovation Products" "\x00"
+	"Malibu Games" "\x00" "Eidos" "\x00" "Playmates Interactive" "\x00"
+	"Fox Interactive" "\x00" "Time Warner Interactive" "\x00" "Disney"
+	" Interactive" "\x00" "Black Pearl" "\x00" "Advanced Productions" "\x00"
+	"GT Interactive" "\x00" "RARE" "\x00" "Crave Entertainment" "\x00"
+	"Absolute Entertainment" "\x00" "Acclaim" "\x00" "Activision" "\x00"
+	"American Sammy" "\x00" "Take 2 Interactive / GameTek" "\x00" "Hi"
+	" Tech" "\x00" "LJN LTD." "\x00" "Mattel" "\x00" "Mindscape / Red"
+	" Orb Entertainment" "\x00" "Romstar" "\x00" "Taxan" "\x00" "Midw"
+	"ay / Tradewest" "\x00" "American Softworks" "\x00" "Majesco Sale"
+	"s Inc" "\x00" "3DO" "\x00" "Hasbro" "\x00" "NewKidCo" "\x00" "Te"
+	"legames" "\x00" "Metro3D" "\x00" "Vatical Entertainment" "\x00" "L"
+	"EGO Media" "\x00" "Xicat Interactive" "\x00" "Cryo Interactive" "\x00"
+	"Red Storm Entertainment" "\x00" "Microids" "\x00" "Data Design /"
+	" Conspiracy / Swing" "\x00" "Titus" "\x00" "Virgin Interactive" "\x00"
+	"Maxis" "\x00" "LucasArts Entertainment" "\x00" "Ocean" "\x00" "B"
+	"ethesda Softworks" "\x00" "Electronic Arts" "\x00" "Laser Beam" "\x00"
+	"Elite Systems" "\x00" "Electro Brain" "\x00" "The Learning Compa"
+	"ny" "\x00" "BBC" "\x00" "Software 2000" "\x00" "UFO Interactive "
+	"Games" "\x00" "BAM! Entertainment" "\x00" "Studio 3" "\x00" "Cla"
+	"ssified Games" "\x00" "TDK Mediactive" "\x00" "DreamCatcher" "\x00"
+	"JoWood Produtions" "\x00" "Sega" "\x00" "Wannado Edition" "\x00" "L"
+	"SP (Light & Shadow Prod.)" "\x00" "ITE Media" "\x00" "Atari (Inf"
+	"ogrames)" "\x00" "Interplay" "\x00" "JVC (US)" "\x00" "Parker Br"
+	"others" "\x00" "Sales Curve (Storm / SCI)" "\x00" "THQ" "\x00" "A"
+	"ccolade" "\x00" "Triffix Entertainment" "\x00" "Microprose Softw"
+	"are" "\x00" "Sierra / Universal Interactive" "\x00" "Kemco" "\x00"
+	"Rage Software" "\x00" "Encore" "\x00" "Kiddinx" "\x00" "Simon & "
+	"Schuster Interactive" "\x00" "Asmik Ace Entertainment Inc." "\x00"
+	"Empire Interactive" "\x00" "Jester Interactive" "\x00" "Rockstar"
+	" Games" "\x00" "Scholastic" "\x00" "Ignition Entertainment" "\x00"
+	"Summitsoft" "\x00" "Stadlbauer" "\x00" "Misawa" "\x00" "Teichiku"
+	"\x00" "Namco Ltd." "\x00" "LOZC" "\x00" "Tokuma Shoten Intermedi"
+	"a" "\x00" "Tsukuda Original" "\x00" "DATAM-Polystar" "\x00" "Bul"
+	"letProof Software (BPS)" "\x00" "Vic Tokai Inc." "\x00" "Charact"
+	"er Soft" "\x00" "I'Max" "\x00" "Saurus" "\x00" "General Entertai"
+	"nment" "\x00" "Cyberfront Korea" "\x00" "Success" "\x00" "Sega J"
+	"apan" "\x00" "Takara Amusement" "\x00" "Chun Soft" "\x00" "Video"
+	" System /  Mc O' River" "\x00" "BEC" "\x00" "Varie" "\x00" "Yone"
+	"zawa / S'pal" "\x00" "Kaneko" "\x00" "Marvelous Entertainment" "\x00"
+	"Nichibutsu / Nihon Bussan" "\x00" "Tecmo" "\x00" "Imagineer" "\x00"
+	"Nova" "\x00" "Take2 / Den'Z / Global Star" "\x00" "Bottom Up" "\x00"
+	"TGL (Technical Group Laboratory)" "\x00" "Hasbro Japan" "\x00" "K"
+	"eynet Inc." "\x00" "Hands-On Entertainment" "\x00" "Telenet" "\x00"
+	"Hori" "\x00" "Scorpion Soft" "\x09" "// FDS" "\x00" "Konami" "\x00"
+	"Kawada Co., Ltd." "\x00" "Takara" "\x00" "Royal Industries" "\x09"
+	"// FDS" "\x00" "Technos Japan Corp." "\x00" "JVC / Victor" "\x00"
+	"Toei Animation" "\x00" "Toho" "\x00" "Namco" "\x00" "Media Rings"
+	" Corporation" "\x00" "J-Wing" "\x00" "Pioneer LDC" "\x00" "KID" "\x00"
+	"Mediafactory" "\x00" "Infogrames / Hudson" "\x00" "Kiratto. Ludi"
+	"c Inc" "\x00" "Yacht Club Games" "\x00" "Acclaim Japan" "\x00" "A"
+	"SCII Corporation" "\x00" "Bandai" "\x00" "Soft Pro Inc." "\x09" "/"
+	"/ FDS" "\x00" "HAL Laboratory" "\x00" "SNK" "\x00" "Pony Canyon" "\x00"
+	"Sunsoft" "\x00" "Toshiba EMI" "\x00" "Sony Imagesoft" "\x00" "Sa"
+	"mmy" "\x00" "Magical" "\x00" "Visco" "\x00" "Compile" "\x00" "MT"
+	"O Inc." "\x00" "Sunrise Interactive" "\x00" "Global A Entertainm"
+	"ent" "\x00" "Fuuki" "\x00" "Taito" "\x00" "Sunsoft / Ask Co., Lt"
+	"d." "\x09" "// FDS" "\x00" "Square" "\x00" "Tokuma Shoten" "\x00"
+	"Data East" "\x00" "Tonkin House / Tokyo Shoseki" "\x00" "East Cu"
+	"be" "\x09" "// FDS" "\x00" "Koei" "\x00" "Konami / Ultra / Palco"
+	"m" "\x00" "NTVIC / VAP" "\x00" "Use Co., Ltd." "\x00" "Meldac" "\x00"
+	"Pony Canyon / FCI" "\x00" "Angel / Sotsu Agency / Sunrise" "\x00"
+	"Yumedia / Aroma Co., Ltd" "\x00" "Boss" "\x00" "Axela / Crea-Tec"
+	"h" "\x00" "Sekaibunka-Sha / Sumire Kobo / Marigul Management Inc"
+	"." "\x00" "Konami Computer Entertainment Osaka" "\x00" "NEC Inte"
+	"rchannel" "\x00" "Enterbrain" "\x00" "From Software" "\x00" "Tai"
+	"to / Disco" "\x00" "Sofel" "\x00" "Quest / Bothtec" "\x00" "Sigm"
+	"a" "\x00" "Ask Kodansha" "\x00" "Naxat" "\x00" "Copya System" "\x00"
+	"Capcom Co., Ltd." "\x00" "Banpresto" "\x00" "Tomy" "\x00" "LJN J"
+	"apan" "\x00" "NCS" "\x00" "Human Entertainment" "\x00" "Altron" "\x00"
+	"Gaps Inc." "\x00" "Elf" "\x00" "Compile Heart" "\x00" "FarSight "
+	"Studios" "\x00" "Yutaka" "\x00" "T&ESoft" "\x00" "Epoch" "\x00" "A"
+	"thena" "\x00" "Asmik" "\x00" "Natsume" "\x00" "King Records" "\x00"
+	"Atlus" "\x00" "Epic / Sony Records" "\x00" "IGS (Information Glo"
+	"bal Service)" "\x00" "Chatnoir" "\x00" "Right Stuff" "\x00" "Spi"
+	"ke" "\x00" "Konami Computer Entertainment Tokyo" "\x00" "Alphadr"
+	"eam Corporation" "\x00" "Sting" "\x00" "Star-Fish" "\x00" "A Wav"
+	"e" "\x00" "Motown Software" "\x00" "Left Field Entertainment" "\x00"
+	"Extreme Ent. Grp." "\x00" "TecMagik" "\x00" "Cybersoft" "\x00" "P"
+	"sygnosis" "\x00" "Davidson / Western Tech." "\x00" "The Game Fac"
+	"tory" "\x00" "Hip Games" "\x00" "Aspyr" "\x00" "Mastiff" "\x00" "i"
+	"Que" "\x00" "Digital Tainment Pool" "\x00" "XS Games / Jack Of A"
+	"ll Games" "\x00" "Daewon Media" "\x00" "Alpha Unit" "\x00" "PCCW"
+	" Japan" "\x00" "Yuke's Media Creations" "\x00" "KiKi Co Ltd" "\x00"
+	"Open Sesame Inc" "\x00" "Sims" "\x00" "Broccoli" "\x00" "Avex" "\x00"
+	"D3 Publisher" "\x00" "Konami Computer Entertainment Japan" "\x00"
+	"Square-Enix" "\x00" "KSG" "\x00" "Micott & Basara Inc." "\x00" "O"
+	"3 Entertainment" "\x00" "Orbital Media" "\x00" "Detn8 Games" "\x00"
+	"Gameloft / Ubi Soft" "\x00" "Gamecock Media Group" "\x00" "Oxyge"
+	"n Games" "\x00" "505 Games" "\x00" "Treasure" "\x00" "Aruze" "\x00"
+	"Ertain" "\x00" "SNK Playmore" "\x00" "Level-5" "\x00" "Genius Pr"
+	"oducts" "\x00" "Reef Entertainment" "\x00" "Nordcurrent" "\x00" "Y"
+	"ojigen" "\x00" "AQ Interactive" "\x00" "Arc System Works" "\x00" "D"
+	"eep Silver" "\x00" "Atari" "\x00" "Nihon System" "\x00" "NIS Ame"
+	"rica" "\x00" "Purple Hills" "\x00" "Trend Verlag / East Entertai"
+	"nment" "\x00" "Legacy Interactive" "\x00" "SilverStar Games" "\x00"
+	"Mumbo Jumbo" "\x00" "Mindscape" "\x00" "Milestone / UFO Interact"
+	"ive" "\x00" "Blast !" "\x00" "Terabox" "\x00" "Nordic Games" "\x00"
+	"Neko Entertainment / Diffusion / Naps team" "\x00" "Nobilis" "\x00"
+	"Namco Bandai" "\x00" "Data Design / Destineer Studios" "\x00" "P"
+	"hoenix Games" "\x00" "Playlogic" "\x00" "Rondomedia" "\x00" "War"
+	"ner Bros. Interactive Entertainment Inc." "\x00" "RTL Games" "\x00"
+	"RealNetworks" "\x00" "Southpeak Interactive" "\x00" "Blade Inter"
+	"active Studios" "\x00" "SevenGames" "\x00" "Storm City" "\x00" "T"
+	"asuke / Works" "\x00" "Tivola" "\x00" "Metro 3D / Data Design" "\x00"
+	"Valcon Games" "\x00" "Virgin Play" "\x00" "Little Orbit" "\x00" "X"
+	"seed Games" "\x00" "Aksys Games" "\x00" "Fun Box Media" "\x00" "O"
+	"2 Games" "\x00" "Bergsala Lightweight" "\x00" "Barunson Creative"
+	"\x00" "Ntreev Soft" "\x00" "WBA Interactive" "\x00" "Internal En"
+	"gine" "\x00" "Zinkia" "\x00" "Judo Baby" "\x00" "Topware Interac"
+	"tive" "\x00";
+
+static const uint16_t NintendoPublishers_offtbl[] = {
+	/* Nintendo Third-Party Publisher '00' */
+	1,14,23,48,63,75,82,89,
+	94,101,111,118,133,160,169,177,
+	192,206,0,215,0,239,0,252,
+	0,259,272,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher '0W' */
+	0,0,0,0,0,0,285,293,
+	0,315,326,340,345,357,364,0,
+	372,387,408,427,448,0,0,453,
+	0,0,0,0,0,466,481,0,
+
+	/* Nintendo Third-Party Publisher '1S' */
+	0,0,0,0,0,0,0,0,
+	500,504,532,571,0,583,89,589,
+	614,626,631,0,645,653,668,674,
+	687,693,0,707,719,736,740,747,
+
+	/* Nintendo Third-Party Publisher '2O' */
+	0,0,766,0,0,0,0,0,
+	0,0,0,0,776,783,795,0,
+	803,812,817,829,857,864,888,897,
+	913,959,968,985,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher '3K' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	1009,1021,1044,0,1055,0,1068,1077,
+	0,1095,1100,1114,1127,1147,0,1160,
+
+	/* Nintendo Third-Party Publisher '4G' */
+	1166,0,0,1188,1204,0,0,0,
+	0,0,1228,0,1247,0,1259,0,
+	0,1280,1295,1300,1320,1343,1351,1362,
+	1377,1406,1414,0,1423,0,1430,1464,
+
+	/* Nintendo Third-Party Publisher '5C' */
+	1472,1478,0,1497,1516,1534,0,0,
+	1538,1545,1554,1564,0,1572,1594,0,
+	1605,1623,0,0,1640,1664,0,1673,
+	1706,1712,1731,0,1737,0,0,1761,
+
+	/* Nintendo Third-Party Publisher '68' */
+	1767,1786,0,1802,0,0,1813,1827,
+	1841,1862,0,1866,1880,1902,1921,0,
+	0,0,1930,0,1947,0,1962,1975,
+	1993,1998,2014,2041,2051,2070,2080,2089,
+
+	/* Nintendo Third-Party Publisher '74' */
+	0,2105,0,0,2131,2135,2144,0,
+	2166,2186,0,2217,2223,2237,0,500,
+	2244,2252,2281,2310,0,0,2329,0,
+	2348,2363,2374,2397,2408,0,0,0,
+
+	/* Nintendo Third-Party Publisher '80' */
+	2419,2426,2435,2446,340,0,2451,2476,
+	2493,0,0,2508,2535,0,2550,2565,
+	2571,0,0,2578,0,0,2600,2617,
+	0,2625,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher '8W' */
+	0,0,0,0,2636,2653,2663,2691,
+	0,2695,2701,2718,0,2725,2749,2775,
+	2781,0,0,2791,2796,2824,0,2834,
+	0,2867,0,2725,0,2880,2892,0,
+
+	/* Nintendo Third-Party Publisher '9S' */
+	0,0,0,0,0,0,0,0,
+	2915,2923,2928,0,2949,985,2956,2973,
+	2980,3004,3024,0,3037,3052,0,3057,
+	3063,3087,0,3094,3106,3110,0,0,
+
+	/* Nintendo Third-Party Publisher 'AO' */
+	0,3123,3143,0,0,0,0,0,
+	0,0,3162,0,3179,3193,3211,3218,
+	89,0,3239,3254,0,3258,631,3270,
+	3278,3290,0,3305,3311,3319,0,3325,
+
+	/* Nintendo Third-Party Publisher 'BK' */
+	0,3333,0,3342,0,3362,3385,0,
+	0,0,0,0,0,0,0,0,
+	3391,3397,2217,3428,3435,3449,3459,3488,
+	3505,0,3510,3534,3546,3560,3567,3585,
+
+	/* Nintendo Third-Party Publisher 'CG' */
+	3616,0,0,3641,3646,3664,3719,3755,
+	0,3772,3783,0,0,0,0,0,
+	0,0,0,0,3797,3811,3817,3833,
+	3839,0,3852,3858,3871,3888,3898,3903,
+
+	/* Nintendo Third-Party Publisher 'DC' */
+	0,3913,3917,3937,111,3944,0,0,
+	0,0,0,3954,0,0,3958,0,
+	0,0,0,3972,0,0,0,0,
+	111,0,3989,2695,3996,4004,0,4010,
+
+	/* Nintendo Third-Party Publisher 'E8' */
+	4017,4023,4031,4044,4050,0,4070,0,
+	4103,4112,0,0,0,4124,4130,4166,
+	0,4189,0,0,4195,0,0,0,
+	0,0,0,0,4205,4212,4228,4253,
+
+	/* Nintendo Third-Party Publisher 'F4' */
+	4271,0,0,0,0,4280,0,4290,
+	0,0,4300,0,0,0,0,0,
+	4325,4342,4352,0,0,4358,4366,4371,
+	4393,4422,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'G0' */
+	4435,4446,4457,0,4480,4492,4508,4513,
+	4522,4527,0,4540,0,4576,4588,4592,
+	4613,4630,0,4644,0,4656,4676,4697,
+	0,0,0,0,0,4710,0,0,
+
+	/* Nintendo Third-Party Publisher 'GW' */
+	0,0,4325,0,0,4720,4729,4735,
+	4742,0,0,0,0,0,0,0,
+	0,0,0,4755,0,0,0,4763,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'HS' */
+	0,0,0,0,0,0,4779,4798,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,4810,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'IO' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,4818,0,0,
+	0,0,0,4833,0,0,0,4850,
+
+	/* Nintendo Third-Party Publisher 'JK' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,4862,0,0,0,
+	0,0,0,0,0,0,4868,0,
+	0,0,0,4881,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'KG' */
+	0,0,0,0,0,0,4850,0,
+	0,4893,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'LC' */
+	0,0,0,0,0,4906,0,0,
+	0,0,0,0,0,0,0,0,
+	0,4940,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'M8' */
+	0,0,0,0,0,0,4959,0,
+	0,0,0,4976,0,0,0,0,
+	0,0,0,4988,4998,5026,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'N4' */
+	0,0,0,0,0,5034,0,0,
+	0,0,0,0,5042,0,0,0,
+	5055,0,0,0,0,5098,5106,5119,
+	4881,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'O0' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'OW' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,5151,0,0,0,
+	0,5165,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'PS' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'QO' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'RK' */
+	0,0,5175,0,0,0,0,0,
+	5186,5230,0,0,5240,0,0,0,
+	0,0,0,0,0,5253,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'SG' */
+	0,0,0,0,0,0,0,0,
+	0,5275,0,0,0,0,0,5301,
+	0,0,0,5312,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'TC' */
+	0,0,0,0,0,0,0,0,
+	5323,0,0,0,0,0,0,0,
+	0,0,0,5338,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'U8' */
+	0,0,0,0,0,0,0,0,
+	5345,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'V4' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,5368,0,5381,0,0,
+	0,0,0,0,0,0,0,5393,
+
+	/* Nintendo Third-Party Publisher 'W0' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,5186,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'WW' */
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,5406,
+	0,0,0,0,0,0,0,0,
+
+	/* Nintendo Third-Party Publisher 'XS' */
+	5418,5430,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,5444,
+	0,0,0,0,0,0,5453,0,
+
+	/* Nintendo Third-Party Publisher 'YO' */
+	0,0,0,0,0,5368,0,0,
+	0,0,0,0,0,5474,0,0,
+	5492,0,0,0,0,0,5504,0,
+	0,0,0,0,0,5520,0,0,
+
+	/* Nintendo Third-Party Publisher 'ZK' */
+	0,0,0,0,0,0,0,0,
+	5536,0,0,0,5543,5553,
+};
