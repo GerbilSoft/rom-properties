GerbilSoft/rom-properties

commit dd40b00c08c4466b26dda6a3b72b7233f960acb6

[win32] Build libromdata as a DLL.
librpthreads: Was missing SetMSVCDebugPath and EXCLUDE_FROM_ALL.
libunixcommon: Was missing "-fpic -fPIC".

[librpbase] ConfReader: Make it public:
Config.hpp(20,1): warning C4275: non dll-interface class
'LibRpBase::ConfReader' used as base for dll-interface class
'LibRpBase::Config'

================

RomData::supportedImageTypes_static(): Need to mark RP_LIBROMDATA_PUBLIC
in RomData_decl.hpp, not just the definition in each .cpp file. This
worked on Linux, but on Windows, it causes an error:

Amiibo.cpp(295,18): error C2375: 'LibRomData::Amiibo::supportedImageTypes_static': redefinition; different linkage

Note that this will result in more exported symbols than is strictly
necessary, but that shouldn't be too much of an issue.

================

Needed to change several libraries from STATIC to OBJECT in order to
ensure that files aren't truncated from libromdata.

TODO: Is it possible to remove "OBJECT"? It makes a mess in the compiler
output, since it links to each .obj individually instead of the .lib
files...

================

Need to define RP_BUILDING_FOR_DLL=1 for anything that links to libromdata
in order to ensure it has the dllimport definitions set up correctly.
This only seems to affect *some* symbols, not all of them... (notably
AboutTabText and ComBase data symbols)

[libwin32common] Moved IIDs from win_iid.c into IListView.hpp to avoid
having to export these symbols from libromdata.

ZLIB_LIBRARY -> ZLIB_LIBRARIES

Removed ZLIB_LIBRARIES from the KDE4 and KF5 UI frontends, since they
don't use zlib directly.

================

Link all tests to libromdata instead of individual static libraries.
This reduces the size of the compiled test executables, at the expense
of requiring libromdata to be built before the tests can be built.
This is required because librptest is linked to libromdata in order to
access librptexture to set the rp_image backend.

src/libwin32common/RegKey.hpp
@@ -2,12 +2,14 @@
  * ROM Properties Page shell extension. (libwin32common)                   *
  * RegKey.hpp: Registry key wrapper.                                       *
  *                                                                         *
- * Copyright (c) 2016-2019 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
 #ifndef __ROMPROPERTIES_LIBWIN32COMMON_REGKEY_HPP__
 #define __ROMPROPERTIES_LIBWIN32COMMON_REGKEY_HPP__
+
+#include "common.h"
 
 // C++ includes.
 #include <string>
@@ -18,7 +20,7 @@
 
 namespace LibWin32Common {
 
-class RegKey
+class RP_LIBROMDATA_PUBLIC RegKey
 {
 	public:
 		/**

cmake/macros/SetMSVCDebugPath.cmake
@@ -37,18 +37,24 @@ IF(MSVC)
 	SET(OUTPUT_NAME_EXPR "${OUTPUT_NAME_EXPR_1}${OUTPUT_NAME_EXPR_2}")
 	SET(OUTPUT_NAME_FULL "${PREFIX_EXPR_FULL}${OUTPUT_NAME_EXPR}$<TARGET_PROPERTY:${_target},POSTFIX>")
 
-	SET(SPLITDEBUG_SOURCE "$<TARGET_FILE:${_target}>")
-	SET(SPLITDEBUG_TARGET "$<TARGET_FILE_DIR:${_target}>/${OUTPUT_NAME_FULL}.debug")
-
 	SET(OUTPUT_NAME_EXPR_1 "$<TARGET_PROPERTY:${_target},PREFIX>$<TARGET_PROPERTY:${_target},OUTPUT_NAME>$<TARGET_PROPERTY:${_target},POSTFIX>")
 	SET(OUTPUT_NAME_EXPR_2 "$<$<STREQUAL:$<TARGET_PROPERTY:${_target},OUTPUT_NAME>,>:$<TARGET_PROPERTY:${_target},PREFIX>${_target}$<TARGET_PROPERTY:${_target},POSTFIX>>")
 
+	# FIXME: "-dll" suffix seems to be missing here.
+	# If it's present in the target name, add it here.
+	IF(${_target} MATCHES "-dll$")
+		SET(_target_suffix "-dll")
+	ELSE()
+		UNSET(_target_suffix)
+	ENDIF()
+
 	# Set the target PDB filename.
 	SET_TARGET_PROPERTIES(${_target}
-		PROPERTIES PDB "$<TARGET_FILE_DIR:${_target}>/${OUTPUT_NAME_EXPR_1}${OUTPUT_NAME_EXPR_2}.pdb"
+		PROPERTIES PDB "$<TARGET_FILE_DIR:${_target}>/${OUTPUT_NAME_EXPR_1}${OUTPUT_NAME_EXPR_2}${_target_suffix}.pdb"
 		)
 
 	UNSET(OUTPUT_NAME_EXPR_1)
 	UNSET(OUTPUT_NAME_EXPR_2)
+	UNSET(_target_suffix)
 ENDIF(MSVC)
 ENDMACRO(SET_MSVC_DEBUG_PATH)

src/CMakeLists.txt
@@ -75,11 +75,11 @@ CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/config.version.h.in" "${CMAKE_CURREN
 # Write stdbool.h.
 CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/stdboolx.h.in" "${CMAKE_CURRENT_BINARY_DIR}/stdboolx.h")
 
-# Build certain libraries as shared libraries on Linux.
-IF(UNIX AND NOT APPLE)
+# Build certain libraries as shared libraries on Linux and Windows.
+IF(WIN32 OR (UNIX AND NOT APPLE))
 	SET(RP_LIBROMDATA_IS_DLL 1)
 	ADD_DEFINITIONS(-DRP_LIBROMDATA_IS_DLL=1)
-ENDIF(UNIX AND NOT APPLE)
+ENDIF(WIN32 OR (UNIX AND NOT APPLE))
 
 # Source Code subdirectories.
 ADD_SUBDIRECTORY(librpsecure)

src/common.h
@@ -233,8 +233,8 @@
 
 // Visibility macros for libromdata.
 // Reference: https://gcc.gnu.org/wiki/Visibility
-#ifdef RP_LIBROMDATA_IS_DLL
-#  if defined(_WIN32) || defined (__CYGWIN__)
+#if defined(RP_LIBROMDATA_IS_DLL)
+#  if (defined(_WIN32) || defined (__CYGWIN__)) && defined(RP_BUILDING_FOR_DLL)
 #    ifdef RP_BUILDING_LIBROMDATA
 #      ifdef __GNUC__
 #        define RP_LIBROMDATA_PUBLIC __attribute__((dllexport))
@@ -258,7 +258,7 @@
 #      define RP_LIBROMDATA_LOCAL
 #    endif
 #  endif
-#else /* !RP_LIBROMDATA_IS_DLL */
+#else /* !defined(RP_LIBROMDATA_IS_DLL) */
 #  define RP_LIBROMDATA_PUBLIC
 #  define RP_LIBROMDATA_LOCAL
 #endif

src/kde/kde4/CMakeLists.txt
@@ -107,7 +107,6 @@ IF(BUILD_KDE4)
 			$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/../..>	# src
 		)
 	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE romdata)
-	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE ${ZLIB_LIBRARY})
 	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PUBLIC ${KDE4_KFILE_LIBRARY} ${KDE4_KDEUI_LIBRARY})
 	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PUBLIC Qt4::QtGui Qt4::QtCore)
 	TARGET_INCLUDE_DIRECTORIES(rom-properties-kde4 PUBLIC ${KDE4_INCLUDE_DIR})

src/kde/kf5/CMakeLists.txt
@@ -177,7 +177,6 @@ IF(BUILD_KF5)
 			$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/../..>	# src
 		)
 	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE romdata)
-	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE ${ZLIB_LIBRARY})
 	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PUBLIC KF5::KIOCore KF5::KIOWidgets KF5::WidgetsAddons KF5::FileMetaData)
 	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PUBLIC Qt5::Widgets Qt5::Gui Qt5::Core)
 	IF(HAVE_QtDBus_NOTIFY)

src/libcachecommon/CMakeLists.txt
@@ -25,26 +25,57 @@ CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/config.lib${PROJECT_NAME}.h.in" "${C
 # Build the library. #
 ######################
 
-INCLUDE(SetMSVCDebugPath)
+# NOTE: ${PROJECT_NAME} is the original target.
+# ${PROJECT_NAME}-dll is the target with dllimport/dllexport for libromdata.
+IF(RP_LIBROMDATA_IS_DLL AND WIN32)
+	# On Win32, the dll target needs dllimport/dllexport.
+	SET(TARGETS ${PROJECT_NAME} ${PROJECT_NAME}-dll)
+ELSE()
+	# On other platforms, the dll target will be an alias to the regular target.
+	# That will be set up later, since the regular target doesn't exist yet.
+	SET(TARGETS ${PROJECT_NAME})
+ENDIF()
 
-ADD_LIBRARY(${PROJECT_NAME} STATIC
-	${${PROJECT_NAME}_SRCS} ${${PROJECT_NAME}_H}
-	)
-SET_MSVC_DEBUG_PATH(${PROJECT_NAME})
-TARGET_INCLUDE_DIRECTORIES(${PROJECT_NAME}
-	PUBLIC  $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
-		$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
-	PRIVATE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/..>
-		$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/..>
-	)
-# Exclude from ALL builds.
-SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES EXCLUDE_FROM_ALL TRUE)
-TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE rpthreads)
-IF(WIN32)
-	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE win32common)
-ELSE(WIN32)
-	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE unixcommon)
-ENDIF(WIN32)
+FOREACH(_target ${TARGETS})
+	ADD_LIBRARY(${_target} STATIC
+		${${PROJECT_NAME}_SRCS} ${${PROJECT_NAME}_H}
+		)
+	INCLUDE(SetMSVCDebugPath)
+	SET_MSVC_DEBUG_PATH(${_target})
+	# Exclude from ALL builds.
+	SET_TARGET_PROPERTIES(${_target} PROPERTIES EXCLUDE_FROM_ALL TRUE)
+
+	# DLL targets need the appropriate macros.
+	TARGET_COMPILE_DEFINITIONS(${_target} PRIVATE RP_BUILDING_LIBROMDATA=1)
+	IF(${_target} MATCHES "-dll$")
+		SET(_target_suffix "-dll")
+		TARGET_COMPILE_DEFINITIONS(${_target}
+			PUBLIC		RP_BUILDING_FOR_DLL=1
+			PRIVATE		RP_BUILDING_LIBROMDATA=1
+			)
+	ELSE()
+		UNSET(_target_suffix)
+	ENDIF()
+
+	TARGET_INCLUDE_DIRECTORIES(${_target}
+		PUBLIC  $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+			$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
+		PRIVATE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/..>
+			$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/..>
+		)
+	TARGET_LINK_LIBRARIES(${_target} PRIVATE rpthreads${_target_suffix})
+	IF(WIN32)
+		TARGET_LINK_LIBRARIES(${_target} PRIVATE ${_target_suffix})
+	ELSE(WIN32)
+		TARGET_LINK_LIBRARIES(${_target} PRIVATE ${_target_suffix})
+	ENDIF(WIN32)
+ENDFOREACH(_target)
+UNSET(_target_suffix)
+
+IF(NOT TARGET ${PROJECT_NAME}-dll)
+	# On other platforms, the dll target will be an alias to the regular target.
+	ADD_LIBRARY(${PROJECT_NAME}-dll ALIAS ${PROJECT_NAME})
+ENDIF()
 
 # Unix: Add -fpic/-fPIC in order to use these static libraries in plugins.
 IF(UNIX AND NOT APPLE)

src/libcachecommon/CacheKeys.hpp
@@ -2,12 +2,14 @@
  * ROM Properties Page shell extension. (libcachecommon)                   *
  * CacheKeys.cpp: Cache key handling functions.                            *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
 #ifndef __ROMPROPERTIES_LIBCACHECOMMON_CACHEKEYS_HPP__
 #define __ROMPROPERTIES_LIBCACHECOMMON_CACHEKEYS_HPP__
+
+#include "common.h"
 
 // C++ includes.
 #include <string>
@@ -20,6 +22,7 @@ namespace LibCacheCommon {
  * @param cacheKey Cache key. (Must be UTF-8, NULL-terminated.)
  * @return 0 on success; negative POSIX error code on error.
  */
+RP_LIBROMDATA_PUBLIC
 int filterCacheKey(char *pCacheKey);
 
 /**

commit 7300af1dc58b3cfe8701977b64af0d3a41b6b8bc

[libwin32common] RegKey::RegisterComObject(): Add HINSTANCE parameter.
Since RegKey is now located in romdata-1.dll, any attempts to use
RegKey::RegisterComObject() would result in the class being registered
with romdata-1.dll's filename, not the caller's filename.

Add an HINSTANCE parameter that will be used to get the correct filename.

This explains why it worked occasionally before. When it wasn't working,
I'd unregister it, rebuild without romdata-1.dll, and register it, and
libwin32common (in rom-properties.dll) would write "rom-properties.dll"
to the registry. Then I'd rebuild with romdata-1.dll, restart Explorer,
and it'd still be working because I didn't re-register the DLL.

I found this by running a simple CoCreateInstance() test case in wine:

$ wine CoCreateTest.exe
CoInitializeEx() -> 00000000
CLSIDFromString() -> 00000000
013c:err:ole:apartment_add_dll couldn't find function DllGetClassObject in L"romdata-1.dll"
013c:err:ole:create_server class {4723df58-463e-4590-8f4a-8d9dd4f4355a} not registered
013c:err:ole:com_get_class_object no class object {4723df58-463e-4590-8f4a-8d9dd4f4355a} could be created for context 0x5
CoCreateInstance() -> 80040154

It should have been looking in rom-properties.dll, not romdata-1.dll.
COM error 0x80040154 is CO_E_ERRORINDLL.

It sure would have been nice if MSVC gave me this information...

This fixes a regression from commit dd40b00c08c4466b26dda6a3b72b7233f960acb6.
([win32] Build libromdata as a DLL.)

src/libwin32common/RegKey.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libwin32common)                   *
  * RegKey.hpp: Registry key wrapper.                                       *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -619,13 +619,15 @@ LONG RegKey::RegisterFileType(LPCTSTR fileType, RegKey **pHkey_Assoc)
 }
 
 /**
- * Register a COM object in this DLL.
- * @param rclsid CLSID.
- * @param progID ProgID.
- * @param description Description of the COM object.
+ * Register a COM object in a DLL.
+ * @param hInstance DLL to be used for registration
+ * @param rclsid CLSID
+ * @param progID ProgID
+ * @param description Description of the COM object
  * @return ERROR_SUCCESS on success; WinAPI error on error.
  */
-LONG RegKey::RegisterComObject(REFCLSID rclsid, LPCTSTR progID, LPCTSTR description)
+LONG RegKey::RegisterComObject(HINSTANCE hInstance, REFCLSID rclsid,
+	LPCTSTR progID, LPCTSTR description)
 {
 	TCHAR szClsid[40];
 	LONG lResult = StringFromGUID2(rclsid, szClsid, _countof(szClsid));
@@ -660,13 +662,14 @@ LONG RegKey::RegisterComObject(REFCLSID rclsid, LPCTSTR progID, LPCTSTR descript
 
 	// Create an InprocServer32 subkey.
 	RegKey hkcr_InprocServer32(hkcr_Obj_CLSID, _T("InprocServer32"), KEY_WRITE, true);
-	if (!hkcr_InprocServer32.isOpen())
+	if (!hkcr_InprocServer32.isOpen()) {
 		return hkcr_InprocServer32.lOpenRes();
-	// Set the default value to the DLL filename.
-	// TODO: Get this once and save it?
+	}
+
+	// Set the default value to the filename of the specified DLL.
 	// TODO: Duplicated from win32/. Consolidate the two?
 	TCHAR dll_filename[MAX_PATH];
-	DWORD dwResult = GetModuleFileName(HINST_THISCOMPONENT, dll_filename, _countof(dll_filename));
+	DWORD dwResult = GetModuleFileName(hInstance, dll_filename, _countof(dll_filename));
 	if (dwResult == 0 || GetLastError() != ERROR_SUCCESS) {
 		// Cannot get the DLL filename.
 		// TODO: Windows XP doesn't SetLastError() if the
@@ -698,8 +701,8 @@ LONG RegKey::RegisterComObject(REFCLSID rclsid, LPCTSTR progID, LPCTSTR descript
 
 /**
  * Register a shell extension as an approved extension.
- * @param rclsid CLSID.
- * @param description Description of the shell extension.
+ * @param rclsid CLSID
+ * @param description Description of the shell extension
  * @return ERROR_SUCCESS on success; WinAPI error on error.
  */
 LONG RegKey::RegisterApprovedExtension(REFCLSID rclsid, LPCTSTR description)
@@ -724,8 +727,8 @@ LONG RegKey::RegisterApprovedExtension(REFCLSID rclsid, LPCTSTR description)
 
 /**
  * Unregister a COM object in this DLL.
- * @param rclsid CLSID.
- * @param progID ProgID.
+ * @param rclsid CLSID
+ * @param progID ProgID
  * @return ERROR_SUCCESS on success; WinAPI error on error.
  */
 LONG RegKey::UnregisterComObject(REFCLSID rclsid, LPCTSTR progID)

src/libwin32common/RegKey.hpp
@@ -196,26 +196,28 @@ class RP_LIBROMDATA_PUBLIC RegKey
 		static LONG RegisterFileType(LPCTSTR fileType, RegKey **pHkey_Assoc);
 
 		/**
-		 * Register a COM object in this DLL.
-		 * @param rclsid CLSID.
-		 * @param progID ProgID.
-		 * @param description Description of the COM object.
+		 * Register a COM object in a DLL.
+		 * @param hInstance DLL to be used for registration
+		 * @param rclsid CLSID
+		 * @param progID ProgID
+		 * @param description Description of the COM object
 		 * @return ERROR_SUCCESS on success; WinAPI error on error.
 		 */
-		static LONG RegisterComObject(REFCLSID rclsid, LPCTSTR progID, LPCTSTR description);
+		static LONG RegisterComObject(HINSTANCE hInstance, REFCLSID rclsid,
+			LPCTSTR progID, LPCTSTR description);
 
 		/**
 		 * Register a shell extension as an approved extension.
-		 * @param rclsid CLSID.
-		 * @param description Description of the shell extension.
+		 * @param rclsid CLSID
+		 * @param description Description of the shell extension
 		 * @return ERROR_SUCCESS on success; WinAPI error on error.
 		 */
 		static LONG RegisterApprovedExtension(REFCLSID rclsid, LPCTSTR description);
 
 		/**
-		 * Unregister a COM object in this DLL.
-		 * @param rclsid CLSID.
-		 * @param progID ProgID.
+		 * Unregister a COM object.
+		 * @param rclsid CLSID
+		 * @param progID ProgID
 		 * @return ERROR_SUCCESS on success; WinAPI error on error.
 		 */
 		static LONG UnregisterComObject(REFCLSID rclsid, LPCTSTR progID);

src/win32/RP_ShellIconOverlayIdentifier_Register.cpp
@@ -3,7 +3,7 @@
  * RP_ShellIconOverlayIdentifier_Register.cpp: IShellIconOverlayIdentifier *
  * COM registration functions.                                             *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -29,7 +29,8 @@ LONG RP_ShellIconOverlayIdentifier::RegisterCLSID(void)
 	static const TCHAR description[] = _T("ROM Properties Page - Shell Icon Overlay Identifier");
 
 	// Register the COM object.
-	LONG lResult = RegKey::RegisterComObject(__uuidof(RP_ShellIconOverlayIdentifier), RP_ProgID, description);
+	LONG lResult = RegKey::RegisterComObject(HINST_THISCOMPONENT,
+		__uuidof(RP_ShellIconOverlayIdentifier), RP_ProgID, description);
 	if (lResult != ERROR_SUCCESS) {
 		return lResult;
 	}

src/win32/clsid_common.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * clsid_common.hpp: CLSID common macros.                                  *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -51,7 +51,8 @@ LONG klass::RegisterCLSID(void) \
 	extern const TCHAR RP_ProgID[]; \
 	\
 	/* Register the COM object. */ \
-	LONG lResult = RegKey::RegisterComObject(__uuidof(klass), RP_ProgID, description); \
+	LONG lResult = RegKey::RegisterComObject(HINST_THISCOMPONENT, \
+		__uuidof(klass), RP_ProgID, description); \
 	if (lResult != ERROR_SUCCESS) { \
 		return lResult; \
 	} \