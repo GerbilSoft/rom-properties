GerbilSoft/rom-properties

commit 5ff0abbc7b15c9e84891ab6c7e30cf0b0ecf14c0

[win32] KeyManagerTab: Adjust the ListView positioning and size for the MessageWidget.
MessageWidget: Handle multi-line messages better. Note that we're not
vertically centering it anymore, since DrawText() doesn't support
DT_VCENTER for multi-line messages, but it's better than before.

KeyManagerTabPrivate::showKeyImportReturnStatus(): Remove an unneeded
call to MapDialogRect().

src/win32/MessageWidget.cpp
@@ -227,15 +227,19 @@ void MessageWidgetPrivate::paint(void)
 		}
 
 		// Message
+		RECT textRect = {
+			rect.left, rect.top + (szIcon.cy / 4),
+			rect.right, rect.bottom - ((szIcon.cy / 4) * 2)
+		};
 		TCHAR tbuf[128];
 		int len = GetWindowTextLength(hWnd);
 		if (len < 128) {
 			GetWindowText(hWnd, tbuf, _countof(tbuf));
-			DrawText(hDC, tbuf, len, &rect, DT_SINGLELINE);
+			DrawText(hDC, tbuf, len, &textRect, 0);
 		} else {
 			TCHAR *const tmbuf = static_cast<TCHAR*>(malloc((len+1) * sizeof(TCHAR)));
 			GetWindowText(hWnd, tmbuf, len+1);
-			DrawText(hDC, tmbuf, len, &rect, DT_SINGLELINE);
+			DrawText(hDC, tmbuf, len, &textRect, 0);
 			free(tmbuf);
 		}
 	} else {

src/win32/config/KeyManagerTab.cpp
@@ -133,6 +133,8 @@ class KeyManagerTabPrivate
 
 		// MessageWidget for ROM operation notifications.
 		HWND hMessageWidget;
+		POINT ptListView;	// Original ListView position
+		SIZE szListView;	// Original ListView size
 
 		// EDIT box for ListView.
 		HWND hEditBox;
@@ -452,6 +454,17 @@ void KeyManagerTabPrivate::initUI(void)
 	// Auto-size the "Valid?" column.
 	ListView_SetColumnWidth(hListView, 2, LVSCW_AUTOSIZE_USEHEADER);
 
+	// Get the ListView's initial position and size.
+	// This will be needed to adjust the ListView when
+	// displaying the MessageWidget.
+	RECT rectListView;
+	GetWindowRect(hListView, &rectListView);
+	MapWindowPoints(HWND_DESKTOP, hWndPropSheet, (LPPOINT)&rectListView, 2);
+	ptListView.x = rectListView.left;
+	ptListView.y = rectListView.top;
+	szListView.cx = rectListView.right - rectListView.left;
+	szListView.cy = rectListView.bottom - rectListView.top;
+
 	// Subclass the ListView.
 	// TODO: Error handling?
 	SetWindowSubclass(hListView, ListViewSubclassProc,
@@ -584,6 +597,11 @@ INT_PTR CALLBACK KeyManagerTabPrivate::dlgProc(HWND hDlg, UINT uMsg, WPARAM wPar
 					d->save();
 					break;
 
+				case PSN_SETACTIVE:
+					// Disable the "Defaults" button.
+					RpPropSheet_EnableDefaults(GetParent(hDlg), false);
+					break;
+
 				case LVN_GETDISPINFO: {
 					// Get data for an LVS_OWNERDRAW ListView.
 					if (!d->keyStore || pHdr->idFrom != IDC_KEYMANAGER_LIST)
@@ -607,10 +625,16 @@ INT_PTR CALLBACK KeyManagerTabPrivate::dlgProc(HWND hDlg, UINT uMsg, WPARAM wPar
 					return true;
 				}
 
-				case PSN_SETACTIVE:
-					// Disable the "Defaults" button.
-					RpPropSheet_EnableDefaults(GetParent(hDlg), false);
+				case MSGWN_CLOSED: {
+					// MessageWidget's Close button was pressed.
+					HWND hListView = GetDlgItem(hDlg, IDC_KEYMANAGER_LIST);
+					assert(hListView != nullptr);
+					SetWindowPos(hListView, nullptr,
+						d->ptListView.x, d->ptListView.y,
+						d->szListView.cx, d->szListView.cy,
+						SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 					break;
+				}
 
 				default:
 					break;
@@ -1583,11 +1607,6 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 		}
 	}
 
-	// Align to the bottom of the dialog and center-align the text.
-	// 7x7 DLU margin is recommended by the Windows UX guidelines.
-	// Reference: http://stackoverflow.com/questions/2118603/default-dialog-padding
-	RECT tmpRect = {7, 7, 8, 8};
-	MapDialogRect(hWndPropSheet, &tmpRect);
 	RECT winRect;
 	GetClientRect(hWndPropSheet, &winRect);
 	// NOTE: We need to move left by 1px.
@@ -1627,6 +1646,14 @@ void KeyManagerTabPrivate::showKeyImportReturnStatus(
 		SetWindowPos(hMessageWidget, nullptr, 0, 0, szMsgw.cx, szMsgw.cy,
 			SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_NOMOVE);
 	}
+
+	// Adjust the ListView positioning and size.
+	HWND hListView = GetDlgItem(hWndPropSheet, IDC_KEYMANAGER_LIST);
+	assert(hListView != nullptr);
+	SetWindowPos(hListView, nullptr,
+		ptListView.x, ptListView.y + szMsgw.cy,
+		szListView.cx, szListView.cy - szMsgw.cy,
+		SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 
 	// Show the message widget.
 	// TODO: Adjust the ListView positioning and size?

commit d9fcfa637092951704465a32d249742414526970

[win32] MessageWidget: Don't subtract 2x szIcon.cy from rect.bottom; only 1x.
Otherwise, descenders in e.g. 'g' and 'y' get cut off.

This fixes a regression from commit 5ff0abbc7b15c9e84891ab6c7e30cf0b0ecf14c0.
([win32] KeyManagerTab: Adjust the ListView positioning and size for the MessageWidget.)

src/win32/MessageWidget.cpp
@@ -229,7 +229,9 @@ void MessageWidgetPrivate::paint(void)
 		// Message
 		RECT textRect = {
 			rect.left, rect.top + (szIcon.cy / 4),
-			rect.right, rect.bottom - ((szIcon.cy / 4) * 2)
+			// NOTE: Not subtracting 2x szIcon.cy in order to
+			// leave room for descenders, e.g. in 'g' and 'y'.
+			rect.right, rect.bottom - (szIcon.cy / 4)
 		};
 		TCHAR tbuf[128];
 		int len = GetWindowTextLength(hWnd);