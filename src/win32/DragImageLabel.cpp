GerbilSoft/rom-properties

commit 178fe4c52a392395b4de5d3a9d435a3c9129e3be

Reorganized variables in several classes to reduce memory usage by reducing padding.
These classes had a somewhat significant size reduction: (64-bit Linux)
- RomDataPrivate: 100 -> 96 (-4)
- GcnFstPrivate: 104 -> 88 (-16)
- ELFPrivate: 296 -> 280 (-16)

Converted some static classes to namespaces:
- [win32] RpImageWin32
- [libromdata] CtrKeyScrambler

[win32] MessageWidgetPrivate:
- Un-inline the constructor.
- Don't save the USER32 pointer; it's only needed when changing the icon.

RomFields: Rearranged some fields in Field::desc::bitfield
and Field::desc::list_data to reduce padding.
- This causes ABI breakage, but libromdata.so hasn't been released
  yet, so it's okay.

RomMetaData: Expand Property to `int` for future expansion in case
more properties are added later.
- Again, ABI breakage, but better to do it now before the ABI is
  frozen for this SOVERSION than to do it later.

WiiSave: Move key_idx[] and key_status[] to ENABLE_DECRYPTION.
- NOTE: Not really used much...

NintendoDSPrivate::getDefaultLC(): Removed 'inline'.

NCCHReaderPrivate: Changed headers_loaded from uint32_t to uint8_t.

src/win32/DragImageLabel.cpp
@@ -41,7 +41,6 @@ class DragImageLabelPrivate
 
 	public:
 		HWND hwndParent;
-		bool useNearestNeighbor;
 
 		// Position.
 		POINT position;
@@ -59,11 +58,11 @@ class DragImageLabelPrivate
 
 		// Animated icon data.
 		struct anim_vars {
-			HWND m_hwndParent;
 			const LibRpBase::IconAnimData *iconAnimData;
+			std::array<HBITMAP, IconAnimData::MAX_FRAMES> iconFrames;
+			IconAnimHelper iconAnimHelper;
+			HWND m_hwndParent;
 			UINT_PTR animTimerID;
-			std::array<HBITMAP, LibRpBase::IconAnimData::MAX_FRAMES> iconFrames;
-			LibRpBase::IconAnimHelper iconAnimHelper;
 			int last_frame_number;		// Last frame number.
 
 			anim_vars(HWND hwndParent)
@@ -88,6 +87,9 @@ class DragImageLabelPrivate
 			}
 		};
 		anim_vars *anim;
+
+		// Use nearest-neighbor scaling?
+		bool useNearestNeighbor;
 
 	public:
 		/**
@@ -124,10 +126,10 @@ class DragImageLabelPrivate
 
 DragImageLabelPrivate::DragImageLabelPrivate(HWND hwndParent)
 	: hwndParent(hwndParent)
-	, useNearestNeighbor(false)
 	, img(nullptr)
 	, hbmpImg(nullptr)
 	, anim(nullptr)
+	, useNearestNeighbor(false)
 {
 	// TODO: Set rect/size as parameters?
 	requiredSize.cx = DIL_REQ_IMAGE_SIZE;

commit 467df681c639157c17e4a48a5971c6bdf3e78d29

[win32] DragImageLabel: Reorder initialization to match variable order.
src/win32/DragImageLabel.cpp: In constructor ‘DragImageLabelPrivate::anim_vars::anim_vars(HWND)’:
src/win32/DragImageLabel.cpp:64:30: warning: ‘DragImageLabelPrivate::anim_vars::m_hwndParent’ will be initialized after [-Wreorder]
   64 |                         HWND m_hwndParent;
      |                              ^~~~~~~~~~~~
src/win32/DragImageLabel.cpp:61:56: warning:   ‘const LibRpBase::IconAnimData* DragImageLabelPrivate::anim_vars::iconAnimData’ [-Wreorder]
   61 |                         const LibRpBase::IconAnimData *iconAnimData;
      |                                                        ^~~~~~~~~~~~
src/win32/DragImageLabel.cpp:68:25: warning:   when initialized here [-Wreorder]
   68 |                         anim_vars(HWND hwndParent)
      |                         ^~~~~~~~~

This fixes a regression from commit 178fe4c52a392395b4de5d3a9d435a3c9129e3be.
(Reorganized variables in several classes to reduce memory usage by reducing padding.)

src/win32/DragImageLabel.cpp
@@ -66,8 +66,8 @@ class DragImageLabelPrivate
 			int last_frame_number;		// Last frame number.
 
 			anim_vars(HWND hwndParent)
-				: m_hwndParent(hwndParent)
-				, iconAnimData(nullptr)
+				: iconAnimData(nullptr)
+				, m_hwndParent(hwndParent)
 				, animTimerID(0)
 				, last_frame_number(0)
 			{