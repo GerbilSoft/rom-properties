GerbilSoft/rom-properties

commit 178fe4c52a392395b4de5d3a9d435a3c9129e3be

Reorganized variables in several classes to reduce memory usage by reducing padding.
These classes had a somewhat significant size reduction: (64-bit Linux)
- RomDataPrivate: 100 -> 96 (-4)
- GcnFstPrivate: 104 -> 88 (-16)
- ELFPrivate: 296 -> 280 (-16)

Converted some static classes to namespaces:
- [win32] RpImageWin32
- [libromdata] CtrKeyScrambler

[win32] MessageWidgetPrivate:
- Un-inline the constructor.
- Don't save the USER32 pointer; it's only needed when changing the icon.

RomFields: Rearranged some fields in Field::desc::bitfield
and Field::desc::list_data to reduce padding.
- This causes ABI breakage, but libromdata.so hasn't been released
  yet, so it's okay.

RomMetaData: Expand Property to `int` for future expansion in case
more properties are added later.
- Again, ABI breakage, but better to do it now before the ABI is
  frozen for this SOVERSION than to do it later.

WiiSave: Move key_idx[] and key_status[] to ENABLE_DECRYPTION.
- NOTE: Not really used much...

NintendoDSPrivate::getDefaultLC(): Removed 'inline'.

NCCHReaderPrivate: Changed headers_loaded from uint32_t to uint8_t.

src/win32/config/KeyManagerTab.cpp
@@ -124,6 +124,9 @@ class KeyManagerTabPrivate
 		// Font Handler.
 		FontHandler fontHandler;
 
+		// wtsapi32.dll for Remote Desktop status. (WinXP and later)
+		WTSSessionNotification wts;
+
 		// EDIT box for ListView.
 		HWND hEditBox;
 		int iEditItem;		// Item being edited. (-1 for none)
@@ -132,9 +135,6 @@ class KeyManagerTabPrivate
 
 		// Is this COMCTL32.dll v6.10 or later?
 		bool isComCtl32_v610;
-
-		// wtsapi32.dll for Remote Desktop status. (WinXP and later)
-		WTSSessionNotification wts;
 
 		// Icons for the "Valid?" column.
 		// NOTE: "?" and "X" are copies from User32.
@@ -1549,16 +1549,6 @@ void KeyManagerTab::reset(void)
 {
 	RP_D(KeyManagerTab);
 	d->reset();
-}
-
-/**
- * Load the default configuration.
- * This does NOT save, and will only emit modified()
- * if it's different from the current configuration.
- */
-void KeyManagerTab::loadDefaults(void)
-{
-	// Not implemented for this tab.
 }
 
 /**

commit b0bac3484e06279852cf8c5bab743f2ab7dc5b01

[win32] KeyManagerTab: Reorder initialization to match variable order.
src/win32/config/KeyManagerTab.cpp: In constructor ‘KeyManagerTabPrivate::KeyManagerTabPrivate()’:
src/win32/config/KeyManagerTab.cpp:140:22: warning: ‘KeyManagerTabPrivate::hEditBox’ will be initialized after [-Wreorder]
  140 |                 HWND hEditBox;
      |                      ^~~~~~~~
src/win32/config/KeyManagerTab.cpp:135:22: warning:   ‘HWND__* KeyManagerTabPrivate::hMessageWidget’ [-Wreorder]
  135 |                 HWND hMessageWidget;
      |                      ^~~~~~~~~~~~~~
src/win32/config/KeyManagerTab.cpp:224:1: warning:   when initialized here [-Wreorder]
  224 | KeyManagerTabPrivate::KeyManagerTabPrivate()
      | ^~~~~~~~~~~~~~~~~~~~

This fixes a regression from commit 178fe4c52a392395b4de5d3a9d435a3c9129e3be.
(Reorganized variables in several classes to reduce memory usage by reducing padding.)

src/win32/config/KeyManagerTab.cpp
@@ -228,8 +228,8 @@ KeyManagerTabPrivate::KeyManagerTabPrivate()
 	, keyStore(new KeyStoreWin32(nullptr))
 	, keyStore_ownerDataCallback(nullptr)
 	, fontHandler(nullptr)
-	, hEditBox(nullptr)
 	, hMessageWidget(nullptr)
+	, hEditBox(nullptr)
 	, iEditItem(-1)
 	, bCancelEdit(false)
 	, bAllowKanji(false)