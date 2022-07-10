0b031c0cb30a61e2369f721ab6f65c7849155371
David Korth <gerbilsoft@gerbilsoft.com>
2022-07-10 00:50:31 -0400

    [libromdata] Fix header comments in *_ops.cpp files.
    (cherry picked from commit 281e089ffb4d19eb27d779d13e635b647b7841e7)

--- src/libromdata/Console/WiiWAD_ops.cpp
+++ src/libromdata/Console/WiiWAD_ops.cpp
@@ -1,8 +1,8 @@
 /***************************************************************************
  * ROM Properties Page shell extension. (libromdata)                       *
- * WiiWAD.cpp: Nintendo Wii WAD file reader. (ROM operations)              *
+ * WiiWAD_ops.cpp: Nintendo Wii WAD file reader. (ROM operations)          *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
