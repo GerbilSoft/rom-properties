GerbilSoft/rom-properties

commit 9dda274dc078915a12983af4c24a46772b2085ae

[kde] Consolidated the Qt/KDE version macros.
They're now located in RpQt.hpp.

RpQt.hpp: #include <QtCore/QObject>, needed for findDirectChild().
This only caused a problem with Qt 6, though...

check-uid.hpp: The macros now have the Qt version number in them, so we
don't need to add it using a printf format specifier.

src/kde/ExtractorPlugin.hpp
@@ -16,25 +16,12 @@
 #include <QtCore/qglobal.h>
 #include <kfilemetadata/extractorplugin.h>
 
-#if QT_VERSION >= QT_VERSION_CHECK(7,0,0)
-#  error Needs updating for Qt7
-#elif QT_VERSION >= QT_VERSION_CHECK(6,0,0)
-#  define PFN_CREATEEXTRACTORPLUGINKDE_FN createExtractorPluginKF6
-#  define PFN_CREATEEXTRACTORPLUGINKDE_NAME "createExtractorPluginKF6"
-#  define RomPropertiesKDE RomPropertiesKF6
-#elif QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-#  define PFN_CREATEEXTRACTORPLUGINKDE_FN createExtractorPluginKF5
-#  define PFN_CREATEEXTRACTORPLUGINKDE_NAME "createExtractorPluginKF5"
-#  define RomPropertiesKDE RomPropertiesKF5
-#elif QT_VERSION >= QT_VERSION_CHECK(4,0,0)
-#  define PFN_CREATEEXTRACTORPLUGINKDE_FN createExtractorPluginKDE4
-#  define PFN_CREATEEXTRACTORPLUGINKDE_NAME "createExtractorPluginKDE4"
-#  define RomPropertiesKDE RomPropertiesKDE4
-#else /* QT_VERSION < QT_VERSION_CHECK(4,0,0) */
-#  error Qt version is too old
-#endif
+#include "RpQt.hpp"
 
-namespace RomPropertiesKF5 {
+#define PFN_CREATEEXTRACTORPLUGINKDE_FN createExtractorPlugin ## RP_KDE_SUFFIX
+#define PFN_CREATEEXTRACTORPLUGINKDE_NAME "createExtractorPlugin" RP_KDE_UPPER
+
+namespace RomPropertiesKDE {
 
 class ExtractorPlugin final : public ::KFileMetaData::ExtractorPlugin
 {

src/kde/OverlayIconPlugin.hpp
@@ -16,23 +16,10 @@
 #include <QtCore/qglobal.h>
 #include <KOverlayIconPlugin>
 
-#if QT_VERSION >= QT_VERSION_CHECK(7,0,0)
-#  error Needs updating for Qt7
-#elif QT_VERSION >= QT_VERSION_CHECK(6,0,0)
-#  define PFN_CREATEOVERLAYICONPLUGINKDE_FN createOverlayIconPluginKF6
-#  define PFN_CREATEOVERLAYICONPLUGINKDE_NAME "createOverlayIconPluginKF6"
-#  define RomPropertiesKDE RomPropertiesKF6
-#elif QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-#  define PFN_CREATEOVERLAYICONPLUGINKDE_FN createOverlayIconPluginKF5
-#  define PFN_CREATEOVERLAYICONPLUGINKDE_NAME "createOverlayIconPluginKF5"
-#  define RomPropertiesKDE RomPropertiesKF5
-#elif QT_VERSION >= QT_VERSION_CHECK(4,0,0)
-#  define PFN_CREATEOVERLAYICONPLUGINKDE_FN createOverlayIconPluginKDE4
-#  define PFN_CREATEOVERLAYICONPLUGINKDE_NAME "createOverlayIconPluginKDE4"
-#  define RomPropertiesKDE RomPropertiesKDE4
-#else /* QT_VERSION < QT_VERSION_CHECK(4,0,0) */
-#  error Qt version is too old
-#endif
+#include "RpQt.hpp"
+
+#define PFN_CREATEOVERLAYICONPLUGINKDE_FN createOverlayIconPlugin ## RP_KDE_SUFFIX
+#define PFN_CREATEOVERLAYICONPLUGINKDE_NAME "createOverlayIconPlugin" RP_KDE_UPPER
 
 namespace RomPropertiesKDE {
 

src/kde/RpQt.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KF5)                         *
  * RpQt.hpp: Qt wrappers for some libromdata functionality.                *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-202 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -19,19 +19,33 @@ namespace LibRpTexture {
 // C++ includes.
 #include <string>
 
-// Qt includes.
+// Qt includes
+#include <qglobal.h>
+#include <QtCore/QObject>
 #include <QtCore/QString>
 #include <QtCore/QUrl>
 #include <QtGui/QImage>
 
-// KDE Frameworks prefix. (KDE4/KF5)
-#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-#  define RP_KDE_UPPER "KF"
-#  define RP_KDE_LOWER "kf"
-#else /* !QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
-#  define RP_KDE_UPPER "KDE"
-#  define RP_KDE_LOWER "kde"
-#endif /* QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
+#if QT_VERSION >= QT_VERSION_CHECK(7,0,0)
+#  error Needs updating for Qt7
+#elif QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+#  define RP_KDE_SUFFIX KF6
+#  define RP_KDE_UPPER "KF6"
+#  define RP_KDE_LOWER "kf6"
+#  define RomPropertiesKDE RomPropertiesKF6
+#elif QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#  define RP_KDE_SUFFIX KF5
+#  define RP_KDE_UPPER "KF5"
+#  define RP_KDE_LOWER "kf5"
+#  define RomPropertiesKDE RomPropertiesKF5
+#elif QT_VERSION >= QT_VERSION_CHECK(4,0,0)
+#  define RP_KDE_SUFFIX KDE4
+#  define RP_KDE_UPPER "KDE4"
+#  define RP_KDE_LOWER "kde4"
+#  define RomPropertiesKDE RomPropertiesKDE4
+#else /* QT_VERSION < QT_VERSION_CHECK(4,0,0) */
+#  error Qt version is too old
+#endif
 
 /** Text conversion **/
 

src/kde/ExtractorPlugin.cpp
@@ -40,14 +40,14 @@ using namespace KFileMetaData::Property;
  * rom-properties, and is called by a forwarder library.
  */
 extern "C" {
-	Q_DECL_EXPORT RomPropertiesKF5::ExtractorPlugin *PFN_CREATEEXTRACTORPLUGINKDE_FN(QObject *parent)
+	Q_DECL_EXPORT RomPropertiesKDE::ExtractorPlugin *PFN_CREATEEXTRACTORPLUGINKDE_FN(QObject *parent)
 	{
 		CHECK_UID_RET(nullptr);
-		return new RomPropertiesKF5::ExtractorPlugin(parent);
+		return new RomPropertiesKDE::ExtractorPlugin(parent);
 	}
 }
 
-namespace RomPropertiesKF5 {
+namespace RomPropertiesKDE {
 
 ExtractorPlugin::ExtractorPlugin(QObject *parent)
 	: super(parent)

src/kde/RomThumbCreator.cpp
@@ -38,19 +38,6 @@ using std::unique_ptr;
 // Used to find the KDE proxy settings.
 #include <kprotocolmanager.h>
 
-// Qt major version.
-#if QT_VERSION >= QT_VERSION_CHECK(7,0,0)
-#  error Needs updating for Qt7
-#elif QT_VERSION >= QT_VERSION_CHECK(6,0,0)
-#  define QT_MAJOR_STR "6"
-#elif QT_VERSION >= QT_VERSION_CHECK(5,0,0)
-#  define QT_MAJOR_STR "5"
-#elif QT_VERSION >= QT_VERSION_CHECK(4,0,0)
-#  define QT_MAJOR_STR "4"
-#else /* QT_VERSION < QT_VERSION_CHECK(4,0,0) */
-#  error Qt version is too old
-#endif
-
 /**
  * Factory method.
  * References:
@@ -370,7 +357,7 @@ Q_DECL_EXPORT int RP_C_API rp_create_thumbnail(const char *source_file, const ch
 	}
 
 	// Software.
-	static const char sw[] = "ROM Properties Page shell extension (" RP_KDE_UPPER QT_MAJOR_STR ")";
+	static const char sw[] = "ROM Properties Page shell extension (" RP_KDE_UPPER ")";
 	kv.emplace_back("Software", sw);
 
 	// Local filename.

src/kde/check-uid.hpp
@@ -15,8 +15,7 @@
 // Prevent running as root.
 #define CHECK_UID() do { \
 	if (getuid() == 0 || geteuid() == 0) { \
-		qCritical("*** rom-properties-" RP_KDE_LOWER "%u does not support running as root.", \
-			static_cast<unsigned int>(QT_VERSION) >> 16); \
+		qCritical("*** rom-properties-" RP_KDE_LOWER " does not support running as root."); \
 		return; \
 	} \
 } while (0)
@@ -25,8 +24,7 @@
 // If running as root, returns errval.
 #define CHECK_UID_RET(errval) do { \
 	if (getuid() == 0 || geteuid() == 0) { \
-		qCritical("*** rom-properties-" RP_KDE_LOWER "%u does not support running as root.", \
-			static_cast<unsigned int>(QT_VERSION) >> 16); \
+		qCritical("*** rom-properties-" RP_KDE_LOWER " does not support running as root."); \
 	} \
 } while (0)
 

src/kde/kf5/ExtractorPluginForwarder.cpp
@@ -12,10 +12,10 @@
 
 #include "config.kf5.h"
 #include "../check-uid.hpp"
+#include "../RpQt.hpp"
 
 #include "ExtractorPluginForwarder.hpp"
 #include "../ExtractorPlugin.hpp"
-#include "../RpQt.hpp"
 
 // C includes.
 #include <dlfcn.h>

src/kde/kf5/OverlayIconPluginForwarder.cpp
@@ -12,10 +12,10 @@
 
 #include "config.kf5.h"
 #include "../check-uid.hpp"
+#include "../RpQt.hpp"
 
 #include "OverlayIconPluginForwarder.hpp"
 #include "../OverlayIconPlugin.hpp"
-#include "../RpQt.hpp"
 
 // C includes.
 #include <dlfcn.h>

commit 267e0dc023a176bf97338d4f9f370a048d072fc4

[kde] *Plugin.hpp: Fix the function name macros.
We need to use some macro magic to properly expand RP_KDE_SUFFIX.
Otherwise, we end up with e.g. createExtractorPluginRP_KDE_SUFFIX,
which isn't handled by the forwarder plugins, and then both the
metadata extraction and overlay icon functionality stops working.

This fixes a regression from commit 9dda274dc078915a12983af4c24a46772b2085ae.
([kde] Consolidated the Qt/KDE version macros.)

src/kde/ExtractorPlugin.hpp
@@ -6,7 +6,7 @@
  * multiple plugins, so this file acts as a KFileMetaData ExtractorPlugin, *
  * and then forwards the request to the main library.                      *
  *                                                                         *
- * Copyright (c) 2018-2020 by David Korth.                                 *
+ * Copyright (c) 2018-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -18,7 +18,7 @@
 
 #include "RpQt.hpp"
 
-#define PFN_CREATEEXTRACTORPLUGINKDE_FN createExtractorPlugin ## RP_KDE_SUFFIX
+#define PFN_CREATEEXTRACTORPLUGINKDE_FN CONCAT_FN(createExtractorPlugin, RP_KDE_SUFFIX)
 #define PFN_CREATEEXTRACTORPLUGINKDE_NAME "createExtractorPlugin" RP_KDE_UPPER
 
 namespace RomPropertiesKDE {

src/kde/OverlayIconPlugin.hpp
@@ -18,7 +18,7 @@
 
 #include "RpQt.hpp"
 
-#define PFN_CREATEOVERLAYICONPLUGINKDE_FN createOverlayIconPlugin ## RP_KDE_SUFFIX
+#define PFN_CREATEOVERLAYICONPLUGINKDE_FN CONCAT_FN(createOverlayIconPlugin, RP_KDE_SUFFIX)
 #define PFN_CREATEOVERLAYICONPLUGINKDE_NAME "createOverlayIconPlugin" RP_KDE_UPPER
 
 namespace RomPropertiesKDE {

src/kde/RpQt.hpp
@@ -26,6 +26,9 @@ namespace LibRpTexture {
 #include <QtCore/QUrl>
 #include <QtGui/QImage>
 
+#define CONCAT_FN(fn, suffix)		CONCAT_FN_INT(fn, suffix)
+#define CONCAT_FN_INT(fn, suffix)	fn ## suffix
+
 #if QT_VERSION >= QT_VERSION_CHECK(7,0,0)
 #  error Needs updating for Qt7
 #elif QT_VERSION >= QT_VERSION_CHECK(6,0,0)