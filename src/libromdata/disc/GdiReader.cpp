GerbilSoft/rom-properties

commit 9f3548a569bb61b1829293211886b512b9d1cc12

Use range-based `for` loops in more places.
Started with `auto &&`, but switched to `const auto &` later on.

Will go back and replace `auto &&` with `const auto &` where it
might work better.

src/libromdata/disc/GdiReader.cpp
@@ -504,20 +504,18 @@ int GdiReader::readBlock(uint32_t blockIdx, int pos, void *ptr, size_t size)
 
 	// Find the block.
 	// TODO: Cache this lookup somewhere or something.
+	// NOTE: Using volatile because it can change in d->openTrack().
 	const GdiReaderPrivate::BlockRange *blockRange = nullptr;
-	const auto blockRanges_cend = d->blockRanges.cend();
-	for (auto iter = d->blockRanges.cbegin(); iter != blockRanges_cend; ++iter) {
-		// NOTE: Using volatile because it can change in d->openTrack().
-		const volatile GdiReaderPrivate::BlockRange *const vbr = &(*iter);
-		if (blockIdx < vbr->blockStart) {
+	for (const volatile GdiReaderPrivate::BlockRange &vbr : d->blockRanges) {
+		if (blockIdx < vbr.blockStart) {
 			// Not in this track.
 			continue;
 		}
 
 		// Is the track loaded?
-		if (vbr->blockEnd == 0) {
+		if (vbr.blockEnd == 0) {
 			// Track isn't loaded. Load it.
-			int ret = d->openTrack(vbr->trackNumber);
+			int ret = d->openTrack(vbr.trackNumber);
 			if (ret != 0) {
 				// Unable to load the track.
 				// Skip for now.
@@ -526,9 +524,9 @@ int GdiReader::readBlock(uint32_t blockIdx, int pos, void *ptr, size_t size)
 		}
 
 		// Check the end block.
-		if (vbr->blockEnd != 0 && blockIdx <= vbr->blockEnd) {
+		if (vbr.blockEnd != 0 && blockIdx <= vbr.blockEnd) {
 			// Found the track.
-			blockRange = (const GdiReaderPrivate::BlockRange*)vbr;
+			blockRange = (const GdiReaderPrivate::BlockRange*)&vbr;
 			break;
 		}
 	}

src/amiibo-data/amiiboc.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (amiibo-data)                      *
  * amiiboc.cpp: Nintendo amiibo binary data compiler.                      *
  *                                                                         *
- * Copyright (c) 2016-2021 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -564,17 +564,16 @@ int main(int argc, char *argv[])
 	const uint32_t char_len = static_cast<uint32_t>(charTable.size() * sizeof(CharTableEntry));
 	binHeader.char_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
 	binHeader.char_len = cpu_to_le32(char_len);
-	const auto charTable_cend = charTable.cend();
-	for (auto iter = charTable.cbegin(); iter != charTable_cend; ++iter) {
-#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+	for (auto &&p : charTable) {
+#if SYS_BYTEORDER != SYS_LIL_ENDIAN
 		// Byteswap the entry first.
-		CharTableEntry entry = iter->second;
+		CharTableEntry entry = p.second;
 		entry.char_id	= cpu_to_le32(entry.char_id);
 		entry.name	= cpu_to_le32(entry.name);
 		fwrite(&entry, 1, sizeof(entry), f_out);
 #else /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
 		// No byteswap is needed.
-		fwrite(&(iter->second), 1, sizeof(iter->second), f_out);
+		fwrite(&(p.second), 1, sizeof(p.second), f_out);
 #endif
 	}
 
@@ -582,22 +581,19 @@ int main(int argc, char *argv[])
 	alignFileTo16Bytes(f_out);
 	uint32_t cvar_len = 0;
 	binHeader.cvar_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
-	const auto charVarTable_cend = charVarTable.cend();
-	for (auto iter = charVarTable.cbegin(); iter != charVarTable_cend; ++iter) {
-		const CharVariantMap &map = iter->second;
-		const auto map_cend = map.cend();
-		for (auto iter = map.cbegin(); iter != map_cend; ++iter) {
-#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+	for (auto &&p : charVarTable) {
+		for (auto &&q : p.second) {
+#if SYS_BYTEORDER != SYS_LIL_ENDIAN
 			// Byteswap the entry first.
-			CharVariantTableEntry entry = iter->second;
+			CharVariantTableEntry entry = q.second;
 			entry.char_id	= cpu_to_le16(entry.char_id);
 			entry.name	= cpu_to_le32(entry.name);
 			fwrite(&entry, 1, sizeof(entry), f_out);
 #else /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
 			// No byteswap is needed.
-			fwrite(&(iter->second), 1, sizeof(iter->second), f_out);
+			fwrite(&(q.second), 1, sizeof(q.second), f_out);
 #endif
-			cvar_len += static_cast<uint32_t>(sizeof(iter->second));
+			cvar_len += static_cast<uint32_t>(sizeof(q.second));
 		}
 	}
 	binHeader.cvar_len = cpu_to_le32(cvar_len);
@@ -618,11 +614,10 @@ int main(int argc, char *argv[])
 	const uint32_t amiibo_len = static_cast<uint32_t>(amiiboTable.size() * sizeof(AmiiboIDTableEntry));
 	binHeader.amiibo_offset = cpu_to_le32(static_cast<uint32_t>(ftello(f_out)));
 	binHeader.amiibo_len = cpu_to_le32(amiibo_len);
-#if SYS_BYTEORDER == SYS_BIG_ENDIAN
+#if SYS_BYTEORDER != SYS_LIL_ENDIAN
 	// Byteswapping is needed, so we have to process each entry.
-	const auto amiiboTable_cend = amiiboTable.cend();
-	for (auto iter = amiiboTable.cbegin(); iter != amiiboTable_cend; ++iter) {
-		AmiiboIDTableEntry entry = *iter;
+	for (auto &&p : amiiboTable) {
+		AmiiboIDTableEntry entry = p;
 		entry.release_no	= cpu_to_le16(entry.release_no);
 		entry.name		= cpu_to_le32(entry.name);
 		fwrite(&entry, 1, sizeof(entry), f_out);

src/gtk/OptionsMenuButton.cpp
@@ -594,21 +594,23 @@ options_menu_button_reinit_menu(OptionsMenuButton *widget,
 		g_menu_append_section(menuModel, nullptr, G_MENU_MODEL(menuRomOps));
 
 		int i = 0;
-		const auto ops_end = ops.cend();
-		for (auto iter = ops.cbegin(); iter != ops_end; ++iter, i++) {
+		for (auto &&op : ops) {
 			// Create the action.
 			char buf[128];
 			snprintf(buf, sizeof(buf), "%d", i);
 			GSimpleAction *const action = g_simple_action_new(buf, nullptr);
-			g_simple_action_set_enabled(action, !!(iter->flags & RomData::RomOp::ROF_ENABLED));
+			g_simple_action_set_enabled(action, !!(op.flags & RomData::RomOp::ROF_ENABLED));
 			g_object_set_data(G_OBJECT(action), "menuOptions_id", GINT_TO_POINTER(i));
 			g_signal_connect(action, "activate", G_CALLBACK(action_triggered_signal_handler), widget);
 			g_action_map_add_action(G_ACTION_MAP(actionGroup), G_ACTION(action));
 
 			// Create the menu item.
-			const string desc = convert_accel_to_gtk(iter->desc);
+			const string desc = convert_accel_to_gtk(op.desc);
 			snprintf(buf, sizeof(buf), "%s.%d", prefix, i);
 			g_menu_append(menuRomOps, desc.c_str(), buf);
+
+			// Next operation.
+			i++;
 		}
 	}
 #else /* !USE_G_MENU_MODEL */
@@ -635,15 +637,17 @@ options_menu_button_reinit_menu(OptionsMenuButton *widget,
 		gtk_menu_shell_append(GTK_MENU_SHELL(menuOptions), menuItem);
 
 		int i = 0;
-		const auto ops_end = ops.cend();
-		for (auto iter = ops.cbegin(); iter != ops_end; ++iter, i++) {
-			const string desc = convert_accel_to_gtk(iter->desc);
+		for (auto &&op : ops) {
+			const string desc = convert_accel_to_gtk(op.desc);
 			menuItem = gtk_menu_item_new_with_mnemonic(desc.c_str());
-			gtk_widget_set_sensitive(menuItem, !!(iter->flags & RomData::RomOp::ROF_ENABLED));
+			gtk_widget_set_sensitive(menuItem, !!(op.flags & RomData::RomOp::ROF_ENABLED));
 			g_object_set_data(G_OBJECT(menuItem), "menuOptions_id", GINT_TO_POINTER(i));
 			g_signal_connect(menuItem, "activate", G_CALLBACK(menuOptions_triggered_signal_handler), widget);
 			gtk_widget_show(menuItem);
 			gtk_menu_shell_append(GTK_MENU_SHELL(menuOptions), menuItem);
+
+			// Next operation.
+			i++;
 		}
 	}
 #endif /* USE_G_MENU_MODEL */

src/gtk/RomDataView.cpp
@@ -1438,12 +1438,9 @@ rom_data_view_update_multi(RomDataView *page, uint32_t user_lc)
 	set<uint32_t> set_lc;
 
 	// RFT_STRING_MULTI
-	const auto vecStringMulti_cend = cxx->vecStringMulti.cend();
-	for (auto iter = cxx->vecStringMulti.cbegin();
-	     iter != vecStringMulti_cend; ++iter)
-	{
-		GtkWidget *const lblString = iter->first;
-		const RomFields::Field *const pField = iter->second;
+	for (auto &&vsm : cxx->vecStringMulti) {
+		GtkWidget *const lblString = vsm.first;
+		const RomFields::Field *const pField = vsm.second;
 		const auto *const pStr_multi = pField->data.str_multi;
 		assert(pStr_multi != nullptr);
 		assert(!pStr_multi->empty());
@@ -1455,11 +1452,8 @@ rom_data_view_update_multi(RomDataView *page, uint32_t user_lc)
 		if (!page->cboLanguage) {
 			// Need to add all supported languages.
 			// TODO: Do we need to do this for all of them, or just one?
-			const auto pStr_multi_cend = pStr_multi->cend();
-			for (auto iter_sm = pStr_multi->cbegin();
-			     iter_sm != pStr_multi_cend; ++iter_sm)
-			{
-				set_lc.emplace(iter_sm->first);
+			for (auto &&psm : *pStr_multi) {
+				set_lc.emplace(psm.first);
 			}
 		}
 
@@ -1470,12 +1464,9 @@ rom_data_view_update_multi(RomDataView *page, uint32_t user_lc)
 	}
 
 	// RFT_LISTDATA_MULTI
-	const auto vecListDataMulti_cend = cxx->vecListDataMulti.cend();
-	for (auto iter = cxx->vecListDataMulti.cbegin();
-	     iter != vecListDataMulti_cend; ++iter)
-	{
-		GtkListStore *const listStore = iter->listStore;
-		const RomFields::Field *const pField = iter->field;
+	for (auto &&vldm : cxx->vecListDataMulti) {
+		GtkListStore *const listStore = vldm.listStore;
+		const RomFields::Field *const pField = vldm.field;
 		const auto *const pListData_multi = pField->data.list_data.data.multi;
 		assert(pListData_multi != nullptr);
 		assert(!pListData_multi->empty());
@@ -1487,11 +1478,8 @@ rom_data_view_update_multi(RomDataView *page, uint32_t user_lc)
 		if (!page->cboLanguage) {
 			// Need to add all supported languages.
 			// TODO: Do we need to do this for all of them, or just one?
-			const auto pListData_multi_cend = pListData_multi->cend();
-			for (auto iter_sm = pListData_multi->cbegin();
-			     iter_sm != pListData_multi_cend; ++iter_sm)
-			{
-				set_lc.emplace(iter_sm->first);
+			for (auto &&pldm : *pListData_multi) {
+				set_lc.emplace(pldm.first);
 			}
 		}
 
@@ -1535,7 +1523,7 @@ rom_data_view_update_multi(RomDataView *page, uint32_t user_lc)
 			// Resize the columns to fit the contents.
 			// NOTE: Only done on first load.
 			if (!page->cboLanguage) {
-				gtk_tree_view_columns_autosize(GTK_TREE_VIEW(iter->treeView));
+				gtk_tree_view_columns_autosize(GTK_TREE_VIEW(vldm.treeView));
 			}
 		}
 	}

src/kde/DragImageTreeView.cpp
@@ -34,11 +34,11 @@ void DragImageTreeView::startDrag(Qt::DropActions supportedActions)
 	// NOTE: Each column is technically considered an item.
 	// Find the first item with a valid RpImageRole.
 	QModelIndexList items;
-	for (auto iter = indexes.begin(); iter != indexes.end(); ++iter) {
-		void *img = iter->data(RpImageRole).value<void*>();
+	for (auto &&p : indexes) {
+		void *img = p.data(RpImageRole).value<void*>();
 		if (img != nullptr) {
 			// Index has a valid image.
-			items.append(*iter);
+			items.append(p);
 		}
 	}
 
@@ -49,9 +49,7 @@ void DragImageTreeView::startDrag(Qt::DropActions supportedActions)
 	QMimeData *const mimeData = new QMimeData;
 	QIcon dragIcon;
 	bool hasOne = false;
-	const auto items_end = items.end();
-	for (auto iter = items.begin(); iter != items_end; ++iter) {
-		const QModelIndex &index = *iter;
+	for (auto &&index : items) {
 		const rp_image *const img = static_cast<const rp_image*>(index.data(RpImageRole).value<void*>());
 		if (!img)
 			continue;

src/kde/ListDataModel.cpp
@@ -180,9 +180,7 @@ void ListDataModelPrivate::updateIconPixmaps(void)
 	icons.clear();
 	icons.reserve(icons_rp.size());
 
-	const auto icons_rp_cend = icons_rp.cend();
-	for (auto iter = icons_rp.cbegin(); iter != icons_rp_cend; ++iter) {
-		const rp_image *const img = *iter;
+	for (auto &&img : icons_rp) {
 		if (!img) {
 			icons.emplace_back(QPixmap());
 			continue;
@@ -220,9 +218,7 @@ vector<QString> ListDataModelPrivate::convertListDataToVector(const RomFields::L
 	const int rowCount = list_data->size();
 
 	data.reserve(columnCount * rowCount);
-	const auto list_data_cend = list_data->cend();
-	for (auto iter = list_data->cbegin(); iter != list_data_cend; ++iter) {
-		const vector<string> &data_row = *iter;
+	for (auto &&data_row : *list_data) {
 		if (hasCheckboxes && data_row.empty()) {
 			// Skip this row.
 			continue;
@@ -231,9 +227,13 @@ vector<QString> ListDataModelPrivate::convertListDataToVector(const RomFields::L
 		// Add item text.
 		assert((int)data_row.size() == columnCount);
 		int cols = columnCount;
-		const auto data_row_cend = data_row.cend();
-		for (auto iter = data_row.cbegin(); iter != data_row_cend && cols > 0; ++iter, cols--) {
-			data.emplace_back(U82Q(*iter));
+		for (auto &&u8_str : data_row) {
+			data.emplace_back(U82Q(u8_str));
+
+			cols--;
+			if (cols <= 0) {
+				break;
+			}
 		}
 		// If there's fewer columns in the data row than we have allocated,
 		// add blank QStrings.
@@ -545,9 +545,8 @@ void ListDataModel::setField(const RomFields::Field *pField)
 		d->headers.clear();
 		d->headers.reserve(columnCount);
 
-		const auto names_cend = listDataDesc.names->cend();
-		for (auto iter = listDataDesc.names->cbegin(); iter != names_cend; ++iter) {
-			d->headers.emplace_back(U82Q(*iter));
+		for (auto &&u8_str : *(listDataDesc.names)) {
+			d->headers.emplace_back(U82Q(u8_str));
 		}
 	} else {
 		// No column headers.
@@ -577,15 +576,13 @@ void ListDataModel::setField(const RomFields::Field *pField)
 		// RFT_LISTDATA_MULTI: Multiple languages.
 		const auto *const multi = pField->data.list_data.data.multi;
 		// NOTE: Assuming all languages have the same number of rows.
-		auto iter = multi->cbegin();
-		rowCount = static_cast<int>(iter->second.size());
+		rowCount = static_cast<int>(multi->cbegin()->second.size());
 
-		const auto multi_cend = multi->cend();
-		for (; iter != multi_cend; ++iter) {
-			assert(static_cast<int>(iter->second.size()) == rowCount);
-			auto pair = d->map_data.emplace(iter->first,
-				d->convertListDataToVector(&iter->second, hasCheckboxes));
-			if (!d->pData && iter->first == d->lc) {
+		for (auto &pdm : *multi) {
+			assert(static_cast<int>(pdm.second.size()) == rowCount);
+			auto pair = d->map_data.emplace(pdm.first,
+				d->convertListDataToVector(&(pdm.second), hasCheckboxes));
+			if (!d->pData && pdm.first == d->lc) {
 				d->pData = &(pair.first->second);
 			}
 		}
@@ -609,9 +606,7 @@ void ListDataModel::setField(const RomFields::Field *pField)
 		// Also, we can assume all rows are present, since
 		// icons and checkboxes are mutually exclusive.
 		d->icons.reserve(rowCount);
-		const auto icons_cend = pField->data.list_data.mxd.icons->cend();
-		for (auto iter = pField->data.list_data.mxd.icons->cbegin(); iter != icons_cend; ++iter) {
-			const rp_image *const icon = *iter;
+		for (auto &&icon : *(pField->data.list_data.mxd.icons)) {
 			d->icons_rp.emplace_back(icon ? icon->ref() : nullptr);
 		}
 		// Update the icons pixmap vector.
@@ -687,9 +682,8 @@ set<uint32_t> ListDataModel::getLCs(void) const
 		}
 	}
 
-	const auto map_data_cend = d->map_data.cend();
-	for (auto iter = d->map_data.cbegin(); iter != map_data_cend; ++iter) {
-		set_lc.insert(iter->first);
+	for (auto &&pmd : d->map_data) {
+		set_lc.insert(pmd.first);
 	}
 	return set_lc;
 }

src/kde/OptionsMenuButton.cpp
@@ -87,10 +87,9 @@ void OptionsMenuButton::reinitMenu(const LibRpBase::RomData *romData)
 		romOps_firstActionIndex = menuOptions->children().count();
 
 		int i = 0;
-		const auto ops_end = ops.cend();
-		for (auto iter = ops.cbegin(); iter != ops_end; ++iter, i++) {
-			QAction *const action = menuOptions->addAction(U82Q(iter->desc));
-			action->setEnabled(!!(iter->flags & RomData::RomOp::ROF_ENABLED));
+		for (auto &&op : ops) {
+			QAction *const action = menuOptions->addAction(U82Q(op.desc));
+			action->setEnabled(!!(op.flags & RomData::RomOp::ROF_ENABLED));
 #ifdef RP_OMB_USE_LAMBDA_FUNCTIONS
 			// Qt5: Use a lambda function.
 			QObject::connect(action, &QAction::triggered,
@@ -101,6 +100,9 @@ void OptionsMenuButton::reinitMenu(const LibRpBase::RomData *romData)
 				mapperOptionsMenu, SLOT(map()));
 			mapperOptionsMenu->setMapping(action, i);
 #endif /* RP_OMB_USE_LAMBDA_FUNCTIONS */
+
+			// Next operation.
+			i++;
 		}
 	}
 }

src/kde/RomDataView.cpp
@@ -1006,10 +1006,9 @@ void RomDataViewPrivate::updateMulti(uint32_t user_lc)
 	set<uint32_t> set_lc;
 
 	// RFT_STRING_MULTI
-	const auto vecStringMulti_cend = vecStringMulti.cend();
-	for (auto iter = vecStringMulti.cbegin(); iter != vecStringMulti_cend; ++iter) {
-		QLabel *const lblString = iter->first;
-		const RomFields::Field *const pField = iter->second;
+	for (auto &&vsm : vecStringMulti) {
+		QLabel *const lblString = vsm.first;
+		const RomFields::Field *const pField = vsm.second;
 		const auto *const pStr_multi = pField->data.str_multi;
 		assert(pStr_multi != nullptr);
 		assert(!pStr_multi->empty());
@@ -1021,11 +1020,8 @@ void RomDataViewPrivate::updateMulti(uint32_t user_lc)
 		if (!cboLanguage) {
 			// Need to add all supported languages.
 			// TODO: Do we need to do this for all of them, or just one?
-			const auto pStr_multi_cend = pStr_multi->cend();
-			for (auto iter_sm = pStr_multi->cbegin();
-			     iter_sm != pStr_multi_cend; ++iter_sm)
-			{
-				set_lc.emplace(iter_sm->first);
+			for (auto &&psm : *pStr_multi) {
+				set_lc.emplace(psm.first);
 			}
 		}
 
@@ -1036,10 +1032,9 @@ void RomDataViewPrivate::updateMulti(uint32_t user_lc)
 	}
 
 	// RFT_LISTDATA_MULTI
-	const auto vecListDataMulti_cend = vecListDataMulti.cend();
-	for (auto iter = vecListDataMulti.cbegin(); iter != vecListDataMulti_cend; ++iter) {
-		QTreeView *const treeView = iter->first;
-		ListDataModel *const listModel = iter->second;
+	for (auto &&vldm : vecListDataMulti) {
+		QTreeView *const treeView = vldm.first;
+		ListDataModel *const listModel = vldm.second;
 
 		if (listModel != nullptr) {
 			if (!cboLanguage) {

commit ac34d0ffc5c034c0af1c2bf9472ca43ef2c0b513

[libromdata] GdiReader: Don't use `volatile`.
This apparently breaks the range-based `for` loop:

V834 (Hi): Incorrect 'const volatile BlockRange&' type of the 'vbr' loop
variable. This leads to the variable binding to a temporary object
instead of a range element. The type of range elements is 'BlockRange'.

This fixes a regression from commit 9f3548a569bb61b1829293211886b512b9d1cc12.
(Use range-based `for` loops in more places.)

src/libromdata/disc/GdiReader.cpp
@@ -507,9 +507,8 @@ int GdiReader::readBlock(uint32_t blockIdx, int pos, void *ptr, size_t size)
 
 	// Find the block.
 	// TODO: Cache this lookup somewhere or something.
-	// NOTE: Using volatile because it can change in d->openTrack().
 	const GdiReaderPrivate::BlockRange *blockRange = nullptr;
-	for (const volatile GdiReaderPrivate::BlockRange &vbr : d->blockRanges) {
+	for (const GdiReaderPrivate::BlockRange &vbr : d->blockRanges) {
 		if (blockIdx < vbr.blockStart) {
 			// Not in this track.
 			continue;