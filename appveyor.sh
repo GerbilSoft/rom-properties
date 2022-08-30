6a91ee5759e3cf67e231c0b50111abbf68bdaa53
David Korth <gerbilsoft@gerbilsoft.com>
2022-08-30 18:01:03 -0400

    appveyor.yml: Initial attempt to use AppVeyor for Linux builds.
    - Temporarily disabled most of the Windows builds.
    
    - Added Linux build scripts, including a script run as root to
      install packages via `apt-get`.

---
+++ appveyor.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ev
+cmake --version
+
+# Enable or disable optional features?
+if [ "$1" = "OFF" ]; then
+	export OPTEN=OFF
+else
+	export OPTEN=ON
+fi
+
+OPTFEAT="-DENABLE_EXTRA_SECURITY=${OPTEN} -DENABLE_JPEG=${OPTEN} -DENABLE_XML=${OPTEN} -DENABLE_DECRYPTION=${OPTEN} -DENABLE_UNICE68=${OPTEN} -DENABLE_LIBMSPACK=${OPTEN} -DENABLE_PVRTC=${OPTEN} -DENABLE_ZSTD=${OPTEN} -DENABLE_LZ4=${OPTEN} -DENABLE_LZO=${OPTEN}"
+
+mkdir build
+cd build
+cmake .. -G Ninja -DBUILD_TESTING=ON -DENABLE_LTO=OFF -DENABLE_PCH=OFF %OPTFEAT%
