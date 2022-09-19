65cfe4454519b06169e95147bb3bacd8fa9171f2
David Korth <gerbilsoft@gerbilsoft.com>
2022-09-18 21:19:45 -0400

    [libromdata] EXEPrivate::findNERuntimeDLL(): Disable an assertion.
    It seems to be happening on EXEs that don't use a VB runtime.
    
    Disable the assertion for now and simply skip the section.

--- src/libromdata/Other/EXE_NE.cpp
+++ src/libromdata/Other/EXE_NE.cpp
@@ -210,7 +210,8 @@ int EXEPrivate::findNERuntimeDLL(string &refDesc, string &refLink, bool &refHasK
 		}
 
 		const uint8_t count = static_cast<uint8_t>(nameTable[nameOffset]);
-		assert(nameOffset + 1 + count < nameTable.size());
+		// FIXME: This assertion happens on EXEs that don't use a VB runtime.
+		//assert(nameOffset + 1 + count < nameTable.size());
 		if (nameOffset + 1 + count >= nameTable.size()) {
 			// Out of range.
 			// TODO: Return an error?
