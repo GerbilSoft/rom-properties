GerbilSoft/rom-properties

commit 7c7cdd278986163ba6a5ef6ed6c07682296526ea

Use std::lower_bound() instead of bsearch().
std::lower_bound() can inline the comparator, which improves performance
and sometimes reduces code size.

Also removed some NULL terminators at the end of some lists, which aren't
needed when doing a binary search with exact list sizes.

w32err.c, http-status.c: Changed to C++ to use std::lower_bound().

Code size difference: (64-bit Gentoo Linux, gcc-12.2.0, release build, no LTO)

   text    data     bss     dec     hex filename
1595083   64360     704 1660147  1954f3 libromdata.so.1.0 [before]
1594283   64040     704 1659027  195093 libromdata.so.1.0 [after]
   -800    -320       0   -1120    -460 Difference

   text    data     bss     dec     hex filename
  32792    1344      72   34208    85a0 rp-download [before]
  32808    1344      72   34224    85b0 rp-download [after]
    +16       0       0     +16     +10 Difference

src/libromdata/data/AmiiboData.cpp
@@ -133,22 +133,6 @@ class AmiiboDataPrivate {
 		 * @return String, or nullptr on error.
 		 */
 		inline const char *strTbl_lookup(uint32_t idx) const;
-
-		/**
-		 * CharTableEntry bsearch() comparison function.
-		 * @param a
-		 * @param b
-		 * @return
-		 */
-		static int RP_C_API CharTableEntry_compar(const void *a, const void *b);
-
-		/**
-		 * CharVariantTableEntry bsearch() comparison function.
-		 * @param a
-		 * @param b
-		 * @return
-		 */
-		static int RP_C_API CharVariantTableEntry_compar(const void *a, const void *b);
 };
 
 // amiibo-data.bin filename
@@ -507,55 +491,6 @@ inline const char *AmiiboDataPrivate::strTbl_lookup(uint32_t idx) const
 	return &pStrTbl[idx];
 }
 
-/**
- * CharTableEntry bsearch() comparison function.
- * @param a
- * @param b
- * @return
- */
-int RP_C_API AmiiboDataPrivate::CharTableEntry_compar(const void *a, const void *b)
-{
-	uint32_t id1 = le32_to_cpu(static_cast<const CharTableEntry*>(a)->char_id);
-	uint32_t id2 = le32_to_cpu(static_cast<const CharTableEntry*>(b)->char_id);
-
-	// NOTE: Masking off the character variant flag.
-	id1 &= ~CHARTABLE_VARIANT_FLAG;
-	id2 &= ~CHARTABLE_VARIANT_FLAG;
-
-	if (id1 < id2) return -1;
-	else if (id1 > id2) return 1;
-	else return 0;
-}
-
-/**
- * CharVariantTableEntry bsearch() comparison function.
- * @param a
- * @param b
- * @return
- */
-int RP_C_API AmiiboDataPrivate::CharVariantTableEntry_compar(const void *a, const void *b)
-{
-	const CharVariantTableEntry *pA = static_cast<const CharVariantTableEntry*>(a);
-	const CharVariantTableEntry *pB = static_cast<const CharVariantTableEntry*>(b);
-
-	if (pA->char_id == pB->char_id) {
-		// Same character ID. Compare the variant ID.
-		const uint8_t varA = pA->var_id;
-		const uint8_t varB = pB->var_id;
-		if (varA < varB) return -1;
-		else if (varA > varB) return 1;
-		else return 0;
-	}
-
-	// Character ID doesn't match.
-	const uint16_t idA = le16_to_cpu(pA->char_id);
-	const uint16_t idB = le16_to_cpu(pB->char_id);
-
-	if (idA < idB) return -1;
-	else if (idA > idB) return 1;
-	else return 0;
-}
-
 /** AmiiboData **/
 
 // Singleton instance.
@@ -623,49 +558,52 @@ const char *AmiiboData::lookup_char_name(uint32_t char_id) const
 	if (d->loadIfNeeded() != 0)
 		return nullptr;
 
-	// NOTE: Search is done on little-endian data,
-	// so the id needs to be 32-bit byteswapped.
-	const uint32_t id = cpu_to_le32((char_id >> 16) & 0xFFFF);
+	const uint32_t id = ((char_id >> 16) & 0xFFFF);
 
 	// Do a binary search.
-	const CharTableEntry key = {id, 0};
-	const CharTableEntry *const cres =
-		static_cast<const CharTableEntry*>(bsearch(&key,
-			d->pCharTbl,
-			d->charTbl_count,
-			sizeof(CharTableEntry),
-			AmiiboDataPrivate::CharTableEntry_compar));
-	if (!cres) {
+	//const CharTableEntry key = {id, 0};
+	const CharTableEntry *const pCharTblEnd = d->pCharTbl + d->charTbl_count;
+	auto pCTEntry = std::lower_bound(d->pCharTbl, pCharTblEnd, (id & ~CHARTABLE_VARIANT_FLAG),
+		[](const CharTableEntry &entry, uint32_t id2) {
+			uint32_t id1 = le32_to_cpu(entry.char_id) & ~CHARTABLE_VARIANT_FLAG;
+			return (id1 < id2);
+		});
+	if (pCTEntry == pCharTblEnd) {
 		// Character ID not found.
 		return nullptr;
 	}
 
 	// Check for variants.
 	const char *name = nullptr;
-	if (le32_to_cpu(cres->char_id) & CHARTABLE_VARIANT_FLAG) {
+	if (le32_to_cpu(pCTEntry->char_id) & CHARTABLE_VARIANT_FLAG) {
 		// Do a binary search in the character variant table.
-		const uint16_t cv_char_id = cpu_to_le16((char_id >> 16) & 0xFFFF);
+		const uint16_t cv_char_id = ((char_id >> 16) & 0xFFFF);
 		const uint8_t variant_id = (char_id >> 8) & 0xFF;
 		const CharVariantTableEntry key = {cv_char_id, variant_id, 0, 0};
-		const CharVariantTableEntry *const vres =
-			static_cast<const CharVariantTableEntry*>(bsearch(&key,
-				d->pCharVarTbl,
-				d->charVarTbl_count,
-				sizeof(CharVariantTableEntry),
-				AmiiboDataPrivate::CharVariantTableEntry_compar));
 
-		if (vres) {
+		const CharVariantTableEntry *const pCharVarTblEnd = d->pCharVarTbl + d->charVarTbl_count;
+		auto pCVTEntry = std::lower_bound(d->pCharVarTbl, pCharVarTblEnd, key,
+			[](const CharVariantTableEntry &key1, const CharVariantTableEntry &key2) {
+				// Compare the character ID first.
+				if (le16_to_cpu(key1.char_id) < key2.char_id)
+					return true;
+
+				// Compare the variant ID.
+				return (key1.var_id < key2.var_id);
+			});
+
+		if (pCVTEntry != pCharVarTblEnd) {
 			// Character variant ID found.
-			name = d->strTbl_lookup(le32_to_cpu(vres->name));
+			name = d->strTbl_lookup(le32_to_cpu(pCVTEntry->name));
 		} else {
 			// Character variant ID not found.
 			// Maybe it's an error...
 			// Default to the main character name.
-			name = d->strTbl_lookup(le32_to_cpu(cres->name));
+			name = d->strTbl_lookup(le32_to_cpu(pCTEntry->name));
 		}
 	} else {
 		// No variants.
-		name = d->strTbl_lookup(le32_to_cpu(cres->name));
+		name = d->strTbl_lookup(le32_to_cpu(pCTEntry->name));
 	}
 
 	return name;

src/libromdata/data/ELFData.cpp
@@ -55,24 +55,7 @@ static const ELFMachineType ELFMachineTypes_other[] = {
 	{0xFEBA,	"Vitesse IQ2000 (unofficial)"},
 	{0xFEBB,	"NIOS (unofficial)"},
 	{0xFEED,	"Moxie (unofficial)"},
-
-	{0, nullptr}
 };
-
-/**
- * bsearch() comparison function for ELFMachineType.
- * @param a
- * @param b
- * @return
- */
-static int RP_C_API ELFMachineType_compar(const void *a, const void *b)
-{
-	const uint16_t cpu1 = static_cast<const ELFMachineType*>(a)->cpu;
-	const uint16_t cpu2 = static_cast<const ELFMachineType*>(b)->cpu;
-	if (cpu1 < cpu2) return -1;
-	if (cpu1 > cpu2) return 1;
-	return 0;
-}
 
 /** Public functions **/
 
@@ -90,14 +73,13 @@ const char *lookup_cpu(uint16_t cpu)
 
 	// CPU ID is in the "other" IDs array.
 	// Do a binary search.
-	const ELFMachineType key = {cpu, nullptr};
-	const ELFMachineType *res =
-		static_cast<const ELFMachineType*>(bsearch(&key,
-			ELFMachineTypes_other,
-			ARRAY_SIZE(ELFMachineTypes_other)-1,
-			sizeof(ELFMachineType),
-			ELFMachineType_compar));
-	return (res ? res->name : nullptr);
+	static const ELFMachineType *const pELFMachineTypes_other_end =
+		&ELFMachineTypes_other[ARRAY_SIZE(ELFMachineTypes_other)];
+	auto pELF = std::lower_bound(ELFMachineTypes_other, pELFMachineTypes_other_end, cpu,
+		[](const ELFMachineType &elf, uint16_t cpu) {
+			return (elf.cpu < cpu);
+		});
+	return (pELF != pELFMachineTypes_other_end) ? pELF->name : nullptr;
 }
 
 /**

src/libromdata/data/EXEData.cpp
@@ -56,8 +56,6 @@ static const MachineType machineTypes_PE[] = {
 	{IMAGE_FILE_MACHINE_M32R,	"Mitsubishi M32R"},
 	{IMAGE_FILE_MACHINE_ARM64,	"ARM (64-bit)"},
 	{IMAGE_FILE_MACHINE_CEE,	"MSIL"},
-
-	{0, nullptr}
 };
 
 // LE machine types.
@@ -72,24 +70,7 @@ static const MachineType machineTypes_LE[] = {
 	{LE_CPU_MIPS_I,		"MIPS Mark I (R2000, R3000"},
 	{LE_CPU_MIPS_II,	"MIPS Mark II (R6000)"},
 	{LE_CPU_MIPS_III,	"MIPS Mark III (R4000)"},
-
-	{0, nullptr}
 };
-
-/**
- * bsearch() comparison function for MachineType.
- * @param a
- * @param b
- * @return
- */
-static int RP_C_API MachineType_compar(const void *a, const void *b)
-{
-	const uint16_t cpu1 = static_cast<const MachineType*>(a)->cpu;
-	const uint16_t cpu2 = static_cast<const MachineType*>(b)->cpu;
-	if (cpu1 < cpu2) return -1;
-	if (cpu1 > cpu2) return 1;
-	return 0;
-}
 
 /** Public functions **/
 
@@ -101,14 +82,13 @@ static int RP_C_API MachineType_compar(const void *a, const void *b)
 const char *lookup_pe_cpu(uint16_t cpu)
 {
 	// Do a binary search.
-	const MachineType key = {cpu, nullptr};
-	const MachineType *res =
-		static_cast<const MachineType*>(bsearch(&key,
-			machineTypes_PE,
-			ARRAY_SIZE(machineTypes_PE)-1,
-			sizeof(MachineType),
-			MachineType_compar));
-	return (res ? res->name : nullptr);
+	static const MachineType *const pMachineTypes_PE_end =
+		&machineTypes_PE[ARRAY_SIZE(machineTypes_PE)];
+	auto pPE = std::lower_bound(machineTypes_PE, pMachineTypes_PE_end, cpu,
+		[](const MachineType &pe, uint16_t cpu) {
+			return (pe.cpu < cpu);
+		});
+	return (pPE != pMachineTypes_PE_end) ? pPE->name : nullptr;
 }
 
 /**
@@ -119,14 +99,13 @@ const char *lookup_pe_cpu(uint16_t cpu)
 const char *lookup_le_cpu(uint16_t cpu)
 {
 	// Do a binary search.
-	const MachineType key = {cpu, nullptr};
-	const MachineType *res =
-		static_cast<const MachineType*>(bsearch(&key,
-			machineTypes_LE,
-			ARRAY_SIZE(machineTypes_LE)-1,
-			sizeof(MachineType),
-			MachineType_compar));
-	return (res ? res->name : nullptr);
+	static const MachineType *const pMachineTypes_LE_end =
+		&machineTypes_LE[ARRAY_SIZE(machineTypes_LE)];
+	auto pLE = std::lower_bound(machineTypes_LE, pMachineTypes_LE_end, cpu,
+		[](const MachineType &pe, uint16_t cpu) {
+			return (pe.cpu < cpu);
+		});
+	return (pLE != pMachineTypes_LE_end) ? pLE->name : nullptr;
 }
 
 } }

src/libromdata/data/NESMappers.cpp
@@ -73,7 +73,7 @@ enum class NESMirroring : uint8_t {
  */
 #include "NESMappers_data.h"
 
-/** Submappers. **/
+/** Submappers **/
 
 /**
  * NES 2.0 submapper information.
@@ -332,39 +332,7 @@ const NESSubmapperEntry submappers[] = {
 	NES2_SUBMAPPER(444, mapper444_submappers),		// NC7000M multicart (MMC3-compatible)
 	NES2_SUBMAPPER(561, mapper006_submappers),		// Bung Super Game Doctor
 	NES2_SUBMAPPER(562, mapper006_submappers),		// Venus Turbo Game Doctor
-
-	{0, 0, nullptr}
 };
-
-/**
- * bsearch() comparison function for NESSubmapperInfo.
- * @param a
- * @param b
- * @return
- */
-static int RP_C_API NESSubmapperInfo_compar(const void *a, const void *b)
-{
-	uint8_t submapper1 = static_cast<const NESSubmapperInfo*>(a)->submapper;
-	uint8_t submapper2 = static_cast<const NESSubmapperInfo*>(b)->submapper;
-	if (submapper1 < submapper2) return -1;
-	if (submapper1 > submapper2) return 1;
-	return 0;
-}
-
-/**
- * bsearch() comparison function for NESSubmapperEntry.
- * @param a
- * @param b
- * @return
- */
-static int RP_C_API NESSubmapperEntry_compar(const void *a, const void *b)
-{
-	uint16_t mapper1 = static_cast<const NESSubmapperEntry*>(a)->mapper;
-	uint16_t mapper2 = static_cast<const NESSubmapperEntry*>(b)->mapper;
-	if (mapper1 < mapper2) return -1;
-	if (mapper1 > mapper2) return 1;
-	return 0;
-}
 
 /**
  * Look up an iNES mapper number.
@@ -407,27 +375,23 @@ const NESSubmapperInfo *lookup_nes2_submapper_info(int mapper, int submapper)
 	}
 
 	// Do a binary search in submappers[].
-	const NESSubmapperEntry key = { static_cast<uint16_t>(mapper), 0, nullptr };
-	const NESSubmapperEntry *const res =
-		static_cast<const NESSubmapperEntry*>(bsearch(&key,
-			submappers,
-			ARRAY_SIZE(submappers)-1,
-			sizeof(NESSubmapperEntry),
-			NESSubmapperEntry_compar));
-	if (!res || !res->info || res->info_size == 0)
+	static const NESSubmapperEntry *const pSubmappers_end =
+		&submappers[ARRAY_SIZE(submappers)];
+	auto pSubmapper = std::lower_bound(submappers, pSubmappers_end, static_cast<uint16_t>(mapper),
+		[](const NESSubmapperEntry &submapper, uint16_t mapper) {
+			return (submapper.mapper < mapper);
+		});
+	if (pSubmapper == pSubmappers_end || !pSubmapper->info || pSubmapper->info_size == 0)
 		return nullptr;
 
-	// Do a binary search in res->info.
-	const NESSubmapperInfo key2 = {
-		static_cast<uint8_t>(submapper), 0, 0,
-		nullptr, NESMirroring::Unknown
-	};
-	const NESSubmapperInfo *const res2 =
-		static_cast<const NESSubmapperInfo*>(bsearch(&key2,
-			res->info, res->info_size,
-			sizeof(NESSubmapperInfo),
-			NESSubmapperInfo_compar));
-	return res2;
+	// Do a binary search in pSubmapper->info.
+	const NESSubmapperInfo *const pInfo_end =
+		&pSubmapper->info[pSubmapper->info_size];
+	auto pSubmapperInfo = std::lower_bound(pSubmapper->info, pInfo_end, static_cast<uint8_t>(submapper),
+		[](const NESSubmapperInfo &submapperInfo, uint8_t submapper) {
+			return (submapperInfo.submapper < submapper);
+		});
+	return (pSubmapperInfo != pInfo_end) ? pSubmapperInfo : nullptr;
 }
 
 /**

src/libromdata/data/Nintendo3DSFirmData.cpp
@@ -60,24 +60,7 @@ static const FirmBin_t firmBins[] = {
 	{0xF5D833A2, {2,51, 2}, {11,1}, true},
 	{0xFA7997F7, {2,56, 0}, {11,12}, false},
 	{0xFFA6777A, {2,48, 3}, { 9,3}, true},
-
-	{0, {0,0,0}, {0,0}, false}
 };
-
-/**
- * Comparison function for bsearch().
- * @param a
- * @param b
- * @return
- */
-static int RP_C_API compar(const void *a, const void *b)
-{
-	uint32_t crc_1 = static_cast<const FirmBin_t*>(a)->crc;
-	uint32_t crc_2 = static_cast<const FirmBin_t*>(b)->crc;
-	if (crc_1 < crc_2) return -1;
-	if (crc_1 > crc_2) return 1;
-	return 0;
-}
 
 /** Public functions **/
 
@@ -89,12 +72,13 @@ static int RP_C_API compar(const void *a, const void *b)
 const FirmBin_t *lookup_firmBin(const uint32_t crc)
 {
 	// Do a binary search.
-	const FirmBin_t key = {crc, {0,0,0}, {0,0}, false};
-	return static_cast<const FirmBin_t*>(bsearch(&key,
-			firmBins,
-			ARRAY_SIZE(firmBins)-1,
-			sizeof(FirmBin_t),
-			compar));
+	static const FirmBin_t *const pFirmBins_end =
+		&firmBins[ARRAY_SIZE(firmBins)];
+	auto pFirmBin = std::lower_bound(firmBins, pFirmBins_end, crc,
+		[](const FirmBin_t &firmBin, uint32_t crc) {
+			return (firmBin.crc < crc);
+		});
+	return (pFirmBin != pFirmBins_end) ? pFirmBin : nullptr;
 }
 
 } }

src/libromdata/data/WiiSystemMenuVersion.cpp
@@ -49,25 +49,7 @@ static const SysVersionEntry_t sysVersionList[] = {
 	// NOTE 2: vWii also has 512, 513, and 514.
 	{544, "4.3J"}, {545, "4.3U"}, {546, "4.3E"},
 	{608, "4.3J"}, {609, "4.3U"}, {610, "4.3E"},
-
-	// End of list.
-	{0, ""}
 };
-
-/**
- * Comparison function for bsearch().
- * @param a
- * @param b
- * @return
- */
-static int RP_C_API compar(const void *a, const void *b)
-{
-	unsigned int v1 = static_cast<const SysVersionEntry_t*>(a)->version;
-	unsigned int v2 = static_cast<const SysVersionEntry_t*>(b)->version;
-	if (v1 < v2) return -1;
-	if (v1 > v2) return 1;
-	return 0;
-}
 
 /** Public functions **/
 
@@ -79,14 +61,13 @@ static int RP_C_API compar(const void *a, const void *b)
 const char *lookup(unsigned int version)
 {
 	// Do a binary search.
-	const SysVersionEntry_t key = {static_cast<uint16_t>(version), ""};
-	const SysVersionEntry_t *res =
-		static_cast<const SysVersionEntry_t*>(bsearch(&key,
-			sysVersionList,
-			ARRAY_SIZE(sysVersionList)-1,
-			sizeof(SysVersionEntry_t),
-			compar));
-	return (res ? res->str : nullptr);
+	static const SysVersionEntry_t *const pSysVersionList_end =
+		&sysVersionList[ARRAY_SIZE(sysVersionList)];
+	auto pVer = std::lower_bound(sysVersionList, pSysVersionList_end, version,
+		[](const SysVersionEntry_t &sysVersion, unsigned int version) {
+			return (sysVersion.version < version);
+		});
+	return (pVer != pSysVersionList_end) ? pVer->str : nullptr;
 }
 
 } }

src/libromdata/data/WiiUData.cpp
@@ -227,8 +227,6 @@ static const WiiUDiscPublisher disc_publishers_noregion[] = {
 	{'WGSx', '00CX'},	// Giana Sisters: Twisted Dreams - Director's Cut
 	{'WKNx', '00AY'},	// Shovel Knight
 	{'WTBx', '00EY'},	// Shakedown: Hawaii
-
-	{0, 0}
 };
 
 /**
@@ -276,24 +274,7 @@ static const WiiUDiscPublisher disc_publishers_region[] = {
 	{'BWPE', '0001'},	// Hyrule Warriors (NTSC-U)
 	{'BWPJ', '00C8'},	// Zelda Musou (NTSC-J)
 	{'BWPP', '0001'},	// Hyrule Warriors (PAL)
-
-	{0, 0}
 };
-
-/**
- * Comparison function for bsearch().
- * @param a
- * @param b
- * @return
- */
-static int RP_C_API compar(const void *a, const void *b)
-{
-	unsigned int id4_1 = static_cast<const WiiUDiscPublisher*>(a)->id4;
-	unsigned int id4_2 = static_cast<const WiiUDiscPublisher*>(b)->id4;
-	if (id4_1 < id4_2) return -1;
-	if (id4_1 > id4_2) return 1;
-	return 0;
-}
 
 /** Public functions **/
 
@@ -317,15 +298,15 @@ uint32_t lookup_disc_publisher(const char *id4)
 	key.publisher = 0;
 
 	// Do a binary search.
-	const WiiUDiscPublisher *res =
-		static_cast<const WiiUDiscPublisher*>(bsearch(&key,
-			disc_publishers_noregion,
-			ARRAY_SIZE(disc_publishers_noregion)-1,
-			sizeof(WiiUDiscPublisher),
-			compar));
-	if (res) {
+	static const WiiUDiscPublisher *const p_disc_publishers_noregion_end =
+		&disc_publishers_noregion[ARRAY_SIZE(disc_publishers_noregion)];
+	auto pPubNoRegion = std::lower_bound(disc_publishers_noregion, p_disc_publishers_noregion_end, key,
+		[](const WiiUDiscPublisher &pub, const WiiUDiscPublisher &key) {
+			return (pub.id4 < key.id4);
+		});
+	if (pPubNoRegion != p_disc_publishers_noregion_end) {
 		// Found a publisher in the region-independent list.
-		return res->publisher;
+		return pPubNoRegion->publisher;
 	}
 
 	// Check the region-specific list.
@@ -333,13 +314,13 @@ uint32_t lookup_disc_publisher(const char *id4)
 	key.id4 |= static_cast<uint8_t>(id4[3]);
 
 	// Do a binary search.
-	res = static_cast<const WiiUDiscPublisher*>(bsearch(&key,
-			disc_publishers_region,
-			ARRAY_SIZE(disc_publishers_region)-1,
-			sizeof(WiiUDiscPublisher),
-			compar));
-
-	return (res ? res->publisher : 0);
+	static const WiiUDiscPublisher *const p_disc_publishers_region_end =
+		&disc_publishers_region[ARRAY_SIZE(disc_publishers_region)];
+	auto pPubRegion = std::lower_bound(disc_publishers_region, p_disc_publishers_region_end, key,
+		[](const WiiUDiscPublisher &pub, const WiiUDiscPublisher &key) {
+			return (pub.id4 < key.id4);
+		});
+	return (pPubRegion != p_disc_publishers_region_end) ? pPubRegion->publisher : 0;
 }
 
 } }

commit 6e8850d98c10c1b34b51a551bcd056b11ea098e0

Fix std::lower_bound() uses to verify the search key.
Unlike bsearch(), std::lower_bound() returns the *lower bound* of the
search results, so if the exact search key isn't found, it'll return
the next available item.

I was testing adding messages to http-status and it was returning "OK"
for [104,200]; obviously this message is only correct for 200. Adding
the extra check fixes it.

This fixes a regression from commit 7c7cdd278986163ba6a5ef6ed6c07682296526ea.
(Use std::lower_bound() instead of bsearch().)

Code size difference: (64-bit Gentoo Linux, gcc-12.2.0, release build, no LTO)

   text    data     bss     dec     hex filename
1753821  120416     704 1874941  1c9bfd libromdata.so.1.0 [before]
1754013  120416     704 1875133  1c9cbd libromdata.so.1.0 [after]
   +192       0       0    +192     +c0 Difference

   text    data     bss     dec     hex filename
  32808    1344      72   34224    85b0 rp-download [before]
  32808    1344      72   34224    85b0 rp-download [after]
      0       0       0       0       0 Difference

The increase here is less than the decrease from adding std::lower_bound()
so it's still a net win.

src/libromdata/data/AmiiboData.cpp
@@ -563,19 +563,19 @@ const char *AmiiboData::lookup_char_name(uint32_t char_id) const
 	// Do a binary search.
 	//const CharTableEntry key = {id, 0};
 	const CharTableEntry *const pCharTblEnd = d->pCharTbl + d->charTbl_count;
-	auto pCTEntry = std::lower_bound(d->pCharTbl, pCharTblEnd, (id & ~CHARTABLE_VARIANT_FLAG),
+	auto pCTEntry = std::lower_bound(d->pCharTbl, pCharTblEnd, id,
 		[](const CharTableEntry &entry, uint32_t id2) {
 			uint32_t id1 = le32_to_cpu(entry.char_id) & ~CHARTABLE_VARIANT_FLAG;
 			return (id1 < id2);
 		});
-	if (pCTEntry == pCharTblEnd) {
+	if (pCTEntry == pCharTblEnd || (le32_to_cpu(pCTEntry->char_id) & ~CHARTABLE_VARIANT_FLAG) != id) {
 		// Character ID not found.
 		return nullptr;
 	}
 
 	// Check for variants.
 	const char *name = nullptr;
-	if (le32_to_cpu(pCTEntry->char_id) & CHARTABLE_VARIANT_FLAG) {
+	if (pCTEntry->char_id & cpu_to_le32(CHARTABLE_VARIANT_FLAG)) {
 		// Do a binary search in the character variant table.
 		const uint16_t cv_char_id = ((char_id >> 16) & 0xFFFF);
 		const uint8_t variant_id = (char_id >> 8) & 0xFF;
@@ -592,7 +592,10 @@ const char *AmiiboData::lookup_char_name(uint32_t char_id) const
 				return (key1.var_id < key2.var_id);
 			});
 
-		if (pCVTEntry != pCharVarTblEnd) {
+		if (pCVTEntry != pCharVarTblEnd &&
+		    le16_to_cpu(pCVTEntry->char_id) == cv_char_id &&
+		    pCVTEntry->var_id == variant_id)
+		{
 			// Character variant ID found.
 			name = d->strTbl_lookup(le32_to_cpu(pCVTEntry->name));
 		} else {

src/libromdata/data/ELFData.cpp
@@ -79,7 +79,10 @@ const char *lookup_cpu(uint16_t cpu)
 		[](const ELFMachineType &elf, uint16_t cpu) {
 			return (elf.cpu < cpu);
 		});
-	return (pELF != pELFMachineTypes_other_end) ? pELF->name : nullptr;
+	if (pELF == pELFMachineTypes_other_end || pELF->cpu != cpu) {
+		return nullptr;
+	}
+	return pELF->name;
 }
 
 /**

src/libromdata/data/EXEData.cpp
@@ -88,7 +88,10 @@ const char *lookup_pe_cpu(uint16_t cpu)
 		[](const MachineType &pe, uint16_t cpu) {
 			return (pe.cpu < cpu);
 		});
-	return (pPE != pMachineTypes_PE_end) ? pPE->name : nullptr;
+	if (pPE == pMachineTypes_PE_end || pPE->cpu != cpu) {
+		return nullptr;
+	}
+	return pPE->name;
 }
 
 /**
@@ -105,7 +108,10 @@ const char *lookup_le_cpu(uint16_t cpu)
 		[](const MachineType &pe, uint16_t cpu) {
 			return (pe.cpu < cpu);
 		});
-	return (pLE != pMachineTypes_LE_end) ? pLE->name : nullptr;
+	if (pLE == pMachineTypes_LE_end || pLE->cpu != cpu) {
+		return nullptr;
+	}
+	return pLE->name;
 }
 
 } }

src/libromdata/data/NESMappers.cpp
@@ -381,8 +381,11 @@ const NESSubmapperInfo *lookup_nes2_submapper_info(int mapper, int submapper)
 		[](const NESSubmapperEntry &submapper, uint16_t mapper) {
 			return (submapper.mapper < mapper);
 		});
-	if (pSubmapper == pSubmappers_end || !pSubmapper->info || pSubmapper->info_size == 0)
+	if (pSubmapper == pSubmappers_end || pSubmapper->mapper != mapper ||
+	    !pSubmapper->info || pSubmapper->info_size == 0)
+	{
 		return nullptr;
+	}
 
 	// Do a binary search in pSubmapper->info.
 	const NESSubmapperInfo *const pInfo_end =
@@ -391,7 +394,10 @@ const NESSubmapperInfo *lookup_nes2_submapper_info(int mapper, int submapper)
 		[](const NESSubmapperInfo &submapperInfo, uint8_t submapper) {
 			return (submapperInfo.submapper < submapper);
 		});
-	return (pSubmapperInfo != pInfo_end) ? pSubmapperInfo : nullptr;
+	if (pSubmapperInfo == pInfo_end || pSubmapperInfo->submapper != submapper) {
+		return nullptr;
+	}
+	return pSubmapperInfo;
 }
 
 /**

src/libromdata/data/Nintendo3DSFirmData.cpp
@@ -78,7 +78,10 @@ const FirmBin_t *lookup_firmBin(const uint32_t crc)
 		[](const FirmBin_t &firmBin, uint32_t crc) {
 			return (firmBin.crc < crc);
 		});
-	return (pFirmBin != pFirmBins_end) ? pFirmBin : nullptr;
+	if (pFirmBin == pFirmBins_end || pFirmBin->crc != crc) {
+		return nullptr;
+	}
+	return pFirmBin;
 }
 
 } }

src/libromdata/data/WiiSystemMenuVersion.cpp
@@ -67,7 +67,10 @@ const char *lookup(unsigned int version)
 		[](const SysVersionEntry_t &sysVersion, unsigned int version) {
 			return (sysVersion.version < version);
 		});
-	return (pVer != pSysVersionList_end) ? pVer->str : nullptr;
+	if (pVer == pSysVersionList_end || pVer->version != version) {
+		return nullptr;
+	}
+	return pVer->str;
 }
 
 } }

src/libromdata/data/WiiUData.cpp
@@ -291,36 +291,40 @@ static const WiiUDiscPublisher disc_publishers_region[] = {
 uint32_t lookup_disc_publisher(const char *id4)
 {
 	// Check the region-independent list first.
-	WiiUDiscPublisher key;
-	key.id4 = (static_cast<uint8_t>(id4[0]) << 24) |
-		  (static_cast<uint8_t>(id4[1]) << 16) |
-		  (static_cast<uint8_t>(id4[2]) << 8) | 'x';
-	key.publisher = 0;
+	uint32_t id4_u32 = (static_cast<uint8_t>(id4[0]) << 24) |
+	                   (static_cast<uint8_t>(id4[1]) << 16) |
+	                   (static_cast<uint8_t>(id4[2]) << 8) | 'x';
 
 	// Do a binary search.
 	static const WiiUDiscPublisher *const p_disc_publishers_noregion_end =
 		&disc_publishers_noregion[ARRAY_SIZE(disc_publishers_noregion)];
-	auto pPubNoRegion = std::lower_bound(disc_publishers_noregion, p_disc_publishers_noregion_end, key,
-		[](const WiiUDiscPublisher &pub, const WiiUDiscPublisher &key) {
-			return (pub.id4 < key.id4);
+	auto pPubNoRegion = std::lower_bound(disc_publishers_noregion, p_disc_publishers_noregion_end, id4_u32,
+		[](const WiiUDiscPublisher &pub, const uint32_t id4_u32) {
+			return (pub.id4 < id4_u32);
 		});
-	if (pPubNoRegion != p_disc_publishers_noregion_end) {
+	if (pPubNoRegion != p_disc_publishers_noregion_end && pPubNoRegion->id4 == id4_u32) {
 		// Found a publisher in the region-independent list.
 		return pPubNoRegion->publisher;
 	}
 
 	// Check the region-specific list.
-	key.id4 &= ~0xFF;
-	key.id4 |= static_cast<uint8_t>(id4[3]);
+	id4_u32 &= ~0xFF;
+	id4_u32 |= static_cast<uint8_t>(id4[3]);
 
 	// Do a binary search.
 	static const WiiUDiscPublisher *const p_disc_publishers_region_end =
 		&disc_publishers_region[ARRAY_SIZE(disc_publishers_region)];
-	auto pPubRegion = std::lower_bound(disc_publishers_region, p_disc_publishers_region_end, key,
-		[](const WiiUDiscPublisher &pub, const WiiUDiscPublisher &key) {
-			return (pub.id4 < key.id4);
+	auto pPubRegion = std::lower_bound(disc_publishers_region, p_disc_publishers_region_end, id4_u32,
+		[](const WiiUDiscPublisher &pub, uint32_t id4_u32) {
+			return (pub.id4 < id4_u32);
 		});
-	return (pPubRegion != p_disc_publishers_region_end) ? pPubRegion->publisher : 0;
+	if (pPubRegion != p_disc_publishers_region_end && pPubRegion->id4 == id4_u32) {
+		// Found a publisher in the region-dependent list.
+		return pPubRegion->publisher;
+	}
+
+	// Not found.
+	return 0;
 }
 
 } }

src/libromdata/data/Xbox360_STFS_ContentType.cpp
@@ -74,9 +74,10 @@ const char *lookup(uint32_t contentType)
 		[](const ContentTypeEntry &cte, uint32_t contentType) {
 			return (cte.id < contentType);
 		});
-	return (pContentType != pContentTypeList_end)
-		? dpgettext_expr(RP_I18N_DOMAIN, "Xbox360_STFS|ContentType", pContentType->contentType)
-		: nullptr;
+	if (pContentType == pContentTypeList_end || pContentType->id != contentType) {
+		return nullptr;
+	}
+	return dpgettext_expr(RP_I18N_DOMAIN, "Xbox360_STFS|ContentType", pContentType->contentType);
 }
 
 } }

src/librpbase/SystemRegion.cpp
@@ -343,7 +343,10 @@ const char *getLocalizedLanguageName(uint32_t lc)
 		[](const LangName_t &langName, uint32_t lc) {
 			return (langName.lc < lc);
 		});
-	return (pLangName != pLangNames_end) ? pLangName->name : nullptr;
+	if (pLangName == pLangNames_end || pLangName->lc != lc) {
+		return nullptr;
+	}
+	return pLangName->name;
 }
 
 /**

src/librptexture/data/GLenumStrings.cpp
@@ -540,7 +540,7 @@ const char *lookup_glEnum(unsigned int glEnum)
 		[](const OffTbl_t &entry, unsigned int glEnum) {
 			return (entry.id < glEnum);
 		});
-	if (pEntry == pGlEnum_offtbl_end || pEntry->offset == 0) {
+	if (pEntry == pGlEnum_offtbl_end || pEntry->id != glEnum || pEntry->offset == 0) {
 		return nullptr;
 	}
 	return &glEnum_strtbl[pEntry->offset];

src/libwin32common/w32err.cpp
@@ -155,7 +155,7 @@ int w32err_to_posix(DWORD w32err)
 		[](const errmap &err, DWORD w32err) {
 			return (err.w32 < w32err);
 		});
-	if (pErr != p_w32_to_posix_end) {
+	if (pErr != p_w32_to_posix_end && pErr->w32 == w32err) {
 		// Found an error code.
 		return pErr->posix;
 	}

src/rp-download/http-status.cpp
@@ -219,10 +219,13 @@ const TCHAR *http_status_string(int code)
 		[](const HttpStatusMsg_t &msg, int code) {
 			return (msg.code < code);
 		});
+	if (pHttp == pHttpStatusMsgs_end || pHttp->code != code) {
+		return nullptr;
+	}
 
 	// Putting static_assert() after the variable declarations
 	// to prevent issues with MSVC 2010.
 	static_assert(ARRAY_SIZE(httpStatusMsgs_str) == 1240, "httpStatusMsgs_str[] is the wrong size!");
 
-	return (pHttp != pHttpStatusMsgs_end) ? &httpStatusMsgs_str[pHttp->offset] : nullptr;
+	return &httpStatusMsgs_str[pHttp->offset];
 }