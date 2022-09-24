GerbilSoft/rom-properties

commit 5eeeeb5bd1867a2c6f232fd08160884bab57ce1f

[libromdata] ISO, PlayStationDisc: Added support for 2448-byte sector file formats.
RomDataFactory: Detect 2448-byte sector images.

TODO: Add 2448-byte sector image support to Sega Mega CD, Sega Saturn,
Sega Dreamcast, and PlayStation Portable. This probably isn't a major
requirement, since these systems don't generally use subchannels.

src/libromdata/Console/PlayStationDisc.cpp
@@ -50,17 +50,6 @@ class PlayStationDiscPrivate final : public RomDataPrivate
 		RP_DISABLE_COPY(PlayStationDiscPrivate)
 
 	public:
-		// Disc type.
-		enum class DiscType {
-			Unknown	= -1,
-
-			Iso2048	= 0,	// CD (2048-byte scetors) or DVD
-			Iso2352	= 1,	// CD (2352-byte sectors)
-
-			Max
-		};
-		DiscType discType;
-
 		ISO_Primary_Volume_Descriptor pvd;
 
 		// SYSTEM.CNF contents
@@ -121,7 +110,6 @@ class PlayStationDiscPrivate final : public RomDataPrivate
 
 PlayStationDiscPrivate::PlayStationDiscPrivate(PlayStationDisc *q, IRpFile *file)
 	: super(q, file)
-	, discType(DiscType::Unknown)
 	, discReader(nullptr)
 	, isoPartition(nullptr)
 	, bootExeData(nullptr)
@@ -327,7 +315,6 @@ PlayStationDisc::PlayStationDisc(IRpFile *file)
 	}
 
 	IDiscReader *discReader = nullptr;
-	PlayStationDiscPrivate::DiscType discType = PlayStationDiscPrivate::DiscType::Unknown;
 
 	// Check for a PVD with 2048-byte sectors.
 	size_t size = d->file->seekAndRead(ISO_PVD_ADDRESS_2048, &d->pvd, sizeof(d->pvd));
@@ -337,27 +324,26 @@ PlayStationDisc::PlayStationDisc(IRpFile *file)
 	}
 	if (ISO::checkPVD(reinterpret_cast<const uint8_t*>(&d->pvd)) >= 0) {
 		// Disc has 2048-byte sectors.
-		discType = PlayStationDiscPrivate::DiscType::Iso2048;
 		discReader = new DiscReader(d->file);
 	} else {
-		// Check for a PVD with 2352-byte sectors.
+		// Check for a PVD with 2352-byte or 2448-byte sectors.
+		static const unsigned int sector_sizes[] = {2352, 2448, 0};
 		CDROM_2352_Sector_t sector;
-		size_t size = d->file->seekAndRead(ISO_PVD_ADDRESS_2352, &sector, sizeof(sector));
-		if (size != sizeof(sector)) {
-			UNREF_AND_NULL_NOCHK(d->file);
-			return;
-		}
 
-		const uint8_t *const pData = cdromSectorDataPtr(&sector);
-		if (ISO::checkPVD(sector.m2xa_f1.data) >= 0) {
-			// Disc has 2352-byte sectors.
-			memcpy(&d->pvd, pData, sizeof(d->pvd));
-			discType = PlayStationDiscPrivate::DiscType::Iso2352;
-			discReader = new Cdrom2352Reader(d->file);
-		} else {
-			// Valid PVD not found.
-			UNREF_AND_NULL_NOCHK(d->file);
-			return;
+		for (const unsigned int *p = sector_sizes; *p != 0; p++) {
+			size_t size = d->file->seekAndRead(*p * ISO_PVD_LBA, &sector, sizeof(sector));
+			if (size != sizeof(d->pvd)) {
+				UNREF_AND_NULL_NOCHK(d->file);
+				return;
+			}
+
+			const uint8_t *const pData = cdromSectorDataPtr(&sector);
+			if (ISO::checkPVD(pData) >= 0) {
+				// Found the correct sector size.
+				memcpy(&d->pvd, pData, sizeof(d->pvd));
+				discReader = new Cdrom2352Reader(d->file, *p);
+				break;
+			}
 		}
 	}
 
@@ -468,7 +454,6 @@ PlayStationDisc::PlayStationDisc(IRpFile *file)
 	}
 
 	// Disc image is ready.
-	d->discType = discType;
 	d->consoleType = consoleType;
 	d->discReader = discReader;
 	d->isoPartition = isoPartition;
@@ -522,7 +507,7 @@ int PlayStationDisc::isRomSupported_static(
 	assert(pvd != nullptr);
 	if (!pvd) {
 		// Bad.
-		return static_cast<int>(PlayStationDiscPrivate::DiscType::Unknown);
+		return -1;
 	}
 
 	// PlayStation 1 and 2 discs have the system ID "PLAYSTATION".
@@ -542,7 +527,7 @@ int PlayStationDisc::isRomSupported_static(
 
 	if (pos < 0) {
 		// Not valid.
-		return static_cast<int>(PlayStationDiscPrivate::DiscType::Unknown);
+		return -1;
 	}
 
 	// Make sure the rest of the system ID is either spaces or NULLs.
@@ -558,12 +543,12 @@ int PlayStationDisc::isRomSupported_static(
 
 	if (isOK) {
 		// Valid PVD.
-		// Caller will need to check for 2048 vs. 2352.
+		// Caller will need to check for the sector size.
 		return 0;
 	}
 
 	// Not a PlayStation 1 or 2 disc.
-	return static_cast<int>(PlayStationDiscPrivate::DiscType::Unknown);
+	return -1;
 }
 
 /**
@@ -667,7 +652,7 @@ int PlayStationDisc::loadFieldData(void)
 	} else if (!d->file || !d->file->isOpen()) {
 		// File isn't open.
 		return -EBADF;
-	} else if (!d->isValid || (int)d->discType < 0) {
+	} else if (!d->isValid) {
 		// Unknown disc type.
 		return -EIO;
 	}

src/libromdata/Handheld/PSP.cpp
@@ -22,7 +22,6 @@ using namespace LibRpTexture;
 // DiscReader
 #include "cdrom_structs.h"
 #include "iso_structs.h"
-#include "disc/Cdrom2352Reader.hpp"
 #include "disc/IsoPartition.hpp"
 #include "disc/CisoPspReader.hpp"
 #include "disc/PartitionFile.hpp"

src/libromdata/Other/ISO.cpp
@@ -65,11 +65,11 @@ class ISOPrivate final : public RomDataPrivate
 		} pvd;
 
 		// Sector size.
-		// Usually 2048 or 2352.
+		// Usually 2048 or 2352. (2448 if subchannels are present)
 		unsigned int sector_size;
 
 		// Sector offset.
-		// Usually 0 (for 2048) or 16 (for 2352).
+		// Usually 0 (for 2048) or 16 (for 2352 or 2448).
 		unsigned int sector_offset;
 
 		// UDF version.
@@ -431,26 +431,31 @@ ISO::ISO(IRpFile *file)
 		d->sector_size = ISO_SECTOR_SIZE_MODE1_COOKED;
 		d->sector_offset = ISO_DATA_OFFSET_MODE1_COOKED;
 	} else {
-		// Try again using 2352-byte sectors.
+		// Try again using raw sectors: 2352, 2448
+		static const unsigned int sector_sizes[] = {2352, 2448, 0};
 		CDROM_2352_Sector_t sector;
-		size = d->file->seekAndRead(ISO_PVD_ADDRESS_2352, &sector, sizeof(sector));
-		if (size != sizeof(sector)) {
-			// Seek and/or read error.
-			UNREF_AND_NULL_NOCHK(d->file);
-			return;
+
+		for (const unsigned int *p = sector_sizes; *p != 0; p++) {
+			size_t size = d->file->seekAndRead(*p * ISO_PVD_LBA, &sector, sizeof(sector));
+			if (size != sizeof(sector)) {
+				// Unable to read the PVD.
+				UNREF_AND_NULL_NOCHK(d->file);
+				return;
+			}
+
+			const uint8_t *const pData = cdromSectorDataPtr(&sector);
+			d->discType = static_cast<ISOPrivate::DiscType>(ISO::checkPVD(pData));
+			if (d->discType > ISOPrivate::DiscType::Unknown) {
+				// Found the correct sector size.
+				memcpy(&d->pvd, pData, sizeof(d->pvd));
+				d->sector_size = *p;
+				d->sector_offset = (sector.mode == 2 ? ISO_DATA_OFFSET_MODE2_XA : ISO_DATA_OFFSET_MODE1_RAW);
+				break;
+			}
 		}
 
-		// Copy the PVD from the sector.
-		// NOTE: Sector user data area position depends on the sector mode.
-		memcpy(&d->pvd, cdromSectorDataPtr(&sector), sizeof(d->pvd));
-
-		d->discType = d->checkPVD();
-		if (d->discType > ISOPrivate::DiscType::Unknown) {
-			// Found the PVD using 2352-byte sectors.
-			d->sector_size = ISO_SECTOR_SIZE_MODE1_RAW;
-			d->sector_offset = (sector.mode == 2 ? ISO_DATA_OFFSET_MODE2_XA : ISO_DATA_OFFSET_MODE1_RAW);
-		} else {
-			// Not a PVD.
+		if (d->sector_size == 0) {
+			// Could not find a valid PVD.
 			UNREF_AND_NULL_NOCHK(d->file);
 			return;
 		}

src/libromdata/RomDataFactory.cpp
@@ -453,28 +453,35 @@ RomData *RomDataFactoryPrivate::checkISO(IRpFile *file)
 		return nullptr;
 	}
 
-	const ISO_Primary_Volume_Descriptor *pvd;
+	const ISO_Primary_Volume_Descriptor *pvd = nullptr;
 	int discType = ISO::checkPVD(sector.m1.data);
+	printf("discType for 2048: %d\n", discType);
 	if (discType >= 0) {
-		// Found a Mode 1 PVD.
+		// Found a PVD with 2048-byte sectors.
 		pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(sector.m1.data);
 	} else {
-		// Check for a CD file system with 2352-byte sectors.
-		size_t size = file->seekAndRead(ISO_PVD_ADDRESS_2352, &sector, sizeof(sector));
-		if (size != sizeof(sector)) {
-			// Unable to read the PVD.
-			return nullptr;
-		}
+		// Check for a PVD with 2352-byte or 2448-byte sectors.
+		static const unsigned int sector_sizes[] = {2352, 2448, 0};
 
-		const uint8_t *const pData = cdromSectorDataPtr(&sector);
-		discType = ISO::checkPVD(pData);
-		if (discType >= 0) {
-			// Found a Mode 2 PVD.
-			pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(pData);
-		} else {
-			// Not a supported disc.
-			return nullptr;
+		for (const unsigned int *p = sector_sizes; *p != 0; p++) {
+			size_t size = file->seekAndRead(*p * ISO_PVD_LBA, &sector, sizeof(sector));
+			if (size != sizeof(sector)) {
+				// Unable to read the PVD.
+				return nullptr;
+			}
+
+			const uint8_t *const pData = cdromSectorDataPtr(&sector);
+			if (ISO::checkPVD(pData) >= 0) {
+				// Found the correct sector size.
+				pvd = reinterpret_cast<const ISO_Primary_Volume_Descriptor*>(pData);
+				break;
+			}
 		}
+	}
+
+	if (!pvd) {
+		// Unable to get the PVD.
+		return nullptr;
 	}
 
 	// Xbox / Xbox 360

src/libromdata/disc/Cdrom2352Reader.cpp
@@ -25,7 +25,7 @@ namespace LibRomData {
 
 class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 	public:
-		Cdrom2352ReaderPrivate(Cdrom2352Reader *q);
+		Cdrom2352ReaderPrivate(Cdrom2352Reader *q, unsigned int physBlockSize = 2352);
 
 	private:
 		typedef SparseDiscReaderPrivate super;
@@ -36,7 +36,8 @@ class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 		static const uint8_t CDROM_2352_MAGIC[12];
 
 		// Physical block size.
-		static const unsigned int physBlockSize = 2352;
+		// Supported block sizes: 2352 (raw), 2448 (raw+subchan)
+		unsigned int physBlockSize;
 
 		// Number of 2352-byte blocks.
 		unsigned int blockCount;
@@ -48,25 +49,42 @@ class Cdrom2352ReaderPrivate : public SparseDiscReaderPrivate {
 const uint8_t Cdrom2352ReaderPrivate::CDROM_2352_MAGIC[12] =
 	{0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00};
 
-Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q)
+Cdrom2352ReaderPrivate::Cdrom2352ReaderPrivate(Cdrom2352Reader *q, unsigned int physBlockSize)
 	: super(q)
+	, physBlockSize(physBlockSize)
 	, blockCount(0)
 { }
 
 /** Cdrom2352Reader **/
 
 Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
-	: super(new Cdrom2352ReaderPrivate(this), file)
+	: super(new Cdrom2352ReaderPrivate(this, 2352), file)
+{
+	init();
+}
+
+Cdrom2352Reader::Cdrom2352Reader(IRpFile *file, unsigned int physBlockSize)
+	: super(new Cdrom2352ReaderPrivate(this, physBlockSize), file)
+{
+	init();
+}
+
+/**
+ * Common initialization function.
+ */
+void Cdrom2352Reader::init(void)
 {
 	if (!m_file) {
 		// File could not be ref()'d.
 		return;
 	}
 
+	RP_D(Cdrom2352Reader);
+
 	// Check the disc size.
-	// Should be a multiple of 2352.
+	// Should be a multiple of the physical block size.
 	const off64_t fileSize = m_file->size();
-	if (fileSize <= 0 || fileSize % 2352 != 0) {
+	if (fileSize <= 0 || fileSize % d->physBlockSize != 0) {
 		// Invalid disc size.
 		UNREF_AND_NULL_NOCHK(m_file);
 		m_lastError = EIO;
@@ -75,10 +93,9 @@ Cdrom2352Reader::Cdrom2352Reader(IRpFile *file)
 
 	// Disc parameters.
 	// NOTE: A 32-bit block count allows for ~8 TiB with 2048-byte sectors.
-	RP_D(Cdrom2352Reader);
-	d->blockCount = static_cast<unsigned int>(fileSize / 2352);
-	d->block_size = 2048;
-	d->disc_size = fileSize / 2352 * 2048;
+	d->blockCount = static_cast<unsigned int>(fileSize / 2352LL);
+	d->block_size = 2048U;
+	d->disc_size = fileSize / (off64_t)d->physBlockSize * 2048LL;
 
 	// Reset the disc position.
 	d->pos = 0;
@@ -176,6 +193,8 @@ int Cdrom2352Reader::readBlock(uint32_t blockIdx, int pos, void *ptr, size_t siz
 	// NOTE: We need to read the entire 2352-byte block in order to
 	// determine the data offset, since Mode 1 and Mode 2 XA have different
 	// sector layouts.
+	// NOTE 2: No changes neeed for 2448-byte mode, since subchannels are
+	// stored *after* the 2352-byte sector data.
 	CDROM_2352_Sector_t sector;
 	size_t sz_read = m_file->seekAndRead(physBlockAddr, &sector, sizeof(sector));
 	m_lastError = m_file->lastError();

src/libromdata/disc/Cdrom2352Reader.hpp
@@ -21,15 +21,34 @@ class Cdrom2352Reader : public LibRpBase::SparseDiscReader
 		 * Construct a Cdrom2352Reader with the specified file.
 		 * The file is ref()'d, so the original file can be
 		 * unref()'d by the caller afterwards.
+		 *
+		 * Defaults to 2352-byte sectors.
+		 *
 		 * @param file File to read from.
 		 */
 		explicit Cdrom2352Reader(LibRpFile::IRpFile *file);
+
+		/**
+		 * Construct a Cdrom2352Reader with the specified file.
+		 * The file is ref()'d, so the original file can be
+		 * unref()'d by the caller afterwards.
+		 *
+		 * @param file File to read from.
+		 * @param physBlockSize Sector size. (2352, 2446)
+		 */
+		explicit Cdrom2352Reader(LibRpFile::IRpFile *file, unsigned int physBlockSize);
 
 	private:
 		typedef SparseDiscReader super;
 		RP_DISABLE_COPY(Cdrom2352Reader)
 	private:
 		friend class Cdrom2352ReaderPrivate;
+
+	private:
+		/**
+		 * Common initialization function.
+		 */
+		void init(void);
 
 	public:
 		/** Disc image detection functions. **/

src/libromdata/iso_structs.h
@@ -21,8 +21,9 @@ extern "C" {
 #endif
 
 // ISO-9660 sector sizes.
-#define ISO_SECTOR_SIZE_MODE1_RAW    2352
-#define ISO_SECTOR_SIZE_MODE1_COOKED 2048
+#define ISO_SECTOR_SIZE_MODE1_COOKED		2048
+#define ISO_SECTOR_SIZE_MODE1_RAW		2352
+#define ISO_SECTOR_SIZE_MODE1_RAW_SUBCHAN	2448
 
 // Data offsets.
 #define ISO_DATA_OFFSET_MODE1_RAW    16
@@ -247,8 +248,9 @@ ASSERT_STRUCT(ISO_Primary_Volume_Descriptor, ISO_SECTOR_SIZE_MODE1_COOKED);
 #define ISO_VD_MAGIC "CD001"
 #define ISO_VD_VERSION 0x01
 #define ISO_PVD_LBA 0x10
-#define ISO_PVD_ADDRESS_2048 (ISO_PVD_LBA * ISO_SECTOR_SIZE_MODE1_COOKED)
-#define ISO_PVD_ADDRESS_2352 (ISO_PVD_LBA * ISO_SECTOR_SIZE_MODE1_RAW)
+#define ISO_PVD_ADDRESS_2048	(ISO_PVD_LBA * ISO_SECTOR_SIZE_MODE1_COOKED)
+#define ISO_PVD_ADDRESS_2352	(ISO_PVD_LBA * ISO_SECTOR_SIZE_MODE1_RAW)
+#define ISO_PVD_ADDRESS_2448	(ISO_PVD_LBA * ISO_SECTOR_SIZE_MODE1_RAW_SUBCHAN)
 #pragma pack(1)
 typedef union PACKED _ISO_Volume_Descriptor {
 	ISO_Volume_Descriptor_Header header;

commit 2a523c98e23460468da5270f77330e9ead1fe88f

[libromdata] PlayStationDisc: Fix detection of 2352/2448-byte images.
We're reading a 2352-byte sector, so we have to compare the read size
against the sector size, not the size of d->pvd (2048).

This fixes a regression from commit 5eeeeb5bd1867a2c6f232fd08160884bab57ce1f.
([libromdata] ISO, PlayStationDisc: Added support for 2448-byte sector file formats.)

This broke in v1.8.

Fixes #354: PS2 BIN/CUE CD Mode 2 images aren't detected
Reported by @DankRank.

src/libromdata/Console/PlayStationDisc.cpp
@@ -354,7 +354,7 @@ PlayStationDisc::PlayStationDisc(IRpFile *file)
 
 		for (auto p : sector_sizes) {
 			size_t size = d->file->seekAndRead(p * ISO_PVD_LBA, &sector, sizeof(sector));
-			if (size != sizeof(d->pvd)) {
+			if (size != sizeof(sector)) {
 				UNREF_AND_NULL_NOCHK(d->file);
 				return;
 			}