GerbilSoft/rom-properties

commit 5070dd7634542927b6efee5a7ced9fb4ec33e06f

[rp-download] Add sys/ with "If-Modified-Since" checking.
This will be used for update checking, once I figure out how to add a
decent updater UI to rp-config.

Unlike other cache keys, which aren't redownloaded if they exist,
files within sys/ will always be redownloaded unless the file hasn't
been modified on the server. This is done by using the "If-Modified-Since"
HTTP header.

src/rp-download/IDownloader.cpp
@@ -29,9 +29,10 @@ namespace RpDownload {
 
 IDownloader::IDownloader()
 	: m_mtime(-1)
-	, m_inProgress(false)
+	, m_if_modified_since(-1)
 	, m_maxSize(0)
 #ifdef _WIN32
+	, m_inProgress(false)
 	, m_isWinXP(false)
 #endif /* _WIN32 */
 {
@@ -41,9 +42,10 @@ IDownloader::IDownloader()
 IDownloader::IDownloader(const TCHAR *url)
 	: m_url(url)
 	, m_mtime(-1)
-	, m_inProgress(false)
+	, m_if_modified_since(-1)
 	, m_maxSize(0)
 #ifdef _WIN32
+	, m_inProgress(false)
 	, m_isWinXP(false)
 #endif /* _WIN32 */
 {
@@ -53,8 +55,12 @@ IDownloader::IDownloader(const TCHAR *url)
 IDownloader::IDownloader(const tstring &url)
 	: m_url(url)
 	, m_mtime(-1)
-	, m_inProgress(false)
+	, m_if_modified_since(-1)
 	, m_maxSize(0)
+#ifdef _WIN32
+	, m_inProgress(false)
+	, m_isWinXP(false)
+#endif /* _WIN32 */
 {
 	createUserAgent();
 }
@@ -122,6 +128,26 @@ void IDownloader::setMaxSize(size_t maxSize)
 	assert(!m_inProgress);
 	// TODO: Don't set if m_inProgress?
 	m_maxSize = maxSize;
+}
+
+/**
+ * Get the If-Modified-Since request timestamp.
+ * @return If-Modified-Since timestamp (-1 for none)
+ */
+time_t IDownloader::ifModifiedSince(void) const
+{
+	return m_if_modified_since;
+}
+
+/**
+ * Set the If-Modified-Since request timestamp.
+ * @param timestamp If-Modified-Since timestamp (-1 for none)
+ */
+void IDownloader::setIfModifiedSince(time_t timestamp)
+{
+	assert(!m_inProgress);
+	// TODO: Don't set if m_inProgress?
+	m_if_modified_since = timestamp;
 }
 
 /** Data accessors. **/

src/rp-download/CurlDownloader.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (rp-download)                      *
  * CurlDownloader.cpp: libcurl-based file downloader.                      *
  *                                                                         *
- * Copyright (c) 2016-2020 by David Korth.                                 *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -185,6 +185,15 @@ int CurlDownloader::download(void)
 	// Redirection is required for https://amiibo.life/nfc/%08X-%08X
 	// TODO: Limit the number of redirects?
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, true);
+	// Request file modification time.
+	// NOTE: Probably not needed for http...
+	curl_easy_setopt(curl, CURLOPT_FILETIME, 1L);
+
+	if (m_if_modified_since >= 0) {
+		// Add an "If-Modified-Since" header.
+		curl_easy_setopt(curl, CURLOPT_TIMEVALUE_LARGE, static_cast<curl_off_t>(m_if_modified_since));
+		curl_easy_setopt(curl, CURLOPT_TIMECONDITION, CURL_TIMECOND_IFMODSINCE);
+	}
 
 	// Header and data functions.
 	curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, parse_header);
@@ -211,7 +220,17 @@ int CurlDownloader::download(void)
 	CURLcode res = curl_easy_perform(curl);
 	switch (res) {
 		case CURLE_OK:
-			// File downloaded successfully.
+			// If the file is empty, check for a 304.
+			if (m_data.empty() && m_if_modified_since >= 0) {
+				long unmet = 0;
+				if (!curl_easy_getinfo(curl, CURLINFO_CONDITION_UNMET, &unmet) && unmet) {
+					// HTTP 304 Not Modified
+					ret = 304;
+					break;
+				}
+			}
+
+			// File downloaded successfull.
 			ret = 0;
 			break;
 

src/rp-download/IDownloader.hpp
@@ -78,6 +78,18 @@ class IDownloader
 		 */
 		void setMaxSize(size_t maxSize);
 
+		/**
+		 * Get the If-Modified-Since request timestamp.
+		 * @return If-Modified-Since timestamp (-1 for none)
+		 */
+		time_t ifModifiedSince(void) const;
+
+		/**
+		 * Set the If-Modified-Since request timestamp.
+		 * @param timestamp If-Modified-Since timestamp (-1 for none)
+		 */
+		void setIfModifiedSince(time_t timestamp);
+
 	public:
 		/** Data accessors. **/
 
@@ -130,16 +142,15 @@ class IDownloader
 		// Reference: http://andreoffringa.org/?q=uvector
 		ao::uvector<uint8_t> m_data;
 
-		// Last-Modified time.
-		time_t m_mtime;
+		time_t m_mtime;			// Last-Modified response
+		time_t m_if_modified_since;	// If-Modified-Since request
 
-		bool m_inProgress;	// Set when downloading.
-		size_t m_maxSize;	// Maximum buffer size. (0 == unlimited)
+		size_t m_maxSize;		// Maximum buffer size. (0 == unlimited)
+		std::tstring m_userAgent;	// User-Agent
 
-		// User-Agent.
-		std::tstring m_userAgent;
+		bool m_inProgress;		// Set when downloading
 #ifdef _WIN32
-		bool m_isWinXP;		// Set for Windows versions older than Vista.
+		bool m_isWinXP;			// Set for Windows versions older than Vista
 #endif /* _WIN32 */
 };
 

src/rp-download/WinInetDownloader.cpp
@@ -80,12 +80,27 @@ int WinInetDownloader::download(void)
 		}
 	}
 
+	tstring headers;
+	if (m_if_modified_since >= 0) {
+		// Add an "If-Modified-Since" header.
+		// FIXME: +4 is needed to avoid ERROR_INSUFFICIENT_BUFFER.
+		TCHAR szTime[INTERNET_RFC1123_BUFSIZE+4];
+		SYSTEMTIME st;
+
+		UnixTimeToSystemTime(m_if_modified_since, &st);
+		BOOL bRet = InternetTimeFromSystemTimeW(&st, INTERNET_RFC1123_FORMAT, szTime, sizeof(szTime));
+		if (bRet) {
+			headers = _T("If-Modified-Since: ");
+			headers += szTime;
+		}
+	}
+
 	// Request the URL.
 	HINTERNET hURL = InternetOpenUrl(
 		hConnection,	// hInternet
 		m_url.c_str(),	// lpszUrl (Latin-1 characters only!)
-		nullptr,	// lpszHeaders
-		0,		// dwHeaderLength
+		!headers.empty() ? headers.data() : nullptr,	// lpszHeaders
+		static_cast<DWORD>(headers.size()),	// dwHeaderLength
 		dwFlags,	// dwFlags
 		reinterpret_cast<DWORD_PTR>(this));	// dwContext
 	if (!hURL) {

src/rp-download/rp-download.cpp
@@ -475,7 +475,12 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	// - ngp:    https://rpdb.gerbilsoft.com/ngp/[key]
 	// - ngpc:   https://rpdb.gerbilsoft.com/ngpc/[key]
 	// - ws:     https://rpdb.gerbilsoft.com/ws/[key]
-	// - c64:
+	// - c64:    https://rpdb.gerbilsoft.com/c64/[key]
+	// - c128:   https://rpdb.gerbilsoft.com/c128/[key]
+	// - cbmII:  https://rpdb.gerbilsoft.com/cbmII/[key]
+	// - vic20:  https://rpdb.gerbilsoft.com/vic20/[key]
+	// - plus4:  https://rpdb.gerbilsoft.com/plus4/[key]
+	// - sys:    https://rpdb.gerbilsoft.com/sys/[key] [system info, e.g. update version]
 	const TCHAR *slash_pos = _tcschr(cache_key, _T('/'));
 	if (slash_pos == nullptr || slash_pos == cache_key ||
 		slash_pos[1] == '\0')
@@ -501,9 +506,19 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 		SHOW_ERROR(_T("Cache key '%s' is invalid."), cache_key);
 		return EXIT_FAILURE;
 	}
-	if (_tcscmp(lastdot, _T(".png")) != 0 &&
-	    _tcscmp(lastdot, _T(".jpg")) != 0)
+	if (!_tcscmp(lastdot, _T(".png")) != 0 ||
+	    !_tcscmp(lastdot, _T(".jpg")) != 0)
 	{
+		// Image file extension is supported.
+	}
+	else if (!_tcscmp(lastdot, _T(".txt")))
+	{
+		// .txt is supported for sys/ only.
+		if (_tcsncmp(cache_key, _T("sys/"), 4) != 0) {
+			SHOW_ERROR(_T("Cache key '%s' is invalid."), cache_key);
+			return EXIT_FAILURE;
+		}
+	} else {
 		// Not a supported file extension.
 		SHOW_ERROR(_T("Cache key '%s' is invalid."), cache_key);
 		return EXIT_FAILURE;
@@ -516,6 +531,7 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 
 	// Determine the full URL based on the cache key.
 	bool ok = false;
+	bool check_newer = false;	// for [sys]: always check, but only download if newer
 	TCHAR full_url[256];
 	if ((prefix_len == 3 && (!_tcsncmp(cache_key, _T("wii"), 3) || !_tcsncmp(cache_key, _T("3ds"), 3))) ||
 	    (prefix_len == 4 && !_tcsncmp(cache_key, _T("wiiu"), 4)) ||
@@ -560,6 +576,11 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 				    !_tcsncmp(cache_key, _T("c64"), 3))
 				{
 					ok = true;
+				}
+				else if (!_tcsncmp(cache_key, _T("sys"), 3))
+				{
+					ok = true;
+					check_newer = true;
 				}
 				break;
 			case 4:
@@ -633,15 +654,15 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 
 	// Get the cache file information.
 	off64_t filesize = 0;
-	time_t filemtime = 0;
+	time_t filemtime = -1;
 	int ret = get_file_size_and_mtime(cache_filename.c_str(), &filesize, &filemtime);
 	if (ret == 0) {
 		// Check if the file is 0 bytes.
 		// TODO: How should we handle errors?
-		if (filesize == 0) {
-			// File is 0 bytes, which indicates it didn't exist
-			// on the server. If the file is older than a week,
-			// try to redownload it.
+		if (filesize == 0 && !check_newer) {
+			// File is 0 bytes, which indicates it didn't exist on the server.
+			// If the file is older than a week, try to redownload it.
+			// NOTE: Not used for "check_newer" files, e.g. "sys/".
 			// TODO: Configurable time.
 			const time_t systime = time(nullptr);
 			if ((systime - filemtime) < (86400*7)) {
@@ -663,15 +684,17 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 		} else if (filesize > 0) {
 			// File is larger than 0 bytes, which indicates
 			// it was previously cached successfully
-			if (likely(!force)) {
-				SHOW_INFO(_T("Cache file for '%s' is already downloaded."), cache_key);
-				return EXIT_SUCCESS;
-			} else {
+			if (unlikely(check_newer)) {
+				SHOW_INFO(_T("Cache file for '%s' is already downloaded, but this cache key is set to download-if-newer."), cache_key);
+			} else if (unlikely(force)) {
 				SHOW_INFO(_T("Cache file for '%s' is already downloaded, but -f was specified. Redownloading anyway."), cache_key);
 				if (_tremove(cache_filename.c_str()) != 0) {
 					SHOW_ERROR(_T("Error deleting cache file for '%s': %s"), cache_key, _tcserror(errno));
 					return EXIT_FAILURE;
 				}
+			} else {
+				SHOW_INFO(_T("Cache file for '%s' is already downloaded."), cache_key);
+				return EXIT_SUCCESS;
 			}
 		}
 	} else if (ret == -ENOENT) {
@@ -696,17 +719,14 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	unique_ptr<IDownloader> m_downloader(new CurlDownloader());
 #endif /* _WIN32 */
 
-	// Open the cache file now so we can use it as a negative hit
-	// if the download fails.
-	FILE *f_out = _tfopen(cache_filename.c_str(), _T("wb"));
-	if (!f_out) {
-		// Error opening the cache file.
-		SHOW_ERROR(_T("Error writing to cache file: %s"), _tcserror(errno));
-		return EXIT_FAILURE;
-	}
-
 	// TODO: Configure this somewhere?
 	m_downloader->setMaxSize(4*1024*1024);
+
+	if (check_newer && filemtime >= 0) {
+		// Only download if the file on the server is newer than
+		// what's in our cache directory.
+		m_downloader->setIfModifiedSince(filemtime);
+	}
 
 	m_downloader->setUrl(full_url);
 	ret = m_downloader->download();
@@ -716,6 +736,15 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 			if (ret < 0) {
 				// POSIX error code
 				show_error(_T("Error downloading file: %s"), _tcserror(-ret));
+				// Create a 0-byte file to indicate an error occurred.
+				FILE *f_out = _tfopen(cache_filename.c_str(), _T("wb"));
+				if (f_out) {
+					fclose(f_out);
+				}
+			} else if (ret == 304 && check_newer) {
+				// HTTP 304 Not Modified
+				SHOW_ERROR(_T("File has not been modified on the server. Not redownloading."));
+				return EXIT_SUCCESS;
 			} else /*if (ret > 0)*/ {
 				// HTTP status code
 				const TCHAR *msg = http_status_string(ret);
@@ -724,16 +753,26 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 				} else {
 					show_error(_T("Error downloading file: HTTP %d"), ret);
 				}
+				// Create a 0-byte file to indicate an error occurred.
+				FILE *f_out = _tfopen(cache_filename.c_str(), _T("wb"));
+				if (f_out) {
+					fclose(f_out);
+				}
 			}
 		}
-		fclose(f_out);
 		return EXIT_FAILURE;
 	}
 
 	if (m_downloader->dataSize() <= 0) {
 		// No data downloaded...
 		SHOW_ERROR(_T("Error downloading file: 0 bytes received"));
-		fclose(f_out);
+		return EXIT_FAILURE;
+	}
+
+	FILE *f_out = _tfopen(cache_filename.c_str(), _T("wb"));
+	if (!f_out) {
+		// Error opening the cache file.
+		SHOW_ERROR(_T("Error writing to cache file: %s"), _tcserror(errno));
 		return EXIT_FAILURE;
 	}
 

commit 13fd3537744c79df20c6abab3b09fc96e372a25c

[rp-download] IDownloader: Don't initialize m_inProgress in the #ifdef _WIN32 block.
m_inProgress should be initialized regardless of OS.

This fixes some broken assertions on m_inProgress when setting the
maximum filesize in some cases. (In my testing, it happened on files
with long Japanese names.)

This fixes a regression from commit 5070dd7634542927b6efee5a7ced9fb4ec33e06f.
([rp-download] Add sys/ with "If-Modified-Since" checking.)

src/rp-download/IDownloader.cpp
@@ -31,8 +31,8 @@ IDownloader::IDownloader()
 	: m_mtime(-1)
 	, m_if_modified_since(-1)
 	, m_maxSize(0)
-#ifdef _WIN32
 	, m_inProgress(false)
+#ifdef _WIN32
 	, m_isWinXP(false)
 #endif /* _WIN32 */
 {
@@ -44,8 +44,8 @@ IDownloader::IDownloader(const TCHAR *url)
 	, m_mtime(-1)
 	, m_if_modified_since(-1)
 	, m_maxSize(0)
-#ifdef _WIN32
 	, m_inProgress(false)
+#ifdef _WIN32
 	, m_isWinXP(false)
 #endif /* _WIN32 */
 {
@@ -57,8 +57,8 @@ IDownloader::IDownloader(const tstring &url)
 	, m_mtime(-1)
 	, m_if_modified_since(-1)
 	, m_maxSize(0)
-#ifdef _WIN32
 	, m_inProgress(false)
+#ifdef _WIN32
 	, m_isWinXP(false)
 #endif /* _WIN32 */
 {