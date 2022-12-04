ed42d667db45a136f2a4adbb4e2c11d988b8b1bb
David Korth <gerbilsoft@gerbilsoft.com>
2022-12-04 13:17:31 -0500

    [win32] CreateThumbnail: Implement isMetered().
    Compile-tested and run on Windows 7. I don't have a Win10 v2004+ system
    to actually test the isMetered() functionality, though...

--- src/win32/CreateThumbnail.cpp
+++ src/win32/CreateThumbnail.cpp
@@ -119,6 +119,87 @@ int CreateThumbnail::getImgClassSize(const HBITMAP &imgClass, ImgSize *pOutSize)
 	return -1;
 }
 
+/**
+ * Is the system using a metered connection?
+ *
+ * Note that if the system doesn't support identifying if the
+ * connection is metered, it will be assumed that the network
+ * connection is unmetered.
+ *
+ * @return True if metered; false if not.
+ */
+bool CreateThumbnail::isMetered(void)
+{
+	// from nldef.h
+	typedef enum _NL_NETWORK_CONNECTIVITY_LEVEL_HINT {
+		NetworkConnectivityLevelHintUnknown = 0,
+		NetworkConnectivityLevelHintNone,
+		NetworkConnectivityLevelHintLocalAccess,
+		NetworkConnectivityLevelHintInternetAccess,
+		NetworkConnectivityLevelHintConstrainedInternetAccess,
+		NetworkConnectivityLevelHintHidden
+	} NL_NETWORK_CONNECTIVITY_LEVEL_HINT;
+
+	typedef enum _NL_NETWORK_CONNECTIVITY_COST_HINT {
+		NetworkConnectivityCostHintUnknown = 0,
+		NetworkConnectivityCostHintUnrestricted,
+		NetworkConnectivityCostHintFixed,
+		NetworkConnectivityCostHintVariable
+	} NL_NETWORK_CONNECTIVITY_COST_HINT;
+
+	typedef struct _NL_NETWORK_CONNECTIVITY_HINT {
+		NL_NETWORK_CONNECTIVITY_LEVEL_HINT ConnectivityLevel;
+		NL_NETWORK_CONNECTIVITY_COST_HINT  ConnectivityCost;
+		BOOLEAN                            ApproachingDataLimit;
+		BOOLEAN                            OverDataLimit;
+		BOOLEAN                            Roaming;
+	} NL_NETWORK_CONNECTIVITY_HINT;
+
+	// FIXME: Can't use NTSTATUS here, so use LONG instead.
+#define NTSTATUS LONG
+	typedef NTSTATUS (WINAPI *PFNGETNETWORKCONNECTIVITYHINT)(_Out_ NL_NETWORK_CONNECTIVITY_HINT *ConnectivityHint);
+
+	// Default to unmetered if we can't determine the setting.
+	bool bRet = false;
+
+	// GetNetworkConnectivityHint() was added in Windows 10 v2004.
+	// Reference: https://learn.microsoft.com/en-us/windows/win32/api/netioapi/nf-netioapi-getnetworkconnectivityhint
+	// TODO: Cache this?
+	HMODULE hIPhlpapiDll = LoadLibraryEx(_T("IPHLPAPI.DLL"), nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+	if (!hIPhlpapiDll) {
+		return bRet;
+	}
+
+	PFNGETNETWORKCONNECTIVITYHINT pfnGetNetworkConnectivityHint =
+		reinterpret_cast<PFNGETNETWORKCONNECTIVITYHINT>(GetProcAddress(hIPhlpapiDll, "GetNetworkConnectivityHint"));
+	if (!pfnGetNetworkConnectivityHint) {
+		FreeLibrary(hIPhlpapiDll);
+		return bRet;
+	}
+
+	NL_NETWORK_CONNECTIVITY_HINT connectivityHint;
+	if (pfnGetNetworkConnectivityHint(&connectivityHint) == NO_ERROR) {
+		// Obtained the network connectivity hint.
+		switch (connectivityHint.ConnectivityCost) {
+			default:
+			case NetworkConnectivityCostHintUnknown:
+			case NetworkConnectivityCostHintUnrestricted:
+				// Unmetered connection, or unknown
+				bRet = false;
+				break;
+
+			case NetworkConnectivityCostHintFixed:
+			case NetworkConnectivityCostHintVariable:
+				// Metered connection
+				bRet = true;
+				break;
+		}
+	}
+
+	FreeLibrary(hIPhlpapiDll);
+	return bRet;
+}
+
 /** CreateThumbnailNoAlpha **/
 
 /**
