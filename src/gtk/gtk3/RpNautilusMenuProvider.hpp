f2f4aae9ea4f6f9acb13e744bdfcf94e09708a74
David Korth <gerbilsoft@gerbilsoft.com>
2022-12-09 20:37:59 -0500

    [gtk3] RpNautilusMenuProvider: Added a "Convert to PNG" menu item.
    Basically the same code as the Thunar version, but modified for Nautilus.
    
    Most of the interfaces are similar, but different. I could probably
    consolidate the code a bit...

---
+++ src/gtk/gtk3/RpNautilusMenuProvider.hpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * ROM Properties Page shell extension. (GTK+ 3.x)                            *
+ * RpNautilusMenuProvider.cpp: Nautilus (and forks) Menu Provider Definition. *
+ *                                                                            *
+ * Copyright (c) 2017-2022 by David Korth.                                    *
+ * SPDX-License-Identifier: GPL-2.0-or-later                                  *
+ ******************************************************************************/
+
+#ifndef __ROMPROPERTIES_GTK3_RPNAUTILUSMENUPROVIDER_HPP__
+#define __ROMPROPERTIES_GTK3_RPNAUTILUSMENUPROVIDER_HPP__
+
+#include "RpNautilusPlugin.hpp"
+
+G_BEGIN_DECLS
+
+typedef struct _RpNautilusMenuProviderClass	RpNautilusMenuProviderClass;
+typedef struct _RpNautilusMenuProvider		RpNautilusMenuProvider;
+
+#define TYPE_RP_NAUTILUS_MENU_PROVIDER			(rp_nautilus_menu_provider_get_type())
+#define RP_NAUTILUS_MENU_PROVIDER(obj)			(G_TYPE_CHECK_INSTANCE_CAST((obj), TYPE_RP_NAUTILUS_MENU_PROVIDER, RpNautilusMenuProvider))
+#define RP_NAUTILUS_MENU_PROVIDER_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST((klass),  TYPE_RP_NAUTILUS_MENU_PROVIDER, RpNautilusMenuProviderClass))
+#define IS_RP_NAUTILUS_MENU_PROVIDER(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), TYPE_RP_NAUTILUS_MENU_PROVIDER))
+#define IS_RP_NAUTILUS_MENU_PROVIDER_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE((klass),  TYPE_RP_NAUTILUS_MENU_PROVIDER))
+#define RP_NAUTILUS_MENU_PROVIDER_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS((obj),  TYPE_RP_NAUTILUS_MENU_PROVIDER, RpNautilusMenuProviderClass))
+
+/* these two functions are implemented automatically by the G_DEFINE_DYNAMIC_TYPE macro */
+GType		rp_nautilus_menu_provider_get_type		(void) G_GNUC_CONST G_GNUC_INTERNAL;
+/* NOTE: G_DEFINE_DYNAMIC_TYPE() declares the actual function as static. */
+void		rp_nautilus_menu_provider_register_type_ext	(GTypeModule *module) G_GNUC_INTERNAL;
+
+G_END_DECLS
+
+#endif /* !__ROMPROPERTIES_GTK3_RPNAUTILUSMENUPROVIDER_HPP__ */
--- src/gtk/gtk3/nautilus-extension-mini.h
+++ src/gtk/gtk3/nautilus-extension-mini.h
@@ -17,12 +17,28 @@ G_BEGIN_DECLS
 // Struct definitions from libnautilus-extension.
 // GTK3 version; Nautilus 42 and older.
 
+// N.B.: This interface is removed in Nautilus 43 (GTK4).
+// Replaced by NautilusPropertiesModelProviderInterface.
 struct _NautilusPropertyPageProviderInterface {
 	GTypeInterface g_iface;
 
 	GList *(*get_pages) (NautilusPropertyPageProvider *provider,
 	                     GList                        *files);
 };
+typedef struct _NautilusPropertyPageProviderInterface NautilusPropertyPageProviderInterface;
+
+// N.B.: This interface is changed in Nautilus 43 (GTK4).
+struct _NautilusMenuProviderInterface {
+	GTypeInterface g_iface;
+
+	GList *(*get_file_items)       (NautilusMenuProvider *provider,
+					GtkWidget            *window,
+					GList                *files);
+	GList *(*get_background_items) (NautilusMenuProvider *provider,
+					GtkWidget            *window,
+					NautilusFileInfo     *current_folder);
+};
+typedef struct _NautilusMenuProviderInterface NautilusMenuProviderInterface;
 
 G_END_DECLS
 
