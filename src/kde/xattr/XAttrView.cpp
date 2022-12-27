594297a1039c14c053d38c680268311e4e4ef918
David Korth <gerbilsoft@gerbilsoft.com>
2022-12-26 20:32:04 -0500

    [kde] XAttrView: Factor out file opening and pass a file descriptor to the attribute loading functions.

--- src/kde/xattr/XAttrView.cpp
+++ src/kde/xattr/XAttrView.cpp
@@ -50,15 +50,17 @@ class XAttrViewPrivate
 	private:
 		/**
 		 * Load Linux attributes, if available.
+		 * @param fd File descriptor of the open file
 		 * @return 0 on success; negative POSIX error code on error.
 		 */
-		int loadLinuxAttrs(void);
+		int loadLinuxAttrs(int fd);
 
 		/**
 		 * Load MS-DOS attributes, if available.
+		 * @param fd File descriptor of the open file
 		 * @return 0 on success; negative POSIX error code on error.
 		 */
-		int loadDosAttrs(void);
+		int loadDosAttrs(int fd);
 
 	public:
 		/**
@@ -76,61 +78,16 @@ class XAttrViewPrivate
 
 /**
  * Load Linux attributes, if available.
+ * @param fd File descriptor of the open file
  * @return 0 on success; negative POSIX error code on error.
  */
-int XAttrViewPrivate::loadLinuxAttrs(void)
+int XAttrViewPrivate::loadLinuxAttrs(int fd)
 {
 	// Hide by default.
 	// If we do have attributes, we'll show the widgets there.
 	ui.grpLinuxAttributes->hide();
 
-	// TODO: Handle non-local QUrls?
-	if (filename.scheme().isEmpty() || filename.isLocalFile()) {
-		// Local URL. We'll allow it.
-	} else {
-		// Not a local URL. Clear the display widgets.
-		return -ENOTSUP;
-	}
-
-	const string s_local_filename = filename.toLocalFile().toUtf8().constData();
-
 #ifdef __gnu_linux__
-	// Make sure this is a regular file.
-	// TODO: Use statx() if available.
-	struct stat sb;
-	errno = 0;
-	if (!stat(s_local_filename.c_str(), &sb) && !S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) {
-		// stat() failed, or this is neither a regular file nor a directory.
-		int err = -errno;
-		if (err == 0) {
-			err = -ENOTSUP;
-		}
-		return err;
-	}
-
-	// Open the file to get attributes.
-	// TODO: Move this to librpbase or libromdata,
-	// and add configure checks for HAVE_EXT2_IOCTLS.
-	// (e2fsprogs enables HAVE_EXT2_IOCTLS on Linux only)
-	#define OPEN_FLAGS (O_RDONLY|O_NONBLOCK|O_LARGEFILE|O_NOFOLLOW)
-	int fd = open(s_local_filename.c_str(), OPEN_FLAGS);
-	if (fd < 0) {
-		// Error opening the file.
-		return -errno;
-	}
-
-	// Verify the file mode again using fstat().
-	errno = 0;
-	if (!fstat(fd, &sb) && !S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) {
-		// fstat() failed, or this is neither a regular file nor a directory.
-		int err = -errno;
-		if (err == 0) {
-			err = -ENOTSUP;
-		}
-		close(fd);
-		return err;
-	}
-
 	// Attempt to get EXT2 flags.
 	// NOTE: The ioctl is defined as using long, but the actual
 	// kernel code uses int.
@@ -145,9 +102,6 @@ int XAttrViewPrivate::loadLinuxAttrs(void)
 		// Assume this file system doesn't support them.
 		ui.linuxAttrView->clearFlags();
 	}
-
-	// Done using the file.
-	close(fd);
 	return 0;
 #else /* !__gnu_linux__ */
 	// Can't use HAVE_EXT2_IOCTLs.
@@ -157,60 +111,16 @@ int XAttrViewPrivate::loadLinuxAttrs(void)
 
 /**
  * Load MS-DOS attributes, if available.
+ * @param fd File descriptor of the open file
  * @return 0 on success; negative POSIX error code on error.
  */
-int XAttrViewPrivate::loadDosAttrs(void)
+int XAttrViewPrivate::loadDosAttrs(int fd)
 {
 	// Hide by default.
 	// If we do have attributes, we'll show the widgets there.
 	ui.grpDosAttributes->hide();
 
-	// TODO: Handle non-local QUrls?
-	if (filename.scheme().isEmpty() || filename.isLocalFile()) {
-		// Local URL. We'll allow it.
-	} else {
-		// Not a local URL. Clear the display widgets.
-		return -ENOTSUP;
-	}
-
-	const string s_local_filename = filename.toLocalFile().toUtf8().constData();
-
 #ifdef __gnu_linux__
-	// Make sure this is a regular file.
-	// TODO: Use statx() if available.
-	struct stat sb;
-	errno = 0;
-	if (!stat(s_local_filename.c_str(), &sb) && !S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) {
-		// stat() failed, or this is neither a regular file nor a directory.
-		int err = -errno;
-		if (err == 0) {
-			err = -ENOTSUP;
-		}
-		return err;
-	}
-
-	// Open the file to get attributes.
-	// TODO: Move this to librpbase or libromdata,
-	// and add configure checks for FAT_IOCTL_GET_ATTRIBUTES.
-	#define OPEN_FLAGS (O_RDONLY|O_NONBLOCK|O_LARGEFILE|O_NOFOLLOW)
-	int fd = open(s_local_filename.c_str(), OPEN_FLAGS);
-	if (fd < 0) {
-		// Error opening the file.
-		return -errno;
-	}
-
-	// Verify the file mode again using fstat().
-	errno = 0;
-	if (!fstat(fd, &sb) && !S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) {
-		// fstat() failed, or this is neither a regular file nor a directory.
-		int err = -errno;
-		if (err == 0) {
-			err = -ENOTSUP;
-		}
-		close(fd);
-		return err;
-	}
-
 	// Attempt to get MS-DOS attributes.
 	// TODO: Also check xattrs.
 	// ntfs3 has: system.dos_attrib, system.ntfs_attrib
@@ -226,9 +136,6 @@ int XAttrViewPrivate::loadDosAttrs(void)
 		// Assume this is not an MS-DOS file system.
 		ui.dosAttrView->clearAttrs();
 	}
-
-	// Done using the file.
-	close(fd);
 	return 0;
 #else /* !__gnu_linux__ */
 	// Can't use HAVE_EXT2_IOCTLs.
@@ -260,16 +167,62 @@ int XAttrViewPrivate::loadAttributes(void)
 		return -ENOTSUP;
 	}
 
-	bool hasAnyAttrs = false;
+	const string s_local_filename = filename.toLocalFile().toUtf8().constData();
 
-	int ret = loadLinuxAttrs();
+	// Make sure this is a regular file.
+	// TODO: Use statx() if available.
+	struct stat sb;
+	errno = 0;
+	if (!stat(s_local_filename.c_str(), &sb) && !S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) {
+		// stat() failed, or this is neither a regular file nor a directory.
+		int err = -errno;
+		if (err == 0) {
+			err = -ENOTSUP;
+		}
+		hasAttributes = false;
+		clearDisplayWidgets();
+		return err;
+	}
+
+	// Open the file to get attributes.
+	// TODO: Move this to librpbase or libromdata,
+	// and add configure checks for FAT_IOCTL_GET_ATTRIBUTES.
+#define OPEN_FLAGS (O_RDONLY|O_NONBLOCK|O_LARGEFILE|O_NOFOLLOW)
+	int fd = open(s_local_filename.c_str(), OPEN_FLAGS);
+	if (fd < 0) {
+		// Error opening the file.
+		hasAttributes = false;
+		clearDisplayWidgets();
+		return -errno;
+	}
+
+	// Verify the file mode again using fstat().
+	errno = 0;
+	if (!fstat(fd, &sb) && !S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) {
+		// fstat() failed, or this is neither a regular file nor a directory.
+		int err = -errno;
+		if (err == 0) {
+			err = -ENOTSUP;
+		}
+		close(fd);
+		hasAttributes = false;
+		clearDisplayWidgets();
+		return err;
+	}
+
+	// Load the attributes.
+	bool hasAnyAttrs = false;
+	int ret = loadLinuxAttrs(fd);
 	if (ret == 0) {
 		hasAnyAttrs = true;
 	}
-	ret = loadDosAttrs();
+	ret = loadDosAttrs(fd);
 	if (ret == 0) {
 		hasAnyAttrs = true;
 	}
+
+	// We don't need the file descriptor anymore.
+	close(fd);
 
 	// If we have attributes, great!
 	// If not, clear the display widgets.
