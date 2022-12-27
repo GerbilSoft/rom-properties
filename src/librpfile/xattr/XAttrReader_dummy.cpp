f85cca59be659b89cccc679b62ff72d5204c4a22
David Korth <gerbilsoft@gerbilsoft.com>
2022-12-26 23:43:48 -0500

    [librpfile] XAttrReader_dummy.cpp: Dummy implementation.

---
+++ src/librpfile/xattr/XAttrReader_dummy.cpp
@@ -0,0 +1,69 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (librpfile)                        *
+ * XAttrReader_dummy.cpp: Extended Attribute reader (dummy version)        *
+ *                                                                         *
+ * Copyright (c) 2016-2022 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "stdafx.h"
+#include "XAttrReader.hpp"
+#include "XAttrReader_p.hpp"
+
+// XAttrReader isn't used by libromdata directly,
+// so use some linker hax to force linkage.
+extern "C" {
+	extern uint8_t RP_LibRpFile_XAttrReader_impl_ForceLinkage;
+	uint8_t RP_LibRpFile_XAttrReader_impl_ForceLinkage;
+}
+
+namespace LibRpFile {
+
+/** XAttrReaderPrivate **/
+
+XAttrReaderPrivate::XAttrReaderPrivate(const char *filename)
+	: lastError(0)
+	, hasLinuxAttributes(false)
+	, hasDosAttributes(false)
+	, hasGenericXAttrs(false)
+	, linuxAttributes(0)
+	, dosAttributes(0)
+{
+	RP_UNUSED(filename);
+}
+
+/**
+ * Load Linux attributes, if available.
+ * @param fd File descriptor of the open file
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int XAttrReaderPrivate::loadLinuxAttrs(int fd)
+{
+	RP_UNUSED(fd);
+	return -ENOTSUP;
+}
+
+/**
+ * Load MS-DOS attributes, if available.
+ * @param fd File descriptor of the open file
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int XAttrReaderPrivate::loadDosAttrs(int fd)
+{
+	RP_UNUSED(fd);
+	return -ENOTSUP;
+}
+
+/**
+ * Load generic xattrs, if available.
+ * (POSIX xattr on Linux; ADS on Windows)
+ * @param fd File descriptor of the open file
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int XAttrReaderPrivate::loadGenericXattrs(int fd)
+{
+	RP_UNUSED(fd);
+	return -ENOTSUP;
+}
+
+}
