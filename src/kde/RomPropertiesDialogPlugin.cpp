ffcaccb69016b34a1a0e36cec30de3ec8c1e1964
David Korth <gerbilsoft@gerbilsoft.com>
2022-12-26 20:17:38 -0500

    Revert "[libromdata] RomDataFactory: Sort MIME types so we can use std::lower_bound()."
    This reverts commit 99031fe8f5d3e8ff92f08ff08cc8879ea0ad16ef.
    
    Not needed anymore due to splitting out XAttrView into a separate plugin.
    
    Conflicts:
    	src/kde/RomPropertiesDialogPlugin.cpp

--- src/kde/RomPropertiesDialogPlugin.cpp
+++ src/kde/RomPropertiesDialogPlugin.cpp
@@ -40,17 +40,14 @@ using std::vector;
 RomDataView *RomPropertiesDialogPlugin::createRomDataView(const KFileItem &fileItem, KPropertiesDialog *props)
 {
 	// Check if the MIME type is supported.
-	// RomDataFactory::supportedMimeTypes() returns MIME types in
-	// alphabetical order, so we can use std::lower_bound().
-	// FIXME: May break the libromdata.so.2 ABI...
-	const string u8_mimeType(fileItem.mimetype().toUtf8().constData());
+	// TODO: Rework supportedMimeTypes() to return a set to make lookups faster?
+	const string u8_mimeType = fileItem.mimetype().toUtf8().toStdString();
 	const vector<const char*> &mimeTypes = RomDataFactory::supportedMimeTypes();
-	auto iter = std::lower_bound(mimeTypes.begin(), mimeTypes.end(), u8_mimeType,
-		[](const char *check, const string &search) -> bool {
-			return (strcmp(check, search.c_str()) < 0);
+	bool match = std::any_of(mimeTypes.begin(), mimeTypes.end(),
+		[&u8_mimeType](const char *mime) {
+			return u8_mimeType.compare(mime) == 0;
 		});
-	if (iter == mimeTypes.end() || strcmp(*iter, u8_mimeType.c_str()) != 0) {
-		// MIME type is not supported.
+	if (!match) {
 		return nullptr;
 	}
 
