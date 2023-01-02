4f1faf2f4bcc4cf968c0c9020897796f6fb3ec8f
David Korth <gerbilsoft@gerbilsoft.com>
2023-01-02 04:50:38 -0500

    [gtk] Use templates for rp-gtk-enums.
    Reference: https://developer-old.gnome.org/gobject/stable/glib-mkenums.html
    
    The templates are based on the recommended templates from the
    documentation.
    
    Major changes compared to the old version:
    
    - #pragma once
    
    - rp-gtk-enum.c: #include <glib-object.h> since we can't easily
      include the generated .h file anymore. (The template doesn't know what
      the output filename is.)
    
    - The type ID variable is no longer volatile. This fixes a clang warning
      because g_once_init_enter() isn't expecting a volatile pointer.

---
+++ src/gtk/rp-gtk-enums.template.c
@@ -0,0 +1,39 @@
+/*** BEGIN file-header ***/
+
+/*** END file-header ***/
+
+/*** BEGIN file-production ***/
+#include <glib-object.h>
+
+/* enumerations from "@filename@" */
+#include "@filename@"
+/*** END file-production ***/
+
+/*** BEGIN value-header ***/
+GType
+@enum_name@_get_type (void)
+{
+  static gsize static_g_@type@_type_id;
+
+  if (g_once_init_enter (&static_g_@type@_type_id))
+    {
+      static const G@Type@Value values[] = {
+/*** END value-header ***/
+
+/*** BEGIN value-production ***/
+            { @VALUENAME@, "@VALUENAME@", "@valuenick@" },
+/*** END value-production ***/
+
+/*** BEGIN value-tail ***/
+            { 0, NULL, NULL }
+      };
+
+      GType g_@type@_type_id =
+        g_@type@_register_static (g_intern_static_string ("@EnumName@"), values);
+
+      g_once_init_leave (&static_g_@type@_type_id, g_@type@_type_id);
+    }
+  return static_g_@type@_type_id;
+}
+
+/*** END value-tail ***/
