8c9811bd2fe502c10b484e3ebea9ed0fcce78860
David Korth <gerbilsoft@gerbilsoft.com>
2023-01-01 22:55:19 -0500

    [gtk] LanguageComboBox.cpp: Minor formatting change.

--- src/gtk/LanguageComboBox.cpp
+++ src/gtk/LanguageComboBox.cpp
@@ -261,9 +261,9 @@ rp_language_combo_box_new(void)
 
 static void
 rp_language_combo_box_set_property(GObject	*object,
-			   guint	 prop_id,
-			   const GValue	*value,
-			   GParamSpec	*pspec)
+				   guint	 prop_id,
+				   const GValue	*value,
+				   GParamSpec	*pspec)
 {
 	RpLanguageComboBox *const widget = RP_LANGUAGE_COMBO_BOX(object);
 
@@ -284,9 +284,9 @@ rp_language_combo_box_set_property(GObject	*object,
 
 static void
 rp_language_combo_box_get_property(GObject	*object,
-			   guint	 prop_id,
-			   GValue	*value,
-			   GParamSpec	*pspec)
+				   guint	 prop_id,
+				   GValue	*value,
+				   GParamSpec	*pspec)
 {
 	RpLanguageComboBox *const widget = RP_LANGUAGE_COMBO_BOX(object);
 
