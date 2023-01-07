GerbilSoft/rom-properties

commit 7f17b78d132c6e2a1ad3de6d99b96e28903e1e5f

[librptexture] DirectDrawSurface: Detect BC4S and BC5S, but disable SNORM/SINT decoding.
Signed texture decoding doesn't work properly right now.

src/win32/RP_XAttrView.cpp
@@ -17,8 +17,10 @@
 #include "RP_XAttrView_p.hpp"
 #include "res/resource.h"
 
-// librpfile
+// librpbase, librpfile
+#include "librpbase/config/Config.hpp"
 #include "librpfile/xattr/XAttrReader.hpp"
+using LibRpBase::Config;
 using LibRpFile::XAttrReader;
 
 // MS-DOS and Windows attributes
@@ -312,6 +314,13 @@ IFACEMETHODIMP RP_XAttrView::Initialize(
 	// https://code.msdn.microsoft.com/windowsapps/CppShellExtPropSheetHandler-d93b49b7
 	if (!pDataObj) {
 		return E_INVALIDARG;
+	}
+
+	// Check if XAttrView is enabled.
+	const Config *const config = Config::instance();
+	if (!config->showXAttrView()) {
+		// XAttrView is disabled.
+		goto cleanup;
 	}
 
 	// TODO: Handle CFSTR_MOUNTEDVOLUME for volumes mounted on an NTFS mount point.

src/librptexture/fileformat/DirectDrawSurface.cpp
@@ -346,9 +346,11 @@ int DirectDrawSurfacePrivate::updatePixelFormat(void)
 
 			{DDPF_FOURCC_ATI1, DXGI_FORMAT_BC4_UNORM, DDS_ALPHA_MODE_STRAIGHT},
 			{DDPF_FOURCC_BC4U, DXGI_FORMAT_BC4_UNORM, DDS_ALPHA_MODE_STRAIGHT},
+			{DDPF_FOURCC_BC4S, DXGI_FORMAT_BC4_UNORM, DDS_ALPHA_MODE_STRAIGHT},
 
 			{DDPF_FOURCC_ATI2, DXGI_FORMAT_BC5_UNORM, DDS_ALPHA_MODE_STRAIGHT},
 			{DDPF_FOURCC_BC5U, DXGI_FORMAT_BC5_UNORM, DDS_ALPHA_MODE_STRAIGHT},
+			{DDPF_FOURCC_BC5S, DXGI_FORMAT_BC5_SNORM, DDS_ALPHA_MODE_STRAIGHT},
 
 			// TODO: PVRTC no-alpha formats?
 			{DDPF_FOURCC_PTC2, DXGI_FORMAT_FAKE_PVRTC_2bpp, DDS_ALPHA_MODE_STRAIGHT},
@@ -391,21 +393,21 @@ int DirectDrawSurfacePrivate::updatePixelFormat(void)
 				{DXGI_FORMAT_R8G8B8A8_UNORM,		ImageDecoder::PixelFormat::ABGR8888, 4},
 				{DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,	ImageDecoder::PixelFormat::ABGR8888, 4},
 				{DXGI_FORMAT_R8G8B8A8_UINT,		ImageDecoder::PixelFormat::ABGR8888, 4},
-				{DXGI_FORMAT_R8G8B8A8_SNORM,		ImageDecoder::PixelFormat::ABGR8888, 4},
-				{DXGI_FORMAT_R8G8B8A8_SINT,		ImageDecoder::PixelFormat::ABGR8888, 4},
+				//{DXGI_FORMAT_R8G8B8A8_SNORM,		ImageDecoder::PixelFormat::ABGR8888, 4},
+				//{DXGI_FORMAT_R8G8B8A8_SINT,		ImageDecoder::PixelFormat::ABGR8888, 4},
 
 				{DXGI_FORMAT_R16G16_TYPELESS,		ImageDecoder::PixelFormat::G16R16, 4},
 				{DXGI_FORMAT_R16G16_FLOAT,		ImageDecoder::PixelFormat::G16R16, 4},
 				{DXGI_FORMAT_R16G16_UNORM,		ImageDecoder::PixelFormat::G16R16, 4},
 				{DXGI_FORMAT_R16G16_UINT,		ImageDecoder::PixelFormat::G16R16, 4},
-				{DXGI_FORMAT_R16G16_SNORM,		ImageDecoder::PixelFormat::G16R16, 4},
-				{DXGI_FORMAT_R16G16_SINT,		ImageDecoder::PixelFormat::G16R16, 4},
+				//{DXGI_FORMAT_R16G16_SNORM,		ImageDecoder::PixelFormat::G16R16, 4},
+				//{DXGI_FORMAT_R16G16_SINT,		ImageDecoder::PixelFormat::G16R16, 4},
 
 				{DXGI_FORMAT_R8G8_TYPELESS,		ImageDecoder::PixelFormat::GR88, 2},
 				{DXGI_FORMAT_R8G8_UNORM,		ImageDecoder::PixelFormat::GR88, 2},
 				{DXGI_FORMAT_R8G8_UINT,			ImageDecoder::PixelFormat::GR88, 2},
-				{DXGI_FORMAT_R8G8_SNORM,		ImageDecoder::PixelFormat::GR88, 2},
-				{DXGI_FORMAT_R8G8_SINT,			ImageDecoder::PixelFormat::GR88, 2},
+				//{DXGI_FORMAT_R8G8_SNORM,		ImageDecoder::PixelFormat::GR88, 2},
+				//{DXGI_FORMAT_R8G8_SINT,		ImageDecoder::PixelFormat::GR88, 2},
 
 				{DXGI_FORMAT_A8_UNORM,			ImageDecoder::PixelFormat::A8, 1},
 
@@ -581,6 +583,7 @@ const rp_image *DirectDrawSurfacePrivate::loadImage(void)
 	// which use premultiplied alpha.
 
 	// TODO: Handle sRGB.
+	// TODO: Handle signed textures.
 
 	// NOTE: Mipmaps are stored *after* the main image.
 	// Hence, no mipmap processing is necessary.
@@ -612,7 +615,7 @@ const rp_image *DirectDrawSurfacePrivate::loadImage(void)
 			case DXGI_FORMAT_BC1_UNORM_SRGB:
 			case DXGI_FORMAT_BC4_TYPELESS:
 			case DXGI_FORMAT_BC4_UNORM:
-			case DXGI_FORMAT_BC4_SNORM:
+			//case DXGI_FORMAT_BC4_SNORM:
 				// 16 pixels compressed into 64 bits. (4bpp)
 				// NOTE: Width and height must be rounded to the nearest tile. (4x4)
 				expected_size = ImageSizeCalc::T_calcImageSize(
@@ -627,7 +630,7 @@ const rp_image *DirectDrawSurfacePrivate::loadImage(void)
 			case DXGI_FORMAT_BC3_UNORM_SRGB:
 			case DXGI_FORMAT_BC5_TYPELESS:
 			case DXGI_FORMAT_BC5_UNORM:
-			case DXGI_FORMAT_BC5_SNORM:
+			//case DXGI_FORMAT_BC5_SNORM:
 			case DXGI_FORMAT_BC7_TYPELESS:
 			case DXGI_FORMAT_BC7_UNORM:
 			case DXGI_FORMAT_BC7_UNORM_SRGB:
@@ -801,7 +804,7 @@ const rp_image *DirectDrawSurfacePrivate::loadImage(void)
 
 			case DXGI_FORMAT_BC4_TYPELESS:
 			case DXGI_FORMAT_BC4_UNORM:
-			case DXGI_FORMAT_BC4_SNORM:
+			//case DXGI_FORMAT_BC4_SNORM:
 				img = ImageDecoder::fromBC4(
 					ddsHeader.dwWidth, ddsHeader.dwHeight,
 					buf.get(), expected_size);
@@ -809,7 +812,7 @@ const rp_image *DirectDrawSurfacePrivate::loadImage(void)
 
 			case DXGI_FORMAT_BC5_TYPELESS:
 			case DXGI_FORMAT_BC5_UNORM:
-			case DXGI_FORMAT_BC5_SNORM:
+			//case DXGI_FORMAT_BC5_SNORM:
 				img = ImageDecoder::fromBC5(
 					ddsHeader.dwWidth, ddsHeader.dwHeight,
 					buf.get(), expected_size);

commit a8495ea3815c50c0131847e87a4f50e07e594272

[win32] RP_XAttrView::Initialize(): Move the hr/tfilename variable block to before `goto cleanup;`.
MSVC complains that tfilename might not be initialized:

src\win32\rp_xattrview.cpp(401): error C4703: potentially uninitialized
local pointer variable 'tfilename' used

This, and the previous commit for `fe`, are likely regressions from
commit 7f17b78d132c6e2a1ad3de6d99b96e28903e1e5f.
([librptexture] DirectDrawSurface: Detect BC4S and BC5S, but disable SNORM/SINT decoding.)

...because I accidentally committed the XAttrView enable check in this
commit. It should still work, but rp-config on Windows hasn't been
updated to support it yet.

src/win32/RP_XAttrView.cpp
@@ -320,6 +320,10 @@ IFACEMETHODIMP RP_XAttrView::Initialize(
 	FORMATETC fe = { CF_HDROP, NULL, DVASPECT_CONTENT, -1, TYMED_HGLOBAL };
 	STGMEDIUM stm;
 
+	HRESULT hr = E_FAIL;
+	UINT nFiles, cchFilename;
+	TCHAR *tfilename = nullptr;
+
 	// Check if XAttrView is enabled.
 	const Config *const config = Config::instance();
 	if (!config->showXAttrView()) {
@@ -340,11 +344,6 @@ IFACEMETHODIMP RP_XAttrView::Initialize(
 		return E_FAIL;
 	}
 
-	HRESULT hr = E_FAIL;
-	UINT nFiles, cchFilename;
-	TCHAR *tfilename = nullptr;
-	//const Config *config;
-
 	// Determine how many files are involved in this operation. This
 	// code sample displays the custom context menu item when only
 	// one file is selected.
@@ -362,6 +361,10 @@ IFACEMETHODIMP RP_XAttrView::Initialize(
 	}
 
 	tfilename = static_cast<TCHAR*>(malloc((cchFilename+1) * sizeof(TCHAR)));
+	if (!tfilename) {
+		hr = E_OUTOFMEMORY;
+		goto cleanup;
+	}
 	cchFilename = DragQueryFile(hDrop, 0, tfilename, cchFilename+1);
 	if (cchFilename == 0) {
 		// No filename.