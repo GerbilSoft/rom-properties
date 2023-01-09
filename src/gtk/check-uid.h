GerbilSoft/rom-properties

commit e26dd687ce7935b8e76f1e8c125f419a779acd54

Consolidate root UID checks into check-uid.h / check-uid.hpp.

src/gtk/check-uid.h
@@ -1,0 +1,32 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (GTK+ 3.x)                         *
+ * check-uid.h: CHECK_UID() macro.                                         *
+ *                                                                         *
+ * Copyright (c) 2017-2021 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#ifndef __ROMPROPERTIES_GTK_CHECK_UID_H__
+#define __ROMPROPERTIES_GTK_CHECK_UID_H__
+
+#include <unistd.h>
+#include <glib.h>
+
+// Prevent running as root.
+#define CHECK_UID() do { \
+	if (getuid() == 0 || geteuid() == 0) { \
+		g_critical("*** rom-properties-" G_LOG_DOMAIN " does not support running as root."); \
+		return; \
+	} \
+} while (0)
+
+// Prevent running as root.
+// If running as root, returns errval.
+#define CHECK_UID_RET(errval) do { \
+	if (getuid() == 0 || geteuid() == 0) { \
+		g_critical("*** rom-properties-" G_LOG_DOMAIN " does not support running as root."); \
+		return (errval); \
+	} \
+} while (0)
+
+#endif /* __ROMPROPERTIES_GTK_CHECK_UID_H__ */

src/gtk/CMakeLists.txt
@@ -61,6 +61,7 @@ SET(${PROJECT_NAME}_H
 	gtk-compat.h
 	LanguageComboBox.hpp
 	OptionsMenuButton.hpp
+	check-uid.h
 	)
 
 IF(ENABLE_ACHIEVEMENTS)

src/gtk/CreateThumbnail.cpp
@@ -7,6 +7,7 @@
  ***************************************************************************/
 
 #include "stdafx.h"
+#include "check-uid.h"
 
 // librpbase, librptexture
 using namespace LibRpBase;
@@ -273,11 +274,7 @@ G_MODULE_EXPORT int RP_C_API rp_create_thumbnail(const char *source_file, const 
 {
 	// Some of this is based on the GNOME Thumbnailer skeleton project.
 	// https://github.com/hadess/gnome-thumbnailer-skeleton/blob/master/gnome-thumbnailer-skeleton.c
-
-	if (getuid() == 0 || geteuid() == 0) {
-		g_critical("*** " G_LOG_DOMAIN " does not support running as root.");
-		return RPCT_RUNNING_AS_ROOT;
-	}
+	CHECK_UID_RET(RPCT_RUNNING_AS_ROOT);
 
 	// Make sure glib is initialized.
 	// NOTE: This is a no-op as of glib-2.36.

src/gtk/gtk3/RpNautilusPlugin.cpp
@@ -51,6 +51,7 @@ extern "C" G_MODULE_EXPORT void \
 prefix##_module_initialize(GTypeModule *module) \
 { \
 	CHECK_UID(); \
+	SHOW_INIT_MESSAGE(); \
 \
 	assert(libextension_so == NULL); \
 	if (libextension_so != NULL) { \

src/gtk/gtk3/RpThunarPlugin.cpp
@@ -64,16 +64,12 @@ extern "C" G_MODULE_EXPORT void
 thunar_extension_initialize(ThunarxProviderPlugin *plugin)
 {
 	CHECK_UID();
+	SHOW_INIT_MESSAGE();
 
 	assert(libextension_so == NULL);
 	if (libextension_so != NULL) {
 		// TODO: Reference count?
 		g_critical("*** " G_LOG_DOMAIN ": thunar_extension_initialize() called twice?");
-		return;
-	}
-
-	if (getuid() == 0 || geteuid() == 0) {
-		g_critical("*** " G_LOG_DOMAIN " does not support running as root.");
 		return;
 	}
 

src/gtk/gtk3/plugin-helper.h
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (GTK+ 3.x)                         *
  * plugin-helper.h: Plugin helper macros.                                  *
  *                                                                         *
- * Copyright (c) 2017-2020 by David Korth.                                 *
+ * Copyright (c) 2017-2021 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -13,6 +13,7 @@
 #include <dlfcn.h>
 
 #include <glib.h>
+#include "check-uid.h"
 
 G_BEGIN_DECLS
 
@@ -21,14 +22,6 @@ G_BEGIN_DECLS
 #else
 # define SHOW_INIT_MESSAGE() do { } while (0)
 #endif
-
-#define CHECK_UID() do { \
-	if (getuid() == 0 || geteuid() == 0) { \
-		g_critical("*** " G_LOG_DOMAIN " does not support running as root."); \
-		return; \
-	} \
-	SHOW_INIT_MESSAGE(); \
-} while (0)
 
 #define DLSYM(symvar, symdlopen) do { \
 	pfn_##symvar = (__typeof__(pfn_##symvar))dlsym(libextension_so, #symdlopen); \

src/gtk/thumbnailer-dbus/rp-thumbnailer-main.cpp
@@ -7,6 +7,7 @@
  ***************************************************************************/
 
 #include "common.h"
+#include "check-uid.h"
 #include "libunixcommon/userdirs.hpp"
 #include "libunixcommon/dll-search.h"
 #include "rp-thumbnailer-dbus.h"
@@ -118,12 +119,8 @@ int main(int argc, char *argv[])
 	RP_UNUSED(argc);
 	RP_UNUSED(argv);
 
-	if (getuid() == 0 || geteuid() == 0) {
-		fprintf(stderr, "*** %s does not support running as root.", argv[0]);
-		return EXIT_FAILURE;
-	}
-
 	// Enable security options.
+	CHECK_UID_RET(EXIT_FAILURE);
 	rpt_do_security_options();
 
 #if !GLIB_CHECK_VERSION(2,36,0)

src/kde/CMakeLists.txt
@@ -53,6 +53,7 @@ SET(${PROJECT_NAME}_H
 	ListDataSortProxyModel.hpp
 	LanguageComboBox.hpp
 	OptionsMenuButton.hpp
+	check-uid.hpp
 	config/ConfigDialog.hpp
 	config/ITab.hpp
 	config/ImageTypesTab.hpp

commit 3a4782f7758fb841da1f1f675f556357f46fce9d

[gtk] check-uid.h: G_LOG_DOMAIN already has "rom-properties-".
The KDE version uses RP_KDE_LOWER, which only has "kde4"/"kf5"/"kf6".
Hence, this is probably a copy/paste error.

Note that this was actually introduced in a clean-up commit that
split root checking into check-uid.h.

This fixes a regression from commit e26dd687ce7935b8e76f1e8c125f419a779acd54.
(Consolidate root UID checks into check-uid.h / check-uid.hpp.)

Bug was introduced in v1.9.

src/gtk/check-uid.h
@@ -14,7 +14,7 @@
 // Prevent running as root.
 #define CHECK_UID() do { \
 	if (getuid() == 0 || geteuid() == 0) { \
-		g_critical("*** rom-properties-" G_LOG_DOMAIN " does not support running as root."); \
+		g_critical("*** " G_LOG_DOMAIN " does not support running as root."); \
 		return; \
 	} \
 } while (0)
@@ -23,7 +23,7 @@
 // If running as root, returns errval.
 #define CHECK_UID_RET(errval) do { \
 	if (getuid() == 0 || geteuid() == 0) { \
-		g_critical("*** rom-properties-" G_LOG_DOMAIN " does not support running as root."); \
+		g_critical("*** " G_LOG_DOMAIN " does not support running as root."); \
 		return (errval); \
 	} \
 } while (0)