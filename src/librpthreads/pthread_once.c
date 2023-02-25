GerbilSoft/rom-properties

commit 4b1461b75db5e580b06b47f775825c31658cd840

[libpthreads] pthread_once.c: Use sched_yield() instead of pthread_yield().
Both are marked with version symbol GLIBC_2.2.5, indicating they're both
been supported for years, but pthread_yield is marked as deprecated as of
glibc-2.34.

src/librpthreads/pthread_once.c
@@ -57,7 +57,7 @@ int pthread_once(pthread_once_t *once_control, void (*init_routine)(void))
 					return 0;
 				default:
 					// The initializer is being processed by another thread.
-					pthread_yield();
+					sched_yield();
 					break;
 			}
 		}

commit 60825f8b4b8427c13480bc346da783a68f6f8b99

[libpthreads] pthread_once.c: #define sched_yield() on Windows instead of pthread_yield().
This fixes a Windows regression from commit 4b1461b75db5e580b06b47f775825c31658cd840.
([libpthreads] pthread_once.c: Use sched_yield() instead of pthread_yield().)

src/librpthreads/pthread_once.c
@@ -21,7 +21,7 @@
 #  define WIN32_LEAN_AND_MEAN 1
 # endif
 # include <windows.h>
-# define pthread_yield() SwitchToThread()
+# define sched_yield() SwitchToThread()
 #else
 # error Unsupported threading model.
 #endif