GerbilSoft/rom-properties

commit 86746b44000fc10d0ac5941ef534c5794d26237f

[win32] AchWin32: Initialize wndClass using a static const struct.
No code size changes, but this makes it more obvious how it's initialized.

Use RegisterClassEx() everywhere. This eliminates the need to import
the RegisterClass() function.

No code size differences, except for MessageWidget, which now initializes
a WNDCLASSEX struct instead of WNDCLASS.

Code size difference: (64-bit MinGW-w64 10.0.0, gcc-12.2.0, release build, no LTO)

   text    data     bss     dec     hex filename
   3988      16      16    4020     fb4 MessageWidget.cpp.obj [before]
   4020      16      16    4052     fd4 MessageWidget.cpp.obj [after]
    +32       0       0     +32     +20 Difference

src/win32/LanguageComboBox.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * LanguageComboBox.cpp: Language ComboBoxEx superclass.                   *
  *                                                                         *
- * Copyright (c) 2017-2022 by David Korth.                                 *
+ * Copyright (c) 2017-2023 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -417,8 +417,8 @@ void LanguageComboBoxRegister(void)
 	// Get ComboBoxEx class information.
 	// NOTE: ComboBoxEx was introduced in MSIE 3.0, so we don't
 	// need to fall back to regular ComboBox.
-	WNDCLASS wndClass;
-	BOOL bRet = GetClassInfo(nullptr, WC_COMBOBOXEX, &wndClass);
+	WNDCLASSEX wndClass;
+	BOOL bRet = GetClassInfoEx(nullptr, WC_COMBOBOXEX, &wndClass);
 	assert(bRet != FALSE);
 	if (!bRet) {
 		// Error getting class info.
@@ -430,7 +430,7 @@ void LanguageComboBoxRegister(void)
 	wndClass.hInstance = HINST_THISCOMPONENT;
 	wndClass.lpszClassName = WC_LANGUAGECOMBOBOX;
 
-	atom_languageComboBox = RegisterClass(&wndClass);
+	atom_languageComboBox = RegisterClassEx(&wndClass);
 }
 
 void LanguageComboBoxUnregister(void)

src/win32/OptionsMenuButton.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * OptionsMenuButton.cpp: Options menu button WC_BUTTON superclass.        *
  *                                                                         *
- * Copyright (c) 2017-2022 by David Korth.                                 *
+ * Copyright (c) 2017-2023 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -262,8 +262,8 @@ void OptionsMenuButtonRegister(void)
 
 	// Get Button class information.
 	// TODO: Do we need to use GetClassInfo for BUTTON?
-	WNDCLASS wndClass;
-	BOOL bRet = GetClassInfo(nullptr, WC_BUTTON, &wndClass);
+	WNDCLASSEX wndClass;
+	BOOL bRet = GetClassInfoEx(nullptr, WC_BUTTON, &wndClass);
 	assert(bRet != FALSE);
 	if (!bRet) {
 		// Error getting class info.
@@ -275,7 +275,7 @@ void OptionsMenuButtonRegister(void)
 	wndClass.hInstance = HINST_THISCOMPONENT;
 	wndClass.lpszClassName = WC_OPTIONSMENUBUTTON;
 
-	atom_optionsMenuButton = RegisterClass(&wndClass);
+	atom_optionsMenuButton = RegisterClassEx(&wndClass);
 }
 
 void OptionsMenuButtonUnregister(void)

src/win32/AchWin32.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KF5)                         *
  * AchWin32.hpp: Win32 notifications for achievements.                     *
  *                                                                         *
- * Copyright (c) 2020-2022 by David Korth.                                 *
+ * Copyright (c) 2020-2023 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -136,19 +136,20 @@ AchWin32Private::AchWin32Private()
 	// NOTE: Cannot register with the Achievements class here because the
 	// static Achievements instance might not be fully initialized yet.
 
-	WNDCLASSEX wndClass;
-	wndClass.cbSize = sizeof(wndClass);
-	wndClass.style = CS_HREDRAW | CS_VREDRAW;
-	wndClass.lpfnWndProc = RpAchNotifyWndProc;
-	wndClass.cbClsExtra = 0;
-	wndClass.cbWndExtra = 0;
-	wndClass.hInstance = HINST_THISCOMPONENT;
-	wndClass.hIcon = nullptr;
-	wndClass.hCursor = nullptr;
-	wndClass.hbrBackground = nullptr;
-	wndClass.lpszMenuName = nullptr;
-	wndClass.lpszClassName = _T("RpAchNotifyWnd");
-	wndClass.hIconSm = nullptr;
+	static const WNDCLASSEX wndClass = {
+		sizeof(WNDCLASSEX),		// cbSize
+		CS_HREDRAW | CS_VREDRAW,	// style
+		RpAchNotifyWndProc,		// lpfnWndProc
+		0,				// cbClsExtra
+		0,				// cbWndExtra
+		HINST_THISCOMPONENT,		// hInstance
+		nullptr,			// hIcon
+		nullptr,			// hCursor
+		nullptr,			// hbrBackground
+		nullptr,			// lpszMenuName
+		_T("RpAchNotifyWnd"),		// lpszClassName
+		nullptr				// hIconSm
+	};
 
 	// Register the window class.
 	atomWindowClass = RegisterClassEx(&wndClass);

src/win32/MessageWidget.cpp
@@ -468,7 +468,8 @@ void MessageWidgetRegister(void)
 	if (atom_messageWidget != 0)
 		return;
 
-	static const WNDCLASS wndClass = {
+	static const WNDCLASSEX wndClass = {
+		sizeof(WNDCLASSEX),		// cbSize
 		CS_HREDRAW | CS_VREDRAW,	// style
 		MessageWidgetWndProc,		// lpfnWndProc
 		0,				// cbClsExtra
@@ -479,9 +480,10 @@ void MessageWidgetRegister(void)
 		nullptr,			// hbrBackground
 		nullptr,			// lpszMenuName
 		WC_MESSAGEWIDGET,		// lpszClassName
+		nullptr				// hIconSm
 	};
 
-	atom_messageWidget = RegisterClass(&wndClass);
+	atom_messageWidget = RegisterClassEx(&wndClass);
 }
 
 void MessageWidgetUnregister(void)

commit 341d9569240996e19d23af2308d252b03ade8dbd

[win32] LanguageComboBox: Revert to WNDCLASS instead of WNDCLASSEX.
For some reason, WNDCLASSEX was preventing it from functioning properly.
This resulted in the "Options" tab disappearing when it was clicked.

OptionsMenuButton: Not tested, but since it used the same setup as
LanguageComboBox, this has been reverted as well.

MessageWidget still uses WNDCLASSEX but it doesn't have this problem.

This fixes a regression from commit 86746b44000fc10d0ac5941ef534c5794d26237f.
([win32] AchWin32: Initialize wndClass using a static const struct.)

src/win32/LanguageComboBox.cpp
@@ -420,8 +420,8 @@ void LanguageComboBoxRegister(void)
 	// Get ComboBoxEx class information.
 	// NOTE: ComboBoxEx was introduced in MSIE 3.0, so we don't
 	// need to fall back to regular ComboBox.
-	WNDCLASSEX wndClass;
-	BOOL bRet = GetClassInfoEx(nullptr, WC_COMBOBOXEX, &wndClass);
+	WNDCLASS wndClass;
+	BOOL bRet = GetClassInfo(nullptr, WC_COMBOBOXEX, &wndClass);
 	assert(bRet != FALSE);
 	if (!bRet) {
 		// Error getting class info.
@@ -434,7 +434,7 @@ void LanguageComboBoxRegister(void)
 	wndClass.hInstance = HINST_THISCOMPONENT;
 	wndClass.lpszClassName = WC_LANGUAGECOMBOBOX;
 
-	atom_languageComboBox = RegisterClassEx(&wndClass);
+	atom_languageComboBox = RegisterClass(&wndClass);
 }
 
 void LanguageComboBoxUnregister(void)

src/win32/OptionsMenuButton.cpp
@@ -265,8 +265,8 @@ void OptionsMenuButtonRegister(void)
 
 	// Get Button class information.
 	// TODO: Do we need to use GetClassInfo for BUTTON?
-	WNDCLASSEX wndClass;
-	BOOL bRet = GetClassInfoEx(nullptr, WC_BUTTON, &wndClass);
+	WNDCLASS wndClass;
+	BOOL bRet = GetClassInfo(nullptr, WC_BUTTON, &wndClass);
 	assert(bRet != FALSE);
 	if (!bRet) {
 		// Error getting class info.
@@ -279,7 +279,7 @@ void OptionsMenuButtonRegister(void)
 	wndClass.hInstance = HINST_THISCOMPONENT;
 	wndClass.lpszClassName = WC_OPTIONSMENUBUTTON;
 
-	atom_optionsMenuButton = RegisterClassEx(&wndClass);
+	atom_optionsMenuButton = RegisterClass(&wndClass);
 }
 
 void OptionsMenuButtonUnregister(void)