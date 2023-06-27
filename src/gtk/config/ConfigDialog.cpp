GerbilSoft/rom-properties

commit 412422ea191054f275a5d49eb78449c90574fda7

[gtk] ConfigDialog: Reworked the RP_CONFIG_TAB checks.
These are *always* going to be RpConfigTab, so we don't need to do
as many nullptr checks.

Code size differences: (64-bit Gentoo Linux, gcc-12.2.0, release build, no LTO)

   text    data     bss     dec     hex filename
   5721     288      16    6025    1789 ConfigDialog.cpp.o [XFCE] [before]
   5689     288      16    5993    1769 ConfigDialog.cpp.o [XFCE] [after]
    -32       0       0     -32     -20 Difference

   text    data     bss     dec     hex filename
   5427     232      16    5675    162b ConfigDialog.cpp.o [GTK3] [before]
   5379     232      16    5627    15fb ConfigDialog.cpp.o [GTK3] [after]
    -48       0       0     -48     -30 Difference

   text    data     bss     dec     hex filename
   5963     232      28    6223    184f ConfigDialog.cpp.o [GTK4] [before]
   5931     232      28    6191    182f ConfigDialog.cpp.o [GTK4] [after]
    -31       0       0     -32     -20 Difference

src/gtk/config/ConfigDialog.cpp
@@ -383,12 +383,10 @@ rp_config_dialog_init(RpConfigDialog *dialog)
 	gtk_widget_set_sensitive(dialog->btnReset, FALSE);
 
 	// Adjust btnDefaults for the first tab.
-	GtkWidget *const tab_0 = gtk_notebook_get_nth_page(GTK_NOTEBOOK(dialog->tabWidget), 0);
-	assert(RP_IS_CONFIG_TAB(tab_0));
-	if (RP_IS_CONFIG_TAB(tab_0)) {
-		gtk_widget_set_sensitive(dialog->btnDefaults,
-			rp_config_tab_has_defaults(RP_CONFIG_TAB(tab_0)));
-	}
+	RpConfigTab *const tab_0 = RP_CONFIG_TAB(gtk_notebook_get_nth_page(GTK_NOTEBOOK(dialog->tabWidget), 0));
+	assert(tab_0 != nullptr);
+	gtk_widget_set_sensitive(dialog->btnDefaults,
+		rp_config_tab_has_defaults(RP_CONFIG_TAB(tab_0)));
 
 	// Escape key handler
 	g_signal_connect(dialog, "close", G_CALLBACK(rp_config_dialog_close), nullptr);
@@ -476,12 +474,12 @@ rp_config_dialog_apply(RpConfigDialog *dialog)
 	// Save the settings.
 	// NOTE: Saving KeyManagerTab for later.
 #ifdef ENABLE_DECRYPTION
-	GtkWidget *tabKeyManager = nullptr;
+	RpConfigTab *tabKeyManager = nullptr;
 #endif /* ENABLE_DECRYPTION */
 	const gint n_pages = gtk_notebook_get_n_pages(GTK_NOTEBOOK(dialog->tabWidget));
 	for (gint i = 0; i < n_pages; i++) {
-		GtkWidget *const tab = gtk_notebook_get_nth_page(GTK_NOTEBOOK(dialog->tabWidget), i);
-		assert(RP_IS_CONFIG_TAB(tab));
+		RpConfigTab *const tab = RP_CONFIG_TAB(gtk_notebook_get_nth_page(GTK_NOTEBOOK(dialog->tabWidget), i));
+		assert(tab != nullptr);
 #ifdef ENABLE_DECRYPTION
 		if (RP_IS_KEY_MANAGER_TAB(tab)) {
 			// Found KeyManagerTab.
@@ -492,7 +490,7 @@ rp_config_dialog_apply(RpConfigDialog *dialog)
 		{
 			// rp_config_tab_save() checks tab's type,
 			// so no need to check RP_IS_CONFIG_TAB() here.
-			rp_config_tab_save(RP_CONFIG_TAB(tab), keyFile);
+			rp_config_tab_save(tab, keyFile);
 		}
 	}
 
@@ -537,7 +535,7 @@ rp_config_dialog_apply(RpConfigDialog *dialog)
 		assert(tabKeyManager != nullptr);
 		// rp_config_tab_save() checks tab's type,
 		// so no need to check RP_IS_CONFIG_TAB() here.
-		rp_config_tab_save(RP_CONFIG_TAB(tabKeyManager), keyFile);
+		rp_config_tab_save(tabKeyManager, keyFile);
 
 		// Commit the changes.
 		// NOTE: g_key_file_save_to_file() was added in glib-2.40.
@@ -580,9 +578,7 @@ rp_config_dialog_reset(RpConfigDialog *dialog)
 	for (int i = 0; i < num_pages; i++) {
 		RpConfigTab *const tab = RP_CONFIG_TAB(gtk_notebook_get_nth_page(tabWidget, i));
 		assert(tab != nullptr);
-		if (tab) {
-			rp_config_tab_reset(tab);
-		}
+		rp_config_tab_reset(tab);
 	}
 
 	// Disable the "Apply" and "Reset" buttons.

commit 95f5bc6e2fb16038cb8293a15b7c27453b40d637

[gtk] ConfigDialog: Fix a GTK assertion on startup in the GTK2 version.
The RpConfigTab is contained within a GtkAlignment.

This would have broken the enable/disable state of the "Defaults" button
in the GTK2 version, though since ImageTypesTab has it enabled, there's
effectively no user-visible difference (aside from the assertion in the
console).

This fixes a regression from commit 412422ea191054f275a5d49eb78449c90574fda7.
([gtk] ConfigDialog: Reworked the RP_CONFIG_TAB checks.)

Prior to this commit, the functionality *still* didn't work, but it
didn't cause a GTK assertion, either.

src/gtk/config/ConfigDialog.cpp
@@ -383,8 +383,13 @@ rp_config_dialog_init(RpConfigDialog *dialog)
 	gtk_widget_set_sensitive(dialog->btnReset, FALSE);
 
 	// Adjust btnDefaults for the first tab.
-	RpConfigTab *const tab_0 = RP_CONFIG_TAB(gtk_notebook_get_nth_page(GTK_NOTEBOOK(dialog->tabWidget), 0));
+	GtkWidget *tab_0 = gtk_notebook_get_nth_page(GTK_NOTEBOOK(dialog->tabWidget), 0);
 	assert(tab_0 != nullptr);
+#ifdef RP_USE_GTK_ALIGNMENT
+	// tab_0 is a GtkAlignment. Get the actual RP_CONFIG_TAB.
+	tab_0 = gtk_bin_get_child(GTK_BIN(tab_0));
+	assert(tab_0 != nullptr);
+#endif /* RP_USE_GTK_ALIGNMENT */
 	gtk_widget_set_sensitive(dialog->btnDefaults,
 		rp_config_tab_has_defaults(RP_CONFIG_TAB(tab_0)));
 