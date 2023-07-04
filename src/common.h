1e846228c13217208195bd68567192b82f140df6
David Korth <gerbilsoft@gerbilsoft.com>
2023-07-04 11:51:37 -0400

    common.h: Add parentheses to the non-gcc version of likely/unlikely.

--- src/common.h
+++ src/common.h
@@ -110,8 +110,8 @@
 #  define likely(x)	__builtin_expect(!!(x), 1)
 #  define unlikely(x)	__builtin_expect(!!(x), 0)
 #else
-#  define likely(x)	x
-#  define unlikely(x)	x
+#  define likely(x)	(x)
+#  define unlikely(x)	(x)
 #endif
 
 // C99 restrict macro.
