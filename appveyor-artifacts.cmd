e6bd641911439186d82ef58aa706837f608ff71a
David Korth <gerbilsoft@gerbilsoft.com>
2023-07-06 22:12:42 -0400

    appveyor-artifacts.cmd: Check %APPVEYOR_BUILD_WORKER_IMAGE% instead of %COMPILER%.
    %COMPILER% *used* to be set when using msvc2013, but it isn't set when
    building with msvc2015.

--- appveyor-artifacts.cmd
+++ appveyor-artifacts.cmd
@@ -1,11 +1,9 @@
-@ECHO ON
-echo compiler: %compiler%
-echo configuration: %configuration%
-if not "%compiler%" == "msvc2015" exit /b 0
-if not "%configuration%" == "Release" exit /b 0
+@ECHO OFF
+if not "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" exit /b 0
+if not "%CONFIGURATION%" == "Release" exit /b 0
 
 cpack --version
-cpack -C "%configuration%"
+cpack -C "%CONFIGURATION%"
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 for %%z in (*.zip) do appveyor PushArtifact "%%z"
 exit /b %ERRORLEVEL%
