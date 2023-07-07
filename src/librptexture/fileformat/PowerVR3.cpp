GerbilSoft/rom-properties

commit b050ffd8f7f94653fbdebe484e593c0415ef5d99

Various `for` loop changes, including more ranged `for` and int -> unsigned int.
AchievementsItemDelegate.cpp: Move the QFontMetrics definition out of the
loop. This may improve performance for entries with more than two lines.

[win32] DllRegisterServer.cpp, RP_ContextMenu.cpp: Use `const char*`
instead of `auto` for texture_ext.

src/librptexture/fileformat/PowerVR3.cpp
@@ -170,8 +170,6 @@ const struct PowerVR3Private::FmtLkup_t PowerVR3Private::fmtLkup_tbl_U8[] = {
 	{  '\0bgr', 0x00101010, ImageDecoder::PixelFormat::B16G16R16,		48},
 	{  '\0rgb', 0x000B0B0A, ImageDecoder::PixelFormat::R11G11B10,		32},	// NOTE: May be float.
 #endif
-
-	{0, 0, ImageDecoder::PixelFormat::Unknown, 0}
 };
 
 // Uncompressed format lookup table. (USHORT, USHORT_NORM)
@@ -182,8 +180,6 @@ const struct PowerVR3Private::FmtLkup_t PowerVR3Private::fmtLkup_tbl_U16[] = {
 	// TODO: High-bit-depth luminance.
 	{ '\0\0al', 0x00001010, ImageDecoder::PixelFormat::A16L16,	32},
 #endif
-
-	{0, 0, ImageDecoder::PixelFormat::Unknown, 0}
 };
 
 // Uncompressed format lookup table. (UINT, UINT_NORM)
@@ -197,8 +193,6 @@ const struct PowerVR3Private::FmtLkup_t PowerVR3Private::fmtLkup_tbl_U32[] = {
 	{'\0\0\0l', 0x00000020, ImageDecoder::PixelFormat::L32,		32},
 	{ '\0\0al', 0x00002020, ImageDecoder::PixelFormat::A32L32,	32},
 #endif
-
-	{0, 0, ImageDecoder::PixelFormat::Unknown, 0}
 };
 
 PowerVR3Private::PowerVR3Private(PowerVR3 *q, IRpFile *file)
@@ -324,11 +318,11 @@ const rp_image *PowerVR3Private::loadImage(int mip)
 			return nullptr;
 		}
 
-		for (const FmtLkup_t *p = fmtLkup_tbl_U8; p->pixel_format != 0; p++) {
-			if (p->pixel_format == pvr3Header.pixel_format &&
-			    p->channel_depth == pvr3Header.channel_depth)
+		for (const auto &p : fmtLkup_tbl_U8) {
+			if (p.pixel_format == pvr3Header.pixel_format &&
+			    p.channel_depth == pvr3Header.channel_depth)
 			{
-				fmtLkup = p;
+				fmtLkup = &p;
 				break;
 			}
 		}

src/gtk/RomDataView_ops.cpp
@@ -82,9 +82,8 @@ rp_rom_data_view_update_field(RpRomDataView *page, int fieldIdx)
 	// NOTE: Linear search through all display objects, since
 	// this function isn't used that often.
 	GtkWidget *widget = nullptr;
-	const auto tabs_cend = cxx->tabs.cend();
-	for (auto iter = cxx->tabs.cbegin(); iter != tabs_cend && widget == nullptr; ++iter) {
-		GtkWidget *const table = iter->table;	// GtkTable (2.x); GtkGrid (3.x)
+	for (const auto &tab : cxx->tabs) {
+		GtkWidget *const table = tab.table;	// GtkTable (2.x); GtkGrid (3.x)
 
 #if GTK_CHECK_VERSION(4,0,0)
 		// Enumerate the child widgets.

src/kde/RomDataView_ops.cpp
@@ -73,9 +73,8 @@ int RomDataViewPrivate::updateField(int fieldIdx)
 	// NOTE: Linear search through all display objects, since
 	// this function isn't used that often.
 	QObject *qObj = nullptr;
-	const auto tabs_cend = tabs.cend();
-	for (auto iter = tabs.cbegin(); iter != tabs_cend && qObj == nullptr; ++iter) {
-		QFormLayout *const form = iter->form;
+	for (const auto &tab : tabs) {
+		QFormLayout *const form = tab.form;
 		const int rowCount = form->rowCount();
 		for (int row = 0; row < rowCount && qObj == nullptr; row++) {
 			// TODO: Also check LabelRole in some cases?

src/kde/config/AchievementsItemDelegate.cpp
@@ -163,10 +163,13 @@ void AchievementsItemDelegate::paint(QPainter *painter,
 	// Text boundaries.
 	array<QRect, LINE_COUNT> v_rect;
 
+	QFontMetrics fm(fontName);
 	for (size_t i = 0; i < sl.size(); i++) {
 		// Name uses the normal font.
 		// Description lines use a slightly smaller font.
-		const QFontMetrics fm(i == 0 ? fontName : fontDesc);
+		if (i == 1) {
+			fm = QFontMetrics(fontDesc);
+		}
 		sl[i] = fm.elidedText(sl[i], Qt::ElideRight, textRect.width()-1);
 		const QRect tmpRect(textRect.x(), textRect.y() + textHeight, textRect.width(), fm.height());
 		textHeight += fm.height();
@@ -274,11 +277,14 @@ QSize AchievementsItemDelegate::sizeHint(const QStyleOptionViewItem &option,
 	const QFont fontName = d->fontName(bgOption.widget);
 	const QFont fontDesc = d->fontDesc(bgOption.widget);
 
+	QFontMetrics fm(fontName);
 	QSize sz;
 	for (size_t i = 0; i < sl.size(); i++) {
 		// Name uses the normal font.
 		// Description lines use a slightly smaller font.
-		const QFontMetrics fm(i == 0 ? fontName : fontDesc);
+		if (i == 1) {
+			fm = QFontMetrics(fontDesc);
+		}
 		const QSize szLine = fm.size(0, sl[i]);
 		sz.setHeight(sz.height() + szLine.height());
 

src/libromdata/Console/PlayStationDisc.cpp
@@ -358,7 +358,7 @@ PlayStationDisc::PlayStationDisc(IRpFile *file)
 		static const unsigned int sector_sizes[] = {2352, 2448};
 		CDROM_2352_Sector_t sector;
 
-		for (auto p : sector_sizes) {
+		for (const unsigned int p : sector_sizes) {
 			size_t size = d->file->seekAndRead(p * ISO_PVD_LBA, &sector, sizeof(sector));
 			if (size != sizeof(sector)) {
 				UNREF_AND_NULL_NOCHK(d->file);
@@ -559,10 +559,9 @@ int PlayStationDisc::isRomSupported_static(
 	}
 
 	// Make sure the rest of the system ID is either spaces or NULLs.
-	const char *p = &pvd->sysID[pos];
-	const char *const p_end = &pvd->sysID[sizeof(pvd->sysID)];
 	bool isOK = true;
-	for (; p < p_end; p++) {
+	const char *const p_end = &pvd->sysID[sizeof(pvd->sysID)];
+	for (const char *p = &pvd->sysID[pos]; p < p_end; p++) {
 		if (*p != ' ' && *p != '\0') {
 			isOK = false;
 			break;

src/libromdata/Console/WiiU.cpp
@@ -566,7 +566,7 @@ int WiiU::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) const
 	// Replace any non-printable characters with underscores.
 	// (GameCube NDDEMO has ID6 "00\0E01".)
 	char id6[7];
-	for (int i = 0; i < 4; i++) {
+	for (unsigned int i = 0; i < 4; i++) {
 		id6[i] = (ISPRINT(discHeader->id4[i]))
 			? discHeader->id4[i]
 			: '_';

src/libromdata/Console/Xbox360_STFS.cpp
@@ -697,9 +697,8 @@ int Xbox360_STFS::isRomSupported_static(const DetectInfo *info)
 			// This area overlaps the Nintendo DS logo section,
 			// which is *never* empty.
 			// TODO: Vectorize this?
-			const uint8_t *pPadding = stfsHeader->ms.padding;
-			const uint8_t *const pPadding_end = pPadding + sizeof(stfsHeader->ms.padding);
-			for (; pPadding < pPadding_end; pPadding++) {
+			const uint8_t *const pPadding_end = &stfsHeader->ms.padding[sizeof(stfsHeader->ms.padding)];
+			for (const uint8_t *pPadding = stfsHeader->ms.padding; pPadding < pPadding_end; pPadding++) {
 				if (*pPadding != 0) {
 					// Not empty.
 					// This is not padding.

src/libromdata/Handheld/GameBoyAdvance.cpp
@@ -205,7 +205,7 @@ int GameBoyAdvance::isRomSupported_static(const DetectInfo *info)
 
 		// Verify the header checksum.
 		uint8_t chk = 0;
-		for (int i = 0xA0; i <= 0xBC; i++) {
+		for (unsigned int i = 0xA0U; i <= 0xBCU; i++) {
 			chk -= info->header.pData[i];
 		}
 		chk -= 0x19;
@@ -216,7 +216,7 @@ int GameBoyAdvance::isRomSupported_static(const DetectInfo *info)
 			// "Action Replay".
 
 			// The entry point for expansion cartridges is 0xFFFFFFFF.
-			if (gba_header->entry_point == cpu_to_le32(0xFFFFFFFF)) {
+			if (gba_header->entry_point == cpu_to_le32(0xFFFFFFFFU)) {
 				// This is a Nintendo DS expansion cartridge.
 				romType = GameBoyAdvancePrivate::RomType::NDS_Expansion;
 			} else {
@@ -349,7 +349,7 @@ int GameBoyAdvance::loadFieldData(void)
 	// Replace any non-printable characters with underscores.
 	// (Action Replay has ID6 "\0\0\0\001".)
 	char id6[7];
-	for (int i = 0; i < 6; i++) {
+	for (unsigned int i = 0; i < 6; i++) {
 		id6[i] = (ISPRINT(romHeader->id6[i])
 			? romHeader->id6[i]
 			: '_');

commit 691f9b055c73b88565044ee7236332f8b29c97bb

[librptexture] PowerVR3.cpp: fmtLkup_tbl_U32[] now has 0 elements.
MSVC complained:

src\librptexture\fileformat\PowerVR3.cpp(196): error C2466: cannot
allocate an array of constant size 0

...but gcc did not. Weird.

fmtLkup_tbl_U32[] isn't used right now, so #if 0 it.

This fixes a regression from commit b050ffd8f7f94653fbdebe484e593c0415ef5d99.
(Various `for` loop changes, including more ranged `for` and int -> unsigned int.)

src/librptexture/fileformat/PowerVR3.cpp
@@ -101,7 +101,9 @@ class PowerVR3Private final : public FileFormatPrivate
 
 		static const struct FmtLkup_t fmtLkup_tbl_U8[];
 		static const struct FmtLkup_t fmtLkup_tbl_U16[];
+#if 0
 		static const struct FmtLkup_t fmtLkup_tbl_U32[];
+#endif
 
 		/**
 		 * Load the image.
@@ -182,6 +184,7 @@ const struct PowerVR3Private::FmtLkup_t PowerVR3Private::fmtLkup_tbl_U16[] = {
 #endif
 };
 
+#if 0
 // Uncompressed format lookup table. (UINT, UINT_NORM)
 const struct PowerVR3Private::FmtLkup_t PowerVR3Private::fmtLkup_tbl_U32[] = {
 	//{'\0\0\0r', 0x00000020, ImageDecoder::PixelFormat::R32,			32},
@@ -194,6 +197,7 @@ const struct PowerVR3Private::FmtLkup_t PowerVR3Private::fmtLkup_tbl_U32[] = {
 	{ '\0\0al', 0x00002020, ImageDecoder::PixelFormat::A32L32,	32},
 #endif
 };
+#endif
 
 PowerVR3Private::PowerVR3Private(PowerVR3 *q, IRpFile *file)
 	: super(q, file, &textureInfo)