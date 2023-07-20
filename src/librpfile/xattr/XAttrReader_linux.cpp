aba3f8f8cfc5dc68b9c37e58a4c9022fcb528d01
David Korth <gerbilsoft@gerbilsoft.com>
2023-07-19 23:42:30 -0400

    [librpfile] XAttrReader_linux.cpp, XAttrReaderPrivate::init(): Use statx() if available.
    This was already done in the constructor, but not in init().

--- src/librpfile/xattr/XAttrReader_linux.cpp
+++ src/librpfile/xattr/XAttrReader_linux.cpp
@@ -64,7 +64,6 @@ XAttrReaderPrivate::XAttrReaderPrivate(const char *filename)
 		}
 		return;
 	}
-
 	mode = sbx.stx_mode;
 #else /* !HAVE_STATX */
 	struct stat sb;
@@ -77,7 +76,6 @@ XAttrReaderPrivate::XAttrReaderPrivate(const char *filename)
 		}
 		return;
 	}
-
 	mode = sb.st_mode;
 #endif /* HAVE_STATX */
 
@@ -114,16 +112,38 @@ XAttrReaderPrivate::XAttrReaderPrivate(const char *filename)
  */
 int XAttrReaderPrivate::init(void)
 {
-	// Verify the file mode again using fstat().
-	struct stat sb;
-	errno = 0;
-	if (!fstat(fd, &sb) && !S_ISREG(sb.st_mode) && !S_ISDIR(sb.st_mode)) {
-		// fstat() failed, or this is neither a regular file nor a directory.
+	// Verify the file type again using fstat().
+	mode_t mode;
+
+#ifdef HAVE_STATX
+	struct statx sbx;
+	int ret = statx(fd, "", AT_EMPTY_PATH, STATX_TYPE, &sbx);
+	if (ret != 0 || !(sbx.stx_mask & STATX_TYPE)) {
+		// An error occurred.
 		int err = -errno;
 		if (err == 0) {
 			err = -EIO;
 		}
 		return err;
+	}
+	mode = sbx.stx_mode;
+#else /* !HAVE_STATX */
+	struct stat sb;
+	errno = 0;
+	if (!fstat(fd, &sb)) {
+		// fstat() failed.
+		int err = -errno;
+		if (err == 0) {
+			err = -EIO;
+		}
+		return err;
+	}
+	mode = sb.st_mode;
+#endif /* HAVE_STATX */
+
+	if (!S_ISREG(mode) && !S_ISDIR(mode)) {
+		// This is neither a regular file nor a directory.
+		return -ENOTSUP;
 	}
 
 	// Load the attributes.
