64228d0c2c9ff93dcc38e29d348c697345325be1
David Korth <gerbilsoft@gerbilsoft.com>
2023-08-01 18:39:28 -0400

    [rpcli] rpcli.cpp: Remove a debug line from DoFile().
    This was accidentally added in commit 38940db9946d12e505e17c2eb5b870d3923831c8.
    ([win32] setlocale(LC_CTYPE, "C") after setting LC_ALL.)

--- src/rpcli/rpcli.cpp
+++ src/rpcli/rpcli.cpp
@@ -197,7 +197,6 @@ static void ExtractImages(const RomData *romData, vector<ExtractParam>& extract)
 static void DoFile(const char *filename, bool json, vector<ExtractParam>& extract,
 	uint32_t lc = 0, unsigned int flags = 0)
 {
-	printf("\n\nDoFile(): %s\n\n", filename);
 	cerr << "== " << rp_sprintf(C_("rpcli", "Reading file '%s'..."), filename) << endl;
 	RpFile *const file = new RpFile(filename, RpFile::FM_OPEN_READ_GZ);
 	if (file->isOpen()) {
