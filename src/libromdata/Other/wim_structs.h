ed3a0064a6760fcc6c665620cb26d2c73cf634af
David Korth <gerbilsoft@gerbilsoft.com>
2023-08-01 18:24:17 -0400

    [libromdata] wim_structs.h: Cleaned up this file.
    WIM_Header:
    - Add unused1, which is padding between bootable_index and
      integrity_resource.
    - Reduce unused2 (formerly unused) to 0x38 in order to make the
      header size 0xD0, which matches the WIM images I've checked.

--- src/libromdata/Other/wim_structs.h
+++ src/libromdata/Other/wim_structs.h
@@ -35,76 +35,67 @@ extern "C" {
 #define FLAG_HEADER_COMPRESS_LZX        0x00040000
 
 
-typedef struct 
-{
-    char unknown;
-    uint8_t minor_version;
-    uint8_t major_version;
-    char unknown2;
+typedef struct {
+	char unknown;
+	uint8_t minor_version;
+	uint8_t major_version;
+	char unknown2;
 } WIM_Version;
 ASSERT_STRUCT(WIM_Version, 0x4);
 
 typedef enum {
-    Wim113_014 = 0,
-    Wim109_112 = 1,
-    Wim107_108 = 2,
+	Wim113_014 = 0,
+	Wim109_112 = 1,
+	Wim107_108 = 2,
 } WIM_Version_Type;
 
 typedef enum {
-    header_reserved = 0x1,
-    has_compression = 0x2,
-    read_only = 0x4,
-    spanned = 0x8,
-    resource_only = 0x10,
-    metadata_only = 0x20,
-    write_in_progress = 0x40,
-    rp_fix = 0x80,
+	header_reserved = 0x1,
+	has_compression = 0x2,
+	read_only = 0x4,
+	spanned = 0x8,
+	resource_only = 0x10,
+	metadata_only = 0x20,
+	write_in_progress = 0x40,
+	rp_fix = 0x80,
 } WIM_Flags;
 
 typedef enum {
-    compress_reserved = 0x10000,
-    compress_xpress = 0x20000,
-    compress_lzx = 0x40000,
-    compress_lzms = 0x80000,
-    compress_xpress2 = 0x200000,
+	compress_reserved = 0x10000,
+	compress_xpress = 0x20000,
+	compress_lzx = 0x40000,
+	compress_lzms = 0x80000,
+	compress_xpress2 = 0x200000,
 } WIM_Compression_Flags;
 
-typedef struct _WIM_File_Resource_ 
-{
-    //this is 7 bytes but there isn't a good way of
-    //representing that 
-    uint64_t size;
-    uint64_t offset_of_xml;
-    uint64_t not_important;
+typedef struct _WIM_File_Resource {
+	//this is 7 bytes but there isn't a good way of
+	//representing that 
+	uint64_t size;
+	uint64_t offset_of_xml;
+	uint64_t not_important;
 } WIM_File_Resource;
 ASSERT_STRUCT(WIM_File_Resource, 0x18);
 
-typedef struct _WIM_Header_ 
-{
-    // 0x0
-    char header[8]; // MSWIM\0\0\0
-    uint32_t header_size;
-    WIM_Version version;
-    // 0x10
-    uint32_t flags;
-    uint32_t chunk_size;
-    char guid[0x10];
-    // 0x28
-    uint16_t part_number;
-    uint16_t total_parts;
-    uint32_t number_of_images;
-    // 0x30
-    WIM_File_Resource offset_table;
-    // 0x48
-    WIM_File_Resource xml_resource;
-    // 0x60
-    WIM_File_Resource boot_metadata_resource;
-    // 0x78
-    uint32_t bootable_index;
-    WIM_File_Resource integrity_resource;
-    char unused[0x3C];
+typedef struct _WIM_Header {
+	char header[8];					// [0x000] "MSWIM\0\0\0"
+	uint32_t header_size;				// [0x008]
+	WIM_Version version;				// [0x00C]
+	uint32_t flags;					// [0x010]
+	uint32_t chunk_size;				// [0x014]
+	char guid[0x10];				// [0x018]
+	uint16_t part_number;				// [0x028]
+	uint16_t total_parts;				// [0x02A]
+	uint32_t number_of_images;			// [0x02C]
+	WIM_File_Resource offset_table;			// [0x030]
+	WIM_File_Resource xml_resource;			// [0x048]
+	WIM_File_Resource boot_metadata_resource;	// [0x060]
+	uint32_t bootable_index;			// [0x078]
+	uint32_t unused1;				// [0x07C]
+	WIM_File_Resource integrity_resource;		// [0x080]
+	char unused2[0x38];				// [0x098]
 } WIM_Header;
-
+ASSERT_STRUCT(WIM_Header, 0xD0);
 
 #ifdef __cplusplus
 }
