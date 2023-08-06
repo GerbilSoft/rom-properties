c39cb81a49751800f8ca7304a7ebeaf75f8b3e38
David Korth <gerbilsoft@gerbilsoft.com>
2023-08-05 21:13:26 -0400

    RomDataFactory.hpp, FileFormatFactory.hpp: Apply some clang-format changes.

--- src/librptexture/FileFormatFactory.hpp
+++ src/librptexture/FileFormatFactory.hpp
@@ -22,46 +22,46 @@ namespace LibRpTexture {
 class FileFormat;
 class FileFormatFactory
 {
-	public:
-		// Static class
-		FileFormatFactory() = delete;
-		~FileFormatFactory() = delete;
-	private:
-		RP_DISABLE_COPY(FileFormatFactory)
+public:
+	// Static class
+	FileFormatFactory() = delete;
+	~FileFormatFactory() = delete;
+private:
+	RP_DISABLE_COPY(FileFormatFactory)
 
-	public:
-		/**
-		 * Create a FileFormat subclass for the specified texture file.
-		 *
-		 * NOTE: FileFormat::isValid() is checked before returning a
-		 * created FileFormat instance, so returned objects can be
-		 * assumed to be valid as long as they aren't nullptr.
-		 *
-		 * If imgbf is non-zero, at least one of the specified image
-		 * types must be supported by the FileFormat subclass in order to
-		 * be returned.
-		 *
-		 * @param file Texture file.
-		 * @return FileFormat subclass, or nullptr if the texture file isn't supported.
-		 */
-		static LibRpTexture::FileFormat *create(LibRpFile::IRpFile *file);
+public:
+	/**
+	 * Create a FileFormat subclass for the specified texture file.
+	 *
+	 * NOTE: FileFormat::isValid() is checked before returning a
+	 * created FileFormat instance, so returned objects can be
+	 * assumed to be valid as long as they aren't nullptr.
+	 *
+	 * If imgbf is non-zero, at least one of the specified image
+	 * types must be supported by the FileFormat subclass in order to
+	 * be returned.
+	 *
+	 * @param file Texture file.
+	 * @return FileFormat subclass, or nullptr if the texture file isn't supported.
+	 */
+	static LibRpTexture::FileFormat *create(LibRpFile::IRpFile *file);
 
-		/**
-		 * Get all supported file extensions.
-		 * Used for Win32 COM registration.
-		 *
-		 * @return All supported file extensions, including the leading dot.
-		 */
-		RP_LIBROMDATA_PUBLIC	/* TODO: Export on Windows only? */
-		static const std::vector<const char*> &supportedFileExtensions(void);
+	/**
+	 * Get all supported file extensions.
+	 * Used for Win32 COM registration.
+	 *
+	 * @return All supported file extensions, including the leading dot.
+	 */
+	RP_LIBROMDATA_PUBLIC	/* TODO: Export on Windows only? */
+	static const std::vector<const char*> &supportedFileExtensions(void);
 
-		/**
-		 * Get all supported MIME types.
-		 * Used for KFileMetaData.
-		 *
-		 * @return All supported MIME types.
-		 */
-		static std::vector<const char*> supportedMimeTypes(void);
+	/**
+	 * Get all supported MIME types.
+	 * Used for KFileMetaData.
+	 *
+	 * @return All supported MIME types.
+	 */
+	static std::vector<const char*> supportedMimeTypes(void);
 };
 
-}
+} //namespace LibRpTexture
