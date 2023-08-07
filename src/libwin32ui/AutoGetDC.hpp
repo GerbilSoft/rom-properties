b1f69e7a3f7bfdd6a2dca3e412e35bae3480757c
David Korth <gerbilsoft@gerbilsoft.com>
2023-08-07 00:44:55 -0400

    [libwin32ui] AutoGetDC.hpp: Un-indent the class. (clang-format)

--- src/libwin32ui/AutoGetDC.hpp
+++ src/libwin32ui/AutoGetDC.hpp
@@ -22,57 +22,57 @@ namespace LibWin32UI {
  */
 class AutoGetDC
 {
-	public:
-		explicit inline AutoGetDC(HWND hWnd)
-			: m_hWnd(hWnd)
-			, m_hFontOrig(nullptr)
-			, m_bAdjFont(false)
-		{
-			assert(hWnd != nullptr);
-			m_hDC = GetDC(hWnd);
+public:
+	explicit inline AutoGetDC(HWND hWnd)
+		: m_hWnd(hWnd)
+		, m_hFontOrig(nullptr)
+		, m_bAdjFont(false)
+	{
+		assert(hWnd != nullptr);
+		m_hDC = GetDC(hWnd);
+	}
+
+	explicit inline AutoGetDC(HWND hWnd, HFONT hFont)
+		: m_hWnd(hWnd)
+		, m_bAdjFont(true)
+	{
+		assert(hWnd != nullptr);
+		assert(hFont != nullptr);
+		m_hDC = GetDC(m_hWnd);
+		m_hFontOrig = (m_hDC ? SelectFont(m_hDC, hFont) : nullptr);
+	}
+
+	inline ~AutoGetDC() {
+		if (!m_hDC) {
+			return;
 		}
 
-		explicit inline AutoGetDC(HWND hWnd, HFONT hFont)
-			: m_hWnd(hWnd)
-			, m_bAdjFont(true)
-		{
-			assert(hWnd != nullptr);
-			assert(hFont != nullptr);
-			m_hDC = GetDC(m_hWnd);
-			m_hFontOrig = (m_hDC ? SelectFont(m_hDC, hFont) : nullptr);
+		if (m_bAdjFont) {
+			SelectFont(m_hDC, m_hFontOrig);
 		}
+		ReleaseDC(m_hWnd, m_hDC);
+	}
 
-		inline ~AutoGetDC() {
-			if (!m_hDC) {
-				return;
-			}
+	inline operator HDC() {
+		return m_hDC;
+	}
 
-			if (m_bAdjFont) {
-				SelectFont(m_hDC, m_hFontOrig);
-			}
-			ReleaseDC(m_hWnd, m_hDC);
-		}
-
-		inline operator HDC() {
-			return m_hDC;
-		}
-
-		// Disable copy/assignment constructors.
+	// Disable copy/assignment constructors.
 #if __cplusplus >= 201103L
-	public:
-		AutoGetDC(const AutoGetDC &) = delete;
-		AutoGetDC &operator=(const AutoGetDC &) = delete;
+public:
+	AutoGetDC(const AutoGetDC &) = delete;
+	AutoGetDC &operator=(const AutoGetDC &) = delete;
 #else /* __cplusplus < 201103L */
-	private:
-		AutoGetDC(const AutoGetDC &);
-		AutoGetDC &operator=(const AutoGetDC &);
+private:
+	AutoGetDC(const AutoGetDC &);
+	AutoGetDC &operator=(const AutoGetDC &);
 #endif /* __cplusplus */
 
-	private:
-		HWND m_hWnd;
-		HDC m_hDC;
-		HFONT m_hFontOrig;
-		bool m_bAdjFont;
+private:
+	HWND m_hWnd;
+	HDC m_hDC;
+	HFONT m_hFontOrig;
+	bool m_bAdjFont;
 };
 
-}
+} //namespace LibWin32UI
