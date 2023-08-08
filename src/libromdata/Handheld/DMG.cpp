59dc1f240ac572c5820cf1b3771fae36174a958b
David Korth <gerbilsoft@gerbilsoft.com>
2023-08-08 01:51:45 -0400

    [libromdata] DMG::loadFieldData(): Remove an extra d->file->size() call.
    We have to add back d->copier_offset, but this still reduces code size
    regardless.
    
    Code size difference: (64-bit Gentoo Linux, gcc-13.2.0, release build, no LTO)
    
       text    data     bss     dec     hex filename
      26323    1216       0   27539    6b93 DMG.cpp.o [before]
      26307    1216       0   27523    6b83 DMG.cpp.o [after]
        -16       0       0     -16     -10 Difference

--- src/libromdata/Handheld/DMG.cpp
+++ src/libromdata/Handheld/DMG.cpp
@@ -820,7 +820,7 @@ DMG::DMG(IRpFile *file)
 		// No Nintendo logo. Assume a copier header is present.
 		d->copier_offset = 0x200;
 	}
-	const int64_t filesize = d->file->size() - d->copier_offset;
+	const int64_t fileSize = d->file->size() - d->copier_offset;
 
 	// Check for MMM01 menu headers at 0xF8000 (1 MiB) and 0x78000 (512 KiB).
 	// NOTE: 512 KiB versions indicates MBC3 (0x11), not MMM01, in the menu bank.
@@ -828,7 +828,7 @@ DMG::DMG(IRpFile *file)
 	static const unsigned int mmm01_rom_size_check[] = {1048576U, 524288U};
 	d->is_mmm01_multicart = false;
 	for (unsigned int mmm01_rom_size : mmm01_rom_size_check) {
-		if (filesize != mmm01_rom_size)
+		if (fileSize != mmm01_rom_size)
 			continue;
 
 		// Check for an MMM01 menu header at 0xF8000 or 0x78000.
@@ -867,7 +867,7 @@ DMG::DMG(IRpFile *file)
 	}
 
 	// Attempt to read the GBX footer.
-	const off64_t addr = filesize + d->copier_offset - sizeof(GBX_Footer);
+	const off64_t addr = fileSize + d->copier_offset - sizeof(GBX_Footer);
 	if (addr >= (off64_t)sizeof(GBX_Footer)) {
 		size = file->seekAndRead(addr, &d->gbxFooter, sizeof(d->gbxFooter));
 		if (size != sizeof(d->gbxFooter)) {
@@ -1118,9 +1118,9 @@ int DMG::loadFieldData(void)
 
 	// Check if this might be an MBC1 multicart.
 	// TODO: "Mani 4 in 1 - Tetris + Alleyway + Yakuman + Tennis (China) (Ja).gb" is MBC3M with 32 KB banks?
-	const int64_t filesize = d->file->size() - d->copier_offset;
+	const int64_t fileSize = d->file->size() - d->copier_offset;
 	const bool may_be_mbc1m = (cart_type.hardware == DMGPrivate::DMG_Hardware::MBC1) &&
-				  (filesize == 1048576U);
+				  (fileSize == 1048576U);
 	if (d->is_mmm01_multicart || may_be_mbc1m) {
 		// MMM01 multicart, or possibly an MBC1M multicart.
 		// Check for additional ROM headers.
@@ -1144,13 +1144,13 @@ int DMG::loadFieldData(void)
 		unsigned int bank_increment;
 		if (d->is_mmm01_multicart) {
 			// MMC01 bank increment is 0x40000 for 1 MiB carts; 0x20000 for 512 KiB carts
-			bank_increment = (filesize == 1048576U) ? 0x40000U : 0x20000U;
+			bank_increment = (fileSize == 1048576U) ? 0x40000U : 0x20000U;
 		} else {
 			// MBC1M bank increment is 0x40000 for 1 MiB carts.
 			bank_increment = 0x40000U;
 		}
 
-		for (unsigned int addr = base_addr + d->copier_offset; addr < filesize; addr += bank_increment) {
+		for (unsigned int addr = base_addr + d->copier_offset; addr < fileSize; addr += bank_increment) {
 			size_t size = d->file->seekAndRead(addr, mmm01_header.u8, sizeof(mmm01_header.u8));
 			if (size == sizeof(mmm01_header)) {
 				DMGPrivate::RomType mmm01_romType = static_cast<DMGPrivate::RomType>(isRomSupported_static(&info));
@@ -1303,8 +1303,7 @@ int DMG::loadFieldData(void)
 			if (size == sizeof(gbs_magic) && gbs_magic == cpu_to_be32(GBS_MAGIC)) {
 				// Found the GBS magic number.
 				// Open the GBS.
-				const off64_t fileSize = d->file->size();
-				SubFile *const gbsFile = new SubFile(d->file, jp_addr, fileSize - jp_addr);
+				SubFile *const gbsFile = new SubFile(d->file, jp_addr, fileSize + d->copier_offset - jp_addr);
 				if (gbsFile->isOpen()) {
 					// Open the GBS.
 					GBS *const gbs = new GBS(gbsFile);
