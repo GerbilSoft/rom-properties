GerbilSoft/rom-properties

commit c37d5ee39a52a955310d6d340a9accfd1beebb0d

[gtk] MessageWidget.c: Use gtk_widget_(add|remove)_css_class() on GTK4.
GtkStyleContext is deprecated as of GTK 4.10.

FIXME: Better way to determine if a dark theme is in use, since
gtk_style_context_lookup_color() is deprecated in GTK4.

src/gtk/MessageWidget.c
@@ -333,10 +333,39 @@ rp_message_widget_set_message_type(RpMessageWidget *widget, GtkMessageType messa
 			"gsrp_msgw_question_dark",
 			"gsrp_msgw_error_dark",
 		};
+		static const IconInfo_t *const pIconInfo_tbl_end = &iconInfo_tbl[ARRAY_SIZE(iconInfo_tbl)];
 
+#  if GTK_CHECK_VERSION(4,0,0)
+		// Remove all of our CSS classes first.
+		for (const IconInfo_t *p = iconInfo_tbl; p < pIconInfo_tbl_end; p++) {
+			gtk_widget_remove_css_class(GTK_WIDGET(widget), p->css_class);
+		}
+		for (unsigned int i = 0; i < ARRAY_SIZE(dark_css_class_tbl); i++) {
+			gtk_widget_remove_css_class(GTK_WIDGET(widget), dark_css_class_tbl[i]);
+		}
+
+		// Get the text color. If its grayscale value is >= 0.75,
+		// assume we're using a dark theme.
+		// FIXME: Better way to determine if a dark theme is in use.
+		// (gtk_style_context_lookup_color() is deprecated in GTK4)
+		bool dark = false;
+		GdkRGBA color;
+		gboolean bRet = gtk_style_context_lookup_color(
+			gtk_widget_get_style_context(GTK_WIDGET(widget)), "theme_text_color", &color);
+		if (bRet) {
+			// BT.601 grayscale conversion
+			const gfloat grayscale = (color.red   * 0.299f) +
+			                         (color.green * 0.587f) +
+						 (color.blue  * 0.114f);
+			dark = (grayscale >= 0.750f);
+		}
+
+		// Add the new CSS class.
+		gtk_widget_add_css_class(GTK_WIDGET(widget),
+			(likely(!dark) ? pIconInfo->css_class : dark_css_class_tbl[messageType]));
+#  else /* !GTK_CHECK_VERSION(4,0,0) */
 		// Remove all of our CSS classes first.
 		GtkStyleContext *const context = gtk_widget_get_style_context(GTK_WIDGET(widget));
-		static const IconInfo_t *const pIconInfo_tbl_end = &iconInfo_tbl[ARRAY_SIZE(iconInfo_tbl)];
 		for (const IconInfo_t *p = iconInfo_tbl; p < pIconInfo_tbl_end; p++) {
 			gtk_style_context_remove_class(context, p->css_class);
 		}
@@ -360,6 +389,7 @@ rp_message_widget_set_message_type(RpMessageWidget *widget, GtkMessageType messa
 		// Add the new CSS class.
 		gtk_style_context_add_class(context,
 			(likely(!dark) ? pIconInfo->css_class : dark_css_class_tbl[messageType]));
+#  endif /* GTK_CHECK_VERSION(4,0,0) */
 #else /* !GTK_CHECK_VERSION(3,0,0) */
 		GdkColor color;
 		color.pixel	 = pIconInfo->border_color;

commit 92ee8fae030d1ddb073e53c255ca9d9198f734ce

[gtk] MessageWidget: Don't attempt to add an empty CSS class.
In GTK4, this prints an assertion:

(com.gerbilsoft.rom-properties.rp-config):
Gtk-CRITICAL **: gtk_widget_remove_css_class: assertion 'css_class[0] != '\0'' failed

Also adjusted the GTK3 version, even though I didn't see the
assertion there.

This fixes a regression from commit c37d5ee39a52a955310d6d340a9accfd1beebb0d.
([gtk] MessageWidget.c: Use gtk_widget_(add|remove)_css_class() on GTK4.)

src/gtk/MessageWidget.c
@@ -337,7 +337,7 @@ rp_message_widget_set_message_type(RpMessageWidget *widget, GtkMessageType messa
 
 #  if GTK_CHECK_VERSION(4,0,0)
 		// Remove all of our CSS classes first.
-		for (const IconInfo_t *p = iconInfo_tbl; p < pIconInfo_tbl_end; p++) {
+		for (const IconInfo_t *p = iconInfo_tbl; p < pIconInfo_tbl_end-1; p++) {
 			gtk_widget_remove_css_class(GTK_WIDGET(widget), p->css_class);
 		}
 		for (unsigned int i = 0; i < ARRAY_SIZE(dark_css_class_tbl); i++) {
@@ -366,7 +366,7 @@ rp_message_widget_set_message_type(RpMessageWidget *widget, GtkMessageType messa
 #  else /* !GTK_CHECK_VERSION(4,0,0) */
 		// Remove all of our CSS classes first.
 		GtkStyleContext *const context = gtk_widget_get_style_context(GTK_WIDGET(widget));
-		for (const IconInfo_t *p = iconInfo_tbl; p < pIconInfo_tbl_end; p++) {
+		for (const IconInfo_t *p = iconInfo_tbl; p < pIconInfo_tbl_end-1; p++) {
 			gtk_style_context_remove_class(context, p->css_class);
 		}
 		for (unsigned int i = 0; i < ARRAY_SIZE(dark_css_class_tbl); i++) {