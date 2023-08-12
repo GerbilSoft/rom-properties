GerbilSoft/rom-properties

commit 96f50979d6f254d9ba37ae01603f57e0390a1fa7

[librpfile] FileSystem: Add UTF-16 overloads for set_mtime(), get_mtime(), and get_file_size_and_mtime().
Windows only, as per usual.

The actual functions now take `const char*` or `const wchar_t*` instead
of `const string&` or `const wstring&`. Inline wrapper functions are
provided for string/wstring source compatibility.

src/librpfile/FileSystem_posix.cpp
@@ -149,35 +149,39 @@ off64_t filesize(const char *filename)
 
 /**
  * Set the modification timestamp of a file.
- * @param filename Filename.
- * @param mtime Modification time.
+ * @param filename	[in] Filename (UTF-16)
+ * @param mtime		[in] Modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int set_mtime(const string &filename, time_t mtime)
+int set_mtime(const char *filename, time_t mtime)
 {
+	assert(filename && filename[0] != '\0');
+	if (!filename || filename[0] == '\0')
+		return -EINVAL;
+
 	// FIXME: time_t is 32-bit on 32-bit Linux.
 	// TODO: Add a static_warning() macro?
 	// - http://stackoverflow.com/questions/8936063/does-there-exist-a-static-warning
 	struct utimbuf utbuf;
 	utbuf.actime = time(nullptr);
 	utbuf.modtime = mtime;
-	int ret = utime(filename.c_str(), &utbuf);
+	int ret = utime(filename, &utbuf);
 
 	return (ret == 0 ? 0 : -errno);
 }
 
 /**
  * Get the modification timestamp of a file.
- * @param filename Filename.
- * @param pMtime Buffer for the modification timestamp.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int get_mtime(const string &filename, time_t *pMtime)
+int get_mtime(const char *filename, time_t *pMtime)
 {
+	assert(filename && filename[0] != '\0');
 	assert(pMtime != nullptr);
-	if (!pMtime) {
+	if (!filename || filename[0] == '\0' || !pMtime)
 		return -EINVAL;
-	}
 
 	// FIXME: time_t is 32-bit on 32-bit Linux.
 	// TODO: Add a static_warning() macro?
@@ -185,7 +189,7 @@ int get_mtime(const string &filename, time_t *pMtime)
 
 #ifdef HAVE_STATX
 	struct statx sbx;
-	int ret = statx(AT_FDCWD, filename.c_str(), 0, STATX_MTIME, &sbx);
+	int ret = statx(AT_FDCWD, filename, 0, STATX_MTIME, &sbx);
 	if (ret != 0 || !(sbx.stx_mask & STATX_MTIME)) {
 		// statx() failed and/or did not return the modification time.
 		int ret = -errno;
@@ -194,7 +198,7 @@ int get_mtime(const string &filename, time_t *pMtime)
 	*pMtime = sbx.stx_mtime.tv_sec;
 #else /* !HAVE_STATX */
 	struct stat sb;
-	int ret = stat(filename.c_str(), &sb);
+	int ret = stat(filename, &sb);
 	if (ret != 0) {
 		// stat() failed.
 		int ret = -errno;
@@ -409,17 +413,17 @@ bool isOnBadFS(const char *filename, bool allowNetFS)
 
 /**
  * Get a file's size and time.
- * @param filename	[in] Filename.
- * @param pFileSize	[out] File size.
- * @param pMtime	[out] Modification time.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int get_file_size_and_mtime(const string &filename, off64_t *pFileSize, time_t *pMtime)
+int get_file_size_and_mtime(const char *filename, off64_t *pFileSize, time_t *pMtime)
 {
-	assert(!filename.empty());
+	assert(filename && filename[0] != '\0');
 	assert(pFileSize != nullptr);
 	assert(pMtime != nullptr);
-	if (unlikely(filename.empty() || !pFileSize || !pMtime)) {
+	if (unlikely(filename || filename[0] == '\0' || !pFileSize || !pMtime)) {
 		return -EINVAL;
 	}
 
@@ -429,7 +433,7 @@ int get_file_size_and_mtime(const string &filename, off64_t *pFileSize, time_t *
 
 #ifdef HAVE_STATX
 	struct statx sbx;
-	int ret = statx(AT_FDCWD, filename.c_str(), 0, STATX_TYPE | STATX_MTIME | STATX_SIZE, &sbx);
+	int ret = statx(AT_FDCWD, filename, 0, STATX_TYPE | STATX_MTIME | STATX_SIZE, &sbx);
 	if (ret != 0 || (sbx.stx_mask & (STATX_MTIME | STATX_SIZE)) != (STATX_MTIME | STATX_SIZE)) {
 		// statx() failed and/or did not return the modification time or file size.
 		int ret = -errno;
@@ -447,7 +451,7 @@ int get_file_size_and_mtime(const string &filename, off64_t *pFileSize, time_t *
 	*pMtime = sbx.stx_mtime.tv_sec;
 #else /* !HAVE_STATX */
 	struct stat sb;
-	int ret = stat(filename.c_str(), &sb);
+	int ret = stat(filename, &sb);
 	if (ret != 0) {
 		// stat() failed.
 		int ret = -errno;

src/librpfile/FileSystem.hpp
@@ -118,19 +118,83 @@ const std::string &getConfigDirectory(void);
 
 /**
  * Set the modification timestamp of a file.
- * @param filename Filename.
- * @param mtime Modification time.
+ * @param filename	[in] Filename (UTF-8)
+ * @param mtime		[in] Modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int set_mtime(const std::string &filename, time_t mtime);
+int set_mtime(const char *filename, time_t mtime);
+
+/**
+ * Set the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-8)
+ * @param mtime		[in] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+static inline int set_mtime(const std::string &filename, time_t mtime)
+{
+	return set_mtime(filename.c_str(), mtime);
+}
+
+#ifdef _WIN32
+/**
+ * Set the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-16)
+ * @param mtime		[in] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int set_mtime(const wchar_t *filenameW, time_t mtime);
+
+/**
+ * Set the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-16)
+ * @param mtime		[in] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+static inline int set_mtime(const std::wstring &filenameW, time_t mtime)
+{
+	return set_mtime(filenameW.c_str(), mtime);
+}
+#endif /* _WIN32 */
 
 /**
  * Get the modification timestamp of a file.
- * @param filename Filename.
- * @param pMtime Buffer for the modification timestamp.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int get_mtime(const std::string &filename, time_t *pMtime);
+int get_mtime(const char *filename, time_t *pMtime);
+
+/**
+ * Get the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+static inline int get_mtime(const std::string &filename, time_t *pMtime)
+{
+	return get_mtime(filename.c_str(), pMtime);
+}
+
+#ifdef _WIN32
+/**
+ * Get the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int get_mtime(const wchar_t *filenameW, time_t *pMtime);
+
+/**
+ * Get the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+static inline int get_mtime(const std::wstring &filenameW, time_t *pMtime)
+{
+	return get_mtime(filenameW.c_str(), pMtime);
+}
+#endif /* _WIN32 */
 
 /**
  * Delete a file.
@@ -280,12 +344,47 @@ bool isOnBadFS(const wchar_t *filenameW, bool allowNetFS = false);
 
 /**
  * Get a file's size and time.
- * @param filename	[in] Filename.
- * @param pFileSize	[out] File size.
- * @param pMtime	[out] Modification time.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int get_file_size_and_mtime(const std::string &filename, off64_t *pFileSize, time_t *pMtime);
+int get_file_size_and_mtime(const char *filename, off64_t *pFileSize, time_t *pMtime);
+
+/**
+ * Get a file's size and time.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+static inline int get_file_size_and_mtime(const std::string &filename, off64_t *pFileSize, time_t *pMtime)
+{
+	return get_file_size_and_mtime(filename.c_str(), pFileSize, pMtime);
+}
+
+#ifdef _WIN32
+/**
+ * Get a file's size and time.
+ * @param filenameW	[in] Filename (UTF-16)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int get_file_size_and_mtime(const wchar_t *filenameW, off64_t *pFileSize, time_t *pMtime);
+
+/**
+ * Get a file's size and time.
+ * @param filenameW	[in] Filename (UTF-16)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+static inline int get_file_size_and_mtime(const std::wstring &filenameW, off64_t *pFileSize, time_t *pMtime)
+{
+	return get_file_size_and_mtime(filenameW.c_str(), pFileSize, pMtime);
+}
+#endif /* _WIN32 */
 
 #ifdef _WIN32
 /**

src/librpfile/win32/FileSystem_win32.cpp
@@ -353,19 +353,19 @@ off64_t wfilesize(const wchar_t *filename)
 
 /**
  * Set the modification timestamp of a file.
- * @param filename Filename.
- * @param mtime Modification time.
+ * @param tfilename	[in] Filename (tstring)
+ * @param mtime		[in] Modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int set_mtime(const string &filename, time_t mtime)
+static int set_mtime_int(const tstring &tfilename, time_t mtime)
 {
 	// TODO: Add a static_warning() macro?
 	// - http://stackoverflow.com/questions/8936063/does-there-exist-a-static-warning
 #if _USE_32BIT_TIME_T
-# error 32-bit time_t is not supported. Get a newer compiler.
+#  error 32-bit time_t is not supported. Get a newer compiler.
 #endif
-	const tstring tfilename = makeWinPath(filename);
 
+	// TODO: Use Win32 API directly instead of MSVCRT?
 	struct __utimbuf64 utbuf;
 	utbuf.actime = _time64(nullptr);
 	utbuf.modtime = mtime;
@@ -375,24 +375,43 @@ int set_mtime(const string &filename, time_t mtime)
 }
 
 /**
- * Get the modification timestamp of a file.
- * @param filename Filename.
- * @param pMtime Buffer for the modification timestamp.
+ * Set the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-8)
+ * @param mtime		[in] Modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int get_mtime(const string &filename, time_t *pMtime)
+int set_mtime(const char *filename, time_t mtime)
+{
+	return set_mtime_int(makeWinPath(filename), mtime);
+}
+
+/**
+ * Set the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-16)
+ * @param mtime		[in] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int set_mtime(const wchar_t *filenameW, time_t mtime)
+{
+	return set_mtime_int(makeWinPath(filenameW), mtime);
+}
+
+/**
+ * Get the modification timestamp of a file. (internal function)
+ * @param tfilename	[in] Filename (tstring)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+static int get_mtime_int(const tstring &tfilename, time_t *pMtime)
 {
 	assert(pMtime != nullptr);
-	if (!pMtime) {
+	if (!pMtime)
 		return -EINVAL;
-	}
-
-	const tstring tfilename = makeWinPath(filename);
 
 	// TODO: Add a static_warning() macro?
 	// - http://stackoverflow.com/questions/8936063/does-there-exist-a-static-warning
 #if _USE_32BIT_TIME_T
-# error 32-bit time_t is not supported. Get a newer compiler.
+#  error 32-bit time_t is not supported. Get a newer compiler.
 #endif
 	// Use GetFileTime() instead of _stati64().
 	HANDLE hFile = CreateFile(tfilename.c_str(),
@@ -414,6 +433,28 @@ int get_mtime(const string &filename, time_t *pMtime)
 	// Convert to Unix timestamp.
 	*pMtime = FileTimeToUnixTime(&mtime);
 	return 0;
+}
+
+/**
+ * Get the modification timestamp of a file.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int get_mtime(const char *filename, time_t *pMtime)
+{
+	return get_mtime_int(makeWinPath(filename), pMtime);
+}
+
+/**
+ * Get the modification timestamp of a file.
+ * @param filenameW	[in] Filename (UTF-16)
+ * @param pMtime	[out] Buffer for the modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int get_mtime(const wchar_t *filenameW, time_t *pMtime)
+{
+	return get_mtime_int(makeWinPath(filenameW), pMtime);
 }
 
 /**
@@ -738,26 +779,24 @@ bool isOnBadFS(const wchar_t *filenameW, bool allowNetFS)
 }
 
 /**
- * Get a file's size and mtime.
- * @param filename	[in] Filename.
- * @param pFileSize	[out] File size.
- * @param pMtime	[out] Modification time.
+ * Get a file's size and time. (internal function)
+ * @param tfilename	[in] Filename (tstring)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
  * @return 0 on success; negative POSIX error code on error.
  */
-int get_file_size_and_mtime(const string &filename, off64_t *pFileSize, time_t *pMtime)
+static int get_file_size_and_mtime_int(const tstring &tfilename, off64_t *pFileSize, time_t *pMtime)
 {
-	assert(!filename.empty());
 	assert(pFileSize != nullptr);
 	assert(pMtime != nullptr);
-	if (unlikely(filename.empty() || !pFileSize || !pMtime)) {
+	if (unlikely(!pFileSize || !pMtime)) {
 		return -EINVAL;
 	}
-	const tstring tfilename = makeWinPath(filename);
 
 	// TODO: Add a static_warning() macro?
 	// - http://stackoverflow.com/questions/8936063/does-there-exist-a-static-warning
 #if _USE_32BIT_TIME_T
-# error 32-bit time_t is not supported. Get a newer compiler.
+#  error 32-bit time_t is not supported. Get a newer compiler.
 #endif
 
 	// Use FindFirstFile() to get the file information.
@@ -789,6 +828,30 @@ int get_file_size_and_mtime(const string &filename, off64_t *pFileSize, time_t *
 
 	// We're done here.
 	return 0;
+}
+
+/**
+ * Get a file's size and time.
+ * @param filename	[in] Filename (UTF-8)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int get_file_size_and_mtime(const char *filename, off64_t *pFileSize, time_t *pMtime)
+{
+	return get_file_size_and_mtime_int(makeWinPath(filename), pFileSize, pMtime);
+}
+
+/**
+ * Get a file's size and time.
+ * @param filenameW	[in] Filename (UTF-16)
+ * @param pFileSize	[out] File size
+ * @param pMtime	[out] Modification time (UNIX timestamp)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int get_file_size_and_mtime(const wchar_t *filenameW, off64_t *pFileSize, time_t *pMtime)
+{
+	return get_file_size_and_mtime_int(makeWinPath(filenameW), pFileSize, pMtime);
 }
 
 /**

commit 5232137479f1340e64838faef5a60d5b219c09e5

[librpfile] FileSystem::get_file_size_and_mtime(): Fix incorrect filename check.
Should be checking for !filename, not filename.

This caused a warning that was handled at an error in the Linux release
build (gcc-13.2.0):

src/librpfile/FileSystem_posix.cpp: In function ‘int LibRpFile::FileSystem::get_file_size_and_mtime(const char*, off64_t*, time_t*)’:
src/librpfile/FileSystem_posix.cpp:432:24: error: argument 2 null where non-null expected [-Werror=nonnull]
  432 |         int ret = statx(AT_FDCWD, filename, 0, STATX_TYPE | STATX_MTIME | STATX_SIZE, &sbx);
      |                   ~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
In file included from /usr/include/bits/statx.h:39,
                 from /usr/include/sys/stat.h:465,
                 from src/librpfile/FileSystem_posix.cpp:15:
/usr/include/bits/statx-generic.h:61:5: note: in a call to function ‘int statx(int, const char*, int, unsigned int, statx*)’ declared ‘nonnull’
   61 | int statx (int __dirfd, const char *__restrict __path, int __flags,
      |     ^~~~~
cc1plus: some warnings being treated as errors

This fixes a regression from commit 96f50979d6f254d9ba37ae01603f57e0390a1fa7.
([librpfile] FileSystem: Add UTF-16 overloads for set_mtime(), get_mtime(), and get_file_size_and_mtime().)

src/librpfile/FileSystem_posix.cpp
@@ -419,7 +419,7 @@ int get_file_size_and_mtime(const char *filename, off64_t *pFileSize, time_t *pM
 	assert(filename && filename[0] != '\0');
 	assert(pFileSize != nullptr);
 	assert(pMtime != nullptr);
-	if (unlikely(filename || filename[0] == '\0' || !pFileSize || !pMtime)) {
+	if (unlikely(!filename || filename[0] == '\0' || !pFileSize || !pMtime)) {
 		return -EINVAL;
 	}
 