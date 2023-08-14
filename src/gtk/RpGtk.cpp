GerbilSoft/rom-properties

commit 0230baa74e0652fbbd635776a679da1acbb3b3fb

[gtk] Split all of the file dialog code from KeyManagerTab into RpGtk.
A common interface is now provided for both GtkFileChooserDialog and
GTK 4.10's GtkFileDialog, along with callbacks and structs.

KeyManagerTab only has to handle one type of callback and one set of
initialization instead of a bunch of different versions.

TODO: Port RomDataView_ops.cpp to the new rpGtk_getSaveFileName()
function. The filter functions have been temporarily disabled because
they're no longer declared in RpGtk.hpp and are now marked as static.

src/gtk/RpGtk.cpp
@@ -8,6 +8,12 @@
 
 #include "stdafx.h"
 #include "RpGtk.hpp"
+#include "gtk-i18n.h"
+
+// Use the new GtkFileDialog class on GTK 4.10 and later.
+#if GTK_CHECK_VERSION(4,9,1)
+#  define USE_GTK4_FILE_DIALOG 1
+#endif /* GTK_CHECK_VERSION(4,9,1) */
 
 // C++ STL classes
 using std::string;
@@ -64,8 +70,6 @@ static GtkFileFilter *rpFileFilterToGtk_int(const gchar *const *pStrv)
 	return fileFilter;
 }
 
-// TODO: Consolidate the GTK2/GTK3 and GTK4 filter functions.
-
 /**
  * Convert an RP file dialog filter to GTK2/GTK3 for GtkFileChooser.
  *
@@ -84,7 +88,7 @@ static GtkFileFilter *rpFileFilterToGtk_int(const gchar *const *pStrv)
  * @param filter RP file dialog filter (UTF-8, from gettext())
  * @return 0 on success; negative POSIX error code on error
  */
-int rpFileFilterToGtkFileChooser(GtkFileChooser *fileChooser, const char *filter)
+static int rpFileFilterToGtkFileChooser(GtkFileChooser *fileChooser, const char *filter)
 {
 	assert(fileChooser != nullptr);
 	assert(filter != nullptr && filter[0] != '\0');
@@ -145,7 +149,7 @@ int rpFileFilterToGtkFileChooser(GtkFileChooser *fileChooser, const char *filter
  * @param filter RP file dialog filter (UTF-8, from gettext())
  * @return 0 on success; negative POSIX error code on error.
  */
-int rpFileFilterToGtkFileDialog(GtkFileDialog *fileDialog, const char *filter)
+static int rpFileFilterToGtkFileDialog(GtkFileDialog *fileDialog, const char *filter)
 {
 	assert(fileDialog != nullptr);
 	assert(filter != nullptr && filter[0] != '\0');
@@ -197,6 +201,208 @@ int rpFileFilterToGtkFileDialog(GtkFileDialog *fileDialog, const char *filter)
 	return ret;
 }
 #endif /* GTK_CHECK_VERSION(4,9,1) */
+
+// Simple struct for passing multiple values to the rpGtk_getFileName_int() callback functions.
+typedef struct _rpGtk_getFileName_int_callback_data_t {
+	rpGtk_fileDialogCallback callback;
+	gpointer user_data;
+	bool bSave;
+} rpGtk_getFileName_int_callback_data_t;
+
+#if USE_GTK4_FILE_DIALOG
+/**
+ * Callback from rpGtk_getFileName_int(). (GtkFileDialog version)
+ * @param fileDialog GtkFileDialog
+ * @param res GAsyncResult
+ * @param gfncbdata Callback data (user_data)
+ */
+static void
+rpGtk_getFileName_AsyncReadyCallback(GtkFileDialog *fileDialog, GAsyncResult *res, rpGtk_getFileName_int_callback_data_t *gfncbdata)
+{
+	GFile *file;
+	if (gfncbdata->bSave) {
+		file = gtk_file_dialog_save_finish(fileDialog, res, nullptr);
+	} else {
+		file = gtk_file_dialog_open_finish(fileDialog, res, nullptr);
+	}
+	g_object_unref(fileDialog);
+
+	// Run the callback.
+	// NOTE: Callback function takes ownership of the GFile.
+	gfncbdata->callback(file, gfncbdata->user_data);
+	g_free(gfncbdata);
+}
+#else /* !USE_GTK4_FILE_DIALOG */
+/**
+ * Callback from rpGtk_getFileName_int(). (GtkFileChooserDialog version)
+ * @param fileDialog GtkFileChooserDialog
+ * @param response_id Response ID
+ * @param gfncbdata Callback data (user_data)
+ */
+static void
+rpGtk_getFileName_fileDialog_response(GtkFileChooserDialog *fileDialog, gint response_id, rpGtk_getFileName_int_callback_data_t *gfncbdata)
+{
+	GFile *file = nullptr;
+	if (response_id == GTK_RESPONSE_ACCEPT) {
+		// Get the GFile from the dialog.
+		file = gtk_file_chooser_get_file(GTK_FILE_CHOOSER(fileDialog));
+	}
+
+	// Dialog is no longer needed.
+#if GTK_CHECK_VERSION(4,0,0)
+	gtk_window_destroy(GTK_WINDOW(fileDialog));
+#else /* !GTK_CHECK_VERSION(4,0,0) */
+	gtk_widget_destroy(GTK_WIDGET(fileDialog));
+#endif /* GTK_CHECK_VERSION(4,0,0) */
+
+	// Run the callback.
+	// NOTE: Callback function takes ownership of the GFile.
+	gfncbdata->callback(file, gfncbdata->user_data);
+	g_free(gfncbdata);
+}
+#endif /* !USE_GTK4_FILE_DIALOG */
+
+/**
+ * Prompt the user to open or save a file. (internal function)
+ *
+ * The dialog is opened as modal, but is handled asynchronously.
+ * The callback function is run when the dialog is closed.
+ *
+ * @param gfndata (in): rpGtk_getFileName_t
+ * @param bSave (in): True for save; false for open
+ * @return 0 on success; negative POSIX error code on error
+ */
+static int rpGtk_getFileName_int(const rpGtk_getFileName_t *gfndata, bool bSave)
+{
+#if USE_GTK4_FILE_DIALOG
+	// GTK 4.10.0 introduces a new GtkFileDialog.
+	GtkFileDialog *const fileDialog = gtk_file_dialog_new();
+	if (gfndata->title) {
+		gtk_file_dialog_set_title(fileDialog, gfndata->title);
+	}
+#else /* !USE_GTK4_FILE_DIALOG */
+	GtkFileChooserAction accept_action;
+	const char *accept_text;
+	if (bSave) {
+		accept_action = GTK_FILE_CHOOSER_ACTION_SAVE;
+		accept_text = GTK_I18N_STR_SAVE;
+	} else {
+		accept_action = GTK_FILE_CHOOSER_ACTION_OPEN;
+		accept_text = GTK_I18N_STR_OPEN;
+	}
+	GtkWidget *const fileDialog = gtk_file_chooser_dialog_new(
+		gfndata->title,			// title
+		gfndata->parent,		// parent
+		accept_action,			// action
+		GTK_I18N_STR_CANCEL, GTK_RESPONSE_CANCEL,
+		accept_text, GTK_RESPONSE_ACCEPT,
+		nullptr);
+	gtk_widget_set_name(fileDialog, "rpGtk_getFileName_int");
+#endif /* USE_GTK4_FILE_DIALOG */
+
+#if GTK_CHECK_VERSION(4,0,0)
+	// GTK4, GtkFileChooserDialog and/or GtkFileDialog
+	// Set the initial folder. (A GFile is required.)
+	if (gfndata->init_dir) {
+		GFile *const set_file = g_file_new_for_path(gfndata->init_dir);
+		if (set_file) {
+#  if USE_GTK4_FILE_DIALOG
+			gtk_file_dialog_set_initial_folder(fileDialog, set_file);
+#  else /* !USE_GTK4_FILE_DIALOG */
+			gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(fileDialog), set_file, nullptr);
+#  endif /* !USE_GTK4_FILE_DIALOG */
+                        g_object_unref(set_file);
+		}
+	}
+#else /* !GTK_CHECK_VERSION(4,0,0) */
+	// GTK2/GTK3: Require overwrite confirmation. (save dialogs only)
+	// NOTE: GTK4 has *mandatory* overwrite confirmation.
+	// Reference: https://gitlab.gnome.org/GNOME/gtk/-/commit/063ad28b1a06328e14ed72cc4b99cd4684efed12
+	if (bSave) {
+		gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(fileDialog), true);
+	}
+
+	// Set the initial folder.
+	if (gfndata->init_dir) {
+		gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(fileDialog), gfndata->init_dir);
+	}
+#endif
+
+	// Set the file filters.
+	if (gfndata->filter) {
+#if USE_GTK4_FILE_DIALOG
+		rpFileFilterToGtkFileDialog(fileDialog, gfndata->filter);
+#else /* !USE_GTK4_FILE_DIALOG */
+		rpFileFilterToGtkFileChooser(GTK_FILE_CHOOSER(fileDialog), gfndata->filter);
+#endif /* USE_GTK4_FILE_DIALOG */
+	}
+
+	// Data for the GtkFileChooserDialog/GtkFileDialog callback function.
+	rpGtk_getFileName_int_callback_data_t *const gfncbdata =
+		static_cast<rpGtk_getFileName_int_callback_data_t*>(g_malloc(sizeof(gfncbdata)));
+	gfncbdata->callback = gfndata->callback;
+	gfncbdata->user_data = gfndata->user_data;
+	gfncbdata->bSave = bSave;
+
+	// Prompt for a filename.
+#if USE_GTK4_FILE_DIALOG
+	gtk_file_dialog_set_modal(fileDialog, true);
+	if (bSave) {
+		gtk_file_dialog_save(fileDialog, gfndata->parent, nullptr,
+			(GAsyncReadyCallback)rpGtk_getFileName_AsyncReadyCallback, gfncbdata);
+	} else {
+		gtk_file_dialog_open(fileDialog, gfndata->parent, nullptr,
+			(GAsyncReadyCallback)rpGtk_getFileName_AsyncReadyCallback, gfncbdata);
+	}
+#else /* !USE_GTK4_FILE_DIALOG */
+	g_signal_connect(fileDialog, "response", G_CALLBACK(rpGtk_getFileName_fileDialog_response), gfncbdata);
+	gtk_window_set_transient_for(GTK_WINDOW(fileDialog), gfndata->parent);
+	gtk_window_set_modal(GTK_WINDOW(fileDialog), true);
+	gtk_widget_set_visible(GTK_WIDGET(fileDialog), true);
+#endif /* !USE_GTK4_FILE_DIALOG */
+
+	return 0;
+}
+
+/**
+ * Prompt the user to open a file.
+ *
+ * RP syntax: "Sega Mega Drive ROM images|*.gen;*.bin|application/x-genesis-rom|All Files|*.*:-"
+ * Similar the same as Windows, but with '|' instead of '\0'.
+ * Also, no terminator sequence is needed.
+ * The "(*.bin; *.srl)" part is added to the display name if needed.
+ * A third segment provides for semicolon-separated MIME types. (May be "-" for 'any'.)
+ *
+ * The dialog is opened as modal, but is handled asynchronously.
+ * The callback function is run when the dialog is closed.
+ *
+ * @param gfndata (in): rpGtk_getFileName_t
+ * @return 0 on success; negative POSIX error code on error
+ */
+int rpGtk_getOpenFileName(const rpGtk_getFileName_t *gfndata)
+{
+	return rpGtk_getFileName_int(gfndata, false);
+}
+
+/**
+ * Prompt the user to save a file.
+ *
+ * RP syntax: "Sega Mega Drive ROM images|*.gen;*.bin|application/x-genesis-rom|All Files|*.*:-"
+ * Similar the same as Windows, but with '|' instead of '\0'.
+ * Also, no terminator sequence is needed.
+ * The "(*.bin; *.srl)" part is added to the display name if needed.
+ * A third segment provides for semicolon-separated MIME types. (May be "-" for 'any'.)
+ *
+ * The dialog is opened as modal, but is handled asynchronously.
+ * The callback function is run when the dialog is closed.
+ *
+ * @param gfndata (in): rpGtk_getFileName_t
+ * @return 0 on success; negative POSIX error code on error
+ */
+int rpGtk_getSaveFileName(const rpGtk_getFileName_t *gfndata)
+{
+	return rpGtk_getFileName_int(gfndata, true);
+}
 
 /**
  * Convert Win32/Qt-style accelerator notation ('&') to GTK-style ('_').

src/gtk/RomDataView_ops.cpp
@@ -337,8 +337,11 @@ rp_rom_data_view_doRomOp_stdop(RpRomDataView *page, int id)
 	}
 #endif /* GTK_CHECK_VERSION(4,0,0) */
 
+	// TODO: Port to the new rpGtk_getSaveFileName() function.
+#if 0
 	// Set the filters.
 	rpFileFilterToGtkFileChooser(GTK_FILE_CHOOSER(fileDialog), filter);
+#endif
 
 	// Set the operation ID in the dialog.
 	g_object_set_qdata(G_OBJECT(fileDialog), RomDataView_romOp_quark, GINT_TO_POINTER(id));
@@ -489,6 +492,8 @@ btnOptions_triggered_signal_handler(RpOptionsMenuButton *menuButton,
 		gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(fileDialog), TRUE);
 #endif /* !GTK_CHECK_VERSION(4,0,0) */
 
+		// TODO: Port to the new rpGtk_getSaveFileName() function.
+#if 0
 		// Set the filters.
 		rpFileFilterToGtkFileChooser(GTK_FILE_CHOOSER(fileDialog), op->sfi.filter);
 
@@ -498,6 +503,7 @@ btnOptions_triggered_signal_handler(RpOptionsMenuButton *menuButton,
 		gtk_file_filter_set_name(allFilesFilter, C_("RomData", "All Files"));
 		gtk_file_filter_add_pattern(allFilesFilter, "*");
 		gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(fileDialog), allFilesFilter);
+#endif
 
 		// Initial file and directory, based on the current file.
 		// NOTE: Not checking if it's a file or a directory. Assuming it's a file.

src/gtk/RpGtk.hpp
@@ -1,4 +1,4 @@
-/***************************************************************************
+/**************************************************************************
  * ROM Properties Page shell extension. (GTK+ common)                      *
  * RpGtk.hpp: glib/gtk+ wrappers for some libromdata functionality.        *
  *                                                                         *
@@ -13,46 +13,57 @@
 G_BEGIN_DECLS
 
 /**
- * Convert an RP file dialog filter to GTK2/GTK3 for GtkFileChooser.
- *
- * RP syntax: "Sega Mega Drive ROM images|*.gen;*.bin|application/x-genesis-rom|All Files|*.*|-"
- * Similar the same as Windows, but with '|' instead of '\0'.
- * Also, no terminator sequence is needed.
- * The "(*.bin; *.srl)" part is added to the display name if needed.
- * A third segment provides for semicolon-separated MIME types. (May be "-" for 'any'.)
- *
- * NOTE: GTK+ doesn't use strings for file filters. Instead, it has
- * GtkFileFilter objects that are added to a GtkFileChooser.
- * To reduce overhead, the GtkFileChooser is passed to this function
- * so the GtkFileFilter objects can be added directly.
- *
- * @param fileChooser GtkFileChooser*
- * @param filter RP file dialog filter (UTF-8, from gettext())
- * @return 0 on success; negative POSIX error code on error.
+ * File dialog callback function.
+ * @param file (in) (transfer full): Selected file, or nullptr if no file was selected
+ * @param user_data (in) (transfer full): Specified user data when calling the original function
  */
-int rpFileFilterToGtkFileChooser(GtkFileChooser *fileChooser, const char *filter);
+typedef void (*rpGtk_fileDialogCallback)(GFile *file, gpointer user_data);
 
-#if GTK_CHECK_VERSION(4,9,1)
 /**
- * Convert an RP file dialog filter to GTK4 for GtkFileDialog.
+ * File dialog data struct
+ */
+typedef struct _rpGtk_getFileName_t {
+	GtkWindow *parent;			// (nullable) Parent window to set as modal
+	const char *title;			// Dialog title
+	const char *filter;			// RP file dialog filter (UTF-8, from gettext())
+	const char *init_dir;			// (nullable) Initial directory
+	rpGtk_fileDialogCallback callback;	// Callback function
+	gpointer user_data;			// User data for the callback function
+} rpGtk_getFileName_t;
+
+/**
+ * Prompt the user to open a file.
  *
- * RP syntax: "Sega Mega Drive ROM images|*.gen;*.bin|application/x-genesis-rom|All Files|*.*|-"
+ * RP syntax: "Sega Mega Drive ROM images|*.gen;*.bin|application/x-genesis-rom|All Files|*.*:-"
  * Similar the same as Windows, but with '|' instead of '\0'.
  * Also, no terminator sequence is needed.
  * The "(*.bin; *.srl)" part is added to the display name if needed.
  * A third segment provides for semicolon-separated MIME types. (May be "-" for 'any'.)
  *
- * NOTE: GTK+ doesn't use strings for file filters. Instead, a
- * GListModel of GtkFileFilter objects is added to a GtkFileDialog.
- * To reduce overhead, the GtkFileDialog is passed to this function
- * so the GtkFileFilter objects can be added directly.
+ * The dialog is opened as modal, but is handled asynchronously.
+ * The callback function is run when the dialog is closed.
  *
- * @param fileDialog GtkFileDialog*
- * @param filter RP file dialog filter (UTF-8, from gettext())
- * @return 0 on success; negative POSIX error code on error.
+ * @param gfndata (in): rpGtk_getFileName_t
+ * @return 0 on success; negative POSIX error code on error
  */
-int rpFileFilterToGtkFileDialog(GtkFileDialog *fileDialog, const char *filter);
-#endif /* GTK_CHECK_VERSION(4,9,1) */
+int rpGtk_getOpenFileName(const rpGtk_getFileName_t *gfndata);
+
+/**
+ * Prompt the user to save a file.
+ *
+ * RP syntax: "Sega Mega Drive ROM images|*.gen;*.bin|application/x-genesis-rom|All Files|*.*:-"
+ * Similar the same as Windows, but with '|' instead of '\0'.
+ * Also, no terminator sequence is needed.
+ * The "(*.bin; *.srl)" part is added to the display name if needed.
+ * A third segment provides for semicolon-separated MIME types. (May be "-" for 'any'.)
+ *
+ * The dialog is opened as modal, but is handled asynchronously.
+ * The callback function is run when the dialog is closed.
+ *
+ * @param gfndata (in): rpGtk_getFileName_t
+ * @return 0 on success; negative POSIX error code on error
+ */
+int rpGtk_getSaveFileName(const rpGtk_getFileName_t *gfndata);
 
 G_END_DECLS
 

src/gtk/config/KeyManagerTab.cpp
@@ -18,11 +18,6 @@ using LibRomData::KeyStoreUI;
 // C++ STL classes
 using std::string;
 
-// Use the new GtkFileDialog class on GTK 4.10 and later.
-#if GTK_CHECK_VERSION(4,9,1)
-#  define USE_GTK4_FILE_DIALOG 1
-#endif /* GTK_CHECK_VERSION(4,9,1) */
-
 // KeyStoreUI::ImportFileID
 static const char *const import_menu_actions[] = {
 	"Wii keys.bin",
@@ -50,6 +45,11 @@ static gboolean	btnImport_event_signal_handler			(GtkButton		*button,
 								 GdkEvent 		*event,
 								 RpKeyManagerTab	*tab);
 #endif /* !USE_GTK_MENU_BUTTON */
+
+static void rp_key_manager_tab_show_key_import_return_status	(RpKeyManagerTab	*tab,
+								 const char		*filename,
+								 const char		*keyType,
+								 const KeyStoreUI::ImportReturn &iret);
 
 #ifdef USE_G_MENU_MODEL
 static void	action_triggered_signal_handler			(GSimpleAction		*action,
@@ -422,13 +422,45 @@ btnImport_event_signal_handler(GtkButton *button, GdkEvent *event, RpKeyManagerT
 }
 #endif /* !USE_GTK_MENU_BUTTON */
 
-#if USE_GTK4_FILE_DIALOG
+// Simple struct for passing both KeyManagerTab and the file ID.
+struct open_data_t {
+	RpKeyManagerTab *tab;
+	KeyStoreUI::ImportFileID id;
+};
+
+/**
+ * File dialog callback function.
+ * @param file (in) (transfer full): Selected file, or nullptr if no file was selected
+ * @param open_data (in) (transfer full): open_data_t specified as user_data when rpGtk_getOpenFileName() was called.
+ */
 static void
-rp_key_manager_tab_menu_action_AsyncCallback(GtkFileDialog *fileDialog, GAsyncResult *res, RpKeyManagerTab *tab);
-#else /* !USE_GTK4_FILE_DIALOG */
-static void
-rp_key_manager_tab_menu_action_response(GtkFileChooserDialog *fileDialog, gint response_id, RpKeyManagerTab *tab);
-#endif /*  USE_GTK4_FILE_DIALOG */
+rp_key_manager_getOpenFileDialog_callback(GFile *file, open_data_t *open_data)
+{
+	if (!file) {
+		// No file selected.
+		g_free(open_data);
+		return;
+	}
+
+	// TODO: URIs?
+	gchar *const filename = g_file_get_path(file);
+	g_object_unref(file);
+	if (!filename) {
+		// No filename...
+		g_free(open_data);
+		return;
+	}
+
+	// for convenience purposes
+	RpKeyManagerTab *const tab = open_data->tab;
+
+	KeyStoreUI *const keyStoreUI = rp_key_store_gtk_get_key_store_ui(tab->keyStore);
+	const KeyStoreUI::ImportReturn iret = keyStoreUI->importKeysFromBin(open_data->id, filename);
+
+	rp_key_manager_tab_show_key_import_return_status(tab, filename, import_menu_actions[(int)open_data->id], iret);
+	g_free(filename);
+	g_free(open_data);
+}
 
 /**
  * Handle a menu action.
@@ -467,81 +499,38 @@ rp_key_manager_tab_handle_menu_action(RpKeyManagerTab *tab, gint id)
 		NOP_C_("KeyManagerTab", "aeskeydb.bin|aeskeydb.bin|-|Binary Files|*.bin|-|All Files|*.*|-"),
 	};
 
-	const char *const s_title = dpgettext_expr(
+	GtkWindow *const parent = gtk_widget_get_toplevel_window(GTK_WIDGET(tab));
+	const char *const title = dpgettext_expr(
 		RP_I18N_DOMAIN, "KeyManagerTab", dialog_titles_tbl[id]);
-	const char *const s_filter = dpgettext_expr(
+	const char *const filter = dpgettext_expr(
 		RP_I18N_DOMAIN, "KeyManagerTab", file_filters_tbl[id]);
 
-	GtkWindow *const parent = gtk_widget_get_toplevel_window(GTK_WIDGET(tab));
+	open_data_t *const open_data = static_cast<open_data_t*>(g_malloc(sizeof(*open_data)));
+	open_data->tab = tab;
+	open_data->id = static_cast<LibRomData::KeyStoreUI::ImportFileID>(id);
 
-#if USE_GTK4_FILE_DIALOG
-	// GTK 4.10.0 introduces a new GtkFileDialog.
-	GtkFileDialog *const fileDialog = gtk_file_dialog_new();
-	gtk_file_dialog_set_title(fileDialog, s_title);
-#else /* !USE_GTK4_FILE_DIALOG */
-	GtkWidget *const fileDialog = gtk_file_chooser_dialog_new(
-		s_title,			// title
-		parent,				// parent
-		GTK_FILE_CHOOSER_ACTION_OPEN,	// action
-		GTK_I18N_STR_CANCEL, GTK_RESPONSE_CANCEL,
-		GTK_I18N_STR_OPEN, GTK_RESPONSE_ACCEPT,
-		nullptr);
-	gtk_widget_set_name(fileDialog, "fileDialog");
-#endif /* USE_GTK4_FILE_DIALOG */
-
-#if GTK_CHECK_VERSION(4,0,0)
-	// GTK4, GtkFileChooserDialog and/or GtkFileDialog
-	// Set the initial folder. (A GFile is required.)
-        if (tab->prevOpenDir) {
-                GFile *const set_file = g_file_new_for_path(tab->prevOpenDir);
-                if (set_file) {
-#  if USE_GTK4_FILE_DIALOG
-			gtk_file_dialog_set_initial_folder(fileDialog, set_file);
-#  else /* !USE_GTK4_FILE_DIALOG */
-			gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(fileDialog), set_file, nullptr);
-#  endif /* !USE_GTK4_FILE_DIALOG */
-                        g_object_unref(set_file);
-		}
+	const rpGtk_getFileName_t gfndata = {
+		parent,			// parent
+		title,			// title
+		filter,			// filter
+		tab->prevOpenDir,	// init_dir
+		(rpGtk_fileDialogCallback)rp_key_manager_getOpenFileDialog_callback,	// callback
+		open_data,		// user_data
+	};
+	int ret = rpGtk_getOpenFileName(&gfndata);
+	if (ret != 0) {
+		// rpGtk_getOpenFileName() failed.
+		// free() the open_data_t because the callback won't be run.
+		free(open_data);
 	}
-#else /* !GTK_CHECK_VERSION(4,0,0) */
-	// GTK2/GTK3: Require overwrite confirmation.
-	// NOTE: GTK4 has *mandatory* overwrite confirmation.
-	// Reference: https://gitlab.gnome.org/GNOME/gtk/-/commit/063ad28b1a06328e14ed72cc4b99cd4684efed12
-	gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(fileDialog), TRUE);
 
-	// Set the initial folder.
-	if (tab->prevOpenDir) {
-		gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(fileDialog), tab->prevOpenDir);
-	}
-#endif
-
-	// Set the filters.
-#if USE_GTK4_FILE_DIALOG
-	rpFileFilterToGtkFileDialog(fileDialog, s_filter);
-#else /* !USE_GTK4_FILE_DIALOG */
-	rpFileFilterToGtkFileChooser(GTK_FILE_CHOOSER(fileDialog), s_filter);
-#endif /* USE_GTK4_FILE_DIALOG */
-
-	// Set the file ID in the dialog.
-	g_object_set_qdata(G_OBJECT(fileDialog), KeyManagerTab_fileID_quark, GINT_TO_POINTER(id));
-
-	// Prompt for a filename.
-#if USE_GTK4_FILE_DIALOG
-	gtk_file_dialog_set_modal(fileDialog, true);
-	gtk_file_dialog_open(fileDialog, parent, nullptr, (GAsyncReadyCallback)rp_key_manager_tab_menu_action_AsyncCallback, tab);
-#else /* !USE_GTK4_FILE_DIALOG */
-	g_signal_connect(fileDialog, "response", G_CALLBACK(rp_key_manager_tab_menu_action_response), tab);
-	gtk_window_set_transient_for(GTK_WINDOW(fileDialog), parent);
-	gtk_window_set_modal(GTK_WINDOW(fileDialog), true);
-	gtk_widget_set_visible(GTK_WIDGET(fileDialog), true);
-#endif /* !USE_GTK4_FILE_DIALOG */
-
-	// GtkFileChooserDialog will send the "response" signal when the dialog is closed.
+	// rpGtk_getOpenFileName() will call rp_key_manager_getOpenFileDialog_callback()
+	// when the dialog is closed.
 }
 
 /**
  * Show key import return status.
- * @praam tab KeyManagerTab
+ * @param tab KeyManagerTab
  * @param filename Filename
  * @param keyType Key type
  * @param iret ImportReturn
@@ -716,86 +705,6 @@ rp_key_manager_tab_show_key_import_return_status(RpKeyManagerTab	*tab,
 	rp_message_widget_set_text(RP_MESSAGE_WIDGET(tab->messageWidget), msg.c_str());
 	gtk_widget_set_visible(tab->messageWidget, true);
 }
-
-#if USE_GTK4_FILE_DIALOG
-static void
-rp_key_manager_tab_menu_action_AsyncCallback(GtkFileDialog *fileDialog, GAsyncResult *res, RpKeyManagerTab *tab)
-{
-	GFile *const get_file = gtk_file_dialog_open_finish(fileDialog, res, nullptr);
-	g_object_unref(fileDialog);
-	if (!get_file) {
-		// No file selected.
-		return;
-	}
-
-	// Get the file ID from the dialog.
-	const KeyStoreUI::ImportFileID id = static_cast<KeyStoreUI::ImportFileID>(
-		GPOINTER_TO_INT(g_object_get_qdata(G_OBJECT(fileDialog), KeyManagerTab_fileID_quark)));
-
-	// TODO: URIs?
-	gchar *const in_filename = g_file_get_path(get_file);
-	g_object_unref(get_file);
-	if (!in_filename) {
-		// No filename...
-		return;
-	}
-
-	KeyStoreUI *const keyStoreUI = rp_key_store_gtk_get_key_store_ui(tab->keyStore);
-	const KeyStoreUI::ImportReturn iret = keyStoreUI->importKeysFromBin(id, in_filename);
-
-	rp_key_manager_tab_show_key_import_return_status(tab, in_filename, import_menu_actions[(int)id], iret);
-	g_free(in_filename);
-}
-#else /* !USE_GTK4_FILE_DIALOG */
-/**
- * The Save dialog for a Standard ROM Operation has been closed.
- * @param fileDialog GtkFileChooserDialog
- * @param response_id Response ID
- * @param tab KeyManagerTab
- */
-static void
-rp_key_manager_tab_menu_action_response(GtkFileChooserDialog *fileDialog, gint response_id, RpKeyManagerTab *tab)
-{
-	if (response_id != GTK_RESPONSE_ACCEPT) {
-		// User cancelled the dialog.
-#if GTK_CHECK_VERSION(4,0,0)
-		gtk_window_destroy(GTK_WINDOW(fileDialog));
-#else /* !GTK_CHECK_VERSION(4,0,0) */
-		gtk_widget_destroy(GTK_WIDGET(fileDialog));
-#endif /* GTK_CHECK_VERSION(4,0,0) */
-		return;
-	}
-
-	// Get the file ID from the dialog.
-	const KeyStoreUI::ImportFileID id = static_cast<KeyStoreUI::ImportFileID>(
-		GPOINTER_TO_INT(g_object_get_qdata(G_OBJECT(fileDialog), KeyManagerTab_fileID_quark)));
-
-#if GTK_CHECK_VERSION(4,0,0)
-	// TODO: URIs?
-	gchar *in_filename = nullptr;
-	GFile *const get_file = gtk_file_chooser_get_file(GTK_FILE_CHOOSER(fileDialog));
-	if (get_file) {
-		in_filename = g_file_get_path(get_file);
-		g_object_unref(get_file);
-	}
-	gtk_window_destroy(GTK_WINDOW(fileDialog));
-#else /* !GTK_CHECK_VERSION(4,0,0) */
-	gchar *const in_filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(fileDialog));
-	gtk_widget_destroy(GTK_WIDGET(fileDialog));
-#endif /* GTK_CHECK_VERSION(4,0,0) */
-
-	if (!in_filename) {
-		// No filename...
-		return;
-	}
-
-	KeyStoreUI *const keyStoreUI = rp_key_store_gtk_get_key_store_ui(tab->keyStore);
-	const KeyStoreUI::ImportReturn iret = keyStoreUI->importKeysFromBin(id, in_filename);
-
-	rp_key_manager_tab_show_key_import_return_status(tab, in_filename, import_menu_actions[(int)id], iret);
-	g_free(in_filename);
-}
-#endif /* !USE_GTK4_FILE_DIALOG */
 
 #ifdef USE_G_MENU_MODEL
 /**

commit 684f5e35898b8a7a22e8e6f37927857d9943cf65

[gtk] RpGtk: Allocate sizeof(*gfndata), not sizeof(gfndata).
struct rpGtk_getFileName_t is 17 bytes; sizeof(gfndata) is 8 bytes.
I'm not sure how this worked at all, or maybe it only worked in the
debug build due to padding...

Noticed the following warnings when building the release build:
(64-bit Gentoo Linux, gcc-13.2.0, release build, no LTO)

src/gtk/RpGtk.cpp: In function ‘int rpGtk_getFileName_int(const rpGtk_getFileName_t*, bool)’:
src/gtk/RpGtk.cpp:357:20: warning: array subscript ‘rpGtk_getFileName_int_callback_data_t {aka _rpGtk_getFileName_int_callback_data_t}[0]’ is partly outside array bounds of ‘unsigned char [8]’ [-Warray-bounds=]
  357 |         gfncbdata->user_data = gfndata->user_data;
      |         ~~~~~~~~~~~^~~~~~~~~
src/gtk/RpGtk.cpp:355:77: note: object of size 8 allocated by ‘g_malloc’
  355 |                 static_cast<rpGtk_getFileName_int_callback_data_t*>(g_malloc(sizeof(gfncbdata)));
      |                                                                     ~~~~~~~~^~~~~~~~~~~~~~~~~~~
src/gtk/RpGtk.cpp:358:20: warning: array subscript ‘rpGtk_getFileName_int_callback_data_t {aka _rpGtk_getFileName_int_callback_data_t}[0]’ is partly outside array bounds of ‘unsigned char [8]’ [-Warray-bounds=]
  358 |         gfncbdata->bSave = bSave;
      |         ~~~~~~~~~~~^~~~~
src/gtk/RpGtk.cpp:355:77: note: object of size 8 allocated by ‘g_malloc’
  355 |                 static_cast<rpGtk_getFileName_int_callback_data_t*>(g_malloc(sizeof(gfncbdata)));
      |                                                                     ~~~~~~~~^~~~~~~~~~~~~~~~~~~

This fixes a regression from commit 0230baa74e0652fbbd635776a679da1acbb3b3fb.
([gtk] Split all of the file dialog code from KeyManagerTab into RpGtk.)

src/gtk/RpGtk.cpp
@@ -352,7 +352,7 @@ static int rpGtk_getFileName_int(const rpGtk_getFileName_t *gfndata, bool bSave)
 
 	// Data for the GtkFileChooserDialog/GtkFileDialog callback function.
 	rpGtk_getFileName_int_callback_data_t *const gfncbdata =
-		static_cast<rpGtk_getFileName_int_callback_data_t*>(g_malloc(sizeof(gfncbdata)));
+		static_cast<rpGtk_getFileName_int_callback_data_t*>(g_malloc(sizeof(*gfncbdata)));
 	gfncbdata->callback = gfndata->callback;
 	gfncbdata->user_data = gfndata->user_data;
 	gfncbdata->bSave = bSave;