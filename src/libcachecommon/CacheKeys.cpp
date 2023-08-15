GerbilSoft/rom-properties

commit 027d98fde45fe83ad6338441ab7cf1a2d2fcf95a

[libcachecommon] urlencode(): Encode '^' as "%5E".
Both Firefox and Chrome encode '^'. The Apache server doesn't seem to
have a problem with either unencoded '^' or encoded "%5E", but use the
encoded version to match both Firefox and Chrome.

For CGB "LSDj^4.2.9-00-J.png" (Little Sound DJ).

src/libcachecommon/CacheKeys.cpp
@@ -458,6 +458,9 @@ string urlencode(const char *url)
 				case '%':
 					s_ret += "%25";
 					break;
+				case '^':
+					s_ret += "%5E";
+					break;
 				default:
 					s_ret += *url;
 					break;

commit bdf362767de0715836713c53afc5735c7b9b786f

[libcachecommon] urlencode(wchar_t*): Encode '^' as "%5E".
Forgot to update the wchar_t version.

Fixes: 027d98fde45fe83ad6338441ab7cf1a2d2fcf95a
([libcachecommon] urlencode(): Encode '^' as "%5E".)

src/libcachecommon/CacheKeys.cpp
@@ -495,6 +495,9 @@ wstring urlencode(const wchar_t *url)
 			case L'%':
 				ws_ret += L"%25";
 				break;
+			case L'^':
+				s_ret += L"%5E";
+				break;
 			default:
 				ws_ret += *url;
 				break;