GerbilSoft/rom-properties

commit bdf362767de0715836713c53afc5735c7b9b786f

[libcachecommon] urlencode(wchar_t*): Encode '^' as "%5E".
Forgot to update the wchar_t version.

Fixes: 027d98fde45fe83ad6338441ab7cf1a2d2fcf95a
([libcachecommon] urlencode(): Encode '^' as "%5E".)

src/libcachecommon/CacheKeys.cpp
@@ -495,6 +495,9 @@ wstring urlencode(const wchar_t *url)
 			case L'%':
 				ws_ret += L"%25";
 				break;
+			case L'^':
+				s_ret += L"%5E";
+				break;
 			default:
 				ws_ret += *url;
 				break;

commit 0cee2008795aeb47db5a9c0a286dcd58598ec19c

[libcachecommon] urlencode(wchar_t*): s/s_ret/ws_ret/
Fixes: bdf362767de0715836713c53afc5735c7b9b786f
([libcachecommon] urlencode(wchar_t*): Encode '^' as "%5E".)

src/libcachecommon/CacheKeys.cpp
@@ -496,7 +496,7 @@ wstring urlencode(const wchar_t *url)
 				ws_ret += L"%25";
 				break;
 			case L'^':
-				s_ret += L"%5E";
+				ws_ret += L"%5E";
 				break;
 			default:
 				ws_ret += *url;