GerbilSoft/rom-properties

commit 520afc1df28d9ebbdaf65f5311d37ed059d5e177

[libcachecommon] filterCacheKey(wchar_t*): Fix UTF-16 surrogate pair handling.
The first (high) code point must be U+D800-U+DBFF. The second (low)
code point must be U+DC00-U+DFFF. It seems my check was backwards.

In addition, the low surrogate check should be looking to see if
the second code point is *not* a low surrogate, instead of checking
if it *is* a high surrogate, since that potentially breaks if the
second code point isn't a surrogate pair code point at all.

TODO: Add unit tests for the wchar_t version.

src/libcachecommon/CacheKeys.cpp
@@ -231,10 +231,10 @@ int filterCacheKey(wchar_t *pCacheKey)
 
 		// Surrogate pair check
 		const wchar_t schk = (chr & 0xDC00);
-		if (schk == 0xDC00) {
+		if (schk == 0xD800) {
 			// Start of UTF-16 surrogate pair.
 			// Verify that the next code point is a low surrogate.
-			if ((p[1] & 0xDC00) == 0xD800) {
+			if ((p[1] & 0xDC00) != 0xDC00) {
 				// Not a low surrogate.
 				// This is invalid.
 				*p = L'_';

commit be66133813199bf458f53274e4087cafbe86a80e

[libcachecommon] filterCacheKey(wchar_t*): Fix UTF-16 surrogate pair handling, again.
The sceond part, which checks for a low surrogate character that didn't
have a preceding high surrogate character, was incorrectly checking for
a high surrogate character.

Fixes: 520afc1df28d9ebbdaf65f5311d37ed059d5e177
([libcachecommon] filterCacheKey(wchar_t*): Fix UTF-16 surrogate pair handling.)

Affects: v2.2.1, though probably not in any significant way.

src/libcachecommon/CacheKeys.cpp
@@ -232,7 +232,7 @@ int filterCacheKey(wchar_t *pCacheKey)
 		// Surrogate pair check
 		const wchar_t schk = (chr & 0xDC00);
 		if (schk == 0xD800) {
-			// Start of UTF-16 surrogate pair.
+			// High surrogate: Start of UTF-16 surrogate pair.
 			// Verify that the next code point is a low surrogate.
 			if ((p[1] & 0xDC00) != 0xDC00) {
 				// Not a low surrogate.
@@ -244,7 +244,7 @@ int filterCacheKey(wchar_t *pCacheKey)
 			// This is a low surrogate.
 			p++;
 			continue;
-		} else if (schk == 0xD800) {
+		} else if (schk == 0xDC00) {
 			// Unpaired low surrogate.
 			// This is invalid.
 			*p = L'_';