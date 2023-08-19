GerbilSoft/rom-properties

commit 08a653722cd3ea5afc3039d48ec9f2cfb30b300b

[win32] Add wchar_t* overloads to RpFile and related classes.
This allows us to use UTF-16 filenames directly without converting to/from
UTF-8. NTFS originally used UCS-2, so it doesn't enforce normalized UTF-16
strings, which means unpaired surrogate characters can appear in filenames.
UTF-8 translation breaks those unpaired surrogate characters.

Added UTF-16 overloads for the following functions:
- RomDataFactory::create()
- FileSystem::is_symlink()
- FileSystem::resolve_symlink()
- FileSystem::openRelatedFile()
- RpFile::RpFile()
- RpFile::filenameW()
- RpFilePrivate::RpFilePrivate()

The 'W' suffix is added when the function only differs from the non-'W'
function by the return type.

Un-inlined RpFilePrivate::RpFilePrivate().

RpFile_win32.cpp: Use filenameW for all operations internally. The UTF-8
filename is only allocated if filename() is called.

Win32 COM classes: Pass d->olefilename or d->tfilename directly to
RomDataFactory::create().

Tested by adding U+D800 to a Nintendo DS ROM image filename and it can
now be viewed by rom-properties.

TODO: rpcli still uses UTF-8 conversion. Not sure if it's worth updating
rpcli to use UTF-16 because the Windows command line will likely have
its own problems with broken UTF-16 filenames.

src/librpfile/win32/RpFile_win32.cpp
@@ -39,6 +39,14 @@ DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 
 /** RpFilePrivate **/
 
+RpFilePrivate::RpFilePrivate(RpFile *q, const wchar_t *filenameW, RpFile::FileMode mode)
+	: q_ptr(q), file(INVALID_HANDLE_VALUE), filename(nullptr)
+	, mode(mode), gzfd(nullptr), gzsz(-1), devInfo(nullptr)
+{
+	assert(filenameW != nullptr);
+	this->filenameW = wcsdup(filenameW);
+}
+
 RpFilePrivate::~RpFilePrivate()
 {
 	if (gzfd) {
@@ -48,6 +56,7 @@ RpFilePrivate::~RpFilePrivate()
 		CloseHandle(file);
 	}
 	free(filename);
+	free(filenameW);
 	delete devInfo;
 }
 
@@ -102,7 +111,7 @@ inline int RpFilePrivate::mode_to_win32(RpFile::FileMode mode,
 int RpFilePrivate::reOpenFile(void)
 {
 	RP_Q(RpFile);
-	if (!filename || filename[0] == '\0') {
+	if (!filenameW || filenameW[0] == L'\0') {
 		// No filename...
 		q->m_lastError = EINVAL;
 		return -EINVAL;
@@ -118,37 +127,43 @@ int RpFilePrivate::reOpenFile(void)
 		return -EINVAL;
 	}
 
-	// Converted filename for Windows.
-	tstring tfilename;
-
 	// If the filename is "X:", change it to "X:\\".
-	if (ISASCII(filename[0]) && ISALPHA(filename[0]) &&
-	    filename[1] == ':' && filename[2] == '\0')
+	if (ISASCII(filenameW[0]) && ISALPHA(filenameW[0]) &&
+	    filenameW[1] == L':' && filenameW[2] == L'\0')
 	{
 		// Drive letter. Append '\\'.
-		char *drvfilename = static_cast<char*>(malloc(4));
+		wchar_t *drvfilename = static_cast<wchar_t*>(malloc(4*sizeof(wchar_t)));
 		drvfilename[0] = filename[0];
-		drvfilename[1] = ':';
-		drvfilename[2] = '\\';
-		drvfilename[3] = '\0';
+		drvfilename[1] = _T(':');
+		drvfilename[2] = _T('\\');
+		drvfilename[3] = _T('\0');
 
-		std::swap(drvfilename, this->filename);
+		std::swap(drvfilename, this->filenameW);
 		free(drvfilename);
+		if (this->filename) {
+			free(this->filename);
+			this->filename = nullptr;
+		}
 	}
 
+	// Adjusted filename for Windows.
+	tstring tfilename;
+
 	// Check if the path starts with a drive letter.
-	if (ISASCII(filename[0]) && ISALPHA(filename[0]) &&
-	    filename[1] == ':' && filename[2] == '\\')
+	// NEXT COMMIT TODO: Add ISDRIVELETTER() because ISASCII/ISALPHA only
+	// handle 8-bit chars, and iswascii/iswalpha are affected by locale.
+	if (ISASCII(filenameW[0]) && ISALPHA(filenameW[0]) &&
+	    filenameW[1] == L':' && filenameW[2] == L'\\')
 	{
 		// Is it only a drive letter?
-		if (filename[3] == '\0') {
+		if (filenameW[3] == L'\0') {
 			// This is a drive letter.
 			// Only CD-ROM (and similar) drives are supported.
 			// TODO: Verify if opening by drive letter works,
 			// or if we have to resolve the physical device name.
 			// NOTE: filename is UTF-8, but we can use it as if
 			// it's ANSI for a drive letter.
-			const UINT driveType = GetDriveTypeA(filename);
+			const UINT driveType = GetDriveType(filenameW);
 			switch (driveType) {
 				case DRIVE_CDROM:
 					// CD-ROM works.
@@ -167,17 +182,17 @@ int RpFilePrivate::reOpenFile(void)
 			// Create a raw device filename.
 			// Reference: https://support.microsoft.com/en-us/help/138434/how-win32-based-applications-read-cd-rom-sectors-in-windows-nt
 			tfilename = _T("\\\\.\\X:");
-			tfilename[4] = filename[0];
+			tfilename[4] = filenameW[0];
 			q->m_fileType = DT_BLK;	// this is a device
 		} else {
 			// Absolute path: Prepend "\\?\" in order to support filenames longer than MAX_PATH.
 			tfilename = _T("\\\\?\\");
-			tfilename += U82T_c(filename);
+			tfilename += filenameW;
 		}
 	} else {
 		// Not an absolute path, or "\\?\" is already
 		// prepended. Use it as-is.
-		tfilename = U82T_c(filename);
+		tfilename = filenameW;
 	}
 
 	if (q->m_fileType == DT_BLK) {
@@ -271,12 +286,12 @@ int RpFilePrivate::reOpenFile(void)
 /**
  * Open a file.
  * NOTE: Files are always opened in binary mode.
- * @param filename Filename.
- * @param mode File mode.
+ * @param filename Filename (UTF-8)
+ * @param mode File mode
  */
 RpFile::RpFile(const char *filename, FileMode mode)
 	: super()
-	, d_ptr(new RpFilePrivate(this, filename, mode))
+	, d_ptr(new RpFilePrivate(this, U82W_c(filename), mode))
 {
 	init();
 }
@@ -284,12 +299,38 @@ RpFile::RpFile(const char *filename, FileMode mode)
 /**
  * Open a file.
  * NOTE: Files are always opened in binary mode.
- * @param filename Filename.
- * @param mode File mode.
+ * @param filename Filename (UTF-8)
+ * @param mode File mode
  */
 RpFile::RpFile(const string &filename, FileMode mode)
 	: super()
-	, d_ptr(new RpFilePrivate(this, filename.c_str(), mode))
+	, d_ptr(new RpFilePrivate(this, U82W_s(filename), mode))
+{
+	init();
+}
+
+/**
+ * Open a file.
+ * NOTE: Files are always opened in binary mode.
+ * @param filename Filename (UTF-16)
+ * @param mode File mode
+ */
+RpFile::RpFile(const wchar_t *filenameW, FileMode mode)
+	: super()
+	, d_ptr(new RpFilePrivate(this, filenameW, mode))
+{
+	init();
+}
+
+/**
+ * Open a file.
+ * NOTE: Files are always opened in binary mode.
+ * @param filename Filename (UTF-16)
+ * @param mode File mode
+ */
+RpFile::RpFile(const wstring &filenameW, FileMode mode)
+	: super()
+	, d_ptr(new RpFilePrivate(this, filenameW.c_str(), mode))
 {
 	init();
 }
@@ -734,12 +775,30 @@ off64_t RpFile::size(void)
 
 /**
  * Get the filename.
- * @return Filename. (May be nullptr if the filename is not available.)
+ * @return Filename (UTF-8) (May be nullptr if the filename is not available.)
  */
 const char *RpFile::filename(void) const
 {
 	RP_D(const RpFile);
-	return (d->filename != nullptr && d->filename[0] != '\0') ? d->filename : nullptr;
+	if (d->filename && d->filename[0] != '\0') {
+		return d->filename;
+	} else if (d->filenameW && d->filenameW[0] != L'\0') {
+		const_cast<RpFilePrivate*>(d)->filename = strdup(W2U8(d->filenameW).c_str());
+		return d->filename;
+	}
+
+	return nullptr;
+}
+
+/**
+ * Get the filename.
+ * Get the filename. (Windows only: returns UTF-16.)
+ * @return Filename (UTF-16) (May be nullptr if the filename is not available.)
+ */
+const wchar_t *RpFile::filenameW(void) const
+{
+	RP_D(const RpFile);
+	return (d->filenameW != nullptr && d->filenameW[0] != L'\0') ? d->filenameW : nullptr;
 }
 
 /** Extra functions **/

src/libromdata/RomDataFactory.cpp
@@ -868,7 +868,7 @@ RomData *RomDataFactory::create(IRpFile *file, unsigned int attrs)
  * types must be supported by the RomData subclass in order to
  * be returned.
  *
- * @param filename ROM filename
+ * @param filename ROM filename (UTF-8)
  * @param attrs RomDataAttr bitfield. If set, RomData subclass must have the specified attributes.
  * @return RomData subclass, or nullptr if the ROM isn't supported.
  */
@@ -890,6 +890,46 @@ RomData *RomDataFactory::create(const char *filename, unsigned int attrs)
 	// TODO: Check for RomData subclasses that support directories.
 	return nullptr;
 }
+
+#ifdef _WIN32
+/**
+ * Create a RomData subclass for the specified ROM file.
+ *
+ * This version creates a base RpFile for the RomData object.
+ * It does not support extended virtual filesystems like GVfs
+ * or KIO, but it does support directories.
+ *
+ * NOTE: RomData::isValid() is checked before returning a
+ * created RomData instance, so returned objects can be
+ * assumed to be valid as long as they aren't nullptr.
+ *
+ * If imgbf is non-zero, at least one of the specified image
+ * types must be supported by the RomData subclass in order to
+ * be returned.
+ *
+ * @param filenameW ROM filename (UTF-16)
+ * @param attrs RomDataAttr bitfield. If set, RomData subclass must have the specified attributes.
+ * @return RomData subclass, or nullptr if the ROM isn't supported.
+ */
+RomData *RomDataFactory::create(const wchar_t *filenameW, unsigned int attrs)
+{
+	// Check if this is a file or a directory.
+	// If it's a file, we'll create an RpFile and then
+	// call create(IRpFile*,unsigned int).
+	if (!FileSystem::is_directory(filenameW)) {
+		// Not a directory.
+		RpFile *const file = new RpFile(filenameW, RpFile::FM_OPEN_READ_GZ);
+		if (file->isOpen()) {
+			RomData *const romData = create(file, attrs);
+			file->unref();
+			return romData;
+		}
+	}
+
+	// TODO: Check for RomData subclasses that support directories.
+	return nullptr;
+}
+#endif /* _WIN32 */
 
 /**
  * Initialize the vector of supported file extensions.

src/libromdata/RomDataFactory.hpp
@@ -92,12 +92,36 @@ class RomDataFactory
 		 * types must be supported by the RomData subclass in order to
 		 * be returned.
 		 *
-		 * @param filename ROM filename
+		 * @param filename ROM filename (UTF-8)
 		 * @param attrs RomDataAttr bitfield. If set, RomData subclass must have the specified attributes.
 		 * @return RomData subclass, or nullptr if the ROM isn't supported.
 		 */
 		RP_LIBROMDATA_PUBLIC
 		static LibRpBase::RomData *create(const char *filename, unsigned int attrs = 0);
+
+#ifdef _WIN32
+		/**
+		 * Create a RomData subclass for the specified ROM file.
+		 *
+		 * This version creates a base RpFile for the RomData object.
+		 * It does not support extended virtual filesystems like GVfs
+		 * or KIO, but it does support directories.
+		 *
+		 * NOTE: RomData::isValid() is checked before returning a
+		 * created RomData instance, so returned objects can be
+		 * assumed to be valid as long as they aren't nullptr.
+		 *
+		 * If imgbf is non-zero, at least one of the specified image
+		 * types must be supported by the RomData subclass in order to
+		 * be returned.
+		 *
+		 * @param filenameW ROM filename (UTF-16)
+		 * @param attrs RomDataAttr bitfield. If set, RomData subclass must have the specified attributes.
+		 * @return RomData subclass, or nullptr if the ROM isn't supported.
+		 */
+		RP_LIBROMDATA_PUBLIC
+		static LibRpBase::RomData *create(const wchar_t *filenameW, unsigned int attrs = 0);
+#endif /* _WIN32 */
 
 		struct ExtInfo {
 			const char *ext;

src/librpfile/FileSystem.hpp
@@ -184,9 +184,23 @@ std::string replace_ext(const char *filename, const char *ext);
  * Symbolic links are NOT resolved; otherwise wouldn't check
  * if the specified file was a symlink itself.
  *
+ * @param filename Filename (UTF-8)
  * @return True if the file is a symbolic link; false if not.
  */
 bool is_symlink(const char *filename);
+
+#ifdef _WIN32
+/**
+ * Check if the specified file is a symbolic link.
+ *
+ * Symbolic links are NOT resolved; otherwise wouldn't check
+ * if the specified file was a symlink itself.
+ *
+ * @param filenameW Filename (UTF-16)
+ * @return True if the file is a symbolic link; false if not.
+ */
+bool is_symlink(const wchar_t *filenameW);
+#endif /* _WIN32 */
 
 /**
  * Resolve a symbolic link.
@@ -194,19 +208,45 @@ bool is_symlink(const char *filename);
  * If the specified filename is not a symbolic link,
  * the filename will be returned as-is.
  *
- * @param filename Filename of symbolic link.
+ * @param filename Filename of symbolic link (UTF-8)
  * @return Resolved symbolic link, or empty string on error.
  */
 std::string resolve_symlink(const char *filename);
+
+#ifdef _WIN32
+/**
+ * Resolve a symbolic link.
+ *
+ * If the specified filename is not a symbolic link,
+ * the filename will be returned as-is.
+ *
+ * @param filenameW Filename of symbolic link (UTF-16)
+ * @return Resolved symbolic link, or empty string on error.
+ */
+std::wstring resolve_symlink(const wchar_t *filenameW);
+#endif /* _WIN32 */
 
 /**
  * Check if the specified file is a directory.
  *
  * Symbolic links are resolved as per usual directory traversal.
  *
+ * @param filename Filename to check (UTF-8)
  * @return True if the file is a directory; false if not.
  */
 bool is_directory(const char *filename);
+
+#ifdef _WIN32
+/**
+ * Check if the specified file is a directory.
+ *
+ * Symbolic links are resolved as per usual directory traversal.
+ *
+ * @param filenameW Filename to check (UTF-16)
+ * @return True if the file is a directory; false if not.
+ */
+bool is_directory(const wchar_t *filenameW);
+#endif /* _WIN32 */
 
 /**
  * Is a file located on a "bad" file system?

src/librpfile/FileSystem_posix.cpp
@@ -234,6 +234,7 @@ int delete_file(const char *filename)
  * Symbolic links are NOT resolved; otherwise wouldn't check
  * if the specified file was a symlink itself.
  *
+ * @param filename Filename (UTF-8)
  * @return True if the file is a symbolic link; false if not.
  */
 bool is_symlink(const char *filename)
@@ -271,7 +272,7 @@ bool is_symlink(const char *filename)
  * If the specified filename is not a symbolic link,
  * the filename will be returned as-is.
  *
- * @param filename Filename of symbolic link.
+ * @param filename Filename of symbolic link (UTF-8)
  * @return Resolved symbolic link, or empty string on error.
  */
 string resolve_symlink(const char *filename)
@@ -297,6 +298,7 @@ string resolve_symlink(const char *filename)
  *
  * Symbolic links are resolved as per usual directory traversal.
  *
+ * @param filename Filename to check (UTF-8)
  * @return True if the file is a directory; false if not.
  */
 bool is_directory(const char *filename)

src/librpfile/RelatedFile.cpp
@@ -11,8 +11,11 @@
 #include "FileSystem.hpp"
 #include "RpFile.hpp"
 
-// C++ STL classes.
+// C++ STL classes
 using std::string;
+#ifdef _WIN32
+using std::wstring;
+#endif /* _WIN32 */
 
 namespace LibRpFile { namespace FileSystem {
 
@@ -76,8 +79,8 @@ IRpFile *openRelatedFile(const char *filename, const char *basename, const char 
 		[](unsigned char c) noexcept -> char { return std::toupper(c); });
 
 	// Attempt to open the related file.
-	string rel_filename = s_dir + s_basename + s_ext;
-	IRpFile *test_file = new RpFile(rel_filename, RpFile::FM_OPEN_READ);
+	string s_rel_filename = s_dir + s_basename + s_ext;
+	IRpFile *test_file = new RpFile(s_rel_filename, RpFile::FM_OPEN_READ);
 	if (!test_file->isOpen()) {
 		// Error opening the related file.
 		test_file->unref();
@@ -85,8 +88,8 @@ IRpFile *openRelatedFile(const char *filename, const char *basename, const char 
 		// Try again with a lowercase extension.
 		std::transform(s_ext.begin(), s_ext.end(), s_ext.begin(),
 			[](unsigned char c) noexcept -> char { return std::tolower(c); });
-		rel_filename.replace(rel_filename.size() - s_ext.size(), s_ext.size(), s_ext);
-		test_file = new RpFile(rel_filename, RpFile::FM_OPEN_READ);
+		s_rel_filename.replace(s_rel_filename.size() - s_ext.size(), s_ext.size(), s_ext);
+		test_file = new RpFile(s_rel_filename, RpFile::FM_OPEN_READ);
 		if (!test_file->isOpen()) {
 			// Still can't open the related file.
 			UNREF_AND_NULL_NOCHK(test_file);
@@ -104,5 +107,96 @@ IRpFile *openRelatedFile(const char *filename, const char *basename, const char 
 	}
 	return test_file;
 }
+
+#ifdef _WIN32
+/**
+ * Attempt to open a related file. (read-only)
+ *
+ * Related files are located in the same directory as the
+ * primary file, but may have a different filename and/or
+ * file extension.
+ *
+ * If the primary file is a symlink, the related file may
+ * be located in the original file's directory.
+ *
+ * @param filename	[in] Primary filename. (UTF-16)
+ * @param basename	[in,opt] New basename. (UTF-16) If nullptr, uses the existing basename.
+ * @param ext		[in] New extension, including leading dot. (UTF-16)
+ * @return IRpFile*, or nullptr if not found.
+ */
+IRpFile *openRelatedFile(const wchar_t *filenameW, const wchar_t *basenameW, const wchar_t *extW)
+{
+	assert(filenameW != nullptr);
+	assert(extW != nullptr);
+	if (!filenameW || !extW) {
+		return nullptr;
+	}
+
+	// Get the directory portion of the filename.
+	wstring ws_dir = filenameW;
+	const size_t slash_pos = ws_dir.find_last_of(DIR_SEP_CHR);
+	if (slash_pos != string::npos) {
+		ws_dir.resize(slash_pos+1);
+	} else {
+		// No directory. Probably a filename in the
+		// current directory, e.g. when using `rpcli`.
+		ws_dir.clear();
+	}
+
+	// Get the base name.
+	wstring ws_basename;
+	if (basenameW) {
+		ws_basename = basenameW;
+	} else {
+		ws_basename = &filenameW[slash_pos+1];
+
+		// Check for any dots.
+		const size_t dot_pos = ws_basename.find_last_of(L'.');
+		if (dot_pos != string::npos) {
+			// Remove the extension.
+			ws_basename.resize(dot_pos);
+		}
+	}
+
+	// NOTE: Windows 10 1709 supports per-directory case-sensitivity on NTFS,
+	// and Linux 5.2 supports per-directory case-insensitivity on EXT4.
+	// Hence, we should check for both uppercase and lowercase extensions
+	// on all platforms.
+
+	// Check for uppercase extensions first.
+	wstring ws_ext = extW;
+	std::transform(ws_ext.begin(), ws_ext.end(), ws_ext.begin(),
+		[](wchar_t c) noexcept -> wchar_t { return std::towupper(c); });
+
+	// Attempt to open the related file.
+	wstring ws_rel_filename = ws_dir + ws_basename + ws_ext;
+	IRpFile *test_file = new RpFile(ws_rel_filename, RpFile::FM_OPEN_READ);
+	if (!test_file->isOpen()) {
+		// Error opening the related file.
+		test_file->unref();
+
+		// Try again with a lowercase extension.
+		std::transform(ws_ext.begin(), ws_ext.end(), ws_ext.begin(),
+			[](wchar_t c) noexcept -> wchar_t { return std::towlower(c); });
+		ws_rel_filename.replace(ws_rel_filename.size() - ws_ext.size(), ws_ext.size(), ws_ext);
+		test_file = new RpFile(ws_rel_filename, RpFile::FM_OPEN_READ);
+		if (!test_file->isOpen()) {
+			// Still can't open the related file.
+			UNREF_AND_NULL_NOCHK(test_file);
+		}
+	}
+
+	if (!test_file && FileSystem::is_symlink(filenameW)) {
+		// Could not open the related file, but the
+		// primary file is a symlink. Dereference the
+		// symlink and check the original directory.
+		const wstring deref_filename = FileSystem::resolve_symlink(filenameW);
+		if (!deref_filename.empty()) {
+			test_file = openRelatedFile(deref_filename.c_str(), basenameW, extW);
+		}
+	}
+	return test_file;
+}
+#endif /* _WIN32 */
 
 } }

src/librpfile/RelatedFile.hpp
@@ -24,11 +24,30 @@ namespace LibRpFile { namespace FileSystem {
  * If the primary file is a symlink, the related file may
  * be located in the original file's directory.
  *
- * @param filename	[in] Primary filename.
- * @param basename	[in,opt] New basename. If nullptr, uses the existing basename.
- * @param ext		[in] New extension, including leading dot.
+ * @param filename	[in] Primary filename. (UTF-8)
+ * @param basename	[in,opt] New basename. (UTF-8) If nullptr, uses the existing basename.
+ * @param ext		[in] New extension, including leading dot. (UTF-8)
  * @return IRpFile*, or nullptr if not found.
  */
 LibRpFile::IRpFile *openRelatedFile(const char *filename, const char *basename, const char *ext);
+
+#ifdef _WIN32
+/**
+ * Attempt to open a related file. (read-only)
+ *
+ * Related files are located in the same directory as the
+ * primary file, but may have a different filename and/or
+ * file extension.
+ *
+ * If the primary file is a symlink, the related file may
+ * be located in the original file's directory.
+ *
+ * @param filename	[in] Primary filename. (UTF-16)
+ * @param basename	[in,opt] New basename. (UTF-16) If nullptr, uses the existing basename.
+ * @param ext		[in] New extension, including leading dot. (UTF-16)
+ * @return IRpFile*, or nullptr if not found.
+ */
+LibRpFile::IRpFile *openRelatedFile(const wchar_t *filenameW, const wchar_t *basenameW, const wchar_t *extW);
+#endif /* _WIN32 */
 
 } }

src/librpfile/RpFile.hpp
@@ -48,13 +48,27 @@ class RpFile final : public IRpFile
 		/**
 		 * Open a file.
 		 * NOTE: Files are always opened in binary mode.
-		 * @param filename Filename.
-		 * @param mode File mode.
+		 * @param filename Filename (UTF-8)
+		 * @param mode File mode
 		 */
 		RP_LIBROMDATA_PUBLIC
 		RpFile(const char *filename, FileMode mode);
 		RP_LIBROMDATA_PUBLIC
 		RpFile(const std::string &filename, FileMode mode);
+
+#ifdef _WIN32
+		/**
+		 * Open a file.
+		 * NOTE: Files are always opened in binary mode.
+		 * @param filenameW Filename (UTF-16)
+		 * @param mode File mode
+		 */
+		RP_LIBROMDATA_PUBLIC
+		RpFile(const wchar_t *filenameW, FileMode mode);
+		RP_LIBROMDATA_PUBLIC
+		RpFile(const std::wstring &filenameW, FileMode mode);
+#endif /* _WIN32 */
+
 	private:
 		void init(void);
 	protected:
@@ -140,10 +154,19 @@ class RpFile final : public IRpFile
 
 		/**
 		 * Get the filename.
-		 * @return Filename. (May be nullptr if the filename is not available.)
+		 * @return Filename (UTF-8) (May be nullptr if the filename is not available.)
 		 */
 		RP_LIBROMDATA_PUBLIC
 		const char *filename(void) const final;
+
+#ifdef _WIN32
+		/**
+		 * Get the filename. (Windows only: returns UTF-16.)
+		 * @return Filename (UTF-16) (May be nullptr if the filename is not available.)
+		 */
+		RP_LIBROMDATA_PUBLIC
+		const wchar_t *filenameW(void) const;
+#endif /* _WIN32 */
 
 	public:
 		/** Extra functions **/

commit 271099ea434b0ec0b998124686df1bb7f982e004

[librpfile] RpFile_win32.cpp: Use `filenameW[0]`, not `filename[0]`.
`filename` might not be initialized at this point.

This probably broke device access on Windows.

This fixes a regression from commit 08a653722cd3ea5afc3039d48ec9f2cfb30b300b.
([win32] Add wchar_t* overloads to RpFile and related classes.)

src/librpfile/win32/RpFile_win32.cpp
@@ -133,7 +133,7 @@ int RpFilePrivate::reOpenFile(void)
 	{
 		// Drive letter. Append '\\'.
 		wchar_t *drvfilename = static_cast<wchar_t*>(malloc(4*sizeof(wchar_t)));
-		drvfilename[0] = filename[0];
+		drvfilename[0] = filenameW[0];
 		drvfilename[1] = _T(':');
 		drvfilename[2] = _T('\\');
 		drvfilename[3] = _T('\0');