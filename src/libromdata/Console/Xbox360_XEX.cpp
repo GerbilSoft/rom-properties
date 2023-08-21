GerbilSoft/rom-properties

commit bfd6ddf4b421e204212735ea755b11a3a0947c3f

[librpbase] IDiscReader: Use shared_ptr<> instead of RefBase.
ParititonFile: FIXME: Cannot take IDiscReaderPtr because PartitionFile
is created by various IDiscReader subclasses, so they can't get their
own shared_ptr<>.

GdiPartition: Hack for IsoPartition due to it only working with
IDiscReader. Need to clean this up at some point.
- Verified that GDIs still show up correctly.

src/libromdata/Console/Xbox360_XEX.cpp
@@ -206,15 +206,15 @@ class Xbox360_XEX_Private final : public RomDataPrivate
 	public:
 		// CBC reader for encrypted PE executables.
 		// Also used for unencrypted executables.
-		CBCReader *peReader;
+		CBCReaderPtr peReader;
 		EXE *pe_exe;
 		Xbox360_XDBF *pe_xdbf;
 
 		/**
 		 * Initialize the PE executable reader.
-		 * @return peReader on success; nullptr on error.
+		 * @return 0 on success (saved in this->peReader); negative POSIX error code on error.
 		 */
-		CBCReader *initPeReader(void);
+		int initPeReader(void);
 
 		/**
 		 * Initialize the EXE object.
@@ -293,7 +293,6 @@ Xbox360_XEX_Private::Xbox360_XEX_Private(const IRpFilePtr &file)
 	, xexType(XexType::Unknown)
 	, isExecutionIDLoaded(false)
 	, keyInUse(-1)
-	, peReader(nullptr)
 	, pe_exe(nullptr)
 	, pe_xdbf(nullptr)
 {
@@ -308,7 +307,6 @@ Xbox360_XEX_Private::~Xbox360_XEX_Private()
 {
 	UNREF(pe_xdbf);
 	UNREF(pe_exe);
-	UNREF(peReader);
 }
 
 /**
@@ -554,28 +552,31 @@ const XEX2_Resource_Info *Xbox360_XEX_Private::getXdbfResInfo(const char *resour
 
 /**
  * Initialize the PE executable reader.
- * @return peReader on success; nullptr on error.
+ * @return 0 on success (saved in this->peReader); negative POSIX error code on error.
  */
-CBCReader *Xbox360_XEX_Private::initPeReader(void)
+int Xbox360_XEX_Private::initPeReader(void)
 {
 	if (peReader) {
 		// PE Reader is already initialized.
-		return peReader;
+		return 0;
 	}
 #ifdef ENABLE_LIBMSPACK
 	if (!lzx_peHeader.empty()) {
 		// LZX has been decompressed.
-		return peReader;
+		return 0;
 	}
 #endif /* ENABLE_LIBMSPACK */
+
+	// NOTE: Since we're returning CBCReaderPtr&, we'll return
+	// peReader even if it isn't initialized.
 	if (!file) {
 		// File is closed. Can't initialize PE Reader.
-		return nullptr;
+		return -EIO;
 	}
 
 	if (xexType <= XexType::Unknown || xexType >= XexType::Max) {
 		// Invalid XEX type.
-		return nullptr;
+		return -EIO;
 	}
 
 	// Get the file format info.
@@ -583,7 +584,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 	size_t size = getOptHdrData(XEX2_OPTHDR_FILE_FORMAT_INFO, u8_ffi);
 	if (size < sizeof(fileFormatInfo)) {
 		// Seek and/or read error.
-		return nullptr;
+		return -EIO;
 	}
 
 	// Copy the file format information.
@@ -602,14 +603,13 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 	//
 	// We need to decrypt before we decompress, so we can't check
 	// if the decryption key works until decompression is done.
-	array<CBCReader*, 2> reader;
-	reader.fill(nullptr);
+	array<CBCReaderPtr, 2> reader;
 
 	// Create the CBCReader for decryption.
 	const size_t pe_length = (size_t)file->size() - xex2Header.pe_offset;
 	if (fileFormatInfo.encryption_type == XEX2_ENCRYPTION_TYPE_NONE) {
 		// No encryption.
-		reader[0] = new CBCReader(file, xex2Header.pe_offset, pe_length, nullptr, nullptr);
+		reader[0] = std::make_shared<CBCReader>(file, xex2Header.pe_offset, pe_length, nullptr, nullptr);
 	}
 #ifdef ENABLE_DECRYPTION
 	else {
@@ -684,10 +684,10 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			}
 
 			// Initialize the CBCReader.
-			reader[i] = new CBCReader(file, xex2Header.pe_offset, pe_length, dec_title_key, zero16);
+			reader[i] = std::make_shared<CBCReader>(file, xex2Header.pe_offset, pe_length, dec_title_key, zero16);
 			if (!reader[i]->isOpen()) {
 				// Unable to open the CBCReader.
-				UNREF_AND_NULL_NOCHK(reader[i]);
+				reader[i].reset();
 				continue;
 			}
 
@@ -700,9 +700,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 	    (!reader[1] || !reader[1]->isOpen()))
 	{
 		// Unable to open any CBCReader.
-		UNREF(reader[0]);
-		UNREF(reader[1]);
-		return nullptr;
+		return -EIO;
 	}
 
 	// Check the compression type.
@@ -719,9 +717,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			assert(fileFormatInfo.size > sizeof(fileFormatInfo));
 			if (fileFormatInfo.size <= sizeof(fileFormatInfo)) {
 				// No segment information is available.
-				UNREF(reader[0]);
-				UNREF(reader[1]);
-				return nullptr;
+				return -EIO;
 			}
 
 			const uint32_t seg_len = fileFormatInfo.size - sizeof(fileFormatInfo);
@@ -752,9 +748,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			assert(fileFormatInfo.size >= sizeof(fileFormatInfo) + sizeof(XEX2_Compression_Normal_Header));
 			if (fileFormatInfo.size < sizeof(fileFormatInfo) + sizeof(XEX2_Compression_Normal_Header)) {
 				// No segment information is available.
-				UNREF(reader[0]);
-				UNREF(reader[1]);
-				return nullptr;
+				return -EIO;
 			}
 
 			// Image size must be at least 8 KB.
@@ -765,9 +759,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			assert(image_size >= PE_HEADER_SIZE);
 			if (image_size < PE_HEADER_SIZE) {
 				// Too small.
-				UNREF(reader[0]);
-				UNREF(reader[1]);
-				return nullptr;
+				return -EIO;
 			}
 
 			// Window size.
@@ -799,9 +791,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			const off64_t fileSize = file->size();
 			if (fileSize > 64*1024*1024 || image_size > 64*1024*1024) {
 				// 64 MB is our compressed and uncompressed limit.
-				UNREF(reader[0]);
-				UNREF(reader[1]);
-				return nullptr;
+				return -ENOMEM;
 			}
 
 			// Compressed EXE buffer.
@@ -819,9 +809,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			unsigned int rd_idx = (reader[0] ? 0 : 1);
 			if (!reader[rd_idx]) {
 				// No readers available...
-				// FIXME: Not sure if this is needed...
-				UNREF(reader[!rd_idx]);
-				return nullptr;
+				return -EIO;
 			}
 
 			// Start at the beginning.
@@ -833,9 +821,11 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 				size = reader[rd_idx]->read(&lzx_blocks[!lzx_idx], sizeof(lzx_blocks[!lzx_idx]));
 				if (size != sizeof(lzx_blocks[!lzx_idx])) {
 					// Seek and/or read error.
-					UNREF(reader[0]);
-					UNREF(reader[1]);
-					return nullptr;
+					int err = reader[rd_idx]->lastError();
+					if (err == 0) {
+						err = EIO;
+					}
+					return -err;
 				}
 
 				// Does the block size make sense?
@@ -847,15 +837,14 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 					// Switch to the other reader.
 					if (rd_idx == 1) {
 						// No more readers...
-						UNREF(reader[1]);
-						return nullptr;
+						return -EIO;
 					}
-					UNREF_AND_NULL(reader[0]);
+					reader[0].reset();
 
 					// reader[1] might be nullptr here...
 					if (!reader[1]) {
 						// Cannot continue.
-						return nullptr;
+						return -EIO;
 					}
 					rd_idx = 1;
 
@@ -874,9 +863,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 				assert(block_size > sizeof(lzx_blocks[!lzx_idx]));
 				if (block_size <= sizeof(lzx_blocks[!lzx_idx])) {
 					// Block is missing the "next block" header...
-					UNREF(reader[0]);
-					UNREF(reader[1]);
-					return nullptr;
+					return -EIO;
 				}
 				block_size -= sizeof(lzx_blocks[!lzx_idx]);
 
@@ -886,9 +873,11 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 					size = reader[rd_idx]->read(&chunk_size, sizeof(chunk_size));
 					if (size != sizeof(chunk_size)) {
 						// Seek and/or read error.
-						UNREF(reader[0]);
-						UNREF(reader[1]);
-						return nullptr;
+						int err = reader[rd_idx]->lastError();
+						if (err == 0) {
+							err = EIO;
+						}
+						return -err;
 					}
 #if SYS_BYTEORDER == SYS_LIL_ENDIAN
 					chunk_size = be16_to_cpu(chunk_size);
@@ -902,18 +891,13 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 					// Do we have enough space?
 					if (p_dblk + chunk_size >= p_dblk_end) {
 						// Out of memory.
-						// TODO: Error reporting.
-						UNREF(reader[0]);
-						UNREF(reader[1]);
-						return nullptr;
+						return -ENOMEM;
 					}
 
 					size = reader[rd_idx]->read(p_dblk, chunk_size);
 					if (size != chunk_size) {
 						// Seek and/or read error.
-						UNREF(reader[0]);
-						UNREF(reader[1]);
-						return nullptr;
+						return -EIO;
 					}
 
 					p_dblk += chunk_size;
@@ -937,9 +921,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 				window_size, nullptr, 0);
 			if (res != MSPACK_ERR_OK) {
 				// Error decompressing the data.
-				UNREF(reader[0]);
-				UNREF(reader[1]);
-				return nullptr;
+				return -EIO;
 			}
 
 			// Verify the MZ header.
@@ -948,9 +930,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			if (mz != cpu_to_be16('MZ')) {
 				// MZ header is not valid.
 				// TODO: Other checks?
-				UNREF(reader[0]);
-				UNREF(reader[1]);
-				return nullptr;
+				return -EIO;
 			}
 
 			// Copy the PE header.
@@ -975,10 +955,9 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 			}
 
 			// Save the correct reader.
-			this->peReader = reader[rd_idx];
-			reader[rd_idx] = nullptr;
+			this->peReader = std::move(reader[rd_idx]);
 			keyInUse = rd_idx;
-			break;
+			return 0;
 		}
 #endif /* ENABLE_LIBMSPACK */
 	}
@@ -998,8 +977,7 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 				if (mz == cpu_to_be16('MZ')) {
 					// MZ header is valid.
 					// TODO: Other checks?
-					this->peReader = reader[i];
-					reader[i] = nullptr;
+					this->peReader = std::move(reader[i]);
 					keyInUse = static_cast<int>(i);
 					break;
 				}
@@ -1007,12 +985,8 @@ CBCReader *Xbox360_XEX_Private::initPeReader(void)
 		}
 	}
 
-	// Delete the incorrect CBCReaders.
-	UNREF(reader[0]);
-	UNREF(reader[1]);
-
 	// CBCReader is open and file decompression has been initialized.
-	return this->peReader;
+	return 0;
 }
 
 /**
@@ -1224,7 +1198,7 @@ const EXE *Xbox360_XEX_Private::initEXE(void)
 	} else
 #endif /* ENABLE_LIBMSPACK */
 	{
-		peFile_tmp = std::make_shared<PartitionFile>(peReader, 0, PE_HEADER_SIZE);
+		peFile_tmp = std::make_shared<PartitionFile>(peReader.get(), 0, PE_HEADER_SIZE);
 	}
 	if (peFile_tmp->isOpen()) {
 		EXE *const pe_exe_tmp = new EXE(peFile_tmp);
@@ -1291,7 +1265,7 @@ const Xbox360_XDBF *Xbox360_XEX_Private::initXDBF(void)
 				xdbf_physaddr -= (iter->vaddr - iter->physaddr);
 			}
 		}
-		peFile_tmp = std::make_shared<PartitionFile>(peReader, xdbf_physaddr, pResInfo->size);
+		peFile_tmp = std::make_shared<PartitionFile>(peReader.get(), xdbf_physaddr, pResInfo->size);
 	}
 	if (peFile_tmp->isOpen()) {
 		// FIXME: XEX1 XDBF is either encrypted or garbage...
@@ -1462,7 +1436,7 @@ void Xbox360_XEX::close(void)
 		d->pe_exe->close();
 	}
 
-	UNREF_AND_NULL(d->peReader);
+	d->peReader.reset();
 
 #ifdef ENABLE_LIBMSPACK
 	d->lzx_peHeader.clear();
@@ -1631,7 +1605,7 @@ int Xbox360_XEX::loadFieldData(void)
 
 	// Is the encryption key available?
 	bool noKeyAvailable = false;
-	if (d->initPeReader() != nullptr) {
+	if (d->initPeReader() == 0) {
 		if (d->keyInUse < 0 &&
 		    d->fileFormatInfo.encryption_type != cpu_to_be16(XEX2_ENCRYPTION_TYPE_NONE))
 		{

src/libromdata/Console/Dreamcast.cpp
@@ -39,7 +39,7 @@ class DreamcastPrivate final : public RomDataPrivate
 {
 	public:
 		DreamcastPrivate(const IRpFilePtr &file);
-		~DreamcastPrivate() final;
+		~DreamcastPrivate() final = default;
 
 	private:
 		typedef RomDataPrivate super;
@@ -63,12 +63,13 @@ class DreamcastPrivate final : public RomDataPrivate
 		};
 		DiscType discType;
 
-		// Disc reader.
-		union {
-			IDiscReader *discReader;
-			GdiReader *gdiReader;
-		};
-		IsoPartition *isoPartition;
+		// Disc reader
+		// TODO: Before shared_ptr<>, discReader and gdiReader were in a union{}.
+		// TODO: Use std::variant<>?
+		IDiscReaderPtr discReader;
+		GdiReaderPtr gdiReader;
+
+		IsoPartitionPtr isoPartition;
 
 		// Disc header.
 		DC_IP0000_BIN_t discHeader;
@@ -145,18 +146,10 @@ const RomDataInfo DreamcastPrivate::romDataInfo = {
 DreamcastPrivate::DreamcastPrivate(const IRpFilePtr &file)
 	: super(file, &romDataInfo)
 	, discType(DiscType::Unknown)
-	, discReader(nullptr)
-	, isoPartition(nullptr)
 	, iso_start_offset(-1)
 {
 	// Clear the disc header struct.
 	memset(&discHeader, 0, sizeof(discHeader));
-}
-
-DreamcastPrivate::~DreamcastPrivate()
-{
-	UNREF(isoPartition);
-	UNREF(discReader);
 }
 
 /**
@@ -195,7 +188,7 @@ rp_image_const_ptr DreamcastPrivate::load0GDTEX(void)
 	if (pvrData) {
 		// Image has already been loaded.
 		return pvrData->image();
-	} else if (!this->file || !this->discReader) {
+	} else if (!this->file || (!this->discReader && !this->gdiReader)) {
 		// Can't load the image.
 		return nullptr;
 	}
@@ -208,11 +201,11 @@ rp_image_const_ptr DreamcastPrivate::load0GDTEX(void)
 		} else {
 			// Standalone track.
 			// Using the ISO start offset calculated earlier.
-			isoPartition = new IsoPartition(discReader, 0, iso_start_offset);
+			isoPartition = std::make_shared<IsoPartition>(discReader, 0, iso_start_offset);
 		}
 		if (!isoPartition->isOpen()) {
 			// Unable to open the ISO-9660 partition.
-			UNREF_AND_NULL_NOCHK(isoPartition);
+			isoPartition.reset();
 			return nullptr;
 		}
 	}
@@ -362,7 +355,7 @@ Dreamcast::Dreamcast(const IRpFilePtr &file)
 			d->mimeType = "application/x-dreamcast-rom";	// unofficial
 			memcpy(&d->discHeader, &sector, sizeof(d->discHeader));
 			d->iso_start_offset = -1;
-			d->discReader = new DiscReader(d->file);
+			d->discReader = std::make_shared<DiscReader>(d->file);
 			if (d->file->size() <= 64*1024) {
 				// 64 KB is way too small for a Dreamcast disc image.
 				// We'll assume this is IP.bin.
@@ -375,7 +368,7 @@ Dreamcast::Dreamcast(const IRpFilePtr &file)
 			d->mimeType = "application/x-dreamcast-rom";	// unofficial
 			const uint8_t *const data = cdromSectorDataPtr(&sector);
 			memcpy(&d->discHeader, data, sizeof(d->discHeader));
-			d->discReader = new Cdrom2352Reader(d->file);
+			d->discReader = std::make_shared<Cdrom2352Reader>(d->file);
 			d->iso_start_offset = static_cast<int>(cdrom_msf_to_lba(&sector.msf));
 			break;
 		}
@@ -383,12 +376,12 @@ Dreamcast::Dreamcast(const IRpFilePtr &file)
 		case DreamcastPrivate::DiscType::GDI: {
 			// GD-ROM cuesheet.
 			// iso_start_offset isn't used for GDI.
-			d->gdiReader = new GdiReader(d->file);
+			d->gdiReader = std::make_shared<GdiReader>(d->file);
 			// Read the actual track 3 disc header.
 			const int lba_track03 = d->gdiReader->startingLBA(3);
 			if (lba_track03 < 0) {
 				// Error getting the track 03 LBA.
-				UNREF_AND_NULL_NOCHK(d->gdiReader);
+				d->gdiReader.reset();
 				d->file.reset();
 				return;
 			}
@@ -415,8 +408,9 @@ void Dreamcast::close(void)
 
 	// Close any child RomData subclasses.
 	d->pvrData.reset();
-	UNREF_AND_NULL(d->isoPartition);
-	UNREF_AND_NULL(d->discReader);
+	d->isoPartition.reset();
+	d->gdiReader.reset();
+	d->discReader.reset();
 
 	// Call the superclass function.
 	super::close();
@@ -786,7 +780,8 @@ int Dreamcast::loadFieldData(void)
 		isoData = d->gdiReader->openIsoRomData(3);
 	} else {
 		// ISO object for ISO-9660 PVD
-		shared_ptr<PartitionFile> isoFile = std::make_shared<PartitionFile>(d->discReader, 0, d->discReader->size());
+		PartitionFilePtr isoFile = std::make_shared<PartitionFile>(
+			d->discReader.get(), 0, d->discReader->size());
 		if (isoFile->isOpen()) {
 			isoData = new ISO(isoFile);
 		}

src/libromdata/Console/GameCube.cpp
@@ -44,6 +44,7 @@ using namespace LibRpTexture;
 
 // C++ STL classes
 using std::array;
+using std::shared_ptr;
 using std::string;
 using std::unique_ptr;
 using std::vector;
@@ -100,23 +101,27 @@ class GameCubePrivate final : public RomDataPrivate
 
 		// Disc type and reader.
 		int discType;
-		IDiscReader *discReader;
+		IDiscReaderPtr discReader;
 
 		// Disc header.
 		GCN_DiscHeader discHeader;
 		RVL_RegionSetting regionSetting;
 
 		// opening.bnr
-		union {
-			struct {
-				// opening.bnr file and object.
-				GcnPartition *partition;
-				GameCubeBNR *data;
-			} gcn;
-			struct {
-				// Wii opening.bnr. (IMET section)
-				Wii_IMET_t *imet;
-			} wii;
+		struct {
+			// FIXME: gcn_partition used to be in the 'gcn' union.
+			// Can't do that with shared_ptr...
+			GcnPartitionPtr gcn_partition;	// GcnPartition for opening.bnr
+			union {
+				struct {
+					// GameCube opening.bnr object
+					GameCubeBNR *data;
+				} gcn;
+				struct {
+					// Wii opening.bnr (IMET section)
+					Wii_IMET_t *imet;
+				} wii;
+			};
 		} opening_bnr;
 
 		// Do we have certain things loaded?
@@ -132,19 +137,19 @@ class GameCubePrivate final : public RomDataPrivate
 		 * Decoded from the actual on-disc tables.
 		 */
 		struct WiiPartEntry {
-			off64_t start;		// Starting address, in bytes.
-			off64_t size;		// Estimated partition size, in bytes.
+			off64_t start;		// Starting address, in bytes
+			off64_t size;		// Estimated partition size, in bytes
 
-			WiiPartition *partition;	// Partition object.
-			uint32_t type;		// Partition type. (See WiiPartitionType.)
-			uint8_t vg;		// Volume group number.
-			uint8_t pt;		// Partition number.
+			WiiPartitionPtr partition;	// Partition object
+			uint32_t type;		// Partition type (See WiiPartitionType.)
+			uint8_t vg;		// Volume group number
+			uint8_t pt;		// Partition number
 		};
 		vector<WiiPartEntry> wiiPtbl;
 
 		// Pointers to specific partitions within wiiPtbl.
-		WiiPartition *updatePartition;
-		WiiPartition *gamePartition;
+		WiiPartitionPtr updatePartition;
+		WiiPartitionPtr gamePartition;
 
 		/**
 		 * Load the Wii volume group and partition tables.
@@ -198,7 +203,7 @@ class GameCubePrivate final : public RomDataPrivate
 		 * @param partition Partition to check.
 		 * @return nullptr if partition is readable; error message if not.
 		 */
-		const char *wii_getCryptoStatus(WiiPartition *partition);
+		const char *wii_getCryptoStatus(const WiiPartitionPtr &partition);
 };
 
 ROMDATA_IMPL(GameCube)
@@ -264,7 +269,6 @@ const uint8_t GameCubePrivate::nddemo_header[64] = {
 GameCubePrivate::GameCubePrivate(const IRpFilePtr &file)
 	: super(file, &romDataInfo)
 	, discType(DISC_UNKNOWN)
-	, discReader(nullptr)
 	, hasRegionCode(false)
 	, wiiPtblLoaded(false)
 	, hasDiscHeader(false)
@@ -275,7 +279,9 @@ GameCubePrivate::GameCubePrivate(const IRpFilePtr &file)
 	// Clear the various structs.
 	memset(&discHeader, 0, sizeof(discHeader));
 	memset(&regionSetting, 0, sizeof(regionSetting));
-	memset(&opening_bnr, 0, sizeof(opening_bnr));
+
+	// opening_bnr has some C++ objects, so manually clear it.
+	opening_bnr.gcn.data = nullptr;
 }
 
 GameCubePrivate::~GameCubePrivate()
@@ -285,9 +291,6 @@ GameCubePrivate::~GameCubePrivate()
 	gamePartition = nullptr;
 
 	// Clear the existing partition table vector.
-	for (WiiPartEntry &entry : wiiPtbl) {
-		UNREF(entry.partition);
-	}
 	wiiPtbl.clear();
 
 	if (discType > DISC_UNKNOWN) {
@@ -295,7 +298,7 @@ GameCubePrivate::~GameCubePrivate()
 		switch (discType & DISC_SYSTEM_MASK) {
 			case DISC_SYSTEM_GCN:
 				UNREF(opening_bnr.gcn.data);
-				UNREF(opening_bnr.gcn.partition);
+				opening_bnr.gcn_partition.reset();
 				break;
 			case DISC_SYSTEM_WII:
 				delete opening_bnr.wii.imet;
@@ -304,8 +307,6 @@ GameCubePrivate::~GameCubePrivate()
 				break;
 		}
 	}
-
-	UNREF(discReader);
 }
 
 /**
@@ -327,9 +328,6 @@ int GameCubePrivate::loadWiiPartitionTables(void)
 	}
 
 	// Clear the existing partition table vector.
-	for (WiiPartEntry &entry : wiiPtbl) {
-		UNREF(entry.partition);
-	}
 	wiiPtbl.clear();
 
 	// Assuming a maximum of 128 partitions per table.
@@ -425,7 +423,7 @@ int GameCubePrivate::loadWiiPartitionTables(void)
 
 	// Create the WiiPartition objects.
 	for (auto &p : wiiPtbl) {
-		p.partition = new WiiPartition(discReader, p.start, p.size,
+		p.partition = std::make_shared<WiiPartition>(discReader, p.start, p.size,
 			(WiiPartition::CryptoMethod)cryptoMethod);
 
 		if (p.type == RVL_PT_UPDATE && !updatePartition) {
@@ -474,7 +472,7 @@ string GameCubePrivate::getPublisher(void) const
  */
 int GameCubePrivate::gcn_loadOpeningBnr(void)
 {
-	assert(discReader != nullptr);
+	assert((bool)discReader);
 	assert((discType & DISC_SYSTEM_MASK) == DISC_SYSTEM_GCN);
 	if (!discReader) {
 		return -EIO;
@@ -492,10 +490,9 @@ int GameCubePrivate::gcn_loadOpeningBnr(void)
 	// NOTE: The GCN partition needs to stay open,
 	// since we have a subclass for reading the object.
 	// since we don't need to access more than one file.
-	GcnPartition *const gcnPartition = new GcnPartition(discReader, 0);
+	GcnPartitionPtr gcnPartition = std::make_shared<GcnPartition>(discReader, 0);
 	if (!gcnPartition->isOpen()) {
 		// Could not open the partition.
-		gcnPartition->unref();
 		return -EIO;
 	}
 
@@ -503,7 +500,6 @@ int GameCubePrivate::gcn_loadOpeningBnr(void)
 	if (!f_opening_bnr) {
 		// Error opening "opening.bnr".
 		const int err = -gcnPartition->lastError();
-		gcnPartition->unref();
 		return err;
 	}
 
@@ -512,12 +508,11 @@ int GameCubePrivate::gcn_loadOpeningBnr(void)
 	if (!bnr->isOpen()) {
 		// Unable to open the subclass.
 		bnr->unref();
-		gcnPartition->unref();
 		return -EIO;
 	}
 
 	// GameCubeBNR subclass is open.
-	opening_bnr.gcn.partition = gcnPartition;
+	opening_bnr.gcn_partition = std::move(gcnPartition);
 	opening_bnr.gcn.data = bnr;
 	return 0;
 }
@@ -528,7 +523,7 @@ int GameCubePrivate::gcn_loadOpeningBnr(void)
  */
 int GameCubePrivate::wii_loadOpeningBnr(void)
 {
-	assert(discReader != nullptr);
+	assert((bool)discReader);
 	assert((discType & DISC_SYSTEM_MASK) == DISC_SYSTEM_WII);
 	if (!discReader) {
 		return -EIO;
@@ -660,7 +655,7 @@ int GameCubePrivate::wii_addBannerName(void)
  * @param partition Partition to check.
  * @return nullptr if partition is readable; error message if not.
  */
-const char *GameCubePrivate::wii_getCryptoStatus(WiiPartition *partition)
+const char *GameCubePrivate::wii_getCryptoStatus(const WiiPartitionPtr &partition)
 {
 	const KeyManager::VerifyResult res = partition->verifyResult();
 	if (res == KeyManager::VerifyResult::KeyNotFound) {
@@ -741,11 +736,11 @@ GameCube::GameCube(const IRpFilePtr &file)
 	switch (d->discType & GameCubePrivate::DISC_FORMAT_MASK) {
 		case GameCubePrivate::DISC_FORMAT_RAW:
 		case GameCubePrivate::DISC_FORMAT_PARTITION:
-			d->discReader = new DiscReader(d->file);
+			d->discReader = std::make_shared<DiscReader>(d->file);
 			break;
 		case GameCubePrivate::DISC_FORMAT_SDK:
 			// Skip the SDK header.
-			d->discReader = new DiscReader(d->file, 32768, -1);
+			d->discReader = std::make_shared<DiscReader>(d->file, 32768, -1);
 			break;
 
 		case GameCubePrivate::DISC_FORMAT_TGC: {
@@ -754,7 +749,7 @@ GameCube::GameCube(const IRpFilePtr &file)
 			// Check the TGC header for the disc offset.
 			const GCN_TGC_Header *tgcHeader = reinterpret_cast<const GCN_TGC_Header*>(header);
 			const uint32_t gcm_offset = be32_to_cpu(tgcHeader->header_size);
-			d->discReader = new DiscReader(d->file, gcm_offset, -1);
+			d->discReader = std::make_shared<DiscReader>(d->file, gcm_offset, -1);
 			break;
 		}
 
@@ -771,32 +766,28 @@ GameCube::GameCube(const IRpFilePtr &file)
 					break;
 				}
 
-				IRpFile *const wbfs0 = FileSystem::openRelatedFile(filename, nullptr, ".wbfs");
+				IRpFilePtr wbfs0 = FileSystem::openRelatedFile(filename, nullptr, ".wbfs");
 				if (unlikely(!wbfs0) || !wbfs0->isOpen()) {
 					// Unable to open the .wbfs file.
-					UNREF(wbfs0);
 					d->discType = GameCubePrivate::DISC_UNKNOWN;
 					break;
 				}
 
 				// Split .wbfs/.wbf1.
-				DualFile *const dualFile = new DualFile(wbfs0, d->file);
+				DualFile *const dualFile = std::make_shared<DualFile>(wbfs0, d->file);
 				if (!dualFile->isOpen()) {
 					// Unable to open DualFile.
-					dualFile->unref();
+					delete dualFile;
 					d->discType = GameCubePrivate::DISC_UNKNOWN;
 					break;
 				}
 
 				// DualFile maintains its own references, so unreference
 				// d->file and replace it with the DualFile.
-				IRpFile *const file_tmp = d->file;
-				d->file = dualFile;
-				file_tmp->unref();
-				wbfs0->unref();
+				d->file.reset(dualFile);
 
 				// Open the WbfsReader.
-				d->discReader = new WbfsReader(d->file);
+				d->discReader = std::make_shared<WbfsReader>(d->file);
 			} else*/ if ((d->discType & GameCubePrivate::DISC_FORMAT_MASK) == GameCubePrivate::DISC_FORMAT_WBFS) {
 				// First part of split WBFS.
 				// Check for a WBF1 file.
@@ -813,7 +804,7 @@ GameCube::GameCube(const IRpFilePtr &file)
 
 				if (likely(!wbfs1)) {
 					// Single .wbfs file.
-					d->discReader = new WbfsReader(d->file);
+					d->discReader = std::make_shared<WbfsReader>(d->file);
 					break;
 				}
 
@@ -832,7 +823,7 @@ GameCube::GameCube(const IRpFilePtr &file)
 				d->file.reset(dualFile);
 
 				// Open the WbfsReader.
-				d->discReader = new WbfsReader(d->file);
+				d->discReader = std::make_shared<WbfsReader>(d->file);
 			} else {
 				// Not supported.
 				d->discType = GameCubePrivate::DISC_UNKNOWN;
@@ -843,15 +834,15 @@ GameCube::GameCube(const IRpFilePtr &file)
 
 		case GameCubePrivate::DISC_FORMAT_CISO:
 			d->mimeType = "application/x-cso";
-			d->discReader = new CisoGcnReader(d->file);
+			d->discReader = std::make_shared<CisoGcnReader>(d->file);
 			break;
 		case GameCubePrivate::DISC_FORMAT_NASOS:
 			d->mimeType = "application/x-nasos-image";
-			d->discReader = new NASOSReader(d->file);
+			d->discReader = std::make_shared<NASOSReader>(d->file);
 			break;
 		case GameCubePrivate::DISC_FORMAT_GCZ:
 			d->mimeType = "application/x-gcz-image";
-			d->discReader = new GczReader(d->file);
+			d->discReader = std::make_shared<GczReader>(d->file);
 			break;
 
 		case GameCubePrivate::DISC_FORMAT_WIA:
@@ -932,7 +923,7 @@ GameCube::GameCube(const IRpFilePtr &file)
 		pt.size = d->file->size();
 		pt.vg = 0;
 		pt.pt = 0;
-		pt.partition = new WiiPartition(d->discReader, pt.start, pt.size);
+		pt.partition = std::make_shared<WiiPartition>(d->discReader, pt.start, pt.size);
 
 		// Determine the partition type.
 		// TODO: Super Smash Bros. Brawl "Masterpieces" partitions.
@@ -971,7 +962,7 @@ GameCube::GameCube(const IRpFilePtr &file)
 			if (pt.partition->verifyResult() != KeyManager::VerifyResult::IncrementingValues) {
 				// Not Incrementing Values.
 				// Error reading the partition header.
-				UNREF_AND_NULL_NOCHK(pt.partition);
+				pt.partition.reset();
 				d->wiiPtbl.clear();
 				goto notSupported;
 			}
@@ -1116,7 +1107,7 @@ GameCube::GameCube(const IRpFilePtr &file)
 
 notSupported:
 	// This disc image is not supported.
-	UNREF_AND_NULL(d->discReader);
+	d->discReader.reset();
 	d->file.reset();
 	d->discType = GameCubePrivate::DISC_UNKNOWN;
 	d->isValid = false;
@@ -1137,7 +1128,7 @@ void GameCube::close(void)
 				if (d->opening_bnr.gcn.data) {
 					d->opening_bnr.gcn.data->close();
 				}
-				UNREF_AND_NULL(d->opening_bnr.gcn.partition);
+				d->opening_bnr.gcn_partition.reset();
 				break;
 			case GameCubePrivate::DISC_SYSTEM_WII:
 				// No subclass for Wii yet.
@@ -2317,9 +2308,10 @@ int GameCube::checkViewedAchievements(void) const
 	}
 
 	// Check for the main partition.
-	const WiiPartition *pt = d->gamePartition;
+	// NOTE: Using raw pointers here for efficiency.
+	const WiiPartition *pt = d->gamePartition.get();
 	if (!pt) {
-		pt = d->updatePartition;
+		pt = d->updatePartition.get();
 	}
 	if (!pt) {
 		// No partitions...

commit da9b2366416e944e33def89634e8819451458780

[libromdata] Xbox360_XEX: Fix incorrect initPeReader() return value checks.
This broke XDBF handling, which prevented achievement icons from
being displayed.

This fixes a regression from commit bfd6ddf4b421e204212735ea755b11a3a0947c3f.
([librpbase] IDiscReader: Use shared_ptr<> instead of RefBase.)

src/libromdata/Console/Xbox360_XEX.cpp
@@ -567,8 +567,6 @@ int Xbox360_XEX_Private::initPeReader(void)
 	}
 #endif /* ENABLE_LIBMSPACK */
 
-	// NOTE: Since we're returning CBCReaderPtr&, we'll return
-	// peReader even if it isn't initialized.
 	if (!file) {
 		// File is closed. Can't initialize PE Reader.
 		return -EIO;
@@ -1181,7 +1179,7 @@ const EXE *Xbox360_XEX_Private::initEXE(void)
 	}
 
 	// Initialize the PE reader.
-	if (!initPeReader()) {
+	if (initPeReader() != 0) {
 		// Error initializing the PE reader.
 		return nullptr;
 	}
@@ -1224,7 +1222,7 @@ const Xbox360_XDBF *Xbox360_XEX_Private::initXDBF(void)
 	}
 
 	// Initialize the PE reader.
-	if (!initPeReader()) {
+	if (initPeReader() != 0) {
 		// Error initializing the PE reader.
 		return nullptr;
 	}