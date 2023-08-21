GerbilSoft/rom-properties

commit 44689d0aa84926a19864313d6a1db48846362024

[librptexture] rp_image: Use shared_ptr<> instead of RefBase.
Other changes:
- TGA: loadTGAImage() -> loadImage()

Code size difference: (64-bit Gentoo Linux, gcc-13.2.0, release build, no LTO)
   text    data     bss     dec     hex filename
1910495   67296     736 1978527  1e309f libromdata.so.4.0 [RefBase]
1984837   67352     736 2052925  1f533d libromdata.so.4.0 [shared_ptr]
 +74342     +56       0  +74398  +1229e Difference

Code size difference: (64-bit Gentoo Linux, gcc-13.2.0, release build, with LTO)
   text    data     bss     dec     hex filename
1705957   66408     608 1772973  1b0dad libromdata.so.4.0 [RefBase]
1731599   66544     608 1798751  1b725f libromdata.so.4.0 [shared_ptr]
 +25642    +136       0  +25778   +64b2 Difference

Code size difference: (64-bit Windows 7, MSVC 2022 17.6.5, release build, no LTO)
   text    data     bss     dec     hex filename
1517492   12928       0 1530420  175a34 romdata-4.dll [RefBase]
1569112   12928       0 1582040  1823d8 romdata-4.dll [shared_ptr]
 +51620       0       0  +51620   +c9a4 Difference

Code size difference: (64-bit Windows 7, MSVC 2022 17.6.5, release build, with LTO)
   text    data     bss     dec     hex filename
1509452   12928       0 1522380  173acc romdata-4.dll [RefBase]
1555584   12928       0 1568512  17ef00 romdata-4.dll [shared_ptr]
 +46132       0       0  +46132   +b434 Difference

The code size definitely increased here, possibly due to some shared_ptr
instances using `rp_image` and others using `const rp_image`.

src/gtk/DragImage.cpp
@@ -91,8 +91,8 @@ struct _RpDragImage {
 	GtkWidget *menuEcksBawks;	// GtkMenu
 #endif /* USE_G_MENU_MODEL */
 
-	// rp_image. [ref()'d]
-	const rp_image *img;
+	// rp_image (C++ shared_ptr)
+	shared_ptr<const rp_image> *pImg;
 
 	// Animated icon data.
 	struct anim_vars {
@@ -147,8 +147,6 @@ rp_drag_image_init(RpDragImage *image)
 {
 	image->minimumImageSize.width = DIL_MIN_IMAGE_SIZE;
 	image->minimumImageSize.height = DIL_MIN_IMAGE_SIZE;
-	image->img = nullptr;
-	image->anim = nullptr;
 
 	// Create the child GtkImage widget.
 	image->imageWidget = gtk_image_new();
@@ -185,8 +183,9 @@ rp_drag_image_dispose(GObject *object)
 	delete image->anim;
 	image->anim = nullptr;
 
-	// Unreference the image.
-	UNREF_AND_NULL(image->img);
+	// Delete the image shared_ptr.
+	delete image->pImg;
+	image->pImg = nullptr;
 
 #ifdef USE_G_MENU_MODEL
 #  if GTK_CHECK_VERSION(4,0,0)
@@ -250,7 +249,7 @@ rp_drag_image_update_pixmaps(RpDragImage *image)
 				anim->iconFrames[i] = nullptr;
 			}
 
-			const rp_image *const frame = iconAnimData->frames[i];
+			const shared_ptr<const rp_image> &frame = iconAnimData->frames[i];
 			if (frame && frame->isValid()) {
 				// NOTE: Allowing NULL frames here...
 				anim->iconFrames[i] = rp_image_to_PIMGTYPE(frame);
@@ -270,9 +269,9 @@ rp_drag_image_update_pixmaps(RpDragImage *image)
 		image->curFrame = PIMGTYPE_ref(anim->iconFrames[anim->iconAnimHelper.frameNumber()]);
 		gtk_image_set_from_PIMGTYPE(GTK_IMAGE(image->imageWidget), image->curFrame);
 		bRet = true;
-	} else if (image->img && image->img->isValid()) {
+	} else if (image->pImg && (*image->pImg)->isValid()) {
 		// Single image.
-		image->curFrame = rp_image_to_PIMGTYPE(image->img);
+		image->curFrame = rp_image_to_PIMGTYPE(image->pImg);
 		gtk_image_set_from_PIMGTYPE(GTK_IMAGE(image->imageWidget), image->curFrame);
 		bRet = true;
 	}
@@ -435,15 +434,11 @@ void rp_drag_image_set_ecks_bawks(RpDragImage *image, bool new_ecks_bawks)
  * @return True on success; false on error or if clearing.
  */
 bool
-rp_drag_image_set_rp_image(RpDragImage *image, const LibRpTexture::rp_image *img)
+rp_drag_image_set_rp_image(RpDragImage *image, const shared_ptr<const rp_image> &img)
 {
 	g_return_val_if_fail(RP_IS_DRAG_IMAGE(image), false);
 
-	// NOTE: We're not checking if the image pointer matches the
-	// previously stored image, since the underlying image may
-	// have changed.
-	UNREF_AND_NULL(image->img);
-
+	(*image->pImg) = img;
 	if (!img) {
 		if (!image->anim || !image->anim->iconAnimData) {
 			gtk_image_clear(GTK_IMAGE(image->imageWidget));
@@ -452,8 +447,6 @@ rp_drag_image_set_rp_image(RpDragImage *image, const LibRpTexture::rp_image *img
 		}
 		return false;
 	}
-
-	image->img = img->ref();
 	return rp_drag_image_update_pixmaps(image);
 }
 
@@ -489,7 +482,7 @@ rp_drag_image_set_icon_anim_data(RpDragImage *image, const LibRpBase::IconAnimDa
 	if (!iconAnimData) {
 		g_clear_handle_id(&anim->tmrIconAnim, g_source_remove);
 
-		if (!image->img) {
+		if (!(*image->pImg)) {
 			gtk_image_clear(GTK_IMAGE(image->imageWidget));
 		} else {
 			return rp_drag_image_update_pixmaps(image);
@@ -517,7 +510,7 @@ rp_drag_image_clear(RpDragImage *image)
 		UNREF_AND_NULL(anim->iconAnimData);
 	}
 
-	UNREF_AND_NULL(image->img);
+	(*image->pImg).reset();
 	gtk_image_clear(GTK_IMAGE(image->imageWidget));
 }
 
@@ -680,11 +673,11 @@ rp_drag_image_drag_data_get(RpDragImage *image, GdkDragContext *context, GtkSele
 	if (isAnimated) {
 		// Animated icon.
 		pngWriter = new RpPngWriter(pngData, anim->iconAnimData);
-	} else if (image->img) {
+	} else if (image->pImg && (*image->pImg)) {
 		// Standard icon.
 		// NOTE: Using the source image because we want the original
 		// size, not the resized version.
-		pngWriter = new RpPngWriter(pngData, image->img);
+		pngWriter = new RpPngWriter(pngData, *image->pImg);
 	} else {
 		// No icon...
 		return;

commit 179af7dbb49df5900b3a81f58ccd130c7baa8afb

[gtk] rp_drag_image_init(): Initialize the C++ shared_ptr.
Otherwise, if the RomData object has a non-animated image, RomDataView
will attempt to set it, which results in a NULL pointer dereference
due to having to allocate C++ objects manually.

RomData objects with animated images worked fine, though.

This fixes a regression from commit 44689d0aa84926a19864313d6a1db48846362024.
([librptexture] rp_image: Use shared_ptr<> instead of RefBase.)

src/gtk/DragImage.cpp
@@ -141,6 +141,9 @@ rp_drag_image_class_init(RpDragImageClass *klass)
 static void
 rp_drag_image_init(RpDragImage *image)
 {
+	// Initialize the C++ shared_ptr.
+	image->pImg = new rp_image_const_ptr();
+
 	image->minimumImageSize.width = DIL_MIN_IMAGE_SIZE;
 	image->minimumImageSize.height = DIL_MIN_IMAGE_SIZE;
 