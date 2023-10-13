9ffc428c8d75730a4e1b447dcaec3a7758afefc8
David Korth <gerbilsoft@gerbilsoft.com>
2023-10-13 19:55:14 -0400

    [gtk] AchSpriteSheet.hpp: Remove private variables.
    These are now in ISpriteSheet.

--- src/gtk/AchSpriteSheet.hpp
+++ src/gtk/AchSpriteSheet.hpp
@@ -48,9 +48,4 @@ public:
 	 * @return Achievements icon, or nullptr on error. (caller must free the icon)
 	 */
 	PIMGTYPE getIcon(LibRpBase::Achievements::ID id, bool gray = false);
-
-private:
-	LibRpTexture::rp_image_ptr m_img;
-	LibRpTexture::rp_image_ptr m_imgGray;
-	int m_iconSize;
 };
