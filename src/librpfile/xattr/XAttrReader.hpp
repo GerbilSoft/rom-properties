d6f02420d170b7c09d842f0c303148938119dfc7
David Korth <gerbilsoft@gerbilsoft.com>
2023-11-16 19:45:03 -0500

    [librpfile] XAttrReader: Un-indent the public and private class definitions.

--- src/librpfile/xattr/XAttrReader.hpp
+++ src/librpfile/xattr/XAttrReader.hpp
@@ -25,89 +25,89 @@ namespace LibRpFile {
 class XAttrReaderPrivate;
 class RP_LIBROMDATA_PUBLIC XAttrReader
 {
-	public:
-		explicit XAttrReader(const char *filename);
+public:
+	explicit XAttrReader(const char *filename);
 #if defined(_WIN32) && defined(_UNICODE)
-		explicit XAttrReader(const wchar_t *filename);
+	explicit XAttrReader(const wchar_t *filename);
 #endif /* _WIN32 && _UNICODE */
-	public:
-		~XAttrReader();
+public:
+	~XAttrReader();
 
-	private:
-		RP_DISABLE_COPY(XAttrReader)
-	protected:
-		friend class XAttrReaderPrivate;
-		XAttrReaderPrivate *const d_ptr;
+private:
+	RP_DISABLE_COPY(XAttrReader)
+protected:
+	friend class XAttrReaderPrivate;
+	XAttrReaderPrivate *const d_ptr;
 
-	public:
-		/**
-		 * Get the last error number.
-		 * @return Last error number (POSIX error code)
-		 */
-		int lastError(void) const;
+public:
+	/**
+	 * Get the last error number.
+	 * @return Last error number (POSIX error code)
+	 */
+	int lastError(void) const;
 
-	public:
-		/**
-		 * Does this file have Ext2 attributes?
-		 * @return True if it does; false if not.
-		 */
-		bool hasExt2Attributes(void) const;
+public:
+	/**
+	 * Does this file have Ext2 attributes?
+	 * @return True if it does; false if not.
+	 */
+	bool hasExt2Attributes(void) const;
 
-		/**
-		 * Get this file's Ext2 attributes.
-		 * @return Ext2 attributes
-		 */
-		int ext2Attributes(void) const;
+	/**
+	 * Get this file's Ext2 attributes.
+	 * @return Ext2 attributes
+	 */
+	int ext2Attributes(void) const;
 
-		/**
-		 * Does this file have XFS attributes?
-		 * @return True if it does; false if not.
-		 */
-		bool hasXfsAttributes(void) const;
+	/**
+	 * Does this file have XFS attributes?
+	 * @return True if it does; false if not.
+	 */
+	bool hasXfsAttributes(void) const;
 
-		/**
-		 * Get this file's XFS xflags.
-		 * @return XFS xflags
-		 */
-		uint32_t xfsXFlags(void) const;
+	/**
+	 * Get this file's XFS xflags.
+	 * @return XFS xflags
+	 */
+	uint32_t xfsXFlags(void) const;
 
-		/**
-		 * Get this file's XFS project ID.
-		 * @return XFS project ID
-		 */
-		uint32_t xfsProjectId(void) const;
+	/**
+	 * Get this file's XFS project ID.
+	 * @return XFS project ID
+	 */
+	uint32_t xfsProjectId(void) const;
 
-		/**
-		 * Does this file have MS-DOS attributes?
-		 * @return True if it does; false if not.
-		 */
-		bool hasDosAttributes(void) const;
+	/**
+	 * Does this file have MS-DOS attributes?
+	 * @return True if it does; false if not.
+	 */
+	bool hasDosAttributes(void) const;
 
-		/**
-		 * Get this file's MS-DOS attributes.
-		 * @return MS-DOS attributes.
-		 */
-		unsigned int dosAttributes(void) const;
+	/**
+	 * Get this file's MS-DOS attributes.
+	 * @return MS-DOS attributes.
+	 */
+	unsigned int dosAttributes(void) const;
 
-		/**
-		 * Does this file have generic extended attributes?
-		 * (POSIX xattr on Linux; ADS on Windows)
-		 * @return True if it does; false if not.
-		 */
-		bool hasGenericXAttrs(void) const;
+	/**
+	 * Does this file have generic extended attributes?
+	 * (POSIX xattr on Linux; ADS on Windows)
+	 * @return True if it does; false if not.
+	 */
+	bool hasGenericXAttrs(void) const;
 
-		/**
-		 * Extended attribute map (UTF-8)
-		 * - Key: Name
-		 * - Value: Value
-		 */
-		typedef std::map<std::string, std::string> XAttrList;
+	/**
+	 * Extended attribute map (UTF-8)
+	 * - Key: Name
+	 * - Value: Value
+	 */
+	typedef std::map<std::string, std::string> XAttrList;
 
-		/**
-		 * Get the list of extended attributes.
-		 * @return Extended attributes
-		 */
-		const XAttrList &genericXAttrs(void) const;
+	/**
+	 * Get the list of extended attributes.
+	 * @return Extended attributes
+	 */
+	const XAttrList &genericXAttrs(void) const;
 };
 
 }
