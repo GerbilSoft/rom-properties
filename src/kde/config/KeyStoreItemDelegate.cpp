GerbilSoft/rom-properties

commit 049822829163005fa45e9e2b0c13107ef355ea26

[kde] KeyStoreItemDelegate: Fix vertical icon positioning on High DPI systems.
Tested on Qt5. (TODO: Test on Qt6.)

src/kde/config/KeyStoreItemDelegate.cpp
@@ -129,7 +129,11 @@ void KeyStoreItemDelegate::paint(QPainter *painter,
 
 	// Center-align the image within the rectangle.
 	// TODO: Use Qt::TextAlignmentRole?
-	const int x = ((optionv4.rect.width() - pxm.width()) / 2) + optionv4.rect.left();
-	const int y = ((optionv4.rect.height() - pxm.height()) / 2) + optionv4.rect.top();
-	painter->drawPixmap(x, y, pxm);
+	// FIXME: Verify High DPI on Qt6.
+	const qreal dpm = pxm.devicePixelRatio();
+	const QPointF pointF(
+		((((optionv4.rect.width() * dpm) - pxm.width()) / 2) + (optionv4.rect.left() * dpm)) / dpm,
+		((((optionv4.rect.height() * dpm) - pxm.height()) / 2) + (optionv4.rect.top() * dpm)) / dpm
+	);
+	painter->drawPixmap(pointF, pxm);
 }

commit 6e8f1837be24096f3efbdd820fd6e3869f31d690

[kde] KeyStoreItemDelegate::paint(): QPixmap::devicePixelRatio() was added in Qt5.
This broke the Qt4 build on AppVeyor.

This fixes a regression from commit 049822829163005fa45e9e2b0c13107ef355ea26.
([kde] KeyStoreItemDelegate: Fix vertical icon positioning on High DPI systems.)

src/kde/config/KeyStoreItemDelegate.cpp
@@ -130,7 +130,11 @@ void KeyStoreItemDelegate::paint(QPainter *painter,
 	// Center-align the image within the rectangle.
 	// TODO: Use Qt::TextAlignmentRole?
 	// FIXME: Verify High DPI on Qt6.
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
 	const qreal dpm = pxm.devicePixelRatio();
+#else /* QT_VERSION < QT_VERSION_CHECK(5,0,0) */
+	static const qreal dpm = 1.0;
+#endif /* QT_VERSION >= QT_VERSION_CHECK(5,0,0) */
 	const QPointF pointF(
 		((((optionv4.rect.width() * dpm) - pxm.width()) / 2) + (optionv4.rect.left() * dpm)) / dpm,
 		((((optionv4.rect.height() * dpm) - pxm.height()) / 2) + (optionv4.rect.top() * dpm)) / dpm