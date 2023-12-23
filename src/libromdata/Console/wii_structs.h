GerbilSoft/rom-properties

commit 67b360b31a5a84de77c45dbf48d4cce030f0dfac

[libromdata] GameCube: Convert uint34_rshift2_t into a struct with helper functions.
With the typedef, it was possible to accidentally manipulate it as if
it was a uint32_t. With the struct, it has to be converted to either
uint64_t or off64_t before it can be accessed.

This shouldn't have any functional changes.

wii_structs.h: Add struct uint34_rshift2_t with C++ helper functions:
- get(): Get as uint64_t.
- get_be(): Get as uint64_t, byteswapped from big-endian if necessary.
- geto(): Get as off64_t.
- geto_be(): Get as off64_t, byteswapped from big-endian if necessary.
- set(): Set from uint64_t.
- seto(): Set from off64_t.

NOTE: Only geto_be() is used right now.
TODO: Add set_be() and seto_be()?

src/libromdata/Console/wii_structs.h
@@ -16,12 +16,77 @@
 #include "nintendo_system_id.h"
 
 #ifdef __cplusplus
+// for uint34_rshift2_t's inline helper functions
+#  include <cassert>
+#  include "byteswap_rp.h"
+#endif /* __cplusplus */
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
 // 34-bit value stored in uint32_t.
 // The value must be lshifted by 2.
-typedef uint32_t uint34_rshift2_t;
+typedef struct _uint34_rshift2_t {
+	uint32_t val;
+#ifdef __cplusplus
+	/**
+	 * Get this uint34_rshift2_t as uint64_t.
+	 * No byteswapping in this version.
+	 * @return uint64_t
+	 */
+	uint64_t get(void) const {
+		return (static_cast<uint64_t>(val) << 2);
+	}
+
+	/**
+	 * Get this uint34_rshift2_t as uint64_t.
+	 * Byteswapped from big-endian, if necessary.
+	 * @return uint64_t
+	 */
+	uint64_t get_be(void) const {
+		return (static_cast<uint64_t>(val) << 2);
+	}
+
+	/**
+	 * Get this uint34_rshift2_t as off64_t.
+	 * No byteswapping in this version.
+	 * @return off64_t
+	 */
+	off64_t geto(void) const {
+		return (static_cast<off64_t>(val) << 2);
+	}
+
+	/**
+	 * Get this uint34_rshift2_t as off64_t.
+	 * Byteswapped from big-endian, if necessary.
+	 * @return uint64_t
+	 */
+	off64_t geto_be(void) const {
+		return (static_cast<off64_t>(val) << 2);
+	}
+
+	/**
+	 * Save a uint64_t in this uint34_rshift2_t.
+	 * No byteswapping in this version.
+	 * @param val34 34-bit value (2 low bits must be 0)
+	 */
+	void set(uint64_t val34) {
+		assert((val34 & 0xFFFFFFFC00000003ULL) == 0);
+		val = static_cast<uint32_t>(val34 >> 2);
+	}
+
+	/**
+	 * Save a uint64_t in this uint34_rshift2_t.
+	 * No byteswapping in this version.
+	 * @param val34 34-bit value (2 low bits must be 0)
+	 */
+	void seto(off64_t val34) {
+		assert((val34 & 0xFFFFFFFC00000003ULL) == 0);
+		val = static_cast<uint32_t>(val34 >> 2);
+	}
+#endif /* __cplusplus */
+} uint34_rshift2_t;
 
 /**
  * Wii volume group table.

src/libromdata/Console/GameCube.cpp
@@ -378,7 +378,7 @@ int GameCubePrivate::loadWiiPartitionTables(void)
 		}
 
 		// Read the partition table entries.
-		const off64_t pt_addr = static_cast<off64_t>(be32_to_cpu(vgtbl.vg[i].addr)) << 2;
+		const off64_t pt_addr = vgtbl.vg[i].addr.geto_be();
 		static constexpr size_t pt_size = pt.size() * sizeof(pt[0]);
 		size = discReader->seekAndRead(pt_addr, pt.data(), pt_size);
 		if (size != pt_size) {
@@ -394,7 +394,7 @@ int GameCubePrivate::loadWiiPartitionTables(void)
 
 			entry.vg = static_cast<uint8_t>(i);
 			entry.pt = static_cast<uint8_t>(j);
-			entry.start = static_cast<off64_t>(be32_to_cpu(pt[j].addr)) << 2;
+			entry.start = pt[j].addr.geto_be();
 			entry.type = be32_to_cpu(pt[j].type);
 		}
 	}

src/libromdata/disc/WiiPartition.cpp
@@ -535,8 +535,8 @@ WiiPartition::WiiPartition(const IDiscReaderPtr &discReader, off64_t partition_o
 	}
 
 	// Save important data.
-	d->data_offset = static_cast<off64_t>(be32_to_cpu(d->partitionHeader.data_offset)) << 2;
-	d->data_size   = static_cast<off64_t>(be32_to_cpu(d->partitionHeader.data_size)) << 2;
+	d->data_offset = d->partitionHeader.data_offset.geto_be();
+	d->data_size   = d->partitionHeader.data_size.geto_be();
 	if (d->data_size == 0) {
 		// NoCrypto RVT-H images sometimes have this set to 0.
 		// Use the calculated partition size.
@@ -819,7 +819,7 @@ off64_t WiiPartition::partition_size_used(void) const
 
 	// Add the data offset from the partition header.
 	RP_D(const WiiPartition);
-	size += (static_cast<off64_t>(be32_to_cpu(d->partitionHeader.data_offset)) << 2);
+	size += d->partitionHeader.data_offset.geto_be();
 
 	// Are sectors hashed?
 	if ((d->cryptoMethod & WiiPartition::CM_MASK_SECTOR) == CM_1K_31K) {

commit fa8510e7b34621f940800cc8b849bc31add01992

[libromdata] wii_structs.h, uint32_rshift2_t: Actually byteswap when using get_be() or geto_be().
This completely broke parsing Wii disc images.

This fixes a regression from commit 67b360b31a5a84de77c45dbf48d4cce030f0dfac.
([libromdata] GameCube: Convert uint34_rshift2_t into a struct with helper functions.)

src/libromdata/Console/wii_structs.h
@@ -45,7 +45,7 @@ typedef struct _uint34_rshift2_t {
 	 * @return uint64_t
 	 */
 	uint64_t get_be(void) const {
-		return (static_cast<uint64_t>(val) << 2);
+		return (static_cast<uint64_t>(be32_to_cpu(val)) << 2);
 	}
 
 	/**
@@ -63,7 +63,7 @@ typedef struct _uint34_rshift2_t {
 	 * @return uint64_t
 	 */
 	off64_t geto_be(void) const {
-		return (static_cast<off64_t>(val) << 2);
+		return (static_cast<off64_t>(be32_to_cpu(val)) << 2);
 	}
 
 	/**