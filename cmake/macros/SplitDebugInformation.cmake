GerbilSoft/rom-properties

commit 356908fe5ca465413b22d926f822f5aba02120de

[cmake] SplitDebugInformation.cmake: Combine the .gnu_debuglink removal into the add-gnu-debuglink command.
objcopy can do both of these in a single command.

cmake/macros/SplitDebugInformation.cmake
@@ -87,9 +87,9 @@ IF(SPLIT_OK)
 	ADD_CUSTOM_COMMAND(TARGET ${_target} POST_BUILD
 		COMMAND ${CMAKE_OBJCOPY} --only-keep-debug ${OBJCOPY_COMPRESS_DEBUG_SECTIONS_PARAM}
 			${SPLITDEBUG_SOURCE} ${SPLITDEBUG_TARGET}
-		COMMAND ${CMAKE_STRIP} -R .gnu_debuglink
-			${SPLITDEBUG_SOURCE}
-		COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink="${SPLITDEBUG_TARGET}"
+		COMMAND ${CMAKE_OBJCOPY}
+			--remove-section=.gnu_debuglink
+			--add-gnu-debuglink="${SPLITDEBUG_TARGET}"
 			${SPLITDEBUG_SOURCE}
 		)
 

commit e7ceb1d648f2d0171ba71bc4702e862d7e4e8c83

[cmake] SplitDebugInformation.cmake: Add `--strip-all` to the second objcopy command.
Combining the removal of .gnu_debuglink into the objcopy command had the
effect of losing `strip`'s usual stripping functionality. Add the
`--strip-all` parameter to restore it.

This fixes a regression from commit 356908fe5ca465413b22d926f822f5aba02120de.
([cmake] SplitDebugInformation.cmake: Combine the .gnu_debuglink removal into the add-gnu-debuglink command.)

cmake/macros/SplitDebugInformation.cmake
@@ -88,6 +88,7 @@ IF(SPLIT_OK)
 		COMMAND ${CMAKE_OBJCOPY} --only-keep-debug ${OBJCOPY_COMPRESS_DEBUG_SECTIONS_PARAM}
 			${SPLITDEBUG_SOURCE} ${SPLITDEBUG_TARGET}
 		COMMAND ${CMAKE_OBJCOPY}
+			--strip-all
 			--remove-section=.gnu_debuglink
 			--add-gnu-debuglink="${SPLITDEBUG_TARGET}"
 			${SPLITDEBUG_SOURCE}