66e21494e0d678e254773f749fc9af2c1eddd38f
David Korth <gerbilsoft@gerbilsoft.com>
2024-02-06 23:18:18 -0500

    [libwin32ui] LoadResource_i18n.cpp: Increase the lc_mappings size.
    std::array<> doesn't have an implicit size, so this broke:
    
    src\libwin32ui\LoadResource_i18n.cpp(55): error C2078: too many initializers

--- src/libwin32ui/LoadResource_i18n.cpp
+++ src/libwin32ui/LoadResource_i18n.cpp
@@ -43,7 +43,7 @@ LPVOID LoadResource_i18n(HMODULE hModule, LPCTSTR lpType, DWORD dwResId)
 		uint32_t lc;
 		WORD wLanguage;
 	};
-	static const std::array<lc_mapping_t, 7> lc_mappings = {{
+	static const std::array<lc_mapping_t, 8> lc_mappings = {{
 		{'de', MAKELANGID(LANG_GERMAN, SUBLANG_GERMAN)},
 		{'es', MAKELANGID(LANG_SPANISH, SUBLANG_SPANISH)},
 		{'fr', MAKELANGID(LANG_FRENCH, SUBLANG_FRENCH)},
