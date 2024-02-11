GerbilSoft/rom-properties

commit 35ad18f10e1493f4e4e48e2b0de08417fd73636a

[rp-stub] rp-stub.c: Use strlcat() instead of strcat().
strlcat() was added in glibc-2.38, and is available on most BSDs
and Mac OS X.

For older systems, a copy of strlcat() is provided in librptext/libc.c.

clang-tidy 17.0.1 complained about strcat():

rp-stub.c:348:5: warning: Call to function 'strcat' is insecure as it
does not provide bounding of the memory buffer. Replace unbounded copy
functions with analogous functions that support length arguments such
as 'strlcat'. CWE-119 [clang-analyzer-security.insecureAPI.strcpy]
  348 |                                 strcat(output_file, ".png");
      |                                 ^~~~~~

rp-stub.c:358:6: warning: Call to function 'strcat' is insecure as it
does not provide bounding of the memory buffer. Replace unbounded copy
functions with analogous functions that support length arguments such
as 'strlcat'. CWE-119 [clang-analyzer-security.insecureAPI.strcpy]
  358 |                                         strcat(output_file, ".png");
      |                                         ^~~~~~

src/librptext/libc.c
@@ -44,7 +44,8 @@ void *rp_memmem(const void *haystack, size_t haystacklen,
 {
 	// Reference: https://opensource.apple.com/source/Libc/Libc-1044.1.2/string/FreeBSD/memmem.c
 	// NOTE: haystack was originally 'l'; needle was originally 's'.
-	register const char *cur, *last;
+	// NOTE: 'register' keywords were removed.
+	const char *cur, *last;
 	const char *cl = (const char *)haystack;
 	const char *cs = (const char *)needle;
 
@@ -71,3 +72,41 @@ void *rp_memmem(const void *haystack, size_t haystacklen,
 	return NULL;
 }
 #endif /* HAVE_MEMMEM */
+
+#ifndef HAVE_STRLCAT
+/**
+ * strcat() but with a length parameter to prevent buffer overflows.
+ * @param dst [in,out] Destination string
+ * @param src [in] Source string
+ * @param size [in] Size of destination string
+ */
+size_t rp_strlcat(char *dst, const char *src, size_t size)
+{
+	// Reference: https://opensource.apple.com/source/Libc/Libc-262/string/strlcat.c.auto.html
+	// NOTE: 'size' was originally 'siz'.
+	// NOTE: 'register' keywords were removed.
+	register char *d = dst;
+	register const char *s = src;
+	register size_t n = size;
+	size_t dlen;
+
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
+
+	if (n == 0)
+		return(dlen + strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+
+	return(dlen + (s - src));	/* count does not include NUL */
+}
+#endif /* HAVE_STRLCAT */

src/librptext/CMakeLists.txt
@@ -54,6 +54,9 @@ SET(CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS} -D_GNU_SOURCE)
 CHECK_SYMBOL_EXISTS(memmem "string.h" HAVE_MEMMEM)
 UNSET(CMAKE_REQUIRED_DEFINITIONS)
 
+# Added in glibc-2.38; also the various BSDs and Mac OS X.
+CHECK_SYMBOL_EXISTS(strlcat "string.h" HAVE_STRLCAT)
+
 # wcwidth() requires _XOPEN_SOURCE.
 SET(CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS} -D_XOPEN_SOURCE)
 CHECK_SYMBOL_EXISTS(wcwidth "wchar.h" HAVE_WCWIDTH)

src/librptext/config.librptext.h.in
@@ -17,6 +17,9 @@
 /* Define to 1 if you have the `memmem' function. */
 #cmakedefine HAVE_MEMMEM 1
 
+/* Define to 1 if yo uhave the `strlcat` function. */
+#cmakedefine HAVE_STRLCAT 1
+
 /* Define to 1 if you have the `nl_langinfo` function. */
 #cmakedefine HAVE_NL_LANGINFO 1
 

src/librptext/libc.h
@@ -55,6 +55,21 @@ void *rp_memmem(const void *haystack, size_t haystacklen,
 #  define memmem(haystack, haystacklen, needle, needlelen) rp_memmem((haystack), (haystacklen), (needle), (needlelen))
 #endif /* !HAVE_MEMMEM */
 
+#ifndef HAVE_STRLCAT
+/**
+ * strcat() but with a length parameter to prevent buffer overflows.
+ * @param dst [in,out] Destination string
+ * @param src [in] Source string
+ * @param size [in] Size of destination string
+ */
+size_t rp_strlcat(char *dst, const char *src, size_t size);
+
+#  ifdef strlcat
+#    undef strlcat
+#  endif
+#  define strlcat(dst, src, size) rp_strlcat((dst), (src), (size))
+#endif /* !HAVE_STRLCAT */
+
 #ifdef __cplusplus
 }
 #endif

src/rp-stub/CMakeLists.txt
@@ -24,7 +24,8 @@ IF(TARGET git_version)
 ENDIF(TARGET git_version)
 
 # dll-search.c is in libunixcommon.
-TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE unixcommon rpsecure)
+# strlcat() is in librptext.
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE unixcommon rptext rpsecure)
 
 IF(ENABLE_NLS)
 	TARGET_LINK_LIBRARIES(${PROJECT_NAME} PRIVATE i18n)

src/rp-stub/rp-stub.c
@@ -13,6 +13,7 @@
  * function from the library.
  */
 #include "config.version.h"
+#include "config.librptext.h"
 #include "git.h"
 
 #include "libi18n/i18n.h"
@@ -345,7 +346,7 @@ int main(int argc, char *argv[])
 			char *const dotpos = strrchr(output_file, '.');
 			if (!dotpos) {
 				// No file extension. Add it.
-				strcat(output_file, ".png");
+				strlcat(output_file, ".png", output_len + 16);
 			} else {
 				// If the dot is after the last slash, we already have a file extension.
 				// Otherwise, we don't have one, and need to add it.
@@ -355,7 +356,7 @@ int main(int argc, char *argv[])
 					strcpy(dotpos, ".png");
 				} else {
 					// No file extension.
-					strcat(output_file, ".png");
+					strlcat(output_file, ".png", output_len + 16);
 				}
 			}
 		} else {

commit 891843a19bf2408e634933aa640cad17f4e640ca

[librptext] libc.c, rp_strlcat(): Missed a 'siz'.
This broke the MSVC build.

This fixes a regression from commit 35ad18f10e1493f4e4e48e2b0de08417fd73636a.
([rp-stub] rp-stub.c: Use strlcat() instead of strcat().)

src/librptext/libc.c
@@ -94,7 +94,7 @@ size_t rp_strlcat(char *dst, const char *src, size_t size)
 	while (n-- != 0 && *d != '\0')
 		d++;
 	dlen = d - dst;
-	n = siz - dlen;
+	n = size - dlen;
 
 	if (n == 0)
 		return(dlen + strlen(s));