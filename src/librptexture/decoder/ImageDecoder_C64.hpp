5cde047bf2b90415c8ae3cbf233370ab5e89232e
David Korth <gerbilsoft@gerbilsoft.com>
2024-02-24 20:49:27 -0500

    [librptexture] ImageDecoder_C64: Add a multi-color sprite decoder.
    Not currently used...
    
    Palette order matches the bits used in multi-color sprites:
    - 0: Transparent
    - 1: Multicolor register #0 ($D025)
    - 2: Sprite color register
    - 3: Multicolor register #1 ($D026)

--- src/librptexture/decoder/ImageDecoder_C64.hpp
+++ src/librptexture/decoder/ImageDecoder_C64.hpp
@@ -23,4 +23,14 @@ ATTR_ACCESS_SIZE(read_only, 1, 2)
 rp_image_ptr fromC64_SingleColor_Sprite(
 	const uint8_t *RESTRICT img_buf, size_t img_siz);
 
+/**
+ * Convert a Commodore 64 multi-color sprite (12x21) to rp_image.
+ * A default 4-color grayscale palette is used.
+ * @param img_buf CI4 image buffer
+ * @param img_siz Size of image data [must be >= 12*21/4 (63)]
+ * @return rp_image, or nullptr on error.
+ */
+rp_image_ptr fromC64_MultiColor_Sprite(
+	const uint8_t *RESTRICT img_buf, size_t img_siz);
+
 } }
