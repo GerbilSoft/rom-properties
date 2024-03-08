GerbilSoft/rom-properties

commit d23023d9f1e598f7721f98d281e2409d3a6d0f83

Use LibRpBase::Hash for CRC32 instead of calling zlib's crc32() directly.
This does add some overhead, but it lets us centralize all of the
Windows DelayLoad stuff, which cleans up the code quite a bit.

NOTE: Not changing RpPngFormatTest, since that tests requires zlib
in order to function anyway. (Also, it doesn't use DelayLoad.)

src/librpbase/Achievements.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * Achievements.cpp: Achievements class.                                   *
  *                                                                         *
- * Copyright (c) 2020-2023 by David Korth.                                 *
+ * Copyright (c) 2020-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -10,9 +10,11 @@
 #include "Achievements.hpp"
 #include "libi18n/i18n.h"
 
-// librpfile, librptexture
+// librpbase, librpfile
+#include "librpbase/crypto/Hash.hpp"
 #include "librpfile/FileSystem.hpp"
 #include "librpfile/RpFile.hpp"
+using namespace LibRpBase;
 using namespace LibRpFile;
 
 // C++ STL classes
@@ -22,13 +24,6 @@ using std::unordered_map;
 
 // Uninitialized vector class
 #include "uvector.h"
-
-// zlib for CRC32.
-#include <zlib.h>
-#ifdef _MSC_VER
-// MSVC: Exception handling for /DELAYLOAD.
-#  include "libwin32common/DelayLoadHelper.h"
-#endif /* _MSC_VER */
 
 #ifdef _WIN32
 // Win32 is needed for GetCurrentProcessId().
@@ -44,11 +39,6 @@ using std::unordered_map;
 //#define FORCE_OBFUSCATE 1
 
 namespace LibRpBase {
-
-#ifdef _MSC_VER
-// DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
-#endif /* _MSC_VER */
 
 class AchievementsPrivate
 {
@@ -349,19 +339,12 @@ void AchievementsPrivate::doObfuscate(uint16_t iv, uint8_t *buf, size_t size)
  */
 int AchievementsPrivate::save(void) const
 {
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-	// Delay load verification.
-	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_get_crc_table() != 0) {
-		// Delay load failed.
+	Hash crc32Hash(Hash::Algorithm::CRC32);
+	if (!crc32Hash.isUsable()) {
+		// zlib could not be initialized.
 		// Can't calculate the CRC32.
 		return -ENOTSUP;
 	}
-#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
-	// zlib isn't in a DLL, but we need to ensure that the
-	// CRC table is initialized anyway.
-	get_crc_table();
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 	// Create the achievements file in memory.
 	rp::uvector<uint8_t> buf;
@@ -435,9 +418,8 @@ int AchievementsPrivate::save(void) const
 	// CRC32 of achievement data.
 	// Includes count.
 	if (buf.size() > HeaderSizeMinusCount) {
-		header->crc32 = cpu_to_le32(
-			crc32(0, &buf.data()[HeaderSizeMinusCount],
-			      static_cast<uInt>(buf.size() - HeaderSizeMinusCount)));
+		crc32Hash.process(&buf.data()[HeaderSizeMinusCount], buf.size() - HeaderSizeMinusCount);
+		header->crc32 = cpu_to_le32(crc32Hash.getHash32());
 	}
 
 #if defined(NDEBUG) || defined(FORCE_OBFUSCATE)
@@ -499,15 +481,12 @@ int AchievementsPrivate::save(void) const
  */
 int AchievementsPrivate::load(void)
 {
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-	// Delay load verification.
-	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_get_crc_table() != 0) {
-		// Delay load failed.
+	Hash crc32Hash(Hash::Algorithm::CRC32);
+	if (!crc32Hash.isUsable()) {
+		// zlib could not be initialized.
 		// Can't calculate the CRC32.
 		return -ENOTSUP;
 	}
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
 
 	// Clear loaded achievements.
 	loaded = false;
@@ -602,7 +581,8 @@ int AchievementsPrivate::load(void)
 	header = reinterpret_cast<const AchBinHeader*>(buf.data());
 
 	// Verify the CRC32.
-	const uint32_t crc = crc32(0, &buf.data()[HeaderSizeMinusCount], data_len);
+	crc32Hash.process(&buf.data()[HeaderSizeMinusCount], data_len);
+	const uint32_t crc = crc32Hash.getHash32();
 	if (crc != le32_to_cpu(header->crc32)) {
 		// Incorrect CRC32.
 		return -EBADF;

src/libromdata/Console/CBMCart.cpp
@@ -12,6 +12,7 @@
 #include "data/CBMData.hpp"
 
 // Other rom-properties libraries
+#include "librpbase/crypto/Hash.hpp"
 using namespace LibRpBase;
 using namespace LibRpFile;
 using namespace LibRpText;
@@ -21,19 +22,7 @@ using std::string;
 using std::unique_ptr;
 using std::vector;
 
-// zlib for crc32()
-#include <zlib.h>
-#ifdef _MSC_VER
-// MSVC: Exception handling for /DELAYLOAD.
-#  include "libwin32common/DelayLoadHelper.h"
-#endif /* _MSC_VER */
-
 namespace LibRomData {
-
-#ifdef _MSC_VER
-// DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
-#endif /* _MSC_VER */
 
 class CBMCartPrivate final : public RomDataPrivate
 {
@@ -71,13 +60,6 @@ public:
 	// Used for the external image URL.
 	// NOTE: Calculated by extURLs() on demand.
 	uint32_t rom_16k_crc32;
-
-public:
-	/**
-	 * Initialize zlib.
-	 * @return 0 on success; non-zero on error.
-	 */
-	static int zlibInit(void);
 };
 
 ROMDATA_IMPL(CBMCart)
@@ -113,30 +95,6 @@ CBMCartPrivate::CBMCartPrivate(const IRpFilePtr &file)
 {
 	// Clear the ROM header struct.
 	memset(&romHeader, 0, sizeof(romHeader));
-}
-
-/**
- * Initialize zlib.
- * @return 0 on success; non-zero on error.
- */
-int CBMCartPrivate::zlibInit(void)
-{
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-	// Delay load verification.
-	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_get_crc_table() != 0) {
-		// Delay load failed.
-		// Can't calculate the CRC32.
-		return -ENOENT;
-	}
-#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
-	// zlib isn't in a DLL, but we need to ensure that the
-	// CRC table is initialized anyway.
-	get_crc_table();
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
-
-	// zlib initialized.
-	return 0;
 }
 
 /** CBMCart **/
@@ -499,7 +457,8 @@ int CBMCart::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) co
 	// then it's the CRC32 of whatever's available.
 	if (d->rom_16k_crc32 == 0) {
 		// CRC32 hasn't been calculated yet.
-		if (d->zlibInit() != 0) {
+		Hash crc32Hash(Hash::Algorithm::CRC32);
+		if (!crc32Hash.isUsable()) {
 			// zlib could not be initialized.
 			return -EIO;
 		}
@@ -566,7 +525,8 @@ int CBMCart::extURLs(ImageType imageType, vector<ExtURL> *pExtURLs, int size) co
 		}
 
 		// Calculate the CRC32 of whatever data we could read.
-		d->rom_16k_crc32 = crc32(0, buf.get(), static_cast<uInt>(sz_rd_total));
+		crc32Hash.process(buf.get(), sz_rd_total);
+		d->rom_16k_crc32 = crc32Hash.getHash32();
 	}
 
 	// Lowercase hex CRC32s are used.

src/libromdata/Console/MegaDrive.cpp
@@ -17,6 +17,7 @@
 
 // Other rom-properties libraries
 #include "librpbase/Achievements.hpp"
+#include "librpbase/crypto/Hash.hpp"
 using namespace LibRpBase;
 using namespace LibRpFile;
 using namespace LibRpText;
@@ -28,19 +29,7 @@ using namespace LibRpText;
 using std::string;
 using std::vector;
 
-// zlib for crc32()
-#include <zlib.h>
-#ifdef _MSC_VER
-// MSVC: Exception handling for /DELAYLOAD.
-#  include "libwin32common/DelayLoadHelper.h"
-#endif /* _MSC_VER */
-
 namespace LibRomData {
-
-#ifdef _MSC_VER
-// DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
-#endif /* _MSC_VER */
 
 class MegaDrivePrivate final : public RomDataPrivate
 {
@@ -205,12 +194,6 @@ public:
 	 * @return Publisher, or "Unknown" if unknown.
 	 */
 	static string getPublisher(const MD_RomHeader *pRomHeader);
-
-	/**
-	 * Initialize zlib.
-	 * @return 0 on success; non-zero on error.
-	 */
-	static int zlibInit(void);
 };
 
 ROMDATA_IMPL(MegaDrive)
@@ -709,30 +692,6 @@ string MegaDrivePrivate::getPublisher(const MD_RomHeader *pRomHeader)
 	return s_publisher;
 }
 
-/**
- * Initialize zlib.
- * @return 0 on success; non-zero on error.
- */
-int MegaDrivePrivate::zlibInit(void)
-{
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-	// Delay load verification.
-	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_get_crc_table() != 0) {
-		// Delay load failed.
-		// Can't calculate the CRC32.
-		return -ENOENT;
-	}
-#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
-	// zlib isn't in a DLL, but we need to ensure that the
-	// CRC table is initialized anyway.
-	get_crc_table();
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
-
-	// zlib initialized.
-	return 0;
-}
-
 /** MegaDrive **/
 
 /**
@@ -778,7 +737,6 @@ MegaDrive::MegaDrive(const IRpFilePtr &file)
 	d->romType = isRomSupported_static(&info);
 
 	// Mega CD security program CRC32
-	// TODO: zlib delay-load on Windows.
 	uint32_t mcd_sp_crc = 0;
 
 	if (d->romType >= 0) {
@@ -836,8 +794,12 @@ MegaDrive::MegaDrive(const IRpFilePtr &file)
 				// able to distinguish between them.
 				// NOTE: Only the first 0x4C0 bytes seem to be identical
 				// between USA and EUR, and 0x150 in JPN...
-				if (size >= (0x200 + 0x150) && !d->zlibInit()) {
-					mcd_sp_crc = crc32(0, &header[0x200], 0x150);
+				if (size >= (0x200 + 0x150)) {
+					Hash crc32Hash(Hash::Algorithm::CRC32);
+					if (crc32Hash.isUsable()) {
+						crc32Hash.process(&header[0x200], 0x150);
+						mcd_sp_crc = crc32Hash.getHash32();
+					}
 				}
 				break;
 
@@ -863,8 +825,12 @@ MegaDrive::MegaDrive(const IRpFilePtr &file)
 				// able to distinguish between them.
 				// NOTE: Only the first 0x4C0 bytes seem to be identical
 				// between USA and EUR, and 0x150 in JPN...
-				if (size >= (0x210 + 0x150) && !d->zlibInit()) {
-					mcd_sp_crc = crc32(0, &header[0x210], 0x150);
+				if (size >= (0x210 + 0x150)) {
+					Hash crc32Hash(Hash::Algorithm::CRC32);
+					if (crc32Hash.isUsable()) {
+						crc32Hash.process(&header[0x210], 0x150);
+						mcd_sp_crc = crc32Hash.getHash32();
+					}
 				}
 				break;
 
@@ -927,7 +893,8 @@ MegaDrive::MegaDrive(const IRpFilePtr &file)
 	    !memcmp(s_serial_number, "GM 00054503-00", sizeof(d->romHeader.serial_number)) &&
 	    (d->romType & MegaDrivePrivate::ROM_FORMAT_MASK) == MegaDrivePrivate::ROM_FORMAT_CART_BIN)
 	{
-		if (d->zlibInit() != 0) {
+		Hash crc32Hash(Hash::Algorithm::CRC32);
+		if (!crc32Hash.isUsable()) {
 			// Can't initialize zlib to calculate the CRC32.
 			return;
 		}
@@ -937,7 +904,8 @@ MegaDrive::MegaDrive(const IRpFilePtr &file)
 		uint8_t buf[256];
 		size_t size = d->file->seekAndRead(0x20000, buf, sizeof(buf));
 		if (size == sizeof(buf)) {
-			d->gt_crc = crc32(0, buf, sizeof(buf));
+			crc32Hash.process(buf, sizeof(buf));
+			d->gt_crc = crc32Hash.getHash32();
 		}
 	}
 	// If this is S&K, try reading the locked-on ROM header.

src/libromdata/Handheld/Nintendo3DS.cpp
@@ -16,6 +16,7 @@
 
 // Other rom-properties libraries
 #include "librpbase/config/Config.hpp"
+#include "librpbase/crypto/Hash.hpp"
 #include "librpbase/Achievements.hpp"
 #include "librpbase/SystemRegion.hpp"
 #include "librpfile/SubFile.hpp"
@@ -37,19 +38,7 @@ using std::string;
 using std::unique_ptr;
 using std::vector;
 
-// zlib for crc32()
-#include <zlib.h>
-#ifdef _MSC_VER
-// MSVC: Exception handling for /DELAYLOAD.
-#  include "libwin32common/DelayLoadHelper.h"
-#endif /* _MSC_VER */
-
 namespace LibRomData {
-
-#ifdef _MSC_VER
-// DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
-#endif /* _MSC_VER */
 
 ROMDATA_IMPL(Nintendo3DS)
 ROMDATA_IMPL_IMG_SIZES(Nintendo3DS)
@@ -745,28 +734,14 @@ void Nintendo3DSPrivate::addTitleIdAndProductCodeFields(bool showContentType)
 	if (f_logo) {
 		const off64_t szFile = f_logo->size();
 		if (szFile == 8192) {
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-			// Delay load verification.
-			// TODO: Only if linked with /DELAYLOAD?
-			bool has_zlib = true;
-			if (DelayLoad_test_get_crc_table() != 0) {
-				// Delay load failed.
-				// Can't calculate the CRC32.
-				has_zlib = false;
-			}
-#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
-			// zlib isn't in a DLL, but we need to ensure that the
-			// CRC table is initialized anyway.
-			static const bool has_zlib = true;
-			get_crc_table();
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
-
-			if (has_zlib) {
+			Hash crc32Hash(Hash::Algorithm::CRC32);
+			if (crc32Hash.isUsable()) {
 				// Calculate the CRC32.
 				unique_ptr<uint8_t[]> buf(new uint8_t[static_cast<unsigned int>(szFile)]);
 				size_t size = f_logo->read(buf.get(), static_cast<unsigned int>(szFile));
 				if (size == static_cast<unsigned int>(szFile)) {
-					crc = crc32(0, buf.get(), static_cast<unsigned int>(szFile));
+					crc32Hash.process(buf.get(), szFile);
+					crc = crc32Hash.getHash32();
 				}
 			}
 		} else if (szFile > 0) {

src/libromdata/Handheld/Nintendo3DSFirm.cpp
@@ -12,6 +12,7 @@
 #include "data/Nintendo3DSFirmData.hpp"
 
 // Other rom-properties libraries
+#include "librpbase/crypto/Hash.hpp"
 using namespace LibRpBase;
 using namespace LibRpFile;
 using namespace LibRpText;
@@ -23,19 +24,7 @@ using namespace LibRpText;
 using std::string;
 using std::unique_ptr;
 
-// zlib for crc32()
-#include <zlib.h>
-#ifdef _MSC_VER
-// MSVC: Exception handling for /DELAYLOAD.
-#  include "libwin32common/DelayLoadHelper.h"
-#endif /* _MSC_VER */
-
 namespace LibRomData {
-
-#ifdef _MSC_VER
-// DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
-#endif /* _MSC_VER */
 
 class Nintendo3DSFirmPrivate final : public RomDataPrivate
 {
@@ -239,25 +228,12 @@ int Nintendo3DSFirm::loadFieldData(void)
 	bool checkCustomFIRM = false;	// Check for a custom FIRM, e.g. Boot9Strap.
 	bool checkARM9 = false;		// Check for ARM9 homebrew.
 	if (arm11_entrypoint != 0 && arm9_entrypoint != 0) {
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-		// Delay load verification.
-		// TODO: Only if linked with /DELAYLOAD?
-		bool has_zlib = true;
-		if (DelayLoad_test_get_crc_table() != 0) {
-			// Delay load failed.
-			// Can't calculate the CRC32.
-			has_zlib = false;
-		}
-#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
-		// zlib isn't in a DLL, but we need to ensure that the
-		// CRC table is initialized anyway.
-		static const bool has_zlib = true;
-		get_crc_table();
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
-
 		// Calculate the CRC32 and look it up.
-		if (has_zlib && firmBuf) {
-			const uint32_t crc = crc32(0, firmBuf.get(), static_cast<unsigned int>(szFile));
+		// TODO: Check firmBuf before initializing CRC32?
+		Hash crc32Hash(Hash::Algorithm::CRC32);
+		if (crc32Hash.isUsable() && firmBuf) {
+			crc32Hash.process(firmBuf.get(), szFile);
+			const uint32_t crc = crc32Hash.getHash32();;
 			firmBin = Nintendo3DSFirmData::lookup_firmBin(crc);
 			if (firmBin != nullptr) {
 				// Official firmware binary.

src/libromdata/crypto/KeyStoreUI.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * KeyStoreUI.cpp: Key store UI base class.                                *
  *                                                                         *
- * Copyright (c) 2012-2023 by David Korth.                                 *
+ * Copyright (c) 2012-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -13,6 +13,7 @@
 #include "librpbase/crypto/KeyManager.hpp"
 #include "librpbase/crypto/IAesCipher.hpp"
 #include "librpbase/crypto/AesCipherFactory.hpp"
+#include "librpbase/crypto/Hash.hpp"
 #include "librpfile/RpFile.hpp"
 using namespace LibRpBase;
 using namespace LibRpText;
@@ -31,13 +32,6 @@ using std::u16string;
 using std::unique_ptr;
 using std::vector;
 
-// zlib for crc32()
-#include <zlib.h>
-#ifdef _MSC_VER
-// MSVC: Exception handling for /DELAYLOAD.
-#  include "libwin32common/DelayLoadHelper.h"
-#endif /* _MSC_VER */
-
 // for Qt-style signal emission
 #ifdef emit
 #undef emit
@@ -45,11 +39,6 @@ using std::vector;
 #define emit
 
 namespace LibRomData {
-
-#ifdef _MSC_VER
-// DelayLoad test implementation.
-DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
-#endif /* _MSC_VER */
 
 class KeyStoreUIPrivate
 {
@@ -1279,27 +1268,13 @@ KeyStoreUI::ImportReturn KeyStoreUIPrivate::importN3DSboot9bin(IRpFile *file)
 		return iret;
 	}
 
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-	// Delay load verification.
-	// TODO: Only if linked with /DELAYLOAD?
-	bool has_zlib = true;
-	if (DelayLoad_test_get_crc_table() != 0) {
-		// Delay load failed.
-		// Can't calculate the CRC32.
-		has_zlib = false;
-	}
-#else /* !defined(_MSC_VER) || !defined(ZLIB_IS_DLL) */
-	// zlib isn't in a DLL, but we need to ensure that the
-	// CRC table is initialized anyway.
-	static const bool has_zlib = true;
-	get_crc_table();
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
-
-	if (has_zlib) {
+	Hash crc32Hash(Hash::Algorithm::CRC32);
+	if (crc32Hash.isUsable()) {
 		// Check the CRC32.
 		// NOTE: CRC32 isn't particularly strong, so we'll still
 		// verify the keys before importing them.
-		const uint32_t crc = crc32(0, buf.get(), 32768);
+		crc32Hash.process(buf.get(), 32768);
+		const uint32_t crc = crc32Hash.getHash32();
 		if (crc != 0x9D50A525) {
 			// Incorrect CRC32.
 			iret.status = KeyStoreUI::ImportStatus::InvalidFile;

commit 4bc8b418da66d93c65a23ff8fd7101061fc94e25

[librpbase] Achievements.cpp: Missed a few DelayLoad checks.
This broke the MSVC build.

This fixes a regression from commit d23023d9f1e598f7721f98d281e2409d3a6d0f83.
(Use LibRpBase::Hash for CRC32 instead of calling zlib's crc32() directly.)

src/librpbase/Achievements.cpp
@@ -771,17 +771,6 @@ void Achievements::clearNotifyFunction(NotifyFunc func, intptr_t user_data)
  */
 int Achievements::unlock(ID id, int bit)
 {
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-	// Delay load verification.
-	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_get_crc_table() != 0) {
-		// Delay load failed.
-		// We won't be able to calculate CRC32s, so don't
-		// enable achievements at all.
-		return -ENOTSUP;
-	}
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
-
 	// If this achievement is bool/count, increment the value.
 	// If the value has hit the maximum, achievement is unlocked.
 	assert((int)id >= 0);
@@ -882,17 +871,6 @@ int Achievements::unlock(ID id, int bit)
  */
 time_t Achievements::isUnlocked(ID id) const
 {
-#if defined(_MSC_VER) && defined(ZLIB_IS_DLL)
-	// Delay load verification.
-	// TODO: Only if linked with /DELAYLOAD?
-	if (DelayLoad_test_get_crc_table() != 0) {
-		// Delay load failed.
-		// We won't be able to calculate CRC32s, so don't
-		// enable achievements at all.
-		return -1;
-	}
-#endif /* defined(_MSC_VER) && defined(ZLIB_IS_DLL) */
-
 	// If this achievement is bool/count, increment the value.
 	// If the value has hit the maximum, achievement is unlocked.
 	assert((int)id >= 0);