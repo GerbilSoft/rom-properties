GerbilSoft/rom-properties

commit cb408e8351dadf89759e2a6553e6b41e632b618d

[cmake] Very basic support for PowerPC.
I decided to try building rom-properties on Wii U Linux because why not.

cmake/macros/CPUInstructionSetFlags.cmake
@@ -52,6 +52,14 @@ ELSE()
 			SET(CPU_arm 1)
 			SET(WIN32_MANIFEST_PROCESSOR_ARCHITECTURE "arm")
 		ENDIF()
+	ELSEIF(arch MATCHES "^ppc")
+		IF(CMAKE_CL_64 OR ("${CMAKE_SIZEOF_VOID_P}" EQUAL 8))
+			SET(CPU_arm64 1)
+			SET(WIN32_MANIFEST_PROCESSOR_ARCHITECTURE "ppc64")
+		ELSE()
+			SET(CPU_arm 1)
+			SET(WIN32_MANIFEST_PROCESSOR_ARCHITECTURE "ppc")
+		ENDIF()
 	ENDIF()
 	UNSET(arch)
 ENDIF()

cmake/macros/DirInstallPaths.cmake
@@ -23,6 +23,10 @@ ELSEIF(CPU_arm)
 	SET(arch "arm")
 ELSEIF(CPU_arm64)
 	SET(arch "arm64")
+ELSEIF(CPU_ppc)
+	SET(arch "ppc")
+ELSEIF(CPU_ppc64)
+	SET(arch "ppc64")
 ELSE()
 	MESSAGE(FATAL_ERROR "Unsupported CPU architecture, please fix!")
 ENDIF()

commit 6cd3b8560b62a237730f657498328ac15e41a648

[cmake] CPUInstructionSetFlags.cmake: PowerPC should set CPU_ppc64 or CPU_ppc, *not* CPU_arm64 or CPU_arm.
Copy/paste error. Oops.

This fixes a regression from commit cb408e8351dadf89759e2a6553e6b41e632b618d.
([cmake] Very basic support for PowerPC.)

This was commit on July 4, 2022, and affects v2.0 - v2.3.
It's not likely that anyone would notice, though...

cmake/macros/CPUInstructionSetFlags.cmake
@@ -61,10 +61,10 @@ ELSE()
 			SET(CPU_ppc64le 1)
 			SET(WIN32_MANIFEST_PROCESSOR_ARCHITECTURE "ppc64le")
 		ELSEIF(CMAKE_CL_64 OR ("${CMAKE_SIZEOF_VOID_P}" EQUAL 8))
-			SET(CPU_arm64 1)
+			SET(CPU_ppc64 1)
 			SET(WIN32_MANIFEST_PROCESSOR_ARCHITECTURE "ppc64")
 		ELSE()
-			SET(CPU_arm 1)
+			SET(CPU_ppc 1)
 			SET(WIN32_MANIFEST_PROCESSOR_ARCHITECTURE "ppc")
 		ENDIF()
 	ELSEIF(arch MATCHES "^riscv")