GerbilSoft/rom-properties

commit 70cf0687ab56b2080c1ebcb28b8171b514479e4b

[gtk] Split common stuff from NautilusMenuProvider and ThunarMenuProvider into MenuProviderCommon.c.
is_file_uri() -> rp_is_file_uri(), but only for a string URI.
The NautilusFileInfo/ThunarFileInfo versions have been inlined into
their calling locations.

Use (nautilus|thunarx)_file_info_get_uri_scheme() to get the URI scheme
instead of getting the URI and then calling rp_is_file_uri(). This does
require adding a new function pointer...

CreateThumbnail.hpp: Remove `extern "C"`. This is already done by
G_BEGIN_DECLS / G_END_DECLS and breaks when included by C code.

Code size difference: (64-bit Gentoo Linux, gcc-13.1.0, release build, no LTO)

   text    data     bss     dec     hex filename
 378603    9456    1056  389115   5effb rom-properties-xfce.so [before]
 378867    9456    1072  389395   5f113 rom-properties-xfce.so [after]
   +264       0     +16    +280    +118 Difference

   text    data     bss     dec     hex filename
 383899    9792    1144  394835   60653 rom-properties-gtk3.so [before]
 384131    9792    1160  395083   6074b rom-properties-gtk3.so [after]
   +232       0     +16    +248     +f8 Difference

   text    data     bss     dec     hex filename
 389980    9504    1424  400908   61e0c rom-properties-gtk4.so
 390268    9504    1440  401212   61f3c rom-properties-gtk4.so
   -288       0     +16    +304    +130 Difference

src/gtk/gtk3/NautilusMenuProvider.cpp
@@ -10,9 +10,7 @@
 
 #include "stdafx.h"
 #include "NautilusMenuProvider.hpp"
-
-#include "img/TCreateThumbnail.hpp"
-#include "CreateThumbnail.hpp"
+#include "MenuProviderCommon.h"
 
 #include "../RomDataView.hpp"
 
@@ -102,35 +100,6 @@ rp_nautilus_menu_provider_page_provider_init(NautilusMenuProviderInterface *ifac
 	iface->get_file_items = rp_nautilus_menu_provider_get_file_items;
 }
 
-static bool
-is_file_uri(const gchar *uri)
-{
-	bool ret = false;
-	if (G_UNLIKELY(!uri))
-		return ret;
-
-	gchar *const scheme = g_uri_parse_scheme(uri);
-	if (!g_ascii_strcasecmp(scheme, "file")) {
-		// It's file:// protocol!
-		ret = true;
-	}
-
-	g_free(scheme);
-	return ret;
-}
-
-static bool
-is_file_uri(NautilusFileInfo *file_info)
-{
-	gchar *const uri = nautilus_file_info_get_uri(file_info);
-	if (G_UNLIKELY(!uri))
-		return false;
-
-	const bool ret = is_file_uri(uri);
-	g_free(uri);
-	return ret;
-}
-
 static gpointer
 rp_item_convert_to_png_ThreadFunc(GList *files)
 {
@@ -139,54 +108,10 @@ rp_item_convert_to_png_ThreadFunc(GList *files)
 		if (G_UNLIKELY(!source_uri))
 			continue;
 
-		// FIXME: We don't support writing to non-local files right now.
-		// Only allow file:// protocol.
-		if (!is_file_uri(source_uri)) {
-			// Not file:// protocol.
-			g_free(source_uri);
-			continue;
-		}
+		// TODO: Check for errors.
+		rp_menu_provider_convert_to_png(source_uri);
 
-		// Create the output filename based on the input filename.
-		const size_t source_len = strlen(source_uri);
-		if (source_len < 8) {
-			// Doesn't have "file://".
-			g_free(source_uri);
-			continue;
-		}
-		// Skip the "file://" portion.
-		// NOTE: Needs to be urldecoded.
-		const size_t output_len_esc = source_len - 7 + 16;
-		gchar *output_file_esc = static_cast<gchar*>(g_malloc(output_len_esc));
-		g_strlcpy(output_file_esc, &source_uri[7], output_len_esc);
-
-		// Find the current extension and replace it.
-		gchar *const dotpos = strrchr(output_file_esc, '.');
-		if (!dotpos) {
-			// No file extension. Add it.
-			g_strlcat(output_file_esc, ".png", output_len_esc);
-		} else {
-			// If the dot is after the last slash, we already have a file extension.
-			// Otherwise, we don't have one, and need to add it.
-			gchar *const slashpos = strrchr(output_file_esc, DIR_SEP_CHR);
-			if (slashpos < dotpos) {
-				// We already have a file extension.
-				strcpy(dotpos, ".png");
-			} else {
-				// No file extension.
-				g_strlcat(output_file_esc, ".png", output_len_esc);
-			}
-		}
-
-		// Unescape the URI.
-		gchar *const output_file = g_uri_unescape_string(output_file_esc, nullptr);
-
-		// Convert the file using rp_create_thumbnail2().
-		// TODO: Check for errors?
-		rp_create_thumbnail2(source_uri, output_file, 0, RPCT_FLAG_NO_XDG_THUMBNAIL_METADATA);
 		g_free(source_uri);
-		g_free(output_file_esc);
-		g_free(output_file);
 	}
 
 	nautilus_file_info_list_free(files);
@@ -232,7 +157,10 @@ rp_nautilus_menu_provider_get_file_items(
 
 		// FIXME: We don't support writing to non-local files right now.
 		// Only allow file:// protocol.
-		if (!is_file_uri(file_info)) {
+		gchar *const scheme = nautilus_file_info_get_uri_scheme(file_info);
+		const bool is_file = (scheme && g_ascii_strcasecmp(scheme, "file") != 0);
+		g_free(scheme);
+		if (!is_file) {
 			// Not file:// protocol.
 			continue;
 		}

src/gtk/gtk3/ThunarMenuProvider.cpp
@@ -10,9 +10,7 @@
 
 #include "stdafx.h"
 #include "ThunarMenuProvider.hpp"
-
-#include "CreateThumbnail.hpp"
-#include "img/TCreateThumbnail.hpp"
+#include "MenuProviderCommon.h"
 
 // thunarx.h mini replacement
 #include "thunarx-mini.h"
@@ -91,35 +89,6 @@ rp_thunar_menu_provider_page_provider_init(ThunarxMenuProviderIface *iface)
 	iface->get_file_menu_items = rp_thunar_menu_provider_get_file_menu_items;
 }
 
-static bool
-is_file_uri(const gchar *uri)
-{
-	bool ret = false;
-	if (G_UNLIKELY(!uri))
-		return ret;
-
-	gchar *const scheme = g_uri_parse_scheme(uri);
-	if (!g_ascii_strcasecmp(scheme, "file")) {
-		// It's file:// protocol!
-		ret = true;
-	}
-
-	g_free(scheme);
-	return ret;
-}
-
-static bool
-is_file_uri(ThunarxFileInfo *file_info)
-{
-	gchar *const uri = thunarx_file_info_get_uri(file_info);
-	if (G_UNLIKELY(!uri))
-		return false;
-
-	const bool ret = is_file_uri(uri);
-	g_free(uri);
-	return ret;
-}
-
 static gpointer
 rp_item_convert_to_png_ThreadFunc(GList *files)
 {
@@ -128,54 +97,10 @@ rp_item_convert_to_png_ThreadFunc(GList *files)
 		if (G_UNLIKELY(!source_uri))
 			continue;
 
-		// FIXME: We don't support writing to non-local files right now.
-		// Only allow file:// protocol.
-		if (!is_file_uri(source_uri)) {
-			// Not file:// protocol.
-			g_free(source_uri);
-			continue;
-		}
+		// TODO: Check for errors.
+		rp_menu_provider_convert_to_png(source_uri);
 
-		// Create the output filename based on the input filename.
-		const size_t source_len = strlen(source_uri);
-		if (source_len < 8) {
-			// Doesn't have "file://".
-			g_free(source_uri);
-			continue;
-		}
-		// Skip the "file://" portion.
-		// NOTE: Needs to be urldecoded.
-		const size_t output_len_esc = source_len - 7 + 16;
-		gchar *output_file_esc = static_cast<gchar*>(g_malloc(output_len_esc));
-		g_strlcpy(output_file_esc, &source_uri[7], output_len_esc);
-
-		// Find the current extension and replace it.
-		gchar *const dotpos = strrchr(output_file_esc, '.');
-		if (!dotpos) {
-			// No file extension. Add it.
-			g_strlcat(output_file_esc, ".png", output_len_esc);
-		} else {
-			// If the dot is after the last slash, we already have a file extension.
-			// Otherwise, we don't have one, and need to add it.
-			gchar *const slashpos = strrchr(output_file_esc, DIR_SEP_CHR);
-			if (slashpos < dotpos) {
-				// We already have a file extension.
-				strcpy(dotpos, ".png");
-			} else {
-				// No file extension.
-				g_strlcat(output_file_esc, ".png", output_len_esc);
-			}
-		}
-
-		// Unescape the URI.
-		gchar *const output_file = g_uri_unescape_string(output_file_esc, nullptr);
-
-		// Convert the file using rp_create_thumbnail2().
-		// TODO: Check for errors?
-		rp_create_thumbnail2(source_uri, output_file, 0, RPCT_FLAG_NO_XDG_THUMBNAIL_METADATA);
 		g_free(source_uri);
-		g_free(output_file_esc);
-		g_free(output_file);
 	}
 
 	thunarx_file_info_list_free(files);
@@ -219,7 +144,10 @@ rp_thunar_menu_provider_get_file_menu_items(ThunarxMenuProvider *provider, GtkWi
 
 		// FIXME: We don't support writing to non-local files right now.
 		// Only allow file:// protocol.
-		if (!is_file_uri(file_info)) {
+		gchar *const scheme = thunarx_file_info_get_uri_scheme(file_info);
+		const bool is_file = (scheme && g_ascii_strcasecmp(scheme, "file") != 0);
+		g_free(scheme);
+		if (!is_file) {
 			// Not file:// protocol.
 			continue;
 		}

src/gtk/CreateThumbnail.hpp
@@ -23,7 +23,6 @@ G_BEGIN_DECLS
  * @param flags Flags (see RpCreateThumbnailFlags)
  * @return 0 on success; non-zero on error.
  */
-extern "C"
 G_MODULE_EXPORT int RP_C_API rp_create_thumbnail2(const char *source_file, const char *output_file, int maximum_size, unsigned int flags);
 
 G_END_DECLS

src/gtk/gtk3/CMakeLists.txt
@@ -117,6 +117,7 @@ SET(${PROJECT_NAME}_SRCS
 	ThunarPlugin.cpp
 	ThunarPropertyPageProvider.cpp
 	ThunarMenuProvider.cpp
+	MenuProviderCommon.c
 	)
 SET(${PROJECT_NAME}_H
 	${${PROJECT_NAME}_CH} ${${PROJECT_NAME}_H}
@@ -130,6 +131,7 @@ SET(${PROJECT_NAME}_H
 	thunarx-mini.h
 	nautilus-extension-mini.h
 	mime-types.convert-to-png.h
+	MenuProviderCommon.h
 	)
 
 IF(HAVE_GSOUND OR (HAVE_LIBCANBERRA AND HAVE_LIBCANBERRA_GTK3))

src/gtk/gtk3/MenuProviderCommon.c
@@ -1,0 +1,91 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (GTK+ 3.x)                         *
+ * MenuProviderCommon.h: Common functions for Menu Providers               *
+ *                                                                         *
+ * Copyright (c) 2017-2023 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "stdafx.h"
+#include "MenuProviderCommon.h"
+
+#include "CreateThumbnail.hpp"
+#include "img/TCreateThumbnail.hpp"
+
+#include "tcharx.h"	// for DIR_SEP_CHR
+
+/**
+ * Is a URI using the file:// scheme?
+ * @param uri URI
+ * @return TRUE if using file://; FALSE if not.
+ */
+gboolean rp_is_file_uri(const gchar *uri)
+{
+	gboolean ret = FALSE;
+	if (G_UNLIKELY(!uri))
+		return ret;
+
+	gchar *const scheme = g_uri_parse_scheme(uri);
+	if (!g_ascii_strcasecmp(scheme, "file")) {
+		// It's file:// protocol!
+		ret = true;
+	}
+
+	g_free(scheme);
+	return ret;
+}
+
+/**
+ * Convert the source URI to a PNG image.
+ * @param source_uri URI
+ * @return 0 on success; non-zero on error.
+ */
+int rp_menu_provider_convert_to_png(const gchar *source_uri)
+{
+	// FIXME: We don't support writing to non-local files right now.
+	// Only allow file:// protocol.
+	if (!rp_is_file_uri(source_uri)) {
+		// Not file:// protocol.
+		return -1;
+	}
+
+	// Create the output filename based on the input filename.
+	const size_t source_len = strlen(source_uri);
+	if (source_len < 8) {
+		// Doesn't have "file://".
+		return -2;
+	}
+	// Skip the "file://" portion.
+	// NOTE: Needs to be urldecoded.
+	const size_t output_len_esc = source_len - 7 + 16;
+	gchar *const output_file_esc = g_malloc(output_len_esc);
+	g_strlcpy(output_file_esc, &source_uri[7], output_len_esc);
+
+	// Find the current extension and replace it.
+	gchar *const dotpos = strrchr(output_file_esc, '.');
+	if (!dotpos) {
+		// No file extension. Add it.
+		g_strlcat(output_file_esc, ".png", output_len_esc);
+	} else {
+		// If the dot is after the last slash, we already have a file extension.
+		// Otherwise, we don't have one, and need to add it.
+		gchar *const slashpos = strrchr(output_file_esc, DIR_SEP_CHR);
+		if (slashpos < dotpos) {
+			// We already have a file extension.
+			strcpy(dotpos, ".png");
+		} else {
+			// No file extension.
+			g_strlcat(output_file_esc, ".png", output_len_esc);
+		}
+	}
+
+	// Unescape the URI.
+	gchar *const output_file = g_uri_unescape_string(output_file_esc, NULL);
+
+	// Convert the file using rp_create_thumbnail2().
+	// TODO: Check for errors?
+	int ret = rp_create_thumbnail2(source_uri, output_file, 0, RPCT_FLAG_NO_XDG_THUMBNAIL_METADATA);
+	g_free(output_file_esc);
+	g_free(output_file);
+	return ret;
+}

src/gtk/gtk3/MenuProviderCommon.h
@@ -1,0 +1,29 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (GTK+ 3.x)                         *
+ * MenuProviderCommon.h: Common functions for Menu Providers               *
+ *                                                                         *
+ * Copyright (c) 2017-2023 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#pragma once
+
+#include "glib-compat.h"
+
+G_BEGIN_DECLS
+
+/**
+ * Is a URI using the file:// scheme?
+ * @param uri URI
+ * @return TRUE if using file://; FALSE if not.
+ */
+gboolean rp_is_file_uri(const gchar *uri);
+
+/**
+ * Convert the source URI to a PNG image.
+ * @param source_uri URI
+ * @return 0 on success; non-zero on error.
+ */
+int rp_menu_provider_convert_to_png(const gchar *source_uri);
+
+G_END_DECLS

src/gtk/gtk3/NautilusPlugin.cpp
@@ -25,6 +25,7 @@ static void *libextension_so;
 PFN_NAUTILUS_FILE_INFO_GET_TYPE			pfn_nautilus_file_info_get_type;
 PFN_NAUTILUS_FILE_INFO_GET_MIME_TYPE		pfn_nautilus_file_info_get_mime_type;
 PFN_NAUTILUS_FILE_INFO_GET_URI			pfn_nautilus_file_info_get_uri;
+PFN_NAUTILUS_FILE_INFO_GET_URI_SCHEME		pfn_nautilus_file_info_get_uri_scheme;
 PFN_NAUTILUS_FILE_INFO_LIST_COPY		pfn_nautilus_file_info_list_copy;
 PFN_NAUTILUS_FILE_INFO_LIST_FREE		pfn_nautilus_file_info_list_free;
 PFN_NAUTILUS_MENU_ITEM_GET_TYPE			pfn_nautilus_menu_item_get_type;
@@ -81,6 +82,7 @@ prefix##_module_initialize(GTypeModule *module) \
 	DLSYM(nautilus_file_info_get_type,		prefix##_file_info_get_type); \
 	DLSYM(nautilus_file_info_get_mime_type,		prefix##_file_info_get_mime_type); \
 	DLSYM(nautilus_file_info_get_uri,		prefix##_file_info_get_uri); \
+	DLSYM(nautilus_file_info_get_uri_scheme,	prefix##_file_info_get_uri_scheme); \
 	DLSYM(nautilus_file_info_list_copy,		prefix##_file_info_list_copy); \
 	DLSYM(nautilus_file_info_list_free,		prefix##_file_info_list_free); \
 	DLSYM(nautilus_menu_item_get_type,		prefix##_menu_item_get_type); \

src/gtk/gtk3/NautilusPlugin.hpp
@@ -43,6 +43,7 @@ typedef struct _NautilusPropertyPage NautilusPropertyPage;
 typedef GType (*PFN_NAUTILUS_FILE_INFO_GET_TYPE)(void);
 typedef gchar* (*PFN_NAUTILUS_FILE_INFO_GET_MIME_TYPE)(NautilusFileInfo *file_info);
 typedef char* (*PFN_NAUTILUS_FILE_INFO_GET_URI)(NautilusFileInfo *file);
+typedef char* (*PFN_NAUTILUS_FILE_INFO_GET_URI_SCHEME)(NautilusFileInfo *file);
 typedef GList* (*PFN_NAUTILUS_FILE_INFO_LIST_COPY)(GList *files);
 typedef void (*PFN_NAUTILUS_FILE_INFO_LIST_FREE)(GList *files);
 
@@ -58,6 +59,7 @@ typedef NautilusPropertyPage* (*PFN_NAUTILUS_PROPERTY_PAGE_NEW)(const char *name
 extern PFN_NAUTILUS_FILE_INFO_GET_TYPE			pfn_nautilus_file_info_get_type;
 extern PFN_NAUTILUS_FILE_INFO_GET_MIME_TYPE		pfn_nautilus_file_info_get_mime_type;
 extern PFN_NAUTILUS_FILE_INFO_GET_URI			pfn_nautilus_file_info_get_uri;
+extern PFN_NAUTILUS_FILE_INFO_GET_URI_SCHEME		pfn_nautilus_file_info_get_uri_scheme;
 extern PFN_NAUTILUS_FILE_INFO_LIST_COPY			pfn_nautilus_file_info_list_copy;
 extern PFN_NAUTILUS_FILE_INFO_LIST_FREE			pfn_nautilus_file_info_list_free;
 extern PFN_NAUTILUS_MENU_ITEM_GET_TYPE			pfn_nautilus_menu_item_get_type;
@@ -70,6 +72,7 @@ extern PFN_NAUTILUS_PROPERTY_PAGE_NEW			pfn_nautilus_property_page_new;
 #define nautilus_file_info_get_type()			(pfn_nautilus_file_info_get_type ())
 #define nautilus_file_info_get_mime_type(file_info)	(pfn_nautilus_file_info_get_mime_type(file_info))
 #define nautilus_file_info_get_uri(file)		(pfn_nautilus_file_info_get_uri(file))
+#define nautilus_file_info_get_uri_scheme(file)		(pfn_nautilus_file_info_get_uri_scheme(file))
 #define nautilus_file_info_list_copy(files)		(pfn_nautilus_file_info_list_copy(files))
 #define nautilus_file_info_list_free(files)		(pfn_nautilus_file_info_list_free(files))
 #define nautilus_menu_item_get_type()			(pfn_nautilus_menu_item_get_type ())

commit e322dd037aef7d93f5485610582ebbf08c5db44d

[gtk] *MenuProvider.cpp: Fix URI scheme comparison.
This broke the "Convert to PNG" menu item, since it was returning *false*
if the scheme matched "file".

This fixes a regression from commit 70cf0687ab56b2080c1ebcb28b8171b514479e4b.
([gtk] Split common stuff from NautilusMenuProvider and ThunarMenuProvider into MenuProviderCommon.c.)

Affects: v2.2.1 - v2.3

src/gtk/gtk3/NautilusMenuProvider.cpp
@@ -158,7 +158,7 @@ rp_nautilus_menu_provider_get_file_items(
 		// FIXME: We don't support writing to non-local files right now.
 		// Only allow file:// protocol.
 		gchar *const scheme = nautilus_file_info_get_uri_scheme(file_info);
-		const bool is_file = (scheme && g_ascii_strcasecmp(scheme, "file") != 0);
+		const bool is_file = (scheme && !g_ascii_strcasecmp(scheme, "file"));
 		g_free(scheme);
 		if (!is_file) {
 			// Not file:// protocol.

src/gtk/gtk3/ThunarMenuProvider.cpp
@@ -145,7 +145,7 @@ rp_thunar_menu_provider_get_file_menu_items(ThunarxMenuProvider *provider, GtkWi
 		// FIXME: We don't support writing to non-local files right now.
 		// Only allow file:// protocol.
 		gchar *const scheme = thunarx_file_info_get_uri_scheme(file_info);
-		const bool is_file = (scheme && g_ascii_strcasecmp(scheme, "file") != 0);
+		const bool is_file = (scheme && !g_ascii_strcasecmp(scheme, "file"));
 		g_free(scheme);
 		if (!is_file) {
 			// Not file:// protocol.