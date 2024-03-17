16df17088e7a659fd7127b86083c40103b0beddc
David Korth <gerbilsoft@gerbilsoft.com>
2024-03-17 00:07:00 -0400

    [librpsecure] secoptions.c: GetAce() expects `LPVOID*` (`void**`).
    MSVC didn't have any problems with the implicit cast, but mingw-w64 gcc
    is now complaining because we added -Werror=incompatible-pointer-types:
    
    src/librpsecure/win32/secoptions.c: In function ‘HardenProcessIntegrityLevelPolicy’:
    src/librpsecure/win32/secoptions.c:147:46: error: passing argument 3 of ‘GetAce’ from incompatible pointer type [-Werror=incompatible-pointer-types]
      147 |                 if (GetAce(sacl, dwAceIndex, &ace) &&
          |                                              ^~~~
          |                                              |
          |                                              struct _SYSTEM_MANDATORY_LABEL_ACE **
    In file included from /usr/x86_64-w64-mingw32/usr/include/winbase.h:34,
                     from /usr/x86_64-w64-mingw32/usr/include/windows.h:70,
                     from src/librpsecure/win32/secoptions.c:21:
    /usr/x86_64-w64-mingw32/usr/include/securitybaseapi.h:152:73: note: expected ‘void **’ but argument is of type ‘struct _SYSTEM_MANDATORY_LABEL_ACE **’
      152 |   WINADVAPI WINBOOL WINAPI GetAce (PACL pAcl, DWORD dwAceIndex, LPVOID *pAce);
          |                                                                 ~~~~~~~~^~~~
    
    This fixes a regression from commit d8538ed6bb48f1e53c3419f319fdc4435b293474.
    ([cmake] gcc.cmake: Add some -Werror options from OpenSSF.)

--- src/librpsecure/win32/secoptions.c
+++ src/librpsecure/win32/secoptions.c
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpsecure/win32)                *
  * secoptions.c: Security options for executables.                         *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -144,7 +144,7 @@ static DWORD HardenProcessIntegrityLevelPolicy(void)
 
 	for (dwAceIndex = 0; dwAceIndex < sacl->AceCount; ++dwAceIndex) {
 		PSYSTEM_MANDATORY_LABEL_ACE ace;
-		if (GetAce(sacl, dwAceIndex, &ace) &&
+		if (GetAce(sacl, dwAceIndex, (void**)&ace) &&
 		    ace->Header.AceType == SYSTEM_MANDATORY_LABEL_ACE_TYPE)
 		{
 			// Found the Mandatory Label ACE.
