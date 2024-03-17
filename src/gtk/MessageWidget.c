GerbilSoft/rom-properties

commit 058f28aa5759587bd875a5e3d513f32ebc237196

[gtk] rp_message_widget_set_text(): Don't set the text if it's the same as what we already have.
This also suppresses the g_object_notify_by_pspec() call.

src/gtk/MessageWidget.c
@@ -439,9 +439,10 @@ rp_message_widget_set_text(RpMessageWidget *widget, const gchar *str)
 {
 	g_return_if_fail(RP_IS_MESSAGE_WIDGET(widget));
 
-	// TODO: Verify that the text has actually changed first.
-	gtk_label_set_text(GTK_LABEL(widget->label), str);
-	g_object_notify_by_pspec(G_OBJECT(widget), props[PROP_TEXT]);
+	if (!g_strcmp0(gtk_label_get_text(GTK_LABEL(widget->label)), str)) {
+		gtk_label_set_text(GTK_LABEL(widget->label), str);
+		g_object_notify_by_pspec(G_OBJECT(widget), props[PROP_TEXT]);
+	}
 }
 
 const gchar*

commit 2cf30d5102371018c0cf86b486805794dd49ef40

[gtk] rp_message_widget_set_text(): Fix inverted g_strcmp0() check.
This ended up breaking setting text, since it would only set text if it
matched the existing label, which is blank at initialization.

This fixes a regression from commit 058f28aa5759587bd875a5e3d513f32ebc237196.
([gtk] rp_message_widget_set_text(): Don't set the text if it's the same as what we already have.)

src/gtk/MessageWidget.c
@@ -439,7 +439,7 @@ rp_message_widget_set_text(RpMessageWidget *widget, const gchar *str)
 {
 	g_return_if_fail(RP_IS_MESSAGE_WIDGET(widget));
 
-	if (!g_strcmp0(gtk_label_get_text(GTK_LABEL(widget->label)), str)) {
+	if (g_strcmp0(gtk_label_get_text(GTK_LABEL(widget->label)), str) != 0) {
 		gtk_label_set_text(GTK_LABEL(widget->label), str);
 		g_object_notify_by_pspec(G_OBJECT(widget), props[PROP_TEXT]);
 	}