GerbilSoft/rom-properties

commit 329faa83a2444f11d34eb22a41e6173756340b80

[librpbase] Config: Add *_default() functions to get default values.
[gtk,kde,win32] OptionsTab: Update to use Config::*_default().

- GTK: There was a duplicate in the reset() function that checked
  against useIntIconForSmallSizes_default for chkUseIntIconForSmallSizes.
  The default value was the same for each, so this was effectively a nop.

- GTK: New typedef gtk_cbo_index_t for the combo box index.
  - GtkComboBox: gint
  - GtkDropDown: guint

- KDE: Use LanguageComboBox's LC functions instead of setting/getting
  the QComboBox currentIndex.
  - The default value in Config is based on an LC, not an index.

- Win32: Likewise.

src/kde/config/OptionsTab.cpp
@@ -30,7 +30,6 @@ class OptionsTabPrivate
 
 		// PAL language codes for GameTDB.
 		static const uint32_t pal_lc[];
-		static const int pal_lc_idx_def;	// Default index in pal_lc[].
 };
 
 /** OptionsTabPrivate **/
@@ -39,7 +38,6 @@ class OptionsTabPrivate
 // NOTE: 'au' is technically not a language code, but
 // GameTDB handles it as a separate language.
 const uint32_t OptionsTabPrivate::pal_lc[] = {'au', 'de', 'en', 'es', 'fr', 'it', 'nl', 'pt', 'ru'};
-const int OptionsTabPrivate::pal_lc_idx_def = 2;
 
 OptionsTabPrivate::OptionsTabPrivate()
 	: changed(false)
@@ -108,17 +106,7 @@ void OptionsTab::reset(void)
 	d->ui.chkShowXAttrView->setChecked(config->showXAttrView());
 
 	// PAL language code
-	const uint32_t lc = config->palLanguageForGameTDB();
-	int idx = 0;
-	for (; idx < ARRAY_SIZE_I(d->pal_lc); idx++) {
-		if (d->pal_lc[idx] == lc)
-			break;
-	}
-	if (idx >= ARRAY_SIZE_I(d->pal_lc)) {
-		// Out of range. Default to 'en'.
-		idx = d->pal_lc_idx_def;
-	}
-	d->ui.cboGameTDBPAL->setCurrentIndex(idx);
+	d->ui.cboGameTDBPAL->setSelectedLC(config->palLanguageForGameTDB());
 
 	d->changed = false;
 }
@@ -130,70 +118,59 @@ void OptionsTab::reset(void)
  */
 void OptionsTab::loadDefaults(void)
 {
-	// TODO: Get the defaults from Config.
-	// For now, hard-coding everything here.
-
-	// Downloads
-	static const bool extImgDownloadEnabled_default = true;
-	static const bool useIntIconForSmallSizes_default = true;
-	static const bool storeFileOriginInfo_default = true;
-	static const int palLanguageForGameTDB_default =
-		OptionsTabPrivate::pal_lc_idx_def;	// cboGameTDBPAL index ('en')
-
-	// Image bandwidth options
-	static const Config::ImgBandwidth imgBandwidthUnmetered_default = Config::ImgBandwidth::HighRes;
-	static const Config::ImgBandwidth imgBandwidthMetered_default = Config::ImgBandwidth::NormalRes;
-
-	// Options
-	static const bool showDangerousPermissionsOverlayIcon_default = true;
-	static const bool enableThumbnailOnNetworkFS_default = false;
-	static const bool showXAttrView_default = true;
+	// Has any value changed due to resetting to defaults?
 	bool isDefChanged = false;
 
 	Q_D(OptionsTab);
 
 	// Downloads
-	if (d->ui.grpExtImgDownloads->isChecked() != extImgDownloadEnabled_default) {
-		d->ui.grpExtImgDownloads->setChecked(extImgDownloadEnabled_default);
+	bool bdef = Config::enableThumbnailOnNetworkFS_default();
+	if (d->ui.grpExtImgDownloads->isChecked() != bdef) {
+		d->ui.grpExtImgDownloads->setChecked(bdef);
 		isDefChanged = true;
 	}
-	if (d->ui.chkUseIntIconForSmallSizes->isChecked() != useIntIconForSmallSizes_default) {
-		d->ui.chkUseIntIconForSmallSizes->setChecked(useIntIconForSmallSizes_default);
+	bdef = Config::useIntIconForSmallSizes_default();
+	if (d->ui.chkUseIntIconForSmallSizes->isChecked() != bdef) {
+		d->ui.chkUseIntIconForSmallSizes->setChecked(bdef);
 		isDefChanged = true;
 	}
-	if (d->ui.chkStoreFileOriginInfo->isChecked() != storeFileOriginInfo_default) {
-		d->ui.chkStoreFileOriginInfo->setChecked(storeFileOriginInfo_default);
+	bdef = Config::storeFileOriginInfo_default();
+	if (d->ui.chkStoreFileOriginInfo->isChecked() != bdef) {
+		d->ui.chkStoreFileOriginInfo->setChecked(bdef);
 		isDefChanged = true;
 	}
-	if (d->ui.cboGameTDBPAL->currentIndex() != palLanguageForGameTDB_default) {
-		d->ui.cboGameTDBPAL->setCurrentIndex(palLanguageForGameTDB_default);
+	const uint32_t u32def = Config::palLanguageForGameTDB_default();
+	if (d->ui.cboGameTDBPAL->selectedLC() != u32def) {
+		d->ui.cboGameTDBPAL->setSelectedLC(u32def);
 		isDefChanged = true;
 	}
 
 	// Image bandwidth options
-	if (d->ui.cboUnmeteredConnection->currentIndex() != static_cast<int>(imgBandwidthUnmetered_default)) {
-		d->ui.cboUnmeteredConnection->setCurrentIndex(static_cast<int>(imgBandwidthUnmetered_default));
+	int idef = static_cast<int>(Config::imgBandwidthUnmetered_default());
+	if (d->ui.cboUnmeteredConnection->currentIndex() != idef) {
+		d->ui.cboUnmeteredConnection->setCurrentIndex(idef);
 		isDefChanged = true;
 	}
-	if (d->ui.cboMeteredConnection->currentIndex() != static_cast<int>(imgBandwidthMetered_default)) {
-		d->ui.cboMeteredConnection->setCurrentIndex(static_cast<int>(imgBandwidthMetered_default));
+	idef = static_cast<int>(Config::imgBandwidthMetered_default());
+	if (d->ui.cboMeteredConnection->currentIndex() != idef) {
+		d->ui.cboMeteredConnection->setCurrentIndex(idef);
 		isDefChanged = true;
 	}
 
 	// Options
-	if (d->ui.chkShowDangerousPermissionsOverlayIcon->isChecked() !=
-		showDangerousPermissionsOverlayIcon_default)
-	{
-		d->ui.chkShowDangerousPermissionsOverlayIcon->setChecked(
-			showDangerousPermissionsOverlayIcon_default);
+	bdef = Config::showDangerousPermissionsOverlayIcon_default();
+	if (d->ui.chkShowDangerousPermissionsOverlayIcon->isChecked() != bdef) {
+		d->ui.chkShowDangerousPermissionsOverlayIcon->setChecked(bdef);
 		isDefChanged = true;
 	}
-	if (d->ui.chkEnableThumbnailOnNetworkFS->isChecked() != enableThumbnailOnNetworkFS_default) {
-		d->ui.chkEnableThumbnailOnNetworkFS->setChecked(enableThumbnailOnNetworkFS_default);
+	bdef = Config::enableThumbnailOnNetworkFS_default();
+	if (d->ui.chkEnableThumbnailOnNetworkFS->isChecked() != bdef) {
+		d->ui.chkEnableThumbnailOnNetworkFS->setChecked(bdef);
 		isDefChanged = true;
 	}
-	if (d->ui.chkShowXAttrView->isChecked() != showXAttrView_default) {
-		d->ui.chkShowXAttrView->setChecked(showXAttrView_default);
+	bdef = Config::showXAttrView_default();
+	if (d->ui.chkShowXAttrView->isChecked() != bdef) {
+		d->ui.chkShowXAttrView->setChecked(bdef);
 		isDefChanged = true;
 	}
 

src/gtk/config/OptionsTab.cpp
@@ -24,7 +24,6 @@ using namespace LibRpBase;
 // TODO: Combine with the KDE version.
 // NOTE: GTK LanguageComboBox uses a NULL-terminated pal_lc[] array.
 static const uint32_t pal_lc[] = {'au', 'de', 'en', 'es', 'fr', 'it', 'nl', 'pt', 'ru', 0};
-static const int pal_lc_def = 'en';
 
 #if GTK_CHECK_VERSION(3,0,0)
 typedef GtkBoxClass superclass;
@@ -406,78 +405,60 @@ rp_options_tab_load_defaults(RpOptionsTab *tab)
 	g_return_if_fail(RP_IS_OPTIONS_TAB(tab));
 	tab->inhibit = true;
 
-	// TODO: Get the defaults from Config.
-	// For now, hard-coding everything here.
-
-	// Downloads
-	static const bool extImgDownloadEnabled_default = true;
-	static const bool useIntIconForSmallSizes_default = true;
-	static const bool storeFileOriginInfo_default = true;
-	static const uint32_t palLanguageForGameTDB_default = pal_lc_def;
-
-	// Image bandwidth options
-	static const Config::ImgBandwidth imgBandwidthUnmetered_default = Config::ImgBandwidth::HighRes;
-	static const Config::ImgBandwidth imgBandwidthMetered_default = Config::ImgBandwidth::NormalRes;
-
-	// Options
-	static const bool showDangerousPermissionsOverlayIcon_default = true;
-	static const bool enableThumbnailOnNetworkFS_default = false;
-	static const bool showXAttrView_default = true;
+	// Has any value changed due to resetting to defaults?
 	bool isDefChanged = false;
 
 	// Downloads
-	if (COMPARE_CHK(tab->chkExtImgDownloadEnabled, extImgDownloadEnabled_default)) {
-		SET_CHK(tab->chkExtImgDownloadEnabled, extImgDownloadEnabled_default);
+	bool bdef = Config::extImgDownloadEnabled_default();
+	if (COMPARE_CHK(tab->chkExtImgDownloadEnabled, bdef)) {
+		SET_CHK(tab->chkExtImgDownloadEnabled, bdef);
 		isDefChanged = true;
 		// Update sensitivity
 		rp_options_tab_chkExtImgDownloadEnabled_toggled(GTK_CHECK_BUTTON(tab->chkExtImgDownloadEnabled), tab);
 	}
-	if (COMPARE_CHK(tab->chkUseIntIconForSmallSizes, useIntIconForSmallSizes_default)) {
-		SET_CHK(tab->chkUseIntIconForSmallSizes, extImgDownloadEnabled_default);
+	bdef = Config::useIntIconForSmallSizes_default();
+	if (COMPARE_CHK(tab->chkUseIntIconForSmallSizes, bdef)) {
+		SET_CHK(tab->chkUseIntIconForSmallSizes, bdef);
 		isDefChanged = true;
 	}
-	if (COMPARE_CHK(tab->chkUseIntIconForSmallSizes, useIntIconForSmallSizes_default)) {
-		SET_CHK(tab->chkUseIntIconForSmallSizes, useIntIconForSmallSizes_default);
+	bdef = Config::storeFileOriginInfo_default();
+	if (COMPARE_CHK(tab->chkStoreFileOriginInfo, bdef)) {
+		SET_CHK(tab->chkStoreFileOriginInfo, bdef);
 		isDefChanged = true;
 	}
-	if (COMPARE_CHK(tab->chkStoreFileOriginInfo, storeFileOriginInfo_default)) {
-		SET_CHK(tab->chkStoreFileOriginInfo, storeFileOriginInfo_default);
-		isDefChanged = true;
-	}
-	if (rp_language_combo_box_get_selected_lc(RP_LANGUAGE_COMBO_BOX(tab->cboGameTDBPAL)) != palLanguageForGameTDB_default) {
+	const uint32_t u32def = Config::palLanguageForGameTDB_default();
+	if (rp_language_combo_box_get_selected_lc(RP_LANGUAGE_COMBO_BOX(tab->cboGameTDBPAL)) != u32def) {
 		rp_language_combo_box_set_selected_lc(
-			RP_LANGUAGE_COMBO_BOX(tab->cboGameTDBPAL),
-			palLanguageForGameTDB_default);
+			RP_LANGUAGE_COMBO_BOX(tab->cboGameTDBPAL), u32def);
 		isDefChanged = true;
 	}
 
 	// Image bandwidth options
-	if (COMPARE_CBO(tab->cboUnmeteredConnection, static_cast<int>(imgBandwidthUnmetered_default))) {
-		SET_CBO(tab->cboUnmeteredConnection, static_cast<int>(imgBandwidthUnmetered_default));
+	gtk_cbo_index_t idxdef = static_cast<gtk_cbo_index_t>(Config::imgBandwidthUnmetered_default());
+	if (COMPARE_CBO(tab->cboUnmeteredConnection, idxdef)) {
+		SET_CBO(tab->cboUnmeteredConnection, idxdef);
 		isDefChanged = true;
 	}
-	if (COMPARE_CBO(tab->cboMeteredConnection, static_cast<int>(imgBandwidthMetered_default))) {
-		SET_CBO(tab->cboMeteredConnection, static_cast<int>(imgBandwidthMetered_default));
+	idxdef = static_cast<gtk_cbo_index_t>(Config::imgBandwidthMetered_default());
+	if (COMPARE_CBO(tab->cboMeteredConnection, idxdef)) {
+		SET_CBO(tab->cboMeteredConnection, idxdef);
 		isDefChanged = true;
 	}
 
 	// Options
-	if (COMPARE_CHK(tab->chkShowDangerousPermissionsOverlayIcon, showDangerousPermissionsOverlayIcon_default)) {
-		gtk_check_button_set_active(
-			GTK_CHECK_BUTTON(tab->chkShowDangerousPermissionsOverlayIcon),
-			showDangerousPermissionsOverlayIcon_default);
+	bdef = Config::showDangerousPermissionsOverlayIcon_default();
+	if (COMPARE_CHK(tab->chkShowDangerousPermissionsOverlayIcon, bdef)) {
+		gtk_check_button_set_active(GTK_CHECK_BUTTON(tab->chkShowDangerousPermissionsOverlayIcon), bdef);
 		isDefChanged = true;
 	}
-	if (COMPARE_CHK(tab->chkEnableThumbnailOnNetworkFS, enableThumbnailOnNetworkFS_default)) {
-		gtk_check_button_set_active(
-			GTK_CHECK_BUTTON(tab->chkEnableThumbnailOnNetworkFS),
-			enableThumbnailOnNetworkFS_default);
+	bdef = Config::enableThumbnailOnNetworkFS_default();
+	if (COMPARE_CHK(tab->chkEnableThumbnailOnNetworkFS, bdef)) {
+		gtk_check_button_set_active(GTK_CHECK_BUTTON(tab->chkEnableThumbnailOnNetworkFS), bdef);
 		isDefChanged = true;
 	}
-	if (COMPARE_CHK(tab->chkShowXAttrView, showXAttrView_default)) {
-		gtk_check_button_set_active(
-			GTK_CHECK_BUTTON(tab->chkShowXAttrView),
-			showXAttrView_default);
+	bdef = Config::showXAttrView_default();
+	if (COMPARE_CHK(tab->chkShowXAttrView, bdef)) {
+		gtk_check_button_set_active(GTK_CHECK_BUTTON(tab->chkShowXAttrView), bdef);
 		isDefChanged = true;
 	}
 

src/win32/config/OptionsTab.cpp
@@ -116,7 +116,6 @@ class OptionsTabPrivate
 
 		// PAL language codes for GameTDB.
 		static const uint32_t pal_lc[];
-		static const int pal_lc_idx_def;
 };
 
 /** OptionsTabPrivate **/
@@ -127,7 +126,6 @@ class OptionsTabPrivate
 // TODO: Combine with the KDE version.
 // NOTE: Win32 LanguageComboBox uses a NULL-terminated pal_lc[] array.
 const uint32_t OptionsTabPrivate::pal_lc[] = {'au', 'de', 'en', 'es', 'fr', 'it', 'nl', 'pt', 'ru', 0};
-const int OptionsTabPrivate::pal_lc_idx_def = 2;
 
 OptionsTabPrivate::OptionsTabPrivate()
 	: hPropSheetPage(nullptr)
@@ -173,18 +171,9 @@ void OptionsTabPrivate::reset(void)
 	CheckDlgButton(hWndPropSheet, IDC_OPTIONS_DANGEROUSPERMISSIONS, BST_UNCHECKED);
 	EnableWindow(GetDlgItem(hWndPropSheet, IDC_OPTIONS_DANGEROUSPERMISSIONS), FALSE);
 
-	// PAL language code.
-	const uint32_t lc = config->palLanguageForGameTDB();
-	int idx = 0;
-	for (; idx < ARRAY_SIZE_I(pal_lc); idx++) {
-		if (pal_lc[idx] == lc)
-			break;
-	}
-	if (idx >= ARRAY_SIZE_I(pal_lc)) {
-		// Out of range. Default to 'en'.
-		idx = pal_lc_idx_def;
-	}
-	ComboBox_SetCurSel(GetDlgItem(hWndPropSheet, IDC_OPTIONS_PALLANGUAGEFORGAMETDB), idx);
+	// PAL language code
+	HWND cboGameTDBPAL = GetDlgItem(hWndPropSheet, IDC_OPTIONS_PALLANGUAGEFORGAMETDB);
+	LanguageComboBox_SetSelectedLC(cboGameTDBPAL, config->palLanguageForGameTDB());
 
 	// No longer changed.
 	changed = false;
@@ -195,64 +184,51 @@ void OptionsTabPrivate::loadDefaults(void)
 	assert(hWndPropSheet != nullptr);
 	if (!hWndPropSheet)
 		return;
-
-	// TODO: Get the defaults from Config.
-	// For now, hard-coding everything here.
-	// Downloads
-	static const bool extImgDownloadEnabled_default = true;
-	static const bool useIntIconForSmallSizes_default = true;
-	static const bool storeFileOriginInfo_default = true;
-	static const int palLanguageForGameTDB_default =
-		OptionsTabPrivate::pal_lc_idx_def;	// cboGameTDBPAL index ('en')
-
-	// Image bandwidth options
-	static const Config::ImgBandwidth imgBandwidthUnmetered_default = Config::ImgBandwidth::HighRes;
-	static const Config::ImgBandwidth imgBandwidthMetered_default = Config::ImgBandwidth::NormalRes;
-
-	// Options
-	// FIXME: Uncomment this once the "dangerous" permissions overlay
-	// is working on Windows.
-	//static const bool showDangerousPermissionsOverlayIcon_default = true;
-	static const bool enableThumbnailOnNetworkFS_default = false;
+	// Has any value changed due to resetting to defaults?
 	bool isDefChanged = false;
 
 	// Downloads
-	bool cur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_CHKEXTIMGDL));
-	if (cur != extImgDownloadEnabled_default) {
-		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_CHKEXTIMGDL, boolToBstChecked(extImgDownloadEnabled_default));
+	bool bcur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_CHKEXTIMGDL));
+	bool bdef = Config::extImgDownloadEnabled_default();
+	if (bcur != bdef) {
+		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_CHKEXTIMGDL, boolToBstChecked(bdef));
 		isDefChanged = true;
 		// Update sensitivity
 		updateGrpExtImgDl();
 	}
-	cur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_INTICONSMALL));
-	if (cur != useIntIconForSmallSizes_default) {
-		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_INTICONSMALL, boolToBstChecked(useIntIconForSmallSizes_default));
+	bcur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_INTICONSMALL));
+	bdef = Config::useIntIconForSmallSizes_default();
+	if (bcur != bdef) {
+		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_INTICONSMALL, boolToBstChecked(bdef));
 		isDefChanged = true;
 	}
-	cur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_STOREFILEORIGININFO));
-	if (cur != storeFileOriginInfo_default) {
-		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_STOREFILEORIGININFO,
-			boolToBstChecked(storeFileOriginInfo_default));
+	bcur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_STOREFILEORIGININFO));
+	bdef = Config::storeFileOriginInfo_default();
+	if (bcur != bdef) {
+		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_STOREFILEORIGININFO, boolToBstChecked(bdef));
 		isDefChanged = true;
 	}
 	HWND cboGameTDBPAL = GetDlgItem(hWndPropSheet, IDC_OPTIONS_PALLANGUAGEFORGAMETDB);
-	int idx = ComboBox_GetCurSel(cboGameTDBPAL);
-	if (idx != palLanguageForGameTDB_default) {
-		ComboBox_SetCurSel(cboGameTDBPAL, idx);
+	uint32_t u32cur = LanguageComboBox_GetSelectedLC(cboGameTDBPAL);
+	uint32_t u32def = Config::palLanguageForGameTDB_default();
+	if (u32cur != u32def) {
+		LanguageComboBox_SetSelectedLC(cboGameTDBPAL, u32def);
 		isDefChanged = true;
 	}
 
 	// Image bandwidth options
 	HWND cboUnmeteredDL = GetDlgItem(hWndPropSheet, IDC_OPTIONS_CBO_UNMETERED_DL);
-	idx = ComboBox_GetCurSel(cboUnmeteredDL);
-	if (idx != static_cast<int>(imgBandwidthUnmetered_default)) {
-		ComboBox_SetCurSel(cboUnmeteredDL, static_cast<int>(imgBandwidthUnmetered_default));
+	int icur = ComboBox_GetCurSel(cboUnmeteredDL);
+	int idef = static_cast<int>(Config::imgBandwidthUnmetered_default());
+	if (icur != idef) {
+		ComboBox_SetCurSel(cboUnmeteredDL, idef);
 		isDefChanged = true;
 	}
 	HWND cboMeteredDL = GetDlgItem(hWndPropSheet, IDC_OPTIONS_CBO_METERED_DL);
-	idx = ComboBox_GetCurSel(cboMeteredDL);
-	if (idx != static_cast<int>(imgBandwidthMetered_default)) {
-		ComboBox_SetCurSel(cboMeteredDL, static_cast<int>(imgBandwidthMetered_default));
+	icur = ComboBox_GetCurSel(cboMeteredDL);
+	idef = static_cast<int>(Config::imgBandwidthMetered_default());
+	if (icur != idef) {
+		ComboBox_SetCurSel(cboMeteredDL, idef);
 		isDefChanged = true;
 	}
 
@@ -260,17 +236,17 @@ void OptionsTabPrivate::loadDefaults(void)
 	// FIXME: Uncomment this once the "dangerous" permissions overlay
 	// is working on Windows.
 	/*
-	cur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_DANGEROUSPERMISSIONS));
-	if (cur != showDangerousPermissionsOverlayIcon_default) {
-		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_DANGEROUSPERMISSIONS,
-			boolToBstChecked(showDangerousPermissionsOverlayIcon_default));
+	bcur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_DANGEROUSPERMISSIONS));
+	bdef = Config::showDangerousPermissionsOverlayIcon_default();
+	if (bcur != bdef) {
+		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_DANGEROUSPERMISSIONS, boolToBstChecked(bdef));
 		isDefChanged = true;
 	}
 	*/
-	cur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_ENABLETHUMBNAILONNETWORKFS));
-	if (cur != enableThumbnailOnNetworkFS_default) {
-		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_ENABLETHUMBNAILONNETWORKFS,
-			boolToBstChecked(enableThumbnailOnNetworkFS_default));
+	bcur = bstCheckedToBool(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_ENABLETHUMBNAILONNETWORKFS));
+	bdef = Config::enableThumbnailOnNetworkFS_default();
+	if (bcur != bdef) {
+		CheckDlgButton(hWndPropSheet, IDC_OPTIONS_ENABLETHUMBNAILONNETWORKFS, boolToBstChecked(bdef));
 		isDefChanged = true;
 	}
 
@@ -318,12 +294,9 @@ void OptionsTabPrivate::save(void)
 	btstr = bstCheckedToBoolString(IsDlgButtonChecked(hWndPropSheet, IDC_OPTIONS_STOREFILEORIGININFO));
 	WritePrivateProfileString(_T("Downloads"), _T("StoreFileOriginInfo"), btstr, tfilename.c_str());
 
-	int idx = ComboBox_GetCurSel(GetDlgItem(hWndPropSheet, IDC_OPTIONS_PALLANGUAGEFORGAMETDB));
-	if (idx < 0 || idx >= ARRAY_SIZE_I(pal_lc)) {
-		idx = pal_lc_idx_def;
-	}
+	uint32_t lc = LanguageComboBox_GetSelectedLC(GetDlgItem(hWndPropSheet, IDC_OPTIONS_PALLANGUAGEFORGAMETDB));
 	WritePrivateProfileString(_T("Downloads"), _T("PalLanguageForGameTDB"),
-		SystemRegion::lcToTString(pal_lc[idx]).c_str(), tfilename.c_str());
+		SystemRegion::lcToTString(lc).c_str(), tfilename.c_str());
 
 	// Image bandwidth options
 	// TODO: Consolidate this.

src/gtk/gtk-compat.h
@@ -355,6 +355,7 @@ typedef GtkComboBox OurComboBox;
 #endif /* GTK_CHECK_VERSION(3,99,0) */
 
 #ifdef USE_GTK_DROP_DOWN
+typedef guint gtk_cbo_index_t;
 #  define COMPARE_CBO(widget, defval) \
 	(gtk_drop_down_get_selected(GTK_DROP_DOWN(widget)) != (defval))
 #  define SET_CBO(widget, value) \
@@ -362,6 +363,7 @@ typedef GtkComboBox OurComboBox;
 #  define GET_CBO(widget) \
 	gtk_drop_down_get_selected(GTK_DROP_DOWN(widget))
 #else /* !USE_GTK_DROP_DOWN */
+typedef gint gtk_cbo_index_t;
 #  define COMPARE_CBO(widget, defval) \
 	(gtk_combo_box_get_active(GTK_COMBO_BOX(widget)) != (defval))
 #  define SET_CBO(widget, value) \

src/librpbase/config/Config.cpp
@@ -90,13 +90,38 @@ public:
 	bool isNewBandwidthOptionSet;
 	bool downloadHighResScans;
 
-	// DMG title screen mode. [index is ROM type]
+	// DMG title screen mode [index is ROM type]
 	Config::DMG_TitleScreen_Mode dmgTSMode[Config::DMG_TitleScreen_Mode::DMG_TS_MAX];
 
-	// Other options.
+	// Other options
 	bool showDangerousPermissionsOverlayIcon;
 	bool enableThumbnailOnNetworkFS;
 	bool showXAttrView;
+
+public:
+	/** Default values **/
+
+	// Download options
+	static const constexpr uint32_t palLanguageForGameTDB_default = 'en';
+	static const constexpr bool extImgDownloadEnabled_default = true;
+	static const constexpr bool useIntIconForSmallSizes_default = true;
+	static const constexpr bool storeFileOriginInfo_default = true;
+
+	// Image bandwidth options
+	static const constexpr Config::ImgBandwidth imgBandwidthUnmetered_default = Config::ImgBandwidth::HighRes;
+	static const constexpr Config::ImgBandwidth imgBandwidthMetered_default = Config::ImgBandwidth::NormalRes;
+
+	// DMG title screen mode [index is ROM type]
+	static const constexpr Config::DMG_TitleScreen_Mode dmgTSMode_default[Config::DMG_TitleScreen_Mode::DMG_TS_MAX] = {
+		Config::DMG_TitleScreen_Mode::DMG_TS_DMG,
+		Config::DMG_TitleScreen_Mode::DMG_TS_SGB,
+		Config::DMG_TitleScreen_Mode::DMG_TS_CGB
+	};
+
+	// Other options
+	static const constexpr bool showDangerousPermissionsOverlayIcon_default = true;
+	static const constexpr bool enableThumbnailOnNetworkFS_default = false;
+	static const constexpr bool showXAttrView_default = true;
 };
 
 /** ConfigPrivate **/
@@ -127,25 +152,25 @@ const uint8_t ConfigPrivate::defImgTypePrio[] = {
 ConfigPrivate::ConfigPrivate()
 	: super("rom-properties.conf")
 	// Download options
-	, palLanguageForGameTDB('en')
-	, extImgDownloadEnabled(true)
-	, useIntIconForSmallSizes(true)
-	, storeFileOriginInfo(true)
+	, palLanguageForGameTDB(palLanguageForGameTDB_default)
+	, extImgDownloadEnabled(extImgDownloadEnabled_default)
+	, useIntIconForSmallSizes(useIntIconForSmallSizes_default)
+	, storeFileOriginInfo(storeFileOriginInfo_default)
 	// Image bandwidth options
-	, imgBandwidthUnmetered(Config::ImgBandwidth::HighRes)
-	, imgBandwidthMetered(Config::ImgBandwidth::NormalRes)
+	, imgBandwidthUnmetered(imgBandwidthUnmetered_default)
+	, imgBandwidthMetered(imgBandwidthMetered_default)
 	// Compatibility with older settings
 	, isNewBandwidthOptionSet(false)
 	, downloadHighResScans(true)
 	// Overlay icon
-	, showDangerousPermissionsOverlayIcon(true)
+	, showDangerousPermissionsOverlayIcon(showDangerousPermissionsOverlayIcon_default)
 	// Enable thumbnailing and metadata on network FS
-	, enableThumbnailOnNetworkFS(false)
+	, enableThumbnailOnNetworkFS(enableThumbnailOnNetworkFS_default)
 	// Show the Extended Attributes tab
-	, showXAttrView(true)
+	, showXAttrView(showXAttrView_default)
 {
 	// NOTE: Configuration is also initialized in the reset() function.
-	memset(dmgTSMode, 0, sizeof(dmgTSMode));
+	memcpy(dmgTSMode, dmgTSMode_default, sizeof(dmgTSMode));
 }
 
 /**
@@ -165,9 +190,9 @@ void ConfigPrivate::reset(void)
 #endif
 
 	// Download options
-	extImgDownloadEnabled = true;
-	useIntIconForSmallSizes = true;
-	storeFileOriginInfo = true;
+	extImgDownloadEnabled = extImgDownloadEnabled_default;
+	useIntIconForSmallSizes = useIntIconForSmallSizes_default;
+	storeFileOriginInfo = storeFileOriginInfo_default;
 
 	// Image bandwidth options
 	imgBandwidthUnmetered = Config::ImgBandwidth::HighRes;
@@ -176,17 +201,15 @@ void ConfigPrivate::reset(void)
 	isNewBandwidthOptionSet = false;
 	downloadHighResScans = false;
 
-	// DMG title screen mode.
-	dmgTSMode[Config::DMG_TitleScreen_Mode::DMG_TS_DMG] = Config::DMG_TitleScreen_Mode::DMG_TS_DMG;
-	dmgTSMode[Config::DMG_TitleScreen_Mode::DMG_TS_SGB] = Config::DMG_TitleScreen_Mode::DMG_TS_SGB;
-	dmgTSMode[Config::DMG_TitleScreen_Mode::DMG_TS_CGB] = Config::DMG_TitleScreen_Mode::DMG_TS_CGB;
+	// DMG title screen mode
+	memcpy(dmgTSMode, dmgTSMode_default, sizeof(dmgTSMode));
 
 	// Overlay icon
-	showDangerousPermissionsOverlayIcon = true;
+	showDangerousPermissionsOverlayIcon = showDangerousPermissionsOverlayIcon_default;
 	// Enable thumbnail and metadata on network FS
-	enableThumbnailOnNetworkFS = false;
+	enableThumbnailOnNetworkFS = enableThumbnailOnNetworkFS_default;
 	// Show the Extended Attributes tab
-	showXAttrView = true;
+	showXAttrView = showXAttrView_default;
 }
 
 /**
@@ -713,5 +736,39 @@ bool Config::showXAttrView(void) const
 	RP_D(const Config);
 	return d->showXAttrView;
 }
+
+/**** Default values ****/
+
+/** Download options **/
+
+#define DEFAULT_VALUE_IMPL(T, name) \
+T Config::name##_default(void) \
+{ \
+	return ConfigPrivate::name##_default; \
+}
+DEFAULT_VALUE_IMPL(bool, extImgDownloadEnabled)
+DEFAULT_VALUE_IMPL(bool, useIntIconForSmallSizes)
+DEFAULT_VALUE_IMPL(bool, storeFileOriginInfo)
+DEFAULT_VALUE_IMPL(uint32_t, palLanguageForGameTDB)
+DEFAULT_VALUE_IMPL(Config::ImgBandwidth, imgBandwidthUnmetered)
+DEFAULT_VALUE_IMPL(Config::ImgBandwidth, imgBandwidthMetered)
+
+Config::DMG_TitleScreen_Mode Config::dmgTitleScreenMode_default(DMG_TitleScreen_Mode romType)
+{
+	assert(romType >= DMG_TitleScreen_Mode::DMG_TS_DMG);
+	assert(romType <  DMG_TitleScreen_Mode::DMG_TS_MAX);
+	if (romType <  DMG_TitleScreen_Mode::DMG_TS_DMG ||
+	    romType >= DMG_TitleScreen_Mode::DMG_TS_MAX)
+	{
+		// Invalid ROM type. Return DMG.
+		return DMG_TitleScreen_Mode::DMG_TS_DMG;
+	}
+
+	return ConfigPrivate::dmgTSMode_default[romType];
+}
+
+DEFAULT_VALUE_IMPL(bool, showDangerousPermissionsOverlayIcon)
+DEFAULT_VALUE_IMPL(bool, enableThumbnailOnNetworkFS)
+DEFAULT_VALUE_IMPL(bool, showXAttrView)
 
 }

src/librpbase/config/Config.hpp
@@ -174,6 +174,84 @@ public:
 	 * @return True if we should enable; false if not.
 	 */
 	bool showXAttrView(void) const;
+
+public:
+	/**** Default values ****/
+
+	/** Download options **/
+
+	/**
+	 * Should we download images from external databases? (default value)
+	 * @return True if downloads are enabled; false if not.
+	 */
+	static bool extImgDownloadEnabled_default(void);
+
+	/**
+	 * Always use the internal icon (if present) for small sizes. (default value)
+	 * TODO: Clarify "small sizes".
+	 * @return True if we should use the internal icon for small sizes; false if not.
+	 */
+	static bool useIntIconForSmallSizes_default(void);
+
+	/**
+	 * Store file origin information? (default value)
+	 * @return True if we should; false if not.
+	 */
+	static bool storeFileOriginInfo_default(void);
+
+	/**
+	 * Language code for PAL titles on GameTDB. (default value)
+	 * @return Language code.
+	 */
+	static uint32_t palLanguageForGameTDB_default(void);
+
+	/* Image bandwidth options */
+
+	/**
+	 * What type of images should be downloaded on unmetered connections? (default value)
+	 * These connections do not charge for usage.
+	 * @return ImgBandwidth for unmetered connections
+	 */
+	static ImgBandwidth imgBandwidthUnmetered_default(void);
+
+	/**
+	 * What type of images should be downloaded on metered connections? (default value)
+	 * These connections may charge for usage.
+	 * @return ImgBandwidth for metered connections
+	 */
+	static ImgBandwidth imgBandwidthMetered_default(void);
+
+	/** DMG title screen mode **/
+
+	/**
+	 * Which title screen should we use for the specified DMG ROM type? (default value)
+	 * @param romType DMG ROM type.
+	 * @return Title screen to use.
+	 */
+	static DMG_TitleScreen_Mode dmgTitleScreenMode_default(DMG_TitleScreen_Mode romType);
+
+	/** Other options **/
+
+	/**
+	 * Show an overlay icon for "dangerous" permissions?
+	 * NOTE: Call load() before using this function.
+	 * @return True if we should show the overlay icon; false if not.
+	 */
+	static bool showDangerousPermissionsOverlayIcon_default(void);
+
+	/**
+	 * Enable thumbnailing and metadata on network filesystems?
+	 * NOTE: Call load() before using this function.
+	 * @return True if we should enable; false if not.
+	 */
+	static bool enableThumbnailOnNetworkFS_default(void);
+
+	/**
+	 * Show the Extended Attributes tab? (default value)
+	 * NOTE: Call load() before using this function.
+	 * @return True if we should enable; false if not.
+	 */
+	static bool showXAttrView_default(void);
 };
 
 }

commit c600197c074559eb2cb7aaeaf00f2d9f15c19dc3

[kde] OptionsTab::loadDefaults(): Use the correct default setting for External Image Downloads.
It was using the "enable thumbnailing on network FS" default, which is
false; it should be true.

This only affects the KDE UI frontend's "Defaults" button. The underlying
Config class sets the correct default setting.

This fixes a regression from commit 329faa83a2444f11d34eb22a41e6173756340b80.
([librpbase] Config: Add *_default() functions to get default values.)

Affects: v2.3

src/kde/config/OptionsTab.cpp
@@ -124,7 +124,7 @@ void OptionsTab::loadDefaults(void)
 	Q_D(OptionsTab);
 
 	// Downloads
-	bool bdef = Config::enableThumbnailOnNetworkFS_default();
+	bool bdef = Config::extImgDownloadEnabled_default();
 	if (d->ui.grpExtImgDownloads->isChecked() != bdef) {
 		d->ui.grpExtImgDownloads->setChecked(bdef);
 		isDefChanged = true;