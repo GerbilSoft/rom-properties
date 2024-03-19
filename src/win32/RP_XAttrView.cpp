GerbilSoft/rom-properties

commit eb54f1d727dcb7dddec5a59f5a18353309239593

[librpbase] Config: Use a single function to get boolean configuration options (and their defaults).
getBoolConfigOption() and getBoolConfigOption_default() take an enum
parameter to determine which option to retrieve.

This reduces the number of exported functions in LibRpBase::Config
by 10. (6 options, current + default == 12; minus 2 for the new
dispatch functions)

src/win32/RP_XAttrView.cpp
@@ -335,7 +335,7 @@ IFACEMETHODIMP RP_XAttrView::Initialize(
 
 	// Check if XAttrView is enabled.
 	const Config *const config = Config::instance();
-	if (!config->showXAttrView()) {
+	if (!config->getBoolConfigOption_default(Config::BoolConfig::Options_ShowXAttrView)) {
 		// XAttrView is disabled.
 		return E_FAIL;
 	}
@@ -394,7 +394,7 @@ IFACEMETHODIMP RP_XAttrView::Initialize(
 	// TODO: Check for "bad" file systems before checking ADS?
 #if 0
 	config = Config::instance();
-	if (FileSystem::isOnBadFS(tfilename.c_str(), config->enableThumbnailOnNetworkFS())) {
+	if (FileSystem::isOnBadFS(tfilename.c_str(), config->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS))) {
 		// This file is on a "bad" file system.
 		goto cleanup;
 	}

src/gtk/CreateThumbnail.cpp
@@ -195,7 +195,7 @@ static RomDataPtr openFromFilenameOrURI(const char *source_file, string &s_uri, 
 	RomDataPtr romData;
 
 	s_uri.clear();
-	const bool enableThumbnailOnNetworkFS = Config::instance()->enableThumbnailOnNetworkFS();
+	const bool enableThumbnailOnNetworkFS = Config::instance()->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS);
 
 	char *const uri_scheme = g_uri_parse_scheme(source_file);
 	if (uri_scheme != nullptr) {

src/gtk/config/OptionsTab.cpp
@@ -367,13 +367,13 @@ rp_options_tab_reset(RpOptionsTab *tab)
 	// Downloads
 	gtk_check_button_set_active(
 		GTK_CHECK_BUTTON(tab->chkExtImgDownloadEnabled),
-		config->extImgDownloadEnabled());
+		config->getBoolConfigOption(Config::BoolConfig::Downloads_ExtImgDownloadEnabled));
 	gtk_check_button_set_active(
 		GTK_CHECK_BUTTON(tab->chkUseIntIconForSmallSizes),
-		config->useIntIconForSmallSizes());
+		config->getBoolConfigOption(Config::BoolConfig::Downloads_UseIntIconForSmallSizes));
 	gtk_check_button_set_active(
 		GTK_CHECK_BUTTON(tab->chkStoreFileOriginInfo),
-		config->storeFileOriginInfo());
+		config->getBoolConfigOption(Config::BoolConfig::Downloads_StoreFileOriginInfo));
 
 	// Image bandwidth options
 	SET_CBO(tab->cboUnmeteredConnection, static_cast<int>(config->imgBandwidthUnmetered()));
@@ -384,13 +384,13 @@ rp_options_tab_reset(RpOptionsTab *tab)
 	// Options
 	gtk_check_button_set_active(
 		GTK_CHECK_BUTTON(tab->chkShowDangerousPermissionsOverlayIcon),
-		config->showDangerousPermissionsOverlayIcon());
+		config->getBoolConfigOption(Config::BoolConfig::Options_ShowDangerousPermissionsOverlayIcon));
 	gtk_check_button_set_active(
 		GTK_CHECK_BUTTON(tab->chkEnableThumbnailOnNetworkFS),
-		config->enableThumbnailOnNetworkFS());
+		config->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS));
 	gtk_check_button_set_active(
 		GTK_CHECK_BUTTON(tab->chkShowXAttrView),
-		config->showXAttrView());
+		config->getBoolConfigOption(Config::BoolConfig::Options_ShowXAttrView));
 
 	// PAL language code
 	rp_language_combo_box_set_selected_lc(RP_LANGUAGE_COMBO_BOX(tab->cboGameTDBPAL), config->palLanguageForGameTDB());
@@ -409,19 +409,19 @@ rp_options_tab_load_defaults(RpOptionsTab *tab)
 	bool isDefChanged = false;
 
 	// Downloads
-	bool bdef = Config::extImgDownloadEnabled_default();
+	bool bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Downloads_ExtImgDownloadEnabled);
 	if (COMPARE_CHK(tab->chkExtImgDownloadEnabled, bdef)) {
 		SET_CHK(tab->chkExtImgDownloadEnabled, bdef);
 		isDefChanged = true;
 		// Update sensitivity
 		rp_options_tab_chkExtImgDownloadEnabled_toggled(GTK_CHECK_BUTTON(tab->chkExtImgDownloadEnabled), tab);
 	}
-	bdef = Config::useIntIconForSmallSizes_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Downloads_UseIntIconForSmallSizes);
 	if (COMPARE_CHK(tab->chkUseIntIconForSmallSizes, bdef)) {
 		SET_CHK(tab->chkUseIntIconForSmallSizes, bdef);
 		isDefChanged = true;
 	}
-	bdef = Config::storeFileOriginInfo_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Downloads_StoreFileOriginInfo);
 	if (COMPARE_CHK(tab->chkStoreFileOriginInfo, bdef)) {
 		SET_CHK(tab->chkStoreFileOriginInfo, bdef);
 		isDefChanged = true;
@@ -446,17 +446,17 @@ rp_options_tab_load_defaults(RpOptionsTab *tab)
 	}
 
 	// Options
-	bdef = Config::showDangerousPermissionsOverlayIcon_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Options_ShowDangerousPermissionsOverlayIcon);
 	if (COMPARE_CHK(tab->chkShowDangerousPermissionsOverlayIcon, bdef)) {
 		gtk_check_button_set_active(GTK_CHECK_BUTTON(tab->chkShowDangerousPermissionsOverlayIcon), bdef);
 		isDefChanged = true;
 	}
-	bdef = Config::enableThumbnailOnNetworkFS_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS);
 	if (COMPARE_CHK(tab->chkEnableThumbnailOnNetworkFS, bdef)) {
 		gtk_check_button_set_active(GTK_CHECK_BUTTON(tab->chkEnableThumbnailOnNetworkFS), bdef);
 		isDefChanged = true;
 	}
-	bdef = Config::showXAttrView_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Options_ShowXAttrView);
 	if (COMPARE_CHK(tab->chkShowXAttrView, bdef)) {
 		gtk_check_button_set_active(GTK_CHECK_BUTTON(tab->chkShowXAttrView), bdef);
 		isDefChanged = true;

src/gtk/gtk3/NautilusPropertyPageProvider.cpp
@@ -181,7 +181,7 @@ rp_nautilus_property_page_provider_get_pages(NautilusPropertyPageProvider *provi
 
 	// Check if XAttrView is enabled.
 	const Config *const config = Config::instance();
-	if (config->showXAttrView()) {
+	if (config->getBoolConfigOption_default(Config::BoolConfig::Options_ShowXAttrView)) {
 		// XAttrView is enabled.
 		page = rp_nautilus_property_page_provider_get_XAttrView(uri);
 		if (page) {

src/gtk/gtk3/ThunarPropertyPageProvider.cpp
@@ -170,7 +170,7 @@ rp_thunar_property_page_provider_get_pages(ThunarxPropertyPageProvider *page_pro
 
 	// Check if XAttrView is enabled.
 	const Config *const config = Config::instance();
-	if (config->showXAttrView()) {
+	if (config->getBoolConfigOption_default(Config::BoolConfig::Options_ShowXAttrView)) {
 		// XAttrView is enabled.
 		page = rp_thunar_property_page_provider_get_XAttrView(uri);
 		if (page) {

src/kde/RpQUrl.cpp
@@ -143,7 +143,7 @@ IRpFilePtr openQUrl(const QUrl &url, bool isThumbnail)
 	if (isThumbnail) {
 		// We're thumbnailing the file. Check "bad FS" settings.
 		const Config *const config = Config::instance();
-		const bool enableThumbnailOnNetworkFS = config->enableThumbnailOnNetworkFS();
+		const bool enableThumbnailOnNetworkFS = config->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS);
 		if (!s_local_filename.empty()) {
 			// This is a local file. Check if it's on a "bad" file system.
 			if (FileSystem::isOnBadFS(s_local_filename.c_str(), enableThumbnailOnNetworkFS)) {

src/kde/config/OptionsTab.cpp
@@ -92,18 +92,18 @@ void OptionsTab::reset(void)
 	Q_D(OptionsTab);
 
 	// Downloads
-	d->ui.grpExtImgDownloads->setChecked(config->extImgDownloadEnabled());
-	d->ui.chkUseIntIconForSmallSizes->setChecked(config->useIntIconForSmallSizes());
-	d->ui.chkStoreFileOriginInfo->setChecked(config->storeFileOriginInfo());
+	d->ui.grpExtImgDownloads->setChecked(config->getBoolConfigOption(Config::BoolConfig::Downloads_ExtImgDownloadEnabled));
+	d->ui.chkUseIntIconForSmallSizes->setChecked(config->getBoolConfigOption(Config::BoolConfig::Downloads_UseIntIconForSmallSizes));
+	d->ui.chkStoreFileOriginInfo->setChecked(config->getBoolConfigOption(Config::BoolConfig::Downloads_StoreFileOriginInfo));
 
 	// Image bandwidth options
 	d->ui.cboUnmeteredConnection->setCurrentIndex(static_cast<int>(config->imgBandwidthUnmetered()));
 	d->ui.cboMeteredConnection->setCurrentIndex(static_cast<int>(config->imgBandwidthMetered()));
 
 	// Options
-	d->ui.chkShowDangerousPermissionsOverlayIcon->setChecked(config->showDangerousPermissionsOverlayIcon());
-	d->ui.chkEnableThumbnailOnNetworkFS->setChecked(config->enableThumbnailOnNetworkFS());
-	d->ui.chkShowXAttrView->setChecked(config->showXAttrView());
+	d->ui.chkShowDangerousPermissionsOverlayIcon->setChecked(config->getBoolConfigOption(Config::BoolConfig::Options_ShowDangerousPermissionsOverlayIcon));
+	d->ui.chkEnableThumbnailOnNetworkFS->setChecked(config->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS));
+	d->ui.chkShowXAttrView->setChecked(config->getBoolConfigOption(Config::BoolConfig::Options_ShowXAttrView));
 
 	// PAL language code
 	d->ui.cboGameTDBPAL->setSelectedLC(config->palLanguageForGameTDB());
@@ -124,17 +124,17 @@ void OptionsTab::loadDefaults(void)
 	Q_D(OptionsTab);
 
 	// Downloads
-	bool bdef = Config::extImgDownloadEnabled_default();
+	bool bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Downloads_ExtImgDownloadEnabled);
 	if (d->ui.grpExtImgDownloads->isChecked() != bdef) {
 		d->ui.grpExtImgDownloads->setChecked(bdef);
 		isDefChanged = true;
 	}
-	bdef = Config::useIntIconForSmallSizes_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Downloads_UseIntIconForSmallSizes);
 	if (d->ui.chkUseIntIconForSmallSizes->isChecked() != bdef) {
 		d->ui.chkUseIntIconForSmallSizes->setChecked(bdef);
 		isDefChanged = true;
 	}
-	bdef = Config::storeFileOriginInfo_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Downloads_StoreFileOriginInfo);
 	if (d->ui.chkStoreFileOriginInfo->isChecked() != bdef) {
 		d->ui.chkStoreFileOriginInfo->setChecked(bdef);
 		isDefChanged = true;
@@ -158,17 +158,17 @@ void OptionsTab::loadDefaults(void)
 	}
 
 	// Options
-	bdef = Config::showDangerousPermissionsOverlayIcon_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Options_ShowDangerousPermissionsOverlayIcon);
 	if (d->ui.chkShowDangerousPermissionsOverlayIcon->isChecked() != bdef) {
 		d->ui.chkShowDangerousPermissionsOverlayIcon->setChecked(bdef);
 		isDefChanged = true;
 	}
-	bdef = Config::enableThumbnailOnNetworkFS_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS);
 	if (d->ui.chkEnableThumbnailOnNetworkFS->isChecked() != bdef) {
 		d->ui.chkEnableThumbnailOnNetworkFS->setChecked(bdef);
 		isDefChanged = true;
 	}
-	bdef = Config::showXAttrView_default();
+	bdef = Config::getBoolConfigOption_default(Config::BoolConfig::Options_ShowXAttrView);
 	if (d->ui.chkShowXAttrView->isChecked() != bdef) {
 		d->ui.chkShowXAttrView->setChecked(bdef);
 		isDefChanged = true;

src/kde/plugins/OverlayIconPlugin.cpp
@@ -52,7 +52,7 @@ QStringList OverlayIconPlugin::getOverlays(const QUrl &item)
 	QStringList sl;
 
 	const Config *const config = Config::instance();
-	if (!config->showDangerousPermissionsOverlayIcon()) {
+	if (!config->getBoolConfigOption(Config::BoolConfig::Options_ShowDangerousPermissionsOverlayIcon)) {
 		// Overlay icon is disabled.
 		return sl;
 	}

commit 745ac80b706f624f4fab07b06b0ca0142c588ec1

[win32] RP_XAttrView: Call config->getBoolConfigOption(), not config->getBoolConfigOption_default().
We want to read the *current* setting, not the default setting.

Note that I forgot to add the checkbox to rp-config, but it will still
read the value from the configuration file if it's present.

This fixes a regression from commit eb54f1d727dcb7dddec5a59f5a18353309239593.
([librpbase] Config: Use a single function to get boolean configuration options (and their defaults).)

src/win32/RP_XAttrView.cpp
@@ -335,7 +335,7 @@ IFACEMETHODIMP RP_XAttrView::Initialize(
 
 	// Check if XAttrView is enabled.
 	const Config *const config = Config::instance();
-	if (!config->getBoolConfigOption_default(Config::BoolConfig::Options_ShowXAttrView)) {
+	if (!config->getBoolConfigOption(Config::BoolConfig::Options_ShowXAttrView)) {
 		// XAttrView is disabled.
 		return E_FAIL;
 	}