5d989314d682398cce9214ba70366d599061fe1a
David Korth <gerbilsoft@gerbilsoft.com>
2024-03-20 00:05:03 -0400

    Initial implementation of a RomDataView test program.
    This is implemented as part of rp-stub.
    
    [gtk] stub-export.c: Initial RomDataView test program.
    
    It works on GTK2 and GTK3, but with GTK4 (which hasn't been tested
    due to a lack of GTK4 file browsers that support arbitrary widgets),
    there's an assertion failure:
    
    (com.gerbilsoft.rom-properties.rp-config:1094138): GLib-GObject-CRITICAL **: 00:04:18.493: g_object_set_is_valid_property: object class 'RpRomDataView' has no property named 'border-width'
    rp-stub: src/gtk/RomDataView.cpp:1570: void rp_rom_data_view_create_options_button(RpRomDataView*): Assertion `GTK_IS_BOX(parent)' failed.
    
    Also, on GTK2 and GTK3, the Options button isn't being created.

--- src/gtk/config/stub-export.c
+++ src/gtk/config/stub-export.c
@@ -2,13 +2,15 @@
  * ROM Properties Page shell extension. (GTK)                              *
  * stub-export.c: Exported function for the rp-config stub.                *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
 #include "stdafx.h"
-#include "ConfigDialog.hpp"
 #include "check-uid.h"
+
+#include "ConfigDialog.hpp"
+#include "RomDataView.hpp"
 
 #if !GTK_CHECK_VERSION(2,90,2)
 // GtkApplication was introduced in GTK3.
@@ -38,6 +40,8 @@ config_dialog_delete_event(RpConfigDialog *dialog, GdkEvent *event, gpointer use
 }
 #endif /* !GTK_CHECK_VERSION(2,90,2) */
 
+/** rp-config **/
+
 /**
  * GtkApplication activate() signal handler.
  * Also used manually for GTK2.
@@ -45,7 +49,7 @@ config_dialog_delete_event(RpConfigDialog *dialog, GdkEvent *event, gpointer use
  * @param user_data
  */
 static void
-app_activate(GtkApplication *app, gpointer user_data)
+rp_config_app_activate(GtkApplication *app, gpointer user_data)
 {
 	RP_UNUSED(user_data);
 
@@ -102,7 +106,7 @@ int RP_C_API rp_show_config_dialog(int argc, char *argv[])
 		"com.gerbilsoft.rom-properties.rp-config", G_APPLICATION_FLAGS_NONE);
 	// NOTE: GApplication is supposed to set this, but KDE isn't seeing it...
 	g_set_prgname("com.gerbilsoft.rom-properties.rp-config");
-	g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+	g_signal_connect(app, "activate", G_CALLBACK(rp_config_app_activate), NULL);
 
 	// NOTE: We aren't setting up command line options in GApplication,
 	// so it will complain if argv has any parameters.
@@ -113,7 +117,142 @@ int RP_C_API rp_show_config_dialog(int argc, char *argv[])
 	// Not going to implement the Startup Notification protocol manually
 	// because GTK2 desktops likely wouldn't support it, anyway.
 	gtk_init(NULL, NULL);
-	app_activate(NULL, 0);
+	rp_config_app_activate(NULL, 0);
+	gtk_main();
+	status = 0;
+#endif /* GTK_CHECK_VERSION(2,90,2) */
+
+	return status;
+}
+
+/** RomDataView test program **/
+
+/**
+ * GtkApplication activate() signal handler.
+ * Also used manually for GTK2.
+ * @param app GtkApplication (or nullptr on GTK2)
+ * @param user_data URI to open
+ */
+static void
+rp_RomDataView_app_activate(GtkApplication *app, const gchar *uri)
+{
+	// Initialize base i18n.
+	rp_i18n_init();
+
+	// Create a GtkDialog.
+	// TODO: Use GtkWindow on GTK4?
+	char s_title[40];
+	snprintf(s_title, sizeof(s_title), "RomDataView GTK%u test program", (unsigned int)GTK_MAJOR_VERSION);
+	GtkWidget *const dialog = gtk_dialog_new_with_buttons(
+		"RomDataView GTK test program",
+		NULL,
+		0,
+		"OK", GTK_RESPONSE_ACCEPT,
+		NULL);
+	gtk_widget_set_name(dialog, "RomDataView-test-dialog");
+	gtk_widget_set_visible(dialog, TRUE);
+
+	GtkWidget *const contentArea  = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+
+	// Create a GtkNotebook to simulate Nautilus, Thunar, etc.
+	GtkWidget *const notebook = gtk_notebook_new();
+	gtk_widget_set_name(notebook, "notebook");
+	gtk_widget_set_visible(notebook, TRUE);
+#if GTK_CHECK_VERSION(4,0,0)
+	gtk_box_append(GTK_BOX(contentArea), notebook);
+#else /* !GTK_CHECK_VERSION(4,0,0) */
+	gtk_box_pack_start(GTK_BOX(contentArea), notebook, TRUE, TRUE, 4);
+#endif /* GTK_CHECK_VERSION(4,0,0) */
+
+	// NOTE: Thunar has an extra widget between the GtkNotebook and RomDataView.
+	// TODO: Have RomDataView check for this instead of expecting it when using RP_DFT_XFCE?
+	GtkWidget *const vboxNotebookPage = rp_gtk_vbox_new(4);
+	gtk_widget_set_name(vboxNotebookPage, "vboxNotebookPage");
+	gtk_widget_set_visible(vboxNotebookPage, TRUE);
+
+	// Create a RomDataView object with the specified URI.
+	// TODO: Which RpDescFormatType?
+	printf("URI = %s\n", uri);
+	GtkWidget *const romDataView = rp_rom_data_view_new_with_uri(uri, RP_DFT_XFCE);
+	gtk_widget_set_visible(romDataView, TRUE);
+	gtk_widget_set_name(romDataView, "romDataView");
+#if GTK_CHECK_VERSION(4,0,0)
+	gtk_box_append(GTK_BOX(vboxNotebookPage), romDataView);
+#else /* !GTK_CHECK_VERSION(4,0,0) */
+	gtk_box_pack_start(GTK_BOX(vboxNotebookPage), romDataView, TRUE, TRUE, 4);
+#endif /* GTK_CHECK_VERSION(4,0,0) */
+
+	// Label for the tab.
+	GtkWidget *const tabLabel = gtk_label_new("ROM Properties");
+	gtk_widget_set_name(tabLabel, "tabLabel");
+
+	// Add the RomDataView to the GtkNotebook.
+	gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vboxNotebookPage, tabLabel);
+
+#if GTK_CHECK_VERSION(2,90,2)
+	gtk_application_add_window(app, GTK_WINDOW(dialog));
+#else /* GTK_CHECK_VERSION(2,90,2) */
+	// GTK2: No GtkApplication to manage the main loop, so we
+	// need to to ensure it exits when the window is closed.
+	RP_UNUSED(app);
+	g_signal_connect(dialog, "delete-event", G_CALLBACK(config_dialog_delete_event), NULL);
+#endif /* !GTK_CHECK_VERSION(2,90,2) */
+}
+
+/**
+ * Exported function for the RomDataView test program stub.
+ * @param argc
+ * @param argv
+ * @return 0 on success; non-zero on error.
+ */
+G_MODULE_EXPORT
+int RP_C_API rp_show_RomDataView_dialog(int argc, char *argv[])
+{
+	// NOTE: Not passing command line parameters to GTK+, since
+	// g_application_run() returns immediately if any parameters
+	// that it doesn't recognize are found. rp-config's parameters
+	// are for rp-stub only, and aren't used by ConfigDialog.
+
+	// TODO: argv[] needs to be updated such that [0] == argv[0] and [1] == URI.
+	// For now, assuming the last element is the URI.
+	if (argc < 2) {
+		// Not enough parameters...
+		fprintf(stderr, "*** GTK%u rp_show_RomDataView_dialog(): ERROR: No URI specified.\n", (unsigned int)GTK_MAJOR_VERSION);
+		return EXIT_FAILURE;
+	}
+
+#if !GLIB_CHECK_VERSION(2,35,1)
+        // g_type_init() is automatic as of glib-2.35.1
+        // and is marked deprecated.
+        g_type_init();
+#endif
+#if !GLIB_CHECK_VERSION(2,32,0)
+        // g_thread_init() is automatic as of glib-2.32.0
+        // and is marked deprecated.
+        if (!g_thread_supported()) {
+                g_thread_init(NULL);
+        }
+#endif
+
+	CHECK_UID_RET(EXIT_FAILURE);
+	int status;
+#if GTK_CHECK_VERSION(2,90,2)
+	GtkApplication *const app = gtk_application_new(
+		"com.gerbilsoft.rom-properties.rp-config", G_APPLICATION_FLAGS_NONE);
+	// NOTE: GApplication is supposed to set this, but KDE isn't seeing it...
+	g_set_prgname("com.gerbilsoft.rom-properties.rp-config");
+	g_signal_connect(app, "activate", G_CALLBACK(rp_RomDataView_app_activate), argv[argc-1]);
+
+	// NOTE: We aren't setting up command line options in GApplication,
+	// so it will complain if argv has any parameters.
+	char *argv_new[1] = { argv[0] };
+	status = g_application_run(G_APPLICATION(app), 1, argv_new);
+#else /* !GTK_CHECK_VERSION(2,90,2) */
+	// NOTE: GTK2 doesn't send a startup notification.
+	// Not going to implement the Startup Notification protocol manually
+	// because GTK2 desktops likely wouldn't support it, anyway.
+	gtk_init(NULL, NULL);
+	rp_RomDataView_app_activate(NULL, argv[argc-1]);
 	gtk_main();
 	status = 0;
 #endif /* GTK_CHECK_VERSION(2,90,2) */
--- src/gtk/is-supported.cpp
+++ src/gtk/is-supported.cpp
@@ -19,8 +19,39 @@ using namespace LibRpFile;
 using namespace LibRomData;
 
 /**
+ * Attempt to open a RomData object from the specified filename.
+ * @param filename Filename (must be a filename, NOT a URI!)
+ * @return RomData object if supported; nullptr if not.
+ */
+static RomDataPtr rp_gtk_open_filename(const char *filename)
+{
+	// TODO: Check file extensions and/or MIME types?
+	RomDataPtr romData;
+
+	// Local file:
+	// - If this is a diretory: Call RomDataFactory::create() with the pathname.
+	// - If this is a file: Create an RpFile first.
+	if (likely(!FileSystem::is_directory(filename))) {
+		// File: Open the file and call RomDataFactory::create() with the opened file.
+		IRpFilePtr file = std::make_shared<RpFile>(filename, RpFile::FM_OPEN_READ_GZ);
+		if (!file->isOpen()) {
+			// Unable to open the file...
+			// TODO: Return an error code?
+			return {};
+		}
+		romData = RomDataFactory::create(file);
+	} else {
+		// Directory: Call RomDataFactory::create() with the filename.
+		// (NOTE: Local filenames only!)
+		romData = RomDataFactory::create(filename);
+	}
+
+	return romData;
+}
+
+
+/**
  * Attempt to open a RomData object from the specified GVfs URI.
- * If it is, the RomData object will be opened.
  * @param uri URI from e.g. nautilus_file_info_get_uri() [UTF-8]
  * @return RomData object if supported; nullptr if not.
  */
@@ -34,32 +65,19 @@ RomDataPtr rp_gtk_open_uri(const char *uri)
 	// Check if the URI maps to a local file.
 	gchar *const filename = g_filename_from_uri(uri, nullptr, nullptr);
 	if (filename) {
-		// Local file:
-		// - If this is a diretory: Call RomDataFactory::create() with the pathname.
-		// - If this is a file: Create an RpFile first.
-		if (likely(!FileSystem::is_directory(filename))) {
-			// File: Open the file and call RomDataFactory::create() with the opened file.
-			IRpFilePtr file = std::make_shared<RpFile>(filename, RpFile::FM_OPEN_READ_GZ);
-			g_free(filename);
-			if (!file->isOpen()) {
-				// Unable to open the file...
-				// TODO: Return an error code?
-				return {};
-			}
-			romData = RomDataFactory::create(file);
-		} else {
-			// Directory: Call RomDataFactory::create() with the filename.
-			// (NOTE: Local filenames only!)
-			romData = RomDataFactory::create(filename);
-			g_free(filename);
-		}
+		romData = rp_gtk_open_filename(filename);
+		g_free(filename);
 	} else {
-		// Not a local file. Use RpFileGio.
-		IRpFilePtr file = std::make_shared<RpFileGio>(uri);
-		if (!file->isOpen()) {
-			// Unable to open the file...
-			// TODO: Return an error code?
-			return {};
+		// This might be a plain filename and not a URI.
+		if (access(uri, R_OK) == 0) {
+			// It's a plain filename.
+			romData = rp_gtk_open_filename(uri);
+		} else {
+			// Not a local file. Use RpFileGio.
+			IRpFilePtr file = std::make_shared<RpFileGio>(uri);
+			if (file->isOpen()) {
+				romData = RomDataFactory::create(file);
+			}
 		}
 	}
 
