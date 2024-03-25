GerbilSoft/rom-properties

commit a1fa5efedc94635d531f6a5f402230acbb9d5f62

Mark inline functions that don't depend on external data as constexpr.
DMGPrivate::RomSize(): Cannot mark the constexpr arrays as static.
This was added in C++23, but we're not requiring C++23 right now.
Since the function is constexpr, removing static shouldn't be a problem.

FIXME: Cannot mark the following functions as constexpr on MSVC 2022:
- uilog2() [and nextPow2()]
- VirtualBoy: isPublisherID(), isGameID()
- ImageDecoder_BC7: get_mode()

Among other things, MSVC's intrinsics are not constexpr-safe.

Code size difference: (64-bit Gentoo Linux, gcc-13.2.1_p20240210, release build, no LTO)

   text    data     bss     dec     hex filename
2163723   76120     664 2240507  222ffb libromdata.so.5 [before]
2163723   76120     664 2240507  222ffb libromdata.so.5 [after]
      0       0       0       0       0 Difference

Code size difference: (64-bit Gentoo Linux, gcc-13.2.1_p20240210, release build, with LTO)

   text    data     bss     dec     hex filename
1884056   75104     496 1959656  1de6e8 libromdata.so.5 [before]
1884307   75080     504 1959891  1de7d3 libromdata.so.5 [after]
   +251     -24      +8    +235     +eb Difference

src/librptext/conversion.cpp
@@ -201,7 +201,7 @@ const char16_t *u16_memchr(const char16_t *wcs, char16_t c, size_t n)
 /** Other useful text functions **/
 
 template<typename T>
-static inline int calc_frac_part(T val, T mask)
+static inline constexpr int calc_frac_part(T val, T mask)
 {
 	const float f = static_cast<float>(val & (mask - 1)) / static_cast<float>(mask);
 	int frac_part = static_cast<int>(f * 1000.0f);

src/kde/config/KeyStoreModel.cpp
@@ -73,13 +73,13 @@ public:
 // - LOWORD: Section.
 // - HIWORD: Key index. (0xFFFF for section header)
 #ifndef _WIN32
-static inline uint16_t LOWORD(uint32_t dwValue) {
+static inline constexpr uint16_t LOWORD(uint32_t dwValue) {
 	return (dwValue & 0xFFFF);
 }
-static inline uint16_t HIWORD(uint32_t dwValue) {
+static inline constexpr uint16_t HIWORD(uint32_t dwValue) {
 	return (dwValue >> 16);
 }
-static inline uint32_t MAKELONG(uint16_t wLow, uint16_t wHigh) {
+static inline constexpr uint32_t MAKELONG(uint16_t wLow, uint16_t wHigh) {
 	return (wLow | (wHigh << 16));
 }
 #endif /* _WIN32 */

src/libromdata/Console/SNES.cpp
@@ -101,7 +101,7 @@ public:
 	 * Is a character a valid game ID character?
 	 * @return True if it is; false if it isn't.
 	 */
-	static inline bool isValidGameIDChar(char x)
+	static inline constexpr bool isValidGameIDChar(char x)
 	{
 		return (x >= '0' && x <= '9') || (x >= 'A' && x <= 'Z');
 	}

src/libromdata/Console/WiiSave.cpp
@@ -71,7 +71,7 @@ public:
 	 * @return Next highest multiple of 64.
 	 */
 	template<typename T>
-	static inline T toNext64(T val)
+	static inline constexpr T toNext64(T val)
 	{
 		return (val + (T)63) & ~((T)63);
 	}

src/libromdata/Console/WiiWAD_p.hpp
@@ -100,7 +100,7 @@ public:
 	 * @return Next highest multiple of 64.
 	 */
 	template<typename T>
-	static inline T toNext64(T val)
+	static inline constexpr T toNext64(T val)
 	{
 		return (val + (T)63) & ~((T)63);
 	}

src/libromdata/Handheld/DMG.cpp
@@ -127,14 +127,14 @@ public:
 	 * @param type Cartridge type byte
 	 * @return dmg_cart_type struct
 	 */
-	static inline dmg_cart_type CartType(uint8_t type);
+	static inline constexpr dmg_cart_type CartType(uint8_t type);
 
 	/**
 	 * Convert the ROM size value to an actual size.
 	 * @param type ROM size value.
 	 * @return ROM size, in kilobytes. (-1 on error)
 	 */
-	static inline int RomSize(uint8_t type);
+	static inline constexpr int RomSize(uint8_t type);
 
 public:
 	// DMG RAM size array
@@ -331,7 +331,7 @@ uint32_t DMGPrivate::systemID(const DMG_RomHeader *pRomHeader)
  * @param type Cartridge type byte.
  * @return dmg_cart_type struct.
  */
-inline DMGPrivate::dmg_cart_type DMGPrivate::CartType(uint8_t type)
+inline constexpr DMGPrivate::dmg_cart_type DMGPrivate::CartType(uint8_t type)
 {
 	// Check for low cartridge types.
 	if (type < dmg_cart_types_start.size()) {
@@ -353,10 +353,10 @@ inline DMGPrivate::dmg_cart_type DMGPrivate::CartType(uint8_t type)
  * @param type ROM size value.
  * @return ROM size, in kilobytes. (-1 on error)
  */
-inline int DMGPrivate::RomSize(uint8_t type)
+inline constexpr int DMGPrivate::RomSize(uint8_t type)
 {
-	static constexpr array<uint16_t, 8> rom_size = {{32, 64, 128, 256, 512, 1024, 2048, 4096}};
-	static constexpr array<uint16_t, 4> rom_size_52 = {{1152, 1280, 1536}};
+	constexpr array<uint16_t, 8> rom_size = {{32, 64, 128, 256, 512, 1024, 2048, 4096}};
+	constexpr array<uint16_t, 4> rom_size_52 = {{1152, 1280, 1536}};
 	if (type < rom_size.size()) {
 		return rom_size[type];
 	} else if (type >= 0x52 && type < 0x52+rom_size_52.size()) {

src/libromdata/Handheld/Nintendo3DS_p.hpp
@@ -3,7 +3,7 @@
  * Nintendo3DS.hpp: Nintendo 3DS ROM reader. (Private class)               *
  * Handles CCI/3DS, CIA, and SMDH files.                                   *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -153,7 +153,7 @@ public:
 	 * @return Next highest multiple of 64.
 	 */
 	template<typename T>
-	static inline T toNext64(T val)
+	static inline constexpr T toNext64(T val)
 	{
 		return (val + static_cast<T>(63)) & ~(static_cast<T>(63));
 	}

src/libromdata/Handheld/VirtualBoy.cpp
@@ -41,23 +41,42 @@ public:
 	/**
 	 * Is character a valid JIS X 0201 codepoint?
 	 * @param c The character
-	 * @return Wether or not character is valid
+	 * @return Whether or not character is valid
 	 */
-	static bool inline isJISX0201(unsigned char c);
-		
+	static inline constexpr bool isJISX0201(unsigned char c)
+	{
+		return (c >= ' ' && c <= '~') || (c > 0xA0 && c < 0xE0);
+	}
+
 	/**
 	 * Is character a valid Publisher ID character?
 	 * @param c The character
 	 * @return Wether or not character is valid
 	 */
-	static bool inline isPublisherID(char c);
+	static inline bool isPublisherID(char c)
+	{
+		// Valid characters:
+		// - Uppercase letters
+		// - Digits
+		// FIXME: This is not constexpr on MSVC 2022.
+		return (ISUPPER(c) || ISDIGIT(c));
+	}
 
 	/**
 	 * Is character a valid Game ID character?
 	 * @param c The character
 	 * @return Wether or not character is valid
 	 */
-	static bool inline isGameID(char c);
+	static inline bool isGameID(char c)
+	{
+		// Valid characters:
+		// - Uppercase letters
+		// - Digits
+		// - Space (' ')
+		// - Hyphen ('-')
+		// FIXME: This is not constexpr on MSVC 2022.
+		return (ISUPPER(c) || ISDIGIT(c) || c == ' ' || c == '-');
+	}
 	
 public:
 	// ROM footer
@@ -92,41 +111,6 @@ VirtualBoyPrivate::VirtualBoyPrivate(const IRpFilePtr &file)
 {
 	// Clear the ROM footer struct.
 	memset(&romFooter, 0, sizeof(romFooter));
-}
-
-/**
- * Is character a valid JIS X 0201 codepoint?
- * @param c The character
- * @return Wether or not character is valid
- */
-bool inline VirtualBoyPrivate::isJISX0201(unsigned char c){
-	return (c >= ' ' && c <= '~') || (c > 0xA0 && c < 0xE0);
-}
-
-/**
- * Is character a valid Game ID character?
- * @param c The character
- * @return Wether or not character is valid
- */
-bool inline VirtualBoyPrivate::isPublisherID(char c){
-	// Valid characters:
-	// - Uppercase letters
-	// - Digits
-	return (ISUPPER(c) || ISDIGIT(c));
-}
-
-/**
- * Is character a valid Game ID character?
- * @param c The character
- * @return Wether or not character is valid
- */
-bool inline VirtualBoyPrivate::isGameID(char c){
-	// Valid characters:
-	// - Uppercase letters
-	// - Digits
-	// - Space (' ')
-	// - Hyphen ('-')
-	return (ISUPPER(c) || ISDIGIT(c) || c == ' ' || c == '-');
 }
 
 /** VirtualBoy **/

commit b273effdfa6bf8e8534eb1cd4f54181ec64f5c2e

[librptext] conversion.cpp, calc_frac_part(): Remove constexpr.
This is causing failures on MSVC 2015 on AppVeyor:

src\librptext\conversion.cpp(207): error C3250: 'frac_part': declaration is not allowed in 'constexpr' function body
src\librptext\conversion.cpp(217): error C3250: 'round_adj': declaration is not allowed in 'constexpr' function body
src\librptext\conversion.cpp(218): error C3249: illegal statement or sub-expression for 'constexpr' function
  src\librptext\conversion.cpp(218): note: see usage of '/='
src\librptext\conversion.cpp(219): error C3249: illegal statement or sub-expression for 'constexpr' function
  src\librptext\conversion.cpp(219): note: see usage of '+='
src\librptext\conversion.cpp(220): error C3256: 'frac_part': variable use does not produce a constant expression
src\librptext\conversion.cpp(204): error C3259: 'constexpr' functions can only have one return statement

This fixes a regression from commit a1fa5efedc94635d531f6a5f402230acbb9d5f62.
(Mark inline functions that don't depend on external data as constexpr.)

src/librptext/conversion.cpp
@@ -201,7 +201,7 @@ const char16_t *u16_memchr(const char16_t *wcs, char16_t c, size_t n)
 /** Other useful text functions **/
 
 template<typename T>
-static inline constexpr int calc_frac_part(T val, T mask)
+static inline int calc_frac_part(T val, T mask)
 {
 	const float f = static_cast<float>(val & (mask - 1)) / static_cast<float>(mask);
 	int frac_part = static_cast<int>(f * 1000.0f);