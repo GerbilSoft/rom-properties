eba36a781b3fce546e92ee086d0ea6b4a1b8ff45
David Korth <gerbilsoft@gerbilsoft.com>
2024-03-26 21:42:03 -0400

    [libi18n] i18n.h: Fix the no-NLS pgettext_expr() and npgettext_expr() macros.
    They shouldn't take a "domain" argument.

--- src/libi18n/i18n.h
+++ src/libi18n/i18n.h
@@ -34,9 +34,9 @@
 #  define C_(msgctxt, msgid)			(msgid)
 #  define N_(msgid1, msgid2, n)			((n) == 1 ? (msgid1) : (msgid2))
 #  define NC_(msgctxt, msgid1, msgid2, n)	((n) == 1 ? (msgid1) : (msgid2))
-#  define pgettext_expr(domain, msgctxt, msgid)			(msgid)
+#  define pgettext_expr(msgctxt, msgid)				(msgid)
 #  define dpgettext_expr(domain, msgctxt, msgid)		(msgid)
-#  define npgettext_expr(domain, msgctxt, msgid1, msgid2, n)	((n) == 1 ? (msgid1) : (msgid2))
+#  define npgettext_expr(msgctxt, msgid1, msgid2, n)		((n) == 1 ? (msgid1) : (msgid2))
 #  define dnpgettext_expr(domain, msgctxt, msgid1, msgid2, n)	((n) == 1 ? (msgid1) : (msgid2))
 #endif
 
