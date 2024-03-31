527ce52962ca5df401bdb8b9cbd29849f0800183
David Korth <gerbilsoft@gerbilsoft.com>
2024-03-30 21:01:08 -0400

    [librpbyteswap] bitstuff.c: Remove #pragma once.
    This should only be in .h files, not .c files.
    
    src/librpbyteswap/bitstuff.c:9:9: warning: #pragma once in main file
        9 | #pragma once
          |         ^~~~

--- src/librpbyteswap/bitstuff.c
+++ src/librpbyteswap/bitstuff.c
@@ -6,8 +6,6 @@
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
-#pragma once
-
 #include "bitstuff.h"
 
 /**
