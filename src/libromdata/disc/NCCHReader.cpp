GerbilSoft/rom-properties

commit e9a82d09bdfef7bfa6bac145a0baab38ee4128c5

[libromdata] NCCHReader: Call q->close() directly instead of closeFileOrDiscReader().
closeFileOrDiscReader() isn't needed anymore now that IDiscReader is
derived from IRpFile, so it has been removed. This function was calling
q->m_file->close(), which is what IDiscReader::close() does.

Code size difference: (64-bit Gentoo Linux, gcc-13.2.1_p20240210, release build, no LTO)

   text    data     bss     dec     hex filename
  13823     632       0   14455    3877 NCCHReader.cpp.o [before]
  13359     632       0   13991    36a7 NCCHReader.cpp.o [after]
   -464       0       0    -464    -1d0 Difference

src/libromdata/disc/NCCHReader.cpp
@@ -58,7 +58,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 		// NOTE: readFromROM() sets q->m_lastError.
 		// TODO: Better verifyResult?
 		verifyResult = KeyManager::VerifyResult::WrongKey;
-		closeFileOrDiscReader();
+		q->close();
 		return;
 	}
 
@@ -71,7 +71,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 			// 0004800F-484E4841
 			verifyResult = KeyManager::VerifyResult::OK;
 			nonNcchContentType = NonNCCHContentType::NDHT;
-			closeFileOrDiscReader();
+			q->close();
 			return;
 		}
 
@@ -89,7 +89,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 			}
 		}
 
-		closeFileOrDiscReader();
+		q->close();
 		return;
 	}
 	headers_loaded |= HEADER_NCCH;
@@ -143,7 +143,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 		if (size != sizeof(exefs_header)) {
 			// Read error.
 			// NOTE: readFromROM() sets q->m_lastError.
-			closeFileOrDiscReader();
+			q->close();
 			return;
 		}
 

src/libromdata/disc/NCCHReader_p.hpp
@@ -3,7 +3,7 @@
  * NCCHReader_p.hpp: Nintendo 3DS NCCH reader.                             *
  * Private class declaration.                                              *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -170,17 +170,6 @@ protected:
 	 */
 	int findEncSection(uint32_t address) const;
 #endif /* ENABLE_DECRYPTION */
-
-	/**
-	 * Close the file and/or IDiscReader.
-	 */
-	inline void closeFileOrDiscReader(void)
-	{
-		RP_Q(NCCHReader);
-		if (q->m_file) {
-			q->m_file.reset();
-		}
-	}
 };
 
 }

commit 38b687ccd4aa8545d3c7130939fe5e1d461244fa

[libromdata] NCCHReader: Call q->m_file.reset(), not q->close().
q->close() calls m_file.close(), which breaks when reading a 3DS CIA
that has an embedded Nintendo DS SRLfile.

This fixes a regression from commit e9a82d09bdfef7bfa6bac145a0baab38ee4128c5.
([libromdata] NCCHReader: Call q->close() directly instead of closeFileOrDiscReader().)

src/libromdata/disc/NCCHReader.cpp
@@ -58,7 +58,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 		// NOTE: readFromROM() sets q->m_lastError.
 		// TODO: Better verifyResult?
 		verifyResult = KeyManager::VerifyResult::WrongKey;
-		q->close();
+		q->m_file.reset();
 		return;
 	}
 
@@ -71,7 +71,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 			// 0004800F-484E4841
 			verifyResult = KeyManager::VerifyResult::OK;
 			nonNcchContentType = NonNCCHContentType::NDHT;
-			q->close();
+			q->m_file.reset();
 			return;
 		}
 
@@ -89,7 +89,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 			}
 		}
 
-		q->close();
+		q->m_file.reset();
 		return;
 	}
 	headers_loaded |= HEADER_NCCH;
@@ -143,7 +143,7 @@ NCCHReaderPrivate::NCCHReaderPrivate(NCCHReader *q,
 		if (size != sizeof(exefs_header)) {
 			// Read error.
 			// NOTE: readFromROM() sets q->m_lastError.
-			q->close();
+			q->m_file.reset();
 			return;
 		}
 