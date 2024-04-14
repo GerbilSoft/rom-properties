130a0e023a0384dcbb041e89049413d3159df288
David Korth <gerbilsoft@gerbilsoft.com>
2024-04-13 20:34:43 -0400

    [win32] LvData.hpp: Update copyright year.

--- src/win32/LvData.hpp
+++ src/win32/LvData.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * LvData.hpp: ListView data internal implementation.                      *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
