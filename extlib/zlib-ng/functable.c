GerbilSoft/rom-properties

commit e9e16f425e8e3c943049ca408f3f0794acfce694

[zlib-ng] Update: v2.2.1 -> v2.2.2

extlib/zlib-ng/functable.c
@@ -5,14 +5,16 @@
 #ifndef DISABLE_RUNTIME_CPU_DETECTION
 
 #include "zbuild.h"
-#include "functable.h"
-#include "cpu_features.h"
-#include "arch_functions.h"
 
 // rom-properties: MSVC ARM64EC build fails if intrin.h is included here for some reason...
 #if defined(_MSC_VER) && !defined(_M_ARM64EC)
+#if defined(_MSC_VER)
 #  include <intrin.h>
 #endif
+
+#include "functable.h"
+#include "cpu_features.h"
+#include "arch_functions.h"
 
 /* Platform has pointer size atomic store */
 #if defined(__GNUC__) || defined(__clang__)

extlib/zlib-ng/CMakeLists.txt
@@ -204,9 +204,9 @@ elseif(MSVC)
     # (who'd use cmake from an IDE...) but checking for ICC before checking for MSVC should
     # avoid mistakes.
     # /Oi ?
-    set(WARNFLAGS /W3)
+    set(WARNFLAGS /W3 /w34242 /WX)
     set(WARNFLAGS_MAINTAINER /W4)
-    set(WARNFLAGS_DISABLE)
+    set(WARNFLAGS_DISABLE /wd4206 /wd4054)
     if(BASEARCH_ARM_FOUND)
         add_definitions(-D_ARM_WINAPI_PARTITION_DESKTOP_SDK_AVAILABLE)
         if(NOT "${ARCH}" MATCHES "aarch64")

extlib/zlib-ng/Makefile.in
@@ -30,7 +30,7 @@ LDSHARED=$(CC)
 LDSHAREDFLAGS=-shared
 LDVERSIONSCRIPT=
 
-VER=2.2.1
+VER=2.2.2
 VER1=2
 
 STATICLIB=$(LIBNAME1).a

extlib/zlib-ng/arch/arm/chunkset_neon.c
@@ -84,7 +84,9 @@ static inline chunk_t GET_CHUNK_MAG(uint8_t *buf, uint32_t *chunk_rem, uint32_t 
     a = vld1_u8(buf);
     b = vld1_u8(buf + 8);
     ret0 = vtbl1_u8(a, perm_vec0);
-    uint8x8x2_t ab = {{a, b}};
+    uint8x8x2_t ab;
+    ab.val[0] = a;
+    ab.val[1] = b;
     ret1 = vtbl2_u8(ab, perm_vec1);
     return vcombine_u8(ret0, ret1);
 #endif

extlib/zlib-ng/arch/arm/neon_intrins.h
@@ -36,20 +36,20 @@
 #  ifndef ARM_NEON_HASLD4
 
 static inline uint16x8x4_t vld1q_u16_x4(uint16_t const *a) {
-    uint16x8x4_t ret = (uint16x8x4_t) {{
-                          vld1q_u16(a),
-                          vld1q_u16(a+8),
-                          vld1q_u16(a+16),
-                          vld1q_u16(a+24)}};
+    uint16x8x4_t ret;
+    ret.val[0] = vld1q_u16(a);
+    ret.val[1] = vld1q_u16(a+8);
+    ret.val[2] = vld1q_u16(a+16);
+    ret.val[3] = vld1q_u16(a+24);
     return ret;
 }
 
 static inline uint8x16x4_t vld1q_u8_x4(uint8_t const *a) {
-    uint8x16x4_t ret = (uint8x16x4_t) {{
-                          vld1q_u8(a),
-                          vld1q_u8(a+16),
-                          vld1q_u8(a+32),
-                          vld1q_u8(a+48)}};
+    uint8x16x4_t ret;
+    ret.val[0] = vld1q_u8(a);
+    ret.val[1] = vld1q_u8(a+16);
+    ret.val[2] = vld1q_u8(a+32);
+    ret.val[3] = vld1q_u8(a+48);
     return ret;
 }
 

extlib/zlib-ng/arch/arm/slide_hash_armv6.c
@@ -39,7 +39,8 @@ static inline void slide_hash_chain(Pos *table, uint32_t entries, uint16_t wsize
 }
 
 Z_INTERNAL void slide_hash_armv6(deflate_state *s) {
-    unsigned int wsize = s->w_size;
+    Assert(s->w_size <= UINT16_MAX, "w_size should fit in uint16_t");
+    uint16_t wsize = (uint16_t)s->w_size;
 
     slide_hash_chain(s->head, HASH_SIZE, wsize);
     slide_hash_chain(s->prev, wsize, wsize);

extlib/zlib-ng/arch/arm/slide_hash_neon.c
@@ -38,7 +38,8 @@ static inline void slide_hash_chain(Pos *table, uint32_t entries, uint16_t wsize
 }
 
 Z_INTERNAL void slide_hash_neon(deflate_state *s) {
-    unsigned int wsize = s->w_size;
+    Assert(s->w_size <= UINT16_MAX, "w_size should fit in uint16_t");
+    uint16_t wsize = (uint16_t)s->w_size;
 
     slide_hash_chain(s->head, HASH_SIZE, wsize);
     slide_hash_chain(s->prev, wsize, wsize);

extlib/zlib-ng/arch/generic/crc32_braid_c.c
@@ -192,7 +192,8 @@ Z_INTERNAL uint32_t PREFIX(crc32_braid)(uint32_t crc, const uint8_t *buf, size_t
 #endif
 #endif
         words += N;
-        c = ZSWAPWORD(comb);
+        Assert(comb <= UINT32_MAX, "comb should fit in uint32_t");
+        c = (uint32_t)ZSWAPWORD(comb);
 
         /* Update the pointer to the remaining bytes to process. */
         buf = (const unsigned char *)words;

commit 4050a03ad45e0d3df28dd0e3c9d8254dd7f0c391

[zlib-ng] Fix a broken extra #ifdef introduced when updating zlib-ng to v2.2.2.
This fixes a regression from commit e9e16f425e8e3c943049ca408f3f0794acfce694.
([zlib-ng] Update: v2.2.1 -> v2.2.2)

extlib/zlib-ng/functable.c
@@ -8,7 +8,6 @@
 
 // rom-properties: MSVC ARM64EC build fails if intrin.h is included here for some reason...
 #if defined(_MSC_VER) && !defined(_M_ARM64EC)
-#if defined(_MSC_VER)
 #  include <intrin.h>
 #endif
 
4050a03ad45e0d3df28dd0e3c9d8254dd7f0c391
David Korth <gerbilsoft@gerbilsoft.com>
2024-09-27 01:08:32 -0400

    [zlib-ng] Fix a broken extra #ifdef introduced when updating zlib-ng to v2.2.2.
    This fixes a regression from commit e9e16f425e8e3c943049ca408f3f0794acfce694.
    ([zlib-ng] Update: v2.2.1 -> v2.2.2)

--- extlib/zlib-ng/functable.c
+++ extlib/zlib-ng/functable.c
@@ -8,7 +8,6 @@
 
 // rom-properties: MSVC ARM64EC build fails if intrin.h is included here for some reason...
 #if defined(_MSC_VER) && !defined(_M_ARM64EC)
-#if defined(_MSC_VER)
 #  include <intrin.h>
 #endif
 
