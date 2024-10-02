GerbilSoft/rom-properties

commit d24ea352fad0ee01f22ebf685501df85f1940ce8

[rp-download] IDownloader::getOSRelease(): Check if f_in is nullptr before fclose()'ing it.
Found using clang-tidy 19.1.0:

src/rp-download/IDownloader.cpp:310:3: warning: Null pointer passed to 1st parameter expecting 'nonnull' [clang-analyzer-core.NonNullParamChecker]
  310 |                 fclose(f_in);
      |                 ^

src/rp-download/IDownloader.cpp
@@ -307,7 +307,9 @@ tstring IDownloader::getOSRelease(void)
 	}
 	if (!ctx.field_name) {
 		// No field name...
-		fclose(f_in);
+		if (!f_in) {
+			fclose(f_in);
+		}
 		return {};
 	}
 

commit 7e55d3577cbde820aa6fcc9e841938396261b960

[rp-download] IDownloader::getOSRelease(): Fix f_in check.
Check that f_in is *not* nullptr before calling fclose().

Otherwise, fclose() is only called if f_in is nullptr, which means a
potential leak if f_in is valid, and a NULL dereference if it isn't.

This fixes a regression from commit d24ea352fad0ee01f22ebf685501df85f1940ce8.
([rp-download] IDownloader::getOSRelease(): Check if f_in is nullptr before fclose()'ing it.)

src/rp-download/IDownloader.cpp
@@ -307,7 +307,7 @@ tstring IDownloader::getOSRelease(void)
 	}
 	if (!ctx.field_name) {
 		// No field name...
-		if (!f_in) {
+		if (f_in) {
 			fclose(f_in);
 		}
 		return {};