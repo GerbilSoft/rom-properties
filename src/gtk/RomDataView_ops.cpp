GerbilSoft/rom-properties

commit cbfa9bd266466dd568741f1410435a4074d92b4b

Use C++ casts instead of C casts where possible.
Fixes this clang-tidy warning:

warning: C-style casts are discouraged; use static_cast [google-readability-casting]

Cast some enums to size_t instead of int when being used for array indexes.

[libcachecommon] CacheKeys.cpp, filterCacheKey():
- Use `unsigned char` instead of `uint8_t` for chr.
  (Effectively the same thing, but is more consistent with librpbase's
   TextOut_text.cpp.)

[librpbase] KeyManager: Make VerifyResult an int8_t and store VerifyResult
in mapInvalidKeyNames instead of uint8_t.

[librpbase] RpJpegPrivate: Change INPUT_BUF_SIZE from `unsigned int` to
`size_t`.

[libromdata] DpfReader::read(): Use off64_t more consistently instead of
int64_t.

[libromdata] Nintendo3DSPrivate::loadNCCH(): Check if idx >= 0.
- TODO: Make idx unsigned?

[libromdata] PowerVR3Private::loadImage(): Get file->size() once,
not twice.

src/gtk/RomDataView_ops.cpp
@@ -351,8 +351,8 @@ rp_rom_data_view_getSaveFileDialog_callback(GFile *file, save_data_t *save_data)
 		// TODO: Don't keep rebuilding this vector...
 		// NOTE: Assuming the RomOps vector order hasn't changed.
 		vector<RomData::RomOp> ops = romData->romOps();
-		assert(id < (int)ops.size());
-		if (id < (int)ops.size()) {
+		assert(id < static_cast<int>(ops.size()));
+		if (id < static_cast<int>(ops.size())) {
 			rp_options_menu_button_update_op(RP_OPTIONS_MENU_BUTTON(page->btnOptions), id, &ops[id]);
 		}
 
@@ -519,8 +519,8 @@ btnOptions_triggered_signal_handler(RpOptionsMenuButton *menuButton,
 	// Run a ROM operation.
 	// TODO: Don't keep rebuilding this vector...
 	vector<RomData::RomOp> ops = romData->romOps();
-	assert(id < (int)ops.size());
-	if (id >= (int)ops.size()) {
+	assert(id < static_cast<int>(ops.size()));
+	if (id < static_cast<int>(ops.size())) {
 		// ID is out of range.
 		return;
 	}

src/kde/RomDataView_ops.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (KDE4/KF5)                         *
  * RomDataView.cpp: RomData viewer. (ROM operations)                       *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -305,8 +305,8 @@ void RomDataView::btnOptions_triggered(int id)
 	// Run a ROM operation.
 	// TODO: Don't keep rebuilding this vector...
 	vector<RomData::RomOp> ops = d->romData->romOps();
-	assert(id < (int)ops.size());
-	if (id >= (int)ops.size()) {
+	assert(id < static_cast<int>(ops.size()));
+	if (id < static_cast<int>(ops.size())) {
 		// ID is out of range.
 		return;
 	}
@@ -349,8 +349,8 @@ void RomDataView::btnOptions_triggered(int id)
 		// Update the RomOp menu entry in case it changed.
 		// NOTE: Assuming the RomOps vector order hasn't changed.
 		ops = d->romData->romOps();
-		assert(id < (int)ops.size());
-		if (id < (int)ops.size()) {
+		assert(id < static_cast<int>(ops.size()));
+		if (id < static_cast<int>(ops.size())) {
 			d->btnOptions->updateOp(id, &ops[id]);
 		}
 

src/win32/RP_ShellPropSheetExt_ops.cpp
@@ -472,8 +472,8 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 	// TODO: Don't keep rebuilding this vector...
 	vector<RomData::RomOp> ops = romData->romOps();
 	const int id = menuId - IDM_OPTIONS_MENU_BASE;
-	assert(id < (int)ops.size());
-	if (id >= (int)ops.size()) {
+	assert(id < static_cast<int>(ops.size()));
+	if (id < static_cast<int>(ops.size())) {
 		// ID is out of range.
 		return;
 	}
@@ -520,8 +520,8 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 		// Update the RomOp menu entry in case it changed.
 		// NOTE: Assuming the RomOps vector order hasn't changed.
 		ops = romData->romOps();
-		assert(id < (int)ops.size());
-		if (id < (int)ops.size()) {
+		assert(id < static_cast<int>(ops.size()));
+		if (id < static_cast<int>(ops.size())) {
 			OptionsMenuButton_UpdateOp(hBtnOptions, id, &ops[id]);
 		}
 	} else {

commit d24ebb88ca6139ad29293b8f8aed83fd921c5f54

[gtk,kde,win32] RomDataView_ops, RP_ShellPropSheetExt_ops: Fix accidentally-broken RomData-specific RomOps.
This fixes a regression from commit cbfa9bd266466dd568741f1410435a4074d92b4b.
(Use C++ casts instead of C casts where possible.)

TODO: Check the commit for any more broken checks?

src/gtk/RomDataView_ops.cpp
@@ -520,7 +520,7 @@ btnOptions_triggered_signal_handler(RpOptionsMenuButton *menuButton,
 	// TODO: Don't keep rebuilding this vector...
 	vector<RomData::RomOp> ops = romData->romOps();
 	assert(id < static_cast<int>(ops.size()));
-	if (id < static_cast<int>(ops.size())) {
+	if (id >= static_cast<int>(ops.size())) {
 		// ID is out of range.
 		return;
 	}

src/kde/RomDataView_ops.cpp
@@ -306,7 +306,7 @@ void RomDataView::btnOptions_triggered(int id)
 	// TODO: Don't keep rebuilding this vector...
 	vector<RomData::RomOp> ops = d->romData->romOps();
 	assert(id < static_cast<int>(ops.size()));
-	if (id < static_cast<int>(ops.size())) {
+	if (id >= static_cast<int>(ops.size())) {
 		// ID is out of range.
 		return;
 	}

src/win32/RP_ShellPropSheetExt_ops.cpp
@@ -473,7 +473,7 @@ void RP_ShellPropSheetExt_Private::btnOptions_action_triggered(int menuId)
 	vector<RomData::RomOp> ops = romData->romOps();
 	const int id = menuId - IDM_OPTIONS_MENU_BASE;
 	assert(id < static_cast<int>(ops.size()));
-	if (id < static_cast<int>(ops.size())) {
+	if (id >= static_cast<int>(ops.size())) {
 		// ID is out of range.
 		return;
 	}