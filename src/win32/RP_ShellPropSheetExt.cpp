GerbilSoft/rom-properties

commit 2d51365fc5bc9504d9186a7231b16f6a556afc0d

[librpbase] RomFields, RomMetaData: Add begin()/end() aliases that return const_iterator.
These are merely wrappers around cbegin()/cend().

Range-based `for` loops only support begin()/end(), so adding these
aliases allows us to mak euse of range-based `for` loops.

Update other classes that make use of RomFields and RomMetaData
to use range-based `for` loops.

TODO: GTK RomDataView.cpp can't be updated to use it yet because of
the way it checks for "last field on the tab". Need to rework it.

src/win32/RP_ShellPropSheetExt.cpp
@@ -1707,9 +1707,7 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 
 	// tr: Field description label.
 	const char *const desc_label_fmt = C_("RomDataView", "%s:");
-	const auto pFields_cend = pFields->cend();
-	for (auto iter = pFields->cbegin(); iter != pFields_cend; ++iter) {
-		const RomFields::Field &field = *iter;
+	for (const RomFields::Field &field : *pFields) {
 		assert(field.isValid());
 		if (!field.isValid())
 			continue;
@@ -1924,22 +1922,26 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 	}
 
 	int fieldIdx = 0;	// needed for control IDs
-	auto iter_desc = t_desc_text.cbegin();
-	for (auto iter = pFields->cbegin(); iter != pFields_cend; ++iter, ++iter_desc, fieldIdx++) {
-		assert(iter_desc != t_desc_text.cend());
-		const RomFields::Field &field = *iter;
+	for (const RomFields::Field &field : *pFields) {
 		assert(field.isValid());
-		if (!field.isValid())
+		if (!field.isValid()) {
+			++iter_desc;
+			fieldIdx++;
 			continue;
+		}
 
 		// Verify the tab index.
 		const int tabIdx = field.tabIdx;
 		assert(tabIdx >= 0 && tabIdx < (int)tabs.size());
 		if (tabIdx < 0 || tabIdx >= (int)tabs.size()) {
 			// Tab index is out of bounds.
+			++iter_desc;
+			fieldIdx++;
 			continue;
 		} else if (!tabs[tabIdx].hDlg) {
 			// Tab name is empty. Tab is hidden.
+			++iter_desc;
+			fieldIdx++;
 			continue;
 		}
 
@@ -2071,6 +2073,9 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 			// Remove the description label.
 			DestroyWindow(hStatic);
 		}
+
+		++iter_desc;
+		fieldIdx++;
 	}
 
 	// Update scrollbar settings.

src/gtk/gtk4/NautilusPropertiesModel.cpp
@@ -236,12 +236,11 @@ rp_nautilus_properties_model_load_from_romData(RpNautilusPropertiesModel *self,
 
 	const uint32_t def_lc = pFields->defaultLanguageCode();
 
-	const auto pFields_cend = pFields->cend();
-	for (auto iter = pFields->cbegin(); iter != pFields_cend; ++iter) {
-		const RomFields::Field &field = *iter;
+	for (const RomFields::Field &field : *pFields) {
 		assert(field.isValid());
-		if (!field.isValid())
+		if (!field.isValid()) {
 			continue;
+		}
 
 		switch (field.type) {
 			case RomFields::RFT_INVALID:
@@ -260,7 +259,7 @@ rp_nautilus_properties_model_load_from_romData(RpNautilusPropertiesModel *self,
 				rp_nautilus_properties_model_init_bitfield(self, field);
 				break;
 			case RomFields::RFT_LISTDATA:
-				// Can't easily do RFT_LISTDATA here.
+				// TODO: Allow single-column RFT_LISTDATA?
 				break;
 			case RomFields::RFT_DATETIME:
 				rp_nautilus_properties_model_init_datetime(self, field);

src/gtk/tracker/rp-tracker.cpp
@@ -40,10 +40,7 @@ using std::array;
 static void
 add_metadata_properties_v1(const RomMetaData *metaData, TrackerSparqlBuilder *builder)
 {
-	const auto iter_end = metaData->cend();
-	for (auto iter = metaData->cbegin(); iter != iter_end; ++iter) {
-		const RomMetaData::MetaData &prop = *iter;
-
+	for (const RomMetaData::MetaData &prop : *metaData) {
 		switch (prop.name) {
 			default:
 				// TODO
@@ -146,10 +143,7 @@ static void
 add_metadata_properties_v2(const RomMetaData *metaData, TrackerResource *resource)
 {
 	// TODO: Make use of tracker_resource_set_relation(), like in tracker-extract-mp3.c?
-	const auto iter_end = metaData->cend();
-	for (auto iter = metaData->cbegin(); iter != iter_end; ++iter) {
-		const RomMetaData::MetaData &prop = *iter;
-
+	for (const RomMetaData::MetaData &prop : *metaData) {
 		switch (prop.name) {
 			default:
 				// TODO

src/kde/RomDataView.cpp
@@ -988,21 +988,23 @@ void RomDataViewPrivate::initDisplayWidgets(void)
 	// Create the data widgets.
 	int prevTabIdx = 0;
 	int fieldIdx = 0;
-	const auto pFields_cend = pFields->cend();
-	for (auto iter = pFields->cbegin(); iter != pFields_cend; ++iter, fieldIdx++) {
-		const RomFields::Field &field = *iter;
+	for (const RomFields::Field &field : *pFields) {
 		assert(field.isValid());
-		if (!field.isValid())
+		if (!field.isValid()) {
+			fieldIdx++;
 			continue;
+		}
 
 		// Verify the tab index.
 		const int tabIdx = field.tabIdx;
 		assert(tabIdx >= 0 && tabIdx < static_cast<int>(tabs.size()));
 		if (tabIdx < 0 || tabIdx >= static_cast<int>(tabs.size())) {
 			// Tab index is out of bounds.
+			fieldIdx++;
 			continue;
 		} else if (!tabs[tabIdx].form) {
 			// Tab name is empty. Tab is hidden.
+			fieldIdx++;
 			continue;
 		}
 
@@ -1064,6 +1066,8 @@ void RomDataViewPrivate::initDisplayWidgets(void)
 			// Set RFT_fieldIdx for ROM operations.
 			obj->setProperty("RFT_fieldIdx", fieldIdx);
 		}
+
+		fieldIdx++;
 	}
 
 	// Initial update of RFT_STRING_MULTI and RFT_LISTDATA_MULTI fields.

src/kde/plugins/ExtractorPlugin.cpp
@@ -78,10 +78,7 @@ void ExtractorPlugin::extract_properties(KFileMetaData::ExtractionResult *result
 	}
 
 	// Process the metadata.
-	const auto iter_end = metaData->cend();
-	for (auto iter = metaData->cbegin(); iter != iter_end; ++iter) {
-		const RomMetaData::MetaData &prop = *iter;
-
+	for (const RomMetaData::MetaData &prop : *metaData) {
 		// RomMetaData's property indexes match KFileMetaData.
 		// No conversion is necessary.
 		switch (prop.type) {
@@ -283,4 +280,4 @@ void ExtractorPlugin::extract(ExtractionResult *result)
 	// Finished extracting metadata.
 }
 
-} //namespace RomPropertiesKDE
+} // namespace RomPropertiesKDE

src/libromdata/Other/RpTextureWrapper.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (libromdata)                       *
  * RpTextureWrapper.hpp: librptexture file format wrapper.                 *
  *                                                                         *
- * Copyright (c) 2019-2023 by David Korth.                                 *
+ * Copyright (c) 2019-2024 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -426,11 +426,10 @@ const char *RpTextureWrapper::dx10Format(void) const
 	// NOTE: The string is localized, but our Google Test initializer
 	// sets LC_ALL=C, which disables localization.
 	// NOTE 2: This should not be used outside of tests for now!
-	const auto fields_cend = d->fields.cend();
-	for (auto iter = d->fields.cbegin(); iter != fields_cend; ++iter) {
-		if (iter->type == RomFields::RFT_STRING && !strcmp(iter->name, "DX10 Format")) {
+	for (const RomFields::Field &field : d->fields) {
+		if (field.type == RomFields::RFT_STRING && !strcmp(field.name, "DX10 Format")) {
 			// Found the DX10 format.
-			return iter->data.str;
+			return field.data.str;
 		}
 	}
 
@@ -438,4 +437,4 @@ const char *RpTextureWrapper::dx10Format(void) const
 	return nullptr;
 }
 
-}
+} // namespace LibRomData

src/libromdata/img/TCreateThumbnail.cpp
@@ -368,16 +368,16 @@ skip_image_check:
 	if (imgpf & RomData::IMGPF_RESCALE_RFT_DIMENSIONS_2) {
 		// Find the second RFT_DIMENSIONS field.
 		const RomFields *const fields = romData->fields();
+		assert(fields != nullptr);
 		array<const RomFields::Field*, 2> field = {{nullptr, nullptr}};
-		const auto iter_end = fields->cend();
-		for (auto iter = fields->cbegin(); iter != iter_end; ++iter) {
-			if (iter->type != RomFields::RFT_DIMENSIONS)
+		for (const RomFields::Field &pf : *fields) {
+			if (pf.type != RomFields::RFT_DIMENSIONS)
 				continue;
 			// Found an RFT_DIMENSIONS.
 			if (!field[0]) {
-				field[0] = &(*iter);
+				field[0] = &pf;
 			} else {
-				field[1] = &(*iter);
+				field[1] = &pf;
 				break;
 			}
 		}
@@ -585,4 +585,4 @@ int TCreateThumbnail<ImgClass>::getThumbnail(const char *filename, int reqSize, 
 	return getThumbnail(romData, reqSize, pOutParams);
 }
 
-}
+} // namespace LibRomData

src/librpbase/RomFields.hpp
@@ -455,6 +455,26 @@ class RomFields
 		RP_LIBROMDATA_PUBLIC
 		const_iterator cend(void) const;
 
+		/**
+		 * Get a const iterator pointing to the beginning of the RomMetaData.
+		 * Alias function required for range-based `for` loops.
+		 * @return Const iterator
+		 */
+		const_iterator begin(void) const
+		{
+			return cbegin();
+		}
+
+		/**
+		 * Get a const iterator pointing to the end of the RomMetaData.
+		 * Alias function required for range-based `for` loops.
+		 * @return Const iterator
+		 */
+		const_iterator end(void) const
+		{
+			return cend();
+		}
+
 	public:
 		/**
 		 * Get the abbreviation of an age rating organization.
@@ -856,4 +876,4 @@ class RomFields
 			uint32_t def_lc = 'en', unsigned int flags = 0);
 };
 
-}
+} // namespace LibRpBase

commit 93692e3369d269c0c08dffe16088945b6eb07c6c

[win32] RP_ShellPropSheetExt.cpp: Really revert the ranged `for` change.
Need to add pFields_cend to where it's reverted, since the first loop
was *not* reverted.

This fixes a regression from commit 2d51365fc5bc9504d9186a7231b16f6a556afc0d.
([librpbase] RomFields, RomMetaData: Add begin()/end() aliases that return const_iterator.)

src/win32/RP_ShellPropSheetExt.cpp
@@ -1923,6 +1923,7 @@ void RP_ShellPropSheetExt_Private::initDialog(void)
 
 	int fieldIdx = 0;	// needed for control IDs
 	auto iter_desc = t_desc_text.cbegin();
+	const auto pFields_cend = pFields->cend();
 	for (auto iter = pFields->cbegin(); iter != pFields_cend; ++iter, ++iter_desc, fieldIdx++) {
 		assert(iter_desc != t_desc_text.cend());
 		const RomFields::Field &field = *iter;