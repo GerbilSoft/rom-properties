4f2ef9465a50e344420d9ec5ad0e0d77eedbf279
David Korth <gerbilsoft@gerbilsoft.com>
2024-11-09 15:25:48 -0500

    [kde] DosAttrView: Convert the macro-like functions into inline functions.
    This actually *reduces* the code size a bit:
    
    Code size difference: (64-bit Gentoo Linux, gcc-14.2.0_p4, release build, no LTO)
    
       text    data     bss     dec     hex filename
       6863       8       0    6871    1ad7 [kf5] DosAttrView.cpp.o [before]
       5237       8       0    5245    147d [kf5] DosAttrView.cpp.o [after]
      -1626       0       0   -1626    -65a Difference
    
       text    data     bss     dec     hex filename
       6791      24       0    6815    1a9f [kf6] DosAttrView.cpp.o [before]
       5150      24       0    5174    1436 [kf6] DosAttrView.cpp.o [after]
      -1641       0       0   -1641    -669 Difference
    
    Fixes two clang-tidy warnings:
    
    DosAttrView.cpp:47:9: warning: function-like macro 'UPDATE_CHECKBOX' used; consider a 'constexpr' template function [cppcoreguidelines-macro-usage]
       47 | #define UPDATE_CHECKBOX(attr, obj) \
          |         ^
    DosAttrView.cpp:71:9: warning: function-like macro 'CONNECT_CHECKBOX_SIGNAL' used; consider a 'constexpr' template function [cppcoreguidelines-macro-usage]
       71 | #define CONNECT_CHECKBOX_SIGNAL(obj) \
          |         ^

--- src/kde/xattr/DosAttrView.cpp
+++ src/kde/xattr/DosAttrView.cpp
@@ -33,29 +33,61 @@ public:
 
 public:
 	/**
+	 * Update a single checkbox.
+	 * @param attr Attribute to check
+	 * @param checkBox Checkbox widget
+	 */
+	inline void updateCheckbox(unsigned int attr, QCheckBox *checkBox);
+
+	/**
 	 * Update the attributes display.
 	 */
 	void updateAttrsDisplay(void);
+
+public:
+	/**
+	 * Connect a checkbox signal.
+	 * @param checkBox Checkbox
+	 * @param receiver Receiver
+	 */
+	static inline void connectCheckboxSignal(QCheckBox *checkBox, DosAttrView *receiver);
 };
+
+/**
+ * Update a single checkbox.
+ * @param attr Attribute to check
+ * @param checkBox Checkbox widget
+ */
+inline void DosAttrViewPrivate::updateCheckbox(unsigned int attr, QCheckBox *checkBox)
+{
+	const bool val = !!(attrs & attr);
+	checkBox->setChecked(val);
+	checkBox->setProperty("DosAttrView.value", val);
+}
 
 /**
  * Update the attributes display.
  */
 void DosAttrViewPrivate::updateAttrsDisplay(void)
 {
-	bool val;
-#define UPDATE_CHECKBOX(attr, obj) \
-	val = !!(attrs & (attr)); \
-	ui.obj->setChecked(val); \
-	ui.obj->setProperty("DosAttrView.value", val)
+	updateCheckbox(FILE_ATTRIBUTE_READONLY, ui.chkReadOnly);
+	updateCheckbox(FILE_ATTRIBUTE_HIDDEN, ui.chkHidden);
+	updateCheckbox(FILE_ATTRIBUTE_ARCHIVE, ui.chkArchive);
+	updateCheckbox(FILE_ATTRIBUTE_SYSTEM, ui.chkSystem);
 
-	UPDATE_CHECKBOX(FILE_ATTRIBUTE_READONLY, chkReadOnly);
-	UPDATE_CHECKBOX(FILE_ATTRIBUTE_HIDDEN, chkHidden);
-	UPDATE_CHECKBOX(FILE_ATTRIBUTE_ARCHIVE, chkArchive);
-	UPDATE_CHECKBOX(FILE_ATTRIBUTE_SYSTEM, chkSystem);
+	updateCheckbox(FILE_ATTRIBUTE_COMPRESSED, ui.chkCompressed);
+	updateCheckbox(FILE_ATTRIBUTE_ENCRYPTED, ui.chkEncrypted);
+}
 
-	UPDATE_CHECKBOX(FILE_ATTRIBUTE_COMPRESSED, chkCompressed);
-	UPDATE_CHECKBOX(FILE_ATTRIBUTE_ENCRYPTED, chkEncrypted);
+/**
+ * Connect a checkbox signal.
+ * @param checkBox Checkbox
+ * @param receiver Receiver
+ */
+inline void DosAttrViewPrivate::connectCheckboxSignal(QCheckBox *checkBox, DosAttrView *receiver)
+{
+	QObject::connect(checkBox, SIGNAL(clicked(bool)),
+	                 receiver, SLOT(checkBox_clicked_slot(bool)));
 }
 
 /** DosAttrView **/
@@ -68,17 +100,13 @@ DosAttrView::DosAttrView(QWidget *parent)
 	d->ui.setupUi(this);
 
 	// Connect checkbox signals.
-#define CONNECT_CHECKBOX_SIGNAL(obj) \
-	connect(d->ui.obj, SIGNAL(clicked(bool)), \
-		this, SLOT(checkBox_clicked_slot(bool)))
+	d->connectCheckboxSignal(d->ui.chkReadOnly, this);
+	d->connectCheckboxSignal(d->ui.chkHidden, this);
+	d->connectCheckboxSignal(d->ui.chkArchive, this);
+	d->connectCheckboxSignal(d->ui.chkSystem, this);
 
-	CONNECT_CHECKBOX_SIGNAL(chkReadOnly);
-	CONNECT_CHECKBOX_SIGNAL(chkHidden);
-	CONNECT_CHECKBOX_SIGNAL(chkArchive);
-	CONNECT_CHECKBOX_SIGNAL(chkSystem);
-
-	CONNECT_CHECKBOX_SIGNAL(chkCompressed);
-	CONNECT_CHECKBOX_SIGNAL(chkEncrypted);
+	d->connectCheckboxSignal(d->ui.chkCompressed, this);
+	d->connectCheckboxSignal(d->ui.chkEncrypted, this);
 }
 
 /**
