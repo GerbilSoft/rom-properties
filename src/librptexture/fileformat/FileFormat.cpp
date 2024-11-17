GerbilSoft/rom-properties

commit 4cc1225157914b6b1ffdc1476b5769a25af77847

Various std::array<> improvements.
- Use std::array<>::fill() instead of memset() in many places.
  fill() can be vectorized by most modern compilers.

- Convert some more C arrays to std::array<>.

- Instead of memsetting starting at 2 for Nintendo IVs, fill()
  the whole thing, then copy the 2 bytes. This is more efficient
  because it means the fill() can be vectorized.

- ImageSizeCalc.hpp: Add a templated std::array<> overload.
  - ValveVTF: Use this overload.
  - GodotSTEX: FIXME: Can't easily use this overload due to the use
    of two different arrays for v3 and v4.

src/librptexture/fileformat/FileFormat.cpp
@@ -39,8 +39,8 @@ FileFormatPrivate::FileFormatPrivate(FileFormat *q, const IRpFilePtr &file, cons
 	assert(pTextureInfo != nullptr);
 
 	// Clear the arrays.
-	memset(dimensions, 0, sizeof(dimensions));
-	memset(rescale_dimensions, 0, sizeof(rescale_dimensions));
+	dimensions.fill(0);
+	rescale_dimensions.fill(0);
 
 	// Initialize i18n.
 	rp_i18n_init();
@@ -143,7 +143,7 @@ int FileFormat::getDimensions(int pBuf[3]) const
 		return -EBADF;
 	}
 
-	memcpy(pBuf, d->dimensions, sizeof(d->dimensions));
+	memcpy(pBuf, d->dimensions.data(), d->dimensions.size());
 	return 0;
 }
 
@@ -170,7 +170,7 @@ int FileFormat::getRescaleDimensions(int pBuf[2]) const
 		return -ENOENT;
 	}
 
-	memcpy(pBuf, d->rescale_dimensions, sizeof(d->rescale_dimensions));
+	memcpy(pBuf, d->rescale_dimensions.data(), d->rescale_dimensions.size());
 	return 0;
 }
 

src/amiibo-data/amiiboc.cpp
@@ -24,10 +24,12 @@
 #include <cstring>
 
 // C++ includes
+#include <array>
 #include <map>
 #include <string>
 #include <unordered_map>
 #include <vector>
+using std::array;
 using std::map;
 using std::string;
 using std::unordered_map;
@@ -103,10 +105,10 @@ static void alignFileTo16Bytes(FILE *f)
 	if (offset_mod16 == 0)
 		return;
 
-	uint8_t zerobytes[16];
-	memset(zerobytes, 0, sizeof(zerobytes));
+	array<uint8_t, 16> zerobytes;
+	zerobytes.fill(0);
 	const unsigned int count = 16 - offset_mod16;
-	fwrite(zerobytes, 1, count, f);
+	fwrite(zerobytes.data(), 1, count, f);
 }
 
 /**

src/libromdata/Audio/SNDH.cpp
@@ -60,12 +60,12 @@ public:
 		string ripper;		// Ripper name
 		string converter;	// Converter name
 
-		unsigned int subtunes;		// Subtune count (If 0 or 1, entire file is one song.)
-						// NOTE: 0 (or missing) means SNDHv1; 1 means SNDHv2.
-		unsigned int vblank_freq;	// VBlank frequency (50/60)
-		unsigned int timer_freq[4];	// Timer frequencies (A, B, C, D) [0 if not specified]
-		unsigned int year;		// Year of release
-		unsigned int def_subtune;	// Default subtune
+		unsigned int subtunes;			// Subtune count (If 0 or 1, entire file is one song.)
+							// NOTE: 0 (or missing) means SNDHv1; 1 means SNDHv2.
+		unsigned int vblank_freq;		// VBlank frequency (50/60)
+		array<unsigned int, 4> timer_freq;	// Timer frequencies (A, B, C, D) [0 if not specified]
+		unsigned int year;			// Year of release
+		unsigned int def_subtune;		// Default subtune
 
 		// TODO: Use std::pair<>?
 		// The SNDH format uses separate tags for each, though...
@@ -75,7 +75,7 @@ public:
 		TagData() : tags_read(false), subtunes(0), vblank_freq(0), year(0), def_subtune(0)
 		{
 			// Clear the timer frequencies.
-			memset(&timer_freq, 0, sizeof(timer_freq));
+			timer_freq.fill(0);
 		}
 	};
 

src/libromdata/Console/WiiTicket.cpp
@@ -610,13 +610,13 @@ int WiiTicket::decryptTitleKey(uint8_t *pKeyBuf, size_t size)
 	// Get the IV.
 	// First 8 bytes are the title ID.
 	// Second 8 bytes are all 0.
-	uint8_t iv[16];
-	memcpy(iv, d->ticket.v0.title_id.u8, 8);
+	array<uint8_t, 16> iv;
+	memcpy(iv.data(), d->ticket.v0.title_id.u8, 8);
 	memset(&iv[8], 0, 8);
 
 	// Decrypt the title key.
 	memcpy(pKeyBuf, d->ticket.v0.enc_title_key, sizeof(d->ticket.v0.enc_title_key));
-	if (cipher->decrypt(pKeyBuf, sizeof(d->ticket.v0.enc_title_key), iv, sizeof(iv)) != sizeof(d->ticket.v0.enc_title_key)) {
+	if (cipher->decrypt(pKeyBuf, sizeof(d->ticket.v0.enc_title_key), iv.data(), iv.size()) != sizeof(d->ticket.v0.enc_title_key)) {
 		// Error decrypting the title key.
 		// TODO: Return verifyResult?
 		d->verifyResult = KeyManager::VerifyResult::IAesCipherDecryptErr;

src/libromdata/Console/WiiWAD.cpp
@@ -92,7 +92,7 @@ WiiWADPrivate::WiiWADPrivate(const IRpFilePtr &file)
 	memset(&tmdHeader, 0, sizeof(tmdHeader));
 
 #ifdef ENABLE_DECRYPTION
-	memset(dec_title_key, 0, sizeof(dec_title_key));
+	dec_title_key.fill(0);
 	memset(&imet, 0, sizeof(imet));
 #endif /* ENABLE_DECRYPTION */
 }
@@ -179,12 +179,12 @@ int WiiWADPrivate::openSRL(void)
 		// Data area IV:
 		// - First two bytes are the big-endian content index.
 		// - Remaining bytes are zero.
-		uint8_t iv[16];
-		memcpy(iv, &pIMETContent->index, sizeof(pIMETContent->index));
-		memset(&iv[2], 0, sizeof(iv)-2);
+		array<uint8_t, 16> iv;
+		iv.fill(0);
+		memcpy(iv.data(), &pIMETContent->index, sizeof(pIMETContent->index));
 
 		cbcReader = std::make_shared<CBCReader>(file,
-			data_offset, data_size, dec_title_key, iv);
+			data_offset, data_size, dec_title_key.data(), iv.data());
 		if (!cbcReader->isOpen()) {
 			// Unable to open a CBC reader.
 			int ret = -cbcReader->lastError();
@@ -405,7 +405,7 @@ WiiWAD::WiiWAD(const IRpFilePtr &file)
 	// Initialize the CBC reader for the main data area.
 
 	// First, decrypt the title key.
-	int ret = d->wiiTicket->decryptTitleKey(d->dec_title_key, sizeof(d->dec_title_key));
+	int ret = d->wiiTicket->decryptTitleKey(d->dec_title_key.data(), d->dec_title_key.size());
 	d->key_status = d->wiiTicket->verifyResult();
 	if (ret != 0) {
 		// Failed to decrypt the title key.
@@ -420,14 +420,14 @@ WiiWAD::WiiWAD(const IRpFilePtr &file)
 	// Data area IV:
 	// - First two bytes are the big-endian content index.
 	// - Remaining bytes are zero.
-	uint8_t iv[16];
-	memcpy(iv, &d->pIMETContent->index, sizeof(d->pIMETContent->index));
-	memset(&iv[2], 0, sizeof(iv)-2);
+	array<uint8_t, 16> iv;
+	iv.fill(0);
+	memcpy(iv.data(), &d->pIMETContent->index, sizeof(d->pIMETContent->index));
 
 	// Create a CBC reader to decrypt the data section.
 	// TODO: Verify some known data?
 	d->cbcReader = std::make_shared<CBCReader>(d->file,
-		d->data_offset, d->data_size, d->dec_title_key, iv);
+		d->data_offset, d->data_size, d->dec_title_key.data(), iv.data());
 
 	if (d->tmdHeader.title_id.sysID != cpu_to_be16(3)) {
 		// Wii: Contents may be one of the following:

src/libromdata/Console/WiiWAD_p.hpp
@@ -110,7 +110,7 @@ public:
 	LibRpBase::RomDataPtr mainContent;	// WiiWIBN or NintendoDS
 
 	// Decrypted title key
-	uint8_t dec_title_key[16];
+	std::array<uint8_t, 16> dec_title_key;
 
 	// Main data headers
 	Wii_IMET_t imet;	// NOTE: May be WIBN.

src/libromdata/Console/Xbox360_XEX.cpp
@@ -611,7 +611,7 @@ int Xbox360_XEX_Private::initPeReader(void)
 
 		// Zero data
 		array<uint8_t, 16> zero16;
-		memset(zero16.data(), 0, zero16.size());
+		zero16.fill(0);
 
 		// Key data.
 		// - 0: retail

src/libromdata/Handheld/NintendoDS_ops.cpp
@@ -287,9 +287,10 @@ int NintendoDS::doRomOp_int(int id, RomOpParams *pParams)
 					return -EIO;
 				}
 
-#define UNTRIM_BLOCK_SIZE (64*1024)
-				unique_ptr<uint8_t[]> ff_block(new uint8_t[UNTRIM_BLOCK_SIZE]);
-				memset(ff_block.get(), 0xFF, UNTRIM_BLOCK_SIZE);
+				static constexpr size_t UNTRIM_BLOCK_SIZE = 64U * 1024U;
+				typedef array<uint8_t, UNTRIM_BLOCK_SIZE> ff_block_t;
+				unique_ptr<ff_block_t> ff_block(new ff_block_t);
+				ff_block->fill(0xFF);
 
 				off64_t pos = static_cast<off64_t>(total_used_rom_size);
 				int ret = d->file->seek(pos);
@@ -309,7 +310,7 @@ int NintendoDS::doRomOp_int(int id, RomOpParams *pParams)
 				const unsigned int partial = static_cast<unsigned int>(pos % UNTRIM_BLOCK_SIZE);
 				if (partial != 0) {
 					const unsigned int toWrite = UNTRIM_BLOCK_SIZE - partial;
-					size_t size = d->file->write(ff_block.get(), toWrite);
+					size_t size = d->file->write(ff_block->data(), toWrite);
 					if (size != toWrite) {
 						// Write error.
 						ret = -d->file->lastError();
@@ -325,7 +326,7 @@ int NintendoDS::doRomOp_int(int id, RomOpParams *pParams)
 
 				// Write remaining full blocks.
 				for (; pos < next_pow2; pos += UNTRIM_BLOCK_SIZE) {
-					size_t size = d->file->write(ff_block.get(), UNTRIM_BLOCK_SIZE);
+					size_t size = d->file->write(ff_block->data(), UNTRIM_BLOCK_SIZE);
 					if (size != UNTRIM_BLOCK_SIZE) {
 						// Write error.
 						ret = -d->file->lastError();

commit 500f3e2c0181a716c6ec6054051eeb27a00325bd

[librptexture] FileFormat: Need to multiply the (rescale_)dimensions size by sizeof(int).
Otherwise, memcpy() doesn't copy enough, which results in dimensions
having garbage data.

Fixes RomHeaderTest.

This fixes a regression from commit 4cc1225157914b6b1ffdc1476b5769a25af77847.
(Various std::array<> improvements.)

src/librptexture/fileformat/FileFormat.cpp
@@ -143,7 +143,7 @@ int FileFormat::getDimensions(int pBuf[3]) const
 		return -EBADF;
 	}
 
-	memcpy(pBuf, d->dimensions.data(), d->dimensions.size());
+	memcpy(pBuf, d->dimensions.data(), d->dimensions.size() * sizeof(int));
 	return 0;
 }
 
@@ -170,7 +170,7 @@ int FileFormat::getRescaleDimensions(int pBuf[2]) const
 		return -ENOENT;
 	}
 
-	memcpy(pBuf, d->rescale_dimensions.data(), d->rescale_dimensions.size());
+	memcpy(pBuf, d->rescale_dimensions.data(), d->rescale_dimensions.size() * sizeof(int));
 	return 0;
 }
 