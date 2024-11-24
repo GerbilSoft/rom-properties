GerbilSoft/rom-properties

commit e277504f395a742a4cf532328d5895177a0f7c45

[win32] ISpriteSheet::getIcon(): Use a raw RpFile_windres instead of shared_ptr<RpFile_windres>.

src/win32/ISpriteSheet.cpp
@@ -138,15 +138,17 @@ HBITMAP ISpriteSheet::getIcon(int col, int row, bool gray, UINT dpi) const
 			return nullptr;
 		}
 
-		shared_ptr<RpFile_windres> f_res = std::make_shared<RpFile_windres>(
+		RpFile_windres *const f_res = std::make_shared<RpFile_windres>(
 			HINST_THISCOMPONENT, resourceID, MAKEINTRESOURCE(RT_PNG));
 		assert(f_res->isOpen());
 		if (!f_res->isOpen()) {
 			// Unable to open the resource.
+			delete f_res;
 			return nullptr;
 		}
 
 		imgSpriteSheet = RpPng::load(f_res);
+		delete f_res;
 		assert((bool)imgSpriteSheet);
 		if (!imgSpriteSheet) {
 			// Unable to load the resource as a PNG image.

commit 0ec0b7621685e3bbd16368bae5d4d73e520c72cc

[win32] ISpriteSheet.cpp: Fix build error caused by an incomplete change.
This fixes a regression from commit e277504f395a742a4cf532328d5895177a0f7c45.
([win32] ISpriteSheet::getIcon(): Use a raw RpFile_windres instead of shared_ptr<RpFile_windres>.)

src/win32/ISpriteSheet.cpp
@@ -138,7 +138,7 @@ HBITMAP ISpriteSheet::getIcon(int col, int row, bool gray, UINT dpi) const
 			return nullptr;
 		}
 
-		RpFile_windres *const f_res = std::make_shared<RpFile_windres>(
+		RpFile_windres *const f_res = new RpFile_windres(
 			HINST_THISCOMPONENT, resourceID, MAKEINTRESOURCE(RT_PNG));
 		assert(f_res->isOpen());
 		if (!f_res->isOpen()) {