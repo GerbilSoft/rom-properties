GerbilSoft/rom-properties

commit 3ad5a7c0510c00b93048bbe3b3c36b2558aee930

[win32] Remove unnecessary memset()s of LVITEM and LVCOLUMN.
Make sure the iItem and iSubItem fields are initialized.

We don't have to memset() the whole struct; just set the fields
that are needed, based on the mask field.

src/win32/config/KeyManagerTab.cpp
@@ -363,27 +363,30 @@ void KeyManagerTabPrivate::initDialog(void)
 	ListView_SetItemCountEx(hListView, keyStore->totalKeyCount(),
 		LVSICF_NOINVALIDATEALL | LVSICF_NOSCROLL);
 
-	// Column title.
+	// Column title
 	tstring tsColTitle;
 
-	// tr: Column 0: Key Name.
+	// tr: Column 0: Key Name
 	tsColTitle = TC_("KeyManagerTab", "Key Name");
-	LVCOLUMN lvCol;
-	memset(&lvCol, 0, sizeof(lvCol));
-	lvCol.mask = LVCF_FMT | LVCF_TEXT | LVCF_SUBITEM;
-	lvCol.fmt = LVCFMT_LEFT;
-	lvCol.pszText = const_cast<LPTSTR>(tsColTitle.c_str());
-	ListView_InsertColumn(hListView, 0, &lvCol);
+	LVCOLUMN lvColumn;
+	lvColumn.mask = LVCF_FMT | LVCF_TEXT | LVCF_SUBITEM;
+	lvColumn.fmt = LVCFMT_LEFT;
+	lvColumn.pszText = const_cast<LPTSTR>(tsColTitle.c_str());
+	lvColumn.iItem = 0;
+	lvColumn.iSubItem = 0;
+	ListView_InsertColumn(hListView, 0, &lvColumn);
 
-	// tr: Column 1: Value.
+	// tr: Column 1: Value
 	tsColTitle = TC_("KeyManagerTab", "Value");
-	lvCol.pszText = const_cast<LPTSTR>(tsColTitle.c_str());
-	ListView_InsertColumn(hListView, 1, &lvCol);
+	lvColumn.pszText = const_cast<LPTSTR>(tsColTitle.c_str());
+	lvColumn.iSubItem = 1;
+	ListView_InsertColumn(hListView, 1, &lvColumn);
 
-	// tr: Column 2: Verification status.
+	// tr: Column 2: Verification status
 	tsColTitle = TC_("KeyManagerTab", "Valid?");
-	lvCol.pszText = const_cast<LPTSTR>(tsColTitle.c_str());
-	ListView_InsertColumn(hListView, 2, &lvCol);
+	lvColumn.pszText = const_cast<LPTSTR>(tsColTitle.c_str());
+	lvColumn.iSubItem = 2;
+	ListView_InsertColumn(hListView, 2, &lvColumn);
 
 	if (isComCtl32_v610) {
 		// Set the IOwnerDataCallback.

src/win32/RP_XAttrView.cpp
@@ -121,8 +121,8 @@ int RP_XAttrView_Private::loadADS(void)
 	const XAttrReader::XAttrList &xattrList = xattrReader->genericXAttrs();
 
 	LVITEM lvItem;
-	memset(&lvItem, 0, sizeof(lvItem));
 	lvItem.mask = LVIF_TEXT;
+	lvItem.iItem = 0;
 	for (const auto &xattr : xattrList) {
 		tstring tstr = U82T_c(xattr.first.c_str());
 		lvItem.iSubItem = 0;

src/win32/config/CacheTab.cpp
@@ -259,7 +259,6 @@ void CacheTabPrivate::enumDrivesXP(void)
 	TCHAR path[] = _T("X:\\");
 	SHFILEINFO sfi;
 	LVITEM lvi;
-	memset(&lvi, 0, sizeof(lvi));
 	lvi.mask = LVIF_IMAGE | LVIF_PARAM | LVIF_TEXT;
 	for (unsigned int i = 0; i < 26 && dwDrives != 0; i++, dwDrives >>= 1) {
 		// Ignore missing drives and network drives.
@@ -299,8 +298,6 @@ void CacheTabPrivate::updateDrivesXP(DWORD unitmask)
 	TCHAR path[] = _T("X:\\");
 	SHFILEINFO sfi;
 	LVITEM lviNew, lviCur;
-	memset(&lviNew, 0, sizeof(lviNew));
-	memset(&lviCur, 0, sizeof(lviCur));
 	lviNew.mask = LVIF_IMAGE | LVIF_PARAM | LVIF_TEXT;
 	lviCur.mask = LVIF_PARAM;
 

commit cadbae4527ad23a2e24664612607c374c2d5bf10

[win32] KeyManagerTabPrivate::initDialog(): No LVCOLUMN::iItem field.
This fixes a regression from commit 3ad5a7c0510c00b93048bbe3b3c36b2558aee930.
([win32] Remove unnecessary memset()s of LVITEM and LVCOLUMN.)

src/win32/config/KeyManagerTab.cpp
@@ -372,7 +372,6 @@ void KeyManagerTabPrivate::initDialog(void)
 	lvColumn.mask = LVCF_FMT | LVCF_TEXT | LVCF_SUBITEM;
 	lvColumn.fmt = LVCFMT_LEFT;
 	lvColumn.pszText = const_cast<LPTSTR>(tsColTitle.c_str());
-	lvColumn.iItem = 0;
 	lvColumn.iSubItem = 0;
 	ListView_InsertColumn(hListView, 0, &lvColumn);
 