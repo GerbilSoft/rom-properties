4444f6afc9bcd34c40d665be5797e5739f4ea334
David Korth <gerbilsoft@gerbilsoft.com>
2025-01-27 18:25:10 -0500

    [librptexture] FileFormatPrivate: Remove `ref_cnt`.
    This was from the *original* reference counting implementation, before
    it was converted to RefBase in commit 89b1d9e52dbe58ee57e091fdd77017eed5c8c86d
    (June 2020), and before it was converted to std::shared_ptr<> in
    commit 2a12422844e2dffec17a64c3e07e740e29c9ca76 (August 2023).
    
    `ref_cnt` hasn't been used for anything since the RefBase conversion,
    so it just wasted 4 bytes of memory per FileFormatPrivate object.

--- src/librptexture/fileformat/FileFormat.cpp
+++ src/librptexture/fileformat/FileFormat.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librptexture)                     *
  * FileFormat.cpp: Texture file format base class.                         *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -28,7 +28,6 @@ namespace LibRpTexture {
  */
 FileFormatPrivate::FileFormatPrivate(FileFormat *q, const IRpFilePtr &file, const TextureInfo *pTextureInfo)
 	: q_ptr(q)
-	, ref_cnt(1)
 	, isValid(false)
 	, file(file)
 	, pTextureInfo(pTextureInfo)
--- src/librptexture/fileformat/FileFormat_p.hpp
+++ src/librptexture/fileformat/FileFormat_p.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librptexture)                     *
  * FileFormat.hpp: Texture file format base class. (PRIVATE CLASS)         *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -26,41 +26,40 @@ struct TextureInfo {
 class FileFormat;
 class FileFormatPrivate
 {
-	public:
-		/**
-		 * Initialize a FileFormatPrivate storage class.
-		 *
-		 * @param q FileFormat class
-		 * @param file Texture file
-		 * @param pTextureInfo FileFormat subclass information
-		 */
-		explicit FileFormatPrivate(FileFormat *q, const LibRpFile::IRpFilePtr &file, const TextureInfo *pTextureInfo);
-	public:
-		virtual ~FileFormatPrivate() = default;
+public:
+	/**
+	 * Initialize a FileFormatPrivate storage class.
+	 *
+	 * @param q FileFormat class
+	 * @param file Texture file
+	 * @param pTextureInfo FileFormat subclass information
+	 */
+	explicit FileFormatPrivate(FileFormat *q, const LibRpFile::IRpFilePtr &file, const TextureInfo *pTextureInfo);
+public:
+	virtual ~FileFormatPrivate() = default;
 
-	private:
-		RP_DISABLE_COPY(FileFormatPrivate)
-	protected:
-		friend class FileFormat;
-		FileFormat *const q_ptr;
+private:
+	RP_DISABLE_COPY(FileFormatPrivate)
+protected:
+	friend class FileFormat;
+	FileFormat *const q_ptr;
 
-	public:
-		volatile int ref_cnt;		// Reference count.
-		bool isValid;			// Subclass must set this to true if the ROM is valid.
-		LibRpFile::IRpFilePtr file;	// Open file
+public:
+	bool isValid;			// Subclass must set this to true if the texture is valid.
+	LibRpFile::IRpFilePtr file;	// Open file
 
-	public:
-		/** These fields must be set by FileFormat subclasses in their constructors. **/
-		const TextureInfo *pTextureInfo;	// FileFormat subclass information
-		const char *mimeType;			// MIME type (ASCII) (default is nullptr)
-		const char *textureFormatName;		// Texture format name
-		std::array<int, 3> dimensions;		// Dimensions (width, height, depth)
+public:
+	/** These fields must be set by FileFormat subclasses in their constructors. **/
+	const TextureInfo *pTextureInfo;	// FileFormat subclass information
+	const char *mimeType;			// MIME type (ASCII) (default is nullptr)
+	const char *textureFormatName;		// Texture format name
+	std::array<int, 3> dimensions;		// Dimensions (width, height, depth)
 							// 2D textures have depth=0
-		std::array<int, 2> rescale_dimensions;	// Rescale dimensions, (width, height)
-							// Needed for e.g. ETC2 where a power-of-2 size
-							// is used but the image should be rescaled before
-							// displaying in a UI frontend.
-		int mipmapCount;			// Mipmap count (0 == none; -1 == not supported)
+	std::array<int, 2> rescale_dimensions;	// Rescale dimensions, (width, height)
+						// Needed for e.g. ETC2 where a power-of-2 size
+						// is used but the image should be rescaled before
+						// displaying in a UI frontend.
+	int mipmapCount;			// Mipmap count (0 == none; -1 == not supported)
 };
 
 }
