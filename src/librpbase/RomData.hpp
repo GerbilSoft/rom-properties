0f020572ce93bbe76d6c99285c909e91f9dabe88
David Korth <gerbilsoft@gerbilsoft.com>
2025-02-04 00:05:15 -0500

    [librpbase] RomData.hpp: Update the copyright year.

--- src/librpbase/RomData.hpp
+++ src/librpbase/RomData.hpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RomData.hpp: ROM data base class.                                       *
  *                                                                         *
- * Copyright (c) 2016-2024 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
