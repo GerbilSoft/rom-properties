GerbilSoft/rom-properties

commit 5cf435cafaf7a8882e0889605550997184883f8a

[librpfile] Some miscellaneous string optimizations.
recursiveScan(): std::move() the full file name, then access it from
the iterator.

FileSystem_win32.cpp, isOnBadFS():
- rmkdir(): Use U82W_s() instead of U82T_s().
- isOnBadFS(): Template the function.

src/librpfile/RecursiveScan_posix.cpp
@@ -144,8 +144,9 @@ int recursiveScan(const TCHAR *path, forward_list<pair<tstring, uint8_t> > &rlis
 		// Check the filename to see if we should delete it.
 		if (d_type == DT_REG || d_type == DT_UNKNOWN) {
 			// Thumbs.db files can be deleted.
-			if (!strcasecmp(dirent->d_name, "Thumbs.db"))
+			if (!strcasecmp(dirent->d_name, "Thumbs.db")) {
 				goto isok;
+			}
 
 			// Check the extension.
 			const size_t len = strlen(dirent->d_name);
@@ -171,14 +172,14 @@ int recursiveScan(const TCHAR *path, forward_list<pair<tstring, uint8_t> > &rlis
 
 	isok:
 		// Add the filename and file type.
-		rlist.emplace_front(fullpath, d_type);
+		const auto &elem = rlist.emplace_front(std::move(fullpath), d_type);
 
 		// If this is a directory, recursively scan it.
 		// This is done *after* adding the directory because forward_list
 		// enumerates items in reverse order.
 		if (d_type == DT_DIR) {
 			// Recursively scan the directory.
-			recursiveScan(fullpath.c_str(), rlist);
+			recursiveScan(elem.first.c_str(), rlist);
 		}
 	};
 	closedir(pdir);

src/librpfile/win32/RecursiveScan_win32.cpp
@@ -62,8 +62,9 @@ int recursiveScan(const TCHAR *path, forward_list<pair<tstring, uint8_t> > &rlis
 		// Make sure we should delete this file.
 		if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
 			// Thumbs.db files can be deleted.
-			if (!_tcsicmp(findFileData.cFileName, _T("Thumbs.db")))
+			if (!_tcsicmp(findFileData.cFileName, _T("Thumbs.db"))) {
 				goto isok;
+			}
 
 			// Check the extension.
 			size_t len = _tcslen(findFileData.cFileName);
@@ -93,14 +94,15 @@ int recursiveScan(const TCHAR *path, forward_list<pair<tstring, uint8_t> > &rlis
 		fullFileName += findFileData.cFileName;
 
 		// Add the filename and d_type.
-		rlist.emplace_front(fullFileName, FileSystem::win32_attrs_to_d_type(findFileData.dwFileAttributes));
+		const auto &elem = rlist.emplace_front(std::move(fullFileName),
+			FileSystem::win32_attrs_to_d_type(findFileData.dwFileAttributes));
 
 		// If this is a directory, recursively scan it.
 		// This is done *after* adding the directory because forward_list
 		// enumerates items in reverse order.
 		if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 			// Recursively scan it.
-			recursiveScan(fullFileName.c_str(), rlist);
+			recursiveScan(elem.first.c_str(), rlist);
 		}
 	} while (FindNextFile(hFindFile, &findFileData));
 	FindClose(hFindFile);

src/librpfile/win32/FileSystem_win32.cpp
@@ -241,8 +241,11 @@ int rmkdir(const string &path, int mode)
 	static_assert(sizeof(wchar_t) == sizeof(char16_t), "wchar_t is not 16-bit!");
 	RP_UNUSED(mode);
 
+	// NOTE: Explicitly calling U82W_s(), not U82T_s(), to prevent
+	// infinite recursion in ANSI builds.
+	// NOTE 2: This probably won't work in ANSI builds anyway...
 	// TODO: makeWinPath()?
-	return rmkdir(U82T_s(path));
+	return rmkdir(U82W_s(path));
 }
 
 /**
@@ -709,7 +712,7 @@ wstring resolve_symlink(const wchar_t *filename)
 {
 	assert(filename != nullptr);
 	assert(filename[0] != L'\0');
-	if (unlikely(!filename || filename[0] == '\0')) {
+	if (unlikely(!filename || filename[0] == L'\0')) {
 		return {};
 	}
 	const tstring tfilename = makeWinPath(filename);
@@ -764,12 +767,14 @@ bool is_directory(const wchar_t *filename)
  * We don't want to check files on e.g. procfs,
  * or on network file systems if the option is disabled.
  *
- * @param filename Filename (UTF-8)
+ * @tparam CharType Character type (char for UTF-8; wchar_t for Windows UTF-16)
+ * @param filename Filename
  * @param allowNetFS If true, allow network file systems.
  *
  * @return True if this file is on a "bad" file system; false if not.
  */
-bool isOnBadFS(const char *filename, bool allowNetFS)
+template<typename CharType>
+static inline bool T_isOnBadFS(const CharType *filename, bool allowNetFS)
 {
 	// TODO: More comprehensive check.
 	// For now, merely checking if it starts with "\\\\"
@@ -791,6 +796,22 @@ bool isOnBadFS(const char *filename, bool allowNetFS)
  * We don't want to check files on e.g. procfs,
  * or on network file systems if the option is disabled.
  *
+ * @param filename Filename (UTF-8)
+ * @param allowNetFS If true, allow network file systems.
+ *
+ * @return True if this file is on a "bad" file system; false if not.
+ */
+bool isOnBadFS(const char *filename, bool allowNetFS)
+{
+	return T_isOnBadFS(filename, allowNetFS);
+}
+
+/**
+ * Is a file located on a "bad" file system?
+ *
+ * We don't want to check files on e.g. procfs,
+ * or on network file systems if the option is disabled.
+ *
  * @param filename Filename (UTF-16)
  * @param allowNetFS If true, allow network file systems.
  *
@@ -798,18 +819,7 @@ bool isOnBadFS(const char *filename, bool allowNetFS)
  */
 bool isOnBadFS(const wchar_t *filename, bool allowNetFS)
 {
-	// TODO: More comprehensive check.
-	// For now, merely checking if it starts with "\\\\"
-	// and the third character is not '?' or '.'.
-	if (filename[0] == L'\\' && filename[1] == L'\\' &&
-	    filename[2] != L'\0' && filename[2] != L'?' && filename[2] != L'.')
-	{
-		// This file is located on a network share.
-		return !allowNetFS;
-	}
-
-	// Not on a network share.
-	return false;
+	return T_isOnBadFS(filename, allowNetFS);
 }
 
 /**

commit 817f8ed4eb45027db1ca907541ed4efc8bfd3ca4

[librpfile] RecursiveScan: std::forward_list<>::emplace_front() only returns a value starting with C++17.
This broke the MSVC 2015 build.

This fixes a regression from commit 5cf435cafaf7a8882e0889605550997184883f8a.
([librpfile] Some miscellaneous string optimizations.)

src/librpfile/RecursiveScan_posix.cpp
@@ -172,14 +172,14 @@ int recursiveScan(const TCHAR *path, forward_list<pair<tstring, uint8_t> > &rlis
 
 	isok:
 		// Add the filename and file type.
-		const auto &elem = rlist.emplace_front(std::move(fullpath), d_type);
+		rlist.emplace_front(fullpath, d_type);
 
 		// If this is a directory, recursively scan it.
 		// This is done *after* adding the directory because forward_list
 		// enumerates items in reverse order.
 		if (d_type == DT_DIR) {
 			// Recursively scan the directory.
-			recursiveScan(elem.first.c_str(), rlist);
+			recursiveScan(fullpath.c_str(), rlist);
 		}
 	};
 	closedir(pdir);

src/librpfile/win32/RecursiveScan_win32.cpp
@@ -94,15 +94,14 @@ int recursiveScan(const TCHAR *path, forward_list<pair<tstring, uint8_t> > &rlis
 		fullFileName += findFileData.cFileName;
 
 		// Add the filename and d_type.
-		const auto &elem = rlist.emplace_front(std::move(fullFileName),
-			FileSystem::win32_attrs_to_d_type(findFileData.dwFileAttributes));
+		rlist.emplace_front(fullFileName, FileSystem::win32_attrs_to_d_type(findFileData.dwFileAttributes));
 
 		// If this is a directory, recursively scan it.
 		// This is done *after* adding the directory because forward_list
 		// enumerates items in reverse order.
 		if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 			// Recursively scan it.
-			recursiveScan(elem.first.c_str(), rlist);
+			recursiveScan(fullFileName.c_str(), rlist);
 		}
 	} while (FindNextFile(hFindFile, &findFileData));
 	FindClose(hFindFile);