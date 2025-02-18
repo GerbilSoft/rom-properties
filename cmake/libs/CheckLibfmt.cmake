GerbilSoft/rom-properties

commit 06f2f46655a8d578d10a7d78a9c020deddaf3ce6

[cmake] CheckLibfmt.cmake: Remove FMT_SHARED from INTERFACE_COMPILE_DEFINITIONS.
This causes parts of `class format_error` to be exported from
libromdata.so, which could result in multiple definitions:

V typeinfo for fmt::v11::format_error
V typeinfo name for fmt::v11::format_error
V vtable for fmt::v11::format_error

NOTE: Not needed for Win32.

cmake/libs/CheckLibfmt.cmake
@@ -59,3 +59,13 @@ IF(USE_INTERNAL_FMT)
 ELSE(USE_INTERNAL_FMT)
 	SET(USE_INTERNAL_FMT_DLL OFF)
 ENDIF(USE_INTERNAL_FMT)
+
+# WORKAROUND: fmt sets INTERFACE_COMPILE_DEFINITIONS=FMT_SHARED.
+# This causes parts of `class format_error` to be exported from
+# libromdata.so, which could result in multiple definitions.
+# (Not needed for Win32.)
+IF(NOT WIN32)
+	SET_TARGET_PROPERTIES(fmt::fmt PROPERTIES
+		INTERFACE_COMPILE_DEFINITIONS ""
+		)
+ENDIF(NOT WIN32)

commit 84163ba5d52985d69305bf2258bbbc76023e9c34

[cmake] CheckLibfmt.cmake: Only remove FMT_SHARED if fmt::fmt is a valid target.
If the internal libfmt is used, it might not be a valid target.

This fixes a regression from commit 06f2f46655a8d578d10a7d78a9c020deddaf3ce6.
([cmake] CheckLibfmt.cmake: Remove FMT_SHARED from INTERFACE_COMPILE_DEFINITIONS.)

cmake/libs/CheckLibfmt.cmake
@@ -64,8 +64,8 @@ ENDIF(USE_INTERNAL_FMT)
 # This causes parts of `class format_error` to be exported from
 # libromdata.so, which could result in multiple definitions.
 # (Not needed for Win32.)
-IF(NOT WIN32)
+IF(NOT WIN32 AND TARGET fmt::fmt)
 	SET_TARGET_PROPERTIES(fmt::fmt PROPERTIES
 		INTERFACE_COMPILE_DEFINITIONS ""
 		)
-ENDIF(NOT WIN32)
+ENDIF(NOT WIN32 AND TARGET fmt::fmt)