9f9338fe40eddf3ac60ad73342480876c65ad257
David Korth <gerbilsoft@gerbilsoft.com>
2025-03-07 23:39:53 -0500

    [librpbase] timeconv.h: +#include <time.h> for time_t

--- src/librpbase/timeconv.h
+++ src/librpbase/timeconv.h
@@ -12,6 +12,7 @@
 
 // C includes
 #include <stdint.h>
+#include <time.h>
 
 // Macros from MinGW-w64's gettimeofday.c.
 #define FILETIME_1970 116444736000000000LL	// Seconds between 1/1/1601 and 1/1/1970.
