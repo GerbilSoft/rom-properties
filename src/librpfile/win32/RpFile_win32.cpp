GerbilSoft/rom-properties

commit 72339ff68b6fd25b127d4dc1bdac75ac4af9bd75

Switch back to C++ strings for stored filenames.
The C strings reduced memory usage (slightly), but added cognitive
overhead with regards to ensuring strings were freed properly.

Switching back to C++ strings also simplifies the code a bit, since
we don't have to call free() or check if the string is NULL; just empty.

RpFile_IStream::filename(): TODO: Cache "filename obtained"?

src/librpfile/win32/RpFile_win32.cpp
@@ -47,7 +47,7 @@ RpFilePrivate::RpFilePrivate(RpFile *q, const wchar_t *filenameW, RpFile::FileMo
 	, mode(mode), gzfd(nullptr), gzsz(-1)
 {
 	assert(filenameW != nullptr);
-	this->filenameW = wcsdup(filenameW);
+	this->filenameW.assign(filenameW);
 }
 
 RpFilePrivate::~RpFilePrivate()
@@ -58,8 +58,6 @@ RpFilePrivate::~RpFilePrivate()
 	if (file && file != INVALID_HANDLE_VALUE) {
 		CloseHandle(file);
 	}
-	free(filename);
-	free(filenameW);
 }
 
 /**
@@ -113,7 +111,7 @@ inline int RpFilePrivate::mode_to_win32(RpFile::FileMode mode,
 int RpFilePrivate::reOpenFile(void)
 {
 	RP_Q(RpFile);
-	if (!filenameW || filenameW[0] == L'\0') {
+	if (unlikely(filenameW.empty())) {
 		// No filename...
 		q->m_lastError = EINVAL;
 		return -EINVAL;
@@ -134,18 +132,9 @@ int RpFilePrivate::reOpenFile(void)
 	    filenameW[1] == L':' && filenameW[2] == L'\0')
 	{
 		// Drive letter. Append '\\'.
-		wchar_t *drvfilename = static_cast<wchar_t*>(malloc(4*sizeof(wchar_t)));
-		drvfilename[0] = filenameW[0];
-		drvfilename[1] = _T(':');
-		drvfilename[2] = _T('\\');
-		drvfilename[3] = _T('\0');
-
-		std::swap(drvfilename, this->filenameW);
-		free(drvfilename);
-		if (this->filename) {
-			free(this->filename);
-			this->filename = nullptr;
-		}
+		const wchar_t drvfilename[4] = {filenameW[0], L':', L'\\', L'\0'};
+		filenameW.assign(drvfilename);
+		filename.clear();
 	}
 
 	// Adjusted filename for Windows.
@@ -163,7 +152,7 @@ int RpFilePrivate::reOpenFile(void)
 			// or if we have to resolve the physical device name.
 			// NOTE: filename is UTF-8, but we can use it as if
 			// it's ANSI for a drive letter.
-			const UINT driveType = GetDriveType(filenameW);
+			const UINT driveType = GetDriveType(filenameW.c_str());
 			switch (driveType) {
 				case DRIVE_CDROM:
 					// CD-ROM works.
@@ -764,11 +753,11 @@ off64_t RpFile::size(void)
 const char *RpFile::filename(void) const
 {
 	RP_D(const RpFile);
-	if (d->filename && d->filename[0] != '\0') {
-		return d->filename;
-	} else if (d->filenameW && d->filenameW[0] != L'\0') {
-		const_cast<RpFilePrivate*>(d)->filename = strdup(W2U8(d->filenameW).c_str());
-		return d->filename;
+	if (unlikely(!d->filename.empty())) {
+		return d->filename.c_str();
+	} else if (likely(!d->filenameW.empty())) {
+		const_cast<RpFilePrivate*>(d)->filename = W2U8(d->filenameW);
+		return d->filename.c_str();
 	}
 
 	return nullptr;
@@ -782,7 +771,7 @@ const char *RpFile::filename(void) const
 const wchar_t *RpFile::filenameW(void) const
 {
 	RP_D(const RpFile);
-	return (d->filenameW != nullptr && d->filenameW[0] != L'\0') ? d->filenameW : nullptr;
+	return (likely(!d->filenameW.empty())) ? d->filenameW.c_str() : nullptr;
 }
 
 /** Extra functions **/

src/librpbase/RomData.cpp
@@ -41,10 +41,6 @@ RomDataPrivate::RomDataPrivate(const IRpFilePtr &file, const RomDataInfo *pRomDa
 	, isPAL(false)
 	, isCompressed(false)
 	, file(file)
-	, filename(nullptr)
-#ifdef _WIN32
-	, filenameW(nullptr)
-#endif /* _WIN32 */
 {
 	assert(pRomDataInfo != nullptr);
 
@@ -65,7 +61,7 @@ RomDataPrivate::RomDataPrivate(const IRpFilePtr &file, const RomDataInfo *pRomDa
 	if (rpFile) {
 		const wchar_t *const filenameW = rpFile->filenameW();
 		if (filenameW) {
-			this->filenameW = wcsdup(filenameW);
+			this->filenameW.assign(filenameW);
 		}
 	}
 #endif /* _WIN32 */
@@ -73,16 +69,8 @@ RomDataPrivate::RomDataPrivate(const IRpFilePtr &file, const RomDataInfo *pRomDa
 	// TODO: Don't set if filenameW was set?
 	const char *const filename = this->file->filename();
 	if (filename) {
-		this->filename = strdup(filename);
+		this->filename.assign(filename);
 	}
-}
-
-RomDataPrivate::~RomDataPrivate()
-{
-	free(filename);
-#ifdef _WIN32
-	free(filenameW);
-#endif /* _WIN32 */
 }
 
 /** Convenience functions **/
@@ -616,7 +604,7 @@ const char *RomData::filename(void) const
 {
 	// TODO: filenameW() variant on Windows?
 	RP_D(const RomData);
-	return (d->filename != nullptr && d->filename[0] != '\0') ? d->filename : nullptr;
+	return (likely(!d->filename.empty())) ? d->filename.c_str() : nullptr;
 }
 
 /**
@@ -1144,7 +1132,7 @@ int RomData::doRomOp(int id, RomOpParams *pParams)
 		closeFileAfter = true;
 		IRpFilePtr file;
 #ifdef _WIN32
-		if (d->filenameW) {
+		if (likely(!d->filenameW.empty())) {
 			file = std::make_shared<RpFile>(d->filenameW, RpFile::FM_OPEN_WRITE);
 		} else
 #endif /* _WIN32 */

src/librpbase/RomData_p.hpp
@@ -49,7 +49,7 @@ protected:
 	 */
 	RomDataPrivate(const LibRpFile::IRpFilePtr &file, const RomDataInfo *pRomDataInfo);
 public:
-	virtual ~RomDataPrivate();
+	virtual ~RomDataPrivate() = default;
 
 private:
 	RP_DISABLE_COPY(RomDataPrivate)
@@ -65,9 +65,9 @@ public:
 	/** These fields are set by RomData's own constructor. **/
 	bool isCompressed;		// True if the file is compressed. (transparent decompression)
 	LibRpFile::IRpFilePtr file;	// Open file
-	char *filename;			// Copy of the filename (UTF-8)
+	std::string filename;		// Filename (UTF-8)
 #ifdef _WIN32
-	wchar_t *filenameW;		// Copy of the filename (UTF-16; Windows only)
+	std::wstring filenameW;		// Filename (UTF-16; Windows only)
 #endif /* _WIN32 */
 public:
 	RomFields fields;		// ROM fields

src/librpfile/RpFile_p.hpp
@@ -77,9 +77,9 @@ private:
 
 public:
 	FILE_TYPE file;		// File pointer
-	char *filename;		// Filename (UTF-8)
+	std::string filename;	// Filename (UTF-8)
 #ifdef _WIN32
-	wchar_t *filenameW;	// Filename (UTF-16)
+	std::wstring filenameW;	// Filename (UTF-16)
 				// NOTE: Win32 version uses this as primary.
 				// UTF-8 filename is only used for the filename() function.
 #endif /* _WIN32 */

src/librpfile/RpFile_stdio.cpp
@@ -36,7 +36,7 @@ RpFilePrivate::RpFilePrivate(RpFile *q, const char *filename, RpFile::FileMode m
 	, mode(mode), gzfd(nullptr), gzsz(-1)
 {
 	assert(filename != nullptr);
-	this->filename = strdup(filename);
+	this->filename.assign(filename);
 }
 
 RpFilePrivate::~RpFilePrivate()
@@ -47,7 +47,6 @@ RpFilePrivate::~RpFilePrivate()
 	if (file) {
 		fclose(file);
 	}
-	free(filename);
 }
 
 /**
@@ -101,7 +100,7 @@ int RpFilePrivate::reOpenFile(void)
 	uint8_t fileType = 0;
 #ifdef HAVE_STATX
 	struct statx sbx;
-	int ret = statx(AT_FDCWD, filename, 0, STATX_TYPE, &sbx);
+	int ret = statx(AT_FDCWD, filename.c_str(), 0, STATX_TYPE, &sbx);
 	if (ret == 0 && (sbx.stx_mask & STATX_TYPE)) {
 		// statx() succeeded.
 		hasFileMode = true;
@@ -109,7 +108,7 @@ int RpFilePrivate::reOpenFile(void)
 	}
 #else /* !HAVE_STATX */
 	struct stat sb;
-	if (!stat(filename, &sb)) {
+	if (!stat(filename.c_str(), &sb)) {
 		// fstat() succeeded.
 		hasFileMode = true;
 		fileType = IFTODT(sb.st_mode);
@@ -183,7 +182,7 @@ int RpFilePrivate::reOpenFile(void)
 #ifndef NO_PATTERNS_FOR_THIS_OS
 		bool isMatch = false;
 		for (const auto &pattern : fileNamePatterns) {
-			if (!strncasecmp(filename, &pattern[1], static_cast<uint8_t>(pattern[0]))) {
+			if (!strncasecmp(filename.c_str(), &pattern[1], static_cast<uint8_t>(pattern[0]))) {
 				// Found a match!
 				isMatch = true;
 				break;
@@ -199,7 +198,7 @@ int RpFilePrivate::reOpenFile(void)
 #endif /* NO_PATTERNS_FOR_THIS_OS */
 	}
 
-	file = fopen(filename, mode_str);
+	file = fopen(filename.c_str(), mode_str);
 
 	// If fopen() failed (and returned nullptr),
 	// return the non-zero error code.
@@ -608,7 +607,7 @@ off64_t RpFile::size(void)
 const char *RpFile::filename(void) const
 {
 	RP_D(const RpFile);
-	return (d->filename != nullptr && d->filename[0] != '\0') ? d->filename : nullptr;
+	return (likely(!d->filename.empty())) ? d->filename.c_str() : nullptr;
 }
 
 /** Extra functions **/
@@ -630,7 +629,7 @@ int RpFile::makeWritable(void)
 	RP_D(RpFile);
 	off64_t prev_pos = ftello(d->file);
 	fclose(d->file);
-	d->file = fopen(d->filename, "rb+" FOPEN_CLOEXEC_FLAG);
+	d->file = fopen(d->filename.c_str(), "rb+" FOPEN_CLOEXEC_FLAG);
 	if (d->file) {
 		// File is now writable.
 #if !defined(HAVE_FOPEN_CLOEXEC) && defined(FD_CLOEXEC)
@@ -640,7 +639,7 @@ int RpFile::makeWritable(void)
 	} else {
 		// Failed to open the file as writable.
 		// Try reopening as read-only.
-		d->file = fopen(d->filename, "rb" FOPEN_CLOEXEC_FLAG);
+		d->file = fopen(d->filename.c_str(), "rb" FOPEN_CLOEXEC_FLAG);
 		if (d->file) {
 #if !defined(HAVE_FOPEN_CLOEXEC) && defined(FD_CLOEXEC)
 			d->set_FD_CLOEXEC_flag(d->file, true);

src/librpfile/scsi/RpFile_scsi_win32.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpfile)                        *
  * RpFile_scsi_win32.cpp: Standard file object. (Win32 SCSI)               *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -63,7 +63,7 @@ int RpFile::rereadDeviceSizeOS(off64_t *pDeviceSize, uint32_t *pSectorSize)
 	DWORD w32err = 0;
 
 	RP_D(RpFile);
-	if (!d->filenameW || d->filenameW[0] == _T('\0')) {
+	if (unlikely(d->filenameW.empty())) {
 		// No filename... Assume this isn't a device.
 		return -ENODEV;
 	}

src/win32/RP_ExtractIcon.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (Win32)                            *
  * RP_ExtractIcon.cpp: IExtractIcon implementation.                        *
  *                                                                         *
- * Copyright (c) 2016-2024 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -28,7 +28,6 @@ const CLSID CLSID_RP_ExtractIcon =
 #include "RP_ExtractIcon_p.hpp"
 
 RP_ExtractIcon_Private::RP_ExtractIcon_Private()
-	: olefilename(nullptr)
 {
 	// Enable icon squaring only on Windows XP.
 	// On Windows 7 and 11, it causes the icon to look "squished"
@@ -39,11 +38,6 @@ RP_ExtractIcon_Private::RP_ExtractIcon_Private()
 		// Enable icon squaring.
 		thumbnailer.setDoSquaring(true);
 	}
-}
-
-RP_ExtractIcon_Private::~RP_ExtractIcon_Private()
-{
-	free(olefilename);
 }
 
 /** RP_ExtractIcon **/
@@ -105,28 +99,28 @@ IFACEMETHODIMP RP_ExtractIcon::Load(_In_ LPCOLESTR pszFileName, DWORD dwMode)
 	// the file is not supported.
 	RP_UNUSED(dwMode);	// TODO
 
+	if (unlikely(!pszFileName)) {
+		return E_POINTER;
+	}
+
 	// If we already have a RomData object, unref() it first.
 	RP_D(RP_ExtractIcon);
 	d->romData.reset();
 
 	// pszFileName is the file being worked on.
 	// TODO: If the file was already loaded, don't reload it.
-	free(d->olefilename);
-	d->olefilename = _wcsdup(pszFileName);
-	if (!d->olefilename) {
-		return E_OUTOFMEMORY;
-	}
+	d->olefilename.assign(pszFileName);
 
 	// Check for "bad" file systems.
 	const Config *const config = Config::instance();
-	if (FileSystem::isOnBadFS(d->olefilename, config->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS))) {
+	if (FileSystem::isOnBadFS(d->olefilename.c_str(), config->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS))) {
 		// This file is on a "bad" file system.
 		return S_OK;
 	}
 
 	// If ThumbnailDirectoryPackages is disabled, make sure this is *not* a directory.
 	if (!config->getBoolConfigOption(Config::BoolConfig::Options_ThumbnailDirectoryPackages)) {
-		if (FileSystem::is_directory(d->olefilename)) {
+		if (FileSystem::is_directory(d->olefilename.c_str())) {
 			// It's a directory. Don't thumbnail it.
 			return S_OK;
 		}
@@ -134,7 +128,7 @@ IFACEMETHODIMP RP_ExtractIcon::Load(_In_ LPCOLESTR pszFileName, DWORD dwMode)
 
 	// Get the appropriate RomData class for this ROM file.
 	// RomData class *must* support at least one image type.
-	d->romData = RomDataFactory::create(d->olefilename, RomDataFactory::RDA_HAS_THUMBNAIL);
+	d->romData = RomDataFactory::create(d->olefilename.c_str(), RomDataFactory::RDA_HAS_THUMBNAIL);
 	return S_OK;
 }
 
@@ -157,7 +151,7 @@ IFACEMETHODIMP RP_ExtractIcon::GetCurFile(_In_ LPOLESTR *ppszFileName)
 		return E_POINTER;
 
 	RP_D(const RP_ExtractIcon);
-	if (!d->olefilename) {
+	if (unlikely(d->olefilename.empty())) {
 		// No filename. Create an empty string.
 		LPOLESTR psz = static_cast<LPOLESTR>(CoTaskMemAlloc(sizeof(OLECHAR)));
 		if (!psz) {
@@ -169,13 +163,13 @@ IFACEMETHODIMP RP_ExtractIcon::GetCurFile(_In_ LPOLESTR *ppszFileName)
 	} else {
 		// Copy the filename.
 		// NOTE: Can't use _wcsdup() because we have to allocate memory using CoTaskMemAlloc().
-		const size_t cb = (wcslen(d->olefilename) + 1) * sizeof(OLECHAR);
+		const size_t cb = (d->olefilename.size() + 1) * sizeof(OLECHAR);
 		LPOLESTR psz = static_cast<LPOLESTR>(CoTaskMemAlloc(cb));
 		if (!psz) {
 			*ppszFileName = nullptr;
 			return E_OUTOFMEMORY;
 		}
-		memcpy(psz, d->olefilename, cb);
+		memcpy(psz, d->olefilename.c_str(), cb);
 		*ppszFileName = psz;
 	}
 
@@ -199,7 +193,7 @@ IFACEMETHODIMP RP_ExtractIcon::GetIconLocation(UINT uFlags,
 
 	// If the file wasn't set via IPersistFile::Load(), that's an error.
 	RP_D(RP_ExtractIcon);
-	if (!d->olefilename || d->olefilename[0] == L'\0') {
+	if (unlikely(d->olefilename.empty())) {
 		return E_UNEXPECTED;
 	}
 
@@ -233,7 +227,7 @@ IFACEMETHODIMP RP_ExtractIcon::Extract(_In_ LPCWSTR pszFile, UINT nIconIndex,
 
 	// Make sure a filename was set by calling IPersistFile::Load().
 	RP_D(RP_ExtractIcon);
-	if (!d->olefilename || d->olefilename[0] == L'\0') {
+	if (unlikely(d->olefilename.empty())) {
 		return E_UNEXPECTED;
 	}
 

src/win32/RP_ExtractIcon_Fallback.cpp
@@ -3,7 +3,7 @@
  * RP_ExtractIcon_Fallback.cpp: IExtractIcon implementation.               *
  * Fallback functions for unsupported files.                               *
  *                                                                         *
- * Copyright (c) 2016-2023 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -47,7 +47,7 @@ LONG RP_ExtractIcon_Private::DoExtractIconW(_In_ IExtractIconW *pExtractIconW,
 	}
 
 	// Load the file.
-	hr = pPersistFile->Load(this->olefilename, STGM_READ);
+	hr = pPersistFile->Load(this->olefilename.c_str(), STGM_READ);
 	if (FAILED(hr)) {
 		// Failed to load the file.
 		return ERROR_FILE_NOT_FOUND;
@@ -135,7 +135,7 @@ LONG RP_ExtractIcon_Private::DoExtractIconA(_In_ IExtractIconA *pExtractIconA,
 	}
 
 	// Load the file.
-	hr = pPersistFile->Load(this->olefilename, STGM_READ);
+	hr = pPersistFile->Load(this->olefilename.c_str(), STGM_READ);
 	if (FAILED(hr)) {
 		// Failed to load the file.
 		return ERROR_FILE_NOT_FOUND;
@@ -295,10 +295,10 @@ LONG RP_ExtractIcon_Private::Fallback(HICON *phiconLarge, HICON *phiconSmall, UI
 	// TODO: Check HKCU first.
 
 	// Get the file extension.
-	if (!olefilename || olefilename[0] == L'\0') {
+	if (unlikely(olefilename.empty())) {
 		return ERROR_FILE_NOT_FOUND;
 	}
-	const wchar_t *wfile_ext = FileSystem::file_ext(olefilename);
+	const wchar_t *wfile_ext = FileSystem::file_ext(olefilename.c_str());
 	if (!wfile_ext) {
 		// Invalid or missing file extension.
 		return ERROR_FILE_NOT_FOUND;

commit 5f723215af7a5c2441d54dc8c01cda35e02ba878

[librpfile] RpFile_win32.cpp: Don't incorrectly initialize filename to nullptr.
filename is now an std::string, so this attempts to strcpy() from
nullptr, which crashes.

This fixes a regression from commit 72339ff68b6fd25b127d4dc1bdac75ac4af9bd75.
(Switch back to C++ strings for stored filenames.)

src/librpfile/win32/RpFile_win32.cpp
@@ -43,8 +43,11 @@ DELAYLOAD_TEST_FUNCTION_IMPL0(get_crc_table);
 /** RpFilePrivate **/
 
 RpFilePrivate::RpFilePrivate(RpFile *q, const wchar_t *filenameW, RpFile::FileMode mode)
-	: q_ptr(q), file(INVALID_HANDLE_VALUE), filename(nullptr)
-	, mode(mode), gzfd(nullptr), gzsz(-1)
+	: q_ptr(q)
+	, file(INVALID_HANDLE_VALUE)
+	, mode(mode)
+	, gzfd(nullptr)
+	, gzsz(-1)
 {
 	assert(filenameW != nullptr);
 	this->filenameW.assign(filenameW);