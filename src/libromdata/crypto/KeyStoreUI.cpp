GerbilSoft/rom-properties

commit bffa3b1e275ba9f3a447003b0862079149f755e8

Some more Coverity fixes, mostly related to std::move().
NintendoBadgePrivate::loadImage(): std::move(img48)
- Fixes Coverity #1638283: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)

KeyStoreUI.cpp:
- KeyStoreUIPrivate::importKeysFromBlob(): std::move(new_value)
  - Fixes Coverity #1638276: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)
- KeyStoreUI::setKey(): std::move(convKey)
  - Fixes Coverity #1638268: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)
- KeyStoreUI::setKey(): std::move(new_value)
  - Fixes Coverity #1638264: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)
- KeyStoreUIPrivate::reset(): std::move(value)
  - Fixes Coverity #1638225: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)
- KeyStoreUIPrivate::importN3DSaeskeydb(): std::move(new_value)
  - Fixes Coverity #1638215: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)

gtk/RomDataView_ops.cpp, btnOptions_triggered_signal_handler(): std::move(fullFilename)
- Fixes Coverity #1638258: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)

Xbox360_XDBF.cpp:
- Xbox360_XDBF_Private::addFields_achievements_GPD(): Use std::move(s_gamerscore).
  - Fixes Coverity #1638256: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)
- Xbox360_XDBF_Private::addFields_achievements_GPD(): Use std::move(s_achievement_id).
  - Fixes Coverity #1638209: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)
- Also, use push_back() instead of emplace_back(), since we aren't
  constructing a new object in place.

NintendoDS_BNR.cpp:
- NintendoDS_BNR_Private::loadIcon(): Use std::move(flipimg).
  - Fixes Coverity #1638253: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)
- NintendoDS_BNR_Private::loadIcon(): Use std::move(img).
  - Fixes Coverity #1638246: Variable copied when it could be moved (COPY_INSTEAD_OF_MOVE)

KhronosKTX2Private::loadImage(): Use std::move(flipimg).

src/libromdata/crypto/KeyStoreUI.cpp
@@ -365,9 +365,9 @@ void KeyStoreUIPrivate::reset(void)
 					assert(keyData.length > 0);
 					assert(keyData.length <= 32);
 					if (keyData.key != nullptr && keyData.length > 0 && keyData.length <= 32) {
-						const string value = binToHexStr(keyData.key, keyData.length);
+						string value = binToHexStr(keyData.key, keyData.length);
 						if (pKey->value != value) {
-							pKey->value = value;
+							pKey->value = std::move(value);
 							hasChanged = true;
 						}
 
@@ -529,9 +529,9 @@ KeyStoreUI::ImportReturn KeyStoreUIPrivate::importKeysFromBlob(SectionID sectIdx
 		if (!verifyData) {
 			// Can't verify this key...
 			// Import it anyway.
-			const string new_value = binToHexStr(keyData, 16);
+			string new_value = binToHexStr(keyData, 16);
 			if (pKey->value != new_value) {
-				pKey->value = new_value;
+				pKey->value = std::move(new_value);
 				pKey->status = KeyStoreUI::Key::Status::Unknown;
 				pKey->modified = true;
 				iret.keysImportedNoVerify++;
@@ -1001,7 +1001,8 @@ int KeyStoreUI::setKey(int sectIdx, int keyIdx, const char *value)
 			if (convKey.size() > expected_key_len) {
 				convKey.resize(expected_key_len);
 			}
-			new_value = convKey;
+			new_value = std::move(convKey);
+			new_value = string();
 		}
 	}
 
@@ -1028,7 +1029,7 @@ int KeyStoreUI::setKey(int sectIdx, int keyIdx, const char *value)
 	}
 
 	if (key.value != new_value) {
-		key.value = new_value;
+		key.value = std::move(new_value);
 		key.modified = true;
 		// Verify the key.
 		d->verifyKey(sectIdx, keyIdx);
@@ -1546,9 +1547,9 @@ KeyStoreUI::ImportReturn KeyStoreUIPrivate::importN3DSaeskeydb(IRpFile *file)
 			} else {
 				// Can't verify this key...
 				// Import it anyway.
-				const string new_value = binToHexStr(aesKey->key, sizeof(aesKey->key));
+				string new_value = binToHexStr(aesKey->key, sizeof(aesKey->key));
 				if (pKey->value != new_value) {
-					pKey->value = new_value;
+					pKey->value = std::move(new_value);
 					pKey->status = KeyStoreUI::Key::Status::Unknown;
 					pKey->modified = true;
 					iret.keysImportedNoVerify++;

src/gtk/RomDataView_ops.cpp
@@ -545,7 +545,7 @@ btnOptions_triggered_signal_handler(RpOptionsMenuButton *menuButton,
 
 		// Initial file and directory, based on the current file.
 		// NOTE: Not checking if it's a file or a directory. Assuming it's a file.
-		const string fullFilename = FileSystem::replace_ext(romData->filename(), op->sfi.ext);
+		string fullFilename = FileSystem::replace_ext(romData->filename(), op->sfi.ext);
 		string init_dir, init_name;
 		if (!fullFilename.empty()) {
 			// Split the directory and basename.
@@ -556,7 +556,7 @@ btnOptions_triggered_signal_handler(RpOptionsMenuButton *menuButton,
 				init_dir.assign(fullFilename, 0, slash_pos);
 			} else {
 				// Not a full path. We can only set the filename.
-				init_name = fullFilename;
+				init_name = std::move(fullFilename);
 			}
 		}
 

src/libromdata/Console/Xbox360_XDBF.cpp
@@ -1289,7 +1289,7 @@ int Xbox360_XDBF_Private::addFields_avatarAwards_SPA(void)
 			}
 
 			// TODO: Formatting value indicating that the first line should be bold.
-			data_row.emplace_back(std::move(desc));
+			data_row.push_back(std::move(desc));
 		}
 	}
 
@@ -1457,8 +1457,8 @@ int Xbox360_XDBF_Private::addFields_achievements_GPD(void)
 		vv_icons->push_back(loadImage(be32_to_cpu(pGPD->image_id)));
 
 		// TODO: Localized numeric formatting?
-		const string s_achievement_id = fmt::to_string(be32_to_cpu(pGPD->achievement_id));
-		const string s_gamerscore = fmt::to_string(be32_to_cpu(pGPD->gamerscore));
+		string s_achievement_id = fmt::to_string(be32_to_cpu(pGPD->achievement_id));
+		string s_gamerscore = fmt::to_string(be32_to_cpu(pGPD->gamerscore));
 
 		// Get the strings.
 		const char16_t *pTitle = nullptr, *pUnlockedDesc = nullptr, *pLockedDesc = nullptr;
@@ -1503,10 +1503,10 @@ int Xbox360_XDBF_Private::addFields_achievements_GPD(void)
 		// Add to RFT_LISTDATA.
 		vector<string> data_row;
 		data_row.reserve(3);
-		data_row.push_back(s_achievement_id);
-		data_row.emplace_back(std::move(desc));
-		data_row.push_back(s_gamerscore);
-		vv_xach->emplace_back(std::move(data_row));
+		data_row.push_back(std::move(s_achievement_id));
+		data_row.push_back(std::move(desc));
+		data_row.push_back(std::move(s_gamerscore));
+		vv_xach->push_back(std::move(data_row));
 	}
 
 	// FIXME: Figure out why Dolphin segfaults if the list is empty.

src/libromdata/Handheld/NintendoDS_BNR.cpp
@@ -3,7 +3,7 @@
  * NintendoDS_BNR.cpp: Nintendo DS icon/title data reader.                 *
  * Handles BNR files and icon/title sections.                              *
  *                                                                         *
- * Copyright (c) 2016-2024 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -218,12 +218,12 @@ rp_image_const_ptr NintendoDS_BNR_Private::loadIcon(void)
 						// V-flip
 						flipOp = static_cast<rp_image::FlipOp>(flipOp | rp_image::FLIP_V);
 					}
-					const rp_image_ptr flipimg = img->flip(flipOp);
+					rp_image_ptr flipimg = img->flip(flipOp);
 					if (flipimg && flipimg->isValid()) {
-						img = flipimg;
+						img = std::move(flipimg);
 					}
 				}
-				iconAnimData->frames[bmp_idx] = img;
+				iconAnimData->frames[bmp_idx] = std::move(img);
 				arr_bmpUsed[high_token] = bmp_idx;
 				bmp_idx++;
 			}

src/libromdata/Other/NintendoBadge.cpp
@@ -278,9 +278,9 @@ rp_image_const_ptr NintendoBadgePrivate::loadImage(int idx)
 
 		if (badgeType == BadgeType::CABS) {
 			// Need to crop the 64x64 image to 48x48.
-			const rp_image_ptr img48 = img->resized(48, 48);
+			rp_image_ptr img48 = img->resized(48, 48);
 			if (img48) {
-				img = img48;
+				img = std::move(img48);
 			}
 		}
 	} else {

src/libromdata/RomDataFactory.cpp
@@ -729,7 +729,7 @@ RomDataPtr create(const IRpFilePtr &file, unsigned int attrs)
 	{
 		// Dreamcast .VMI+.VMS pair.
 		// Attempt to open the other file in the pair.
-		const RomDataPtr romData = Private::openDreamcastVMSandVMI(file);
+		RomDataPtr romData = Private::openDreamcastVMSandVMI(file);
 		if (romData) {
 			// .VMI+.VMS pair opened.
 			return romData;

src/librpbase/img/RpPng.cpp
@@ -515,7 +515,7 @@ rp_image_ptr load(IRpFile *file)
 	png_set_read_fn(png_ptr, file, png_io_IRpFile_read);
 
 	// Call the actual PNG image reading function.
-	const rp_image_ptr img = loadPng(png_ptr, info_ptr);
+	rp_image_ptr img = loadPng(png_ptr, info_ptr);
 
 	// Free the PNG structs.
 	png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);

src/librptexture/FileFormatFactory.cpp
@@ -172,7 +172,7 @@ FileFormatPtr create(const IRpFilePtr &file)
 
 		if (fileFormat && fileFormat->isValid()) {
 			// FileFormat subclass obtained.
-			return FileFormatPtr(fileFormat);
+			return fileFormat;
 		}
 	}
 

commit 48c5182296fa97bc56ee17a040638e14fce97dfd

[libromdata] KeyStoreUI: Remove an incorrect line that broke Kanji decoding.
Not sure how this line got here...

This fixes a regression from commit bffa3b1e275ba9f3a447003b0862079149f755e8.
(Some more Coverity fixes, mostly related to std::move().)

src/libromdata/crypto/KeyStoreUI.cpp
@@ -1002,7 +1002,6 @@ int KeyStoreUI::setKey(int sectIdx, int keyIdx, const char *value)
 				convKey.resize(expected_key_len);
 			}
 			new_value = std::move(convKey);
-			new_value = string();
 		}
 	}
 