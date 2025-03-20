GerbilSoft/rom-properties

commit 0fc5333a394711aa27e186f7ebb644700eb652cd

Add some std::(w)string wrappers to a few functions.
Wrappers added for:
- RomDataFactory::create()
- FileSystem::file_ext()
- FileSystem::is_directory()
- FileSystem::isOnBadFS()

src/librpfile/FileSystem.hpp
@@ -242,6 +242,17 @@ static inline int delete_file(const std::string &filename)
 RP_LIBROMDATA_PUBLIC
 const char *file_ext(const char *filename);
 
+/**
+ * Get the file extension from a filename or pathname.
+ * NOTE: Returned value points into the specified filename.
+ * @param filename Filename (UTF-8)
+ * @return File extension, including the leading dot; nullptr if no extension.
+ */
+static inline const char *file_ext(const std::string filename)
+{
+	return file_ext(filename.c_str());
+}
+
 #ifdef _WIN32
 /**
  * Get the file extension from a filename or pathname. (wchar_t version)
@@ -251,6 +262,17 @@ const char *file_ext(const char *filename);
  */
 RP_LIBROMDATA_PUBLIC
 const wchar_t *file_ext(const wchar_t *filename);
+
+/**
+ * Get the file extension from a filename or pathname. (wchar_t version)
+ * NOTE: Returned value points into the specified filename.
+ * @param filename Filename (UTF-16)
+ * @return File extension, including the leading dot; nullptr if no extension.
+ */
+static inline const wchar_t *file_ext(const std::wstring filename)
+{
+	return file_ext(filename.c_str());
+}
 #endif /* _WIN32 */
 
 /**
@@ -321,6 +343,19 @@ std::wstring resolve_symlink(const wchar_t *filename);
 RP_LIBROMDATA_PUBLIC
 bool is_directory(const char *filename);
 
+/**
+ * Check if the specified file is a directory.
+ *
+ * Symbolic links are resolved as per usual directory traversal.
+ *
+ * @param filename Filename to check (UTF-8)
+ * @return True if the file is a directory; false if not.
+ */
+static inline bool is_directory(const std::string &filename)
+{
+	return is_directory(filename.c_str());
+}
+
 #ifdef _WIN32
 /**
  * Check if the specified file is a directory.
@@ -332,6 +367,19 @@ bool is_directory(const char *filename);
  */
 RP_LIBROMDATA_PUBLIC
 bool is_directory(const wchar_t *filename);
+
+/**
+ * Check if the specified file is a directory.
+ *
+ * Symbolic links are resolved as per usual directory traversal.
+ *
+ * @param filename Filename to check (UTF-16)
+ * @return True if the file is a directory; false if not.
+ */
+static inline bool is_directory(const std::wstring &filename)
+{
+	return is_directory(filename.c_str());
+}
 #endif /* _WIN32 */
 
 /**
@@ -348,6 +396,22 @@ bool is_directory(const wchar_t *filename);
 RP_LIBROMDATA_PUBLIC
 bool isOnBadFS(const char *filename, bool allowNetFS = false);
 
+/**
+ * Is a file located on a "bad" file system?
+ *
+ * We don't want to check files on e.g. procfs,
+ * or on network file systems if the option is disabled.
+ *
+ * @param filename Filename (UTF-8)
+ * @param allowNetFS If true, allow network file systems.
+ *
+ * @return True if this file is on a "bad" file system; false if not.
+ */
+static inline bool isOnBadFS(const std::string &filename, bool allowNetFS = false)
+{
+	return isOnBadFS(filename.c_str(), allowNetFS);
+}
+
 #ifdef _WIN32
 /**
  * Is a file located on a "bad" file system?
@@ -362,6 +426,22 @@ bool isOnBadFS(const char *filename, bool allowNetFS = false);
  */
 RP_LIBROMDATA_PUBLIC
 bool isOnBadFS(const wchar_t *filename, bool allowNetFS = false);
+
+/**
+ * Is a file located on a "bad" file system?
+ *
+ * We don't want to check files on e.g. procfs,
+ * or on network file systems if the option is disabled.
+ *
+ * @param filename Filename (UTF-16)
+ * @param allowNetFS If true, allow network file systems.
+ *
+ * @return True if this file is on a "bad" file system; false if not.
+ */
+static inline bool isOnBadFS(const std::wstring &filename, bool allowNetFS = false)
+{
+	return isOnBadFS(filename.c_str(), allowNetFS);
+}
 #endif /* _WIN32 */
 
 /**

src/kde/RpQUrl.cpp
@@ -148,7 +148,7 @@ IRpFilePtr openQUrl(const QUrl &url, bool isThumbnail)
 		const bool enableThumbnailOnNetworkFS = config->getBoolConfigOption(Config::BoolConfig::Options_EnableThumbnailOnNetworkFS);
 		if (!s_local_filename.empty()) {
 			// This is a local file. Check if it's on a "bad" file system.
-			if (FileSystem::isOnBadFS(s_local_filename.c_str(), enableThumbnailOnNetworkFS)) {
+			if (FileSystem::isOnBadFS(s_local_filename, enableThumbnailOnNetworkFS)) {
 				// This file is on a "bad" file system.
 				return {};
 			}

src/kde/plugins/ExtractorPlugin.cpp
@@ -210,7 +210,7 @@ void ExtractorPlugin::extract(ExtractionResult *result)
 	// Check if this is a directory.
 	const QUrl localUrl = localizeQUrl(inputUrl);
 	const string s_local_filename = localUrl.toLocalFile().toUtf8().constData();
-	if (unlikely(!s_local_filename.empty() && FileSystem::is_directory(s_local_filename.c_str()))) {
+	if (unlikely(!s_local_filename.empty() && FileSystem::is_directory(s_local_filename))) {
 		const Config *const config = Config::instance();
 		if (!config->getBoolConfigOption(Config::BoolConfig::Options_ThumbnailDirectoryPackages)) {
 			// Directory package thumbnailing is disabled.
@@ -218,7 +218,7 @@ void ExtractorPlugin::extract(ExtractionResult *result)
 		}
 
 		// Directory: Call RomDataFactory::create() with the filename.
-		romData = RomDataFactory::create(s_local_filename.c_str());
+		romData = RomDataFactory::create(s_local_filename);
 	} else {
 		// File: Open the file and call RomDataFactory::create() with the opened file.
 		IRpFilePtr file(openQUrl(localUrl, false));

src/kde/plugins/RomPropertiesDialogPlugin.cpp
@@ -69,7 +69,7 @@ RomDataView *RomPropertiesDialogPlugin::createRomDataView(const KFileItem &fileI
 		}
 
 		if (likely(!s_local_filename.empty())) {
-			romData = RomDataFactory::create(s_local_filename.c_str());
+			romData = RomDataFactory::create(s_local_filename);
 		}
 	}
 

src/kde/plugins/RomThumbCreator.cpp
@@ -94,7 +94,7 @@ bool RomThumbCreator::create(const QString &path, int width, int height, QImage 
 		}
 
 		// Directory: Call RomDataFactory::create() with the filename.
-		romData = RomDataFactory::create(s_filename.c_str());
+		romData = RomDataFactory::create(s_filename);
 	} else {
 		// File: Open the file and call RomDataFactory::create() with the opened file.
 

src/kde/plugins/RomThumbnailCreator.cpp
@@ -56,7 +56,7 @@ KIO::ThumbnailResult RomThumbnailCreator::create(const KIO::ThumbnailRequest &re
 	// Check if this is a directory.
 	const QUrl localUrl = localizeQUrl(url);
 	const string s_local_filename = localUrl.toLocalFile().toUtf8().constData();
-	if (unlikely(!s_local_filename.empty() && FileSystem::is_directory(s_local_filename.c_str()))) {
+	if (unlikely(!s_local_filename.empty() && FileSystem::is_directory(s_local_filename))) {
 		const Config *const config = Config::instance();
 		if (!config->getBoolConfigOption(Config::BoolConfig::Options_ThumbnailDirectoryPackages)) {
 			// Directory package thumbnailing is disabled.
@@ -64,7 +64,7 @@ KIO::ThumbnailResult RomThumbnailCreator::create(const KIO::ThumbnailRequest &re
 		}
 
 		// Directory: Call RomDataFactory::create() with the filename.
-		romData = RomDataFactory::create(s_local_filename.c_str());
+		romData = RomDataFactory::create(s_local_filename);
 	} else {
 		// File: Open the file and call RomDataFactory::create() with the opened file.
 

src/kde/rp_create_thumbnail.cpp
@@ -59,7 +59,7 @@ Q_DECL_EXPORT int RP_C_API rp_create_thumbnail2(const char *source_file, const c
 	// Check if this is a directory.
 	const QUrl localUrl = localizeQUrl(QUrl(QString::fromUtf8(source_file)));
 	const string s_local_filename = localUrl.toLocalFile().toUtf8().constData();
-	if (unlikely(!s_local_filename.empty() && FileSystem::is_directory(s_local_filename.c_str()))) {
+	if (unlikely(!s_local_filename.empty() && FileSystem::is_directory(s_local_filename))) {
 		const Config *const config = Config::instance();
 		if (!config->getBoolConfigOption(Config::BoolConfig::Options_ThumbnailDirectoryPackages)) {
 			// Directory package thumbnailing is disabled.
@@ -67,7 +67,7 @@ Q_DECL_EXPORT int RP_C_API rp_create_thumbnail2(const char *source_file, const c
 		}
 
 		// Directory: Call RomDataFactory::create() with the filename.
-		romData = RomDataFactory::create(s_local_filename.c_str());
+		romData = RomDataFactory::create(s_local_filename);
 	} else {
 		// File: Open the file and call RomDataFactory::create() with the opened file.
 

src/libromdata/RomDataFactory.hpp
@@ -95,6 +95,30 @@ LibRpBase::RomDataPtr create(const LibRpFile::IRpFilePtr &file, unsigned int att
 RP_LIBROMDATA_PUBLIC
 LibRpBase::RomDataPtr create(const char *filename, unsigned int attrs = 0);
 
+/**
+ * Create a RomData subclass for the specified ROM file.
+ *
+ * This version creates a base RpFile for the RomData object.
+ * It does not support extended virtual filesystems like GVfs
+ * or KIO, but it does support directories.
+ *
+ * NOTE: RomData::isValid() is checked before returning a
+ * created RomData instance, so returned objects can be
+ * assumed to be valid as long as they aren't nullptr.
+ *
+ * If imgbf is non-zero, at least one of the specified image
+ * types must be supported by the RomData subclass in order to
+ * be returned.
+ *
+ * @param filename ROM filename (UTF-8)
+ * @param attrs RomDataAttr bitfield. If set, RomData subclass must have the specified attributes.
+ * @return RomData subclass, or nullptr if the ROM isn't supported.
+ */
+static inline LibRpBase::RomDataPtr create(const std::string &filename, unsigned int attrs = 0)
+{
+	return create(filename.c_str(), attrs);
+}
+
 #if defined(_WIN32) && defined(_UNICODE)
 /**
  * Create a RomData subclass for the specified ROM file.
@@ -117,6 +141,30 @@ LibRpBase::RomDataPtr create(const char *filename, unsigned int attrs = 0);
  */
 RP_LIBROMDATA_PUBLIC
 LibRpBase::RomDataPtr create(const wchar_t *filename, unsigned int attrs = 0);
+
+/**
+ * Create a RomData subclass for the specified ROM file.
+ *
+ * This version creates a base RpFile for the RomData object.
+ * It does not support extended virtual filesystems like GVfs
+ * or KIO, but it does support directories.
+ *
+ * NOTE: RomData::isValid() is checked before returning a
+ * created RomData instance, so returned objects can be
+ * assumed to be valid as long as they aren't nullptr.
+ *
+ * If imgbf is non-zero, at least one of the specified image
+ * types must be supported by the RomData subclass in order to
+ * be returned.
+ *
+ * @param filename ROM filename (UTF-16)
+ * @param attrs RomDataAttr bitfield. If set, RomData subclass must have the specified attributes.
+ * @return RomData subclass, or nullptr if the ROM isn't supported.
+ */
+static inline LibRpBase::RomDataPtr create(const std::wstring &filename, unsigned int attrs = 0)
+{
+	return create(filename.c_str(), attrs);
+}
 #endif /* _WIN32 && _UNICODE */
 
 #ifdef ROMDATAFACTORY_USE_FILE_EXTENSIONS

commit 501db35dad9c68f298b8a2cb446f8d3c28324af1

[librpfile] FileSystem: file_ext() (w)string wrpapers need to take `const std::(w)string&` by reference.
Otherwise, the function will return a dangling pointer.

This fixes a regression from commit 0fc5333a394711aa27e186f7ebb644700eb652cd.
(Add some std::(w)string wrappers to a few functions.)

src/librpfile/FileSystem.hpp
@@ -248,7 +248,7 @@ const char *file_ext(const char *filename);
  * @param filename Filename (UTF-8)
  * @return File extension, including the leading dot; nullptr if no extension.
  */
-static inline const char *file_ext(const std::string filename)
+static inline const char *file_ext(const std::string &filename)
 {
 	return file_ext(filename.c_str());
 }
@@ -269,7 +269,7 @@ const wchar_t *file_ext(const wchar_t *filename);
  * @param filename Filename (UTF-16)
  * @return File extension, including the leading dot; nullptr if no extension.
  */
-static inline const wchar_t *file_ext(const std::wstring filename)
+static inline const wchar_t *file_ext(const std::wstring &filename)
 {
 	return file_ext(filename.c_str());
 }