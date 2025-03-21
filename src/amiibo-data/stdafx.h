734a72ca78813e90ff53a8861dbe7ffaa92fac72
David Korth <gerbilsoft@gerbilsoft.com>
2025-03-20 20:49:11 -0400

    MSVC: Set /DEPENDENTLOADFLAG:0x800 to restrict DLL loading to SYSTEM32 on startup.
    On Windows 10 1607+, this flag is equivalent to calling the
    SetDefaultDllDirectories() function before the program starts.
    
    We're already calling SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_SYSTEM32)
    in librpsecure/win32/restrict-dll.c, but this ensures that it's set even
    before the program starts.
    
    Related changes:
    
    - amiiboc: Use delay-loading for libfmt. Otherwise, it fails with
      error code 0xC0000135.
      - Added a dummy stdafx.h file, required by DelayLoadHelper.c.
    
    - All tests: Set /DEPENDENTLOADFLAG:0xA00 because gtest cannot be
      delay-loaded.
    
    - msvc.cmake: Fix linker flags for RelWithDebInfo.
    
    TODO:
    - EXE: Show DEPENDENTLOADFLAG.
    - Test svrplus.

---
+++ src/amiibo-data/stdafx.h
@@ -0,0 +1,11 @@
+/***************************************************************************
+ * ROM Properties Page shell extension. (amiibo-data)                      *
+ * stdafx.h: Common definitions and includes.                              *
+ *                                                                         *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#pragma once
+
+// NOTE: Dummy file for DelayLoadHelper.c.
