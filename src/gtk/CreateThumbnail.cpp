GerbilSoft/rom-properties

commit e8a8cf8ba30d7bb5eb22ccd7ab3fdfc361529438

Allocate some RpPngWriters on the stack instead of using unique_ptr<>.
Stack allocation makes sense here, since RpPngWriter uses the "pimpl"
method, so the actual RpPngWriter object is only 1 pointer.

Code size difference: (64-bit Gentoo Linux, gcc-14.2.1_p20250301, release build, no LTO)

   text    data     bss     dec     hex filename
   5498     128       0    5626    15fa RpPng.cpp.o [before]
   5423     128       0    5551    15af RpPng.cpp.o [after]
    -75       0       0     -75     -4b Difference

RpPng.cpp has 6 RpPngWriter invocations, so I only checked this file.

NOTE: Some unique_ptr<RpPngWriter> uses were not changed, since we
can't easily switch between the two types (static vs. animated)
without using a pointer.

src/gtk/CreateThumbnail.cpp
@@ -418,10 +418,10 @@ G_MODULE_EXPORT int RP_C_API rp_create_thumbnail2(
 	// gdk-pixbuf doesn't support CI8, so we'll assume all
 	// images are ARGB32. (Well, ABGR32, but close enough.)
 	// TODO: Verify channels, etc.?
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(output_file,
+	RpPngWriter pngWriter(output_file,
 		outParams.thumbSize.width, outParams.thumbSize.height,
 		rp_image::Format::ARGB32));
-	if (!pngWriter->isOpen()) {
+	if (!pngWriter.isOpen()) {
 		// Could not open the PNG writer.
 		ret = RPCT_ERROR_OUTPUT_FILE_FAILED;
 		goto cleanup;
@@ -497,13 +497,13 @@ G_MODULE_EXPORT int RP_C_API rp_create_thumbnail2(
 	}
 
 	// Write the tEXt chunks.
-	pngWriter->write_tEXt(kv);
+	pngWriter.write_tEXt(kv);
 
 	/** IHDR **/
 
 	// If sBIT wasn't found, all fields will be 0.
 	// RpPngWriter will ignore sBIT in this case.
-	pwRet = pngWriter->write_IHDR(&outParams.sBIT);
+	pwRet = pngWriter.write_IHDR(&outParams.sBIT);
 	if (pwRet != 0) {
 		// Error writing IHDR.
 		// TODO: Unlink the PNG image.
@@ -550,7 +550,7 @@ G_MODULE_EXPORT int RP_C_API rp_create_thumbnail2(
 	// GdkPixbuf use ABGR32.
 	static constexpr bool is_abgr = true;
 #endif
-	pwRet = pngWriter->write_IDAT(row_pointers.get(), is_abgr);
+	pwRet = pngWriter.write_IDAT(row_pointers.get(), is_abgr);
 #ifdef RP_GTK_USE_GDKTEXTURE
 	g_free(texdata);
 #endif /* RP_GTK_USE_GDKTEXTURE */

src/kde/rp_create_thumbnail.cpp
@@ -127,9 +127,9 @@ Q_DECL_EXPORT int RP_C_API rp_create_thumbnail2(const char *source_file, const c
 			return RPCT_ERROR_OUTPUT_FILE_FAILED;
 	}
 
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(output_file,
-		outParams.retImg.width(), height, format));
-	if (!pngWriter->isOpen()) {
+	RpPngWriter pngWriter(output_file,
+		outParams.retImg.width(), height, format);
+	if (!pngWriter.isOpen()) {
 		// Could not open the PNG writer.
 		return RPCT_ERROR_OUTPUT_FILE_FAILED;
 	}
@@ -186,7 +186,7 @@ Q_DECL_EXPORT int RP_C_API rp_create_thumbnail2(const char *source_file, const c
 	}
 
 	// Write the tEXt chunks.
-	pngWriter->write_tEXt(kv);
+	pngWriter.write_tEXt(kv);
 
 	/** IHDR **/
 
@@ -197,7 +197,7 @@ Q_DECL_EXPORT int RP_C_API rp_create_thumbnail2(const char *source_file, const c
 
 	// If sBIT wasn't found, all fields will be 0.
 	// RpPngWriter will ignore sBIT in this case.
-	int pwRet = pngWriter->write_IHDR(&outParams.sBIT,
+	int pwRet = pngWriter.write_IHDR(&outParams.sBIT,
 		colorTable.constData(), colorTable.size());
 	if (pwRet != 0) {
 		// Error writing IHDR.
@@ -216,7 +216,7 @@ Q_DECL_EXPORT int RP_C_API rp_create_thumbnail2(const char *source_file, const c
 	}
 
 	// Write the IDAT section.
-	pwRet = pngWriter->write_IDAT(row_pointers.get());
+	pwRet = pngWriter.write_IDAT(row_pointers.get());
 	if (pwRet != 0) {
 		// Error writing IDAT.
 		// TODO: Unlink the PNG image.

src/librpbase/img/RpPng.cpp
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librpbase)                        *
  * RpPng.cpp: PNG image handler.                                           *
  *                                                                         *
- * Copyright (c) 2016-2024 by David Korth.                                 *
+ * Copyright (c) 2016-2025 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -541,17 +541,19 @@ int save(const IRpFilePtr &file, const rp_image_const_ptr &img)
 		return -EINVAL;
 
 	// Create a PNG writer.
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(file, img));
-	if (!pngWriter->isOpen())
-		return -pngWriter->lastError();
+	RpPngWriter pngWriter(file, img);
+	if (!pngWriter.isOpen()) {
+		return -pngWriter.lastError();
+	}
 
 	// Write the PNG IHDR.
-	int ret = pngWriter->write_IHDR();
-	if (ret != 0)
+	int ret = pngWriter.write_IHDR();
+	if (ret != 0) {
 		return ret;
+	}
 
 	// Write the PNG image data.
-	return pngWriter->write_IDAT();
+	return pngWriter.write_IDAT();
 }
 
 /**
@@ -570,17 +572,19 @@ int save(const char *filename, const rp_image_const_ptr &img)
 		return -EINVAL;
 
 	// Create a PNG writer.
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(filename, img));
-	if (!pngWriter->isOpen())
-		return -pngWriter->lastError();
+	RpPngWriter pngWriter(filename, img);
+	if (!pngWriter.isOpen()) {
+		return -pngWriter.lastError();
+	}
 
 	// Write the PNG IHDR.
-	int ret = pngWriter->write_IHDR();
-	if (ret != 0)
+	int ret = pngWriter.write_IHDR();
+	if (ret != 0) {
 		return ret;
+	}
 
 	// Write the PNG image data.
-	return pngWriter->write_IDAT();
+	return pngWriter.write_IDAT();
 }
 
 #ifdef _WIN32
@@ -600,17 +604,19 @@ int save(const wchar_t *filename, const rp_image_const_ptr &img)
 		return -EINVAL;
 
 	// Create a PNG writer.
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(filename, img));
-	if (!pngWriter->isOpen())
-		return -pngWriter->lastError();
+	RpPngWriter pngWriter(filename, img);
+	if (!pngWriter.isOpen()) {
+		return -pngWriter.lastError();
+	}
 
 	// Write the PNG IHDR.
-	int ret = pngWriter->write_IHDR();
-	if (ret != 0)
+	int ret = pngWriter.write_IHDR();
+	if (ret != 0) {
 		return ret;
+	}
 
 	// Write the PNG image data.
-	return pngWriter->write_IDAT();
+	return pngWriter.write_IDAT();
 }
 #endif /* _WIN32 */
 
@@ -637,21 +643,24 @@ int save(const IRpFilePtr &file, const IconAnimDataConstPtr &iconAnimData)
 {
 	assert((bool)file);
 	assert((bool)iconAnimData);
-	if (!file || !file->isOpen() || !iconAnimData)
+	if (!file || !file->isOpen() || !iconAnimData) {
 		return -EINVAL;
+	}
 
 	// Create a PNG writer.
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(file, iconAnimData));
-	if (!pngWriter->isOpen())
-		return -pngWriter->lastError();
+	RpPngWriter pngWriter(file, iconAnimData);
+	if (!pngWriter.isOpen()) {
+		return -pngWriter.lastError();
+	}
 
 	// Write the PNG IHDR.
-	int ret = pngWriter->write_IHDR();
-	if (ret != 0)
+	int ret = pngWriter.write_IHDR();
+	if (ret != 0) {
 		return ret;
+	}
 
 	// Write the PNG image data.
-	return pngWriter->write_IDAT();
+	return pngWriter.write_IDAT();
 }
 
 /**
@@ -675,21 +684,24 @@ int save(const char *filename, const IconAnimDataConstPtr &iconAnimData)
 	assert(filename != nullptr);
 	assert(filename[0] != '\0');
 	assert((bool)iconAnimData);
-	if (!filename || filename[0] == '\0' || !iconAnimData)
+	if (!filename || filename[0] == '\0' || !iconAnimData) {
 		return -EINVAL;
+	}
 
 	// Create a PNG writer.
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(filename, iconAnimData));
-	if (!pngWriter->isOpen())
-		return -pngWriter->lastError();
+	RpPngWriter pngWriter(filename, iconAnimData);
+	if (!pngWriter.isOpen()) {
+		return -pngWriter.lastError();
+	}
 
 	// Write the PNG IHDR.
-	int ret = pngWriter->write_IHDR();
-	if (ret != 0)
+	int ret = pngWriter.write_IHDR();
+	if (ret != 0) {
 		return ret;
+	}
 
 	// Write the PNG image data.
-	return pngWriter->write_IDAT();
+	return pngWriter.write_IDAT();
 }
 
 #ifdef _WIN32
@@ -714,21 +726,24 @@ int save(const wchar_t *filename, const IconAnimDataConstPtr &iconAnimData)
 	assert(filename != nullptr);
 	assert(filename[0] != L'\0');
 	assert((bool)iconAnimData);
-	if (!filename || filename[0] == L'\0' || !iconAnimData)
+	if (!filename || filename[0] == L'\0' || !iconAnimData) {
 		return -EINVAL;
+	}
 
 	// Create a PNG writer.
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(filename, iconAnimData));
-	if (!pngWriter->isOpen())
-		return -pngWriter->lastError();
+	RpPngWriter pngWriter(filename, iconAnimData);
+	if (!pngWriter.isOpen()) {
+		return -pngWriter.lastError();
+	}
 
 	// Write the PNG IHDR.
-	int ret = pngWriter->write_IHDR();
-	if (ret != 0)
+	int ret = pngWriter.write_IHDR();
+	if (ret != 0) {
 		return ret;
+	}
 
 	// Write the PNG image data.
-	return pngWriter->write_IDAT();
+	return pngWriter.write_IDAT();
 }
 #endif /* _WIN32 */
 

src/win32/RP_ContextMenu.cpp
@@ -130,9 +130,9 @@ int RP_ContextMenu_Private::convert_to_png(LPCTSTR source_filename)
 	// tEXt chunks
 	RpPngWriter::kv_vector kv;
 
-	unique_ptr<RpPngWriter> pngWriter(new RpPngWriter(output_filename.get(),
-		img->width(), height, img->format()));
-	if (!pngWriter->isOpen()) {
+	RpPngWriter pngWriter(output_filename.get(),
+		img->width(), height, img->format());
+	if (!pngWriter.isOpen()) {
 		// Could not open the PNG writer.
 		return RPCT_ERROR_OUTPUT_FILE_FAILED;
 	}
@@ -143,7 +143,7 @@ int RP_ContextMenu_Private::convert_to_png(LPCTSTR source_filename)
 	kv.emplace_back("Software", "ROM Properties Page shell extension (Win32)");
 
 	// Write the tEXt chunks.
-	pngWriter->write_tEXt(kv);
+	pngWriter.write_tEXt(kv);
 
 	/** IHDR **/
 
@@ -153,7 +153,7 @@ int RP_ContextMenu_Private::convert_to_png(LPCTSTR source_filename)
 	if (img->get_sBIT(&sBIT) != 0) {
 		memset(&sBIT, 0, sizeof(sBIT));
 	}
-	int pwRet = pngWriter->write_IHDR(&sBIT,
+	int pwRet = pngWriter.write_IHDR(&sBIT,
 		img->palette(), img->palette_len());
 	if (pwRet != 0) {
 		// Error writing IHDR.
@@ -172,7 +172,7 @@ int RP_ContextMenu_Private::convert_to_png(LPCTSTR source_filename)
 	}
 
 	// Write the IDAT section.
-	pwRet = pngWriter->write_IDAT(row_pointers.get());
+	pwRet = pngWriter.write_IDAT(row_pointers.get());
 	if (pwRet != 0) {
 		// Error writing IDAT.
 		// TODO: Unlink the PNG image.

commit d32f201c073c2a1b4f44ccb0086ddd4415359b06

[gtk] CreateThumbnail.cpp: -')'
This fixes a regression from commit e8a8cf8ba30d7bb5eb22ccd7ab3fdfc361529438.
(Allocate some RpPngWriters on the stack instead of using unique_ptr<>.)

src/gtk/CreateThumbnail.cpp
@@ -420,7 +420,7 @@ G_MODULE_EXPORT int RP_C_API rp_create_thumbnail2(
 	// TODO: Verify channels, etc.?
 	RpPngWriter pngWriter(output_file,
 		outParams.thumbSize.width, outParams.thumbSize.height,
-		rp_image::Format::ARGB32));
+		rp_image::Format::ARGB32);
 	if (!pngWriter.isOpen()) {
 		// Could not open the PNG writer.
 		ret = RPCT_ERROR_OUTPUT_FILE_FAILED;