9b3c7a2e95f8fad801ccbe9c9d162c865c65bd37
David Korth <gerbilsoft@gerbilsoft.com>
2025-03-29 17:46:31 -0400

    [librpcpuid] cpuflags_arm.c: Add a Linux check for ARM NEON using getauxval(AT_HWCAP).
    FIXME: Might not be correct (or available) on Android. Needs testing!

--- src/librpcpuid/cpuflags_arm.c
+++ src/librpcpuid/cpuflags_arm.c
@@ -10,6 +10,11 @@
 #if !defined(RP_CPU_ARM) && !defined(RP_CPU_ARM64)
 #  error Do not compile cpuflags_arm.c on non-ARM CPUs!
 #endif
+#include <stdio.h>
+#ifdef __linux__
+#  include <sys/auxv.h>
+#  include <asm/hwcap.h>
+#endif /* __linux__ */
 
 // librpthreads
 // FIXME: Cannot use librpthreads at the moment due to static linkage.
@@ -29,9 +34,20 @@ static void RP_CPU_Flags_arm_Init_int(void)
 	// Windows on ARM also always has NEON. (desktop Windows, e.g. Windows RT [Win8])
 	RP_CPU_Flags_arm = RP_CPUFLAG_ARM_NEON;
 #elif defined(RP_CPU_ARM)
-	// TODO: Detect ARM NEON.
-	// OS-specific stuff goes here.
+	// Initialize the CPU flags variable.
 	RP_CPU_Flags_arm = 0;
+
+	// Detect ARM NEON.
+#  ifdef __linux__
+	// Linux: Check HWCAP.
+	// FIXME: Might not be correct (or available) on Android. Needs testing!
+	const unsigned long hwcap = getauxval(AT_HWCAP);
+	if (hwcap & HWCAP_NEON) {
+		RP_CPU_Flags_arm |= RP_CPUFLAG_ARM_NEON;
+	}
+#  else
+#    warning No CPU flag detection for this OS on ARM
+#  endif
 #endif
 
 	// CPU flags initialized.
