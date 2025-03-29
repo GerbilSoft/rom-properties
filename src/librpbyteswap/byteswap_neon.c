7d5dd140fa04332ebf3ad1264378858a9dd83e78
David Korth <gerbilsoft@gerbilsoft.com>
2025-03-29 18:40:25 -0400

    [librpbyteswap] byteswap_neon.c: Use uint8x8_t on 32-bit ARM.
    It seems 32-bit ARM NEON doesn't have 128-bit vtbl instructions.
    
    TODO: Figure out if this is in fact the case, and it's not just
    MSVC rejecting the intrinsics for 32-bit ARM.
    
    TODO: Actually test the 32-bit ARM version.
    
    TODO: uint8x8x4_t for 32-bit, and uint8x16x2_t for 64-bit?

--- src/librpbyteswap/byteswap_neon.c
+++ src/librpbyteswap/byteswap_neon.c
@@ -15,6 +15,11 @@
 // ARM NEON intrinsics
 #include <arm_neon.h>
 
+// FIXME: MSVC for 32-bit ARM is missing 128-bit vtbl intrinsics.
+// I'm not sure if it's an MSVC issue or if 32-bit ARM NEON doesn't have it...
+// TODO: Test the 32-bit ARM NEON code.
+// TODO: uint8x8x4_t for 32-bit, and uint8x16x2_t for 64-bit?
+
 /**
  * 16-bit byteswap function.
  * NEON-optimized version.
@@ -29,8 +34,13 @@ void RP_C_API rp_byte_swap_16_array_neon(uint16_t *ptr, size_t n)
 	assert((n & 1) == 0);
 	n &= ~1;
 
+#ifdef RP_CPU_ARM
+	static const uint8_t shuf_mask_u8[8] = {1,0, 3,2, 5,4, 7,6};
+	uint8x8_t shuf_mask = vld1_u8(shuf_mask_u8);
+#else /* RP_CPU_ARM64 */
 	static const uint8_t shuf_mask_u8[16] = {1,0, 3,2, 5,4, 7,6, 9,8, 11,10, 13,12, 15,14};
 	uint8x16_t shuf_mask = vld1q_u8(shuf_mask_u8);
+#endif
 
 	// TODO: Don't bother with ARM64 vector intrinsics if n is below a certain size?
 
@@ -42,6 +52,22 @@ void RP_C_API rp_byte_swap_16_array_neon(uint16_t *ptr, size_t n)
 
 	// Process 16 WORDs per iteration using ARM64 vector intrinsics.
 	for (; n >= 32; n -= 32, ptr += 16) {
+#ifdef RP_CPU_ARM
+		uint8x8_t vec0 = vld1_u16(ptr);
+		uint8x8_t vec1 = vld1_u16(ptr+4);
+		uint8x8_t vec2 = vld1_u16(ptr+8);
+		uint8x8_t vec3 = vld1_u16(ptr+12);
+
+		vec0 = vtbl1_u8(vec0, shuf_mask);
+		vec1 = vtbl1_u8(vec1, shuf_mask);
+		vec2 = vtbl1_u8(vec2, shuf_mask);
+		vec3 = vtbl1_u8(vec3, shuf_mask);
+
+		vst1_u16(ptr, vec0);
+		vst1_u16(ptr+4, vec1);
+		vst1_u16(ptr+8, vec2);
+		vst1_u16(ptr+12, vec3);
+#else /* RP_CPU_ARM64 */
 		uint8x16_t vec0 = vld1q_u16(ptr);
 		uint8x16_t vec1 = vld1q_u16(ptr+8);
 
@@ -50,6 +76,7 @@ void RP_C_API rp_byte_swap_16_array_neon(uint16_t *ptr, size_t n)
 
 		vst1q_u16(ptr, vec0);
 		vst1q_u16(ptr+8, vec1);
+#endif
 	}
 
 	// Process the remaining data, one WORD at a time.
@@ -72,8 +99,13 @@ void RP_C_API rp_byte_swap_32_array_neon(uint32_t *ptr, size_t n)
 	assert((n & 3) == 0);
 	n &= ~3;
 
+#ifdef RP_CPU_ARM
+	static const uint8_t shuf_mask_u8[8] = {3,2,1,0, 7,6,5,4};
+	uint8x8_t shuf_mask = vld1_u8(shuf_mask_u8);
+#else /* RP_CPU_ARM64 */
 	static const uint8_t shuf_mask_u8[16] = {3,2,1,0, 7,6,5,4, 11,10,9,8, 15,14,13,12};
 	uint8x16_t shuf_mask = vld1q_u8(shuf_mask_u8);
+#endif
 
 	// TODO: Don't bother with ARM64 vector intrinsics if n is below a certain size?
 
@@ -85,6 +117,22 @@ void RP_C_API rp_byte_swap_32_array_neon(uint32_t *ptr, size_t n)
 
 	// Process 8 DWORDs per iteration using ARM64 vector intrinsics.
 	for (; n >= 32; n -= 32, ptr += 8) {
+#ifdef RP_CPU_ARM
+		uint8x8_t vec0 = vld1_u32(ptr);
+		uint8x8_t vec1 = vld1_u32(ptr+2);
+		uint8x8_t vec2 = vld1_u32(ptr+4);
+		uint8x8_t vec3 = vld1_u32(ptr+6);
+
+		vec0 = vtbl1_u8(vec0, shuf_mask);
+		vec1 = vtbl1_u8(vec1, shuf_mask);
+		vec2 = vtbl1_u8(vec2, shuf_mask);
+		vec3 = vtbl1_u8(vec3, shuf_mask);
+
+		vst1_u32(ptr, vec0);
+		vst1_u32(ptr+2, vec1);
+		vst1_u32(ptr+4, vec2);
+		vst1_u32(ptr+6, vec3);
+#else /* RP_CPU_ARM64 */
 		uint8x16_t vec0 = vld1q_u32(ptr);
 		uint8x16_t vec1 = vld1q_u32(ptr+4);
 
@@ -93,6 +141,7 @@ void RP_C_API rp_byte_swap_32_array_neon(uint32_t *ptr, size_t n)
 
 		vst1q_u32(ptr, vec0);
 		vst1q_u32(ptr+4, vec1);
+#endif
 	}
 
 	// Process the remaining data, one DWORD at a time.
