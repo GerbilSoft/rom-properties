ea09088e89c69fc5f056be57d24e826bbf2772c4
David Korth <gerbilsoft@gerbilsoft.com>
2025-04-01 02:54:20 -0400

    Add an internal copy of PugiXML and remove TinyXML2 completely.
    TODO:
    - Add the 'd' postfix to the debug DLL.
    - Configure PugiXML options and hide them from the user, since these are
      configured by rom-properties.

---
+++ cmake/libs/CheckPugiXML.cmake
@@ -0,0 +1,58 @@
+# Check for PugiXML.
+# If PugiXML isn't found, extlib/tinyxml2/ will be used instead.
+
+IF(ENABLE_XML)
+
+IF(NOT USE_INTERNAL_XML)
+	IF(PugiXML_LIBRARY MATCHES "^pugixml")
+		# Internal PugiXML was previously in use.
+		UNSET(XML_FOUND)
+		UNSET(HAVE_XML)
+		UNSET(PugiXML_LIBRARY CACHE)
+	ENDIF()
+
+	# Check for PugiXML.
+	FIND_PACKAGE(pugixml)
+	IF(pugixml_FOUND)
+		# Found system PugiXML.
+		SET(HAVE_XML 1)
+		# NOTE: PugiXML's CMake configuration files don't set pugixml_LIBRARY.
+		SET(pugixml_LIBRARY pugixml::pugixml CACHE INTERNAL "PugiXML library" FORCE)
+	ELSE()
+		# System PugiXML was not found.
+		MESSAGE(STATUS "Using the internal copy of PugiXML since a system version was not found.")
+		SET(USE_INTERNAL_XML ON CACHE BOOL "Use the internal copy of PugiXML" FORCE)
+	ENDIF()
+ELSE()
+	MESSAGE(STATUS "Using the internal copy of PugiXML.")
+ENDIF(NOT USE_INTERNAL_XML)
+
+IF(USE_INTERNAL_XML)
+	# Using the internal PugiXML library.
+	# NOTE: The tinyxml2 target has implicit include directories,
+	# so we don't need to set the variables.
+	SET(pugixml_FOUND 1)
+	SET(HAVE_XML 1)
+	IF(WIN32)
+		# Using DLLs on Windows.
+		# TODO: Build a dylib for Mac OS X.
+		SET(USE_INTERNAL_XML_DLL ON)
+	ELSE()
+		# Using static linking on other systems.
+		SET(USE_INTERNAL_XML_DLL OFF)
+	ENDIF()
+	# FIXME: Verify this on v1.10 and older. (Ubuntu 16.04 has v1.7.)
+	SET(pugixml_LIBRARY pugixml::pugixml CACHE INTERNAL "PugiXML library" FORCE)
+	SET(pugixml_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/extlib/pugixml/src")
+ELSE(USE_INTERNAL_XML)
+	SET(USE_INTERNAL_XML_DLL OFF)
+ENDIF(USE_INTERNAL_XML)
+
+ELSE(ENABLE_XML)
+	# Disable PugiXML.
+	UNSET(pugixml_FOUND)
+	UNSET(HAVE_XML)
+	UNSET(USE_INTERNAL_XML)
+	UNSET(USE_INTERNAL_XML_DLL)
+	UNSET(pugixml_LIBRARY)
+ENDIF(ENABLE_XML)
