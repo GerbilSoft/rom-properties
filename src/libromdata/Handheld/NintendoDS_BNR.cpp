GerbilSoft/rom-properties

commit f5d34d97cb6d84800ce142e9045b9cfe6e342864

[libromdata] NintendoDS_BNR::imgpf(): Check for animated icons.
This was previously handled by NintendoDS before banner parsing was
split out into a separate RomData subclass.

Without IMGPF_ICON_ANIMATED, rpcli won't indicate if an animated icon
is present, but it can still be extracted with `-a`.

This fixes a regression from commit f2565965b22be535a322745f44f02a736c57b4c3.
([libromdata] NintendoDS_BNR: Split icon/title data parsing out of NintendoDS.)

Affects: v2.4 - v2.5

src/libromdata/Handheld/NintendoDS_BNR.cpp
@@ -553,10 +553,21 @@ uint32_t NintendoDS_BNR::imgpf(ImageType imageType) const
 
 	uint32_t ret = 0;
 	switch (imageType) {
-		case IMG_INT_ICON:
-			// Use nearest-neighbor scaling.
-			ret = IMGPF_RESCALE_NEAREST;
+		case IMG_INT_ICON: {
+			// Is this an animated icon?
+			RP_D(const NintendoDS_BNR);
+			if (le16_to_cpu(d->nds_icon_title.version) < NDS_ICON_VERSION_DSi ||
+			    (d->nds_icon_title.dsi_icon_seq[0] & cpu_to_le16(0xFF)) == 0)
+			{
+				// Not an animated icon.
+				ret = IMGPF_RESCALE_NEAREST;
+			} else {
+				// This is an animated icon.
+				ret = IMGPF_RESCALE_NEAREST | IMGPF_ICON_ANIMATED;
+			}
 			break;
+		}
+
 		default:
 			break;
 	}

commit cdfdf04d7e6b1fa4bba3fe77601e0612c030c654

[libromdata] NintendoDS_BNR: Assume the icon isn't animated if frame 0 delay is 1 but frame 1 delay is 0.
Delay of 0 means "end of sequence". This effectively is the same as a
static icon, but the previous method of just checking frame 0's delay
didn't work.

This resulted in the following ROM headers as being detected as having
an animated icon when they didn't:
- DMFEA4
- DSYESZ
- VSOE8P

TODO: Add one that does.

nds_structs.h: Mark some enum constants as unsigned.

This fixes a regression from commit f5d34d97cb6d84800ce142e9045b9cfe6e342864.
([libromdata] NintendoDS_BNR::imgpf(): Check for animated icons.)

src/libromdata/Handheld/NintendoDS_BNR.cpp
@@ -149,7 +149,8 @@ rp_image_const_ptr NintendoDS_BNR_Private::loadIcon(void)
 	// NDS icon for the standard icon instead of the first frame
 	// of the animated DSi icon? (Except for DSiWare...)
 	if (le16_to_cpu(nds_icon_title.version) < NDS_ICON_VERSION_DSi ||
-	    (nds_icon_title.dsi_icon_seq[0] & cpu_to_le16(0xFF)) == 0)
+	    (nds_icon_title.dsi_icon_seq[0] & cpu_to_le16(0xFF)) == 0 ||
+	    (nds_icon_title.dsi_icon_seq[1] & cpu_to_le16(0xFF)) == 0)
 	{
 		// Either this isn't a DSi icon/title struct (pre-v0103),
 		// or the animated icon sequence is invalid.
@@ -557,7 +558,8 @@ uint32_t NintendoDS_BNR::imgpf(ImageType imageType) const
 			// Is this an animated icon?
 			RP_D(const NintendoDS_BNR);
 			if (le16_to_cpu(d->nds_icon_title.version) < NDS_ICON_VERSION_DSi ||
-			    (d->nds_icon_title.dsi_icon_seq[0] & cpu_to_le16(0xFF)) == 0)
+			    (d->nds_icon_title.dsi_icon_seq[0] & cpu_to_le16(0xFF)) == 0 ||
+			    (d->nds_icon_title.dsi_icon_seq[1] & cpu_to_le16(0xFF)) == 0)
 			{
 				// Not an animated icon.
 				ret = IMGPF_RESCALE_NEAREST;

src/libromdata/Handheld/nds_structs.h
@@ -246,12 +246,12 @@ typedef enum {
  * Nintendo DSi file type.
  */
 typedef enum {
-	DSi_FTYPE_CARTRIDGE		= 0x00,
-	DSi_FTYPE_DSiWARE		= 0x04,
-	DSi_FTYPE_SYSTEM_FUN_TOOL	= 0x05,
-	DSi_FTYPE_NONEXEC_DATA		= 0x0F,
-	DSi_FTYPE_SYSTEM_BASE_TOOL	= 0x15,
-	DSi_FTYPE_SYSTEM_MENU		= 0x17,
+	DSi_FTYPE_CARTRIDGE		= 0x00U,
+	DSi_FTYPE_DSiWARE		= 0x04U,
+	DSi_FTYPE_SYSTEM_FUN_TOOL	= 0x05U,
+	DSi_FTYPE_NONEXEC_DATA		= 0x0FU,
+	DSi_FTYPE_SYSTEM_BASE_TOOL	= 0x15U,
+	DSi_FTYPE_SYSTEM_MENU		= 0x17U,
 } DSi_FileType;
 
 /**
@@ -271,18 +271,18 @@ typedef enum {
 
 // NDS_IconTitleData version.
 typedef enum {
-	NDS_ICON_VERSION_ORIGINAL	= 0x0001,	// Original
-	NDS_ICON_VERSION_HANS		= 0x0002,	// +HANS
-	NDS_ICON_VERSION_HANS_KO	= 0x0003,	// +KO
-	NDS_ICON_VERSION_DSi		= 0x0103,	// +DSi
+	NDS_ICON_VERSION_ORIGINAL	= 0x0001U,	// Original
+	NDS_ICON_VERSION_HANS		= 0x0002U,	// +HANS
+	NDS_ICON_VERSION_HANS_KO	= 0x0003U,	// +KO
+	NDS_ICON_VERSION_DSi		= 0x0103U,	// +DSi
 } NDS_IconTitleData_Version;
 
 // NDS_IconTitleData sizes.
 typedef enum {
-	NDS_ICON_SIZE_ORIGINAL		= 0x0840,	// Original
-	NDS_ICON_SIZE_HANS		= 0x0940,	// +HANS
-	NDS_ICON_SIZE_HANS_KO		= 0x0A40,	// +KO
-	NDS_ICON_SIZE_DSi		= 0x23C0,	// +DSi
+	NDS_ICON_SIZE_ORIGINAL		= 0x0840U,	// Original
+	NDS_ICON_SIZE_HANS		= 0x0940U,	// +HANS
+	NDS_ICON_SIZE_HANS_KO		= 0x0A40U,	// +KO
+	NDS_ICON_SIZE_DSi		= 0x23C0U,	// +DSi
 } NDS_IconTitleData_Size;
 
 // Icon/title languages.