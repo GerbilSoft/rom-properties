GerbilSoft/rom-properties

commit aa6f1aba073fe06fe0b813655c517f56bb5f2ebe

[rpcli] rpcli.cpp: New function ConsolePrint(), now used by ShowUsage().
On Windows, ConsolePrint() uses win32_write_to_console() to print
Unicode text using WriteConsoleW() if a real console is in use.

src/rpcli/rpcli.cpp
@@ -141,6 +141,57 @@ static void RestoreConsoleOutputCP(void)
 }
 #endif /* _WIN32 */
 
+/**
+ * Print text to the console.
+ *
+ * On Windows, if a real console is in use, use WriteConsole().
+ *
+ * On other systems, or if we're not using a real console on Windows,
+ * use regular stdio functions.
+ *
+ * @param stream Output stream
+ * @param str String
+ * @param newline If true, print a newline afterwards.
+ */
+static void ConsolePrint(FILE *stream, const char *str, bool newline = false)
+{
+#ifdef _WIN32
+	// Windows: If printing to console and UTF-8 is not enabled,
+	// convert to UTF-16 and use WriteConsoleW().
+	// TODO: Store handles in ConsoleInfo_t and pass that instead of FILE*?
+	const ConsoleInfo_t *ci;
+	if (stream == stdout) {
+		ci = &ci_stdout;
+	} else if (stream == stderr) {
+		ci = &ci_stderr;
+	} else {
+		// Invalid stream!
+		assert(!"Invalid stream specified");
+		ci = &ci_stdout;
+	}
+
+	if (ci->is_console) {
+		fflush(stream);
+		// TODO: win32_write_to_console(): Take a handle.
+		int ret = win32_write_to_console(str);
+		if (ret != 0) {
+			// Failed to write to console.
+			// Use stdio as a fallback.
+			fputs(str, stream);
+		}
+	} else
+#endif /* _WIN32 */
+	{
+		// Regular stdio output.
+		fputs(str, stream);
+	}
+
+	if (newline) {
+		// TODO: Use WriteConsole() for this if using a Win32 console?
+		fputc('\n', stream);
+	}
+}
+
 struct ExtractParam {
 	const TCHAR *filename;	// Target filename. Can be null due to argv[argc]
 	int imageType;		// Image Type. -1 = iconAnimData, MUST be between -1 and IMG_INT_MAX
@@ -154,10 +205,10 @@ struct ExtractParam {
 };
 
 /**
-* Extracts images from romdata
-* @param romData RomData containing the images
-* @param extract Vector of image extraction parameters
-*/
+ * Extracts images from romdata
+ * @param romData RomData containing the images
+ * @param extract Vector of image extraction parameters
+ */
 static void ExtractImages(const RomData *romData, const vector<ExtractParam> &extract)
 {
 	const uint32_t supported = romData->supportedImageTypes();
@@ -581,14 +632,13 @@ static void DoAtaIdentifyDevice(const TCHAR *filename, bool json, bool packet)
 static void ShowUsage(void)
 {
 	// TODO: Use argv[0] instead of hard-coding 'rpcli'?
-
 #ifdef ENABLE_DECRYPTION	
-	fputs(C_("rpcli", "Usage: rpcli [-k] [-c] [-p] [-j] [-l lang] [[-xN outfile]... [-mN outfile]... [-a apngoutfile] filename]..."), stderr);
+	const char *const s_usage = C_("rpcli", "Usage: rpcli [-k] [-c] [-p] [-j] [-l lang] [[-xN outfile]... [-mN outfile]... [-a apngoutfile] filename]...");
 	fputc('\n', stderr);
 #else /* !ENABLE_DECRYPTION */
-	fputs(C_("rpcli", "Usage: rpcli [-c] [-p] [-j] [-l lang] [[-xN outfile]... [-mN outfile]... [-a apngoutfile] filename]..."), stderr);
-	fputc('\n', stderr);
+	const char *const s_usage = C_("rpcli", "Usage: rpcli [-c] [-p] [-j] [-l lang] [[-xN outfile]... [-mN outfile]... [-a apngoutfile] filename]...");
 #endif /* ENABLE_DECRYPTION */
+	ConsolePrint(stderr, s_usage, true);
 
 	struct cmd_t {
 		char opt[8];	// TODO: Automatic padding?
@@ -613,9 +663,8 @@ static void ShowUsage(void)
 	}};
 
 	for (const auto &p : cmds) {
-		fputs(p.opt, stderr);
-		fputs(pgettext_expr("rpcli", p.desc), stderr);
-		fputc('\n', stderr);
+		ConsolePrint(stderr, p.opt);
+		ConsolePrint(stderr, pgettext_expr("rpcli", p.desc), true);
 	}
 	fputc('\n', stderr);
 
@@ -627,21 +676,21 @@ static void ShowUsage(void)
 		{"  -ip: ", NOP_C_("rpcli", "Run an ATA IDENTIFY PACKET DEVICE command.")},
 	}};
 
-	fputs(C_("rpcli", "Special options for devices:"), stderr);
-	fputc('\n', stderr);
+	ConsolePrint(stderr, C_("rpcli", "Special options for devices:"), true);
 	for (const auto &p : cmds_dev) {
-		fputs(p.opt, stderr);
-		fputs(pgettext_expr("rpcli", p.desc), stderr);
-		fputc('\n', stderr);
+		ConsolePrint(stderr, p.opt);
+		ConsolePrint(stderr, pgettext_expr("rpcli", p.desc), true);
 	}
 	fputc('\n', stderr);
 #endif /* RP_OS_SCSI_SUPPORTED */
 
-	fputs(C_("rpcli", "Examples:"), stderr); fputc('\n', stderr);
-	fputs("* rpcli s3.gen\n", stderr);
-	fputs("\t ", stderr); fputs(C_("rpcli", "displays info about s3.gen"), stderr); fputc('\n', stderr);
-	fputs("* rpcli -x0 icon.png pokeb2.nds\n", stderr);
-	fputs("\t ", stderr); fputs(C_("rpcli", "extracts icon from pokeb2.nds"), stderr); fputc('\n', stderr);
+	ConsolePrint(stderr, C_("rpcli", "Examples:"), true);
+	ConsolePrint(stderr, "* rpcli s3.gen\n");
+	ConsolePrint(stderr, "\t ");
+		ConsolePrint(stderr, C_("rpcli", "displays info about s3.gen"), true);
+	ConsolePrint(stderr, "* rpcli -x0 icon.png pokeb2.nds\n");
+	ConsolePrint(stderr, "\t ");
+		ConsolePrint(stderr, C_("rpcli", "extracts icon from pokeb2.nds"), true);
 	fflush(stderr);
 }
 
@@ -731,6 +780,9 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 	}
 #endif /* _WIN32 */
 
+	// Detect console information.
+	init_vt();
+
 	// Initialize i18n.
 	rp_i18n_init();
 
@@ -750,7 +802,6 @@ int RP_C_API _tmain(int argc, TCHAR *argv[])
 
 	// TODO: Add a command line option to override color output.
 	// NOTE: Only checking ci_stdout here, since actual data is printed on stdout.
-	init_vt();
 	if (ci_stdout.is_console) {
 		flags |= OF_Text_UseAnsiColor;
 	}

commit 7273c8b6587669dec9b49a84f19e6400d4df546d

[rpcli] rpcli.cpp, ShowUsage(): Forgot to remove a `fputc('\n', stderr);`.
This resulted in an extra newline when showing usage in builds that
support decryption.

This fixes a regression from commit aa6f1aba073fe06fe0b813655c517f56bb5f2ebe.
([rpcli] rpcli.cpp: New function ConsolePrint(), now used by ShowUsage().)

src/rpcli/rpcli.cpp
@@ -583,7 +583,6 @@ static void ShowUsage(void)
 	// TODO: Use argv[0] instead of hard-coding 'rpcli'?
 #ifdef ENABLE_DECRYPTION	
 	const char *const s_usage = C_("rpcli", "Usage: rpcli [-k] [-c] [-p] [-j] [-l lang] [[-xN outfile]... [-mN outfile]... [-a apngoutfile] filename]...");
-	fputc('\n', stderr);
 #else /* !ENABLE_DECRYPTION */
 	const char *const s_usage = C_("rpcli", "Usage: rpcli [-c] [-p] [-j] [-l lang] [[-xN outfile]... [-mN outfile]... [-a apngoutfile] filename]...");
 #endif /* ENABLE_DECRYPTION */