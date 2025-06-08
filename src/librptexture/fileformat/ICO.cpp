GerbilSoft/rom-properties

commit de802835f71c8ddea12035266fde536158c227b3

[librptexture] ICO: Handle 8-bit color icons.
- Forgot to move convimg to img when converting from CI8 to ARGB32.

- Make sure sBIT is updated with alpha=1. Otherwise, RpPng::save()
  will tell libpng that there's no alpha, so the resulting PNG image
  saved using `rpcli -x3` won't be transparent.

src/librptexture/fileformat/ICO.cpp
@@ -393,7 +393,16 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(void)
 
 		case 4:
 			// 16-color
-			img = ImageDecoder::fromLinearCI4(ImageDecoder::PixelFormat::Host_ARGB32, true,
+			img = ImageDecoder::fromLinearCI4(ImageDecoder::PixelFormat::Host_xRGB32, true,
+				width, half_height,
+				icon_data, icon_size,
+				pal_data.data(), pal_data.size() * sizeof(uint32_t),
+				stride);
+			break;
+
+		case 8:
+			// 256-color
+			img = ImageDecoder::fromLinearCI8(ImageDecoder::PixelFormat::Host_xRGB32,
 				width, half_height,
 				icon_data, icon_size,
 				pal_data.data(), pal_data.size() * sizeof(uint32_t),
@@ -416,6 +425,8 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(void)
 	}
 
 	// Apply the icon mask.
+	rp_image::sBIT_t sBIT;
+	img->get_sBIT(&sBIT);
 	if (bitcount < 8) {
 		// Keep the icon as CI8 and add a transparency color.
 		// TODO: Set sBIT.
@@ -452,6 +463,12 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(void)
 			bits += dest_stride_adj;
 			mask += mask_stride_adj;
 		}
+
+		// Update the sBIT metadata.
+		if (sBIT.alpha == 0) {
+			sBIT.alpha = 1;
+			img->set_sBIT(&sBIT);
+		}
 	} else {
 		// CI8 needs to be converted to ARGB32.
 		if (img->format() != rp_image::Format::ARGB32) {
@@ -468,9 +485,9 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(void)
 				}
 				return img;
 			}
+			img = std::move(convimg);
 		}
 
-		// TODO: Set sBIT.
 		assert(img->format() == rp_image::Format::ARGB32);
 
 		uint32_t *bits = static_cast<uint32_t*>(img->bits());
@@ -499,6 +516,12 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(void)
 			// Next row.
 			bits += dest_stride_adj;
 			mask += mask_stride_adj;
+		}
+
+		// Update the sBIT metadata.
+		if (sBIT.alpha == 0) {
+			sBIT.alpha = 1;
+			img->set_sBIT(&sBIT);
 		}
 	}
 

commit 8378697be1e61d8a2de78336d0ee8e37e6df8dee

[librptexture] ICOPrivate::loadImage_Win3(): Use Host_ARGB32, not Host_xRGB32, for the palette.
fromLinearCI4() doesn't support Host_xRGB32, and we're filling
in the alpha channel ourselves.

TODO: Add Host_xRGB32 support to fromLinearCI4()?
fromLinearCI8() supports it...

This fixes a regression from commit de802835f71c8ddea12035266fde536158c227b3.
([librptexture] ICO: Handle 8-bit color icons.)

src/librptexture/fileformat/ICO.cpp
@@ -493,7 +493,9 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(void)
 
 		case 4:
 			// 16-color
-			img = ImageDecoder::fromLinearCI4(ImageDecoder::PixelFormat::Host_xRGB32, true,
+			// NOTE: fromLinearCI4() doesn't support Host_xRGB32,
+			// and we're setting the alpha channel to 0xFF ourselves.
+			img = ImageDecoder::fromLinearCI4(ImageDecoder::PixelFormat::Host_ARGB32, true,
 				width, half_height,
 				icon_data, icon_size,
 				pal_data.data(), pal_data.size() * sizeof(uint32_t),