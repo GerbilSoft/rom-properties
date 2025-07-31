0379e0149bff0ed1fe3ac9497009a6b7e7f06154
David Korth <gerbilsoft@gerbilsoft.com>
2025-07-31 00:59:26 -0400

    [kde/tests] RomDataViewTest: Added a test for RFT_BITFIELD without sparse elements.
    TODO: Add an RFT_BITFIELD sparse test, i.e. a bitfield where some bits
    have a nullptr description and don't show up in the UI.
    
    RFT_STRING test: Use EXPECT_EQ() with QLatin1String instead of converting
    the QString to a C string.
    
    Code coverage change: (with KF5 and KF6 built)
    
    Before:
      source files: 616
      lines.......: 24.0% (11770 of 49055 lines)
      functions...: 24.0% (953 of 3963 functions)
      branches....: 16.8% (8534 of 50932 branches)
    
    After:
      source files: 616
      lines.......: 24.0% (11791 of 49055 lines)
      functions...: 24.1% (954 of 3963 functions)
      branches....: 16.8% (8549 of 50932 branches)

--- src/kde/tests/RomDataViewTest.cpp
+++ src/kde/tests/RomDataViewTest.cpp
@@ -31,13 +31,16 @@ using LibRpFile::VectorFilePtr;
 #include "RomDataView.hpp"
 
 // Qt includes
+#include <QCheckBox>
 #include <QFormLayout>
 #include <QLabel>
 #include <QVBoxLayout>
 
 // C++ STL classes
 using std::array;
+using std::string;
 using std::unique_ptr;
+using std::vector;
 
 namespace RomPropertiesKDE { namespace Tests {
 
@@ -88,6 +91,8 @@ TEST_F(RomDataViewTest, RFT_STRING)
 	RomFields *const fields = m_romData->getWritableFields();
 	fields->addField_string(s_field_desc, s_field_value);
 
+	/** Verify the Qt widgets. **/
+
 	// Create a RomDataView.
 	m_romDataView.reset(new RomDataView(m_romData));
 
@@ -116,7 +121,97 @@ TEST_F(RomDataViewTest, RFT_STRING)
 	qs_field_desc += QChar(L':');
 
 	EXPECT_EQ(qs_field_desc, lblDesc->text()) << "Field description is incorrect.";
-	EXPECT_STREQ(s_field_value, lblValue->text().toUtf8().constData()) << "Field value is incorrect.";
+	EXPECT_EQ(QLatin1String(s_field_value), lblValue->text()) << "Field value is incorrect.";
+}
+
+/**
+ * Test RomDataView with a RomData object with an RFT_BITFIELD field.
+ * Non-sparse: Bitfield has 16 contiguous bits.
+ */
+TEST_F(RomDataViewTest, RFT_BITFIELD_non_sparse)
+{
+	// Add an RFT_BITFIELD field.
+	static const char s_field_desc[] = "RFT_BITFIELD 0";
+
+	static const array<const char*, 16> bitfield_names = {{
+		"bit 0", "bit 1", "bit 2", "bit 3",
+		"bit 4", "bit 5", "bit 6", "bit 7",
+		"bit 8", "bit 9", "bit 10", "bit 11",
+		"bit 12", "bit 13", "bit 14", "bit 15",
+	}};
+	static constexpr uint32_t bitfield_value = 0xAA55;
+
+	vector<string> *const v_bitfield_names = RomFields::strArrayToVector(bitfield_names);
+
+	RomFields *const fields = m_romData->getWritableFields();
+	fields->addField_bitfield(s_field_desc, v_bitfield_names, 4, bitfield_value);
+
+	/** Verify the Qt widgets. **/
+
+	// Create a RomDataView.
+	m_romDataView.reset(new RomDataView(m_romData));
+
+	// There shouldn't be any tabs.
+	// Get the first VBox and form layout.
+	QVBoxLayout *const vboxTab0 = findDirectChild<QVBoxLayout*>(m_romDataView.get(), QLatin1String("vboxTab0"));
+	ASSERT_NE(nullptr, vboxTab0);
+	QFormLayout *const formTab0 = findDirectChild<QFormLayout*>(vboxTab0, QLatin1String("formTab0"));
+	ASSERT_NE(nullptr, formTab0);
+
+	// Get the layout items for the first row.
+	QLayoutItem *const itemDesc = formTab0->itemAt(0, QFormLayout::LabelRole);
+	ASSERT_NE(nullptr, itemDesc);
+	QLayoutItem *const itemValue = formTab0->itemAt(0, QFormLayout::FieldRole);
+	ASSERT_NE(nullptr, itemValue);
+
+	// Label should be QLabel.
+	QLabel *const lblDesc = qobject_cast<QLabel*>(itemDesc->widget());
+	ASSERT_NE(nullptr, lblDesc);
+
+	// Verify the label contents.
+	// NOTE: Description label will have an added ':'.
+	QString qs_field_desc = QLatin1String(s_field_desc);
+	qs_field_desc += QChar(L':');
+	EXPECT_EQ(qs_field_desc, lblDesc->text()) << "Field description is incorrect.";
+
+	// The value item will be a QGridLayout.
+	QGridLayout *const gridLayout = qobject_cast<QGridLayout*>(itemValue->layout());
+	ASSERT_NE(nullptr, gridLayout);
+
+	// Grid should be 4x4, since we specified 4 items per column,
+	// and we have 16 items.
+	EXPECT_EQ(4, gridLayout->columnCount());
+	EXPECT_EQ(4, gridLayout->rowCount());
+
+	// Go through each item.
+	unsigned int bit = 0;
+	const int rowCount = gridLayout->rowCount();
+	const int columnCount = gridLayout->columnCount();
+	for (int row = 0; row < rowCount && bit < bitfield_names.size(); row++) {
+		for (int col = 0; col < columnCount && bit < bitfield_names.size(); col++, bit++) {
+			QLayoutItem *const layoutItem = gridLayout->itemAtPosition(row, col);
+			EXPECT_NE(nullptr, layoutItem);
+			if (!layoutItem) {
+				continue;
+			}
+
+			// Get the QCheckBox.
+			QCheckBox *const checkBox = qobject_cast<QCheckBox*>(layoutItem->widget());
+			EXPECT_NE(nullptr, checkBox);
+			if (!checkBox) {
+				continue;
+			}
+
+			// Verify the checkbox's label.
+			EXPECT_EQ(QLatin1String(bitfield_names[bit]), checkBox->text()) << "QCheckBox " << bit << " label is incorrect.";
+
+			// Verify the checkbox's value.
+			EXPECT_EQ(!!(bitfield_value & (1U << bit)), checkBox->isChecked()) << "QCheckBox " << bit << " value is incorrect.";
+		}
+	}
+
+	// Make sure we've processed all of the bits.
+	EXPECT_EQ(bit, bitfield_names.size()) << "Incorrect number of bits processed.";
 }
 
 } }
