GerbilSoft/rom-properties

commit f52ed03bb968ca2d71d0ac69ce342d903550fc89

[libromdata] SNES: Added an extra check for BS-X memory pack ROMs.
These have a 6-byte magic number at 0x7F00 (or 0xFF00), plus a cartridge
type identifier. The high nybble is 0x7 for ROM, or 0x1-0x4 for FLASH.
The low nybble is the cartridge size: (1024 << (val & 0x0F))

src/libromdata/Console/SNES.cpp
@@ -309,6 +309,34 @@ SNES::SNES(IRpFile *file)
 	}
 
 	if (d->romType == SNESPrivate::ROM_UNKNOWN) {
+		// Check for BS-X "Memory Pack" headers.
+		static const uint16_t bsx_addrs[2] = {0x7F00, 0xFF00};
+		static const uint8_t bsx_mempack_magic[6] = {'M', 0, 'P', 0, 0, 0};
+		uint8_t buf[7];
+
+		for (unsigned int i = 0; i < 2; i++) {
+			size_t size = d->file->seekAndRead(bsx_addrs[i], buf, sizeof(buf));
+			if (size != sizeof(buf)) {
+				// Read error.
+				return;
+			}
+
+			if (!memcmp(buf, bsx_mempack_magic, sizeof(bsx_mempack_magic))) {
+				// Found BS-X memory pack magic.
+				// Check the memory pack type.
+				// (7 is ROM; 1 to 4 is FLASH.)
+				if ((buf[6] & 0xF0) == 0x70) {
+					// ROM cartridge
+					// TODO: Use the size value.
+					// Size is (1024 << (buf[6] & 0x0F))
+					d->romType = SNESPrivate::ROM_BSX;
+					break;
+				}
+			}
+		}
+	}
+
+	if (d->romType == SNESPrivate::ROM_UNKNOWN) {
 		// SNES ROMs don't necessarily have a header at the start of the file.
 		// Therefore, we need to do a few reads and guessing.
 
@@ -361,9 +389,6 @@ SNES::SNES(IRpFile *file)
 			}
 		}
 	}
-
-	// TODO: BS-X memory pack ROMs have the following at 0x7F00 or 0xFF00:
-	// {'M', 0, 'P', 0, 0, 0, 0x79}
 
 	// Header addresses to check.
 	// If a copier header is detected, use index 1,

commit da55974018945106bb369628f8ade9919cd359bb

[libromdata] SNES: Don't fail immediately if the BS-X memory pack read fails.
Otherwise, the 32 KiB test files from RomHeaderTest will fail if
loaded outside of the test suite, since it tries reading data from
0xFF00. (The test suite has a 64 KiB buffer, so this check didn't
fail in the actual tests.)

Note that most users would never encounter this bug, since it only
affects SNES ROMs smaller than 65,287 bytes (which are rare).

This fixes a regression from commit f52ed03bb968ca2d71d0ac69ce342d903550fc89.
([libromdata] SNES: Added an extra check for BS-X memory pack ROMs.)

Affects: v1.3 - v2.6

src/libromdata/Console/SNES.cpp
@@ -969,9 +969,8 @@ SNES::SNES(const IRpFilePtr &file)
 		for (const uint16_t bsx_addr : bsx_addrs) {
 			size_t size = d->file->seekAndRead(bsx_addr, buf, sizeof(buf));
 			if (size != sizeof(buf)) {
-				// Read error.
-				d->file.reset();
-				return;
+				// Read error. Skip this check and continue.
+				continue;
 			}
 
 			if (!memcmp(buf, bsx_mempack_magic.data(), bsx_mempack_magic.size())) {