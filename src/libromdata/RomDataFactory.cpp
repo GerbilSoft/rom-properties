GerbilSoft/rom-properties

commit 894982e37c1dd1c4433332ceb91ab0703f24a27c

[libromdata] RomDataFactory: Improve handling of formats that use ZIP.
AndroidAPK and J2ME both use ZIP files. Check for the ZIP magic,
then try the two classes.

TODO: Open the ZIP file here and pass the unzFile to the subclass.
This prevents us from having to re-open the ZIP file multiple times.

Note that J2ME is still in romDataFns_header[] because it also handles
.jad files (standalone manifests).

TODO: Split .jad into its own class?

src/libromdata/RomDataFactory.cpp
@@ -301,8 +301,7 @@ static const array<RomDataFns, 40> romDataFns_header = {{
 	GetRomDataFns(WiiWAD, ATTR_HAS_THUMBNAIL | ATTR_HAS_METADATA),
 
 	// Handhelds
-	GetRomDataFns(AndroidAPK, ATTR_HAS_THUMBNAIL | ATTR_HAS_METADATA | ATTR_HAS_DPOVERLAY),	// .apk [MUST BE BEFORE J2ME] (TODO: Handle .zip files like .iso?)
-	GetRomDataFns(J2ME, ATTR_HAS_THUMBNAIL | ATTR_HAS_METADATA),	// .jar and .jad (TODO: Handle .zip files like .iso?)
+	GetRomDataFns(J2ME, ATTR_HAS_METADATA),	// .jad only; .jar is handled separately
 	GetRomDataFns(Nintendo3DS, ATTR_HAS_THUMBNAIL | ATTR_HAS_DPOVERLAY | ATTR_HAS_METADATA),
 	GetRomDataFns(PalmOS, ATTR_HAS_THUMBNAIL | ATTR_HAS_METADATA),	// TODO: Magic at 0x40?
 
@@ -772,6 +771,34 @@ RomDataPtr create(const IRpFilePtr &file, unsigned int attrs)
 	} else {
 		// No SparseDiscReader. Use the original file.
 		reader = file;
+	}
+
+	// Check for a .zip file. (AndroidAPK, J2ME)
+	static constexpr uint32_t zip_magic = 0x504B0304;
+	if (header.u32[0] == cpu_to_be32(zip_magic)) {
+		// This is a .zip file. Try AndroidAPK and J2ME.
+		// TODO: Open the .zip file here, check for certain files, then
+		// pass the unzFile to the RomData subclass directly?
+
+		// AndroidAPK: .apk
+		static constexpr unsigned int AndroidAPK_attrs = ATTR_HAS_THUMBNAIL | ATTR_HAS_METADATA | ATTR_HAS_DPOVERLAY;
+		if ((attrs & AndroidAPK_attrs) == attrs) {
+			RomDataPtr romData = std::make_shared<AndroidAPK>(reader);
+			if (romData->isValid()) {
+				// RomData subclass obtained.
+				return romData;
+			}
+		}
+
+		// J2ME: .jar
+		static constexpr unsigned int J2ME_attrs = ATTR_HAS_THUMBNAIL | ATTR_HAS_METADATA;
+		if ((attrs & J2ME_attrs) == attrs) {
+			RomDataPtr romData = std::make_shared<J2ME>(reader);
+			if (romData->isValid()) {
+				// RomData subclass obtained.
+				return romData;
+			}
+		}
 	}
 
 	// Check RomData subclasses that take a header at 0

commit 652d4c71221cb688d5e25ac2155027e10a2f110c

[libromdata] RomDataFactory: Forgot to remove 1 from romDataFns_header's size.
This broke regsvr32 on Windows for both registration and
unregistration, since it presumably accessed invalid data
at the end of the array.

This fixes a regression from commit 894982e37c1dd1c4433332ceb91ab0703f24a27c.
([libromdata] RomDataFactory: Improve handling of formats that use ZIP.)

src/libromdata/RomDataFactory.cpp
@@ -279,7 +279,7 @@ static const array<RomDataFns, 41> romDataFns_magic = {{
  * Headers with addresses other than 0 should be
  * placed at the end of this array.
  */
-static const array<RomDataFns, 40> romDataFns_header = {{
+static const array<RomDataFns, 39> romDataFns_header = {{
 	// Consoles
 	GetRomDataFns(ColecoVision, ATTR_HAS_METADATA),
 	GetRomDataFns(Dreamcast, ATTR_HAS_THUMBNAIL | ATTR_HAS_METADATA | ATTR_SUPPORTS_DEVICES),