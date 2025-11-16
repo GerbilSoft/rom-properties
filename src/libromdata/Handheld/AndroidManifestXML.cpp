GerbilSoft/rom-properties

commit e854d25b0e1001141c412818cd903b06eb2d43ea

[libromdata] AndroidManifestXMLPrivate::decompressAndroidBinaryXml(): Handle corrupt chunks where the size is too small.
If the size is 0, we end up infinitely looping over the same chunk.
Make sure the chunk size is at least the size of the header.

Found using aflplusplus-4.21c.

src/libromdata/Handheld/AndroidManifestXML.cpp
@@ -190,8 +190,14 @@ xml_document *AndroidManifestXMLPrivate::decompressAndroidBinaryXml(const uint8_
 			// Found the first XML tag.
  			break;
  		}
+
 		// Skip this chunk.
-		p += le32_to_cpu(pHdr->size);
+		const size_t chunk_size = le32_to_cpu(pHdr->size);
+		assert(pHdr->size >= sizeof(ResChunk_header));
+		if (pHdr->size < sizeof(ResChunk_header)) {
+			// Corrupted chunk?
+			return nullptr;
+		}
 	}
 	assert(p < pEnd);
 	if (p >= pEnd) {
@@ -224,8 +230,9 @@ xml_document *AndroidManifestXMLPrivate::decompressAndroidBinaryXml(const uint8_
 
 		// Make sure this node doesn't go out of bounds.
 		const uint32_t node_size = le32_to_cpu(node->header.size);
+		assert(node_size >= sizeof(ResXMLTree_node));
 		assert(p + node_size <= pEnd);
-		if (p + node_size > pEnd) {
+		if (node_size < sizeof(ResXMLTree_node) || p + node_size > pEnd) {
 			break;
 		}
 

commit 016d212f81d16fe274609f2e5eede2d04d8fb3ae

[libromdata] AndroidManifestXMLPrivate::decompressAndroidBinaryXml(): Restore `p += le32_to_cpu(pHdr->size);`.
This resulted in hangs when parsing not-corrupted AndroidManifest.xml
files.

This fixes a regression from commit e854d25b0e1001141c412818cd903b06eb2d43ea.
([libromdata] AndroidManifestXMLPrivate::decompressAndroidBinaryXml(): Handle corrupt chunks where the size is too small.)

src/libromdata/Handheld/AndroidManifestXML.cpp
@@ -198,6 +198,7 @@ xml_document *AndroidManifestXMLPrivate::decompressAndroidBinaryXml(const uint8_
 			// Corrupted chunk?
 			return nullptr;
 		}
+		p += chunk_size;
 	}
 	assert(p < pEnd);
 	if (p >= pEnd) {