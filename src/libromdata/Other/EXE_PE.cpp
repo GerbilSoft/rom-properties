GerbilSoft/rom-properties

commit 7ba842c5ce6fc400a14dc05fc95f807e50536428

[libromdata] EXE: Skip the invalid entry instead of stopping parsing entirely.
Only the individual entry might be corrupted.

No crash observed with the fuzzed test case from before.

src/libromdata/Other/EXE_PE.cpp
@@ -1100,12 +1100,12 @@ int EXEPrivate::addFields_PE_Import(void)
 				return false;
 			}
 			ilt = &ilt_buf[(le32_to_cpu(peImportDir[dir_index].rvaImportLookupTable) - dll_ilt_base)/4];
+			dir_index++;
 			if (ilt >= ilt_end) {
-				// Corrupt import directory?
-				return false;
+				// Corrupt directory entry?
+				continue;
 			}
 			dllname = &peImportNames[dir_index];
-			dir_index++;
 			// check for NULL entry
 			if (!ilt[0] && (!is64 || !ilt[1])) {
 				ilt = ilt_end;

commit bdafb350baf74295157ce494c5b9f04947f6a7b6

[libromdata] EXEPrivate::addFields_PE_Import(): Restore the correct position of `dir_index++;`.
This caused the wrong DLLs to be listed for various imports.

This fixes a regression from commit 7ba842c5ce6fc400a14dc05fc95f807e50536428.
([libromdata] EXE: Skip the invalid entry instead of stopping parsing entirely.)

Affects: v2.7

src/libromdata/Other/EXE_PE.cpp
@@ -1100,12 +1100,12 @@ int EXEPrivate::addFields_PE_Import(void)
 				return false;
 			}
 			ilt = &ilt_buf[(le32_to_cpu(peImportDir[dir_index].rvaImportLookupTable) - dll_ilt_base)/4];
-			dir_index++;
 			if (ilt >= ilt_end || dir_index >= peImportDir.size()) {
 				// Corrupt directory entry?
 				continue;
 			}
 			dllname = &peImportNames[dir_index];
+			dir_index++;
 			// check for NULL entry
 			if (!ilt[0] && (!is64 || !ilt[1])) {
 				ilt = ilt_end;