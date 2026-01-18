GerbilSoft/rom-properties

commit 66411c55b6afba0a4513a66400d86731a29f974b

Xbox360_XDBF.cpp: loadString_GPD(): Actually search for the correct string ID.
This didn't cause a problem anywhere yet because for GPDs, only the
title was searched, and title is usually the first entry.

Convert strings from DOS to Unix line endings, like loadString_SPA().

Also, added dos2unix() in addFields_achievements_GPD().
Otherwise, '␍' symbols show up in GPD achievements.

Found using scan-build from clang-14.0.0:

src/libromdata/Console/Xbox360_XDBF.cpp:556:2: warning: Value stored to 'string_id' is never read [deadcode.DeadStores]
        string_id = cpu_to_be16(string_id);
        ^           ~~~~~~~~~~~~~~~~~~~~~~

src/libromdata/Console/Xbox360_XDBF.cpp
@@ -569,11 +569,9 @@ string Xbox360_XDBF_Private::loadString_GPD(uint16_t string_id)
 			continue;
 		}
 
-		// Check for Sync List or Sync Data entry.
-		if (p.resource_id == cpu_to_be64(XDBF_GPD_SYNC_LIST_ENTRY) ||
-		    p.resource_id == cpu_to_be64(XDBF_GPD_SYNC_DATA_ENTRY))
-		{
-			// Skip this entry.
+		// Check for a matching resource ID.
+		if (p.resource_id != string_id) {
+			// Not a match. Skip this entry.
 			continue;
 		}
 
@@ -600,8 +598,9 @@ string Xbox360_XDBF_Private::loadString_GPD(uint16_t string_id)
 			continue;
 		}
 
-		// Convert from UTF-16BE.
-		ret = utf16be_to_utf8(sbuf.get(), length / 2);
+		// Convert from UTF-16BE and DOS line endings.
+		ret = dos2unix(utf16be_to_utf8(sbuf.get(), length / 2));
+		break;
 	}
 
 	return ret;
@@ -1501,13 +1500,13 @@ int Xbox360_XDBF_Private::addFields_achievements_GPD(void)
 		// For now, assuming all achievements are unlocked.
 		string desc;
 		if (pTitle) {
-			desc = utf16be_to_utf8(pTitle, -1);
+			desc = dos2unix(utf16be_to_utf8(pTitle, -1));
 		}
 		if (pUnlockedDesc) {
 			if (!desc.empty()) {
 				desc += '\n';
 			}
-			desc += utf16be_to_utf8(pUnlockedDesc, -1);
+			desc += dos2unix(utf16be_to_utf8(pUnlockedDesc, -1));
 		}
 
 		// Add to RFT_LISTDATA.

commit 4e94ca5bfa1b40eb9302cc2a4ee90d45d7913bb4

[libromdata] Xbox360_XDBF_Private::loadString_GPD(): Fix string lookups.
- p.resource_id is 64-bit, so we have to use cpu_to_be64() on the 16-bit
  string ID. Otherwise, we'll never find the string.

This fixes a regression from commit 66411c55b6afba0a4513a66400d86731a29f974b.
(Xbox360_XDBF.cpp: loadString_GPD(): Actually search for the correct string ID.)

Affects: v2.0 - v2.7

Fixes #475: 360 GPD achievement files all show with Title: Unknown
Reported by @Masamune3210.

src/libromdata/Console/Xbox360_XDBF.cpp
@@ -545,11 +545,6 @@ string Xbox360_XDBF_Private::loadString_GPD(uint16_t string_id)
 		return {};
 	}
 
-#if SYS_BYTEORDER == SYS_LIL_ENDIAN
-	// Byteswap the ID to make it easier to find things.
-	string_id = cpu_to_be16(string_id);
-#endif /* SYS_BYTEORDER == SYS_LIL_ENDIAN */
-
 	// TODO: Optimize by creating an unordered_map of IDs to strings?
 	// Might not be a good optimization if we don't have that many strings...
 
@@ -557,6 +552,8 @@ string Xbox360_XDBF_Private::loadString_GPD(uint16_t string_id)
 
 	// GPD doesn't have string tables.
 	// Instead, each string is its own entry in the main resource table.
+	// Because of this, string IDs are 64-bit.
+	const uint64_t string_id64 = cpu_to_be64(string_id);
 	string s_ret;
 	for (const XDBF_Entry &p : entryTable) {
 		if (p.namespace_id != cpu_to_be16(XDBF_GPD_NAMESPACE_STRING)) {
@@ -565,7 +562,7 @@ string Xbox360_XDBF_Private::loadString_GPD(uint16_t string_id)
 		}
 
 		// Check for a matching resource ID.
-		if (p.resource_id != string_id) {
+		if (p.resource_id != string_id64) {
 			// Not a match. Skip this entry.
 			continue;
 		}
@@ -577,8 +574,9 @@ string Xbox360_XDBF_Private::loadString_GPD(uint16_t string_id)
 		assert(length > 2);
 		assert(length <= 4096);
 		assert(length % 2 == 0);
-		if (length < 2 || length > 4096 || length % 2 != 0)
+		if (length < 2 || length > 4096 || length % 2 != 0) {
 			continue;
+		}
 
 		// Length includes the NULL terminator, so remove it.
 		// NOTE: Some GPD files only have one NULL byte if the string