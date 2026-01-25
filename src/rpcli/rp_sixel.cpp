a2d975cec0d5d0bccc36dd8568a63f012f9059e6
David Korth <gerbilsoft@gerbilsoft.com>
2026-01-24 20:41:19 -0500

    [rpcli] rp_sixel.cpp: Add support for the Kitty graphics protocol.
    Kitty supports 24-bit and 32-bit color, plus PNG format (maybe later).
    The pixel data must be encoded using base64, but since we might have
    images with strides that don't match the width, we have to handle it
    line-by-line. Kitty's base64 decoder doesn't seem to mind "empty"
    bytes in the stream (i.e. equals signs not at the end), but it doesn't
    like whitespace showing up.
    
    NOTE: Kitty's protocol swaps the R and B channels compared to rp_image.
    This does match some others, e.g. gdk-pixbuf.
    
    Graphics protocol priority:
    - Kitty (highest priority)
    - Sixel
    
    TODO: Verify if Sixel works with Windows Terminal. WT doesn't support
    Kitty protocol and probably won't support it anytime soon.

--- src/rpcli/rp_sixel.cpp
+++ src/rpcli/rp_sixel.cpp
@@ -42,6 +42,7 @@ using namespace LibRpTexture;
 
 // C++ STL classes
 #include <mutex>
+using std::array;
 using std::unique_ptr;
 
 namespace SixelDll {
@@ -206,14 +207,8 @@ static int print_sixel_image(sixel_output_t *output, const rp_image_const_ptr &i
 	return 0;
 }
 
-void print_sixel_icon_banner(const RomDataPtr &romData)
+static void print_sixel_icon_banner_int(const RomDataPtr &romData)
 {
-	// Make sure the terminal supports sixel.
-	if (!gsvt_supports_sixel()) {
-		// Sixel is not supported.
-		return;
-	}
-
 	// Load libsixel.
 	std::call_once(SixelDll::sixel_once_flag, SixelDll::init_sixel_once);
 	if (!SixelDll::libsixel_dll) {
@@ -296,4 +291,219 @@ void print_sixel_icon_banner(const RomDataPtr &romData)
 	fflush(stdout);
 
 	SixelDll::psixel_output_destroy(output);
+}
+
+static int print_kitty_image(rp_image_const_ptr image_src)
+{
+	// Kitty requires R/B to be swapped.
+	rp_image_ptr image = image_src->dup_ARGB32();
+	if (!image->isValid()) {
+		// Failed to dup() and/or convert to ARGB32...
+		return -EINVAL;
+	}
+
+	// Swap the R/B channels.
+	{
+		argb32_t *bits = reinterpret_cast<argb32_t*>(image->bits());
+		const int strideDiff = image->row_bytes() - (image->width() * sizeof(uint32_t));
+		for (unsigned int y = (unsigned int)image->height(); y > 0; y--) {
+			unsigned int x;
+			for (x = (unsigned int)image->width(); x > 1; x -= 2) {
+				// Swap the R and B channels
+				std::swap(bits[0].r, bits[0].b);
+				std::swap(bits[1].r, bits[1].b);
+				bits += 2;
+			}
+			if (x == 1) {
+				// Last pixel
+				std::swap(bits->r, bits->b);
+				bits++;
+			}
+
+			// Next line.
+			bits += strideDiff;
+		}
+	}
+
+	const int width = image->width();
+	const int height = image->height();
+
+	// Print the Kitty header.
+	printf("\x1B_Gf=32,s=%d,v=%d,a=T;", width, height);
+
+	// Allocate a line buffer for base64 conversion.
+	// 3 input bytes == 4 output bytes
+	// NOTE: Console output must be printed in 4 KB chunks.
+	// Extra 8 bytes to handle scanline shenanigans.
+	size_t linebuf_size = ((static_cast<size_t>(width) * sizeof(uint32_t)) * 4 / 3) + 8;
+	// Round up to 4 bytes.
+	size_t rem = linebuf_size % 4;
+	if (rem != 0) {
+		rem = (4 - rem);
+		linebuf_size += rem;
+	}
+
+	unique_ptr<char[]> linebuf(new char[linebuf_size]);
+
+	static const array<char, 64> base64_encoding_table = {{
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+		'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', '0', '1', '2', '3',
+		'4', '5', '6', '7', '8', '9', '+', '/'
+	}};
+
+	const uint8_t *bits = static_cast<const uint8_t*>(image->bits());
+	const size_t row_bytes = static_cast<size_t>(image->row_bytes());
+	const size_t stride_adj = (static_cast<size_t>(image->stride()) - row_bytes);
+	for (int y = height; y > 0; y--, bits += stride_adj) {
+		// Process 3 input bytes at a time.
+		size_t n;
+		char *p = linebuf.get();
+		for (n = row_bytes; n >= 3; n -= 3, bits += 3, p += 4) {
+			uint32_t data = (bits[0] << 16) | (bits[1] << 8) | bits[2];
+			//printf("data: %08X, bits: %02X %02X %02X\n", data, bits[0], bits[1], bits[2]);
+			p[0] = base64_encoding_table[(data >> (3*6)) & 0x3F];
+			p[1] = base64_encoding_table[(data >> (2*6)) & 0x3F];
+			p[2] = base64_encoding_table[(data >> (1*6)) & 0x3F];
+			p[3] = base64_encoding_table[(data >> (0*6)) & 0x3F];
+		}
+
+		if (n > 0) {
+			// Encode the remaining bytes using equal signs to indicate
+			// that some bytes are missing.
+			uint32_t data = 0;
+			for (size_t o = n; o > 0; o--, bits++) {
+				data <<= 8;
+				data |= *bits;
+			}
+			data <<= ((3 - n) * 8);
+
+			p[0] = base64_encoding_table[(data >> (3*6)) & 0x3F];
+			p[1] = base64_encoding_table[(data >> (2*6)) & 0x3F];
+			p[2] = base64_encoding_table[(data >> (1*6)) & 0x3F];
+			p[3] = base64_encoding_table[(data >> (0*6)) & 0x3F];
+
+			// Number of bytes to overwrite depends on number of source bytes remaining:
+			// - n = 1: overwrite 2, 3
+			// - n = 2: overwrite    3
+			switch (n) {
+				default:
+					break;
+				case 1:
+					p[2] = '=';
+					// fall-through
+				case 2:
+					p[3] = '=';
+					break;
+			}
+			p += 4;
+		}
+
+		// Write the encoded data in 4 KB chunks.
+		static constexpr size_t BASE64_CHUNK_SIZE = 4096U;
+		size_t linebuf_used = static_cast<size_t>(p - linebuf.get());
+		p = linebuf.get();
+		while (linebuf_used > 0) {
+			size_t bytes_to_print = std::min(linebuf_used, BASE64_CHUNK_SIZE);
+			fwrite(p, 1, bytes_to_print, stdout);
+			p += bytes_to_print;
+			linebuf_used -= bytes_to_print;
+		}
+	}
+
+	// End of data.
+	fwrite("\x1B\\", 1, 2, stdout);
+	return 0;
+}
+
+static void print_kitty_icon_banner_int(const RomDataPtr &romData)
+{
+	// Kitty protocol is very simple: it accepts base64-encoded
+	// PNG or raw graphics data.
+	// TODO: Stride handling for raw graphics data.
+	// TODO: Pass-through PNG data?
+	// TODO: Also supports animated icons.
+
+	// Get the character cell size.
+	int cell_size_w = 0, cell_size_h = 0;
+	int ret = gsvt_get_cell_size(&cell_size_w, &cell_size_h);
+	if (ret != 0 || cell_size_w <= 0 || cell_size_h <= 0) {
+		// Unable to get the character cell size...
+		return;
+	}
+
+	// Get the icon and banner, and determine the maximum height.
+	// NOTE: No scaling here...
+	int max_height = 0;
+	rp_image_const_ptr icon, banner;
+	// Check for an icon first.
+	const unsigned int imgbf = romData->supportedImageTypes();
+	if (imgbf & RomData::IMGBF_INT_ICON) {
+		icon = romData->image(RomData::IMG_INT_ICON);
+		if (icon) {
+			max_height = icon->height();
+		}
+	}
+	if (imgbf & RomData::IMGBF_INT_BANNER) {
+		banner = romData->image(RomData::IMG_INT_BANNER);
+		if (banner) {
+			max_height = std::max(max_height, banner->height());
+		}
+	}
+	if (!icon && !banner) {
+		// No images...
+		return;
+	}
+
+	// Determine how many rows are needed.
+	int rows = max_height / cell_size_h;
+	if (max_height % cell_size_h != 0) {
+		rows++;
+	}
+	for (int i = 0; i < rows; i++) {
+		putchar('\n');
+	}
+	printf("\x1B[%dA\x1B[s", rows);	// move up `rows` rows, save cursor position
+
+	int cur_col = 0;
+	if (icon) {
+		print_kitty_image(icon);
+		const int icon_width = icon->width() + 8;
+		int icon_cols = icon_width / cell_size_w;
+		if (icon_width % cell_size_w != 0) {
+			icon_cols++;
+		}
+		cur_col += icon_cols;
+		printf("\x1B[u\x1B[%dC", cur_col);	// restore cursor position; move right `cur_col` columns
+	}
+	if (banner) {
+		print_kitty_image(banner);
+		const int banner_width = banner->width() + 8;
+		int banner_cols = banner_width / cell_size_w;
+		if (banner_width % cell_size_w != 0) {
+			banner_cols++;
+		}
+		cur_col += banner_cols;
+		printf("\x1B[u\x1B[%dC", cur_col);	// restore cursor position; move right `cur_col` columns
+	}
+
+	// Move back down to after the icon/banner.
+	printf("\x1B[%dE", rows);
+	fflush(stdout);
+}
+
+void print_sixel_icon_banner(const RomDataPtr &romData)
+{
+	// Check if the terminal supports Kitty.
+	if (gsvt_supports_kitty()) {
+		// Use the Kitty graphics protocol.
+		print_kitty_icon_banner_int(romData);
+	} else if (gsvt_supports_sixel()) {
+		// Use the Sixel graphics protocol.
+		print_sixel_icon_banner_int(romData);
+	}
 }
