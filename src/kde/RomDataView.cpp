GerbilSoft/rom-properties

commit b6a6a6797063b2ce67af379df982e19c44eff88a

[gtk,kde] RomDataView: Automatically wrap long strings.
Useful for Android packages, which often have long descriptions without
hard line breaks.

[kde] Set the horizontal size policy to QSizePolicy::Expanding.
Otherwise, it ends up wrapping "F-Droid" to two lines, and probably
worse for other strings.

src/kde/RomDataView.cpp
@@ -315,6 +315,10 @@ QLabel *RomDataViewPrivate::initString(QLabel *lblDesc,
 		}
 	}
 
+	// Automatically wrap long strings.
+	lblString->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred));
+	lblString->setWordWrap(true);
+
 	// Enable strong focus so we can tab into the label.
 	lblString->setFocusPolicy(Qt::StrongFocus);
 

src/gtk/RomDataView.cpp
@@ -771,10 +771,12 @@ rp_rom_data_view_init_string(RpRomDataView *page,
 	const RomFields::Field &field,
 	const char *str = nullptr)
 {
-	// String type.
+	// String type
 	GtkWidget *widget = gtk_label_new(nullptr);
 	// NOTE: No name for this GtkWidget.
 	gtk_label_set_use_underline(GTK_LABEL(widget), false);
+	// Automatically wrap long strings.
+	gtk_label_set_wrap(GTK_LABEL(widget), true);
 #if !GTK_CHECK_VERSION(4, 0, 0)
 	gtk_widget_show(widget);
 #endif /* !GTK_CHECK_VERSION(4, 0, 0) */

commit 9470270b9262033fe6f3cb7ac066d03f897df4da

[kde] RomDataView: Fix display of the "Credits" row for e.g. amiibo.
This broke when adjustments were made to handle Android package
descriptions better.

This fixes a regression from commit b6a6a6797063b2ce67af379df982e19c44eff88a.
([gtk,kde] RomDataView: Automatically wrap long strings.)

Affects: v2.7 - v2.7.1

src/kde/RomDataView.cpp
@@ -297,6 +297,8 @@ QLabel *RomDataViewPrivate::initString(QLabel *lblDesc,
 		lblString->setTextInteractionFlags(
 			Qt::LinksAccessibleByMouse | Qt::LinksAccessibleByKeyboard);
 
+		lblString->setWordWrap(false);
+
 		// Replace newlines with "<br/>".
 		QString text;
 		if (str) {
@@ -308,10 +310,16 @@ QLabel *RomDataViewPrivate::initString(QLabel *lblDesc,
 		lblString->setText(text);
 	} else {
 		// tr: Standard text with no formatting.
-		lblString->setTextInteractionFlags(
-			Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
 		lblString->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 		lblString->setTextFormat(Qt::PlainText);
+		lblString->setTextInteractionFlags(
+			Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
+
+		// Allow the label to be shrunken horizontally.
+		// TODO: Scrolling.
+		lblString->setMinimumWidth(1);
+		lblString->setWordWrap(true);
+
 		if (str) {
 			lblString->setText(*str);
 		} else if (field.data.str) {
@@ -319,16 +327,11 @@ QLabel *RomDataViewPrivate::initString(QLabel *lblDesc,
 		}
 	}
 
-	// Automatically wrap long strings.
-	lblString->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred));
-	lblString->setWordWrap(true);
+	// Expand horizontally, but use preferred size for vertical.
+	lblString->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
 
 	// Enable strong focus so we can tab into the label.
 	lblString->setFocusPolicy(Qt::StrongFocus);
-
-	// Allow the label to be shrunken horizontally.
-	// TODO: Scrolling.
-	lblString->setMinimumWidth(1);
 
 	// Check for any formatting options. (RFT_STRING only)
 	if (field.type == RomFields::RomFieldType::RFT_STRING) {