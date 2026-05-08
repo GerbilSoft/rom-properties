GerbilSoft/rom-properties

commit 34d63d3cd18a1b064df643e70eac24d13129757f

[librptexture] rp_image_ops_*.cpp: Use a vector OR instead of scalar OR for the shuffle mask.
I'm not sure if this will actually improve performance that much,
since it has setup time, but it reduces the number of instructions.

Code size difference: (64-bit Gentoo Linux, gcc-16.1.0, release build, no LTO)

   text    data     bss     dec     hex filename
   2342       0       0    2342     926 rp_image_ops_ssse3.cpp.o [before]
   2353       0       0    2353     931 rp_image_ops_ssse3.cpp.o [after]
    +11       0       0     +11      +b Difference

Slight increase in code size, possibly because the SSSE3 instructions
are larger than the scalar instructions.

src/librptexture/img/rp_image_ops_neon.cpp
@@ -30,6 +30,7 @@ typedef uint32x4_t uint32xVTBL_t;
 #define vld1VTBL_u32 vld1q_u32
 #define vst1VTBL_u8  vst1q_u8
 #define vst1VTBL_u32 vst1q_u32
+#define vorrVTBL_u32 vorrq_u32
 #define vqtbl1q_u8_u32(a, b) vreinterpretq_u32_u8(vqtbl1q_u8(vreinterpretq_u8_u32(a), vreinterpretq_u8_u32(b)))
 #elif defined(RP_CPU_ARM)
 //static constexpr size_t VEC_LEN_U8 = 8;
@@ -41,6 +42,7 @@ typedef uint32x2_t uint32xVTBL_t;
 #define vld1VTBL_u32 vld1_u32
 #define vst1VTBL_u8  vst1_u8
 #define vst1VTBL_u32 vst1_u32
+#define vorrVTBL_u32 vorr_u32
 #define vtbl1_u8_u32(a, b) vreinterpret_u32_u8(vtbl1_u8(vreinterpret_u8_u32(a), vreinterpretq_u8_u32(b)))
 #else
 #  error Unsupported CPU?
@@ -128,16 +130,23 @@ int rp_image::swizzle_neon(const char *swz_spec)
 	SWIZZLE_MASK_VAL(2);
 	SWIZZLE_MASK_VAL(3);
 
-	const array<uint32_t, VEC_LEN_U32> pshufb_mask_u32 = {{
-		pshufb_mask_vals.u32,
-		pshufb_mask_vals.u32 + 0x04040404,
+	// TODO: Compare disassembly between this and the previous method.
+	static const array<uint32_t, VEC_LEN_U32> pshufb_mask_u32_chnum_or = {{
+		0, 0x04040404,
 #ifdef RP_CPU_ARM64
-		pshufb_mask_vals.u32 + 0x08080808,
-		pshufb_mask_vals.u32 + 0x0C0C0C0C
+		0x08080808, 0x0C0C0C0C
 #endif /* RP_CPU_ARM64 */
 	}};
 
-	uint32xVTBL_t shuf_mask = vld1VTBL_u32(pshufb_mask_u32.data());
+	// Shuffle mask
+	// NOTE: The channel numbers are OR'd here instead of added in
+	// pshufb_mask_u32_or so we can take advantage of a vector OR
+	// instead of adding each value individually.
+	const uint32xVTBL_t shuf_chnum_or = vld1VTBL_u32(pshufb_mask_u32_chnum_or.data());
+	const uint32xVTBL_t shuf_mask = vorVTBL_u32(
+		vdupVTBL_n_u32(pshufb_mask_vals.u32), shuf_chnum_or);
+
+	// OR mask for '1' channels.
 	uint32xVTBL_t or_mask = vdupVTBL_n_u32(por_mask_vals.u32);
 
 	// Channel indexes

src/librptexture/img/rp_image_ops_ssse3.cpp
@@ -91,12 +91,14 @@ int rp_image::swizzle_ssse3(const char *swz_spec)
 	SWIZZLE_MASK_VAL(2);
 	SWIZZLE_MASK_VAL(3);
 
-	const __m128i pshufb_mask = _mm_setr_epi32(
-		pshufb_mask_vals.u32,
-		pshufb_mask_vals.u32 + 0x04040404,
-		pshufb_mask_vals.u32 + 0x08080808,
-		pshufb_mask_vals.u32 + 0x0C0C0C0C
-	);
+	// Shuffle mask
+	// NOTE: The channel numbers are OR'd here instead of added in
+	// pshufb_mask_u32_or so we can take advantage of a vector OR
+	// instead of adding each value individually.
+	const __m128i pshufb_mask_chnum_or = _mm_setr_epi32(0, 0x04040404, 0x08080808, 0x0C0C0C0C);
+	const __m128i pshufb_mask = _mm_or_si128(
+		_mm_set1_epi32(pshufb_mask_vals.u32), pshufb_mask_chnum_or);
+
 	const __m128i por_mask = _mm_set1_epi32(por_mask_vals.u32);
 
 	// Channel indexes

commit d74e93a1173010b2b921cc22555bf2e1b20f7f7f

[librptexture] rp_image_ops_neon.cpp: s/vorVTBL_u32/vorrVTBL_u32/
This somehow didn't cause any problems in the debug build, but it failed
miserably in the release build. [vorVTBL_u32 is undefined...]

This fixes a regression from commit 34d63d3cd18a1b064df643e70eac24d13129757f.
([librptexture] rp_image_ops_*.cpp: Use a vector OR instead of scalar OR for the shuffle mask.)

src/librptexture/img/rp_image_ops_neon.cpp
@@ -143,7 +143,7 @@ int rp_image::swizzle_neon(const char *swz_spec)
 	// pshufb_mask_u32_or so we can take advantage of a vector OR
 	// instead of adding each value individually.
 	const uint32xVTBL_t shuf_chnum_or = vld1VTBL_u32(pshufb_mask_u32_chnum_or.data());
-	const uint32xVTBL_t shuf_mask = vorVTBL_u32(
+	const uint32xVTBL_t shuf_mask = vorrVTBL_u32(
 		vdupVTBL_n_u32(pshufb_mask_vals.u32), shuf_chnum_or);
 
 	// OR mask for '1' channels.