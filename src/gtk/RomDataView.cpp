GerbilSoft/rom-properties

commit 1239ba0908d5226660ebc25b2a5a52c51faf5cd6

[gtk,kde,win32] RomDataView,RP_ShellPropSheetExt: Factor out the image height adjustment code.
It's used for both Icon and Banner, so split it out.

More lines are reduced in the Win32 and KDE versions than the GTK
version, but all of them have been refactored for consistency.

src/gtk/RomDataView.cpp
@@ -71,6 +71,10 @@ static void	rp_rom_data_view_finalize	(GObject	*object);
 static void	rp_rom_data_view_desc_format_type_changed(RpRomDataView *page,
 						 RpDescFormatType desc_format_type);
 
+static void	rp_rom_data_view_adjust_image_height	(RpDragImage	*image,
+							 int		 img_w,
+							 int		 img_h,
+							 int		 imgStdHeight);
 static void	rp_rom_data_view_init_header_row(RpRomDataView	*page);
 static gboolean	rp_rom_data_view_update_display	(RpRomDataView	*page);
 static gboolean	rp_rom_data_view_load_rom_data	(RpRomDataView	*page);
@@ -639,6 +643,31 @@ rp_rom_data_view_is_showing_data(RpRomDataView *page)
 	return (bool)page->cxx->romData;
 }
 
+/**
+ * Adjust the height for an image based on the "standard" height.
+ * Used by initHeaderRow().
+ *
+ * @param image DragImage
+ * @param img_w rp_image width
+ * @param img_h rp_image width
+ * @param imgStdHeight "Standard" height (usually 32px at 96dpi)
+ */
+static void
+rp_rom_data_view_adjust_image_height(RpDragImage *image, int img_w, int img_h, int imgStdHeight)
+{
+	assert(img_w > 0);
+	assert(img_h > 0);
+
+	if (img_h != imgStdHeight) {
+		// Need to scale the image to match the aspect ratio.
+		const int img_scaled_w = rintf((float)imgStdHeight * ((float)img_w / (float)img_h));
+		gtk_widget_set_size_request(GTK_WIDGET(image), img_scaled_w, imgStdHeight);
+	} else {
+		// Use the original image size.
+		gtk_widget_set_size_request(GTK_WIDGET(image), img_w, img_h);
+	}
+}
+
 static void
 rp_rom_data_view_init_header_row(RpRomDataView *page)
 {
@@ -687,16 +716,8 @@ rp_rom_data_view_init_header_row(RpRomDataView *page)
 		const rp_image_const_ptr img = romData->image(RomData::IMG_INT_BANNER);
 		ok = rp_drag_image_set_rp_image(RP_DRAG_IMAGE(page->imgBanner), img);
 		if (ok) {
-			const int banner_w = img->width();
-			const int banner_h = img->height();
-			if (banner_h != imgStdHeight) {
-				// Need to scale the banner image to match the aspect ratio.
-				const int banner_scaled_w = rintf((float)imgStdHeight * ((float)banner_w / (float)banner_h));
-				gtk_widget_set_size_request(page->imgBanner, banner_scaled_w, imgStdHeight);
-			} else {
-				// Use the original banner size.
-				gtk_widget_set_size_request(page->imgBanner, banner_w, banner_h);
-			}
+			// Adjust the banner size.
+			rp_rom_data_view_adjust_image_height(RP_DRAG_IMAGE(page->imgBanner), img->width(), img->height(), imgStdHeight);
 		}
 	}
 	gtk_widget_set_visible(page->imgBanner, ok);
@@ -736,16 +757,8 @@ rp_rom_data_view_init_header_row(RpRomDataView *page)
 			}
 
 			if (ok) {
-				assert(icon_w > 0);
-				assert(icon_h > 0);
-				if (icon_h != imgStdHeight) {
-					// Need to scale the icon image to match the aspect ratio.
-					const int icon_scaled_w = rintf((float)imgStdHeight * ((float)icon_w / (float)icon_h));
-					gtk_widget_set_size_request(page->imgIcon, icon_scaled_w, imgStdHeight);
-				} else {
-					// Use the original icon size.
-					gtk_widget_set_size_request(page->imgIcon, icon_w, icon_h);
-				}
+				// Adjust the icon size.
+				rp_rom_data_view_adjust_image_height(RP_DRAG_IMAGE(page->imgBanner), icon_w, icon_h, imgStdHeight);
 			}
 		}
 	}

src/kde/RomDataView.cpp
@@ -116,6 +116,35 @@ void RomDataViewPrivate::createOptionsButton(void)
 }
 
 /**
+ * Adjust the height for an image based on the "standard" height.
+ * Used by initHeaderRow().
+ *
+ * @param label DragImageLabel
+ * @param imgSize rp_image size
+ * @param imgStdHeight "Standard" height (usually 32px at 96dpi)
+ */
+void RomDataViewPrivate::adjustImageHeight(DragImageLabel *label, QSize imgSize, int imgStdHeight)
+{
+	assert(imgSize.width() > 0);
+	assert(imgSize.height() > 0);
+
+	if (imgSize.height() != imgStdHeight) {
+		// Need to scale the banner label to match the aspect ratio.
+		const QSize labelScaledSize(rintf(
+			static_cast<float>(imgStdHeight) * (static_cast<float>(imgSize.width()) / static_cast<float>(imgSize.height()))),
+			imgStdHeight);
+		label->setMinimumSize(labelScaledSize);
+		label->setMaximumSize(labelScaledSize);
+		label->setScaledContents(true);
+	} else {
+		// Use the original banner size.
+		label->setMinimumSize(imgSize);
+		label->setMaximumSize(imgSize);
+		label->setScaledContents(false);
+	}
+}
+
+/**
  * Initialize the header row widgets.
  * The widgets must have already been created by ui.setupUi().
  */
@@ -163,21 +192,7 @@ void RomDataViewPrivate::initHeaderRow(void)
 			ok = ui.lblBanner->setRpImage(img);
 			if (ok) {
 				// Adjust the banner size.
-				const QSize bannerSize(img->width(), img->height());
-				if (bannerSize.height() != imgStdHeight) {
-					// Need to scale the banner label to match the aspect ratio.
-					const QSize bannerScaledSize(rintf(
-						static_cast<float>(imgStdHeight) * (static_cast<float>(bannerSize.width()) / static_cast<float>(bannerSize.height()))),
-						imgStdHeight);
-					ui.lblBanner->setMinimumSize(bannerScaledSize);
-					ui.lblBanner->setMaximumSize(bannerScaledSize);
-					ui.lblBanner->setScaledContents(true);
-				} else {
-					// Use the original banner size.
-					ui.lblBanner->setMinimumSize(bannerSize);
-					ui.lblBanner->setMaximumSize(bannerSize);
-					ui.lblBanner->setScaledContents(false);
-				}
+				adjustImageHeight(ui.lblBanner, QSize(img->width(), img->height()), imgStdHeight);
 			}
 		}
 	}
@@ -219,20 +234,8 @@ void RomDataViewPrivate::initHeaderRow(void)
 			}
 
 			if (ok) {
-				if (iconSize.height() != imgStdHeight) {
-					// Need to scale the icon label to match the aspect ratio.
-					const QSize iconScaledSize(rintf(
-						static_cast<float>(imgStdHeight) * (static_cast<float>(iconSize.width()) / static_cast<float>(iconSize.height()))),
-						imgStdHeight);
-					ui.lblIcon->setMinimumSize(iconScaledSize);
-					ui.lblIcon->setMaximumSize(iconScaledSize);
-					ui.lblIcon->setScaledContents(true);
-				} else {
-					// Use the original icon size.
-					ui.lblIcon->setMinimumSize(iconSize);
-					ui.lblIcon->setMaximumSize(iconSize);
-					ui.lblIcon->setScaledContents(false);
-				}
+				// Adjust the icon size.
+				adjustImageHeight(ui.lblIcon, iconSize, imgStdHeight);
 			}
 		}
 	}

src/kde/RomDataView_p.hpp
@@ -119,6 +119,16 @@ public:
 
 public:
 	/**
+	 * Adjust the height for an image based on the "standard" height.
+	 * Used by initHeaderRow().
+	 *
+	 * @param label DragImageLabel
+	 * @param imgSize rp_image size
+	 * @param imgStdHeight "Standard" height (usually 32px at 96dpi)
+	 */
+	static void adjustImageHeight(DragImageLabel *label, QSize imgSize, int imgStdHeight);
+
+	/**
 	 * Initialize the header row widgets.
 	 * The widgets must have already been created by ui.setupUi().
 	 */

src/win32/RP_ShellPropSheetExt.cpp
@@ -94,6 +94,31 @@ RP_ShellPropSheetExt_Private::~RP_ShellPropSheetExt_Private()
 }
 
 /**
+ * Adjust the height for an image based on the "standard" height.
+ * Used by loadImages().
+ *
+ * @param label DragImageLabel
+ * @param imgStdHeight "Standard" height (usually 32px at 96dpi)
+ */
+void RP_ShellPropSheetExt_Private::adjustImageHeight(DragImageLabel *label, int imgStdHeight)
+{
+	const SIZE labelSize = {label->width(), label->height()};
+	if (labelSize.cy != imgStdHeight) {
+		// Need to scale the label to match the aspect ratio.
+		const SIZE labelScaledSize = {
+			static_cast<LONG>(rintf(
+				static_cast<float>(imgStdHeight) * (static_cast<float>(labelSize.cx) /
+				static_cast<float>(labelSize.cy)))),
+			imgStdHeight
+		};
+		label->setRequiredSize(labelScaledSize);
+	} else {
+		// Use the original size.
+		label->setRequiredSize(labelSize);
+	}
+}
+
+/**
  * Load the banner and icon as HBITMAPs.
  *
  * This function should be called on startup and if
@@ -124,21 +149,7 @@ void RP_ShellPropSheetExt_Private::loadImages(void)
 
 			ok = lblBanner->setRpImage(banner);
 			if (ok) {
-				// Adjust the banner size.
-				const SIZE bannerSize = {banner->width(), banner->height()};
-				if (bannerSize.cy != imgStdHeight) {
-					// Need to scale the banner label to match the aspect ratio.
-					const SIZE bannerScaledSize = {
-						static_cast<LONG>(rintf(
-							static_cast<float>(imgStdHeight) * (static_cast<float>(bannerSize.cx) /
-							static_cast<float>(bannerSize.cy)))),
-						imgStdHeight
-					};
-					lblBanner->setRequiredSize(bannerScaledSize);
-				} else {
-					// Use the original banner size.
-					lblBanner->setRequiredSize(bannerSize);
-				}
+				adjustImageHeight(lblBanner.get(), imgStdHeight);
 			}
 		}
 	}
@@ -167,21 +178,7 @@ void RP_ShellPropSheetExt_Private::loadImages(void)
 			}
 
 			if (ok) {
-				// Adjust the icon size.
-				const SIZE iconSize = {icon->width(), icon->height()};
-				if (iconSize.cy != imgStdHeight) {
-					// Need to scale the icon label to match the aspect ratio.
-					const SIZE iconScaledSize = {
-						static_cast<LONG>(rintf(
-							static_cast<float>(imgStdHeight) * (static_cast<float>(iconSize.cx) /
-							static_cast<float>(iconSize.cy)))),
-						imgStdHeight
-					};
-					lblIcon->setRequiredSize(iconScaledSize);
-				} else {
-					// Use the original icon size.
-					lblIcon->setRequiredSize(iconSize);
-				}
+				adjustImageHeight(lblIcon.get(), imgStdHeight);
 			}
 		}
 	}

src/win32/RP_ShellPropSheetExt_p.hpp
@@ -191,6 +191,15 @@ public:
 
 public:
 	/**
+	 * Adjust the height for an image based on the "standard" height.
+	 * Used by loadImages().
+	 *
+	 * @param label DragImageLabel
+	 * @param imgStdHeight "Standard" height (usually 32px at 96dpi)
+	 */
+	static void adjustImageHeight(DragImageLabel *label, int imgStdHeight);
+
+	/**
 	 * Load the banner and icon as HBITMAPs.
 	 *
 	 * This function should bee called on startup and if

commit 377c4f9c93462944466e95a83ed0434010cac9a5

[gtk] RomDataView.cpp, rp_rom_data_view_init_header_row(): Fix copy/paste error in the icon resizing code.
It ended up adjusting imgBanner to what imgIcon's size should be, and
imgIcon was never resized.

Noticed this while checking an Xbox 360 .xex.

This fixes a regression from commit 1239ba0908d5226660ebc25b2a5a52c51faf5cd6.
([gtk,kde,win32] RomDataView,RP_ShellPropSheetExt: Factor out the image height adjustment code.)

This commit was after v2.7.1, so no releases are affected.

src/gtk/RomDataView.cpp
@@ -758,7 +758,7 @@ rp_rom_data_view_init_header_row(RpRomDataView *page)
 
 			if (ok) {
 				// Adjust the icon size.
-				rp_rom_data_view_adjust_image_height(RP_DRAG_IMAGE(page->imgBanner), icon_w, icon_h, imgStdHeight);
+				rp_rom_data_view_adjust_image_height(RP_DRAG_IMAGE(page->imgIcon), icon_w, icon_h, imgStdHeight);
 			}
 		}
 	}