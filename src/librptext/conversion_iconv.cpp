GerbilSoft/rom-properties

commit c449f74e189776981c0eb8d7b6b389826814b297

[librptext] conversion_iconv.cpp, T_cpN_to_unicode<>(): Handle libiconv cp932 quirks for UTF-16.
Not likely to happen, but this also ensures that the UTF-8 code path
isn't included in the UTF-16 code path.

src/librptext/conversion_iconv.cpp
@@ -281,23 +281,40 @@ static std::basic_string<T> T_cpN_to_unicode(const char *out_encoding, unsigned 
 			// Some versions of libiconv map characters differently compared to cp932:
 			// - FreeBSD Shift-JIS: 8160: mapped to U+301C (WAVE DASH); cp932 uses U+FF5E (FULLWIDTH TILDE)
 			// - Termux libiconv: 817C: mapped to U+2212 (MINUS SIGN); cp932 uses U+FF0D (FULLWIDTH HYPHEN-MINUS)
-			const auto ret_end = ret.end();
-			for (auto p = ret.begin(); p != ret_end; ++p) {
-				if ((uint8_t)p[0] == 0xE3 && (uint8_t)p[1] == 0x80 && (uint8_t)p[2] == 0x9C) {
-					// Found U+301C: WAVE DASH
-					// Convert to U+FF5E: FULLWIDTH TILDE
-					p[0] = (uint8_t)0xEF;
-					p[1] = (uint8_t)0xBD;
-					p[2] = (uint8_t)0x9E;
-					p += 2;
-				} else if ((uint8_t)p[0] == 0xE2 && (uint8_t)p[1] == 0x88 && (uint8_t)p[2] == 0x92) {
-					// Found U+2212: MINUS SIGN
-					// Convert to U+FF0D: FULLWIDTH HYPHEN-MINUS
-					p[0] = (uint8_t)0xEF;
-					p[1] = (uint8_t)0xBC;
-					p[2] = (uint8_t)0x8D;
-					p += 2;
+			if_constexpr (sizeof(T) == sizeof(uint8_t)) {
+				const auto ret_end = ret.end();
+				for (auto p = ret.begin(); p != ret_end; ++p) {
+					if ((uint8_t)p[0] == 0xE3 && (uint8_t)p[1] == 0x80 && (uint8_t)p[2] == 0x9C) {
+						// Found U+301C: WAVE DASH
+						// Convert to U+FF5E: FULLWIDTH TILDE
+						p[0] = (uint8_t)0xEF;
+						p[1] = (uint8_t)0xBD;
+						p[2] = (uint8_t)0x9E;
+						p += 2;
+					} else if ((uint8_t)p[0] == 0xE2 && (uint8_t)p[1] == 0x88 && (uint8_t)p[2] == 0x92) {
+						// Found U+2212: MINUS SIGN
+						// Convert to U+FF0D: FULLWIDTH HYPHEN-MINUS
+						p[0] = (uint8_t)0xEF;
+						p[1] = (uint8_t)0xBC;
+						p[2] = (uint8_t)0x8D;
+						p += 2;
+					}
 				}
+			} else if_constexpr (sizeof(T) == sizeof(uint16_t)) {
+				const auto ret_end = ret.end();
+				for (auto p = ret.begin(); p != ret_end; ++p) {
+					if ((uint16_t)*p = 0x301C) {
+						// Found U+301C: WAVE DASH
+						// Convert to U+FF5E: FULLWIDTH TILDE
+						*p = 0xFF5E;
+					} else if ((uint16_t)*p == 0x2212) {
+						// Found U+2212: MINUS SIGN
+						// Convert to U+FF0D: FULLWIDTH HYPHEN-MINUS
+						*p = 0xFF0D;
+					}
+				}
+			} else {
+				static_assert(sizeof(T) == sizeof(uint8_t) || sizeof(T) == sizeof(uint16_t), "Wrong character size!");
 			}
 		}
 #endif /* HAVE_ICONV_LIBICONV */

commit 155cfac2c65fca5385c5bef25acc5677bacab205

[librptext] conversion_iconv.cpp, T_cpN_to_unicode(): Fix the UTF-16 code path on clang-21.
Testing with Termux on Android, this fails:

src/librptext/conversion_iconv.cpp:306:10: error: assignment to cast is illegal, lvalue casts are not supported
  306 |                                         if ((uint16_t)*p = 0x301C) {
      |                                             ^~~~~~~~~~~~ ~

Note the single equals sign. This would have broken anything that
used this code path.

Also, since we're only using one code point from *p, cast it early
and store it in a uint16_t variable.

This fixes a regression from commit c449f74e189776981c0eb8d7b6b389826814b297.
([librptext] conversion_iconv.cpp, T_cpN_to_unicode<>(): Handle libiconv cp932 quirks for UTF-16.)

src/librptext/conversion_iconv.cpp
@@ -303,11 +303,12 @@ static std::basic_string<T> T_cpN_to_unicode(const char *out_encoding, unsigned 
 			} else if_constexpr (sizeof(T) == sizeof(uint16_t)) {
 				const auto ret_end = ret.end();
 				for (auto p = ret.begin(); p != ret_end; ++p) {
-					if ((uint16_t)*p = 0x301C) {
+					const uint16_t chr = static_cast<uint16_t>(*p);
+					if (chr == 0x301C) {
 						// Found U+301C: WAVE DASH
 						// Convert to U+FF5E: FULLWIDTH TILDE
 						*p = 0xFF5E;
-					} else if ((uint16_t)*p == 0x2212) {
+					} else if (chr == 0x2212) {
 						// Found U+2212: MINUS SIGN
 						// Convert to U+FF0D: FULLWIDTH HYPHEN-MINUS
 						*p = 0xFF0D;