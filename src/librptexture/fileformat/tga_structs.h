ab6e9c63939a35f6c5cdcaf2b94857fffa21bdaa
David Korth <gerbilsoft@gerbilsoft.com>
2026-06-05 19:56:43 -0400

    [librptexture] tga_structs.h: Increase TGA_MAX_SIZE from 16 MiB to 72 MiB.
    Pico Park: Classic Edition has a font TGA file that comes in at
    67,108,882 bytes, which is slightly (18 bytes) larger than 64 MiB.
    That corresponds to the 18-byte TGA header, and no TGA 2.0 footer.

--- src/librptexture/fileformat/tga_structs.h
+++ src/librptexture/fileformat/tga_structs.h
@@ -2,7 +2,7 @@
  * ROM Properties Page shell extension. (librptexture)                     *
  * tga_structs.h: TrueVision TGA texture format data structures.           *
  *                                                                         *
- * Copyright (c) 2017-2025 by David Korth.                                 *
+ * Copyright (c) 2017-2026 by David Korth.                                 *
  * SPDX-License-Identifier: GPL-2.0-or-later                               *
  ***************************************************************************/
 
@@ -23,8 +23,8 @@ extern "C" {
 
 // NOTE: 16-bit color is 15-bit RGB + 1-bit transparency.
 
-// Maximum supported TGA file size is 16 MB.
-#define TGA_MAX_SIZE (16U*1024*1024)
+// Maximum supported TGA file size is 72 MiB.
+#define TGA_MAX_SIZE (72U*1024*1024)
 
 /**
  * TrueVision TGA: Main header.
