5d289b83fe8e602ae421496d4eba04ec800c0100
David Korth <gerbilsoft@gerbilsoft.com>
2026-06-13 16:31:27 -0400

    [kde] kde_register_backends.hpp: Register the LibRpText user setting query function.
    Only for KF6, since KCoreAddons v5.245.0 added this setting.
    
    The setting is read directly from ~/.config/kdeglobals .
    Note that it's read every time without caching, in case the user
    changes settings while e.g. Dolphin is running.
    
    TODO: Any sort of file caching to reduce the performance hit?

--- src/kde/kde_register_backends.hpp
+++ src/kde/kde_register_backends.hpp
@@ -10,6 +10,9 @@
 
 #include "config.kde.h"
 
+// Qt includes
+#include <QtCore/qglobal.h>
+
 // RpQImageBackend
 #include "RpQImageBackend.hpp"
 using LibRpTexture::rp_image;
@@ -19,15 +22,49 @@ using LibRpTexture::rp_image;
 #include "AchQtDBus.hpp"
 #endif /* !RP_KDE_DISABLE_REGISTER_ACHQTDBUS */
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+// LibRpText for Binary Unit BinaryUnitDialect
+#  include "librptext/formatting.hpp"
+// Qt
+#  include <QtCore/QSettings>
+#  include <QtCore/QStandardPaths>
+
+/**
+ * Get the BinaryUnitDialect from KDE Globals.
+ * Added in KCoreAddons v5.245.0.
+ * @param user_data
+ * @return BinaryUnitDialect
+ */
+static LibRpText::BinaryUnitDialect getBinaryUnitDialect(void *user_data)
+{
+	// from KFormatPrivate::formatByteSize()
+	// NOTE: KFormat::BinaryUnitDialect maps directly to LibRpText::BinaryUnitDialect.
+	static constexpr LibRpText::BinaryUnitDialect fallbackDialect = LibRpText::BinaryUnitDialect::IECBinaryDialect;
+	RP_UNUSED(user_data);
+
+	const QString kdeglobals = QStandardPaths::locate(QStandardPaths::GenericConfigLocation, QLatin1String("kdeglobals"));
+	QSettings settings(kdeglobals, QSettings::IniFormat);
+	return static_cast<LibRpText::BinaryUnitDialect>(settings.value("Locale/BinaryUnitDialect", static_cast<int>(fallbackDialect)).toInt());
+}
+#endif /* QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) */
+
 /**
  * Register the KDE backends for e.g. rp_image.
  * This must be called when a plugin or executable is loaded.
  */
 static inline void kde_register_backends(void)
 {
-	// Register RpQImageBackend and AchQtDBus.
+	// Register RpQImageBackend for rp_image.
 	rp_image::setBackendCreatorFn(RpQImageBackend::creator_fn);
+
 #if !defined(RP_KDE_DISABLE_REGISTER_ACHQTDBUS) && defined(ENABLE_ACHIEVEMENTS) && defined(HAVE_QtDBus_NOTIFY)
+	// Register AchQtDBus for achievements.
 	AchQtDBus::instance();
 #endif /* !RP_KDE_DISABLE_REGISTER_ACHQTDBUS && ENABLE_ACHIEVEMENTS && HAVE_QtDBus_NOTIFY */
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+	// Register getBinaryUnitDialect() for LibRpText::formatFileSize().
+	// Added in KCoreAddons v5.245.0.
+	LibRpText::setUserSettingQueryFunction(getBinaryUnitDialect, nullptr);
+#endif /* QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) */
 }
