056cec1e13b44f7f4dcde51f75055c4e23e3b7f2
David Korth <gerbilsoft@gerbilsoft.com>
2026-06-26 20:38:54 -0400

    [cmake] ReplaceHardcodedPrefix.cmake: Convert from a macro to a function.
    Copied from file-emblems.

--- cmake/macros/ReplaceHardcodedPrefix.cmake
+++ cmake/macros/ReplaceHardcodedPrefix.cmake
@@ -2,13 +2,12 @@
 # Parameters:
 # - _var: Variable containing the path with the prefix to replcae.
 # - _prefix: Prefix to replace.
-
-MACRO(REPLACE_HARDCODED_PREFIX _var _prefix)
+FUNCTION(REPLACE_HARDCODED_PREFIX _var _prefix)
 	IF(NOT "${_prefix}" STREQUAL "${CMAKE_INSTALL_PREFIX}")
 		STRING(LENGTH "${_prefix}" prefix_LEN)
 		IF(${_var})
 			STRING(SUBSTRING "${${_var}}" ${prefix_LEN} -1 prefix_SUB)
-			SET(${_var} "${CMAKE_INSTALL_PREFIX}${prefix_SUB}")
+			SET(${_var} PARENT_SCOPE "${CMAKE_INSTALL_PREFIX}${prefix_SUB}")
 		ENDIF()
 	ENDIF()
-ENDMACRO()
+ENDFUNCTION()
