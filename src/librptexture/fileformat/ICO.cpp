GerbilSoft/rom-properties

commit a4ca7a46ea8b3b0670bfaee22a84fb7c7be87623

[librptexture] ICOPrivate::getIconBitmapHeaderData(): Convert the if/else to switch/case; add a case for invalid bitcount.
Using 64-bit Gentoo Linux, gcc-16.1.1_p20260613, release build, no LTO

Code size difference for the initial switch/case conversion, without the
case for invalid bitcount:

   text    data     bss     dec     hex filename
  40583     928       0   41511    a227 ICO.cpp.o [before]
  40559     928       0   41487    a20f ICO.cpp.o [after]
    -24       0       0     -24     -18 Difference

Code size difference after adding the invalid case handling:

   text    data     bss     dec     hex filename
  40559     928       0   41487    a20f ICO.cpp.o [before]
  40590     928       0   41518    a22e ICO.cpp.o [after]
    +31       0       0     +31     +1f Difference

Overall code size difference:

   text    data     bss     dec     hex filename
  40583     928       0   41511    a227 ICO.cpp.o [before]
  40590     928       0   41518    a22e ICO.cpp.o [after]
     +7       0       0      +7      +7 Difference

src/librptexture/fileformat/ICO.cpp
@@ -396,15 +396,24 @@ ICOPrivate::IconBitmapHeader_data ICOPrivate::getIconBitmapHeaderData(const Icon
 
 	// Determine pixel format based on bitcount.
 	// TODO: Other bitcounts?
-	if (data.bitcount == 1) {
-		// tr: Abbreviation of "Monochrome".
-		snprintf(data.pixel_format, sizeof(data.pixel_format), "%s", C_("ICO|PixelFormat", "Mono"));
-	} else if (data.bitcount <= 8) {
-		snprintf(data.pixel_format, sizeof(data.pixel_format), "CI%u", data.bitcount);
-	} else if (data.bitcount == 24) {
-		strcpy(data.pixel_format, "RGB");
-	} else if (data.bitcount == 32) {
-		strcpy(data.pixel_format, "ARGB");
+	switch (data.bitcount) {
+		case 1:
+			// tr: Abbreviation of "Monochrome".
+			snprintf(data.pixel_format, sizeof(data.pixel_format), "%s", C_("ICO|PixelFormat", "Mono"));
+			break;
+		case 8:
+			snprintf(data.pixel_format, sizeof(data.pixel_format), "CI%u", data.bitcount);
+			break;
+		case 24:
+			strcpy(data.pixel_format, "RGB");
+			break;
+		case 32:
+			strcpy(data.pixel_format, "ARGB");
+			break;
+		default:
+			// Invalid bitcount?
+			snprintf(data.pixel_format, sizeof(data.pixel_format), "%u-bit", data.bitcount);
+			break;
 	}
 
 	return data;

commit 149afa1f9dd54cdb9be746f8c52317a0a0799b47

[librptexture] ICOPrivate::getIconBitmapHeaderData(): Handle 4-bit images as "CI4".
This broke ImageDecoderTest for some of the older 16-color Notepad icons.

This fixes a regression from commit a4ca7a46ea8b3b0670bfaee22a84fb7c7be87623.
([librptexture] ICOPrivate::getIconBitmapHeaderData(): Convert the if/else to switch/case; add a case for invalid bitcount.)

src/librptexture/fileformat/ICO.cpp
@@ -401,6 +401,7 @@ ICOPrivate::IconBitmapHeader_data ICOPrivate::getIconBitmapHeaderData(const Icon
 			// tr: Abbreviation of "Monochrome".
 			snprintf(data.pixel_format, sizeof(data.pixel_format), "%s", C_("ICO|PixelFormat", "Mono"));
 			break;
+		case 4:
 		case 8:
 			snprintf(data.pixel_format, sizeof(data.pixel_format), "CI%u", data.bitcount);
 			break;