GerbilSoft/rom-properties

commit eeaf1f24185f8cb418964c7a08e0e1b4df7c04e3

Reduce padding in some structs.
NOTE: The values here are from debug builds, which may be larger than
release builds due to extra checks in STL classes.

librptexture:
- GodotSTEXPrivate: 312 -> 304
  - Remove hasEmbeddedFile. Add a function hasEmbeddedFile() that checks
    if embedHeader.size != 0.
- KhronosKTX2Private: 384 -> 376

Console:
- DreamcastSavePrivate: 416 -> 408
- WiiPartition: 65,720 -> 65,712
  - Make CryptoMethod uint8_t.
- WiiWADPrivate: 2,944 -> 2,936
- Xbox360_XEX_Private: 912 -> 904
- Xbox360_XDBF_Private: 448 -> 432

disc:
- CisoPspReaderPrivate: 392 -> 384
- NASOSReaderPrivate: 208 -> 200

Media:
- ISOPrivate: 2,288 -> 2,280

The GodotSTEX changes also reduced the code size in the release build a bit.

Code size difference: (64-bit Gentoo Linux, gcc-16.1.1_p20260613, release build, no LTO)

   text    data     bss     dec     hex filename
  17730    1304       0   19034    4a5a GodotSTEX.cpp.o [before]
  17714    1304       0   19018    4a4a GodotSTEX.cpp.o [after]
    -16       0       0     -16     -10 Difference

src/libromdata/Media/ISO.cpp
@@ -81,13 +81,6 @@ public:
 	// to get the actual sector size.
 	CdromSectorInfo cdromSectorInfo;
 
-	// Sector offset
-	// Usually 0 (for 2048) or 16 (for 2352 or 2448).
-	// NOTE: If SparseDiscReader is used, this will almost
-	// always be 0. Query SparseDiscReader to get the
-	// actual sector information.
-	unsigned int sector_offset;
-
 	// Joliet level
 	enum class JolietSVDType : uint8_t {
 		None		= 0,
@@ -96,6 +89,13 @@ public:
 		UCS2_Level3	= 3,	// NOTE: UCS-2 BE
 	};
 	JolietSVDType jolietSVDType;
+
+	// Sector offset
+	// Usually 0 (for 2048) or 16 (for 2352 or 2448).
+	// NOTE: If SparseDiscReader is used, this will almost
+	// always be 0. Query SparseDiscReader to get the
+	// actual sector information.
+	unsigned int sector_offset;
 
 public:
 	// El Torito boot catalog LBA. (present if non-zero)

src/libromdata/Console/DreamcastSave.cpp
@@ -104,6 +104,9 @@ public:
 	// Directory entry
 	DC_VMS_DirEnt vms_dirent;
 
+	// Is this a VMS game file?
+	bool isGameFile;
+
 	// Creation time. Converted from binary or BCD,
 	// depending on if we loaded a VMI or DCI.
 	// If the original value is invalid, this will
@@ -112,9 +115,6 @@ public:
 
 	// Time conversion functions
 	static time_t vmi_to_unix_time(const DC_VMI_Timestamp *vmi_tm);
-
-	// Is this a VMS game file?
-	bool isGameFile;
 
 	/**
 	 * Check a Dreamcast VMS field for invalid characters.
@@ -233,8 +233,8 @@ DreamcastSavePrivate::DreamcastSavePrivate(const IRpFilePtr &file)
 	, vmi_file(nullptr)
 	, data_area_offset(0)
 	, vms_header_offset(0)
-	, ctime(-1)
 	, isGameFile(false)
+	, ctime(-1)
 {
 	// Clear the various structs.
 	memset(&vms_header, 0, sizeof(vms_header));

src/libromdata/Console/WiiWAD.cpp
@@ -82,8 +82,8 @@ WiiWADPrivate::WiiWADPrivate(const IRpFilePtr &file)
 	, wadType(WadType::Unknown)
 	, data_offset(0)
 	, data_size(0)
-	, pIMETContent(nullptr)
 	, imetContentOffset(0)
+	, pIMETContent(nullptr)
 	, key_idx(WiiTicket::EncryptionKeys::Unknown)
 	, key_status(KeyManager::VerifyResult::Unknown)
 {

src/libromdata/Console/WiiWAD_p.hpp
@@ -76,21 +76,21 @@ public:
 	RVL_Ticket ticket;
 	RVL_TMD_Header tmdHeader;
 
-	// WiiTicket
-	std::unique_ptr<WiiTicket> wiiTicket;
-
 	// Data offset and size
 	uint32_t data_offset;
 	uint32_t data_size;
 
+	// TMD contents table
+	uint32_t imetContentOffset;	// relative to start of data area
+	rp::uvector<RVL_Content_Entry> tmdContentsTbl;
+	const RVL_Content_Entry *pIMETContent;
+
+	// WiiTicket
+	std::unique_ptr<WiiTicket> wiiTicket;
+
 	// Name (BroadOn WADs only)
 	// FIXME: This is the same "meta" section as Nintendo WADs...
 	std::string wadName;
-
-	// TMD contents table
-	rp::uvector<RVL_Content_Entry> tmdContentsTbl;
-	const RVL_Content_Entry *pIMETContent;
-	uint32_t imetContentOffset;	// relative to start of data area
 
 #ifdef ENABLE_DECRYPTION
 	// CBC reader for the main data area

src/libromdata/Console/Xbox360_XDBF.cpp
@@ -66,28 +66,23 @@ public:
 	};
 	XDBFType xdbfType;
 
-	// Internal icon
-	// Points to an rp_image within map_images.
-	rp_image_const_ptr img_icon;
-
-	// Loaded images.
-	// - Key: resource_id
-	// - Value: rp_image*
-	unordered_map<uint64_t, rp_image_ptr > map_images;
-
 public:
 	// XDBF header
 	XDBF_Header xdbfHeader;
+
+	// Data start offset within the file.
+	uint32_t data_offset;
 
 	// Entry table
 	// NOTE: Data is *not* byteswapped on load.
 	rp::uvector<XDBF_Entry> entryTable;
 
-	// Data start offset within the file.
-	uint32_t data_offset;
-
 	// Cached language ID
 	XDBF_Language_e m_langID;
+
+	// If true, this XDBF section is in an XEX executable.
+	// Some fields shouldn't be displayed.
+	const bool xex;
 
 	// String table indexes
 	// These are indexes into entryTable that indicate
@@ -102,10 +97,17 @@ public:
 	// NOTE: These are *pointers* to rp::uvector<>.
 	array<rp::uvector<char>*, XDBF_LANGUAGE_MAX> strTbls;
 
-	// If true, this XDBF section is in an XEX executable.
-	// Some fields shouldn't be displayed.
-	const bool xex;
+public:
+	// Internal icon
+	// Points to an rp_image within map_images.
+	rp_image_const_ptr img_icon;
 
+	// Loaded images.
+	// - Key: resource_id
+	// - Value: rp_image*
+	unordered_map<uint64_t, rp_image_ptr > map_images;
+
+public:
 	/**
 	 * Find a resource in the entry table.
 	 * @param namespace_id Namespace ID

src/libromdata/Console/Xbox360_XEX.cpp
@@ -107,6 +107,15 @@ public:
 	bool isExecutionIDLoaded;
 	XEX2_Execution_ID executionID;
 
+	// Encryption key in use.
+	// If fileFormatInfo indicates the PE is encrypted:
+	// - -1: Unknown
+	// -  0: Retail (may be either XEX1 or XEX2)
+	// -  1: Debug
+	// NOTE: We can't use EncryptionKeys because the debug key
+	// is all zeroes, so we're not handling it here.
+	int keyInUse;
+
 	// Resource information. (XEX2_OPTHDR_RESOURCE_INFO)
 	// Initialized by getXdbfResInfo().
 	// NOTE: These structs **ARE** byteswapped.
@@ -118,15 +127,6 @@ public:
 	// Initialized by initPeReader().
 	// NOTE: This struct **IS** byteswapped.
 	XEX2_File_Format_Info fileFormatInfo;
-
-	// Encryption key in use.
-	// If fileFormatInfo indicates the PE is encrypted:
-	// - -1: Unknown
-	// -  0: Retail (may be either XEX1 or XEX2)
-	// -  1: Debug
-	// NOTE: We can't use EncryptionKeys because the debug key
-	// is all zeroes, so we're not handling it here.
-	int keyInUse;
 
 	// Basic compression: Data segments.
 	struct BasicZDataSeg_t {

src/libromdata/disc/CisoPspReader.cpp
@@ -79,6 +79,9 @@ public:
 	rp::uvector<uint8_t> blockCache;
 	uint32_t blockCacheIdx;
 
+	uint8_t index_shift;		// Index shift value (CISO/ZISO only)
+	bool isDaxWithoutNCTable;	// Convenience variable
+
 	// Decompression buffer
 	// (Same size as blockCache.)
 	rp::uvector<uint8_t> z_buffer;
@@ -86,9 +89,6 @@ public:
 	// DAX: Size and NC area tables
 	rp::uvector<uint16_t> daxSizeTable;
 	std::vector<uint8_t> daxNCTable;	// 0 = compressed; 1 = not compressed
-
-	uint8_t index_shift;		// Index shift value (CISO/ZISO only)
-	bool isDaxWithoutNCTable;	// Convenience variable
 
 	/**
 	 * Get the compressed size of a block.

src/libromdata/disc/NASOSReader.cpp
@@ -50,15 +50,15 @@ public:
 	};
 	DiscType discType;
 
-	// Block map
-	// Values are absolute block addresses, possibly with a shift amount.
-	// Special value: 0xFFFFFFFF == empty block
-	rp::uvector<uint32_t> blockMap;
-
 	// Block address shift
 	// - GCML: 0
 	// - WIIx: 8
 	uint8_t blockMapShift;
+
+	// Block map
+	// Values are absolute block addresses, possibly with a shift amount.
+	// Special value: 0xFFFFFFFF == empty block
+	rp::uvector<uint32_t> blockMap;
 };
 
 /** NASOSReaderPrivate **/

commit ec0a1efb6ca1244af49b4ffaa1267f7c3f0f49cb

[libromdata] ISOPrivate: Fix initialization ordering in the constructor.
src/libromdata/Media/ISO.cpp: In constructor ‘LibRomData::ISOPrivate::ISOPrivate(const LibRpFile::IRpFilePtr&)’:
src/libromdata/Media/ISO.cpp:98:22: error: ‘LibRomData::ISOPrivate::sector_offset’ will be initialized after [-Werror=reorder]
   98 |         unsigned int sector_offset;
      |                      ^~~~~~~~~~~~~
src/libromdata/Media/ISO.cpp:91:23: error:   ‘LibRomData::ISOPrivate::JolietSVDType LibRomData::ISOPrivate::jolietSVDType’ [-Werror=reorder]
   91 |         JolietSVDType jolietSVDType;
      |                       ^~~~~~~~~~~~~
src/libromdata/Media/ISO.cpp:287:1: error:   when initialized here [-Werror=reorder]
  287 | ISOPrivate::ISOPrivate(const IRpFilePtr &file)
      | ^~~~~~~~~~
cc1plus: all warnings being treated as errors

This fixes a regression from commit eeaf1f24185f8cb418964c7a08e0e1b4df7c04e3.
(Reduce padding in some structs.)

src/libromdata/Media/ISO.cpp
@@ -287,8 +287,8 @@ const RomDataInfo ISOPrivate::romDataInfo = {
 ISOPrivate::ISOPrivate(const IRpFilePtr &file)
 	: super(file, &romDataInfo)
 	, discType(DiscType::Unknown)
-	, sector_offset(0)
 	, jolietSVDType(JolietSVDType::None)
+	, sector_offset(0)
 	, boot_catalog_LBA(0)
 	, boot_platforms(0)
 	, s_udf_version(nullptr)