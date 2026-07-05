GerbilSoft/rom-properties

commit f9ec7d2d46af310700c018f4bf00d046aee9a72b

[librpbase] TextOut_text.cpp: Use libfmt instead of printf()-style functions.
In addition, libfmt has wrappers for thread-safe gmtime() and
localtime() on all platforms, which means we can get rid of our own
wrapper functions. Tested on godbolt.org, and fmt::localtime() expands
to localtime_r() on Linux. (On Windows, it should expand to
localtime_s(), but godbolt.org's MSVC isn't able to find fmt/format.h
for some reason...)

rp-libfmt.h: #include <fmt/chrono.h> for fmt::gmtime() and fmt::localtime().

TODO: Check if libfmt can handle strftime()-style time formatting.
<fmt/chrono.h> may help with this.

src/librpbase/TextOut_text.cpp
@@ -271,30 +271,25 @@ public:
 
 /**
  * Format an RFT_DATETIME field (or an `is_timestamp` column in RFT_LISTDATA).
- * @param buf		[out] Output buffer
- * @param size		[in] Size of `buf`
  * @param timestamp	[in] Timestamp to format
  * @param dtflags	[in] DateTimeFlags
- * @return 0 on success; non-zero on error.
+ * @return Formatted RFT_DATETIME on success; empty string on error.
  */
-static int formatDateTime(char *buf, size_t size, time_t timestamp, RomFields::DateTimeFlags dtflags)
+static string formatDateTime(time_t timestamp, RomFields::DateTimeFlags dtflags)
 {
-	struct tm tm_struct;
-	struct tm *ret;
-	if (dtflags & RomFields::RFT_DATETIME_IS_UTC) {
-		ret = gmtime_r(&timestamp, &tm_struct);
-	} else {
-		tzset();
-		ret = localtime_r(&timestamp, &tm_struct);
-	}
+	string s_ret;
 
-	if (!ret) {
-		// gmtime_r() or localtime_r() failed.
-		return -1;
+	struct tm tm_struct;
+	if (dtflags & RomFields::RFT_DATETIME_IS_UTC) {
+		tm_struct = fmt::gmtime(timestamp);
+	} else {
+		tzset(); // FIXME: Is this still needed?
+		tm_struct = fmt::localtime(timestamp);
 	}
 
 	if (likely(SystemRegion::getLanguageCode() != 0)) {
 		// Localized time format
+		// FIXME: Does fmt::format() support `struct tm`?
 		static constexpr char formats_strtbl[] =
 			"\0"		// [0] No date or time
 			"%x\0"		// [1] Date
@@ -313,15 +308,17 @@ static int formatDateTime(char *buf, size_t size, time_t timestamp, RomFields::D
 		const char *format = &formats_strtbl[formats_offtbl[offset]];
 		assert(format[0] != '\0');
 		if (likely(format[0] != '\0')) {
-			strftime(buf, size, format, &tm_struct);
-		} else {
-			return -2;
+			// TODO: fmt::format()? [may need <fmt/chrono.h>]
+			char buf[64];
+			strftime(buf, sizeof(buf), format, &tm_struct);
+			s_ret.assign(buf);
 		}
 	} else {
 		// LC_ALL=C
 		// Always use the same format regardless of platform.
 		// This is needed on Windows because LC_ALL doesn't affect
 		// MSVCRT's strftime().
+		// TODO: This might not be needed if fmt::format() is used...
 
 		// Month names for dates without years
 		static constexpr char months[12][4] = {
@@ -334,23 +331,23 @@ static int formatDateTime(char *buf, size_t size, time_t timestamp, RomFields::D
 			case RomFields::RFT_DATETIME_NO_YEAR:
 			default:
 				// Nothing to do...
-				return -3;
+				break;
 
 			case RomFields::RFT_DATETIME_HAS_DATE:
 				// Date, with year
-				snprintf(buf, size, "%04d/%02d/%02d",
+				s_ret = fmt::format(FSTR("{:0>4d}/{:0>2d}/{:0>2d}"),
 					tm_struct.tm_year + 1900, tm_struct.tm_mon + 1, tm_struct.tm_mday);
 				break;
 			case RomFields::RFT_DATETIME_HAS_TIME:
 			case RomFields::RFT_DATETIME_HAS_TIME | RomFields::RFT_DATETIME_NO_YEAR:
 				// Time
-				snprintf(buf, size, "%02d:%02d:%02d",
+				s_ret = fmt::format(FSTR("{:0>2d}:{:0>2d}:{:0>2d}"),
 					 tm_struct.tm_hour, tm_struct.tm_min, tm_struct.tm_sec);
 				break;
 			case RomFields::RFT_DATETIME_HAS_DATE |
 			     RomFields::RFT_DATETIME_HAS_TIME:
 				// Date and time (with year)
-				snprintf(buf, size, "%04d/%02d/%02d %02d:%02d:%02d",
+				s_ret = fmt::format(FSTR("{:0>4d}/{:0>2d}/{:0>2d} {:0>2d}:{:0>2d}:{:0>2d}"),
 					tm_struct.tm_year + 1900, tm_struct.tm_mon + 1, tm_struct.tm_mday,
 					tm_struct.tm_hour, tm_struct.tm_min, tm_struct.tm_sec);
 				break;
@@ -361,7 +358,7 @@ static int formatDateTime(char *buf, size_t size, time_t timestamp, RomFields::D
 					? months[tm_struct.tm_mon]
 					: "Unk";
 
-				snprintf(buf, size, "%s %02d", s_mon, tm_struct.tm_mday);
+				s_ret = fmt::format(FSTR("{:s} {:0>2d}"), s_mon, tm_struct.tm_mday);
 				break;
 			}
 
@@ -372,7 +369,7 @@ static int formatDateTime(char *buf, size_t size, time_t timestamp, RomFields::D
 					? months[tm_struct.tm_mon]
 					: "Unk";
 
-				snprintf(buf, size, "%s %02d %02d:%02d:%02d",
+				s_ret = fmt::format(FSTR("{:s} {:0>2d} {:0>2d}:{:0>2d}:{:0>2d}"),
 					s_mon, tm_struct.tm_mday,
 					tm_struct.tm_hour, tm_struct.tm_min, tm_struct.tm_sec);
 				break;
@@ -380,7 +377,7 @@ static int formatDateTime(char *buf, size_t size, time_t timestamp, RomFields::D
 		}
 	}
 
-	return 0;
+	return s_ret;
 }
 
 class ListDataField {
@@ -459,12 +456,11 @@ public:
 				if (unlikely(is_timestamp & 1)) {
 					// This is a timestamp column.
 					// Use a dummy timestamp to figure out the width.
-					char buf[128];
-					int ret = formatDateTime(buf, sizeof(buf), 0,
-						listDataDesc.col_attrs.dtflags);
-					if (likely(ret == 0)) {
+					const string s_timestamp = formatDateTime(
+						0, listDataDesc.col_attrs.dtflags);
+					if (likely(!s_timestamp.empty())) {
 						// Got the column width.
-						colSize[i] = std::max(colSize[i], utf8_disp_strlen(buf));
+						colSize[i] = std::max(colSize[i], utf8_disp_strlen(s_timestamp.c_str()));
 					}
 				}
 
@@ -480,12 +476,11 @@ public:
 				if (unlikely(is_timestamp & 1)) {
 					// This is a timestamp column.
 					// Use a dummy timestamp to figure out the width.
-					char buf[128];
-					int ret = formatDateTime(buf, sizeof(buf), 0,
-						listDataDesc.col_attrs.dtflags);
-					if (likely(ret == 0)) {
+					const string s_timestamp = formatDateTime(
+						0, listDataDesc.col_attrs.dtflags);
+					if (likely(!s_timestamp.empty())) {
 						// Got the column width.
-						colSize[i] = utf8_disp_strlen(buf);
+						colSize[i] = utf8_disp_strlen(s_timestamp.c_str());
 					}
 				}
 			}
@@ -658,13 +653,10 @@ public:
 							RomFields::TimeString_t time_string;
 							memcpy(time_string.str, jt->data(), 8);
 
-							char buf[128];
-							int ret = formatDateTime(buf, sizeof(buf),
+							str = formatDateTime(
 								static_cast<time_t>(time_string.time),
 								listDataDesc.col_attrs.dtflags);
-							if (likely(ret == 0)) {
-								str.assign(buf);
-							} else {
+							if (unlikely(str.empty())) {
 								str = C_("RomData", "Unknown");
 							}
 						} else {
@@ -751,13 +743,11 @@ public:
 			return os;
 		}
 
-		char str[128];
-		str[0] = '\0';
-		int ret = formatDateTime(str, sizeof(str),
+		const string s_timestamp = formatDateTime(
 			romField.data.date_time,
 			static_cast<RomFields::DateTimeFlags>(romField.flags));
-		if (likely(ret == 0)) {
-			os << str;
+		if (likely(!s_timestamp.empty())) {
+			os << s_timestamp;
 		} else {
 			os << C_("RomData", "Unknown");
 		}

src/rp-libfmt.h
@@ -13,6 +13,9 @@
 // Base libfmt header
 #include <fmt/format.h>
 
+// Chrono functions
+#include <fmt/chrono.h>
+
 // libfmt-7.1.0 is required.
 #if FMT_VERSION < 70100
 #  error libfmt-7.1.0 or later is required!

commit 0e0aebb52a94010b56583299b22de0bec4aac0bf

[librpbase] TextOut_text.cpp, formatDateTime(): Catch fmt::format_error and return an empty string.
aflplusplus-4.40c fuzzed a Unix timestamp of 2449958199039238587, which
caused fmt::gmtime() to throw an exception. I tested this value with
gmtime_r(), and it returned nullptr, so instead of switching back to
gmtime_r(), just catch the exception.

This fixes a regression from commit f9ec7d2d46af310700c018f4bf00d046aee9a72b.
([librpbase] TextOut_text.cpp: Use libfmt instead of printf()-style functions.)

Affects: v2.5 - v2.8

src/librpbase/TextOut_text.cpp
@@ -351,7 +351,12 @@ static string formatDateTime(time_t timestamp, RomFields::DateTimeFlags dtflags)
 
 	struct tm tm_struct;
 	if (dtflags & RomFields::RFT_DATETIME_IS_UTC) {
-		tm_struct = fmt::gmtime(timestamp);
+		try {
+			tm_struct = fmt::gmtime(timestamp);
+		} catch (const fmt::format_error&) {
+			// fmt::gmtime() failed.
+			return {};
+		}
 	} else {
 		tzset();
 		if (!localtime_r(&timestamp, &tm_struct)) {