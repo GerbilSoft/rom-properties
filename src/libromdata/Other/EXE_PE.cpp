GerbilSoft/rom-properties

commit 9c5167023ebc250bef56b5bb0bb7fe492b266869

Some more ubsan fixes.
NESPrivate::get_prg_rom_size(): Handle invalid NES 2.0 PRG ROM sizes when
using the exponent-multipler method. Also, support exponent-multipler for
CHR ROM sizes.

src/libromdata/Console/NES.cpp:342:24: runtime error: shift exponent 62 is too large for 32-bit type 'unsigned int'
src/libromdata/Console/NES.cpp:967:25: runtime error: shift exponent 55 is too large for 32-bit type 'unsigned int'

ICOPrivate::loadImage_Win3(): Make sure width is in range before calling abs().
src/librptexture/fileformat/ICO.cpp:728:34: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself

EXEPrivate::addFields_PE_Export(): Make sure the tables are 32-bit aligned.
src/libromdata/Other/EXE_PE.cpp:930:15: runtime error: load of misaligned address 0x561fb337b22f for type 'const uint32_t' (aka 'const unsigned int'), which requires 4 byte alignment
0x561fb337b22f: note: pointer points here
 00 00 00 00 00  2e 44 95 54 6a 25 fb 07  6a 25 fb 07 6a 25 fb 07  00 00 00 00 00 00 00 00  01 01 00
             ^

EXEPrivate::addFields_NE_Import(): Make sure FileAlnSzShftCnt is valid. (24 or less)
src/libromdata/Other/EXE_NE.cpp:906:61: runtime error: shift exponent 29952 is too large for 64-bit type 'size_t' (aka 'unsigned long')

src/libromdata/Other/EXE_PE.cpp
@@ -878,6 +878,10 @@ int EXEPrivate::addFields_PE_Export(void)
 
 	// Export Address Table
 	const uint32_t rvaExpAddrTbl = le32_to_cpu(pExpDirTbl->AddressOfFunctions);
+	if (!IS_ALIGNED_OFFSET(rvaExpAddrTbl, 4)) {
+		// Not aligned???
+		return -ENOENT;
+	}
 	uint32_t szExpAddrTbl = le32_to_cpu(pExpDirTbl->NumberOfFunctions);
 	// Ignore ordinals greater than 65535
 	szExpAddrTbl = std::min(65536-ordinalBase, szExpAddrTbl);
@@ -889,6 +893,10 @@ int EXEPrivate::addFields_PE_Export(void)
 
 	// Export Name Table
 	const uint32_t rvaExpNameTbl = le32_to_cpu(pExpDirTbl->AddressOfNames);
+	if (!IS_ALIGNED_OFFSET(rvaExpNameTbl, 4)) {
+		// Not aligned???
+		return -ENOENT;
+	}
 	const uint32_t szExpNameTbl = le32_to_cpu(pExpDirTbl->NumberOfNames);
 	static constexpr uint32_t szExpNameTbl_MAX = 16U * 1024U * 1024U;
 	assert(szExpNameTbl <= szExpNameTbl_MAX);
@@ -903,6 +911,10 @@ int EXEPrivate::addFields_PE_Export(void)
 
 	// Export Ordinal Table
 	const uint32_t rvaExpOrdTbl = le32_to_cpu(pExpDirTbl->AddressOfNameOrdinals);
+	if (!IS_ALIGNED_OFFSET(rvaExpOrdTbl, 4)) {
+		// Not aligned???
+		return -ENOENT;
+	}
 	const uint16_t *const expOrdTbl = reinterpret_cast<const uint16_t*>(
 		checkBounds(rvaExpOrdTbl, szExpNameTbl*sizeof(uint16_t)));
 	if (!expOrdTbl) {

src/libromdata/Console/NES.cpp
@@ -332,15 +332,20 @@ unsigned int NESPrivate::get_prg_rom_size(void) const
 			// NES 2.0 has an alternate method for indicating PRG ROM size,
 			// so we need to check for that.
 			if (likely((header.ines.nes2.banks_hi & 0x0F) != 0x0F)) {
-				// Standard method.
+				// Standard method
 				prg_rom_size = ((header.ines.prg_banks |
 						((header.ines.nes2.banks_hi & 0x0F) << 8))
 						* INES_PRG_BANK_SIZE);
 			} else {
 				// Alternate method: [EEEE EEMM] -> 2^E * (MM*2 + 1)
 				// TODO: Verify that this works.
-				prg_rom_size = (1U << (header.ines.prg_banks >> 2)) *
-					       ((header.ines.prg_banks & 0x03) + 1);
+				if ((header.ines.prg_banks >> 2) > 24) {
+					// Sanity check: Too big???
+					prg_rom_size = 0xFFFFFFFF;
+				} else {
+					prg_rom_size = (1U << (header.ines.prg_banks >> 2)) *
+					               ((header.ines.prg_banks & 0x03) + 1);
+				}
 			}
 			break;
 		}
@@ -373,9 +378,24 @@ unsigned int NESPrivate::get_chr_rom_size(void) const
 			break;
 
 		case NESPrivate::ROM_FORMAT_NES2:
-			chr_rom_size = ((header.ines.chr_banks |
-					((header.ines.nes2.banks_hi & 0xF0) << 4))
-					* INES_CHR_BANK_SIZE);
+			// NES 2.0 has an alternate method for indicating CHR ROM size,
+			// so we need to check for that.
+			if (likely((header.ines.nes2.banks_hi & 0xF0) != 0xF0)) {
+				// Standard method
+				chr_rom_size = ((header.ines.chr_banks |
+						((header.ines.nes2.banks_hi & 0xF0) << 4))
+						* INES_CHR_BANK_SIZE);
+			} else {
+				// Alternate method: [EEEE EEMM] -> 2^E * (MM*2 + 1)
+				// TODO: Verify that this works.
+				if ((header.ines.chr_banks >> 2) > 24) {
+					// Sanity check: Too big???
+					chr_rom_size = 0xFFFFFFFF;
+				} else {
+					chr_rom_size = (1U << (header.ines.chr_banks >> 2)) *
+					               ((header.ines.chr_banks & 0x03) + 1);
+				}
+			}
 			break;
 
 		case NESPrivate::ROM_FORMAT_TNES:
@@ -937,15 +957,20 @@ int NES::isRomSupported_static(const DetectInfo *info)
 				// so we need to check for that.
 				unsigned int prg_rom_size, chr_rom_size;
 				if (likely((inesHeader->nes2.banks_hi & 0x0F) != 0x0F)) {
-					// Standard method.
+					// Standard method
 					prg_rom_size = ((inesHeader->prg_banks |
 							((inesHeader->nes2.banks_hi & 0x0F) << 8))
 							* INES_PRG_BANK_SIZE);
 				} else {
 					// Alternate method: [EEEE EEMM] -> 2^E * (MM*2 + 1)
 					// TODO: Verify that this works.
-					prg_rom_size = (1U << (inesHeader->prg_banks >> 2)) *
-						((inesHeader->prg_banks & 0x03) + 1);
+					if ((inesHeader->prg_banks >> 2) > 24) {
+						// Sanity check: Too big???
+						prg_rom_size = 0xFFFFFFFF;
+					} else {
+						prg_rom_size = (1U << (inesHeader->prg_banks >> 2)) *
+						               ((inesHeader->prg_banks & 0x03) + 1);
+					}
 				}
 				chr_rom_size = ((inesHeader->chr_banks |
 						((inesHeader->nes2.banks_hi & 0xF0) << 4))

src/libromdata/Other/EXE_NE.cpp
@@ -894,6 +894,12 @@ int EXEPrivate::addFields_NE_Import(void)
 	};
 	std::unordered_set<std::pair<uint16_t, uint16_t>, hash2x16> ordinal_set, name_set;
 
+	const uint16_t FileAlnSzShftCnt = le16_to_cpu(hdr.ne.FileAlnSzShftCnt);
+	if (FileAlnSzShftCnt > 24) {
+		// Out of range?
+		return -EIO;
+	}
+
 	for (const auto &seg : NE_data.segment_table) {
 		if (seg.offset == 0) {
 			continue; // No data
@@ -903,7 +909,7 @@ int EXEPrivate::addFields_NE_Import(void)
 		}
 
 		// the logic for seg_size is from Wine's NE_LoadSegment
-		const size_t seg_offset = (size_t)le16_to_cpu(seg.offset) << le16_to_cpu(hdr.ne.FileAlnSzShftCnt);
+		const size_t seg_offset = static_cast<size_t>(le16_to_cpu(seg.offset)) << FileAlnSzShftCnt;
 		const size_t seg_size =
 			seg.filesz ? le16_to_cpu(seg.filesz) :
 			seg.memsz ? le16_to_cpu(seg.memsz) : 0x10000;

src/librptexture/fileformat/ICO.cpp
@@ -723,8 +723,12 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(int idx)
 	// Make sure width and height are valid.
 	// Height cannot be 0 or an odd number.
 	// NOTE: Negative height is allowed for "right-side up".
-	const unsigned int width = le32_to_cpu((unsigned int)(bih->biWidth));
 	const int orig_height = static_cast<int>(le32_to_cpu(bih->biHeight));
+	if (orig_height < -32768 || orig_height > 32768) {
+		// Out of range...
+		return {};
+	}
+	const int width = le32_to_cpu(bih->biWidth);
 	const unsigned int height = abs(orig_height);
 	if (width <= 0 || height == 0 || (height & 1)) {
 		// Invalid bitmap size.
@@ -744,7 +748,7 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(int idx)
 	// Row must be 32-bit aligned.
 	// FIXME: Including for 24-bit images?
 	const unsigned int bitcount = le16_to_cpu(bih->biBitCount);
-	unsigned int stride = width;
+	unsigned int stride = static_cast<unsigned int>(width);
 	switch (bitcount) {
 		default:
 			// Unsupported bitcount.
@@ -773,7 +777,8 @@ rp_image_const_ptr ICOPrivate::loadImage_Win3(int idx)
 	stride = ALIGN_BYTES(4, stride);
 
 	// Mask row is 1bpp and must also be 32-bit aligned.
-	unsigned int mask_stride = ALIGN_BYTES(4, ((width / 8) + ((width % 8) > 0)));
+	const unsigned int mask_stride = ALIGN_BYTES(4,
+		((static_cast<unsigned int>(width) / 8) + ((static_cast<unsigned int>(width) % 8) > 0)));
 
 	// Icon file (this->file for .ico; IResourceReader::open() for .exe/.dll)
 	IRpFilePtr f_icon;

commit e4c03910034ec7fcd7e90805b280a848a6e2cb58

[libromdata] EXEPrivate::addFields_PE_Export(): Revert an incorrect ubsan fix that broke the PE Exports tab.
This fixes a regression from commit 9c5167023ebc250bef56b5bb0bb7fe492b266869.
(Some more ubsan fixes.)

Fixes #498: Exports table disappeared from win32 shell ext page
Reported by @ksharperd.

Affects: v2.8

src/libromdata/Other/EXE_PE.cpp
@@ -877,45 +877,36 @@ int EXEPrivate::addFields_PE_Export(void)
 	};
 
 	// Export Address Table
+	// NOTE: rvaExpAddrTbl might not be 32-bit aligned!
 	const uint32_t rvaExpAddrTbl = le32_to_cpu(pExpDirTbl->AddressOfFunctions);
-	if (!IS_ALIGNED_OFFSET(rvaExpAddrTbl, 4)) {
-		// Not aligned???
-		return -ENOENT;
-	}
 	uint32_t szExpAddrTbl = le32_to_cpu(pExpDirTbl->NumberOfFunctions);
 	// Ignore ordinals greater than 65535
 	szExpAddrTbl = std::min(65536-ordinalBase, szExpAddrTbl);
-	const uint32_t *expAddrTbl = reinterpret_cast<const uint32_t*>(
+	const uint32_t *expAddrTbl = static_cast<const uint32_t*>(
 		checkBounds(rvaExpAddrTbl, szExpAddrTbl*sizeof(uint32_t)));
 	if (!expAddrTbl) {
 		return -ENOENT;
 	}
 
 	// Export Name Table
+	// NOTE: rvaExpNameTbl might not be 32-bit aligned!
 	const uint32_t rvaExpNameTbl = le32_to_cpu(pExpDirTbl->AddressOfNames);
-	if (!IS_ALIGNED_OFFSET(rvaExpNameTbl, 4)) {
-		// Not aligned???
-		return -ENOENT;
-	}
 	const uint32_t szExpNameTbl = le32_to_cpu(pExpDirTbl->NumberOfNames);
 	static constexpr uint32_t szExpNameTbl_MAX = 16U * 1024U * 1024U;
 	assert(szExpNameTbl <= szExpNameTbl_MAX);
 	if (szExpNameTbl > szExpNameTbl_MAX) {
 		return -ENOMEM;
 	}
-	const uint32_t *const expNameTbl = reinterpret_cast<const uint32_t*>(
+	const uint32_t *const expNameTbl = static_cast<const uint32_t*>(
 		checkBounds(rvaExpNameTbl, szExpNameTbl*sizeof(uint32_t)));
 	if (!expNameTbl) {
 		return -ENOENT;
 	}
 
 	// Export Ordinal Table
+	// NOTE: rvaExpOrdTbl might not be 16-bit aligned!
 	const uint32_t rvaExpOrdTbl = le32_to_cpu(pExpDirTbl->AddressOfNameOrdinals);
-	if (!IS_ALIGNED_OFFSET(rvaExpOrdTbl, 4)) {
-		// Not aligned???
-		return -ENOENT;
-	}
-	const uint16_t *const expOrdTbl = reinterpret_cast<const uint16_t*>(
+	const uint16_t *const expOrdTbl = static_cast<const uint16_t*>(
 		checkBounds(rvaExpOrdTbl, szExpNameTbl*sizeof(uint16_t)));
 	if (!expOrdTbl) {
 		return -ENOENT;