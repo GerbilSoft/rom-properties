5e53e4ec5d2d1ac86fb7c7a4b1a8bbb5e31fe765
David Korth <gerbilsoft@gerbilsoft.com>
2026-07-11 16:15:26 -0400

    [minizip-ng] Support using the system MiniZip-NG on Linux systems.
    MiniZip-NG 2.2.4 added UTF-8 to UTF-16 filename conversion on Windows,
    but that isn't relevant for Linux, so I'm not going to add a minimum
    version for now. I may add one later if issues are found.
    
    TODO: Add MiniZip-NG to AboutTab.

---
+++ cmake/libs/CheckMinizipNG.cmake
@@ -0,0 +1,40 @@
+# Check for minizip. (NOTE: minizip-ng *only*, not original minizip)
+# If zlib isn't found, extlib/zlib/ will be used instead.
+
+IF(NOT USE_INTERNAL_MINIZIP)
+	# Check for minizip-ng.
+	FIND_PACKAGE(MinizipNG)
+	IF(MinizipNG_FOUND)
+		# Found system minizip.
+		SET(HAVE_MinizipNG 1)
+	ELSE()
+		# System minizip was not found.
+		MESSAGE(STATUS "Using the internal copy of minizip-ng since a system version was not found.")
+		SET(USE_INTERNAL_MINIZIP ON CACHE BOOL "Use the internal copy of minizip-ng" FORCE)
+	ENDIF()
+ELSE()
+	MESSAGE(STATUS "Using the internal copy of minizip-ng.")
+ENDIF(NOT USE_INTERNAL_MINIZIP)
+
+IF(USE_INTERNAL_MINIZIP)
+	# Using the internal minizip library.
+	SET(MinizipNG_FOUND 1)
+	SET(HAVE_MinizipNG 1)
+	IF(WIN32)
+		# Using DLLs on Windows.
+		SET(USE_INTERNAL_MINIZIP_DLL ON)
+	ELSE()
+		# Using static linking on other systems.
+		SET(USE_INTERNAL_MINIZIP_DLL OFF)
+	ENDIF()
+	SET(MinizipNG_LIBRARY minizip-ng CACHE INTERNAL "minizip library" FORCE)
+	SET(MinizipNG_LIBRARIES ${MinizipNG_LIBRARY})
+	# FIXME: When was it changed from DIR to DIRS?
+	SET(MINIZIP_INCLUDE_DIR
+		${CMAKE_SOURCE_DIR}/extlib/minizip-ng
+		${CMAKE_BINARY_DIR}/extlib/minizip-ng
+		)
+	SET(MINIZIP_INCLUDE_DIRS ${ZLIB_INCLUDE_DIR})
+ELSE(USE_INTERNAL_MINIZIP)
+	SET(USE_INTERNAL_MINIZIP_DLL OFF)
+ENDIF(USE_INTERNAL_MINIZIP)
