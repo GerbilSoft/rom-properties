6373d7896e0b7f67a236f47500d8788292efa864
David Korth <gerbilsoft@gerbilsoft.com>
2026-07-13 19:46:07 -0400

    [libromdata] EXEPrivate::addFields_PE_PDB(): Clean up some of the code a bit.
    - Using a lambda function to get the IMAGE_DATA_DIRECTORY seems
      unnecessary. Also, set a pointer instead of copying the entire
      directory.
    
    - Use actual types instead of auto in a few places.
    
    - Don't use std::make_unique<> for pre-C++17 compatibility.

--- src/libromdata/Other/EXE_PE.cpp
+++ src/libromdata/Other/EXE_PE.cpp
@@ -1331,18 +1331,14 @@ int EXEPrivate::addFields_PE_PDB(void)
 	PE_data_t &PE_data = std::get<PE_data_t>(EXE_data);
 	constexpr uintptr_t win32_maxpath = 260; // max path define on windows, should be this for all dos like paths
 						 // (iirc nt paths are like 64k, but for pdbs this shooould be enough)
-	IMAGE_DATA_DIRECTORY debug_dir = [&]() -> IMAGE_DATA_DIRECTORY {
-		if (exeType == ExeType::PE) {
-			const auto &opthdr = hdr.pe.OptionalHeader.opt32;
-			return opthdr.DataDirectory[IMAGE_DATA_DIRECTORY_DEBUG_INFO];
-		} else /*if (exeType == ExeType::PE32PLUS)*/ {
-			const auto &opthdr = hdr.pe.OptionalHeader.opt64;
-			return opthdr.DataDirectory[IMAGE_DATA_DIRECTORY_DEBUG_INFO];
-		}
-		return {}; // meow shouldnt run here but in that case the next check should fail as size and va are zeroed
-	}();
+	const IMAGE_DATA_DIRECTORY *pDebugDir;
+	if (exeType == ExeType::PE) {
+		pDebugDir = &hdr.pe.OptionalHeader.opt32.DataDirectory[IMAGE_DATA_DIRECTORY_DEBUG_INFO];
+	} else /*if (exeType == ExeType::PE32PLUS)*/ {
+		pDebugDir = &hdr.pe.OptionalHeader.opt64.DataDirectory[IMAGE_DATA_DIRECTORY_DEBUG_INFO];
+	}
 
-	auto safe_read_vmem = [&](uint32_t va, uint32_t size, void* to) {
+	auto safe_read_vmem = [&](uint32_t va, uint32_t size, void* to) -> bool {
 		if (file && file->isOpen()){
 			auto addr = pe_vaddr_to_paddr(va,size);
 			file->seek(addr);
@@ -1353,13 +1349,13 @@ int EXEPrivate::addFields_PE_PDB(void)
 		return false;
 	};
 
-	auto size = le32_to_cpu(debug_dir.Size);
-	if (size && debug_dir.VirtualAddress && (size/sizeof(IMAGE_DEBUG_DIRECTORY)) < 16) { // cap to a reasonable limit
+	uint32_t size = le32_to_cpu(pDebugDir->Size);
+	if (size && pDebugDir->VirtualAddress != 0 && (size/sizeof(IMAGE_DEBUG_DIRECTORY)) < 16) { // cap to a reasonable limit
 		vector<IMAGE_DEBUG_DIRECTORY> debug_ents( size/sizeof(IMAGE_DEBUG_DIRECTORY) );
-		if (safe_read_vmem(le32_to_cpu(debug_dir.VirtualAddress), size, debug_ents.data())) {
+		if (safe_read_vmem(le32_to_cpu(pDebugDir->VirtualAddress), size, debug_ents.data())) {
 			// oki we read all of them safely in
 			bool found_cv = false;
-			for (const auto& dir : debug_ents) {
+			for (const IMAGE_DEBUG_DIRECTORY &dir : debug_ents) {
 				// sane bounds, cv data +  path + whatever-they-might-add-in-the-future
 				if (le32_to_cpu(dir.Type) == IMAGE_DEBUG_TYPE_CODEVIEW && le32_to_cpu(dir.SizeOfData) <=  sizeof(CODEVIEW_INFO_PDB70) + win32_maxpath) {
 					// aand we have a proper codeview entry
@@ -1370,7 +1366,7 @@ int EXEPrivate::addFields_PE_PDB(void)
 					found_cv = true;
 
 					fields.addTab("PDB");
-					auto cv_timestamp = le32_to_cpu(dir.TimeDateStamp);
+					uint32_t cv_timestamp = le32_to_cpu(dir.TimeDateStamp);
 					if (cv_timestamp != 0) {
 						fields.addField_dateTime("CodeView timestamp",
 							static_cast<time_t>(cv_timestamp),
@@ -1380,7 +1376,7 @@ int EXEPrivate::addFields_PE_PDB(void)
 
 					CODEVIEW_INFO_PDB70 cv {};
 					if (safe_read_vmem(le32_to_cpu(dir.AddressOfRawData), sizeof(CODEVIEW_INFO_PDB70),&cv)){
-						auto symsrv_path = fmt::format(
+						string symsrv_path = fmt::format(
 							"{:08X}{:04X}{:04X}"
 							"{:02X}{:02X}{:02X}{:02X}"
 							"{:02X}{:02X}{:02X}{:02X}"
@@ -1399,12 +1395,12 @@ int EXEPrivate::addFields_PE_PDB(void)
 						uint32_t str_addie = le32_to_cpu(dir.AddressOfRawData) + offsetof(CODEVIEW_INFO_PDB70, ImageName);
 						// - 1, null term, we ensure it ourselves
 						uint32_t strlen_chars = le32_to_cpu(dir.SizeOfData) - offsetof(CODEVIEW_INFO_PDB70, ImageName) - 1;
-						auto path_buf = std::make_unique<char[]>(strlen_chars + 1);
+						unique_ptr<char[]> path_buf(new char[strlen_chars + 1]);
 						if (safe_read_vmem(str_addie, strlen_chars, &path_buf[0])) {
 							path_buf[strlen_chars] = '\0'; // ensure nullterm
 							fields.addField_string("PDB Path", &path_buf[0]);
 							char* last_path_component = &path_buf[0];
-							for (auto* cur = &path_buf[strlen_chars]; cur != &path_buf[0]; cur--) {
+							for (char *cur = &path_buf[strlen_chars]; cur != &path_buf[0]; cur--) {
 								if (*cur == '\\'){
 									last_path_component = cur + 1;
 									break;
