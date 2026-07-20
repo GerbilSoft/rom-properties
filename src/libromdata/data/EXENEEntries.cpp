GerbilSoft/rom-properties

commit f77b5d7faba8a5dcd28571784f57c9f7b91e086a

[libromdata] EXENEEntries.cpp: Switch back to bsearch().
std::lower_bound() seemed better, but it doesn't actually do a binary
search. When testing with SKI.EXE (the original 16-bit SkiFree),
the std::lower_bound() version did 3,462 string comparisons, whereas
the bsearch() version only did 209 string comparisons.

std::lower_bound() *does* require sorted arrays, so I'm not sure
why it's not doing a binary search...

Also, when implementing html_entities.cpp, I found that std::lower_bound()
was failing. It was claiming some issue with the sort order and throwing
an exception. The bsearch() version works fine, though.

This will eventually revert all of commit 7c7cdd278986163ba6a5ef6ed6c07682296526ea
(Use std::lower_bound() instead of bsearch().)
...though EXENEEntries.cpp was added *after* this commit, so it
always used std::lower_bound().

src/libromdata/data/EXENEEntries.cpp
@@ -15,8 +15,9 @@
 // C includes
 #include "tcharx.h"
 
-// C++ STL classes
-#include <algorithm>
+// C includes (C++ namespace)
+#include <cstring>
+#include <cstdlib>
 
 namespace LibRomData { namespace EXENEEntries {
 
@@ -42,22 +43,46 @@ struct OrdinalNameTable {
  */
 const char *lookup_ordinal(const char *modname, uint16_t ordinal)
 {
-	static const OrdinalNameTable *const pEntries_end = &entries[ARRAY_SIZE(entries)];
-	auto it = std::lower_bound(entries, pEntries_end, modname,
-		[](const OrdinalNameTable &lhs, const char *rhs) noexcept -> bool {
-			return (strncasecmp(lhs.modname, rhs, 8) < 0);
-		});
-	if (it == pEntries_end || strncasecmp(it->modname, modname, 8) != 0)
-		return nullptr;
+	OrdinalNameTable key;
+	strncpy(key.modname, modname, sizeof(key.modname));
+	key.table = nullptr;
+	key.count = 0;
 
-	const OrdinalName *const pOrdinals_end = &it->table[it->count];
-	auto it2 = std::lower_bound(it->table, pOrdinals_end, ordinal,
-		[](OrdinalName lhs, uint16_t rhs) noexcept -> bool {
-			return (lhs.ordinal < rhs);
+	void *ptr = bsearch(&key, entries,
+		ARRAY_SIZE(entries), sizeof(entries[0]),
+		[](const void *a, const void *b) -> int
+		{
+			const OrdinalNameTable *const pa = static_cast<const OrdinalNameTable*>(a);
+			const OrdinalNameTable *const pb = static_cast<const OrdinalNameTable*>(b);
+			return strncasecmp(pa->modname, pb->modname, 8);
 		});
-	if (it2 == pOrdinals_end || it2->ordinal != ordinal)
+	if (!ptr) {
 		return nullptr;
-	return (likely(it2->offset != 0) ? &EXENEEntries_strtbl[it2->offset] : nullptr);
+	}
+
+	const OrdinalNameTable *const pNameTbl = static_cast<const OrdinalNameTable*>(ptr);
+	const OrdinalName key2 = {ordinal, 0};
+
+	ptr = bsearch(&key2, pNameTbl->table,
+		pNameTbl->count, sizeof(pNameTbl->table[0]),
+		[](const void *a, const void *b) -> int
+		{
+			const OrdinalName *const pa = static_cast<const OrdinalName*>(a);
+			const OrdinalName *const pb = static_cast<const OrdinalName*>(b);
+			// TODO: C++20 spaceship operator?
+			if (pa->ordinal < pb->ordinal) {
+				return -1;
+			} else if (pa->ordinal > pb->ordinal) {
+				return 1;
+			}
+			return 0;
+		});
+	if (!ptr) {
+		return nullptr;
+	}
+
+	const OrdinalName *const pNameInfo = static_cast<const OrdinalName*>(ptr);
+	return (likely(pNameInfo->offset != 0) ? &EXENEEntries_strtbl[pNameInfo->offset] : nullptr);
 }
 
 } } // namespace LibRomData::EXENEEntries

commit 609e33d10382cdbb72b76ea4e3862910e6c5e821

[libromdata] EXENEEntries.cpp, lookup_ordinal(): Don't use strncpy() here.
key.modname is *not* NULL-terminated, and MSVC's strncpy_s() doesn't
like this. Do some manual shenanigans with strlen() and memcpy()
instead.

Fixes this assertion:
[----------] 88 tests from EXE_Notepad/RomHeaderTest
[ RUN      ] EXE_Notepad/RomHeaderTest.Text/notepad_1_01_exe
minkernel\crts\ucrt\inc\corecrt_internal_string_templates.h(218) : Assertion failed: (L"Buffer is too small" && 0)

In the Release build, it still crashes, but without printing the
assertion failure message.

This fixes a regression from commit f77b5d7faba8a5dcd28571784f57c9f7b91e086a.
([libromdata] EXENEEntries.cpp: Switch back to bsearch().)

src/libromdata/data/EXENEEntries.cpp
@@ -43,10 +43,18 @@ struct OrdinalNameTable {
  */
 const char *lookup_ordinal(const char *modname, uint16_t ordinal)
 {
+	// NOTE: key.modname is *not* NULL-terminated.
+	// MSVC's strncpy_s() doesn't like this, so we'll have to
+	// do some manual shenanigans.
 	OrdinalNameTable key;
-	strncpy(key.modname, modname, sizeof(key.modname));
-	key.table = nullptr;
-	key.count = 0;
+	memset(&key, 0, sizeof(key));
+	size_t modname_len = strlen(modname);
+	assert(modname_len <= sizeof(key.modname));
+	if (modname_len > sizeof(key.modname)) {
+		// Not possible to match a module name longer than 8 chars.
+		return nullptr;
+	}
+	memcpy(key.modname, modname, modname_len);
 
 	void *ptr = bsearch(&key, entries,
 		ARRAY_SIZE(entries), sizeof(entries[0]),