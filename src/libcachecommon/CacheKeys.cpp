GerbilSoft/rom-properties

commit 93074b497b71607a6c45ae233fc7fe6cac7c4304

[cmake] gcc.cmake: Add -Wnrvo and fix NRVO issues for most libraries and the GTK and KDE UI frontends.
[librptexture] NOTE: Need to reset the rp_image_ptr if it's allocated
but something failed later on. This ends up making the generated code
even larger than before fixing NRVO, but that might be fixable later.

(Also affects RomDataPtr in some cases.)

rp_image::flip(): Return if flipimg could not be allocated.

Change some `str` return variable names to `s_ret`.
In particular, for RomFields age ratings functions.

Code size differences: (64-bit Gentoo Linux, gcc-16.1.1_p20260613, release build, no LTO)

   text    data     bss     dec     hex filename
2896134   93112     896 2990142  2da03e libromdata.so.9.1 [before]
2887644   93112     896 2981652  2d7f14 libromdata.so.9.1 [after]
  -8490       0       0   -8490   -212a Difference

   text    data     bss     dec     hex filename
 643136   29544      88  672768   a4400 rom-properties-kf6.so [before]
 643520   29544      88  673152   a4580 rom-properties-kf6.so [after]
   +384       0       0    +384    +180 Difference

   text    data     bss     dec     hex filename
 499429   10984    1752  512165   7d0a5 rom-properties-gtk4.so
 501516   10984    1752  514252   7d8cc rom-properties-gtk4.so
  +2087       0       0   +2087    +827 Difference

src/libcachecommon/CacheKeys.cpp
@@ -312,11 +312,13 @@ int filterCacheKey(wchar_t *pCacheKey)
  */
 string getCacheFilename(const char *pCacheKey)
 {
+	string cacheFilename;
+
 	assert(pCacheKey != nullptr);
 	assert(pCacheKey[0] != '\0');
 	if (!pCacheKey || pCacheKey[0] == '\0') {
 		// No cache key...
-		return {};
+		return cacheFilename;
 	}
 
 	// Filter the cache key.
@@ -324,11 +326,8 @@ string getCacheFilename(const char *pCacheKey)
 	int ret = filterCacheKey(filteredCacheKey);
 	if (ret != 0) {
 		// Invalid cache key.
-		return {};
+		return cacheFilename;
 	}
-
-	// Cache filename in the user's directory.
-	string cacheFilename_user;
 
 	// Make sure the cache directory is initialized.
 	// NOTE: May be empty if the cache directory isn't
@@ -337,37 +336,39 @@ string getCacheFilename(const char *pCacheKey)
 	if (!cache_dir.empty()) {
 		// Get the cache filename.
 		// This is the cache directory plus the cache key.
-		cacheFilename_user = cache_dir;
-		if (cacheFilename_user.at(cacheFilename_user.size()-1) != DIR_SEP_CHR) {
-			cacheFilename_user += DIR_SEP_CHR;
+		cacheFilename.assign(cache_dir);
+		if (cacheFilename.at(cacheFilename.size()-1) != DIR_SEP_CHR) {
+			cacheFilename += DIR_SEP_CHR;
 		}
-		cacheFilename_user += filteredCacheKey;
+		cacheFilename += filteredCacheKey;
+
+		// Verify that the cache file exists in the user's cache directory.
+		if (!access(cacheFilename.c_str(), R_OK)) {
+			// Cache file exists.
+			return cacheFilename;
+		}
 	}
 
 #ifdef DIR_INSTALL_CACHE
+	// File is not present in the user's cache directory.
 	// If the requested file is in the system-wide cache directory,
-	// but is not in the user's cache directory, use the system-wide
-	// version. This is useful in cases where the thumbnailer cannot
-	// download files, e.g. bubblewrap.
-	string cacheFilename_sys = DIR_INSTALL_CACHE;
-	if (cacheFilename_sys.at(cacheFilename_sys.size()-1) != DIR_SEP_CHR) {
-		cacheFilename_sys += DIR_SEP_CHR;
+	// use that version. This is useful in cases where the thumbnailer
+	// cannot download files, e.g. bubblewrap.
+	cacheFilename.assign(DIR_INSTALL_CACHE);
+	if (cacheFilename.at(cacheFilename.size()-1) != DIR_SEP_CHR) {
+		cacheFilename += DIR_SEP_CHR;
 	}
-	cacheFilename_sys += filteredCacheKey;
+	cacheFilename += filteredCacheKey;
 
-	if (!access(cacheFilename_sys.c_str(), R_OK)) {
+	if (!access(cacheFilename.c_str(), R_OK)) {
 		// File is in the system-wide cache.
-		if (!cacheFilename_user.empty() && !access(cacheFilename_user.c_str(), R_OK)) {
-			// File is also in the user's cache.
-			// User version overrides the system version.
-			return cacheFilename_user;
-		}
-		// File is not in the user's cache.
-		return cacheFilename_sys;
+		return cacheFilename;
 	}
 #endif /* DIR_INSTALL_CACHE */
 
-	return cacheFilename_user;
+	// Unable to retrieve a cache filename...
+	cacheFilename.clear();
+	return cacheFilename;
 }
 
 #ifdef _WIN32

commit 10a9c10b9beb1aa9f7d27f0c6db70321b86c762d

[libcachecommon] CacheKeys.cpp, getCacheFilename(): Return filenames in the user cache directory *without* verifying that they exist.
The file verification is only needed for the system-wide cache directory.
Checking the user cache directory prevents rp-download from downloading
the image if it's not found, since rp-download will think the cache key
is invalid if it gets an empty string back from this function.

This fixes a regression from commit 93074b497b71607a6c45ae233fc7fe6cac7c4304.
([cmake] gcc.cmake: Add -Wnrvo and fix NRVO issues for most libraries and the GTK and KDE UI frontends.)

src/libcachecommon/CacheKeys.cpp
@@ -357,33 +357,27 @@ string getCacheFilename(const char *pCacheKey)
 			cacheFilename += DIR_SEP_CHR;
 		}
 		cacheFilename += filteredCacheKey;
-
-		// Verify that the cache file exists in the user's cache directory.
-		if (!access(cacheFilename.c_str(), R_OK)) {
-			// Cache file exists.
-			return cacheFilename;
-		}
 	}
 
 #ifdef DIR_INSTALL_CACHE
-	// File is not present in the user's cache directory.
 	// If the requested file is in the system-wide cache directory,
-	// use that version. This is useful in cases where the thumbnailer
-	// cannot download files, e.g. bubblewrap.
-	cacheFilename.assign(DIR_INSTALL_CACHE);
-	if (cacheFilename.at(cacheFilename.size()-1) != DIR_SEP_CHR) {
-		cacheFilename += DIR_SEP_CHR;
+	// but not the user's cache directory, use the one in the system-wide
+	// directory. This is useful in cases where the thumbnailer cannot
+	// download files, e.g. bubblewrap.
+	string cacheFilename_sys = DIR_INSTALL_CACHE;
+	if (cacheFilename_sys.at(cacheFilename_sys.size()-1) != DIR_SEP_CHR) {
+		cacheFilename_sys += DIR_SEP_CHR;
 	}
-	cacheFilename += filteredCacheKey;
+	cacheFilename_sys += filteredCacheKey;
 
-	if (!access(cacheFilename.c_str(), R_OK)) {
+	if (!access(cacheFilename_sys.c_str(), R_OK)) {
 		// File is in the system-wide cache.
+		cacheFilename = std::move(cacheFilename_sys);
 		return cacheFilename;
 	}
 #endif /* DIR_INSTALL_CACHE */
 
-	// Unable to retrieve a cache filename...
-	cacheFilename.clear();
+	// Return the filename in the user's cache directory.
 	return cacheFilename;
 }
 