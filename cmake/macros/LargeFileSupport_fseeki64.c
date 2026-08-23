13e43d6d4b961c9b44655fffb15f883ceb10cc57
David Korth <gerbilsoft@gerbilsoft.com>
2026-08-22 20:20:29 -0400

    [cmake] LargeFileSupport_fseeki64.c: Also specify stdin instead of NULL.
    MSVCRT doesn't crash with NULL here, but let's use stdin for consistency
    with the non-MSVCRT version.

--- cmake/macros/LargeFileSupport_fseeki64.c
+++ cmake/macros/LargeFileSupport_fseeki64.c
@@ -9,7 +9,7 @@ int main(int argc, const char **argv)
 {
 	// _fseeki64() and _ftelli64() use __int64.
 	// NOTE: __int64 is an MSVC-proprietary type.
-	__int64 offset = _ftelli64(NULL);
-	_fseeki64(NULL, offset, SEEK_SET);
+	__int64 offset = _ftelli64(stdin);
+	_fseeki64(stdin, offset, SEEK_SET);
 	return 0;
 }
